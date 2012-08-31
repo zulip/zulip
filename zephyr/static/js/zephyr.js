cd5b623cfd6140695f7fda3e369de6703679401e
Luke Faraone <lfaraone@humbughq.com>
2012-08-31 16:50:24 -0400

    Reply to the recipient instead of the sender if replying to my own personal
    Previously, if hamlet wrote to iago, and then hit "r" on his own message,
    the form would prompt hamlet to reply to himself. Now, Humbug will DTRT and
    prefill the form with iago instead.
    
    (imported from commit cb3260d1d0bc89b184dac84ebf1e5642d0bc1606)

--- zephyr/static/js/zephyr.js
+++ zephyr/static/js/zephyr.js
@@ -146,7 +146,12 @@ $(document).keydown(function(event) {
                 $("#new_zephyr").focus();
                 $("#new_zephyr").select();
             } else { // No instance, must be a personal
-                prepare_personal(parent.find("span.zephyr_sender").text());
+
+                var recipient = parent.find("span.zephyr_sender").text();
+                if (recipient == username) { // that is, we sent the original message
+                    recipient = parent.find("span.zephyr_personal_recipient").text();
+                }
+                prepare_personal(recipient);
                 $("#new_personal_zephyr").focus();
                 $("#new_personal_zephyr").select();
             }
