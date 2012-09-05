ebff1bc830a108d4224d5d1cdaa6b553cb7eabbf
Luke Faraone <lfaraone@humbughq.com>
2012-09-05 10:20:39 -0400

    Don't show the user's password when registering.
    (imported from commit 9268c07560de744abdedf11b8d39cd8045baeff9)

--- zephyr/forms.py
+++ zephyr/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class RegistrationForm(forms.Form):
     username = forms.CharField(max_length=100)
-    password = forms.CharField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput, max_length=100)
