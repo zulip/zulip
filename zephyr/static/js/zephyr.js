1211995d2ed8e9dad725dd024c964f0279d4130e
Keegan McAllister <keegan@humbughq.com>
2012-09-07 14:18:34 -0400

    Fix indentation
    (imported from commit 8f4880ceaa2e84fa842ba195ebe40800fc168eab)

--- zephyr/static/js/zephyr.js
+++ zephyr/static/js/zephyr.js
@@ -19,27 +19,27 @@ $(function () {
 });
 
 $.ajaxSetup({
-     beforeSend: function (xhr, settings) {
-         function getCookie(name) {
-             var cookieValue = null;
-             if (document.cookie && document.cookie != '') {
-                 var cookies = document.cookie.split(';');
-                 for (var i = 0; i < cookies.length; i++) {
-                     var cookie = jQuery.trim(cookies[i]);
-                     // Does this cookie string begin with the name we want?
-                 if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                     cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                     break;
-                 }
-             }
-         }
-         return cookieValue;
-         }
-         if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-             // Only send the token to relative URLs i.e. locally.
-             xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-         }
-     }
+    beforeSend: function (xhr, settings) {
+        function getCookie(name) {
+            var cookieValue = null;
+            if (document.cookie && document.cookie != '') {
+                var cookies = document.cookie.split(';');
+                for (var i = 0; i < cookies.length; i++) {
+                    var cookie = jQuery.trim(cookies[i]);
+                    // Does this cookie string begin with the name we want?
+                    if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                    }
+                }
+            }
+            return cookieValue;
+        }
+        if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+            // Only send the token to relative URLs i.e. locally.
+            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+        }
+    }
 });
 
 function sub(zephyr_class) {
@@ -196,10 +196,10 @@ var goto_pressed = false;
 
 $(function () {
     $('input, textarea, button').focus(function () {
-          allow_hotkeys = false;
+        allow_hotkeys = false;
     });
     $('input, textarea, button').blur(function () {
-          allow_hotkeys = true;
+        allow_hotkeys = true;
     });
 });
 
