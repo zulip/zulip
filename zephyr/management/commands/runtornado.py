7d51dfc0a729ceacf2c105d442e423633b273155
Keegan McAllister <keegan@humbughq.com>
2012-09-07 13:52:07 -0400

    Remove dead function in runtornado.py
    The code is buggy; the variable 'escape' is undefined.  Right now we aren't
    using this code because we bypass Tornado for login.
    
    (imported from commit 91cf6e8fc9477039c8847c7e9501d645993278b8)

--- zephyr/management/commands/runtornado.py
+++ zephyr/management/commands/runtornado.py
@@ -133,21 +133,6 @@ class AsyncDjangoHandler(tornado.web.RequestHandler, base.BaseHandler):
         self.initLock.release()
         self._auto_finish = False
 
-    def prepare(func):
-        """Patches the Cookie header in the Tornado request to fulfull
-        Django's strict string-type cookie policy"""
-        def inner_func(self,**kwargs):
-            if u'Cookie' in self.request.headers:
-                raw_cookie = self.request.headers[u'Cookie']
-                if isinstance(raw_cookie, unicode):
-                    if hasattr(escape, "native_str"):
-                        self.request.headers[u'Cookie'] = escape.native_str(raw_cookie)
-                    else:
-                        print "Method 'native_str' in module 'escape' not found."
-                        self.request.headers[u'Cookie'] = str(raw_cookie)
-            return func(self)
-        return inner_func
-
     def get(self):
         from tornado.wsgi import WSGIContainer
         from django.core.handlers.wsgi import WSGIRequest
