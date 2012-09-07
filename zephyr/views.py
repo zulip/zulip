5a38fd2c485ce2472b84691c622f41635a8e140e
Keegan McAllister <keegan@humbughq.com>
2012-09-07 13:41:47 -0400

    Import something needed by @require_post decorator
    (imported from commit a2537ad75ae6e8eeb3e543c425b14214c03775b0)

--- zephyr/views.py
+++ zephyr/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.shortcuts import render
