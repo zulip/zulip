2793ec15fdb7c71c1be281ef1fd56b385a449aa5
Tim Abbott <tabbott@humbughq.com>
2012-09-14 13:22:56 -0400

    Use longer timeouts for sqlite lock contention.
    (imported from commit 34fcc295b032d93ec58435ad18baa790ee8d0cfe)

--- humbug/settings.py
+++ humbug/settings.py
@@ -18,6 +18,9 @@ DATABASES = {
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'OPTIONS': {
+            'timeout': 20,
+        },
     },
     'mysql': {
         'ENGINE': 'django.db.backends.mysql',
