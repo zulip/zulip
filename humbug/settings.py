af941e7779611dd1a86a005e18fb7c31f318e3b6
Keegan McAllister <keegan@humbughq.com>
2012-09-18 13:41:01 -0400

    settings.py: Disable error email to admins
    Sending email was hanging Tornado's event loop.
    
    (imported from commit ab0ec21fa886e68a6acb21b6ee8e036265ce6f08)

--- humbug/settings.py
+++ humbug/settings.py
@@ -139,25 +139,6 @@ INSTALLED_APPS = (
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
 }
 
 ACCOUNT_ACTIVATION_DAYS=7
