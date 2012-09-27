b43dd303b6ce272cb194e2f6c12c920895628067
Tim Abbott <tabbott@humbughq.com>
2012-09-27 17:37:05 -0400

    Use tornado for new /api/get_updates query.
    (imported from commit 84b85fc5bb0de4144fde00ea13d14f74b1dd9bd1)

--- zephyr/management/commands/runtornado.py
+++ zephyr/management/commands/runtornado.py
@@ -90,6 +90,7 @@ class Command(BaseCommand):
             try:
                 # Application is an instance of Django's standard wsgi handler.
                 application = web.Application([(r"/get_updates", AsyncDjangoHandler),
+                                               (r"/api/get_updates", AsyncDjangoHandler),
                                                (r".*", FallbackHandler, dict(fallback=django_app)),
                                                ], debug=django.conf.settings.DEBUG)
 
