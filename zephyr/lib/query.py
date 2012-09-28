d91ec7056fc16ff15db56a91f2679bc24f0bac5a
Keegan McAllister <keegan@humbughq.com>
2012-09-28 19:33:15 -0400

    Add a Django query helper
    (imported from commit cf1f87eb4fb2b6f44ec0ebafc4cad8dcc9bc5e5c)

---
+++ zephyr/lib/query.py
@@ -0,0 +1,10 @@
+def last_n(n, query_set):
+    """Get the last n results from a Django QuerySet, in a semi-efficient way.
+       Returns a list."""
+
+    # We don't use reversed() because we would get a generator,
+    # which causes bool(last_n(...)) to be True always.
+
+    xs = list(query_set.reverse()[:n])
+    xs.reverse()
+    return xs
