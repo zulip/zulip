4dcde9b11a5f049ebcd414d1ce3c26bd49e70d51
Luke Faraone <lfaraone@humbughq.com>
2012-10-02 15:20:09 -0400

    Unescape Zephyr content before wrapping.
    Now we no longer have weird wrapping based on the length of escaped lines.
    
    (imported from commit fa2c9bc0903e22858acc2af4cd6be34e28ea25db)

--- zephyr/zephyr_mirror.py
+++ zephyr/zephyr_mirror.py
@@ -260,9 +260,12 @@ def send_zephyr(message):
     if ' dot ' in zsig:
         print "ERROR!  Couldn't compute zsig for %s!" % (message["sender_email"])
         return
+
+    content = message["content"]
+    cleaned_content = content.replace('&lt;','<').replace('&gt;','>').replace('&amp;', '&')
     wrapped_content = "\n".join("\n".join(textwrap.wrap(line))
-            for line in message["content"].split("\n"))
-    wrapped_content = wrapped_content.replace('&lt;','<').replace('&gt;','>').replace('&amp;', '&')
+            for line in cleaned_content.split("\n"))
+
     print "Sending message from %s humbug=>zephyr at %s" % (message["sender_email"], datetime.datetime.now())
     if message['type'] == "class":
         zeph = zephyr.ZNotice(sender=message["sender_email"].replace("mit.edu", "ATHENA.MIT.EDU"),
