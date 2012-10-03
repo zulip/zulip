120d0aa99d1c24c3a40bf7c86df1d010c076cc96
Luke Faraone <lfaraone@humbughq.com>
2012-10-03 17:43:25 -0400

    Validate the register.html page.
    (imported from commit 10ce8b637ae40ab733c01c2adbf6a2ad61a85d82)

--- zephyr/static/js/signup.js
+++ zephyr/static/js/signup.js
@@ -10,10 +10,23 @@ function validate_email_domain(value, element, param) {
     return $.inArray(domain, disallowed_domains) !== -1;
 }
 
+
 $.validator.addMethod("fromDomain", validate_email_domain,
     "Please use your company email address to sign up. Otherwise, we won’t be able to connect you with your coworkers.");
 
 $(function () {
+    $('#registration').validate({
+        errorElement: "p",
+        errorPlacement: function (error, element) {
+            console.log('hi');
+            error.appendTo(element.parent()).addClass('help-inline');
+            element.parent().parent().removeClass('success').addClass('error');
+        },
+        success: function (label) {
+            label.parent().parent().removeClass('error').addClass('success');
+        },
+    });
+
     $("#email_signup").validate({
         rules: {
             email: {
@@ -21,8 +34,16 @@ $(function () {
                 email: true
             }
         },
-        errorLabelContainer: "#errors",
-        errorElement: "div",
-        errorClass: "alert"
+        errorElement: "p",
+        errorClass: "validation-failed",
+        errorPlacement: function (error, element) {
+            $('#errors').empty();
+            element.parent().parent().removeClass('success').addClass('error');
+            error.appendTo("#errors")
+                 .addClass("text-error")
+        },
+        success: function (label) {
+            label.parent().parent().removeClass('error').addClass('success');
+        },
     });
 });
