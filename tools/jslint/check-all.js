367feda74753c71d7b1871a9a84f40f2499bc3b0
Jessica McKellar <jesstess@humbughq.com>
2012-10-05 16:29:59 -0400

    check-all: add new viewport globals.
    (imported from commit e163dc07ad196ffd2731f7ccc67bfaada8e8580a)

--- tools/jslint/check-all.js
+++ tools/jslint/check-all.js
@@ -42,6 +42,7 @@ var globals =
     + ' select_zephyr select_zephyr_by_id'
     + ' scroll_to_selected select_and_show_by_id'
     + ' selected_zephyr selected_zephyr_id'
+    + ' at_top_of_viewport at_bottom_of_viewport'
 
     ;
 
