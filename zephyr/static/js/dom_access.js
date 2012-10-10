0c89972c545a4ad75b9d383360e4373ad3a07a6b
Tim Abbott <tabbott@humbughq.com>
2012-10-10 10:24:02 -0400

    Do zephyr=>message rename for local variables in dom_access.js.
    (imported from commit f6160f56730301c9e8e00e5edf79914d42eceeaf)

--- zephyr/static/js/dom_access.js
+++ zephyr/static/js/dom_access.js
@@ -1,13 +1,13 @@
-function get_next_visible(zephyr_row) {
-    if (zephyr_row === undefined)
+function get_next_visible(message_row) {
+    if (message_row === undefined)
         return [];
-    return zephyr_row.nextAll('.zephyr_row:first');
+    return message_row.nextAll('.zephyr_row:first');
 }
 
-function get_prev_visible(zephyr_row) {
-    if (zephyr_row === undefined)
+function get_prev_visible(message_row) {
+    if (message_row === undefined)
         return [];
-    return zephyr_row.prevAll('.zephyr_row:first');
+    return message_row.prevAll('.zephyr_row:first');
 }
 
 function get_first_visible() {
@@ -18,12 +18,12 @@ function get_last_visible() {
     return $('.focused_table .zephyr_row:last');
 }
 
-function get_id(zephyr_row) {
-    return zephyr_row.attr('zid');
+function get_id(message_row) {
+    return message_row.attr('zid');
 }
 
-function get_message_row(zephyr_id, table_name) {
+function get_message_row(message_id, table_name) {
     if (table_name === undefined)
         table_name = (narrowed ? 'zfilt' : 'zhome');
-    return $('#' + table_name + zephyr_id);
+    return $('#' + table_name + message_id);
 }
