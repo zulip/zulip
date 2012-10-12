b2103e61efe559e854528e41e5ba2f177e7c3fd0
Keegan McAllister <keegan@humbughq.com>
2012-10-12 11:10:59 -0400

    print_initial_password: Print API keys too
    (imported from commit b4fccc2947cb2c2d9e5f732c6e151bc12f091683)

--- zephyr/management/commands/print_initial_password.py
+++ zephyr/management/commands/print_initial_password.py
@@ -1,13 +1,15 @@
 from django.core.management.base import BaseCommand
-from zephyr.lib.initial_password import initial_password
+from zephyr.lib.initial_password import initial_password, initial_api_key
 
 class Command(BaseCommand):
-    help = "Print the initial password for accounts as created by populate_db"
+    help = "Print the initial password and API key for accounts as created by populate_db"
+
+    fmt = '%-30s %-16s  %-32s'
 
     def handle(self, *args, **options):
-        print
+        print self.fmt % ('email', 'password', 'API key')
         for email in args:
             if '@' not in email:
                 print 'ERROR: %s does not look like an email address' % (email,)
                 continue
-            print '%-30s %-16s' % (email, initial_password(email))
+            print self.fmt % (email, initial_password(email), initial_api_key(email))
