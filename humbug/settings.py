122edf6a8c04a2a8e52fb1ae7e310bce2af49bbe
Keegan McAllister <keegan@humbughq.com>
2012-10-16 16:52:51 -0400

    Tweak logging config
    (imported from commit 686a843c6ccbc2f514669bb7901daacbe639697c)

--- humbug/settings.py
+++ humbug/settings.py
@@ -154,14 +154,9 @@ CACHES = {
     }
 }
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': False,
+    'disable_existing_loggers': True,
     'formatters': {
         'default': {
             'format': '%(asctime)s %(levelname)-8s %(message)s'
