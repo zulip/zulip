62fad52ad66690acfa02a762941985eb705be0c6
Keegan McAllister <keegan@humbughq.com>
2012-10-17 15:27:29 -0400

    Remove unused Django staticfiles app
    (imported from commit 275db1a8a752fe307d2b94aa83937ea96fab8a28)

--- humbug/settings.py
+++ humbug/settings.py
@@ -106,7 +106,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'jstemplate',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
