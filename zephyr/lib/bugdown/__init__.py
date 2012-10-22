4a0cf8e608dae8220020d17daf6a24d5602eec15
Keegan McAllister <keegan@humbughq.com>
2012-10-21 20:40:58 -0400

    bugdown: Disable builtin 'autolink' feature
    This only linkifies inside angle brackets, per
    
        print md.inlinePatterns['autolink'].getCompiledRegExp().pattern
    
    We have our own linkification extension.
    
    (imported from commit 20cab11aaafee075e0caf933d8d197717976988c)

--- zephyr/lib/bugdown/__init__.py
+++ zephyr/lib/bugdown/__init__.py
@@ -22,7 +22,7 @@ class AutoLink(markdown.inlinepatterns.Pattern):
 
 class Bugdown(markdown.Extension):
     def extendMarkdown(self, md, md_globals):
-        for k in ('image_link', 'image_reference', 'automail'):
+        for k in ('image_link', 'image_reference', 'automail', 'autolink'):
             del md.inlinePatterns[k]
 
         for k in ('hashheader', 'setextheader'):
