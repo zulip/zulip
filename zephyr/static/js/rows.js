3ebee51ce06f893c7d1c9b66ea568d3395156a19
Keegan McAllister <keegan@humbughq.com>
2012-10-26 20:01:18 -0400

    rows.js: Use full module pattern
    (imported from commit 770741958798317401e918a7b8331cfc2f73bf77)

--- zephyr/static/js/rows.js
+++ zephyr/static/js/rows.js
@@ -1,11 +1,11 @@
-// This file doesn't have any local state or helper functions, so it has
-// a simpler structure than the proper modules.
+var rows = (function () {
 
-var rows = {
+    var exports = {};
+
     // We don't need an andSelf() here because we already know
     // that our next element is *not* a message_row, so this
     // isn't going to end up empty unless we're at the bottom or top.
-    next_visible: function (message_row) {
+    exports.next_visible = function (message_row) {
         if (message_row === undefined)
             return [];
         var row = message_row.next('.message_row');
@@ -13,9 +13,9 @@ var rows = {
             return row;
         }
         return message_row.nextUntil('.message_row').next('.message_row');
-    },
+    };
 
-    prev_visible: function (message_row) {
+    exports.prev_visible = function (message_row) {
         if (message_row === undefined)
             return [];
         var row = message_row.prev('.message_row');
@@ -23,23 +23,26 @@ var rows = {
             return row;
         }
         return message_row.prevUntil('.message_row').prev('.message_row');
-    },
+    };
 
-    first_visible: function () {
+    exports.first_visible = function () {
         return $('.focused_table .message_row:first');
-    },
+    };
 
-    last_visible: function () {
+    exports.last_visible = function () {
         return $('.focused_table .message_row:last');
-    },
+    };
 
-    id: function (message_row) {
+    exports.id = function (message_row) {
         return message_row.attr('zid');
-    },
+    };
 
-    get: function (message_id, table_name) {
+    exports.get = function (message_id, table_name) {
         if (table_name === undefined)
             table_name = (narrow.active() ? 'zfilt' : 'zhome');
         return $('#' + table_name + message_id);
-    }
-};
+    };
+
+    return exports;
+
+}());
