044b08b4097f5c1739ed8d212d1815edfc1468d1
Keegan McAllister <keegan@humbughq.com>
2012-10-29 21:44:23 -0400

    Remove more commented out example code
    (imported from commit 83f7c8763d96af5341fe630d1d8be11eef1f33aa)

--- humbug/wsgi.py
+++ humbug/wsgi.py
@@ -22,7 +22,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "humbug.settings")
 # setting points here.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
