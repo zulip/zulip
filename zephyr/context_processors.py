b81ef9cca786878db1c7127da97851a577e180e9
Keegan McAllister <keegan@humbughq.com>
2012-10-31 14:00:40 -0400

    Replace template variables static_{third,public} with hardcoded paths
    So that the 500 error view will render properly, even though no variables are
    set.  We keep the variable static_hidden, which by design is not used on the
    500 page.
    
    Fixes #240.
    
    (imported from commit 3c7534f896479b7d7edbe5ef13958481e169a13c)

--- zephyr/context_processors.py
+++ zephyr/context_processors.py
@@ -5,7 +5,5 @@ def add_settings(context):
         'full_navbar':   settings.FULL_NAVBAR,
 
         # c.f. Django's STATIC_URL variable
-        'static_public': '/static/public/',
-        'static_third':  '/static/third/',
         'static_hidden': '/static/4nrjx8cwce2bka8r/',
     }
