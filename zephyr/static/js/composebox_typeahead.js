41290f3e23c31c6db266aeb2b5a46b9bf3e9f18f
Waseem Daher <wdaher@humbughq.com>
2012-11-01 14:12:43 -0400

    Fix XSS issue in composebox typeahead.
    Whenever we use a typeahead, we should use the escaping highlighter
    unless we're explicitly going to put sanitized content in it.
    
    (imported from commit 33086327fe56a7bcbbbf8a08fe3f39800b195e75)

--- zephyr/static/js/composebox_typeahead.js
+++ zephyr/static/js/composebox_typeahead.js
@@ -29,13 +29,48 @@ exports.update_autocomplete = function () {
     autocomplete_needs_update = false;
 };
 
+//************************************
+// AN IMPORTANT NOTE ABOUT TYPEAHEADS
+//************************************
+// They do not do any HTML escaping, at all.
+// And your input to them is rendered as though it were HTML by
+// the default highlighter.
+//
+// So if you are not using trusted input, you MUST use the
+// escaping_highlighter.
+function escaping_highlighter(item) {
+    // (BASED ON Bootstrap's default highlighter, but with escaping added.)
+    var query = this.query.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, '\\$&');
+    var regex = new RegExp('(' + query + ')', 'ig');
+    // The result of the split will include the query term, because our regex
+    // has parens in it.
+    // (as per https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/String/split)
+    // However, "not all browsers support this capability", so this is a place to look
+    // if we have an issue here in, e.g. IE.
+    var pieces = item.split(regex);
+    // We need to assemble this manually (as opposed to doing 'join') because we need to
+    // (1) escape all the pieces and (2) the regex is case-insensitive, and we need
+    // to know the case of the content we're replacing (you can't just use a bolded
+    // version of 'query')
+    var result = "";
+    $.each(pieces, function(idx, piece) {
+        if (piece.match(regex)) {
+            result += "<strong>" + Handlebars.Utils.escapeExpression(piece) + "</strong>";
+        } else {
+            result += Handlebars.Utils.escapeExpression(piece);
+        }
+    });
+    return result;
+}
+
 exports.initialize = function () {
     // limit number of items so the list doesn't fall off the screen
     $( "#stream" ).typeahead({
         source: function (query, process) {
             return stream_list;
         },
-        items: 3
+        items: 3,
+        highlighter: escaping_highlighter
     });
     $( "#subject" ).typeahead({
         source: function (query, process) {
@@ -45,13 +80,15 @@ exports.initialize = function () {
             }
             return [];
         },
-        items: 2
+        items: 2,
+        highlighter: escaping_highlighter
     });
     $( "#huddle_recipient" ).typeahead({
         source: function (query, process) {
             return huddle_typeahead_list;
         },
         items: 4,
+        highlighter: escaping_highlighter,
         matcher: function (item) {
             // Assumes email addresses don't have commas or semicolons in them
             var recipients = this.query.split(/[,;] */);
@@ -73,7 +110,6 @@ exports.initialize = function () {
             var email = email_re.exec(item)[0];
             return previous_recipients + email.substring(1, email.length - 1) + ", ";
         }
-
     });
 
     $( "#huddle_recipient" ).blur(function (event) {
