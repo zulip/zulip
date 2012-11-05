114339dc2afb128d8831c9d9c924920a4c685c1b
Zev Benjamin <zev@humbughq.com>
2012-11-05 17:13:03 -0500

    Remove some no-longer-extant globals in check-all.js
    (imported from commit f83e19c891d438ed8a2ddd0e2849cd543562631b)

--- tools/jslint/check-all.js
+++ tools/jslint/check-all.js
@@ -52,7 +52,6 @@ var globals =
     + ' select_message select_message_by_id'
     + ' scroll_to_selected'
     + ' selected_message selected_message_id'
-    + ' start_reload_app reloading_app do_reload_app_preserving_compose'
     + ' at_top_of_viewport at_bottom_of_viewport'
     + ' viewport'
     ;
