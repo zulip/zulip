0c25a091d225828d83c36ca6765ebcde942b2d6e
Tim Abbott <tabbott@humbughq.com>
2012-11-06 14:09:46 -0500

    Install ntp on all our servers.
    (imported from commit 59cb9ef4350a8ec9a528623fb3247e7ba6c15405)

--- servers/puppet/manifests/site.pp
+++ servers/puppet/manifests/site.pp
@@ -29,7 +29,7 @@ define common::append ($file, $line) {
 }
 
 class humbug_base {
-  $packages = [ "screen", "strace", "vim", "emacs", "git", "python-tz", "sqlite3",
+  $packages = [ "screen", "strace", "vim", "emacs", "git", "python-tz", "sqlite3", "ntp",
                 "python-tornado",  "python-simplejson", "python-pygments", "ipython",
                 "python-django", "openssh-server", "python-pip", "puppet-el", "python-flup", ]
   package { $packages: ensure => "installed" }
