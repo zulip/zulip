ed1caec147c4be2da6b8470c4bbf13ad5f84bff0
Luke Faraone <lfaraone@humbughq.com>
2012-11-07 17:31:08 -0500

    Document the selctors in signup.js per trac #327
    (imported from commit b21f5c4dbdc1b4dc5dbdc5a3152fdc8cbbb872ef)

--- zephyr/static/js/signup.js
+++ zephyr/static/js/signup.js
@@ -3,9 +3,17 @@ $(function () {
         errorElement: "p",
         errorPlacement: function (error, element) {
             error.appendTo(element.parent()).addClass('help-inline');
+            /* element is the invalid element. We want to access the control
+             * group, which is that element's parent's parent.
+             *
+             * Adding an error class colours the entire row red.
+             */
             element.parent().parent().removeClass('success').addClass('error');
         },
         success: function (label) {
+            /* Similarly, see above comment.
+             * Adding a success class colours the entire row green.
+             */
             label.parent().parent().removeClass('error').addClass('success');
         }
     });
@@ -14,12 +22,20 @@ $(function () {
         errorElement: "p",
         errorClass: "validation-failed",
         errorPlacement: function (error, element) {
+            /* element is the invalid element. We want to access the control
+             * group, which is that element's parent's parent.
+             *
+             * Adding an error class colours the entire row red.
+             */
             $('#errors').empty();
             element.parent().parent().removeClass('success').addClass('error');
             error.appendTo("#errors")
                  .addClass("text-error");
         },
         success: function (label) {
+            /* Similarly, see above comment.
+             * Adding a success class colours the entire row green.
+             */
             label.parent().parent().removeClass('error').addClass('success');
         }
     });
