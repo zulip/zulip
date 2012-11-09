16c8baf99b90abe5f8f273647f02604b6e11f8b2
Tim Abbott <tabbott@humbughq.com>
2012-11-09 12:13:16 -0500

    Fix running tests when the default database is MySQL.
    (imported from commit b692b64219fb67792cdfd3bd208df2c6103d23ad)

--- humbug/test_settings.py
+++ humbug/test_settings.py
@@ -1,3 +1,6 @@
 from settings import *
 
-DATABASES['default']["NAME"] = "zephyr/tests/zephyrdb.test"
+DATABASES["default"] = {"NAME": "zephyr/tests/zephyrdb.test",
+                        "ENGINE": "django.db.backends.sqlite3",
+                        "OPTIONS": { "timeout": 20, },}
+
