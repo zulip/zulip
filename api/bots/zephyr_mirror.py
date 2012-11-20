f72427d83c4850add9778ab16b413a39b57951eb
Tim Abbott <tabbott@humbughq.com>
2012-11-20 15:23:30 -0500

    zephyr_mirror: Fix running --sync-subscriptions repeatedly.
    (imported from commit 15bead790590c91cd4a1d794799f94341d31c181)

--- api/bots/zephyr_mirror.py
+++ api/bots/zephyr_mirror.py
@@ -10,11 +10,16 @@ from zephyr_mirror_backend import parse_args
 
 (options, args) = parse_args()
 
+args = [os.path.join(options.root_path, "user_root", "zephyr_mirror_backend.py")]
+args.extend(sys.argv[1:])
+
+if options.sync_subscriptions:
+    subprocess.call(args)
+    sys.exit(0)
+
 while True:
     print "Starting zephyr mirroring bot"
     try:
-        args = [os.path.join(options.root_path, "user_root", "zephyr_mirror_backend.py")]
-        args.extend(sys.argv[1:])
         subprocess.call(args)
     except:
         traceback.print_exc()
