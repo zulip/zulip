ca1b1b297442c4aeeaf460393898b1c3ac23981a
Keegan McAllister <keegan@humbughq.com>
2012-11-28 00:04:11 -0500

    Remove some unused @asynchronous-related code
    (imported from commit aa332263777f26a29539ba55bec8853df59bbc07)

--- zephyr/decorator.py
+++ zephyr/decorator.py
@@ -12,22 +12,12 @@ import types
 
 class TornadoAsyncException(Exception): pass
 
-class _DefGen_Return(BaseException):
-    def __init__(self, value):
-        self.value = value
-
-def returnResponse(value):
-    raise _DefGen_Return(value)
-
 def asynchronous(method):
     @wraps(method)
     def wrapper(request, *args, **kwargs):
-        try:
-            v = method(request, handler=request._tornado_handler, *args, **kwargs)
-            if v == None or type(v) == types.GeneratorType:
-                raise TornadoAsyncException
-        except _DefGen_Return, e:
-            request._tornado_handler.finish(e.value.content)
+        v = method(request, handler=request._tornado_handler, *args, **kwargs)
+        if v == None or type(v) == types.GeneratorType:
+            raise TornadoAsyncException
         return v
     if getattr(method, 'csrf_exempt', False):
         wrapper.csrf_exempt = True
