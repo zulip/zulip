3715583d29374ca8a1d041319385f56431f4e477
Keegan McAllister <keegan@humbughq.com>
2012-11-27 19:06:05 -0500

    Implement a command to expunge old UserMessages and Messages from the database
    (imported from commit a4873dfa8737c483411d12f30daaebebebf859f9)

---
+++ zephyr/management/commands/expunge_db.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from zephyr.retention_policy     import get_UserMessages_to_expunge
+from zephyr.models               import Message
+
+class Command(BaseCommand):
+    help = ('Expunge old UserMessages and Messages from the database, '
+            + 'according to the retention policy.')
+
+    def handle(self, *args, **kwargs):
+        get_UserMessages_to_expunge().delete()
+        Message.remove_unreachable()
