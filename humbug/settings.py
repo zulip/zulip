aa0924f36bd68a20c9c2b9b0aeaa4271fb79e2ae
Tim Abbott <tabbott@humbughq.com>
2012-12-03 12:46:24 -0500

    Switch Tim's laptop to use postgres.
    (imported from commit e7f9d359843d23986ba25576531f0935f99f4c12)

--- humbug/settings.py
+++ humbug/settings.py
@@ -63,6 +63,14 @@ elif DEPLOYED:
             'read_default_file': '/etc/mysql/my.cnf',
         },
     }
+elif False:
+    DATABASES["default"] = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'humbug',
+        'USER': 'humbug',
+        'PASSWORD': 'yuHavmefbek5',
+        'HOST': ''
+        }
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
