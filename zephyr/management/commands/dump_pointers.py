a086307e6aac341ed8a6596d0a05b7a8d198c7ec
Tim Abbott <tabbott@humbughq.com>
2012-12-11 16:49:20 -0500

    Add command to dump and restore user pointers.
    For use in database migrations.
    
    (imported from commit f06ae569fe986da5e7d144c277bf27be534c04f9)

---
+++ zephyr/management/commands/dump_pointers.py
@@ -0,0 +1,29 @@
+from optparse import make_option
+from django.core.management.base import BaseCommand
+from zephyr.models import Realm, UserProfile
+import simplejson
+
+def dump():
+    pointers = []
+    for u in UserProfile.objects.select_related("user__email").all():
+        pointers.append((u.user.email, u.pointer))
+    file("dumped-pointers", "w").write(simplejson.dumps(pointers) + "\n")
+
+def restore(change):
+    for (email, pointer) in simplejson.loads(file("dumped-pointers").read()):
+        u = UserProfile.objects.get(user__email=email)
+        print "%s: pointer %s => %s" % (email, u.pointer, pointer)
+        if change:
+            u.pointer = pointer
+            u.save()
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('--restore', default=False, action='store_true'),
+        make_option('--dry-run', '-n', default=False, action='store_true'),)
+
+    def handle(self, *args, **options):
+        if options["restore"]:
+            restore(change=not options['dry_run'])
+        else:
+            dump()
