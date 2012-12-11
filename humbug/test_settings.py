7c74017bc0d76ecb34e3fab44767290f51d98a09
Keegan McAllister <keegan@humbughq.com>
2012-12-11 12:12:40 -0500

    Decrease get_updates timeout for client test suite
    Fixes #475.
    
    (imported from commit d8f908c55f2e519541e5383a742edbf23183539c)

--- humbug/test_settings.py
+++ humbug/test_settings.py
@@ -5,3 +5,7 @@ DATABASES["default"] = {"NAME": "zephyr/tests/zephyrdb.test",
                         "OPTIONS": { "timeout": 20, },}
 
 TORNADO_SERVER = 'http://localhost:9983'
+
+# Decrease the get_updates timeout to 1 second.
+# This allows CasperJS to proceed quickly to the next test step.
+POLL_TIMEOUT = 1000
