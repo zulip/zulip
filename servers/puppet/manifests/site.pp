45e6550270952c0e7b7000015f7d2aa3dccf0cff
Tim Abbott <tabbott@humbughq.com>
2013-01-02 15:03:33 -0500

    Add starting point for zmirror puppet configuration.
    I expect this will be fleshed out more later.
    
    (imported from commit c05e994e737ca2ab4ca24d4a4ac03ce46c52336a)

--- servers/puppet/manifests/site.pp
+++ servers/puppet/manifests/site.pp
@@ -297,6 +297,20 @@ class humbug_nagios {
   #TODO: Need to install our Nagios config
 }
 
+class humbug_zmirror {
+  $zmirror_packages = [ "cython", "libzephyr-dev", "comerr-dev", "python-dev", "libzephyr4-krb5", "zephyr-clients",
+                        "krb5-config", "krb5-user", "krb5-clients"]
+  package { $zmirror_packages: ensure => "installed" }
+
+  # TODO: Do the rest of our setup, which includes at least:
+  # Configuring Kerberos and Zephyr for the MIT realm
+  # Building python-zephyr after cloning it from https://github.com/ebroder/python-zephyr
+  # Putting tabbott/extra's keytab on the system at /home/humbug/tabbott.extra.keytab
+  # Setting api/bots/zephyr-mirror-crontab to be the Humbug user's crontab
+  # Running the mirroring bot in a screen session with these arguments:
+  # /home/humbug/api/bots/zephyr_mirror.py --root-path=/home/humbug/ --user=tabbott/extra --enable-log=/home/humbug/all_zephyrs_log --forward-class-messages
+}
+
 class humbug_postgres {
   $postgres_packages = [ "postgresql-9.1", "pgtune", ]
   package { $postgres_packages: ensure => "installed" }
@@ -333,3 +347,4 @@ class { "humbug_base": }
 #class { "humbug_app_frontend": }
 #class { "humbug_database": }
 #class { "humbug_postgres": }
+#class { "humbug_zmirror": }
