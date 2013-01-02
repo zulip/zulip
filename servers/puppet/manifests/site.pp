f7237ac2aa8a705d26f64c2d05ea26790fdee873
Zev Benjamin <zev@humbughq.com>
2013-01-02 16:06:03 -0500

    puppet: Install postgres config files
    (imported from commit a01bd1d0b14b1436e50605fc14c6267fe77989fc)

--- servers/puppet/manifests/site.pp
+++ servers/puppet/manifests/site.pp
@@ -322,6 +322,24 @@ class humbug_postgres {
     mode   => 644
   }
 
+  file { "/etc/postgresql/9.1/main/postgresql.conf":
+    require => Package["postgresql-9.1"],
+    ensure => file,
+    owner  => "postgres",
+    group  => "postgres",
+    mode => 644,
+    source => "/root/humbug/servers/puppet/files/postgresql/postgresql.conf",
+  }
+
+  file { "/etc/postgresql/9.1/main/pg_hba.conf":
+    require => Package["postgresql-9.1"],
+    ensure => file,
+    owner  => "postgres",
+    group  => "postgres",
+    mode => 640,
+    source => "/root/humbug/servers/puppet/files/postgresql/pg_hba.conf",
+  }
+
   common::append_if_no_such_line { 'shmmax':
     require    => Package['postgresql-9.1'],
     file       => '/etc/sysctl.d/30-postgresql-shm.conf',
