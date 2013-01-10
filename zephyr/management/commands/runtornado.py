0fdc3b772e9038f7b8304a5ee13cc74e2b677e0f
Keegan McAllister <keegan@humbughq.com>
2013-01-10 17:21:24 -0500

    runtornado: Print busy percentage at most once every 5s
    (imported from commit 017bd01f96689f06c6d166bea47b5112fad05f8c)

--- zephyr/management/commands/runtornado.py
+++ zephyr/management/commands/runtornado.py
@@ -22,6 +22,7 @@ class InstrumentedPoll(object):
     def __init__(self):
         self._underlying = orig_poll_impl()
         self._times = []
+        self._last_print = 0
 
     # Python won't let us subclass e.g. select.epoll, so instead
     # we proxy every method.  __getattr__ handles anything we
@@ -46,13 +47,15 @@ class InstrumentedPoll(object):
         while self._times and self._times[0][0] < t1 - 60:
             self._times.pop(0)
 
-        # Report the percentage of time spent outside poll
-        if self._times:
+        # Report (at most once every 5s) the percentage of time spent
+        # outside poll
+        if self._times and t1 - self._last_print >= 5:
             total = t1 - self._times[0][0]
             in_poll = sum(b-a for a,b in self._times)
             if total > 0:
                 logging.info('Tornado %5.1f%% busy over the past %4.1f seconds'
                     % (100 * (1 - in_poll/total), total))
+                self._last_print = t1
 
         return result
 
