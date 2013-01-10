62aed7c52727aa650993ed4f75a076e1f3d45c9a
Tim Abbott <tabbott@humbughq.com>
2013-01-10 13:47:26 -0500

    tornado: Move the add_*_callback functions a little earlier.
    (imported from commit 2925e345fb9bd44c605350aac631c65a8f0601b6)

--- zephyr/tornadoviews.py
+++ zephyr/tornadoviews.py
@@ -39,6 +39,12 @@ class Callbacks(object):
 
 callbacks_table = Callbacks()
 
+def add_receive_callback(user_profile, cb):
+    callbacks_table.add(user_profile.id, Callbacks.TYPE_RECEIVE, cb)
+
+def add_pointer_update_callback(user_profile, cb):
+    callbacks_table.add(user_profile.id, Callbacks.TYPE_POINTER_UPDATE, cb)
+
 # The user receives this message
 def receive(user_profile, message):
     callbacks_table.call(user_profile.id, Callbacks.TYPE_RECEIVE,
@@ -48,12 +54,6 @@ def update_pointer(user_profile, new_pointer, pointer_updater):
     callbacks_table.call(user_profile.id, Callbacks.TYPE_POINTER_UPDATE,
                          new_pointer=new_pointer,
                          update_types=["pointer_update"])
-
-def add_receive_callback(user_profile, cb):
-    callbacks_table.add(user_profile.id, Callbacks.TYPE_RECEIVE, cb)
-
-def add_pointer_update_callback(user_profile, cb):
-    callbacks_table.add(user_profile.id, Callbacks.TYPE_POINTER_UPDATE, cb)
 
 @internal_notify_view
 def notify_new_message(request):
