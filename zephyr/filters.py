e7762422e4aff66359bb484fd8090c5d5a7a4709
Luke Faraone <lfaraone@humbughq.com>
2013-01-25 13:34:07 -0500

    Filter out all cookies and the csrfmiddlewaretoken.
    We also remove META here since it rarely contains anything useful, and
    often contains sensitive environment vars.
    
    (imported from commit 2909613f9f52684bef9175600961801104644c75)

--- zephyr/filters.py
+++ zephyr/filters.py
@@ -1,12 +1,21 @@
 from django.views.debug import SafeExceptionReporterFilter
+from django.http import build_request_repr
 
 class HumbugExceptionReporterFilter(SafeExceptionReporterFilter):
     def get_post_parameters(self, request):
         filtered_post = SafeExceptionReporterFilter.get_post_parameters(self, request).copy()
         filtered_vars = ['content', 'secret', 'password', 'key', 'api-key', 'subject', 'stream',
-                         'subscriptions', 'to']
+                         'subscriptions', 'to', 'csrfmiddlewaretoken']
 
         for var in filtered_vars:
             if var in filtered_post:
                 filtered_post[var] = '**********'
         return filtered_post
+    def get_request_repr(self, request):
+        if request is None:
+            return repr(None)
+        else:
+            return build_request_repr(request,
+                                      POST_override=self.get_post_parameters(request),
+                                      COOKIES_override="**********",
+                                      META_override="**********")
