018f45d15135a66d79b431605ae61aba5aa0b397
Jessica McKellar <jesstess@humbughq.com>
2013-01-31 15:01:53 -0500

    subscriptions: add HTML and templates for new stream creation modal.
    (imported from commit d19991c7fcdd29feafa877ca8203034028b1e0b2)

--- zephyr/static/js/setup.js
+++ zephyr/static/js/setup.js
@@ -13,7 +13,7 @@ $(function () {
     // Compile Handlebars templates.
     $.each(['message', 'subscription',
             'userinfo_popover_title', 'userinfo_popover_content',
-            'timeinfo_popover_content', 'invite_subscription'],
+            'timeinfo_popover_content', 'invite_subscription', 'new_stream_users'],
         function (index, name) {
             templates[name] = Handlebars.compile($('#template_'+name).html());
         }
