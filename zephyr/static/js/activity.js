495f7835c8aa0880a8ac1cfa29cde45a64b6bdeb
Leo Franchi <leo@humbughq.com>
2013-02-12 10:40:21 -0500

    Make linter happy.
    (imported from commit 0c30f2ef176249cd1a06a21946e51a20d6ca7a32)

--- zephyr/static/js/activity.js
+++ zephyr/static/js/activity.js
@@ -7,7 +7,7 @@ var activity = (function () {
 /* 5 minutes after no activity, idle regardless of focus */
 var DEFAULT_IDLE_TIMEOUT = 5 * 60 * 1000 ;
 /* 1 minute between keep-alive pings */
-var ACTIVE_PING_INTERVAL = 1 * 60 * 1000;
+var ACTIVE_PING_INTERVAL = 60 * 1000;
 /* Twice the ping interval is the cutoff for dead clients */
 var IDLE_THRESHOLD_SECS = 2 * ACTIVE_PING_INTERVAL / 1000;
 
