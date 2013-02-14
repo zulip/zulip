ddef0c3b285b2b0e5d3f5b3f88103b75087c4fe0
Leo Franchi <leo@humbughq.com>
2013-02-13 22:50:56 -0500

    Sort user list by name not email
    (imported from commit cc13d1c65625727a923616e402e4c9fd2b5afbb4)

--- zephyr/static/js/activity.js
+++ zephyr/static/js/activity.js
@@ -41,13 +41,7 @@ function sort_users(users, user_info) {
         }
 
         // Sort equivalent PM names alphabetically
-        if (a < b) {
-            return -1;
-        } else if (a === b) {
-            return 0;
-        } else {
-            return 1;
-        }
+        return people_dict[a].full_name.localeCompare(people_dict[b].full_name);
     });
 
     return users;
