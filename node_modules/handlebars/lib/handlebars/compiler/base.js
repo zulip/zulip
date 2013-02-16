9b9d85eb36a19f23b9cf3eac1a66fcdcd66b66d4
Keegan McAllister <keegan@humbughq.com>
2013-02-16 05:01:51 -0500

    Import Handlebars compiler and dependencies
    Storing these in Git seems kind of weird, but seems to be [1] the done thing.
    I have to say, it's a lot more appealing than getting 3 packages from npm
    installed globally on every prod and dev machine -- we already have too much of
    that with 'pip install'.
    
    We can upgrade these in the future by running 'npm update' in the repo root
    directory.
    
    [1] http://www.futurealoof.com/posts/nodemodules-in-git.html
    
    (imported from commit 60a9d6a7cafe742442d87e9f3abc25750e179780)

---
+++ node_modules/handlebars/lib/handlebars/compiler/base.js
@@ -0,0 +1,24 @@
+var handlebars = require("./parser");
+
+exports.attach = function(Handlebars) {
+
+// BEGIN(BROWSER)
+
+Handlebars.Parser = handlebars;
+
+Handlebars.parse = function(input) {
+
+  // Just return if an already-compile AST was passed in.
+  if(input.constructor === Handlebars.AST.ProgramNode) { return input; }
+
+  Handlebars.Parser.yy = Handlebars.AST;
+  return Handlebars.Parser.parse(input);
+};
+
+Handlebars.print = function(ast) {
+  return new Handlebars.PrintVisitor().accept(ast);
+};
+// END(BROWSER)
+
+return Handlebars;
+};
