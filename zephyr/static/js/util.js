da7f6e0fe873061f4cc7629eb4f233e48b02bcf1
Keegan McAllister <keegan@humbughq.com>
2013-02-15 23:22:18 -0500

    Add a helper util.memoize
    (imported from commit 16b5c49580e30a51583bc0bc8b11293bba595b3a)

--- zephyr/static/js/util.js
+++ zephyr/static/js/util.js
@@ -91,5 +91,24 @@ exports.destroy_first_run_message = function () {
     $('#first_run_message').remove();
 };
 
+// Takes a one-argument function.  Returns a variant of that
+// function which caches result values.
+//
+// Since this uses a JavaScript object as the cache structure,
+// arguments with the same string representation will be
+// considered equal.
+exports.memoize = function (fun) {
+    var table = {};
+
+    return function (arg) {
+        // See #351; we should have a generic associative data
+        // structure instead.
+        if (! Object.prototype.hasOwnProperty.call(table, arg)) {
+            table[arg] = fun(arg);
+        }
+        return table[arg];
+    };
+};
+
 return exports;
 }());
