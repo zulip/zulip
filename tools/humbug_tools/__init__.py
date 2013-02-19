5f33298bf4acc23c0a8cbc66bc5fe5ae150a6fcb
Keegan McAllister <keegan@humbughq.com>
2013-02-18 20:36:59 -0500

    [manual] tools: Move check_output into a shared file
    We leave the stuff under api/ alone for now, since we need to be able to ship
    it as a standalone thing.
    
    tools/post-receive wasn't using the function anyway.
    
    For push to master: Push this commit, update post-receive per instructions at
    the top of that file, then push the rest of the branch to confirm that the hook
    still works.
    
    No manual instructions for prod.
    
    (imported from commit 9bcbe14c08d15eda47d82f0b702bad33e217a074)

---
+++ tools/humbug_tools/__init__.py
@@ -0,0 +1,16 @@
+import subprocess
+
+# check_output is backported from subprocess.py in Python 2.7
+
+def check_output(*popenargs, **kwargs):
+    if 'stdout' in kwargs:
+        raise ValueError('stdout argument not allowed, it will be overridden.')
+    process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+    output, unused_err = process.communicate()
+    retcode = process.poll()
+    if retcode:
+        cmd = kwargs.get("args")
+        if cmd is None:
+            cmd = popenargs[0]
+        raise subprocess.CalledProcessError(retcode, cmd, output=output)
+    return output
