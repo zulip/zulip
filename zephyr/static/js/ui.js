382f94b5b510da720ba08b44ad0cc45617f64afb
Jessica McKellar <jesstess@humbughq.com>
2013-02-20 21:49:18 -0500

    Move presence information to a right sidebar.
    (imported from commit c5533988606f1fc05de6b83eeb39f8f913f6ad42)

--- zephyr/static/js/ui.js
+++ zephyr/static/js/ui.js
@@ -245,9 +245,11 @@ function resizehandler(e) {
     /* total viewport - height of navbar - height of upper sidebar - padding*/
     var bottom_sidebar_height = viewport.height() - $("#top_navbar").height() - $(".upper_sidebar").height() - 40;
     $(".bottom_sidebar").height(bottom_sidebar_height);
+    var right_sidebar_height = viewport.height() - $("#top_navbar").height();
+    $(".right_sidebar").height(right_sidebar_height);
 
-    $("#stream_filters").css('max-height', bottom_sidebar_height * 0.35);
-    $("#user_presences").css('max-height', bottom_sidebar_height * 0.35);
+    $("#stream_filters").css('max-height', bottom_sidebar_height * 0.75);
+    $("#user_presences").css('max-height', right_sidebar_height * 0.90);
 
     // This function might run onReady (if we're in a narrow window),
     // but before we've loaded in the messages; in that case, don't
