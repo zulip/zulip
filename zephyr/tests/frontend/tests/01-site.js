d5493ed580b390796637872c6a7f0120632ea57f
Zev Benjamin <zev@humbughq.com>
2013-03-05 10:32:55 -0500

    tests: Split site.js out into 00-login.js and 01-site.js
    (imported from commit cdd3b2c675b6ae33456377546424fcc5cad58cf5)

--- zephyr/tests/frontend/tests/01-site.js
+++ zephyr/tests/frontend/tests/01-site.js
@@ -119,41 +119,12 @@ function un_narrow() {
     keypress(27); // Esc
 }
 
-// Start of test script.
 casper.start('http://localhost:9981/', function () {
-    // Fail if we get a JavaScript error in the page's context.
-    // Based on the example at http://phantomjs.org/release-1.5.html
-    //
-    // casper.on('error') doesn't work (it never gets called) so we
-    // set this at the PhantomJS level.  We do it inside 'start' so
-    // that we know we have a page object.
-    casper.page.onError = function (msg, trace) {
-        casper.test.error(msg);
-        casper.echo('Traceback:');
-        trace.forEach(function (item) {
-            casper.echo('  ' + item.file + ':' + item.line);
-        });
-        casper.exit(1);
-    };
-
-    casper.test.assertHttpStatus(302);
-    casper.test.assertUrlMatch(/^http:\/\/[^\/]+\/accounts\/home/, 'Redirected to /accounts/home');
-    casper.click('a[href^="/accounts/login"]');
-});
-
-// casper.then will perform the action after the effects of previous clicks etc. are finished.
-casper.then(function () {
-    casper.test.info('Logging in');
-    casper.fill('form[action^="/accounts/login"]', {
-        username: 'iago@humbughq.com',
-        password: 'FlokrWdZefyEWkfI'
-    }, true /* submit form */);
-});
-
-casper.then(function () {
     // URL like http://localhost:9981/ or http://localhost:9981/#
     casper.test.assertUrlMatch(/^http:\/\/[^\/]+\/#?$/, 'On home page');
+});    
 
+casper.then(function () {
     casper.test.info('Sanity-checking existing messages');
 
     var msg = get_rendered_messages('zhome');
@@ -175,12 +146,7 @@ casper.then(function () {
         recipient: 'humbug+tutorial@humbughq.com',
         content: 'exit'
     });
-});
 
-// Give the tutorial bot a little time to respond to us
-wait_for_receive(function () {});
-
-casper.then(function () {
     casper.test.info('Sending messages');
 });
 
