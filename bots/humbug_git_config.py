ba41c635dad89dc2b23b0641687ef0c9ce51c831
Keegan McAllister <keegan@humbughq.com>
2013-03-18 18:15:52 -0400

    humbug_git_config: Fix typo
    (imported from commit 37e92f9b9ab11ee3e64c5af579bd902aa251ffd1)

--- bots/humbug_git_config.py
+++ bots/humbug_git_config.py
@@ -23,7 +23,7 @@ HUMBUG_API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 # * subject "deploy => master" (using a pretty unicode right arrow)
 # And similarly for branch "test-post-receive" (for use when testing).
 def commit_notice_destination(repo, branch, commit):
-    if branch in ["master", "prod", "post-receive-test"]:
+    if branch in ["master", "prod", "test-post-receive"]:
         return dict(stream  = "commits",
                     subject = u"deploy \u21D2 %s" % (branch,))
 
