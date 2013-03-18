c0fece1cbd56ab9045defd9861a7acfba56f66e9
Tim Abbott <tabbott@humbughq.com>
2013-03-18 11:31:45 -0400

    get_stream: Fetch the realm object as well.
    (imported from commit 065ae574cb933cfc48371efd4327e70d37184dd4)

--- zephyr/models.py
+++ zephyr/models.py
@@ -179,7 +179,8 @@ def get_stream(stream_name, realm):
     else:
         realm_id = realm
     try:
-        return Stream.objects.get(name__iexact=stream_name.strip(), realm_id=realm_id)
+        return Stream.objects.select_related("realm").get(
+            name__iexact=stream_name.strip(), realm_id=realm_id)
     except Stream.DoesNotExist:
         return None
 
