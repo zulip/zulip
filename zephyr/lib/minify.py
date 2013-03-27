279f0b984209f27743791aca9cf3da7941e5d520
Keegan McAllister <keegan@humbughq.com>
2013-03-27 17:10:58 -0400

    Make it easier to find the source map for app.js
    (imported from commit bca27c9838573fb4b74e2d269b253a48702c9e1c)

--- zephyr/lib/minify.py
+++ zephyr/lib/minify.py
@@ -11,8 +11,18 @@ class ClosureSourceMapCompressor(SubProcessCompressor):
         # name.  So we just pick a unique arbitrary name.  This is
         # okay because we can figure out from the source map file
         # contents which JavaScript files it corresponds to.
-        source_map = path.join(settings.PIPELINE_CLOSURE_SOURCE_MAP_DIR,
-                               sha1(js).hexdigest() + '.map')
+
+        # As a hack to make things easier, assume that any large input
+        # corresponds to app.js.  This is like 60 times bigger than
+        # any other input file, at present.
+
+        if len(js) > 100000:
+            source_map_name = 'app.js.map'
+        else:
+            source_map_name = sha1(js).hexdigest() + '.map'
+
+        source_map = path.join(
+            settings.PIPELINE_CLOSURE_SOURCE_MAP_DIR, source_map_name)
 
         command = '%s --create_source_map %s' % (
             settings.PIPELINE_CLOSURE_BINARY, source_map)
