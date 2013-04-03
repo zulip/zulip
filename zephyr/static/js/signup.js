c65f9b548f020be828b69edd2f7a0e17c2c22b90
Keegan McAllister <keegan@humbughq.com>
2013-04-03 13:08:11 -0400

    signup: Remove error / success class setting for send_confirm
    Also seems to have no effect.
    
    (imported from commit 2e2f913ee0f0f3b0620153bde68b8179ebb781a5)

--- zephyr/static/js/signup.js
+++ zephyr/static/js/signup.js
@@ -21,21 +21,12 @@ $(function () {
     $("#send_confirm").validate({
         errorElement: "p",
         errorPlacement: function (error, element) {
-            /* element is the invalid element. We want to access the control
-             * group, which is that element's parent's parent.
-             *
-             * Adding an error class colours the entire row red.
-             */
             $('#errors').empty();
-            element.parent().parent().removeClass('success').addClass('error');
             error.appendTo("#errors")
                  .addClass("text-error");
         },
-        success: function (label) {
-            /* Similarly, see above comment.
-             * Adding a success class colours the entire row green.
-             */
-            label.parent().parent().removeClass('error').addClass('success');
+        success: function () {
+            $('#errors').empty();
         }
     });
 });
