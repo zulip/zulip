dc0736ef9790d1d0400227a1938ba96a26e0d961
Tim Abbott <tabbott@humbughq.com>
2013-04-11 15:41:29 -0400

    Redefine is_public to be 'humbughq.com realm and not invite_only'.
    (imported from commit 52a5b461eff926b28c2bb914b26b697de5fbfc3d)

--- zephyr/models.py
+++ zephyr/models.py
@@ -122,7 +122,8 @@ class Stream(models.Model):
         return self.__repr__()
 
     def is_public(self):
-        return self.realm.domain in ["humbughq.com"]
+        return (self.realm.domain in ["humbughq.com"] and
+                not self.invite_only)
 
     class Meta:
         unique_together = ("name", "realm")
