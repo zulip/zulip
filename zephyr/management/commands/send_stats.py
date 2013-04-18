29ad205b01a45b87d939c8bebb3c2d92ef98f6cb
Leo Franchi <leo@humbughq.com>
2013-04-18 16:58:19 -0400

    Add a management command to send a statsd message
    (imported from commit 25ead4a0dee185a139eb5401e109c9cbe937c7bf)

---
+++ zephyr/management/commands/send_stats.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+class Command(BaseCommand):
+    help = """Send some stats to statsd.
+
+Usage: python manage.py send_stats [incr|decr|timing|timer|gauge] name val"""
+
+    def handle(self, *args, **options):
+        if len(args) != 3:
+            print "Usage: python manage.py send_stats [incr|decr|timing|timer|gauge] name val"
+            exit(1)
+
+        operation = args[0]
+        name = args[1]
+        val = args[2]
+
+        if settings.USING_STATSD:
+            from statsd import statsd
+
+            func = getattr(statsd, operation)
+            func(name, val)
