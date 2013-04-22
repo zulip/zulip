986ca06c4448388f7656c8b307d69413b9cc7655
Zev Benjamin <zev@humbughq.com>
2013-04-22 11:24:28 -0400

    puppet: Add wal-e to Postgres config
    (imported from commit 55727a95cc51afb69f14c27df89a6ae287ec0f3f)

--- servers/puppet/modules/humbug/manifests/postgres-common.pp
+++ servers/puppet/modules/humbug/manifests/postgres-common.pp
@@ -1,8 +1,25 @@
 class humbug::postgres-common {
   class { 'humbug::base': }
 
-  $postgres_packages = [ "postgresql-9.1", "pgtune", ]
+  $postgres_packages = [ "postgresql-9.1", "pgtune", "python-boto",
+                         "python-argparse", "python-gevent/squeeze-backports",
+                         "lzop", "pv"]
   package { $postgres_packages: ensure => "installed" }
+
+  exec {"pip_wal-e":
+    command  => "/usr/bin/pip install git+git://github.com/zbenjamin/wal-e.git#egg=wal-e",
+    creates  => "/usr/local/bin/wal-e",
+    require  => Package['python-pip', 'python-boto', 'python-argparse',
+                        'python-gevent/squeeze-backports', 'lzop', 'pv'],
+  }
+
+  file { "/usr/local/bin/env-wal-e":
+    ensure => file,
+    owner => "root",
+    group => "postgres",
+    mode => 750,
+    source => "puppet:///modules/humbug/postgresql/env-wal-e",
+  }
 
   file { "/etc/postgresql/9.1/main/pg_hba.conf":
     require => Package["postgresql-9.1"],
