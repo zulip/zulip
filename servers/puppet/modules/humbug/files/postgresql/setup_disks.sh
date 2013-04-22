f280e7cdfa77f9c6499d2802cd9cda0adc25d2db
Zev Benjamin <zev@humbughq.com>
2013-04-22 11:06:54 -0400

    puppet: Use deadline scheduler for disks on Postgres master
    (imported from commit 41061cb4535b94b4afea8c3a2228120073bf06ee)

--- servers/puppet/modules/humbug/files/postgresql/setup_disks.sh
+++ servers/puppet/modules/humbug/files/postgresql/setup_disks.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 umount /mnt
 yes | mdadm --create /dev/md0 --raid-devices=2 --level=1 /dev/xvdb /dev/xvdc
+
 blockdev --setra 4096 /dev/xvdb
 blockdev --setra 4096 /dev/xvdc
 blockdev --setra 4096 /dev/md0
+echo deadline > /sys/block/xvdb/queue/scheduler
+echo deadline > /sys/block/xvdc/queue/scheduler
+
 mkfs.xfs /dev/md0
 echo "/dev/md0   /srv  xfs    noatime,barrier 1 1" >> /etc/fstab
 mount /srv
