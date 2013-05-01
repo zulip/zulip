4828c90c64fe41738a56e03e54c405cbe47d0122
Luke Faraone <lfaraone@humbughq.com>
2013-05-01 11:36:55 -0700

    Implement Humbug SVN integration.
    This post-commit hook depends on pysvn. After a transaction is completed,
    a Humbug is sent to a configurable stream with the repo modified, actor,
    and commit message.
    
    (imported from commit 75cab82d5fe993ea7c4c05be07a7b61e770aff81)

---
+++ api/integrations/svn/humbug_svn_config.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2013 Humbug, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+# Change these values to configure authentication for the plugin
+HUMBUG_USER = "svn@example.com"
+HUMBUG_API_KEY = "0123456789abcdef0123456789abcdef"
+
+# commit_notice_destination() lets you customize where commit notices
+# are sent to with the full power of a Python function.
+#
+# It takes the following arguments:
+# * path   = the path to the svn repository on the server
+# * commit = the commit id
+#
+# Returns a dictionary encoding the stream and subject to send the
+# notification to (or None to send no notification).
+#
+# The default code below will send every commit except for the "master-plan"
+# and "secret" repos to
+# * stream "commits"
+# * subject "deploy => branch_name" (using a pretty unicode right arrow)
+def commit_notice_destination(path, commit):
+    repo = path.split('/')[-1]
+    if repo not in ["evil-master-plan", "my-super-secret-repository"]:
+        return dict(stream  = "commits",
+                    subject = u"deploy \u21D2 %s" % (repo,))
+
+    # Return None for cases where you don't want a notice sent
+    return None
+
+## If properly installed, the Humbug API should be in your import
+## path, but if not, set a custom path below
+HUMBUG_API_PATH = None
+
+# This should not need to change unless you have a custom Humbug subdomain.
+HUMBUG_SITE = "https://humbughq.com"
