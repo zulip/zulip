cc321636b1a755381067d18444a951c852771f21
Jessica McKellar <jesstess@humbughq.com>
2013-05-01 13:50:52 -0400

    Add a management command to bankrupt users.
    (imported from commit 58fbd08fc31a69c9ee7fb73b9302d44eb87db1fa)

---
+++ zephyr/management/commands/bankrupt_users.py
@@ -0,0 +1,36 @@
+from __future__ import absolute_import
+
+from django.core.management.base import BaseCommand
+
+from zephyr.lib.actions import update_message_flags
+from zephyr.models import UserProfile, Message, get_user_profile_by_email
+
+class Command(BaseCommand):
+    help = """Bankrupt one or many users.
+
+Usage: python manage.py bankrupt_users <list of email addresses>"""
+
+    def handle(self, *args, **options):
+        if len(args) < 1:
+            print "Please provide at least one e-mail address."
+            exit(1)
+
+        for email in args:
+            try:
+                user_profile = get_user_profile_by_email(email)
+            except UserProfile.DoesNotExist:
+                print "e-mail %s doesn't exist in the system, skipping" % (email,)
+                continue
+
+            update_message_flags(user_profile, "add", "read", None, True)
+
+            messages = Message.objects.filter(
+                usermessage__user_profile=user_profile).order_by('-id')[:1]
+            if messages:
+                old_pointer = user_profile.pointer
+                new_pointer = messages[0].id
+                user_profile.pointer = new_pointer
+                user_profile.save()
+                print "%s: %d => %d" % (email, old_pointer, new_pointer)
+            else:
+                print "%s has no messages, can't bankrupt!" % (email,)
