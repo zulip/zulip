aedb81c4907bcd7ba744f8d98428825043b092c6
acrefoot <acrefoot@humbughq.com>
2013-05-22 15:18:45 -0400

    Make manage.py spit out tracebacks, without a patched django
    I first made --traceback the default in my patches to django, but this
    broke several regressions tests (when Luke built .deb packages), so he reverted that change,
    and I'm putting this default into our manage.py instead
    
    (imported from commit 460253561e637d8a6692c68bbd3859f266dbf83d)

--- manage.py
+++ manage.py
@@ -3,6 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
+
+    if "--no-traceback" not in sys.argv and len(sys.argv) > 1:
+        sys.argv.append("--traceback")
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "humbug.settings")
 
     from django.core.management import execute_from_command_line
