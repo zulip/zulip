53de71c0b3144a6c16e26a147d63895d18ea13f7
Jessica McKellar <jesstess@humbughq.com>
2013-05-29 10:48:20 -0400

    api: bump version to 0.1.7.
    (imported from commit 71d1af3806dea82a55ff7f620a92ffb7f1b63a56)

--- api/humbug/__init__.py
+++ api/humbug/__init__.py
@@ -33,7 +33,7 @@ from distutils.version import LooseVersion
 from ConfigParser import SafeConfigParser
 
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
 # Check that we have a recent enough version
 # Older versions don't provide the 'json' attribute on responses.
