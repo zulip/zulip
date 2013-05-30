441e4296e0ae4678154564ec7dfc181533cb74cd
Zev Benjamin <zev@humbughq.com>
2013-05-30 10:40:05 -0400

    Add copyright notices to third-party files
    These will get slurped into the minified files we generate.
    
    (imported from commit 46375d6d8c382bf681c192d2e6a0cb6d941bdac0)

--- zephyr/static/third/jquery-autosize/jquery.autosize.js
+++ zephyr/static/third/jquery-autosize/jquery.autosize.js
@@ -1,3 +1,27 @@
+/** @preserve
+ Software from "Autosize 1.13", a jQuery plugin for textareas, is
+ Copyright (c) 2012 Jack Moore and is provided under the following license:
+ --
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ --
+*/
+
 // Autosize 1.13 - jQuery plugin for textareas
 // (c) 2012 Jack Moore - jacklmoore.com
 // license: www.opensource.org/licenses/mit-license.php
