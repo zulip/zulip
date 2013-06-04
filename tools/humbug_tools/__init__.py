795173ac8684a67bff8ffe7e001a9d5f9816ad3e
Tim Abbott <tabbott@humbughq.com>
2013-06-04 18:21:47 -0400

    update-deployment: Move color codes to humbug_tools.py.
    (imported from commit 96a79a699ea032714b24aa6572fd188433b979a8)

--- tools/humbug_tools/__init__.py
+++ tools/humbug_tools/__init__.py
@@ -19,3 +19,10 @@ def check_output(*popenargs, **kwargs):
 DEPLOYMENTS_DIR = "/home/humbug/humbug-deployments"
 LOCK_DIR = os.path.join(DEPLOYMENTS_DIR, "lock")
 TIMESTAMP_FORMAT = '%Y-%m-%d-%H-%M-%S'
+
+# Color codes
+OKBLUE = '\033[94m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
