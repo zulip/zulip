25c9f5bace6d76541f20c599385528ec36b6b426
Steve Howell <steve@humbughq.com>
2013-06-19 14:23:27 -0400

    Subsume Django method into our codebase for code review clarity.
    (This commit will probably be squashed, but it makes the subsequent
    diffs more clear with respect to how we remove functionality from
    run_tests.)
    
    (imported from commit 9d0edcd5282a0f90ebd38e0ac229ef874dc00504)

--- zephyr/tests.py
+++ zephyr/tests.py
@@ -2932,3 +2932,14 @@ class Runner(DjangoTestSuiteRunner):
 
     def __init__(self, *args, **kwargs):
         DjangoTestSuiteRunner.__init__(self, *args, **kwargs)
+
+    # Subsume run_tests under our control.  This is literally copied
+    # from /Library/Python/2.7/site-packages/django/test/simple.py.
+    def run_tests(self, test_labels, extra_tests=None, **kwargs):
+        self.setup_test_environment()
+        suite = self.build_suite(test_labels, extra_tests)
+        old_config = self.setup_databases()
+        result = self.run_suite(suite)
+        self.teardown_databases(old_config)
+        self.teardown_test_environment()
+        return self.suite_result(suite, result)
