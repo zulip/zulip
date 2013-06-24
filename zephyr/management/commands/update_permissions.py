69884909800ac468b7ad7d1fc9491facec9e79b6
Luke Faraone <lfaraone@humbughq.com>
2013-06-24 12:24:41 -0700

    Add a management command to create objects for newly defined permissions
    This script does not remove permissions that already exist.
    
    (imported from commit 15d18266a05a84b9cac6cc7d2104668b41b48f35)

---
+++ zephyr/management/commands/update_permissions.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+from django.core.management.base import BaseCommand
+from django.db.models import get_app, get_models
+from django.contrib.auth.management import create_permissions
+
+class Command(BaseCommand):
+    help = "Sync newly created object permissions to the database"
+
+    def handle(self, *args, **options):
+        # From http://stackoverflow.com/a/11914435/90777
+        create_permissions(get_app("zephyr"), get_models(), 2)
