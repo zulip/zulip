70369159335165515c3a99351064fb946ed23dda
Leo Franchi <leo@humbughq.com>
2013-06-28 14:40:05 -0400

    Add windows sparkle files
    (imported from commit b7c0770acd34f44e961014a00d2059dfc7bef701)

--- servers/puppet/modules/humbug/manifests/prod_app_frontend.pp
+++ servers/puppet/modules/humbug/manifests/prod_app_frontend.pp
@@ -15,7 +15,8 @@ class humbug::prod_app_frontend {
   }
 
   file { [ "/srv/www/", "/srv/www/dist/", "/srv/www/dist/api",
-           "/srv/www/dist/apps/", "/srv/www/dist/apps/mac/" ]:
+           "/srv/www/dist/apps/", "/srv/www/dist/apps/mac/",
+           "/srv/www/dist/apps/win/" ]:
     ensure => "directory",
     owner      => "humbug",
     group      => "humbug",
@@ -28,7 +29,7 @@ class humbug::prod_app_frontend {
     owner  => "humbug",
     group  => "humbug",
     mode => 644,
-    source => "puppet:///modules/humbug/sparkle/sparkle.xml",
+    source => "puppet:///modules/humbug/sparkle/mac/sparkle.xml",
   }
   file { "/srv/www/dist/apps/mac/sparkle-changelog.html":
     ensure => file,
@@ -36,6 +37,22 @@ class humbug::prod_app_frontend {
     owner  => "humbug",
     group  => "humbug",
     mode => 644,
-    source => "puppet:///modules/humbug/sparkle/sparkle-changelog.html",
+    source => "puppet:///modules/humbug/sparkle/mac/sparkle-changelog.html",
+  }
+  file { "/srv/www/dist/apps/win/sparkle.xml":
+    ensure => file,
+    require    => File['/srv/www/dist/apps/win/'],
+    owner  => "humbug",
+    group  => "humbug",
+    mode => 644,
+    source => "puppet:///modules/humbug/sparkle/win/sparkle.xml",
+  }
+  file { "/srv/www/dist/apps/win/sparkle-changelog.html":
+    ensure => file,
+    require    => File['/srv/www/dist/apps/win/'],
+    owner  => "humbug",
+    group  => "humbug",
+    mode => 644,
+    source => "puppet:///modules/humbug/sparkle/win/sparkle-changelog.html",
   }
 }
