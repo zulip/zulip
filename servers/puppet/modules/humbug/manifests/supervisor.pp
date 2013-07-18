508ddca539cb6b24f43253570140e3e9232a22ca
Tim Abbott <tabbott@humbughq.com>
2013-07-18 16:38:40 -0400

    puppet: Specify commands for restarting supervisord.
    (imported from commit e1a6322fd9ecf1655ec4325d0fcaf0ad160202ae)

--- servers/puppet/modules/humbug/manifests/supervisor.pp
+++ servers/puppet/modules/humbug/manifests/supervisor.pp
@@ -7,5 +7,17 @@ class humbug::supervisor {
     require => [File["/var/log/humbug"],
                 Package["supervisor"],
                 ],
+
+    hasstatus => true,
+    status => "supervisorctl status",
+
+    # The "restart" option in the init script does not work.  We could
+    # tell Puppet to fall back to stop/start, which does work, but the
+    # better option is to tell supervisord to restart via supervisorctl
+    #
+    # Idealy we would use the "reread" command, but that does't seem
+    # to actually work.
+    hasrestart => true,
+    restart => "supervisorctl reload"
   }
 }
