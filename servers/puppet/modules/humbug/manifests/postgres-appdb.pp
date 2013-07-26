0008edc9d5479c82291c2e62802419bb7467e52a
Zev Benjamin <zev@humbughq.com>
2013-07-26 17:36:54 -0400

    [manual] Add worker process for processing asynchronous FTS updates
    Unlike the other workers, this process runs on the database server.
    To prevent duplicate work, only the worker on the master actually
    processes updates.
    
    To deploy, we in theory just need to apply the new puppet config.  In
    practice, the database servers aren't on wheezy yet, so the changes
    should be applied by hand to postgres0 and postgres1.  This only
    needs to be done on deploy to staging.
    
    (imported from commit d679ffc0838f9fc8c7f0bede08a5568b339b7ddb)

--- servers/puppet/modules/humbug/manifests/postgres-appdb.pp
+++ servers/puppet/modules/humbug/manifests/postgres-appdb.pp
@@ -1,6 +1,28 @@
 class humbug::postgres-appdb {
   class { 'humbug::base': }
   class { 'humbug::postgres-common': }
+  class { 'humbug::supervisor': }
+
+  $appdb_packages = [ "python-psycopg2",]
+  package { $appdb_packages: ensure => "installed" }
+
+  file { "/usr/local/bin/process_fts_updates":
+    ensure => file,
+    owner => "root",
+    group => "root",
+    mode => 755,
+    source => "puppet:///modules/humbug/postgresql/process_fts_updates",
+  }
+
+  file { "/etc/supervisor/conf.d/zulip_db.conf":
+    require => Package[supervisor],
+    ensure => file,
+    owner => "root",
+    group => "root",
+    mode => 644,
+    source => "puppet:///modules/humbug/supervisord/conf.d/zulip_db.conf",
+    notify => Service[supervisor],
+  }
 
   file { "/etc/postgresql/9.1/main/pg_hba.conf":
     require => Package["postgresql-9.1"],
