0663c7243cdfd26d6ebb3ebf648f75e35a5d7e74
Waseem Daher <wdaher@zulip.com>
2013-08-01 12:01:18 -0400

    Enable always-open-compose for MIT, CUSTOMER4, Zulip.
    (imported from commit 43602ebeed5396de038c59240a9d695ec411db89)

--- static/js/feature_flags.js
+++ static/js/feature_flags.js
@@ -2,7 +2,12 @@ var feature_flags = (function () {
 
 var exports = {};
 
-exports.always_open_compose = page_params.staging;
+exports.always_open_compose =  _.contains(['mit.edu',
+                                           'customer4.invalid',
+                                           'users.customer4.invalid',
+                                           'zulip.com'],
+                                          page_params.domain);
+
 exports.mark_read_at_bottom = page_params.staging;
 
 return exports;
