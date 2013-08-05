0f42feb131341b3788f984986a1488cab998932b
Steve Howell <steve@humbughq.com>
2013-08-05 18:07:57 -0400

    Turn on always-open-compose for all realms.
    (imported from commit 0a7b35a4144e5645adbfd9b8fffb78573e676ad4)

--- static/js/feature_flags.js
+++ static/js/feature_flags.js
@@ -2,13 +2,7 @@ var feature_flags = (function () {
 
 var exports = {};
 
-exports.always_open_compose =  _.contains(['mit.edu',
-                                           'customer4.invalid',
-                                           'users.customer4.invalid'
-                                           // 'zulip.com'
-                                           ],
-                                          page_params.domain);
-
+exports.always_open_compose =  true;
 exports.mark_read_at_bottom = page_params.staging;
 
 return exports;
