f9614d76a2aa403ede91159b651f672f8a278537
Steve Howell <steve@humbughq.com>
2013-08-05 11:06:56 -0400

    Disable always_open_compose on staging
    (imported from commit 787d4c15c3e0a9dabebae1651cd5a670f5549b4d)

--- static/js/feature_flags.js
+++ static/js/feature_flags.js
@@ -4,8 +4,9 @@ var exports = {};
 
 exports.always_open_compose =  _.contains(['mit.edu',
                                            'customer4.invalid',
-                                           'users.customer4.invalid',
-                                           'zulip.com'],
+                                           'users.customer4.invalid'
+                                           // 'zulip.com'
+                                           ],
                                           page_params.domain);
 
 exports.mark_read_at_bottom = page_params.staging;
