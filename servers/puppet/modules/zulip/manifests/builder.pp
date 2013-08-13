91ff01085375d0f252b0bfcd0aa0203503372cf5
Luke Faraone <lfaraone@zulip.com>
2013-08-13 10:12:24 -0400

    Configuration to create schroots.
    (imported from commit 27288bfdc5b39aa5c8804ac9628dc960b621ca7f)

---
+++ servers/puppet/modules/zulip/manifests/builder.pp
@@ -0,0 +1,58 @@
+class zulip::builder {
+  class { 'zulip::base': }
+
+  $buildd_packages = [
+    "apt-spy",
+    "netselect-apt",
+    "ubuntu-dev-tools",
+    "schroot",
+    "sbuild",
+    ]
+  package { $buildd_packages: ensure => "installed" }
+
+  file { "/home/humbug/.sbuildrc":
+    require => Package[sbuild],
+    ensure => file,
+    owner  => "humbug",
+    group  => "humbug",
+    mode => 644,
+    source => "puppet:///modules/zulip/builder/sbuildrc",
+  }
+
+  file { "/usr/share/keyrings/ubuntu-archive-keyring.gpg":
+    ensure => file,
+    mode => 644,
+    source => "puppet:///modules/zulip/builder/ubuntu-archive-keyring.gpg",
+  }
+
+
+  file { "/root/.sbuildrc":
+    ensure => 'link',
+    target => '/home/humbug/.sbuildrc',
+  }
+
+  exec { "adduser root sbuild": }
+  exec { "adduser humbug sbuild": }
+  chroot { "precise":
+    distro => "ubuntu",
+    ensure => present,
+  }
+  chroot { "quantal":
+    distro => "ubuntu",
+    ensure => present,
+  }
+  chroot { "raring":
+    distro => "ubuntu",
+    ensure => present,
+  }
+  chroot { "stable":
+    distro => "debian",
+    ensure => present,
+  }
+  chroot { "testing":
+    distro => "debian",
+    ensure => present,
+  }
+
+
+}
