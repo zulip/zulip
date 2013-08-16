e51cfeaed451588043861feb4b8745af8b2330e4
Jeff Arnold <jbarnold@zulip.com>
2013-08-16 14:23:30 -0400

    Only add an edit icon next to the last paragraph
    (imported from commit c6d2893faef144f85518c4d45856282ada33a9d1)

--- static/js/ui.js
+++ static/js/ui.js
@@ -261,7 +261,7 @@ function message_hover(message_row) {
     message_unhover();
     message_row.addClass('message_hovered');
     if (feature_flags.edit_appears_on_hover && message && message.sent_by_me) {
-        message_row.find('.message_content').find('p').append(edit_content_button);
+        message_row.find('.message_content').find('p:last').append(edit_content_button);
     }
     current_message_hover = message_row;
 }
