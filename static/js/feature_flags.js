b166accc334acf743b052dd030faaf7e66eb567e
Jeff Arnold <jbarnold@zulip.com>
2013-08-16 11:44:07 -0400

    Revert "Show a button for editing messages when the user hovers over them."
    This reverts commit 782e76210ac835d0617d91679933eccff0caad98.
    
    (imported from commit 24889d0e0e38cc46d4b2d06c12a5737a4772fd6c)

--- static/js/feature_flags.js
+++ static/js/feature_flags.js
@@ -8,7 +8,6 @@ exports.twenty_four_hour_time = _.contains([],
                                   page_params.email);
 exports.dropbox_integration = page_params.staging || _.contains(['dropbox.com'], page_params.domain);
 exports.email_forwarding = page_params.staging;
-exports.edit_appears_on_hover = page_params.staging || _.contains(['customer7.invalid'], page_params.domain);
 
 
 exports.mandatory_topics = _.contains([
