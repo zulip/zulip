6850f25907a72e07f1a9972e0128dd396390a1e4
Tim Abbott <tabbott@zulip.com>
2013-08-19 14:43:32 -0400

    [manual] puppet: Move rabbitmq cron jobs into puppet.
    When we deploy this, we should remove the relevant jobs from root's
    crontab on our app servers.
    
    (imported from commit 749be952d504f5a4d243cf59f6430acc689fc821)

--- servers/puppet/modules/zulip/manifests/rabbit.pp
+++ servers/puppet/modules/zulip/manifests/rabbit.pp
@@ -2,5 +2,22 @@ class zulip::rabbit {
   $rabbit_packages = [ "rabbitmq-server" ]
   package { $rabbit_packages: ensure => "installed" }
 
+  file { "/etc/cron.d/rabbitmq-queuesize":
+    require => Package[rabbitmq-server],
+    ensure => file,
+    owner  => "root",
+    group  => "root",
+    mode => 644,
+    source => "puppet:///modules/zulip/cron.d/rabbitmq-queuesize",
+  }
+  file { "/etc/cron.d/rabbitmq-numconsumner":
+    require => Package[rabbitmq-server],
+    ensure => file,
+    owner  => "root",
+    group  => "root",
+    mode => 644,
+    source => "puppet:///modules/zulip/cron.d/rabbitmq-numconsumers",
+  }
+
   # TODO: Should also call exactly once "servers/configure-rabbitmq"
 }
