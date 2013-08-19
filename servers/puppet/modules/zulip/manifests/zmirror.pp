00279ae70543bb17c5f454dc55e1c97c2fb87745
Tim Abbott <tabbott@zulip.com>
2013-08-19 12:10:44 -0400

    puppet: Add debathena.list to zmirror configuration.
    (imported from commit 3d54bd495b5f8f582ec4d1f7a859ac0c0b63930b)

--- servers/puppet/modules/zulip/manifests/zmirror.pp
+++ servers/puppet/modules/zulip/manifests/zmirror.pp
@@ -2,11 +2,18 @@ class zulip::zmirror {
   class { 'zulip::base': }
 
   $zmirror_packages = [ "cython", "libzephyr-dev", "comerr-dev", "python-dev", "libzephyr4-krb5", "zephyr-clients",
-                        "krb5-config", "krb5-user", "krb5-clients"]
+                        "krb5-config", "krb5-user", "krb5-clients", "debathena-kerberos-config", "debathena-zephyr-config"]
   package { $zmirror_packages: ensure => "installed" }
 
+  file { '/etc/apt/sources.list.d/debathena.list':
+    ensure     => file,
+    mode       => 644,
+    owner      => "root",
+    group      => "root",
+    source     => 'puppet:///modules/zulip/debathena.list',
+  }
+
   # TODO: Do the rest of our setup, which includes at least:
-  # Configuring Kerberos and Zephyr for the MIT realm
   # Building python-zephyr after cloning it from https://github.com/ebroder/python-zephyr
   # Putting tabbott/extra's keytab on the system at /home/humbug/tabbott.extra.keytab
   # Setting api/bots/zephyr-mirror-crontab to be the humbug user's crontab
