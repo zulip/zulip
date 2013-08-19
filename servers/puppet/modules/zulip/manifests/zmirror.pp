c7be859d21006b2f798bc2480970316485fad930
Tim Abbott <tabbott@zulip.com>
2013-08-19 13:51:22 -0400

    puppet: Run zephyr_mirror.py via supervisord.
    (imported from commit 09086a1dd298e7ee9b655ccd6c58ac720bbc7798)

--- servers/puppet/modules/zulip/manifests/zmirror.pp
+++ servers/puppet/modules/zulip/manifests/zmirror.pp
@@ -1,5 +1,6 @@
 class zulip::zmirror {
   class { 'zulip::base': }
+  class { 'zulip::supervisor': }
 
   $zmirror_packages = [ "cython", "libzephyr-dev", "comerr-dev", "python-dev", "libzephyr4-krb5", "zephyr-clients",
                         "krb5-config", "krb5-user", "krb5-clients", "debathena-kerberos-config", "debathena-zephyr-config"]
@@ -12,11 +13,18 @@ class zulip::zmirror {
     group      => "root",
     source     => 'puppet:///modules/zulip/debathena.list',
   }
+  file { "/etc/supervisor/conf.d/zmirror.conf":
+    require => Package[supervisor],
+    ensure => file,
+    owner => "root",
+    group => "root",
+    mode => 644,
+    source => "puppet:///modules/zulip/supervisord/conf.d/zmirror.conf",
+    notify => Service["supervisor"],
+  }
 
   # TODO: Do the rest of our setup, which includes at least:
   # Building python-zephyr after cloning it from https://github.com/ebroder/python-zephyr
   # Putting tabbott/extra's keytab on the system at /home/humbug/tabbott.extra.keytab
   # Setting api/bots/zephyr-mirror-crontab to be the humbug user's crontab
-  # Running the mirroring bot in a screen session with these arguments:
-  # /home/humbug/api/bots/zephyr_mirror.py --root-path=/home/humbug/ --user=tabbott/extra --enable-log=/home/humbug/all_zephyrs_log --forward-class-messages
 }
