864ae3ad51eb93313f0e415545ac4adffa2a2a6a
Jessica McKellar <jesstess@humbughq.com>
2013-08-29 17:46:47 -0400

    Enable the email forwarding UI for CUSTOMER6.
    (imported from commit 587159412ad208d1f5e44479f50ca2f25a98f70a)

--- static/js/feature_flags.js
+++ static/js/feature_flags.js
@@ -8,7 +8,8 @@ exports.summarize_read_while_narrowed = page_params.staging;
 exports.twenty_four_hour_time = _.contains([],
                                   page_params.email);
 exports.dropbox_integration = page_params.staging || _.contains(['dropbox.com'], page_params.domain);
-exports.email_forwarding = page_params.staging;
+exports.email_forwarding = page_params.staging ||
+                           _.contains(['customer6.invalid'], page_params.domain);
 exports.edit_appears_on_hover = true;
 
 
