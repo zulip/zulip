c0d1481e838289dac1ff2a678be14c0e921fd836
Steve Howell <steve@zulip.com>
2013-09-05 12:13:11 -0400

    [third] Fix copyright for jquery.autosize.js
    (imported from commit c680058ad5dc5e51e76308f5e79bd84a96c1de6a)

--- static/third/jquery-autosize/jquery.autosize.js
+++ static/third/jquery-autosize/jquery.autosize.js
@@ -1,8 +1,26 @@
-/*!
+/** @preserve
 	Autosize v1.17.7 - 2013-09-03
 	Automatically adjust textarea height based on user input.
 	(c) 2013 Jack Moore - http://www.jacklmoore.com/autosize
 	license: http://www.opensource.org/licenses/mit-license.php
+	---
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
 */
 (function (factory) {
 	if (typeof define === 'function' && define.amd) {
@@ -97,7 +115,7 @@
 			// The mirror width must exactly match the textarea width, so using getBoundingClientRect because it doesn't round the sub-pixel value.
 			function setWidth() {
 				var style, width;
-				
+
 				if ('getComputedStyle' in window) {
 					style = window.getComputedStyle(ta);
 					width = ta.getBoundingClientRect().width;
