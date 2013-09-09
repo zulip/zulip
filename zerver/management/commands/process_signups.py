a33957db32006d663112a1e6a7f0832bb0bdbedd
Leo Franchi <leo@zulip.com>
2013-09-09 10:57:09 -0400

    Handle mailchimp error 214 (duplicate email) in signup worker
    (imported from commit cb34c153fc96bca7c8faed01d019aa2433fcf568)

--- zerver/management/commands/process_signups.py
+++ zerver/management/commands/process_signups.py
@@ -1,8 +1,10 @@
 from __future__ import absolute_import
 
-from postmonkey import PostMonkey
+from postmonkey import PostMonkey, MailChimpException
 from django.core.management.base import BaseCommand
 from django.conf import settings
+
+import logging
 
 from zerver.lib.queue import SimpleQueueClient
 
@@ -10,12 +12,18 @@ class Command(BaseCommand):
     pm = PostMonkey(settings.MAILCHIMP_API_KEY, timeout=10)
 
     def subscribe(self, ch, method, properties, data):
-        self.pm.listSubscribe(
-                id=settings.ZULIP_FRIENDS_LIST_ID,
-                email_address=data['EMAIL'],
-                merge_vars=data['merge_vars'],
-                double_optin=False,
-                send_welcome=False)
+        try:
+            self.pm.listSubscribe(
+                    id=settings.ZULIP_FRIENDS_LIST_ID,
+                    email_address=data['EMAIL'],
+                    merge_vars=data['merge_vars'],
+                    double_optin=False,
+                    send_welcome=False)
+        except MailChimpException, e:
+            if e.code == 214:
+                logging.warning("Attempted to sign up already existing email to list: %s" % (data['EMAIL'],))
+            else:
+                raise e
 
     def handle(self, *args, **options):
         q = SimpleQueueClient()
