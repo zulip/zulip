2eb8c6aae3709f48c6a32248f2362565ff8666ee
Leo Franchi <leo@zulip.com>
2013-09-10 11:25:35 -0400

    Add documentation and a duration argument to analyze_user_activity.py
    (imported from commit 5668ec02a23dcfeb29760b55d8acee17cfdea9cf)

--- zerver/management/commands/analyze_user_activity.py
+++ zerver/management/commands/analyze_user_activity.py
@@ -10,12 +10,13 @@ from django.utils.timezone import utc, is_naive
 
 def analyze_activity(options):
     day_start = datetime.datetime.strptime(options["date"], "%Y-%m-%d").replace(tzinfo=utc)
-    day_end = day_start + datetime.timedelta(days=1)
+    day_end = day_start + datetime.timedelta(days=options["duration"])
 
     user_profile_query = UserProfile.objects.all()
     if options["realm"]:
         user_profile_query = user_profile_query.filter(realm__domain=options["realm"])
 
+    print "Per-user online duration:\n"
     total_duration = datetime.timedelta(0)
     for user_profile in user_profile_query:
         intervals = UserActivityInterval.objects.filter(user_profile=user_profile,
@@ -30,16 +31,32 @@ def analyze_activity(options):
             duration += end - start
 
         total_duration += duration
-        print user_profile.email, duration
+        print "%-*s%s" % (37, user_profile.email, duration, )
 
-    print "Total Duration: %s" % (total_duration,)
-    print "Total Duration in minutes: %s" % (total_duration.total_seconds() / 60.,)
+    print "\nTotal Duration:                      %s" % (total_duration,)
+    print "\nTotal Duration in minutes:           %s" % (total_duration.total_seconds() / 60.,)
     print "Total Duration amortized to a month: %s" % (total_duration.total_seconds() * 30. / 60.,)
 
 class Command(BaseCommand):
+    help = """Report analytics of user activity on a per-user and realm basis.
+
+This command aggregates user activity data that is collected by each user using Zulip. It attempts
+to approximate how much each user has been using Zulip per day, measured by recording each 15 minute
+period where some activity has occurred (mouse move or keyboard activity).
+
+It will correctly not count server-initiated reloads in the activity statistics.
+
+The duration flag can be used to control how many days to show usage duration for
+
+Usage: python manage.py analyze_user_activity [--realm=zulip.com] [--date=2013-09-10] [--duration=1]
+
+By default, if no date is selected 2013-09-10 is used. If no realm is provided, information
+is shown for all realms"""
+
     option_list = BaseCommand.option_list + (
         make_option('--realm', action='store'),
         make_option('--date', action='store', default="2013-09-06"),
+        make_option('--duration', action='store', default=1, type=int, help="How many days to show usage information for"),
         )
 
     def handle(self, *args, **options):
