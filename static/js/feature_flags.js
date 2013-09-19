f063921260d63accb06aaa0aa7365b04faffd52e
Jeff Arnold <jbarnold@zulip.com>
2013-09-19 10:51:11 -0400

    Enable summarizing for everyone
    (imported from commit c03d48ad9c0fe6dfe86539fbff1f501b42b3b6d1)

--- static/js/feature_flags.js
+++ static/js/feature_flags.js
@@ -2,13 +2,9 @@ var feature_flags = (function () {
 
 var exports = {};
 
-exports.mark_read_at_bottom = page_params.staging ||
-                              _.contains(['mit.edu',
-                                          'customer4.invalid',
-                                          'users.customer4.invalid'
-                                          ],
-                                          page_params.domain);
-exports.summarize_read_while_narrowed = page_params.staging;
+exports.mark_read_at_bottom = true;
+exports.summarize_read_while_narrowed = true;
+
 exports.twenty_four_hour_time = _.contains([],
                                   page_params.email);
 exports.dropbox_integration = page_params.staging || _.contains(['dropbox.com'], page_params.domain);
