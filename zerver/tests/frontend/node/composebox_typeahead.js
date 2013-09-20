d9dba4176789d4c45c004768510e89642eb58288
Leo Franchi <leo@zulip.com>
2013-09-20 13:47:00 -0400

    Add some tests for composebox_typeahead.compose_content_begins_typeahead
    (imported from commit 01e71ebe87da6e866817dc56555a7e6ee61cb003)

--- zerver/tests/frontend/node/composebox_typeahead.js
+++ zerver/tests/frontend/node/composebox_typeahead.js
@@ -7,6 +7,14 @@ add_dependencies({
 
 var ct = require('js/composebox_typeahead.js');
 
+var people_in_realm = [{email: 'othello@zulip.com', full_name: "Othello, Moor of Venice"},
+					   {email: 'cordelia@zulip.com', full_name: "Cordelia Lear"}];
+var emoji_list = [{emoji_name: "tada", emoji_url: "TBD"},
+			      {emoji_name: "moneybags", emoji_url: "TBD"}];
+
+set_global('page_params', {people_list: people_in_realm});
+set_global('emoji', {emojis: emoji_list});
+
 (function test_add_topic () {
     ct.add_topic('Denmark', 'civil fears');
     ct.add_topic('devel', 'fading');
@@ -15,4 +23,36 @@ var ct = require('js/composebox_typeahead.js');
     ct.add_topic('Denmark', 'With Twisted Metal');
 
     assert.deepEqual(ct.topics_seen_for('Denmark'), ['With Twisted Metal', 'acceptance', 'civil fears']);
+}());
+
+(function test_begins_typeahead () {
+	// Stub out split_at_cursor that uses $(':focus')
+	ct.split_at_cursor = function (word) { return [word, '']; };
+
+    var begin_typehead_this = {options: {completions: {emoji: true,
+                                                      mention: true}}};
+
+    function assert_typeahead_equals(input, reference) {
+        var returned = ct.compose_content_begins_typeahead.call(begin_typehead_this, input);
+        assert.deepEqual(returned, reference);
+    }
+
+    assert_typeahead_equals("test", false);
+    assert_typeahead_equals("test one two", false);
+    assert_typeahead_equals("test *", false);
+    assert_typeahead_equals("test @", false);
+    assert_typeahead_equals("test no@o", false);
+    assert_typeahead_equals("test :-P", false);
+
+    var all_item = { special_item_text: 'all (Notify everyone)', email: 'all', pm_recipient_count: Infinity, full_name: 'all' };
+    var people_with_all = people_in_realm.concat([all_item]);
+
+    assert_typeahead_equals("test @o", people_with_all);
+    assert_typeahead_equals("test @z", people_with_all);
+    assert_typeahead_equals("@zuli", people_with_all);
+
+    assert_typeahead_equals("hi emoji :", false);
+    assert_typeahead_equals("hi emoji :ta", emoji_list);
+    assert_typeahead_equals("hi emoji :da", emoji_list);
+
 }());
