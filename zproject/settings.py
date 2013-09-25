939c5c0e000f063368050f9ac5c918bc55392c5d
Tim Abbott <tabbott@zulip.com>
2013-09-25 14:45:48 -0400

    Remove unused ERROR_LOG_DIR variable.
    (imported from commit 03d054af4e74297c82e947e315f2dee954d03151)

--- zproject/settings.py
+++ zproject/settings.py
@@ -449,14 +449,12 @@ CACHES = {
 if DEPLOYED:
     SERVER_LOG_PATH = "/var/log/zulip/server.log"
     EVENT_LOG_DIR = '/home/humbug/logs/event_log'
-    ERROR_LOG_DIR = '/home/humbug/logs/errors'
     STATS_DIR = '/home/humbug/stats'
     PERSISTENT_QUEUE_FILENAME = "/home/humbug/tornado/event_queues.pickle"
     EMAIL_LOG_PATH = "/var/log/zulip/email-mirror.log"
 else:
     EVENT_LOG_DIR = 'event_log'
     SERVER_LOG_PATH = "server.log"
-    ERROR_LOG_DIR = 'errors'
     STATS_DIR = 'stats'
     PERSISTENT_QUEUE_FILENAME = "event_queues.pickle"
     EMAIL_LOG_PATH = "email-mirror.log"
