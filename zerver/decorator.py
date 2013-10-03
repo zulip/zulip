97adbedfcaf126d0795212c6e8e1fe5e4c38897e
Steve Howell <steve@zulip.com>
2013-10-03 13:48:03 -0400

    Support request._query in update_user_activity().
    If request._query is set in the call to update_user_activity(),
    we will use that instead of request.META['PATH_INFO'] for
    the query field of the UserActivity row we write.
    
    (imported from commit fcee30098e1c7c5cb4195a1e5905fc7b88af804f)

--- zerver/decorator.py
+++ zerver/decorator.py
@@ -43,7 +43,13 @@ def update_user_activity(request, user_profile):
     # redundant to log that here as well.
     if request.META["PATH_INFO"] == '/json/update_active_status':
         return
-    event={'query': request.META["PATH_INFO"],
+
+    if hasattr(request, '_query'):
+        query = request._query
+    else:
+        query = request.META['PATH_INFO']
+
+    event={'query': query,
            'user_profile_id': user_profile.id,
            'time': datetime_to_timestamp(now()),
            'client': request.client.name}
