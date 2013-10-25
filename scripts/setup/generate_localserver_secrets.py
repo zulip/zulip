38c5629505389719f73c657939e0606eb2997383
Tim Abbott <tabbott@zulip.com>
2013-10-25 17:34:44 -0400

    Move generate_localserver_secrets.py to scripts/setup.
    (imported from commit 8d9ca2fc78700a38ec3ce5866b2b1032aabe1635)

--- scripts/setup/generate_localserver_secrets.py
+++ scripts/setup/generate_localserver_secrets.py
@@ -3,7 +3,7 @@
 
 import sys, os, os.path
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'zproject.settings'
 
 from django.utils.crypto import get_random_string
