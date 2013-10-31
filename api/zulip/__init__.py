3be10e50ef51146791c832273ae4ee21343557dd
Luke Faraone <lfaraone@zulip.com>
2013-10-31 10:48:36 -0400

    API 0.2.2 release.
    (imported from commit 891ab7549e96c30dceb70bb18add1cd533fafd36)

--- api/zulip/__init__.py
+++ api/zulip/__init__.py
@@ -34,7 +34,7 @@ from ConfigParser import SafeConfigParser
 import logging
 
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 # Check that we have a recent enough version
 # Older versions don't provide the 'json' attribute on responses.
