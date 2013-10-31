0e35424a7cd9340f7b9b4429cd25ee9e9f4305f1
Tim Abbott <tabbott@zulip.com>
2013-10-31 14:07:43 -0400

    Rename humbug => zulip in some log file names.
    (imported from commit 0bb5284566861e5a2f260cc66dcb53ebac0d7b97)

--- zproject/test_settings.py
+++ zproject/test_settings.py
@@ -21,7 +21,7 @@ else:
 POLL_TIMEOUT = 1000
 
 # Don't use the real message log for tests
-EVENT_LOG_DIR = '/tmp/humbug-test-event-log'
+EVENT_LOG_DIR = '/tmp/zulip-test-event-log'
 
 # Print our emails rather than sending them
 EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
