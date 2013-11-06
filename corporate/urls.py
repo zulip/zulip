6193f60d4536637631ca08d41781732a8c024ab0
Steve Howell <steve@zulip.com>
2013-11-06 06:50:13 -0500

    Move static MIT-related pages to corporate app.
    (imported from commit e2c5f72d3f3e4d8f8fa888326b2ba455beb9156c)

--- corporate/urls.py
+++ corporate/urls.py
@@ -5,4 +5,9 @@ urlpatterns = patterns('',
     # Job postings
     url(r'^jobs/$', TemplateView.as_view(template_name='corporate/jobs/index.html')),
     url(r'^jobs/lead-designer/$', TemplateView.as_view(template_name='corporate/jobs/lead-designer.html')),
+
+    # Zephyr/MIT
+    url(r'^zephyr/$', TemplateView.as_view(template_name='corporate/zephyr.html')),
+    url(r'^mit/$', TemplateView.as_view(template_name='corporate/mit.html')),
+    url(r'^zephyr-mirror/$', TemplateView.as_view(template_name='corporate/zephyr-mirror.html')),
 )
