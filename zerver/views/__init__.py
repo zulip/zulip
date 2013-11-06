591d06cb240f40b3fe2ef0c66706bb1a36a45d4f
Leo Franchi <leo@zulip.com>
2013-11-06 15:41:46 -0500

    Only log valid stats to statsd
    (imported from commit 449b7f5e750913c04e4df8f2a895274ded99ddd9)

--- zerver/views/__init__.py
+++ zerver/views/__init__.py
@@ -1884,8 +1884,10 @@ def json_report_send_time(request, user_profile,
                           displayed=REQ(converter=to_non_negative_int, default="(unknown)")):
     logging.info("End-to-end send time: %sms/%sms/%sms (%s)" % (time, received, displayed, user_profile.email))
     statsd.timing("endtoend.send_time.%s" % (statsd_key(user_profile.realm.domain),), time)
-    statsd.timing("endtoend.receive_time.%s" % (statsd_key(user_profile.realm.domain),), received)
-    statsd.timing("endtoend.displayed_time.%s" % (statsd_key(user_profile.realm.domain),), displayed)
+    if received != "(unknown)":
+        statsd.timing("endtoend.receive_time.%s" % (statsd_key(user_profile.realm.domain),), received)
+    if displayed != "(unknown)":
+        statsd.timing("endtoend.displayed_time.%s" % (statsd_key(user_profile.realm.domain),), displayed)
     return json_success()
 
 @authenticated_json_post_view
