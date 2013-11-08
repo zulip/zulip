442ae115a2a5f8c327604b22286164293316fba0
Tim Abbott <tabbott@zulip.com>
2013-11-08 16:45:20 -0500

    Remove legacy check_output implementation for pre-2.7 Pythons.
    We still need it in integrations, because those don't require Python
    2.7, but we don't need it in any of our code that runs on internal
    servers.
    
    (imported from commit 3c340567f1a372dcb4206c6af9a6e5e18005b1b8)

--- zulip_tools.py
+++ zulip_tools.py
@@ -1,21 +1,6 @@
 import subprocess
 import os
 
-# check_output is backported from subprocess.py in Python 2.7
-
-def check_output(*popenargs, **kwargs):
-    if 'stdout' in kwargs:
-        raise ValueError('stdout argument not allowed, it will be overridden.')
-    process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
-    output, unused_err = process.communicate()
-    retcode = process.poll()
-    if retcode:
-        cmd = kwargs.get("args")
-        if cmd is None:
-            cmd = popenargs[0]
-        raise subprocess.CalledProcessError(retcode, cmd, output=output)
-    return output
-
 DEPLOYMENTS_DIR = "/home/zulip/deployments"
 LOCK_DIR = os.path.join(DEPLOYMENTS_DIR, "lock")
 TIMESTAMP_FORMAT = '%Y-%m-%d-%H-%M-%S'
