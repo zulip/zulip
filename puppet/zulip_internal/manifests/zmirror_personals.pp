709850ab9ef79278ad842baf667494d991ee9cd8
Tim Abbott <tabbott@zulip.com>
2013-11-08 15:16:02 -0500

    puppet: Organize the puppet configuration for zmirror.
    (imported from commit 4e9d1771fd5198b1f47f8fac187b915287568510)

--- puppet/zulip_internal/manifests/zmirror_personals.pp
+++ puppet/zulip_internal/manifests/zmirror_personals.pp
@@ -2,8 +2,20 @@ class zulip_internal::zmirror_personals {
   include zulip_internal::base
   include zulip::supervisor
 
-  $zmirror_packages = [ "cython", "libzephyr-dev", "comerr-dev", "python-dev", "libzephyr4-krb5", "zephyr-clients",
-                        "krb5-config", "krb5-user", "krb5-clients", "debathena-kerberos-config", "debathena-zephyr-config"]
+  $zmirror_packages = [# Packages needed to run the mirror
+                       "libzephyr4-krb5",
+                       "zephyr-clients",
+                       "krb5-config",
+                       "krb5-user",
+                       "krb5-clients",
+                       "debathena-kerberos-config",
+                       "debathena-zephyr-config",
+                       # Packages needed to build pyzephyr
+                       "libzephyr-dev",
+                       "comerr-dev",
+                       "python-dev",
+                       "cython",
+                       ]
   package { $zmirror_packages: ensure => "installed" }
 
   file { '/etc/apt/sources.list.d/debathena.list':
