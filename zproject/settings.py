828917cac84d7d8bff0fba6fd4ab02f192f24325
Tim Abbott <tabbott@zulip.com>
2013-11-08 11:44:43 -0500

    Enabled the Django cached template loader.
    This was recommended by:
    
    https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
    
    Since we don't change our deployments without restarting Django and
    don't use any custom template magic, this should be a free performance
    win.
    
    (imported from commit fd498ab97d0669c3a14b342b2d2f01994a1f1ee1)

--- zproject/settings.py
+++ zproject/settings.py
@@ -138,8 +138,10 @@ USE_X_FORWARDED_HOST = True
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
 )
 
 MIDDLEWARE_CLASSES = (
