031dfbcc46019a0a82e7c4fc8b2db9946d7d58dc
Zev Benjamin <zev@zulip.com>
2013-11-13 14:57:31 -0500

    zulip_tools.py: Add a make_deploy_path function and make it invokable from the commandline
    (imported from commit 94578d117864fba76f9353784734c712c89c4bf2)

--- zulip_tools.py
+++ zulip_tools.py
@@ -1,5 +1,7 @@
-import subprocess
+#!/usr/bin/python
 import os
+import sys
+import datetime
 
 DEPLOYMENTS_DIR = "/home/zulip/deployments"
 LOCK_DIR = os.path.join(DEPLOYMENTS_DIR, "lock")
@@ -11,3 +13,12 @@ OKGREEN = '\033[92m'
 WARNING = '\033[93m'
 FAIL = '\033[91m'
 ENDC = '\033[0m'
+
+def make_deploy_path():
+    timestamp = datetime.datetime.now().strftime(TIMESTAMP_FORMAT)
+    return os.path.join(DEPLOYMENTS_DIR, timestamp)
+
+if __name__ == '__main__':
+    cmd = sys.argv[1]
+    if cmd == 'make_deploy_path':
+        print make_deploy_path()
