85ef4988e1f25b586d8dff63b4ade83a2222849f
Tim Abbott <tabbott@zulip.com>
2013-12-11 11:53:11 -0500

    Add api_key to filtered variables.
    We don't use it yet, but the plan is the migrate there and it's better
    to just have the filtering in place.
    
    (imported from commit d0e7f40e8a439b8e8751da954e79b5f67226e5a9)

--- zerver/filters.py
+++ zerver/filters.py
@@ -7,7 +7,7 @@ class ZulipExceptionReporterFilter(SafeExceptionReporterFilter):
     def get_post_parameters(self, request):
         filtered_post = SafeExceptionReporterFilter.get_post_parameters(self, request).copy()
         filtered_vars = ['content', 'secret', 'password', 'key', 'api-key', 'subject', 'stream',
-                         'subscriptions', 'to', 'csrfmiddlewaretoken']
+                         'subscriptions', 'to', 'csrfmiddlewaretoken', 'api_key']
 
         for var in filtered_vars:
             if var in filtered_post:
