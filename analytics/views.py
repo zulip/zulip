ea1fefa5b544ff6d25b6eff67c4aa20410bf958a
Steve Howell <steve@zulip.com>
2013-12-19 14:01:49 -0500

    Show message history in reverse chronological order.
    (imported from commit 6237369793044cec7ae8ffec42aaac5d443e2ce1)

--- analytics/views.py
+++ analytics/views.py
@@ -75,7 +75,7 @@ def get_realm_day_counts():
 
     result = {}
     for domain in counts:
-        cnts = [counts[domain].get(age, 0) for age in reversed(range(8))]
+        cnts = [counts[domain].get(age, 0) for age in range(8)]
         min_cnt = min(cnts)
         max_cnt = max(cnts)
 
