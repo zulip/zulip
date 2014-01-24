6d9edcf06fc8ed58694fa9372b6f45650b309454
Leo Franchi <leo@zulip.com>
2014-01-24 16:03:34 -0500

    Re-enable local echo for staging
    (imported from commit e8b579e2289271732d3998cbe16854e39e55d5d6)

--- static/js/feature_flags.js
+++ static/js/feature_flags.js
@@ -43,7 +43,7 @@ exports.show_autoscroll_forever_option = page_params.show_autoscroll_forever_opt
 // Still very beta:
 
 exports.full_width = false; //page_params.staging;
-exports.local_echo = false; // page_params.staging;
+exports.local_echo = page_params.staging;
 
 exports.show_digest_email_setting = page_params.staging;
 
