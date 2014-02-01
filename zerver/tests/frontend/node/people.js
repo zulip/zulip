fa69047613510e877314ab3ce188fe41e071a666
Steve Howell <steve@zulip.com>
2014-02-01 10:14:40 -0500

    Add node test for people.js (just the basics for now).
    (imported from commit 2f4714bedd54c731667160c65280f386061ce803)

---
+++ zerver/tests/frontend/node/people.js
@@ -0,0 +1,43 @@
+var assert = require('assert');
+var people = require("js/people.js");
+
+set_global('page_params', {
+    people_list: []
+});
+set_global('activity', {
+    set_user_statuses: function () {}
+});
+
+(function test_basics() {
+    var full_name = 'Isaac Newton';
+    var email = 'isaac@example.com';
+    var isaac = {
+        email: email,
+        full_name: full_name
+    };
+    people.add(isaac);
+
+    var person = people.get_by_name(full_name);
+    assert.equal(person.email, email);
+    person = people.get_by_email(email);
+    assert.equal(person.full_name, full_name);
+    person = people.realm_get(email);
+    assert(!person);
+    people.add_in_realm(isaac);
+    person = people.realm_get(email);
+    assert.equal(person.email, email);
+
+    people.update({email: email, is_admin: true});
+    person = people.get_by_email(email);
+    assert.equal(person.full_name, full_name);
+    assert.equal(person.is_admin, true);
+
+    people.update({email: email, full_name: 'Sir Isaac'});
+    person = people.get_by_email(email);
+    assert.equal(person.full_name, 'Sir Isaac');
+    assert.equal(person.is_admin, true);
+
+    people.remove(person);
+    person = people.get_by_email(email);
+    assert(!person);
+}());
