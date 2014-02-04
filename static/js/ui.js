515e64175fd10615ed409fefe173f03ce6a4ba0f
Jason Michalski <armooo@zulip.com>
2014-02-04 07:48:03 -0500

    Add extra logging when the selected row can not be found in the cache
    The leading theories this will test are:
    
    1) MessageList._items becomes unsorted and the binary search starts
    returning the wrong index from selected_idx.
    
    2) MessageListView.render is not rendering the message or it is not
    being inserted into the cache.
    
    (imported from commit 926b7d7bf9df338320a8cba6241038176ce4b47d)

--- static/js/ui.js
+++ static/js/ui.js
@@ -1453,13 +1453,20 @@ $(function () {
 
         if (event.then_scroll) {
             if (row.length === 0) {
+                var row_from_dom = $('.message_row[zid=' + event.id  + ']');
                 blueslip.debug("message_selected missing selected row", {
                     previously_selected: event.previously_selected,
                     selected_id: event.id,
                     selected_idx: event.msg_list.selected_idx(),
                     selected_idx_exact: event.msg_list._items.indexOf(event.msg_list.get(event.id)),
                     render_start: event.msg_list.view._render_win_start,
-                    render_end: event.msg_list.view._render_win_end
+                    render_end: event.msg_list.view._render_win_end,
+                    selected_id_from_idx: event.msg_list._items[event.msg_list.selected_idx()].id,
+                    msg_list_sorted: _.isEqual(
+                        _.pluck(event.msg_list._items, 'id'),
+                        _.chain(current_msg_list._items).pluck('id').clone().value().sort()
+                    ),
+                    found_in_dom: row_from_dom.length
                 });
             }
 
