639a39aaceaf331d4a10e883ab856a110da1551b
Steve Howell <steve@zulip.com>
2014-02-07 16:47:30 -0500

    Move some code into test_decorators.py
    (imported from commit 7133c0fe1ae3028098b72ac4e14693e540aac4d7)

--- zerver/tests.py
+++ zerver/tests.py
@@ -14,9 +14,7 @@ from zerver.models import UserProfile, Recipient, \
     UserMessage, \
     get_user_profile_by_email, split_email_to_domain, get_realm, \
     get_client
-from zerver.decorator import \
-    REQ, has_request_variables, json_to_list, RequestVariableMissingError, \
-    RequestVariableConversionError, JsonableError
+
 from zerver.lib.initial_password import initial_password
 from zerver.lib.actions import \
     get_emails_from_user_ids, do_deactivate_user, do_reactivate_user, \
@@ -24,8 +22,6 @@ from zerver.lib.actions import \
     do_set_realm_name, get_realm_name, do_deactivate_realm
 from zerver.lib.alert_words import alert_words_in_realm, user_alert_words, \
     add_user_alert_words, remove_user_alert_words
-from zerver.lib.validator import check_string, check_list, check_dict, \
-    check_bool, check_int
 from zerver.middleware import is_slow_query
 
 from zerver.worker import queue_processors
@@ -71,151 +67,6 @@ class SlowQueryTest(TestCase):
         self.assertFalse(is_slow_query(2, '/user_activity/whatever'))
         self.assertFalse(is_slow_query(9, '/accounts/webathena_kerberos_login/'))
         self.assertTrue(is_slow_query(11, '/accounts/webathena_kerberos_login/'))
-
-class DecoratorTestCase(TestCase):
-    def test_REQ_converter(self):
-
-        @has_request_variables
-        def get_total(request, numbers=REQ(converter=json_to_list)):
-            return sum(numbers)
-
-        class Request:
-            pass
-
-        request = Request()
-        request.REQUEST = {}
-
-        with self.assertRaises(RequestVariableMissingError):
-            get_total(request)
-
-        request.REQUEST['numbers'] = 'bad_value'
-        with self.assertRaises(RequestVariableConversionError) as cm:
-            get_total(request)
-        self.assertEqual(str(cm.exception), "Bad value for 'numbers': bad_value")
-
-        request.REQUEST['numbers'] = ujson.dumps([1,2,3,4,5,6])
-        result = get_total(request)
-        self.assertEqual(result, 21)
-
-    def test_REQ_validator(self):
-
-        @has_request_variables
-        def get_total(request, numbers=REQ(validator=check_list(check_int))):
-            return sum(numbers)
-
-        class Request:
-            pass
-
-        request = Request()
-        request.REQUEST = {}
-
-        with self.assertRaises(RequestVariableMissingError):
-            get_total(request)
-
-        request.REQUEST['numbers'] = 'bad_value'
-        with self.assertRaises(JsonableError) as cm:
-            get_total(request)
-        self.assertEqual(str(cm.exception), 'argument "numbers" is not valid json.')
-
-        request.REQUEST['numbers'] = ujson.dumps([1,2,"what?",4,5,6])
-        with self.assertRaises(JsonableError) as cm:
-            get_total(request)
-        self.assertEqual(str(cm.exception), 'numbers[2] is not an integer')
-
-        request.REQUEST['numbers'] = ujson.dumps([1,2,3,4,5,6])
-        result = get_total(request)
-        self.assertEqual(result, 21)
-
-class ValidatorTestCase(TestCase):
-    def test_check_string(self):
-        x = "hello"
-        self.assertEqual(check_string('x', x), None)
-
-        x = 4
-        self.assertEqual(check_string('x', x), 'x is not a string')
-
-    def test_check_bool(self):
-        x = True
-        self.assertEqual(check_bool('x', x), None)
-
-        x = 4
-        self.assertEqual(check_bool('x', x), 'x is not a boolean')
-
-    def test_check_int(self):
-        x = 5
-        self.assertEqual(check_int('x', x), None)
-
-        x = [{}]
-        self.assertEqual(check_int('x', x), 'x is not an integer')
-
-    def test_check_list(self):
-        x = 999
-        error = check_list(check_string)('x', x)
-        self.assertEqual(error, 'x is not a list')
-
-        x = ["hello", 5]
-        error = check_list(check_string)('x', x)
-        self.assertEqual(error, 'x[1] is not a string')
-
-        x = [["yo"], ["hello", "goodbye", 5]]
-        error = check_list(check_list(check_string))('x', x)
-        self.assertEqual(error, 'x[1][2] is not a string')
-
-        x = ["hello", "goodbye", "hello again"]
-        error = check_list(check_string, length=2)('x', x)
-        self.assertEqual(error, 'x should have exactly 2 items')
-
-    def test_check_dict(self):
-        keys = [
-            ('names', check_list(check_string)),
-            ('city', check_string),
-        ]
-
-        x = {
-            'names': ['alice', 'bob'],
-            'city': 'Boston',
-        }
-        error = check_dict(keys)('x', x)
-        self.assertEqual(error, None)
-
-        x = 999
-        error = check_dict(keys)('x', x)
-        self.assertEqual(error, 'x is not a dict')
-
-        x = {}
-        error = check_dict(keys)('x', x)
-        self.assertEqual(error, 'names key is missing from x')
-
-        x = {
-            'names': ['alice', 'bob', {}]
-        }
-        error = check_dict(keys)('x', x)
-        self.assertEqual(error, 'x["names"][2] is not a string')
-
-        x = {
-            'names': ['alice', 'bob'],
-            'city': 5
-        }
-        error = check_dict(keys)('x', x)
-        self.assertEqual(error, 'x["city"] is not a string')
-
-    def test_encapsulation(self):
-        # There might be situations where we want deep
-        # validation, but the error message should be customized.
-        # This is an example.
-        def check_person(val):
-            error = check_dict([
-                ['name', check_string],
-                ['age', check_int],
-            ])('_', val)
-            if error:
-                return 'This is not a valid person'
-
-        person = {'name': 'King Lear', 'age': 42}
-        self.assertEqual(check_person(person), None)
-
-        person = 'misconfigured data'
-        self.assertEqual(check_person(person), 'This is not a valid person')
 
 class RealmTest(AuthedTestCase):
     def assert_user_profile_cache_gets_new_name(self, email, new_realm_name):
