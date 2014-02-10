bb60e9691060a538ef6a38f47770dc891871c7c4
Jessica McKellar <jesstess@zulip.com>
2014-02-10 13:49:17 -0500

    Enable digest emails for CUSTOMER4.
    (imported from commit 08b4adb4fd95fdfd661ccbd9cdb5782bf0d743f0)

--- zerver/management/commands/enqueue_digest_emails.py
+++ zerver/management/commands/enqueue_digest_emails.py
@@ -92,8 +92,7 @@ in a while.
 
         all_domains = Realm.objects.filter(
             deactivated=False).values_list('domain', flat=True)
-        non_digest_domains = set(("users.customer4.invalid", "mit.edu",
-                                  "customer29.invalid", "customer20.invalid"))
+        non_digest_domains = set(("mit.edu", "customer29.invalid", "customer20.invalid"))
         digest_domains = set(all_domains) - non_digest_domains
 
         deployment_domains = domains_for_this_deployment()
