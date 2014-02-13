01a04c31698dedfce5819016cdebecfd3d9a12bc
Allen Rabinovich <allenr@zulip.com>
2014-02-12 17:21:04 -0700

    Add offsets to fixed elements based on the width of the browser scrollbar.
    (imported from commit 2026725165dead381d0b99181ef84a67a5bd6626)

--- static/js/ui.js
+++ static/js/ui.js
@@ -352,6 +352,16 @@ function set_user_list_heights(res, usable_height, user_presences, group_pms) {
     res.group_pms_max_height = blocks[1].max_height;
 }
 
+function scrollbarWidth() {
+    $('body').prepend('<div id="outertest" style="width:200px; height:150px; position: absolute; top: 0; left: 0; overflow-x:hidden; overflow-y:scroll; background: #ff0000; visibility: hidden;"><div id="innertest" style="width:100%; height: 200px; overflow-y: visible;">&nbsp;</div></div>');
+
+    var scrollwidth = $("#outertest").outerWidth() - $("#innertest").outerWidth();
+
+    $("#outertest").remove();
+
+    return scrollwidth;
+}
+
 function get_new_heights() {
     var res = {};
     var viewport_height = viewport.height();
@@ -1875,6 +1885,31 @@ $(function () {
     }
 });
 
+// Workaround for browsers with fixed scrollbars
+$(function () {
+
+
+   var sbWidth = scrollbarWidth();
+
+   if (sbWidth > 0) {
+
+    $(".header").css("left", "-" + sbWidth + "px");
+    $(".header-main").css("left", sbWidth + "px");
+    $(".header-main").css("max-width", (1400 + sbWidth) + "px");
+    $(".header-main .column-middle").css("margin-right", (250 + sbWidth) + "px");
+
+    $(".fixed-app").css("left", "-" + sbWidth + "px");
+    $(".fixed-app .app-main").css("max-width", (1400 + sbWidth) + "px");
+    $(".fixed-app .column-middle").css("margin-left", (250 + sbWidth) + "px");
+
+    $("#compose").css("left", "-" + sbWidth + "px");
+    $(".compose-content").css({"left": sbWidth + "px",
+                               "margin-right": (250 + sbWidth) + "px"});
+    $("#compose-container").css("max-width", (1400 + sbWidth) + "px");
+
+   }
+
+});
 
 return exports;
 }());
