7822be5cd937ded748556d8e9ac1391c80506bd5
Allen Rabinovich <allenr@zulip.com>
2014-02-18 11:47:37 -0700

    Fix failing stream sidebar node test
    (imported from commit 3187a8f34389bf1f0b37e80edec9fb56f7281306)

--- static/js/stream_list.js
+++ static/js/stream_list.js
@@ -152,6 +152,7 @@ exports.add_stream_to_sidebar = function (stream_name) {
 exports.redraw_stream_privacy = function (stream_name) {
     var li = exports.get_stream_li(stream_name);
     var div = li.find('.stream-privacy');
+    var swatch = li.find('.streamlist_swatch');
     var sub = stream_data.get_sub(stream_name);
     var color = stream_data.get_color(stream_name);
     var dark_background = stream_color.get_color_class(color);
@@ -160,6 +161,13 @@ exports.redraw_stream_privacy = function (stream_name) {
         invite_only: sub.invite_only,
         dark_background: dark_background
     };
+
+    if (sub.invite_only) {
+        swatch.addClass("private-stream-swatch");
+    }
+    else {
+        swatch.removeClass("private-stream-swatch");
+    }
 
     var html = templates.render('stream_privacy', args);
     div.html(html);
