148959f1b7a2a7200c50a32080dc3bd2b5590932
acrefoot <acrefoot@zulip.com>
2014-05-05 21:55:33 -0700

    Rewrite humbug-user-uploads.s3.amazonaws.com urls
    add_messages is a good entrypoint for this, since it gets called by:
    1) get_old_messages
    2) get_events_success (for new messages, via insert_new_messages)
    
    which is all the places that rewrites should happen, but nowhere
    where extra work is being done.
    
    (imported from commit 844c33bc32d35aa39c9cdacf42eb7e8ddf5ae63c)

--- static/js/message_store.js
+++ static/js/message_store.js
@@ -8,6 +8,9 @@ var load_more_enabled = true;
 // since the last time that we ran load_more_messages(), we do
 // not load_more_messages().
 
+// used for our URL rewriting in insert_new_messages
+var humbug_images_re = new RegExp("https://humbug-user-uploads.s3.amazonaws.com/([^\"]+)", 'g');
+
 exports.get = function get(message_id) {
     return stored_messages[message_id];
 };
@@ -150,6 +153,10 @@ exports.add_messages = function add_messages(messages, msg_list, opts) {
     }
 
     opts = _.extend({messages_are_new: false, delay_render: false}, opts);
+
+    _.each(messages, function (msg) {
+        msg.content = msg.content.replace(humbug_images_re, "/user_uploads/unk/$1");
+    });
 
     loading.destroy_indicator($('#page_loading_indicator'));
     $('#first_run_message').remove();
