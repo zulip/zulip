1b5f9e4374355c6921bb3fc3ad21a62184d6ca9b
Anders Kaseorg <andersk@mit.edu>
2015-08-20 16:29:55 -0700

    Remove corporate jobs pages
    (imported from commit 3f1789f6a4f90a33ddd2352fab85d3cf751b0f46)

--- corporate/urls.py
+++ corporate/urls.py
@@ -2,10 +2,6 @@ from django.conf.urls import patterns, url
 from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = patterns('',
-    # Job postings
-    url(r'^jobs/$', TemplateView.as_view(template_name='corporate/jobs/index.html')),
-    url(r'^jobs/lead-designer/$', TemplateView.as_view(template_name='corporate/jobs/lead-designer.html')),
-
     # Zephyr/MIT
     url(r'^zephyr/$', TemplateView.as_view(template_name='corporate/zephyr.html')),
     url(r'^mit/$', TemplateView.as_view(template_name='corporate/mit.html')),
