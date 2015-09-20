3b84a9a010526669b711fbfe72737c8eb2ef42ef
Tim Abbott <tabbott@zulip.com>
2015-09-19 19:10:27 -0700

    Modernize management command for deactivating a realm.
    (imported from commit 9ef681193ff04e447c0afbba66187357624d98f3)

--- zerver/management/commands/deactivate_realm.py
+++ zerver/management/commands/deactivate_realm.py
@@ -5,13 +5,14 @@ from django.core.management.base import BaseCommand
 from zerver.lib.actions import do_deactivate_realm
 from zerver.models import get_realm
 
-domains_to_deactivate = """"""
-
 class Command(BaseCommand):
     help = """One-off script to deactivate our old realms."""
 
+    def add_arguments(self, parser):
+        parser.add_argument('domain', metavar='<domain>', type=str,
+                            help='domain of realm to deactivate')
+
     def handle(self, *args, **options):
-        for domain in [elt.strip() for elt in domains_to_deactivate.split("\n")]:
-            print "Deactivating", domain
-            do_deactivate_realm(get_realm(domain))
+        print "Deactivating", options["domain"]
+        do_deactivate_realm(get_realm(options["domain"]))
         print "Done!"
