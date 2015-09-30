956fd7c420b4a725b982ba565d92f4907064a57a
Guillaume Simon <ipernet@gmail.com>
2015-09-30 09:17:08 -0700

    puppet: Ensure rabbitmq-server and epmd services are running.
    [tabbott@mit.edu: Added a few comments]

--- puppet/zulip/manifests/rabbit.pp
+++ puppet/zulip/manifests/rabbit.pp
@@ -1,5 +1,6 @@
 class zulip::rabbit {
   $rabbit_packages = [# Needed to run rabbitmq
+                      "erlang-base",
                       "rabbitmq-server",
                       ]
   package { $rabbit_packages: ensure => "installed" }
@@ -37,6 +38,22 @@ class zulip::rabbit {
     group  => "root",
     mode => 644,
     source => "puppet:///modules/zulip/rabbitmq/rabbitmq.config",
+  }
+
+  # epmd doesn't have an init script.  This won't leak epmd processes
+  # because epmd checks if one is already running and exits if so.
+  #
+  # TODO: Ideally we'd still check if it's already running to keep the
+  # puppet log for what is being changed clean
+  exec { "epmd":
+    command => "epmd -daemon",
+    require => Package[erlang-base],
+    path    => "/usr/bin/:/bin/",
+  }
+
+  service { "rabbitmq-server":
+    ensure => running,
+    require => Exec["epmd"],
   }
 
   # TODO: Should also call exactly once "configure-rabbitmq"
