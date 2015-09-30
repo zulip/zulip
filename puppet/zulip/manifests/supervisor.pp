b2e15eba783413d87d957af278449c9aaba9c153
ip <ipernet@gmail.com>
2015-09-30 09:57:01 +0200

    Fallback to class stop/start for puppet (#61)

--- puppet/zulip/manifests/supervisor.pp
+++ puppet/zulip/manifests/supervisor.pp
@@ -11,18 +11,7 @@ class zulip::supervisor {
                 ],
 
     hasstatus => true,
-    status => "supervisorctl status",
-
-    # The "restart" option in the init script does not work.  We could
-    # tell Puppet to fall back to stop/start, which does work, but the
-    # better option is to tell supervisord to reread its config via
-    # supervisorctl and then to "update".  You need to do both --
-    # after a "reread", supervisor won't actually take actual based on
-    # the changed configuration until you do an "update" (I assume
-    # this is so you can check if your config file parses without
-    # doing anything, but it's really confusing)
-    hasrestart => true,
-    restart => "bash -c 'supervisorctl reread && supervisorctl update'"
+    status => "supervisorctl status"
   }
 
   file { "/etc/supervisor/supervisord.conf":
