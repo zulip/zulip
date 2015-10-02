77fad7a16ee7917fc71fe3a7897a36cdacbdb091
Darren Worrall <darren@iweb.co.uk>
2015-10-02 12:23:26 +0100

    Add an api endpoint to fetch GOOGLE_CLIENT_ID
    Further to #102, this provides an endpoint suitable for mobile apps to
    consume the GOOGLE_CLIENT_ID if configured.

--- zerver/views/__init__.py
+++ zerver/views/__init__.py
@@ -1881,6 +1881,12 @@ def json_fetch_api_key(request, user_profile, password=REQ(default='')):
         return json_error("Your username or password is incorrect.")
     return json_success({"api_key": user_profile.api_key})
 
+@csrf_exempt
+def api_fetch_google_client_id(request):
+    if not settings.GOOGLE_CLIENT_ID:
+        return json_error("GOOGLE_CLIENT_ID is not configured", status=400)
+    return json_success({"google_client_id": settings.GOOGLE_CLIENT_ID})
+
 def get_status_list(requesting_user_profile):
     return {'presences': get_status_dict(requesting_user_profile),
             'server_timestamp': time.time()}
