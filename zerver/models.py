08195e0b912f128ea8c64d34c3a8fb97fd221a20
Lauren Long <laurenzlong@gmail.com>
2015-10-14 14:39:35 -0400

    added starred field to Subscription model

--- zerver/models.py
+++ zerver/models.py
@@ -1048,6 +1048,7 @@ class Subscription(models.Model):
 
     DEFAULT_STREAM_COLOR = "#c2c2c2"
     color = models.CharField(max_length=10, default=DEFAULT_STREAM_COLOR)
+    starred = models.BooleanField(default=False)
 
     desktop_notifications = models.BooleanField(default=True)
     audible_notifications = models.BooleanField(default=True)
