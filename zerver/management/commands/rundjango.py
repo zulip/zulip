d7b7ae2d0fadb8072f98a1d08d084792f5a1fe76
Tim Abbott <tabbott@mit.edu>
2016-01-27 12:49:19 -0800

    rundjango: Fix confusing return line in log_message_monkey.

--- zerver/management/commands/rundjango.py
+++ zerver/management/commands/rundjango.py
@@ -6,7 +6,7 @@ def log_message_monkey(self, format, *args):
     # Filter output for 200 or 304 responses.
     if args[1] == '200' or args[1] == '304':
         return
-    return orig_log_message(self, format, *args)
+    orig_log_message(self, format, *args)
 
 WSGIRequestHandler.log_message = log_message_monkey
 
