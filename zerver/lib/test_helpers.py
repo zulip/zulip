fe626e1b27bd322bc07ad3bcb1c14e8ca9ff1c7a
Vladislav Manchev <manchev@bin.bz>
2016-01-30 16:31:34 +0200

    Fixing backend test problems

--- zerver/lib/test_helpers.py
+++ zerver/lib/test_helpers.py
@@ -213,7 +213,9 @@ class AuthedTestCase(TestCase):
         if password is None:
             password = initial_password(email)
         return self.client.post('/accounts/login/',
-                                {'username':email, 'password':password})
+                                {'auth-username':email,
+                                 'auth-password':password,
+                                 'login_view-current_step': 'auth'})
 
     def register(self, username, password, domain="zulip.com"):
         self.client.post('/accounts/home/',
