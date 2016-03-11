917feca8de868e976cb63fe98084cbefcb761ad0
Anindya Chakravarti <anindyac.iitr@gmail.com>
2016-03-11 19:53:03 +0530

    Testing webpage served by travis for test 12-subscription_announcement.js

--- frontend_tests/casper_tests/12-subscription_announcement.js
+++ frontend_tests/casper_tests/12-subscription_announcement.js
@@ -2,23 +2,32 @@ var common = require('../casper_lib/common.js').common;
 var test_credentials_secondary = require('../casper_lib/test_credentials_secondary.js').test_credentials_secondary;
 
 common.start_and_log_in(test_credentials_secondary);
+
 casper.then(function () {
-        casper.test.info('Settings page');
+        casper.test.info('Home Page');
         casper.click('a[href^="#"]');
+
+        var js = this.evaluate(function() {
+		return document; 
+		});	
+    	this.echo(js.all[0].outerHTML); 
+    	
         casper.test.assertUrlMatch(/^http:\/\/[^\/]+\//, 'URL suggests we are on home page');
         casper.test.assertTextExists('Subscribe to waseemio', "Stream waseemio created by iago exists");
-        this.clickLabel('Subscribe to waseemio', 'button');
+        // this.clickLabel('Subscribe to waseemio', 'button');
 });
- casper.waitForText('Unsubscribe from waseemio', function () {
-    casper.test.assertTextExists('Unsubscribe from waseemio', "Subsribed to waseemio, now testing Unsubscribe");
-    this.clickLabel('Unsubscribe from waseemio', 'button');
-});
-casper.waitForText('Subscribe to waseemio', function () {
-        casper.test.assertTextExists('Subscribe to waseemio', "Unsubscribed from waseemio");
-});
+//  casper.waitForText('Unsubscribe from waseemio', function () {
+//     casper.test.assertTextExists('Unsubscribe from waseemio', "Subsribed to waseemio, now testing Unsubscribe");
+//     this.clickLabel('Unsubscribe from waseemio', 'button');
+// });
+// casper.waitForText('Subscribe to waseemio', function () {
+//         casper.test.assertTextExists('Subscribe to waseemio', "Unsubscribed from waseemio");
+// });
 
 common.then_log_out();
 
 casper.run(function () {
     casper.test.done();
 });
+
+
