5bfe1c050c56da6e6d878074949dea2844aa3821
Eklavya Sharma <ekurgn@gmail.com>
2016-03-23 00:49:43 +0530

    Add argument modified_only to lister.py.
    Add option of only showing modified files in tools/lister.py.

--- tools/lister.py
+++ tools/lister.py
@@ -4,13 +4,18 @@ from __future__ import print_function
 import os
 import subprocess
 
-def list_files(targets=[]):
+def list_files(targets=[], modified_only=False):
     """
     List files tracked by git.
     Returns a list of files which are either in targets or in directories in targets.
     If targets is [], list of all tracked files in current directory is returned.
+
+    Other arguments:
+    modified_only - Only include files which have been modified.
     """
     cmdline = ['git', 'ls-files'] + targets
+    if modified_only:
+        cmdline.append('-m')
 
     files_gen = (x.strip() for x in subprocess.check_output(cmdline, universal_newlines=True).split('\n'))
     # throw away empty lines and non-files (like symlinks)
