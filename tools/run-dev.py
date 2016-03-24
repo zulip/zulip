946bbc5d1b3d004ace4c45a28c1cce54c5f1306e
Ryan Fletcher <ryansnowboarder@gmail.com>
2016-03-24 16:54:18 -0400

    ReAdd call to compile-handlebars-templates
    Whoops! Needed that...

--- tools/run-dev.py
+++ tools/run-dev.py
@@ -73,7 +73,8 @@ os.setpgrp()
 
 # Pass --nostatic because we configure static serving ourselves in
 # zulip/urls.py.
-cmds = [['./tools/webpack', 'watch'],
+cmds = [['./tools/compile-handlebars-templates', 'forever'],
+        ['./tools/webpack', 'watch'],
         ['python', 'manage.py', 'rundjango'] +
           manage_args + ['localhost:%d' % (django_port,)],
         ['python', 'manage.py', 'runtornado'] +
