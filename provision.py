398fc377a438e8ea56903e527483e857fbe0cfaa
Ryan Fletcher <ryansnowboarder@gmail.com>
2016-03-25 16:19:18 -0400

    separate provision.py into multiple stages
    npm install needs to be different for different host os systems, so a solution is to split the provision.py into at least 3 parts. Part 1 runs on all systems. Part 2 is platform-specific. Part 3 runs on all systems.

--- provision.py
+++ provision.py
@@ -175,25 +175,6 @@ def main():
     else:
         sh.npm.install(NO_SYM_LINK, **LOUD)
         
-
-    os.system("tools/download-zxcvbn")
-    os.system("tools/emoji_dump/build_emoji")
-    os.system("generate_secrets.py -d")
-    if "--travis" in sys.argv:
-        os.system("sudo service rabbitmq-server restart")
-        os.system("sudo service redis-server restart")
-        os.system("sudo service memcached restart")
-    elif "--docker" in sys.argv:
-        os.system("sudo service rabbitmq-server restart")
-        os.system("sudo pg_dropcluster --stop 9.3 main")
-        os.system("sudo pg_createcluster -e utf8 --start 9.3 main")
-        os.system("sudo service redis-server restart")
-        os.system("sudo service memcached restart")
-    sh.configure_rabbitmq(**LOUD)
-    sh.postgres_init_dev_db(**LOUD)
-    sh.do_destroy_rebuild_database(**LOUD)
-    sh.postgres_init_test_db(**LOUD)
-    sh.do_destroy_rebuild_test_database(**LOUD)
     return 0
 
 if __name__ == "__main__":
