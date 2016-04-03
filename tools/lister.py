be0a4f349d3f28dca354804e0a66e7c19658d78b
Eklavya Sharma <ekurgn@gmail.com>
2016-04-03 18:04:22 +0530

    Apply Python 3 futurize transform libmodernize.fixes.fix_filter.

--- tools/lister.py
+++ tools/lister.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
+from __future__ import absolute_import
 import os
 import sys
 import subprocess
 import re
 from collections import defaultdict
 import argparse
+from six.moves import filter
 
 def get_ftype(fpath, use_shebang):
     ext = os.path.splitext(fpath)[1]
