770f3155aae5671083329344b85718e43544bb50
lizonr1 <rafal.lizon@aalto.fi>
2016-04-14 15:47:49 +0300

    Add librato integration

--- zerver/tests/test_hooks.py
+++ zerver/tests/test_hooks.py
@@ -953,3 +953,49 @@ class YoHookTests(AuthedTestCase):
 
         msg = Message.objects.filter().order_by('-id')[0]
         self.assertEqual(msg.content, (u"Yo from IAGO"))
+
+
+class LibratoHookTests(AuthedTestCase):
+    def send_librato_message(self, action, topic="", attachment=False):
+        email = "hamlet@zulip.com"
+        api_key = self.get_api_key(email)
+        body = urllib.parse.urlencode({'payload': self.fixture_data("librato", action, file_type='json')})
+        if attachment:
+            body = self.fixture_data("librato", action, file_type='json')
+        url = "/api/v1/external/librato?api_key={api_key}&stream=librato&topic={topic}".format(api_key=api_key,
+                                                                                               topic=topic)
+        return self.send_json_payload(email,
+                                      url,
+                                      body,
+                                      stream_name="librato",
+                                      content_type="application/x-www-form-urlencoded")
+
+    def test_alert_message_with_default_topic(self):
+        msg     = self.send_librato_message('alert')
+        expected_message = "Alert [alert_name](https://metrics.librato.com/alerts#/6294535) has triggered! [Reaction steps](http://www.google.pl)\n>Metric `librato.cpu.percent.idle`, sum was below 44 by 300s, recorded at 2016-03-31 05:11:42\n>Metric `librato.swap.swap.cached`, average was absent  by 300s, recorded at 2016-03-31 05:11:42\n>Metric `librato.swap.swap.cached`, derivative was above 9 by 300s, recorded at 2016-03-31 05:11:42"
+        self.assertEqual(expected_message, msg.content)
+        self.assertEqual(msg.subject, 'Alert alert.name')
+
+    def test_alert_message_with_custom_topic(self):
+        msg = self.send_librato_message('alert', 'custom_name')
+        expected_message = "Alert [alert_name](https://metrics.librato.com/alerts#/6294535) has triggered! [Reaction steps](http://www.google.pl)\n>Metric `librato.cpu.percent.idle`, sum was below 44 by 300s, recorded at 2016-03-31 05:11:42\n>Metric `librato.swap.swap.cached`, average was absent  by 300s, recorded at 2016-03-31 05:11:42\n>Metric `librato.swap.swap.cached`, derivative was above 9 by 300s, recorded at 2016-03-31 05:11:42"
+        self.assertEqual(expected_message, msg.content)
+        self.assertEqual(msg.subject, 'custom_name')
+
+    def test_three_conditions_alert_message(self):
+        msg = self.send_librato_message('three_conditions_alert')
+        expected_message = "Alert [alert_name](https://metrics.librato.com/alerts#/6294535) has triggered! [Reaction steps](http://www.use.water.pl)\n>Metric `collectd.interface.eth0.if_octets.tx`, absolute_value was above 4 by 300s, recorded at 2016-04-11 16:40:14\n>Metric `collectd.load.load.longterm`, max was above 99, recorded at 2016-04-11 16:40:14\n>Metric `librato.swap.swap.cached`, average was absent  by 60s, recorded at 2016-04-11 16:40:14"
+        self.assertEqual(expected_message, msg.content)
+        self.assertEqual(msg.subject, 'Alert ToHighTemeprature')
+
+    def test_alert_clear(self):
+        msg = self.send_librato_message('alert_cleared')
+        expected_message = "Alert [alert_name](https://metrics.librato.com/alerts#/6309313) has cleared at 2016-04-12 09:11:44!"
+        self.assertEqual(expected_message, msg.content)
+        self.assertEqual(msg.subject, 'Alert Alert_name')
+
+    def test_snapshot(self):
+        msg = self.send_librato_message('snapshot', 'Snapshots', attachment=True)
+        expected_message = "**Hamlet** send a [snapshot](http://snapshots.librato.com/chart/nr5l3n0c-82162.png) of [metric](https://metrics.librato.com/s/spaces/167315/explore/1731491?duration=72039&end_time=1460569409)"
+        self.assertEqual(expected_message, msg.content)
+        self.assertEqual(msg.subject, 'Snapshots')
