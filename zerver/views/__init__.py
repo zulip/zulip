5f5e5b28b9598e810a1f03f5dfe24b2ae2ff7de8
Tim Abbott <tabbott@mit.edu>
2016-05-18 14:57:55 -0700

    Fix fetching user API keys via settings page when using LDAP backend.
    Previously, json_fetch_api_key was hardcoding a check using Zulip's
    built-in password functionality, rather than using authenticate().

--- zerver/views/__init__.py
+++ zerver/views/__init__.py
@@ -1096,8 +1096,9 @@ def api_fetch_api_key(request, username=REQ, password=REQ):
 @authenticated_json_post_view
 @has_request_variables
 def json_fetch_api_key(request, user_profile, password=REQ(default='')):
-    if password_auth_enabled(user_profile.realm) and not user_profile.check_password(password):
-        return json_error("Your username or password is incorrect.")
+    if password_auth_enabled(user_profile.realm):
+        if not authenticate(username=user_profile.email, password=password):
+            return json_error("Your username or password is incorrect.")
     return json_success({"api_key": user_profile.api_key})
 
 @csrf_exempt
