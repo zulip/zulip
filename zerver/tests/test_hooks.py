6d79fd6a58d3c8d22876bdcb555a92ca83cff7ae
Tomasz Kolek <tomasz-kolek@o2.pl>
2016-05-28 09:41:38 +0200

    Add Crashlytics integration.

--- zerver/tests/test_hooks.py
+++ zerver/tests/test_hooks.py
@@ -1222,3 +1222,22 @@ class TransifexHookTests(WebhookTestCase):
 
     def get_body(self, fixture_name):
         return {}
+
+class CrashlyticsHookTests(WebhookTestCase):
+    STREAM_NAME = 'crashlytics'
+    URL_TEMPLATE = "/api/v1/external/crashlytics?stream={stream}&api_key={api_key}"
+    FIXTURE_DIR_NAME = 'crashlytics'
+
+    def test_crashlytics_verification_message(self):
+        last_message_before_request = self.get_last_message()
+        payload = self.get_body('verification')
+        url = self.build_webhook_url()
+        result = self.client.post(url, payload, content_type="application/json")
+        last_message_after_request = self.get_last_message()
+        self.assert_json_success(result)
+        self.assertEqual(last_message_after_request.pk, last_message_before_request.pk)
+
+    def test_crashlytics_build_in_success_status(self):
+        expected_subject = u"123: Issue Title"
+        expected_message = u"[Issue](http://crashlytics.com/full/url/to/issue) impacts at least 16 device(s)."
+        self.send_and_test_stream_message('issue_message', expected_subject, expected_message)
