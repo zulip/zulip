ab6448f01831b88e00886152dc9f71e7f9c84ecd
Max <iamaspacecow@gmail.com>
2016-06-04 10:52:48 -0700

    Annotate camo.py, mandrill_client.py, query.py, session_user.py

--- zerver/lib/session_user.py
+++ zerver/lib/session_user.py
@@ -1,8 +1,12 @@
 from __future__ import absolute_import
 
 from django.contrib.auth import SESSION_KEY, get_user_model
+from django.contrib.sessions.models import Session
+
+from typing import Dict, Optional
 
 def get_session_dict_user(session_dict):
+    # type: (Dict[str, int]) -> Optional[int]
     # Compare django.contrib.auth._get_user_session_key
     try:
         return get_user_model()._meta.pk.to_python(session_dict[SESSION_KEY])
@@ -10,4 +14,5 @@ def get_session_dict_user(session_dict):
         return None
 
 def get_session_user(session):
+    # type: (Session) -> int
     return get_session_dict_user(session.get_decoded())
