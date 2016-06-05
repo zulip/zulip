d066616cdb58b71f39deb9b1be021ce205987bed
medullaskyline <medullaskyline@gmail.com>
2016-06-05 14:18:47 -0700

    Annotate zerver.views.webhooks.newrelic.

--- zerver/views/webhooks/newrelic.py
+++ zerver/views/webhooks/newrelic.py
@@ -1,12 +1,15 @@
 # Webhooks for external integrations.
 from __future__ import absolute_import
+from typing import Any, Callable, Iterable, Optional, Tuple
 
 from django.utils.translation import ugettext as _
+from django.http import HttpRequest, HttpResponse
 
 from zerver.lib.actions import check_send_message
 from zerver.lib.response import json_success, json_error
 from zerver.lib.validator import check_dict
 from zerver.decorator import REQ, has_request_variables, api_key_only_webhook_view
+from zerver.models import UserProfile, Stream, Client
 
 
 @api_key_only_webhook_view("NewRelic")
@@ -14,6 +17,7 @@ from zerver.decorator import REQ, has_request_variables, api_key_only_webhook_vi
 def api_newrelic_webhook(request, user_profile, client, stream=REQ(),
                          alert=REQ(validator=check_dict([]), default=None),
                          deployment=REQ(validator=check_dict([]), default=None)):
+    # type: (HttpRequest, UserProfile, Client, Optional[Stream], Optional[Dict[str, Any]], Optional[Dict[str, Any]]) -> HttpResponse
     if alert:
         # Use the message as the subject because it stays the same for
         # "opened", "acknowledged", and "closed" messages that should be
