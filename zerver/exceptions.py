157a3efb78686fefe0c40a648f11ddd0035f985b
Tim Abbott <tabbott@mit.edu>
2016-06-04 22:51:18 -0700

    Annotate zerver.exceptions, zerver.filters, zerver.logging_handlers.

--- zerver/exceptions.py
+++ zerver/exceptions.py
@@ -4,4 +4,5 @@ from django.core.exceptions import PermissionDenied
 
 class RateLimited(PermissionDenied):
     def __init__(self, msg=""):
+        # type: (str) -> None
         super(RateLimited, self).__init__(msg)
--- zerver/filters.py
+++ zerver/filters.py
@@ -1,10 +1,13 @@
 from __future__ import absolute_import
 
+from typing import Any
+
 from django.views.debug import SafeExceptionReporterFilter
-from django.http import build_request_repr
+from django.http import HttpRequest, build_request_repr
 
 class ZulipExceptionReporterFilter(SafeExceptionReporterFilter):
     def get_post_parameters(self, request):
+        # type: (HttpRequest) -> Dict[str, Any]
         filtered_post = SafeExceptionReporterFilter.get_post_parameters(self, request).copy()
         filtered_vars = ['content', 'secret', 'password', 'key', 'api-key', 'subject', 'stream',
                          'subscriptions', 'to', 'csrfmiddlewaretoken', 'api_key']
@@ -14,6 +17,7 @@ class ZulipExceptionReporterFilter(SafeExceptionReporterFilter):
                 filtered_post[var] = '**********'
         return filtered_post
     def get_request_repr(self, request):
+        # type: (HttpRequest) -> str
         if request is None:
             return repr(None)
         else:
