2bcf313a8578d737f22729e2848c7b2080831243
Dalek-Sec <the.phantom.of.the.opera007@gmail.com>
2016-06-05 02:45:53 +0000

    Added MyPy types to zerver/views/webhooks/codeship.py

--- zerver/views/webhooks/codeship.py
+++ zerver/views/webhooks/codeship.py
@@ -2,10 +2,13 @@
 from __future__ import absolute_import
 
 from django.utils.translation import ugettext as _
+from django.http import HttpRequest, HttpResponse
+from typing import Any
 
 from zerver.lib.actions import check_send_message
 from zerver.lib.response import json_success, json_error
 from zerver.decorator import REQ, has_request_variables, api_key_only_webhook_view
+from zerver.models import UserProfile, Client
 
 import ujson
 
@@ -25,6 +28,7 @@ CODESHIP_STATUS_MAPPER = {
 @has_request_variables
 def api_codeship_webhook(request, user_profile, client, payload=REQ(argument_type='body'),
                          stream=REQ(default='codeship')):
+    # type: (HttpRequest, UserProfile, Client, Dict[str, Any], str) -> HttpResponse
     try:
         payload = payload['build']
         subject = get_subject_for_http_request(payload)
@@ -37,10 +41,12 @@ def api_codeship_webhook(request, user_profile, client, payload=REQ(argument_typ
 
 
 def get_subject_for_http_request(payload):
+    # type: (Dict[str, Any]) -> str
     return CODESHIP_SUBJECT_TEMPLATE.format(project_name=payload['project_name'])
 
 
 def get_body_for_http_request(payload):
+    # type: (Dict[str, Any]) -> str
     return CODESHIP_MESSAGE_TEMPLATE.format(
         build_url=payload['build_url'],
         committer=payload['committer'],
@@ -50,5 +56,6 @@ def get_body_for_http_request(payload):
 
 
 def get_status_message(payload):
+    # type: (Dict[str, Any]) -> str
     build_status = payload['status']
     return CODESHIP_STATUS_MAPPER.get(build_status, CODESHIP_DEFAULT_STATUS.format(status=build_status))
