cebc8dcc6775401b3f7ecd6d0dc95c195a811042
Evan Palmer <evjpalmer@gmail.com>
2016-06-05 14:41:44 -0700

    Annotate zerver/views/webhooks/zendesk.py.

--- zerver/views/webhooks/zendesk.py
+++ zerver/views/webhooks/zendesk.py
@@ -1,9 +1,10 @@
 # Webhooks for external integrations.
 from __future__ import absolute_import
-from zerver.models import get_client
+from zerver.models import get_client, UserProfile
 from zerver.lib.actions import check_send_message
 from zerver.lib.response import json_success
 from zerver.decorator import authenticated_rest_api_view, REQ, has_request_variables
+from django.http import HttpRequest, HttpResponse
 
 def truncate(string, length):
     if len(string) > length:
@@ -14,6 +15,7 @@ def truncate(string, length):
 @has_request_variables
 def api_zendesk_webhook(request, user_profile, ticket_title=REQ(), ticket_id=REQ(),
                         message=REQ(), stream=REQ(default="zendesk")):
+                        # type: (HttpRequest, UserProfile, str, str, str, str) -> HttpResponse
     """
     Zendesk uses trigers with message templates. This webhook uses the
     ticket_id and ticket_title to create a subject. And passes with zendesk
