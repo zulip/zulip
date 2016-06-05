89c099922631345661c5e0184b6297b0eb6a9427
Daw-Ran Liou <dawran6@gmail.com>
2016-06-05 15:00:13 -0700

    Annotate zerver.views.webhooks.stash.

--- zerver/views/webhooks/stash.py
+++ zerver/views/webhooks/stash.py
@@ -1,20 +1,25 @@
 # Webhooks for external integrations.
 from __future__ import absolute_import
 
+from django.http import HttpRequest, HttpResponse
 from django.utils.translation import ugettext as _
 
 from zerver.models import get_client
 from zerver.lib.actions import check_send_message
 from zerver.lib.response import json_success, json_error
 from zerver.decorator import REQ, has_request_variables, authenticated_rest_api_view
+from zerver.models import UserProfile
 import ujson
 
+from six import text_type
+from typing import Any, Dict
 
 
 @authenticated_rest_api_view
 @has_request_variables
 def api_stash_webhook(request, user_profile, payload=REQ(argument_type='body'),
                       stream=REQ(default='commits')):
+    # type: (HttpRequest, UserProfile, Dict[str, Any], text_type) -> HttpResponse
     # We don't get who did the push, or we'd try to report that.
     try:
         repo_name = payload["repository"]["name"]
