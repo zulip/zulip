63ce893bf430f804c5965e6f62c2fb15ae0c646f
Evan Palmer <evjpalmer@gmail.com>
2016-06-05 15:14:45 -0700

    annotate webhooks/transifex.py, webhooks/yo.py.

--- zerver/views/webhooks/yo.py
+++ zerver/views/webhooks/yo.py
@@ -3,6 +3,9 @@ from __future__ import absolute_import
 from zerver.lib.actions import check_send_message
 from zerver.lib.response import json_success
 from zerver.decorator import REQ, has_request_variables, api_key_only_webhook_view
+from zerver.models import UserProfile, Client
+from django.http import HttpRequest, HttpResponse
+from typing import Optional
 
 import ujson
 
@@ -11,6 +14,7 @@ import ujson
 def api_yo_app_webhook(request, user_profile, client, email=REQ(default=None),
                        username=REQ(default='Yo Bot'), topic=REQ(default='None'),
                        user_ip=REQ(default='None')):
+                       # type: (HttpRequest, UserProfile, Client, Optional[str], str, str, str) -> HttpResponse
 
     body = ('Yo from %s') % (username,)
     check_send_message(user_profile, client, 'private', [email], topic, body)
