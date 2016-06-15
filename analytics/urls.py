3fdf0c90744a545dd66e25426ab5ecddcb925383
Asif Saifuddin Auvi <auvipy@gmail.com>
2016-06-15 13:31:53 +0600

    updated analytics/urls to 1.8+

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+from . import views
 
 i18n_urlpatterns = [
-    url(r'^activity$', 'analytics.views.get_activity'),
-    url(r'^realm_activity/(?P<realm>[\S]+)/$', 'analytics.views.get_realm_activity'),
-    url(r'^user_activity/(?P<email>[\S]+)/$', 'analytics.views.get_user_activity'),
+    url(r'^activity$', views.get_activity),
+    url(r'^realm_activity/(?P<realm>[\S]+)/$', views.get_realm_activity),
+    url(r'^user_activity/(?P<email>[\S]+)/$', views.get_user_activity),
 ]
 
-urlpatterns = patterns('', *i18n_urlpatterns)
+urlpatterns = i18n_urlpatterns
