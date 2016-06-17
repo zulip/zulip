63fd40b859e78de6c548ecb0f75a03749b98f1af
Tim Abbott <tabbott@mit.edu>
2016-06-16 17:30:48 -0700

    Refactor out zproject/dev_settings.py.

--- zproject/settings.py
+++ zproject/settings.py
@@ -79,26 +79,7 @@ TUTORIAL_ENABLED = True
 if PRODUCTION:
     from .local_settings import *
 else:
-    # For the Dev VM environment, we use the same settings as the
-    # sample local_settings.py file, with a few exceptions.
-    from .local_settings_template import *
-    LOCAL_UPLOADS_DIR = 'uploads'
-    EXTERNAL_HOST = 'localhost:9991'
-    ALLOWED_HOSTS = ['localhost']
-    AUTHENTICATION_BACKENDS = ('zproject.backends.DevAuthBackend',)
-    # Add some of the below if you're testing other backends
-    # AUTHENTICATION_BACKENDS = ('zproject.backends.EmailAuthBackend',
-    #                            'zproject.backends.GoogleMobileOauth2Backend',)
-    EXTERNAL_URI_SCHEME = "http://"
-    EMAIL_GATEWAY_PATTERN = "%s@" + EXTERNAL_HOST
-    ADMIN_DOMAIN = "zulip.com"
-    NOTIFICATION_BOT = "notification-bot@zulip.com"
-    ERROR_BOT = "error-bot@zulip.com"
-    NEW_USER_BOT = "new-user-bot@zulip.com"
-    EMAIL_GATEWAY_BOT = "emailgateway@zulip.com"
-    EXTRA_INSTALLED_APPS = ["zilencer", "analytics"]
-    # Disable Camo in development
-    CAMO_URI = ''
+    from .dev_settings import *
 
 ########################################################################
 # DEFAULT VALUES FOR SETTINGS
