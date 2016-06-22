9482cdccf45ffae675722f144fde97a191c5e57f
Eklavya Sharma <ekurgn@gmail.com>
2016-06-22 23:34:14 +0530

    scripts/lib/setup-venv.py: Accept None as target_venv_path.
    If target_venv_path in setup_virtualenv is None, create a virtualenv
    named 'venv' but don't make a symlink to it.  Also return
    cached_venv_path.

--- scripts/lib/setup_venv.py
+++ scripts/lib/setup_venv.py
@@ -15,7 +15,7 @@ if '--travis' in sys.argv:
 
 if False:
     # Don't add a runtime dependency on typing
-    from typing import List
+    from typing import List, Optional
 
 VENV_DEPENDENCIES = [
     "libffi-dev",
@@ -29,22 +29,27 @@ VENV_DEPENDENCIES = [
 ]
 
 def setup_virtualenv(target_venv_path, requirements_file, virtualenv_args=None):
-    # type: (str, str, List[str]) -> None
+    # type: (Optional[str], str, Optional[List[str]]) -> str
 
     # Check if a cached version already exists
     path = os.path.join(ZULIP_PATH, 'scripts', 'lib', 'hash_reqs.py')
     output = subprocess.check_output([path, requirements_file])
     sha1sum = output.split()[0]
-    cached_venv_path = os.path.join(VENV_CACHE_PATH, sha1sum, os.path.basename(target_venv_path))
+    if target_venv_path is None:
+        cached_venv_path = os.path.join(VENV_CACHE_PATH, sha1sum, 'venv')
+    else:
+        cached_venv_path = os.path.join(VENV_CACHE_PATH, sha1sum, os.path.basename(target_venv_path))
     success_stamp = os.path.join(cached_venv_path, "success-stamp")
     if not os.path.exists(success_stamp):
         do_setup_virtualenv(cached_venv_path, requirements_file, virtualenv_args or [])
         run(["touch", success_stamp])
 
     print("Using cached Python venv from %s" % (cached_venv_path,))
-    run(["sudo", "ln", "-nsf", cached_venv_path, target_venv_path])
-    activate_this = os.path.join(target_venv_path, "bin", "activate_this.py")
+    if target_venv_path is not None:
+        run(["sudo", "ln", "-nsf", cached_venv_path, target_venv_path])
+    activate_this = os.path.join(cached_venv_path, "bin", "activate_this.py")
     exec(open(activate_this).read(), {}, dict(__file__=activate_this)) # type: ignore # https://github.com/python/mypy/issues/1577
+    return cached_venv_path
 
 def do_setup_virtualenv(venv_path, requirements_file, virtualenv_args):
     # type: (str, str, List[str]) -> None
