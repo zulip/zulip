4aaeec92d53b2777caf5d9797884efc5d2b9cf7f
Preston Hansen <prestonhansen01@gmail.com>
2016-06-23 18:53:49 -0500

    Update alert word UI to match the style of bots.
    Resolves #1075.

--- static/js/alert_words_ui.js
+++ static/js/alert_words_ui.js
@@ -2,14 +2,13 @@ var alert_words_ui = (function () {
 
 var exports = {};
 
-function update_word_alerts() {
+function update_alert_words() {
     var words = _.map($('.alert-word-item'), function (e) {
-        return $(e).data('word');
+        return $(e).data('word').toString();
     });
     words = _.filter(words, function (word) {
         return word !== "";
     });
-
     channel.post({
         url: '/json/users/me/alert_words',
         idempotent: true,
@@ -20,27 +19,26 @@ function add_alert_word(word, event) {
     if (word === '') {
         return;
     }
-
     var final_li = templates.render('alert_word_settings_item', {'word': word, editing: false});
 
-    var li = $(event.target).parent();
+    var li = $(event.target).parents('li');
     li.replaceWith(final_li);
 
     var new_word = templates.render('alert_word_settings_item', {'word': '', editing: true});
-    var word_list = $('#word-alerts');
+    var word_list = $('#alert_words_list');
     word_list.append(new_word);
 
     if (word_list.find('input').length > 0) {
         word_list.find('input').focus();
     }
 
-    update_word_alerts();
+    update_alert_words();
 }
 
 exports.set_up_alert_words = function () {
     // The settings page must be rendered before this function gets called.
 
-    var word_list = $('#word-alerts');
+    var word_list = $('#alert_words_list');
     _.each(alert_words.words, function (word) {
         var li = templates.render('alert_word_settings_item', {'word': word});
         word_list.append(li);
@@ -48,19 +46,19 @@ exports.set_up_alert_words = function () {
     var new_word = templates.render('alert_word_settings_item', {'word': '', editing: true});
     word_list.append(new_word);
 
-    $('#word-alerts').on('click', '.add-alert-word', function (event) {
-        var word = $(event.target).siblings('input').val();
+    $('#alert_words_list').on('click', '.add-alert-word', function (event) {
+        var word = $('#create_alert_word_name').val();
         add_alert_word(word, event);
     });
 
-    $('#word-alerts').on('click', '.remove-alert-word', function (event) {
-        var li = $(event.currentTarget).parent();
+    $('#alert_words_list').on('click', '.remove-alert-word', function (event) {
+        var li = $(event.currentTarget).parents('li');
         li.remove();
 
-        update_word_alerts();
+        update_alert_words();
     });
 
-    $('#word-alerts').on('keypress', '.edit-alert-word', function (event) {
+    $('#alert_words_list').on('keypress', '#create_alert_word_name', function (event) {
         var key = event.which;
         // Handle enter (13) as "add".
         if (key === 13) {
