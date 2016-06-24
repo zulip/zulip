e4ed9195dcb4d8bcd3d3f36d72d5059dc589fd43
acrefoot <acrefoot@zulip.com>
2016-06-23 17:26:09 -0700

    Remove rest_dispatch hack and optimize imports.
    For a long time, rest_dispatch has had this hack where we have to
    create a copy of it in each views file using it, in order to directly
    access the globals list in that file.  This removes that hack, instead
    making rest_dispatch just use Django's import_string to access the
    target method to use.
    
    [tweaked and reorganized from acrefoot's original branch in various
    ways by tabbott]

--- zerver/views/alert_words.py
+++ zerver/views/alert_words.py
@@ -1,20 +1,16 @@
 from __future__ import absolute_import
 
-from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpRequest
 
 from typing import List
 from zerver.models import UserProfile
 
-from zerver.decorator import authenticated_json_post_view, has_request_variables, REQ
+from zerver.decorator import has_request_variables, REQ
 from zerver.lib.response import json_success
 from zerver.lib.validator import check_list, check_string
 
 from zerver.lib.actions import do_add_alert_words, do_remove_alert_words, do_set_alert_words
 from zerver.lib.alert_words import user_alert_words
-
-from zerver.lib.rest import rest_dispatch as _rest_dispatch
-rest_dispatch = csrf_exempt((lambda request, *args, **kwargs: _rest_dispatch(request, globals(), *args, **kwargs)))
 
 from six import text_type
 
--- zerver/views/realm_emoji.py
+++ zerver/views/realm_emoji.py
@@ -1,16 +1,12 @@
 from django.http import HttpRequest, HttpResponse
 from django.core.exceptions import ValidationError
-from django.views.decorators.csrf import csrf_exempt
 
 from zerver.models import UserProfile
 
 from zerver.lib.response import json_success, json_error
 from zerver.lib.actions import check_add_realm_emoji, do_remove_realm_emoji
 
-from zerver.lib.rest import rest_dispatch as _rest_dispatch
 from six import text_type
-rest_dispatch = csrf_exempt((lambda request, *args, **kwargs: _rest_dispatch(request, globals(), *args, **kwargs)))
-
 
 def list_emoji(request, user_profile):
     # type: (HttpRequest, UserProfile) -> HttpResponse
