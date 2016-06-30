cb18ef07a794c73388fc0e176cc189e264882628
Tim Abbott <tabbott@mit.edu>
2016-06-29 17:39:29 -0700

    Rename hashchange.zulip event to avoid jquery 1.8 interaction.

--- static/js/message_list.js
+++ static/js/message_list.js
@@ -631,7 +631,7 @@ exports.all = new exports.MessageList(
 // doing something.  Be careful, though, if you try to capture
 // mousemove, then you will have to contend with the autoscroll
 // itself generating mousemove events.
-$(document).on('message_selected.zulip hashchange.zulip mousewheel', function (event) {
+$(document).on('message_selected.zulip zuliphashchange.zulip mousewheel', function (event) {
     viewport.stop_auto_scrolling();
 });
 
