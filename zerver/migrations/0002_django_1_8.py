438bd88c9e2f574c4929aca0766bd49e4d3ec4d6
Eklavya Sharma <ekurgn@gmail.com>
2016-07-03 14:07:06 +0530

    zerver/migrations: Squash an AlterField from 0002 into 0001.
    Squash the AlterField on UserProfile.groups in 0002 into the
    AddField in 0001.  This is done to avoid a probable bug in Django,
    where running migrations in python 3 sometimes led to a KeyError.

--- zerver/migrations/0002_django_1_8.py
+++ zerver/migrations/0002_django_1_8.py
@@ -55,11 +55,6 @@ class Migration(migrations.Migration):
         ),
         migrations.AlterField(
             model_name='userprofile',
-            name='groups',
-            field=models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Group', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', verbose_name='groups'),
-        ),
-        migrations.AlterField(
-            model_name='userprofile',
             name='last_login',
             field=models.DateTimeField(null=True, verbose_name='last login', blank=True),
         ),
