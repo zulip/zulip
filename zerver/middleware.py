2f38521dd8b39a78e6151258163c2d48fe8aeb71
Tim Abbott <tabbott@mit.edu>
2016-07-12 18:19:44 -0700

    slow queries: Include full log line in slow query log.
    The extra data is useful, and I think this won't make the lines annoying long.

--- zerver/middleware.py
+++ zerver/middleware.py
@@ -205,7 +205,7 @@ def write_log_line(log_data, path, method, remote_ip, email, client_name,
         logger.info(logger_line)
 
     if (is_slow_query(time_delta, path)):
-        queue_json_publish("slow_queries", "%s (%s)" % (logger_timing, email), lambda e: None)
+        queue_json_publish("slow_queries", "%s (%s)" % (logger_line, email), lambda e: None)
 
     if settings.PROFILE_ALL_REQUESTS:
         log_data["prof"].disable()
