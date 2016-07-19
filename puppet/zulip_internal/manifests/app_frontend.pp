601ab24d2a5a3384b33ab1f6321ab1a93c3f0888
Tim Abbott <tabbott@mit.edu>
2016-07-19 10:04:38 -0700

    zulip_internal::app_frontend: Remove now-unnecessary dependencies.
    These Python packages are no longer relevant, since we install all of
    our Python dependencies via virtualenvs.

--- puppet/zulip_internal/manifests/app_frontend.pp
+++ puppet/zulip_internal/manifests/app_frontend.pp
@@ -6,10 +6,6 @@ class zulip_internal::app_frontend {
   $app_packages = [# Needed for minify-js
                    "yui-compressor",
                    "nodejs",
-                   # Needed for statsd reporting
-                   "python-django-statsd-mozilla",
-                   # Needed only for a disabled integration
-                   "python-embedly",
                    # Needed for the ssh tunnel to the redis server
                    "autossh",
                    ]
