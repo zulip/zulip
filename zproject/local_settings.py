de679a23c9ba0b34b9d1436ff164530c9ae781a3
Tim Abbott <tabbott@mit.edu>
2016-07-18 21:52:34 -0700

    settings: Extract settings.REGISTER_LINK_DISABLED.

--- zproject/local_settings.py
+++ zproject/local_settings.py
@@ -30,6 +30,7 @@ if not ZULIP_COM:
 ZULIP_FRIENDS_LIST_ID = '84b2f3da6b'
 SHARE_THE_LOVE = True
 SHOW_OSS_ANNOUNCEMENT = True
+REGISTER_LINK_DISABLED = True
 
 # This can be filled in automatically from the database, maybe
 DEPLOYMENT_ROLE_NAME = 'zulip.com'
