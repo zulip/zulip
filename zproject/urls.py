8028f37d438079ced0a293c7baf51998b775989b
acrefoot <acrefoot@alum.mit.edu>
2016-07-20 12:57:47 -0700

    Change HTTP verb for create_user_backend to PUT (#1326)

--- zproject/urls.py
+++ zproject/urls.py
@@ -136,7 +136,7 @@ v1_api_and_json_patterns = [
     # users -> zerver.views.users
     url(r'^users$', 'zerver.lib.rest.rest_dispatch',
         {'GET': 'zerver.views.users.get_members_backend',
-         'POST': 'zerver.views.users.create_user_backend'}),
+         'PUT': 'zerver.views.users.create_user_backend'}),
     url(r'^users/(?P<email>(?!me)[^/]*)/reactivate$', 'zerver.lib.rest.rest_dispatch',
         {'POST': 'zerver.views.users.reactivate_user_backend'}),
     url(r'^users/(?P<email>(?!me)[^/]*)$', 'zerver.lib.rest.rest_dispatch',
