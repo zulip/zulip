b9234eacdc649f97815fc3d142b08922dd0540dc
Eklavya Sharma <ekurgn@gmail.com>
2016-07-21 01:12:33 +0530

    Patch activate script only in development.

--- tools/setup/setup_venvs.py
+++ tools/setup/setup_venvs.py
@@ -13,10 +13,11 @@ from scripts.lib.setup_venv import setup_virtualenv
 def main():
     # type: () -> None
     PY2_DEV_REQS_FILE = os.path.join(ZULIP_PATH, "requirements", "py2_dev.txt")
-    setup_virtualenv("/srv/zulip-venv", PY2_DEV_REQS_FILE)
+    setup_virtualenv("/srv/zulip-venv", PY2_DEV_REQS_FILE, patch_activate_script=True)
 
     PY3_DEV_REQS_FILE = os.path.join(ZULIP_PATH, "requirements", "py3_dev.txt")
-    setup_virtualenv("/srv/zulip-py3-venv", PY3_DEV_REQS_FILE, virtualenv_args=['-p', 'python3'])
+    setup_virtualenv("/srv/zulip-py3-venv", PY3_DEV_REQS_FILE, patch_activate_script=True,
+                     virtualenv_args=['-p', 'python3'])
 
 if __name__ == "__main__":
     main()
