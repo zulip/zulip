cf6873514c610bcd2cc825b986f1ad3030f26bfc
Tim Abbott <tabbott@mit.edu>
2016-07-20 14:55:15 -0700

    Revert "Factor out venv-installation into a script."
    This reverts commit 852c49a44e00edac6f9e57c5b02906da8b294765.
    
    I think this may have broken provisioning without a venv.

--- tools/provision.py
+++ tools/provision.py
@@ -178,7 +178,10 @@ def main():
             DEV_REQS_FILE = os.path.join(ZULIP_PATH, "requirements", "py3_dev.txt")
             setup_virtualenv(VENV_PATH, DEV_REQS_FILE, virtualenv_args=['-p', 'python3'])
     else:
-        run(["tools/setup/setup-venvs"])
+        DEV_REQS_FILE = os.path.join(ZULIP_PATH, "requirements", "py2_dev.txt")
+        setup_virtualenv(PY2_VENV_PATH, DEV_REQS_FILE)
+        DEV_REQS_FILE = os.path.join(ZULIP_PATH, "requirements", "py3_dev.txt")
+        setup_virtualenv(PY3_VENV_PATH, DEV_REQS_FILE, virtualenv_args=['-p', 'python3'])
 
     # Put Python2 virtualenv activation in our .bash_profile.
     with open(os.path.expanduser('~/.bash_profile'), 'w+') as bash_profile:
