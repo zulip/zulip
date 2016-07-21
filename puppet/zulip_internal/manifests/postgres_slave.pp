2a209e46dd39161e8bde1aabf6c6969303d9cf99
Tim Abbott <tabbott@mit.edu>
2016-07-20 21:09:54 -0700

    puppet: Fix remaining hardcoding of postgres 9.1 in manifests.

--- puppet/zulip_internal/manifests/postgres_slave.pp
+++ puppet/zulip_internal/manifests/postgres_slave.pp
@@ -10,8 +10,8 @@ class zulip_internal::postgres_slave {
     source   => 'puppet:///modules/zulip_internal/postgresql/40-postgresql.conf.slave',
   }
 
-  file { "/etc/postgresql/9.1/main/postgresql.conf":
-    require => Package["postgresql-9.1"],
+  file { "/etc/postgresql/${zulip::base::postgres_version}/main/postgresql.conf":
+    require => Package["postgresql-${zulip::base::postgres_version}"],
     ensure => file,
     owner  => "postgres",
     group  => "postgres",
