62affa53c9eb8f473a9790620ab2e684ef9bce66
Tim Abbott <tabbott@mit.edu>
2016-07-26 14:16:20 -0700

    views: Move update_realm to new realm.py views file.

--- zerver/views/__init__.py
+++ zerver/views/__init__.py
@@ -31,9 +31,7 @@ from zerver.lib.actions import do_change_password, do_change_full_name, do_chang
     do_activate_user, do_create_user, do_create_realm, set_default_streams, \
     internal_send_message, update_user_presence, do_events_register, \
     get_status_dict, do_change_enable_offline_email_notifications, \
-    do_change_enable_digest_emails, do_set_realm_name, do_set_realm_restricted_to_domain, \
-    do_set_realm_invite_required, do_set_realm_invite_by_admins_only, \
-    do_set_realm_create_stream_by_admins_only, do_set_realm_message_editing, \
+    do_change_enable_digest_emails, \
     get_default_subs, user_email_is_unique, do_invite_users, do_refer_friend, \
     compute_mit_user_fullname, do_set_muted_topics, clear_followup_emails_queue, \
     do_update_pointer, realm_user_count
@@ -47,7 +45,7 @@ from zerver.lib import bugdown
 from zerver.lib.validator import check_string, check_list, check_bool
 from zerver.decorator import require_post, authenticated_json_post_view, \
     has_request_variables, authenticated_json_view, to_non_negative_int, \
-    JsonableError, get_user_profile_by_email, REQ, require_realm_admin, \
+    JsonableError, get_user_profile_by_email, REQ, \
     zulip_login_required
 from zerver.lib.avatar import avatar_url
 from zerver.lib.response import json_success, json_error
@@ -1102,45 +1100,6 @@ def get_profile_backend(request, user_profile):
         result['max_message_id'] = messages[0].id
 
     return json_success(result)
-
-@require_realm_admin
-@has_request_variables
-def update_realm(request, user_profile, name=REQ(validator=check_string, default=None),
-                 restricted_to_domain=REQ(validator=check_bool, default=None),
-                 invite_required=REQ(validator=check_bool, default=None),
-                 invite_by_admins_only=REQ(validator=check_bool, default=None),
-                 create_stream_by_admins_only=REQ(validator=check_bool, default=None),
-                 allow_message_editing=REQ(validator=check_bool, default=None),
-                 message_content_edit_limit_seconds=REQ(converter=to_non_negative_int, default=None)):
-    # type: (HttpRequest, UserProfile, Optional[str], Optional[bool], Optional[bool], Optional[bool], Optional[bool], Optional[bool], Optional[int]) -> HttpResponse
-    realm = user_profile.realm
-    data = {} # type: Dict[str, Any]
-    if name is not None and realm.name != name:
-        do_set_realm_name(realm, name)
-        data['name'] = 'updated'
-    if restricted_to_domain is not None and realm.restricted_to_domain != restricted_to_domain:
-        do_set_realm_restricted_to_domain(realm, restricted_to_domain)
-        data['restricted_to_domain'] = restricted_to_domain
-    if invite_required is not None and realm.invite_required != invite_required:
-        do_set_realm_invite_required(realm, invite_required)
-        data['invite_required'] = invite_required
-    if invite_by_admins_only is not None and realm.invite_by_admins_only != invite_by_admins_only:
-        do_set_realm_invite_by_admins_only(realm, invite_by_admins_only)
-        data['invite_by_admins_only'] = invite_by_admins_only
-    if create_stream_by_admins_only is not None and realm.create_stream_by_admins_only != create_stream_by_admins_only:
-        do_set_realm_create_stream_by_admins_only(realm, create_stream_by_admins_only)
-        data['create_stream_by_admins_only'] = create_stream_by_admins_only
-    if (allow_message_editing is not None and realm.allow_message_editing != allow_message_editing) or \
-       (message_content_edit_limit_seconds is not None and
-        realm.message_content_edit_limit_seconds != message_content_edit_limit_seconds):
-        if allow_message_editing is None:
-            allow_message_editing = realm.allow_message_editing
-        if message_content_edit_limit_seconds is None:
-            message_content_edit_limit_seconds = realm.message_content_edit_limit_seconds
-        do_set_realm_message_editing(realm, allow_message_editing, message_content_edit_limit_seconds)
-        data['allow_message_editing'] = allow_message_editing
-        data['message_content_edit_limit_seconds'] = message_content_edit_limit_seconds
-    return json_success(data)
 
 @csrf_exempt
 @require_post
