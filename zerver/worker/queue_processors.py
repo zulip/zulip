4201ea829c81acd4bc6cee4018c480cd202a4237
Eklavya Sharma <ekurgn@gmail.com>
2016-07-28 12:11:46 +0530

    Fix string handling in email_mirror queue_processor.

--- zerver/worker/queue_processors.py
+++ zerver/worker/queue_processors.py
@@ -339,7 +339,7 @@ class MirrorWorker(QueueProcessingWorker):
     # who gets a digest is entirely determined by the enqueue_digest_emails
     # management command, not here.
     def consume(self, event):
-        mirror_email(email.message_from_string(event["message"].encode("utf-8")),
+        mirror_email(email.message_from_string(event["message"]),
                      rcpt_to=event["rcpt_to"], pre_checked=True)
 
 @assign_queue('test')
