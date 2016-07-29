0024aa8ec157578942c6d1543e73b2dab4cadecc
Tim Abbott <tabbott@mit.edu>
2016-07-29 15:33:29 -0700

    Annotate zilencer/models.py and fix str types in zilencer views.

--- zilencer/models.py
+++ zilencer/models.py
@@ -1,26 +1,34 @@
 from django.db import models
+from django.db.models import Manager
+from six import text_type
+
 import zerver.models
 
 def get_deployment_by_domain(domain):
+    # type: (text_type) -> Deployment
     return Deployment.objects.get(realms__domain=domain)
 
 class Deployment(models.Model):
-    realms = models.ManyToManyField(zerver.models.Realm, related_name="_deployments")
-    is_active = models.BooleanField(default=True)
+    realms = models.ManyToManyField(zerver.models.Realm,
+                                    related_name="_deployments") # type: Manager
+    is_active = models.BooleanField(default=True) # type: bool
 
     # TODO: This should really become the public portion of a keypair, and
     # it should be settable only with an initial bearer "activation key"
-    api_key = models.CharField(max_length=32, null=True)
+    api_key = models.CharField(max_length=32, null=True) # type: text_type
 
-    base_api_url = models.CharField(max_length=128)
-    base_site_url = models.CharField(max_length=128)
+    base_api_url = models.CharField(max_length=128) # type: text_type
+    base_site_url = models.CharField(max_length=128) # type: text_type
 
     @property
     def endpoints(self):
+        # type: () -> Dict[str, text_type]
         return {'base_api_url': self.base_api_url, 'base_site_url': self.base_site_url}
 
     @property
     def name(self):
+        # type: () -> text_type
+
         # TODO: This only does the right thing for prod because prod authenticates to
         # staging with the zulip.com deployment key, while staging is technically the
         # deployment for the zulip.com realm.
