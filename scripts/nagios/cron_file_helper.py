9dc2c69f6795f798c92c04d543f7046e6b7b301c
Tim Abbott <tabbott@mit.edu>
2016-07-29 16:09:08 -0700

    Annotate cron_file_helper.

--- scripts/nagios/cron_file_helper.py
+++ scripts/nagios/cron_file_helper.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 import time
 
+# Avoid requiring the typing module to be installed
+if False: from typing import Tuple
+
 def nagios_from_file(results_file):
+    # type: (str) -> Tuple[int, str]
     """Returns a nagios-appropriate string and return code obtained by
     parsing the desired file on disk. The file on disk should be of format
 
