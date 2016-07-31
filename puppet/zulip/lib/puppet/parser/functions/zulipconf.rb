fdaf03df68384dff5f230841bfdda6a7ac763708
Tim Abbott <tabbott@mit.edu>
2016-07-30 23:29:03 -0700

    puppet: Add new zulipsecret function.

--- puppet/zulip/lib/puppet/parser/functions/zulipconf.rb
+++ puppet/zulip/lib/puppet/parser/functions/zulipconf.rb
@@ -9,4 +9,15 @@ module Puppet::Parser::Functions
       default
     end
   end
+
+  newfunction(:zulipsecret, :type => :rvalue) do |args|
+    default = args.pop
+    joined = args.join(" ")
+    output = `/usr/bin/crudini --get /etc/zulip/zulip-secrets.conf #{joined} 2>&1`; result=$?.success?
+    if result
+      output.strip()
+    else
+      default
+    end
+  end
 end
