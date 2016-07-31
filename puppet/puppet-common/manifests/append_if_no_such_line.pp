639660377acc127f3b2966a6777fce38dfead4f5
Tim Abbott <tabbott@mit.edu>
2016-07-31 16:01:17 -0700

    Remove unused puppet-common third-party module code.

--- puppet/puppet-common/manifests/append_if_no_such_line.pp
+++
@@ -1,8 +0,0 @@
-define common::append_if_no_such_line($file, $line, $refreshonly = 'false') {
-   exec { "/bin/echo '$line' >> '$file'":
-      unless => "/bin/grep -Fxqe '$line' '$file'",
-      path => "/bin",
-      refreshonly => $refreshonly,
-      subscribe => File[$file],
-   }
-}
--- puppet/puppet-common/manifests/require_lsbdistcodename.pp
+++
@@ -1,4 +0,0 @@
-# To fail the complete compilation, include this class
-class common::require_lsbdistcodename inherits common::assert_lsbdistcodename {
-	exec { "false # require_lsbdistcodename": require => Exec[require_lsbdistcodename], }
-}
