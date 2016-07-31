75b5d021fa981adf58868e511a548bc88af01590
Tim Abbott <tabbott@mit.edu>
2016-07-31 16:01:17 -0700

    Remove unused puppet-common third-party module code.

--- puppet/puppet-common/lib/puppet/parser/functions/prefix_with.rb
+++
@@ -1,9 +0,0 @@
-# prefix arguments 2..n with first argument
-
-module Puppet::Parser::Functions
-	newfunction(:prefix_with, :type => :rvalue) do |args|
-		prefix = args.shift
-		args.collect {|v| "%s%s" % [prefix, v] }
-	end
-end
-
