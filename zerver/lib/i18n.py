013a59cf319b3370349a17fe5a97cb00066324b6
Umair Khan <umair.waheed@gmail.com>
2016-08-02 17:29:03 +0500

    Return unformatted list from get_language_list.

--- zerver/lib/i18n.py
+++ zerver/lib/i18n.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
 from django.conf import settings
 from django.utils import translation
 from django.utils.translation import ugettext as _
@@ -26,8 +28,6 @@ def get_language_list():
         for lang_info in languages['languages']:
             name = lang_info['name']
             lang_info['name'] = with_language(name, lang_info['code'])
-            if 'percent_translated' not in lang_info:
-                lang_info['percent_translated'] = 'N/A'
             lang_list.append(lang_info)
 
         return sorted(lang_list, key=lambda i: i['name'])
@@ -37,3 +37,4 @@ def get_available_language_codes():
     language_list = get_language_list()
     codes = [language['code'] for language in language_list]
     return codes
+
