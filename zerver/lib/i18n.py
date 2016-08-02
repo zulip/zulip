efa2b99d37c1de0103bc8590ea2914727ad02877
Umair Khan <umair.waheed@gmail.com>
2016-08-02 17:34:12 +0500

    Send double columned language list in page_params.

--- zerver/lib/i18n.py
+++ zerver/lib/i18n.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+import operator
 
 from django.conf import settings
 from django.utils import translation
 from django.utils.translation import ugettext as _
 
 from six import text_type
+from six.moves import urllib, zip_longest, zip, range
 from typing import Any, List, Dict, Optional
 
 import os
@@ -31,6 +33,40 @@ def get_language_list():
             lang_list.append(lang_info)
 
         return sorted(lang_list, key=lambda i: i['name'])
+
+def get_language_list_for_templates(default_language):
+    # type: (text_type) -> List[Dict[str, Dict[str, str]]]
+    language_list = [l for l in get_language_list()
+                     if 'percent_translated' not in l or
+                        l['percent_translated'] >= 5.]
+
+    formatted_list = []
+    lang_len = len(language_list)
+    firsts_end = (lang_len // 2) + operator.mod(lang_len, 2)
+    firsts = list(range(0, firsts_end))
+    seconds = list(range(firsts_end, lang_len))
+    assert len(firsts) + len(seconds) == lang_len
+    for row in zip_longest(firsts, seconds):
+        item = {}
+        for position, ind in zip(['first', 'second'], row):
+            if ind is None:
+                continue
+
+            lang = language_list[ind]
+            percent = name = lang['name']
+            if 'percent_translated' in lang:
+                percent = u"{} ({}%)".format(name, lang['percent_translated'])
+
+            item[position] = {
+                'name': name,
+                'code': lang['code'],
+                'percent': percent,
+                'selected': True if default_language == lang['code'] else False
+            }
+
+        formatted_list.append(item)
+
+    return formatted_list
 
 def get_language_name(code):
     # type: (str) -> Optional[text_type]
