1eeac01edc01683a30adc33ba0797c33df4a86c9
Rishi Gupta <rishig@kandralabs.com>
2016-08-04 17:17:52 -0700

    wip

--- a2/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-i18n_urlpatterns = [
-    url(r'^a2$', 'a2.views.get_activity'),
-    url(r'^a2realm/(?P<realm>[\S]+)/$', 'a2.views.get_realm_activity'),
-    url(r'^a2user/(?P<email>[\S]+)/$', 'a2.views.get_user_activity'),
-]
-
-urlpatterns = patterns('', *i18n_urlpatterns)
