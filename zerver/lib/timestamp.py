00495716f9e49a7930915712bfb38fc48e5af208
Rishi Gupta <rishig@kandralabs.com>
2016-08-04 17:21:44 -0700

    datetime

--- zerver/lib/timestamp.py
+++ zerver/lib/timestamp.py
@@ -1,23 +1,21 @@
 from __future__ import absolute_import
 
-from datetime import datetime
+import datetime
 import calendar
 from django.utils.timezone import utc
 
-from typing import Tuple
-
 def timestamp_to_datetime(timestamp):
-    # type: (float) -> datetime
-    return datetime.utcfromtimestamp(float(timestamp)).replace(tzinfo=utc)
+    # type: (float) -> datetime.datetime
+    return datetime.datetime.utcfromtimestamp(float(timestamp)).replace(tzinfo=utc)
 
 def datetime_to_timestamp(datetime_object):
-    # type: (datetime) -> int
+    # type: (datetime.datetime) -> int
     return calendar.timegm(datetime_object.timetuple())
 
 def datetime_to_string(datetime_object):
-    # type: (datetime) -> text_type
+    # type: (datetime.datetime) -> text_type
     return datetime_object.strftime('%Y-%m-%d %H:%M:%S')
 
 def string_to_datetime(datetime_str):
-    # type: (text_type) -> datetime
-    return datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
+    # type: (text_type) -> datetime.datetime
+    return datetime.datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
