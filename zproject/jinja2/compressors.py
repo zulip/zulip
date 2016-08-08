1259bc3057b9e8763a45c75c4d0f7a01d693bcd7
Umair Khan <umair.waheed@gmail.com>
2016-08-08 13:59:59 +0500

    Annotate jinja2/compressors.py.

--- zproject/jinja2/compressors.py
+++ zproject/jinja2/compressors.py
@@ -3,6 +3,8 @@
 """
 from __future__ import absolute_import  # Python 2 only
 
+from six import text_type
+
 from django.conf import settings
 from django.template import TemplateSyntaxError
 
@@ -10,6 +12,7 @@ from zerver.templatetags.minified_js import MinifiedJSNode
 
 
 def minified_js(sourcefile):
+    # type: (str) -> text_type
     if sourcefile not in settings.JS_SPECS:
         raise TemplateSyntaxError(
             "Invalid argument: no JS file %s".format(sourcefile))
