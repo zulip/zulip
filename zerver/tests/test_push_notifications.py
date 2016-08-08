8c249f859eca19ff4a8b638e42ff2dceb4ff7f0d
Umair Khan <umair.waheed@gmail.com>
2016-08-08 17:20:41 +0500

    Push Notification: Add tests for GCM.

--- zerver/tests/test_push_notifications.py
+++ zerver/tests/test_push_notifications.py
@@ -1,6 +1,9 @@
 import mock
+from mock import call
 import time
 from typing import Any, Union, SupportsInt
+
+import gcmclient
 
 from django.test import TestCase
 from django.conf import settings
@@ -159,3 +162,165 @@ class APNsFeedbackTest(PushNotificationTest):
         self.assertEqual(PushDeviceToken.objects.all().count(), 2)
         apn.check_apns_feedback()
         self.assertEqual(PushDeviceToken.objects.all().count(), 1)
+
+class GCMTest(PushNotificationTest):
+    def setUp(self):
+        # type: () -> None
+        super(GCMTest, self).setUp()
+        apn.gcm = gcmclient.GCM('fake key')
+        self.gcm_tokens = [u'1111', u'2222']
+        for token in self.gcm_tokens:
+            PushDeviceToken.objects.create(
+                kind=PushDeviceToken.GCM,
+                token=apn.hex_to_b64(token),
+                user=self.user_profile,
+                ios_app_id=None)
+
+    def get_gcm_data(self, **kwargs):
+        # type: (**Any) -> Dict[str, Any]
+        data = {
+            'key 1': 'Data 1',
+            'key 2': 'Data 2',
+        }
+        data.update(kwargs)
+        return data
+
+class GCMNotSetTest(GCMTest):
+    @mock.patch('logging.error')
+    def test_gcm_is_none(self, mock_error):
+        # type: (mock.MagicMock) -> None
+        apn.gcm = None
+        apn.send_android_push_notification(self.user_profile, {})
+        mock_error.assert_called_with("Attempting to send a GCM push "
+                                      "notification, but no API key was "
+                                      "configured")
+
+class GCMSuccessTest(GCMTest):
+    @mock.patch('logging.warning')
+    @mock.patch('logging.info')
+    @mock.patch('gcmclient.GCM.send')
+    def test_success(self, mock_send, mock_info, mock_warning):
+        # type: (mock.MagicMock, mock.MagicMock, mock.MagicMock) -> None
+        res = mock.MagicMock()
+        res.success = {token: ind for ind, token in enumerate(self.gcm_tokens)}
+        res.needs_retry.return_value = False
+        mock_send.return_value = res
+
+        data = self.get_gcm_data()
+        apn.send_android_push_notification(self.user_profile, data)
+        self.assertEqual(mock_info.call_count, 2)
+        c1 = call("GCM: Sent 1111 as 0")
+        c2 = call("GCM: Sent 2222 as 1")
+        mock_info.assert_has_calls([c1, c2])
+        mock_warning.assert_not_called()
+
+class GCMCanonicalTest(GCMTest):
+    @mock.patch('logging.warning')
+    @mock.patch('gcmclient.GCM.send')
+    def test_equal(self, mock_send, mock_warning):
+        # type: (mock.MagicMock, mock.MagicMock) -> None
+        res = mock.MagicMock()
+        res.canonical = {1: 1}
+        res.needs_retry.return_value = False
+        mock_send.return_value = res
+
+        data = self.get_gcm_data()
+        apn.send_android_push_notification(self.user_profile, data)
+        mock_warning.assert_called_once_with("GCM: Got canonical ref but it "
+                                             "already matches our ID 1!")
+
+    @mock.patch('logging.warning')
+    @mock.patch('gcmclient.GCM.send')
+    def test_pushdevice_not_present(self, mock_send, mock_warning):
+        # type: (mock.MagicMock, mock.MagicMock) -> None
+        res = mock.MagicMock()
+        t1 = apn.hex_to_b64(u'1111')
+        t2 = apn.hex_to_b64(u'3333')
+        res.canonical = {t1: t2}
+        res.needs_retry.return_value = False
+        mock_send.return_value = res
+
+        def get_count(token):
+            token = apn.hex_to_b64(token)
+            return PushDeviceToken.objects.filter(
+                token=token, kind=PushDeviceToken.GCM).count()
+
+        self.assertEqual(get_count(u'1111'), 1)
+        self.assertEqual(get_count(u'3333'), 0)
+
+        data = self.get_gcm_data()
+        apn.send_android_push_notification(self.user_profile, data)
+        msg = ("GCM: Got canonical ref %s "
+               "replacing %s but new ID not "
+               "registered! Updating.")
+        mock_warning.assert_called_once_with(msg % (t2, t1))
+
+        self.assertEqual(get_count(u'1111'), 0)
+        self.assertEqual(get_count(u'3333'), 1)
+
+    @mock.patch('logging.info')
+    @mock.patch('gcmclient.GCM.send')
+    def test_pushdevice_different(self, mock_send, mock_info):
+        # type: (mock.MagicMock, mock.MagicMock) -> None
+        res = mock.MagicMock()
+        old_token = apn.hex_to_b64(u'1111')
+        new_token = apn.hex_to_b64(u'2222')
+        res.canonical = {old_token: new_token}
+        res.needs_retry.return_value = False
+        mock_send.return_value = res
+
+        def get_count(token):
+            token = apn.hex_to_b64(token)
+            return PushDeviceToken.objects.filter(
+                token=token, kind=PushDeviceToken.GCM).count()
+
+        self.assertEqual(get_count(u'1111'), 1)
+        self.assertEqual(get_count(u'2222'), 1)
+
+        data = self.get_gcm_data()
+        apn.send_android_push_notification(self.user_profile, data)
+        mock_info.assert_called_once_with(
+            "GCM: Got canonical ref %s, dropping %s" % (new_token, old_token))
+
+        self.assertEqual(get_count(u'1111'), 0)
+        self.assertEqual(get_count(u'2222'), 1)
+
+class GCMNotRegisteredTest(GCMTest):
+    @mock.patch('logging.info')
+    @mock.patch('gcmclient.GCM.send')
+    def test_not_registered(self, mock_send, mock_info):
+        # type: (mock.MagicMock, mock.MagicMock) -> None
+        res = mock.MagicMock()
+        token = apn.hex_to_b64(u'1111')
+        res.not_registered = [token]
+        res.needs_retry.return_value = False
+        mock_send.return_value = res
+
+        def get_count(token):
+            token = apn.hex_to_b64(token)
+            return PushDeviceToken.objects.filter(
+                token=token, kind=PushDeviceToken.GCM).count()
+
+        self.assertEqual(get_count(u'1111'), 1)
+
+        data = self.get_gcm_data()
+        apn.send_android_push_notification(self.user_profile, data)
+        mock_info.assert_called_once_with("GCM: Removing %s" % (token,))
+        self.assertEqual(get_count(u'1111'), 0)
+
+class GCMFailureTest(GCMTest):
+    @mock.patch('logging.warning')
+    @mock.patch('gcmclient.GCM.send')
+    def test_failure(self, mock_send, mock_warn):
+        # type: (mock.MagicMock, mock.MagicMock) -> None
+        res = mock.MagicMock()
+        token = apn.hex_to_b64(u'1111')
+        res.failed = {token: 1}
+        res.needs_retry.return_value = True
+        mock_send.return_value = res
+
+        data = self.get_gcm_data()
+        apn.send_android_push_notification(self.user_profile, data)
+        c1 = call("GCM: Delivery to %s failed: 1" % (token,))
+        c2 = call("GCM: delivery needs a retry but ignoring")
+        mock_warn.assert_has_calls([c1, c2])
