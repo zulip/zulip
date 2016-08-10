bcf70561bf84c95f271aa5689504e0bd3fdcb1ca
Tim Abbott <tabbott@mit.edu>
2016-08-09 18:40:07 -0700

    travis: Remove rabbitmq nodename dependency on hostname.
    Because rabbitmq doesn't support changing the nodename of a running
    rabbitmq node, Zulip installations suffered a plague of issues where
    e.g. a Zulip server would reboot, the hostname would change, and
    suddenly the local rabbitmq instance being used by Zulip would stop
    working.
    
    We address this problem by using, by default, a fixed rabbitmq
    nodename, but providing server administrators the option to set the
    rabbitmq nodename used by Zulip however they choose.
    
    To upgrade an existing server to use this new configuration, one will
    need to add something like the following to /etc/zulip/zulip.conf:
    
    [rabbitmq]
    nodename = zulip@localhost
    
    However, I don't believe we have the puppet code in place to make this
    work correctly at initial installation without rabbitmq-server being
    already installed (but off), as we can easily setup in Travis CI but I
    haven't been willing to do for the installer.  So for now, this just
    fixes our Travis CI problems.
    
    Fixes: #1579.

--- puppet/zulip/manifests/rabbit.pp
+++ puppet/zulip/manifests/rabbit.pp
@@ -40,6 +40,18 @@ class zulip::rabbit {
     source => "puppet:///modules/zulip/rabbitmq/rabbitmq.config",
   }
 
+  $rabbitmq_nodename = zulipconf("rabbitmq", "nodename", "")
+  if $rabbitmq_nodename != "" {
+    file { "/etc/rabbitmq/rabbitmq-env.conf":
+      require => Package[rabbitmq-server],
+      before => Service[rabbitmq-server],
+      ensure => file,
+      owner  => "root",
+      group  => "root",
+      mode => 644,
+      content => template("zulip/rabbitmq-env.conf.template.erb"),
+    }
+  }
   # epmd doesn't have an init script, so we just check if it is
   # running, and if it isn't, start it.  Even in case of a race, this
   # won't leak epmd processes, because epmd checks if one is already
@@ -53,7 +65,9 @@ class zulip::rabbit {
 
   service { "rabbitmq-server":
     ensure => running,
-    require => Exec["epmd"],
+    require => [Exec["epmd"],
+                File["/etc/rabbitmq/rabbitmq.config"],
+                File["/etc/default/rabbitmq-server"]],
   }
 
   # TODO: Should also call exactly once "configure-rabbitmq"
