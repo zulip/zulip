2620065f032c0e1aa21a95ae302daf5c10fc1bb8
Rishi Gupta <rishig@kandralabs.com>
2016-08-13 18:32:11 -0700

    Refactor /integrations page links to support subdomains.
    Also changes all links to /integrations to be relative rather than
    absolute, so that users will primarily access the /integrations page
    for their subdomain.

--- zerver/views/integrations.py
+++ zerver/views/integrations.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 from django.views.generic import TemplateView
+from django.conf import settings
 
 from zerver.lib.integrations import INTEGRATIONS
 
@@ -10,4 +11,16 @@ class IntegrationView(TemplateView):
         context = super(IntegrationView, self).get_context_data(**kwargs)
         alphabetical_sorted_integration = OrderedDict(sorted(INTEGRATIONS.items()))
         context['integrations_dict'] = alphabetical_sorted_integration
+
+        settings_html = '<a href="../#settings">Zulip settings page</a>'
+        subscriptions_html = '<a target="_blank" href="../#subscriptions">subscriptions page</a>'
+
+        external_api_path_subdomain = settings.EXTERNAL_API_PATH
+        external_api_uri_subdomain = settings.EXTERNAL_API_URI
+
+        context['settings_html'] = settings_html
+        context['subscriptions_html'] = subscriptions_html
+        context['external_api_path_subdomain'] = external_api_path_subdomain
+        context['external_api_uri_subdomain'] = external_api_uri_subdomain
+
         return context
