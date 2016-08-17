804b2593bf171b20c222e1ca24b929f75e59cc25
Brock Whittaker <brockwhittaker@Brocks-MacBook.local>
2016-08-17 12:02:59 -0700

    Live update new avatar across users.
    This sends an event when a new avatar is uploaded that refreshes the
    avatar on all screens without the need to reload the browser anymore.
    
    Fixes: #1359.

--- static/js/people.js
+++ static/js/people.js
@@ -143,6 +143,20 @@ exports.update = function update(person) {
         }
     }
 
+    if (_.has(person, 'avatar_url')) {
+        var url = person.avatar_url;
+        person_obj.avatar_url = url;
+
+        if (util.is_current_user(person.email)) {
+          page_params.avatar_url = url;
+          $("#user-settings-avatar").attr("src", url);
+        }
+
+        $(".inline_profile_picture").css({
+          "background-image": "url(" + url + ")"
+        });
+    }
+
     activity.set_user_statuses([]);
 
     // TODO: update sender names on messages
--- zerver/lib/actions.py
+++ zerver/lib/actions.py
@@ -1649,6 +1649,15 @@ def do_change_avatar_source(user_profile, avatar_source, log=True):
                         bot=dict(email=user_profile.email,
                                 avatar_url=avatar_url(user_profile),)),
                     bot_owner_userids(user_profile))
+    else:
+        payload = dict(email=user_profile.email,avatar_url=avatar_url(user_profile))
+        send_event(
+            dict(type='realm_user',
+                 op='update',
+                 person=payload
+            ),
+            active_user_ids(user_profile.realm)
+        )
 
 def _default_stream_permision_check(user_profile, stream):
     # type: (UserProfile, Optional[Stream]) -> None
