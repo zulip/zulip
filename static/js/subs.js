0f8cf924ad8961be79fcd9031fa33609a859d8d9
Brock Whittaker <brock@zulipchat.com>
2016-08-23 14:25:28 -0700

    Fix for broken 'for' attribute.
    The ‘for’ attribute is not valid HTML in the case of this because the
    emails are invalid character sets and the input has no ID with the
    email.
    
    This changes it to a data-name which is still searchable but doesn’t
    interfere with typical input behavior.

--- static/js/subs.js
+++ static/js/subs.js
@@ -807,7 +807,7 @@ $(function () {
         // Hide users which aren't in filtered users
         _.each(users, function (user) {
             var display_type = filtered_users.hasOwnProperty(user.email)? "block" : "none";
-            $("label[for='" + user.email + "']").css({"display":display_type});
+            $("label[data-name='" + user.email + "']").css({"display":display_type});
         });
 
         update_announce_stream_state();
