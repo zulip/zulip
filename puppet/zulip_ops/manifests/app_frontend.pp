1d1b0894a334355c861caea9c0ef7ebf7ca6f775
Tim Abbott <tabbott@zulipchat.com>
2016-08-23 22:42:33 -0700

    zulip_ops: Add logrotate configuration from main zulip.

--- puppet/zulip_ops/manifests/app_frontend.pp
+++ puppet/zulip_ops/manifests/app_frontend.pp
@@ -9,6 +9,14 @@ class zulip_ops::app_frontend {
                    ]
   package { $app_packages: ensure => "installed" }
 
+  file { "/etc/logrotate.d/zulip":
+    ensure => file,
+    owner  => "root",
+    group  => "root",
+    mode => 644,
+    source => "puppet:///modules/zulip/logrotate/zulip",
+  }
+
   file { '/etc/log2zulip.conf':
     ensure     => file,
     owner      => "zulip",
