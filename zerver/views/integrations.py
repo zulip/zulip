f9e41b1387e7a7098b75f055add3ba187ff28f9e
Tim Abbott <tabbott@zulipchat.com>
2016-09-13 10:09:03 -0700

    integrations: Factor out ApiURLView base class.

--- zerver/views/integrations.py
+++ zerver/views/integrations.py
@@ -11,25 +11,33 @@ from zerver.lib import bugdown
 from zerver.lib.integrations import INTEGRATIONS
 from zproject.jinja2 import render_to_response
 
-class IntegrationView(TemplateView):
+class ApiURLView(TemplateView):
+    def get_context_data(self, **kwargs):
+        # type: (Optional[Dict[str, Any]]) -> Dict[str, str]
+        context = super(ApiURLView, self).get_context_data(**kwargs)
+
+        external_api_path_subdomain = settings.EXTERNAL_API_PATH
+        external_api_uri_subdomain = settings.EXTERNAL_API_URI
+
+        context['external_api_path_subdomain'] = external_api_path_subdomain
+        context['external_api_uri_subdomain'] = external_api_uri_subdomain
+
+        return context
+
+class IntegrationView(ApiURLView):
     template_name = 'zerver/integrations.html'
 
     def get_context_data(self, **kwargs):
-        # type: (Optional[Dict[str, Any]]) -> Dict[str, str]
-        context = super(IntegrationView, self).get_context_data(**kwargs)
+        # type: (Optional[Dict[str, Any]]) -> Dict[str, Any]
+        context = super(IntegrationView, self).get_context_data(**kwargs)  # type: Dict[str, Any]
         alphabetical_sorted_integration = OrderedDict(sorted(INTEGRATIONS.items()))
         context['integrations_dict'] = alphabetical_sorted_integration
 
         settings_html = '<a href="../#settings">Zulip settings page</a>'
         subscriptions_html = '<a target="_blank" href="../#subscriptions">subscriptions page</a>'
 
-        external_api_path_subdomain = settings.EXTERNAL_API_PATH
-        external_api_uri_subdomain = settings.EXTERNAL_API_URI
-
         context['settings_html'] = settings_html
         context['subscriptions_html'] = subscriptions_html
-        context['external_api_path_subdomain'] = external_api_path_subdomain
-        context['external_api_uri_subdomain'] = external_api_uri_subdomain
 
         return context
 
