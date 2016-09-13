dec01624f4bb3ec3f740095e7e224f5aee9eac7f
Tomasz Kolek <tomasz-kolek@o2.pl>
2016-09-13 22:40:13 +0200

    Move generate-fixtures checking parts to manage.py commands.
    Create libs of getting migration status and checking if database
    exists. Add new commands to manage.py that uses these libraries.

---
+++ zerver/management/commands/check_if_database_exist.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import argparse
+from typing import Any
+from django.core.management.base import BaseCommand
+
+from tools.lib.check_if_database_exist import check_if_database_exist
+
+
+class Command(BaseCommand):
+    help = "Checks if database, by given database's name, exists."
+
+    def add_arguments(self, parser):
+        # type: (argparse.ArgumentParser) -> None
+        parser.add_argument('database_name', nargs='?',
+                            help="Database name that existence we'd like to check")
+
+    def handle(self, *args, **options):
+        # type: (*Any, **Any) -> None
+        if check_if_database_exist(**options):
+            output = "0"
+        else:
+            output = "1"
+        self.stdout.write(output)
