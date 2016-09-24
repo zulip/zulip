9e16a0d4c87b76bacdf55c758e6cbedf9d6e37da
Mohsen Ibrahim <mohsen47@hotmail.co.uk>
2016-09-24 10:12:31 +0300

    Resolve #1743
    I had to check if the operator is in the list of known operators
    before continuing to run the code which will narrow to the search
    results if it was NOT in the list of konwn operators I woudl change
    its operand to the contactination of the operator + : + the previous
    operand and change the operator to "search" then continue running
    the code as normal.

--- static/js/narrow.js
+++ static/js/narrow.js
@@ -148,6 +148,11 @@ exports.activate = function (raw_operators, opts) {
     if (raw_operators.length === 0) {
         return exports.deactivate();
     }
+    known_operators = ["stream", "topic", "is", "pm-with"];
+    if (known_operators.indexOf(raw_operators[0].operator) == -1){
+      raw_operators[0].operand = raw_operators[0].operator + ":" + raw_operators[0].operand;
+      raw_operators[0].operator = "search";
+    }
     var filter = new Filter(raw_operators);
     var operators = filter.operators();
 
