d21d443d29764940f411074e9fb261f6686fe232
Steve Howell <showell30@yahoo.com>
2016-10-04 09:12:09 -0700

    Deprecate render_old_messages command.

--- zilencer/management/commands/render_old_messages.py
+++ zilencer/management/commands/render_old_messages.py
@@ -9,6 +9,7 @@ import zerver.lib.bugdown as bugdown
 from zerver.lib.message import re_render_content_for_management_command
 from zerver.models import Message
 import datetime
+import sys
 import time
 
 class Command(BaseCommand):
@@ -18,6 +19,14 @@ Usage: python manage.py render_old_messages"""
 
     def handle(self, *args, **options):
         # type: (*Any, **Any) -> None
+        print('''
+            This command is currently not supported, and it can be somewhat
+            dangerous to run on large instances.  Before upgrading messages
+            to a new version, you should make sure that the old renderings
+            are actually invalid; it could be quite the opposite (we might
+            not want to render V1 messages with V2).
+            ''')
+        sys.exit(1)
         total_rendered = 0
         while True:
             messages = Message.objects.exclude(rendered_content_version=bugdown.version)[0:100]
