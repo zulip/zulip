32934a02e9989841c244b231428b2f81b4b65ed6
Tomasz Kolek <tomasz-kolek@go2.pl>
2016-10-05 19:21:47 +0200

    Add template for subject that contains repo name and branch name.

--- zerver/lib/webhooks/git.py
+++ zerver/lib/webhooks/git.py
@@ -1,6 +1,8 @@
 from six import text_type
 from typing import Optional, Any
 
+SUBJECT_WITH_BRANCH_TEMPLATE = u'{repo} / {branch}'
+
 PUSH_COMMITS_LIMIT = 10
 PUSH_PUSHED_TEXT_WITH_URL = u"[pushed]({compare_url})"
 PUSH_PUSHED_TEXT_WITHOUT_URL = u"pushed"
