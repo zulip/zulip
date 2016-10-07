35cf9092d5b60f89a5763ba7bc9001806435520d
Rishi Gupta <rishig@kandralabs.com>
2016-10-06 17:47:05 -0700

    analytics: Canonicalize creation of model objects in tests.
    Add a default_realm object to AnalyticsTestCase, created 2 days before
    AnalyticsTestCase.TIME_ZERO.
    
    Add lightweight create_user, create_stream, and create_message methods to
    AnalyticsTestCase, with sensible defaults. In particular, all objects are by
    default created at AnalyticsTestCase.TIME_LAST_HOUR, so that they are
    included when running AnalyticsTestCase.process_last_hour.

--- analytics/tests/test_counts.py
+++ analytics/tests/test_counts.py
@@ -10,8 +10,7 @@ from analytics.lib.counts import CountStat, process_count_stat, \
     zerver_count_message_by_huddle
 from analytics.models import UserCount, RealmCount, StreamCount, InstallationCount, Stream, Recipient
 
-from zerver.lib.test_helpers import make_client, get_stream
-from zerver.models import Realm, UserProfile, Message, get_user_profile_by_email
+from zerver.models import Realm, UserProfile, Message, get_user_profile_by_email, get_client
 
 from typing import Any
 from six import text_type
@@ -23,6 +22,11 @@ class AnalyticsTestCase(TestCase):
     TIME_ZERO = datetime(2042, 3, 14).replace(tzinfo=timezone.utc)
     TIME_LAST_HOUR = TIME_ZERO - HOUR
 
+    def setUp(self):
+        # type: () -> None
+        self.default_realm = Realm.objects.create(domain='analytics.test', name='Realm Test',
+                                                  date_created=self.TIME_ZERO - 2*self.DAY)
+
     def process_last_hour(self, stat):
         # type: (CountStat) -> None
         # The last two arguments below are eventually passed as the first and
@@ -30,50 +34,44 @@ class AnalyticsTestCase(TestCase):
         # inclusive range.
         process_count_stat(stat, self.TIME_ZERO, self.TIME_ZERO)
 
-class TestDataCollectors(AnalyticsTestCase):
+    # Lightweight creation of users, streams, and messages
     def create_user(self, email, **kwargs):
         # type: (str, **Any) -> UserProfile
-        defaults = {'realm': self.realm,
-                    'full_name': 'full_name',
-                    'short_name': 'short_name',
-                    'pointer': -1,
-                    'last_pointer_updater': 'seems unused?',
-                    'api_key': '42'}
+        defaults = {
+            'date_joined': self.TIME_LAST_HOUR,
+            'full_name': 'full_name',
+            'short_name': 'short_name',
+            'pointer': -1,
+            'last_pointer_updater': 'seems unused?',
+            'realm': self.default_realm,
+            'api_key': '42'}
         for key, value in defaults.items():
             kwargs[key] = kwargs.get(key, value)
-        user = UserProfile(email=email, **kwargs)
-        user.save()
-        return user
+        return UserProfile.objects.create(email=email, **kwargs)
 
     def create_stream(self, **kwargs):
         # type: (**Any) -> Stream
-        defaults = {'realm': self.realm,
-                    'description': ''}
+        defaults = {'name': 'stream name',
+                    'realm': self.default_realm,
+                    'date_created': self.TIME_LAST_HOUR}
         for key, value in defaults.items():
             kwargs[key] = kwargs.get(key, value)
-        stream = Stream(**kwargs)
-        stream.save()
-        return stream
+        return Stream.objects.create(**kwargs)
 
     def create_message(self, sender, recipient, **kwargs):
         # type: (UserProfile, Recipient, **Any) -> Message
-        sending_client = make_client(name="analytics test")
         defaults = {
             'sender': sender,
             'recipient': recipient,
-            'subject': 'whatever',
-            'content': 'hello **world**',
-            'pub_date': self.TIME_ZERO,
-            'sending_client': sending_client,
-            'last_edit_time': self.TIME_ZERO,
-            'edit_history': '[]'
-        }
+            'subject': 'subject',
+            'content': 'hi',
+            'pub_date': self.TIME_LAST_HOUR,
+            'sending_client': get_client("website")}
         for key, value in defaults.items():
             kwargs[key] = kwargs.get(key, value)
-        message = Message(**kwargs)
-        message.save()
-        return message
+        return Message.objects.create(**kwargs)
 
+class TestDataCollectors(AnalyticsTestCase):
     # TODO uk refactor tests to use this assert
     def assertRealmCountEquals(self, realm, property, interval, value):
         # type: (Realm, str, str, Any) -> None
@@ -82,18 +80,6 @@ class TestDataCollectors(AnalyticsTestCase):
                                                       interval=interval).values_list('value', flat=True)[0]
 
         self.assertEqual(realm_count_value, value)
-
-    def setUp(self):
-        # type: () -> None
-        # almost every test will need a time_interval, realm, and user
-        self.realm = Realm(domain='analytics.test', name='Realm Test',
-                           date_created=self.TIME_ZERO - 2*self.DAY)
-        self.realm.save()
-        # don't pull the realm object back from the database every time we need its id
-        self.realm_id = self.realm.id
-        self.user = self.create_user('email', is_bot=False, is_active=True,
-                                     date_joined=self.TIME_ZERO)
-        self.user_id = self.user.id
 
     def test_human_and_bot_count_by_realm(self):
         # type: () -> None
@@ -105,22 +91,19 @@ class TestDataCollectors(AnalyticsTestCase):
                       'hour', 'hour')]
 
         # TODO these dates should probably be explicit, since the default args for the commands are timezone.now() dependent.
-        self.create_user('test_bot1', is_bot=True, is_active=True,
-                         date_joined=self.TIME_LAST_HOUR)
-        self.create_user('test_bot2', is_bot=True, is_active=True,
-                         date_joined=self.TIME_LAST_HOUR)
-        self.create_user('test_human', is_bot=False, is_active=True,
-                         date_joined=self.TIME_LAST_HOUR)
+        self.create_user('email1-bot', is_bot=True)
+        self.create_user('email2-bot', is_bot=True)
+        self.create_user('email3-human', is_bot=False)
 
         for stat in stats:
             self.process_last_hour(stat)
 
-        human_row = RealmCount.objects.filter(realm=self.realm, interval='day',
+        human_row = RealmCount.objects.filter(realm=self.default_realm, interval='day',
                                               property='test_active_humans') \
                                       .values_list('value', flat=True)[0]
         assert (human_row == 1)
 
-        bot_row = RealmCount.objects.filter(realm=self.realm, interval='day',
+        bot_row = RealmCount.objects.filter(realm=self.default_realm, interval='day',
                                             property='test_active_bots') \
                                     .values_list('value', flat=True)[0]
         assert (bot_row == 2)
@@ -131,16 +114,16 @@ class TestDataCollectors(AnalyticsTestCase):
         stat = CountStat('add_new_user_test', zerver_count_user_by_realm, {}, 'hour', 'hour')
 
         # add new users to realm in last hour
-        self.create_user('email_1', date_joined=self.TIME_LAST_HOUR)
-        self.create_user('email_2', date_joined=self.TIME_LAST_HOUR)
+        self.create_user('email1')
+        self.create_user('email2')
 
         # add a new user before an hour
-        self.create_user('email_3', date_joined=self.TIME_ZERO - 2*self.HOUR)
+        self.create_user('email3', date_joined=self.TIME_ZERO - 2*self.HOUR)
 
         # check if user added before the hour is not included
         self.process_last_hour(stat)
         # do_update is writing the stat.property to all zerver tables
-        row = RealmCount.objects.filter(realm=self.realm, property='add_new_user_test',
+        row = RealmCount.objects.filter(realm=self.default_realm, property='add_new_user_test',
                                         interval='hour').values_list('value', flat=True)[0]
         # assert only 2 users
         assert (row == 2)
@@ -153,18 +136,15 @@ class TestDataCollectors(AnalyticsTestCase):
                          {'invite_only': False}, 'hour', 'hour')
 
         # add some stuff to zerver_*
-        self.create_stream(name='stream1', description='test_analytics_stream',
-                           date_created=self.TIME_ZERO)
-        self.create_stream(name='stream2', description='test_analytics_stream',
-                           date_created=self.TIME_ZERO)
-        self.create_stream(name='stream3', description='test_analytics_stream',
-                           date_created=self.TIME_ZERO)
+        self.create_stream(name='stream1')
+        self.create_stream(name='stream2')
+        self.create_stream(name='stream3')
 
         # run do_pull_from_zerver
         self.process_last_hour(stat)
 
         # check analytics_* values are correct
-        row = RealmCount.objects.filter(realm=self.realm, interval='day',
+        row = RealmCount.objects.filter(realm=self.default_realm, interval='day',
                                         property='test_stat_write').values_list('value', flat=True)[0]
         assert (row == 3)
 
@@ -172,12 +152,9 @@ class TestDataCollectors(AnalyticsTestCase):
     def test_process_count(self):
         # type: () -> None
         # add some active and inactive users that are human
-        self.create_user('inactive_human_1', is_bot=False, is_active=False,
-                         date_joined=self.TIME_LAST_HOUR)
-        self.create_user('inactive_human_2', is_bot=False, is_active=False,
-                         date_joined=self.TIME_LAST_HOUR)
-        self.create_user('active_human', is_bot=False, is_active=True,
-                         date_joined=self.TIME_LAST_HOUR)
+        self.create_user('email1', is_bot=False, is_active=False)
+        self.create_user('email2', is_bot=False, is_active=False)
+        self.create_user('email3', is_bot=False, is_active=True)
 
         # run stat to pull active humans
         stat = CountStat('active_humans', zerver_count_user_by_realm,
@@ -186,7 +163,7 @@ class TestDataCollectors(AnalyticsTestCase):
         self.process_last_hour(stat)
 
         # get row in analytics table
-        row_before = RealmCount.objects.filter(realm=self.realm, interval='day',
+        row_before = RealmCount.objects.filter(realm=self.default_realm, interval='day',
                                                property='active_humans')\
                                        .values_list('value', flat=True)[0]
 
@@ -194,7 +171,7 @@ class TestDataCollectors(AnalyticsTestCase):
         self.process_last_hour(stat)
 
         # check if row is same as before
-        row_after = RealmCount.objects.filter(realm=self.realm, interval='day',
+        row_after = RealmCount.objects.filter(realm=self.default_realm, interval='day',
                                               property='active_humans').values_list('value', flat=True)[0]
 
         assert (row_before == 1)
@@ -205,18 +182,14 @@ class TestDataCollectors(AnalyticsTestCase):
         # type: () -> None
         stat = CountStat('test_messages_sent', zerver_count_message_by_user, {}, 'hour', 'hour')
 
-        self.create_user('human1', is_bot=False, is_active=True,
-                         date_joined=self.TIME_LAST_HOUR)
-
-        human1 = get_user_profile_by_email('human1')
-        human2 = get_user_profile_by_email('email')
-
-        self.create_message(human1, Recipient(human2.id),
-                            pub_date=self.TIME_LAST_HOUR)
+        user1 = self.create_user('email1')
+        user2 = self.create_user('email2')
+        recipient = Recipient.objects.create(type_id=user2.id, type=Recipient.PERSONAL)
+        self.create_message(user1, recipient)
 
         # run command
         self.process_last_hour(stat)
-        usercount_row = UserCount.objects.filter(realm=self.realm, interval='hour',
+        usercount_row = UserCount.objects.filter(realm=self.default_realm, interval='hour',
                                                  property='test_messages_sent').values_list(
             'value', flat=True)[0]
         assert (usercount_row == 1)
@@ -227,12 +200,12 @@ class TestDataCollectors(AnalyticsTestCase):
 
         # check new row has no entries, old ones still there
         updated_usercount_row = UserCount.objects.filter(
-            realm=self.realm, interval='hour', property='test_messages_sent') \
+            realm=self.default_realm, interval='hour', property='test_messages_sent') \
                                                  .values_list('value', flat=True)[0]
 
-        new_row = UserCount.objects.filter(realm=self.realm, interval='hour', property='test_messages_sent',
+        new_row = UserCount.objects.filter(realm=self.default_realm, interval='hour',
+                                           property='test_messages_sent',
                                            end_time=self.TIME_ZERO - 3*self.HOUR).exists()
-
         self.assertFalse(new_row)
 
         assert (updated_usercount_row == 1)
@@ -244,32 +217,24 @@ class TestDataCollectors(AnalyticsTestCase):
         stat = CountStat('test_messages_aggregate', zerver_count_message_by_user, {}, 'day', 'hour')
 
         # write some messages
-        self.create_user('human1', is_bot=False, is_active=True,
-                         date_joined=self.TIME_LAST_HOUR)
+        user1 = self.create_user('email1')
+        user2 = self.create_user('email2')
+        recipient = Recipient.objects.create(type_id=user2.id, type=Recipient.PERSONAL)
 
-        human1 = get_user_profile_by_email('human1')
-        human2 = get_user_profile_by_email('email')
-
-        self.create_message(human1, Recipient(human2.id),
-                            pub_date=self.TIME_LAST_HOUR,
-                            content="hi")
-        self.create_message(human1, Recipient(human2.id),
-                            pub_date=self.TIME_LAST_HOUR,
-                            content="hello")
-        self.create_message(human1, Recipient(human2.id),
-                            pub_date=self.TIME_LAST_HOUR,
-                            content="bye")
+        self.create_message(user1, recipient)
+        self.create_message(user1, recipient)
+        self.create_message(user1, recipient)
 
         # run command
         self.process_last_hour(stat)
 
         # check no rows for hour interval on usercount granularity
-        usercount_row = UserCount.objects.filter(realm=self.realm, interval='hour').exists()
+        usercount_row = UserCount.objects.filter(realm=self.default_realm, interval='hour').exists()
 
         self.assertFalse(usercount_row)
 
         # see if aggregated correctly to realmcount and installationcount
-        realmcount_row = RealmCount.objects.filter(realm=self.realm, interval='day',
+        realmcount_row = RealmCount.objects.filter(realm=self.default_realm, interval='day',
                                                    property='test_messages_aggregate').values_list(
             'value', flat=True)[0]
         assert (realmcount_row == 3)
@@ -284,22 +249,18 @@ class TestDataCollectors(AnalyticsTestCase):
         stat = CountStat('test_messages_to_stream', zerver_count_message_by_stream, {}, 'hour', 'hour')
 
         # write some messages
-        user = get_user_profile_by_email('email')
+        user = self.create_user('email')
+        stream = self.create_stream(date_created=self.TIME_ZERO - 2*self.HOUR)
 
-        self.create_stream(name='stream1', description='test_analytics_stream',
-                           date_created=self.TIME_ZERO - 2*self.HOUR)
-
-        stream1 = get_stream('stream1', self.realm)
-        recipient = Recipient(type_id=stream1.id, type=2)
+        recipient = Recipient(type_id=stream.id, type=Recipient.STREAM)
         recipient.save()
 
-        self.create_message(user, recipient, pub_date=self.TIME_LAST_HOUR,
-                            content='hi')
+        self.create_message(user, recipient = recipient)
 
         # run command
         self.process_last_hour(stat)
 
-        stream_row = StreamCount.objects.filter(realm=self.realm, interval='hour',
+        stream_row = StreamCount.objects.filter(realm=self.default_realm, interval='hour',
                                                 property='test_messages_to_stream').values_list(
             'value', flat=True)[0]
         assert (stream_row == 1)
@@ -309,16 +270,14 @@ class TestDataCollectors(AnalyticsTestCase):
         stat = CountStat('test_active_humans', zerver_count_user_by_realm,
                          {'is_bot': False, 'is_active': True}, 'hour', 'hour')
 
-        self.realm.date_created = self.TIME_ZERO
-        self.realm.save()
-        self.create_user('human1', is_bot=False, is_active=True,
-                         date_joined=self.TIME_ZERO)
+        realm = Realm.objects.create(domain='domain', name='name', date_created=self.TIME_ZERO)
+        self.create_user('email', realm=realm)
 
         # run count prior to realm creation
         process_count_stat(stat, range_start=self.TIME_ZERO - 2*self.HOUR,
                            range_end=self.TIME_LAST_HOUR)
         realm_count = RealmCount.objects.values('realm__name', 'value', 'property') \
-                                        .filter(realm=self.realm, interval='hour').exists()
+                                        .filter(realm=realm, interval='hour').exists()
         # assert no rows exist
         self.assertFalse(realm_count)
 
@@ -329,13 +288,12 @@ class TestDataCollectors(AnalyticsTestCase):
         stat = CountStat('test_active_humans', zerver_count_user_by_realm,
                          {'is_bot': False, 'is_active': True}, 'hour', 'hour')
 
-        self.create_user('human1', is_bot=False, is_active=True,
-                         date_joined=self.TIME_LAST_HOUR)
+        self.create_user('email')
 
         process_count_stat(stat, range_start=self.TIME_ZERO - 2*self.HOUR,
                            range_end=self.TIME_ZERO)
         realm_count = RealmCount.objects.values('end_time', 'value') \
-                                        .filter(realm=self.realm, interval='hour')
+                                        .filter(realm=self.default_realm, interval='hour')
         empty1 = realm_count.filter(end_time=self.TIME_ZERO - 2*self.HOUR) \
                             .values_list('value', flat=True)[0]
         empty2 = realm_count.filter(end_time=self.TIME_LAST_HOUR) \
