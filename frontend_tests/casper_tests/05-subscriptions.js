b086f2b74b6391f90a2151ba93f78c1d8f11cbdb
trueskawka <malavarena@gmail.com>
2016-10-12 03:45:25 -0400

    stream_creation: Add a clear error message for duplicate streams.
    User is now unable to create a stream with duplicate or empty name
    through the create stream modal. An appropriate error message appears
    on attempt.

--- frontend_tests/casper_tests/05-subscriptions.js
+++ frontend_tests/casper_tests/05-subscriptions.js
@@ -66,7 +66,8 @@ casper.waitFor(function () {
     });
 });
 casper.then(function () {
-    casper.test.assertSelectorHasText('.subscription_name', 'Waseemio', 'Subscribing to a stream');
+    casper.test.info("User should be subscribed to stream Waseemio");
+    casper.test.assertSelectorHasText('.subscription_name', 'Waseemio');
     casper.fill('form#add_new_subscription', {stream_name: 'WASeemio'});
     casper.click('form#add_new_subscription input.btn');
 });
@@ -74,19 +75,23 @@ casper.waitForText('Already subscribed', function () {
     casper.test.assertTextExists('Already subscribed', "Can't subscribe twice to a stream");
     casper.fill('form#add_new_subscription', {stream_name: '  '});
     casper.click('form#add_new_subscription input.btn');
-});
-casper.waitForText('Create stream', function () {
-    casper.test.assertTextExists('Create stream', 'Modal for specifying new stream users');
     casper.fill('form#stream_creation_form', {stream_name: '  '});
     casper.click('form#stream_creation_form button.btn.btn-primary');
 });
-casper.waitForText('Error creating stream', function () {
-    casper.test.assertTextExists('Invalid stream name', "Can't create a stream without a name");
+casper.waitForText('A stream needs to have a name', function () {
+    casper.test.assertTextExists('A stream needs to have a name', "Can't create a stream with an empty name");
+    casper.click('form#stream_creation_form button.btn.btn-default');
+    casper.fill('form#add_new_subscription', {stream_name: '  '});
+    casper.click('form#add_new_subscription input.btn');
+    casper.fill('form#stream_creation_form', {stream_name: 'Waseemio'});
+    casper.click('form#stream_creation_form button.btn.btn-primary');
 });
-casper.then(function () {
+casper.waitForText('A stream with this name already exists', function () {
+    casper.test.assertTextExists('A stream with this name already exists', "Can't create a stream with a duplicate name");
     casper.test.info('Streams should be filtered when typing in the create box');
+    casper.click('form#stream_creation_form button.btn.btn-default');
 });
-casper.then(function () {
+casper.waitForText('Filter by stream name', function () {
     casper.test.assertSelectorHasText('.subscription_row .subscription_name', 'Verona', 'Verona stream exists before filtering');
     casper.test.assertSelectorDoesntHaveText('.subscription_row.notdisplayed .subscription_name', 'Verona', 'Verona stream shown before filtering');
 });
@@ -97,7 +102,7 @@ casper.then(function () {
         .trigger($.Event('input'));
     });
 });
-casper.then(function () {
+casper.waitForSelectorTextChange('form#add_new_subscription', function () {
     casper.test.assertSelectorHasText('.subscription_row.notdisplayed .subscription_name', 'Verona', 'Verona stream not shown after filtering');
     casper.test.assertSelectorHasText('.subscription_row .subscription_name', 'Waseemio', 'Waseemio stream exists after filtering');
     casper.test.assertSelectorDoesntHaveText('.subscription_row.notdisplayed .subscription_name', 'Waseemio', 'Waseemio stream shown after filtering');
