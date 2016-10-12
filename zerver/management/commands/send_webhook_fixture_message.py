f392f1b1f657f5293b1c097bbb097f0f5eaba3a1
Laura Hampton <lhampton@gmail.com>
2016-10-12 18:10:08 -0400

    converted a few management commands from optparse to argparse (WIP)

--- zerver/management/commands/send_webhook_fixture_message.py
+++ zerver/management/commands/send_webhook_fixture_message.py
@@ -5,7 +5,7 @@ from typing import Any
 
 import os
 import ujson
-from optparse import make_option
+from argparse import ArgumentParser
 
 from django.test import Client
 from django.conf import settings
@@ -22,16 +22,15 @@ Example:
 
 """
 
-    option_list = BaseCommand.option_list + (
-        make_option('-f', '--fixture',
+    def parse_arguments(self, parser):
+        parser.add_argument('-f', '--fixture',
                     dest='fixture',
                     type='str',
-                    help='The path to the fixture you\'d like to send into Zulip'),
-        make_option('-u', '--url',
+                    help='The path to the fixture you\'d like to send into Zulip')
+        parser.add_argument('-u', '--url',
                     dest='url',
                     type='str',
-                    help='The url on your Zulip server that you want to post the fixture to'),
-        )
+                    help='The url on your Zulip server that you want to post the fixture to')
 
     def handle(self, **options):
         # type: (*Any, **str) -> None
