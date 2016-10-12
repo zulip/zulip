735f29925e65edcd6533b4bc02bbce60c4ce8060
Laura Hampton <lhampton@gmail.com>
2016-10-12 18:10:08 -0400

    converted a few management commands from optparse to argparse (WIP)

--- zerver/management/commands/logout_all_users.py
+++ zerver/management/commands/logout_all_users.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 from typing import Any
-from optparse import make_option
+from argparse import ArgumentParser
 
 from django.core.management.base import BaseCommand
 
@@ -12,17 +12,16 @@ from zerver.models import get_realm
 class Command(BaseCommand):
     help = "Log out all users."
 
-    option_list = BaseCommand.option_list + (
-        make_option('--realm',
+    def add_arguments(self, parser):
+        parser.add_argument('--realm',
                     dest='realm',
                     action='store',
                     default=None,
-                    help="Only logout all users in a particular realm"),
-        make_option('--deactivated-only',
+                    help="Only logout all users in a particular realm")
+        parser.add_argument('--deactivated-only',
                     action='store_true',
                     default=False,
-                    help="Only logout all users who are deactivated"),
-        )
+                    help="Only logout all users who are deactivated")
 
     def handle(self, *args, **options):
         # type: (*Any, **Any) -> None
