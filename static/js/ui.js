efc80836cc6c91059a8acc32a52c6d3ea20b9172
Brock Whittaker <brock@zulipchat.com>
2016-10-17 13:02:25 -0700

    Change Lightbox to Support YouTube Videos.
    The lightbox will now distinguish between whether or not something is a
    photo and a YouTube video by the class name of the message inline
    preview. It embeds the YouTube video in the lightbox as an iFrame
    rather than previewing the video screenshot.

--- static/js/ui.js
+++ static/js/ui.js
@@ -281,25 +281,58 @@ exports.small_avatar_url = function (message) {
     }
 };
 
+exports.lightbox = function (data) {
+    switch (data.type) {
+        case "photo":
+            exports.lightbox_photo(data.image, data.user);
+            break;
+        case "youtube":
+            exports.youtube_video(data.id);
+            break;
+        default:
+            break;
+    }
+
+    $("#overlay").addClass("show");
+};
+
 exports.lightbox_photo = function (image, user) {
     // image should be an Image Object in JavaScript.
     var url = $(image).attr("src"),
         title = $(image).parent().attr("title");
 
+    $("#overlay .player-container").hide();
+    $("#overlay .image-actions, .image-description, .download").show();
+
     $("#overlay .image-preview")
+        .show()
         .css("background-image", "url(" + url + ")");
 
-    $("#overlay").addClass("show");
-
-    $(".title").text(title || "N/A");
-    $(".user").text(user);
+    $(".image-description .title").text(title || "N/A");
+    $(".image-description .user").text(user);
 
     $(".image-actions .open, .image-actions .download").attr("href", url);
 };
 
 exports.exit_lightbox_photo = function (image) {
     $("#overlay").removeClass("show");
+    $(".player-container iframe").remove();
 };
+
+exports.youtube_video = function (id) {
+    $("#overlay .image-preview, .image-description, .download").hide();
+
+    var iframe = document.createElement("iframe");
+    iframe.width = window.innerWidth;
+    iframe.height = window.innerWidth * 0.5625;
+    iframe.src = "https://www.youtube.com/embed/" + id;
+    iframe.setAttribute("frameborder", 0);
+    iframe.setAttribute("allowfullscreen", true);
+
+    $("#overlay .player-container").html("").show().append(iframe);
+    $(".image-actions .open").attr("href", "https://youtu.be/" + id);
+};
+// k3O01EfM5fU
 
 var loading_more_messages_indicator_showing = false;
 exports.show_loading_more_messages_indicator = function () {
