5210b0a6a4452eb8bff0b1016fdb0037216903cb
Tim Abbott <tabbott@zulipchat.com>
2016-10-16 19:20:39 -0700

    zulip_ops: Cleanup old redis configuration.
    One can now just use the improved configuration we've merged into the
    main Zulip repo.

--- puppet/zulip_ops/manifests/redis.pp
+++ puppet/zulip_ops/manifests/redis.pp
@@ -1,7 +1,3 @@
 class zulip_ops::redis inherits zulip::redis {
   include zulip_ops::base
-
-  File[ "/etc/redis/redis.conf"] {
-    source => "puppet:///modules/zulip_ops/redis/redis.conf",
-  }
 }
