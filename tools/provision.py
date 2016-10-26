zulip/zulip

commit 8e82257444666cf9daa31bc3a3b5cb5f81f85f9b

Fix node_cache code to not require root.
The previous code caused problems using a system where the zulip user
doesn't have sudo rights.

tools/provision.py
@@ -16,7 +16,7 @@ ZULIP_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(ZULIP_PATH)
 from scripts.lib.zulip_tools import run, subprocess_text_output, OKBLUE, ENDC, WARNING
 from scripts.lib.setup_venv import setup_virtualenv, VENV_DEPENDENCIES
-from scripts.lib.node_cache import setup_node_modules
+from scripts.lib.node_cache import setup_node_modules, NPM_CACHE_PATH
 
 from version import PROVISION_VERSION
 if False:
@@ -208,6 +208,8 @@ def main(options):
         # issue with the symlinks being improperly owned by root.
         if os.path.islink("node_modules"):
             run(["sudo", "rm", "-f", "node_modules"])
+        if os.path.isdir(NPM_CACHE_PATH):
+            run(["sudo", "chown", "zulip:zulip", NPM_CACHE_PATH])
         setup_node_modules()
     except subprocess.CalledProcessError:
         print(WARNING + "`npm install` failed; retrying..." + ENDC)

puppet/zulip/manifests/app_frontend_base.pp
@@ -88,6 +88,12 @@ class zulip::app_frontend_base {
     owner  => 'zulip',
     group  => 'zulip',
   }
+  file { "/srv/zulip-npm-cache":
+    ensure => directory,
+    owner => "zulip",
+    group => "zulip",
+    mode => 755,
+  }
   file { "/etc/cron.d/email-mirror":
     ensure => absent,
   }

scripts/lib/node_cache.py
@@ -49,9 +49,8 @@ def do_npm_install(target_path, npm_args, stdout=None, stderr=None, copy_modules
                    success_stamp=None):
     # type: (str, List[str], Optional[IO], Optional[IO], Optional[bool], Optional[str]) -> None
     cmds = [
-        ["sudo", "rm", "-rf", target_path],
-        ['sudo', 'mkdir', '-p', target_path],
-        ["sudo", "chown", "{}:{}".format(os.getuid(), os.getgid()), target_path],
+        ["rm", "-rf", target_path],
+        ['mkdir', '-p', target_path],
         ['cp', 'package.json', target_path],
     ]
     if copy_modules:

commit 129f1472fb1cc0371e8862d806e5b83cb66c051c

provision: Fix hardcoding of current user.
This fixes a regression in 8e82257444666cf9daa31bc3a3b5cb5f81f85f9b
and 2b8324b778a653fc420781e0afd6fe49f20ad909.

tools/provision.py
@@ -123,6 +123,7 @@ REPO_STOPWORDS_PATH = os.path.join(
 
 LOUD = dict(_out=sys.stdout, _err=sys.stderr)
 
+user_id = os.getuid()
 
 def main(options):
     # type: (Any) -> int
@@ -176,7 +177,7 @@ def main(options):
 
     run(["tools/setup/download-zxcvbn"])
     if os.path.isdir(EMOJI_CACHE_PATH):
-        run(["sudo", "chown", "zulip:zulip", EMOJI_CACHE_PATH])
+        run(["sudo", "chown", "%s:%s" % (user_id, user_id), EMOJI_CACHE_PATH])
     run(["python", "tools/setup/emoji_dump/build_emoji"])
     run(["scripts/setup/generate_secrets.py", "--development"])
     if options.is_travis and not options.is_production_travis:
@@ -217,7 +218,7 @@ def main(options):
         if os.path.islink("node_modules"):
             run(["sudo", "rm", "-f", "node_modules"])
         if os.path.isdir(NPM_CACHE_PATH):
-            run(["sudo", "chown", "zulip:zulip", NPM_CACHE_PATH])
+            run(["sudo", "chown", "%s:%s" % (user_id, user_id), NPM_CACHE_PATH])
         setup_node_modules()
     except subprocess.CalledProcessError:
         print(WARNING + "`npm install` failed; retrying..." + ENDC)