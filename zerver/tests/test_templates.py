2738fad1022e03ab9b1b2f610f7c85db4908b952
Tim Abbott <tabbott@zulipchat.com>
2016-11-09 12:52:42 -0800

    render_markdown_path: Add anchors for table-of-contents.

--- zerver/tests/test_templates.py
+++ zerver/tests/test_templates.py
@@ -123,7 +123,7 @@ class TemplateTestCase(ZulipTestCase):
 
         content_sans_whitespace = content.replace(" ", "").replace('\n', '')
         self.assertEqual(content_sans_whitespace,
-                         'header<h1>Hello!</h1><p>Thisissome<em>boldtext</em>.</p>footer')
+                         'header<h1id="hello">Hello!</h1><p>Thisissome<em>boldtext</em>.</p>footer')
 
     def test_custom_tos_template(self):
         # type: () -> None
