9fec519b3d3e9adae76ae63927bc775b2e20305f
Tim Abbott <tabbott@zulipchat.com>
2016-11-16 17:33:42 -0800

    Zephyr: Remove obsolete mit.html template.

--- corporate/urls.py
+++ corporate/urls.py
@@ -4,7 +4,6 @@ from django.views.generic import TemplateView, RedirectView
 i18n_urlpatterns = [
     # Zephyr/MIT
     url(r'^zephyr/$', TemplateView.as_view(template_name='corporate/zephyr.html')),
-    url(r'^mit/$', TemplateView.as_view(template_name='corporate/mit.html')),
     url(r'^zephyr-mirror/$', TemplateView.as_view(template_name='corporate/zephyr-mirror.html')),
 
     # Terms of service and privacy policy
