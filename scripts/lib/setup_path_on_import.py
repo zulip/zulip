ed48feb09d76b1f0a0f3228e150bd234aa471e36
Anders Kaseorg <andersk@mit.edu>
2016-11-19 23:45:53 -0500

    Use zulip-py3-venv when running on Python 3
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- scripts/lib/setup_path_on_import.py
+++ scripts/lib/setup_path_on_import.py
@@ -8,7 +8,11 @@ from os.path import dirname, abspath
 import sys
 
 BASE_DIR = dirname(dirname(dirname(abspath(__file__))))
-activate_this = os.path.join(BASE_DIR, "zulip-venv", "bin", "activate_this.py")
+activate_this = os.path.join(
+    BASE_DIR,
+    "zulip-venv" if sys.version_info[0] == 2 else "zulip-py3-venv",
+    "bin",
+    "activate_this.py")
 if os.path.exists(activate_this):
     # this file will exist in production
     exec(open(activate_this).read(), {}, dict(__file__=activate_this))
