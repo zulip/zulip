5a091a34661ccb42a9aaefd955b8e0aa2e51209e
aakash-cr7 <aakash.tyg93@gmail.com>
2016-11-20 07:32:23 +0000

    Add node test for the user avatar template.

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -2,18 +2,17 @@ var navbar = (function () {
 var exports = {};
 
 exports.display_user_avatar = function () {
-    var avatar_url = page_params.avatar_url
+    var avatar_url = page_params.avatar_url;
     var template = templates.render("user_avatar", {avatar_url: avatar_url});
-    var $avatar = $("#user-avatar")
+    var $avatar = $("#user-avatar");
 
     $avatar.append(template);
 };
 
-$(function(){
+$(function () {
     exports.display_user_avatar();
 });
 
 return exports;
 
 }());
-
