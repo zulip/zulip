3d538a120c43266b15088dfde1ab058ee84fdcd3
Anders Kaseorg <andersk@mit.edu>
2016-11-21 19:23:47 -0500

    Replace python -u with PYTHONUNBUFFERED=1
    (Why is -u needed at all?  I’m not sure, but test-run-dev spins forever
    “Polling run-dev...” without it.)
    
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- tools/run-dev.py
+++ tools/run-dev.py
@@ -125,7 +125,7 @@ os.setpgrp()
 cmds = [['./tools/compile-handlebars-templates', 'forever'],
         ['python', 'manage.py', 'rundjango'] +
         manage_args + ['127.0.0.1:%d' % (django_port,)],
-        ['python', '-u', 'manage.py', 'runtornado'] +
+        ['env', 'PYTHONUNBUFFERED=1', 'python', 'manage.py', 'runtornado'] +
         manage_args + ['127.0.0.1:%d' % (tornado_port,)],
         ['./tools/run-dev-queue-processors'] + manage_args,
         ['env', 'PGHOST=127.0.0.1',  # Force password authentication using .pgpass
