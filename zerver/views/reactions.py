91f90ae5d1a910c02c1b94a0325a59443562c59e
Arpith Siromoney <arpith@feedreader.co>
2016-11-30 12:44:46 +0530

    reactions: Add support for removing emoji reactions.
    This commit adds support for removing reactions via DELETE requests to
    the /reactions endpoint with parameters emoji_name and message_id.
    
    The reaction is deleted from the database and a reaction event is sent
    out with 'op' set to 'remove'.
    
    Tests are added to check:
    1. Removing a reaction that does not exist fails
    2. When removing a reaction, the event payload and users are correct

--- zerver/views/reactions.py
+++ zerver/views/reactions.py
@@ -4,7 +4,7 @@ from six import text_type
 
 from zerver.decorator import authenticated_json_post_view,\
     has_request_variables, REQ, to_non_negative_int
-from zerver.lib.actions import do_add_reaction
+from zerver.lib.actions import do_add_reaction, do_remove_reaction
 from zerver.lib.bugdown import emoji_list
 from zerver.lib.message import access_message
 from zerver.lib.request import JsonableError
@@ -32,5 +32,29 @@ def add_reaction_backend(request, user_profile, emoji_name=REQ('emoji'),
         raise JsonableError(_("Reaction already exists"))
 
     do_add_reaction(user_profile, message, emoji_name)
+
+    return json_success()
+
+@has_request_variables
+def remove_reaction_backend(request, user_profile, emoji_name=REQ('emoji'),
+                            message_id = REQ('message_id', converter=to_non_negative_int)):
+    # type: (HttpRequest, UserProfile, text_type, int) -> HttpResponse
+
+    # access_message will throw a JsonableError exception if the user
+    # cannot see the message (e.g. for messages to private streams).
+    message = access_message(user_profile, message_id)[0]
+
+    existing_emojis = set(message.sender.realm.get_emoji().keys()) or set(emoji_list)
+    if emoji_name not in existing_emojis:
+        raise JsonableError(_("Emoji '%s' does not exist" % (emoji_name,)))
+
+    # We could probably just make this check be a try/except for the
+    # IntegrityError from it already existing, but this is a bit cleaner.
+    if not Reaction.objects.filter(user_profile=user_profile,
+                                   message=message,
+                                   emoji_name=emoji_name).exists():
+        raise JsonableError(_("Reaction does not exist"))
+
+    do_remove_reaction(user_profile, message, emoji_name)
 
     return json_success()
