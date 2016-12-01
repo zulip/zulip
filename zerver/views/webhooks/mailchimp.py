d316427077b9e7b51841681ec7af890ccdcd4471
NoahCristino <noahcristino@gmail.com>
2016-12-01 21:41:13 +0000

    Debug #2

--- zerver/views/webhooks/mailchimp.py
+++ zerver/views/webhooks/mailchimp.py
@@ -17,18 +17,32 @@ MAILCHIMP_MESSAGE_TEMPLATE = '{data[merges][FNAME]} {data[merges][LNAME]} ({data
 @has_request_variables
 def api_mailchimp_webhook(request, user_profile, client, payload=REQ(argument_type='body'),
                             stream=REQ(default='mailchimp')):
-  print("\n\n\nGOT TO HERE 1!!!!!\n\n\")
+    print("mark 1")
     # type: (HttpRequest, UserProfile, Client, Dict[str, Any], text_type) -> HttpResponse
     try:
-        print("\n\n\nGOT TO HERE 2!!!!!\n\n\")
-        event = payload['event']
-        return json_success()
-        issue_body = payload['payload']
-        subject = MAILCHIMP_SUBJECT_TEMPLATE.format()
-        body = MAILCHIMP_MESSAGE_TEMPLATE.format()
+        print("mark 2")
+        #print("load: "+payload['event'])
+        #event = payload['event']
+        #print(event)
+        print("a")
+        print("b")
+        print(payload['data[merges][EMAIL]'])
+        print("stop")
+        #'[{fired_at}]{type}|{data[merges][EMAIL]}'
+        subject = "["+payload['fired_at']+"]|"+payload['type']+"d|"+payload['data[merges][EMAIL]']
+        print("===")
+        print(subject)
+        print("===")
+        #'{data[merges][FNAME]} {data[merges][LNAME]} ({data[merges][EMAIL]}) subscribed at {fired_at}'
+        body = payload['data[merges][FNAME]'] + " " + payload['data[merges][LNAME]'] + " (" + payload['data[merges][EMAIL]'] + ") "+payload['type']+"d!"
+        print("===")
+        print(body)
+        print("===")
+        print("mark 3")
     except KeyError as e:
         return json_error(_("Missing key {} in JSON".format(str(e))))
-
     check_send_message(user_profile, client, 'stream', [stream],
                        subject, body)
+    print("!")
+    print(json_success())
     return json_success()
