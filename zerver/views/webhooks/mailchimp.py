2747aaeca541c608e713968afa1b0d71ba50b5c4
NoahCristino <noahcristino@gmail.com>
2016-12-02 01:15:32 +0000

    integrations: Add webhook code, API endpoint, and tests for mailchimp.

--- zerver/views/webhooks/mailchimp.py
+++ zerver/views/webhooks/mailchimp.py
@@ -16,7 +16,8 @@ MAILCHIMP_MESSAGE_TEMPLATE = '{data[merges][FNAME]} {data[merges][LNAME]} ({data
 @api_key_only_webhook_view('MailChimp')
 @has_request_variables
 def api_mailchimp_webhook(request, user_profile, client, payload=REQ(argument_type='body'),
-                            stream=REQ(default='test')):
+                            stream=REQ(default='mailchimp')):
+    payload = "{\n"+payload+"\n{"
     print("mark 1")
     # type: (HttpRequest, UserProfile, Client, Dict[str, Any], text_type) -> HttpResponse
     try:
@@ -39,6 +40,7 @@ def api_mailchimp_webhook(request, user_profile, client, payload=REQ(argument_ty
         print(body)
         print("===")
         print("mark 3")
+        check_send_message(user_profile, client, 'stream', [stream], subject, body)
         return json_success()
     except KeyError as e:
         return json_error(_("Missing key {} in JSON".format(str(e))))
