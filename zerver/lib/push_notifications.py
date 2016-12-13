c24fbd28b2528aa42e34e48632990ac2f31b6e4a
Umair Khan <umair.waheed@gmail.com>
2016-12-13 12:41:48 +0500

    Shift to python-gcm for Android push notifications.
    Fixes #2650

--- zerver/lib/push_notifications.py
+++ zerver/lib/push_notifications.py
@@ -12,7 +12,7 @@ from zerver.lib.utils import generate_random_token
 from zerver.lib.redis_utils import get_redis_client
 
 from apns import APNs, Frame, Payload, SENT_BUFFER_QTY
-import gcmclient
+from gcm import GCM
 
 from django.conf import settings
 
@@ -189,7 +189,7 @@ def check_apns_feedback():
 
 
 if settings.ANDROID_GCM_API_KEY:
-    gcm = gcmclient.GCM(settings.ANDROID_GCM_API_KEY)
+    gcm = GCM(settings.ANDROID_GCM_API_KEY)
 else:
     gcm = None
 
@@ -203,44 +203,47 @@ def send_android_push_notification(user, data):
     reg_ids = [device.token for device in
                PushDeviceToken.objects.filter(user=user, kind=PushDeviceToken.GCM)]
 
-    msg = gcmclient.JSONMessage(reg_ids, data)
-    res = gcm.send(msg)
+    res = gcm.json_request(registration_ids=reg_ids, data=data)
 
-    for reg_id, msg_id in res.success.items():
-        logging.info("GCM: Sent %s as %s" % (reg_id, msg_id))
+    if res and 'success' in res:
+        for reg_id, msg_id in res['success'].items():
+            logging.info("GCM: Sent %s as %s" % (reg_id, msg_id))
 
     # res.canonical will contain results when there are duplicate registrations for the same
     # device. The "canonical" registration is the latest registration made by the device.
     # Ref: http://developer.android.com/google/gcm/adv.html#canonical
-    for reg_id, new_reg_id in res.canonical.items():
-        if reg_id == new_reg_id:
-            # I'm not sure if this should happen. In any case, not really actionable.
-            logging.warning("GCM: Got canonical ref but it already matches our ID %s!" % (reg_id,))
-        elif not PushDeviceToken.objects.filter(token=new_reg_id, kind=PushDeviceToken.GCM).count():
-            # This case shouldn't happen; any time we get a canonical ref it should have been
-            # previously registered in our system.
-            #
-            # That said, recovery is easy: just update the current PDT object to use the new ID.
-            logging.warning(
-                    "GCM: Got canonical ref %s replacing %s but new ID not registered! Updating." %
-                    (new_reg_id, reg_id))
-            PushDeviceToken.objects.filter(
-                    token=reg_id, kind=PushDeviceToken.GCM).update(token=new_reg_id)
-        else:
-            # Since we know the new ID is registered in our system we can just drop the old one.
-            logging.info("GCM: Got canonical ref %s, dropping %s" % (new_reg_id, reg_id))
+    if 'canonical' in res:
+        for reg_id, new_reg_id in res['canonical'].items():
+            if reg_id == new_reg_id:
+                # I'm not sure if this should happen. In any case, not really actionable.
+                logging.warning("GCM: Got canonical ref but it already matches our ID %s!" % (reg_id,))
+            elif not PushDeviceToken.objects.filter(token=new_reg_id, kind=PushDeviceToken.GCM).count():
+                # This case shouldn't happen; any time we get a canonical ref it should have been
+                # previously registered in our system.
+                #
+                # That said, recovery is easy: just update the current PDT object to use the new ID.
+                logging.warning(
+                        "GCM: Got canonical ref %s replacing %s but new ID not registered! Updating." %
+                        (new_reg_id, reg_id))
+                PushDeviceToken.objects.filter(
+                        token=reg_id, kind=PushDeviceToken.GCM).update(token=new_reg_id)
+            else:
+                # Since we know the new ID is registered in our system we can just drop the old one.
+                logging.info("GCM: Got canonical ref %s, dropping %s" % (new_reg_id, reg_id))
 
-            PushDeviceToken.objects.filter(token=reg_id, kind=PushDeviceToken.GCM).delete()
+                PushDeviceToken.objects.filter(token=reg_id, kind=PushDeviceToken.GCM).delete()
 
-    for reg_id in res.not_registered:
-        logging.info("GCM: Removing %s" % (reg_id,))
+    if 'errors' in res:
+        for error, reg_ids in res['errors'].items():
+            if error in ['NotRegistered', 'InvalidRegistration']:
+                for reg_id in reg_ids:
+                    logging.info("GCM: Removing %s" % (reg_id,))
 
-        device = PushDeviceToken.objects.get(token=reg_id, kind=PushDeviceToken.GCM)
-        device.delete()
+                    device = PushDeviceToken.objects.get(token=reg_id, kind=PushDeviceToken.GCM)
+                    device.delete()
+            else:
+                for reg_id in reg_ids:
+                    logging.warning("GCM: Delivery to %s failed: %s" % (reg_id, error))
 
-    for reg_id, err_code in res.failed.items():
-        logging.warning("GCM: Delivery to %s failed: %s" % (reg_id, err_code))
-
-    if res.needs_retry():
-        # TODO
-        logging.warning("GCM: delivery needs a retry but ignoring")
+    # python-gcm handles retrying of the unsent messages.
+    # Ref: https://github.com/geeknam/python-gcm/blob/master/gcm/gcm.py#L497
--- zerver/tests/test_push_notifications.py
+++ zerver/tests/test_push_notifications.py
@@ -3,7 +3,7 @@ from mock import call
 import time
 from typing import Any, Union, SupportsInt, Text
 
-import gcmclient
+import gcm
 
 from django.test import TestCase
 from django.conf import settings
@@ -228,7 +228,7 @@ class GCMTest(PushNotificationTest):
     def setUp(self):
         # type: () -> None
         super(GCMTest, self).setUp()
-        apn.gcm = gcmclient.GCM('fake key')
+        apn.gcm = gcm.GCM('fake key')
         self.gcm_tokens = [u'1111', u'2222']
         for token in self.gcm_tokens:
             PushDeviceToken.objects.create(
@@ -259,12 +259,11 @@ class GCMNotSetTest(GCMTest):
 class GCMSuccessTest(GCMTest):
     @mock.patch('logging.warning')
     @mock.patch('logging.info')
-    @mock.patch('gcmclient.GCM.send')
+    @mock.patch('gcm.GCM.json_request')
     def test_success(self, mock_send, mock_info, mock_warning):
         # type: (mock.MagicMock, mock.MagicMock, mock.MagicMock) -> None
-        res = mock.MagicMock()
-        res.success = {token: ind for ind, token in enumerate(self.gcm_tokens)}
-        res.needs_retry.return_value = False
+        res = {}
+        res['success'] = {token: ind for ind, token in enumerate(self.gcm_tokens)}
         mock_send.return_value = res
 
         data = self.get_gcm_data()
@@ -277,12 +276,11 @@ class GCMSuccessTest(GCMTest):
 
 class GCMCanonicalTest(GCMTest):
     @mock.patch('logging.warning')
-    @mock.patch('gcmclient.GCM.send')
+    @mock.patch('gcm.GCM.json_request')
     def test_equal(self, mock_send, mock_warning):
         # type: (mock.MagicMock, mock.MagicMock) -> None
-        res = mock.MagicMock()
-        res.canonical = {1: 1}
-        res.needs_retry.return_value = False
+        res = {}
+        res['canonical'] = {1: 1}
         mock_send.return_value = res
 
         data = self.get_gcm_data()
@@ -291,14 +289,13 @@ class GCMCanonicalTest(GCMTest):
                                              "already matches our ID 1!")
 
     @mock.patch('logging.warning')
-    @mock.patch('gcmclient.GCM.send')
+    @mock.patch('gcm.GCM.json_request')
     def test_pushdevice_not_present(self, mock_send, mock_warning):
         # type: (mock.MagicMock, mock.MagicMock) -> None
-        res = mock.MagicMock()
+        res = {}
         t1 = apn.hex_to_b64(u'1111')
         t2 = apn.hex_to_b64(u'3333')
-        res.canonical = {t1: t2}
-        res.needs_retry.return_value = False
+        res['canonical'] = {t1: t2}
         mock_send.return_value = res
 
         def get_count(hex_token):
@@ -321,14 +318,13 @@ class GCMCanonicalTest(GCMTest):
         self.assertEqual(get_count(u'3333'), 1)
 
     @mock.patch('logging.info')
-    @mock.patch('gcmclient.GCM.send')
+    @mock.patch('gcm.GCM.json_request')
     def test_pushdevice_different(self, mock_send, mock_info):
         # type: (mock.MagicMock, mock.MagicMock) -> None
-        res = mock.MagicMock()
+        res = {}
         old_token = apn.hex_to_b64(u'1111')
         new_token = apn.hex_to_b64(u'2222')
-        res.canonical = {old_token: new_token}
-        res.needs_retry.return_value = False
+        res['canonical'] = {old_token: new_token}
         mock_send.return_value = res
 
         def get_count(hex_token):
@@ -350,13 +346,12 @@ class GCMCanonicalTest(GCMTest):
 
 class GCMNotRegisteredTest(GCMTest):
     @mock.patch('logging.info')
-    @mock.patch('gcmclient.GCM.send')
+    @mock.patch('gcm.GCM.json_request')
     def test_not_registered(self, mock_send, mock_info):
         # type: (mock.MagicMock, mock.MagicMock) -> None
-        res = mock.MagicMock()
+        res = {}
         token = apn.hex_to_b64(u'1111')
-        res.not_registered = [token]
-        res.needs_retry.return_value = False
+        res['errors'] = {'NotRegistered': [token]}
         mock_send.return_value = res
 
         def get_count(hex_token):
@@ -374,20 +369,18 @@ class GCMNotRegisteredTest(GCMTest):
 
 class GCMFailureTest(GCMTest):
     @mock.patch('logging.warning')
-    @mock.patch('gcmclient.GCM.send')
+    @mock.patch('gcm.GCM.json_request')
     def test_failure(self, mock_send, mock_warn):
         # type: (mock.MagicMock, mock.MagicMock) -> None
-        res = mock.MagicMock()
+        res = {}
         token = apn.hex_to_b64(u'1111')
-        res.failed = {token: 1}
-        res.needs_retry.return_value = True
+        res['errors'] = {'Failed': [token]}
         mock_send.return_value = res
 
         data = self.get_gcm_data()
         apn.send_android_push_notification(self.user_profile, data)
-        c1 = call("GCM: Delivery to %s failed: 1" % (token,))
-        c2 = call("GCM: delivery needs a retry but ignoring")
-        mock_warn.assert_has_calls([c1, c2], any_order=True)
+        c1 = call("GCM: Delivery to %s failed: Failed" % (token,))
+        mock_warn.assert_has_calls([c1], any_order=True)
 
 class TestReceivesNotificationsFunctions(ZulipTestCase):
     def setUp(self):
