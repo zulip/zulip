fa68a678cc3d0adfd03732c4354724dd83be32c9
K.Kanakhin <kkanahin@gmail.com>
2016-12-16 14:07:29 +0600

    help-documentation-tool: Add tools for checking user help documentation.
    - Add spider for documentation crawler, which processes user help
      documentation on test development server.
    - Add tool script for help documentation checking launching.
    
    Fixes #2639

---
+++ tools/documentation_crawler/documentation_crawler/spiders/check_help_documentation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+from .common.spiders import BaseDocumentationSpider
+
+
+class HelpDocumentationSpider(BaseDocumentationSpider):
+    name = "help_documentation_crawler"
+    start_urls = ['http://localhost:9981/help']
+    deny_domains = [] # type: List[str]
+
+    def _is_external_url(self, url):
+        # type: (str) -> bool
+        is_external = url.startswith('http') and 'localhost:9981/help' not in url
+        return is_external or self._has_extension(url)
