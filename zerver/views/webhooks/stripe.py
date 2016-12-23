f88fbedafd0123578d8ba215946664f66e908281
Tomasz Kolek <tomasz-kolek@o2.pl>
2016-12-23 17:21:51 +0100

    Refactor transfer part of stripe integration.

--- zerver/views/webhooks/stripe.py
+++ zerver/views/webhooks/stripe.py
@@ -4,11 +4,10 @@ from django.utils.translation import ugettext as _
 from zerver.lib.actions import check_send_message
 from zerver.lib.response import json_success, json_error
 from zerver.decorator import REQ, has_request_variables, api_key_only_webhook_view
-from zerver.lib.validator import check_dict, check_string
 from zerver.models import Client, UserProfile
 
 from django.http import HttpRequest, HttpResponse
-from typing import Dict, Any, Iterable, Optional, Text
+from typing import Dict, Any, Optional, Text
 
 from datetime import datetime
 
@@ -18,12 +17,10 @@ def api_stripe_webhook(request, user_profile, client,
                        payload=REQ(argument_type='body'), stream=REQ(default='test'),
                        topic=REQ(default='stripe')):
     # type: (HttpRequest, UserProfile, Client, Dict[str, Any], Text, Optional[Text]) -> HttpResponse
-    body = ""
-    event_type = ""
-    body_template = ""
-    data_object = payload["data"]["object"]
+    body = None
+    event_type = payload["type"]
     try:
-        event_type = payload["type"]
+        data_object = payload["data"]["object"]
         if event_type.startswith('charge'):
 
             charge_url = "https://dashboard.stripe.com/payments/{}"
@@ -123,24 +120,29 @@ def api_stripe_webhook(request, user_profile, client,
 
             body = body_template.format(beginning=beginning, id=object_id, link=link, amount=amount_string, end=end)
 
-        elif event_type == "transfer.failed":
-            link = "https://dashboard.stripe.com/transfers/"+payload["data"]["object"]["id"]
-            amount_string = amount(payload["data"]["object"]["amount"], payload["data"]["object"]["currency"])
-            body_template = "The transfer with description **{object[description]}** and id **[{object[id]}]("\
-                            + link + ")** for amount **"\
-                            + amount_string + "** has failed."
-            body = body_template.format(**(payload["data"]))
-        elif event_type == "transfer.paid":
-            link = "https://dashboard.stripe.com/transfers/"+payload["data"]["object"]["id"]
-            amount_string = amount(payload["data"]["object"]["amount"], payload["data"]["object"]["currency"])
-            body_template = "The transfer with description **{object[description]}** and id **[{object[id]}]("\
-                            + link + ")** for amount **"\
-                            + amount_string + "** has been paid."
-            body = body_template.format(**(payload["data"]))
+        elif event_type.startswith('transfer'):
+            object_id = data_object['id']
+            link = "https://dashboard.stripe.com/transfers/{}".format(object_id)
+            amount_string = amount(data_object["amount"], data_object["currency"])
+            body_template = "The transfer with description **{description}** and id **[{id}]({link})** " \
+                            "for amount **{amount}** has {end}."
+            if event_type == "transfer.failed":
+                end = 'failed'
+            else:
+                end = "been paid"
+            body = body_template.format(
+                description=data_object['description'],
+                id=object_id,
+                link=link,
+                amount=amount_string,
+                end=end
+            )
     except KeyError as e:
-        body = "Missing key {} in JSON".format(str(e))
+        return json_error(_("Missing key {} in JSON".format(str(e))))
 
-    # send the message
+    if body is None:
+        return json_error(_("We don't support {} event".format(event_type)))
+
     check_send_message(user_profile, client, 'stream', [stream], topic, body)
 
     return json_success()
