1c044688a41e412f3fd6c22ac324dcbf503c8038
Bojidar Marinov <bojidar.marinov.bg@gmail.com>
2016-12-31 13:33:03 +0200

    compose: Make ctrl-enter, meta-enter, etc. add newline with enter_sends.
    Tweaked by tabbott to ensure we update the compose box size as well.
    
    Fixes #2989.

--- static/js/composebox_typeahead.js
+++ static/js/composebox_typeahead.js
@@ -135,14 +135,21 @@ function handle_keydown(e) {
             }
 
             // Send the message on Ctrl/Cmd-Enter or if the user has configured enter to
-            // send and the shift key is not pressed.
-            if (e.target.id === "new_message_content" && code === 13 &&
-                (e.metaKey || e.ctrlKey || (page_params.enter_sends && !e.shiftKey))
-               ) {
+            // send and the Shift/Ctrl/Cmd/Alt keys are not pressed.
+            // Otherwise, make sure to insert a newline instead
+            if (e.target.id === "new_message_content" && code === 13) {
                 e.preventDefault();
-                if ($("#compose-send-button").attr('disabled') !== "disabled") {
-                    $("#compose-send-button").attr('disabled', 'disabled');
-                    compose.finish();
+
+                if ((!page_params.enter_sends && (e.metaKey || e.ctrlKey)) ||
+                    (page_params.enter_sends && !(e.shiftKey || e.ctrlKey || e.metaKey || e.altKey))
+                ) {
+                    if ($("#compose-send-button").attr('disabled') !== "disabled") {
+                        $("#compose-send-button").attr('disabled', 'disabled');
+                        compose.finish();
+                    }
+                } else {
+                    $("#new_message_content").caret('\n');
+                    compose.autosize_textarea();
                 }
             }
         }
