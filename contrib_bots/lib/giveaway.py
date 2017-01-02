6b70e504c7c93e31b74742e9a7cdedda2af4f938
djob9601 <dob9601@gmail.com>
2017-01-02 11:27:56 +0000

    interactive bots: giveaway.py: made the requested changes
    - Removed all == operators when comparing a bool
    + Added spaces around all + operators
    + Used .format() instead of %s
    + Changed variable names to use underscores instead of  camelCase
    - Compressed down code for remaining time messages into a single method
    with less statements
    - Removed deprecated comment
    - Removed unnecessary variable

--- contrib_bots/lib/giveaway.py
+++ contrib_bots/lib/giveaway.py
@@ -26,77 +26,84 @@ class GiveawayHandler(object):
     as part of Google Code-In 2016-2017
     '''
     def __init__(self):
-        self.giveawayArray = []
-        self.giveawayOn = False
-        self.giveawayName = ""
-        self.giveawayTime = 0
-        self.giveawayCreator = ""
-        self.giveawayStartTime = 0
-        self.giveawayGroup = ""
-        self.giveawaySubject = ""
-        self.giveawayClient = ""
-        self.giveawayTimeMins = 0
-        self.giveawayStage = 0
+        self.giveaway_array = []
+        self.giveaway_on = False
+        self.giveaway_name = ""
+        self.giveaway_time = 0
+        self.giveaway_creator = ""
+        self.giveaway_start_time = 0
+        self.giveaway_group = ""
+        self.giveaway_subject = ""
+        self.giveaway_client = ""
+        self.giveaway_timeMins = 0
+        self.giveaway_stage = 0
         self.one_minute_warning = False
-        self.giveawayWinner = ""
-        self.winnerContactDetails = []
-        self.winnerIndex = 0
+        self.giveaway_winner = ""
+        self.giveaway_winner_contact_address = []
+        self.winner_index = 0
+        self.all_warnings_fired = False
 
     def time_update(self, new_content):
-            if self.giveawayOn == True:
-                self.giveawayClient.send_message(dict(
-                    type='stream',
-                    to=self.giveawayStream,
-                    subject=self.giveawaySubject,
-                    content=new_content,
-                ))
-                threading.Timer(6.0, self.check_time).start()
+        if self.giveaway_on:
+            self.giveaway_client.send_message(dict(
+                type='stream',
+                to=self.giveawayStream,
+                subject=self.giveaway_subject,
+                content=new_content,
+            ))
+            threading.Timer(6.0, self.check_time).start()
+
+    def time_warning(self):
+        if self.giveaway_stage == 0:
+            self.numerator = 1
+            self.denominator = 4
+        elif self.giveaway_stage == 1:
+            self.numerator = 1
+            self.denominator = 2
+        elif self.giveaway_stage == 2:
+            self.numerator = 3
+            self.denominator = 4
+        elif self.giveaway_stage == 0:
+            self.all_warnings_fired = True
+        if time.time() > ((self.giveaway_start_time + self.giveaway_time)-60):
+            if self.giveaway_timeMins > 1.9 and not self.one_minute_warning:
+                new_content = 'Giveaway ends in 1 minute'
+                self.time_update(new_content)
+                self.one_minute_warning = True
+        elif time.time() > (self.giveaway_start_time + (old_div(self.giveaway_time, self.denominator)*self.numerator)):
+            if not self.all_warnings_fired:
+                new_content = ('{}/{} of giveaway time elapsed, {} minutes remaining'
+                               .format(self.numerator, self.denominator, str((old_div(self.giveaway_timeMins, 4))*3)))
+                self.time_update(new_content)
+                self.giveaway_stage = self.giveaway_stage + 1
 
     def check_time(self):
-        if time.time() > (self.giveawayStartTime+self.giveawayTime):
+        if time.time() > (self.giveaway_start_time + self.giveaway_time):
             print("Giveaway ended")
             try:
-                self.giveawayWinner = random.choice(self.giveawayArray)
-                self.winnerIndex = self.giveawayArray.index(self.giveawayWinner)
-                new_content = 'The giveaway has now ended... The winner is... %s!' % (self.giveawayWinner)
-                direct_content = """Congratulations!, you recently partook in the giveaway called %s and won!
-                Please contact %s on details as to how to claim your prize.""" % (self.giveawayName, self.giveawayCreator,)
-                self.giveawayClient.send_message(dict(
+                self.giveaway_winner = random.choice(self.giveaway_array)
+                self.winner_index = self.giveaway_array.index(self.giveaway_winner)
+                new_content = 'The giveaway has now ended... The winner is... {}!'.format(self.giveaway_winner)
+                direct_content = """Congratulations!, you recently partook in the giveaway called {} and won!
+                Please contact {} on details as to how to claim your prize.""".format(self.giveaway_name, self.giveaway_creator,)
+                self.giveaway_client.send_message(dict(
                     type='private',
-                    to=self.winnerContactDetails[self.winnerIndex],
+                    to=self.giveaway_winner_contact_address[self.winner_index],
                     content=direct_content,
                 ))
 
             except:
                 new_content = 'Giveaway ended: No one entered the giveaway :('
-            if self.giveawayOn == True:
-                self.giveawayOn = False
-                self.giveawayClient.send_message(dict(
+            if self.giveaway_on:
+                self.giveaway_on = False
+                self.giveaway_client.send_message(dict(
                     type='stream',
                     to=self.giveawayStream,
-                    subject=self.giveawaySubject,
+                    subject=self.giveaway_subject,
                     content=new_content,
                 ))
-        elif time.time() > (self.giveawayStartTime+(old_div(self.giveawayTime, 4))) and self.giveawayStage < 1:
-            new_content = '1/4 of giveaway time elapsed, %s minutes remaining' % (str((old_div(self.giveawayTimeMins, 4))*3))
-            self.time_update(new_content)
-            self.giveawayStage = self.giveawayStage + 1
-        elif time.time() > (self.giveawayStartTime+(old_div(self.giveawayTime, 2))) and self.giveawayStage < 2:
-            new_content = '1/2 of giveaway time elapsed, %s minutes remaining' % (str(old_div(self.giveawayTimeMins, 2)))
-            self.time_update(new_content)
-            self.giveawayStage = self.giveawayStage + 1
-        elif time.time() > (self.giveawayStartTime+((old_div(self.giveawayTime, 4))*3)) and self.giveawayStage < 3:
-            new_content = '3/4 of giveaway time elapsed, %s minutes remaining' % (str((old_div(self.giveawayTimeMins, 4))*1))
-            self.time_update(new_content)
-            self.giveawayStage = self.giveawayStage + 1
-        elif time.time() > ((self.giveawayStartTime+self.giveawayTime)-60):
-            if self.giveawayTimeMins > 1.9 and self.one_minute_warning == False:
-                new_content = 'Giveaway ends in 1 minute'
-                self.time_update(new_content)
-                self.one_minute_warning = True
-
-        else:
-            threading.Timer(6.0, self.check_time).start() # More efficient to check every 6 seconds instead of 1.
+        self.time_warning()
+        threading.Timer(6.0, self.check_time).start() # More efficient to check every 6 seconds instead of 1.
 
     def usage(self):
         return '''
@@ -125,13 +132,10 @@ class GiveawayHandler(object):
 
         original_content = message['content']
 
-        # This next line of code is defensive, as we
-        # never want to get into an infinite loop of posting links
-        # for own links!
+
         if message['display_recipient'] == 'giveawayer':
             return False
-        is_giveaway = original_content.startswith('@giveaway')
-        return is_giveaway
+        return original_content.startswith('@giveaway')
 
     def handle_message(self, message, client, state_handler):
 
@@ -139,28 +143,28 @@ class GiveawayHandler(object):
         original_sender = message['sender_email']
         if original_content.split(" ")[1] == "init":
             print("Giveaway: init")
-            if self.giveawayOn == False:
+            if not self.giveaway_on:
                 print("Giveaway: No giveaway currently running")
                 try:
-                    self.giveawayName = original_content.split(" ")[2]
-                    self.giveawayCreator = original_sender
+                    self.giveaway_name = original_content.split(" ")[2]
+                    self.giveaway_creator = original_sender
                     try:
-                        self.giveawayTimeMins = round(float(original_content.split(" ")[3]), 1)
-                        if self.giveawayTimeMins > 100:
+                        self.giveaway_timeMins = round(float(original_content.split(" ")[3]), 1)
+                        if self.giveaway_timeMins > 100:
                             # Giveaway time must be less than 100 mins to avoid stack overflow.
                             # However, limit can be increased within python with the command
                             # sys.setrecursionlimit(NewLimit), default is 1000
                             new_content = 'Error: Giveaway time must be less than or equal to 100 mins'
                         else:
-                            self.giveawayTime = self.giveawayTimeMins * 60
-                            new_content = ("""%s has initiated a giveaway called %s for %s minutes"""
-                                           % (message['sender_full_name'], self.giveawayName, self.giveawayTimeMins))
-                            self.giveawayOn = True
-                            self.giveawayStartTime = time.time()
+                            self.giveaway_time = self.giveaway_timeMins * 60
+                            new_content = ("""{} has initiated a giveaway called {} for {} minutes"""
+                                         .format(message['sender_full_name'], self.giveaway_name, self.giveaway_timeMins))
+                            self.giveaway_on = True
+                            self.giveaway_start_time = time.time()
                             self.giveawayStream = message['display_recipient']
-                            self.giveawaySubject = message['subject']
-                            self.giveawayClient = client
-                            self.giveawayStage = 0
+                            self.giveaway_subject = message['subject']
+                            self.giveaway_client = client
+                            self.giveaway_stage = 0
                             self.one_minute_warning = False
                             self.check_time()
                     except:
@@ -168,16 +172,19 @@ class GiveawayHandler(object):
                 except:
                     new_content = 'Error: Please enter a giveaway name'
             else:
-                new_content = 'Error: Giveaway already running. Cancel using @giveaway cancel'
+                new_content = """Error: Giveaway already running in the {} stream. If you
+                                 created the giveaway, you can cancel it using @giveaway cancel.
+                                 If not, please contact the creator of the giveaway and ask them
+                                 to cancel it for you."""
 
         elif original_content.split(" ")[1] == "enter":
             print("Giveaway enter")
-            if self.giveawayOn == True:
-                if message['sender_full_name'] not in self.giveawayArray and original_sender != self.giveawayCreator:
-                    self.giveawayArray.append(message['sender_full_name'])
-                    self.winnerContactDetails.append(original_sender)
-                    new_content = '%s has entered the giveaway' % (message['sender_full_name'])
-                elif original_sender in self.giveawayArray:
+            if self.giveaway_on:
+                if message['sender_full_name'] not in self.giveaway_array and original_sender != self.giveaway_creator:
+                    self.giveaway_array.append(message['sender_full_name'])
+                    self.giveaway_winner_contact_address.append(original_sender)
+                    new_content = '{} has entered the giveaway'.format(message['sender_full_name'])
+                elif original_sender in self.giveaway_array:
                     new_content = 'Error: You are already entered in this giveaway'
                 else:
                     new_content = 'Error: You cannot participate in a giveaway that you created'
@@ -185,19 +192,19 @@ class GiveawayHandler(object):
                 new_content = 'Error: No giveaway currently running'
         elif original_content.split(" ")[1] == "exit":
             print("Giveaway exit")
-            if original_sender in self.giveawayArray:
-                self.giveawayArray.remove(original_sender)
-                new_content = '%s has left the giveaway' % (original_sender,)
+            if original_sender in self.giveaway_array:
+                self.giveaway_array.remove(original_sender)
+                new_content = '{} has left the giveaway'.format(original_sender,)
             else:
                 new_content = 'Error: You are not currently participating in a giveaway'
         elif original_content.split(" ")[1] == "cancel":
             print("Giveaway cancel")
-            if self.giveawayOn == True:
-                if original_sender == self.giveawayCreator:
+            if self.giveaway_on:
+                if original_sender == self.giveaway_creator:
                     new_content = 'Giveaway cancelled'
-                    self.giveawayOn = False
+                    self.giveaway_on = False
                 else:
-                    new_content = 'Error: Giveaway can only be cancelled by its creator (%s)' % (self.giveawayCreator)
+                    new_content = 'Error: Giveaway can only be cancelled by its creator ({})'.format(self.giveaway_creator)
         else:
             new_content = 'Error: Possible commands are "init", "enter", "exit" and "cancel"'
 
