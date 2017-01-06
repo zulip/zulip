153ad188077cc95a4c5672644fc03b85ebe05b4b
adnrs96 <adi.bansal241996@gmail.com>
2017-01-06 19:41:15 +0530

    Lint for duplicate ids in templates.
    In this commit we enhance our current template linter to detect
    duplicate ids and report them during lint checks. html_branches.py
    was topped up with a new function build_id_dict for the purpose.
    
    Also the get_tag_info function in same file was updated to parse
    ids and classes more robustly in cases of template variables.
    split_for_id_and_class function was added to serve this purpose.
    
    Unit tests for both the functions were created under
    tests/test_html_branches. Also a directory under tests called
    test_template_data was created to hold templates for testing under
    newly created functionality.
    
    check_templates was modified to print to console any duplicates
    detected.
    showell reviewed my commit and helped me out.
    
    Fixes #2950.

--- tools/tests/test_html_branches.py
+++ tools/tests/test_html_branches.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 import unittest
+import os
 
 import tools.lib.template_parser
 
@@ -9,6 +10,8 @@ from tools.lib.html_branches import (
     get_tag_info,
     html_branches,
     html_tag_tree,
+    build_id_dict,
+    split_for_id_and_class,
 )
 
 
@@ -100,3 +103,37 @@ class TestHtmlBranches(unittest.TestCase):
         self.assertEqual(branches[0].staircase_text(), '\n    html\n        head\n            title\n')
         self.assertEqual(branches[1].staircase_text(), '\n    html\n        body\n            p\n                br\n')
         self.assertEqual(branches[2].staircase_text(), '\n    html\n        body\n            p\n')
+
+    def test_build_id_dict(self):
+        # type: () -> None
+        TEST_TEMPLATES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_template_data")
+        templates = ["test_template1.html", "test_template2.html"]
+        templates = [os.path.join(TEST_TEMPLATES_DIR, fn) for fn in templates]
+
+        template_id_dict = build_id_dict(templates)
+
+        self.assertEqual(list(template_id_dict.keys()), ['hello_{{ message }}', 'intro', 'below_navbar'])
+        self.assertEqual(template_id_dict['hello_{{ message }}'], [
+                         'Line 12:/srv/zulip/tools/tests/test_template_data/test_template1.html',
+                         'Line 12:/srv/zulip/tools/tests/test_template_data/test_template2.html'])
+        self.assertEqual(template_id_dict['intro'], [
+                         'Line 10:/srv/zulip/tools/tests/test_template_data/test_template1.html',
+                         'Line 11:/srv/zulip/tools/tests/test_template_data/test_template1.html',
+                         'Line 11:/srv/zulip/tools/tests/test_template_data/test_template2.html'])
+        self.assertEqual(template_id_dict['below_navbar'], ['Line 10:/srv/zulip/tools/tests/test_template_data/test_template2.html'])
+
+    def test_split_for_id_and_class(self):
+        # type: () -> None
+        id1 = "{{ red|blue }}"
+        id2 = "search_box_{{ page }}"
+
+        class1 = "chat_box message"
+        class2 = "stream_{{ topic }}"
+        class3 = "foo {{ a|b|c }} bar"
+
+        self.assertEqual(split_for_id_and_class(id1), ['{{ red|blue }}'])
+        self.assertEqual(split_for_id_and_class(id2), ['search_box_{{ page }}'])
+
+        self.assertEqual(split_for_id_and_class(class1), ['chat_box', 'message'])
+        self.assertEqual(split_for_id_and_class(class2), ['stream_{{ topic }}'])
+        self.assertEqual(split_for_id_and_class(class3), ['foo', '{{ a|b|c }}', 'bar'])
