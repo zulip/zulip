8ad7e083756594340dfbbf9a7e0304c596abb571
Rafid Aslam <rafidteam@gmail.com>
2017-01-10 14:32:11 +0700

    deps: Upgrade and move `handlebars` from `static/third` to `npm`.
    - Remove `handlebars.runtime.js` from static/third and fetch it from npm
    - Upgrade `handlebars` to 3.0.3.
    
    I change the test since there is a patch about line, written in
    handlebars'
    v2.0.0-beta.1 release note:
    "Lines containing only block statements and whitespace are now removed."
    
    Fixes part of #1709.

--- frontend_tests/node_tests/templates.js
+++ frontend_tests/node_tests/templates.js
@@ -288,8 +288,7 @@ function render(template_name, args) {
     var all_html = '';
 
     html = render('bookend', args);
-
-    assert.equal($(html).text().trim(), "subscribed to stream\n    \n        \n            \n            Unsubscribe");
+    assert.equal($(html).text().trim(), "subscribed to stream\n    \n        \n            Unsubscribe");
 
     all_html += html;
 
@@ -300,7 +299,7 @@ function render(template_name, args) {
     };
 
     html = render('bookend', args);
-    assert.equal($(html).text().trim(), 'Not subscribed to stream\n    \n        \n            \n            Subscribe');
+    assert.equal($(html).text().trim(), 'Not subscribed to stream\n    \n        \n            Subscribe');
 
     all_html += '<hr />';
     all_html += html;
