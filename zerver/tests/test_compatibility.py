fa02dfdff44bb9d8e5cbe6534f2342c4368e05bf
Tim Abbott <tabbott@zulipchat.com>
2017-01-29 22:21:13 -0800

    mobile: Add an endpoint for checking compatibility.

---
+++ zerver/tests/test_compatibility.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+from zerver.lib.test_classes import ZulipTestCase
+
+class CompatibilityTest(ZulipTestCase):
+    def test_compatibility(self):
+        # type: () -> None
+        result = self.client_get("/compatibility", HTTP_USER_AGENT='ZulipMobile/5.0')
+        self.assert_json_success(result)
+        result = self.client_get("/compatibility", HTTP_USER_AGENT='ZulipInvalid/5.0')
+        self.assert_json_error(result, "Client is too old")
---
+++ zerver/views/compatibility.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+from django.http import HttpResponse, HttpRequest
+from typing import Any, List, Dict, Optional, Text
+
+from zerver.lib.response import json_error, json_success
+from zerver.lib.user_agent import parse_user_agent
+
+def check_compatibility(request):
+    # type: (HttpRequest) -> HttpResponse
+    user_agent = parse_user_agent(request.META["HTTP_USER_AGENT"])
+    if user_agent['name'] == "ZulipInvalid":
+        return json_error("Client is too old")
+    return json_success()
