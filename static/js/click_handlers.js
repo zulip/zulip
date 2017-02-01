548bab5c5d4067f967fd3054829d0cdf2fc2c3c3
brockwhittaker <brock@zulipchat.org>
2017-02-01 11:51:25 -0800

    Replace the modal footers with an exit button at top of overlay.
    This replaces the bootstrap default modal footers that have a
    [data-dismiss] button with an .exit button in the top section of the
    overlay that is styled congruently to the current subscriptions page.

--- static/js/click_handlers.js
+++ static/js/click_handlers.js
@@ -328,7 +328,7 @@ $(function () {
     });
 
     $(".informational-overlays").click(function (e) {
-        if ($(e.target).is(".informational-overlays, [data-dismiss]")) {
+        if ($(e.target).is(".informational-overlays, .exit")) {
             ui.hide_info_overlay();
         }
     });
