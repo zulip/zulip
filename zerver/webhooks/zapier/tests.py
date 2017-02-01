688f95f19049e42afa58385075b67cf815060c04
wizsid11 <siddharth.mahapatra95@gmail.com>
2017-02-01 15:00:32 +0530

    docs: Add weather example to Zapier integration.
    Add documentation for weather update through Zapier.
    Contains tests and fixtures for weather example.
    
    Fixes #3483.

--- zerver/webhooks/zapier/tests.py
+++ zerver/webhooks/zapier/tests.py
@@ -11,3 +11,9 @@ class ZapierHookTests(WebhookTestCase):
         expected_subject = u"New email from zulip@zulip.com"
         expected_message = u"Your email content is: \nMy Email content."
         self.send_and_test_stream_message('correct_subject_and_body', expected_subject, expected_message)
+
+    def test_zapier_weather_update(self):
+        # type: () -> None
+        expected_subject = u"Here is your weather update for the day:"
+        expected_message = u"Foggy in the morning.\nMaximum temperature to be 24.\nMinimum temperature to be 12"
+        self.send_and_test_stream_message('weather_update', expected_subject, expected_message)
