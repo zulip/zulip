69207d84f255de075b6e21002cc4c17e979ae0a6
Rishi Gupta <rishig@zulipchat.com>
2017-02-01 16:29:58 -0800

    analytics: Change time_range to not include current day/hour.
    Current day/hour will always be 0, since we haven't computed it yet for the
    CountStat tables.

--- analytics/lib/time_utils.py
+++ analytics/lib/time_utils.py
@@ -1,20 +1,20 @@
-from zerver.lib.timestamp import ceiling_to_hour, ceiling_to_day, timestamp_to_datetime
+from zerver.lib.timestamp import floor_to_hour, floor_to_day, timestamp_to_datetime
 from analytics.lib.counts import CountStat
 
 from datetime import datetime, timedelta
 from typing import List, Optional
 
-# If min_length is None, returns end_times from ceiling(start) to ceiling(end), inclusive.
+# If min_length is None, returns end_times from ceiling(start) to floor(end), inclusive.
 # If min_length is greater than 0, pads the list to the left.
 # So informally, time_range(Sep 20, Sep 22, day, None) returns [Sep 20, Sep 21, Sep 22],
 # and time_range(Sep 20, Sep 22, day, 5) returns [Sep 18, Sep 19, Sep 20, Sep 21, Sep 22]
 def time_range(start, end, frequency, min_length):
     # type: (datetime, datetime, str, Optional[int]) -> List[datetime]
     if frequency == CountStat.HOUR:
-        end = ceiling_to_hour(end)
+        end = floor_to_hour(end)
         step = timedelta(hours=1)
     elif frequency == CountStat.DAY:
-        end = ceiling_to_day(end)
+        end = floor_to_day(end)
         step = timedelta(days=1)
     else:
         raise ValueError("Unknown frequency: %s" % (frequency,))
