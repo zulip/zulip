zulip/zulip

commit 4060a976562f14d0833d9538a6025458cc6738ab

messages: Strip trailing whitespace in message contents.
I dug into why we never did this before, and it turns out we did, but
using `$.trim()` (which removes leading whitespace as well!).  When
removing the `$.trim()` usage.

Fixes #3294.

zerver/lib/actions.py
@@ -1235,7 +1235,7 @@ def send_pm_if_empty_stream(sender, stream, stream_name, realm):
 # check_message:
 # Returns message ready for sending with do_send_message on success or the error message (string) on error.
 def check_message(sender, client, message_type_name, message_to,
-                  subject_name, message_content, realm=None, forged=False,
+                  subject_name, message_content_raw, realm=None, forged=False,
                   forged_timestamp=None, forwarder_user_profile=None, local_id=None,
                   sender_queue_id=None):
     # type: (UserProfile, Client, Text, Sequence[Text], Text, Text, Optional[Realm], bool, Optional[float], Optional[UserProfile], Optional[Text], Optional[Text]) -> Dict[str, Any]
@@ -1243,9 +1243,10 @@ def check_message(sender, client, message_type_name, message_to,
     if not message_to and message_type_name == 'stream' and sender.default_sending_stream:
         # Use the users default stream
         message_to = [sender.default_sending_stream.name]
-    elif len(message_to) == 0:
+    if len(message_to) == 0:
         raise JsonableError(_("Message must have recipients"))
-    if len(message_content.strip()) == 0:
+    message_content = message_content_raw.strip()
+    if len(message_content) == 0:
         raise JsonableError(_("Message must not be empty"))
     message_content = truncate_body(message_content)
 

zerver/tests/test_messages.py
@@ -853,6 +853,20 @@ class MessagePOSTTest(ZulipTestCase):
         self.assertEqual(ujson.loads(result1.content)['id'],
                          ujson.loads(result2.content)['id'])
 
+    def test_strip_message(self):
+        # type: () -> None
+        """
+        Sending a message longer than the maximum message length succeeds but is
+        truncated.
+        """
+        self.login("hamlet@zulip.com")
+        post_data = {"type": "stream", "to": "Verona", "client": "test suite",
+                     "content": "I like whitespace at the end! \n\n \n", "subject": "Test subject"}
+        result = self.client_post("/json/messages", post_data)
+        self.assert_json_success(result)
+        sent_message = self.get_last_message()
+        self.assertEqual(sent_message.content, "I like whitespace at the end!")
+
     def test_long_message(self):
         # type: () -> None
         """

frontend_tests/casper_tests/11-mention.js
@@ -47,7 +47,7 @@ casper.then(function () {
 
 casper.then(function () {
     common.expected_messages('zhome', ['Verona > Test mention all'],
-     ["<p><span class=\"user-mention user-mention-me\" data-user-id=\"*\">@all</span> </p>"]);
+     ["<p><span class=\"user-mention user-mention-me\" data-user-id=\"*\">@all</span></p>"]);
 });
 
 

zerver/webhooks/deskdotcom/tests.py
@@ -21,7 +21,7 @@ class DeskDotComHookTests(WebhookTestCase):
         # type: () -> None
 
         expected_subject = u"static text notification"
-        expected_message = u"This is a custom action.\n"
+        expected_message = u"This is a custom action."
 
         self.send_and_test_stream_message('static_text', expected_subject, expected_message,
                                           content_type="application/x-www-form-urlencoded",
@@ -32,7 +32,7 @@ class DeskDotComHookTests(WebhookTestCase):
         expected_subject = u"case updated notification"
         expected_message = (u"Case 2 updated. "
                             u"Link: <a href='https://deskdotcomtest.desk.com/web/agent/case/2'>"
-                            u"I have a question</a>\n")
+                            u"I have a question</a>")
 
         self.send_and_test_stream_message('case_updated', expected_subject, expected_message,
                                           content_type="application/x-www-form-urlencoded",
@@ -44,7 +44,7 @@ class DeskDotComHookTests(WebhookTestCase):
         expected_subject = u"case updated notification"
         expected_message = (u"Case 2 updated. "
                             u"Link: <a href='https://deskdotcomtest.desk.com/web/agent/case/2'>"
-                            u"Il mio hovercraft è pieno di anguille.</a>\n")
+                            u"Il mio hovercraft è pieno di anguille.</a>")
 
         self.send_and_test_stream_message('unicode_text_italian', expected_subject, expected_message,
                                           content_type="application/x-www-form-urlencoded",
@@ -56,7 +56,7 @@ class DeskDotComHookTests(WebhookTestCase):
         expected_subject = u"case updated notification"
         expected_message = (u"Case 2 updated. "
                             u"Link: <a href='https://deskdotcomtest.desk.com/web/agent/case/2'>"
-                            u"私のホバークラフトは鰻でいっぱいです</a>\n")
+                            u"私のホバークラフトは鰻でいっぱいです</a>")
 
         self.send_and_test_stream_message('unicode_text_japanese', expected_subject, expected_message,
                                           content_type="application/x-www-form-urlencoded",

zerver/webhooks/greenhouse/tests.py
@@ -14,7 +14,7 @@ class GreenhouseHookTests(WebhookTestCase):
         expected_message = ("Hire Candidate\n>Johnny Smith\nID: 19"
                             "\nApplying for role:\nDeveloper\n**Emails:**"
                             "\nPersonal\npersonal@example.com\nWork\nwork@example.com\n\n\n>"
-                            "**Attachments:**\n[Resume](https://prod-heroku.s3.amazonaws.com/...)\n")
+                            "**Attachments:**\n[Resume](https://prod-heroku.s3.amazonaws.com/...)")
 
         self.send_and_test_stream_message('candidate_hired',
                                           expected_subject,
@@ -28,7 +28,7 @@ class GreenhouseHookTests(WebhookTestCase):
                             "265788\nApplying for role:\nDesigner"
                             "\n**Emails:**\nPersonal\n"
                             "hector.porter.265788@example.com\n\n\n>"
-                            "**Attachments:**\n[Resume](https://prod-heroku.s3.amazonaws.com/...)\n")
+                            "**Attachments:**\n[Resume](https://prod-heroku.s3.amazonaws.com/...)")
 
         self.send_and_test_stream_message('candidate_rejected',
                                           expected_subject,
@@ -44,7 +44,7 @@ class GreenhouseHookTests(WebhookTestCase):
                             "\ngiuseppe.hurley@example.com\n\n\n>"
                             "**Attachments:**\n[Resume](https://prod-heroku.s3.amazonaws.com/...)"
                             "\n[Cover_Letter](https://prod-heroku.s3.amazonaws.com/...)"
-                            "\n[Attachment](https://prod-heroku.s3.amazonaws.com/...)\n")
+                            "\n[Attachment](https://prod-heroku.s3.amazonaws.com/...)")
 
         self.send_and_test_stream_message('candidate_stage_change',
                                           expected_subject,
@@ -58,7 +58,7 @@ class GreenhouseHookTests(WebhookTestCase):
                             "\nID: 968190\nApplying for role:\n"
                             "Designer\n**Emails:**\nPersonal"
                             "\nt.troy@example.com\n\n\n>**Attachments:**"
-                            "\n[Resume](https://prod-heroku.s3.amazonaws.com/...)\n")
+                            "\n[Resume](https://prod-heroku.s3.amazonaws.com/...)")
 
         self.send_and_test_stream_message('prospect_created',
                                           expected_subject,

zerver/webhooks/jira/tests.py
@@ -62,8 +62,7 @@ class JiraHookTests(WebhookTestCase):
         expected_message = """Leo Franchi **added comment to** [BUG-15](http://lfranchi.com:8080/browse/BUG-15) (assigned to **Othello, the Moor of Venice**):
 
 
-Adding a comment. Oh, what a comment it is!
-"""
+Adding a comment. Oh, what a comment it is!"""
         self.send_and_test_stream_message('commented_v1', expected_subject, expected_message)
         self.send_and_test_stream_message('commented_v2', expected_subject, expected_message)
 
@@ -73,8 +72,7 @@ Adding a comment. Oh, what a comment it is!
             expected_message = """Leo Franchi **edited comment on** [BUG-15](http://lfranchi.com:8080/browse/BUG-15) (assigned to **Othello, the Moor of Venice**):
 
 
-Adding a comment. Oh, what a comment it is!
-"""
+Adding a comment. Oh, what a comment it is!"""
             self.send_and_test_stream_message('comment_edited_v2', expected_subject, expected_message)
 
     def test_comment_deleted(self):
@@ -86,7 +84,7 @@ Adding a comment. Oh, what a comment it is!
     def test_commented_markup(self):
         # type: () -> None
         expected_subject = "TEST-7: Testing of rich text"
-        expected_message = """Leonardo Franchi [Administrator] **added comment to** [TEST-7](https://zulipp.atlassian.net/browse/TEST-7):\n\n\nThis is a comment that likes to **exercise** a lot of _different_ `conventions` that `jira uses`.\r\n\r\n~~~\n\r\nthis code is not highlighted, but monospaced\r\n\n~~~\r\n\r\n~~~\n\r\ndef python():\r\n    print "likes to be formatted"\r\n\n~~~\r\n\r\n[http://www.google.com](http://www.google.com) is a bare link, and [Google](http://www.google.com) is given a title.\r\n\r\nThanks!\r\n\r\n~~~ quote\n\r\nSomeone said somewhere\r\n\n~~~\n"""
+        expected_message = """Leonardo Franchi [Administrator] **added comment to** [TEST-7](https://zulipp.atlassian.net/browse/TEST-7):\n\n\nThis is a comment that likes to **exercise** a lot of _different_ `conventions` that `jira uses`.\r\n\r\n~~~\n\r\nthis code is not highlighted, but monospaced\r\n\n~~~\r\n\r\n~~~\n\r\ndef python():\r\n    print "likes to be formatted"\r\n\n~~~\r\n\r\n[http://www.google.com](http://www.google.com) is a bare link, and [Google](http://www.google.com) is given a title.\r\n\r\nThanks!\r\n\r\n~~~ quote\n\r\nSomeone said somewhere\r\n\n~~~"""
         self.send_and_test_stream_message('commented_markup_v1', expected_subject, expected_message)
         self.send_and_test_stream_message('commented_markup_v2', expected_subject, expected_message)
 
@@ -102,8 +100,7 @@ Adding a comment. Oh, what a comment it is!
         expected_subject = "BUG-15: New bug with hook"
         expected_message = """Leo Franchi **updated** [BUG-15](http://lfranchi.com:8080/browse/BUG-15) (assigned to **Othello, the Moor of Venice**):
 
-* Changed assignee to **Othello, the Moor of Venice**
-"""
+* Changed assignee to **Othello, the Moor of Venice**"""
         self.send_and_test_stream_message('reassigned_v1', expected_subject, expected_message)
         self.send_and_test_stream_message('reassigned_v2', expected_subject, expected_message)
 
@@ -112,8 +109,7 @@ Adding a comment. Oh, what a comment it is!
         expected_subject = "TEST-1: Fix That"
         expected_message = """Leonardo Franchi [Administrator] **updated** [TEST-1](https://zulipp.atlassian.net/browse/TEST-1) (assigned to **leo@zulip.com**):
 
-* Changed priority from **Critical** to **Major**
-"""
+* Changed priority from **Critical** to **Major**"""
         self.send_and_test_stream_message('updated_priority_v1', expected_subject, expected_message)
         self.send_and_test_stream_message('updated_priority_v2', expected_subject, expected_message)
 
@@ -122,8 +118,7 @@ Adding a comment. Oh, what a comment it is!
             expected_subject = "TEST-1: Fix That"
             expected_message = """Leonardo Franchi [Administrator] **updated** [TEST-1](https://zulipp.atlassian.net/browse/TEST-1):
 
-* Changed status from **To Do** to **In Progress**
-"""
+* Changed status from **To Do** to **In Progress**"""
             self.send_and_test_stream_message('change_status_v1', expected_subject, expected_message)
             self.send_and_test_stream_message('change_status_v2', expected_subject, expected_message)
 

zerver/webhooks/pivotal/tests.py
@@ -90,8 +90,7 @@ class PivotalV5HookTests(WebhookTestCase):
         # type: () -> None
         expected_subject = '#63486316: Story of the Year'
         expected_message = """Leo Franchi updated [Hard Code](https://www.pivotaltracker.com/s/projects/807213): [Story of the Year](http://www.pivotaltracker.com/story/show/63486316):
-* state changed from **unstarted** to **accepted**
-"""
+* state changed from **unstarted** to **accepted**"""
         self.send_and_test_stream_message('accepted', expected_subject, expected_message, content_type="application/xml")
 
     def test_commented(self):
@@ -117,16 +116,14 @@ A comment on the story
         # type: () -> None
         expected_subject = '#63486316: Story of the Year'
         expected_message = """Leo Franchi updated [Hard Code](https://www.pivotaltracker.com/s/projects/807213): [Story of the Year](http://www.pivotaltracker.com/story/show/63486316):
-* state changed from **accepted** to **delivered**
-"""
+* state changed from **accepted** to **delivered**"""
         self.send_and_test_stream_message('delivered', expected_subject, expected_message, content_type="application/xml")
 
     def test_finished(self):
         # type: () -> None
         expected_subject = '#63486316: Story of the Year'
         expected_message = """Leo Franchi updated [Hard Code](https://www.pivotaltracker.com/s/projects/807213): [Story of the Year](http://www.pivotaltracker.com/story/show/63486316):
-* state changed from **delivered** to **accepted**
-"""
+* state changed from **delivered** to **accepted**"""
         self.send_and_test_stream_message('finished', expected_subject, expected_message, content_type="application/xml")
 
     def test_moved(self):
@@ -143,24 +140,21 @@ A comment on the story
 ~~~quote
 Try again next time
 ~~~
-* state changed from **delivered** to **rejected**
-"""
+* state changed from **delivered** to **rejected**"""
         self.send_and_test_stream_message('rejected', expected_subject, expected_message, content_type="application/xml")
 
     def test_started(self):
         # type: () -> None
         expected_subject = '#63495972: Fresh Story'
         expected_message = """Leo Franchi updated [Hard Code](https://www.pivotaltracker.com/s/projects/807213): [Fresh Story](http://www.pivotaltracker.com/story/show/63495972):
-* state changed from **unstarted** to **started**
-"""
+* state changed from **unstarted** to **started**"""
         self.send_and_test_stream_message('started', expected_subject, expected_message, content_type="application/xml")
 
     def test_created_estimate(self):
         # type: () -> None
         expected_subject = '#63496066: Pivotal Test'
         expected_message = """Leo Franchi updated [Hard Code](https://www.pivotaltracker.com/s/projects/807213): [Pivotal Test](http://www.pivotaltracker.com/story/show/63496066):
-* estimate is now **3 points**
-"""
+* estimate is now **3 points**"""
         self.send_and_test_stream_message('created_estimate', expected_subject, expected_message, content_type="application/xml")
 
     def test_type_changed(self):
@@ -168,8 +162,7 @@ Try again next time
         expected_subject = '#63496066: Pivotal Test'
         expected_message = """Leo Franchi updated [Hard Code](https://www.pivotaltracker.com/s/projects/807213): [Pivotal Test](http://www.pivotaltracker.com/story/show/63496066):
 * estimate changed from 3 to **0 points**
-* type changed from **feature** to **bug**
-"""
+* type changed from **feature** to **bug**"""
         self.send_and_test_stream_message('type_changed', expected_subject, expected_message, content_type="application/xml")
 
     def get_body(self, fixture_name):

commit 6ba1cd797b02e5a65ffbbffeadf8239045916fbc

Fix regression in whitespace stripping at the start of messages.
This fixes a regression in 4060a976562f14d0833d9538a6025458cc6738ab,
which incorrectly stripped whitespace at the start of messages as
well.

Fixes #3719.

zerver/lib/actions.py
@@ -1249,7 +1249,7 @@ def check_message(sender, client, message_type_name, message_to,
         message_to = [sender.default_sending_stream.name]
     if len(message_to) == 0:
         raise JsonableError(_("Message must have recipients"))
-    message_content = message_content_raw.strip()
+    message_content = message_content_raw.rstrip()
     if len(message_content) == 0:
         raise JsonableError(_("Message must not be empty"))
     message_content = truncate_body(message_content)

zerver/tests/test_messages.py
@@ -861,11 +861,11 @@ class MessagePOSTTest(ZulipTestCase):
         """
         self.login("hamlet@zulip.com")
         post_data = {"type": "stream", "to": "Verona", "client": "test suite",
-                     "content": "I like whitespace at the end! \n\n \n", "subject": "Test subject"}
+                     "content": "  I like whitespace at the end! \n\n \n", "subject": "Test subject"}
         result = self.client_post("/json/messages", post_data)
         self.assert_json_success(result)
         sent_message = self.get_last_message()
-        self.assertEqual(sent_message.content, "I like whitespace at the end!")
+        self.assertEqual(sent_message.content, "  I like whitespace at the end!")
 
     def test_long_message(self):
         # type: () -> None