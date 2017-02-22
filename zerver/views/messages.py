0b7bcad98ad893b923a29ccf32f96e8520b99fae
Tim Abbott <tabbott@zulipchat.com>
2017-02-22 14:43:22 -0800

    messages: Fix empty condition for muted streams.
    This fixes a sqlalchemy warning (that caused unnecessary complexity in
    this query).

--- zerver/views/messages.py
+++ zerver/views/messages.py
@@ -491,8 +491,10 @@ def exclude_muting_conditions(user_profile, narrow):
             recipient__type=Recipient.STREAM
         ).values('recipient_id')
         muted_recipient_ids = [row['recipient_id'] for row in rows]
-        condition = not_(column("recipient_id").in_(muted_recipient_ids))
-        conditions.append(condition)
+        if len(muted_recipient_ids) > 0:
+            # No point in adding a condition if there are no muted streams
+            condition = not_(column("recipient_id").in_(muted_recipient_ids))
+            conditions.append(condition)
 
     muted_topics = ujson.loads(user_profile.muted_topics)
     if muted_topics:
