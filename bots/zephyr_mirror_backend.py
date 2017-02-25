0218422e967c6e3e936ceaf60f6e039215d5584e
Rishi Gupta <rishig@zulipchat.com>
2017-02-25 13:11:53 -0800

    Use time.time() instead of datetime.now() to measure elapsed time.
    Both because it is more idiomatic and because we will soon start enforcing
    that all datetimes in Zulip are timezone aware.

--- bots/zephyr_mirror_backend.py
+++ bots/zephyr_mirror_backend.py
@@ -38,6 +38,7 @@ import optparse
 import os
 import datetime
 import textwrap
+import time
 import signal
 import logging
 import hashlib
@@ -762,8 +763,8 @@ def maybe_forward_to_zephyr(message):
             # Don't try forward private messages with non-MIT users
             # to MIT Zephyr.
             return
-        timestamp_now = datetime.datetime.now().strftime("%s")
-        if float(message["timestamp"]) < float(timestamp_now) - 15:
+        timestamp_now = int(time.time())
+        if float(message["timestamp"]) < timestamp_now - 15:
             logger.warning("Skipping out of order message: %s < %s" %
                            (message["timestamp"], timestamp_now))
             return
