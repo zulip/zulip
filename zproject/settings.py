564c8a657ec017c8f406223d16ef484330f0a674
Rishi Gupta <rishig@zulipchat.com>
2017-02-25 21:11:21 -0800

    Set settings.TIME_ZONE to UTC.
    If there are weird time-based regressions in the next couple of days, it's
    probably due to this.

--- zproject/settings.py
+++ zproject/settings.py
@@ -244,7 +244,7 @@ VOYAGER = PRODUCTION and not ZULIP_COM
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/New_York'
+TIME_ZONE = 'UTC'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
