4b731f0ad3a6659c82a02cd6ad45a57c5b16a5f8
Rishi Gupta <rishig@zulipchat.com>
2017-02-25 19:58:25 -0800

    Change timezone-naive datetimes to use timezone.now() where safe to do so.
    Change timezone-naive datetimes to use timezone.now() in cases where there
    is no change in behavior.

--- zerver/management/commands/enqueue_digest_emails.py
+++ zerver/management/commands/enqueue_digest_emails.py
@@ -94,7 +94,7 @@ in a while.
         # To be really conservative while we don't have user timezones or
         # special-casing for companies with non-standard workweeks, only
         # try to send mail on Tuesdays, Wednesdays, and Thursdays.
-        if datetime.datetime.utcnow().weekday() not in VALID_DIGEST_DAYS:
+        if timezone.now().weekday() not in VALID_DIGEST_DAYS:
             return
 
         deployment_realms = realms_for_this_deployment()
