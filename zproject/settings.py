c756616b080cb063ec1a860b458dafcded3c60b0
hollywoodno <hollywoodno@gmail.com>
2017-03-02 19:17:44 +0000

    remove geo path setting it will be separate issue

--- zproject/settings.py
+++ zproject/settings.py
@@ -1168,6 +1168,3 @@ PROFILE_ALL_REQUESTS = False
 CROSS_REALM_BOT_EMAILS = set(('feedback@zulip.com', 'notification-bot@zulip.com'))
 
 CONTRIBUTORS_DATA = os.path.join(STATIC_ROOT, 'generated/github-contributors.json')
-
-# Geo location
-GEOIP_PATH = '/srv/zulip/geo'
