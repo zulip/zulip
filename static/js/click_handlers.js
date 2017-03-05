94ba628003586b39e21e4288f82e98e13e78f153
Raghav Jajodia <jajodia.raghav@gmail.com>
2017-03-05 19:17:00 +0530

    compose: Focus on topic when narrowed to a stream+topic.
    When narrowed to a stream+topic and clicking the "new topic" button,
    focus in on topic and the text remains selected.
    Fixes #3888.

--- static/js/click_handlers.js
+++ static/js/click_handlers.js
@@ -336,7 +336,7 @@ $(function () {
 
 
     $('.compose_stream_button').click(function () {
-        compose.start('stream');
+        compose.start('stream', {trigger: 'new topic button'});
     });
     $('.compose_private_button').click(function () {
         compose.start('private');
