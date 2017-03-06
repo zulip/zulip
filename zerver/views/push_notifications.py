c0ad9c02fd8b27e47b89dbfe810f2ca3f1dc804a
Tim Abbott <tabbott@zulipchat.com>
2017-03-05 19:04:23 -0800

    push: Extract validate_token helper function.

--- zerver/views/push_notifications.py
+++ zerver/views/push_notifications.py
@@ -7,28 +7,33 @@ from django.http import HttpRequest, HttpResponse
 from django.utils.translation import ugettext as _
 
 from zerver.lib.push_notifications import add_push_device_token
-from zerver.lib.request import has_request_variables, REQ
+from zerver.lib.request import has_request_variables, REQ, JsonableError
 from zerver.lib.response import json_success, json_error
 from zerver.lib.validator import check_string, check_list, check_bool
 from zerver.models import PushDeviceToken, UserProfile
 
+def validate_token(token_str):
+    # type: (str) -> None
+    if token_str == '' or len(token_str) > 4096:
+        raise JsonableError(_('Empty or invalid length token'))
+
 @has_request_variables
-def add_apns_device_token(request, user_profile, token=REQ(), appid=REQ(default=settings.ZULIP_IOS_APP_ID)):
+def add_apns_device_token(request, user_profile, token=REQ(),
+                          appid=REQ(default=settings.ZULIP_IOS_APP_ID)):
     # type: (HttpRequest, UserProfile, str, str) -> HttpResponse
+    validate_token(token)
     add_push_device_token(user_profile, token, PushDeviceToken.APNS, ios_app_id=appid)
     return json_success()
 
 @has_request_variables
-def add_android_reg_id(request, user_profile, token_str=REQ("token")):
+def add_android_reg_id(request, user_profile, token=REQ()):
     # type: (HttpRequest, UserProfile, str) -> HttpResponse
-    add_push_device_token(user_profile, token_str, PushDeviceToken.GCM)
+    validate_token(token)
+    add_push_device_token(user_profile, token, PushDeviceToken.GCM)
     return json_success()
 
 def remove_push_device_token(request, user_profile, token_str, kind):
     # type: (HttpRequest, UserProfile, str, int) -> HttpResponse
-    if token_str == '' or len(token_str) > 4096:
-        return json_error(_('Empty or invalid length token'))
-
     try:
         token = PushDeviceToken.objects.get(token=token_str, kind=kind)
         token.delete()
@@ -40,9 +45,11 @@ def remove_push_device_token(request, user_profile, token_str, kind):
 @has_request_variables
 def remove_apns_device_token(request, user_profile, token=REQ()):
     # type: (HttpRequest, UserProfile, str) -> HttpResponse
+    validate_token(token)
     return remove_push_device_token(request, user_profile, token, PushDeviceToken.APNS)
 
 @has_request_variables
 def remove_android_reg_id(request, user_profile, token=REQ()):
     # type: (HttpRequest, UserProfile, str) -> HttpResponse
+    validate_token(token)
     return remove_push_device_token(request, user_profile, token, PushDeviceToken.GCM)
