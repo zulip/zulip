114c80efaedcbf8d3ba07b27b7e1b8caca72e510
Tomasz Kolek <tomasz-kolek@o2.pl>
2017-03-06 07:12:40 +0100

    Add ignore pull requests mechanism in Travis integration.
    Add ignore_pull_requests param to url, with a default of true.
    
    Fixes: #3912.

--- zerver/webhooks/travis/view.py
+++ zerver/webhooks/travis/view.py
@@ -6,7 +6,7 @@ from django.http import HttpRequest, HttpResponse
 from zerver.decorator import REQ, has_request_variables, api_key_only_webhook_view
 from zerver.lib.actions import check_send_message
 from zerver.lib.response import json_success
-from zerver.lib.validator import check_dict, check_string
+from zerver.lib.validator import check_dict, check_string, check_bool
 from zerver.models import UserProfile, Client
 from typing import Dict
 
@@ -26,14 +26,17 @@ MESSAGE_TEMPLATE = (
 def api_travis_webhook(request, user_profile, client,
                        stream=REQ(default='travis'),
                        topic=REQ(default=None),
+                       ignore_pull_requests=REQ(validator=check_bool, default=True),
                        message=REQ('payload', validator=check_dict([
                            ('author_name', check_string),
                            ('status_message', check_string),
                            ('compare_url', check_string),
                        ]))):
-    # type: (HttpRequest, UserProfile, Client, str, str, Dict[str, str]) -> HttpResponse
+    # type: (HttpRequest, UserProfile, Client, str, str, str, Dict[str, str]) -> HttpResponse
 
     message_status = message['status_message']
+    if ignore_pull_requests and message['type'] == 'pull_request':
+        return json_success()
 
     if message_status in GOOD_STATUSES:
         emoji = ':thumbsup:'
