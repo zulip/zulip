4a47b0db6215fbe21fa55e1e275d50b6c31ddd92
Raghav Jajodia <jajodia.raghav@gmail.com>
2017-03-07 10:20:26 +0530

    stats: Added "Last updated" to bottom of stats page.
    "Last updated" has been added to the stats page.
    Fixes #3857.

--- zproject/settings.py
+++ zproject/settings.py
@@ -880,7 +880,6 @@ JS_SPECS = {
     },
     'stats': {
         'source_filenames': [
-            'node_modules/jquery/dist/jquery.js',
             'js/stats/stats.js',
         ],
         'minifed_source_filenames': [
