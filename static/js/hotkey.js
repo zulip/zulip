zulip/zulip

commit 8801158dfd0335cc241fc2b31291a8895a773c86

Add hotkey actions for page up/down in compose box.
The hotkey actions make the caret position go to the top of the compose
box or bottom respectively, while the compose box is open.

Fixes #1910.

static/js/hotkey.js
@@ -323,6 +323,13 @@ function process_hotkey(e) {
         if ((event_name === 'up_arrow' || event_name === 'down_arrow') && focus_in_empty_compose()) {
             compose.cancel();
             // don't return, as we still want it to be picked up by the code below
+        } else if (event_name === "page_up") {
+            $("#new_message_content").caret(0);
+            return true;
+        } else if (event_name === "page_down") {
+            // so that it always goes to the end of the compose box.
+            $("#new_message_content").caret(Infinity);
+            return true;
         } else {
             // Let the browser handle the key normally.
             return false;

commit e61b54b74abd50c5018377e294e77e56912cac37

Fix spacebar being broken in the compose box.
This fixes a regression introduced in
8801158dfd0335cc241fc2b31291a8895a773c86.

Apparently the `page_up`/`page_down` hotkey definitions somewhat
confusingly called `spacebar` `page_down`, etc.  Probably worth doing
further cleanup on this.

static/js/hotkey.js
@@ -34,12 +34,12 @@ var actions_dropdown_hotkeys = [
 var hotkeys_shift = {
     // these can be triggered by shift + key only
     9: {name: 'shift_tab', message_view_only: false}, // tab
-    32: {name: 'page_up', message_view_only: true},  // space bar
+    32: {name: 'shift_spacebar', message_view_only: true},  // space bar
 };
 var hotkeys_no_modifiers = {
     // these can be triggered by key only (without shift)
     9: {name: 'tab', message_view_only: false}, // tab
-    32: {name: 'page_down', message_view_only: true}, // space bar
+    32: {name: 'spacebar', message_view_only: true}, // space bar
     33: {name: 'page_up', message_view_only: true}, // page up
     34: {name: 'page_down', message_view_only: true}, // page down
     35: {name: 'end', message_view_only: true}, // end
@@ -424,12 +424,14 @@ function process_hotkey(e) {
             navigate.to_end();
             return true;
         case 'page_up':
+        case 'shift_spacebar':
             if (!is_settings_page()) {
                 navigate.page_up();
                 return true;
             }
             break;
         case 'page_down':
+        case 'spacebar':
             if (!is_settings_page()) {
                 navigate.page_down();
                 return true;