bce8807be15e3d7821e42d4cdec468306ef62ed4
catthu <catthu@mit.edu>
2017-03-09 23:48:48 +0000

    Revert "Support autocomplete suggestions for searching with both stream and sender."
    This reverts commit 3bf4bed6f9e257d6763824d8f3f284df49457017.

--- frontend_tests/node_tests/filter.js
+++ frontend_tests/node_tests/filter.js
@@ -482,17 +482,9 @@ function get_predicate(operators) {
     assert.equal(Filter.describe(narrow), string);
 
     narrow = [
-        {operator: 'stream', operand: 'devel'},
-        {operator: 'sender', operand: 'tim'},
-    ];
-    string = 'Narrow to devel > messages sent by';
-    assert.equal(Filter.describe(narrow), string);
-
-    narrow = [
         {operator: 'is', operand: 'private'},
         {operator: 'search', operand: 'lunch'},
     ];
-
     string = 'Narrow to all private messages, Search for lunch';
     assert.equal(Filter.describe(narrow), string);
 
@@ -559,7 +551,6 @@ function get_predicate(operators) {
     ];
     string = 'Narrow to stream devel, Exclude messages with one or more image';
     assert.equal(Filter.describe(narrow), string);
-
 
 }());
 
