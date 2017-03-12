87981a2bf16e7c694b01415bbd23e3b308dc76b7
Rishi Gupta <rishig@zulipchat.com>
2017-03-11 23:02:10 -0800

    analytics: Fix direct import of models in migrations.

--- analytics/migrations/0009_remove_messages_to_stream_stat.py
+++ analytics/migrations/0009_remove_messages_to_stream_stat.py
@@ -3,11 +3,21 @@ from django.db.backends.postgresql_psycopg2.schema import DatabaseSchemaEditor
 from django.db.migrations.state import StateApps
 from django.db import migrations
 
-from analytics.lib.counts import do_delete_count_stat
 
 def delete_messages_sent_to_stream_stat(apps, schema_editor):
     # type: (StateApps, DatabaseSchemaEditor) -> None
-    do_delete_count_stat('messages_sent_to_stream:is_bot')
+    UserCount = apps.get_model('analytics', 'UserCount')
+    StreamCount = apps.get_model('analytics', 'StreamCount')
+    RealmCount = apps.get_model('analytics', 'RealmCount')
+    InstallationCount = apps.get_model('analytics', 'InstallationCount')
+    FillState = apps.get_model('analytics', 'FillState')
+
+    property = 'messages_sent_to_stream:is_bot'
+    UserCount.objects.filter(property=property).delete()
+    StreamCount.objects.filter(property=property).delete()
+    RealmCount.objects.filter(property=property).delete()
+    InstallationCount.objects.filter(property=property).delete()
+    FillState.objects.filter(property=property).delete()
 
 class Migration(migrations.Migration):
 
