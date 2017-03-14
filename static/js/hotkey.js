30b750768f496748d5cdfbd989dfae9043b66865
Steve Howell <showell30@yahoo.com>
2017-03-13 18:28:33 -0700

    hotkeys: Remove wasteful resize calls for keydown events.
    This fix prevents us from calling the resize library for nearly
    every single keydown event in the app (ouch!).  Realistically,
    this performance improvement only impacts folks who turned on
    the autoscroll_forever feature, but it should be a significant
    speed-up for them.  We should go further with this fix, but the
    main damage is undone.

--- static/js/hotkey.js
+++ static/js/hotkey.js
@@ -584,9 +584,11 @@ exports.process_keydown = function (e) {
 
 $(document).keydown(function (e) {
     if (exports.process_keydown(e)) {
+        // TODO: We should really move this resize code
+        // so it only executes as part of navigation actions.
+        resize.resize_bottom_whitespace();
         e.preventDefault();
     }
-    resize.resize_bottom_whitespace();
 });
 
 exports.process_keypress = function (e) {
