7b1bfd67039f7c26681e27fbc65e305f879b9afe
Steve Howell <showell30@yahoo.com>
2017-03-17 15:39:26 -0700

    Break typeahead_helper's dependency on compose.js.
    We now have typeahead_helper's callers pass in compose.stream()
    when it's needed for sorting purposes.

--- static/js/composebox_typeahead.js
+++ static/js/composebox_typeahead.js
@@ -369,7 +369,7 @@ exports.initialize_compose_typeahead = function (selector, completions) {
             if (this.completing === 'emoji') {
                 return typeahead_helper.sort_emojis(matches, this.token);
             } else if (this.completing === 'mention') {
-                return typeahead_helper.sort_recipients(matches, this.token);
+                return typeahead_helper.sort_recipients(matches, this.token, compose.stream_name());
             } else if (this.completing === 'stream') {
                 return typeahead_helper.sort_streams(matches, this.token);
             }
@@ -477,7 +477,11 @@ exports.initialize = function () {
 
             return query_matches_person(current_recipient, item);
         },
-        sorter: typeahead_helper.sort_recipientbox_typeahead,
+        sorter: function (matches) {
+            var current_stream = compose.stream_name();
+            return typeahead_helper.sort_recipientbox_typeahead(
+                this.query, matches, current_stream);
+        },
         updater: function (item, event) {
             var previous_recipients = exports.get_cleaned_pm_recipients(this.query);
             previous_recipients.pop();
