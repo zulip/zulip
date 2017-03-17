zulip/zulip

commit 7a6d001592e6f933cc30f9d96988f34bb96e5094

signup: Make error message for a weak password more clear.
"Password is weak" sounds like an fyi rather than "we're going to stop you
from registering until you fix this".

static/js/portico/signup.js
@@ -4,7 +4,7 @@ $(function () {
 
     $.validator.addMethod('password_strength', function (value) {
         return password_quality(value, undefined, $('#id_password, #id_new_password1'));
-    }, 'Password is weak.');
+    }, i18n.t("Password is too weak."));
 
     function highlight(class_to_add) {
         // Set a class on the enclosing control group.

commit 871adc01e35c44b9f0c4ef5048649cfedeec6608

signup: Fix broken password strength checking.
We don't support using the i18n libraries in our portico.

This was broken in 7a6d001592e6f933cc30f9d96988f34bb96e5094.

static/js/portico/signup.js
@@ -4,7 +4,7 @@ $(function () {
 
     $.validator.addMethod('password_strength', function (value) {
         return password_quality(value, undefined, $('#id_password, #id_new_password1'));
-    }, i18n.t("Password is too weak."));
+    }, "Password is too weak.");
 
     function highlight(class_to_add) {
         // Set a class on the enclosing control group.