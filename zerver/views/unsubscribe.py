62a580a37f800f1172f51ac4d9a1be16f7f23428
Umair Khan <umair.waheed@gmail.com>
2017-03-17 10:26:23 +0500

    unsubscribe: Change render_to_response to render.
    Related to #4093

--- zerver/views/unsubscribe.py
+++ zerver/views/unsubscribe.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 from django.conf import settings
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 from typing import Callable
 
 from confirmation.models import Confirmation
@@ -12,17 +13,17 @@ from zerver.context_processors import common_context
 from zproject.jinja2 import render_to_response
 
 def process_unsubscribe(request, token, subscription_type, unsubscribe_function):
-    # type: (HttpRequest, str, Callable[[UserProfile], None]) -> HttpResponse
+    # type: (HttpRequest, str, str, Callable[[UserProfile], None]) -> HttpResponse
     try:
         confirmation = Confirmation.objects.get(confirmation_key=token)
     except Confirmation.DoesNotExist:
-        return render_to_response('zerver/unsubscribe_link_error.html')
+        return render(request, 'zerver/unsubscribe_link_error.html')
 
     user_profile = confirmation.content_object
     unsubscribe_function(user_profile)
     context = common_context(user_profile)
     context.update({"subscription_type": subscription_type})
-    return render_to_response('zerver/unsubscribe_success.html', context)
+    return render(request, 'zerver/unsubscribe_success.html', context=context)
 
 # Email unsubscribe functions. All have the function signature
 # processor(user_profile).
@@ -56,5 +57,4 @@ def email_unsubscribe(request, type, token):
         display_name, unsubscribe_function = email_unsubscribers[type]
         return process_unsubscribe(request, token, display_name, unsubscribe_function)
 
-    return render_to_response('zerver/unsubscribe_link_error.html', {},
-                              request=request)
+    return render(request, 'zerver/unsubscribe_link_error.html')
