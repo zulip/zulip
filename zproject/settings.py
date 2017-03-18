ab34b5ee9fb3502da7899cc4e0dba42c9afd8c19
Steve Howell <showell30@yahoo.com>
2017-03-18 12:29:55 -0700

    Extract ui_init.js.

--- zproject/settings.py
+++ zproject/settings.py
@@ -894,6 +894,7 @@ JS_SPECS = {
             'js/bot_data.js',
             'js/reactions.js',
             'js/typing.js',
+            'js/ui_init.js',
             'js/shim.js',
             # JS bundled by webpack is also included here if PIPELINE_ENABLED setting is true
         ],
