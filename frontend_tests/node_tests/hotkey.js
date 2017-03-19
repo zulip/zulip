fb583494e80d972fb475d6543335a3cc16b0d55f
Joshua Pan <joshuapan8@gmail.com>
2017-03-19 04:19:36 +0000

    hotkey.js: Add opening reactions hotkey.
    Add frontend tests for ":" hotkey.
    
    Fixes #3911.

--- frontend_tests/node_tests/hotkey.js
+++ frontend_tests/node_tests/hotkey.js
@@ -15,6 +15,7 @@ var hotkey = require('js/hotkey.js');
 
 set_global('current_msg_list', {
     selected_id: function () { return 42; },
+    selected_message: function () { return {sent_by_me: true, flags: ["read", "starred"]}; },
 });
 
 function return_true() { return true; }
@@ -157,7 +158,7 @@ function stubbing(func_name_to_stub, test_function) {
 
     // Next, test keys that only work on a selected message.
     global.current_msg_list.empty = return_true;
-    assert_unmapped('@rRjJkKsSi');
+    assert_unmapped('@rRjJkKsSi:');
 
     global.current_msg_list.empty = return_false;
 
@@ -172,6 +173,7 @@ function stubbing(func_name_to_stub, test_function) {
     assert_mapping('S', 'narrow.by_subject');
     assert_mapping('v', 'lightbox.show_from_selected_message');
     assert_mapping('i', 'popovers.open_message_menu');
+    assert_mapping(':', 'popovers.toggle_reactions_popover', true);
 }());
 
 (function test_motion_keys() {
