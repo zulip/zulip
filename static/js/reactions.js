bb7fa2a70a67b8dbc23563a9a91fb9c92e7a9608
Joshua Pan <joshuapan8@gmail.com>
2017-03-19 04:10:39 +0000

    Pressing enter in reactions search picks first reaction.

--- static/js/reactions.js
+++ static/js/reactions.js
@@ -73,6 +73,20 @@ function filter_emojis() {
     }
 }
 
+function maybe_select_emoji(e) {
+    var reaction_list = $(".reaction-popover-reaction");
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        var reaction_show_list = reaction_list.filter(function () {
+            return this.style.display === "block";
+        });
+        if (reaction_show_list.length) {
+            $(".reaction-popover-filter").val("");
+            exports.reaction_popover(current_msg_list.selected_id(), reaction_show_list[0].title);
+        }
+    }
+}
+
 $(document).on('click', '.reaction-popover-reaction', function() {
     // When an emoji is clicked in the popover,
     // if the user has reacted to this message with this emoji
@@ -86,6 +100,7 @@ $(document).on('click', '.reaction-popover-reaction', function() {
 });
 
 $(document).on('input', '.reaction-popover-filter', filter_emojis);
+$(document).on('keydown', '.reaction-popover-filter', maybe_select_emoji);
 
 function full_name(user_id) {
     if (user_id === page_params.user_id) {
