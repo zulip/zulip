e7c32b55bf0dd83f034fc85145d49c857b625a3e
Brock Whittaker <brock@zulipchat.com>
2017-03-20 11:37:23 -0700

    Fix up YouTube embed issues and styling.
    This fixes the styling to stay on the screen of most reasonably sized
    monitors along with extending the JavaScript code to allow for the
    video to be keyed to in the lightbox.

--- static/js/lightbox.js
+++ static/js/lightbox.js
@@ -1,81 +1,104 @@
 var lightbox = (function () {
 var exports = {};
 
-var images = [];
 var is_open = false;
+// the asset map is a map of all retrieved images and YouTube videos that are
+// memoized instead of being looked up multiple times.
+var asset_map = {
 
-var get_image_title = function (image) {
-    var image_title = $(image).attr("title");
-    if (image_title) {
-        return image_title;
-    }
-    return $(image).parent().attr("title");
 };
 
-function display_image(image, user) {
+function render_lightbox_list_images(preview_source) {
     if (!is_open) {
-        images = Array.prototype.slice.call($(".focused_table .messagebox-content img"));
+        var images = Array.prototype.slice.call($(".focused_table .messagebox-content img"));
         var $image_list = $("#lightbox_overlay .image-list").html("");
 
         images.forEach(function (img) {
             var src = img.getAttribute("src");
-            var className = $(image).attr("src").match(src) ? "image selected" : "image";
+            var className = preview_source.match(src) ? "image selected" : "image";
 
             var node = $("<div></div>", {
                 class: className,
-                title: get_image_title(img),
-                "data-zid": $(img).closest(".message_row").attr("zid"),
                 "data-src": src,
             }).css({ backgroundImage: "url(" + src + ")"});
 
             $image_list.append(node);
         }, "");
     }
+}
 
-    // image should be an Image Object in JavaScript.
-    var url = $(image).attr("src");
-    var title = get_image_title(image);
+function display_image(payload) {
+    render_lightbox_list_images(payload.preview);
 
-    $("#lightbox_overlay .player-container").hide();
-    $("#lightbox_overlay .image-actions, .image-description, .download").show();
+    $(".player-container").hide();
+    $(".image-actions, .image-description, .download").show();
 
     var img = new Image();
-    img.src = url;
-    $("#lightbox_overlay .image-preview").html("").show()
-        .append(img);
+    img.src = payload.source;
+    $("#lightbox_overlay .image-preview").html(img).show();
 
-    $(".image-description .title").text(title || "N/A");
-    $(".image-description .user").text(user);
+    $(".image-description .title").text(payload.title || "N/A");
+    $(".image-description .user").text(payload.user);
 
-    $(".image-actions .open, .image-actions .download").attr("href", url);
+    $(".image-actions .open, .image-actions .download").attr("href", payload.source);
 }
 
-function display_youtube_video(id) {
+function display_youtube_video(payload) {
+    render_lightbox_list_images(payload.preview);
+
     $("#lightbox_overlay .image-preview, .image-description, .download").hide();
 
-    var iframe = document.createElement("iframe");
-    iframe.width = window.innerWidth;
-    iframe.height = window.innerWidth * 0.5625;
-    iframe.src = "https://www.youtube.com/embed/" + id;
-    iframe.setAttribute("frameborder", 0);
-    iframe.setAttribute("allowfullscreen", true);
+    var iframe = $("<iframe></iframe>", {
+        src: "https://www.youtube.com/embed/" + payload.source,
+        frameborder: 0,
+        allowfullscreen: true,
+    });
 
-    $("#lightbox_overlay .player-container").html("").show().append(iframe);
-    $(".image-actions .open").attr("href", "https://youtu.be/" + id);
+    $("#lightbox_overlay .player-container").html(iframe).show();
+    $(".image-actions .open").attr("href", "https://youtu.be/" + payload.source);
 }
 
-exports.open = function (data) {
-    switch (data.type) {
-        case "photo":
-            display_image(data.image, data.user);
-            is_open = true;
-            break;
-        case "youtube":
-            display_youtube_video(data.id);
-            is_open = true;
-            break;
-        default:
-            break;
+// the image param is optional, but required on the first preview of an image.
+// this will likely be passed in every time but just ignored if the result is already
+// stored in the `asset_map`.
+exports.open = function (image) {
+    var $image = $(image);
+
+    // if wrapped in the .youtube-video class, it will be length = 1, and therefore
+    // cast to true.
+    var is_youtube_video = !!$image.closest(".youtube-video").length;
+
+    var payload;
+    // if the asset_map already contains the metadata required to display the
+    // asset, just recall that metadata.
+    if (asset_map[$image.attr("src")]) {
+        payload = asset_map[$image.attr("src")];
+
+        if (payload.type === "youtube-video") {
+            display_youtube_video(payload);
+        } else if (payload.type === "image") {
+            display_image(payload);
+        }
+    // otherwise retrieve the metadata from the DOM and store into the asset_map.
+    } else {
+        var $parent = $image.parent();
+        var $message = $parent.closest("[zid]");
+
+        payload = {
+            user: message_store.get($message.attr("zid")).sender_full_name,
+            title: $image.parent().attr("title"),
+            type: is_youtube_video ? "youtube-video" : "image",
+            preview: $image.attr("src"),
+            source: is_youtube_video ? $parent.attr("data-id") : $image.attr("src"),
+        };
+
+        asset_map[payload.preview] = payload;
+
+        if (payload.type === "youtube-video") {
+            display_youtube_video(payload);
+        } else if (payload.type === "image") {
+            display_image(payload);
+        }
     }
 
     $("#lightbox_overlay").addClass("show");
@@ -86,24 +109,7 @@ exports.open = function (data) {
 exports.show_from_selected_message = function () {
     var selected_msg = $(".selected_message").find("img");
     if (selected_msg.length !== 0) {
-      exports.show_from_inline_image(selected_msg);
-    }
-};
-
-exports.show_from_inline_image = function ($img) {
-    var zid = rows.id($img.closest(".message_row"));
-    var user = message_store.get(zid).sender_full_name;
-    if ($img.parent().parent().hasClass("youtube-video")) {
-        lightbox.open({
-            type: "youtube",
-            id: $img.data("id"),
-        });
-    } else {
-        lightbox.open({
-            type: "photo",
-            image: $img[0],
-            user: user,
-        });
+      exports.open(selected_msg);
     }
 };
 
@@ -136,7 +142,7 @@ $(function () {
         // prevent the message compose dialog from happening.
         e.stopPropagation();
 
-        lightbox.show_from_inline_image($img);
+        exports.open($img);
     });
 
     $("#lightbox_overlay .download").click(function () {
@@ -145,17 +151,9 @@ $(function () {
 
     $("#lightbox_overlay").on("click", ".image-list .image", function () {
         var $image_list = $(this).parent();
+        var original_image = $(".message_row img[src='" + $(this).attr('data-src') + "']");
 
-        var image = $("<img />", {
-            src: this.dataset.src,
-            title: this.title,
-        });
-
-        lightbox.open({
-            type: "photo",
-            user: message_store.get($(this).attr("data-zid")).sender_full_name,
-            image: image,
-        });
+        exports.open(original_image);
 
         $(".image-list .image.selected").removeClass("selected");
         $(this).addClass("selected");
