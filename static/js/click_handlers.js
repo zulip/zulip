b1b53a8d3a78197c1349bb1d6cfd7777a9519b1d
Raghav Jajodia <jajodia.raghav@gmail.com>
2017-03-22 11:25:35 +0530

    streams: Prevent cursor jump to the end of editing stream-descr.
    This prevents the cursor to jump at the end of the content-editable
    area when the user types in the middle.
    
    Fixes #4202.

--- static/js/click_handlers.js
+++ static/js/click_handlers.js
@@ -468,7 +468,6 @@ $(function () {
             // wrong.
             if (this.hasChildNodes()) {
                 this.innerText = this.innerText;
-                place_caret_at_end(this);
             }
         });
 
