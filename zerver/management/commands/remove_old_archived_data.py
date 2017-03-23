ce055a4d88e6f953263ab4c652fe94016709c2a5
K.Kanakhin <kkanahin@gmail.com>
2017-03-23 22:17:09 +0600

    retention-period: Add `dry-run` option to management commands.
    - Rewrite retention tools with `dry-run` option. Split queries
      to selection part and executable part.
    - Add `dry-run` option to retention managment commands.
    - Add tests for `dry-run` option.
    
    Fixes #106

--- zerver/management/commands/remove_old_archived_data.py
+++ zerver/management/commands/remove_old_archived_data.py
@@ -1,13 +1,31 @@
 from __future__ import absolute_import
+from __future__ import print_function
 
 from typing import Any
 
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandParser
 from zerver.lib.retention import delete_expired_archived_data
 
 
 class Command(BaseCommand):
+    help = """Remove old archived data
+            Usage: ./manage.py [--dry-run|-d] remove_old_archived_data."""
+
+    def add_arguments(self, parser):
+        # type: (CommandParser) -> None
+        parser.add_argument('-d', '--dry-run',
+                            dest='dry_run',
+                            default=False,
+                            action='store_true',
+                            help="Just get removing info.")
 
     def handle(self, *args, **options):
         # type: (*Any, **str) -> None
-        delete_expired_archived_data()
+        dry_run = options['dry_run']
+        deliting_info = delete_expired_archived_data()
+        if dry_run:
+            for info in deliting_info:
+                print("Realm ID: {}".format(info['realm_id']))
+                print("Deleting archived messages qty: {}".format(info['del_arc_user_messages']))
+                print("Deleting archived user messages qty: {}".format(info['del_arc_messages']))
+                print("Deleting archived attachemnts qty: {}".format(info['del_arc_attachments']))
--- zerver/management/commands/restore_messages_by_realm.py
+++ zerver/management/commands/restore_messages_by_realm.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+from __future__ import print_function
 
 from typing import Any
 from argparse import ArgumentParser
@@ -9,13 +10,28 @@ from zerver.models import get_realm
 
 
 class Command(BaseCommand):
+    help = """Restore archived data by realm
+                Usage: ./manage.py [--dry-run|-d] restore_messages_by_realm."""
 
     def add_arguments(self, parser):
         # type: (ArgumentParser) -> None
         parser.add_argument('domain', metavar='<domain>', type=str,
                             help='The domain of the realm to restore messages data to.')
+        parser.add_argument('-d', '--dry-run',
+                            dest='dry_run',
+                            default=False,
+                            action='store_true',
+                            help="Just get restore info.")
 
     def handle(self, *args, **options):
-        # type: (*Any, **str) -> None
+        # type: (*Any, **Any) -> None
         realm = get_realm(options["domain"])
-        restore_realm_archived_data(realm_id=realm.id)
+        dry_run = options['dry_run']
+        restore_info = restore_realm_archived_data(realm_id=realm.id, dry_run=dry_run)
+        if dry_run:
+            print("Restoring messages qty: {}".format(restore_info['restoring_arc_messages']))
+            print("Restoring user messages qty: {}".format(
+                restore_info['restoring_arc_user_messages']))
+            print("Restoring attachemnts qty: {}".format(restore_info['restoring_arc_attachemnts']))
+            print("Restoring attachemnts messages qty: {}".format(
+                restore_info['rest_arc_attachments_messages']))
