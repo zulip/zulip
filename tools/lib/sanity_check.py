21ba288470a30c6fd365eb21f5581b583af965e1
sinwar <premsinwar4@gmail.com>
2017-04-03 15:18:57 +0530

    tools: update warning for venv check

--- tools/lib/sanity_check.py
+++ tools/lib/sanity_check.py
@@ -3,11 +3,17 @@ from __future__ import print_function
 
 import sys
 
+import getpass
+
 def check_venv(filename):
     # type: (str) -> None
     try:
         import ujson
     except ImportError:
         print("You need to run %s inside a Zulip dev environment." % (filename,))
-        print("If you are using Vagrant, you can `vagrant ssh` to enter the Vagrant guest.")
+        username = getpass.getuser()
+        if username != 'vagrant':
+            print("If you are using Vagrant, you can `vagrant ssh` to enter the Vagrant guest.")
+        else:
+            print("You can `source /srv/zulip-venv/bin/activate` to enter dev environment.")
         sys.exit(1)
