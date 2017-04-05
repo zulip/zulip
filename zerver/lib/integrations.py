e5bc3abdbc05611052cb2c95b2c2328cff92e75b
Tomasz Kolek <tomasz-kolek@o2.pl>
2017-04-05 07:06:24 +0200

    integrations doc: Move codebase doc to separate file.

--- zerver/lib/integrations.py
+++ zerver/lib/integrations.py
@@ -176,7 +176,7 @@ WEBHOOK_INTEGRATIONS = [
 INTEGRATIONS = {
     'asana': Integration('asana', 'asana', doc='zerver/integrations/asana.html'),
     'capistrano': Integration('capistrano', 'capistrano', doc='zerver/integrations/capistrano.html'),
-    'codebase': Integration('codebase', 'codebase'),
+    'codebase': Integration('codebase', 'codebase', doc='zerver/integrations/codebase.html'),
     'email': EmailIntegration('email', 'email'),
     'git': Integration('git', 'git'),
     'google-calendar': Integration('google-calendar', 'google-calendar', display_name='Google Calendar'),
