acd986d959fb3fff8acb9dbfdc6d25ddb2df4cab
Tomasz Kolek <tomasz-kolek@o2.pl>
2017-04-11 07:19:35 +0200

    integrations: Fix custom topic of hellosign integration.
    This fixes support for using custom topics in the HelloSign
    integration.
    
    With significant fixes and rebasing by Eeshan Garg.

--- zerver/webhooks/hellosign/tests.py
+++ zerver/webhooks/hellosign/tests.py
@@ -15,6 +15,15 @@ class HelloSignHookTests(WebhookTestCase):
         self.send_and_test_stream_message('signatures', expected_subject, expected_message,
                                           content_type="application/x-www-form-urlencoded")
 
+    def test_signatures_message_with_own_subject(self):
+        # type: () -> None
+        expected_subject = "Our own subject."
+        self.url = self.build_webhook_url(topic=expected_subject)
+        expected_message = ("The NDA with Acme Co. is awaiting the signature of "
+                            "Jack and was just signed by Jill.")
+        self.send_and_test_stream_message('signatures_with_own_subject', expected_subject, expected_message,
+                                          content_type="application/x-www-form-urlencoded", topic=expected_subject)
+
     def get_body(self, fixture_name):
         # type: (Text) -> Text
         return self.fixture_data("hellosign", fixture_name, file_type="json")
