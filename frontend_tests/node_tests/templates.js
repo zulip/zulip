c98cf5ba635ebc69d51041de058a7149e938e95f
Aditya Bansal <adi.bansal241996@gmail.com>
2017-04-15 08:09:47 +0530

    Clean message_edit_history.handlebars to use 4 space indents.

--- frontend_tests/node_tests/templates.js
+++ frontend_tests/node_tests/templates.js
@@ -698,7 +698,7 @@ function render(template_name, args) {
     global.write_test_output("message_edit_history.handlebars", html);
     var edited_message = $(html).find("div.messagebox-content");
     assert.equal(edited_message.text().trim(),
-                "1468132659\n        Let's go to dinner!\n        Edited by Alice");
+                "1468132659\n            Let's go to dinner!\n            Edited by Alice");
 }());
 
 (function message_reaction() {
