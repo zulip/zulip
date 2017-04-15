70c1f00c5ca4fe0a518386d711d7b8e6dd527340
Rishi Gupta <rishig@zulipchat.com>
2017-04-14 20:50:59 -0700

    hotspots: Change hotspots to have a name and a description.

--- zerver/lib/hotspots.py
+++ zerver/lib/hotspots.py
@@ -1,12 +1,17 @@
 from zerver.models import UserProfile, UserHotspot
 
-from typing import List, Text
+from typing import List, Text, Dict
 
-ALL_HOTSPOTS = ['welcome', 'streams', 'topics', 'narrowing', 'replying', 'get_started']
+ALL_HOTSPOTS = {
+    'click_to_reply': 'Click anywhere on a message to reply.',
+    'new_topic_botton': 'Click the "New topic" button to start a new conversation.',
+    'stream_settings': 'Most discussion on Zulip happens in streams. Click here to create or join additional streams.',
+}
+
 def get_next_hotspots(user):
-    # type: (UserProfile) -> List[Text]
+    # type: (UserProfile) -> List[Dict[str, Text]]
     seen_hotspots = frozenset(UserHotspot.objects.filter(user=user).values_list('hotspot', flat=True))
-    for hotspot in ALL_HOTSPOTS:
+    for hotspot in ['click_to_reply', 'new_topic_botton', 'stream_settings']:
         if hotspot not in seen_hotspots:
-            return [hotspot]
+            return [{'name': hotspot, 'description': ALL_HOTSPOTS[hotspot]}]
     return []
