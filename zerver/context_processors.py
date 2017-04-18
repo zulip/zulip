1fe8df10f07e11e1a8268b3d1d2f08da6b88bc6a
Tim Abbott <tabbott@zulipchat.com>
2017-04-17 22:10:41 -0700

    context: Include realm name, icon, and description.
    This will be used in our upcoming login/registration page redesign.

--- zerver/context_processors.py
+++ zerver/context_processors.py
@@ -8,6 +8,8 @@ from zerver.models import UserProfile, get_realm
 from zproject.backends import (password_auth_enabled, dev_auth_enabled,
                                google_auth_enabled, github_auth_enabled)
 from zerver.lib.utils import get_subdomain
+from zerver.lib.realm_icon import get_realm_icon_url
+
 from version import ZULIP_VERSION
 
 def common_context(user):
@@ -18,7 +20,6 @@ def common_context(user):
         'external_uri_scheme': settings.EXTERNAL_URI_SCHEME,
         'external_host': settings.EXTERNAL_HOST,
     }
-
 
 def add_settings(request):
     # type: (HttpRequest) -> Dict[str, Any]
@@ -32,10 +33,17 @@ def add_settings(request):
 
     if realm is not None:
         realm_uri = realm.uri
+        realm_name = realm.name
+        realm_icon = get_realm_icon_url(realm)
+        realm_description = realm.description or "The coolest place in the universe."
     else:
         realm_uri = settings.SERVER_URI
+        realm_name = None
+        realm_icon = None
+        realm_description = None
 
     return {
+        'realms_have_subdomains': settings.REALMS_HAVE_SUBDOMAINS,
         'custom_logo_url': settings.CUSTOM_LOGO_URL,
         'register_link_disabled': settings.REGISTER_LINK_DISABLED,
         'login_link_disabled': settings.LOGIN_LINK_DISABLED,
@@ -51,6 +59,9 @@ def add_settings(request):
         'external_host': settings.EXTERNAL_HOST,
         'external_uri_scheme': settings.EXTERNAL_URI_SCHEME,
         'realm_uri': realm_uri,
+        'realm_name': realm_name,
+        'realm_icon': realm_icon,
+        'realm_description': realm_description,
         'server_uri': settings.SERVER_URI,
         'api_site_required': settings.EXTERNAL_API_PATH != "api.zulip.com",
         'email_integration_enabled': settings.EMAIL_GATEWAY_BOT != "",
