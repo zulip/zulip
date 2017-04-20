422445e0d8291650cf4f1d760375198c667bea63
Brock Whittaker <brock@zulipchat.com>
2017-04-20 12:49:12 -0700

    Change admin default streams list to render progressively.

--- static/js/settings_streams.js
+++ static/js/settings_streams.js
@@ -31,40 +31,24 @@ exports.build_default_stream_table = function (streams_data) {
 
     self.row_dict = new Dict();
 
-    function set_up_remove_click_hander(row, stream_name) {
-        row.on("click", ".remove-default-stream", function (e) {
-            e.preventDefault();
-            e.stopPropagation();
+    var table = $("#admin_default_streams_table").expectOne();
 
-            channel.del({
-                url: '/json/default_streams'+ '?' + $.param({stream_name: stream_name}),
-                error: function (xhr) {
-                    var button = row.find("button");
-                    if (xhr.status.toString().charAt(0) === "4") {
-                        button.closest("td").html(
-                            $("<p>").addClass("text-error").text(JSON.parse(xhr.responseText).msg)
-                        );
-                    } else {
-                        button.text(i18n.t("Failed!"));
-                    }
-                },
-                success: function () {
-                    row.remove();
-                },
-            });
-        });
-    }
+    list_render(table, streams_data, {
+        name: "default_streams_list",
+        modifier: function (item) {
+            var row = $(templates.render("admin_default_streams_list", { stream: item, can_modify: page_params.is_admin }));
+            self.row_dict.set(item.stream_id, row);
+            return row;
+        },
+        filter: {
+            element: table.closest(".settings-section").find(".search"),
+            callback: function (item, value) {
+                return item.name.toLowerCase().match(value);
+            },
+        },
+    }).init();
 
-    (function () {
-        var table = $("#admin_default_streams_table").expectOne();
-        _.each(streams_data, function (stream) {
-            var row = $(templates.render("admin_default_streams_list", {stream: stream, can_modify: page_params.is_admin}));
-            set_up_remove_click_hander(row, stream.name);
-            self.row_dict.set(stream.stream_id, row);
-            table.append(row);
-        });
-        loading.destroy_indicator($('#admin_page_default_streams_loading_indicator'));
-    }());
+    loading.destroy_indicator($('#admin_page_default_streams_loading_indicator'));
 
     self.remove = function (stream_id) {
         if (self.row_dict.has(stream_id)) {
