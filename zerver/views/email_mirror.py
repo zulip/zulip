1f0c4e5fb31e17d3021d0b07d110b5e5bac700c6
Tim Abbott <tabbott@zulipchat.com>
2017-04-24 22:01:48 -0700

    email mirror: Fix mypy annotation error.

--- zerver/views/email_mirror.py
+++ zerver/views/email_mirror.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 import ujson
 
 from django.http import HttpRequest, HttpResponse
+from typing import Dict
 
 from zerver.decorator import internal_notify_view
 from zerver.lib.email_mirror import mirror_email_message
@@ -17,7 +18,7 @@ def email_mirror_message(request, data=REQ(validator=check_dict([
         ('recipient', check_string),
         ('msg_text', check_string),
 ]))):
-    # type: (HttpRequest) -> HttpResponse
+    # type: (HttpRequest, Dict[str, str]) -> HttpResponse
     result = mirror_email_message(ujson.loads(request.POST['data']))
     if result["status"] == "error":
         return json_error(result['msg'])
