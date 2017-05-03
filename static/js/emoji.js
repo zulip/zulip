b20a24875abcf3b2af2c5935b3022f02c479e331
Tim Abbott <tabbott@zulipchat.com>
2017-05-03 13:11:35 -0700

    emoji: Switch to using iamcal sprite sheets for reactions and pickers.
    Without changing how we render emoji in messages or changing the data
    set used for emoji names, this switches us to the superior
    percentage-based system for choosing which emoji from the spritesheet
    to select and the iamcal sprite sheets.
    
    It requires some small changes to CSS to ensure emoji are centered
    properly in the new design.
    
    Based on Harshit Gupta's work on "Interrelated emoji infrastructure changes".

--- static/js/emoji.js
+++ static/js/emoji.js
@@ -19,18 +19,19 @@ var zulip_emoji = {
 
 _.each(emoji_codes.names, function (value) {
     var base_name = emoji_codes.name_to_codepoint[value];
-    default_emojis.push({emoji_name: value, emoji_url: "/static/generated/emoji/images/emoji/unicode/" + base_name + ".png"});
+    default_emojis.push({emoji_name: value,
+                         codepoint: emoji_codes.name_to_codepoint[value],
+                         emoji_url: "/static/generated/emoji/images/emoji/unicode/" + base_name + ".png"});
 });
 
 _.each(emoji_codes.codepoints, function (value) {
-    default_unicode_emojis.push({emoji_name: value, emoji_url: "/static/generated/emoji/images/emoji/unicode/" + value + ".png"});
+    default_unicode_emojis.push({emoji_name: value,
+                                 codepoint: value,
+                                 emoji_url: "/static/generated/emoji/images/emoji/unicode/" + value + ".png"});
 });
 
 exports.emoji_name_to_css_class = function (emoji_name) {
-    if (emoji_name.indexOf("+") >= 0) {
-        return emoji_name.replace("+", "");
-    }
-    return emoji_name;
+    return emoji_codes.name_to_codepoint[emoji_name];
 };
 
 exports.update_emojis = function update_emojis(realm_emojis) {
@@ -50,7 +51,7 @@ exports.update_emojis = function update_emojis(realm_emojis) {
 
     exports.emojis_by_name = {};
     exports.emojis_name_to_css_class = {};
-    _.each(exports.emojis, function (emoji) {
+    _.each(default_emojis, function (emoji) {
         var css_class = exports.emoji_name_to_css_class(emoji.emoji_name);
         exports.emojis_name_to_css_class[emoji.emoji_name] = css_class;
         exports.emojis_by_name[emoji.emoji_name] = emoji.emoji_url;
@@ -65,7 +66,7 @@ exports.initialize = function initialize() {
     // Load the sprite image in the background so that the browser
     // can cache it for later use.
     var sprite = new Image();
-    sprite.src = '/static/generated/emoji/sprite.png';
+    sprite.src = '/static/generated/emoji/sheet_google_32.png';
 };
 
 exports.update_emojis(page_params.realm_emoji);
