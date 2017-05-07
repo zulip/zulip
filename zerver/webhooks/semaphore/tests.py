fd7a6b17239cb7558d61c606157510957cdf765d
Aditya Bansal <adi.bansal241996@gmail.com>
2017-05-07 23:40:12 +0530

    pep8: Add compliance with rule E261 to semaphore/tests.py.

--- zerver/webhooks/semaphore/tests.py
+++ zerver/webhooks/semaphore/tests.py
@@ -12,7 +12,7 @@ class SemaphoreHookTests(WebhookTestCase):
 
     def test_semaphore_build(self):
         # type: () -> None
-        expected_subject = u"knighthood/master" # repo/branch
+        expected_subject = u"knighthood/master"  # repo/branch
         expected_message = u"""[build 314](https://semaphoreci.com/donquixote/knighthood/branches/master/builds/314): passed
 !avatar(don@lamancha.com) [`a490b8d`](https://github.com/donquixote/knighthood/commit/a490b8d508ebbdab1d77a5c2aefa35ceb2d62daf): Create user account for Rocinante :horse:."""
         self.send_and_test_stream_message('build', expected_subject, expected_message,
