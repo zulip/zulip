6871c227cd742ca9cbe888f0b141dad9e2c7d46a
Tim Abbott <tabbott@zulipchat.com>
2017-05-15 21:49:37 -0700

    puppet: Add missing restarts of Nagios on config updates.

--- puppet/zulip_ops/manifests/nagios.pp
+++ puppet/zulip_ops/manifests/nagios.pp
@@ -63,6 +63,7 @@ class zulip_ops::nagios {
     owner      => "root",
     group      => "root",
     source => '/root/zulip/api/integrations/nagios/zulip_nagios.cfg',
+    notify => Service["nagios3"],
   }
 
   $hosts = $zulip_ops::base::hosts
@@ -72,6 +73,7 @@ class zulip_ops::nagios {
     owner      => "root",
     group      => "root",
     content => template('zulip_ops/nagios_autossh.template.erb'),
+    notify => Service["nagios3"],
   }
   file { '/etc/nagios3/zuliprc':
     ensure     => file,
