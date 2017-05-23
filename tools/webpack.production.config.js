3f5d0e69fbf801a59aa367398ea93b51be7bb940
Mehanig <mehanig@gmail.com>
2017-05-23 15:03:53 -0700

    Build and handle jsfiles using webpack instead of django-pipeline.
    Also renames bundle.js to translations.js.

--- tools/webpack.production.config.js
+++ tools/webpack.production.config.js
@@ -1,3 +1,8 @@
 var config = require('./webpack.config.js');
+var BundleTracker = require('webpack-bundle-tracker');
+
+config.devtool = 'source-map';
+config.output.filename = '[name]-[hash].js';
+config.plugins.push(new BundleTracker({filename: 'static/webpack-bundles/webpack-stats-production.json'}));
 
 module.exports = config;
