e1a3e7dcc61b796f55b40d7db89660d531820e5c
Tyler Hallada <tyler@hallada.net>
2017-05-22 21:24:43 -0400

    Add a service worker fallback for notifications
    Still a work-in-progress. What still needs to be done:
    
    * clicking_notification_causes_narrow
    * use Push API so that the service worker can be restarted if killed
    * figure out if the clients.openWindow works
    * remove second `new Notification()` in `process_notification`
    * will it be an issue that https is required for it to work?
    * needs tests

--- static/js/notifications.js
+++ static/js/notifications.js
@@ -30,12 +30,29 @@ if (window.webkitNotifications) {
         },
         requestPermission: window.Notification.requestPermission,
         createNotification: function createNotification(icon, title, content, tag) {
-            var notification_object = new window.Notification(title, {icon: icon,
-                                                                      body: content,
-                                                                      tag: tag});
-            notification_object.show = function () {};
-            notification_object.cancel = function () { notification_object.close(); };
-            return notification_object;
+            try {
+                var notification_object = new window.Notification(title, {icon: icon,
+                                                                          body: content,
+                                                                          tag: tag});
+                notification_object.show = function () {};
+                notification_object.cancel = function () { notification_object.close(); };
+                return notification_object;
+            } catch (e) {
+                if (e.name == 'TypeError') {
+                    // We are probably on Chrome Android where non-persistent notifications are not supported.
+                    // Details: https://github.com/whatwg/notifications/issues/26
+                    // Use a service worker to send the notification instead.
+                    if ('serviceWorker' in navigator) {
+                        return navigator.serviceWorker.register('/static/js/sw.js').then(function(registration) {
+							return registration.showNotification(title, {
+								icon: icon,
+							  	body: content,
+							  	tag: tag
+							});
+                        });
+                    }
+                }
+            }
         },
     };
 }
@@ -55,7 +72,9 @@ function cancel_notification_object(notification_object) {
         // We must remove the .onclose so that it does not trigger on .cancel
         notification_object.onclose = function () {};
         notification_object.onclick = function () {};
-        notification_object.cancel();
+        if ('cancel' in notification_object) {
+            notification_object.cancel();
+        }
 }
 
 exports.initialize = function () {
@@ -358,7 +377,9 @@ function process_notification(notification) {
         notification_object.onclose = function () {
             delete notice_memory[key];
         };
-        notification_object.show();
+        if ('show' in notification_object) {
+            notification_object.show();
+        }
     } else if (notification.webkit_notify === false && typeof Notification !== "undefined" && /mozilla/i.test(navigator.userAgent) === true) {
         Notification.requestPermission(function (perm) {
             if (perm === 'granted') {
