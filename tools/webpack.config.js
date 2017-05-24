b55e2b9e9894c1c75ed0bc1a4c5f5984af2b966c
Mehanig <mehanig@gmail.com>
2017-05-24 15:18:27 -0700

    Migrate api js files to Webpack bundler, inject dev webpack sockets to katex

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -3,7 +3,9 @@ var path = require('path');
 module.exports =  {
     context: path.resolve(__dirname, "../"),
     entry: {
-        translations: ['./static/js/translations.js'],
+        api: './static/js/portico/api.js',
+        katex: ['./node_modules/katex/dist/katex.js'],
+        translations: './static/js/translations.js',
     },
     output: {
         path: path.resolve(__dirname, '../static/webpack-bundles'),
