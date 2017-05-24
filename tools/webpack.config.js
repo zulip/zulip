ecbf25d49fc1a19b9655134bbbbcf164af827f89
Pweaver (Paul Weaver) <paul@mordor.org>
2017-05-24 11:31:00 -0700

    Add typescript support to asset compilation

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -6,10 +6,29 @@ module.exports =  {
     entry: assets,
     module: {
         noParse: /(min)\.js/,
-    },
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+            },
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                loader: "source-map-loader",
+            },
+            {
+                enforce: 'pre',
+                test: /\.tsx?$/,
+                use: "source-map-loader",
+            },
+        ],
+   },
     output: {
         path: path.resolve(__dirname, '../static/webpack-bundles'),
         filename: '[name].js',
     },
     plugins: [],
+    resolve: {
+        extensions: [".tsx", ".ts", ".js", ".json"],
+    },
 };
