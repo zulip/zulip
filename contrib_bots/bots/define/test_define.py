2878f84f7d4fff8a845a27417f57baeaac676dc0
Abhijeet Kaur <abhijeetkaur96@gmail.com>
2017-05-24 08:56:11 +0530

    Test Suite: Edit tests to compare request and response messages.
    Add "bot_response_messages" function to each "test_<bot>.py" file
    so that we can check if we get the desired response or not.
    
    Each message in "request_messages" has a corresponding message in
    "bot_response_messages".

--- contrib_bots/bots/define/test_define.py
+++ contrib_bots/bots/define/test_define.py
@@ -19,11 +19,25 @@ class TestDefineBot(TestCase):
 
     # Messages to be sent to bot for testing.
     # Eventually more test messages can be added.
-    def messages(self):
+    def request_messages(self):
         # type: None -> List[Dict[str, str]]
         messages = []
         message1 = {'content': "foo", 'type': "private", 'sender_email': "foo"}
-        message2 = {'content': "foo", 'type': "stream", 'display_recipient': "foo", 'subject': "foo"}
+        message2 = {'content': "cat", 'type': "stream", 'display_recipient': "foo", 'subject': "foo"}
+        messages.append(message1)
+        messages.append(message2)
+        return messages
+
+    # Reply messages from the test bot.
+    # Each reply message corresponding to each request message.
+    def bot_response_messages(self):
+        # type: None -> List[str]
+        messages = []
+        message1 = "**foo**:\nDefinition not available."
+        message2 = ("**cat**:\n\n* (**noun**) a small domesticated carnivorous mammal "
+                    "with soft fur, a short snout, and retractile claws. It is widely "
+                    "kept as a pet or for catching mice, and many breeds have been "
+                    "developed.\n&nbsp;&nbsp;their pet cat\n\n")
         messages.append(message1)
         messages.append(message2)
         return messages
@@ -32,6 +46,7 @@ class TestDefineBot(TestCase):
         # type: None -> None
         # Edit bot_module to test different bots, the below code can be looped for all the bots.
         bot_module = "./bots/define/define.py"
-        messages = self.messages()
+        messages = self.request_messages()
+        bot_response = self.bot_response_messages()
         test_case = BotTestCase()
-        test_case.bot_test(messages=messages, bot_module=bot_module)
+        test_case.bot_test(messages=messages, bot_module=bot_module, bot_response=bot_response)
--- contrib_bots/bots_test_lib.py
+++ contrib_bots/bots_test_lib.py
@@ -16,19 +16,19 @@ from contrib_bots import bot_lib
 
 class BotTestCase(TestCase):
 
-    def mock_test(self, messages, message_handler):
+    def mock_test(self, messages, message_handler, bot_response):
         # type: (List[Dict[str, str]], Function) -> None
         # Mocking BotHandlerApi
         with patch('contrib_bots.bot_lib.BotHandlerApi') as MockClass:
             instance = MockClass.return_value
 
-            for message in messages:
+            for (message, response) in zip(messages, bot_response):
                 # Send message to the concerned bot
                 message_handler.handle_message(message, MockClass(), StateHandler())
 
                 # Check if BotHandlerApi is sending a reply message.
                 # This can later be modified to assert the contents of BotHandlerApi.send_message
-                assert instance.send_reply.called is True
+                instance.send_reply.assert_called_with(message, response)
 
 
     def bot_to_run(self, bot_module):
@@ -37,7 +37,7 @@ class BotTestCase(TestCase):
         message_handler = lib_module.handler_class()
         return message_handler
 
-    def bot_test(self, messages, bot_module):
+    def bot_test(self, messages, bot_module, bot_response):
 
         message_handler = self.bot_to_run(bot_module)
-        self.mock_test(messages=messages, message_handler=message_handler)
+        self.mock_test(messages=messages, message_handler=message_handler, bot_response=bot_response)
