d3e27cff98f2b0e680cc36a102bc550c3ffc4396
umkay <umaimah.k@gmail.com>
2017-05-25 17:36:54 -0700

    mypy: Fix strict-optional in analytics.

--- analytics/tests/test_counts.py
+++ analytics/tests/test_counts.py
@@ -201,7 +201,7 @@ class TestProcessCountStat(AnalyticsTestCase):
         with self.assertRaises(ValueError):
             process_count_stat(stat, installation_epoch() + 65*self.MINUTE)
         with self.assertRaises(ValueError):
-            process_count_stat(stat, installation_epoch().replace(tzinfo=None) + self.HOUR)
+            process_count_stat(stat, installation_epoch().replace(tzinfo=None) + self.HOUR)  # type: ignore # https://github.com/python/typeshed/pull/1347
 
     # This tests the LoggingCountStat branch of the code in do_delete_counts_at_hour.
     # It is important that do_delete_counts_at_hour not delete any of the collected
