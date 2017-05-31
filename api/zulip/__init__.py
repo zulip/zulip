fc6125ec2ebe45539073ce2844ae6f9ec2b11b73
Eeshan Garg <jerryguitarist@gmail.com>
2017-05-31 02:47:57 -0230

    api/setup.py: Upgrade the version to 0.3.1.
    Apparently, PyPI is very strict about package file names. Once you
    upload files for 0.3.0, and only wish to make minor changes and
    re-release it as the same version, it doesn't let you and complains
    about identical file names.

--- api/zulip/__init__.py
+++ api/zulip/__init__.py
@@ -40,7 +40,7 @@ import logging
 import six
 from typing import Any, Callable, Dict, Iterable, IO, List, Mapping, Optional, Text, Tuple, Union
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 logger = logging.getLogger(__name__)
 
