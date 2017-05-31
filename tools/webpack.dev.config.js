dbb7aa50ef07754073b3a98567e671ce34d1352e
Pweaver (Paul Weaver) <paul@mordor.org>
2017-05-31 00:58:01 -0400

    Fix webpack-dev-server urls for external resources

--- tools/webpack.dev.config.js
+++ tools/webpack.dev.config.js
@@ -1,9 +1,9 @@
 var config = require('./webpack.config.js');
 var BundleTracker = require('webpack-bundle-tracker');
 
-config.entry.translations.push('webpack-dev-server/client?http://0.0.0.0:9991/socket.io');
+config.entry.translations.push('webpack-dev-server/client?/socket.io');
 config.devtool = 'eval';
-config.output.publicPath = 'http://0.0.0.0:9991/webpack/';
+config.output.publicPath = '/webpack/';
 config.plugins.push(new BundleTracker({filename: 'static/webpack-bundles/webpack-stats-dev.json'}));
 
 config.devServer = {
