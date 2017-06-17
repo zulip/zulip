94398848f23588fa5ac2d8f424385fa010171e97
Robert Hönig <robhoenig@gmail.com>
2017-06-17 13:59:28 +0200

    bots: tests: Remove superfluous sys.path insertion.

--- api/bots/encrypt/test_encrypt.py
+++ api/bots/encrypt/test_encrypt.py
@@ -3,14 +3,6 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-import os
-import sys
-
-our_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.normpath(os.path.join(our_dir)))
-# For dev setups, we can find the API in the repo itself.
-if os.path.exists(os.path.join(our_dir, '..')):
-    sys.path.insert(0, '..')
 from bots_test_lib import BotTestCase
 
 class TestEncryptBot(BotTestCase):
--- api/bots/giphy/test_giphy.py
+++ api/bots/giphy/test_giphy.py
@@ -3,15 +3,8 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-import os
-import sys
 import json
 
-our_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.normpath(os.path.join(our_dir)))
-# For dev setups, we can find the API in the repo itself.
-if os.path.exists(os.path.join(our_dir, '..')):
-    sys.path.insert(0, '..')
 from bots_test_lib import BotTestCase
 
 class TestGiphyBot(BotTestCase):
