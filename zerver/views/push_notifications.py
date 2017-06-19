6a3c8ca2c59ac20776dbce1101c7a191154b9082
Anna Liao <aliao22@gmail.com>
2017-06-19 20:09:35 +0000

    push_notifications: Add human users only decorator.
    This is part of #4517.

--- zerver/views/push_notifications.py
+++ zerver/views/push_notifications.py
@@ -9,6 +9,7 @@ from django.conf import settings
 from django.http import HttpRequest, HttpResponse
 from django.utils.translation import ugettext as _
 
+from zerver.decorator import human_users_only
 from zerver.lib.push_notifications import add_push_device_token, \
     b64_to_hex, remove_push_device_token
 from zerver.lib.request import has_request_variables, REQ, JsonableError
@@ -27,6 +28,7 @@ def validate_token(token_str, kind):
         except Exception:
             raise JsonableError(_('Invalid APNS token'))
 
+@human_users_only
 @has_request_variables
 def add_apns_device_token(request, user_profile, token=REQ(),
                           appid=REQ(default=settings.ZULIP_IOS_APP_ID)):
@@ -35,6 +37,7 @@ def add_apns_device_token(request, user_profile, token=REQ(),
     add_push_device_token(user_profile, token, PushDeviceToken.APNS, ios_app_id=appid)
     return json_success()
 
+@human_users_only
 @has_request_variables
 def add_android_reg_id(request, user_profile, token=REQ()):
     # type: (HttpRequest, UserProfile, bytes) -> HttpResponse
@@ -42,6 +45,7 @@ def add_android_reg_id(request, user_profile, token=REQ()):
     add_push_device_token(user_profile, token, PushDeviceToken.GCM)
     return json_success()
 
+@human_users_only
 @has_request_variables
 def remove_apns_device_token(request, user_profile, token=REQ()):
     # type: (HttpRequest, UserProfile, bytes) -> HttpResponse
@@ -49,6 +53,7 @@ def remove_apns_device_token(request, user_profile, token=REQ()):
     remove_push_device_token(user_profile, token, PushDeviceToken.APNS)
     return json_success()
 
+@human_users_only
 @has_request_variables
 def remove_android_reg_id(request, user_profile, token=REQ()):
     # type: (HttpRequest, UserProfile, bytes) -> HttpResponse
