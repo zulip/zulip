7d54e5e56c39ba40ac6c994fe1774e1ccc6ccfe1
neiljp <github@kepier.clara.net>
2017-06-20 11:44:37 -0700

    Cleaner check_expected_responses implementation

--- api/bots/followup/test_followup.py
+++ api/bots/followup/test_followup.py
@@ -9,23 +9,13 @@ class TestFollowUpBot(BotTestCase):
     bot_name = "followup"
 
     def test_bot(self):
-        expected_send_reply = {
-            "": 'Please specify the message you want to send to followup stream after @mention-bot'
-        }
-        self.check_expected_responses(expected_send_reply, expected_method='send_reply')
-
-        expected_send_message = {
-            "foo": {
-                'type': 'stream',
-                'to': 'followup',
-                'subject': 'foo_sender@zulip.com',
-                'content': 'from foo_sender@zulip.com: foo',
-            },
-            "I have completed my task": {
-                'type': 'stream',
-                'to': 'followup',
-                'subject': 'foo_sender@zulip.com',
-                'content': 'from foo_sender@zulip.com: I have completed my task',
-            },
-        }
-        self.check_expected_responses(expected_send_message, expected_method='send_message')
+        followup_response = dict(self.response_template['stream'],
+                                 to = 'followup', subject = 'foo_sender@zulip.com')
+        expected = [
+            ("", ('Please specify the message you want to send to followup stream after @mention-bot',
+                  'send_reply')),
+            ("foo", 'from foo_sender@zulip.com: foo'),
+            ("I have completed my task", 'from foo_sender@zulip.com: I have completed my task'),
+        ]
+        self.check_expected_responses(expected, default_method='send_message',
+                                      default_response_template=followup_response)
