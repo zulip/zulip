0bf8461a9327f8d5303c121dd0997c7fbed0ce54
Aditya Bansal <adi.bansal241996@gmail.com>
2017-06-25 19:11:01 +0530

    emoji.js: Add test coverage for build_emoji_upload_widget func.

---
+++ frontend_tests/node_tests/emoji.js
@@ -0,0 +1,29 @@
+set_global('$', global.make_zjquery());
+set_global('page_params', {});
+set_global('upload_widget', {});
+
+add_dependencies({
+    emoji_codes: 'generated/emoji/emoji_codes.js',
+});
+
+var emoji = require('js/emoji.js');
+
+(function test_build_emoji_upload_widget() {
+    var build_widget_stub = false;
+    upload_widget.build_widget = function (
+        get_file_input,
+        file_name_field,
+        input_error,
+        clear_button,
+        upload_button
+    ) {
+        assert.deepEqual(get_file_input(), $('#emoji_file_input'));
+        assert.deepEqual(file_name_field, $('#emoji-file-name'));
+        assert.deepEqual(input_error, $('#emoji_file_input_error'));
+        assert.deepEqual(clear_button, $('#emoji_image_clear_button'));
+        assert.deepEqual(upload_button, $('#emoji_upload_button'));
+        build_widget_stub = true;
+    };
+    emoji.build_emoji_upload_widget();
+    assert(build_widget_stub);
+}());
