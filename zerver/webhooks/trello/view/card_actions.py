a740639a36eabee199b1ef4c3af372e86af06e8f
Marco Matarazzo <MarcoMatarazzo@users.noreply.github.com>
2017-06-26 04:21:08 +0200

    Added description management, added comment text
    Added description webhook management, currently filling error logs with UnknownUpdateCardAction exception on card description change.
    
    Better comment management, reporting comment text.

--- zerver/webhooks/trello/view/card_actions.py
+++ zerver/webhooks/trello/view/card_actions.py
@@ -18,6 +18,9 @@ SUPPORTED_CARD_ACTIONS = [
 CREATE = u'createCard'
 CHANGE_LIST = u'changeList'
 CHANGE_NAME = u'changeName'
+SET_DESC = u'setDesc'
+CHANGE_DESC = u'changeDesc'
+REMOVE_DESC = u'removeDesc'
 ARCHIVE = u'archiveCard'
 REOPEN = u'reopenCard'
 SET_DUE_DATE = u'setDueDate'
@@ -37,6 +40,9 @@ ACTIONS_TO_MESSAGE_MAPPER = {
     CREATE: u'created {card_url_template}',
     CHANGE_LIST: u'moved {card_url_template} from {old_list} to {new_list}',
     CHANGE_NAME: u'renamed the card from "{old_name}" to {card_url_template}',
+    SET_DESC: u'set description for {card_url_template} to\n>{desc}\n\n',
+    CHANGE_DESC: u'changed description for {card_url_template} from\n>{old_desc}\n\nto\n>{desc}\n\n',
+    REMOVE_DESC: u'removed description from {card_url_template}',
     ARCHIVE: u'archived {card_url_template}',
     REOPEN: u'reopened {card_url_template}',
     SET_DUE_DATE: u'set due date for {card_url_template} to {due_date}',
@@ -48,7 +54,7 @@ ACTIONS_TO_MESSAGE_MAPPER = {
     REMOVE_MEMBER: u'removed {member_name} from {card_url_template}',
     ADD_ATTACHMENT: u'added [{attachment_name}]({attachment_url}) to {card_url_template}',
     ADD_CHECKLIST: u'added the {checklist_name} checklist to {card_url_template}',
-    COMMENT: u'commented on {card_url_template}'
+    COMMENT: u'commented on {card_url_template}\n>{text}\n'
 }
 
 def prettify_date(date_string):
@@ -70,12 +76,18 @@ def get_proper_action(payload, action_type):
             return CHANGE_LIST
         if old_data.get('name'):
             return CHANGE_NAME
+        if old_data.get('desc') == "":
+            return SET_DESC
+        if old_data.get('desc'):
+            if card_data.get('desc') == "":
+                return REMOVE_DESC
+            else:
+                return CHANGE_DESC
         if old_data.get('due', False) is None:
             return SET_DUE_DATE
         if old_data.get('due'):
             if card_data.get('due', False) is None:
                 return REMOVE_DUE_DATE
-
             else:
                 return CHANGE_DUE_DATE
         if old_data.get('closed') is False and card_data.get('closed'):
@@ -147,6 +159,13 @@ def get_managed_member_body(payload, action_type):
     }
     return fill_appropriate_message_content(payload, action_type, data)
 
+def get_comment_body(payload, action_type):
+    # type: (Mapping[str, Any], text_type) -> text_type
+    data = {
+        'text': get_action_data(payload).get('text'),
+    }
+    return fill_appropriate_message_content(payload, action_type, data)
+
 def get_managed_due_date_body(payload, action_type):
     # type: (Mapping[str, Any], Text) -> Text
     data = {
@@ -159,6 +178,21 @@ def get_changed_due_date_body(payload, action_type):
     data = {
         'due_date': prettify_date(get_action_data(payload)['card'].get('due')),
         'old_due_date': prettify_date(get_action_data(payload)['old'].get('due'))
+    }
+    return fill_appropriate_message_content(payload, action_type, data)
+
+def get_managed_desc_body(payload, action_type):
+    # type: (Mapping[str, Any], Text) -> Text
+    data = {
+        'desc': prettify_date(get_action_data(payload)['card'].get('desc'))
+    }
+    return fill_appropriate_message_content(payload, action_type, data)
+
+def get_changed_desc_body(payload, action_type):
+    # type: (Mapping[str, Any], Text) -> Text
+    data = {
+        'desc': prettify_date(get_action_data(payload)['card'].get('desc')),
+        'old_desc': prettify_date(get_action_data(payload)['old'].get('desc'))
     }
     return fill_appropriate_message_content(payload, action_type, data)
 
@@ -197,6 +231,9 @@ ACTIONS_TO_FILL_BODY_MAPPER = {
     CREATE: get_body_by_action_type_without_data,
     CHANGE_LIST: get_updated_card_body,
     CHANGE_NAME: get_renamed_card_body,
+    SET_DESC: get_managed_desc_body,
+    CHANGE_DESC: get_changed_desc_body,
+    REMOVE_DESC: get_body_by_action_type_without_data,
     ARCHIVE: get_body_by_action_type_without_data,
     REOPEN: get_body_by_action_type_without_data,
     SET_DUE_DATE: get_managed_due_date_body,
@@ -208,5 +245,5 @@ ACTIONS_TO_FILL_BODY_MAPPER = {
     REMOVE_MEMBER: get_managed_member_body,
     ADD_ATTACHMENT: get_added_attachment_body,
     ADD_CHECKLIST: get_added_checklist_body,
-    COMMENT: get_body_by_action_type_without_data,
+    COMMENT: get_comment_body,
 }
