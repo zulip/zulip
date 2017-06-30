f5fa692da820fdf7a5048968d998cc2e8e49620e
Alexander Trost <galexrt@googlemail.com>
2017-06-30 14:45:17 +0200

    Updated the create-zulip-admin.sh script to use the new create_user --password flag

--- scripts/create-zulip-admin.sh
+++ scripts/create-zulip-admin.sh
@@ -8,23 +8,6 @@ set +e
 # Doing everything in python, even I never coded in python #YOLO
 sudo -H -u zulip -g zulip bash <<BASH
 /home/zulip/deployments/current/manage.py create_realm --string_id="$ZULIP_USER_DOMAIN" --name="$ZULIP_USER_DOMAIN"
-/home/zulip/deployments/current/manage.py create_user --this-user-has-accepted-the-tos --realm "$ZULIP_USER_DOMAIN" "$ZULIP_USER_EMAIL" "$ZULIP_USER_FULLNAME"
-/usr/bin/expect <<EOF
-set timeout 5
-spawn /bin/bash
-match_max 100000
-expect "$ "
-send -- "/home/zulip/deployments/current/manage.py changepassword $ZULIP_USER_EMAIL"
-expect -exact "/home/zulip/deployments/current/manage.py changepassword $ZULIP_USER_EMAIL"
-send -- " \r"
-expect "Password: "
-send -- "$ZULIP_USER_PASS\r"
-expect -exact "\r
-Password (again): "
-send -- "$ZULIP_USER_PASS\r"
-expect "$ "
-send -- "exit\r"
-expect eof
-EOF
+/home/zulip/deployments/current/manage.py create_user --this-user-has-accepted-the-tos --realm "$ZULIP_USER_DOMAIN" --password "$ZULIP_USER_PASS" "$ZULIP_USER_EMAIL" "$ZULIP_USER_FULLNAME"
 /home/zulip/deployments/current/manage.py knight "$ZULIP_USER_EMAIL" -f
 BASH
