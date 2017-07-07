e01566a41c690d2bf9cd60707a4bc1e25f137e12
Vishnu Ks <hackerkid@vishnuks.com>
2017-07-08 04:15:51 +0530

    bankrupt_users: Replace get_user_profile_by_email with get_user.

--- zerver/management/commands/bankrupt_users.py
+++ zerver/management/commands/bankrupt_users.py
@@ -4,28 +4,30 @@ from __future__ import print_function
 from typing import Any
 
 from argparse import ArgumentParser
-from django.core.management.base import BaseCommand
+from django.core.management.base import CommandError
 
 from zerver.lib.actions import do_update_message_flags
-from zerver.models import UserProfile, Message, get_user_profile_by_email
+from zerver.lib.management import ZulipBaseCommand
+from zerver.models import Message
 
-class Command(BaseCommand):
+class Command(ZulipBaseCommand):
     help = """Bankrupt one or many users."""
 
     def add_arguments(self, parser):
         # type: (ArgumentParser) -> None
         parser.add_argument('emails', metavar='<email>', type=str, nargs='+',
                             help='email address to bankrupt')
+        self.add_realm_args(parser, True)
 
     def handle(self, *args, **options):
         # type: (*Any, **str) -> None
+        realm = self.get_realm(options)
         for email in options['emails']:
             try:
-                user_profile = get_user_profile_by_email(email)
-            except UserProfile.DoesNotExist:
-                print("e-mail %s doesn't exist in the system, skipping" % (email,))
+                user_profile = self.get_user(email, realm)
+            except CommandError:
+                print("e-mail %s doesn't exist in the realm %s, skipping" % (email, realm))
                 continue
-
             do_update_message_flags(user_profile, "add", "read", None, True, None, None)
 
             messages = Message.objects.filter(
