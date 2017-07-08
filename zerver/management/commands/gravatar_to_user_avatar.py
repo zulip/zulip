cee15b992cc25bb73990b31e9da24d8fad26960e
Vishnu Ks <hackerkid@vishnuks.com>
2017-07-08 23:42:48 +0530

    gravatar_to_user_avatar: Replace get_user_profile_by_email with get_user.

--- zerver/management/commands/gravatar_to_user_avatar.py
+++ zerver/management/commands/gravatar_to_user_avatar.py
@@ -4,13 +4,13 @@ from typing import Any
 
 from argparse import ArgumentParser
 import requests
-from zerver.models import get_user_profile_by_email, UserProfile
+from zerver.models import UserProfile
 from zerver.lib.avatar_hash import gravatar_hash
+from zerver.lib.management import ZulipBaseCommand
 from zerver.lib.upload import upload_avatar_image
-from django.core.management.base import BaseCommand, CommandError
 from django.core.files.uploadedfile import SimpleUploadedFile
 
-class Command(BaseCommand):
+class Command(ZulipBaseCommand):
     help = """Migrate the specified user's Gravatar over to an avatar that we serve.  If two
 email addresses are specified, use the Gravatar for the first and upload the image
 for both email addresses."""
@@ -21,6 +21,7 @@ for both email addresses."""
                             help="user whose Gravatar should be migrated")
         parser.add_argument('new_email', metavar='<new email>', type=str, nargs='?', default=None,
                             help="user to copy the Gravatar to")
+        self.add_realm_args(parser)
 
     def handle(self, *args, **options):
         # type: (*Any, **str) -> None
@@ -31,24 +32,19 @@ for both email addresses."""
         else:
             new_email = old_email
 
+        realm = self.get_realm(options)
         gravatar_url = "https://secure.gravatar.com/avatar/%s?d=identicon" % (gravatar_hash(old_email),)
         gravatar_data = requests.get(gravatar_url).content
         gravatar_file = SimpleUploadedFile('gravatar.jpg', gravatar_data, 'image/jpeg')
 
-        try:
-            user_profile = get_user_profile_by_email(old_email)
-            upload_avatar_image(gravatar_file, user_profile, user_profile)
-            user_profile.avatar_source = UserProfile.AVATAR_FROM_USER
-            user_profile.save(update_fields=['avatar_source'])
-        except UserProfile.DoesNotExist:
-            raise CommandError("Could not find specified user for email %s" % (old_email))
+        user_profile = self.get_user(old_email, realm)
+        upload_avatar_image(gravatar_file, user_profile, user_profile)
+        user_profile.avatar_source = UserProfile.AVATAR_FROM_USER
+        user_profile.save(update_fields=['avatar_source'])
 
         if old_email != new_email:
             gravatar_file.seek(0)
-            try:
-                user_profile = get_user_profile_by_email(new_email)
-                upload_avatar_image(gravatar_file, user_profile, user_profile)
-                user_profile.avatar_source = UserProfile.AVATAR_FROM_USER
-                user_profile.save(update_fields=['avatar_source'])
-            except UserProfile.DoesNotExist:
-                raise CommandError("Could not find specified user for email %s" % (new_email))
+            user_profile = self.get_user(new_email, realm)
+            upload_avatar_image(gravatar_file, user_profile, user_profile)
+            user_profile.avatar_source = UserProfile.AVATAR_FROM_USER
+            user_profile.save(update_fields=['avatar_source'])
