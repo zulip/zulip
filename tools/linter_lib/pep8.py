b9bffaa446f7b9ac5a9eec32d33dc77723c65df1
Aditya Bansal <adi.bansal241996@gmail.com>
2017-07-09 05:24:38 +0530

    pep8: Add compliance with rule E261 to prod_settings_template.py.

--- tools/linter_lib/pep8.py
+++ tools/linter_lib/pep8.py
@@ -92,7 +92,6 @@ def check_pep8(files):
         'zerver/tornado/websocket_client.py',
         'zerver/worker/queue_processors.py',
         'zilencer/management/commands/populate_db.py',
-        'zproject/prod_settings_template.py',
     ]
 
     filtered_files = [fn for fn in files if fn not in IGNORE_FILES_PEPE261]
