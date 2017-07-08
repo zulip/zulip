2da6c5b2ce7204ea847600a426a1251cad7eab94
Vishnu Ks <hackerkid@vishnuks.com>
2017-07-08 17:36:01 +0530

    turn_off_digests: Replace get_user_profile_by_email with get_user.

--- zerver/management/commands/turn_off_digests.py
+++ zerver/management/commands/turn_off_digests.py
@@ -3,43 +3,38 @@ from __future__ import print_function
 
 from typing import Any
 
-from optparse import make_option
-
-from django.core.management.base import BaseCommand, CommandParser
+from django.core.management.base import CommandParser
 
 from zerver.lib.actions import do_change_notification_settings
-from zerver.models import Realm, UserProfile, get_realm, get_user_profile_by_email
+from zerver.lib.management import ZulipBaseCommand
+from zerver.models import UserProfile
 
-class Command(BaseCommand):
+class Command(ZulipBaseCommand):
     help = """Turn off digests for a subdomain/string_id or specified set of email addresses."""
 
     def add_arguments(self, parser):
         # type: (CommandParser) -> None
-        parser.add_argument('-r', '--realm',
-                            dest='string_id',
-                            type=str,
-                            help='Turn off digests for all users in this realm.')
-
         parser.add_argument('-u', '--users',
                             dest='users',
                             type=str,
                             help='Turn off digests for this comma-separated '
                                  'list of email addresses.')
+        self.add_realm_args(parser)
 
     def handle(self, **options):
         # type: (**str) -> None
-        if options["string_id"] is None and options["users"] is None:
+        realm = self.get_realm(options)
+        if realm is None and options["users"] is None:
             self.print_help("./manage.py", "turn_off_digests")
             exit(1)
 
-        if options["string_id"]:
-            realm = get_realm(options["string_id"])
+        if realm and not options["users"]:
             user_profiles = UserProfile.objects.filter(realm=realm)
         else:
             emails = set([email.strip() for email in options["users"].split(",")])
             user_profiles = []
             for email in emails:
-                user_profiles.append(get_user_profile_by_email(email))
+                user_profiles.append(self.get_user(email, realm))
 
         print("Turned off digest emails for:")
         for user_profile in user_profiles:
