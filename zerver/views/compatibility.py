07255d49d5c713a93d574aa7a742138520c4486a
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2017-07-07 21:42:30 -0700

    mypy: Add check for parse_user_agent returning None in compatibility.py.

--- zerver/views/compatibility.py
+++ zerver/views/compatibility.py
@@ -9,6 +9,6 @@ from zerver.lib.user_agent import parse_user_agent
 def check_compatibility(request):
     # type: (HttpRequest) -> HttpResponse
     user_agent = parse_user_agent(request.META["HTTP_USER_AGENT"])
-    if user_agent['name'] == "ZulipInvalid":
+    if user_agent is None or user_agent['name'] == "ZulipInvalid":
         return json_error("Client is too old")
     return json_success()
