b0b809b1951d7d24ee9ffd4f2b8432a79c08fba2
Vishnu Ks <hackerkid@vishnuks.com>
2017-07-18 17:28:16 +0000

    zilencer: Eliminate get_user_profile_by_email from print_initial_password.

--- zilencer/management/commands/print_initial_password.py
+++ zilencer/management/commands/print_initial_password.py
@@ -4,11 +4,10 @@ from __future__ import print_function
 from typing import Any
 
 from argparse import ArgumentParser
-from django.core.management.base import BaseCommand
 from zerver.lib.initial_password import initial_password
-from zerver.models import get_user_profile_by_email
+from zerver.lib.management import ZulipBaseCommand
 
-class Command(BaseCommand):
+class Command(ZulipBaseCommand):
     help = "Print the initial password and API key for accounts as created by populate_db"
 
     fmt = '%-30s %-16s  %-32s'
@@ -17,12 +16,14 @@ class Command(BaseCommand):
         # type: (ArgumentParser) -> None
         parser.add_argument('emails', metavar='<email>', type=str, nargs='*',
                             help="email of user to show password and API key for")
+        self.add_realm_args(parser)
 
     def handle(self, *args, **options):
         # type: (*Any, **str) -> None
+        realm = self.get_realm(options)
         print(self.fmt % ('email', 'password', 'API key'))
         for email in options['emails']:
             if '@' not in email:
                 print('ERROR: %s does not look like an email address' % (email,))
                 continue
-            print(self.fmt % (email, initial_password(email), get_user_profile_by_email(email).api_key))
+            print(self.fmt % (email, initial_password(email), self.get_user(email, realm).api_key))
