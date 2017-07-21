758dbec9e24dc96edd586fa2514296a81fbdb064
Umair Khan <umair.waheed@gmail.com>
2017-07-21 09:55:25 +0500

    tornado: Move setup_tornado_rabbitmq to application.py

--- zerver/management/commands/runtornado.py
+++ zerver/management/commands/runtornado.py
@@ -17,8 +17,8 @@ from tornado.log import app_log
 from typing import Callable
 
 from zerver.lib.debug import interactive_debug_listen
-from zerver.lib.queue import setup_tornado_rabbitmq
-from zerver.tornado.application import create_tornado_application
+from zerver.tornado.application import create_tornado_application, \
+    setup_tornado_rabbitmq
 from zerver.tornado.event_queue import add_client_gc_hook, \
     missedmessage_hook, process_notification, setup_event_queue
 from zerver.tornado.socket import respond_send_message
--- zerver/tornado/application.py
+++ zerver/tornado/application.py
@@ -1,12 +1,22 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
+import atexit
+
 from django.conf import settings
 
 from zerver.tornado.handlers import AsyncDjangoHandler
 from zerver.tornado.socket import get_sockjs_router
+from zerver.lib.queue import get_queue_client
 
 import tornado.web
+
+def setup_tornado_rabbitmq():
+    # type: () -> None
+    # When tornado is shut down, disconnect cleanly from rabbitmq
+    if settings.USING_RABBITMQ:
+        queue_client = get_queue_client()
+        atexit.register(lambda: queue_client.close())
 
 def create_tornado_application():
     # type: () -> tornado.web.Application
