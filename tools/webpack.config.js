3ab95e454d32ffbf5be5db288e274e2dd5dbaca8
Pweaver (Paul Weaver) <paul@mordor.org>
2017-07-28 16:53:37 -0400

    Merge webpack configuration into one file.

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -1,8 +1,12 @@
 var path = require('path');
 var assets = require('./webpack.assets.json');
+var BundleTracker = require('webpack-bundle-tracker');
+var webpack = require('webpack');
 
-module.exports = function(env) {
-    config = {
+
+module.exports = function (env) {
+    var production = env === "production";
+    var config = {
         context: path.resolve(__dirname, "../"),
         entry: assets,
         module: {
@@ -13,8 +17,8 @@ module.exports = function(env) {
                     loader: 'ts-loader',
                 },
                 // This loads and transforms sourcemap files from other compiliers.
-                // The typescript comilier will generate a sourcemap and source-map-loaded will output
-                // the correct sourcemap from that.
+                // The typescript comilier will generate a sourcemap and
+                // source-map-loader will output the correct sourcemap from that.
                 {
                     enforce: 'pre',
                     test: /\.js$/,
@@ -54,12 +58,48 @@ module.exports = function(env) {
         },
         output: {
             path: path.resolve(__dirname, '../static/webpack-bundles'),
-            filename: '[name].js',
+            filename: production ? '[name]-[hash].js' : '[name].js',
         },
-        plugins: [],
         resolve: {
             extensions: [".tsx", ".ts", ".js", ".json"],
         },
+        devtool: production ? 'source-map' : 'eval',
     };
+    if (production) {
+        config.plugins = [
+            new BundleTracker({filename: 'webpack-stats-production.json'}),
+        ];
+    } else {
+        // Built webpack dev asset reloader
+        config.entry.common.unshift('webpack/hot/dev-server');
+        // Use 0.0.0.0 so that we can set a port but still use the host
+        // the browser is connected to.
+        config.entry.common.unshift('webpack-dev-server/client?http://0.0.0.0:9994');
+
+        // Out JS debugging tools
+        config.entry.common.push('./static/js/debug.js');
+
+        config.output.publicPath = '/webpack/';
+        config.plugins = [
+            new BundleTracker({filename: 'var/webpack-stats-dev.json'}),
+            // Hot Reload of code in development
+            new webpack.HotModuleReplacementPlugin(),
+            // Better logging from console for hot reload
+            new webpack.NamedModulesPlugin(),
+            // script-loader should load sourceURL in dev
+            new webpack.LoaderOptionsPlugin({debug: true}),
+        ];
+
+        config.devServer = {
+            clientLogLevel: "warning",
+            hot: true,
+            inline: false,
+            stats: "errors-only",
+            watchOptions: {
+                aggregateTimeout: 300,
+                poll: 1000,
+            },
+        };
+    }
     return config;
 };
