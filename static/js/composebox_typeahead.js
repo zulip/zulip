zulip/zulip

commit 3f60074c338bc162e197e52ef04fe3a7e9bb8e26

emoji: Create tests to check the typeahead resuts.
Also, this commit exports the typeahead matcher and sorter functions.
Fixes #4126.

static/js/composebox_typeahead.js
@@ -378,6 +378,31 @@ exports.content_typeahead_selected = function (item) {
     return beginning + rest;
 };
 
+exports.compose_content_matcher = function (item) {
+    if (this.completing === 'emoji') {
+        return query_matches_emoji(this.token, item);
+    } else if (this.completing === 'mention') {
+        return query_matches_person(this.token, item);
+    } else if (this.completing === 'stream') {
+        return query_matches_stream(this.token, item);
+    } else if (this.completing === 'syntax') {
+        return query_matches_language(this.token, item);
+    }
+};
+
+exports.compose_matches_sorter = function (matches) {
+    if (this.completing === 'emoji') {
+        return typeahead_helper.sort_emojis(matches, this.token);
+    } else if (this.completing === 'mention') {
+        return typeahead_helper.sort_recipients(matches, this.token,
+                                                compose_state.stream_name());
+    } else if (this.completing === 'stream') {
+        return typeahead_helper.sort_streams(matches, this.token);
+    } else if (this.completing === 'syntax') {
+        return typeahead_helper.sort_languages(matches, this.token);
+    }
+};
+
 exports.initialize_compose_typeahead = function (selector, completions) {
     completions = $.extend(
         {mention: false, emoji: false, stream: false, syntax: false}, completions);
@@ -388,30 +413,8 @@ exports.initialize_compose_typeahead = function (selector, completions) {
         fixed: true,
         source: exports.compose_content_begins_typeahead,
         highlighter: exports.content_highlighter,
-        matcher: function (item) {
-            if (this.completing === 'emoji') {
-                return query_matches_emoji(this.token, item);
-            } else if (this.completing === 'mention') {
-                return query_matches_person(this.token, item);
-            } else if (this.completing === 'stream') {
-                return query_matches_stream(this.token, item);
-            } else if (this.completing === 'syntax') {
-                return query_matches_language(this.token, item);
-            }
-        },
-        sorter: function (matches) {
-            if (this.completing === 'emoji') {
-                return typeahead_helper.sort_emojis(matches, this.token);
-            } else if (this.completing === 'mention') {
-                return typeahead_helper.sort_recipients(matches, this.token,
-                                                        compose_state.stream_name(),
-                                                        compose_state.subject());
-            } else if (this.completing === 'stream') {
-                return typeahead_helper.sort_streams(matches, this.token);
-            } else if (this.completing === 'syntax') {
-                return typeahead_helper.sort_languages(matches, this.token);
-            }
-        },
+        matcher: exports.compose_content_matcher,
+        sorter: exports.compose_matches_sorter,
         updater: exports.content_typeahead_selected,
         stopAdvance: true, // Do not advance to the next field on a tab or enter
         completions: completions,

frontend_tests/node_tests/composebox_typeahead.js
@@ -874,3 +874,32 @@ global.people.add(deactivated_user);
     assert(th_render_stream_called);
     assert(th_render_typeahead_item_called);
 }());
+
+(function test_typeahead_results() {
+    function compose_typeahead_results(completing,items,token) {
+        // items -> emoji array, token -> simulates text in input
+        var matcher = ct.compose_content_matcher.bind({completing: completing, token: token});
+        var sorter = ct.compose_matches_sorter.bind({completing: completing, token: token});
+        var matches = [];
+        _.each(items, function (item) {
+            if (matcher(item)) {
+                matches.push(item);
+            }
+        });
+        var sorted_matches = sorter(matches);
+        return sorted_matches;
+    }
+
+    function assert_emoji_matches(input, expected) {
+        var returned = compose_typeahead_results('emoji', emoji_list, input);
+        assert.deepEqual(returned, expected);
+    }
+
+    assert_emoji_matches('da',[{emoji_name: "tada", emoji_url: "TBD"},
+        {emoji_name: "panda_face", emoji_url: "TBD"}]);
+    assert_emoji_matches('da_', [{emoji_name: "panda_face", emoji_url: "TBD"}]);
+    assert_emoji_matches('da ', [{emoji_name: "panda_face", emoji_url: "TBD"}]);
+    assert_emoji_matches('japanese_post_', [{emoji_name: "japanese_post_office", emoji_url: "TBD"}]);
+    assert_emoji_matches('japanese post ', [{emoji_name: "japanese_post_office", emoji_url: "TBD"}]);
+    assert_emoji_matches('notaemoji', []);
+}());

commit e729814cfde23686bf7085ef463963a0820cf2b9

recent_senders: Fix exception in certain typeahead flows.
It appears that a regression introduced in
3f60074c338bc162e197e52ef04fe3a7e9bb8e26 caused undefined to be passed
as the subject to the recent_senders library much more often; this
fixing that, and makes the library handle such cases reasonably
without an exception regardless.

This was causing a huge number of "Tried to call a Dict method with an
undefined key." exceptions.

static/js/composebox_typeahead.js
@@ -395,7 +395,8 @@ exports.compose_matches_sorter = function (matches) {
         return typeahead_helper.sort_emojis(matches, this.token);
     } else if (this.completing === 'mention') {
         return typeahead_helper.sort_recipients(matches, this.token,
-                                                compose_state.stream_name());
+                                                compose_state.stream_name(),
+                                                compose_state.subject());
     } else if (this.completing === 'stream') {
         return typeahead_helper.sort_streams(matches, this.token);
     } else if (this.completing === 'syntax') {

static/js/recent_senders.js
@@ -22,7 +22,7 @@ exports.compare_by_recency = function (user_a, user_b, stream_id, topic) {
     stream_id = stream_id.toString();
 
     var topic_dict = senders.get(stream_id);
-    if (topic_dict !== undefined) {
+    if (topic !== undefined && topic_dict !== undefined) {
         var sender_timestamps = topic_dict.get(topic);
         if (sender_timestamps !== undefined) {
             var b_timestamp = sender_timestamps.get(user_b.user_id) || Number.NEGATIVE_INFINITY;
@@ -30,6 +30,10 @@ exports.compare_by_recency = function (user_a, user_b, stream_id, topic) {
             return b_timestamp - a_timestamp;
         }
     }
+
+    // TODO: We should also compare by stream (and maybe also by
+    // overall message recency in the realm) in the common case where
+    // there's no traffic on the current topic from this user.
 
     return 0;
 };

frontend_tests/node_tests/recent_senders.js
@@ -35,6 +35,9 @@ var _ = require('node_modules/underscore/underscore.js');
     assert.equal(
         rs.compare_by_recency({user_id: sender1}, {user_id: sender2}, stream2, topic1) > 0,
         true);
+    assert.equal(
+        rs.compare_by_recency({user_id: sender1}, {user_id: sender2}, stream2, undefined) === 0,
+        true);
 
     // New topic
     var message3 = {