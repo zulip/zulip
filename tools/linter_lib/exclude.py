5784724d00b90199ee23a309bac09f45c9479511
Greg Price <gnprice@gmail.com>
2017-07-31 16:06:30 -0700

    api: Update lint and analysis code to forget about now-gone files.
    These files don't exist in this repository anymore, so there's
    no need to mention them in exclude lists for lint and mypy.

--- tools/linter_lib/exclude.py
+++ tools/linter_lib/exclude.py
@@ -1,7 +1,6 @@
 # Exclude some directories and files from lint checking
 EXCLUDED_FILES = [
     # Third-party code that doesn't match our style
-    "api/integrations/perforce/git_p4.py",
     "puppet/apt/.forge-release",
     "puppet/apt/README.md",
     "static/third",
