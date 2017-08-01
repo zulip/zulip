92e2d61037b76fc61fde261d608735e1c0807fff
Jack Zhang <jack@zulipchat.com>
2017-07-31 18:11:47 -0700

    urls: Add wildcard to IntegrationView routes after /integrations.

--- zproject/urls.py
+++ zproject/urls.py
@@ -139,9 +139,9 @@ i18n_urls = [
     # API and integrations documentation
     url(r'^api/$', APIView.as_view(template_name='zerver/api.html')),
     url(r'^api/endpoints/$', zerver.views.integrations.api_endpoint_docs, name='zerver.views.integrations.api_endpoint_docs'),
-    url(r'^integrations/+$', IntegrationView.as_view()),
     url(r'^integrations/doc-html/(?P<integration_name>[^/]*)$', zerver.views.integrations.integration_doc,
         name="zerver.views.integrations.integration_doc"),
+    url(r'^integrations/(.*)', IntegrationView.as_view()),
     url(r'^about/$', TemplateView.as_view(template_name='zerver/about.html')),
     url(r'^apps/$', zerver.views.home.apps_view, name='zerver.views.home.apps_view'),
 
