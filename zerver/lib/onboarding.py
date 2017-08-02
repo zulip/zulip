dfa71e71d99fb41f1c96efabff8eea8e0fadf175
Rishi Gupta <rishig@zulipchat.com>
2017-08-01 21:02:06 -0700

    onboarding: Add turtle reaction to an initial message.

--- zerver/lib/onboarding.py
+++ zerver/lib/onboarding.py
@@ -2,8 +2,9 @@ from django.conf import settings
 
 from zerver.lib.actions import set_default_streams, bulk_add_subscriptions, \
     internal_prep_stream_message, internal_send_private_message, \
-    create_stream_if_needed, create_streams_if_needed, do_send_messages
-from zerver.models import Realm, UserProfile, Reaction, get_system_bot
+    create_stream_if_needed, create_streams_if_needed, do_send_messages, \
+    do_add_reaction
+from zerver.models import Realm, UserProfile, Message, Reaction, get_system_bot
 
 from typing import Any, Dict, List, Mapping, Text
 
@@ -44,6 +45,7 @@ def setup_initial_private_stream(user):
 
 def send_initial_realm_messages(realm):
     # type: (Realm) -> None
+    welcome_bot = get_system_bot(settings.WELCOME_BOT)
     # Make sure each stream created in the realm creation process has at least one message below
     # Order corresponds to the ordering of the streams on the left sidebar, to make the initial Home
     # view slightly less overwhelming
@@ -78,6 +80,10 @@ def send_initial_realm_messages(realm):
          "or two words will do it!"},
     ]  # type: List[Dict[str, Text]]
     messages = [internal_prep_stream_message(
-        realm, get_system_bot(settings.WELCOME_BOT),
+        realm, welcome_bot,
         message['stream'], message['topic'], message['content']) for message in welcome_messages]
-    do_send_messages(messages)
+    message_ids = do_send_messages(messages)
+
+    turtle_message = Message.objects.filter(
+        id__in=message_ids, subject='topic demonstration', content__startswith='A second').first()
+    do_add_reaction(welcome_bot, turtle_message, 'turtle')
