d9c1ad5db2f50d2221ccfec68a0d8c7710d3a92b
Jack Zhang <jack@zulipchat.com>
2017-08-01 22:23:27 -0700

    hotspots.py: Add SEND_ALL flag.
    Used to view all hotspots in development.

--- zerver/lib/hotspots.py
+++ zerver/lib/hotspots.py
@@ -2,6 +2,8 @@ from zerver.models import UserProfile, UserHotspot
 
 from typing import List, Text, Dict
 
+SEND_ALL = False
+
 ALL_HOTSPOTS = {
     # TODO: Tag these for translation once we've finalized the content.
     'click_to_reply': {
@@ -20,6 +22,19 @@ ALL_HOTSPOTS = {
 
 def get_next_hotspots(user):
     # type: (UserProfile) -> List[Dict[str, object]]
+
+    if SEND_ALL:
+        result = []
+        for hotspot in ALL_HOTSPOTS:
+            result.append({
+                'name': hotspot,
+                'title': ALL_HOTSPOTS[hotspot]['title'],
+                'description': ALL_HOTSPOTS[hotspot]['description'],
+                'delay': 5,
+            })
+
+        return result
+
     seen_hotspots = frozenset(UserHotspot.objects.filter(user=user).values_list('hotspot', flat=True))
     for hotspot in ['click_to_reply', 'new_topic_button', 'stream_settings']:
         if hotspot not in seen_hotspots:
@@ -29,5 +44,4 @@ def get_next_hotspots(user):
                 'description': ALL_HOTSPOTS[hotspot]['description'],
                 'delay': 5,
             }]
-
     return []
