279af33ea2a50b54bc03ef54b77d154610dcd473
Vishnu Ks <hackerkid@vishnuks.com>
2017-08-07 20:54:52 +0530

    management: Use self.get_realm in deactivate_realm.

--- zerver/management/commands/deactivate_realm.py
+++ zerver/management/commands/deactivate_realm.py
@@ -3,28 +3,24 @@ from __future__ import print_function
 
 from typing import Any
 
-from django.core.management.base import BaseCommand
-
 from argparse import ArgumentParser
-import sys
 
 from zerver.lib.actions import do_deactivate_realm
-from zerver.models import get_realm
+from zerver.lib.management import ZulipBaseCommand
 
-class Command(BaseCommand):
+class Command(ZulipBaseCommand):
     help = """Script to deactivate a realm."""
 
     def add_arguments(self, parser):
         # type: (ArgumentParser) -> None
-        parser.add_argument('string_id', metavar='<string_id>', type=str,
-                            help='string_id of realm to deactivate')
+        self.add_realm_args(parser, True)
 
     def handle(self, *args, **options):
         # type: (*Any, **str) -> None
-        realm = get_realm(options["string_id"])
-        if realm is None:
-            print("Could not find realm %s" % (options["string_id"],))
-            sys.exit(1)
-        print("Deactivating", options["string_id"])
+        realm = self.get_realm(options)
+        if realm.deactivated:
+            print("The realm", options["realm_id"], "is already deactivated.")
+            exit(0)
+        print("Deactivating", options["realm_id"])
         do_deactivate_realm(realm)
         print("Done!")
