b8dacf9bf6c7c36229bf681f3ebfeeefe841536d
Vishnu Ks <hackerkid@vishnuks.com>
2017-08-07 17:57:00 +0000

    management: Remove unused import from checkconfig command.

--- zerver/management/commands/checkconfig.py
+++ zerver/management/commands/checkconfig.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-from optparse import make_option
 from typing import Any
 
 from django.core.management.base import BaseCommand
