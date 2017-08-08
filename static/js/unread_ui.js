274938bf5fb0eef347924e53634d4472ebc83a72
Steve Howell <showell@zulipchat.com>
2017-08-08 19:28:58 -0400

    Eliminate approximate_unread_count().
    We use page_params.unread_msgs.count in the web
    app instead.
    
    Fixes #1300.

--- static/js/unread_ui.js
+++ static/js/unread_ui.js
@@ -82,7 +82,7 @@ function consider_bankruptcy() {
     }
 
     var now = new XDate(true).getTime() / 1000;
-    if ((page_params.unread_count > 500) &&
+    if ((page_params.unread_msgs.count > 500) &&
         (now - page_params.furthest_read_time > 60 * 60 * 24 * 2)) { // 2 days.
         var unread_info = templates.render('bankruptcy_modal',
                                            {unread_count: page_params.unread_count});
