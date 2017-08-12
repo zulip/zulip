2e5ee2deeff01a414472690b55e1985fc6a393ac
Steve Howell <showell@zulipchat.com>
2017-08-12 16:51:06 -0400

    Make tab styling more compact and clean.

--- static/js/tab_bar.js
+++ static/js/tab_bar.js
@@ -129,19 +129,13 @@ exports.colorize_tab_bar = function () {
                            colorspace.getDecimalColor(color_for_stream), 0.2));
 
         if (stream_tab.hasClass("stream")) {
-            stream_tab.css('border-left-color',
-                           color_for_stream).css('background-color',
-                                                 color_for_stream);
+            stream_tab.css('background-color', color_for_stream);
             if (stream_tab.hasClass("inactive")) {
               stream_tab.hover (
                 function () {
-                 $(this).css('border-left-color',
-                             stream_light).css('background-color',
-                             stream_light);
+                 $(this).css('background-color', stream_light);
                 }, function () {
-                 $(this).css('border-left-color',
-                             color_for_stream).css('background-color',
-                                                   color_for_stream);
+                 $(this).css('background-color', color_for_stream);
                 }
               );
             }
