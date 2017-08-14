5f87037bf58c6bd44f6c66ac4e97f9b0d1360015
Tim Abbott <tabbott@zulipchat.com>
2017-08-14 12:05:40 -0700

    Fix upwards scrollback when narrowing to streams centered on old messages.
    Previously, when you switched to a stream narrow with the central
    message outside the range of messages cached in the browser, we would
    reset the UI for loading more messages, but not actually reset the
    state for whether it should be possible.
    
    This seems to have been an oversight in refactoring back in 2014.
    
    Fixes #6109.

--- static/js/narrow.js
+++ static/js/narrow.js
@@ -212,7 +212,7 @@ exports.activate = function (raw_operators, opts) {
         msg_list: message_list.narrowed,
         use_first_unread_anchor: opts.use_initial_narrow_pointer,
         cont: function () {
-            ui.hide_loading_more_messages_indicator();
+            message_fetch.reset_load_more_status();
             if (defer_selecting_closest) {
                 maybe_select_closest();
             }
