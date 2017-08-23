4617a650c015bd0efe3755d250e0f9a351d1bb4b
Brock Whittaker <brock@zulipchat.com>
2017-08-23 12:08:49 -0700

    Compact two `row.find(".alter-copied")` statements into one.

--- static/js/popovers.js
+++ static/js/popovers.js
@@ -556,8 +556,9 @@ exports.register_click_handlers = function () {
         popovers.hide_actions_popover();
         var id = $(this).attr("data-message-id");
         var row = $("[zid='" + id + "']");
-        row.find(".alert-copied").css("display", "block");
-        row.find(".alert-copied").delay(1000).fadeOut(300);
+        row.find(".alert-copied")
+            .css("display", "block")
+            .delay(1000).fadeOut(300);
 
         setTimeout(function () {
             // The Cliboard library works by focusing to a hidden textarea.
