7f0071c7de2dfb52f153d69456d69fae5180856d
Preston Hansen <prestonhansen01@gmail.com>
2017-08-27 09:30:48 -0500

    management: Move enqueue_digest_email handler to digest.

--- zerver/management/commands/enqueue_digest_emails.py
+++ zerver/management/commands/enqueue_digest_emails.py
@@ -8,8 +8,7 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.utils.timezone import now as timezone_now
 
-from zerver.models import UserProfile, Realm
-from zerver.lib.digest import inactive_since, should_process_digest, queue_digest_recipient
+from zerver.lib.digest import enqueue_emails, DIGEST_CUTOFF
 
 ## Logging setup ##
 
@@ -24,9 +23,6 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.addHandler(file_handler)
 
-VALID_DIGEST_DAY = 1  # Tuesdays
-DIGEST_CUTOFF = 5
-
 class Command(BaseCommand):
     help = """Enqueue digest emails for users that haven't checked the app
 in a while.
@@ -34,22 +30,5 @@ in a while.
 
     def handle(self, *args, **options):
         # type: (*Any, **Any) -> None
-        # To be really conservative while we don't have user timezones or
-        # special-casing for companies with non-standard workweeks, only
-        # try to send mail on Tuesdays.
-        if timezone_now().weekday() != VALID_DIGEST_DAY:
-            return
-
-        for realm in Realm.objects.filter(deactivated=False, show_digest_email=True):
-            if not should_process_digest(realm.string_id):
-                continue
-
-            user_profiles = UserProfile.objects.filter(
-                realm=realm, is_active=True, is_bot=False, enable_digest_emails=True)
-
-            for user_profile in user_profiles:
-                cutoff = timezone_now() - datetime.timedelta(days=DIGEST_CUTOFF)
-                if inactive_since(user_profile, cutoff):
-                    queue_digest_recipient(user_profile, cutoff)
-                    logger.info("%s is inactive, queuing for potential digest" % (
-                        user_profile.email,))
+        cutoff = timezone_now() - datetime.timedelta(days=DIGEST_CUTOFF)
+        enqueue_emails(cutoff)
