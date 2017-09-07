0986fe700d7be13fea184751bf0861b18cc429df
Greg Price <gnprice@gmail.com>
2017-09-07 13:00:54 +0900

    i18n: Keep markup out of a translated string where we easily can.
    Use jQuery DOM construction methods, rather than string concatenation,
    to keep things structured and to stay clear of the lint rules introduced
    in ee6235d71.

--- static/js/settings_org.js
+++ static/js/settings_org.js
@@ -108,10 +108,12 @@ exports.populate_auth_methods = function (auth_methods) {
     if (!page_params.is_admin) {
         $(".organization-box [data-name='auth-methods']")
             .find("input, button, select, checked").attr("disabled", true);
+        var tip_box = $("<div class='tip'></div>")
+            .text(i18n.t("Only organization administrators can edit these settings."));
         // Don't prepend a tip to custom emoji settings page. We handle it separately.
         $(".organization-box").find(".settings-section:not(.can-edit)")
             .not("#emoji-settings")
-            .prepend(i18n.t("<div class='tip'>Only organization administrators can edit these settings.</div>"));
+            .prepend(tip_box);
     }
 };
 
