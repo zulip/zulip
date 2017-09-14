010fe47e266e393d8ae563c695e94067a6e2df8e
Steve Howell <showell@zulipchat.com>
2017-09-14 10:47:22 -0700

    Add possible_mentions() to speed up rendering.
    We now triage message content for possible mentions before
    going to the cache/DB to get name info.  This will create an
    extra data hop for messages with mentions, but it will save
    a fairly expensive cache lookup for most messages.  (This will
    be especially helpful for large realms.)
    
    [Note that we need a subsequent commit to actually make the speedup
    happen here, since avatars also cause us to look up all users in
    the realm.]

--- zerver/lib/mention.py
+++ zerver/lib/mention.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import
 
-from typing import Text
+from typing import Optional, Set, Text
+
+import re
+
 # Match multi-word string between @** ** or match any one-word
 # sequences after @
 find_mentions = r'(?<![^\s\'\"\(,:<])@(\*\*[^\*]+\*\*|all|everyone)'
@@ -10,3 +13,21 @@ wildcards = ['all', 'everyone']
 def user_mention_matches_wildcard(mention):
     # type: (Text) -> bool
     return mention in wildcards
+
+def extract_name(s):
+    # type: (Text) -> Optional[Text]
+    if s.startswith("**") and s.endswith("**"):
+        name = s[2:-2]
+        if name in wildcards:
+            return None
+        return name
+
+    # We don't care about @all or @everyone
+    return None
+
+def possible_mentions(content):
+    # type: (Text) -> Set[Text]
+    matches = re.findall(find_mentions, content)
+    names = {extract_name(match) for match in matches}
+    names = {name for name in names if name}
+    return names
