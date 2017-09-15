ddf2075228a8c250cf75ec85914801262cb73177
Aditya Bansal <adi.bansal241996@gmail.com>
2017-09-16 01:53:17 +0530

    Make migration 0032 use an old version of user_avatar_path.
    This fixes upgrading from very old Zulip servers (e.g. 1.4.3) all the
    way to current.
    
    Fixes: #6516.

--- zerver/migrations/0032_verify_all_medium_avatar_images.py
+++ zerver/migrations/0032_verify_all_medium_avatar_images.py
@@ -1,13 +1,31 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.db import migrations
 from django.db.backends.postgresql_psycopg2.schema import DatabaseSchemaEditor
 from django.db.migrations.state import StateApps
 
+from mock import patch
+from zerver.lib.utils import make_safe_digest
 from zerver.lib.upload import upload_backend
+from zerver.models import UserProfile
+from typing import Text
+import hashlib
 
+# We hackishly patch this function in order to revert it to the state
+# it had when this migration was first written.  This is a balance
+# between copying in a historical version of hundreds of lines of code
+# from zerver.lib.upload (which would pretty annoying, but would be a
+# pain) and just using the current version, which doesn't work
+# since we rearranged the avatars in Zulip 1.6.
+def patched_user_avatar_path(user_profile):
+    # type: (UserProfile) -> Text
+    email = user_profile.email
+    user_key = email.lower() + settings.AVATAR_SALT
+    return make_safe_digest(user_key, hashlib.sha1)
 
+@patch('zerver.lib.upload.user_avatar_path', patched_user_avatar_path)
 def verify_medium_avatar_image(apps, schema_editor):
     # type: (StateApps, DatabaseSchemaEditor) -> None
     user_profile_model = apps.get_model('zerver', 'UserProfile')
