f6a505ea7da31611f33a26036b8351ba69332f6f
Brock Whittaker <brock@zulipchat.com>
2017-09-15 16:16:42 -0700

    Display an error for the ZulipDesktop unsupported platform.
    This displays an error at the top of the screen on page load that
    will inform any user with the userAgent string "ZulipDesktop" that
    they should upgrade to our newer electron app
    
    Fixes: #6551.

--- static/js/ui.js
+++ static/js/ui.js
@@ -46,6 +46,16 @@ function update_message_in_all_views(message_id, callback) {
     });
 }
 
+exports.show_error_for_unsupported_platform = function () {
+    if (/ZulipDesktop/.test(navigator.userAgent)) {
+        var error = "Hello! You're using the unsupported old Zulip desktop app," +
+            " which is no longer developed. We recommend switching to the new, " +
+            "modern desktop app, which you can download at zulipchat.com/apps.";
+
+        ui_report.generic_embed_error(error);
+    }
+};
+
 exports.find_message = function (message_id) {
     // Try to find the message object. It might be in the narrow list
     // (if it was loaded when narrowed), or only in the message_list.all
@@ -285,6 +295,7 @@ $(function () {
 
 exports.initialize = function () {
     i18n.ensure_i18n(_setup_info_overlay);
+    exports.show_error_for_unsupported_platform();
 };
 
 return exports;
