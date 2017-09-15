22ea2a5858aa4542f07879b0c27e48f89ec102fe
Tim Abbott <tabbott@zulipchat.com>
2017-09-15 00:36:14 -0700

    event_queue: Separate email and push notification loops.

--- zerver/tornado/event_queue.py
+++ zerver/tornado/event_queue.py
@@ -708,12 +708,13 @@ def process_message_event(event_template, users):
                 notice['stream_name'] = event_template.get('stream_name')
                 queue_json_publish("missedmessage_mobile_notifications", notice, lambda notice: None)
                 notified['push_notified'] = True
-                # Don't send missed message emails if always_push_notify or stream_push_notify is True
-                if idle:
-                    # We require RabbitMQ to do this, as we can't call the email handler
-                    # from the Tornado process. So if there's no rabbitmq support do nothing
-                    queue_json_publish("missedmessage_emails", notice, lambda notice: None)
-                    notified['email_notified'] = True
+
+            # Don't send missed message emails if always_push_notify or stream_push_notify is True
+            if idle:
+                # We require RabbitMQ to do this, as we can't call the email handler
+                # from the Tornado process. So if there's no rabbitmq support do nothing
+                queue_json_publish("missedmessage_emails", notice, lambda notice: None)
+                notified['email_notified'] = True
 
             if len(notified) > 0:
                 extra_user_data[user_profile_id] = notified
