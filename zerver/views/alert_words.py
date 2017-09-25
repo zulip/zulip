3b43841052c2994ae5c759d3af7bd045d9d25341
Alena Volkova <volkova.ag@gmail.com>
2017-09-25 17:41:41 -0400

    urls: Remove the old POST endpoint for alert words.

--- zerver/views/alert_words.py
+++ zerver/views/alert_words.py
@@ -9,7 +9,7 @@ from zerver.decorator import has_request_variables, REQ
 from zerver.lib.response import json_success
 from zerver.lib.validator import check_list, check_string
 
-from zerver.lib.actions import do_add_alert_words, do_remove_alert_words, do_set_alert_words
+from zerver.lib.actions import do_add_alert_words, do_remove_alert_words
 from zerver.lib.alert_words import user_alert_words
 
 def list_alert_words(request, user_profile):
@@ -20,13 +20,6 @@ def clean_alert_words(alert_words):
     # type: (List[Text]) -> List[Text]
     alert_words = [w.strip() for w in alert_words]
     return [w for w in alert_words if w != ""]
-
-@has_request_variables
-def set_alert_words(request, user_profile,
-                    alert_words=REQ(validator=check_list(check_string), default=[])):
-    # type: (HttpRequest, UserProfile, List[Text]) -> HttpResponse
-    do_set_alert_words(user_profile, clean_alert_words(alert_words))
-    return json_success()
 
 @has_request_variables
 def add_alert_words(request, user_profile,
