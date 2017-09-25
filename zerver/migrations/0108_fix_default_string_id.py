632180274abe4cf91f65cf0e84f817dc7124e293
Tim Abbott <tabbott@zulipchat.com>
2017-09-25 06:45:52 -0700

    mypy: Add imports needed for new migration.

--- zerver/migrations/0108_fix_default_string_id.py
+++ zerver/migrations/0108_fix_default_string_id.py
@@ -3,6 +3,8 @@
 from __future__ import unicode_literals
 
 from django.db import migrations
+from django.db.backends.postgresql_psycopg2.schema import DatabaseSchemaEditor
+from django.db.migrations.state import StateApps
 
 def fix_realm_string_ids(apps, schema_editor):
     # type: (StateApps, DatabaseSchemaEditor) -> None
