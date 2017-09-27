ed9b3ca440b06d4471967f60e8c41d2e33dae1a4
rht <rhtbot@protonmail.com>
2017-09-27 23:33:30 +0200

    zilencer: Remove import print_function.

--- zilencer/management/commands/populate_db.py
+++ zilencer/management/commands/populate_db.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import
 from __future__ import division
-from __future__ import print_function
 
 from django.core.management.base import BaseCommand, CommandParser
 from django.utils.timezone import now as timezone_now
