46485322eb22efde39e23fb1cb7d66824eb9c676
Tim Abbott <tabbott@zulipchat.com>
2017-10-05 16:33:25 -0700

    middleware: Remove logic for redirecting to zulipdev.com domains.
    We originally wrote this because when testing subdomains, you wanted
    to be sure you were actually testing subdomains.  Now that subdomains
    is the default, doesn't seem to actually be a good reason why we
    should need this.

--- zerver/middleware.py
+++ zerver/middleware.py
@@ -377,10 +377,6 @@ class SessionHostDomainMiddleware(SessionMiddleware):
         if (not request.path.startswith("/static/") and not request.path.startswith("/api/") and
                 not request.path.startswith("/json/")):
             subdomain = get_subdomain(request)
-            if (request.get_host() == "127.0.0.1:9991" or request.get_host() == "localhost:9991"):
-                return redirect("%s%s%s" % (settings.EXTERNAL_URI_SCHEME,
-                                            settings.EXTERNAL_HOST,
-                                            request.path))
             if subdomain != "":
                 realm = get_realm(subdomain)
                 if (realm is None):
