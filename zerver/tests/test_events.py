73e1c26c15d2f68de3314190172b79653a0e181a
Tim Abbott <tabbott@zulipchat.com>
2017-10-06 13:59:26 -0700

    test_events: Add missing nocoverage tag on debugging code.

--- zerver/tests/test_events.py
+++ zerver/tests/test_events.py
@@ -468,7 +468,7 @@ class EventsRegisterTest(ZulipTestCase):
         # our enormous payload does get updated properly.  We
         # want the diff here to be developer-friendly, hence
         # the somewhat tedious code to provide useful output.
-        if state1 != state2:
+        if state1 != state2:  # nocoverage
             print('\n---States DO NOT MATCH---')
             print('\nEVENTS:\n')
 
