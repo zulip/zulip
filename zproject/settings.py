52069998eb8bda2dbacddcbfa5ed36c5ef9f8127
Garvit <d.garvit@gmail.com>
2017-10-06 14:16:08 +0530

    compose: Preserve links when pasting html.
    This uses the to-markdown.js library to do all the hard work of
    parsing HTML and turning it into markdown and not e.g. uploaded files.
    
    Tweaked by tabbott to better scope when it activates to just include
    pastes of HTML content.
    
    Fixes #5853.

--- zproject/settings.py
+++ zproject/settings.py
@@ -922,6 +922,7 @@ JS_SPECS = {
             'node_modules/string.prototype.codepointat/codepointat.js',
             'node_modules/winchan/winchan.js',
             'node_modules/handlebars/dist/handlebars.runtime.js',
+            'node_modules/to-markdown/dist/to-markdown.js',
             'third/marked/lib/marked.js',
             'generated/emoji/emoji_codes.js',
             'generated/pygments_data.js',
