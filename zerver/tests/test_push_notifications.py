194b07b95a6463e30c2aeb5b75308fc026b3c65c
Harshit Bansal <harshitbansal2015@gmail.com>
2017-10-06 21:16:29 +0000

    tests: Add a test suite for verifying the mobile push notifs content.
    Fixes: #6720.

--- zerver/tests/test_push_notifications.py
+++ zerver/tests/test_push_notifications.py
@@ -7,6 +7,7 @@ import time
 from typing import Any, Dict, List, Union, SupportsInt, Text
 
 import gcm
+import os
 import ujson
 
 from django.test import TestCase, override_settings
@@ -27,7 +28,8 @@ from zerver.models import (
     Stream,
 )
 from zerver.lib import push_notifications as apn
-from zerver.lib.push_notifications import DeviceToken
+from zerver.lib.push_notifications import get_mobile_push_content, \
+    DeviceToken
 from zerver.lib.response import json_success
 from zerver.lib.test_classes import (
     ZulipTestCase,
@@ -35,6 +37,9 @@ from zerver.lib.test_classes import (
 
 from zilencer.models import RemoteZulipServer, RemotePushDeviceToken
 from django.utils.timezone import now
+
+ZERVER_DIR = os.path.dirname(os.path.dirname(__file__))
+FIXTURES_FILE_PATH = os.path.join(ZERVER_DIR, "fixtures", "markdown_test_cases.json")
 
 class BouncerTestCase(ZulipTestCase):
     def setUp(self):
@@ -1133,3 +1138,38 @@ class TestReceivesNotificationsFunctions(ZulipTestCase):
 
         self.user.enable_stream_push_notifications = False
         self.assertFalse(receives_stream_notifications(self.user))
+
+class TestPushNotificationsContent(ZulipTestCase):
+    def test_fixtures(self):
+        # type: () -> None
+        with open(FIXTURES_FILE_PATH) as fp:
+            fixtures = ujson.load(fp)
+        tests = fixtures["regular_tests"]
+        for test in tests:
+            if "text_content" in test:
+                output = get_mobile_push_content(test["expected_output"])
+                self.assertEqual(output, test["text_content"])
+
+    def test_backend_only_fixtures(self):
+        # type: () -> None
+        fixtures = [
+            {
+                'name': 'realm_emoji',
+                'rendered_content': '<p>Testing <img alt=":green_tick:" class="emoji" src="/user_avatars/1/emoji/green_tick.png" title="green tick"> realm emoji.</p>',
+                'expected_output': 'Testing :green_tick: realm emoji.',
+            },
+            {
+                'name': 'mentions',
+                'rendered_content': '<p>Mentioning <span class="user-mention" data-user-email="cordelia@zulip.com" data-user-id="3">@Cordelia Lear</span>.</p>',
+                'expected_output': 'Mentioning @Cordelia Lear.',
+            },
+            {
+                'name': 'stream_names',
+                'rendered_content': '<p>Testing stream names <a class="stream" data-stream-id="5" href="/#narrow/stream/Verona">#Verona</a>.</p>',
+                'expected_output': 'Testing stream names #Verona.',
+            },
+        ]
+
+        for test in fixtures:
+            actual_output = get_mobile_push_content(test["rendered_content"])
+            self.assertEqual(actual_output, test["expected_output"])
