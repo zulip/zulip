1ceaedb2c5de890b94fb0bbd4141848675c071e1
Tim Abbott <tabbott@zulipchat.com>
2017-10-08 15:41:41 -0700

    docs: Remove html_unescape.py.
    This was just for 1-time use exported the original Zulip documentation.

--- docs/html_unescape.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-# Remove HTML entity escaping left over from MediaWiki->rST conversion.
-
-import html
-import sys
-
-for line in sys.stdin:
-    print(html.unescape(line), end='')
