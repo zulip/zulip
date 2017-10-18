28e472b63f9054c7c16b8d6bb228259e4cabc2e2
Tim Abbott <tabbott@zulipchat.com>
2017-10-17 20:47:29 -0700

    export: Fix exporting with an output file at a relative path.
    Fixes #7051.

--- zerver/management/commands/export.py
+++ zerver/management/commands/export.py
@@ -111,6 +111,8 @@ class Command(ZulipBaseCommand):
         output_dir = options["output_dir"]
         if output_dir is None:
             output_dir = tempfile.mkdtemp(prefix="/tmp/zulip-export-")
+        else:
+            output_dir = os.path.realpath(output_dir)
         if os.path.exists(output_dir):
             shutil.rmtree(output_dir)
         os.makedirs(output_dir)
