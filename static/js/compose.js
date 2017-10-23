d69fe8483fb3f495b4fb77c449fb96a7803fd3e0
Aastha Gupta <aastha.gupta4104@gmail.com>
2017-10-23 17:24:20 +0530

    emoji: setting to display emoji as text
    Fixes #6795

--- static/js/compose.js
+++ static/js/compose.js
@@ -648,6 +648,12 @@ exports.initialize = function () {
             preview_html = rendered_content;
         }
         $("#preview_content").html(preview_html);
+        if (page_params.emoji_alt_code) {
+            $("#preview_content").find(".emoji").replaceWith(function () {
+                var text = $(this).attr("title");
+                return ":" + text + ":";
+            });
+         }
     }
 
     $('#compose').on('click', '#video_link', function (e) {
