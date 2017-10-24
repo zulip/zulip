89b97e748028f013b76260dd456f99fb98085ce2
Tim Abbott <tabbott@zulipchat.com>
2017-10-24 11:13:37 -0700

    python3: Fix REMOTE_USER Apache configuration for Python 3.
    We were previously still installing the Python 2 version of mod_wsgi,
    which of course doesn't work and can't use the Zulip virtualenv.

--- puppet/zulip/manifests/apache_sso.pp
+++ puppet/zulip/manifests/apache_sso.pp
@@ -1,8 +1,11 @@
 class zulip::apache_sso {
   include zulip::localhost_sso
 
-  $apache_packages = [ "apache2", "libapache2-mod-wsgi", ]
+  $apache_packages = [ "apache2", "libapache2-mod-wsgi-py3", ]
   package { $apache_packages: ensure => "installed" }
+
+  $apache_former_packages = [ "apache2", "libapache2-mod-wsgi", ]
+  package { $apache_legacy_packages: ensure => "absent" }
 
   apache2mod { [ "headers", "proxy", "proxy_http", "rewrite", "ssl", ]:
     ensure  => present,
