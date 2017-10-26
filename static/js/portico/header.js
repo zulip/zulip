893df6e0101b4e2e1faa7a06cd77f6e008c2bb03
Brock Whittaker <brock@zulipchat.com>
2017-10-26 11:04:27 -0700

    portico-header: Add pill with org/realm name.
    This adds a pill element that includes the org/realm name that you
    are currently signed into.

--- static/js/portico/header.js
+++ static/js/portico/header.js
@@ -7,7 +7,7 @@ $(function () {
     $("body").click(function (e) {
         var $this = $(e.target);
 
-        if ($this.is(".dropdown .header-realm-icon") && !$(".dropdown").hasClass("show")) {
+        if ($this.closest(".dropdown .dropdown-pill").length > 0 && !$(".dropdown").hasClass("show")) {
             $(".dropdown").addClass("show");
         } else if (!$this.is(".dropdown ul") && $this.closest(".dropdown ul").length === 0) {
             $(".dropdown").removeClass("show");
