d140451fb4d7138238c30fc599f4432a99f6ff69
Harshit Bansal <harshitbansal2015@gmail.com>
2017-10-30 20:24:22 +0000

    html_diff: Migrate from `Text` to `str`.

--- zerver/lib/html_diff.py
+++ zerver/lib/html_diff.py
@@ -1,14 +1,14 @@
 import lxml
 
 from lxml.html.diff import htmldiff
-from typing import Optional, Text
+from typing import Optional
 
 def highlight_with_class(text, klass):
-    # type: (Text, Text) -> Text
+    # type: (str, str) -> str
     return '<span class="%s">%s</span>' % (klass, text)
 
 def highlight_html_differences(s1, s2, msg_id=None):
-    # type: (Text, Text, Optional[int]) -> Text
+    # type: (str, str, Optional[int]) -> str
     retval = htmldiff(s1, s2)
     fragment = lxml.html.fromstring(retval)  # type: ignore # https://github.com/python/typeshed/issues/525
 
