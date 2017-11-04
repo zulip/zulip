4e0fc593cfc79eba787570bb72d099b8290ea2d6
rht <rhtbot@protonmail.com>
2017-11-04 05:16:55 +0100

    docs: Remove u prefix from strings.

--- docs/conf.py
+++ docs/conf.py
@@ -43,9 +43,9 @@ templates_path = ['_templates']
 master_doc = 'index'
 
 # General information about the project.
-project = u'Zulip'
-copyright = u'2015-2017, The Zulip Team'
-author = u'The Zulip Team'
+project = 'Zulip'
+copyright = '2015-2017, The Zulip Team'
+author = 'The Zulip Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -238,8 +238,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'zulip-contributor-docs.tex', u'Zulip Documentation',
-     u'The Zulip Team', 'manual'),
+    (master_doc, 'zulip-contributor-docs.tex', 'Zulip Documentation',
+     'The Zulip Team', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -268,7 +268,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'zulip-contributor-docs', u'Zulip Documentation',
+    (master_doc, 'zulip-contributor-docs', 'Zulip Documentation',
      [author], 1)
 ]
 
@@ -282,7 +282,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'zulip-contributor-docs', u'Zulip Documentation',
+    (master_doc, 'zulip-contributor-docs', 'Zulip Documentation',
      author, 'zulip-contributor-docs', 'Documentation for contributing to Zulip.',
      'Miscellaneous'),
 ]
