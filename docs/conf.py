dd10d30d1ed54e2379d934fea9f2b3c373a66c21
David Rosa Tamsen <rosatamsen@gmail.com>
2017-11-06 16:14:48 -0600

    docs: Add setting to conf.py to disable sticky_navigation
    The left navigation bar defaults to "sticking" to the screen as you
    scroll.  This commit adds the sticky_navigation setting to conf.py to
    disable the default behavior.
    
    This addresses part of #5265.

--- docs/conf.py
+++ docs/conf.py
@@ -117,7 +117,9 @@ if not on_rtd:
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'sticky_navigation': False,
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
