cdc1bf3d5e3d2c09ff147113ab0d9f50ec5f1353
Steve Howell <showell@zulipchat.com>
2017-11-08 11:13:19 -0800

    node test: Remove add_dependencies().

--- frontend_tests/zjsunit/index.js
+++ frontend_tests/zjsunit/index.js
@@ -16,7 +16,6 @@ if (_.isEmpty(files)) {
 var namespace = require('./namespace.js');
 global.set_global = namespace.set_global;
 global.patch_builtin = namespace.patch_builtin;
-global.add_dependencies = namespace.add_dependencies;
 global.zrequire = namespace.zrequire;
 global.stub_out_jquery = namespace.stub_out_jquery;
 global.with_overrides = namespace.with_overrides;
