4f3d3ce99ff8396cb260cded8f88308e46670cf1
Steve Howell <showell@zulipchat.com>
2017-11-09 08:32:28 -0800

    node tests: Test compose_ui.smart_insert().

---
+++ frontend_tests/node_tests/compose_ui.js
@@ -0,0 +1,62 @@
+zrequire('compose_ui');
+
+function make_textbox(s) {
+    // Simulate a jQuery textbox for testing purposes.
+    var widget = {};
+
+    widget.s = s;
+    widget.focused = false;
+
+    widget.caret = function (arg) {
+        if (typeof arg === 'number') {
+            widget.pos = arg;
+            return;
+        }
+
+        if (arg) {
+            widget.insert_pos = widget.pos;
+            widget.insert_text = arg;
+            var before = widget.s.slice(0, widget.pos);
+            var after = widget.s.slice(widget.pos);
+            widget.s = before + arg + after;
+            widget.pos += arg.length;
+            return;
+        }
+
+        return widget.pos;
+    };
+
+    widget.focus = function () {
+        widget.focused = true;
+    };
+
+    widget.blur = function () {
+        widget.focused = false;
+    };
+
+    widget.val = function () {
+        return widget.s;
+    };
+
+    return widget;
+}
+
+(function test_smart_insert() {
+    var textbox = make_textbox('abc ');
+    textbox.caret(4);
+
+    compose_ui.smart_insert(textbox, ':smile:');
+    assert.equal(textbox.insert_pos, 4);
+    assert.equal(textbox.insert_text, ':smile:');
+    assert.equal(textbox.val(), 'abc :smile:');
+    assert(textbox.focused);
+
+    // Test the current slightly-broken behavior.
+    textbox.blur();
+    compose_ui.smart_insert(textbox, ':airplane:');
+    assert.equal(textbox.insert_text, ':airplane:');
+    assert.equal(textbox.val(), 'abc :smile::airplane:');
+    assert(textbox.focused);
+
+}());
+
--- static/js/compose_ui.js
+++ static/js/compose_ui.js
@@ -6,13 +6,17 @@ exports.autosize_textarea = function () {
     $("#new_message_content").trigger("autosize.resize");
 };
 
+exports.smart_insert = function (textarea, syntax) {
+    textarea.caret(syntax);
+    textarea.focus();
+};
+
 exports.insert_syntax_and_focus = function (syntax) {
     // Generic helper for inserting syntax into the main compose box
     // where the cursor was and focusing the area.  Mostly a thin
-    // wrapper around $.caret.
+    // wrapper around smart_insert.
     var textarea = $('#new_message_content');
-    textarea.caret(syntax);
-    textarea.focus();
+    exports.smart_insert(textarea, syntax);
 };
 
 return exports;
