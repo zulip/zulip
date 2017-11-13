08a526985ad64cb432b75adb4ca8f07a26557ba0
Cynthia Lin <cynthialin1004@gmail.com>
2017-11-12 20:20:42 -0800

    org settings: Add framework for User groups page.

--- zproject/settings.py
+++ zproject/settings.py
@@ -1079,6 +1079,7 @@ JS_SPECS = {
             'js/settings_streams.js',
             'js/settings_filters.js',
             'js/settings_invites.js',
+            'js/settings_user_groups.js',
             'js/settings.js',
             'js/admin_sections.js',
             'js/admin.js',
