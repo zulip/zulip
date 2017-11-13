8bdd61c7de20fb06207fd9065863c48808e12592
Brock Whittaker <brock@zulipchat.com>
2017-11-13 12:38:00 -0800

    ui_util: Move `place_caret_at_end` to ui_util module.
    This makes it publicly accessible across the codebase.

--- static/js/ui_util.js
+++ static/js/ui_util.js
@@ -19,6 +19,26 @@ exports.focus_on = function (field_id) {
     $("#" + field_id).focus();
 };
 
+// http://stackoverflow.com/questions/4233265/contenteditable-set-caret-at-the-end-of-the-text-cross-browser
+exports.place_caret_at_end = function (el) {
+    el.focus();
+
+    if (typeof window.getSelection !== "undefined"
+            && typeof document.createRange !== "undefined") {
+        var range = document.createRange();
+        range.selectNodeContents(el);
+        range.collapse(false);
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+    } else if (typeof document.body.createTextRange !== "undefined") {
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(el);
+        textRange.collapse(false);
+        textRange.select();
+    }
+};
+
 exports.blur_active_element = function () {
     // this blurs anything that may perhaps be actively focused on.
     document.activeElement.blur();
