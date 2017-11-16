de2a2d0dfa72e4ea533480f636a3cab86ec55a50
Greg Price <greg@zulipchat.com>
2017-11-16 12:40:36 -0800

    docs: Send people to install instructions for latest release.
    Pointing these at the latest release, rather than the latest version
    in master, allows us to make changes to the installer and document
    them properly in master, without making the instructions confusingly
    wrong for people who just go to the website or the GitHub repo page
    and follow instructions to install.

--- version.py
+++ version.py
@@ -1,2 +1,6 @@
+# When bumping this for a release, we should also update the handful
+# of places we link to docs for the latest release, rather than master.
+# To find them:   git grep 'zulip.readthedocs.io/en/[0-9]'
 ZULIP_VERSION = "1.7.0+git"
+
 PROVISION_VERSION = '12.0'
