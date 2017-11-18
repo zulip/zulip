1e9a2843980746477efbf8c7bb4396c54f999198
Sumit Roy <sroy@gmail.com>
2017-11-18 10:51:05 +0530

    /help sidebar fix #6714

--- static/js/portico/help.js
+++ static/js/portico/help.js
@@ -1,33 +1,41 @@
 const Ps = require('perfect-scrollbar');
 
-function registerCodeSection($codeSection) {
-    const $li = $codeSection.find("ul.nav li");
-    const $blocks = $codeSection.find(".blocks div");
-
-    $li.click(function () {
-        const language = this.dataset.language;
-
-        $li.removeClass("active");
-        $li.filter("[data-language="+language+"]").addClass("active");
-
-        $blocks.removeClass("active");
-        $blocks.filter("[data-language="+language+"]").addClass("active");
-    });
-
-    $li.eq(0).click();
-}
-
-function render_code_sections() {
-    $(".code-section").each(function () {
-        registerCodeSection($(this));
-    });
-}
-
 (function () {
     var html_map = {};
     var loading = {
         name: null,
     };
+    var no_of_images = 0;
+    var no_of_loaded_images = 0;
+
+    var scroll_to_anchor = function (path) {
+        var container = $(".markdown")[0];
+        var path_split = path.split("#");
+        if (path_split.length === 2) {
+            document.getElementById(path_split[1]).scrollIntoView();
+        } else {
+               container.scrollTop = 0;
+        }
+    };
+
+    function scroll_when_ready() {
+        no_of_loaded_images += 1;
+        if (no_of_loaded_images === no_of_images) {
+            scroll_to_anchor(window.location.href);
+        }
+    }
+
+    function scroll_to_anchor_once_images_ready() {
+        var imgs = document.images;
+        no_of_loaded_images = 0;
+        no_of_images = (imgs && imgs.length) || 0;
+        if ((!imgs) || no_of_images === 0) {
+            scroll_to_anchor(window.location.href);
+        }
+        [].forEach.call(imgs, function (img) {
+            img.addEventListener('load', scroll_when_ready, false);
+        });
+    }
 
     var fetch_page = function (path, callback) {
         $.get(path, function (res) {
@@ -35,11 +43,10 @@ function render_code_sections() {
             $html.find(".back-to-home").remove();
 
             callback($html.html().trim());
-            render_code_sections();
         });
     };
 
-    $(".sidebar.slide h2").click(function (e) {
+    $(".sidebar h2").click(function (e) {
         var $next = $(e.target).next();
 
         if ($next.is("ul")) {
@@ -62,14 +69,14 @@ function render_code_sections() {
 
         if (html_map[path]) {
             $(".markdown .content").html(html_map[path]);
+            scroll_to_anchor_once_images_ready();
             Ps.update(container);
-            render_code_sections();
         } else {
             loading.name = path;
-
             fetch_page(path, function (res) {
                 html_map[path] = res;
                 $(".markdown .content").html(html_map[path]);
+                scroll_to_anchor_once_images_ready();
                 loading.name = null;
                 Ps.update(container);
             });
@@ -111,6 +118,4 @@ function render_code_sections() {
             $(".sidebar.show").toggleClass("show");
         }
     });
-
-    render_code_sections();
 }());
