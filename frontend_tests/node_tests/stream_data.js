5c3f76fe2c6435abbf2b3df709689808e12fcad1
Jerry Zhang <jerry.w.zhang@gmail.com>
2017-12-02 20:30:58 -0800

    node tests: Cover exports.receives_desktop_notifications.

--- frontend_tests/node_tests/stream_data.js
+++ frontend_tests/node_tests/stream_data.js
@@ -444,3 +444,16 @@ zrequire('stream_data');
     stream_data.add_subscriber('India', 103);
     assert.equal(stream_data.get_subscriber_count('India'), 2);
 }());
+
+(function test_notifications() {
+    var india = {
+        stream_id: 102,
+        name: 'India',
+        subscribed: true,
+        desktop_notifications: true,
+    };
+    stream_data.clear_subscriptions();
+    stream_data.add_sub('India', india);
+    assert(stream_data.receives_desktop_notifications('India'));
+    assert(!stream_data.receives_desktop_notifications('Indiana'));
+}());
