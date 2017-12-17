75206f7a325d7f8d70a6cc7bbd0812d429c5cf16
Cynthia Lin <cynthialin1004@gmail.com>
2017-12-17 10:35:06 -0800

    general UI: Add a description for the message view source icon.

--- static/js/ui_init.js
+++ static/js/ui_init.js
@@ -44,7 +44,7 @@ function message_hover(message_row) {
         !message.status_message) {
         message_row.find(".edit_content").html('<i class="fa fa-pencil edit_content_button" aria-hidden="true" title="Edit"></i>');
     } else {
-        message_row.find(".edit_content").html('<i class="icon-vector-file-text-alt edit_content_button" data-msgid="' + id + '"></i>');
+        message_row.find(".edit_content").html('<i class="fa fa-file-text-o edit_content_button" aria-hidden="true" title="View source" data-msgid="' + id + '"></i>');
     }
 }
 
