be602466085c0b6252b695aa9f7fd39ca57709e2
Cynthia Lin <cynthialin1004@gmail.com>
2017-12-19 11:39:56 -0800

    node tests: Add 100% test coverage for user_groups.js.

--- frontend_tests/node_tests/user_groups.js
+++ frontend_tests/node_tests/user_groups.js
@@ -1,8 +1,18 @@
 set_global('blueslip', {});
+set_global('page_params', {});
 
 zrequire('user_groups');
 
 (function test_user_groups() {
+    var students = {
+        name: 'Students',
+        id: 0,
+    };
+    global.page_params.realm_user_groups = [students];
+
+    user_groups.initialize();
+    assert.equal(user_groups.get_user_group_from_id(students.id), students);
+
     var admins = {
         name: 'Admins',
         id: 1,
@@ -11,14 +21,22 @@ zrequire('user_groups');
         name: 'Everyone',
         id: 2,
     };
+
     user_groups.add(admins);
     assert.equal(user_groups.get_user_group_from_id(admins.id), admins);
+
     var called = false;
     global.blueslip.error = function (msg) {
         assert.equal(msg, "Unknown group_id in get_user_group_from_id: " + all.id);
         called = true;
     };
+
     assert.equal(user_groups.get_user_group_from_id(all.id), undefined);
     assert(called);
-}());
 
+    user_groups.remove(students);
+    global.blueslip.error = function (msg) {
+        assert.equal(msg, "Unknown group_id in get_user_group_from_id: " + students.id);
+    };
+    assert.equal(user_groups.get_user_group_from_id(students.id), undefined);
+}());
