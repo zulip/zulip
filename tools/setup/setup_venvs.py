8c2822b08f3efb695b7d11c7b79828aa868ed4be
Greg Price <greg@zulipchat.com>
2017-12-18 21:49:51 -0800

    provision: Remove transitional Python 2->3 logic in setup_venvs.
    At this point if we were accidentally using `/srv/zulip-venv` for
    anything, we'd have run into it by now.  So just drop the bit of
    historical logic that we had to ensure that.

--- tools/setup/setup_venvs.py
+++ tools/setup/setup_venvs.py
@@ -10,24 +10,17 @@ if ZULIP_PATH not in sys.path:
 from scripts.lib.setup_venv import setup_virtualenv
 from scripts.lib.zulip_tools import run, subprocess_text_output
 
-OLD_VENV_PATH = "/srv/zulip-venv"
 VENV_PATH = "/srv/zulip-py3-venv"
 
 DEV_REQS_FILE = os.path.join(ZULIP_PATH, "requirements", "dev.txt")
 
-def main(is_travis=False):
-    # type: (bool) -> None
+def main() -> None:
     # Get the correct Python interpreter. If we don't do this and use
     # `virtualenv -p python3` to create the venv in Travis, the venv
     # starts referring to the system Python interpreter.
     python_interpreter = subprocess_text_output(['which', 'python3'])
-    if is_travis:
-        setup_virtualenv(VENV_PATH, DEV_REQS_FILE, patch_activate_script=True,
-                         virtualenv_args=['-p', python_interpreter])
-    else:
-        run(['sudo', 'rm', '-f', OLD_VENV_PATH])
-        setup_virtualenv(VENV_PATH, DEV_REQS_FILE, patch_activate_script=True,
-                         virtualenv_args=['-p', python_interpreter])
+    setup_virtualenv(VENV_PATH, DEV_REQS_FILE, patch_activate_script=True,
+                     virtualenv_args=['-p', python_interpreter])
 
 if __name__ == "__main__":
     main()
