f0f1a06ce2bcd021db8febb0b5ee9208fb1dde48
pooja3 <pjmrrk@gmail.com>
2017-12-23 16:55:11 +0100

    channel.js: Improved handling of bad response
    for error code >=400

--- static/js/channel.js
+++ static/js/channel.js
@@ -27,7 +27,6 @@ function call(args, idempotent) {
     }
     args.error = function wrapped_error(xhr, error_type, xhn) {
         remove_pending_request(xhr);
-
         if (xhr.status === 403) {
             try {
                 if (JSON.parse(xhr.responseText).code === 'CSRF_FAILED') {
@@ -42,22 +41,22 @@ function call(args, idempotent) {
                                 args: args},
                                ex.stack);
             }
-        }else if(xhr.status >= 400){
-			try {		
-				var jsonResponse = JSON.parse(xhr.responseText);
-			} catch (ex) {
+        } else if (xhr.status >= 400) {
+            var jsonResponse;
+            try {
+                jsonResponse = JSON.parse(xhr.responseText);
+            } catch (ex) {
                 blueslip.error('Unexpected '+ xhr.status +' response from server',
                                {xhr: xhr.responseText,
                                 args: args},
                                ex.stack);
             }
-			if (jsonResponse.result === 'error') {
-				blueslip.error(jsonResponse.msg,
-							   {xhr: xhr.responseText,
-								args: args},
-							   undefined);
-			}				
-		}
+            if (jsonResponse.result === 'error') {
+                blueslip.error(jsonResponse.msg,
+                              {xhr: xhr.responseText,
+                               args: args},undefined);
+            }
+        }
         return orig_error(xhr, error_type, xhn);
     };
     var orig_success = args.success;
