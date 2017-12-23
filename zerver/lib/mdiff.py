b0126ca8dc765cc6a3aceff935403e654aa5b80b
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2017-12-23 20:26:09 +0000

    mypy: Amend diff_strings to return str or raise DiffException.

--- zerver/lib/mdiff.py
+++ zerver/lib/mdiff.py
@@ -3,12 +3,16 @@ import subprocess
 import logging
 import difflib
 
+class DiffException(Exception):
+    pass
+
 def diff_strings(output: str, expected_output: str) -> str:
 
     mdiff_path = "frontend_tests/zjsunit/mdiff.js"
     if not os.path.isfile(mdiff_path):  # nocoverage
-        logging.error("Cannot find mdiff for markdown diff rendering")
-        return None
+        msg = "Cannot find mdiff for markdown diff rendering"
+        logging.error(msg)
+        raise DiffException(msg)
 
     command = ['node', mdiff_path, output, expected_output]
     diff = subprocess.check_output(command).decode('utf-8')
