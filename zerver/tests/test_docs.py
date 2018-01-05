8f571499738965e98bee5ff477973e7d3ae3e949
Eeshan Garg <jerryguitarist@gmail.com>
2018-01-05 18:01:56 -0330

    api docs: Document the GET /api/v1/users/me endpoint.
    Note that there is no JavaScript equivalent for requesting this
    endpoint in zulip-js yet.

--- zerver/tests/test_docs.py
+++ zerver/tests/test_docs.py
@@ -65,6 +65,7 @@ class DocPageTest(ZulipTestCase):
         self._test('/api/get-events-from-queue', 'dont_block')
         self._test('/api/delete-queue', 'Delete a previously registered queue')
         self._test('/api/update-message', 'propagate_mode')
+        self._test('/api/get-profile', 'takes no arguments')
         self._test('/team/', 'industry veterans')
         self._test('/history/', 'Cambridge, Massachusetts')
         # Test the i18n version of one of these pages.
