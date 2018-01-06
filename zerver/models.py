8050f7775de64d6d40c398908a980a18463fca78
Robert Hönig <robhoenig@gmail.com>
2018-01-06 11:01:22 +0100

    Rename BotUserConfigData to BotConfigData.
    This is in compliance with the naming of
    BotStorageData.

--- zerver/models.py
+++ zerver/models.py
@@ -1904,7 +1904,7 @@ class BotStorageData(models.Model):
     class Meta:
         unique_together = ("bot_profile", "key")
 
-class BotUserConfigData(models.Model):
+class BotConfigData(models.Model):
     bot_profile = models.ForeignKey(UserProfile, on_delete=CASCADE)  # type: UserProfile
     key = models.TextField(db_index=True)  # type: Text
     value = models.TextField()  # type: Text
