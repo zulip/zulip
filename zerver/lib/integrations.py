4b8bd0bc3b92b1baf2330821018cff6af25b1902
Ricky <www.rishabhrawat570@gmail.com>
2018-01-12 18:19:11 +0530

    webhooks: Add Dialogflow integration.
    With minor fixes by eeshangarg!
    
    Fixes #6990

--- zerver/lib/integrations.py
+++ zerver/lib/integrations.py
@@ -284,6 +284,7 @@ WEBHOOK_INTEGRATIONS = [
     WebhookIntegration('circleci', ['continuous-integration'], display_name='CircleCI'),
     WebhookIntegration('codeship', ['continuous-integration', 'deployment']),
     WebhookIntegration('crashlytics', ['monitoring']),
+    WebhookIntegration('dialogflow', ['customer-support'], display_name='Dialogflow'),
     WebhookIntegration('delighted', ['customer-support', 'marketing'], display_name='Delighted'),
     WebhookIntegration(
         'deskdotcom',
