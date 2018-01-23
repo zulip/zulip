c2b77c57198faf3dac5afff3bbae56da4c9c32f0
Greg Price <greg@zulipchat.com>
2018-01-23 13:53:45 -0800

    lint pyflakes: Pull out our error-suppression patterns as data.
    This makes the list much cleaner to understand and edit.

--- tools/linter_lib/pyflakes.py
+++ tools/linter_lib/pyflakes.py
@@ -7,6 +7,27 @@ from .printer import print_err, colors
 
 from typing import Any, Dict, List
 
+suppress_patterns = [
+    (b'', b'imported but unused'),
+    (b'', b'redefinition of unused'),
+
+    # Our ipython startup pythonrc file intentionally imports *
+    (b"scripts/lib/pythonrc.py",
+     b" import *' used; unable to detect undefined names"),
+
+    # Special dev_settings.py import
+    (b'', b"from .prod_settings_template import *"),
+
+    (b"settings.py", b"settings import *' used; unable to detect undefined names"),
+    (b"settings.py", b"may be undefined, or defined from star imports"),
+]
+
+def suppress_line(line: str) -> bool:
+    for file_pattern, line_pattern in suppress_patterns:
+        if file_pattern in line and line_pattern in line:
+            return True
+    return False
+
 def check_pyflakes(options, by_lang):
     # type: (Any, Dict[str, List[str]]) -> bool
     if len(by_lang['py']) == 0:
@@ -16,21 +37,9 @@ def check_pyflakes(options, by_lang):
     pyflakes = subprocess.Popen(['pyflakes'] + by_lang['py'],
                                 stdout=subprocess.PIPE,
                                 stderr=subprocess.PIPE)
-
     assert pyflakes.stdout is not None  # Implied by use of subprocess.PIPE
     for ln in pyflakes.stdout.readlines() + pyflakes.stderr.readlines():
-        if options.full or not (
-            b'imported but unused' in ln or
-            b'redefinition of unused' in ln or
-            # Our ipython startup pythonrc file intentionally imports *
-            (b"scripts/lib/pythonrc.py" in ln and
-             b" import *' used; unable to detect undefined names" in ln) or
-            # Special dev_settings.py import
-            b"from .prod_settings_template import *" in ln or
-            (b"settings.py" in ln and
-             (b"settings import *' used; unable to detect undefined names" in ln or
-              b"may be undefined, or defined from star imports" in ln))):
-
+        if options.full or not suppress_line(ln):
             print_err('pyflakes', color, ln)
             failed = True
     return failed
