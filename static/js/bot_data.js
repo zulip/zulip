58688dc9af21d8f394b5e05cd5d48aefa33034cf
Robert Hönig <robhoenig@gmail.com>
2018-01-30 19:24:18 +0100

    frontend: Store embedded bot data in bot_data.js.
    In specific, this stores config_data and service_name
    for embedded bots.

--- static/js/bot_data.js
+++ static/js/bot_data.js
@@ -6,7 +6,8 @@ var bot_data = (function () {
                       'default_events_register_stream', 'default_sending_stream',
                       'email', 'full_name', 'is_active', 'owner', 'bot_type', 'user_id'];
     var services = {};
-    var services_fields = ['base_url', 'interface'];
+    var services_fields = ['base_url', 'interface',
+                           'config_data', 'service_name'];
 
     var send_change_event = _.debounce(function () {
         $(document).trigger('zulip.bot_data_changed');
