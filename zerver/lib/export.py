cad4083987fbc73e1d7b32d4ab5828a1ff55e073
Greg Price <greg@zulipchat.com>
2018-01-30 15:34:47 -0800

    export: Fix an unnecessary Any.
    This was introduced a few weeks ago in
    ed4054d11 "Import script: Check and add system bots after every import."

--- zerver/lib/export.py
+++ zerver/lib/export.py
@@ -1390,7 +1390,7 @@ def import_uploads(import_dir: Path, processing_avatars: bool=False) -> None:
 # Because the Python object => JSON conversion process is not fully
 # faithful, we have to use a set of fixers (e.g. on DateTime objects
 # and Foreign Keys) to do the import correctly.
-def do_import_realm(import_dir: Path) -> Any:
+def do_import_realm(import_dir: Path) -> Realm:
     logging.info("Importing realm dump %s" % (import_dir,))
     if not os.path.exists(import_dir):
         raise Exception("Missing import directory!")
