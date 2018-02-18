9a74ef50565d64e3d3df099faa9f6b555f87d132
Tim Abbott <tabbott@zulipchat.com>
2018-02-18 10:46:40 -0800

    puppet: Move some cron jobs to app_frontend_once.pp.
    Several cron jobs had incorrectly ended up in the app_frontend.pp
    template, and thus would only run on voyager instances.

--- puppet/zulip/manifests/app_frontend_once.pp
+++ puppet/zulip/manifests/app_frontend_once.pp
@@ -2,6 +2,14 @@
 # in a cluster.
 
 class zulip::app_frontend_once {
+  file { "/etc/cron.d/send-digest-emails":
+    ensure => file,
+    owner  => "root",
+    group  => "root",
+    mode => 644,
+    source => "puppet:///modules/zulip/cron.d/send-digest-emails",
+  }
+
   file { "/etc/cron.d/update-analytics-counts":
     ensure => file,
     owner  => "root",
@@ -16,5 +24,21 @@ class zulip::app_frontend_once {
     group  => "root",
     mode => 644,
     source => "puppet:///modules/zulip/cron.d/check-analytics-state",
+  }
+
+  file { "/etc/cron.d/soft-deactivate-users":
+    ensure => file,
+    owner  => "root",
+    group  => "root",
+    mode => 644,
+    source => "puppet:///modules/zulip/cron.d/soft-deactivate-users",
+  }
+
+  file { "/etc/cron.d/calculate-first-visible-message-id":
+    ensure => file,
+    owner  => "root",
+    group  => "root",
+    mode => 644,
+    source => "puppet:///modules/zulip/cron.d/calculate-first-visible-message-id",
   }
 }
