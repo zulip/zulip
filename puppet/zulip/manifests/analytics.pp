ad7f38ab3b1caa1067d1379dfe04a0cde4e8c577
Tim Abbott <tabbott@zulipchat.com>
2018-02-18 10:43:25 -0800

    puppet: Move analytics cron job to analytics.pp.
    This better groups it with the related code.

--- puppet/zulip/manifests/analytics.pp
+++ puppet/zulip/manifests/analytics.pp
@@ -1,11 +1,19 @@
 # Default configuration for a Zulip app frontend
+# This should only be enabled on exactly 1 Zulip server in a cluster.
 class zulip::analytics {
-  # This should only be enabled on exactly 1 Zulip server in a cluster.
   file { "/etc/cron.d/update-analytics-counts":
     ensure => file,
     owner  => "root",
     group  => "root",
     mode => 644,
     source => "puppet:///modules/zulip/cron.d/update-analytics-counts",
+  }
+
+  file { "/etc/cron.d/check-analytics-state":
+    ensure => file,
+    owner  => "root",
+    group  => "root",
+    mode => 644,
+    source => "puppet:///modules/zulip/cron.d/check-analytics-state",
   }
 }
