77d128a67a2140d8d0b3028b178b369f55b88a97
Priyank <priyank3219@gmail.com>
2018-03-01 22:50:23 +0000

    install-node: Upgrade node, yarn, and nvm.
    node -> v8.9.4
    yarn -> 1.5.1
    nvm -> 0.33.8
    
    Also updates a test in timerender.js which depends on time
    provided by node which is now changed in newer release.

--- frontend_tests/node_tests/timerender.js
+++ frontend_tests/node_tests/timerender.js
@@ -197,7 +197,7 @@ zrequire('timerender');
 
 (function test_set_full_datetime() {
     var message = {
-        timestamp: 1495091573, // 5/18/2017 7:12:53 AM (UTC+0)
+        timestamp: 1495091573, // 2017-5-18 07:12:53 (UTC+0)
     };
     var time_element = $('<span/>');
     var attrs = new Dict();
@@ -207,7 +207,12 @@ zrequire('timerender');
         return time_element;
     };
 
-    var expected = '5/18/2017 7:12:53 AM (UTC+0)';
+    // in node >= 8.9.4 date.toLocaleDateString and
+    // date.toLocaleTimeString are change instead of
+    // 5/18/2017 7:12:53 AM (UTC+0) they now return
+    // 2017-5-18 07:12:53 (UTC+0)
+    var time = new Date(message.timestamp * 1000);
+    var expected = `${time.toLocaleDateString()} 07:12:53 (UTC+0)`;
     timerender.set_full_datetime(message, time_element);
     var actual = attrs.get('title');
     assert.equal(expected, actual);
