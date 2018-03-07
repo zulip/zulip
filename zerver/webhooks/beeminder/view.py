bc562216d98988181adf18dec9c3e1d8659f4417
Ricky <www.rishabhrawat570@gmail.com>
2018-03-07 23:48:23 +0530

    webhooks/beeminder: Mock time.time() to avoid race.

--- zerver/webhooks/beeminder/view.py
+++ zerver/webhooks/beeminder/view.py
@@ -14,6 +14,11 @@ def get_user_name(email: str) -> str:
     name = profile.short_name
     return name
 
+def get_time(payload: Dict[str, Any]) -> Any:
+    losedate = payload["goal"]["losedate"]
+    time_remaining = (losedate - current_time)/3600
+    return time_remaining
+
 @api_key_only_webhook_view("beeminder")
 @has_request_variables
 def api_beeminder_webhook(request: HttpRequest, user_profile: UserProfile,
@@ -24,10 +29,9 @@ def api_beeminder_webhook(request: HttpRequest, user_profile: UserProfile,
 
     secret = payload["goal"]["secret"]
     goal_name = payload["goal"]["slug"]
-    losedate = payload["goal"]["losedate"]
     limsum = payload["goal"]["limsum"]
     pledge = payload["goal"]["pledge"]
-    time_remain = (losedate - current_time)/3600   # time in hours
+    time_remain = get_time(payload)  # time in hours
     # To show user's probable reaction by looking at pledge amount
     if pledge > 0:
         expression = ':worried:'
@@ -36,10 +40,9 @@ def api_beeminder_webhook(request: HttpRequest, user_profile: UserProfile,
 
     if not secret:
         # In this case notifications will be sent to stream
-
         name = get_user_name(email)
-        body = u"Hello **{}**! I am the Beeminder bot! :octopus: \n You are going to derail \
-        from goal **{}** in **{:0.1f} hours** \n You need **{}** to avoid derailing \n * Pledge: **{}$** {}"
+        body = u"Hello **{}**! I am the Beeminder bot! :octopus:\n You are going to derail \
+from goal **{}** in **{:0.1f} hours**\n You need **{}** to avoid derailing\n * Pledge: **{}$** {}"
         body = body.format(name, goal_name, time_remain, limsum, pledge, expression)
         check_send_stream_message(user_profile, request.client, stream, topic, body)
         return json_success()
@@ -47,8 +50,8 @@ def api_beeminder_webhook(request: HttpRequest, user_profile: UserProfile,
     else:
         # In this case PM will be sent to user
         p = get_user_profile_by_email(email)
-        body = u"I am the Beeminder bot! :octopus: \n You are going to derail from \
-        goal **{}** in **{:0.1f} hours** \n You need **{}** to avoid derailing \n * Pledge: **{}$**{}"
+        body = u"I am the Beeminder bot! :octopus:\n You are going to derail from \
+goal **{}** in **{:0.1f} hours**\n You need **{}** to avoid derailing\n * Pledge: **{}$**{}"
         body = body.format(goal_name, time_remain, limsum, pledge, expression)
         check_send_private_message(user_profile, request.client, p, body)
         return json_success()
