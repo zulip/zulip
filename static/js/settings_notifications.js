848701a65307c3cf90d5366fe11a387d6f8535f9
Shubham Dhama <shubhamdhamaofficial@gmail.com>
2018-03-08 04:02:12 +0530

    settings: Refactor notification settings to use do_settings_change.
    Use settings_ui.do_settings_change system for making all requests
    and to show our new settings change status indicator.
    
    Fixes: #8587.

--- static/js/settings_notifications.js
+++ static/js/settings_notifications.js
@@ -38,96 +38,34 @@ function maybe_bulk_update_stream_notification_setting(notification_checkbox,
     });
 }
 
-exports.set_up = function () {
-    var notify_settings_status = $("#notify-settings-status").expectOne();
-    notify_settings_status.hide();
+function change_notification_setting(setting, setting_data, status_element) {
+    var data = {};
+    data[setting] = JSON.stringify(setting_data);
+    settings_ui.do_settings_change('/json/settings/notifications', data, status_element);
+}
 
+exports.set_up = function () {
     if (!page_params.realm_show_digest_email) {
         $("#digest_container").hide();
     }
 
     _.each(pm_mention_notification_settings, function (setting) {
         $("#" + setting).change(function () {
-            var data = {};
-            var setting_name = $('label[for=' + setting + ']').text().trim();
-            var context = {setting_name: setting_name};
-            var setting_data = $(this).prop('checked');
-            data[setting] = JSON.stringify(setting_data);
-
-            channel.patch({
-                url: '/json/settings/notifications',
-                data: data,
-                success: function () {
-                    if (setting_data === true) {
-                        ui_report.success(i18n.t("Enabled: __- setting_name__",
-                            context), notify_settings_status);
-                    } else {
-                        ui_report.success(i18n.t("Disabled: __- setting_name__",
-                            context), notify_settings_status);
-                    }
-                },
-                error: function (xhr) {
-                    ui_report.error(i18n.t('Error updating: __- setting_name__',
-                        context), xhr, notify_settings_status);
-                },
-            });
+            change_notification_setting(setting, $(this).prop('checked'), "#pm-mention-notify-settings-status");
         });
     });
 
     _.each(other_notification_settings, function (setting) {
         $("#" + setting).change(function () {
-            var data = {};
-            var setting_name = $('label[for=' + setting + ']').text().trim();
-            var context = {setting_name: setting_name};
-            var setting_data = $(this).prop('checked');
-            data[setting] = JSON.stringify(setting_data);
-
-            channel.patch({
-                url: '/json/settings/notifications',
-                data: data,
-                success: function () {
-                    if (setting_data === true) {
-                        ui_report.success(i18n.t("Enabled: __- setting_name__",
-                            context), notify_settings_status);
-                    } else {
-                        ui_report.success(i18n.t("Disabled: __- setting_name__",
-                            context), notify_settings_status);
-                    }
-                },
-                error: function (xhr) {
-                    ui_report.error(i18n.t('Error updating: __- setting_name__',
-                        context), xhr, notify_settings_status);
-                },
-            });
+            change_notification_setting(setting, $(this).prop('checked'), "#other-notify-settings-status");
         });
     });
 
     _.each(stream_notification_settings, function (stream_setting) {
         var setting = stream_setting.setting;
         $("#" + setting).change(function () {
-            var data = {};
-            var setting_name = $('label[for=' + setting + ']').text().trim();
-            var context = {setting_name: setting_name};
             var setting_data = $(this).prop('checked');
-            data[setting] = JSON.stringify(setting_data);
-
-            channel.patch({
-                url: '/json/settings/notifications',
-                data: data,
-                success: function () {
-                    if (setting_data === true) {
-                        ui_report.success(i18n.t("Enabled: __- setting_name__",
-                            context), notify_settings_status);
-                    } else {
-                        ui_report.success(i18n.t("Disabled: __- setting_name__",
-                            context), notify_settings_status);
-                    }
-                },
-                error: function (xhr) {
-                    ui_report.error(i18n.t('Error updating: __- setting_name__',
-                        context), xhr, notify_settings_status);
-                },
-            });
+            change_notification_setting(setting, setting_data, "#stream-notify-settings-status");
             maybe_bulk_update_stream_notification_setting($('#' + setting), function () {
                 stream_edit.set_notification_setting_for_all_streams(
                     stream_setting.notifications, setting_data);
