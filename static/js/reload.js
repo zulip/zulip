60f08b069ce70b605fa3fe4fadde0aac592a6699
Steve Howell <showell@zulipchat.com>
2018-03-07 08:16:34 -0500

    reload: Handle errors with compose_actions.start().
    If we have bugs in our compose code, we don't want them to
    cause other strange side effects during reload.  (We recently
    had a test deployment that was a bit buggy, and the reload
    problem was tough to chase down due to the misleading
    symptoms.)

--- static/js/reload.js
+++ static/js/reload.js
@@ -140,13 +140,19 @@ exports.initialize = function () {
     if (vars.msg !== undefined) {
         var send_now = parseInt(vars.send_after_reload, 10);
 
-        // TODO: preserve focus
-        compose_actions.start(vars.msg_type, {stream: vars.stream || '',
-                                      subject: vars.subject || '',
-                                      private_message_recipient: vars.recipient || '',
-                                      content: vars.msg || ''});
-        if (send_now) {
-            compose.finish();
+        try {
+            // TODO: preserve focus
+            compose_actions.start(vars.msg_type, {stream: vars.stream || '',
+                                          subject: vars.subject || '',
+                                          private_message_recipient: vars.recipient || '',
+                                          content: vars.msg || ''});
+            if (send_now) {
+                compose.finish();
+            }
+        } catch (err) {
+            // We log an error if we can't open the compose box, but otherwise
+            // we continue, since this is not critical.
+            blueslip.warn(err.toString());
         }
     }
 
