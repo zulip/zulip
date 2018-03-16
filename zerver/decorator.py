zulip/zulip

commit 93678e89cd29da5b8106e278fa776f90f6fed33f

webhooks: Migrate 14 webhooks to use check_send_webhook_message.
These are the straightforward ones.

Note that there is a line in zerver.lib.test_classes.build_webhook_url
that lost test coverage. That's because most of our tests test using
stream messages so the webhook URLs being tested always have a query
parameter. So the line that accounts for there being no query
parameters never gets called, which is fine, but we should still
keep it.

zerver/webhooks/beanstalk/view.py
@@ -7,10 +7,10 @@ from typing import Any, Callable, Dict, Optional, Text, TypeVar
 from django.http import HttpRequest, HttpResponse
 
 from zerver.decorator import authenticated_rest_api_view
-from zerver.lib.actions import check_send_stream_message
 from zerver.lib.types import ViewFuncT
 from zerver.lib.request import REQ, has_request_variables
 from zerver.lib.response import json_success
+from zerver.lib.webhooks.common import check_send_webhook_message
 from zerver.lib.validator import check_dict
 from zerver.models import UserProfile, get_client
 from zerver.webhooks.github.view import build_message_from_gitlog
@@ -67,6 +67,5 @@ def api_beanstalk_webhook(request: HttpRequest, user_profile: UserProfile,
         subject = "svn r%s" % (revision,)
         content = "%s pushed [revision %s](%s):\n\n> %s" % (author, revision, url, short_commit_msg)
 
-    check_send_stream_message(user_profile, get_client("ZulipBeanstalkWebhook"),
-                              "commits", subject, content)
+    check_send_webhook_message(request, user_profile, subject, content)
     return json_success()

zerver/webhooks/bitbucket/view.py
@@ -3,10 +3,10 @@ from typing import Any, Mapping, Optional, Text
 from django.http import HttpRequest, HttpResponse
 
 from zerver.decorator import authenticated_rest_api_view
-from zerver.lib.actions import check_send_stream_message
 from zerver.lib.request import REQ, has_request_variables
 from zerver.lib.response import json_success
 from zerver.lib.validator import check_dict
+from zerver.lib.webhooks.common import check_send_webhook_message
 from zerver.lib.webhooks.git import SUBJECT_WITH_BRANCH_TEMPLATE, \
     get_push_commits_event_message
 from zerver.models import UserProfile, get_client
@@ -15,7 +15,6 @@ from zerver.models import UserProfile, get_client
 @has_request_variables
 def api_bitbucket_webhook(request: HttpRequest, user_profile: UserProfile,
                           payload: Mapping[Text, Any]=REQ(validator=check_dict([])),
-                          stream: Text=REQ(default='commits'),
                           branches: Optional[Text]=REQ(default=None)) -> HttpResponse:
     repository = payload['repository']
 
@@ -46,6 +45,5 @@ def api_bitbucket_webhook(request: HttpRequest, user_profile: UserProfile,
         content = get_push_commits_event_message(payload['user'], None, branch, commits)
         subject = SUBJECT_WITH_BRANCH_TEMPLATE.format(repo=repository['name'], branch=branch)
 
-    check_send_stream_message(user_profile, get_client("ZulipBitBucketWebhook"),
-                              stream, subject, content)
+    check_send_webhook_message(request, user_profile, subject, content)
     return json_success()

zerver/webhooks/deskdotcom/view.py
@@ -4,9 +4,9 @@ from typing import Text
 from django.http import HttpRequest, HttpResponse
 
 from zerver.decorator import authenticated_rest_api_view
-from zerver.lib.actions import check_send_stream_message
 from zerver.lib.request import REQ, has_request_variables
 from zerver.lib.response import json_success
+from zerver.lib.webhooks.common import check_send_webhook_message
 from zerver.models import UserProfile, get_client
 
 # Desk.com's integrations all make the user supply a template, where it fills
@@ -16,9 +16,8 @@ from zerver.models import UserProfile, get_client
 # from the "data" param and post it, which this does.
 @authenticated_rest_api_view(is_webhook=True)
 @has_request_variables
-def api_deskdotcom_webhook(request: HttpRequest, user_profile: UserProfile, data: Text=REQ(),
-                           topic: Text=REQ(default="Desk.com notification"),
-                           stream: Text=REQ(default="desk.com")) -> HttpResponse:
-    check_send_stream_message(user_profile, get_client("ZulipDeskWebhook"),
-                              stream, topic, data)
+def api_deskdotcom_webhook(request: HttpRequest, user_profile: UserProfile,
+                           data: Text=REQ()) -> HttpResponse:
+    topic = "Desk.com notification"
+    check_send_webhook_message(request, user_profile, topic, data)
     return json_success()

zerver/webhooks/airbrake/view.py
@@ -5,7 +5,7 @@ from django.http import HttpRequest, HttpResponse
 from django.utils.translation import ugettext as _
 
 from zerver.decorator import api_key_only_webhook_view
-from zerver.lib.actions import check_send_stream_message
+from zerver.lib.webhooks.common import check_send_webhook_message
 from zerver.lib.request import REQ, has_request_variables
 from zerver.lib.response import json_error, json_success
 from zerver.models import UserProfile
@@ -16,12 +16,10 @@ AIRBRAKE_MESSAGE_TEMPLATE = '[{error_class}]({error_url}): "{error_message}" occ
 @api_key_only_webhook_view('Airbrake')
 @has_request_variables
 def api_airbrake_webhook(request: HttpRequest, user_profile: UserProfile,
-                         payload: Dict[str, Any] = REQ(argument_type='body'),
-                         stream: Text = REQ(default='airbrake')) -> HttpResponse:
+                         payload: Dict[str, Any]=REQ(argument_type='body')) -> HttpResponse:
     subject = get_subject(payload)
     body = get_body(payload)
-    check_send_stream_message(user_profile, request.client,
-                              stream, subject, body)
+    check_send_webhook_message(request, user_profile, subject, body)
     return json_success()
 
 def get_subject(payload: Dict[str, Any]) -> str:

zerver/webhooks/appfollow/view.py
@@ -6,24 +6,21 @@ from django.http import HttpRequest, HttpResponse
 from django.utils.translation import ugettext as _
 
 from zerver.decorator import api_key_only_webhook_view
-from zerver.lib.actions import check_send_stream_message
 from zerver.lib.request import REQ, has_request_variables
 from zerver.lib.response import json_error, json_success
+from zerver.lib.webhooks.common import check_send_webhook_message
 from zerver.models import UserProfile
 
 @api_key_only_webhook_view("AppFollow")
 @has_request_variables
 def api_appfollow_webhook(request: HttpRequest, user_profile: UserProfile,
-                          stream: Text=REQ(default="appfollow"),
-                          topic: Optional[Text]=REQ(default=None),
                           payload: Dict[str, Any]=REQ(argument_type="body")) -> HttpResponse:
     message = payload["text"]
     app_name = re.search('\A(.+)', message).group(0)
-    if topic is None:
-        topic = app_name
+    topic = app_name
 
-    check_send_stream_message(sender=user_profile, client=request.client, stream_name=stream,
-                              topic=topic, body=convert_markdown(message))
+    check_send_webhook_message(request, user_profile, topic,
+                               body=convert_markdown(message))
     return json_success()
 
 def convert_markdown(text: Text) -> Text:

zerver/webhooks/basecamp/view.py
@@ -5,9 +5,9 @@ from typing import Any, Dict, Text
 from django.http import HttpRequest, HttpResponse
 
 from zerver.decorator import api_key_only_webhook_view
-from zerver.lib.actions import check_send_stream_message
 from zerver.lib.request import REQ, has_request_variables
 from zerver.lib.response import json_error, json_success
+from zerver.lib.webhooks.common import check_send_webhook_message
 from zerver.models import UserProfile
 
 from .support_event import SUPPORT_EVENTS
@@ -25,8 +25,7 @@ TODO_TEMPLATE = "{user_name} {verb} the todo task [{title}]({url})"
 @api_key_only_webhook_view('Basecamp')
 @has_request_variables
 def api_basecamp_webhook(request: HttpRequest, user_profile: UserProfile,
-                         payload: Dict[str, Any]=REQ(argument_type='body'),
-                         stream: Text=REQ(default='basecamp')) -> HttpResponse:
+                         payload: Dict[str, Any]=REQ(argument_type='body')) -> HttpResponse:
     event = get_event_type(payload)
 
     if event not in SUPPORT_EVENTS:
@@ -52,8 +51,7 @@ def api_basecamp_webhook(request: HttpRequest, user_profile: UserProfile,
         logging.warning("Basecamp handling of {} event is not implemented".format(event))
         return json_success()
 
-    check_send_stream_message(user_profile, request.client,
-                              stream, subject, body)
+    check_send_webhook_message(request, user_profile, subject, body)
     return json_success()
 
 def get_project_name(payload: Dict[str, Any]) -> Text:

zerver/webhooks/bitbucket2/view.py
@@ -7,9 +7,9 @@ from django.http import HttpRequest, HttpResponse
 from django.utils.translation import ugettext as _
 
 from zerver.decorator import api_key_only_webhook_view
-from zerver.lib.actions import check_send_stream_message
 from zerver.lib.request import REQ, has_request_variables
 from zerver.lib.response import json_error, json_success
+from zerver.lib.webhooks.common import check_send_webhook_message
 from zerver.lib.webhooks.git import SUBJECT_WITH_BRANCH_TEMPLATE, \
     SUBJECT_WITH_PR_OR_ISSUE_INFO_TEMPLATE, \
     get_commits_comment_action_message, get_force_push_commits_event_message, \
@@ -46,14 +46,12 @@ class UnknownTriggerType(Exception):
 @has_request_variables
 def api_bitbucket2_webhook(request: HttpRequest, user_profile: UserProfile,
                            payload: Dict[str, Any]=REQ(argument_type='body'),
-                           stream: str=REQ(default='bitbucket'),
                            branches: Optional[Text]=REQ(default=None)) -> HttpResponse:
     type = get_type(request, payload)
     if type != 'push':
         subject = get_subject_based_on_type(payload, type)
         body = get_body_based_on_type(type)(payload)
-        check_send_stream_message(user_profile, request.client,
-                                  stream, subject, body)
+        check_send_webhook_message(request, user_profile, subject, body)
     else:
         # ignore push events with no changes
         if not payload['push']['changes']:
@@ -65,8 +63,8 @@ def api_bitbucket2_webhook(request: HttpRequest, user_profile: UserProfile,
         subjects = get_push_subjects(payload)
         bodies_list = get_push_bodies(payload)
         for body, subject in zip(bodies_list, subjects):
-            check_send_stream_message(user_profile, request.client,
-                                      stream, subject, body)
+            check_send_webhook_message(request, user_profile, subject, body)
+
     return json_success()
 
 def get_subject_for_branch_specified_events(payload: Dict[str, Any],

zerver/webhooks/circleci/view.py
@@ -6,9 +6,9 @@ import ujson
 from django.http import HttpRequest, HttpResponse
 
 from zerver.decorator import api_key_only_webhook_view
-from zerver.lib.actions import check_send_stream_message
 from zerver.lib.request import REQ, has_request_variables
 from zerver.lib.response import json_error, json_success
+from zerver.lib.webhooks.common import check_send_webhook_message
 from zerver.models import UserProfile
 
 CIRCLECI_SUBJECT_TEMPLATE = u'{repository_name}'
@@ -19,13 +19,12 @@ FAILED_STATUS = 'failed'
 @api_key_only_webhook_view('CircleCI')
 @has_request_variables
 def api_circleci_webhook(request: HttpRequest, user_profile: UserProfile,
-                         payload: Dict[str, Any]=REQ(argument_type='body'),
-                         stream: Text=REQ(default='circleci')) -> HttpResponse:
+                         payload: Dict[str, Any]=REQ(argument_type='body')) -> HttpResponse:
     payload = payload['payload']
     subject = get_subject(payload)
     body = get_body(payload)
 
-    check_send_stream_message(user_profile, request.client, stream, subject, body)
+    check_send_webhook_message(request, user_profile, subject, body)
     return json_success()
 
 def get_subject(payload: Dict[str, Any]) -> Text:

commit 34e165c1001c0b3a53a1e1d19adb4e15b3712446

webhooks: Fix passing client string to authenticated webhook API views.
This fixes a regression in 93678e89cd29da5b8106e278fa776f90f6fed33f
and a4979410f983e7aaa10f4833a9c302110bea685c, where the webhooks using
authenticated_rest_api_view were migrated to a new model that didn't
include setting a custom Client string for the webhook.

When restoring these webhooks' client strings, we also fix places
where the client string was not capitalized the same was as the
product's name.

zerver/decorator.py
@@ -294,8 +294,13 @@ body:
     message = message.strip(' ')
     webhook_logger.exception(message)
 
+def full_webhook_client_name(raw_client_name: Optional[str]=None) -> Optional[str]:
+    if raw_client_name is None:
+        return None
+    return "Zulip{}Webhook".format(raw_client_name)
+
 # Use this for webhook views that don't get an email passed in.
-def api_key_only_webhook_view(client_name: Text) -> Callable[[ViewFuncT], ViewFuncT]:
+def api_key_only_webhook_view(webhook_client_name: Text) -> Callable[[ViewFuncT], ViewFuncT]:
     # TODO The typing here could be improved by using the Extended Callable types:
     # https://mypy.readthedocs.io/en/latest/kinds_of_types.html#extended-callable-types
     def _wrapped_view_func(view_func: ViewFuncT) -> ViewFuncT:
@@ -305,7 +310,7 @@ def api_key_only_webhook_view(client_name: Text) -> Callable[[ViewFuncT], ViewFu
         def _wrapped_func_arguments(request: HttpRequest, api_key: Text=REQ(),
                                     *args: Any, **kwargs: Any) -> HttpResponse:
             user_profile = validate_api_key(request, None, api_key, is_webhook=True,
-                                            client_name="Zulip{}Webhook".format(client_name))
+                                            client_name=full_webhook_client_name(webhook_client_name))
 
             if settings.RATE_LIMITING:
                 rate_limit_user(request, user_profile, domain='all')
@@ -468,7 +473,11 @@ def authenticated_api_view(is_webhook: bool=False) -> Callable[[ViewFuncT], View
 
 # A more REST-y authentication decorator, using, in particular, HTTP Basic
 # authentication.
-def authenticated_rest_api_view(is_webhook: bool=False) -> Callable[[ViewFuncT], ViewFuncT]:
+#
+# If webhook_client_name is specific, the request is a webhook view
+# with that string as the basis for the client string.
+def authenticated_rest_api_view(*, webhook_client_name: str=None,
+                                is_webhook: bool=False) -> Callable[[ViewFuncT], ViewFuncT]:
     def _wrapped_view_func(view_func: ViewFuncT) -> ViewFuncT:
         @csrf_exempt
         @wraps(view_func)
@@ -490,7 +499,9 @@ def authenticated_rest_api_view(is_webhook: bool=False) -> Callable[[ViewFuncT],
             # Now we try to do authentication or die
             try:
                 # profile is a Union[UserProfile, RemoteZulipServer]
-                profile = validate_api_key(request, role, api_key, is_webhook)
+                profile = validate_api_key(request, role, api_key,
+                                           is_webhook=is_webhook or webhook_client_name is not None,
+                                           client_name=full_webhook_client_name(webhook_client_name))
             except JsonableError as e:
                 return json_unauthorized(e.msg)
             # Apply rate limiting

zerver/lib/rest.py
@@ -99,7 +99,7 @@ def rest_dispatch(request: HttpRequest, **kwargs: Any) -> HttpResponse:
             view_kwargs = {}
             if 'allow_incoming_webhooks' in view_flags:
                 view_kwargs['is_webhook'] = True
-            target_function = authenticated_rest_api_view(**view_kwargs)(target_function)
+            target_function = authenticated_rest_api_view(**view_kwargs)(target_function)  # type: ignore # likely mypy bug
         # Pick a way to tell user they're not authed based on how the request was made
         else:
             # If this looks like a request from a top-level page in a

zerver/webhooks/beanstalk/view.py
@@ -37,7 +37,7 @@ def beanstalk_decoder(view_func: ViewFuncT) -> ViewFuncT:
     return _wrapped_view_func  # type: ignore # https://github.com/python/mypy/issues/1927
 
 @beanstalk_decoder
-@authenticated_rest_api_view(is_webhook=True)
+@authenticated_rest_api_view(webhook_client_name="Beanstalk")
 @has_request_variables
 def api_beanstalk_webhook(request: HttpRequest, user_profile: UserProfile,
                           payload: Dict[str, Any]=REQ(validator=check_dict([])),

zerver/webhooks/bitbucket/view.py
@@ -11,7 +11,7 @@ from zerver.lib.webhooks.git import SUBJECT_WITH_BRANCH_TEMPLATE, \
     get_push_commits_event_message
 from zerver.models import UserProfile, get_client
 
-@authenticated_rest_api_view(is_webhook=True)
+@authenticated_rest_api_view(webhook_client_name="Bitbucket")
 @has_request_variables
 def api_bitbucket_webhook(request: HttpRequest, user_profile: UserProfile,
                           payload: Mapping[Text, Any]=REQ(validator=check_dict([])),

zerver/webhooks/deskdotcom/view.py
@@ -14,7 +14,7 @@ from zerver.models import UserProfile, get_client
 # There's no raw JSON for us to work from. Thus, it makes sense to just write
 # a template Zulip message within Desk.com and have the webhook extract that
 # from the "data" param and post it, which this does.
-@authenticated_rest_api_view(is_webhook=True)
+@authenticated_rest_api_view(webhook_client_name="Desk")
 @has_request_variables
 def api_deskdotcom_webhook(request: HttpRequest, user_profile: UserProfile,
                            data: Text=REQ()) -> HttpResponse:

zerver/webhooks/freshdesk/view.py
@@ -103,7 +103,7 @@ def format_freshdesk_ticket_creation_message(ticket: TicketDict) -> str:
 
     return content
 
-@authenticated_rest_api_view(is_webhook=True)
+@authenticated_rest_api_view(webhook_client_name="Freshdesk")
 @has_request_variables
 def api_freshdesk_webhook(request: HttpRequest, user_profile: UserProfile,
                           payload: Dict[str, Any]=REQ(argument_type='body')) -> HttpResponse:

zerver/webhooks/zendesk/view.py
@@ -14,7 +14,7 @@ def truncate(string: Text, length: int) -> Text:
         string = string[:length-3] + '...'
     return string
 
-@authenticated_rest_api_view(is_webhook=True)
+@authenticated_rest_api_view(webhook_client_name="Zendesk")
 @has_request_variables
 def api_zendesk_webhook(request: HttpRequest, user_profile: UserProfile,
                         ticket_title: str=REQ(), ticket_id: str=REQ(),