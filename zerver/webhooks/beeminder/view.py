3dafbfa5f4882ef8e331565c36fec4b3c1825582
Eeshan Garg <jerryguitarist@gmail.com>
2018-03-17 14:33:42 -0230

    webhooks/beeminder: Use check_send_webhook_message and update docs.
    We now only give users two options, to specify a stream and receive
    public notifications for their goals, or to leave it out and receive
    PMs and thus, keep their goals private. This simplifies the docs!

--- zerver/webhooks/beeminder/view.py
+++ zerver/webhooks/beeminder/view.py
@@ -2,16 +2,11 @@
 from typing import Text, Dict, Any
 from django.http import HttpRequest, HttpResponse
 from zerver.decorator import api_key_only_webhook_view
-from zerver.lib.actions import check_send_stream_message, check_send_private_message
 from zerver.lib.request import REQ, has_request_variables
 from zerver.lib.response import json_success
-from zerver.models import UserProfile, get_user_profile_by_email
+from zerver.lib.webhooks.common import check_send_webhook_message
+from zerver.models import UserProfile
 import time
-
-def get_user_name(email: str) -> str:
-    profile = get_user_profile_by_email(email)
-    name = profile.short_name
-    return name
 
 def get_time(payload: Dict[str, Any]) -> Any:
     losedate = payload["goal"]["losedate"]
@@ -21,12 +16,8 @@ def get_time(payload: Dict[str, Any]) -> Any:
 @api_key_only_webhook_view("beeminder")
 @has_request_variables
 def api_beeminder_webhook(request: HttpRequest, user_profile: UserProfile,
-                          payload: Dict[str, Any]=REQ(argument_type='body'),
-                          stream: Text=REQ(default="beeminder"),
-                          email: str=REQ(default='foo@gmail.com'),
-                          topic: Text=REQ(default='beekeeper')) -> HttpResponse:
+                          payload: Dict[str, Any]=REQ(argument_type='body')) -> HttpResponse:
 
-    secret = payload["goal"]["secret"]
     goal_name = payload["goal"]["slug"]
     limsum = payload["goal"]["limsum"]
     pledge = payload["goal"]["pledge"]
@@ -37,20 +28,9 @@ def api_beeminder_webhook(request: HttpRequest, user_profile: UserProfile,
     else:
         expression = ':relieved:'
 
-    if not secret:
-        # In this case notifications will be sent to stream
-        name = get_user_name(email)
-        body = u"Hello **{}**! I am the Beeminder bot! :octopus:\n You are going to derail \
-from goal **{}** in **{:0.1f} hours**\n You need **{}** to avoid derailing\n * Pledge: **{}$** {}"
-        body = body.format(name, goal_name, time_remain, limsum, pledge, expression)
-        check_send_stream_message(user_profile, request.client, stream, topic, body)
-        return json_success()
-
-    else:
-        # In this case PM will be sent to user
-        p = get_user_profile_by_email(email)
-        body = u"I am the Beeminder bot! :octopus:\n You are going to derail from \
-goal **{}** in **{:0.1f} hours**\n You need **{}** to avoid derailing\n * Pledge: **{}$**{}"
-        body = body.format(goal_name, time_remain, limsum, pledge, expression)
-        check_send_private_message(user_profile, request.client, p, body)
-        return json_success()
+    topic = u'beekeeper'
+    body = u"You are going to derail from goal **{}** in **{:0.1f} hours**\n \
+You need **{}** to avoid derailing\n * Pledge: **{}$** {}"
+    body = body.format(goal_name, time_remain, limsum, pledge, expression)
+    check_send_webhook_message(request, user_profile, topic, body)
+    return json_success()
