1e7dc70c7fd55c1a9c082c44ff76072934839453
Tim Abbott <tabbott@zulipchat.com>
2018-03-21 13:24:34 -0700

    reactions: Fix the hotkey for adding an emoji reaction.
    This was incorrectly migrated in
    a49655e0d4a868d3973e53cf8482b91aa2df1bb1; the toggle_emoji_reaction
    function didn't expect this type.

--- static/js/hotkey.js
+++ static/js/hotkey.js
@@ -706,15 +706,7 @@ exports.process_hotkey = function (e, hotkey) {
             reactions.open_reactions_popover();
             return true;
         case 'thumbs_up_emoji': // '+': reacts with thumbs up emoji on selected message
-            // Use canonical name.
-            var thumbs_up_codepoint = '1f44d';
-            var canonical_name = emoji_codes.codepoint_to_name[thumbs_up_codepoint];
-            var reaction_info = {
-                emoji_name: canonical_name,
-                emoji_code: thumbs_up_codepoint,
-                reaction_type: 'unicode_emoji',
-            };
-            reactions.toggle_emoji_reaction(msg.id, reaction_info);
+            reactions.toggle_emoji_reaction(msg.id, "thumbs_up");
             return true;
         case 'toggle_mute':
             muting_ui.toggle_mute(msg);
