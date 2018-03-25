aac76c14bdc1a6f7041cb996eb74996b6b1ac131
Steve Howell <showell@zulipchat.com>
2018-03-25 09:46:34 -0400

    settings: Avoid duplicate form handlers.
    For forms that are built early in setting up the settings panel,
    we don't want to attach multiple submit handlers every time we
    go into the gear menu, so we use "off" to clear any old handlers.
    
    We also attach handlers directly to the form, instead of
    using delegation up to the container div.

--- static/js/settings_filters.js
+++ static/js/settings_filters.js
@@ -66,7 +66,7 @@ exports.set_up = function () {
         });
     });
 
-    $(".organization").on("submit", "form.admin-filter-form", function (e) {
+    $(".organization form.admin-filter-form").off('submit').on('submit', function (e) {
         e.preventDefault();
         e.stopPropagation();
         var filter_status = $('#admin-filter-status');
