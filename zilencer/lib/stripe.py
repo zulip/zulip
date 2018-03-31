16334a1ba7566c7d83ac1d61b9eca35b83b95250
Rishi Gupta <rishig@zulipchat.com>
2018-03-30 19:13:44 -0700

    billing: Update billing system.

--- zilencer/lib/stripe.py
+++ zilencer/lib/stripe.py
@@ -1,7 +1,8 @@
+import datetime
 from functools import wraps
 import logging
 import os
-from typing import Any, Callable, TypeVar
+from typing import Any, Callable, Optional, TypeVar
 
 from django.conf import settings
 from django.utils.translation import ugettext as _
@@ -9,13 +10,13 @@ import stripe
 
 from zerver.lib.exceptions import JsonableError
 from zerver.lib.logging_util import log_to_file
+from zerver.lib.timestamp import datetime_to_timestamp
 from zerver.models import Realm, UserProfile
-from zilencer.models import Customer
+from zilencer.models import Customer, Plan
 from zproject.settings import get_secret
 
-STRIPE_SECRET_KEY = get_secret('stripe_secret_key')
 STRIPE_PUBLISHABLE_KEY = get_secret('stripe_publishable_key')
-stripe.api_key = STRIPE_SECRET_KEY
+stripe.api_key = get_secret('stripe_secret_key')
 
 BILLING_LOG_PATH = os.path.join('/var/log/zulip'
                                 if not settings.DEVELOPMENT
@@ -25,7 +26,29 @@ billing_logger = logging.getLogger('zilencer.stripe')
 log_to_file(billing_logger, BILLING_LOG_PATH)
 log_to_file(logging.getLogger('stripe'), BILLING_LOG_PATH)
 
+# To generate the fixture data in stripe_fixtures.json:
+# * Set PRINT_STRIPE_FIXTURE_DATA to True
+# * ./manage.py setup_stripe
+# * Customer.objects.all().delete()
+# * Log in as a user, and go to http://localhost:9991/upgrade/
+# * Click Add card. Enter the following billing details:
+#       Name: Ada Starr, Street: Under the sea, City: Pacific,
+#       Zip: 33333, Country: United States
+#       Card number: 4242424242424242, Expiry: 03/33, CVV: 333
+# * Click Make payment.
+# * Copy out the 4 blobs of json from the dev console into stripe_fixtures.json.
+#   The contents of that file are '{\n' + concatenate the 4 json blobs + '\n}'.
+#   Then you can run e.g. `M-x mark-whole-buffer` and `M-x indent-region` in emacs
+#   to prettify the file (and make 4 space indents).
+# * Copy out the customer id, plan id, and quantity values into
+#   zilencer.tests.test_stripe.StripeTest.setUp.
+# * Set PRINT_STRIPE_FIXTURE_DATA to False
+PRINT_STRIPE_FIXTURE_DATA = False
+
 CallableT = TypeVar('CallableT', bound=Callable[..., Any])
+
+def get_seat_count(realm: Realm) -> int:
+    return UserProfile.objects.filter(realm=realm, is_active=True, is_bot=False).count()
 
 class StripeError(JsonableError):
     pass
@@ -36,7 +59,7 @@ def catch_stripe_errors(func: CallableT) -> CallableT:
         if STRIPE_PUBLISHABLE_KEY is None:
             # Dev-only message; no translation needed.
             raise StripeError(
-                "Missing Stripe config. In dev, add to zproject/dev-secrets.conf .")
+                "Missing Stripe config. See https://zulip.readthedocs.io/en/latest/subsystems/billing.html.")
         try:
             return func(*args, **kwargs)
         except stripe.error.StripeError as e:
@@ -54,41 +77,53 @@ def catch_stripe_errors(func: CallableT) -> CallableT:
     return wrapped  # type: ignore # https://github.com/python/mypy/issues/1927
 
 @catch_stripe_errors
-def count_stripe_cards(realm: Realm) -> int:
-    try:
-        customer_obj = Customer.objects.get(realm=realm)
-        cards = stripe.Customer.retrieve(customer_obj.stripe_customer_id).sources.all(object="card")
-        return len(cards["data"])
-    except Customer.DoesNotExist:
-        return 0
+def get_stripe_customer(stripe_customer_id: int) -> Any:
+    stripe_customer = stripe.Customer.retrieve(stripe_customer_id)
+    if PRINT_STRIPE_FIXTURE_DATA:
+        print(''.join(['"retrieve_customer": ', str(stripe_customer), ',']))  # nocoverage
+    return stripe_customer
 
 @catch_stripe_errors
-def save_stripe_token(user: UserProfile, token: str) -> int:
-    """Returns total number of cards."""
-    # The card metadata doesn't show up in Dashboard but can be accessed
-    # using the API.
-    card_metadata = {"added_user_id": user.id, "added_user_email": user.email}
-    try:
-        customer_obj = Customer.objects.get(realm=user.realm)
-        customer = stripe.Customer.retrieve(customer_obj.stripe_customer_id)
-        billing_logger.info("Adding card on customer %s: source=%r, metadata=%r",
-                            customer_obj.stripe_customer_id, token, card_metadata)
-        card = customer.sources.create(source=token, metadata=card_metadata)
-        customer.default_source = card.id
-        customer.save()
-        return len(customer.sources.list(object="card")["data"])
-    except Customer.DoesNotExist:
-        customer_metadata = {"string_id": user.realm.string_id}
-        # Description makes it easier to identify customers in Stripe dashboard
-        description = "{} ({})".format(user.realm.name, user.realm.string_id)
-        billing_logger.info("Creating customer: source=%r, description=%r, metadata=%r",
-                            token, description, customer_metadata)
-        customer = stripe.Customer.create(source=token,
-                                          description=description,
-                                          metadata=customer_metadata)
+def get_upcoming_invoice(stripe_customer_id: int) -> Any:
+    stripe_invoice = stripe.Invoice.upcoming(customer=stripe_customer_id)
+    if PRINT_STRIPE_FIXTURE_DATA:
+        print(''.join(['"upcoming_invoice": ', str(stripe_invoice), ',']))  # nocoverage
+    return stripe_invoice
 
-        card = customer.sources.list(object="card")["data"][0]
-        card.metadata = card_metadata
-        card.save()
-        Customer.objects.create(realm=user.realm, stripe_customer_id=customer.id)
-        return 1
+@catch_stripe_errors
+def payment_source(stripe_customer: Any) -> Any:
+    if stripe_customer.default_source is None:
+        return None  # nocoverage -- no way to get here yet
+    for source in stripe_customer.sources.data:
+        if source.id == stripe_customer.default_source:
+            return source
+    raise AssertionError("Default source not in sources.")
+
+@catch_stripe_errors
+def do_create_customer_with_payment_source(user: UserProfile, stripe_token: str) -> Customer:
+    realm = user.realm
+    stripe_customer = stripe.Customer.create(
+        description="%s (%s)" % (realm.string_id, realm.name),
+        metadata={'realm_id': realm.id, 'realm_str': realm.string_id},
+        source=stripe_token)
+    if PRINT_STRIPE_FIXTURE_DATA:
+        print(''.join(['"create_customer": ', str(stripe_customer), ',']))  # nocoverage
+    return Customer.objects.create(
+        realm=realm,
+        stripe_customer_id=stripe_customer.id,
+        billing_user=user)
+
+@catch_stripe_errors
+def do_subscribe_customer_to_plan(customer: Customer, stripe_plan_id: int,
+                                  seat_count: int, tax_percent: float) -> None:
+    stripe_subscription = stripe.Subscription.create(
+        customer=customer.stripe_customer_id,
+        billing='charge_automatically',
+        items=[{
+            'plan': stripe_plan_id,
+            'quantity': seat_count,
+        }],
+        prorate=True,
+        tax_percent=tax_percent)
+    if PRINT_STRIPE_FIXTURE_DATA:
+        print(''.join(['"create_subscription": ', str(stripe_subscription), ',']))  # nocoverage
