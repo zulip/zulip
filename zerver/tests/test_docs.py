e2f8bc9eac6beb55adb16d2aaef0e2e1919c92a7
Tim Abbott <tabbott@zulipchat.com>
2018-04-16 16:10:35 -0700

    /api: Fix tests for /api homepage.

--- zerver/tests/test_docs.py
+++ zerver/tests/test_docs.py
@@ -51,7 +51,7 @@ class DocPageTest(ZulipTestCase):
 
     @slow("Tests dozens of endpoints, including generating lots of emails")
     def test_doc_endpoints(self) -> None:
-        self._test('/api/', 'We hear you like APIs')
+        self._test('/api/', 'The Zulip API')
         self._test('/api/api-keys', 'you can use its email and API key')
         self._test('/api/installation-instructions', 'No download required!')
         self._test('/api/private-message', 'steal away your hearts')
