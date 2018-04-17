fc536679787cd5813800c3445d9a18dd122e814a
Armaan Ahluwalia <armaanahluwalia@gmail.com>
2018-04-18 01:29:17 +0530

    webpack: Upgrade webpack version 4.5.0.
    Upgrade webpack to latest version at the time of authoring. This
    involves upgrading webpack version and its loaders to compatible
    versions. It also involved editing tools/webpack to use the
    executable for webpack-cli instead because of a change in how the
    webpack package wants you to handle shell execution.
    It also fixes the confugration for TypeScript in the webpack config
    as that was previously broken. Including TypeScript files in JS
    files compiled by webpack now works.

--- tools/webpack.config.ts
+++ tools/webpack.config.ts
@@ -4,16 +4,10 @@ import * as webpack from 'webpack';
 
 const assets = require('./webpack.assets.json');
 
-// Only use the Webpack 2.0 Rule definitions.
-interface Config extends webpack.Configuration {
-  module: {
-    rules: webpack.NewUseRule[]
-  }
-}
-
-export default (env?: string) : Config => {
+export default (env?: string) : webpack.Configuration => {
     const production: boolean = env === "production";
-    let config: Config = {
+    let config: webpack.Configuration = {
+        mode: production ? "production" : "development",
         context: resolve(__dirname, "../"),
         entry: assets,
         module: {
@@ -21,20 +15,10 @@ export default (env?: string) : Config => {
                 // Run the typescript compilier on .ts files before webpack
                 {
                     test: /\.tsx?$/,
-                    use: 'ts-loader',
-                },
-                // This loads and transforms sourcemap files from other compiliers.
-                // The typescript comilier will generate a sourcemap and
-                // source-map-loader will output the correct sourcemap from that.
-                {
-                    enforce: 'pre',
-                    test: /\.js$/,
-                    use: "source-map-loader",
-                },
-                {
-                    enforce: 'pre',
-                    test: /\.tsx?$/,
-                    use: "source-map-loader",
+                    loader: 'ts-loader',
+                    options: {
+                        configFile: require.resolve('../static/ts/tsconfig.json')
+                    }
                 },
                 // Uses script-loader on minified files so we don't change global variables in them.
                 // Also has the effect of making processing these files fast
@@ -94,22 +78,16 @@ export default (env?: string) : Config => {
             new BundleTracker({filename: 'webpack-stats-production.json'}),
         ];
     } else {
-        // Built webpack dev asset reloader
-        config.entry['common'].unshift('webpack/hot/dev-server');
-        // Use 0.0.0.0 so that we can set a port but still use the host
-        // the browser is connected to.
-        config.entry['common'].unshift('webpack-dev-server/client?http://0.0.0.0:9994');
-
         // Out JS debugging tools
         config.entry['common'].push('./static/js/debug.js');
 
         config.output.publicPath = '/webpack/';
         config.plugins = [
             new BundleTracker({filename: 'var/webpack-stats-dev.json'}),
-            // Hot Reload of code in development
-            new webpack.HotModuleReplacementPlugin(),
             // Better logging from console for hot reload
             new webpack.NamedModulesPlugin(),
+            // Hot Reload of code in development
+            new webpack.HotModuleReplacementPlugin(),
             // script-loader should load sourceURL in dev
             new webpack.LoaderOptionsPlugin({debug: true}),
         ];
