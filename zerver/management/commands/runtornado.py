9f844ff681187b959fb4c11d4753f80f028a30b1
Tim Abbott <tabbott@zulipchat.com>
2018-04-17 14:06:30 -0700

    tornado: Fix logging of tornado activity level.
    This logging was apparently broken when sorting imports; it's a fairly
    unique thing in our codebase that this would be a problem.  Prevent
    future regressions by adding this exception explicitly to the isort
    configuration.

--- zerver/management/commands/runtornado.py
+++ zerver/management/commands/runtornado.py
@@ -9,21 +9,22 @@ from django.core.management.base import BaseCommand, \
 from tornado import ioloop
 from tornado.log import app_log
 
-from zerver.lib.debug import interactive_debug_listen
-from zerver.tornado.application import create_tornado_application, \
-    setup_tornado_rabbitmq
-from zerver.tornado.event_queue import add_client_gc_hook, \
-    missedmessage_hook, process_notification, setup_event_queue
 # We must call zerver.tornado.ioloop_logging.instrument_tornado_ioloop
 # before we import anything else from our project in order for our
 # Tornado load logging to work; otherwise we might accidentally import
 # zerver.lib.queue (which will instantiate the Tornado ioloop) before
 # this.
 from zerver.tornado.ioloop_logging import instrument_tornado_ioloop
-from zerver.tornado.socket import respond_send_message
 
 settings.RUNNING_INSIDE_TORNADO = True
 instrument_tornado_ioloop()
+
+from zerver.lib.debug import interactive_debug_listen
+from zerver.tornado.application import create_tornado_application, \
+    setup_tornado_rabbitmq
+from zerver.tornado.event_queue import add_client_gc_hook, \
+    missedmessage_hook, process_notification, setup_event_queue
+from zerver.tornado.socket import respond_send_message
 
 if settings.USING_RABBITMQ:
     from zerver.lib.queue import get_queue_client
