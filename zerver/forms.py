d9d97e087a76d9bbb618b0bda2433178dec69339
Preston Hansen <prestonhansen01@gmail.com>
2018-04-23 13:02:45 -0500

    slack import: Use less strict checks in `check_subdomain_available`.
    Fixes #9166.

--- zerver/forms.py
+++ zerver/forms.py
@@ -55,7 +55,7 @@ def email_is_not_mit_mailing_list(email: Text) -> None:
             else:
                 raise AssertionError("Unexpected DNS error")
 
-def check_subdomain_available(subdomain: str) -> None:
+def check_subdomain_available(subdomain: str, from_management_command: bool=False) -> None:
     error_strings = {
         'too short': _("Subdomain needs to have length 3 or greater."),
         'extremal dash': _("Subdomain cannot start or end with a '-'."),
@@ -70,6 +70,8 @@ def check_subdomain_available(subdomain: str) -> None:
         raise ValidationError(error_strings['extremal dash'])
     if not re.match('^[a-z0-9-]*$', subdomain):
         raise ValidationError(error_strings['bad character'])
+    if from_management_command:
+        return
     if len(subdomain) < 3:
         raise ValidationError(error_strings['too short'])
     if is_reserved_subdomain(subdomain) or \
