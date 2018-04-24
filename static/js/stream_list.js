97b9367d207502667eb740fd3f0d96eebba63904
Steve Howell <showell@zulipchat.com>
2018-04-24 12:09:16 +0000

    refactor: Extract scroll_util.js.
    We now use scroll_element_into_container() in two different
    places, so it's worth extracting.

--- static/js/stream_list.js
+++ static/js/stream_list.js
@@ -622,7 +622,7 @@ function keydown_arrow_key(dir_up, all_streams_selector, scroll_container) {
         current_sel.removeClass('highlighted_stream');
         next_sibling.addClass('highlighted_stream');
 
-        exports.scroll_element_into_container(next_sibling, scroll_container);
+        scroll_util.scroll_element_into_container(next_sibling, scroll_container);
     }
 }
 
@@ -692,64 +692,7 @@ exports.scroll_stream_into_view = function (stream_li) {
         return;
     }
 
-    exports.scroll_element_into_container(stream_li, container);
-};
-
-exports.scroll_delta = function (opts) {
-    var elem_top = opts.elem_top;
-    var container_height = opts.container_height;
-    var elem_bottom = opts.elem_bottom;
-
-    var delta = 0;
-
-    if (elem_top < 0) {
-        delta = Math.max(
-            elem_top,
-            elem_bottom - container_height
-        );
-        delta = Math.min(0, delta);
-    } else {
-        if (elem_bottom > container_height) {
-            delta = Math.min(
-                elem_top,
-                elem_bottom - container_height
-            );
-            delta = Math.max(0, delta);
-        }
-    }
-
-    return delta;
-};
-
-exports.scroll_element_into_container = function (elem, container) {
-    // This is a generic function to make elem visible in
-    // container by scrolling container appropriately.  We may want to
-    // eventually move this into another module, but I couldn't find
-    // an ideal landing space for this.  I considered a few modules, but
-    // some are already kind of bloated (ui.js), some may be deprecated
-    // (scroll_bar.js), and some just aren't exact fits (resize.js).
-    //
-    // This does the minimum amount of scrolling that is needed to make
-    // the element visible.  It doesn't try to center the element, so
-    // this will be non-intrusive to users when they already have
-    // the element visible.
-
-    var elem_top = elem.position().top;
-    var elem_bottom = elem_top + elem.height();
-
-    var opts = {
-        elem_top: elem_top,
-        elem_bottom: elem_bottom,
-        container_height: container.height(),
-    };
-
-    var delta = exports.scroll_delta(opts);
-
-    if (delta === 0) {
-        return;
-    }
-
-    container.scrollTop(container.scrollTop() + delta);
+    scroll_util.scroll_element_into_container(stream_li, container);
 };
 
 return exports;
