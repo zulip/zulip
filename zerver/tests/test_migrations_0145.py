1cd651f86b402400b5f90ea76de876214a24f251
Aditya Bansal <adi.bansal241996@gmail.com>
2018-04-25 05:52:42 +0530

    migration_test: Clean up test for 0145 a bit by using 'use_db_models'.

--- zerver/tests/test_migrations_0145.py
+++ zerver/tests/test_migrations_0145.py
@@ -1,31 +1,28 @@
-from zerver.lib.test_classes import (
-    MigrationsTestCase,
-)
+from zerver.lib.test_classes import MigrationsTestCase
+from zerver.lib.test_helpers import use_db_models, make_client
 from django.utils.timezone import now as timezone_now
 from django.db.migrations.state import StateApps
 from django.db.models.base import ModelBase
+
+from zerver.models import get_stream
 
 class EmojiName2IdTestCase(MigrationsTestCase):
 
     migrate_from = '0144_remove_realm_create_generic_bot_by_admins_only'
     migrate_to = '0145_reactions_realm_emoji_name_to_id'
 
+    @use_db_models
     def setUpBeforeMigration(self, apps: StateApps) -> None:
-        UserProfile = apps.get_model('zerver', 'UserProfile')
         Reaction = apps.get_model('zerver', 'Reaction')
         RealmEmoji = apps.get_model('zerver', 'RealmEmoji')
-        Stream = apps.get_model('zerver', 'Stream')
         Message = apps.get_model('zerver', 'Message')
         Recipient = apps.get_model('zerver', 'Recipient')
-        Realm = apps.get_model('zerver', 'Realm')
-        Client = apps.get_model('zerver', 'Client')
 
-        realm = Realm.objects.get(string_id='zulip')
-        sender = UserProfile.objects.select_related().get(email__iexact='iago@zulip.com'.strip(), realm=realm)
-        sending_client, _ = Client.objects.get_or_create(name="test suite")
+        sender = self.example_user('iago')
+        realm = sender.realm
+        sending_client = make_client(name="test suite")
         stream_name = 'Denmark'
-        stream = Stream.objects.select_related("realm").get(
-            name__iexact=stream_name.strip(), realm_id=realm.id)
+        stream = get_stream(stream_name, realm)
         subject = 'foo'
 
         def send_fake_message(message_content: str, stream: ModelBase) -> ModelBase:
@@ -36,19 +33,25 @@ class EmojiName2IdTestCase(MigrationsTestCase):
                                           content = message_content,
                                           pub_date = timezone_now(),
                                           sending_client = sending_client)
-
         message = send_fake_message('Test 1', stream)
 
+        # Create reactions for all the realm emoji's on the message we faked.
         for realm_emoji in RealmEmoji.objects.all():
             reaction = Reaction(user_profile=sender, message=message,
                                 emoji_name=realm_emoji.name, emoji_code=realm_emoji.name,
                                 reaction_type='realm_emoji')
             reaction.save()
+        realm_emoji_reactions_count = Reaction.objects.filter(reaction_type='realm_emoji').count()
+        self.assertEqual(realm_emoji_reactions_count, 1)
 
     def test_tags_migrated(self) -> None:
         Reaction = self.apps.get_model('zerver', 'Reaction')
         RealmEmoji = self.apps.get_model('zerver', 'RealmEmoji')
-        for reaction in Reaction.objects.filter(reaction_type='realm_emoji'):
+
+        realm_emoji_reactions = Reaction.objects.filter(reaction_type='realm_emoji')
+        realm_emoji_reactions_count = realm_emoji_reactions.count()
+        self.assertEqual(realm_emoji_reactions_count, 1)
+        for reaction in realm_emoji_reactions:
             realm_emoji = RealmEmoji.objects.get(
                 realm_id=reaction.user_profile.realm_id,
                 name=reaction.emoji_name)
