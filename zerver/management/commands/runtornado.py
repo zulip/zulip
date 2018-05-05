28f659f434ab3d9802c94d1769c3708b0fe37f24
Tim Abbott <tabbott@zulipchat.com>
2018-05-05 07:29:51 -0700

    more work

--- zerver/management/commands/runtornado.py
+++ zerver/management/commands/runtornado.py
@@ -19,6 +19,8 @@ from zerver.tornado.ioloop_logging import instrument_tornado_ioloop
 settings.RUNNING_INSIDE_TORNADO = True
 instrument_tornado_ioloop()
 
+instance = ioloop.IOLoop.current()
+
 from zerver.lib.debug import interactive_debug_listen
 from zerver.tornado.application import create_tornado_application, \
     setup_tornado_rabbitmq
@@ -105,8 +107,6 @@ class Command(BaseCommand):
                 setup_event_queue()
                 add_client_gc_hook(missedmessage_hook)
                 setup_tornado_rabbitmq()
-
-                instance = ioloop.IOLoop.current()
 
                 if django.conf.settings.DEBUG:
 #                    instance.set_blocking_log_threshold(5)
--- zerver/tornado/ioloop_logging.py
+++ zerver/tornado/ioloop_logging.py
@@ -14,34 +14,34 @@ from tornado.platform.asyncio import AsyncIOLoop
 # the default poll implementation.  We need to do this before any Tornado code
 # runs that might instantiate the default event loop.
 
-class InstrumentedAsyncIOLoop(AsyncIOLoop):
-    def __init__(self) -> None:
-        self._times = []  # type: List[Tuple[float, float]]
-        self._last_print = 0.0
+_times = []  # type: List[Tuple[float, float]]
+_last_print = 0.0
 
+class InstrumentedAsyncIOLoop(AsyncIOLoop):
     def _handle_events(self, fd, events):
+        global _last_print
+        global _times
         t0 = time.time()
         fileobj, handler_func = self.handlers[fd]
         handler_func(fileobj, events)
         t1 = time.time()
 
         # Log this datapoint and restrict our log to the past minute
-        self._times.append((t0, t1))
-        while self._times and self._times[0][0] < t1 - 60:
-            self._times.pop(0)
+        _times.append((t0, t1))
+        while _times and _times[0][0] < t1 - 60:
+            _times.pop(0)
 
         # Report (at most once every 5s) the percentage of time spent
         # outside poll
-        if self._times and t1 - self._last_print >= 5:
-            total = t1 - self._times[0][0]
-            doing_work = sum(b-a for a, b in self._times)
+        if _times and t1 - _last_print >= 5:
+            total = t1 - _times[0][0]
+            doing_work = sum(b-a for a, b in _times)
             if total > 0:
                 percent_busy = 100 * (doing_work / total)
                 if settings.PRODUCTION or True:
                     logging.info('Tornado %5.1f%% busy over the past %4.1f seconds'
                                  % (percent_busy, total))
-                    self._last_print = t1
+                    _last_print = t1
 
 def instrument_tornado_ioloop() -> None:
-    InstrumentedAsyncIOLoop.configure(InstrumentedAsyncIOLoop)
-
+    AsyncIOLoop._handle_events = InstrumentedAsyncIOLoop._handle_events
