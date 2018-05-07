dc0696af745e6ca8dbb9b6e5191d56e682b01ec9
Tim Abbott <tabbott@zulipchat.com>
2018-05-06 21:51:11 -0700

    puppet: Ensure zulip user owns key /etc/zulip files.
    The main purpose of this change is to make it guaranteed that
    `manage.py register_server --rotate-key` can edit the
    /etc/zulip/zulip-secrets.conf configuration via crudini.
    
    But it also adds value by ensuring zulip-secrets.conf is not readable
    by other users.

--- puppet/zulip/manifests/base.pp
+++ puppet/zulip/manifests/base.pp
@@ -97,6 +97,20 @@ class zulip::base {
     owner  => 'zulip',
     group  => 'zulip',
   }
+  file { ['/etc/zulip/zulip.conf', '/etc/zulip/settings.py']:
+    ensure => 'file',
+    require => File['/etc/zulip'],
+    mode   => '0644',
+    owner  => 'zulip',
+    group  => 'zulip',
+  }
+  file { '/etc/zulip/zulip-secrets.conf':
+    ensure => 'file',
+    require => File['/etc/zulip'],
+    mode   => '0640',
+    owner  => 'zulip',
+    group  => 'zulip',
+  }
 
   file { '/etc/security/limits.conf':
     ensure => file,
