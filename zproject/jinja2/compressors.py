99ef5cf039719bbec68c855ed0a7f641df3e00d7
Aditya Bansal <adi.bansal241996@gmail.com>
2018-05-10 22:23:55 +0530

    zproject: Change use of typing.Text to str.

--- zproject/jinja2/compressors.py
+++ zproject/jinja2/compressors.py
@@ -2,15 +2,13 @@
 `minified_js` is taken from `zerver.templatetags.minified_js.py`
 """
 
-from typing import Text
-
 from django.conf import settings
 from django.template import TemplateSyntaxError
 
 from zerver.templatetags.minified_js import MinifiedJSNode
 
 
-def minified_js(sourcefile: str, csp_nonce: str) -> Text:
+def minified_js(sourcefile: str, csp_nonce: str) -> str:
     if sourcefile not in settings.JS_SPECS:
         raise TemplateSyntaxError(
             "Invalid argument: no JS file %s".format(sourcefile))
