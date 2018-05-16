afe09071b99ebe08499838172d8608a003e6fe9d
Joshua Pan <joshuapan8@gmail.com>
2018-05-16 15:57:24 -0400

    slash-commands: Implement /day and /night.

--- zerver/lib/widget.py
+++ zerver/lib/widget.py
@@ -11,9 +11,24 @@ def do_widget_pre_save_actions(message: MutableMapping[str, Any]) -> None:
     if not settings.ALLOW_SUB_MESSAGES:
         return
 
+    # this prevents errors of cyclical imports
+    from zerver.lib.actions import do_set_user_display_setting
     content = message['message'].content
+    user_profile = message['message'].sender
+
     if content == '/stats':
         message['message'].content = 'We are running **1 server**.'
+        return
+
+    if content == '/night':
+        message['message'].content = 'Changed to night mode! To revert night mode, type `/day`.'
+        do_set_user_display_setting(user_profile, 'night_mode', True)
+        return
+
+    if content == '/day':
+        message['message'].content = 'Changed to day mode! To revert day mode, type `/night`.'
+        do_set_user_display_setting(user_profile, 'night_mode', False)
+        return
 
 def do_widget_post_save_actions(message: MutableMapping[str, Any]) -> None:
     '''
