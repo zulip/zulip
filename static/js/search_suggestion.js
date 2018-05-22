e5b3fb332bc65a258fdec27b8b9e4a26a0edd4cd
Shubham Padia <shubhamapadia@gmail.com>
2018-05-22 18:07:51 +0530

    search: Do not show default suggestion for `is` operator.
    Fixes #9492.
    Default suggestion e.g `abc messages` as a suggestion for `is:abc`
    is not shown in a new suggestion. But if the is operator is already
    present before any other operator, the default message text will be
    used. e.g `is:abc sender:abc@zulipchat.com` will have all the suggestions
    with the prefix `abc messages, sent by abc@zulipchat.com`.

--- static/js/search_suggestion.js
+++ static/js/search_suggestion.js
@@ -581,10 +581,10 @@ exports.get_suggestions = function (query) {
     }
 
     // Display the default first
-    // `has` operator works only on predefined categories. Default suggestion
+    // `has` and `is` operators work only on predefined categories. Default suggestion
     // is not displayed in that case. e.g. `messages with one or more abc` as
     // a suggestion for `has:abc`does not make sense.
-    if (last.operator !== '' && last.operator !== 'has') {
+    if (last.operator !== '' && last.operator !== 'has' && last.operator !== 'is') {
         suggestion = get_default_suggestion(operators);
         result = [suggestion];
     }
