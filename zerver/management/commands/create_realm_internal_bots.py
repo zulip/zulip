d343f25cc6daae0faa849cb428ba5f751825141e
Aditya Bansal <adi.bansal241996@gmail.com>
2018-05-23 09:10:28 +0530

    create_realm_internal_bots: Refactor to extract main op as a function.
    We extract the entire operations of the management command to a
    function create_if_missing_realm_internal_bots in the
    zerver/lib/onboarding.py. The logic for determining if there are any realm
    internal bots which have not been created is extracted to a function
    missing_any_realm_internal_bots in actions.py.

--- zerver/management/commands/create_realm_internal_bots.py
+++ zerver/management/commands/create_realm_internal_bots.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db.models import Count
 
-from zerver.lib.onboarding import setup_realm_internal_bots
+from zerver.lib.onboarding import create_if_missing_realm_internal_bots
 from zerver.models import Realm, UserProfile
 
 class Command(BaseCommand):
@@ -16,19 +16,7 @@ These are normally created when the realm is, so this should be a no-op
 except when upgrading to a version that adds a new realm internal bot.
 """
 
-    @staticmethod
-    def missing_any_bots() -> bool:
-        bot_emails = [bot['email_template'] % (settings.INTERNAL_BOT_DOMAIN,)
-                      for bot in settings.REALM_INTERNAL_BOTS]
-        bot_counts = dict(UserProfile.objects.filter(email__in=bot_emails)
-                                             .values_list('email')
-                                             .annotate(Count('id')))
-        realm_count = Realm.objects.count()
-        return any(bot_counts.get(email, 0) < realm_count for email in bot_emails)
-
     def handle(self, *args: Any, **options: Any) -> None:
-        if self.missing_any_bots():
-            for realm in Realm.objects.all():
-                setup_realm_internal_bots(realm)
-                # create_users is idempotent -- it's a no-op when a given email
-                # already has a user in a given realm.
+        create_if_missing_realm_internal_bots()
+        # create_users is idempotent -- it's a no-op when a given email
+        # already has a user in a given realm.
