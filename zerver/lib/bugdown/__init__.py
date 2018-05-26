445932cc8613c77ced023125248c8b966b3b7528
Tim Abbott <tabbott@zulipchat.com>
2018-05-26 10:11:32 -0700

    bugdown: Increase limit for number of digits in tweet IDs.
    Apprently, they just reached 19 digits.  We set the limit to 30 to
    give plenty of headroom for the future.

--- zerver/lib/bugdown/__init__.py
+++ zerver/lib/bugdown/__init__.py
@@ -403,7 +403,7 @@ def get_tweet_id(url: str) -> Optional[str]:
     if parsed_url.path == '/' and len(parsed_url.fragment) > 5:
         to_match = parsed_url.fragment
 
-    tweet_id_match = re.match(r'^!?/.*?/status(es)?/(?P<tweetid>\d{10,18})(/photo/[0-9])?/?$', to_match)
+    tweet_id_match = re.match(r'^!?/.*?/status(es)?/(?P<tweetid>\d{10,30})(/photo/[0-9])?/?$', to_match)
     if not tweet_id_match:
         return None
     return tweet_id_match.group("tweetid")
