7ccefc3e5dc2b0213cdc45e0edfbfe99b3bffa97
Tim Abbott <tabbott@zulipchat.com>
2018-05-29 07:36:53 -0700

    migrations: Remove dependence on PRIVATE_STREAM_HISTORY_FOR_SUBSCRIBERS.
    We don't want to keep around a declaration of
    PRIVATE_STREAM_HISTORY_FOR_SUBSCRIBERS forever, so we should just move
    this to a getattr; if the user has set it on their server, we'll use
    the value; otherwise, we just use False.

--- zerver/migrations/0164_stream_history_public_to_subscribers.py
+++ zerver/migrations/0164_stream_history_public_to_subscribers.py
@@ -15,7 +15,7 @@ def set_initial_value_for_history_public_to_subscribers(
 
     for stream in streams:
         if stream.invite_only:
-            stream.history_public_to_subscribers = settings.PRIVATE_STREAM_HISTORY_FOR_SUBSCRIBERS
+            stream.history_public_to_subscribers = getattr(settings, 'PRIVATE_STREAM_HISTORY_FOR_SUBSCRIBERS', False)
         else:
             stream.history_public_to_subscribers = True
 
