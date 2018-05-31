e95f972e24680e9be32789dd9da199cca4ebe390
Sampriti Panda <sampdingo@hotmail.com>
2018-05-31 06:04:15 +0530

    migrate: Add do_batch_update method for running batch updates.
    * Refactor pgroonga_0002 migration to use new method.

--- zerver/lib/migrate.py
+++ zerver/lib/migrate.py
@@ -1,7 +1,11 @@
-from typing import Any, Callable, Dict, List, Tuple
 from django.db.models.query import QuerySet
+from psycopg2.extensions import cursor
+from typing import Any, Callable, Dict, List, Tuple, TypeVar
+
 import re
 import time
+
+CursorObj = TypeVar('CursorObj', bound=cursor)
 
 def create_index_if_not_exist(index_name: str, table_name: str, column_string: str,
                               where_clause: str) -> str:
@@ -25,3 +29,43 @@ def create_index_if_not_exist(index_name: str, table_name: str, column_string: s
         END$$;
         ''' % (index_name, index_name, table_name, column_string, where_clause)
     return stmt
+
+
+def do_batch_update(cursor: CursorObj,
+                    table: str,
+                    cols: List[str],
+                    vals: List[str],
+                    batch_size: int=10000,
+                    sleep: float=0.1,
+                    escape: bool=True) -> None:  # nocoverage
+    stmt = '''
+        UPDATE %s
+        SET (%s) = (%s)
+        WHERE id >= %%s AND id < %%s
+    ''' % (table, ', '.join(cols), ', '.join(['%s'] * len(cols)))
+
+    cursor.execute("SELECT MIN(id), MAX(id) FROM %s" % (table,))
+    (min_id, max_id) = cursor.fetchall()[0]
+    if min_id is None:
+        return
+
+    print("\n    Range of rows to update: [%s, %s]" % (min_id, max_id))
+    while min_id <= max_id:
+        lower = min_id
+        upper = min_id + batch_size
+        print('    Updating range [%s,%s)' % (lower, upper))
+        params = list(vals) + [lower, upper]
+        if escape:
+            cursor.execute(stmt, params=params)
+        else:
+            cursor.execute(stmt % tuple(params))
+
+        min_id = upper
+        time.sleep(sleep)
+
+        # Once we've finished, check if any new rows were inserted to the table
+        if min_id > max_id:
+            cursor.execute("SELECT MAX(id) FROM %s" % (table,))
+            max_id = cursor.fetchall()[0][0]
+
+    print("    Finishing...", end='')
