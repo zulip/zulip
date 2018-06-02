7813376934ef1f07bc5cbb58195932e4c59254fa
Tim Abbott <tabbott@zulipchat.com>
2018-06-01 17:19:12 -0700

    webpack: Use filenames based on hashes.
    This should avoid us creating duplicate webpack bundles every time we
    do a deployment, even if none of the files in the bundles themselves
    have changed at all.

--- tools/webpack.config.ts
+++ tools/webpack.config.ts
@@ -119,7 +119,7 @@ export default (env?: string) : webpack.Configuration => {
         },
         output: {
             path: resolve(__dirname, '../static/webpack-bundles'),
-            filename: production ? '[name]-[hash].js' : '[name].js',
+            filename: production ? '[name]-[chunkhash].js' : '[name].js',
         },
         resolve: {
             extensions: [".tsx", ".ts", ".js", ".json", ".scss", ".css"],
@@ -145,7 +145,7 @@ export default (env?: string) : webpack.Configuration => {
                     }
                     return '[name].[contenthash].css';
                 },
-                chunkFilename: "[id].css"
+                chunkFilename: "[chunkhash].css"
             })
         ];
     } else {
@@ -162,7 +162,7 @@ export default (env?: string) : webpack.Configuration => {
             // Extract CSS from files
             new MiniCssExtractPlugin({
                 filename: "[name].css",
-                chunkFilename: "[id].css"
+                chunkFilename: "[chunkhash].css"
             }),
         ];
 
