beecee4b8cbf5a9c99904bedc87af72c02f8a69e
Steve Howell <showell@zulipchat.com>
2018-06-03 15:59:33 +0000

    Move panel click handler into settings_panel_menu.js.
    This is mostly a code move, but because things are more
    modular now, we don't need the two conditionals to find
    out what kind of panel menu we're inside of, and our
    selectors are less brittle.

--- static/js/settings_panel_menu.js
+++ static/js/settings_panel_menu.js
@@ -4,6 +4,8 @@ var exports = {};
 
 exports.make_menu = function (opts) {
     var main_elem = opts.main_elem;
+    var hash_prefix = opts.hash_prefix;
+    var load_section = opts.load_section;
 
     var self = {};
 
@@ -19,15 +21,50 @@ exports.make_menu = function (opts) {
         main_elem.hide();
     };
 
+    self.activate_section = function (opts) {
+        var li_elem = opts.li_elem;
+        var section = li_elem.data('section');
+
+        main_elem.children("li").removeClass("active no-border");
+        li_elem.addClass("active");
+        li_elem.prev().addClass("no-border");
+        window.location.hash = hash_prefix + section;
+
+        $(".settings-section, .settings-wrapper").removeClass("show");
+
+        ui.update_scrollbar($("#settings_content"));
+
+        load_section(section);
+
+        var sel = "[data-name='" + section + "']";
+        $(".settings-section" + sel + ", .settings-wrapper" + sel).addClass("show");
+    };
+
+    main_elem.on("click", "li[data-section]", function (e) {
+        self.activate_section({
+            li_elem: $(this),
+        });
+
+        e.stopPropagation();
+    });
+
     return self;
 };
 
 exports.initialize = function () {
     exports.normal_settings = exports.make_menu({
         main_elem: $('.normal-settings-list'),
+        hash_prefix: "settings/",
+        load_section: function (section) {
+            settings_sections.load_settings_section(section);
+        },
     });
     exports.org_settings = exports.make_menu({
         main_elem: $('.org-settings-list'),
+        hash_prefix: "organization/",
+        load_section: function (section) {
+            admin_sections.load_admin_section(section);
+        },
     });
 };
 
