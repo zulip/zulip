12b5be8050b33ee2fef598a41512e05f29c7d5a0
Shubham Padia <shubhamapadia@gmail.com>
2018-06-08 13:46:04 +0530

    search: Add search_pill_widget.js.
    Also adds the file to the static asset pipeline.
    search_pill_widget.js will be used to access the pills object for
    the search query box. It will act in a similar way to
    compose_pm_pill.js. Why is this needed: Consider you've initiated
    a pills object in search.js for the search query box. Now you want to
    also access that pills object to pre-populate pills after a reload in
    hashchange.js. search_pill_widget.js makes this easy without the use
    of events.

--- static/js/bundles/app.js
+++ static/js/bundles/app.js
@@ -191,6 +191,7 @@ import "js/compose_ui.js";
 import "js/panels.js";
 import 'js/settings_ui.js';
 import 'js/search_pill.js';
+import 'js/search_pill_widget.js';
 
 // Import Styles
 
---
+++ static/js/search_pill_widget.js
@@ -0,0 +1,11 @@
+var search_pill_widget = (function () {
+
+var exports = {};
+return exports;
+}());
+
+if (typeof module !== 'undefined') {
+    module.exports = search_pill_widget;
+}
+
+window.search_pill_widget = search_pill_widget;
