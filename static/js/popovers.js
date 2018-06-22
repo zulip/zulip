a679266009b687a32ba2414c4901f9da5f5c5659
Joshua Pan <joshuapan8@gmail.com>
2018-06-22 12:16:59 -0700

    Hide emails in popover content.

--- static/js/popovers.js
+++ static/js/popovers.js
@@ -153,6 +153,7 @@ function show_user_info_popover(element, user, message) {
             is_bot: user.is_bot,
             is_sender_popover: message.sender_id === user.user_id,
             show_user_profile: !(user.is_bot || page_params.custom_profile_fields.length === 0),
+            display_email: page_params.realm_display_email,
         };
 
 
@@ -703,6 +704,7 @@ exports.register_click_handlers = function () {
             is_bot: user.is_bot,
             is_sender_popover: false,
             show_user_profile: !user.is_bot && page_params.custom_profile_fields,
+            display_email: page_params.realm_display_email,
         };
 
         target.popover({
