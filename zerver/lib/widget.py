b22d2666679df16f5e9f71ce03cb69d18ff4e278
Rhea Parekh <rheaparekh12@gmail.com>
2018-06-29 00:47:55 +0530

    widgets: Add question for poll widget in the message itself.
    Use the command '/poll question?', to start a question.

--- zerver/lib/widget.py
+++ zerver/lib/widget.py
@@ -1,4 +1,4 @@
-from typing import MutableMapping, Any
+from typing import MutableMapping, Any, Optional, List, Tuple
 from django.conf import settings
 
 import re
@@ -17,6 +17,32 @@ def do_widget_pre_save_actions(message: MutableMapping[str, Any]) -> None:
         message['message'].content = 'We are running **1 server**.'
         return
 
+def get_widget_data(content: str) -> Tuple[Optional[str], Optional[str]]:
+    valid_widget_types = ['tictactoe', 'poll']
+    tokens = content.split(' ')
+    if not tokens:
+        return None, None
+
+    if tokens[0].startswith('/'):
+        widget_type = tokens[0][1:]
+        if widget_type in valid_widget_types:
+            extra_data = get_extra_data_from_widget_type(tokens, widget_type)
+            return widget_type, extra_data
+
+    return None, None
+
+def get_extra_data_from_widget_type(tokens: List[str],
+                                    widget_type: Optional[str]) -> Any:
+    if widget_type == 'poll':
+        # This is used to extract the question from the poll command.
+        # The command '/poll question' will pre-set the question in the poll
+        question = ' '.join(tokens[1:])
+        if not question:
+            question = ''
+        extra_data = {'question': question}
+        return extra_data
+    return None
+
 def do_widget_post_save_actions(message: MutableMapping[str, Any]) -> None:
     '''
     This is experimental code that only works with the
@@ -30,9 +56,8 @@ def do_widget_post_save_actions(message: MutableMapping[str, Any]) -> None:
 
     widget_type = None
     extra_data = None
-    if content in ['/poll', '/tictactoe']:
-        widget_type = content[1:]
 
+    widget_type, extra_data = get_widget_data(content)
     widget_content = message.get('widget_content')
     if widget_content is not None:
         # Note that we validate this data in check_message,
