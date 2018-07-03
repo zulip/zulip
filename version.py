a1a36e79712f43c7655121da496a11ddd0d3288d
Anupam Dagar <dagaranupam@gmail.com>
2018-07-03 06:26:51 +0000

    emails: Add preheader to "new login" emails.
    This makes it easier to see what the new login was about in the previews.
    
    Fixes: #9866.

--- version.py
+++ version.py
@@ -8,4 +8,4 @@ ZULIP_VERSION = "1.8.1+git"
 # Typically, adding a dependency only requires a minor version bump, and
 # removing a dependency requires a major version bump.
 
-PROVISION_VERSION = '22.0'
+PROVISION_VERSION = '22.1'
