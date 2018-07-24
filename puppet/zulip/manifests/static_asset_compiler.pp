74a3e9e6bde109b718f12c23f8d4fd05ffb8aa3d
Priyank Patel <priyankp390@gmail.com>
2018-07-24 15:47:43 +0000

    puppet: remove closure-compiler from static_asset_compiler.

--- puppet/zulip/manifests/static_asset_compiler.pp
+++ puppet/zulip/manifests/static_asset_compiler.pp
@@ -1,12 +1,6 @@
 class zulip::static_asset_compiler {
-  if $zulip::base::release_name == 'trusty' {
-    $closure_compiler_package = 'libclosure-compiler-java'
-  } else {
-    $closure_compiler_package = 'closure-compiler'
-  }
   $static_asset_compiler_packages = [
-    # Needed for minify-js
-    $closure_compiler_package,
+    # Needed for minifying js
     'yui-compressor',
     # Used by makemessages i18n
     'gettext',
