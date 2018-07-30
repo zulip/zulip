7853254df74d4e3c875d878566df69b9478b7674
Tim Abbott <tabbott@zulipchat.com>
2018-07-30 16:11:46 -0700

    puppet: Run thumbor by default on voyager systems.
    With this change, all that one needs to do to start using thumbor in
    production is to set the `THUMBOR_URL` setting.
    
    Since without THUMBOR_URL enabled, the thumbor service doesn't
    actually do anything, this is pretty safe.

--- puppet/zulip/manifests/dockervoyager.pp
+++ puppet/zulip/manifests/dockervoyager.pp
@@ -9,6 +9,7 @@ class zulip::dockervoyager {
   include zulip::app_frontend
   include zulip::supervisor
   include zulip::process_fts_updates
+  include zulip::thumbor
 
   file { '/etc/supervisor/conf.d/cron.conf':
     ensure  => file,
