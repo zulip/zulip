2fcc54ac14d1b8996a5c6a11dba509c29c802a03
Joshua Pan <joshuapan8@gmail.com>
2018-08-01 10:02:38 -0700

    ui: Rename update_starred to update_starred_view.

--- static/js/message_flags.js
+++ static/js/message_flags.js
@@ -80,7 +80,7 @@ exports.save_uncollapsed = function (message) {
 exports.update_starred_flag = function (message_id, new_value) {
     var message = message_store.get(message_id);
     message.starred = new_value;
-    ui.update_starred(message_id, new_value);
+    ui.update_starred_view(message_id, new_value);
 };
 
 exports.toggle_starred = function (message) {
@@ -95,7 +95,7 @@ exports.toggle_starred = function (message) {
     message.starred = !message.starred;
 
     unread_ops.notify_server_message_read(message);
-    ui.update_starred(message.id, message.starred);
+    ui.update_starred_view(message.id, message.starred);
 
     if (message.starred) {
         send_flag_update(message, 'starred', 'add');
