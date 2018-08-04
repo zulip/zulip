14fc82f2c5c3dd99b3e240e3b099e2788879cd2d
Tim Abbott <tabbott@zulipchat.com>
2018-08-04 14:26:25 -0700

    lint: Extract run_parallel and do_lint.

--- tools/zulint/command.py
+++ tools/zulint/command.py
@@ -4,6 +4,11 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 import argparse
+import logging
+import os
+import sys
+
+from typing import Any, List
 
 def add_default_linter_arguments(parser):
     # type: (argparse.ArgumentParser) -> None
@@ -16,3 +21,29 @@ def add_default_linter_arguments(parser):
     parser.add_argument('targets',
                         nargs='*',
                         help='Specify directories to check')
+
+def run_parallel(lint_functions):
+    # type: (Dict[str, Callable[[], int]]) -> bool
+    pids = []
+    for name, func in lint_functions.items():
+        pid = os.fork()
+        if pid == 0:
+            logging.info("start " + name)
+            result = func()
+            logging.info("finish " + name)
+            sys.stdout.flush()
+            sys.stderr.flush()
+            os._exit(result)
+        pids.append(pid)
+    failed = False
+
+    for pid in pids:
+        (_, status) = os.waitpid(pid, 0)
+        if status != 0:
+            failed = True
+    return failed
+
+def do_lint(lint_functions):
+    # type: List[Any] -> None
+    failed = run_parallel(lint_functions)
+    sys.exit(1 if failed else 0)
