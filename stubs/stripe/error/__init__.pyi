e804c563a8b1e3b4d82050b76d3d075bb3ed7498
Rishi Gupta <rishig@zulipchat.com>
2018-08-06 17:07:26 -0400

    billing: Update stripe error handling code.

--- stubs/stripe/error/__init__.pyi
+++ stubs/stripe/error/__init__.pyi
@@ -1,8 +1,22 @@
+from typing import Any, Dict
+
+# List of StripeError's from https://stripe.com/docs/api/python#error_handling
+
 class StripeError(Exception):
     http_status: str
+    json_body: Dict[str, Any]
 
-class CardError(Exception):
-    http_status: str
+class CardError(StripeError):
+    ...
 
-class InvalidRequestError(Exception):
+class RateLimitError(StripeError):
+    ...
+
+class InvalidRequestError(StripeError):
+    ...
+
+class AuthenticationError(StripeError):
+    ...
+
+class APIConnectionError(StripeError):
     ...
