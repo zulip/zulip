a3b8ab46b311c0a97d8775e2ae2c4948702a6fa7
Aditya Bansal <adi.bansal241996@gmail.com>
2018-08-07 17:56:46 +0530

    puppet-lint: Include in linter tool chain to run by default.
    We start to use puppet-lint to lint puppet modules by default by
    adding it to tools/lint (which controls our linter tool chain).
    
    We also define a few puppet-lint rules to exclude.
    
    Fixes: #9185.

--- tools/linter_lib/exclude.py
+++ tools/linter_lib/exclude.py
@@ -3,9 +3,21 @@ EXCLUDED_FILES = [
     # Third-party code that doesn't match our style
     "puppet/apt/.forge-release",
     "puppet/apt/README.md",
+    "puppet/apt/manifests/backports.pp",
+    "puppet/apt/manifests/params.pp",
+    "puppet/apt/manifests/unattended_upgrades.pp",
+    "puppet/stdlib/tests/file_line.pp",
     "static/third",
     # Transifex syncs translation.json files without trailing
     # newlines; there's nothing other than trailing newlines we'd be
     # checking for in these anyway.
     "static/locale",
+]
+
+PUPPET_CHECK_RULES_TO_EXCLUDE = [
+    "--no-arrow_alignment-check",
+    "--no-double_quoted_strings-check",
+    "--no-2sp_soft_tabs-check",
+    "--no-documentation-check",
+    "--no-80chars-check",
 ]
