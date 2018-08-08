17d81d46dcfe63439e241f454f1a8397d8179d05
Shubham Padia <shubhamapadia@gmail.com>
2018-08-08 19:46:40 +0530

    Realm filters: Allow `#`(hash) based URLs in filter patterns.
    Fixes #9817.
    Use case: When people try to add a filter to a site that uses
    a hash based router, which makes the URL like:
    `http://some-site.domain.com/#/c/$(id)s`.

--- zerver/models.py
+++ zerver/models.py
@@ -548,7 +548,7 @@ def filter_pattern_validator(value: str) -> None:
         raise ValidationError(error_msg)
 
 def filter_format_validator(value: str) -> None:
-    regex = re.compile(r'^[\.\/:a-zA-Z0-9_?=-]+%\(([a-zA-Z0-9_-]+)\)s[a-zA-Z0-9_-]*$')
+    regex = re.compile(r'^[\.\/:a-zA-Z0-9#_?=-]+%\(([a-zA-Z0-9_-]+)\)s[a-zA-Z0-9_-]*$')
 
     if not regex.match(value):
         raise ValidationError('URL format string must be in the following format: '
