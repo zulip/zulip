4d03c1584844717baaedfc238badfcde4f9bbf34
Tim Abbott <tabbott@zulipchat.com>
2018-08-08 13:24:20 -0700

    url_preview: Don't import beautifulsoup at import time.
    This is a small performance optimization to Django startup, in line
    with other recent commits.

--- zerver/lib/url_preview/parsers/base.py
+++ zerver/lib/url_preview/parsers/base.py
@@ -1,9 +1,11 @@
 from typing import Any
-from bs4 import BeautifulSoup
-
 
 class BaseParser:
     def __init__(self, html_source: str) -> None:
+        # We import BeautifulSoup here, because it's not used by most
+        # processes in production, and bs4 is big enough that
+        # importing it adds 10s of milliseconds to manage.py startup.
+        from bs4 import BeautifulSoup
         self._soup = BeautifulSoup(html_source, "lxml")
 
     def extract_data(self) -> Any:
