zulip/zulip

commit 23246ff8169ab7982836fcd6034ec37c390c9dde

do_update_pointer: Switch to using where_unread().
This produces a more efficient database query (just because postgres
doesn't use the right index by default the other way).

zerver/lib/actions.py
@@ -3546,8 +3546,7 @@ def do_update_pointer(user_profile: UserProfile, client: Client,
         # up until the pointer move
         UserMessage.objects.filter(user_profile=user_profile,
                                    message__id__gt=prev_pointer,
-                                   message__id__lte=pointer,
-                                   flags=~UserMessage.flags.read)        \
+                                   message__id__lte=pointer).extra([UserMessage.where_unread()]) \
                            .update(flags=F('flags').bitor(UserMessage.flags.read))
 
     event = dict(type='pointer', pointer=pointer)

commit 8ba726d47ddf275bde15bdeb5018b055abb7bf0c

do_update_pointer: Fix missing where= declaration.
Fixes a regression introduced in 23246ff8169ab7982836fcd6034ec37c390c9dde.

However, we'll be shortly removing this feature, since it's legacy
support for an app that no longer is supported.

zerver/lib/actions.py
@@ -3549,8 +3549,9 @@ def do_update_pointer(user_profile: UserProfile, client: Client,
         # up until the pointer move
         UserMessage.objects.filter(user_profile=user_profile,
                                    message__id__gt=prev_pointer,
-                                   message__id__lte=pointer).extra([UserMessage.where_unread()]) \
-                           .update(flags=F('flags').bitor(UserMessage.flags.read))
+                                   message__id__lte=pointer).extra(
+                                       where=[UserMessage.where_unread()]).update(
+                                           flags=F('flags').bitor(UserMessage.flags.read))
 
     event = dict(type='pointer', pointer=pointer)
     send_event(event, [user_profile.id])