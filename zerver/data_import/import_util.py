9617b1fbc5346588f8acd06b1ad7166292ecdefe
Rhea Parekh <rheaparekh12@gmail.com>
2018-08-17 04:21:32 +0530

    import: Build Recipient and Subscription with model class.

--- zerver/data_import/import_util.py
+++ zerver/data_import/import_util.py
@@ -7,7 +7,7 @@ import os
 from typing import List, Dict, Any
 from django.forms.models import model_to_dict
 
-from zerver.models import Realm, RealmEmoji
+from zerver.models import Realm, RealmEmoji, Subscription, Recipient
 from zerver.lib.actions import STREAM_ASSIGNMENT_COLORS as stream_colors
 from zerver.lib.avatar_hash import user_avatar_path_from_ids
 from zerver.lib.parallel import run_parallel
@@ -40,26 +40,21 @@ def build_avatar(zulip_user_id: int, realm_id: int, email: str, avatar_url: str,
 
 def build_subscription(recipient_id: int, user_id: int,
                        subscription_id: int) -> ZerverFieldsT:
-    subscription = dict(
-        recipient=recipient_id,
+    subscription = Subscription(
         color=random.choice(stream_colors),
-        audible_notifications=True,
-        push_notifications=False,
-        email_notifications=False,
-        desktop_notifications=True,
-        pin_to_top=False,
-        in_home_view=True,
-        active=True,
-        user_profile=user_id,
         id=subscription_id)
-    return subscription
+    subscription_dict = model_to_dict(subscription, exclude=['user_profile', 'recipient_id'])
+    subscription_dict['user_profile'] = user_id
+    subscription_dict['recipient'] = recipient_id
+    return subscription_dict
 
 def build_recipient(type_id: int, recipient_id: int, type: int) -> ZerverFieldsT:
-    recipient = dict(
+    recipient = Recipient(
         type_id=type_id,  # stream id
         id=recipient_id,
         type=type)
-    return recipient
+    recipient_dict = model_to_dict(recipient)
+    return recipient_dict
 
 def build_realm(zerver_realm: List[ZerverFieldsT], realm_id: int,
                 domain_name: str) -> ZerverFieldsT:
