7dc1b591cbafb8afd5746a320b4628aef1d91fca
Aditya Bansal <adi.bansal241996@gmail.com>
2018-10-25 04:43:35 +0530

    camo: Extract generate_camo_url out from get_camo_url.
    This will help us eliminate camo from our production installs.
    Basically it helps us de duplicate some code from upcoming code
    which will help us check validity of a camo url.

--- zerver/lib/camo.py
+++ zerver/lib/camo.py
@@ -3,14 +3,17 @@ import codecs
 import hashlib
 import hmac
 
+def generate_camo_url(url: str) -> str:
+    encoded_url = url.encode("utf-8")
+    encoded_camo_key = settings.CAMO_KEY.encode("utf-8")
+    digest = hmac.new(encoded_camo_key, encoded_url, hashlib.sha1).hexdigest()
+    hex_encoded_url = codecs.encode(encoded_url, "hex")  # type: ignore # https://github.com/python/typeshed/issues/300
+    return "%s/%s" % (digest, hex_encoded_url.decode("utf-8"))
+
 # Encodes the provided URL using the same algorithm used by the camo
 # caching https image proxy
 def get_camo_url(url: str) -> str:
     # Only encode the url if Camo is enabled
     if settings.CAMO_URI == '':
         return url
-    encoded_url = url.encode("utf-8")
-    encoded_camo_key = settings.CAMO_KEY.encode("utf-8")
-    digest = hmac.new(encoded_camo_key, encoded_url, hashlib.sha1).hexdigest()
-    hex_encoded_url = codecs.encode(encoded_url, "hex")  # type: ignore # https://github.com/python/typeshed/issues/300
-    return "%s%s/%s" % (settings.CAMO_URI, digest, hex_encoded_url.decode("utf-8"))
+    return "%s%s" % (settings.CAMO_URI, generate_camo_url(url))
