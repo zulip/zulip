2f58eb1057a1713729a063c1870368e488016210
Steve Howell <showell@zulipchat.com>
2018-10-28 15:48:52 +0000

    slack import: Extract process_message_files().
    This is mostly an extraction, but it does change the
    way we calculate `content`.  We append the markdown
    links from ALL files to any content that came in the
    message itself.
    
    Separating this out also allows us to add more
    test coverage for the extracted code.

--- zerver/data_import/slack.py
+++ zerver/data_import/slack.py
@@ -514,7 +514,8 @@ def get_all_messages(slack_data_dir: str, added_channels: AddedChannelsT) -> Lis
             all_messages += messages
     return all_messages
 
-def channel_message_to_zerver_message(realm_id: int, users: List[ZerverFieldsT],
+def channel_message_to_zerver_message(realm_id: int,
+                                      users: List[ZerverFieldsT],
                                       added_users: AddedUsersT,
                                       added_recipient: AddedRecipientsT,
                                       all_messages: List[ZerverFieldsT],
@@ -567,7 +568,6 @@ def channel_message_to_zerver_message(realm_id: int, users: List[ZerverFieldsT],
         ]:
             continue
 
-        has_attachment = has_image = False
         try:
             content, mentioned_user_ids, has_link = convert_to_zulip_markdown(
                 message['text'], users, added_channels, added_users)
@@ -595,39 +595,23 @@ def channel_message_to_zerver_message(realm_id: int, users: List[ZerverFieldsT],
         if subtype in ["bot_add", "sh_room_created", "me_message"]:
             content = ('/me %s' % (content))
 
-        files = message.get('files', [])
-        if subtype == 'file_share':
-            # In Slack messages, uploads can either have the subtype as 'file_share' or
-            # have the upload information in 'files' keyword
-            files = [message['file']]
+        file_info = process_message_files(
+            message=message,
+            domain_name=domain_name,
+            realm_id=realm_id,
+            message_id=message_id,
+            user=user,
+            users=users,
+            added_users=added_users,
+            zerver_attachment=zerver_attachment,
+            uploads_list=uploads_list,
+        )
 
-        for fileinfo in files:
-            url = fileinfo['url_private']
-            # For attachments with slack download link
-            if 'files.slack.com' in url:
-                has_attachment = has_link = True
-                has_image = True if 'image' in fileinfo['mimetype'] else False
+        content += file_info['content']
+        has_link = has_link or file_info['has_link']
 
-                file_user = [iterate_user for iterate_user in users if message['user'] == iterate_user['id']]
-                file_user_email = get_user_email(file_user[0], domain_name)
-
-                s3_path, content = get_attachment_path_and_content(fileinfo, realm_id)
-
-                # construct attachments
-                build_uploads(added_users[user], realm_id, file_user_email, fileinfo, s3_path,
-                              uploads_list)
-
-                build_attachment(realm_id, {message_id}, added_users[user],
-                                 fileinfo, s3_path, zerver_attachment)
-            # For attachments with link not from slack
-            # Example: Google drive integration
-            else:
-                has_link = True
-                if 'title' in fileinfo:
-                    file_name = fileinfo['title']
-                else:
-                    file_name = fileinfo['name']
-                content = '[%s](%s)' % (file_name, fileinfo['url_private'])
+        has_attachment = file_info['has_attachment']
+        has_image = file_info['has_image']
 
         # construct message
         subject = 'imported from slack'
@@ -651,6 +635,70 @@ def channel_message_to_zerver_message(realm_id: int, users: List[ZerverFieldsT],
     id_list = (message_id_count, reaction_id_count)
     return zerver_message, zerver_usermessage, zerver_attachment, uploads_list, \
         reaction_list, id_list
+
+def process_message_files(message: ZerverFieldsT,
+                          domain_name: str,
+                          realm_id: int,
+                          message_id: int,
+                          user: str,
+                          users: List[ZerverFieldsT],
+                          added_users: AddedUsersT,
+                          zerver_attachment: List[ZerverFieldsT],
+                          uploads_list: List[ZerverFieldsT]) -> Dict[str, Any]:
+    has_attachment = False
+    has_image = False
+    has_link = False
+
+    files = message.get('files', [])
+
+    subtype = message.get('subtype')
+
+    if subtype == 'file_share':
+        # In Slack messages, uploads can either have the subtype as 'file_share' or
+        # have the upload information in 'files' keyword
+        files = [message['file']]
+
+    markdown_links = []
+
+    for fileinfo in files:
+        url = fileinfo['url_private']
+
+        if 'files.slack.com' in url:
+            # For attachments with slack download link
+            has_attachment = True
+            has_link = True
+            has_image = True if 'image' in fileinfo['mimetype'] else False
+
+            file_user = [iterate_user for iterate_user in users if message['user'] == iterate_user['id']]
+            file_user_email = get_user_email(file_user[0], domain_name)
+
+            s3_path, content_for_link = get_attachment_path_and_content(fileinfo, realm_id)
+            markdown_links.append(content_for_link)
+
+            # construct attachments
+            build_uploads(added_users[user], realm_id, file_user_email, fileinfo, s3_path,
+                          uploads_list)
+
+            build_attachment(realm_id, {message_id}, added_users[user],
+                             fileinfo, s3_path, zerver_attachment)
+        else:
+            # For attachments with link not from slack
+            # Example: Google drive integration
+            has_link = True
+            if 'title' in fileinfo:
+                file_name = fileinfo['title']
+            else:
+                file_name = fileinfo['name']
+            markdown_links.append('[%s](%s)' % (file_name, fileinfo['url_private']))
+
+    content = '\n'.join(markdown_links)
+
+    return dict(
+        content=content,
+        has_attachment=has_attachment,
+        has_image=has_image,
+        has_link=has_link,
+    )
 
 def get_attachment_path_and_content(fileinfo: ZerverFieldsT, realm_id: int) -> Tuple[str,
                                                                                      str]:
