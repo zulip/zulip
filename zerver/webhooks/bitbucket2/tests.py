27976d4b0ceb452d35843128bc771aee7215be9e
Steve Howell <showell@zulipchat.com>
2018-11-01 21:31:24 +0000

    Remove "subject" from test_classes.py.
    We now use "topic" lingo.

--- zerver/webhooks/bitbucket2/tests.py
+++ zerver/webhooks/bitbucket2/tests.py
@@ -238,10 +238,10 @@ class Bitbucket2HookTests(WebhookTestCase):
         }
         self.send_and_test_stream_message('push_more_than_one_tag', **kwargs)
         msg = self.get_last_message()
-        self.do_test_subject(msg, self.EXPECTED_SUBJECT)
+        self.do_test_topic(msg, self.EXPECTED_SUBJECT)
         self.do_test_message(msg, expected_message.format(name='b'))
         msg = self.get_second_to_last_message()
-        self.do_test_subject(msg, self.EXPECTED_SUBJECT)
+        self.do_test_topic(msg, self.EXPECTED_SUBJECT)
         self.do_test_message(msg, expected_message.format(name='a'))
 
     def test_bitbucket2_on_more_than_one_push_event(self) -> None:
@@ -251,10 +251,10 @@ class Bitbucket2HookTests(WebhookTestCase):
         self.send_and_test_stream_message('more_than_one_push_event', **kwargs)
         msg = self.get_second_to_last_message()
         self.do_test_message(msg, 'kolaszek [pushed](https://bitbucket.org/kolaszek/repository-name/branch/master) 1 commit to branch master.\n\n* first commit ([84b96ad](https://bitbucket.org/kolaszek/repository-name/commits/84b96adc644a30fd6465b3d196369d880762afed))')
-        self.do_test_subject(msg, self.EXPECTED_SUBJECT_BRANCH_EVENTS)
+        self.do_test_topic(msg, self.EXPECTED_SUBJECT_BRANCH_EVENTS)
         msg = self.get_last_message()
         self.do_test_message(msg, 'kolaszek pushed tag [a](https://bitbucket.org/kolaszek/repository-name/commits/tag/a)')
-        self.do_test_subject(msg, self.EXPECTED_SUBJECT)
+        self.do_test_topic(msg, self.EXPECTED_SUBJECT)
 
     def test_bitbucket2_on_more_than_one_push_event_filtered_by_branches(self) -> None:
         self.url = self.build_webhook_url(branches='master,development')
@@ -264,10 +264,10 @@ class Bitbucket2HookTests(WebhookTestCase):
         self.send_and_test_stream_message('more_than_one_push_event', **kwargs)
         msg = self.get_second_to_last_message()
         self.do_test_message(msg, 'kolaszek [pushed](https://bitbucket.org/kolaszek/repository-name/branch/master) 1 commit to branch master.\n\n* first commit ([84b96ad](https://bitbucket.org/kolaszek/repository-name/commits/84b96adc644a30fd6465b3d196369d880762afed))')
-        self.do_test_subject(msg, self.EXPECTED_SUBJECT_BRANCH_EVENTS)
+        self.do_test_topic(msg, self.EXPECTED_SUBJECT_BRANCH_EVENTS)
         msg = self.get_last_message()
         self.do_test_message(msg, 'kolaszek pushed tag [a](https://bitbucket.org/kolaszek/repository-name/commits/tag/a)')
-        self.do_test_subject(msg, self.EXPECTED_SUBJECT)
+        self.do_test_topic(msg, self.EXPECTED_SUBJECT)
 
     def test_bitbucket2_on_more_than_one_push_event_filtered_by_branches_ignore(self) -> None:
         self.url = self.build_webhook_url(branches='changes,development')
