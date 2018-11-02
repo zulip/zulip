2951b046139e09e9e3d8ac45b88aafd537efa18b
Steve Howell <showell@zulipchat.com>
2018-11-02 13:56:32 +0000

    bugdown: Avoid unnecessary realm queries.
    We now keep realm in the arguments variable,
    which avoids some lookups.
    
    We also test settings before even trying to
    get realms.

--- zerver/lib/bugdown/arguments.py
+++ zerver/lib/bugdown/arguments.py
@@ -1,10 +1,12 @@
 from typing import Any, Dict, Optional
 
-from zerver.models import Message
+from zerver.models import Message, Realm
 
 # Filters such as UserMentionPattern need a message, but python-markdown
 # provides no way to pass extra params through to a pattern. Thus, a global.
 current_message = None  # type: Optional[Message]
+
+current_realm = None  # type: Optional[Realm]
 
 # We avoid doing DB queries in our markdown thread to avoid the overhead of
 # opening a new DB connection. These connections tend to live longer than the
