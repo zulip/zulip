eb4d279bbc4e7816be486d74faff5b112d43e4db
Steve Howell <showell@zulipchat.com>
2018-11-11 19:35:31 +0000

    Extract get_turtle_message().
    This seems like kind of a silly function to extract
    to topic.py, but it will theoretically help us sweep
    "subject" if we change the DB.
    
    It had test coverage.

--- zerver/lib/onboarding.py
+++ zerver/lib/onboarding.py
@@ -5,6 +5,7 @@ from zerver.lib.actions import set_default_streams, bulk_add_subscriptions, \
     internal_prep_stream_message, internal_send_private_message, \
     create_stream_if_needed, create_streams_if_needed, do_send_messages, \
     do_add_reaction_legacy, create_users, missing_any_realm_internal_bots
+from zerver.lib.topic import get_turtle_message
 from zerver.models import Realm, UserProfile, Message, Reaction, get_system_bot
 
 from typing import Any, Dict, List, Mapping
@@ -114,8 +115,5 @@ def send_initial_realm_messages(realm: Realm) -> None:
     # We find the one of our just-sent messages with turtle.png in it,
     # and react to it.  This is a bit hacky, but works and is kinda a
     # 1-off thing.
-    turtle_message = Message.objects.get(
-        id__in=message_ids,
-        subject='topic demonstration',
-        content__icontains='cute/turtle.png')
+    turtle_message = get_turtle_message(message_ids=message_ids)
     do_add_reaction_legacy(welcome_bot, turtle_message, 'turtle')
