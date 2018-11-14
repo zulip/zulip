67c3593b396f4a35e368e7d01365d149f3d1cf48
Tim Abbott <tabbott@zulipchat.com>
2018-11-14 15:28:35 -0800

    sessions: Add coverage exception for now.

--- zerver/lib/sessions.py
+++ zerver/lib/sessions.py
@@ -49,7 +49,7 @@ def delete_all_deactivated_user_sessions() -> None:
     for session in Session.objects.all():
         user_profile_id = get_session_user(session)
         if user_profile_id is None:
-            continue
+            continue  # nocoverage # to debug
         user_profile = get_user_profile_by_id(user_profile_id)
         if not user_profile.is_active or user_profile.realm.deactivated:
             logging.info("Deactivating session for deactivated user %s" % (user_profile.email,))
