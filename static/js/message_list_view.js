5c06846e208ea14ac63165611d18ce7d084d26af
Steve Howell <showell@zulipchat.com>
2018-11-15 16:13:49 +0000

    subject -> topic: Fix group.subject references.

--- static/js/message_list_view.js
+++ static/js/message_list_view.js
@@ -103,7 +103,7 @@ function populate_group_from_message_container(group, message_container) {
         group.background_color = stream_data.get_color(message_container.msg.stream);
         group.color_class = stream_color.get_color_class(group.background_color);
         group.invite_only = stream_data.get_invite_only(message_container.msg.stream);
-        group.subject = message_container.msg.subject;
+        group.topic = message_container.msg.subject;
         group.match_topic = util.get_match_topic(message_container.msg);
         group.stream_url = message_container.stream_url;
         group.topic_url = message_container.topic_url;
