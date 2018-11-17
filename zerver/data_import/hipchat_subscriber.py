10658ae10b4592a6fbba319e0d4b40c7470f7ab4
Steve Howell <showell@zulipchat.com>
2018-11-17 15:44:58 +0000

    hipchat: Rework stream/subscriber logic.
    We now account for streams having users that
    may be deleted.  We do a couple things:
    
        - use a loop instead of map
        - only pass in users to hipchat_subscriber
        - early-exit if there are not users
        - skip owner/members logic for public streams

--- zerver/data_import/hipchat_subscriber.py
+++ zerver/data_import/hipchat_subscriber.py
@@ -1,37 +1,15 @@
 from typing import Any, Dict, Set
 
 class SubscriberHandler:
-    '''
-    A note on ids here: we borrow Hipchat ids as Zulip
-    ids during the conversion phase.  (They get re-mapped
-    during import, but that doesn't concern use here.)
-
-    So these are all synonymous:
-
-        HipChat room_id == Zulip stream_id
-        member ids = hipchat user ids = Zulip user_id
-        owner id = hipchat user id = Zulip user_id
-
-    In this class, when it's somewhat arbitrary whether
-    to call something a "room" or a "stream", we use
-    the Zulip naming.
-    '''
     def __init__(self) -> None:
-        self.stream_info = dict()  # type: Dict[int, Dict[str, Any]]
+        self.stream_info = dict()  # type: Dict[int, Set[int]]
 
     def set_info(self,
                  stream_id: int,
-                 owner: int,
-                 members: Set[int]) -> None:
-        # Our callers are basically giving us
-        # data straight out of rooms.json.
-        self.stream_info[stream_id] = dict(
-            owner=owner,
-            members=members,
-        )
+                 users: Set[int]) -> None:
+        self.stream_info[stream_id] = users
 
     def get_users(self,
                   stream_id: int) -> Set[int]:
-        info = self.stream_info[stream_id]
-        users = info['members'] | {info['owner']}
+        users = self.stream_info[stream_id]
         return users
