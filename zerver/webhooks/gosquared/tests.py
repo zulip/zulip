c6ce504089d55d73ae5653b833be5bc87dc2dc25
Eeshan Garg <jerryguitarist@gmail.com>
2018-11-20 17:48:53 -0330

    webhooks/gosquared: Support chat message events.

--- zerver/webhooks/gosquared/tests.py
+++ zerver/webhooks/gosquared/tests.py
@@ -2,6 +2,8 @@
 
 from zerver.lib.test_classes import WebhookTestCase
 
+from zerver.webhooks.gosquared.view import CHAT_MESSAGE_TEMPLATE
+
 class GoSquaredHookTests(WebhookTestCase):
     STREAM_NAME = 'gosquared'
     URL_TEMPLATE = "/api/v1/external/gosquared?stream={stream}&api_key={api_key}"
@@ -13,6 +15,17 @@ class GoSquaredHookTests(WebhookTestCase):
         expected_message = u"[requestb.in](https://www.gosquared.com/now/GSN-595854-T) has 33 visitors online."
 
         self.send_and_test_stream_message('traffic_spike', expected_topic, expected_message,
+                                          content_type="application/x-www-form-urlencoded")
+
+    def test_chat_message(self) -> None:
+        expected_topic = "Live Chat Session - Zulip Chat"
+        expected_message = CHAT_MESSAGE_TEMPLATE.format(
+            status='visitor',
+            name='John Smith',
+            content='Zulip is awesome!'
+        )
+
+        self.send_and_test_stream_message('chat_message', expected_topic, expected_message,
                                           content_type="application/x-www-form-urlencoded")
 
     def get_body(self, fixture_name: str) -> str:
