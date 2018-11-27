10e8e2acacee377ff4a25a2cb555a3a17998058d
Tim Abbott <tabbott@zulipchat.com>
2018-11-27 09:51:35 -0800

    push notifications: Tweak text for push notification message.
    The previous content made it sound like we were actually sending a
    push notification, which could be confusing/alarming in some cases
    (see e.g. 9c224ccdd392ca29bcca4277f7a94393f5be3cb6).  Instead, we make
    clear that we're sending it to all clients (which one might correctly
    suspect is vacuous in the development environment).

--- zerver/lib/push_notifications.py
+++ zerver/lib/push_notifications.py
@@ -671,7 +671,7 @@ def handle_push_notification(user_profile_id: int, missed_message: Dict[str, Any
 
     apns_payload = get_apns_payload(user_profile, message)
     gcm_payload = get_gcm_payload(user_profile, message)
-    logger.info("Sending push notification to user %s" % (user_profile_id,))
+    logger.info("Sending push notifications to mobile clients for user %s" % (user_profile_id,))
 
     if uses_notification_bouncer():
         try:
