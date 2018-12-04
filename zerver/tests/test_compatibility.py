762fb0ba1102c20af9ab49460ea9cea814e88fae
Greg Price <greg@zulipchat.com>
2018-12-04 15:46:52 -0800

    compatibility: Add and test a helper find_mobile_os.

--- zerver/tests/test_compatibility.py
+++ zerver/tests/test_compatibility.py
@@ -1,6 +1,6 @@
 
 from zerver.lib.test_classes import ZulipTestCase
-from zerver.views.compatibility import version_lt
+from zerver.views.compatibility import find_mobile_os, version_lt
 
 class VersionTest(ZulipTestCase):
     data = [case.split() for case in '''
@@ -42,6 +42,20 @@ class VersionTest(ZulipTestCase):
                 self.assertIsNone(version_lt(ver2, ver1), msg=msg)
             else:
                 assert False  # nocoverage
+
+    mobile_os_data = [case.split(None, 1) for case in '''
+      android ZulipMobile/1.2.3 (Android 4.5)
+      ios     ZulipMobile/1.2.3 (iPhone OS 2.1)
+      ios     ZulipMobile/1.2.3 (iOS 6)
+      None    ZulipMobile/1.2.3 (Windows 8)
+    '''.strip().split('\n')]
+
+    def test_find_mobile_os(self) -> None:
+        for expected_, user_agent in self.mobile_os_data:
+            expected = None if expected_ == 'None' else expected_
+            self.assertEqual(find_mobile_os(user_agent), expected,
+                             msg=user_agent)
+
 
 class CompatibilityTest(ZulipTestCase):
     def test_compatibility(self) -> None:
