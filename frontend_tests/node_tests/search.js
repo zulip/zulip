9ea4f50c1bc63dbf2cdd6bf28efa31378e800c40
Tim Abbott <tabbott@zulipchat.com>
2018-12-04 13:55:55 -0800

    Revert "search: Open typeahead on empty string only if lookup triggered by Tab."
    This reverts commit b961093c146a6b0d2a034fa564209f4d0c5f1578.

--- frontend_tests/node_tests/search.js
+++ frontend_tests/node_tests/search.js
@@ -89,8 +89,7 @@ run_test('initizalize', () => {
         assert.equal(opts.fixed, true);
         assert.equal(opts.items, 12);
         assert.equal(opts.naturalSearch, true);
-        assert.equal(opts.tabOpensEmptyTypeahead, true);
-        assert.equal(opts.helpOnEmptyStrings, false);
+        assert.equal(opts.helpOnEmptyStrings, true);
         assert.equal(opts.matcher(), true);
 
         {
