97a4fd9b4335bd4f9927232ae155a47777874390
Steve Howell <showell@zulipchat.com>
2018-12-14 16:52:01 +0000

    muting: Add stream_id to markup for mute settings.
    We also prefer `attr` over `data` (it's more greppable).

--- static/js/settings_muting.js
+++ static/js/settings_muting.js
@@ -5,16 +5,10 @@ var exports = {};
 exports.set_up = function () {
     $('body').on('click', '.settings-unmute-topic', function (e) {
         var $row = $(this).closest("tr");
-        var stream = $row.data("stream");
-        var topic = $row.data("topic");
+        var stream_id = $row.attr("data-stream-id");
+        var topic = $row.attr("data-topic");
 
         e.stopImmediatePropagation();
-
-        var stream_id = stream_data.get_stream_id(stream);
-
-        if (!stream_id) {
-            return;
-        }
 
         muting_ui.unmute(stream_id, topic);
         $row.remove();
