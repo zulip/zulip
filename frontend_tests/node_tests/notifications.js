e6e6eea790fe11b242de10e302eb50ba26ea3b22
Steve Howell <showell@zulipchat.com>
2018-12-15 16:58:56 +0000

    node: Clean up node tests for notifications.
    This is mostly about cleaning up the naming convention
    for streams and topics, but it also adds a test that
    specifically tests the muted-topic case (without any
    other factors that would prevent a notification).
    
    Before this commit, it was possible to change the
    API for muting topics and get false positives, even
    when the test setup was clearly broken.

--- frontend_tests/node_tests/notifications.js
+++ frontend_tests/node_tests/notifications.js
@@ -21,57 +21,41 @@ zrequire('people');
 zrequire('notifications');
 
 // Not muted streams
-var two = {
+var general = {
     subscribed: true,
-    name: 'stream_two',
-    stream_id: 20,
+    name: 'general',
+    stream_id: 10,
     in_home_view: true,
 };
 
 // Muted streams
-var one = {
+var muted = {
     subscribed: true,
-    name: 'stream_one',
-    stream_id: 10,
+    name: 'muted',
+    stream_id: 20,
     in_home_view: false,
 };
 
-var three = {
-    subscribed: true,
-    name: 'stream_three',
-    stream_id: 30,
-    in_home_view: false,
-};
+stream_data.add_sub('general', general);
+stream_data.add_sub('muted', muted);
 
-// Add muted topics
-muting.add_muted_topic(one.name, 'topic_one');
-muting.add_muted_topic(one.name, 'topic_three');
-muting.add_muted_topic(three.name, 'topic_five');
-muting.add_muted_topic(three.name, 'topic_seven');
-
-// Subscribe to topic
-stream_data.add_sub('stream_two', two);
+muting.add_muted_topic(general.stream_id, 'muted topic');
 
 run_test('message_is_notifiable', () => {
-    // This function tests logic for 4 cases
-    // that should override any of the user's notifications settings
-    // and a 5th that passes it on the user's notifications settings
-
-
     // Case 1: If the message was sent by this user,
     //  DO NOT notify the user
     // In this test, all other circumstances should trigger notification
     // EXCEPT sent_by_me, which should trump them
     assert.equal(notifications.message_is_notifiable({
-        id: 10,
+        id: muted.stream_id,
         content: 'message number 1',
         sent_by_me: true,
         notification_sent: false,
         mentioned_me_directly: true,
         type: 'stream',
-        stream: 'stream_two',
-        stream_id: 20,
-        subject: 'topic_two',
+        stream: 'general',
+        stream_id: general.stream_id,
+        subject: 'whatever',
     }), false);
 
     // Case 2: If the user has already been sent a notificaton about this message,
@@ -80,15 +64,15 @@ run_test('message_is_notifiable', () => {
     // EXCEPT notification_sent, which should trump them
     // (ie: it mentions user, it's not muted, etc)
     assert.equal(notifications.message_is_notifiable({
-        id: 20,
+        id: general.stream_id,
         content: 'message number 2',
         sent_by_me: false,
         notification_sent: true,
         mentioned_me_directly: true,
         type: 'stream',
-        stream: 'stream_two',
-        stream_id: 20,
-        subject: 'topic_two',
+        stream: 'general',
+        stream_id: general.stream_id,
+        subject: 'whatever',
     }), false);
 
     // Case 3: If a message mentions the user directly,
@@ -96,16 +80,17 @@ run_test('message_is_notifiable', () => {
     // Mentioning trumps muting
     assert.equal(notifications.message_is_notifiable({
         id: 30,
-        content: 'message number three',
+        content: 'message number 3',
         sent_by_me: false,
         notification_sent: false,
         mentioned_me_directly: true,
         type: 'stream',
-        stream: 'stream_one',
-        stream_id: 10,
+        stream: 'muted',
+        stream_id: muted.stream_id,
         subject: 'topic_three',
     }), true);
 
+    // Case 4:
     // Mentioning should trigger notification in unmuted topic
     assert.equal(notifications.message_is_notifiable({
         id: 40,
@@ -114,12 +99,12 @@ run_test('message_is_notifiable', () => {
         notification_sent: false,
         mentioned_me_directly: true,
         type: 'stream',
-        stream: 'stream_two',
-        stream_id: 20,
-        subject: 'topic_two',
+        stream: 'general',
+        stream_id: general.stream_id,
+        subject: 'vanilla',
     }), true);
 
-    // Case 4: If a message is in a muted stream
+    // Case 5: If a message is in a muted stream
     //  and does not mention the user DIRECTLY,
     //  DO NOT notify the user
     assert.equal(notifications.message_is_notifiable({
@@ -129,26 +114,41 @@ run_test('message_is_notifiable', () => {
         notification_sent: false,
         mentioned_me_directly: false,
         type: 'stream',
-        stream: 'stream_one',
-        stream_id: 10,
-        subject: 'topic_one',
+        stream: 'muted',
+        stream_id: muted.stream_id,
+        subject: 'whatever',
     }), false);
 
-    // Case 5
+    // Case 6: If a message is in a muted topic
+    //  and does not mention the user DIRECTLY,
+    //  DO NOT notify the user
+    assert.equal(notifications.message_is_notifiable({
+        id: 50,
+        content: 'message number 6',
+        sent_by_me: false,
+        notification_sent: false,
+        mentioned_me_directly: false,
+        type: 'stream',
+        stream: 'general',
+        stream_id: general.stream_id,
+        subject: 'muted topic',
+    }), false);
+
+    // Case 7
     // If none of the above cases apply
     // (ie: topic is not muted, message does not mention user,
     //  no notification sent before, message not sent by user),
     // return true to pass it to notifications settings
     assert.equal(notifications.message_is_notifiable({
         id: 60,
-        content: 'message number 6',
+        content: 'message number 7',
         sent_by_me: false,
         notification_sent: false,
         mentioned_me_directly: false,
         type: 'stream',
-        stream: 'stream_two',
-        stream_id: 20,
-        subject: 'topic_two',
+        stream: 'general',
+        stream_id: general.stream_id,
+        subject: 'whatever',
     }), true);
 });
 
@@ -186,9 +186,9 @@ run_test('basic_notifications', () => {
         notification_sent: false,
         mentioned_me_directly: true,
         type: 'stream',
-        stream: 'stream_one',
-        stream_id: 10,
-        subject: 'topic_two',
+        stream: 'general',
+        stream_id: muted.stream_id,
+        subject: 'whatever',
     };
 
     var message_2 = {
@@ -199,22 +199,22 @@ run_test('basic_notifications', () => {
         notification_sent: false,
         mentioned_me_directly: true,
         type: 'stream',
-        stream: 'stream_one',
-        stream_id: 10,
-        subject: 'topic_four',
+        stream: 'general',
+        stream_id: muted.stream_id,
+        subject: 'lunch',
     };
 
     // Send notification.
     notifications.process_notification({message: message_1, webkit_notify: true});
     n = notifications.get_notifications();
-    assert.equal('undefined to stream_one > topic_two' in n, true);
+    assert.equal('undefined to general > whatever' in n, true);
     assert.equal(Object.keys(n).length, 1);
     assert.equal(last_shown_message_id, message_1.id);
 
     // Remove notification.
     notifications.close_notification(message_1);
     n = notifications.get_notifications();
-    assert.equal('undefined to stream_one > topic_two' in n, false);
+    assert.equal('undefined to general > whatever' in n, false);
     assert.equal(Object.keys(n).length, 0);
     assert.equal(last_closed_message_id, message_1.id);
 
@@ -222,7 +222,7 @@ run_test('basic_notifications', () => {
     message_1.id = 1001;
     notifications.process_notification({message: message_1, webkit_notify: true});
     n = notifications.get_notifications();
-    assert.equal('undefined to stream_one > topic_two' in n, true);
+    assert.equal('undefined to general > whatever' in n, true);
     assert.equal(Object.keys(n).length, 1);
     assert.equal(last_shown_message_id, message_1.id);
 
@@ -230,14 +230,14 @@ run_test('basic_notifications', () => {
     message_1.id = 1002;
     notifications.process_notification({message: message_1, webkit_notify: true});
     n = notifications.get_notifications();
-    assert.equal('undefined to stream_one > topic_two' in n, true);
+    assert.equal('undefined to general > whatever' in n, true);
     assert.equal(Object.keys(n).length, 1);
     assert.equal(last_shown_message_id, message_1.id);
 
     // Send another message. Notification count should increase.
     notifications.process_notification({message: message_2, webkit_notify: true});
     n = notifications.get_notifications();
-    assert.equal('undefined to stream_one > topic_four' in n, true);
+    assert.equal('undefined to general > lunch' in n, true);
     assert.equal(Object.keys(n).length, 2);
     assert.equal(last_shown_message_id, message_2.id);
 
@@ -245,7 +245,7 @@ run_test('basic_notifications', () => {
     notifications.close_notification(message_1);
     notifications.close_notification(message_2);
     n = notifications.get_notifications();
-    assert.equal('undefined to stream_one > topic_two' in n, false);
+    assert.equal('undefined to general > whatever' in n, false);
     assert.equal(Object.keys(n).length, 0);
     assert.equal(last_closed_message_id, message_2.id);
 });
