d3784b487da14a4b41d03bbb2787b33c5080b550
Tim Abbott <tabbott@zulipchat.com>
2018-12-16 15:23:34 -0800

    csrf: Improve API for when no CSRF input is available.
    Since we're adding this to a large number of portico pages, there's no
    guarantee that these pages actually have a CSRF input.
    
    Though given that the logout template contains a CSRF input,
    realistically it should always be present.

--- static/js/csrf.js
+++ static/js/csrf.js
@@ -1,8 +1,17 @@
 var csrf_token;
 $(function () {
     // This requires that we used Jinja2's {% csrf_input %} somewhere on the page.
-    csrf_token = $('input[name="csrfmiddlewaretoken"]').attr('value');
+    var csrf_input = $('input[name="csrfmiddlewaretoken"]');
+    if (csrf_input.length > 0) {
+        csrf_token = csrf_input.attr('value');
+    } else {
+        csrf_token = undefined;
+    }
     window.csrf_token = csrf_token;
+
+    if (csrf_token === undefined) {
+        return;
+    }
 
     $.ajaxSetup({
         beforeSend: function (xhr, settings) {
