d84d3777fd40ae5985bf4b6c6ec71748a318c277
Tim Abbott <tabbott@zulipchat.com>
2018-12-17 13:43:13 -0800

    casper: Fix admin tests failing.
    This was broken with the recent realm filters commits.

--- frontend_tests/casper_tests/10-admin.js
+++ frontend_tests/casper_tests/10-admin.js
@@ -270,7 +270,7 @@ casper.then(function () {
 
 casper.then(function () {
     casper.waitUntilVisible('div#admin-filter-pattern-status', function () {
-        casper.test.assertSelectorHasText('div#admin-filter-pattern-status', 'Failed: Invalid filter pattern, you must use the following format OPTIONAL_PREFIX(?P<id>.+)');
+        casper.test.assertSelectorHasText('div#admin-filter-pattern-status', 'Failed: Invalid filter pattern');
     });
 });
 
