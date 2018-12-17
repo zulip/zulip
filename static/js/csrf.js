acd63660480ba01869d4ae50f3066c805d3f15f7
Tim Abbott <tabbott@zulipchat.com>
2018-12-16 16:15:54 -0800

    Revert "csrf: Improve API for when no CSRF input is available."
    This reverts commit 9198fa4ec9618f00234b0fcf3c616fb7724e935a.

--- static/js/csrf.js
+++ static/js/csrf.js
@@ -1,17 +1,8 @@
 var csrf_token;
 $(function () {
     // This requires that we used Jinja2's {% csrf_input %} somewhere on the page.
-    var csrf_input = $('input[name="csrfmiddlewaretoken"]');
-    if (csrf_input.length > 0) {
-        csrf_token = csrf_input.attr('value');
-    } else {
-        csrf_token = undefined;
-    }
+    csrf_token = $('input[name="csrfmiddlewaretoken"]').attr('value');
     window.csrf_token = csrf_token;
-
-    if (csrf_token === undefined) {
-        return;
-    }
 
     $.ajaxSetup({
         beforeSend: function (xhr, settings) {
