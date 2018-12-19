a60161e731f9a3d0b815a3e476fb89e0ebcf3f5a
rht <rhtbot@protonmail.com>
2018-12-19 22:05:32 +0000

    puppet: Add CentOS packages to apache_sso.pp.

--- puppet/zulip/manifests/apache_sso.pp
+++ puppet/zulip/manifests/apache_sso.pp
@@ -1,33 +1,46 @@
 class zulip::apache_sso {
   include zulip::localhost_sso
 
-  $apache_packages = [ 'apache2', 'libapache2-mod-wsgi-py3', ]
+  case $::osfamily {
+    'debian': {
+      $apache_packages = [ 'apache2', 'libapache2-mod-wsgi-py3', ]
+      $apache_former_packages = [ 'libapache2-mod-wsgi', ]
+      $conf_dir = '/etc/apache2'
+      $apache2 = 'apache2'
+    }
+    'redhat': {
+      $apache_packages = [ 'httpd', 'python36u-mod_wsgi', ]
+      $apache_former_packages = []
+      $conf_dir = '/etc/httpd'
+      $apache2 = 'httpd'
+    }
+    default: {
+      fail('osfamily not supported')
+    }
+  }
   package { $apache_packages: ensure => 'installed' }
-
-  $apache_former_packages = [ 'libapache2-mod-wsgi', ]
   package { $apache_former_packages: ensure => 'absent' }
 
   apache2mod { [ 'headers', 'proxy', 'proxy_http', 'rewrite', 'ssl', 'wsgi', ]:
     ensure  => present,
-    require => Package['apache2'],
+    require => Package[$apache2],
   }
 
-  file { '/etc/apache2/ports.conf':
+  file { "${conf_dir}/ports.conf":
     ensure  => file,
-    require => Package[apache2],
+    require => Package[$apache2],
     owner   => 'root',
     group   => 'root',
     mode    => '0640',
     source  => 'puppet:///modules/zulip/apache/ports.conf',
   }
 
-  file { '/etc/apache2/sites-available/':
+  file { "${conf_dir}/sites-available/":
     recurse => true,
-    require => Package[apache2],
+    require => Package[$apache2],
     owner   => 'root',
     group   => 'root',
     mode    => '0640',
     source  => 'puppet:///modules/zulip/apache/sites/',
   }
-
 }
