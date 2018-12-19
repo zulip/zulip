53091a1ed357b69e432084d0225c8ab51f958306
rht <rhtbot@protonmail.com>
2018-12-19 22:21:54 +0000

    puppet: Add CentOS packages to redis.pp.

--- puppet/zulip/manifests/redis.pp
+++ puppet/zulip/manifests/redis.pp
@@ -1,6 +1,10 @@
 class zulip::redis {
+  $redis = $::osfamily ? {
+    'debian' => 'redis-server',
+    'redhat' => 'redis',
+  }
   $redis_packages = [ # The server itself
-                      'redis-server',
+                      $redis,
                       ]
 
   package { $redis_packages: ensure => 'installed' }
@@ -11,7 +15,7 @@ class zulip::redis {
     unless  => "/bin/grep -Fxqe '${line}' '${file}'",
     path    => '/bin',
     command => "bash -c \"(/bin/echo; /bin/echo '# Include Zulip-specific configuration'; /bin/echo '${line}') >> '${file}'\"",
-    require => [Package['redis-server'],
+    require => [Package[$redis],
                 File['/etc/redis/zulip-redis.conf'],
                 Exec['rediscleanup']],
   }
@@ -24,14 +28,14 @@ class zulip::redis {
   $redis_password = zulipsecret('secrets', 'redis_password', '')
   file { '/etc/redis/zulip-redis.conf':
     ensure  => file,
-    require => Package[redis-server],
+    require => Package[$redis],
     owner   => 'redis',
     group   => 'redis',
     mode    => '0640',
     content => template('zulip/zulip-redis.template.erb'),
   }
 
-  service { 'redis-server':
+  service { $redis:
     ensure    => running,
     subscribe => [File['/etc/redis/zulip-redis.conf'],
                   Exec['redis']],
