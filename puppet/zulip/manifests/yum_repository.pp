d9091f8e66cca0dc0d797125c3d1515bcb1dd768
rht <rhtbot@protonmail.com>
2018-12-20 04:10:12 +0000

    puppet: Add CentOS packages to postgres_appdb_base.pp.

--- puppet/zulip/manifests/yum_repository.pp
+++ puppet/zulip/manifests/yum_repository.pp
@@ -2,6 +2,6 @@
 class zulip::yum_repository {
   $setup_yum_repo_file = "${::zulip_scripts_path}/lib/setup-yum-repo"
   exec{'setup_yum_repo':
-    command => "bash -c '${setup_yum_repo_file}'",
+    command => "bash -c '${setup_yum_repo_file} --prod'",
   }
 }
