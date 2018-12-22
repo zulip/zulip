615c9c942e20a8a254eb77f3f831e170123b2e65
Steve Howell <showell@zulipchat.com>
2018-12-22 22:04:23 +0000

    subject -> topic: Fix hash_util.js.
    This includes using a more modern URL for topic links.
    (We already supported "../topic/..." urls.)

--- frontend_tests/node_tests/topic_list.js
+++ frontend_tests/node_tests/topic_list.js
@@ -49,7 +49,7 @@ run_test('topic_list_build_widget', () => {
             unread: 3,
             is_zero: false,
             is_muted: false,
-            url: '#narrow/stream/555-devel/subject/coding',
+            url: '#narrow/stream/555-devel/topic/coding',
         };
         assert.deepEqual(info, expected);
         rendered = true;
