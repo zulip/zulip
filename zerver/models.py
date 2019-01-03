f7e4f703a7930e78ccd630ca9b86561fe0bf1ded
Shubham Dhama <shubhamdhamaofficial@gmail.com>
2019-01-03 21:20:36 +0530

    invite: Rename NORMAL_USER to MEMBER of Preregistration.INVITE_AS.
    Renamed to keep the user-facing and the codebase terminologies coherent.

--- zerver/models.py
+++ zerver/models.py
@@ -1048,11 +1048,11 @@ class PreregistrationUser(models.Model):
     realm = models.ForeignKey(Realm, null=True, on_delete=CASCADE)  # type: Optional[Realm]
 
     INVITE_AS = dict(
-        NORMAL_USER = 1,
+        MEMBER = 1,
         REALM_ADMIN = 2,
         GUEST_USER = 3,
     )
-    invited_as = models.PositiveSmallIntegerField(default=INVITE_AS['NORMAL_USER'])  # type: int
+    invited_as = models.PositiveSmallIntegerField(default=INVITE_AS['MEMBER'])  # type: int
 
     invited_as_admin = models.BooleanField(default=False)  # type: bool
 
