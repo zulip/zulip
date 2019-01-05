688940e4bd0a3da16998ce1f26d1dd99d3b5b750
rht <rhtbot@protonmail.com>
2019-01-05 02:19:31 +0000

    puppet: Add CentOS packages to static_asset_compiler.pp.

--- puppet/zulip/manifests/static_asset_compiler.pp
+++ puppet/zulip/manifests/static_asset_compiler.pp
@@ -1,16 +1,31 @@
 class zulip::static_asset_compiler {
-  if $zulip::base::release_name == 'trusty' {
-    $closure_compiler_package = 'libclosure-compiler-java'
-  } else {
-    $closure_compiler_package = 'closure-compiler'
+  include zulip::common
+  case $::osfamily {
+    'debian': {
+      if $zulip::base::release_name == 'trusty' {
+        $closure_compiler_package = 'libclosure-compiler-java'
+      } else {
+        $closure_compiler_package = 'closure-compiler'
+      }
+      $static_asset_compiler_packages = [
+        # Needed for minify-js
+        $closure_compiler_package,
+        'yui-compressor',
+        # Used by makemessages i18n
+        'gettext',
+      ]
+    }
+    'redhat': {
+      $static_asset_compiler_packages = [
+        # TODO CentOS doesn't have closure-compiler
+        'yuicompressor',
+        'gettext',
+      ]
+    }
+    default: {
+      fail('osfamily not supported')
+    }
   }
-  $static_asset_compiler_packages = [
-    # Needed for minify-js
-    $closure_compiler_package,
-    'yui-compressor',
-    # Used by makemessages i18n
-    'gettext',
-  ]
 
   zulip::safepackage { $static_asset_compiler_packages: ensure => 'installed' }
 }
