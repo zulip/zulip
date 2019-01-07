1edcf37eec41fb604d0438cf5ffd39318374d103
Vishnu Ks <yo@vishnuks.com>
2019-01-07 16:39:14 +0530

    billing: Use underscore built in contains function.

--- frontend_tests/node_tests/billing_helpers.js
+++ frontend_tests/node_tests/billing_helpers.js
@@ -2,9 +2,3 @@ set_global('$', global.make_zjquery());
 set_global('page_params', {});
 
 zrequire('helpers', "js/billing/helpers");
-
-run_test("is_in_array", () => {
-    var good_houses_array = ["Gryffindor", "Hufflepuff", "Ravenclaw"];
-    assert.equal(helpers.is_in_array("Hufflepuff", good_houses_array), true);
-    assert.equal(helpers.is_in_array("Slytherin", good_houses_array), false);
-});
