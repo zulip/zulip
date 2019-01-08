416b8be1ac7403f38f1ee0c7bb60266089567f5d
Rohitt Vashishtha <aero31aero@gmail.com>
2019-01-08 08:30:19 +0000

    markdown: Add _@**Name** syntax for silent mentions.
    These mentions look like regular mentions except they do not
    trigger any notification for the person mentioned. These are
    primarily to be used when you make a bot take an action and
    the bot mentions you, or when you quote a message that mentions
    you.
    
    Fixes #11221.

--- frontend_tests/node_tests/markdown.js
+++ frontend_tests/node_tests/markdown.js
@@ -302,6 +302,8 @@ run_test('marked', () => {
          expected: '<p><span aria-label="poop" class="emoji emoji-1f4a9" role="img" title="poop">:poop:</span></p>'},
         {input: '\u{1f6b2}',
          expected: '<p>\u{1f6b2}</p>' },
+        {input: 'Silent mention: _@**Cordelia Lear**',
+         expected: '<p>Silent mention: <span class="user-mention silent" data-user-id="101">@Cordelia Lear</span></p>'},
         // Test only those realm filters which don't return True for
         // `contains_backend_only_syntax()`. Those which return True
         // are tested separately.
