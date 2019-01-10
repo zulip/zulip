b8c68f464d7ef60e92e19d291efd0f6ab0a680f3
Tim Abbott <tabbott@zulipchat.com>
2019-01-09 16:49:39 -0800

    hipchat: Fix importing of private messages.
    Apparently a stupid typing issue meant that we broke this a few weeks
    ago.

--- zerver/data_import/hipchat.py
+++ zerver/data_import/hipchat.py
@@ -581,7 +581,7 @@ def process_message_file(realm_id: int,
                 return None
 
             if is_pm_data:
-                if sender_id != fn_id:
+                if int(sender_id) != int(fn_id):
                     # PMs are in multiple places in the Hipchat export,
                     # and we only use the copy from the sender
                     return None
