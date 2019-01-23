a1679c400fea586da08339a6160a3ecdb3f85bea
Steve Howell <showell@zulipchat.com>
2019-01-23 18:25:22 +0000

    user status: Add frontend data layer for "status_text".
    This sets up the frontend to handle "status_text" data like
    "out to lunch" or "in a meeting".

--- static/js/user_status.js
+++ static/js/user_status.js
@@ -3,21 +3,25 @@ var user_status = (function () {
 var exports = {};
 
 var away_user_ids = new Dict();
+var user_info = new Dict();
 
-exports.server_set_away = function () {
+exports.server_update = function (opts) {
     channel.post({
         url: '/json/users/me/status',
-        data: {away: true},
+        data: {
+            away: opts.away,
+            status_text: opts.status_text,
+        },
         idempotent: true,
     });
 };
 
+exports.server_set_away = function () {
+    exports.server_update({away: true});
+};
+
 exports.server_revoke_away = function () {
-    channel.post({
-        url: '/json/users/me/status',
-        data: {away: false},
-        idempotent: true,
-    });
+    exports.server_update({away: false});
 };
 
 exports.set_away = function (user_id) {
@@ -32,10 +36,27 @@ exports.is_away = function (user_id) {
     return away_user_ids.has(user_id);
 };
 
+exports.get_status_text = function (user_id) {
+    return user_info.get(user_id);
+};
+
+exports.set_status_text = function (opts) {
+    if (!opts.status_text) {
+        user_info.del(opts.user_id);
+        return;
+    }
+
+    user_info.set(opts.user_id, opts.status_text);
+};
+
 exports.initialize = function () {
     _.each(page_params.user_status, function (dct, user_id) {
         if (dct.away) {
             away_user_ids.set(user_id, true);
+        }
+
+        if (dct.status_text) {
+            user_info.set(user_id, dct.status_text);
         }
     });
 
