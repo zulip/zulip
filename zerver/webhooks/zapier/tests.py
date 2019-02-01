d58a724dc744a3ca14bb1cb50249da065f12e7ef
Eeshan Garg <jerryguitarist@gmail.com>
2019-01-31 22:11:57 -0330

    webhooks/zapier: Support authentication for the Zapier Zulip app.

--- zerver/webhooks/zapier/tests.py
+++ zerver/webhooks/zapier/tests.py
@@ -20,3 +20,15 @@ class ZapierHookTests(WebhookTestCase):
         expected_topic = u"Here is your weather update for the day:"
         expected_message = u"Foggy in the morning.\nMaximum temperature to be 24.\nMinimum temperature to be 12"
         self.send_and_test_stream_message('weather_update', expected_topic, expected_message)
+
+class ZapierZulipAppTests(WebhookTestCase):
+    URL_TEMPLATE = "/api/v1/external/zapier?api_key={api_key}"
+    FIXTURE_DIR_NAME = 'zapier'
+
+    def test_auth(self) -> None:
+        payload = self.get_body('zapier_zulip_app_auth')
+        headers = {'HTTP_USER_AGENT': 'ZapierZulipApp'}
+        result = self.client_post(self.url, payload,
+                                  content_type='application/json',
+                                  **headers)
+        self.assert_json_success(result)
