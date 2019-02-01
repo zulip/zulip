95513a2f03c6140caee7eae8b7bf7b591b6026d7
Mateusz Mandera <mateusz.mandera@protonmail.com>
2019-02-01 14:15:16 +0100

    pgp_email: Create a stub of test_pgp.py.
    We add a stub of tests for the pgp functionalities. For now they don't
    do much as signing and encrypting isn't implemented. We use the word
    'subject' in the context of emails in the new file, so it has to be
    added to the list of exceptions from the linting rule that prohibits
    that word.

---
+++ zerver/tests/test_pgp.py
@@ -0,0 +1,49 @@
+from django.core import mail
+from django.test import override_settings
+
+from zerver.lib.test_classes import ZulipTestCase
+from zerver.lib.pgp import pgp_sign_and_encrypt, PGPEmailMessage
+from zerver.models import UserProfile, UserPGP
+
+class TestPGP(ZulipTestCase):
+    @override_settings(ENABLE_EMAIL_ENCRYPTION=True)
+    @override_settings(ENABLE_EMAIL_SIGNATURES=True)
+    def test_pgp_vanilla_message(self) -> None:
+        user_profile = self.example_user('hamlet')
+        user_profile.want_signed_emails = False
+        user_profile.want_encrypted_emails = False
+
+        msg = PGPEmailMessage(subject='Subject', body='Email body')
+        emails = pgp_sign_and_encrypt(msg, [user_profile])
+
+        self.assertEqual(len(emails), 1)
+        self.assertEqual(emails[0].subject, 'Subject')
+        self.assertEqual(emails[0].body, 'Email body')
+
+        emails[0].send()
+        self.assertEqual(len(mail.outbox), 1)
+
+    @override_settings(ENABLE_EMAIL_ENCRYPTION=True)
+    @override_settings(ENABLE_EMAIL_SIGNATURES=True)
+    def test_pgp_sign_message(self) -> None:
+        user_profile = self.example_user('hamlet')
+        user_profile.want_signed_emails = True
+        user_profile.want_encrypted_emails = False
+
+        msg = PGPEmailMessage(subject='Subject', body='Email body')
+        with self.assertRaises(NotImplementedError):
+            pgp_sign_and_encrypt(msg, [user_profile])
+
+    @override_settings(ENABLE_EMAIL_ENCRYPTION=True)
+    @override_settings(ENABLE_EMAIL_SIGNATURES=True)
+    def test_pgp_encrypt_message(self) -> None:
+        user_profile = self.example_user('hamlet')
+        user_profile.want_signed_emails = False
+        user_profile.want_encrypted_emails = True
+
+        user_pgp = UserPGP(user_profile=user_profile, public_key='dummy key')
+        user_pgp.save()
+
+        msg = PGPEmailMessage(subject='Subject', body='Email body')
+        with self.assertRaises(NotImplementedError):
+            pgp_sign_and_encrypt(msg, [user_profile])
