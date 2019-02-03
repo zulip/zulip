76344e021f8e1af4b3cda211c2d7a5ec27af2d1f
Mateusz Mandera <mateusz.mandera@protonmail.com>
2019-02-03 12:33:49 +0100

    pgp_email: Expand test_pgp.py to cover force_single_message=True.

--- zerver/tests/test_pgp.py
+++ zerver/tests/test_pgp.py
@@ -2,8 +2,10 @@ from django.core import mail
 from django.test import override_settings
 
 from zerver.lib.test_classes import ZulipTestCase
-from zerver.lib.pgp import pgp_sign_and_encrypt, PGPEmailMessage
+from zerver.lib.pgp import pgp_sign_and_encrypt, PGPEmailMessage, PGPKeyNotFound
 from zerver.models import UserProfile, UserPGP
+
+from typing import List
 
 class TestPGP(ZulipTestCase):
     @override_settings(ENABLE_EMAIL_ENCRYPTION=True)
@@ -33,6 +35,7 @@ class TestPGP(ZulipTestCase):
         msg = PGPEmailMessage(subject='Subject', body='Email body')
         emails = pgp_sign_and_encrypt(msg, [user_profile])
         self.assertEqual(len(emails), 1)
+        self.assertEqual(emails[0]._signed, True)
         with self.assertRaises(NotImplementedError):
             emails[0].send()
 
@@ -49,5 +52,63 @@ class TestPGP(ZulipTestCase):
         msg = PGPEmailMessage(subject='Subject', body='Email body')
         emails = pgp_sign_and_encrypt(msg, [user_profile])
         self.assertEqual(len(emails), 1)
+        self.assertEqual(emails[0]._encrypted, True)
         with self.assertRaises(NotImplementedError):
             emails[0].send()
+
+    @override_settings(ENABLE_EMAIL_ENCRYPTION=True)
+    @override_settings(ENABLE_EMAIL_SIGNATURES=True)
+    def test_pgp_force_single_message(self) -> None:
+        user_profiles = [self.example_user('hamlet'), self.example_user('cordelia')]
+        user_pgps = []  # type: List[UserPGP]
+        user_pgps.append(UserPGP(user_profile=user_profiles[0], public_key='dummy key 1'))
+        user_pgps.append(UserPGP(user_profile=user_profiles[1], public_key='dummy key 2'))
+        user_pgps[0].save()
+        user_pgps[1].save()
+
+        user_profiles[0].want_signed_emails = True
+        user_profiles[0].want_encrypted_emails = True
+
+        user_profiles[1].want_signed_emails = True
+        user_profiles[1].want_encrypted_emails = True
+
+        msg = PGPEmailMessage(subject='Subject', body='Email body')
+        emails = pgp_sign_and_encrypt(msg, user_profiles, force_single_message=True)
+
+        self.assertEqual(len(emails), 1)
+        self.assertEqual(emails[0]._signed, True)
+        self.assertEqual(emails[0]._encrypted, True)
+
+        user_profiles[0].want_signed_emails = False
+        user_profiles[0].want_encrypted_emails = False
+
+        user_profiles[1].want_signed_emails = False
+        user_profiles[1].want_encrypted_emails = False
+
+        emails = pgp_sign_and_encrypt(msg, user_profiles, force_single_message=True)
+
+        self.assertEqual(len(emails), 1)
+        self.assertEqual(emails[0]._signed, False)
+        self.assertEqual(emails[0]._encrypted, False)
+
+        user_profiles[0].want_signed_emails = True
+        user_profiles[0].want_encrypted_emails = False
+
+        user_profiles[1].want_signed_emails = True
+        user_profiles[1].want_encrypted_emails = False
+
+        emails = pgp_sign_and_encrypt(msg, user_profiles, force_single_message=True)
+
+        self.assertEqual(len(emails), 1)
+        self.assertEqual(emails[0]._signed, True)
+        self.assertEqual(emails[0]._encrypted, False)
+
+    @override_settings(ENABLE_EMAIL_ENCRYPTION=True)
+    def test_pgp_missing_public_key(self) -> None:
+        user_profile = self.example_user('hamlet')
+        user_profile.want_signed_emails = False
+        user_profile.want_encrypted_emails = True
+
+        msg = PGPEmailMessage(subject='Subject', body='Email body')
+        with self.assertRaises(PGPKeyNotFound):
+            pgp_sign_and_encrypt(msg, [user_profile])
