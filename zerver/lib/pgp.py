52ebc36044376e330cf7fb4c6ad35a2aa7c922b6
Mateusz Mandera <mateusz.mandera@protonmail.com>
2019-02-07 12:58:54 +0100

    pgp_email: Implement PGP/MIME formatting of emails.
    When .send() is invoked for an email message, Django sends the output of
    .message() through the apprioprate backend. This .message() output is
    essentially a MIME object from the python "email" module. If we're
    sending an encrypted/signed email, this object needs to have an
    appropriate PGP/MIME structure, as described in RFC3156. In this commit,
    we implement the creation of this structure. Instead of actual
    ciphertexts/signatures, we insert dummy strings for now, as this last
    part of the process isn't implemented yet.
    
    Appropriate tests are added to test_pgp.py.

--- zerver/lib/pgp.py
+++ zerver/lib/pgp.py
@@ -1,12 +1,18 @@
 from django.conf import settings
-from django.core.mail import EmailMultiAlternatives, SafeMIMEMultipart
+from django.core.mail import EmailMultiAlternatives, SafeMIMEMultipart, \
+    SafeMIMEText
+
+from email.encoders import encode_7or8bit
+from email.header import Header
+from email.message import Message
+from email.mime.application import MIMEApplication
 
 from zerver.lib.email_helpers import format_to
 from zerver.lib.logging_util import log_to_file
 from zerver.models import UserProfile, UserPGP
 
 from copy import deepcopy
-from typing import Dict, Optional, List, DefaultDict, Callable, Any
+from typing import Dict, Optional, List, DefaultDict, Callable, Any, Union
 
 class PGPKeyNotFound(Exception):
     pass
@@ -19,27 +25,90 @@ class PGPKeyNotFound(Exception):
 # library. This is the stage at which we have the desired level of control.
 # (See django.core.mail.message for details.)
 # Thus, encryption and signing need to happen by overriding .message().
+# When invoked, .sign() and .encrypt() will generate a PGP/MIME message
+# and store it in self._generated message, which will be returned
+# by the overriden .message() method. If we don't sign/encrypt the email,
+# the original super().message() will be returned.
 class PGPEmailMessage(EmailMultiAlternatives):
+    _locked = False
+
     def __init__(self, *args: Any, **kwargs: Any) -> None:
-        super().__init__(*args, **kwargs)
         self._encrypted = False
         self._signed = False
-        self.public_keys = []  # type: List[str]
+        self._generated_message = None  # type: Optional[Message]
+        super().__init__(*args, **kwargs)
+
+    def __setattr__(self, attr: str, value: Any) -> None:
+        # When locked, we permit explicitly changing self._locked as well as
+        # self.connection because this gets set while sending the email.
+        allowed_attrs = ['_locked', 'connection']
+        if self._locked is False or attr in allowed_attrs:
+            object.__setattr__(self, attr, value)
+        else:
+            raise AttributeError('The message is already signed/encrypted '
+                                 'and can\'t be edited.')
+
+    def _set_headers(self) -> None:
+        assert self._generated_message is not None
+        original_message = super().message()
+
+        # We need to use .get('Header name') on the right side, because mypy throws
+        # error: Incompatible types in assignment
+        # if we use original_message['Header name']
+        self._generated_message['Subject'] = original_message.get('Subject')
+        self._generated_message['From'] = original_message.get('From')
+        self._generated_message['To'] = original_message.get('To')
+        if 'Cc' in original_message:
+            self._generated_message['Cc'] = original_message.get('Cc')
+        if 'Reply-To' in original_message:
+            self._generated_message['Reply-To'] = original_message.get('Reply-To')
+        if 'Date' in original_message:
+            self._generated_message['Date'] = original_message.get('Date')
+        if 'Message-ID' in original_message:
+            self._generated_message['Message-ID'] = original_message.get('Message-ID')
+
+        for name, value in self.extra_headers.items():
+            if name.lower() in ('from', 'to'):
+                continue
+            self._generated_message[name] = value
+
+    def get_base_message(self) -> Union[SafeMIMEMultipart, SafeMIMEText]:
+        # Taken from how Django creates the base message in super().message
+        # before setting Subject/From/To/etc. headers.
+        text_msg = SafeMIMEText(self.body, self.content_subtype, self.encoding)
+        msg = self._create_message(text_msg)
+        if 'MIME-Version' in msg:
+            del msg['MIME-Version']
+
+        return msg
 
     def sign(self) -> None:
         self._signed = True
 
+        base_msg = self.get_base_message()
+        signature_mime = create_signature_mime(base_msg)
+        self._generated_message = form_signed_message(signature_mime, base_msg)
+        self._set_headers()
+        # Message is signed, changing it is not allowed.
+        self._locked = True
+
     def encrypt(self, public_keys: List[str], sign: bool=False) -> None:
         self._encrypted = True
         self._signed = sign
-        self.public_keys = public_keys
 
-    def message(self) -> SafeMIMEMultipart:
-        original = super().message()
-        if self._encrypted or self._signed:
-            raise NotImplementedError()
+        base_msg = self.get_base_message()
+        encrypted_content = gpg_encrypt_content(base_msg.as_string(linesep='\r\n'),
+                                                public_keys, sign)
+        self._generated_message = form_encrypted_message(encrypted_content)
+        self._set_headers()
+        # Message is encrypted, changing it is not allowed.
+        self._locked = True
 
-        return original
+    def message(self) -> Message:
+        if self._generated_message is None:
+            return super().message()
+
+        return self._generated_message
 
 def pgp_sign_and_encrypt(message: PGPEmailMessage, to_users: List[UserProfile],
                          force_single_message: bool=False) -> List[PGPEmailMessage]:
@@ -111,3 +180,46 @@ def _sign_and_encrypt(message: PGPEmailMessage, to_emails: List[str],
         prepared_messages.append(encrypted_message)
 
     return prepared_messages
+
+def create_signature_mime(to_sign: Union[SafeMIMEMultipart, SafeMIMEText]) -> Message:
+    signature = gpg_sign_content(to_sign.as_bytes(linesep='\r\n'))
+    sigtype = 'pgp-signature; name="signature.asc"'
+    signature_mime = MIMEApplication(_data=signature, _subtype=sigtype,
+                                     _encoder=encode_7or8bit)
+    signature_mime['Content-Description'] = 'signature'
+    signature_mime.set_charset('us-ascii')
+    del signature_mime['MIME-Version']
+
+    return signature_mime
+
+def form_signed_message(signature_mime: Message,
+                        base_msg: Union[SafeMIMEMultipart, SafeMIMEText]) -> Message:
+    msg = SafeMIMEMultipart(_subtype='signed', encoding=base_msg.encoding,
+                            micalg='pgp-sha256', protocol='application/pgp-signature')
+    msg.attach(base_msg)
+    msg.attach(signature_mime)
+
+    return msg
+
+def form_encrypted_message(encrypted_content: bytes) -> Message:
+    version_string = "Version: 1"
+    control_mime = MIMEApplication(_data=version_string.encode(), _subtype='pgp-encrypted',
+                                   _encoder=encode_7or8bit)
+    encrypted_mime = MIMEApplication(_data=encrypted_content, _subtype='octet-stream',
+                                     _encoder=encode_7or8bit)
+    del control_mime['MIME-Version']
+    del encrypted_mime['MIME-Version']
+
+    msg = SafeMIMEMultipart(_subtype='encrypted', protocol='application/pgp-encrypted')
+    msg.attach(control_mime)
+    msg.attach(encrypted_mime)
+
+    return msg
+
+def gpg_sign_content(content: str) -> bytes:
+    return "dummysignature".encode()
+
+def gpg_encrypt_content(content: str, public_keys: List[str], sign: bool=False) -> bytes:
+    if sign:
+        return 'dummysignedciphertext'.encode()
+    return 'dummyciphertext'.encode()
--- zerver/tests/test_pgp.py
+++ zerver/tests/test_pgp.py
@@ -1,13 +1,45 @@
+from django.conf import settings
 from django.core import mail
+from django.core.mail import get_connection
+from django.core.mail.message import SafeMIMEText
 from django.test import override_settings
+
+from email.message import Message
 
 from zerver.lib.test_classes import ZulipTestCase
 from zerver.lib.pgp import pgp_sign_and_encrypt, PGPEmailMessage, PGPKeyNotFound
 from zerver.models import UserProfile, UserPGP
 
-from typing import List
+from typing import List, Dict
 
 class TestPGP(ZulipTestCase):
+    def check_signed_msg_structure(self, message: Message) -> None:
+        self.assertTrue(message.is_multipart())
+        self.assertEqual(message.get_content_subtype(), 'signed')
+        self.assertEqual(message.get_param('protocol'), 'application/pgp-signature')
+
+        parts = message.get_payload()
+        assert isinstance(parts, List)
+        assert isinstance(parts[1], Message)
+        self.assertEqual(parts[1].get_content_type(), 'application/pgp-signature')
+        self.assertEqual(parts[1].get_payload(), 'dummysignature')
+
+    def check_encrypted_msg_structure(self, message: Message,
+                                      signed: bool=False) -> None:
+        self.assertTrue(message.is_multipart())
+        self.assertEqual(message.get_content_subtype(), 'encrypted')
+        self.assertEqual(message.get_param('protocol'), 'application/pgp-encrypted')
+
+        parts = message.get_payload()
+        assert isinstance(parts, List)
+        assert isinstance(parts[0], Message)
+        assert isinstance(parts[1], Message)
+        self.assertEqual(parts[0].get_content_type(), 'application/pgp-encrypted')
+        self.assertEqual(parts[0].get_payload(), 'Version: 1')
+        self.assertEqual(parts[1].get_content_type(), 'application/octet-stream')
+        expected_payload = 'dummysignedciphertext' if signed else 'dummyciphertext'
+        self.assertEqual(parts[1].get_payload(), expected_payload)
+
     @override_settings(ENABLE_EMAIL_ENCRYPTION=True)
     @override_settings(ENABLE_EMAIL_SIGNATURES=True)
     def test_pgp_vanilla_message(self) -> None:
@@ -36,8 +68,10 @@ class TestPGP(ZulipTestCase):
         emails = pgp_sign_and_encrypt(msg, [user_profile])
         self.assertEqual(len(emails), 1)
         self.assertEqual(emails[0]._signed, True)
-        with self.assertRaises(NotImplementedError):
-            emails[0].send()
+        self.check_signed_msg_structure(emails[0].message())
+
+        emails[0].send()
+        self.assertEqual(len(mail.outbox), 1)
 
     @override_settings(ENABLE_EMAIL_ENCRYPTION=True)
     @override_settings(ENABLE_EMAIL_SIGNATURES=True)
@@ -53,8 +87,10 @@ class TestPGP(ZulipTestCase):
         emails = pgp_sign_and_encrypt(msg, [user_profile])
         self.assertEqual(len(emails), 1)
         self.assertEqual(emails[0]._encrypted, True)
-        with self.assertRaises(NotImplementedError):
-            emails[0].send()
+        self.check_encrypted_msg_structure(emails[0].message())
+
+        emails[0].send()
+        self.assertEqual(len(mail.outbox), 1)
 
     @override_settings(ENABLE_EMAIL_ENCRYPTION=True)
     @override_settings(ENABLE_EMAIL_SIGNATURES=True)
@@ -78,6 +114,7 @@ class TestPGP(ZulipTestCase):
         self.assertEqual(len(emails), 1)
         self.assertEqual(emails[0]._signed, True)
         self.assertEqual(emails[0]._encrypted, True)
+        self.check_encrypted_msg_structure(emails[0].message(), signed=True)
 
         user_profiles[0].want_signed_emails = False
         user_profiles[0].want_encrypted_emails = False
@@ -102,6 +139,7 @@ class TestPGP(ZulipTestCase):
         self.assertEqual(len(emails), 1)
         self.assertEqual(emails[0]._signed, True)
         self.assertEqual(emails[0]._encrypted, False)
+        self.check_signed_msg_structure(emails[0].message())
 
     @override_settings(ENABLE_EMAIL_ENCRYPTION=True)
     def test_pgp_missing_public_key(self) -> None:
@@ -112,3 +150,37 @@ class TestPGP(ZulipTestCase):
         msg = PGPEmailMessage(subject='Subject', body='Email body')
         with self.assertRaises(PGPKeyNotFound):
             pgp_sign_and_encrypt(msg, [user_profile])
+
+    def test_pgpemailmessage_setattr_lock(self) -> None:
+        pgp_msg = PGPEmailMessage(body='text')
+        pgp_msg._locked = True
+
+        with self.assertRaises(AttributeError):
+            pgp_msg.body = "text 2"
+
+        conn = get_connection()
+        pgp_msg.connection = conn
+        self.assertEqual(pgp_msg.connection, conn)
+
+        pgp_msg._locked = False
+        pgp_msg.body = "text 3"
+        self.assertEqual(pgp_msg.body, 'text 3')
+
+    def test_pgpemailmessage_set_headers(self) -> None:
+        headers = {}  # type: Dict[str, str]
+        headers['Custom-Header'] = 'some content'
+        headers['From'] = self.example_email('iago')
+        msg = PGPEmailMessage(subject='Subject', body='Email body',
+                              to=[self.example_email('hamlet')],
+                              headers=headers,
+                              cc=[self.example_email('cordelia')],
+                              reply_to=[settings.DEFAULT_FROM_EMAIL])
+        original_message = msg.message()
+        msg._generated_message = SafeMIMEText("text")
+
+        msg._set_headers()
+        headers_list = ['Custom-Header', 'Subject', 'To', 'Cc', 'Reply-To']
+        assert msg._generated_message is not None
+        for header in headers_list:
+            self.assertEqual(msg._generated_message[header],
+                             original_message[header])
