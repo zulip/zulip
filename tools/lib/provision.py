zulip/zulip

commit 7d12e2019de5bc2311ea8b1060ff917f8aa5c7c2

provision: Generate static bots files using a function call.
This shaves about 350ms from the no-op provision runtime.

Fixes #10990.

tools/lib/provision.py
@@ -24,6 +24,7 @@ from version import PROVISION_VERSION
 if False:
     from typing import Any
 
+from tools.setup.generate_zulip_bots_static_files import generate_zulip_bots_static_files
 
 SUPPORTED_PLATFORMS = {
     "Ubuntu": [
@@ -299,7 +300,7 @@ def main(options):
     run(["tools/setup/emoji/build_emoji"])
 
     # copy over static files from the zulip_bots package
-    run(["tools/setup/generate_zulip_bots_static_files"])
+    generate_zulip_bots_static_files()
 
     webfont_paths = ["tools/setup/generate-custom-icon-webfont", "static/icons/fonts/template.hbs"]
     webfont_paths += glob.glob('static/assets/icons/*')

commit 7420dc19cfef5ecad00f4bb2ac6e32a4c2e030bd

provision: Run generate_secrets earlier.
Commit 7d12e2019de5bc2311ea8b1060ff917f8aa5c7c2 (#10994) broke fresh
provisions by importing zproject.settings before we were done
modifying settings.  Fix it by moving the generate_secrets invocation
to the earliest reasonable place.

Signed-off-by: Anders Kaseorg <andersk@mit.edu>

tools/lib/provision.py
@@ -398,6 +398,9 @@ def main(options):
     setup_shell_profile('~/.bash_profile')
     setup_shell_profile('~/.zprofile')
 
+    # This needs to happen before anything that imports zproject.settings.
+    run(["scripts/setup/generate_secrets.py", "--development"])
+
     run(["sudo", "cp", REPO_STOPWORDS_PATH, TSEARCH_STOPWORDS_PATH])
 
     # create log directory `zulip/var/log`
@@ -436,8 +439,6 @@ def main(options):
         run(["tools/setup/build_pygments_data"])
     else:
         print("No need to run `tools/setup/build_pygments_data`.")
-
-    run(["scripts/setup/generate_secrets.py", "--development"])
 
     update_authors_json_paths = ["tools/update-authors-json", "zerver/tests/fixtures/authors.json"]
     if file_or_package_hash_updated(update_authors_json_paths, "update_authors_json_hash", options.is_force):