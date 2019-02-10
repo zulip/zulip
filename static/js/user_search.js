5d8664d7259e226c17b0eec690dd0ffcaa6b9dc7
Steve Howell <showell@zulipchat.com>
2019-02-10 18:58:59 +0000

    refactor: Add an explicit `user_search_section`.
    This, among other things, makes it easier to
    troubleshoot the resize code.

--- static/js/user_search.js
+++ static/js/user_search.js
@@ -5,7 +5,7 @@ var user_search = function (opts) {
 
     var self = {};
 
-    var $widget = $('#user-list .input-append').expectOne();
+    var $widget = $('#user_search_section').expectOne();
     var $input = $('.user-list-filter').expectOne();
 
     self.input_field = function () {
