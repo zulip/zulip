579e2e8b2b21f84385574bfc355825d1fa6895e6
Samuel Searles-Bryant <samuel.searles-bryant@unipart.io>
2019-02-27 19:20:18 +0000

    docs: Add Zulip logo to ReadTheDocs heading.
    Add the Zulip logo to the docs that will appear on readthedocs.
    Also remove the house logo and "Zulip" text that appear above the logo.
    
    Fixes: #11700.

--- docs/conf.py
+++ docs/conf.py
@@ -127,6 +127,7 @@ else:
 # documentation.
 html_theme_options = {
     'collapse_navigation': False,
+    'logo_only': True,
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
@@ -141,7 +142,7 @@ html_theme_options = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = '../static/images/logo/zulip-org-logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
