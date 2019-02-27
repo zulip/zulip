dcedaa2ad4f3b07f8fdaae7fa17d55d5e0ddf836
Vishnu Ks <yo@vishnuks.com>
2019-02-27 18:23:43 +0530

    billing: Seperate out tests for invoice and autopay form fields.

--- frontend_tests/node_tests/upgrade.js
+++ frontend_tests/node_tests/upgrade.js
@@ -52,7 +52,7 @@ run_test("initialize", () => {
         assert.equal(config_opts.panelLabel, "Make payment");
         assert.equal(config_opts.label, "Add card");
         assert.equal(config_opts.allowRememberMe, false);
-        assert.equal(config_opts.email, "{{email}}");
+        assert.equal(config_opts.email, "{{ email }}");
         assert.equal(config_opts.description, "Zulip Cloud Standard");
         token_func("stripe_add_card_token");
     };
@@ -132,28 +132,50 @@ run_test("initialize", () => {
     assert.equal($("#invoice_annual_price_per_month").text(), "5.34");
 });
 
-run_test("upgrade_template", () => {
-    var license_options = document.querySelectorAll("input[type=radio][name=license_management]");
+run_test("autopay_form_fields", () => {
+    assert.equal(document.querySelector("#autopay-form").dataset.key, "{{ publishable_key }}");
+    assert.equal(document.querySelector("#autopay-form").dataset.email, "{{ email }}");
+    assert.equal(document.querySelector("#autopay-form [name=seat_count]").value, "{{ seat_count }}");
+    assert.equal(document.querySelector("#autopay-form [name=signed_seat_count]").value, "{{ signed_seat_count }}");
+    assert.equal(document.querySelector("#autopay-form [name=salt]").value, "{{ salt }}");
+    assert.equal(document.querySelector("#autopay-form [name=billing_modality]").value, "charge_automatically");
+    assert.equal(document.querySelector("#autopay-form #automatic_license_count").value, "{{ seat_count }}");
+    assert.equal(document.querySelector("#autopay-form #manual_license_count").min, "{{ seat_count }}");
+
+    var license_options = document.querySelectorAll("#autopay-form input[type=radio][name=license_management]");
     assert.equal(license_options.length, 2);
     assert.equal(license_options[0].value, "automatic");
     assert.equal(license_options[1].value, "manual");
 
-    var schedule_options = document.querySelectorAll("input[type=radio][name=schedule]");
-    assert.equal(schedule_options.length, 3);
+    var schedule_options = document.querySelectorAll("#autopay-form input[type=radio][name=schedule]");
+    assert.equal(schedule_options.length, 2);
     assert.equal(schedule_options[0].value, "annual");
     assert.equal(schedule_options[1].value, "monthly");
-    assert.equal(schedule_options[2].value, "annual");
 
-    // Elements necessary for create_ajax_request
     assert(document.querySelector("#autopay-error"));
     assert(document.querySelector("#autopay-loading"));
     assert(document.querySelector("#autopay"));
     assert(document.querySelector("#autopay-success"));
+    assert(document.querySelector("#autopay_loading_indicator"));
+
+    assert(document.querySelector("input[name=csrfmiddlewaretoken]"));
+});
+
+run_test("invoice_form_fields", () => {
+    assert.equal(document.querySelector("#invoice-form [name=signed_seat_count]").value, "{{ signed_seat_count }}");
+    assert.equal(document.querySelector("#invoice-form [name=salt]").value, "{{ salt }}");
+    assert.equal(document.querySelector("#invoice-form [name=billing_modality]").value, "send_invoice");
+    assert.equal(document.querySelector("#invoice-form [name=licenses]").min, "{{ min_invoiced_licenses }}");
+
+    var schedule_options = document.querySelectorAll("#invoice-form input[type=radio][name=schedule]");
+    assert.equal(schedule_options.length, 1);
+    assert.equal(schedule_options[0].value, "annual");
 
     assert(document.querySelector("#invoice-error"));
     assert(document.querySelector("#invoice-loading"));
     assert(document.querySelector("#invoice"));
     assert(document.querySelector("#invoice-success"));
+    assert(document.querySelector("#invoice_loading_indicator"));
 
     assert(document.querySelector("input[name=csrfmiddlewaretoken]"));
 });
