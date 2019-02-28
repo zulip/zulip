zulip/zulip

commit 65f6bea7d7d5721b8e2be0e78fa3046d00885ad5

org settings: Move video chat provider into other settings.

static/templates/settings/organization-settings-admin.handlebars
@@ -154,10 +154,45 @@
 
         <div id="org-msg-feed" class="org-subsection-parent">
             <div class="subsection-header">
-                <h3>{{t "Message feed" }}</h3>
+                <h3>{{t "Other settings" }}</h3>
                 {{ partial "settings-save-discard-widget" "section_name" "msg-feed" }}
             </div>
             <div class="inline-block organization-settings-parent">
+                <div class="input-group">
+                    <label for="realm_video_chat_provider" class="dropdown-title">
+                        {{t 'Video chat provider' }}
+                    </label>
+                    <select name="realm_video_chat_provider" id="id_realm_video_chat_provider">
+                        {{#each realm_available_video_chat_providers}}
+                        <option value='{{this}}'>{{this}}</option>
+                        {{/each}}
+                    </select>
+                    <div id="google_hangouts_domain">
+                        <label>{{t 'Domain for your <a href="https://gsuite.google.com" target="_blank">G Suite team</a> (required)' }}:</label>
+                        <input type="text" id="id_realm_google_hangouts_domain"
+                          name="realm_google_hangouts_domain"
+                          class="admin-realm-google-hangouts-domain"/>
+                    </div>
+                    <div id="zoom_user_id" class="zoom_credentials">
+                        <label>{{t 'Zoom user ID (required)' }}:</label>
+                        <input type="text" id="id_realm_zoom_user_id"
+                          name="realm_zoom_user_id"
+                          class="admin-realm-zoom-field"/>
+                    </div>
+                    <div id="zoom_api_key" class="zoom_credentials">
+                        <label>{{t 'Zoom API key (required)' }}:</label>
+                        <input type="text" id="id_realm_zoom_api_key"
+                          name="realm_zoom_api_key"
+                          class="admin-realm-zoom-field"/>
+                    </div>
+                    <div id="zoom_api_secret" class="zoom_credentials">
+                        <label>{{t 'Zoom API secret (required)' }}:</label>
+                        <input type="text" id="id_realm_zoom_api_secret"
+                          name="realm_zoom_api_secret"
+                          class="admin-realm-zoom-field"/>
+                    </div>
+                </div>
+
                 {{#if false}}
                 <div class="input-group">
                     <label for="realm_message_retention_days"
@@ -191,46 +226,6 @@
                   "is_checked" realm_inline_url_embed_preview
                   "label" admin_settings_label.realm_inline_url_embed_preview}}
                 {{/if}}
-            </div>
-        </div>
-
-        <div id="org-video-chat" class="org-subsection-parent">
-            <div class="subsection-header">
-                <h3>{{t "Video chat provider" }}</h3>
-                {{ partial "settings-save-discard-widget" "section_name" "video-chat" }}
-            </div>
-            <div class="inline-block organization-settings-parent">
-                <div class="input-group">
-                    <select name="realm_video_chat_provider" id="id_realm_video_chat_provider">
-                        {{#each realm_available_video_chat_providers}}
-                        <option value='{{this}}'>{{this}}</option>
-                        {{/each}}
-                    </select>
-                    <div id="google_hangouts_domain">
-                        <label>{{t 'Domain for your <a href="https://gsuite.google.com" target="_blank">G Suite team</a> (required)' }}:</label>
-                        <input type="text" id="id_realm_google_hangouts_domain"
-                          name="realm_google_hangouts_domain"
-                          class="admin-realm-google-hangouts-domain"/>
-                    </div>
-                    <div id="zoom_user_id" class="zoom_credentials">
-                        <label>{{t 'Zoom user ID (required)' }}:</label>
-                        <input type="text" id="id_realm_zoom_user_id"
-                          name="realm_zoom_user_id"
-                          class="admin-realm-zoom-field"/>
-                    </div>
-                    <div id="zoom_api_key" class="zoom_credentials">
-                        <label>{{t 'Zoom API key (required)' }}:</label>
-                        <input type="text" id="id_realm_zoom_api_key"
-                          name="realm_zoom_api_key"
-                          class="admin-realm-zoom-field"/>
-                    </div>
-                    <div id="zoom_api_secret" class="zoom_credentials">
-                        <label>{{t 'Zoom API secret (required)' }}:</label>
-                        <input type="text" id="id_realm_zoom_api_secret"
-                          name="realm_zoom_api_secret"
-                          class="admin-realm-zoom-field"/>
-                    </div>
-                </div>
             </div>
         </div>
     </form>

commit ab95704d2d9ec6f6f86a7023fbe060024e3415a6

settings: Fix mismatch of templates with JavaScript.
The alignment between the "Organization settings" code/labels and the
actual logic was broken in 65f6bea7d7d5721b8e2be0e78fa3046d00885ad5
and the related commits.

static/js/settings_org.js
@@ -28,7 +28,7 @@ var org_settings = {
             type: 'bool',
         },
     },
-    msg_feed: {
+    other_settings: {
         inline_image_preview: {
             type: 'bool',
         },
@@ -38,8 +38,6 @@ var org_settings = {
         mandatory_topics: {
             type: 'bool',
         },
-    },
-    video_chat: {
         video_chat_provider: {
             type: 'text',
         },
@@ -55,6 +53,9 @@ var org_settings = {
         zoom_api_secret: {
             type: 'text',
         },
+        message_content_allowed_in_email_notifications: {
+            type: 'bool',
+        },
     },
     user_defaults: {
         default_language: {
@@ -66,9 +67,6 @@ var org_settings = {
     },
     notifications: {
         send_welcome_emails: {
-            type: 'bool',
-        },
-        message_content_allowed_in_email_notifications: {
             type: 'bool',
         },
     },

static/templates/settings/organization-settings-admin.handlebars
@@ -144,10 +144,10 @@
             </div>
         </div>
 
-        <div id="org-msg-feed" class="org-subsection-parent">
+        <div id="org-other-settings" class="org-subsection-parent">
             <div class="subsection-header">
                 <h3>{{t "Other settings" }}</h3>
-                {{ partial "settings-save-discard-widget" "section_name" "msg-feed" }}
+                {{ partial "settings-save-discard-widget" "section_name" "other-settings" }}
             </div>
             <div class="inline-block organization-settings-parent">
                 <div class="input-group">