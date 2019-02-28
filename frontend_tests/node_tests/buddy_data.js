ca64b9642b096bbada3879aa3cd5d6afb227e92e
vinitS101 <vinit.singh9013@gmail.com>
2019-02-28 16:12:39 +0530

    user status: Fix buddy_data tests to account for changes in hover text.
    issue #11607

--- frontend_tests/node_tests/buddy_data.js
+++ frontend_tests/node_tests/buddy_data.js
@@ -111,12 +111,12 @@ run_test('buddy_status', () => {
 });
 
 run_test('user_title', () => {
-    assert.equal(buddy_data.user_title(me.user_id), 'Human Myself is active');
+    assert.equal(buddy_data.user_title(me.user_id), 'Human Myself\nLast online: translated: Online now');
     user_status.set_status_text({
         user_id: me.user_id,
         status_text: 'out to lunch',
     });
-    assert.equal(buddy_data.user_title(me.user_id), 'out to lunch');
+    assert.equal(buddy_data.user_title(me.user_id), 'Human Myself\nout to lunch\nLast online: translated: Online now');
 });
 
 run_test('simple search', () => {
