fb750a2656c0858a3fbca0dafc898d158cb31d36
Steve Howell <showell@zulipchat.com>
2019-03-08 18:14:15 +0000

    bug fix: Fix hovers for /me messages.
    This fixes several bugs with /me messages:
    
        * We no longer hover name if you're over
          the message.
    
        * We now launch the user popup if you
          click on the name.
    
        * Even if you click on the avatar, we
          launch the user popup to the right
          of the name.  (I think this is odd,
          but it's consistent with how we
          do it for normal messages.)
    
    The underlying problem here is that you have
    two possible organizations.
    
    From a logical standpoint, the image and
    name go together (and both launch the user menu):
    
        img Alice | says hi
    
    From a physical perspective, the main message
    is "Alice says hi" and it's aligned differently
    from the image:
    
        img | Alice says hi
    
    Our HTML reflects the latter.
    
    HTML doesn't allow overlapping diffs, of course,
    so you have to pick your poison.

--- static/js/ui_init.js
+++ static/js/ui_init.js
@@ -166,12 +166,12 @@ exports.initialize_kitchen_sink_stuff = function () {
         message_unhover();
     });
 
-    $("#main_div").on("mouseover", ".message_sender", function () {
+    $("#main_div").on("mouseover", ".sender_info_hover", function () {
         var row = $(this).closest(".message_row");
         row.addClass("sender_name_hovered");
     });
 
-    $("#main_div").on("mouseout", ".message_sender", function () {
+    $("#main_div").on("mouseout", ".sender_info_hover", function () {
         var row = $(this).closest(".message_row");
         row.removeClass("sender_name_hovered");
     });
