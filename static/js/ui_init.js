1157aef8b32b1d412b423ea82a55515010fca239
Tim Abbott <tabbott@zulipchat.com>
2019-03-08 15:44:11 -0800

    night mode: Fix initial state of night mode.

--- static/js/ui_init.js
+++ static/js/ui_init.js
@@ -357,7 +357,6 @@ exports.initialize_everything = function () {
     sent_messages.initialize();
     hotspots.initialize();
     ui.initialize();
-    night_mode.initialize();
     panels.initialize();
     typing.initialize();
     starred_messages.initialize();
