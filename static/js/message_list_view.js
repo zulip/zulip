bc1ef824efcfe9b831fc4aa4a6aa12ef3b4d2746
Vaibhav <vrongmeal@gmail.com>
2019-03-13 15:49:45 +0530

    message_list_view: Extract `_get_msg_timestring` method.
    This method gets the timestring for the message container when the
    message was last edited.

--- static/js/message_list_view.js
+++ static/js/message_list_view.js
@@ -191,6 +191,15 @@ MessageListView.prototype = {
     // trigger a re-render
     _RENDER_THRESHOLD: 50,
 
+    _get_msg_timestring: function (message_container) {
+        if (message_container.msg.last_edit_timestamp !== undefined) {
+            var last_edit_time = new XDate(message_container.msg.last_edit_timestamp * 1000);
+            var today = new XDate();
+            return timerender.render_date(last_edit_time, undefined, today)[0].textContent +
+                " at " + timerender.stringify_time(last_edit_time);
+        }
+    },
+
     _add_msg_timestring: function (message_container) {
         if (message_container.msg.last_edit_timestamp !== undefined) {
             // Add or update the last_edit_timestr
