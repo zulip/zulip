zulip/zulip

commit dee4e3fb8998c1d896c36b9d6ef097f3a21121ce

ajax: Fix wrong ajax setting name from beforeSubmit to beforeSend.
The beforeSubmit function was a feature of the jquery-form plugin that
we removed months ago; the appropriate similar feature of jQuery's
built-in AJAX library is beforeSend.

static/js/settings_account.js
@@ -329,7 +329,7 @@ exports.set_up = function () {
         channel.patch({
             url: "/json/settings",
             data: data,
-            beforeSubmit: function () {
+            beforeSend: function () {
                 if (page_params.realm_password_auth_enabled !== false) {
                     // FIXME: Check that the two password fields match
                     // FIXME: Use the same jQuery validation plugin as the signup form?

static/js/invite.js
@@ -39,7 +39,7 @@ function submit_invitation_form() {
     channel.post({
         url: "/json/invites",
         data: data,
-        beforeSubmit: function () {
+        beforeSend: function () {
             reset_error_messages();
             // TODO: You could alternatively parse the textarea here, and return errors to
             // the user if they don't match certain constraints (i.e. not real email addresses,

commit 6e8e7feffc6a055d7fd2bb58e15864d0e5807316

settings: Fix webapp password change UI.
Apparently, this has been broken since
dee4e3fb8998c1d896c36b9d6ef097f3a21121ce, due to the beforeSend code
here overriding the default beforeSend function that sets the CSRF
token.  The correct fix was actually to just run the relevant code
directly before the channel.patch call.

Fixes #11938.

static/js/settings_account.js
@@ -326,42 +326,33 @@ exports.set_up = function () {
             confirm_password: $('#confirm_password').val(),
         };
 
+        var new_pw_field = $('#new_password');
+        var new_pw = data.new_password;
+        if (new_pw !== '') {
+            var password_ok = common.password_quality(new_pw, undefined, new_pw_field);
+            if (password_ok === undefined) {
+                // zxcvbn.js didn't load, for whatever reason.
+                settings_change_error(
+                    'An internal error occurred; try reloading the page. ' +
+                        'Sorry for the trouble!');
+                return;
+            } else if (!password_ok) {
+                settings_change_error(i18n.t('New password is too weak'));
+                return;
+            }
+        }
+
         channel.patch({
             url: "/json/settings",
             data: data,
-            beforeSend: function () {
-                if (page_params.realm_password_auth_enabled !== false) {
-                    // FIXME: Check that the two password fields match
-                    // FIXME: Use the same jQuery validation plugin as the signup form?
-                    var field = $('#new_password');
-                    var new_pw = $('#new_password').val();
-                    if (new_pw !== '') {
-                        var password_ok = common.password_quality(new_pw, undefined, field);
-                        if (password_ok === undefined) {
-                            // zxcvbn.js didn't load, for whatever reason.
-                            settings_change_error(
-                                'An internal error occurred; try reloading the page. ' +
-                                'Sorry for the trouble!');
-                            return false;
-                        } else if (!password_ok) {
-                            settings_change_error(i18n.t('New password is too weak'));
-                            return false;
-                        }
-                    }
-                }
-                return true;
-            },
             success: function () {
                 settings_change_success(i18n.t("Updated settings!"));
                 overlays.close_modal('change_password_modal');
-            },
-            complete: function () {
-                // Whether successful or not, clear the password boxes.
-                // TODO: Clear these earlier, while the request is still pending.
                 clear_password_change();
             },
             error: function (xhr) {
                 ui_report.error(i18n.t("Failed"), xhr, change_password_info);
+                clear_password_change();
             },
         });
     });