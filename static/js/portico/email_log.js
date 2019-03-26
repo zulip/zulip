c27d92766359114d1559a18b331ac4e1f19f7ab7
Abhinav Singh <abhinavsingh22@hotmail.com>
2019-03-26 21:47:02 +0530

    refactor: Remove inline javascript code in email_log.html.
    All the inline javascript code present in email_log.html(which is
    rendered when the user visits "/emails" in development mode) is
    transferred to a new file: email_log.js in portico/ directory.
    
    Fixes #11608.

---
+++ static/js/portico/email_log.js
@@ -0,0 +1,44 @@
+$(function () {
+    // This code will be executed when the user visits /emails in
+    // development mode and email_log.html is rendered.
+    $('#toggle').change(function () {
+        if ($('.email-text').css('display') === 'none') {
+            $(".email-text").each(function () {
+                $(this).css("display", "block");
+            });
+            $(".email-html").each(function () {
+                $(this).css("display", "none");
+            });
+        } else {
+            $(".email-text").each(function () {
+                $(this).css("display", "none");
+            });
+            $(".email-html").each(function () {
+                $(this).css("display", "block");
+            });
+        }
+    });
+    $('input[type=radio][name=forward]').on('change', function () {
+        if ($(this).val() === "enabled") {
+            $("#forward_address_sections").show();
+        } else {
+            $("#forward_address_sections").hide();
+        }
+    });
+    $("#save_smptp_details").on("click", function () {
+        var address = $('input[name=forward]:checked').val() === "enabled" ? $("#address").val() : "";
+        var csrf_token = $('input[name="csrfmiddlewaretoken"]').attr('value');
+        var data = {forward_address: address, csrfmiddlewaretoken: csrf_token};
+
+        channel.post({
+            url: "/emails/",
+            data: data,
+            success: function () {
+                $("#smtp_form_status").show();
+                setTimeout(function () {
+                    $("#smtp_form_status").hide();
+                }, 3000);
+            },
+        });
+    });
+});
