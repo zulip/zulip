1a8618ee6d204d6b3cbb96bf440b056748d08a5a
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:52:56 -0700

    Fixed PYTHON_E302 issues in zerver/webhooks/codeship/tests.py

--- zerver/webhooks/codeship/tests.py
+++ zerver/webhooks/codeship/tests.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from zerver.lib.test_classes import WebhookTestCase
 
+
 class CodeshipHookTests(WebhookTestCase):
     STREAM_NAME = 'codeship'
     URL_TEMPLATE = u"/api/v1/external/codeship?stream={stream}&api_key={api_key}"
