dc4e7d05985b6379b701fed9467d15023c5083f8
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:43:43 -0700

    Fixed PYTHON_E302 issues in zerver/lib/timezone.py

--- zerver/lib/timezone.py
+++ zerver/lib/timezone.py
@@ -3,8 +3,10 @@ from typing import List
 
 import pytz
 
+
 def get_all_timezones() -> List[str]:
     return sorted(pytz.all_timezones)
+
 
 def get_timezone(tz: str) -> pytz.datetime.tzinfo:
     return pytz.timezone(tz)
