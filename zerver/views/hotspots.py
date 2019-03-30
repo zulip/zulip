4150e026100a99f5afa542082a2870316ff2750d
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:39:26 -0700

    Fixed PYTHON_E302 issues in zerver/views/hotspots.py

--- zerver/views/hotspots.py
+++ zerver/views/hotspots.py
@@ -9,6 +9,7 @@ from zerver.lib.response import json_error, json_success
 from zerver.lib.validator import check_string
 from zerver.models import UserProfile
 
+
 @human_users_only
 @has_request_variables
 def mark_hotspot_as_read(request: HttpRequest, user: UserProfile,
