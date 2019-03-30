3b51d0bb967fe85354cb4e3d5f0ce1ddcc596ba8
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:50:54 -0700

    Fixed PYTHON_E302 issues in zerver/migrations/0139_fill_last_message_id_in_subscription_logs.py

--- zerver/migrations/0139_fill_last_message_id_in_subscription_logs.py
+++ zerver/migrations/0139_fill_last_message_id_in_subscription_logs.py
@@ -5,12 +5,14 @@ from django.db import migrations
 from django.db.backends.postgresql_psycopg2.schema import DatabaseSchemaEditor
 from django.db.migrations.state import StateApps
 
+
 def backfill_last_message_id(apps: StateApps, schema_editor: DatabaseSchemaEditor) -> None:
     event_type = ['subscription_created', 'subscription_deactivated', 'subscription_activated']
     RealmAuditLog = apps.get_model('zerver', 'RealmAuditLog')
     subscription_logs = RealmAuditLog.objects.filter(
         event_last_message_id__isnull=True, event_type__in=event_type)
     subscription_logs.update(event_last_message_id=-1)
+
 
 class Migration(migrations.Migration):
 
