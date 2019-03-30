14d1231fe203fd3c2aeb8ef873cb996d033e9a01
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:57:57 -0700

    Fixed PYTHON_E302 issues in zerver/webhooks/ifttt/view.py

--- zerver/webhooks/ifttt/view.py
+++ zerver/webhooks/ifttt/view.py
@@ -9,6 +9,7 @@ from zerver.lib.response import json_error, json_success
 from zerver.lib.webhooks.common import check_send_webhook_message
 from zerver.models import UserProfile
 
+
 @api_key_only_webhook_view('IFTTT')
 @has_request_variables
 def api_iftt_app_webhook(request: HttpRequest, user_profile: UserProfile,
