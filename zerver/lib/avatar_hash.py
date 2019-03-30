e3c739c1d8637542f3a5063848c847618b0ad297
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:54:42 -0700

    Fixed PYTHON_E302 issues in zerver/lib/avatar_hash.py

--- zerver/lib/avatar_hash.py
+++ zerver/lib/avatar_hash.py
@@ -7,6 +7,7 @@ from zerver.models import UserProfile
 
 import hashlib
 
+
 def gravatar_hash(email: str) -> str:
     """Compute the Gravatar hash for an email address."""
     # Non-ASCII characters aren't permitted by the currently active e-mail
@@ -15,6 +16,7 @@ def gravatar_hash(email: str) -> str:
     # typo an address or someone manages to give us a non-ASCII address, let's
     # not error out on it.
     return make_safe_digest(email.lower(), hashlib.md5)
+
 
 def user_avatar_hash(uid: str) -> str:
 
@@ -27,11 +29,13 @@ def user_avatar_hash(uid: str) -> str:
     user_key = uid + settings.AVATAR_SALT
     return make_safe_digest(user_key, hashlib.sha1)
 
+
 def user_avatar_path(user_profile: UserProfile) -> str:
 
     # WARNING: If this method is changed, you may need to do a migration
     # similar to zerver/migrations/0060_move_avatars_to_be_uid_based.py .
     return user_avatar_path_from_ids(user_profile.id, user_profile.realm_id)
+
 
 def user_avatar_path_from_ids(user_profile_id: int, realm_id: int) -> str:
     user_id_hash = user_avatar_hash(str(user_profile_id))
