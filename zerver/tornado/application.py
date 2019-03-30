8e5fbef043d5ec610de9a5642927a8f8e851871a
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:54:13 -0700

    Fixed PYTHON_E302 issues in zerver/tornado/application.py

--- zerver/tornado/application.py
+++ zerver/tornado/application.py
@@ -9,12 +9,14 @@ from zerver.lib.queue import get_queue_client
 from zerver.tornado.handlers import AsyncDjangoHandler
 from zerver.tornado.socket import get_sockjs_router
 
+
 def setup_tornado_rabbitmq() -> None:  # nocoverage
     # When tornado is shut down, disconnect cleanly from rabbitmq
     if settings.USING_RABBITMQ:
         queue_client = get_queue_client()
         atexit.register(lambda: queue_client.close())
         autoreload.add_reload_hook(lambda: queue_client.close())
+
 
 def create_tornado_application(port: int) -> tornado.web.Application:
     urls = (
