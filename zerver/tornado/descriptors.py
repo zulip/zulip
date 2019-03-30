392ea33f6bcf1dd14afdbaf6e86060ba5430dc1e
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:54:35 -0700

    Fixed PYTHON_E302 issues in zerver/tornado/descriptors.py

--- zerver/tornado/descriptors.py
+++ zerver/tornado/descriptors.py
@@ -5,12 +5,15 @@ if False:
 
 descriptors_by_handler_id = {}  # type: Dict[int, ClientDescriptor]
 
+
 def get_descriptor_by_handler_id(handler_id: int) -> Optional['ClientDescriptor']:
     return descriptors_by_handler_id.get(handler_id)
+
 
 def set_descriptor_by_handler_id(handler_id: int,
                                  client_descriptor: 'ClientDescriptor') -> None:
     descriptors_by_handler_id[handler_id] = client_descriptor
+
 
 def clear_descriptor_by_handler_id(handler_id: int,
                                    client_descriptor: 'ClientDescriptor') -> None:
