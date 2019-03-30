39b73dc6f0141e4363df7e092714ac5e9798b3c5
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:50:58 -0700

    Fixed PYTHON_E302 issues in zilencer/management/commands/migrate_stream_notifications.py

--- zilencer/management/commands/migrate_stream_notifications.py
+++ zilencer/management/commands/migrate_stream_notifications.py
@@ -5,6 +5,7 @@ from django.core.management.base import BaseCommand
 
 from zerver.models import Subscription
 
+
 class Command(BaseCommand):
     help = """One-off script to migration users' stream notification settings."""
 
