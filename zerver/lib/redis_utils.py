5813c5c15fc496f3de041f7ba7a99219ff917258
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:48:56 -0700

    Fixed PYTHON_E302 issues in zerver/lib/redis_utils.py

--- zerver/lib/redis_utils.py
+++ zerver/lib/redis_utils.py
@@ -3,6 +3,7 @@ from django.conf import settings
 
 import redis
 
+
 def get_redis_client() -> redis.StrictRedis:
     return redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT,
                              password=settings.REDIS_PASSWORD, db=0)
