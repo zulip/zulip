e739ad735a9e12521c6b9d33ede97940a699b800
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:41:17 -0700

    Fixed PYTHON_E302 issues in zerver/webhooks/homeassistant/tests.py

--- zerver/webhooks/homeassistant/tests.py
+++ zerver/webhooks/homeassistant/tests.py
@@ -1,6 +1,7 @@
 
 from zerver.lib.test_classes import WebhookTestCase
 
+
 class HomeAssistantHookTests(WebhookTestCase):
     STREAM_NAME = 'homeassistant'
     URL_TEMPLATE = "/api/v1/external/homeassistant?&api_key={api_key}&stream={stream}"
