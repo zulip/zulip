68052690f604a8f4a9286d4b66d95cf4a75b6bca
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:39:02 -0700

    Fixed PYTHON_E302 issues in zerver/management/commands/checkconfig.py

--- zerver/management/commands/checkconfig.py
+++ zerver/management/commands/checkconfig.py
@@ -4,6 +4,7 @@ from django.core.management.base import BaseCommand
 
 from zerver.lib.management import check_config
 
+
 class Command(BaseCommand):
     help = """Checks your Zulip Voyager Django configuration for issues."""
 
