117d0172dd5d5826893314f78d6c20d1cc8a3c33
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:42:00 -0700

    Fixed PYTHON_E302 issues in zerver/tornado/sharding.py

--- zerver/tornado/sharding.py
+++ zerver/tornado/sharding.py
@@ -2,12 +2,14 @@ from django.conf import settings
 
 from zerver.models import Realm
 
+
 def get_tornado_port(realm: Realm) -> int:
     if settings.TORNADO_SERVER is None:
         return 9993
     if settings.TORNADO_PROCESSES == 1:
         return int(settings.TORNADO_SERVER.split(":")[-1])
     return 9993
+
 
 def get_tornado_uri(realm: Realm) -> str:
     if settings.TORNADO_PROCESSES == 1:
@@ -16,10 +18,12 @@ def get_tornado_uri(realm: Realm) -> str:
     port = get_tornado_port(realm)
     return "http://127.0.0.1:%d" % (port,)
 
+
 def notify_tornado_queue_name(port: int) -> str:
     if settings.TORNADO_PROCESSES == 1:
         return "notify_tornado"
     return "notify_tornado_port_%d" % (port,)
+
 
 def tornado_return_queue_name(port: int) -> str:
     if settings.TORNADO_PROCESSES == 1:
