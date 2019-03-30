0f0b2a3e50b06a75197d39a6af0e76cf9cca48be
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:55:54 -0700

    Fixed PYTHON_E302 issues in zerver/lib/statistics.py

--- zerver/lib/statistics.py
+++ zerver/lib/statistics.py
@@ -6,6 +6,8 @@ from datetime import datetime, timedelta
 
 # Return the amount of Zulip usage for this user between the two
 # given dates
+
+
 def seconds_usage_between(user_profile: UserProfile, begin: datetime, end: datetime) -> timedelta:
     intervals = UserActivityInterval.objects.filter(user_profile=user_profile,
                                                     end__gte=begin,
