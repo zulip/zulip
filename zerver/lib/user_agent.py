1998508f35cd4387ca6f211c6dd2c5ef1277c82c
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:42:46 -0700

    Fixed PYTHON_E302 issues in zerver/lib/user_agent.py

--- zerver/lib/user_agent.py
+++ zerver/lib/user_agent.py
@@ -4,6 +4,8 @@ from typing import Dict
 # Warning: If you change this parsing, please test using
 #   zerver/tests/test_decorators.py
 # And extend zerver/tests/fixtures/user_agents_unique with any new test cases
+
+
 def parse_user_agent(user_agent: str) -> Dict[str, str]:
     match = re.match(
         """^ (?P<name> [^/ ]* [^0-9/(]* )
