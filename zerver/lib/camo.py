967f11b91ea49329bbac3acb8f597145c0352c6d
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:53:15 -0700

    Fixed PYTHON_E302 issues in zerver/lib/camo.py

--- zerver/lib/camo.py
+++ zerver/lib/camo.py
@@ -3,6 +3,7 @@ import codecs
 import hashlib
 import hmac
 
+
 def generate_camo_url(url: str) -> str:
     encoded_url = url.encode("utf-8")
     encoded_camo_key = settings.CAMO_KEY.encode("utf-8")
@@ -12,11 +13,14 @@ def generate_camo_url(url: str) -> str:
 
 # Encodes the provided URL using the same algorithm used by the camo
 # caching https image proxy
+
+
 def get_camo_url(url: str) -> str:
     # Only encode the url if Camo is enabled
     if settings.CAMO_URI == '':
         return url
     return "%s%s" % (settings.CAMO_URI, generate_camo_url(url))
+
 
 def is_camo_url_valid(digest: str, url: str) -> bool:
     camo_url = generate_camo_url(url)
