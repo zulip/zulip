7f60531dd1ee61bd8a9b97e58912f286c48f2941
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:51:24 -0700

    Fixed PYTHON_E302 issues in pgroonga/migrations/0002_html_escape_subject.py

--- pgroonga/migrations/0002_html_escape_subject.py
+++ pgroonga/migrations/0002_html_escape_subject.py
@@ -4,11 +4,13 @@ from django.db.backends.postgresql_psycopg2.schema import DatabaseSchemaEditor
 from django.db.migrations.state import StateApps
 from zerver.lib.migrate import do_batch_update
 
+
 def rebuild_pgroonga_index(apps: StateApps, schema_editor: DatabaseSchemaEditor) -> None:
     with connection.cursor() as cursor:
         do_batch_update(cursor, 'zerver_message', ['search_pgroonga'],
                         ["escape_html(subject) || ' ' || rendered_content"],
                         escape=False, batch_size=10000)
+
 
 class Migration(migrations.Migration):
     atomic = False
