856295827699bd80e1dfd63447cf9cf129ce9721
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:37:47 -0700

    Fixed PYTHON_E302 issues in zerver/views/alert_words.py

--- zerver/views/alert_words.py
+++ zerver/views/alert_words.py
@@ -11,12 +11,15 @@ from zerver.lib.validator import check_list, check_string
 from zerver.lib.actions import do_add_alert_words, do_remove_alert_words
 from zerver.lib.alert_words import user_alert_words
 
+
 def list_alert_words(request: HttpRequest, user_profile: UserProfile) -> HttpResponse:
     return json_success({'alert_words': user_alert_words(user_profile)})
+
 
 def clean_alert_words(alert_words: List[str]) -> List[str]:
     alert_words = [w.strip() for w in alert_words]
     return [w for w in alert_words if w != ""]
+
 
 @has_request_variables
 def add_alert_words(request: HttpRequest, user_profile: UserProfile,
@@ -24,6 +27,7 @@ def add_alert_words(request: HttpRequest, user_profile: UserProfile,
                     ) -> HttpResponse:
     do_add_alert_words(user_profile, clean_alert_words(alert_words))
     return json_success({'alert_words': user_alert_words(user_profile)})
+
 
 @has_request_variables
 def remove_alert_words(request: HttpRequest, user_profile: UserProfile,
