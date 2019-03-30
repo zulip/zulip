d6d905932e874f347025fba8ebf3654ef7e96a81
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:51:02 -0700

    Fixed PYTHON_E302 issues in analytics/models.py

--- analytics/models.py
+++ analytics/models.py
@@ -6,6 +6,7 @@ from django.db import models
 from zerver.lib.timestamp import floor_to_day
 from zerver.models import Realm, Stream, UserProfile
 
+
 class FillState(models.Model):
     property = models.CharField(max_length=40, unique=True)  # type: str
     end_time = models.DateTimeField()  # type: datetime.datetime
@@ -22,9 +23,12 @@ class FillState(models.Model):
 
 # The earliest/starting end_time in FillState
 # We assume there is at least one realm
+
+
 def installation_epoch() -> datetime.datetime:
     earliest_realm_creation = Realm.objects.aggregate(models.Min('date_created'))['date_created__min']
     return floor_to_day(earliest_realm_creation)
+
 
 def last_successful_fill(property: str) -> Optional[datetime.datetime]:
     fillstate = FillState.objects.filter(property=property).first()
@@ -33,6 +37,7 @@ def last_successful_fill(property: str) -> Optional[datetime.datetime]:
     if fillstate.state == FillState.DONE:
         return fillstate.end_time
     return fillstate.end_time - datetime.timedelta(hours=1)
+
 
 class BaseCount(models.Model):
     # Note: When inheriting from BaseCount, you may want to rearrange
@@ -46,6 +51,7 @@ class BaseCount(models.Model):
     class Meta:
         abstract = True
 
+
 class InstallationCount(BaseCount):
 
     class Meta:
@@ -53,6 +59,7 @@ class InstallationCount(BaseCount):
 
     def __str__(self) -> str:
         return "<InstallationCount: %s %s %s>" % (self.property, self.subgroup, self.value)
+
 
 class RealmCount(BaseCount):
     realm = models.ForeignKey(Realm, on_delete=models.CASCADE)
@@ -63,6 +70,7 @@ class RealmCount(BaseCount):
 
     def __str__(self) -> str:
         return "<RealmCount: %s %s %s %s>" % (self.realm, self.property, self.subgroup, self.value)
+
 
 class UserCount(BaseCount):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
@@ -76,6 +84,7 @@ class UserCount(BaseCount):
 
     def __str__(self) -> str:
         return "<UserCount: %s %s %s %s>" % (self.user, self.property, self.subgroup, self.value)
+
 
 class StreamCount(BaseCount):
     stream = models.ForeignKey(Stream, on_delete=models.CASCADE)
