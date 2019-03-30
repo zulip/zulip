fa69e190fc4261f628ab5f193f01264df7323a50
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:56:30 -0700

    Fixed PYTHON_E302 issues in analytics/tests/test_fixtures.py

--- analytics/tests/test_fixtures.py
+++ analytics/tests/test_fixtures.py
@@ -3,6 +3,8 @@ from analytics.lib.fixtures import generate_time_series_data
 from zerver.lib.test_classes import ZulipTestCase
 
 # A very light test suite; the code being tested is not run in production.
+
+
 class TestFixtures(ZulipTestCase):
     def test_deterministic_settings(self) -> None:
         # test basic business_hour / non_business_hour calculation
