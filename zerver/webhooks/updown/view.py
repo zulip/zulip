5f3fc07c4fd53ead1fa6681c8060c5091d4ac3b4
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:38:04 -0700

    Fixed PYTHON_E302 issues in zerver/webhooks/updown/view.py

--- zerver/webhooks/updown/view.py
+++ zerver/webhooks/updown/view.py
@@ -13,12 +13,14 @@ from zerver.models import UserProfile
 
 TOPIC_TEMPLATE = "{service_url}"
 
+
 def send_message_for_event(request: HttpRequest, user_profile: UserProfile,
                            event: Dict[str, Any]) -> None:
     event_type = get_event_type(event)
     subject = TOPIC_TEMPLATE.format(service_url=event['check']['url'])
     body = EVENT_TYPE_BODY_MAPPER[event_type](event)
     check_send_webhook_message(request, user_profile, subject, body)
+
 
 def get_body_for_up_event(event: Dict[str, Any]) -> str:
     body = "Service is `up`"
@@ -29,6 +31,7 @@ def get_body_for_up_event(event: Dict[str, Any]) -> str:
         if string_date:
             body = "{} after {}".format(body, string_date)
     return "{}.".format(body)
+
 
 def get_time_string_based_on_duration(duration: int) -> str:
     days, reminder = divmod(duration, 86400)
@@ -42,6 +45,7 @@ def get_time_string_based_on_duration(duration: int) -> str:
     string_date += add_time_part_to_string_date_if_needed(seconds, 'second')
     return string_date.rstrip()
 
+
 def add_time_part_to_string_date_if_needed(value: int, text_name: str) -> str:
     if value == 1:
         return "1 {} ".format(text_name)
@@ -49,10 +53,12 @@ def add_time_part_to_string_date_if_needed(value: int, text_name: str) -> str:
         return "{} {}s ".format(value, text_name)
     return ''
 
+
 def get_body_for_down_event(event: Dict[str, Any]) -> str:
     return "Service is `down`. It returned a {} error at {}.".format(
         event['downtime']['error'],
         event['downtime']['started_at'].replace('T', ' ').replace('Z', ' UTC'))
+
 
 @api_key_only_webhook_view('Updown')
 @has_request_variables
@@ -68,6 +74,7 @@ EVENT_TYPE_BODY_MAPPER = {
     'up': get_body_for_up_event,
     'down': get_body_for_down_event
 }
+
 
 def get_event_type(event: Dict[str, Any]) -> str:
     event_type_match = re.match('check.(.*)', event['event'])
