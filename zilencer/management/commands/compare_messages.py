3529df0c38744d72fb59f6f2c8a3a6629437fc7e
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:46:07 -0700

    Fixed PYTHON_E302 issues in zilencer/management/commands/compare_messages.py

--- zilencer/management/commands/compare_messages.py
+++ zilencer/management/commands/compare_messages.py
@@ -3,6 +3,7 @@ from typing import Any
 import ijson
 from django.core.management.base import BaseCommand, CommandParser
 
+
 class Command(BaseCommand):
     help = """
     Render messages to a file.
