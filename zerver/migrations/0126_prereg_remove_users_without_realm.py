b0ef8bb3af2b4c2f4878c4e4ece29466922e4ca6
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:47:02 -0700

    Fixed PYTHON_E302 issues in zerver/migrations/0126_prereg_remove_users_without_realm.py

--- zerver/migrations/0126_prereg_remove_users_without_realm.py
+++ zerver/migrations/0126_prereg_remove_users_without_realm.py
@@ -6,10 +6,12 @@ from django.db import migrations
 from django.db.backends.postgresql_psycopg2.schema import DatabaseSchemaEditor
 from django.db.migrations.state import StateApps
 
+
 def remove_prereg_users_without_realm(
         apps: StateApps, schema_editor: DatabaseSchemaEditor) -> None:
     prereg_model = apps.get_model("zerver", "PreregistrationUser")
     prereg_model.objects.filter(realm=None, realm_creation=False).delete()
+
 
 class Migration(migrations.Migration):
 
