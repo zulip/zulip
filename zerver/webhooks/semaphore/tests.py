eb95a787bb8986ed9c2e5542f9a60e14f53d9038
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:39:12 -0700

    Fixed PYTHON_E302 issues in zerver/webhooks/semaphore/tests.py

--- zerver/webhooks/semaphore/tests.py
+++ zerver/webhooks/semaphore/tests.py
@@ -2,6 +2,7 @@
 
 from zerver.lib.test_classes import WebhookTestCase
 
+
 class SemaphoreHookTests(WebhookTestCase):
     STREAM_NAME = 'semaphore'
     URL_TEMPLATE = "/api/v1/external/semaphore?stream={stream}&api_key={api_key}"
