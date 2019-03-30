aee0333f3ea4841a033ddbaa736d04ee8b243597
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 11:07:20 -0700

    Fixed PYTHON_E402 issues in scripts/lib/clean_node_cache.py

--- scripts/lib/clean_node_cache.py
+++ scripts/lib/clean_node_cache.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+from scripts.lib.zulip_tools import \
+    get_environment, get_recent_deployments, parse_cache_script_args, \
+    purge_unused_caches
 import argparse
 import os
 import subprocess
@@ -10,9 +13,6 @@ if False:
 
 ZULIP_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(ZULIP_PATH)
-from scripts.lib.zulip_tools import \
-    get_environment, get_recent_deployments, parse_cache_script_args, \
-    purge_unused_caches
 
 ENV = get_environment()
 NODE_MODULES_CACHE_PATH = "/srv/zulip-npm-cache"
