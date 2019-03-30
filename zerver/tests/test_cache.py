19ac15928a39c971d3c329ae416cf1a15553cd54
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:53:30 -0700

    Fixed PYTHON_E302 issues in zerver/tests/test_cache.py

--- zerver/tests/test_cache.py
+++ zerver/tests/test_cache.py
@@ -3,6 +3,7 @@ from mock import Mock, patch
 from zerver.apps import flush_cache
 from zerver.lib.test_classes import ZulipTestCase
 
+
 class AppsTest(ZulipTestCase):
     def test_cache_gets_flushed(self) -> None:
         with patch('zerver.apps.logging.info') as mock_logging:
