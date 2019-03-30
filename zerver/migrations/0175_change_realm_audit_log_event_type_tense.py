bec34336167cbde96158a3fb2ea664583ffd9b69
deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-03-30 10:46:26 -0700

    Fixed PYTHON_E302 issues in zerver/migrations/0175_change_realm_audit_log_event_type_tense.py

--- zerver/migrations/0175_change_realm_audit_log_event_type_tense.py
+++ zerver/migrations/0175_change_realm_audit_log_event_type_tense.py
@@ -6,11 +6,13 @@ from django.db import migrations
 from django.db.backends.postgresql_psycopg2.schema import DatabaseSchemaEditor
 from django.db.migrations.state import StateApps
 
+
 def change_realm_audit_log_event_type_tense(apps: StateApps, schema_editor: DatabaseSchemaEditor) -> None:
     RealmAuditLog = apps.get_model('zerver', 'RealmAuditLog')
     RealmAuditLog.objects.filter(event_type="user_change_password").update(event_type="user_password_changed")
     RealmAuditLog.objects.filter(event_type="user_change_avatar_source").update(event_type="user_avatar_source_changed")
     RealmAuditLog.objects.filter(event_type="bot_owner_changed").update(event_type="user_bot_owner_changed")
+
 
 class Migration(migrations.Migration):
 
