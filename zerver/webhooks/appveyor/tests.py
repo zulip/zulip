b0eec6c4c4b055019a88b796e28815f2992506c5
Eeshan Garg <jerryguitarist@gmail.com>
2019-04-10 01:37:51 -0230

    webhooks/appveyor: Improve message formatting and punctuation.

--- zerver/webhooks/appveyor/tests.py
+++ zerver/webhooks/appveyor/tests.py
@@ -11,13 +11,12 @@ class AppveyorHookTests(WebhookTestCase):
         Tests if appveyor build success notification is handled correctly
         """
         expected_topic = "Hubot-DSC-Resource"
-        expected_message = ("[Build Hubot-DSC-Resource 2.0.59 completed](https://ci.appveyor.com/project"
-                            "/joebloggs/hubot-dsc-resource/build/2.0.59)\n"
-                            "Commit [c06e208b47](https://github.com/joebloggs/Hubot-DSC-Resource"
-                            "/commit/c06e208b47) by Joe Bloggs on 6/12/2018"
-                            " 6:22 PM: Increment version number.\n"
-                            "Build Started: 9/9/2018 7:04 PM\n"
-                            "Build Finished: 9/9/2018 7:06 PM")
+        expected_message = """
+[Build Hubot-DSC-Resource 2.0.59 completed](https://ci.appveyor.com/project/joebloggs/hubot-dsc-resource/build/2.0.59):
+* **Commit**: [c06e208b47: Increment version number.](https://github.com/joebloggs/Hubot-DSC-Resource/commit/c06e208b47) by Joe Bloggs
+* **Started**: 9/9/2018 7:04 PM
+* **Finished**: 9/9/2018 7:06 PM
+""".strip()
 
         self.send_and_test_stream_message('appveyor_build_success', expected_topic, expected_message)
 
@@ -26,13 +25,12 @@ class AppveyorHookTests(WebhookTestCase):
         Tests if appveyor build failure notification is handled correctly
         """
         expected_topic = "Hubot-DSC-Resource"
-        expected_message = ("[Build Hubot-DSC-Resource 2.0.59 failed](https://ci.appveyor.com/project"
-                            "/joebloggs/hubot-dsc-resource/build/2.0.59)\n"
-                            "Commit [c06e208b47](https://github.com/joebloggs/Hubot-DSC-Resource"
-                            "/commit/c06e208b47) by Joe Bloggs on 6/12/2018"
-                            " 6:22 PM: Increment version number.\n"
-                            "Build Started: 9/9/2018 7:04 PM\n"
-                            "Build Finished: 9/9/2018 7:06 PM")
+        expected_message = """
+[Build Hubot-DSC-Resource 2.0.59 failed](https://ci.appveyor.com/project/joebloggs/hubot-dsc-resource/build/2.0.59):
+* **Commit**: [c06e208b47: Increment version number.](https://github.com/joebloggs/Hubot-DSC-Resource/commit/c06e208b47) by Joe Bloggs
+* **Started**: 9/9/2018 7:04 PM
+* **Finished**: 9/9/2018 7:06 PM
+""".strip()
 
         self.send_and_test_stream_message('appveyor_build_failure', expected_topic, expected_message)
 
