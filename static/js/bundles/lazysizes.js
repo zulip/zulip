2fe7484d34cc2ab03643563d02766ad177ad0140
Priyank Patel <priyankp390@gmail.com>
2019-04-11 15:13:27 +0000

    lazysizes: Load images 500px away from visible viewport.
    We add a lazysizes config option `expand` which loads images 500px away from
    visual viewport. It makes sure that when we render new messages there is
    no flash of grey rectangle and then image even though the image is in browser's cache.

---
+++ static/js/bundles/lazysizes.js
@@ -0,0 +1,7 @@
+/**
+  Seperate bundle for lazysizes since lazysizes requires
+  the config to defined before loading the actual library.
+*/
+
+import 'js/lazysizes_config.js';
+import 'node_modules/lazysizes/lazysizes.js';
---
+++ static/js/lazysizes_config.js
@@ -0,0 +1,2 @@
+window.lazySizesConfig = window.lazySizesConfig || {};
+window.lazySizesConfig.expand = 500; // load images 500px away from visible screen.
