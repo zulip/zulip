f6a601f09fe9deba29e72c837c576e9766a3dd03
Eeshan Garg <jerryguitarist@gmail.com>
2019-04-12 01:06:17 -0230

    webhooks/heroku: Improve message formatting and punctuation.

--- zerver/webhooks/heroku/view.py
+++ zerver/webhooks/heroku/view.py
@@ -8,13 +8,21 @@ from zerver.lib.response import json_success
 from zerver.lib.webhooks.common import check_send_webhook_message
 from zerver.models import UserProfile
 
+TEMPLATE = """
+{user} deployed version {head} of [{app}]({url}):
+
+``` quote
+{git_log}
+```
+""".strip()
+
 @api_key_only_webhook_view("Heroku", notify_bot_owner_on_invalid_json=False)
 @has_request_variables
 def api_heroku_webhook(request: HttpRequest, user_profile: UserProfile,
                        head: str=REQ(), app: str=REQ(), user: str=REQ(),
                        url: str=REQ(), git_log: str=REQ()) -> HttpResponse:
-    template = "{} deployed version {} of [{}]({})\n``` quote\n{}\n```"
-    content = template.format(user, head, app, url, git_log)
+    content = TEMPLATE.format(user=user, head=head, app=app,
+                              url=url, git_log=git_log)
 
     check_send_webhook_message(request, user_profile, app, content)
     return json_success()
