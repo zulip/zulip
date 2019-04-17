18924c3d323c2ecbbc1283318822bb705e84288d
Eeshan Garg <jerryguitarist@gmail.com>
2019-04-17 18:21:57 -0230

    webhooks/papertrail: Improve message formatting and punctuation.

--- zerver/webhooks/papertrail/view.py
+++ zerver/webhooks/papertrail/view.py
@@ -8,33 +8,42 @@ from zerver.lib.response import json_success
 from zerver.lib.webhooks.common import check_send_webhook_message
 from zerver.models import UserProfile
 
+MATCHES_TEMPLATE = '[Search for "{name}"]({url}) found **{number}** matches:\n'
+SEARCH_TEMPLATE = """
+{timestamp} - {source} - {query}:
+``` quote
+{message}
+```
+""".strip()
+
 @api_key_only_webhook_view('Papertrail')
 @has_request_variables
 def api_papertrail_webhook(request: HttpRequest, user_profile: UserProfile,
                            payload: Dict[str, Any]=REQ(argument_type='body')) -> HttpResponse:
 
-    # construct the message of the message
-    message_template = '**"{}"** search found **{}** matches - {}\n```'
-    message = [message_template.format(payload["saved_search"]["name"],
-                                       str(len(payload["events"])),
-                                       payload["saved_search"]["html_search_url"])]
-    for i, event in enumerate(payload["events"]):
-        event_text = '{} {} {}:\n  {}'.format(event["display_received_at"],
-                                              event["source_name"],
-                                              payload["saved_search"]["query"],
-                                              event["message"])
-        message.append(event_text)
-        if i >= 3:
-            message.append('```\n[See more]({})'.format(payload["saved_search"]["html_search_url"]))
-            break
-    else:
-        message.append('```')
-    post = '\n'.join(message)
+    matches = MATCHES_TEMPLATE.format(
+        name=payload["saved_search"]["name"],
+        url=payload["saved_search"]["html_search_url"],
+        number=str(len(payload["events"]))
+    )
+    message = [matches]
 
+    for i, event in enumerate(payload["events"]):
+        event_text = SEARCH_TEMPLATE.format(
+            timestamp=event["display_received_at"],
+            source=event["source_name"],
+            query=payload["saved_search"]["query"],
+            message=event["message"]
+        )
+
+        message.append(event_text)
+
+        if i >= 3:
+            message.append('[See more]({})'.format(payload["saved_search"]["html_search_url"]))
+            break
+
+    post = '\n'.join(message)
     topic = 'logs'
 
-    # send the message
     check_send_webhook_message(request, user_profile, topic, post)
-
-    # return json result
     return json_success()
