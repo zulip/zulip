424061a07a1719dfe9e7b74587c92fb50c609c2e
Eeshan Garg <jerryguitarist@gmail.com>
2019-04-17 18:21:57 -0230

    webhooks/papertrail: Improve message formatting and punctuation.

--- zerver/webhooks/papertrail/tests.py
+++ zerver/webhooks/papertrail/tests.py
@@ -6,36 +6,48 @@ class PapertrailHookTests(WebhookTestCase):
     URL_TEMPLATE = "/api/v1/external/papertrail?&api_key={api_key}&stream={stream}"
     FIXTURE_DIR_NAME = 'papertrail'
 
-    # Note: Include a test function per each distinct message condition your integration supports
     def test_short_message(self) -> None:
         expected_topic = u"logs"
-        expected_message = u'''**"Important stuff"** search found **2** matches - https://papertrailapp.com/searches/42
-```
-May 18 20:30:02 abc cron OR server1:
-  message body
-May 18 20:30:02 server1 cron OR server1:
-  A short event
-```'''
+        expected_message = """
+[Search for "Important stuff"](https://papertrailapp.com/searches/42) found **2** matches:
 
-        # use fixture named papertrail_logs
+May 18 20:30:02 - abc - cron OR server1:
+``` quote
+message body
+```
+May 18 20:30:02 - server1 - cron OR server1:
+``` quote
+A short event
+```
+""".strip()
+
         self.send_and_test_stream_message('short_post', expected_topic, expected_message,
                                           content_type="application/x-www-form-urlencoded")
 
     def test_long_message(self) -> None:
         expected_topic = u"logs"
-        expected_message = u'''**"Important stuff"** search found **5** matches - https://papertrailapp.com/searches/42
+        expected_message = """
+[Search for "Important stuff"](https://papertrailapp.com/searches/42) found **5** matches:
+
+May 18 20:30:02 - abc - cron OR server1:
+``` quote
+message body 1
 ```
-May 18 20:30:02 abc cron OR server1:
-  message body 1
-May 18 20:30:02 abc cron OR server1:
-  message body 2
-May 18 20:30:02 abc cron OR server1:
-  message body 3
-May 18 20:30:02 abc cron OR server1:
-  message body 4
+May 18 20:30:02 - abc - cron OR server1:
+``` quote
+message body 2
 ```
-[See more](https://papertrailapp.com/searches/42)'''
-        # use fixture named papertrail_logs
+May 18 20:30:02 - abc - cron OR server1:
+``` quote
+message body 3
+```
+May 18 20:30:02 - abc - cron OR server1:
+``` quote
+message body 4
+```
+[See more](https://papertrailapp.com/searches/42)
+""".strip()
+
         self.send_and_test_stream_message('long_post', expected_topic, expected_message,
                                           content_type="application/x-www-form-urlencoded")
 
