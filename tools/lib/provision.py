9717afb202bd4cbd5975e9ce36d1f993e511525e
Wyatt Hoodes <hoodesw@hawaii.edu>
2019-04-18 22:10:16 -1000

    provision.py: Add a conditional to remove '.eslintcache'.
    It was discovered that the '.eslintcache' file was causing eslint to
    throw a TypeError after a recent update/addition to the dependencies.
    It makes sense to remove this file as part of the provisioning process
    to avoid such exceptions.

--- tools/lib/provision.py
+++ tools/lib/provision.py
@@ -557,6 +557,11 @@ def main(options):
 
     run(["scripts/lib/clean-unused-caches"])
 
+    # Keeping this cache file around can cause eslint to throw
+    # random TypeErrors when new/updated dependencies are added
+    if os.path.isfile('.eslintcache'):
+        os.remove('.eslintcache')
+
     version_file = os.path.join(UUID_VAR_PATH, 'provision_version')
     print('writing to %s\n' % (version_file,))
     open(version_file, 'w').write(PROVISION_VERSION + '\n')
