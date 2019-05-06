8218bf101c37807cc31240fe95df010d4c4c6f47
Tim Abbott <tabbott@zulipchat.com>
2019-05-05 18:16:37 -0700

    soft_deactivation: Fix buggy handling of other streams.
    Previously, our soft-deactivation logic incorrectly did not filter the
    set of stream subscription changes to look at to only include the
    target stream.
    
    This could result in unspecified buggy behavior.

--- zerver/lib/soft_deactivation.py
+++ zerver/lib/soft_deactivation.py
@@ -39,6 +39,11 @@ def filter_by_subscription_history(user_profile: UserProfile,
                 # check belongs in this inner loop, not the outer loop.
                 break
 
+            if log_entry.modified_stream_id != stream_id:
+                # We only care about subscription changes for the
+                # stream this batch of messages was sent to.
+                continue
+
             if log_entry.event_type == RealmAuditLog.SUBSCRIPTION_DEACTIVATED:
                 for stream_message in stream_messages:
                     if stream_message['id'] <= log_entry.event_last_message_id:
