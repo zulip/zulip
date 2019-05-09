120937a1f4f79563a4a0f72337965775ac3db221
Eeshan Garg <jerryguitarist@gmail.com>
2019-05-08 23:18:17 -0230

    webhooks/mention: Use proper message formatting.

--- zerver/webhooks/mention/tests.py
+++ zerver/webhooks/mention/tests.py
@@ -9,12 +9,14 @@ class MentionHookTests(WebhookTestCase):
 
     def test_mention_webfeed(self) -> None:
         expected_topic = u"news"
-        expected_message = (u"**[Historical Sexual Abuse (Football): 29 Nov 2016: House of Commons debates - TheyWorkForYou]"
-                            u"(https://www.theyworkforyou.com/debates/?id=2016-11-29b.1398.7&p=24887)**:\n"
-                            u"\u2026 Culture, Media and Sport\nNothing is more important than keeping children safe."
-                            u" Child sex abuse is an exceptionally vile crime, and all of Government take it very seriously indeed,"
-                            u" as I know this House does.\nChildren up and down the country are \u2026"
-                            )
+        expected_message = """
+**[Historical Sexual Abuse (Football): 29 Nov 2016: House of Commons debates - TheyWorkForYou](https://www.theyworkforyou.com/debates/?id=2016-11-29b.1398.7&p=24887)**:
+
+``` quote
+\u2026 Culture, Media and Sport\nNothing is more important than keeping children safe. Child sex abuse is an exceptionally vile crime, and all of Government take it very seriously indeed, as I know this House does.
+Children up and down the country are \u2026
+```
+""".strip()
 
         # use fixture named mention_webfeeds
         self.send_and_test_stream_message('webfeeds', expected_topic, expected_message,
