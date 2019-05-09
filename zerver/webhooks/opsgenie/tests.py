5a1f70f6e8665726b975a780e4c9b116915eccb9
Eeshan Garg <jerryguitarist@gmail.com>
2019-05-09 00:19:54 -0230

    webhooks/opsgenie: Improve punctuation and message formatting.

--- zerver/webhooks/opsgenie/tests.py
+++ zerver/webhooks/opsgenie/tests.py
@@ -9,145 +9,172 @@ class OpsGenieHookTests(WebhookTestCase):
 
     def test_acknowledge_alert(self) -> None:
         expected_topic = u"Integration1"
-        expected_message = (u"**OpsGenie: [Alert for Integration1.](https://app.opsgenie.com/alert/V2#/show/052652ac-5d1c-464a-812a-7dd18bbfba8c)**\n"
-                            u"Type: *Acknowledge*\n"
-                            u"Message: *test alert*\n"
-                            u"`tag1` `tag2`"
-                            )
+        expected_message = """
+[OpsGenie Alert for Integration1](https://app.opsgenie.com/alert/V2#/show/052652ac-5d1c-464a-812a-7dd18bbfba8c):
+* **Type**: Acknowledge
+* **Message**: test alert
+* **Tags**: `tag1`, `tag2`
+""".strip()
+
         self.send_and_test_stream_message('acknowledge', expected_topic, expected_message,
                                           content_type="application/x-www-form-urlencoded")
 
     def test_addnote_alert(self) -> None:
         expected_topic = u"Integration1"
-        expected_message = (u"**OpsGenie: [Alert for Integration1.](https://app.opsgenie.com/alert/V2#/show/052652ac-5d1c-464a-812a-7dd18bbfba8c)**\n"
-                            u"Type: *AddNote*\n"
-                            u"Note: *note to test alert*\n"
-                            u"Message: *test alert*\n"
-                            u"`tag1` `tag2`"
-                            )
+        expected_message = """
+[OpsGenie Alert for Integration1](https://app.opsgenie.com/alert/V2#/show/052652ac-5d1c-464a-812a-7dd18bbfba8c):
+* **Type**: AddNote
+* **Note**: note to test alert
+* **Message**: test alert
+* **Tags**: `tag1`, `tag2`
+""".strip()
+
         self.send_and_test_stream_message('addnote', expected_topic, expected_message,
                                           content_type="application/x-www-form-urlencoded")
 
     def test_addrecipient_alert(self) -> None:
         expected_topic = u"Integration1"
-        expected_message = (u"**OpsGenie: [Alert for Integration1.](https://app.opsgenie.com/alert/V2#/show/052652ac-5d1c-464a-812a-7dd18bbfba8c)**\n"
-                            u"Type: *AddRecipient*\n"
-                            u"Recipient: *team2_escalation*\n"
-                            u"Message: *test alert*\n"
-                            u"`tag1` `tag2`"
-                            )
-        # use fixture named helloworld_hello
+        expected_message = """
+[OpsGenie Alert for Integration1](https://app.opsgenie.com/alert/V2#/show/052652ac-5d1c-464a-812a-7dd18bbfba8c):
+* **Type**: AddRecipient
+* **Recipient**: team2_escalation
+* **Message**: test alert
+* **Tags**: `tag1`, `tag2`
+""".strip()
+
         self.send_and_test_stream_message('addrecipient', expected_topic, expected_message,
                                           content_type="application/x-www-form-urlencoded")
 
     def test_addtags_alert(self) -> None:
         expected_topic = u"Integration1"
-        expected_message = (u"**OpsGenie: [Alert for Integration1.](https://app.opsgenie.com/alert/V2#/show/052652ac-5d1c-464a-812a-7dd18bbfba8c)**\n"
-                            u"Type: *AddTags*\n"
-                            u"Added tags: *tag1,tag2,tag3*\n"
-                            u"Message: *test alert*\n"
-                            u"`tag1` `tag2` `tag3`"
-                            )
+        expected_message = """
+[OpsGenie Alert for Integration1](https://app.opsgenie.com/alert/V2#/show/052652ac-5d1c-464a-812a-7dd18bbfba8c):
+* **Type**: AddTags
+* **Tags added**: tag1,tag2,tag3
+* **Message**: test alert
+* **Tags**: `tag1`, `tag2`, `tag3`
+""".strip()
+
         self.send_and_test_stream_message('addtags', expected_topic, expected_message,
                                           content_type="application/x-www-form-urlencoded")
 
     def test_addteam_alert(self) -> None:
         expected_topic = u"Integration1"
-        expected_message = (u"**OpsGenie: [Alert for Integration1.](https://app.opsgenie.com/alert/V2#/show/052652ac-5d1c-464a-812a-7dd18bbfba8c)**\n"
-                            u"Type: *AddTeam*\n"
-                            u"Added team: *team2*\n"
-                            u"Message: *test alert*\n"
-                            u"`tag1` `tag2`"
-                            )
+        expected_message = """
+[OpsGenie Alert for Integration1](https://app.opsgenie.com/alert/V2#/show/052652ac-5d1c-464a-812a-7dd18bbfba8c):
+* **Type**: AddTeam
+* **Team added**: team2
+* **Message**: test alert
+* **Tags**: `tag1`, `tag2`
+""".strip()
+
         self.send_and_test_stream_message('addteam', expected_topic, expected_message,
                                           content_type="application/x-www-form-urlencoded")
 
     def test_assignownership_alert(self) -> None:
         expected_topic = u"Integration1"
-        expected_message = (u"**OpsGenie: [Alert for Integration1.](https://app.opsgenie.com/alert/V2#/show/052652ac-5d1c-464a-812a-7dd18bbfba8c)**\n"
-                            u"Type: *AssignOwnership*\n"
-                            u"Assigned owner: *user2@ifountain.com*\n"
-                            u"Message: *test alert*\n"
-                            u"`tag1` `tag2`"
-                            )
+        expected_message = """
+[OpsGenie Alert for Integration1](https://app.opsgenie.com/alert/V2#/show/052652ac-5d1c-464a-812a-7dd18bbfba8c):
+* **Type**: AssignOwnership
+* **Assigned owner**: user2@ifountain.com
+* **Message**: test alert
+* **Tags**: `tag1`, `tag2`
+""".strip()
+
         self.send_and_test_stream_message('assignownership', expected_topic, expected_message,
                                           content_type="application/x-www-form-urlencoded")
 
     def test_close_alert(self) -> None:
         expected_topic = u"Integration1"
-        expected_message = (u"**OpsGenie: [Alert for Integration1.](https://app.opsgenie.com/alert/V2#/show/052652ac-5d1c-464a-812a-7dd18bbfba8c)**\n"
-                            u"Type: *Close*\n"
-                            u"Message: *test alert*"
-                            )
+        expected_message = """
+[OpsGenie Alert for Integration1](https://app.opsgenie.com/alert/V2#/show/052652ac-5d1c-464a-812a-7dd18bbfba8c):
+* **Type**: Close
+* **Message**: test alert
+""".strip()
+
         self.send_and_test_stream_message('close', expected_topic, expected_message,
                                           content_type="application/x-www-form-urlencoded")
 
     def test_create_alert(self) -> None:
         expected_topic = u"Webhook"
-        expected_message = (u"**OpsGenie: [Alert for Webhook.](https://app.opsgenie.com/alert/V2#/show/ec03dad6-62c8-4c94-b38b-d88f398e900f)**\n"
-                            u"Type: *Create*\n"
-                            u"Message: *another alert*\n"
-                            u"`vip`"
-                            )
+        expected_message = """
+[OpsGenie Alert for Webhook](https://app.opsgenie.com/alert/V2#/show/ec03dad6-62c8-4c94-b38b-d88f398e900f):
+* **Type**: Create
+* **Message**: another alert
+* **Tags**: `vip`
+""".strip()
+
         self.send_and_test_stream_message('create', expected_topic, expected_message,
                                           content_type="application/x-www-form-urlencoded")
 
     def test_customaction_alert(self) -> None:
         expected_topic = u"Integration1"
-        expected_message = (u"**OpsGenie: [Alert for Integration1.](https://app.opsgenie.com/alert/V2#/show/052652ac-5d1c-464a-812a-7dd18bbfba8c)**\n"
-                            u"Type: *TestAction*\n"
-                            u"Message: *test alert*\n"
-                            u"`tag1` `tag2`"
-                            )
+        expected_message = """
+[OpsGenie Alert for Integration1](https://app.opsgenie.com/alert/V2#/show/052652ac-5d1c-464a-812a-7dd18bbfba8c):
+* **Type**: TestAction
+* **Message**: test alert
+* **Tags**: `tag1`, `tag2`
+""".strip()
+
         self.send_and_test_stream_message('customaction', expected_topic, expected_message,
                                           content_type="application/x-www-form-urlencoded")
 
     def test_delete_alert(self) -> None:
         expected_topic = u"Integration1"
-        expected_message = (u"**OpsGenie: [Alert for Integration1.](https://app.opsgenie.com/alert/V2#/show/052652ac-5d1c-464a-812a-7dd18bbfba8c)**\n"
-                            u"Type: *Delete*\n"
-                            u"Message: *test alert*"
-                            )
+        expected_message = """
+[OpsGenie Alert for Integration1](https://app.opsgenie.com/alert/V2#/show/052652ac-5d1c-464a-812a-7dd18bbfba8c):
+* **Type**: Delete
+* **Message**: test alert
+""".strip()
+
         self.send_and_test_stream_message('delete', expected_topic, expected_message,
                                           content_type="application/x-www-form-urlencoded")
 
     def test_escalate_alert(self) -> None:
         expected_topic = u"Webhook_Test"
-        expected_message = (u"**OpsGenie: [Alert for Webhook_Test.](https://app.opsgenie.com/alert/V2#/show/7ba97e3a-d328-4b5e-8f9a-39e945a3869a)**\n"
-                            u"Type: *Escalate*\n"
-                            u"Escalation: *test_esc*"
-                            )
+        expected_message = """
+[OpsGenie Alert for Webhook_Test](https://app.opsgenie.com/alert/V2#/show/7ba97e3a-d328-4b5e-8f9a-39e945a3869a):
+* **Type**: Escalate
+* **Escalation**: test_esc
+""".strip()
+
         self.send_and_test_stream_message('escalate', expected_topic, expected_message,
                                           content_type="application/x-www-form-urlencoded")
 
     def test_removetags_alert(self) -> None:
         expected_topic = u"Integration1"
-        expected_message = (u"**OpsGenie: [Alert for Integration1.](https://app.opsgenie.com/alert/V2#/show/052652ac-5d1c-464a-812a-7dd18bbfba8c)**\n"
-                            u"Type: *RemoveTags*\n"
-                            u"Removed tags: *tag3*\n"
-                            u"Message: *test alert*\n"
-                            u"`tag1` `tag2`"
-                            )
+        expected_message = """
+[OpsGenie Alert for Integration1](https://app.opsgenie.com/alert/V2#/show/052652ac-5d1c-464a-812a-7dd18bbfba8c):
+* **Type**: RemoveTags
+* **Tags removed**: tag3
+* **Message**: test alert
+* **Tags**: `tag1`, `tag2`
+""".strip()
+
         self.send_and_test_stream_message('removetags', expected_topic, expected_message,
                                           content_type="application/x-www-form-urlencoded")
 
     def test_takeownership_alert(self) -> None:
         expected_topic = u"Webhook"
-        expected_message = (u"**OpsGenie: [Alert for Webhook.](https://app.opsgenie.com/alert/V2#/show/8a745a79-3ed3-4044-8427-98e067c0623c)**\n"
-                            u"Type: *TakeOwnership*\n"
-                            u"Message: *message test*\n"
-                            u"`tag1` `tag2`"
-                            )
+        expected_message = """
+[OpsGenie Alert for Webhook](https://app.opsgenie.com/alert/V2#/show/8a745a79-3ed3-4044-8427-98e067c0623c):
+* **Type**: TakeOwnership
+* **Message**: message test
+* **Tags**: `tag1`, `tag2`
+""".strip()
+
         self.send_and_test_stream_message('takeownership', expected_topic, expected_message,
                                           content_type="application/x-www-form-urlencoded")
 
     def test_unacknowledge_alert(self) -> None:
         expected_topic = u"Integration1"
-        expected_message = (u"**OpsGenie: [Alert for Integration1.](https://app.opsgenie.com/alert/V2#/show/052652ac-5d1c-464a-812a-7dd18bbfba8c)**\n"
-                            u"Type: *UnAcknowledge*\n"
-                            u"Message: *test alert*\n"
-                            u"`tag1` `tag2`"
-                            )
+        expected_message = """
+[OpsGenie Alert for Integration1](https://app.opsgenie.com/alert/V2#/show/052652ac-5d1c-464a-812a-7dd18bbfba8c):
+* **Type**: UnAcknowledge
+* **Message**: test alert
+* **Tags**: `tag1`, `tag2`
+""".strip()
+
         self.send_and_test_stream_message('unacknowledge', expected_topic, expected_message,
                                           content_type="application/x-www-form-urlencoded")
 
