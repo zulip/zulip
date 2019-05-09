fa00ab1399867fb2ed470de1783db3c468a52a43
Eeshan Garg <jerryguitarist@gmail.com>
2019-05-09 15:15:26 -0230

    webhooks/taiga: Use proper punctuation and new style string formatting.

--- zerver/webhooks/taiga/view.py
+++ zerver/webhooks/taiga/view.py
@@ -8,6 +8,7 @@ should be in bold.
 """
 
 from typing import Any, Dict, List, Mapping, Optional, Tuple
+import string
 
 from django.http import HttpRequest, HttpResponse
 
@@ -35,93 +36,93 @@ def api_taiga_webhook(request: HttpRequest, user_profile: UserProfile,
 
 templates = {
     'epic': {
-        'create': u'%(user)s created epic **%(subject)s**',
-        'set_assigned_to': u'%(user)s assigned epic **%(subject)s** to %(new)s.',
-        'unset_assigned_to': u'%(user)s unassigned epic **%(subject)s**',
-        'changed_assigned_to': u'%(user)s reassigned epic **%(subject)s**'
-        ' from %(old)s to %(new)s.',
-        'blocked': u'%(user)s blocked epic **%(subject)s**',
-        'unblocked': u'%(user)s unblocked epic **%(subject)s**',
-        'changed_status': u'%(user)s changed status of epic **%(subject)s**'
-        ' from %(old)s to %(new)s.',
-        'renamed': u'%(user)s renamed epic from **%(old)s** to **%(new)s**',
-        'description_diff': u'%(user)s updated description of epic **%(subject)s**',
-        'commented': u'%(user)s commented on epic **%(subject)s**',
-        'delete': u'%(user)s deleted epic **%(subject)s**',
+        'create': u'{user} created epic **{subject}**.',
+        'set_assigned_to': u'{user} assigned epic **{subject}** to {new}.',
+        'unset_assigned_to': u'{user} unassigned epic **{subject}**.',
+        'changed_assigned_to': u'{user} reassigned epic **{subject}**'
+        ' from {old} to {new}.',
+        'blocked': u'{user} blocked epic **{subject}**.',
+        'unblocked': u'{user} unblocked epic **{subject}**.',
+        'changed_status': u'{user} changed status of epic **{subject}**'
+        ' from {old} to {new}.',
+        'renamed': u'{user} renamed epic from **{old}** to **{new}**.',
+        'description_diff': u'{user} updated description of epic **{subject}**.',
+        'commented': u'{user} commented on epic **{subject}**.',
+        'delete': u'{user} deleted epic **{subject}**.',
     },
     'relateduserstory': {
-        'create': (u'%(user)s added a related user story '
-                   u'**%(userstory_subject)s** to the epic **%(epic_subject)s**'),
-        'delete': (u'%(user)s removed a related user story ' +
-                   u'**%(userstory_subject)s** from the epic **%(epic_subject)s**'),
+        'create': (u'{user} added a related user story '
+                   u'**{userstory_subject}** to the epic **{epic_subject}**.'),
+        'delete': (u'{user} removed a related user story ' +
+                   u'**{userstory_subject}** from the epic **{epic_subject}**.'),
     },
     'userstory': {
-        'create': u'%(user)s created user story **%(subject)s**.',
-        'set_assigned_to': u'%(user)s assigned user story **%(subject)s** to %(new)s.',
-        'unset_assigned_to': u'%(user)s unassigned user story **%(subject)s**.',
-        'changed_assigned_to': u'%(user)s reassigned user story **%(subject)s**'
-        ' from %(old)s to %(new)s.',
-        'points': u'%(user)s changed estimation of user story **%(subject)s**.',
-        'blocked': u'%(user)s blocked user story **%(subject)s**.',
-        'unblocked': u'%(user)s unblocked user story **%(subject)s**.',
-        'set_milestone': u'%(user)s added user story **%(subject)s** to sprint %(new)s.',
-        'unset_milestone': u'%(user)s removed user story **%(subject)s** from sprint %(old)s.',
-        'changed_milestone': u'%(user)s changed sprint of user story **%(subject)s** from %(old)s'
-        ' to %(new)s.',
-        'changed_status': u'%(user)s changed status of user story **%(subject)s**'
-        ' from %(old)s to %(new)s.',
-        'closed': u'%(user)s closed user story **%(subject)s**.',
-        'reopened': u'%(user)s reopened user story **%(subject)s**.',
-        'renamed': u'%(user)s renamed user story from %(old)s to **%(new)s**.',
-        'description_diff': u'%(user)s updated description of user story **%(subject)s**.',
-        'commented': u'%(user)s commented on user story **%(subject)s**.',
-        'delete': u'%(user)s deleted user story **%(subject)s**.'
+        'create': u'{user} created user story **{subject}**.',
+        'set_assigned_to': u'{user} assigned user story **{subject}** to {new}.',
+        'unset_assigned_to': u'{user} unassigned user story **{subject}**.',
+        'changed_assigned_to': u'{user} reassigned user story **{subject}**'
+        ' from {old} to {new}.',
+        'points': u'{user} changed estimation of user story **{subject}**.',
+        'blocked': u'{user} blocked user story **{subject}**.',
+        'unblocked': u'{user} unblocked user story **{subject}**.',
+        'set_milestone': u'{user} added user story **{subject}** to sprint {new}.',
+        'unset_milestone': u'{user} removed user story **{subject}** from sprint {old}.',
+        'changed_milestone': u'{user} changed sprint of user story **{subject}** from {old}'
+        ' to {new}.',
+        'changed_status': u'{user} changed status of user story **{subject}**'
+        ' from {old} to {new}.',
+        'closed': u'{user} closed user story **{subject}**.',
+        'reopened': u'{user} reopened user story **{subject}**.',
+        'renamed': u'{user} renamed user story from {old} to **{new}**.',
+        'description_diff': u'{user} updated description of user story **{subject}**.',
+        'commented': u'{user} commented on user story **{subject}**.',
+        'delete': u'{user} deleted user story **{subject}**.'
     },
     'milestone': {
-        'create': u'%(user)s created sprint **%(subject)s**.',
-        'renamed': u'%(user)s renamed sprint from %(old)s to **%(new)s**.',
-        'estimated_start': u'%(user)s changed estimated start of sprint **%(subject)s**'
-        ' from %(old)s to %(new)s.',
-        'estimated_finish': u'%(user)s changed estimated finish of sprint **%(subject)s**'
-        ' from %(old)s to %(new)s.',
-        'delete': u'%(user)s deleted sprint **%(subject)s**.'
+        'create': u'{user} created sprint **{subject}**.',
+        'renamed': u'{user} renamed sprint from {old} to **{new}**.',
+        'estimated_start': u'{user} changed estimated start of sprint **{subject}**'
+        ' from {old} to {new}.',
+        'estimated_finish': u'{user} changed estimated finish of sprint **{subject}**'
+        ' from {old} to {new}.',
+        'delete': u'{user} deleted sprint **{subject}**.'
     },
     'task': {
-        'create': u'%(user)s created task **%(subject)s**.',
-        'set_assigned_to': u'%(user)s assigned task **%(subject)s** to %(new)s.',
-        'unset_assigned_to': u'%(user)s unassigned task **%(subject)s**.',
-        'changed_assigned_to': u'%(user)s reassigned task **%(subject)s**'
-        ' from %(old)s to %(new)s.',
-        'blocked': u'%(user)s blocked task **%(subject)s**.',
-        'unblocked': u'%(user)s unblocked task **%(subject)s**.',
-        'set_milestone': u'%(user)s added task **%(subject)s** to sprint %(new)s.',
-        'changed_milestone': u'%(user)s changed sprint of task '
-                             '**%(subject)s** from %(old)s to %(new)s.',
-        'changed_status': u'%(user)s changed status of task **%(subject)s**'
-        ' from %(old)s to %(new)s.',
-        'renamed': u'%(user)s renamed task %(old)s to **%(new)s**.',
-        'description_diff': u'%(user)s updated description of task **%(subject)s**.',
-        'commented': u'%(user)s commented on task **%(subject)s**.',
-        'delete': u'%(user)s deleted task **%(subject)s**.',
-        'changed_us': u'%(user)s moved task **%(subject)s** from user story %(old)s to %(new)s.'
+        'create': u'{user} created task **{subject}**.',
+        'set_assigned_to': u'{user} assigned task **{subject}** to {new}.',
+        'unset_assigned_to': u'{user} unassigned task **{subject}**.',
+        'changed_assigned_to': u'{user} reassigned task **{subject}**'
+        ' from {old} to {new}.',
+        'blocked': u'{user} blocked task **{subject}**.',
+        'unblocked': u'{user} unblocked task **{subject}**.',
+        'set_milestone': u'{user} added task **{subject}** to sprint {new}.',
+        'changed_milestone': u'{user} changed sprint of task '
+                             '**{subject}** from {old} to {new}.',
+        'changed_status': u'{user} changed status of task **{subject}**'
+        ' from {old} to {new}.',
+        'renamed': u'{user} renamed task {old} to **{new}**.',
+        'description_diff': u'{user} updated description of task **{subject}**.',
+        'commented': u'{user} commented on task **{subject}**.',
+        'delete': u'{user} deleted task **{subject}**.',
+        'changed_us': u'{user} moved task **{subject}** from user story {old} to {new}.'
     },
     'issue': {
-        'create': u'%(user)s created issue **%(subject)s**.',
-        'set_assigned_to': u'%(user)s assigned issue **%(subject)s** to %(new)s.',
-        'unset_assigned_to': u'%(user)s unassigned issue **%(subject)s**.',
-        'changed_assigned_to': u'%(user)s reassigned issue **%(subject)s**'
-        ' from %(old)s to %(new)s.',
-        'changed_priority': u'%(user)s changed priority of issue '
-                            '**%(subject)s** from %(old)s to %(new)s.',
-        'changed_severity': u'%(user)s changed severity of issue '
-                            '**%(subject)s** from %(old)s to %(new)s.',
-        'changed_status': u'%(user)s changed status of issue **%(subject)s**'
-                           ' from %(old)s to %(new)s.',
-        'changed_type': u'%(user)s changed type of issue **%(subject)s** from %(old)s to %(new)s.',
-        'renamed': u'%(user)s renamed issue %(old)s to **%(new)s**.',
-        'description_diff': u'%(user)s updated description of issue **%(subject)s**.',
-        'commented': u'%(user)s commented on issue **%(subject)s**.',
-        'delete': u'%(user)s deleted issue **%(subject)s**.'
+        'create': u'{user} created issue **{subject}**.',
+        'set_assigned_to': u'{user} assigned issue **{subject}** to {new}.',
+        'unset_assigned_to': u'{user} unassigned issue **{subject}**.',
+        'changed_assigned_to': u'{user} reassigned issue **{subject}**'
+        ' from {old} to {new}.',
+        'changed_priority': u'{user} changed priority of issue '
+                            '**{subject}** from {old} to {new}.',
+        'changed_severity': u'{user} changed severity of issue '
+                            '**{subject}** from {old} to {new}.',
+        'changed_status': u'{user} changed status of issue **{subject}**'
+                           ' from {old} to {new}.',
+        'changed_type': u'{user} changed type of issue **{subject}** from {old} to {new}.',
+        'renamed': u'{user} renamed issue {old} to **{new}**.',
+        'description_diff': u'{user} updated description of issue **{subject}**.',
+        'commented': u'{user} commented on issue **{subject}**.',
+        'delete': u'{user} deleted issue **{subject}**.'
     },
 }
 
@@ -260,7 +261,23 @@ def parse_message(message: Mapping[str, Any]) -> List[Dict[str, Any]]:
 
 def generate_content(data: Mapping[str, Any]) -> str:
     """ Gets the template string and formats it with parsed data. """
-    return templates[data['type']][data['event']] % data['values']
+    template = templates[data['type']][data['event']]
+    content = template.format(**data['values'])
+    end_type = ''
+    if template.endswith('{subject}**.'):
+        end_type = 'subject'
+    elif template.endswith('{epic_subject}**.'):
+        end_type = 'epic_subject'
+    elif template.endswith('{new}.') or template.endswith('{new}**.'):
+        end_type = 'new'
+    elif template.endswith('{old}.') or template.endswith('{old}**.'):
+        end_type = 'old'
+    end = data['values'].get(end_type)
+
+    if end[-1] in string.punctuation:
+        content = content[:-1]
+
+    return content
 
 def get_owner_name(message: Mapping[str, Any]) -> str:
     return message["by"]["full_name"]
