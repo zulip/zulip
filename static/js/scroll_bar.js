243db9a22f12a0f906bb41ad0ce8b2db47db26fe
Tim Abbott <tabbott@zulipchat.com>
2019-05-13 11:27:00 -0700

    css: Fix width calculations for fluid width.
    Apparently, while b38ae6e0edf2661dddb02afd06104f38a1dbc8b9 was correct
    for the default Zulip UI, it was not correct for the fluid width case,
    where we did actually want to override the 1400px width rule for the
    non-fixed `.app-main` element.
    
    The right fix here is to add an additional CSS rule tweak to our
    scrollbar logic.  This has been carefully tested with both initial
    setting states as well as toggling the state.

--- static/js/scroll_bar.js
+++ static/js/scroll_bar.js
@@ -74,10 +74,12 @@ exports.set_layout_width = function () {
     // the .app-main CSS rules.
     if (page_params.fluid_layout_width) {
         $(".header-main").css("max-width", "inherit");
+        $(".app .app-main").css("max-width", "inherit");
         $(".fixed-app .app-main").css("max-width", "inherit");
         $("#compose-container").css("max-width", "inherit");
     } else {
         $(".header-main").css("max-width", 1400 + sbWidth + "px");
+        $(".app .app-main").css("max-width", 1400 + "px");
         $(".fixed-app .app-main").css("max-width", 1400 + sbWidth + "px");
         $("#compose-container").css("max-width", 1400 + sbWidth + "px");
     }
