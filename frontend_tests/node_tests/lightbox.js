75635844dbaaefede1938f6372b0baa09d93b4b7
Puneeth Chaganti <punchagan@muse-amuse.in>
2019-05-21 21:06:39 +0530

    lightbox: Set URL in payload instead of computing from video IDs.
    Lightbox previews for youtube playlists use the "current" video in the playlist
    for the preview. The open link for such previews is incorrectly set to the first
    video alone, and not the playlist. This commit fixes the bug by linking to the
    original URL for lightbox preview is being shown, instead of computing the URL.

--- frontend_tests/node_tests/lightbox.js
+++ frontend_tests/node_tests/lightbox.js
@@ -13,29 +13,45 @@ set_global('overlays', {
 set_global('popovers', {
     hide_all: () => {},
 });
-set_global('$', function () {
-    return {
-        hasClass: () => false,
-        closest: () => [],
-        attr: (attr) => attr,
-        parent: () => ({
-            closest: () => ({
-                attr: (attr) => attr,
-            }),
-            attr: (attr) => attr,
-        }),
-        html: () => ({
-            show: () => {},
-        }),
-        hide: () => {},
-        show: () => {},
-        text: () => '',
-    };
-});
+
+set_global('$', global.make_zjquery());
 
 run_test('pan_and_zoom', () => {
-    lightbox.open('<img src="./image.png" data-src-fullsize="./original.png">');
+    $.clear_all_elements();
+
+    var img = '<img src="./image.png" data-src-fullsize="./original.png">';
+    var link = '<a href="https://zulip.com"></a>';
+    var msg = '<div [zid]></div>';
+    $(img).set_parent($(link));
+    $(link).set_parent($(msg));
+
+    // Used by render_lightbox_list_images
+    $.stub_selector('.focused_table .message_inline_image img', []);
+
+    lightbox.open(img);
     assert.equal(blueslip.get_test_logs('error').length, 0);
     lightbox.open('<img src="./image.png">');
+    assert.equal(blueslip.get_test_logs('error').length, 0);
+});
+
+run_test('open_url', () => {
+    $.clear_all_elements();
+
+    var url = 'https://youtube.com/1234';
+    var img = '<img></img>';
+    $(img).attr('src', "https://youtube.com/image.png");
+    var link = '<a></a>';
+    $(link).attr('href', url);
+    var div = '<div class="youtube-video"></div>';
+    var msg = '<div [zid]></div>';
+    $(img).set_parent($(link));
+    $(link).set_parent($(div));
+    $(div).set_parent($(msg));
+
+    // Used by render_lightbox_list_images
+    $.stub_selector('.focused_table .message_inline_image img', []);
+
+    lightbox.open(img);
+    assert.equal($('.image-actions .open').attr('href'), url);
     assert.equal(blueslip.get_test_logs('error').length, 0);
 });
