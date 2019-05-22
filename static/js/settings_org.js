c691ed433b1f02df6bf8f12a6051f1d2374f3636
Shubham Dhama <shubhamdhamaofficial@gmail.com>
2019-05-22 17:01:13 +0530

    settings_org: Fix the typo in `settings_org.show_email()` function.

--- static/js/settings_org.js
+++ static/js/settings_org.js
@@ -53,7 +53,7 @@ exports.email_address_visibility_values = {
 
 exports.show_email = function () {
     // TODO: Extend this when we add support for admins_and_members above.
-    return page_params.email_address_visibility ===
+    return page_params.realm_email_address_visibility ===
         exports.email_address_visibility_values.admins_only.code;
 };
 
