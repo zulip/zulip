d32ce0ed7e34440045f40a83d1c097e019e52b0e
Wyatt Hoodes <hoodesw@hawaii.edu>
2019-05-23 12:01:42 +0100

    test_realm_export.py: Clean up the mocking of `do_export_realm`.
    A FileNotFound error was set as the side-effect of the do_export_realm
    mock and the DeferredWorker was made to consume the event explicitly.
    
    Previously, the mock of do_export_realm was producing spammy output
    as a result of a FileNotFound error coming from the queue processing of
    `do_write_stats_file_for_realm_export`.

--- zerver/tests/test_realm_export.py
+++ zerver/tests/test_realm_export.py
@@ -6,10 +6,11 @@ from django.utils.timezone import now as timezone_now
 from zerver.lib.test_classes import ZulipTestCase
 from zerver.lib.exceptions import JsonableError
 from zerver.lib.test_helpers import use_s3_backend
-from zerver.views.public_export import public_only_realm_export
-from zerver.models import RealmAuditLog
 
-# TODO: Mock export_realm_wrapper to test for s3 or local
+from zerver.models import RealmAuditLog
+from zerver.views.public_export import public_only_realm_export
+from zerver.worker.queue_processors import DeferredWorker
+
 class RealmExportTest(ZulipTestCase):
     def test_export_as_not_admin(self) -> None:
         user = self.example_user('hamlet')
@@ -27,7 +28,6 @@ class RealmExportTest(ZulipTestCase):
             queue_data = mock_publish.call_args_list[0][0]
             worker = mock_publish.call_args_list[0][0][0]
         self.assert_json_success(result)
-
         mock_publish.assert_called_once()
         event = queue_data[1]
         self.assertEqual(worker, 'deferred_work')
@@ -35,33 +35,26 @@ class RealmExportTest(ZulipTestCase):
         self.assertEqual(event['user_profile_id'], 5)
         self.assertEqual(event['type'], 'realm_exported')
 
-        with patch('zerver.lib.export.do_export_realm') as mock_export:
-            result = self.client_post('/json/export/realm')
+        with patch('zerver.lib.export.do_export_realm',
+                   side_effect=FileNotFoundError) as mock_export:
+            with self.assertRaises(FileNotFoundError):
+                DeferredWorker().consume(event)
             args = mock_export.call_args_list[0][1]
-            # TODO: Clean up the way we do the mocking here; we will
-            # want to mock do_export_realm in a way that captures its
-            # arguments but doesn't lead to (silent) error spam from
-            # do_write_stats_file_for_realm_export.
-            #
-            # Probably setting a `side_effect` makes sense?
-        self.assert_json_success(result)
-        self.assertEqual(args['realm'], admin.realm)
-        self.assertEqual(args['public_only'], True)
-        self.assertEqual(args['output_dir'].startswith('/tmp/zulip-export-'), True)
-        self.assertEqual(args['threads'], 6)
+            self.assertEqual(args['realm'], admin.realm)
+            self.assertEqual(args['public_only'], True)
+            self.assertIn('/tmp/zulip-export-', args['output_dir'])
+            self.assertEqual(args['threads'], 6)
 
     @override_settings(LOCAL_UPLOADS_DIR='/var/uploads')
     def test_endpoint_local_uploads(self) -> None:
         admin = self.example_user('iago')
         self.login(admin.email)
 
-        with patch('zerver.lib.export.do_export_realm'):
-            with patch('zerver.views.public_export.queue_json_publish') as mock_publish:
-                result = self.client_post('/json/export/realm')
-                queue_data = mock_publish.call_args_list[0][0]
-                worker = mock_publish.call_args_list[0][0][0]
+        with patch('zerver.views.public_export.queue_json_publish') as mock_publish:
+            result = self.client_post('/json/export/realm')
+            queue_data = mock_publish.call_args_list[0][0]
+            worker = mock_publish.call_args_list[0][0][0]
         self.assert_json_success(result)
-
         mock_publish.assert_called_once()
         event = queue_data[1]
         self.assertEqual(worker, 'deferred_work')
@@ -69,21 +62,15 @@ class RealmExportTest(ZulipTestCase):
         self.assertEqual(event['user_profile_id'], 5)
         self.assertEqual(event['type'], 'realm_exported')
 
-        with patch('zerver.lib.export.do_export_realm') as mock_export:
-            result = self.client_post('/json/export/realm')
+        with patch('zerver.lib.export.do_export_realm',
+                   side_effect=FileNotFoundError) as mock_export:
+            with self.assertRaises(FileNotFoundError):
+                DeferredWorker().consume(event)
             args = mock_export.call_args_list[0][1]
-            # TODO: Clean up the way we do the mocking here; we will
-            # want to mock do_export_realm in a way that captures its
-            # arguments but doesn't lead to (silent) error spam from
-            # do_write_stats_file_for_realm_export.
-            #
-            # Probably setting a `side_effect` makes sense?
-        self.assert_json_success(result)
-
-        self.assertEqual(args['realm'], admin.realm)
-        self.assertEqual(args['public_only'], True)
-        self.assertEqual(args['output_dir'].startswith('/tmp/zulip-export-'), True)
-        self.assertEqual(args['threads'], 6)
+            self.assertEqual(args['realm'], admin.realm)
+            self.assertEqual(args['public_only'], True)
+            self.assertIn('/tmp/zulip-export-', args['output_dir'])
+            self.assertEqual(args['threads'], 6)
 
     def test_realm_export_rate_limited(self) -> None:
         admin = self.example_user('iago')
