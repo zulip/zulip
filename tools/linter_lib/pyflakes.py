337060de0165c3d15e3946fa314d4db08a0bfd25
Aman Agrawal <f2016561@pilani.bits-pilani.ac.in>
2019-05-28 21:59:36 +0530

    lint: Move runnable code form `pyflakes.py` to `zulint/linters`.
    Extract runnable code from `linter_lib/pyflakes.py` and move it to
    `zulint/linters`. This keeps zulip specific pyflakes config separate
    form zulint.

--- tools/linter_lib/pyflakes.py
+++ tools/linter_lib/pyflakes.py
@@ -2,48 +2,29 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 import argparse
-import subprocess
-
-from zulint.printer import print_err, colors
 
 from typing import List
 
-suppress_patterns = [
-    (b"scripts/lib/pythonrc.py", b"imported but unused"),
-    (b'', b"'scripts.lib.setup_path_on_import' imported but unused"),
+from zulint.linters import run_pyflakes
 
-    # Our ipython startup pythonrc file intentionally imports *
-    (b"scripts/lib/pythonrc.py",
-     b" import *' used; unable to detect undefined names"),
-
-    # Special dev_settings.py import
-    (b'', b"from .prod_settings_template import *"),
-
-    (b"settings.py", b"settings import *' used; unable to detect undefined names"),
-    (b"settings.py", b"may be undefined, or defined from star imports"),
-
-    # Sphinx adds `tags` specially to the environment when running conf.py.
-    (b"docs/conf.py", b"undefined name 'tags'"),
-]
-
-def suppress_line(line: str) -> bool:
-    for file_pattern, line_pattern in suppress_patterns:
-        if file_pattern in line and line_pattern in line:
-            return True
-    return False
 
 def check_pyflakes(files, options):
     # type: (List[str], argparse.Namespace) -> bool
-    if len(files) == 0:
-        return False
-    failed = False
-    color = next(colors)
-    pyflakes = subprocess.Popen(['pyflakes'] + files,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-    assert pyflakes.stdout is not None  # Implied by use of subprocess.PIPE
-    for ln in pyflakes.stdout.readlines() + pyflakes.stderr.readlines():
-        if options.full or not suppress_line(ln):
-            print_err('pyflakes', color, ln)
-            failed = True
-    return failed
+    suppress_patterns = [
+        (b"scripts/lib/pythonrc.py", b"imported but unused"),
+        (b'', b"'scripts.lib.setup_path_on_import' imported but unused"),
+
+        # Our ipython startup pythonrc file intentionally imports *
+        (b"scripts/lib/pythonrc.py",
+         b" import *' used; unable to detect undefined names"),
+
+        # Special dev_settings.py import
+        (b'', b"from .prod_settings_template import *"),
+
+        (b"settings.py", b"settings import *' used; unable to detect undefined names"),
+        (b"settings.py", b"may be undefined, or defined from star imports"),
+
+        # Sphinx adds `tags` specially to the environment when running conf.py.
+        (b"docs/conf.py", b"undefined name 'tags'"),
+    ]
+    return run_pyflakes(files, options, suppress_patterns)
--- tools/zulint/linters.py
+++ tools/zulint/linters.py
@@ -1,12 +1,14 @@
 from __future__ import print_function
 from __future__ import absolute_import
 
+import argparse
 import subprocess
 if False:
     # See https://zulip.readthedocs.io/en/latest/testing/mypy.html#mypy-in-production-scripts
-    from typing import List
+    from typing import List, Tuple
 
 from zulint.printer import print_err, colors
+
 
 def run_pycodestyle(files, ignored_rules):
     # type: (List[str], List[str]) -> bool
@@ -22,4 +24,28 @@ def run_pycodestyle(files, ignored_rules):
     for line in iter(pep8.stdout.readline, b''):
         print_err('pep8', color, line)
         failed = True
+    return failed
+
+
+def run_pyflakes(files, options, suppress_patterns=[]):
+    # type: (List[str], argparse.Namespace, List[Tuple[str, str]]) -> bool
+    if len(files) == 0:
+        return False
+    failed = False
+    color = next(colors)
+    pyflakes = subprocess.Popen(['pyflakes'] + files,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+    assert pyflakes.stdout is not None  # Implied by use of subprocess.PIPE
+
+    def suppress_line(line: str) -> bool:
+        for file_pattern, line_pattern in suppress_patterns:
+            if file_pattern in line and line_pattern in line:
+                return True
+        return False
+
+    for ln in pyflakes.stdout.readlines() + pyflakes.stderr.readlines():
+        if options.full or not suppress_line(ln):
+            print_err('pyflakes', color, ln)
+            failed = True
     return failed
