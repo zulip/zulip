472b132f8af736cc888796c42716b3194d46ab81
YashRE42 <33805964+YashRE42@users.noreply.github.com>
2019-06-08 15:45:33 +0530

    tooltips: Fix doubling of keyboard-icon.
    This fixes a bug where the default fade-in animation of bootstrap tool
    tips caused the tool tip over the keyboard-icon to change shape slightly
    while fading away. Fixes #11766.

--- static/js/ui_init.js
+++ static/js/ui_init.js
@@ -266,7 +266,7 @@ exports.initialize_kitchen_sink_stuff = function () {
 
     $('.copy_message[data-toggle="tooltip"]').tooltip();
 
-    $('#keyboard-icon').tooltip();
+    $('#keyboard-icon').tooltip({animation: false});
 
     $("body").on("mouseover", ".message_edit_content", function () {
         $(this).closest(".message_row").find(".copy_message").show();
