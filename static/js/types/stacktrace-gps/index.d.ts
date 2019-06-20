2b6e21abeddfadec91bd8b1f5ccdf564f07ef504
Thomas Ip <hkmp7tommy@gmail.com>
2019-06-20 17:59:55 +0800

    blueslip: Make stack trace more readable.
    The stack trace popup is now sourcemapped and each stackframe have a
    expandable code context window.
    
    [anders@zulipchat.com: Rebased and simplified.]

---
+++ static/js/types/stacktrace-gps/index.d.ts
@@ -0,0 +1,26 @@
+// https://github.com/stacktracejs/stackframe/pull/27
+/// <reference types="stackframe/stackframe" />
+
+import SourceMap from "source-map";
+
+declare namespace StackTraceGPS {
+    type StackTraceGPSOptions = {
+        sourceCache?: { [url: string]: string };
+        sourceMapConsumerCache?: { [sourceMappingUrl: string]: SourceMap.SourceMapConsumer };
+        offline?: boolean;
+        ajax?(url: string): Promise<string>;
+        atob?(base64: string): string;
+    };
+}
+
+// eslint-disable-next-line no-redeclare
+declare class StackTraceGPS {
+    constructor(options?: StackTraceGPS.StackTraceGPSOptions);
+    pinpoint(stackframe: StackFrame.StackFrame): Promise<StackFrame.StackFrame>;
+    findFunctionName(stackframe: StackFrame.StackFrame): Promise<StackFrame.StackFrame>;
+    getMappedLocation(stackframe: StackFrame.StackFrame): Promise<StackFrame.StackFrame>;
+}
+
+export = StackTraceGPS;
+
+export as namespace StackTraceGPS; // global for non-module UMD users
