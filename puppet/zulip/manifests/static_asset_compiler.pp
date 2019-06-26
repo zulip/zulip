aea1279e8ca98958d62c88fa0fd13a9070aa9d47
Tim Abbott <tabbott@zulipchat.com>
2019-06-26 11:31:13 -0700

    puppet: Remove trusty configuration for static_asset_compiler.
    Trusty is desupported.

--- puppet/zulip/manifests/static_asset_compiler.pp
+++ puppet/zulip/manifests/static_asset_compiler.pp
@@ -2,11 +2,7 @@ class zulip::static_asset_compiler {
   include zulip::common
   case $::osfamily {
     'debian': {
-      if $zulip::base::release_name == 'trusty' {
-        $closure_compiler_package = 'libclosure-compiler-java'
-      } else {
-        $closure_compiler_package = 'closure-compiler'
-      }
+      $closure_compiler_package = 'closure-compiler'
       $static_asset_compiler_packages = [
         # Needed for minify-js
         $closure_compiler_package,
