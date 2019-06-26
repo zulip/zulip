8fbd965ab54f9332aa0d5952d6352f69330c83f2
Tim Abbott <tabbott@zulipchat.com>
2019-06-26 11:30:07 -0700

    puppet: Remove legacy pgtune related configuration for trusty.
    Since we no longer support Ubuntu Trusty, we no longer need this
    backwards-compatibility cruft (which we only kept around to avoid
    randomizing configuration for existing systems).

--- puppet/zulip/manifests/postgres_appdb_tuned.pp
+++ puppet/zulip/manifests/postgres_appdb_tuned.pp
@@ -3,66 +3,15 @@
 class zulip::postgres_appdb_tuned {
   include zulip::postgres_appdb_base
 
-$postgres_conf = $::osfamily ? {
-  'debian' => "/etc/postgresql/${zulip::base::postgres_version}/main/postgresql.conf",
-  'redhat' => "/var/lib/pgsql/${zulip::base::postgres_version}/data/postgresql.conf",
-}
-$postgres_restart = $::osfamily ? {
-  'debian' => "pg_ctlcluster ${zulip::base::postgres_version} main restart",
-  'redhat' => "systemctl restart postgresql-${zulip::base::postgres_version}",
-}
-if $zulip::base::release_name == 'trusty' {
-  # tools for database setup
-  $postgres_appdb_tuned_packages = ['pgtune']
-  package { $postgres_appdb_tuned_packages: ensure => 'installed' }
-
-  file { "${postgres_conf}.template":
-    ensure  => file,
-    require => Package[$zulip::postgres_appdb_base::postgresql],
-    owner   => 'postgres',
-    group   => 'postgres',
-    mode    => '0644',
-    content => template("zulip/postgresql/${zulip::base::postgres_version}/postgresql.conf.template.erb"),
+  $postgres_conf = $::osfamily ? {
+    'debian' => "/etc/postgresql/${zulip::base::postgres_version}/main/postgresql.conf",
+    'redhat' => "/var/lib/pgsql/${zulip::base::postgres_version}/data/postgresql.conf",
+  }
+  $postgres_restart = $::osfamily ? {
+    'debian' => "pg_ctlcluster ${zulip::base::postgres_version} main restart",
+    'redhat' => "systemctl restart postgresql-${zulip::base::postgres_version}",
   }
 
-  $half_memory = $zulip::base::total_memory / 2
-  $half_memory_pages = $half_memory / 4096
-
-  file {'/etc/sysctl.d/40-postgresql.conf':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content =>
-"kernel.shmall = ${half_memory_pages}
-kernel.shmmax = ${half_memory}
-
-# These are the defaults on newer kernels
-vm.dirty_ratio = 10
-vm.dirty_background_ratio = 5
-"
-    }
-
-  exec { 'sysctl_p':
-    command     => '/sbin/sysctl -p /etc/sysctl.d/40-postgresql.conf',
-    subscribe   => File['/etc/sysctl.d/40-postgresql.conf'],
-    refreshonly => true,
-  }
-
-  exec { 'pgtune':
-    require     => Package['pgtune'],
-    # Let Postgres use half the memory on the machine
-    command     => "pgtune -T Web -M ${half_memory} -i ${postgres_conf}.template -o ${postgres_conf}",
-    refreshonly => true,
-    subscribe   => File["${postgres_conf}.template"]
-  }
-
-  exec { "pg_ctlcluster ${zulip::base::postgres_version} main restart":
-    require     => Exec['sysctl_p'],
-    refreshonly => true,
-    subscribe   => [ Exec['pgtune'], File['/etc/sysctl.d/40-postgresql.conf'] ]
-  }
-} else {
   $half_memory = $zulip::base::total_memory / 2
   $half_memory_pages = $half_memory / 4096
 
@@ -94,6 +43,4 @@ vm.dirty_background_ratio = 5
     refreshonly => true,
     subscribe   => [ File[$postgres_conf] ]
   }
-}
-
 }
