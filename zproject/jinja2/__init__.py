8a7445de955642534c62dfea5983cf870fb690d3
Anders Kaseorg <anders@zulipchat.com>
2019-06-28 21:45:21 -0700

    zproject: Remove unused template tags minified_js, static.
    `minified_js` was obsoleted by the Webpack migration and `static` has
    never been used.
    
    Signed-off-by: Anders Kaseorg <anders@zulipchat.com>

--- zproject/jinja2/__init__.py
+++ zproject/jinja2/__init__.py
@@ -1,7 +1,6 @@
 
 from typing import Any
 
-from django.contrib.staticfiles.storage import staticfiles_storage
 from django.template.defaultfilters import slugify, pluralize
 from django.urls import reverse
 from django.utils import translation
@@ -9,17 +8,14 @@ from django.utils.timesince import timesince
 from jinja2 import Environment
 from two_factor.templatetags.two_factor import device_action
 
-from .compressors import minified_js
 from zerver.templatetags.app_filters import display_list, render_markdown_path
 
 
 def environment(**options: Any) -> Environment:
     env = Environment(**options)
     env.globals.update({
-        'static': staticfiles_storage.url,
         'url': reverse,
         'render_markdown_path': render_markdown_path,
-        'minified_js': minified_js,
     })
 
     env.install_gettext_translations(translation, True)
