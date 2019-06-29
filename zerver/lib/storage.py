f47f98e2f1c275b5a7c1052f6e286961f6154259
Anders Kaseorg <anders@zulipchat.com>
2019-06-29 02:58:53 -0700

    storage: Remove AddHeaderMixin.
    We should be doing this inside webpack, which would be a lot less
    hacky.
    
    Signed-off-by: Anders Kaseorg <anders@zulipchat.com>

--- zerver/lib/storage.py
+++ zerver/lib/storage.py
@@ -8,53 +8,6 @@ from django.conf import settings
 from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
 from pipeline.storage import PipelineMixin
 
-class AddHeaderMixin:
-    def post_process(self, paths: Dict[str, Tuple['ZulipStorage', str]], dry_run: bool=False,
-                     **kwargs: Any) -> List[Tuple[str, str, bool]]:
-        if dry_run:
-            return []
-
-        with open(settings.STATIC_HEADER_FILE, 'rb') as header_file:
-            header = header_file.read().decode(settings.FILE_CHARSET)
-
-        # A dictionary of path to tuples of (old_path, new_path,
-        # processed).  The return value of this method is the values
-        # of this dictionary
-        ret_dict = {}
-
-        for name in paths:
-            storage, path = paths[name]
-
-            if not path.startswith('min/') or not path.endswith('.css'):
-                ret_dict[path] = (path, path, False)
-                continue
-
-            # Prepend the header
-            with storage.open(path, 'rb') as orig_file:
-                orig_contents = orig_file.read().decode(settings.FILE_CHARSET)
-
-            storage.delete(path)
-
-            with storage.open(path, 'w') as new_file:
-                new_file.write(header + orig_contents)
-
-            ret_dict[path] = (path, path, True)
-
-        super_class = super()
-        if hasattr(super_class, 'post_process'):
-            super_ret = super_class.post_process(paths, dry_run, **kwargs)  # type: ignore # https://github.com/python/mypy/issues/2956
-        else:
-            super_ret = []
-
-        # Merge super class's return value with ours
-        for val in super_ret:
-            old_path, new_path, processed = val
-            if processed:
-                ret_dict[old_path] = val
-
-        return list(ret_dict.values())
-
-
 class RemoveUnminifiedFilesMixin:
     def post_process(self, paths: Dict[str, Tuple['ZulipStorage', str]], dry_run: bool=False,
                      **kwargs: Any) -> List[Tuple[str, str, bool]]:
@@ -122,6 +75,6 @@ if settings.PRODUCTION:
     ManifestStaticFilesStorage.path = path
 
 class ZulipStorage(PipelineMixin,
-                   AddHeaderMixin, RemoveUnminifiedFilesMixin,
+                   RemoveUnminifiedFilesMixin,
                    IgnoreBundlesManifestStaticFilesStorage):
     pass
