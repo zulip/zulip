17c3286770df178165bd9aa12ed6d3681a1ec3aa
Wyatt Hoodes <hoodesw@hawaii.edu>
2019-07-01 09:07:56 -1000

    provision: Improve error messaging when attempting to use Ubuntu Trusty.
    As part of dropping support, we add appropriate error messaging when a
    user attempts to provision while using trusty.  If the user is running
    in Vagrant we append information on how to proceed.

--- tools/lib/provision.py
+++ tools/lib/provision.py
@@ -128,6 +128,12 @@ codename = distro_info['DISTRIB_CODENAME']
 family = distro_info['DISTRIB_FAMILY']
 if not (vendor in SUPPORTED_PLATFORMS and codename in SUPPORTED_PLATFORMS[vendor]):
     logging.critical("Unsupported platform: {} {}".format(vendor, codename))
+    if codename == 'trusty':
+        print()
+        print("Ubuntu Trusty reached end-of-life upstream and is no longer a supported platform for Zulip")
+        if os.path.exists('/home/vagrant'):
+            print("It's recommended to run `vagrant destroy`, and rebuild the VM.\n")
+            print("See: https://zulip.readthedocs.io/en/latest/development/setup-vagrant.html")
     sys.exit(1)
 
 POSTGRES_VERSION_MAP = {
