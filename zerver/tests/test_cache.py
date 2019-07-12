c9d7d00372dda8b56e4245e81c8d1433586aec94
Tim Abbott <tabbott@zulipchat.com>
2019-07-12 15:11:58 -0700

    cache: Fix caching of system bots.
    In the unlikely event that someone edited the properties of a system
    bot and then saved the result, we were still caching the old version
    indefinitely in the get_system_bot cache.
    
    This led to a confusing case where a newly installed Zulip server
    didn't have is_api_super_user properly set on its EMAIL_GATEWAY_BOT in
    memcached.
    
    Co-authored-by: Mateusz Mandera <mateusz.mandera@protonmail.com>

--- zerver/tests/test_cache.py
+++ zerver/tests/test_cache.py
@@ -1,7 +1,10 @@
+from django.conf import settings
+
 from mock import Mock, patch
 
 from zerver.apps import flush_cache
 from zerver.lib.test_classes import ZulipTestCase
+from zerver.models import get_system_bot, get_user_profile_by_email
 
 class AppsTest(ZulipTestCase):
     def test_cache_gets_flushed(self) -> None:
@@ -11,3 +14,23 @@ class AppsTest(ZulipTestCase):
                 flush_cache(Mock())
                 mock.assert_called_once()
             mock_logging.assert_called_once()
+
+class BotCacheKeyTest(ZulipTestCase):
+    def test_bot_profile_key_deleted_on_save(self) -> None:
+        # Get the profile cached on both cache keys:
+        user_profile = get_user_profile_by_email(settings.EMAIL_GATEWAY_BOT)
+        bot_profile = get_system_bot(settings.EMAIL_GATEWAY_BOT)
+        self.assertEqual(user_profile, bot_profile)
+
+        # Flip the setting and save:
+        flipped_setting = not bot_profile.is_api_super_user
+        bot_profile.is_api_super_user = flipped_setting
+        bot_profile.save()
+
+        # The .save() should have deleted cache keys, so if we fetch again,
+        # the returned objects should have is_api_super_user set correctly.
+        bot_profile2 = get_system_bot(settings.EMAIL_GATEWAY_BOT)
+        self.assertEqual(bot_profile2.is_api_super_user, flipped_setting)
+
+        user_profile2 = get_user_profile_by_email(settings.EMAIL_GATEWAY_BOT)
+        self.assertEqual(user_profile2.is_api_super_user, flipped_setting)
