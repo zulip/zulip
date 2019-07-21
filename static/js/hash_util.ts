9e46cab78d358c0804ea55f8b05e5e49510e4f85
Thomas Ip <hkmp7tommy@gmail.com>
2019-07-21 11:49:16 +0800

    typesript: Use ES6/TS syntax in hash_util.ts.

--- static/js/hash_util.ts
+++ static/js/hash_util.ts
@@ -1,26 +1,29 @@
 import _ from 'underscore';
 import { Message, Subscription } from './data_structures';
 
+/** Given "#streams/subscribed", returns "streams" */
 export function get_hash_category(hash: string): string {
-    // given "#streams/subscribed", returns "streams"
     return hash ? hash.replace(/^#/, "").split(/\//)[0] : "";
 }
 
+/**
+ * Given "#settings/your-account", returns "your-account".
+ * Given '#streams/5/social", returns "5"
+ */
 export function get_hash_section(hash: string): string {
-    // given "#settings/your-account", returns "your-account"
-    // given '#streams/5/social", returns "5"
     if (!hash) {
         return '';
     }
 
-    var parts = hash.replace(/\/$/, "").split(/\//);
+    const parts = hash.replace(/\/$/, "").split(/\//);
 
     return parts[1] || '';
 }
 
-// Some browsers zealously URI-decode the contents of
-// window.location.hash.  So we hide our URI-encoding
-// by replacing % with . (like MediaWiki).
+/**
+ * Some browsers zealously URI-decode the contents of window.location.hash.
+ * So we hide our URI-encoding by replacing % with . (like MediaWiki).
+ */
 export function encodeHashComponent(str: string): string {
     return encodeURIComponent(str)
         .replace(/\./g, '%2E')
@@ -29,7 +32,7 @@ export function encodeHashComponent(str: string): string {
 
 export function encode_operand(operator: string, operand: string): string {
     if (operator === 'group-pm-with' || operator === 'pm-with' || operator === 'sender') {
-        var slug = people.emails_to_slug(operand);
+        const slug = people.emails_to_slug(operand);
         if (slug) {
             return slug;
         }
@@ -45,7 +48,7 @@ export function encode_operand(operator: string, operand: string): string {
 export function encode_stream_id(stream_id: number): string {
     // stream_data appends the stream name, but it does not do the
     // URI encoding piece
-    var slug = stream_data.id_to_slug(stream_id);
+    const slug = stream_data.id_to_slug(stream_id);
 
     return encodeHashComponent(slug);
 }
@@ -64,7 +67,7 @@ export function decodeHashComponent(str: string): string {
 
 export function decode_operand(operator: string, operand: string): string {
     if (operator === 'group-pm-with' || operator === 'pm-with' || operator === 'sender') {
-        var emails = people.slug_to_emails(operand);
+        const emails = people.slug_to_emails(operand);
         if (emails) {
             return emails;
         }
@@ -94,20 +97,21 @@ type NarrowOperator = {
     operand: string;
 };
 
-// Encodes an operator list into the
-// corresponding hash: the # component
-// of the narrow URL
+/**
+ * Encodes an operator list into the corresponding hash: the # component of
+ * the narrow URL.
+ */
 export function operators_to_hash(operators: NarrowOperator[] | undefined): string {
-    var hash = '#';
+    let hash = '#';
 
     if (operators !== undefined) {
         hash = '#narrow';
         _.each(operators, function (elem) {
             // Support legacy tuples.
-            var operator = elem.operator;
-            var operand = elem.operand;
+            const operator = elem.operator;
+            const operand = elem.operand;
 
-            var sign = elem.negated ? '-' : '';
+            const sign = elem.negated ? '-' : '';
             hash += '/' + sign + encodeHashComponent(operator)
                   + '/' + encode_operand(operator, operand);
         });
@@ -137,11 +141,11 @@ export function huddle_with_uri(user_ids_string: string): string {
 }
 
 export function by_conversation_and_time_uri(message: Message): string {
-    var absolute_url = window.location.protocol + "//" +
+    const absolute_url = window.location.protocol + "//" +
         window.location.host + "/" +
         window.location.pathname.split('/')[1];
 
-    var suffix = "/near/" + encodeHashComponent(message.id.toString());
+    const suffix = "/near/" + encodeHashComponent(message.id.toString());
 
     if (message.type === "stream") {
         return absolute_url +
@@ -153,35 +157,34 @@ export function by_conversation_and_time_uri(message: Message): string {
 }
 
 export function stream_edit_uri(sub: Subscription): string {
-    var hash = "#streams" + "/" + sub.stream_id + "/" + encodeHashComponent(sub.name);
+    const hash = "#streams" + "/" + sub.stream_id + "/" + encodeHashComponent(sub.name);
     return hash;
 }
 
 export function parse_narrow(hash: string[]): NarrowOperator[] | undefined {
-    var i;
-    var operators = [];
-    for (i = 1; i < hash.length; i += 2) {
+    const operators = [];
+    for (let i = 1; i < hash.length; i += 2) {
         // We don't construct URLs with an odd number of components,
         // but the user might write one.
-        var operator = decodeHashComponent(hash[i]);
+        let operator = decodeHashComponent(hash[i]);
         // Do not parse further if empty operator encountered.
         if (operator === '') {
             break;
         }
 
-        var raw_operand = hash[i + 1];
+        const raw_operand = hash[i + 1];
 
         if (!raw_operand) {
             return undefined;
         }
 
-        var operand  = decode_operand(operator, raw_operand);
-        var negated = false;
-        if (operator[0] === '-') {
+        const operand  = decode_operand(operator, raw_operand);
+        let negated = false;
+        if (operator.startsWith('-')) {
             negated = true;
             operator = operator.slice(1);
         }
-        operators.push({negated: negated, operator: operator, operand: operand});
+        operators.push({ negated, operator, operand });
     }
     return operators;
 }
