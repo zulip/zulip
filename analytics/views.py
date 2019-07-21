e93f8de263a0b7b317a9bc82d50945d5be1be3c9
Rishi Gupta <rishig@zulipchat.com>
2019-07-20 18:58:14 -0700

    support: Use delivery_email instead of email.

--- analytics/views.py
+++ analytics/views.py
@@ -1073,13 +1073,13 @@ def support(request: HttpRequest) -> HttpResponse:
     if query:
         key_words = get_invitee_emails_set(query)
 
-        users = UserProfile.objects.filter(email__in=key_words)
+        users = UserProfile.objects.filter(delivery_email__in=key_words)
         if users:
             for user in users:
                 user.realm.realm_icon_url = realm_icon_url(user.realm)
                 user.realm.admin_emails = ", ".join(
                     user.realm.get_human_admin_users().values_list(
-                        "email",
+                        "delivery_email",
                         flat=True))
                 user.realm.default_discount = get_discount_for_realm(user.realm)
             context["users"] = users
@@ -1104,7 +1104,8 @@ def support(request: HttpRequest) -> HttpResponse:
         if realms:
             for realm in realms:
                 realm.realm_icon_url = realm_icon_url(realm)
-                realm.admin_emails = ", ".join(realm.get_human_admin_users().values_list("email", flat=True))
+                realm.admin_emails = ", ".join(realm.get_human_admin_users().values_list(
+                    "delivery_email", flat=True))
                 realm.default_discount = get_discount_for_realm(realm)
             context["realms"] = realms
     return render(request, 'analytics/support.html', context=context)
