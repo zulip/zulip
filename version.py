31bd3d617de4ab6dd2a3d4002f22fa077898b688
Anders Kaseorg <anders@zulipchat.com>
2019-07-21 19:00:19 -0700

    version: Document when a dependency upgrade requires a major bump.
    Signed-off-by: Anders Kaseorg <anders@zulipchat.com>

--- version.py
+++ version.py
@@ -18,7 +18,12 @@ LATEST_RELEASE_ANNOUNCEMENT = "https://blog.zulip.org/2019/03/01/zulip-2-0-relea
 # the major version to indicate that folks should provision in both
 # directions.
 
-# Typically, adding a dependency only requires a minor version bump, and
-# removing a dependency requires a major version bump.
+# Typically,
+# * adding a dependency only requires a minor version bump;
+# * removing a dependency requires a major version bump;
+# * upgrading a dependency requires a major version bump, unless the
+#   upgraded dependency is backwards compatible with all of our
+#   historical commits sharing the same major version, in which case a
+#   minor version bump suffices.
 
 PROVISION_VERSION = '40.0'
