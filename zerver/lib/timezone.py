1c8106fc67c4ddc4820fd3fbc250f427c359d24b
Wyatt Hoodes <hoodesw@hawaii.edu>
2019-07-23 20:19:43 -1000

    timezone: Fix return type in get_timezone.
    While it's true `datetime` is implicit via `pytz`, it makes sense
    that mypy should now complain about the semantics of calling our
    return type `pytz.datetime.tzinfo`, when such a type doesn't
    actually exist.

--- zerver/lib/timezone.py
+++ zerver/lib/timezone.py
@@ -2,9 +2,10 @@
 from typing import List
 
 import pytz
+import datetime
 
 def get_all_timezones() -> List[str]:
     return sorted(pytz.all_timezones)
 
-def get_timezone(tz: str) -> pytz.datetime.tzinfo:
+def get_timezone(tz: str) -> datetime.tzinfo:
     return pytz.timezone(tz)
