927feb615e8dfc1a1f961bd1657e54924b4aa6d9
Wyatt Hoodes <hoodesw@hawaii.edu>
2019-07-24 21:10:53 -1000

    provision: Comment why NoReturn can't import normally.

--- tools/lib/provision.py
+++ tools/lib/provision.py
@@ -25,7 +25,7 @@ from scripts.lib.node_cache import setup_node_modules, NODE_MODULES_CACHE_PATH
 from tools.setup import setup_venvs
 
 if False:
-    # See https://zulip.readthedocs.io/en/latest/testing/mypy.html#mypy-in-production-scripts
+    # `NoReturn` was introduced in python 3.5.4
     from typing import List, NoReturn
 
 SUPPORTED_PLATFORMS = {
