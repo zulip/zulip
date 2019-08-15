c15b2e3689269da805fc4c5790334acc6b65fec0
Alexandra Ciobica <alexandraciobica@gmail.com>
2019-08-15 22:17:05 +0300

    integrations: Fix arrangement of left sidebar `back to list` button.
    I rearranged the elements of the left sidebar in HTML in order to appear
    in the order they are displayed and removed the absolute positioning,
    because it was not needed if the elements are arranged correctly. I used
    `flex` display to arrange them on column.
    
    I removed the styling that positioned the elements absolutely.
    
    Then I tweaked the margins in order to make the elements look good.
    
    Fixes: #12929

--- static/js/portico/integrations.js
+++ static/js/portico/integrations.js
@@ -169,7 +169,7 @@ function hide_catalog_show_integration() {
         $('#integration-instruction-block .integration-lozenge').remove();
         $("#integration-instruction-block")
             .append($lozenge_icon)
-            .css('display', 'block');
+            .css('display', 'flex');
         $('.integration-instructions#' + state.integration).css('display', 'block');
         $("#integration-list-link").css('display', 'block');
 
