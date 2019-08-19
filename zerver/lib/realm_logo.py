8ee0573136edcd8fa94635c8f8d2a6df8bb86b43
Pragati Agrawal <pragati22066@gmail.com>
2019-08-19 18:34:45 +0530

    realm_logo: Refactor `get_realm_logo_url` function.

--- zerver/lib/realm_logo.py
+++ zerver/lib/realm_logo.py
@@ -9,16 +9,15 @@ def realm_logo_url(realm: Realm, night: bool) -> str:
     return get_realm_logo_url(realm, night)
 
 def get_realm_logo_url(realm: Realm, night: bool) -> str:
-    if not night:
-        if realm.logo_source == 'U':
-            return upload_backend.get_realm_logo_url(realm.id, realm.logo_version, night)
-        else:
-            return settings.DEFAULT_LOGO_URI+'?version=0'
+    if night:
+        logo_source = realm.night_logo_source
+        logo_version = realm.night_logo_version
     else:
-        if realm.night_logo_source == 'U':
-            return upload_backend.get_realm_logo_url(realm.id, realm.night_logo_version, night)
-        else:
-            return settings.DEFAULT_LOGO_URI+'?version=0'
+        logo_source = realm.logo_source
+        logo_version = realm.logo_version
+    if logo_source == 'U':
+        return upload_backend.get_realm_logo_url(realm.id, logo_version, night)
+    return settings.DEFAULT_LOGO_URI+'?version=0'
 
 def get_realm_logo_data(realm: Realm, night: bool) -> Dict[str, Any]:
     if night:
