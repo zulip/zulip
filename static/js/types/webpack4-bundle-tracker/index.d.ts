de4685441c784c7d1cee788b5342c7fc33f35978
Anders Kaseorg <anders@zulipchat.com>
2019-11-04 14:37:12 -0800

    typescript: Type webpack.config.ts correctly.
    Signed-off-by: Anders Kaseorg <anders@zulipchat.com>

---
+++ static/js/types/webpack4-bundle-tracker/index.d.ts
@@ -0,0 +1,17 @@
+import { Plugin } from "webpack";
+
+declare namespace BundleTracker {
+    interface Options {
+        path?: string;
+        filename?: string;
+        publicPath?: string;
+        logTime?: boolean;
+    }
+}
+
+// eslint-disable-next-line no-redeclare
+declare class BundleTracker extends Plugin {
+    constructor(options?: BundleTracker.Options);
+}
+
+export = BundleTracker;
