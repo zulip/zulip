c671fccea2fccb103f0879d6e2300a1cb88ed943
Matheus Melo <matheus.almeida@ccc.ufcg.edu.br>
2019-11-16 11:56:40 -0300

    Rename require_user_group_edit_policy to require_user_group_edit_permission

--- zerver/decorator.py
+++ zerver/decorator.py
@@ -534,10 +534,12 @@ def require_member_or_admin(view_func: ViewFuncT) -> ViewFuncT:
         return view_func(request, user_profile, *args, **kwargs)
     return _wrapped_view_func  # type: ignore # https://github.com/python/mypy/issues/1927
 
-def require_user_group_edit_policy(view_func: ViewFuncT) -> ViewFuncT:
+def require_user_group_edit_permission(view_func: ViewFuncT) -> ViewFuncT:
+    check_member_or_admin = require_member_or_admin(view_func)
     @wraps(view_func)
     def _wrapped_view_func(request: HttpRequest, user_profile: UserProfile,
                            *args: Any, **kwargs: Any) -> HttpResponse:
+        check_member_or_admin(request, user_profile, *args, **kwargs)
         realm = user_profile.realm
         if realm.user_group_edit_policy != Realm.USER_GROUP_EDIT_POLICY_MEMBERS and \
                 not user_profile.is_realm_admin:
--- zerver/views/user_groups.py
+++ zerver/views/user_groups.py
@@ -3,7 +3,7 @@ from django.utils.translation import ugettext as _
 
 from typing import List
 
-from zerver.decorator import require_member_or_admin, require_user_group_edit_policy
+from zerver.decorator import require_member_or_admin, require_user_group_edit_permission
 from zerver.lib.actions import check_add_user_group, do_update_user_group_name, \
     do_update_user_group_description, bulk_add_members_to_user_group, \
     remove_members_from_user_group, check_delete_user_group
@@ -18,7 +18,7 @@ from zerver.models import UserProfile
 from zerver.views.streams import compose_views, FuncKwargPair
 
 @require_member_or_admin
-@require_user_group_edit_policy
+@require_user_group_edit_permission
 @has_request_variables
 def add_user_group(request: HttpRequest, user_profile: UserProfile,
                    name: str=REQ(),
@@ -35,7 +35,7 @@ def get_user_group(request: HttpRequest, user_profile: UserProfile) -> HttpRespo
     return json_success({"user_groups": user_groups})
 
 @require_member_or_admin
-@require_user_group_edit_policy
+@require_user_group_edit_permission
 @has_request_variables
 def edit_user_group(request: HttpRequest, user_profile: UserProfile,
                     user_group_id: int=REQ(validator=check_int),
@@ -55,7 +55,7 @@ def edit_user_group(request: HttpRequest, user_profile: UserProfile,
     return json_success()
 
 @require_member_or_admin
-@require_user_group_edit_policy
+@require_user_group_edit_permission
 @has_request_variables
 def delete_user_group(request: HttpRequest, user_profile: UserProfile,
                       user_group_id: int=REQ(validator=check_int)) -> HttpResponse:
@@ -64,7 +64,7 @@ def delete_user_group(request: HttpRequest, user_profile: UserProfile,
     return json_success()
 
 @require_member_or_admin
-@require_user_group_edit_policy
+@require_user_group_edit_permission
 @has_request_variables
 def update_user_group_backend(request: HttpRequest, user_profile: UserProfile,
                               user_group_id: int=REQ(validator=check_int),
