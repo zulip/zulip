68e93d24354ec4fca32a3d1b2bfbcf2f8ce5fdb9
Rohitt Vashishtha <aero31aero@gmail.com>
2019-11-28 10:26:57 +0000

    update-message: Use MentionData in the update_message_backend code.
    This is a performance optimization, since we can avoid doing work
    related to wildcard mentions in the common case that the message can't
    have any.  We also add a unit test for adding wildcard mentions in a
    message edit.

--- zerver/tests/test_messages.py
+++ zerver/tests/test_messages.py
@@ -2925,7 +2925,8 @@ class EditMessageTest(ZulipTestCase):
                 content=None,
                 rendered_content=None,
                 prior_mention_user_ids=set(),
-                mention_user_ids=set()
+                mention_user_ids=set(),
+                mention_data=None,
             )
 
             mock_send_event.assert_called_with(mock.ANY, mock.ANY, users_to_be_notified)
@@ -2971,6 +2972,42 @@ class EditMessageTest(ZulipTestCase):
         self.login(hamlet.email)
         users_to_be_notified = list(map(notify, [hamlet.id]))
         do_update_message_topic_success(hamlet, message, "Change again", users_to_be_notified)
+
+    @mock.patch("zerver.lib.actions.send_event")
+    def test_wildcard_mention(self, mock_send_event: mock.MagicMock) -> None:
+        stream_name = "Macbeth"
+        hamlet = self.example_user("hamlet")
+        cordelia = self.example_user("cordelia")
+        self.make_stream(stream_name, history_public_to_subscribers=True)
+        self.subscribe(hamlet, stream_name)
+        self.subscribe(cordelia, stream_name)
+        self.login(hamlet.email)
+        message_id = self.send_stream_message(hamlet.email, stream_name, "Hello everyone")
+
+        def notify(user_id: int) -> Dict[str, Any]:
+            return {
+                "id": user_id,
+                "flags": ["wildcard_mentioned"]
+            }
+
+        users_to_be_notified = list(map(notify, [cordelia.id, hamlet.id]))
+        result = self.client_patch("/json/messages/" + str(message_id), {
+            'message_id': message_id,
+            'content': 'Hello @**everyone**',
+        })
+        self.assert_json_success(result)
+
+        # Extract the send_event call where event type is 'update_message'.
+        # Here we assert wildcard_mention_user_ids has been set properly.
+        called = False
+        for call_args in mock_send_event.call_args_list:
+            (arg_realm, arg_event, arg_notified_users) = call_args[0]
+            if arg_event['type'] == 'update_message':
+                self.assertEqual(arg_event['type'], 'update_message')
+                self.assertEqual(arg_event['wildcard_mention_user_ids'], [cordelia.id, hamlet.id])
+                self.assertEqual(arg_notified_users, users_to_be_notified)
+                called = True
+        self.assertTrue(called)
 
     def test_propagate_topic_forward(self) -> None:
         self.login(self.example_email("hamlet"))
