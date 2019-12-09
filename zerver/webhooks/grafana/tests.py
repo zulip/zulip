baea9057301f1b8551f9eae5b21ab32028c6674d
Mike Trame <mtrame@umich.edu>
2019-12-09 14:14:56 -0500

    added test case

--- zerver/webhooks/grafana/tests.py
+++ zerver/webhooks/grafana/tests.py
@@ -1,1 +1,25 @@
-# TODO
+# -*- coding: utf-8 -*-
+from django.conf import settings
+
+from zerver.lib.test_classes import WebhookTestCase
+from zerver.models import get_system_bot
+
+class GrafanaHookTests(WebhookTestCase):
+    STREAM_NAME = 'test'
+    URL_TEMPLATE = "/api/v1/external/grafana?&api_key={api_key}&stream={stream}"
+    FIXTURE_DIR_NAME = 'grafana'
+
+    # Note: Include a test function per each distinct message condition your integration supports
+    def test_grafana_message(self) -> None:
+        expected_topic = "Grafana alert: My alert";
+        expected_message = "Rule: **[Load peaking!](http://url.to.grafana/db/dashboard/my_dashboard?panelId=2)**\n"
+        expected_message += "Rule ID: 1\n"
+        expected_message += "State: alerting\n"
+        expected_message += "Message: Load is peaking. Make sure the traffic is real and spin up more webfronts"
+
+        self.send_and_test_stream_message('grafana', expected_topic, expected_message,
+                                          content_type="application/x-www-form-urlencoded")
+
+    def get_body(self, fixture_name: str) -> str:
+        print(fixture_name)
+        return self.webhook_fixture_data("grafana", fixture_name, file_type="json")
--- zerver/webhooks/grafana/view.py
+++ zerver/webhooks/grafana/view.py
@@ -1,5 +1,5 @@
 """Webhook handler for Grafana."""
-from typing import Any, Dict, Iterable, Optional
+from typing import Any, Dict, Iterable
 
 from django.http import HttpRequest, HttpResponse
 from django.utils.translation import ugettext as _
@@ -11,10 +11,10 @@ from zerver.lib.response import json_error, json_success
 from zerver.lib.validator import check_dict, check_string
 from zerver.models import UserProfile
 
-@api_key_only_webhook_view(webhook_client_name="Grafana")
+@api_key_only_webhook_view("Grafana")
 @has_request_variables
 def api_grafana_webhook(request: HttpRequest, user_profile: UserProfile,
-                        payload: Dict[str, Iterable[Dict[str, Any]]] = REQ(argument_type='body')
+                        payload: Dict[str, Iterable[Dict[str, Any]]] = REQ(argument_type='body'),
                         ) -> HttpResponse:
     """
     Grafana's webhooks are used to communicate state change.
@@ -24,14 +24,15 @@ def api_grafana_webhook(request: HttpRequest, user_profile: UserProfile,
     Source: https://zulipchat.com/api/incoming-webhooks-overview
     """
     topic = "Grafana alert: {title}"
-    body_template = "\nRule: **[{ruleName}]({ruleUrl})**\n"
+    body_template = "Rule: **[{ruleName}]({ruleUrl})**\n"
     body_template += "Rule ID: {ruleId}\n"
     body_template += "State: {state}\n"
     body_template += "Message: {message}"
     # Not including evalMatches at the moment...
-
+    
+    topic = topic.format(**payload)
     body = body_template.format(**payload)
 
-    check_and_send_webhook_message(request, user_profile, topic, body)
+    check_send_webhook_message(request, user_profile, topic, body)
 
     return json_success()
