zulip/zulip

commit 263ac0eb456ca972b5c845c9a74fdded6914ae22

pm_conversations: Initialize using server data.
This simple change switches us to take advantage of the
server-maintained data for the pm_conversations system we implemented
originally for mobile use.

This should make it a lot more convenient to find historical private
message conversations, since one can effectively scroll infinitely
into the history.

We'll need to do some profiling of the backend after this is deployed
in production; it's possible we'll need to add some database indexes,
denormalization, or other optimizations to avoid making loading the
Zulip app significantly slower.

Fixes #12502.

static/js/pm_conversations.js
@@ -19,6 +19,12 @@ exports.recent = (function () {
     const recent_private_messages = [];
 
     self.insert = function (user_ids_string, message_id) {
+        if (user_ids_string === '') {
+            // The API uses '' for self-PMs; convert it to the string
+            // containing the current user's ID, which is the format
+            // the webapp expects.
+            user_ids_string = people.my_current_user_id().toString();
+        }
         let conversation = recent_message_ids.get(user_ids_string);
 
         if (conversation === undefined) {
@@ -34,7 +40,12 @@ exports.recent = (function () {
             recent_private_messages.unshift(conversation);
         } else {
             if (conversation.max_message_id >= message_id) {
-                return; // don't backdate our conversation
+                // don't backdate our conversation.  This is the
+                // common code path after initialization when
+                // processing old messages, since we'll already have
+                // the latest message_id for the conversation from
+                // initialization.
+                return;
             }
 
             // update our latest message_id
@@ -56,6 +67,14 @@ exports.recent = (function () {
         // returns array of structs with user_ids_string and
         // message_id
         return _.pluck(recent_private_messages, 'user_ids_string');
+    };
+
+    self.initialize = function () {
+        _.each(page_params.recent_private_conversations, function (conversation) {
+            const user_ids_string = conversation.user_ids.join(",");
+            self.insert(user_ids_string, conversation.max_message_id);
+        });
+        delete page_params.recent_private_messages;
     };
 
     return self;

frontend_tests/node_tests/pm_conversations.js
@@ -13,7 +13,31 @@ run_test('partners', () => {
     assert.equal(pmc.is_partner(user3_id), true);
 });
 
+zrequire("people");
+
 run_test('insert_recent_private_message', () => {
+    set_global('page_params', {
+        recent_private_conversations: [
+            {user_ids: [1, 2],
+             max_message_id: 150,
+            },
+            {user_ids: [1],
+             max_message_id: 111,
+            },
+            {user_ids: [],
+             max_message_id: 7,
+            },
+        ],
+    });
+    people.initialize_current_user(15);
+    pmc.recent.initialize();
+
+    assert.deepEqual(pmc.recent.get(), [
+        {user_ids_string: '1,2', max_message_id: 150},
+        {user_ids_string: '1', max_message_id: 111},
+        {user_ids_string: '15', max_message_id: 7},
+    ]);
+
     pmc.recent.insert('1', 1001);
     pmc.recent.insert('2', 2001);
     pmc.recent.insert('1', 3001);
@@ -24,7 +48,9 @@ run_test('insert_recent_private_message', () => {
     assert.deepEqual(pmc.recent.get(), [
         {user_ids_string: '1', max_message_id: 3001},
         {user_ids_string: '2', max_message_id: 2001},
+        {user_ids_string: '1,2', max_message_id: 150},
+        {user_ids_string: '15', max_message_id: 7},
     ]);
 
-    assert.deepEqual(pmc.recent.get_strings(), ['1', '2']);
+    assert.deepEqual(pmc.recent.get_strings(), ['1', '2', '1,2', '15']);
 });

frontend_tests/node_tests/ui_init.js
@@ -103,6 +103,7 @@ zrequire('search');
 zrequire('tutorial');
 zrequire('notifications');
 zrequire('pointer');
+zrequire('pm_conversations');
 zrequire('compose_fade');
 zrequire('pm_list');
 zrequire('list_cursor');
@@ -140,6 +141,7 @@ page_params.unsubscribed = [];
 page_params.never_subscribed = [];
 page_params.realm_notifications_stream_id = -1;
 page_params.unread_msgs = {};
+page_params.recent_private_conversations = [];
 
 $('#tab_bar').append = () => {};
 $('#compose').filedrop = () => {};

static/js/ui_init.js
@@ -301,6 +301,7 @@ exports.initialize_everything = function () {
     stream_color.initialize();
     stream_edit.initialize();
     stream_data.initialize();
+    pm_conversations.recent.initialize();
     muting.initialize();
     subs.initialize();
     stream_list.initialize();

zerver/lib/events.py
@@ -145,10 +145,6 @@ def always_want(msg_type: str) -> bool:
     info for every event type.  Defining this at module
     level makes it easier to mock.
     '''
-    if settings.PRODUCTION and msg_type == "recent_private_conversations":  # nocoverage
-        # Temporary: Don't include recent_private_conversations in production
-        # by default while the feature is still experimental.
-        return False
     return True
 
 # Fetch initial data.  When event_types is not specified, clients want

commit 8d8284483569743dd3c81dbbd865f3be25b2cc6e

bug fix: Fix huddles in "Private Messages".
If two user_ids in a recent huddle have ids
that sort lexically differently than numerically,
such as 7 and 66, then we were creating two
different buckets in pm_conversations.

This regression was introduced in
263ac0eb456ca972b5c845c9a74fdded6914ae22 on
November 21, 2019.

static/js/pm_conversations.js
@@ -23,6 +23,8 @@ exports.recent = (function () {
             // The server sends [] for self-PMs.
             user_ids = [people.my_current_user_id()];
         }
+        user_ids.sort((a, b) => a - b);
+
         const user_ids_string = user_ids.join(',');
         let conversation = recent_message_ids.get(user_ids_string);
 

frontend_tests/node_tests/pm_conversations.js
@@ -33,7 +33,7 @@ run_test('insert_recent_private_message', () => {
     pmc.recent.initialize();
 
     assert.deepEqual(pmc.recent.get(), [
-        {user_ids_string: '11,2', max_message_id: 150},
+        {user_ids_string: '2,11', max_message_id: 150},
         {user_ids_string: '1', max_message_id: 111},
         {user_ids_string: '15', max_message_id: 7},
     ]);
@@ -48,9 +48,9 @@ run_test('insert_recent_private_message', () => {
     assert.deepEqual(pmc.recent.get(), [
         {user_ids_string: '1', max_message_id: 3001},
         {user_ids_string: '2', max_message_id: 2001},
-        {user_ids_string: '11,2', max_message_id: 150},
+        {user_ids_string: '2,11', max_message_id: 150},
         {user_ids_string: '15', max_message_id: 7},
     ]);
 
-    assert.deepEqual(pmc.recent.get_strings(), ['1', '2', '11,2', '15']);
+    assert.deepEqual(pmc.recent.get_strings(), ['1', '2', '2,11', '15']);
 });