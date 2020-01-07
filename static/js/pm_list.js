4d78fbdf166aaba08ba5e1f5eb7c7e6ebd8ffe5b
Hashir Sarwar <hashirsarwarch@gmail.com>
2020-01-07 00:14:47 +0000

    pm_list: Extract presence logic from _build_private_message_list().
    This is a part of #13605.
    The presence logic is to be included in a new function
    pm_list.update_presence_info().

--- static/js/pm_list.js
+++ static/js/pm_list.js
@@ -88,22 +88,6 @@ exports._build_private_messages_list = function () {
 
         const is_active = user_ids_string === active_user_ids_string;
 
-        let user_circle_class;
-        let fraction_present;
-
-        if (is_group) {
-            user_circle_class = 'user_circle_fraction';
-            fraction_present = buddy_data.huddle_fraction_present(user_ids_string);
-        } else {
-            const user_id = parseInt(user_ids_string, 10);
-            user_circle_class = buddy_data.get_user_circle_class(user_id);
-            const recipient_user_obj = people.get_person_from_user_id(user_id);
-
-            if (recipient_user_obj.is_bot) {
-                user_circle_class = 'user_circle_green';
-            }
-        }
-
         const display_message = {
             recipients: recipients_string,
             user_ids_string: user_ids_string,
