990aa16b6a50dd59e90546035769d7dfe786e16d
Steve Howell <showell@zulipchat.com>
2020-01-14 22:29:54 +0000

    Fix type errors in LazySet.
    I think the only place that was broken is where
    we copy users from streams.

---
+++ frontend_tests/node_tests/lazy_set.js
@@ -0,0 +1,23 @@
+set_global('blueslip', global.make_zblueslip());
+const LazySet = zrequire('lazy_set').LazySet;
+
+/*
+    We mostly test LazySet indirectly.  This code
+    may be short-lived, anyway, once we change
+    how we download subscribers in page_params.
+*/
+
+run_test('map', () => {
+    const ls = LazySet([1, 2]);
+
+    const triple = (n) => n * 3;
+
+    assert.deepEqual(ls.map(triple), [3, 6]);
+});
+
+run_test('conversions', () => {
+    blueslip.set_test_data('error', 'not a number');
+    const ls = LazySet([1, 2]);
+    ls.add('3');
+    assert(ls.has('3'));
+});
