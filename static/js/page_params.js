ac61ae0de03355f08c1a9597677724b4ea87d48f
Steve Howell <showell@zulipchat.com>
2020-01-15 15:41:56 +0000

    Record page_params_parse_time.
    We put page_params_parse_time on the window object
    to help diagnose customer issues.

--- static/js/page_params.js
+++ static/js/page_params.js
@@ -1,4 +1,7 @@
+const t1 = performance.now();
 window.page_params = $("#page-params").remove().data("params");
+const t2 = performance.now();
+window.page_params_parse_time = t2 - t1;
 if (!window.page_params) {
     throw new Error("Missing page-params");
 }
