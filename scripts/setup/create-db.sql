298d45b46ac1353f468fa9995bd920be3b4a62f9
Anders Kaseorg <anders@zulipchat.com>
2020-01-15 19:02:01 -0500

    create-db.sql: Handle exception if zulip user already exists.
    Fixes #13530.
    
    Signed-off-by: Anders Kaseorg <anders@zulipchat.com>

--- scripts/setup/create-db.sql
+++ scripts/setup/create-db.sql
@@ -1,4 +1,8 @@
-CREATE USER zulip;
+DO $$BEGIN
+    CREATE USER zulip;
+EXCEPTION WHEN duplicate_object THEN
+    RAISE NOTICE 'zulip user already exists';
+END$$;
 ALTER ROLE zulip SET search_path TO zulip,public;
 CREATE DATABASE zulip OWNER=zulip;
 \connect zulip
