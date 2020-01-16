6a747dea4421cd5a7c01d9067c33f050ac618082
Anders Kaseorg <anders@zulipchat.com>
2020-01-16 14:40:40 -0500

    js: Clean up stream_id type confusion.
    Signed-off-by: Anders Kaseorg <anders@zulipchat.com>

--- static/js/settings_muting.js
+++ static/js/settings_muting.js
@@ -1,7 +1,7 @@
 exports.set_up = function () {
     $('body').on('click', '.settings-unmute-topic', function (e) {
         const $row = $(this).closest("tr");
-        const stream_id = $row.attr("data-stream-id");
+        const stream_id = parseInt($row.attr("data-stream-id"), 10);
         const topic = $row.attr("data-topic");
 
         e.stopImmediatePropagation();
