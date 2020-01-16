08dc83cd6714ad8b5b72e2bc1f588a9866ce8a58
Jonathan Cobb <jonathan@kyuss.org>
2020-01-16 02:01:39 -0500

    integrations: Add errbit integration.
    Fixes #13685.

---
+++ zerver/webhooks/errbit/view.py
@@ -0,0 +1,33 @@
+from typing import Any, Dict
+
+from django.http import HttpRequest, HttpResponse
+
+from zerver.decorator import api_key_only_webhook_view
+from zerver.lib.request import REQ, has_request_variables
+from zerver.lib.response import json_success
+from zerver.lib.webhooks.common import check_send_webhook_message
+from zerver.models import UserProfile
+
+ERRBIT_TOPIC_TEMPLATE = '{project_name}'
+ERRBIT_MESSAGE_TEMPLATE = '[{error_class}]({error_url}): "{error_message}" occurred.'
+
+@api_key_only_webhook_view('Errbit')
+@has_request_variables
+def api_errbit_webhook(request: HttpRequest, user_profile: UserProfile,
+                       payload: Dict[str, Any]=REQ(argument_type='body')) -> HttpResponse:
+    subject = get_subject(payload)
+    body = get_body(payload)
+    check_send_webhook_message(request, user_profile, subject, body)
+    return json_success()
+
+def get_subject(payload: Dict[str, Any]) -> str:
+    project = payload['problem']['app_name'] + ' / ' + payload['problem']['environment']
+    return ERRBIT_TOPIC_TEMPLATE.format(project_name=project)
+
+def get_body(payload: Dict[str, Any]) -> str:
+    data = {
+        'error_url': payload['problem']['url'],
+        'error_class': payload['problem']['error_class'],
+        'error_message': payload['problem']['message'],
+    }
+    return ERRBIT_MESSAGE_TEMPLATE.format(**data)
