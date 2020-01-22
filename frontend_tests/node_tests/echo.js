fbd697ee70fba381b394af2b3b0db961961dd1a2
Vishnu KS <yo@vishnuks.com>
2020-01-22 16:07:57 +0530

    tests: Add test for insert_local_message in echo.js.

--- frontend_tests/node_tests/echo.js
+++ frontend_tests/node_tests/echo.js
@@ -1,3 +1,11 @@
+set_global('$', global.make_zjquery());
+set_global('people', {});
+set_global('markdown', {});
+set_global('local_message', {
+    now: () => "timestamp",
+});
+set_global('page_params', {});
+
 zrequire('echo');
 zrequire('util');
 
@@ -79,4 +87,68 @@ run_test('process_from_server for differently rendered messages', () => {
         submessages: new_value,
         subject_links: new_value,
     }]);
+});
+
+run_test('insert_local_message', () => {
+    const local_id = 1;
+    people.my_current_email = () => "iago@zulip.com";
+    people.my_full_name = () => "Iago";
+
+    let apply_markdown_called = false;
+    let add_topic_links_called = false;
+    let insert_message_called = false;
+
+    markdown.apply_markdown = () => {
+        apply_markdown_called = true;
+    };
+
+    markdown.add_topic_links = () => {
+        add_topic_links_called = true;
+    };
+
+    local_message.insert_message = (message) => {
+        assert.equal(message.display_recipient, "general");
+        assert.equal(message.timestamp, "timestamp");
+        assert.equal(message.sender_email, "iago@zulip.com");
+        assert.equal(message.sender_full_name, "Iago");
+        assert.equal(message.sender_id, 123);
+        insert_message_called = true;
+    };
+
+    let message_request = {
+        type: "stream",
+        stream: "general",
+        sender_email: "iago@zulip.com",
+        sender_full_name: "Iago",
+        sender_id: 123,
+    };
+    echo.insert_local_message(message_request, local_id);
+
+    assert(apply_markdown_called);
+    assert(add_topic_links_called);
+    assert(insert_message_called);
+
+    add_topic_links_called = false;
+    apply_markdown_called = false;
+    insert_message_called = false;
+
+    people.get_by_email = () => {};
+
+    local_message.insert_message = (message) => {
+        assert.equal(message.display_recipient.length, 2);
+        insert_message_called = true;
+    };
+
+    message_request = {
+        private_message_recipient: "cordelia@zulip.com,hamlet@zulip.com",
+        type: "private",
+        sender_email: "iago@zulip.com",
+        sender_full_name: "Iago",
+        sender_id: 123,
+    };
+    echo.insert_local_message(message_request, local_id);
+    assert(add_topic_links_called);
+    assert(apply_markdown_called);
+    assert(insert_message_called);
+
 });
