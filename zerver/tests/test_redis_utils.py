4cb8fdbb6fb4d12b067e9aaf47c66c813e28d8f8
Mateusz Mandera <mateusz.mandera@protonmail.com>
2020-01-26 19:01:56 +0100

    redis_utils: Require key_format argument in get_dict_from_redis.

--- zerver/tests/test_redis_utils.py
+++ zerver/tests/test_redis_utils.py
@@ -4,7 +4,7 @@ import mock
 
 from zerver.lib.test_classes import ZulipTestCase
 from zerver.lib.redis_utils import get_redis_client, get_dict_from_redis, put_dict_in_redis, \
-    ZulipRedisKeyTooLongError, MAX_KEY_LENGTH
+    ZulipRedisKeyTooLongError, ZulipRedisKeyOfWrongFormatError, MAX_KEY_LENGTH
 
 class RedisUtilsTest(ZulipTestCase):
     key_format = "test_redis_utils_{token}"
@@ -22,7 +22,7 @@ class RedisUtilsTest(ZulipTestCase):
         }
         key = put_dict_in_redis(self.redis_client, self.key_format, data,
                                 expiration_seconds=self.expiration_seconds)
-        retrieved_data = get_dict_from_redis(self.redis_client, key)
+        retrieved_data = get_dict_from_redis(self.redis_client, self.key_format, key)
         self.assertEqual(data, retrieved_data)
 
     def test_put_data_key_length_check(self) -> None:
@@ -35,7 +35,7 @@ class RedisUtilsTest(ZulipTestCase):
         key = put_dict_in_redis(self.redis_client, self.key_format, data,
                                 expiration_seconds=self.expiration_seconds,
                                 token_length=max_valid_token_length)
-        retrieved_data = get_dict_from_redis(self.redis_client, key)
+        retrieved_data = get_dict_from_redis(self.redis_client, self.key_format, key)
         self.assertEqual(data, retrieved_data)
 
         # Trying to put data under an overly long key should get stopped before even
@@ -49,4 +49,8 @@ class RedisUtilsTest(ZulipTestCase):
 
     def test_get_data_key_length_check(self) -> None:
         with self.assertRaises(ZulipRedisKeyTooLongError):
-            get_dict_from_redis(self.redis_client, 'A' * (MAX_KEY_LENGTH + 1))
+            get_dict_from_redis(self.redis_client, key_format='{token}', key='A' * (MAX_KEY_LENGTH + 1))
+
+    def test_get_data_key_format_validation(self) -> None:
+        with self.assertRaises(ZulipRedisKeyOfWrongFormatError):
+            get_dict_from_redis(self.redis_client, self.key_format, 'nonmatching_format_1111')
