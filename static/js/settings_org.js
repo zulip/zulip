814309ae86da4bcb5268f8962fd03937dd8aaed7
Pragati Agrawal <pragati22066@gmail.com>
2020-01-26 21:25:09 +0530

    settings_org: Use actual value for user group edit policy dropdown options.
    This change is in series of de-duplication of code in "Other permission"
    section for various dropdowns.
    
    Here rather than using "by_admins_only" and "by_admins_only" for `value`
    attribute of options, we use actual numeric values. This helps in
    de-duplicating lot of code which is vulnerable to bugs.

--- static/js/settings_org.js
+++ static/js/settings_org.js
@@ -72,6 +72,19 @@ exports.create_stream_policy_values = {
 
 exports.invite_to_stream_policy_values = exports.create_stream_policy_values;
 
+exports.user_group_edit_policy_values = {
+    by_admins_only: {
+        order: 1,
+        code: 2,
+        description: i18n.t("Admins"),
+    },
+    by_members: {
+        order: 2,
+        code: 1,
+        description: i18n.t("Admins and members"),
+    },
+};
+
 exports.get_sorted_options_list = function (option_values_object) {
     const options_list = Object.keys(option_values_object).map((key) => {
         return _.extend(option_values_object[key], {key: key});
@@ -100,6 +113,8 @@ exports.get_organization_settings_options = () => {
         exports.create_stream_policy_values);
     options.invite_to_stream_policy_values = exports.get_sorted_options_list(
         exports.invite_to_stream_policy_values);
+    options.user_group_edit_policy_values = exports.get_sorted_options_list(
+        exports.user_group_edit_policy_values);
     return options;
 };
 
@@ -142,15 +157,6 @@ function get_property_value(property_name) {
             return "three_days";
         }
         return "custom_days";
-    }
-
-    if (property_name === 'realm_user_group_edit_policy') {
-        if (page_params.realm_user_group_edit_policy === 1) {
-            return "by_members";
-        }
-        if (page_params.realm_user_group_edit_policy === 2) {
-            return "by_admins_only";
-        }
     }
 
     if (property_name === 'realm_private_message_policy') {
@@ -845,7 +851,6 @@ exports.build_page = function () {
                 JSON.stringify(parseInt(new_message_retention_days, 10)) : null;
         } else if (subsection === 'other_permissions') {
             const waiting_period_threshold = $("#id_realm_waiting_period_setting").val();
-            const user_group_edit_policy = $("#id_realm_user_group_edit_policy").val();
             const private_message_policy = $("#id_realm_private_message_policy").val();
             const add_emoji_permission = $("#id_realm_add_emoji_by_admins_only").val();
 
@@ -853,12 +858,6 @@ exports.build_page = function () {
                 data.add_emoji_by_admins_only = true;
             } else if (add_emoji_permission === "by_anyone") {
                 data.add_emoji_by_admins_only = false;
-            }
-
-            if (user_group_edit_policy === "by_admins_only") {
-                data.user_group_edit_policy = 2;
-            } else if (user_group_edit_policy === "by_members") {
-                data.user_group_edit_policy = 1;
             }
 
             if (private_message_policy === "disabled") {
