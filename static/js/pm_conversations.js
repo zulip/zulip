37bcd8b122cb9397091a9ee1179b2fc80fc22ca9
Anders Kaseorg <anders@zulipchat.com>
2020-01-31 19:15:22 -0800

    pm_conversations: Replace partners Dict with Set.
    Signed-off-by: Anders Kaseorg <anders@zulipchat.com>

--- static/js/pm_conversations.js
+++ static/js/pm_conversations.js
@@ -1,14 +1,13 @@
-const Dict = require('./dict').Dict;
 const FoldDict = require('./fold_dict').FoldDict;
 
-const partners = new Dict();
+const partners = new Set();
 
 exports.set_partner = function (user_id) {
-    partners.set(user_id, true);
+    partners.add(user_id);
 };
 
 exports.is_partner = function (user_id) {
-    return partners.get(user_id) || false;
+    return partners.has(user_id);
 };
 
 exports.recent = (function () {
