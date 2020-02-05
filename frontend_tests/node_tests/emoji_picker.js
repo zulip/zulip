cb0578f56bc73ebe2e8fa0408705ddc3148602b6
Anders Kaseorg <anders@zulipchat.com>
2020-02-04 20:45:02 -0800

    dependencies: Upgrade emoji-datasource from 4.0.4 to 5.0.1.
    The “Smileys & People” category has been split into “Smilys & Emotion”
    and “People & Body”.
    
    Also, fix generate_sha1sum_emoji to read the emoji-datasource-google
    version from yarn.lock, since package.json only gives a version range.
    
    Signed-off-by: Anders Kaseorg <anders@zulipchat.com>

--- frontend_tests/node_tests/emoji_picker.js
+++ frontend_tests/node_tests/emoji_picker.js
@@ -7,7 +7,7 @@ run_test('initialize', () => {
     emoji_picker.initialize();
 
     const complete_emoji_catalog = _.sortBy(emoji_picker.complete_emoji_catalog, 'name');
-    assert.equal(complete_emoji_catalog.length, 9);
+    assert.equal(complete_emoji_catalog.length, 10);
     assert.equal(_.keys(emoji.emojis_by_name).length, 1037);
 
     function assert_emoji_category(ele, icon, num) {
@@ -24,13 +24,14 @@ run_test('initialize', () => {
             check_emojis(false);
         }
     }
-    assert_emoji_category(complete_emoji_catalog.pop(), 'fa-car', 177);
-    assert_emoji_category(complete_emoji_catalog.pop(), 'fa-hashtag', 181);
-    assert_emoji_category(complete_emoji_catalog.pop(), 'fa-smile-o', 260);
-    assert_emoji_category(complete_emoji_catalog.pop(), 'fa-thumbs-o-up', 6);
-    assert_emoji_category(complete_emoji_catalog.pop(), 'fa-lightbulb-o', 159);
-    assert_emoji_category(complete_emoji_catalog.pop(), 'fa-cutlery', 89);
+    assert_emoji_category(complete_emoji_catalog.pop(), 'fa-car', 170);
+    assert_emoji_category(complete_emoji_catalog.pop(), 'fa-hashtag', 180);
+    assert_emoji_category(complete_emoji_catalog.pop(), 'fa-smile-o', 129);
+    assert_emoji_category(complete_emoji_catalog.pop(), 'fa-star-o', 6);
+    assert_emoji_category(complete_emoji_catalog.pop(), 'fa-thumbs-o-up', 102);
+    assert_emoji_category(complete_emoji_catalog.pop(), 'fa-lightbulb-o', 191);
+    assert_emoji_category(complete_emoji_catalog.pop(), 'fa-cutlery', 92);
     assert_emoji_category(complete_emoji_catalog.pop(), 'fa-cog', 1);
-    assert_emoji_category(complete_emoji_catalog.pop(), 'fa-leaf', 107);
-    assert_emoji_category(complete_emoji_catalog.pop(), 'fa-soccer-ball-o', 58);
+    assert_emoji_category(complete_emoji_catalog.pop(), 'fa-leaf', 104);
+    assert_emoji_category(complete_emoji_catalog.pop(), 'fa-soccer-ball-o', 63);
 });
