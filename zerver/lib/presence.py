675f8514e872f59ec5754f385df101ee909f1b2b
Steve Howell <showell@zulipchat.com>
2020-02-06 16:41:55 +0000

    presence: Rename get_status_dict().
    We renamed this to get_presences_for_realm(),
    and we have the caller pass in realm, not
    user_profile.

--- zerver/lib/presence.py
+++ zerver/lib/presence.py
@@ -11,6 +11,7 @@ from zerver.lib.timestamp import datetime_to_timestamp
 from zerver.models import (
     query_for_ids,
     PushDeviceToken,
+    Realm,
     UserPresence,
     UserProfile,
 )
@@ -148,17 +149,18 @@ def get_status_dict_by_realm(realm_id: int, slim_presence: bool = False) -> Dict
 
     return get_status_dicts_for_rows(presence_rows, mobile_user_ids, slim_presence)
 
-def get_status_dict(requesting_user_profile: UserProfile,
-                    slim_presence: bool) -> Dict[str, Dict[str, Dict[str, Any]]]:
+def get_presences_for_realm(realm: Realm,
+                            slim_presence: bool) -> Dict[str, Dict[str, Dict[str, Any]]]:
 
-    if requesting_user_profile.realm.presence_disabled:
+    if realm.presence_disabled:
         # Return an empty dict if presence is disabled in this realm
         return defaultdict(dict)
 
-    return get_status_dict_by_realm(requesting_user_profile.realm_id, slim_presence)
+    return get_status_dict_by_realm(realm.id, slim_presence)
 
 def get_presence_response(requesting_user_profile: UserProfile,
                           slim_presence: bool) -> Dict[str, Any]:
+    realm = requesting_user_profile.realm
     server_timestamp = time.time()
-    presences = get_status_dict(requesting_user_profile, slim_presence)
+    presences = get_presences_for_realm(realm, slim_presence)
     return dict(presences=presences, server_timestamp=server_timestamp)
