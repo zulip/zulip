zulip/zulip

commit 612b237cec847fea1d48c42eaa98baa3744374b8

js: Convert remaining _.each(a, …) to a.forEach(…).
The _.each calls with an inline function expression have already been
converted to for…of loops.  We could do that here, but using .forEach
when we’re just reusing an existing function seems like a good
guideline.

Signed-off-by: Anders Kaseorg <anders@zulipchat.com>

static/js/settings_org.js
@@ -753,7 +753,7 @@ exports.build_page = function () {
     $('.organization').on('click', '.subsection-header .subsection-changes-discard .button', function (e) {
         e.preventDefault();
         e.stopPropagation();
-        _.each(get_subsection_property_elements(e.target), discard_property_element_changes);
+        get_subsection_property_elements(e.target).forEach(discard_property_element_changes);
         const save_btn_controls = $(e.target).closest('.save-button-controls');
         exports.change_save_button_state(save_btn_controls, "discarded");
     });

frontend_tests/node_tests/typeahead_helper.js
@@ -46,7 +46,7 @@ run_test('sort_streams', () => {
         {name: 'Denmark', pin_to_top: true, subscribers: popular, subscribed: true},
         {name: 'dead', pin_to_top: false, subscribers: unpopular, subscribed: true},
     ];
-    _.each(test_streams, stream_data.update_calculated_fields);
+    test_streams.forEach(stream_data.update_calculated_fields);
 
     global.stream_data.is_active = function (sub) {
         return sub.name !== 'dead';
@@ -67,7 +67,7 @@ run_test('sort_streams', () => {
         {name: 'Denmark', description: 'visiting Denmark', subscribers: popular, subscribed: true},
         {name: 'dead', description: 'dead stream', subscribers: unpopular, subscribed: true},
     ];
-    _.each(test_streams, stream_data.update_calculated_fields);
+    test_streams.forEach(stream_data.update_calculated_fields);
     test_streams = th.sort_streams(test_streams, 'wr');
     assert.deepEqual(test_streams[0].name, "Docs"); // Description match
     assert.deepEqual(test_streams[1].name, "Denmark"); // Popular stream
@@ -84,7 +84,7 @@ run_test('sort_streams', () => {
         {name: 'Ether', description: 'Destroying ether', subscribed: false, subscribers: popular},
         {name: 'Mew', description: 'Cat mews', subscribed: false, subscribers: popular},
     ];
-    _.each(test_streams, stream_data.update_calculated_fields);
+    test_streams.forEach(stream_data.update_calculated_fields);
 
     test_streams = th.sort_streams(test_streams, 'd');
     assert.deepEqual(test_streams[0].name, "Dev"); // Subscribed and stream name starts with query

static/js/message_fetch.js
@@ -25,7 +25,7 @@ function process_result(data, opts) {
         narrow.show_empty_narrow_message();
     }
 
-    _.each(messages, message_store.set_message_booleans);
+    messages.forEach(message_store.set_message_booleans);
     messages = messages.map(message_store.add_message_metadata);
 
     // In case any of the newly fetched messages are new, add them to

static/js/search_suggestion.js
@@ -599,7 +599,7 @@ function make_attacher(base) {
     };
 
     self.concat = function (suggestions) {
-        _.each(suggestions, self.push);
+        suggestions.forEach(self.push);
     };
 
     self.attach_many = function (suggestions) {

static/js/server_events.js
@@ -96,7 +96,7 @@ function get_events_success(events) {
         try {
             messages = echo.process_from_server(messages);
             if (messages.length > 0) {
-                _.each(messages, message_store.set_message_booleans);
+                messages.forEach(message_store.set_message_booleans);
                 let sent_by_this_client = false;
 
                 for (const msg of messages) {

static/js/stream_list.js
@@ -109,7 +109,7 @@ exports.build_stream_list = function () {
 
     parent.empty();
 
-    _.each(stream_groups.pinned_streams, add_sidebar_li);
+    stream_groups.pinned_streams.forEach(add_sidebar_li);
 
     const any_pinned_streams = stream_groups.pinned_streams.length > 0;
     const any_normal_streams = stream_groups.normal_streams.length > 0;
@@ -119,13 +119,13 @@ exports.build_stream_list = function () {
         elems.push('<hr class="stream-split">');
     }
 
-    _.each(stream_groups.normal_streams, add_sidebar_li);
+    stream_groups.normal_streams.forEach(add_sidebar_li);
 
     if (any_dormant_streams && any_normal_streams) {
         elems.push('<hr class="stream-split">');
     }
 
-    _.each(stream_groups.dormant_streams, add_sidebar_li);
+    stream_groups.dormant_streams.forEach(add_sidebar_li);
 
     parent.append(elems);
 };

commit bbc25ab88bca3652c7f8d8264d70241212e7da5a

org settings: Fix usage of forEach method on jQuery object.
Commit 612b237cec847fea1d48c42eaa98baa3744374b8 introduced a
regression that broke the “Discard” button, because
get_subsection_property_elements returns a jQuery object rather than
array, and jQuery objects don’t have a forEach method.  Change it to
return an array.

[anders@zulipchat.com: Use Array.from instead of .toArray to avoid the
need for extra mocking.]
Signed-off-by: Anders Kaseorg <anders@zulipchat.com>

static/js/settings_org.js
@@ -248,7 +248,7 @@ exports.extract_property_name = function (elem) {
 
 function get_subsection_property_elements(element) {
     const subsection = $(element).closest('.org-subsection-parent');
-    return subsection.find('.prop-element');
+    return Array.from(subsection.find('.prop-element'));
 }
 
 const simple_dropdown_properties = ['realm_create_stream_policy',