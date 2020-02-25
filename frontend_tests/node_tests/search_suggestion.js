e9125f679fbb90c37bcd98c9d9f193f880026291
Steve Howell <showell@zulipchat.com>
2020-02-25 11:46:14 +0000

    refactor: Extract settings_data.py.
    This extracts a new module with three
    functions, which we will test with 100%
    line coverage:
    
        - show_email
        - email_for_user_settings
        - get_time_preferences
    
    The first two break several dependencies
    in the codebase on `settings_org.js`.  The
    `get_time_preferences` breaks an annoying
    dependency on `page_params` within people.
    
    The module is pretty cohesive, in terms that
    all three functions are just light wrappers
    around `page_params` and/or `settings_config`.
    
    Now all the modules that want to call show_email()
    only have to require `settings_data`, instead of
    having a dependency on the much heavier
    `settings_org.js` module.
    
    I also make some of the unit tests here be more
    full-stack, where instead of stubbing show_email,
    I basically just toggle `page_params.is_admin`.

--- frontend_tests/node_tests/search_suggestion.js
+++ frontend_tests/node_tests/search_suggestion.js
@@ -7,6 +7,10 @@ set_global('message_store', {
 });
 
 
+const settings_config = zrequire('settings_config');
+page_params.realm_email_address_visibility =
+    settings_config.email_address_visibility_values.admins_only.code;
+
 zrequire('typeahead_helper');
 set_global('Handlebars', global.make_handlebars());
 zrequire('Filter', 'js/filter');
@@ -35,10 +39,8 @@ function init() {
 }
 init();
 
+page_params.is_admin = true;
 set_global('narrow', {});
-set_global('settings_org', {
-    show_email: () => true,
-});
 
 topic_data.reset();
 
@@ -1071,7 +1073,7 @@ run_test('people_suggestion (Admin only email visibility)', () => {
     only */
     people_suggestion_setup();
     const query = 'te';
-    settings_org.show_email = () => false;
+    page_params.is_admin = false;
     const suggestions = get_suggestions('', query);
     const expected = [
         "te",
