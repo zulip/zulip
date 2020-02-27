5e44bb20d35c03bfde06cb51724469b865ce56cb
Steve Howell <showell@zulipchat.com>
2020-02-27 13:13:35 +0000

    feature_flags: Remove propagate_topic_edits.
    This flag has been set to true since 2013.

--- static/js/feature_flags.js
+++ static/js/feature_flags.js
@@ -1,6 +1,5 @@
 // The features below have all settled into their final states and can
 // be removed when we get a chance
-exports.propagate_topic_edits = true;
 exports.clicking_notification_causes_narrow = true;
 exports.reminders_in_message_action_menu = false;
 
