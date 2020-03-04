b85bfe703202fc5b972b5180d568256e65e39708
Steve Howell <showell@zulipchat.com>
2020-03-04 13:40:30 +0000

    user settings: Inline call to validate_email.
    We are trying to elminate the version of
    `validate_email` that lives in `actions.py`.
    
    Inlining it barely increases the code size, and
    it removes some noise related the three-item
    tuple that `check_incoming_email` returns.

--- zerver/views/user_settings.py
+++ zerver/views/user_settings.py
@@ -1,5 +1,6 @@
 from typing import Optional, Any, Dict
 
+from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext as _
 from django.conf import settings
 from django.contrib.auth import authenticate, update_session_auth_hash
@@ -10,10 +11,12 @@ from zerver.decorator import has_request_variables, \
     REQ, human_users_only
 from zerver.lib.actions import do_change_password, do_change_notification_settings, \
     do_change_enter_sends, do_regenerate_api_key, do_change_avatar_fields, \
-    do_set_user_display_setting, validate_email, do_change_user_delivery_email, \
+    do_set_user_display_setting, do_change_user_delivery_email, \
     do_start_email_change_process, check_change_full_name, \
-    get_available_notification_sounds
+    get_available_notification_sounds, validate_email_is_valid, \
+    validate_email_not_already_in_realm
 from zerver.lib.avatar import avatar_url
+from zerver.lib.email_validation import get_realm_email_validator
 from zerver.lib.send_email import send_email, FromAddress
 from zerver.lib.i18n import get_available_language_codes
 from zerver.lib.response import json_success, json_error
@@ -107,11 +110,21 @@ def json_change_settings(request: HttpRequest, user_profile: UserProfile,
     if user_profile.delivery_email != new_email and new_email != '':
         if user_profile.realm.email_changes_disabled and not user_profile.is_realm_admin:
             return json_error(_("Email address changes are disabled in this organization."))
-        error, skipped, deactivated = validate_email(user_profile, new_email)
+
+        error = validate_email_is_valid(
+            new_email,
+            get_realm_email_validator(user_profile.realm),
+        )
         if error:
             return json_error(error)
-        if skipped:
-            return json_error(skipped)
+
+        try:
+            validate_email_not_already_in_realm(
+                user_profile.realm,
+                new_email,
+            )
+        except ValidationError as e:
+            return json_error(e.code)
 
         do_start_email_change_process(user_profile, new_email)
         result['account_email'] = _("Check your email for a confirmation link. ")
