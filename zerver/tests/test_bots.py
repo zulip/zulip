66422c25db3a7ec993d14a4e590668bd8f96aabe
Steve Howell <showell@zulipchat.com>
2020-03-19 16:13:09 +0000

    bot events: Prevent duplicate add-bot notifications.
    We don't need `do_create_user` to send a partial
    event here for bots.  The only caller to `do_create_user`
    that actually creates bots (apart from some tests that
    just need data setup) is `add_bot_backend`, which
    sends the more complete event including bot "extras"
    like service info.
    
    The modified event tests show the simplification
    here (2 events instead of 3).
    
    Also, the bot tests now use tuple unpacking, which
    will force a ValueError if we duplicate events
    again.

--- zerver/tests/test_bots.py
+++ zerver/tests/test_bots.py
@@ -160,7 +160,11 @@ class BotTest(ZulipTestCase, UploadSerializeMixin):
         email = 'hambot-bot@zulip.testserver'
         bot = self.get_bot_user(email)
 
-        event = [e for e in events if e['event']['type'] == 'realm_bot'][0]
+        (event,) = [
+            e for e in events
+            if e['event']['type'] == 'realm_bot'
+        ]
+
         self.assertEqual(
             dict(
                 type='realm_bot',
@@ -291,7 +295,10 @@ class BotTest(ZulipTestCase, UploadSerializeMixin):
         email = 'hambot-bot@zulip.testserver'
         bot = self.get_bot_user(email)
 
-        event = [e for e in events if e['event']['type'] == 'realm_bot'][0]
+        (event,) = [
+            e for e in events
+            if e['event']['type'] == 'realm_bot'
+        ]
         self.assertEqual(
             dict(
                 type='realm_bot',
@@ -390,7 +397,10 @@ class BotTest(ZulipTestCase, UploadSerializeMixin):
         assert(profile.default_sending_stream is not None)
         self.assertEqual(profile.default_sending_stream.name, 'Denmark')
 
-        event = [e for e in events if e['event']['type'] == 'realm_bot'][0]
+        (event,) = [
+            e for e in events
+            if e['event']['type'] == 'realm_bot'
+        ]
         self.assertEqual(
             dict(
                 type='realm_bot',
@@ -462,7 +472,10 @@ class BotTest(ZulipTestCase, UploadSerializeMixin):
         assert(bot_profile.default_events_register_stream is not None)
         self.assertEqual(bot_profile.default_events_register_stream.name, 'Denmark')
 
-        event = [e for e in events if e['event']['type'] == 'realm_bot'][0]
+        (event,) = [
+            e for e in events
+            if e['event']['type'] == 'realm_bot'
+        ]
         self.assertEqual(
             dict(
                 type='realm_bot',
