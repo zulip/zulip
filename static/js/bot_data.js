4b9b9164ba481e2dd60e31d1862fc81387793ce8
Steve Howell <showell@zulipchat.com>
2020-03-24 23:09:56 +0000

    cosmetic: Remove ugly bot_data__* names.

--- static/js/bot_data.js
+++ static/js/bot_data.js
@@ -10,7 +10,7 @@ const send_change_event = _.debounce(function () {
     settings_bots.render_bots();
 }, 50);
 
-const set_can_admin = function bot_data__set_can_admin(bot) {
+const set_can_admin = function (bot) {
     if (page_params.is_admin) {
         bot.can_admin = true;
     } else if (bot.owner !== undefined && people.is_current_user(bot.owner)) {
@@ -20,7 +20,7 @@ const set_can_admin = function bot_data__set_can_admin(bot) {
     }
 };
 
-exports.add = function bot_data__add(bot) {
+exports.add = function (bot) {
     const clean_bot = _.pick(bot, bot_fields);
     bots.set(bot.user_id, clean_bot);
     set_can_admin(clean_bot);
@@ -30,18 +30,18 @@ exports.add = function bot_data__add(bot) {
     send_change_event();
 };
 
-exports.deactivate = function bot_data__deactivate(bot_id) {
+exports.deactivate = function (bot_id) {
     bots.get(bot_id).is_active = false;
     send_change_event();
 };
 
-exports.del = function bot_data__del(bot_id) {
+exports.del = function (bot_id) {
     bots.delete(bot_id);
     services.delete(bot_id);
     send_change_event();
 };
 
-exports.update = function bot_data__update(bot_id, bot_update) {
+exports.update = function (bot_id, bot_update) {
     const bot = bots.get(bot_id);
     Object.assign(bot, _.pick(bot_update, bot_fields));
     set_can_admin(bot);
@@ -54,7 +54,7 @@ exports.update = function bot_data__update(bot_id, bot_update) {
     send_change_event();
 };
 
-exports.get_all_bots_for_current_user = function bots_data__get_editable() {
+exports.get_all_bots_for_current_user = function () {
     const ret = [];
     for (const bot of bots.values()) {
         if (people.is_current_user(bot.owner)) {
@@ -64,7 +64,7 @@ exports.get_all_bots_for_current_user = function bots_data__get_editable() {
     return ret;
 };
 
-exports.get_editable = function bots_data__get_editable() {
+exports.get_editable = function () {
     const ret = [];
     for (const bot of bots.values()) {
         if (bot.is_active && people.is_current_user(bot.owner)) {
@@ -74,7 +74,7 @@ exports.get_editable = function bots_data__get_editable() {
     return ret;
 };
 
-exports.get = function bot_data__get(bot_id) {
+exports.get = function (bot_id) {
     return bots.get(bot_id);
 };
 
@@ -82,7 +82,7 @@ exports.get_bot_owner_email = function (bot_id) {
     return bots.get(bot_id).owner;
 };
 
-exports.get_services = function bot_data__get_services(bot_id) {
+exports.get_services = function (bot_id) {
     return services.get(bot_id);
 };
 
