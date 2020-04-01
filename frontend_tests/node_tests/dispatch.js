e3a4797f40c9c07b1f0214b56e4bf7d90f07e1d9
sahil839 <sahilbatra839@gmail.com>
2020-04-01 12:24:56 +0530

    tests: Use test_realm_integer instead of test_realm_boolean for integer fields.
    Use test_realm_integer instead of test_realm_boolean for integer fields
    like realm_create_stream_policy, realm_invite_to_stream_policy,
    realm_invite_required in dispatch.js.
    
    Fixes #12284

--- frontend_tests/node_tests/dispatch.js
+++ frontend_tests/node_tests/dispatch.js
@@ -917,11 +917,29 @@ with_overrides(function (override) {
         assert.equal(page_params[parameter_name], true);
     }
 
+    function test_realm_integer(event, parameter_name) {
+        page_params[parameter_name] = 1;
+        event = {...event};
+        event.value = 2;
+        dispatch(event);
+        assert.equal(page_params[parameter_name], 2);
+
+        event = {...event};
+        event.value = 3;
+        dispatch(event);
+        assert.equal(page_params[parameter_name], 3);
+
+        event = {...event};
+        event.value = 1;
+        dispatch(event);
+        assert.equal(page_params[parameter_name], 1);
+    }
+
     let event = event_fixtures.realm__update__create_stream_policy;
-    test_realm_boolean(event, 'realm_create_stream_policy');
+    test_realm_integer(event, 'realm_create_stream_policy');
 
     event = event_fixtures.realm__update__invite_to_stream_policy;
-    test_realm_boolean(event, 'realm_invite_to_stream_policy');
+    test_realm_integer(event, 'realm_invite_to_stream_policy');
 
     event = event_fixtures.realm__update__invite_required;
     test_realm_boolean(event, 'realm_invite_required');
