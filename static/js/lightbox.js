9ffed7afdc807f0cdffaee064117a96eb50bd624
Hashir Sarwar <hashirsarwarch@gmail.com>
2020-04-02 13:52:33 +0000

    lightbox: Parse images' data while rendering `image-list`.
    Previously, lightbox.open() was responsible for retrieving
    the image data from the DOM, saving it in `asset_map` and
    finally displaying the image using that data. This
    implementation wasn't correct for image list at bottom of
    the lightbox because the `image` parameter passed to
    lightbox.open() could contain more than one instances of
    the image that had to be opened.
    
    Now, the metadata of all the images in image-list is stored
    in the `asset_map` while rendering the `image-list` inside
    `render_lightbox_list_images()` and `lightbox.open()` only
    looks for the metadata from `asset_map`.
    
    Fixes #14152.

--- static/js/lightbox.js
+++ static/js/lightbox.js
@@ -18,6 +18,12 @@ function render_lightbox_list_images(preview_source) {
             }).css({ backgroundImage: "url(" + src + ")"});
 
             $image_list.append(node);
+
+            // We parse the data for each image to show in the list,
+            // while we still have its original DOM element handy, so
+            // that navigating within the list only needs the `src`
+            // attribute used to construct the node object above.
+            exports.parse_image_data(img);
         }, "");
     }
 }
@@ -183,8 +189,15 @@ exports.show_from_selected_message = function () {
 };
 
 // retrieve the metadata from the DOM and store into the asset_map.
-exports.parse_image_data = function ($image) {
+exports.parse_image_data = function (image) {
+    const $image = $(image);
     const $preview_src = $image.attr("src");
+
+    if (asset_map.has($preview_src)) {
+        // check if image's data is already present in asset_map.
+        return;
+    }
+
     // if wrapped in the .youtube-video class, it will be length = 1, and therefore
     // cast to true.
     const is_youtube_video = !!$image.closest(".youtube-video").length;
