76e5229ac24e4c219af14595a7a204ffdd070687
Hashir Sarwar <hashirsarwarch@gmail.com>
2020-04-02 13:07:30 +0000

    lightbox: Use `$preview_src` as key for `asset_map`.
    In case of video embeds, the previous logic used
    `data-src-fullsize` or `src` as a key to look
    for the metadata of video in `lightbox.open()`,
    but while parsing, the key used while storing
    the metadata was the video ID.
    
    This doesn't make any sense because video's data
    could never be accessed from `asset_map` and we
    always needed to lookup the DOM for this.
    
    This commit fixes this by using $img.attr('src')
    as a key for `asset_map` for both, images and
    videos. Since `src` is the link of preview image
    in case of video embeds, it will always uniquely
    determine the video ID and we won't loose
    anything with the change in how videos handle
    things.
    
    Part of #14152.

--- static/js/lightbox.js
+++ static/js/lightbox.js
@@ -91,8 +91,8 @@ exports.open = function (image, options) {
 
     // if the asset_map already contains the metadata required to display the
     // asset, just recall that metadata.
-    const $image_source = $image.attr("data-src-fullsize") || $image.attr("src");
-    let payload = asset_map.get($image_source);
+    const $preview_src = $image.attr("src");
+    let payload = asset_map.get($preview_src);
     if (payload === undefined) {
         payload = exports.parse_image_data($image);
     }
@@ -168,6 +168,7 @@ exports.show_from_selected_message = function () {
 
 // retrieve the metadata from the DOM and store into the asset_map.
 exports.parse_image_data = function ($image) {
+    const $preview_src = $image.attr("src");
     // if wrapped in the .youtube-video class, it will be length = 1, and therefore
     // cast to true.
     const is_youtube_video = !!$image.closest(".youtube-video").length;
@@ -196,7 +197,7 @@ exports.parse_image_data = function ($image) {
         if ($image.attr("data-src-fullsize")) {
             $source = $image.attr("data-src-fullsize");
         } else {
-            $source = $image.attr("src");
+            $source = $preview_src;
         }
     }
     let sender_full_name;
@@ -216,12 +217,12 @@ exports.parse_image_data = function ($image) {
         user: sender_full_name,
         title: $parent.attr("title"),
         type: $type,
-        preview: $image.attr("src"),
+        preview: $preview_src,
         source: $source,
         url: $url,
     };
 
-    asset_map.set($source, payload);
+    asset_map.set($preview_src, payload);
     return payload;
 };
 
