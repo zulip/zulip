zulip/zulip

commit 39577b58ba4f5c10e91f8a4b6f2066f5c7387a96

stream settings: Display success/failure status for edits.
We have an alert for when the stream name is changed.
This also adds an alert when subscription settings
are updated and the widget is similar to that used in
the settings page.

This is also necessary because the stream specific
notification settings UI updation goes through this
path and it is necessary to display a confirmation
to match with other settings confirmation pattern.

static/js/stream_edit.js
@@ -337,9 +337,11 @@ exports.stream_setting_clicked = function (e) {
     }
 
     let checkbox = $(e.currentTarget).find('.sub_setting_control');
+    let status_element;
     // sub data is being changed from the notification settings page.
     if (checkbox.length === 0) {
         checkbox = $(e.currentTarget);
+        status_element = checkbox.closest('.subsection-parent').find('.alert-notification');
     }
     const sub = get_sub_for_target(e.target);
     const setting = checkbox.attr('name');
@@ -357,20 +359,23 @@ exports.stream_setting_clicked = function (e) {
             sub[setting] = page_params["enable_stream_" + setting];
         }
     }
-    exports.set_stream_property(sub, setting, !sub[setting]);
+    exports.set_stream_property(sub, setting, !sub[setting], status_element);
 };
 
-exports.bulk_set_stream_property = function (sub_data) {
-    return channel.post({
-        url: '/json/users/me/subscriptions/properties',
-        data: {subscription_data: JSON.stringify(sub_data)},
-        timeout: 10 * 1000,
-    });
+exports.bulk_set_stream_property = function (sub_data, status_element) {
+    const stream_id = sub_data[0].stream_id;
+    const url = '/json/users/me/subscriptions/properties';
+    const data = {subscription_data: JSON.stringify(sub_data)};
+    if (!status_element) {
+        status_element = "#stream_change_property_status" + stream_id;
+    }
+
+    settings_ui.do_settings_change(channel.post, url, data, status_element);
 };
 
-exports.set_stream_property = function (sub, property, value) {
+exports.set_stream_property = function (sub, property, value, status_element) {
     const sub_data = {stream_id: sub.stream_id, property: property, value: value};
-    exports.bulk_set_stream_property([sub_data]);
+    exports.bulk_set_stream_property([sub_data], status_element);
 };
 
 function change_stream_privacy(e) {

static/styles/subscriptions.scss
@@ -958,6 +958,16 @@ form#add_new_subscription {
             display: block;
         }
     }
+
+    .alert-notification:not(:empty) {
+        margin-top: -5px;
+        margin-left: unset;
+    }
+
+    .loading_indicator_text {
+        font-weight: 400;
+        line-height: 20px;
+    }
 }
 
 ul.grey-box {

static/templates/subscription_settings.hbs
@@ -13,6 +13,7 @@
                 <span class="checkmark" data-finish-editing=".stream-name-editable">✓</span>
                 {{/if}}
             </div>
+            <div id="stream_change_property_status{{stream_id}}" class="alert-notification"></div>
             <div class="button-group">
                 {{#if is_admin}}
                 <button class="button small rounded btn-danger deactivate" type="button" name="delete_button" title="{{t 'Delete stream'}}"> <i class="fa fa-trash-o" aria-hidden="true"></i></button>

commit 7cc4f38c787a6971a39686622af3ec897068a1f5

left sidebar: Fix exceptions when updating sub data.
If the subscription data was changed from the left sidebar,
we previously would attempt to display the savings indicator
in the stream edit page which wasn't rendered yet. The bug was
introduced in commit 39577b58ba4f5c10e91f8a4b6f2066f5c7387a96.

This approach is used to harden the codepath against bugs by
keeping the expectOne check in `settings_ui.do_settings_change`
function.

Fixes #14467.

static/js/stream_edit.js
@@ -326,14 +326,14 @@ exports.stream_setting_clicked = function (e) {
         return;
     }
 
+    const sub = get_sub_for_target(e.target);
     let checkbox = $(e.currentTarget).find('.sub_setting_control');
-    let status_element;
+    let status_element = "#stream_change_property_status" + sub.stream_id;
     // sub data is being changed from the notification settings page.
     if (checkbox.length === 0) {
         checkbox = $(e.currentTarget);
         status_element = checkbox.closest('.subsection-parent').find('.alert-notification');
     }
-    const sub = get_sub_for_target(e.target);
     const setting = checkbox.attr('name');
     if (!sub) {
         blueslip.error('undefined sub in stream_setting_clicked()');
@@ -353,11 +353,14 @@ exports.stream_setting_clicked = function (e) {
 };
 
 exports.bulk_set_stream_property = function (sub_data, status_element) {
-    const stream_id = sub_data[0].stream_id;
     const url = '/json/users/me/subscriptions/properties';
     const data = {subscription_data: JSON.stringify(sub_data)};
     if (!status_element) {
-        status_element = "#stream_change_property_status" + stream_id;
+        return channel.post({
+            url: url,
+            data: data,
+            timeout: 10 * 1000,
+        });
     }
 
     settings_ui.do_settings_change(channel.post, url, data, status_element);