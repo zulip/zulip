629f87b071a1312748cb6143414b4b24b0c9fb21
Steve Howell <showell@zulipchat.com>
2020-04-10 11:24:37 +0000

    casper: Use wait_for_text in custom profile test.
    This should avoid some test flakes.

--- frontend_tests/casper_tests/12-custom-profile.js
+++ frontend_tests/casper_tests/12-custom-profile.js
@@ -20,11 +20,11 @@ casper.then(function () {
 casper.then(function () {
     casper.waitUntilVisible('#admin-add-profile-field-status img', function () {
         casper.test.assertSelectorHasText('div#admin-add-profile-field-status', 'Saved');
-    });
-    casper.waitUntilVisible('.profile-field-row span.profile_field_name', function () {
-        casper.test.assertSelectorHasText('.profile-field-row span.profile_field_name', 'Teams');
-        casper.test.assertSelectorHasText('.profile-field-row span.profile_field_type', 'Short text');
-        casper.click('.profile-field-row button.open-edit-form');
+        common.wait_for_text('.profile-field-row span.profile_field_name', 'Teams', function () {
+            casper.test.assertSelectorHasText('.profile-field-row span.profile_field_name', 'Teams');
+            casper.test.assertSelectorHasText('.profile-field-row span.profile_field_type', 'Short text');
+            casper.click('.profile-field-row button.open-edit-form');
+        });
     });
 });
 
