052368bd3ea01df8a72b4d95a64e322fa1bdd937
Abhishek-Balaji <abhishekb10@gmail.com>
2020-04-15 16:04:26 +0530

    alert_words: Move alert_words from UserProfile to separate model.
    Previously, alert words were a JSON list of strings stored in a
    TextField on user_profile.  That hacky model reflected the fact that
    they were an early prototype feature.
    
    This commit migrates from that to a separate table, 'AlertWord'.  The
    new AlertWord has user_profile, word, id and realm(denormalization so
    we can provide a nice index for fetching all the alert words in a
    realm).
    
    This transition requires moving the logic for flushing the Alert Words
    caches to their own independent feature.
    
    Note that this commit should not be cherry-picked without the
    following commit, which fixes case-sensitivity issues with Alert Words.

---
+++ zerver/migrations/0277_migrate_alert_word.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from django.db import migrations
+from django.db.backends.postgresql_psycopg2.schema import DatabaseSchemaEditor
+from django.db.migrations.state import StateApps
+import ujson
+from typing import Dict, List
+
+def move_to_seperate_table(apps: StateApps, schema_editor: DatabaseSchemaEditor) -> None:
+    UserProfile = apps.get_model('zerver', 'UserProfile')
+    AlertWord = apps.get_model('zerver', 'AlertWord')
+
+    for user_profile in UserProfile.objects.all():
+
+        list_of_words = ujson.loads(user_profile.alert_words)
+        AlertWord.objects.bulk_create(
+            AlertWord(user_profile=user_profile, word=word, realm=user_profile.realm) for word in list_of_words)
+
+def move_back_to_user_profile(apps: StateApps, schema_editor: DatabaseSchemaEditor) -> None:
+    AlertWord = apps.get_model('zerver', 'AlertWord')
+    UserProfile = apps.get_model('zerver', 'UserProfile')
+
+    user_ids_and_words = AlertWord.objects.all().values("user_profile_id", "word")
+    user_ids_with_words = dict()  # type: Dict[int, List[str]]
+
+    for id_and_word in user_ids_and_words:
+        user_ids_with_words.setdefault(id_and_word["user_profile_id"], [])
+        user_ids_with_words[id_and_word["user_profile_id"]].append(id_and_word["word"])
+
+    for (user_id, words) in user_ids_with_words.items():
+        user_profile = UserProfile.objects.get(id=user_id)
+        user_profile.alert_words = ujson.dumps(words)
+        user_profile.save(update_fields=['alert_words'])
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('zerver', '0276_alertword'),
+    ]
+
+    operations = [
+        migrations.RunPython(move_to_seperate_table, move_back_to_user_profile)
+    ]
