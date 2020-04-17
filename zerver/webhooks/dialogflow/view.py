f1900dafaeaf26a1f174e266023d26d421887f75
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-04-17 17:20:18 +0530

    webhooks/dialogflow: Remove default value for email parameter.
    The webhook view used a default value for the email, which gave
    non-informative errors when the webhook is incorrectly configured without
    the email parameter.

--- zerver/webhooks/dialogflow/view.py
+++ zerver/webhooks/dialogflow/view.py
@@ -14,7 +14,7 @@ from zerver.models import UserProfile, get_user_profile_by_email
 @has_request_variables
 def api_dialogflow_webhook(request: HttpRequest, user_profile: UserProfile,
                            payload: Dict[str, Any]=REQ(argument_type='body'),
-                           email: str=REQ(default='foo')) -> HttpResponse:
+                           email: str=REQ()) -> HttpResponse:
     status = payload["status"]["code"]
 
     if status == 200:
