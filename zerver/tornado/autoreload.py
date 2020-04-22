cb3197d1d2927ed8195acf42b7d5b73b116f5d3e
Anders Kaseorg <anders@zulipchat.com>
2020-04-21 18:51:22 -0700

    mypy: Remove unnecessary type: ignore annotations.
    Signed-off-by: Anders Kaseorg <anders@zulipchat.com>

--- zerver/tornado/autoreload.py
+++ zerver/tornado/autoreload.py
@@ -76,7 +76,7 @@ _has_execv = sys.platform != 'win32'
 _watched_files = set()
 _reload_hooks = []
 _reload_attempted = False
-_io_loops = weakref.WeakKeyDictionary()  # type: ignore # upstream
+_io_loops = weakref.WeakKeyDictionary()
 needs_to_reload = False
 
 def start(io_loop=None, check_time=500):
