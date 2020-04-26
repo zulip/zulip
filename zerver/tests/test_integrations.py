88c297dffe6f8f833e1008343e2fccbb88084763
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-04-26 12:05:53 +0530

    tools: Allow specifying more options when capturing screenshot.
    The name and directory into which the screenshot should be captured can be
    specified now, apart from specifying the fixture file to use.

--- zerver/tests/test_integrations.py
+++ zerver/tests/test_integrations.py
@@ -1,5 +1,6 @@
 from zerver.lib.test_classes import ZulipTestCase
-from zerver.lib.integrations import split_fixture_path
+from zerver.lib.integrations import (
+    split_fixture_path, get_fixture_and_image_paths, INTEGRATIONS, ScreenshotConfig, WebhookIntegration)
 
 class IntegrationsTestCase(ZulipTestCase):
 
@@ -8,3 +9,11 @@ class IntegrationsTestCase(ZulipTestCase):
         integration_name, fixture_name = split_fixture_path(path)
         self.assertEqual(integration_name, 'semaphore')
         self.assertEqual(fixture_name, 'push')
+
+    def test_get_fixture_and_image_paths(self) -> None:
+        integration = INTEGRATIONS['airbrake']
+        assert isinstance(integration, WebhookIntegration)
+        screenshot_config = ScreenshotConfig('error_message.json', '002.png', 'ci')
+        fixture_path, image_path = get_fixture_and_image_paths(integration, screenshot_config)
+        self.assertEqual(fixture_path, 'zerver/webhooks/airbrake/fixtures/error_message.json')
+        self.assertEqual(image_path, 'static/images/integrations/ci/002.png')
