752fe58f4ff7d4aeaa2a256ab1567283006b0115
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-04-26 14:05:13 +0530

    tools: Allow specifying & capturing screenshots for all integrations.

--- zerver/tests/test_integrations.py
+++ zerver/tests/test_integrations.py
@@ -1,6 +1,7 @@
 from zerver.lib.test_classes import ZulipTestCase
 from zerver.lib.integrations import (
-    split_fixture_path, get_fixture_and_image_paths, INTEGRATIONS, ScreenshotConfig, WebhookIntegration)
+    split_fixture_path, get_fixture_and_image_paths, INTEGRATIONS, ScreenshotConfig, WebhookIntegration,
+    DOC_SCREENSHOT_CONFIG, WEBHOOK_INTEGRATIONS, NO_SCREENSHOT_WEBHOOKS)
 
 class IntegrationsTestCase(ZulipTestCase):
 
@@ -17,3 +18,13 @@ class IntegrationsTestCase(ZulipTestCase):
         fixture_path, image_path = get_fixture_and_image_paths(integration, screenshot_config)
         self.assertEqual(fixture_path, 'zerver/webhooks/airbrake/fixtures/error_message.json')
         self.assertEqual(image_path, 'static/images/integrations/ci/002.png')
+
+    def test_no_missing_doc_screenshot_config(self) -> None:
+        webhook_names = {webhook.name for webhook in WEBHOOK_INTEGRATIONS}
+        webhooks_with_screenshot_config = set(DOC_SCREENSHOT_CONFIG.keys())
+        missing_webhooks = (webhook_names - webhooks_with_screenshot_config - NO_SCREENSHOT_WEBHOOKS)
+        message = (
+            f"These webhooks are missing screenshot config: {missing_webhooks}.\n"
+            "Add them to zerver.lib.integrations.DOC_SCREENSHOT_CONFIG"
+        )
+        self.assertFalse(missing_webhooks, message)
