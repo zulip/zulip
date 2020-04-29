04811e724d16e5723119d14ebf37e9f428e2a8dd
Hemanth V. Alluri <hdrive1999@gmail.com>
2020-04-29 07:33:36 +0530

    webhooks/sentry: Rewrite the sentry webhook for the latest SDKs.
    Sentry has client SDKs for many programming languages and frameworks.
    Sentry has deprecated their old "Raven" series of client SDKs in favor
    of a new series of client SDKs following their unified API format.
    
    As it stood, our Sentry integration was already outdated being written
    for the version 5 payloads (the Raven SDKs stopped at version 6 which
    is already vastly different from version 5) when the current and
    prominently used version is version 7.
    
    This commit completely rewrites the existing Sentry integration.
    
    Tested and supported events:
    - Issue created, resolved, assigned, and ignored events.
    - "Sentry events" for "capture exception" and "capture message" with
    the Golang, Node.js, and Python SDKs (other SDKs should also work but
    only these were used for testing).
    
    For reference:
    - Old (Raven) SDK for python:
        https://github.com/getsentry/raven-python
    - New (Unified API format) SDK for python:
        https://github.com/getsentry/sentry-python
    
    Signed-off-by: Hemanth V. Alluri <hdrive1999@gmail.com>

--- zerver/webhooks/sentry/tests.py
+++ zerver/webhooks/sentry/tests.py
@@ -6,18 +6,153 @@ class SentryHookTests(WebhookTestCase):
     URL_TEMPLATE = "/api/v1/external/sentry?&api_key={api_key}&stream={stream}"
     FIXTURE_DIR_NAME = 'sentry'
 
-    def test_error_issue_message(self) -> None:
-        expected_topic = "zulip"
+    def test_event_for_exception_golang(self) -> None:
+        expected_topic = '*url.Error: Get "bad_url": unsupported protocol scheme ""'
         expected_message = """
-New [issue](https://sentry.io/zulip/zulip/issues/156699934/) (level: ERROR):
-
-``` quote
-This is an example python exception
+**New exception:** [*url.Error: Get "bad_url": unsupported protocol scheme ""](https://sentry.io/organizations/hypro999-personal-organization/issues/1637164584/events/80777a9cc30e4d0eb8904333d5c298b0/)
+```quote
+**level:** error
+**timestamp:** 2020-04-29 11:23:45
+**filename:** trigger-exception.go
 ```
-""".strip()
 
-        self.send_and_test_stream_message(
-            'exception_message',
-            expected_topic,
-            expected_message
-        )
+Traceback:
+```go
+         // Set the timeout to the maximum duration the program can afford to wait.
+         defer sentry.Flush(2 * time.Second)
+
+         resp, err := http.Get(os.Args[1])
+         if err != nil {
+--->         sentry.CaptureException(err)
+             log.Printf("reported to Sentry: %s", err)
+             return
+         }
+         defer resp.Body.Close()
+
+```"""
+        self.send_and_test_stream_message('event_for_exception_golang', expected_topic, expected_message)
+
+    def test_event_for_exception_node(self) -> None:
+        expected_topic = "Error: Sample error from node."
+        expected_message = """
+**New exception:** [Error: Sample error from node.](https://sentry.io/organizations/hypro999-personal-organization/issues/1638852747/events/f9cb0f2afff74a5aa92e766fb7ac3fe3/)
+```quote
+**level:** error
+**timestamp:** 2020-04-30 06:19:33
+**filename:** /home/hemanth/Desktop/sentry/trigger-exception.js
+```
+
+Traceback:
+```javascript
+     const Sentry = require('@sentry/node');
+
+     Sentry.init({
+       dsn: 'https://redacted.ingest.sentry.io/5216640',
+     });
+
+---> Sentry.withScope(function(scope) {
+       scope.addEventProcessor(function(event, hint) {
+         return event;
+       });
+       Sentry.captureException(new Error('Sample error from node.'));
+     });
+
+
+```"""
+        self.send_and_test_stream_message('event_for_exception_node', expected_topic, expected_message)
+
+    def test_event_for_exception_python(self) -> None:
+        expected_topic = "Exception: Custom exception!"
+        expected_message = """
+**New exception:** [Exception: Custom exception!](https://sentry.io/organizations/hypro999-personal-organization/issues/1635244907/events/599349254a1447a99774b5310711c1a8/)
+```quote
+**level:** error
+**timestamp:** 2020-04-28 13:56:05
+**filename:** trigger-exception.py
+```
+
+Traceback:
+```python3
+
+
+     if __name__ == "__main__":
+         sentry_sdk.init(dsn=SECRET_DSN)
+         try:
+--->         raise Exception("Custom exception!")
+         except Exception as e:
+             sentry_sdk.capture_exception(e)
+
+```"""
+        self.send_and_test_stream_message('event_for_exception_python', expected_topic, expected_message)
+
+    def test_event_for_message_golang(self) -> None:
+        expected_topic = "A test message event from golang."
+        expected_message = """
+**New message event:** [A test message event from golang.](https://sentry.io/organizations/hypro999-personal-organization/issues/1638844654/events/01ecb45633bc4f5ca940ada671124c8f/)
+```quote
+**level:** info
+**timestamp:** 2020-04-30 06:14:13
+```"""
+        self.send_and_test_stream_message('event_for_message_golang', expected_topic, expected_message)
+
+    def test_event_for_message_node(self) -> None:
+        expected_topic = "Test event from node."
+        expected_message = """
+**New message event:** [Test event from node.](https://sentry.io/organizations/hypro999-personal-organization/issues/1638840427/events/6886bb1fe7ce4497b7836f6083d5fd34/)
+```quote
+**level:** info
+**timestamp:** 2020-04-30 06:09:56
+```"""
+        self.send_and_test_stream_message('event_for_message_node', expected_topic, expected_message)
+
+    def test_event_for_message_python(self) -> None:
+        expected_topic = "A simple message-based issue."
+        expected_message = """
+**New message event:** [A simple message-based issue.](https://sentry.io/organizations/hypro999-personal-organization/issues/1635261062/events/8da63b42375e4d3b803c377fefb062f8/)
+```quote
+**level:** info
+**timestamp:** 2020-04-28 14:05:04
+```"""
+        self.send_and_test_stream_message('event_for_message_python', expected_topic, expected_message)
+
+    def test_issue_assigned_to_individual(self) -> None:
+        expected_topic = "A test message event from golang."
+        expected_message = """\nIssue **A test message event from golang.** has now been assigned to **Hemanth V. Alluri** by **Hemanth V. Alluri**."""
+        self.send_and_test_stream_message('issue_assigned_to_individual', expected_topic, expected_message)
+
+    def test_issue_assigned_to_team(self) -> None:
+        expected_topic = "Exception: program has entered an invalid state."
+        expected_message = """\nIssue **Exception: program has entered an invalid state.** has now been assigned to **team lone-wolf** by **Hemanth V. Alluri**."""
+        self.send_and_test_stream_message('issue_assigned_to_team', expected_topic, expected_message)
+
+    def test_issue_created_for_exception(self) -> None:
+        expected_topic = "Exception: Custom exception!"
+        expected_message = """
+**New issue created:** Exception: Custom exception!
+```quote
+**level:** error
+**timestamp:** 2020-04-28 13:56:05
+**assignee:** No one
+```"""
+        self.send_and_test_stream_message('issue_created_for_exception', expected_topic, expected_message)
+
+    def test_issue_created_for_message(self) -> None:
+        expected_topic = "A simple message-based issue."
+        expected_message = """
+**New issue created:** A simple message-based issue.
+```quote
+**level:** info
+**timestamp:** 2020-04-28 14:05:04
+**assignee:** No one
+```"""
+        self.send_and_test_stream_message('issue_created_for_message', expected_topic, expected_message)
+
+    def test_issue_ignored(self) -> None:
+        expected_topic = "Exception: program has entered an invalid state."
+        expected_message = """\nIssue **Exception: program has entered an invalid state.** was ignored by **Hemanth V. Alluri**."""
+        self.send_and_test_stream_message('issue_ignored', expected_topic, expected_message)
+
+    def test_issue_resolved(self) -> None:
+        expected_topic = "Exception: program has entered an invalid state."
+        expected_message = """\nIssue **Exception: program has entered an invalid state.** was marked as resolved by **Hemanth V. Alluri**."""
+        self.send_and_test_stream_message('issue_resolved', expected_topic, expected_message)
--- zerver/webhooks/sentry/view.py
+++ zerver/webhooks/sentry/view.py
@@ -1,32 +1,212 @@
-# Webhooks for external integrations.
-from typing import Any, Dict
+from typing import Any, Dict, List, Tuple, Optional
 
 from django.http import HttpRequest, HttpResponse
 
 from zerver.decorator import api_key_only_webhook_view
 from zerver.lib.request import REQ, has_request_variables
 from zerver.lib.response import json_success
-from zerver.lib.webhooks.common import check_send_webhook_message
+from zerver.lib.webhooks.common import UnexpectedWebhookEventType, check_send_webhook_message
 from zerver.models import UserProfile
 
-MESSAGE_TEMPLATE = """
-New [issue]({url}) (level: {level}):
 
-``` quote
-{message}
+MESSAGE_EVENT_TEMPLATE = """
+**New message event:** [{title}]({web_link})
+```quote
+**level:** {level}
+**timestamp:** {datetime}
 ```
-""".strip()
+"""
+
+EXCEPTION_EVENT_TEMPLATE = """
+**New exception:** [{title}]({web_link})
+```quote
+**level:** {level}
+**timestamp:** {datetime}
+**filename:** {filename}
+```
+"""
+
+EXCEPTION_EVENT_TEMPLATE_WITH_TRACEBACK = EXCEPTION_EVENT_TEMPLATE + """
+Traceback:
+```{platform}
+{pre_context}---> {context_line}{post_context}\
+```
+"""
+# Because of the \n added at the end of each context element,
+# this will actually look better in the traceback.
+
+ISSUE_CREATED_MESSAGE_TEMPLATE = """
+**New issue created:** {title}
+```quote
+**level:** {level}
+**timestamp:** {datetime}
+**assignee:** {assignee}
+```
+"""
+
+ISSUE_ASSIGNED_MESSAGE_TEMPLATE = """
+Issue **{title}** has now been assigned to **{assignee}** by **{actor}**.
+"""
+
+ISSUE_RESOLVED_MESSAGE_TEMPLATE = """
+Issue **{title}** was marked as resolved by **{actor}**.
+"""
+
+ISSUE_IGNORED_MESSAGE_TEMPLATE = """
+Issue **{title}** was ignored by **{actor}**.
+"""
+
+platforms_map = {
+    "go": "go",
+    "node": "javascript",
+    "python": "python3",
+}  # We can expand this as and when users use this integration with different platforms.
+
+
+def convert_lines_to_traceback_string(lines: Optional[List[str]]) -> str:
+    traceback = ""
+    if lines is not None:
+        for line in lines:
+            if (line == ""):
+                traceback += "\n"
+            else:
+                traceback += "     {}\n".format(line)
+    return traceback
+
+
+def handle_event_payload(event: Dict[str, Any]) -> Tuple[str, str]:
+    """ Handle either an exception type event or a message type event payload."""
+    subject = event["title"]
+
+    # We shouldn't support the officially deprecated Raven series of SDKs.
+    if int(event["version"]) < 7:
+        raise UnexpectedWebhookEventType("Sentry", "Raven SDK")
+
+    context = {
+        "title": subject,
+        "level": event["level"],
+        "web_link": event["web_url"],
+        "datetime": event["datetime"].split(".")[0].replace("T", " ")
+    }
+
+    if "exception" in event:
+        # The event was triggered by a sentry.capture_exception() call
+        # (in the Python Sentry SDK) or something similar.
+
+        filename = event["metadata"]["filename"]
+        platform = platforms_map[event["platform"]]
+
+        stacktrace = None
+        for value in event["exception"]["values"]:
+            if "stacktrace" in value:
+                stacktrace = value["stacktrace"]
+                break
+
+        if stacktrace:
+            exception_frame = None
+            for frame in stacktrace["frames"]:
+                if frame["filename"] == filename:
+                    exception_frame = frame
+                    break
+
+            if exception_frame:
+                pre_context = convert_lines_to_traceback_string(exception_frame["pre_context"])
+
+                context_line = exception_frame["context_line"] + "\n"
+                if not context_line:
+                    context_line = "\n"  # nocoverage
+
+                post_context = convert_lines_to_traceback_string(exception_frame["post_context"])
+
+                context.update({
+                    "platform": platform,
+                    "filename": filename,
+                    "pre_context": pre_context,
+                    "context_line": context_line,
+                    "post_context": post_context
+                })
+
+                body = EXCEPTION_EVENT_TEMPLATE_WITH_TRACEBACK.format(**context)
+                return (subject, body)
+
+        context.update({"filename": filename})  # nocoverage
+        body = EXCEPTION_EVENT_TEMPLATE.format(**context)  # nocoverage
+        return (subject, body)  # nocoverage
+
+    elif "logentry" in event:
+        # The event was triggered by a sentry.capture_message() call
+        # (in the Python Sentry SDK) or something similar.
+        body = MESSAGE_EVENT_TEMPLATE.format(**context)
+
+    else:
+        raise UnexpectedWebhookEventType("Sentry", "unknown-event type")
+
+    return (subject, body)
+
+
+def handle_issue_payload(action: str, issue: Dict[str, Any], actor: Dict[str, Any]) -> Tuple[str, str]:
+    """ Handle either an issue type event. """
+    subject = issue["title"]
+    datetime = issue["lastSeen"].split(".")[0].replace("T", " ")
+
+    if issue["assignedTo"]:
+        if issue["assignedTo"]["type"] == "team":
+            assignee = "team {}".format(issue["assignedTo"]["name"])
+        else:
+            assignee = issue["assignedTo"]["name"]
+    else:
+        assignee = "No one"
+
+    if action == "created":
+        context = {
+            "title": subject,
+            "level": issue["level"],
+            "datetime": datetime,
+            "assignee": assignee
+        }
+        body = ISSUE_CREATED_MESSAGE_TEMPLATE.format(**context)
+
+    elif action == "resolved":
+        context = {
+            "title": subject,
+            "actor": actor["name"]
+        }
+        body = ISSUE_RESOLVED_MESSAGE_TEMPLATE.format(**context)
+
+    elif action == "assigned":
+        context = {
+            "title": subject,
+            "assignee": assignee,
+            "actor": actor["name"]
+        }
+        body = ISSUE_ASSIGNED_MESSAGE_TEMPLATE.format(**context)
+
+    elif action == "ignored":
+        context = {
+            "title": subject,
+            "actor": actor["name"]
+        }
+        body = ISSUE_IGNORED_MESSAGE_TEMPLATE.format(**context)
+
+    else:
+        raise UnexpectedWebhookEventType("Sentry", "unknown-issue-action type")
+
+    return (subject, body)
+
 
 @api_key_only_webhook_view('Sentry')
 @has_request_variables
 def api_sentry_webhook(request: HttpRequest, user_profile: UserProfile,
-                       payload: Dict[str, Any] = REQ(argument_type='body')) -> HttpResponse:
-    subject = "{}".format(payload.get('project_name'))
-    body = MESSAGE_TEMPLATE.format(
-        level=payload['level'].upper(),
-        url=payload.get('url'),
-        message=payload.get('message')
-    )
+                       payload: Dict[str, Any] = REQ(argument_type="body")) -> HttpResponse:
+    data = payload["data"]
+
+    # We currently support two types of payloads: events and issues.
+    if "event" in data:
+        subject, body = handle_event_payload(data["event"])
+    elif "issue" in data:
+        subject, body = handle_issue_payload(payload["action"], data["issue"], payload["actor"])
+    else:
+        raise UnexpectedWebhookEventType("Sentry", str((list(data.keys()))))
 
     check_send_webhook_message(request, user_profile, subject, body)
     return json_success()
