5443b2f6351aad1ba37b93602a7a3db8ac8392a0
Aman Agrawal <f2016561@pilani.bits-pilani.ac.in>
2020-05-01 11:59:08 +0530

    recent_senders: Update data structures for stream/topic edits.
    * Remove old topic and reprocess both old and new topic to ensure
    that we are correctly storing the last_msg_id of users in the
    topic. Also, Handle topic's stream (& topic) edit updates.
    * Add function to get all messages in a topic in message_utils.js.
    * Send topic edit event to recent_senders.
    * Add func get sorted list of recent_senders to topic.
    The function will be useful to handle topic edits in Recent Topic UI.

--- static/js/recent_senders.js
+++ static/js/recent_senders.js
@@ -30,6 +30,45 @@ exports.process_message_for_senders = function (message) {
     stream_senders.set(stream_id, sender_message_ids);
 };
 
+exports.process_topic_edit = function (old_stream_id, old_topic, new_topic, new_stream_id) {
+    // When topic-editing occurs, we need to update the set of known
+    // senders in each stream/topic pair.  This is complicated by the
+    // fact that the event we receive from the server does not
+    // communicate which senders were present before-and-after; so our
+    // strategy is to just rebuild the data structure for the topic
+    // from message_store data.
+
+    // This removes the old topic_dict
+    const old_topic_dict = topic_senders.get(old_stream_id);
+    old_topic_dict.delete(old_topic);
+
+    // Re-processing every message in both the old and new topics is
+    // expensive.  It also potentially loses data, because
+    // `message_list.all()` only has contiguous message history, not
+    // the complete set of message IDs we've received to the
+    // `message_store` from the server (E.g. from when we narrowed to
+    // a stream).  But it's the most correct implementation we can
+    // sensibly do with existing data structures.
+    const old_topic_msgs = message_util.get_messages_in_topic(old_stream_id, old_topic);
+    for (const msg of old_topic_msgs) {
+        exports.process_message_for_senders(msg);
+    }
+
+    // use new_stream_id if topic was moved to a new stream,
+    // otherwise we just use old_stream_id, implying that
+    // just topic was renamed.
+    new_stream_id = new_stream_id || old_stream_id;
+    const new_topic_msgs = message_util.get_messages_in_topic(new_stream_id, new_topic);
+    for (const msg of new_topic_msgs) {
+        exports.process_message_for_senders(msg);
+    }
+
+    // Note that we don't delete anything from stream_senders here.
+    // Our view is that it's probably better to not do so; users who
+    // recently posted to a stream are relevant for typeahead even if
+    // the messages were moved to another stream or deleted.
+};
+
 exports.compare_by_recency = function (user_a, user_b, stream_id, topic) {
     let a_message_id;
     let b_message_id;
@@ -59,6 +98,27 @@ exports.compare_by_recency = function (user_a, user_b, stream_id, topic) {
     }
 
     return 0;
+};
+
+exports.get_topic_recent_senders = function (stream_id, topic) {
+    const topic_dict = topic_senders.get(stream_id);
+    if (topic_dict === undefined) {
+        return [];
+    }
+
+    const sender_message_ids = topic_dict.get(topic);
+    if (sender_message_ids === undefined) {
+        return [];
+    }
+
+    const sorted_senders = Array.from(sender_message_ids.entries()).sort(
+        (s1, s2) => { return s1[1] - s2[1]; }
+    );
+    const recent_senders = [];
+    for (const item of sorted_senders) {
+        recent_senders.push(item[0]);
+    }
+    return recent_senders;
 };
 
 window.recent_senders = exports;
