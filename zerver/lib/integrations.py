56c31a8647b7ea7fa56da1b5aac7a2d1ca441385
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-05-01 21:49:35 +0530

    tools: Allow specifying the bot name for notifications.

--- zerver/lib/integrations.py
+++ zerver/lib/integrations.py
@@ -199,12 +199,14 @@ def split_fixture_path(path: str) -> Tuple[str, str]:
 
 # FIXME: Change to namedtuple if we drop Python3.6: No default values support on namedtuples (or dataclass)
 class ScreenshotConfig:
-    def __init__(self, fixture_name: str, image_name: str='001.png', image_dir: Optional[str]=None,
+    def __init__(self, fixture_name: str, image_name: str='001.png',
+                 image_dir: Optional[str]=None, bot_name: Optional[str]=None,
                  payload_as_query_param: bool=False, payload_param_name: str='payload',
                  use_basic_auth: bool=False, custom_headers: Optional[Dict[str, str]]=None):
         self.fixture_name = fixture_name
         self.image_name = image_name
         self.image_dir = image_dir
+        self.bot_name = bot_name
         self.payload_as_query_param = payload_as_query_param
         self.payload_param_name = payload_param_name
         self.use_basic_auth = use_basic_auth
