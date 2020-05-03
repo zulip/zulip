11c640d2e3e0b7f9ee84e2d9686d30b91d296d8d
Anders Kaseorg <anders@zulip.com>
2020-05-03 16:15:36 -0700

    migrate: Improve do_batch_update escaping correctness with psycopg2.sql.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- zerver/lib/migrate.py
+++ zerver/lib/migrate.py
@@ -1,4 +1,5 @@
 from psycopg2.extensions import cursor
+from psycopg2.sql import Composable, Identifier, SQL
 from typing import List, TypeVar
 
 import time
@@ -8,21 +9,22 @@ CursorObj = TypeVar('CursorObj', bound=cursor)
 
 def do_batch_update(cursor: CursorObj,
                     table: str,
-                    cols: List[str],
-                    vals: List[str],
+                    assignments: List[Composable],
                     batch_size: int=10000,
-                    sleep: float=0.1,
-                    escape: bool=True) -> None:  # nocoverage
+                    sleep: float=0.1) -> None:  # nocoverage
     # The string substitution below is complicated by our need to
     # support multiple postgres versions.
-    stmt = '''
-        UPDATE %s
-        SET %s
-        WHERE id >= %%s AND id < %%s
-    ''' % (table, ', '.join(['%s = %%s' % (col) for col in cols]))
+    stmt = SQL('''
+        UPDATE {}
+        SET {}
+        WHERE id >= %s AND id < %s
+    ''').format(
+        Identifier(table),
+        SQL(', ').join(assignments),
+    )
 
-    cursor.execute("SELECT MIN(id), MAX(id) FROM %s" % (table,))
-    (min_id, max_id) = cursor.fetchall()[0]
+    cursor.execute(SQL("SELECT MIN(id), MAX(id) FROM {}").format(Identifier(table)))
+    (min_id, max_id) = cursor.fetchone()
     if min_id is None:
         return
 
@@ -31,18 +33,14 @@ def do_batch_update(cursor: CursorObj,
         lower = min_id
         upper = min_id + batch_size
         print('    Updating range [%s,%s)' % (lower, upper))
-        params = list(vals) + [lower, upper]
-        if escape:
-            cursor.execute(stmt, params=params)
-        else:
-            cursor.execute(stmt % tuple(params))
+        cursor.execute(stmt, [lower, upper])
 
         min_id = upper
         time.sleep(sleep)
 
         # Once we've finished, check if any new rows were inserted to the table
         if min_id > max_id:
-            cursor.execute("SELECT MAX(id) FROM %s" % (table,))
-            max_id = cursor.fetchall()[0][0]
+            cursor.execute(SQL("SELECT MAX(id) FROM {}").format(Identifier(table)))
+            (max_id,) = cursor.fetchone()
 
     print("    Finishing...", end='')
