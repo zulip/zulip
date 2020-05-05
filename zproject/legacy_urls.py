83ff27810a910dc7c1724dcfd59919d22a82a3c4
wowol <wbuczek07@gmail.com>
2020-05-05 13:42:02 +0200

    urls: Migrate legacy urls to use modern django pattern.

--- zproject/legacy_urls.py
+++ zproject/legacy_urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path
 import zerver.views
 import zerver.views.streams
 import zerver.views.auth
@@ -15,5 +15,5 @@ legacy_urls = [
     # for devs, and I don't think we need to go to the server
     # any more to find out about subscriptions, since they are already
     # pushed to us via the event system.
-    url(r'^json/subscriptions/exists$', zerver.views.streams.json_stream_exists),
+    path('json/subscriptions/exists', zerver.views.streams.json_stream_exists),
 ]
