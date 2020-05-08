708c6f4f11407ed491130996f295b318fd131410
Anders Kaseorg <anders@zulip.com>
2020-05-08 13:34:38 -0700

    puppet: Finally vanquish the cursed integer conversion conditional.
    We no longer support Puppet 3.
    
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- puppet/zulip/manifests/base.pp
+++ puppet/zulip/manifests/base.pp
@@ -88,18 +88,7 @@ class zulip::base {
     'user_presence',
   ]
 
-  # $::memorysize_mb is a string in Facter < 3.0 and a double in Facter ≥ 3.0.
-  # Either way, convert it to an integer in one of two ways, both of which are
-  # stupid, depending on the Puppet version.  (╯°□°）╯︵ ┻━┻
-  case $::puppetversion {
-    /^3\./: {
-      $total_memory_mb = 0 + inline_template('<%= @memorysize_mb.to_i %>')
-    }
-    default: {
-      $total_memory_mb_array = scanf("${::memorysize_mb} ", '%i')
-      $total_memory_mb = $total_memory_mb_array[0]
-    }
-  }
+  $total_memory_mb = Integer($::memorysize_mb);
 
   group { 'zulip':
     ensure     => present,
