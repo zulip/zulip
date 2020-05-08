e7db36139006a3b35519bc67b46aad355e1c7a8d
Anders Kaseorg <anders@zulip.com>
2020-05-07 21:37:58 -0700

    portico: Add setting to put Google Analytics on selected portico pages.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

---
+++ static/js/portico/google-analytics.js
@@ -0,0 +1,10 @@
+import { gtag, install } from "ga-gtag";
+
+export let config;
+
+if (page_params.google_analytics_id !== undefined) {
+    install(page_params.google_analytics_id);
+    config = (info) => gtag("config", page_params.google_analytics_id, info);
+} else {
+    config = () => {};
+}
