dbdf67301b23f5b586bbf0f06d1cab0d3b5ef9b9
Anders Kaseorg <anders@zulip.com>
2020-05-09 13:23:33 -0700

    memcached: Switch from pylibmc to python-binary-memcached.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -44,4 +44,4 @@ API_FEATURE_LEVEL = 27
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = '95.3'
+PROVISION_VERSION = '96.0'
