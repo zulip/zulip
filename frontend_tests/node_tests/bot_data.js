3ef9afde5b0fce50421b3b77a1864f8ca33c5372
Steve Howell <showell@zulip.com>
2020-05-10 18:47:06 +0000

    bot_data: Remove get_bot_owner_email.

--- frontend_tests/node_tests/bot_data.js
+++ frontend_tests/node_tests/bot_data.js
@@ -132,12 +132,4 @@ run_test('test_basics', () => {
         assert.equal(bots[0].email, 'bot1@zulip.com');
         assert.equal(bots[1].email, 'bot2@zulip.com');
     }());
-
-    (function test_get_bot_owner_email() {
-        let bot_owner_email = bot_data.get_bot_owner_email(test_embedded_bot.user_id);
-        assert.equal('cordelia@zulip.com', bot_owner_email);
-        bot_data.add(test_bot);
-        bot_owner_email = bot_data.get_bot_owner_email(test_bot.user_id);
-        assert.equal(undefined, bot_owner_email);
-    }());
 });
