83380b42969a4a42f3a90d3be00bf39e6d6f6e23
Anders Kaseorg <anders@zulip.com>
2020-05-12 20:55:22 -0700

    CVE-2020-12759: Fix reflected XSS vulnerability in Dropbox webhook.
    Also check the challenge argument’s presence before using it.
    
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- zerver/webhooks/dropbox/view.py
+++ zerver/webhooks/dropbox/view.py
@@ -1,4 +1,8 @@
+from typing import Optional
+
 from django.http import HttpRequest, HttpResponse
+
+from zerver.lib.request import REQ, RequestVariableMissingError
 from zerver.lib.response import json_success
 from zerver.lib.webhooks.common import check_send_webhook_message
 from zerver.decorator import has_request_variables, api_key_only_webhook_view
@@ -6,11 +10,17 @@ from zerver.models import UserProfile
 
 @api_key_only_webhook_view('Dropbox', notify_bot_owner_on_invalid_json=False)
 @has_request_variables
-def api_dropbox_webhook(request: HttpRequest, user_profile: UserProfile) -> HttpResponse:
+def api_dropbox_webhook(
+    request: HttpRequest,
+    user_profile: UserProfile,
+    challenge: Optional[str]=REQ(default=None),
+) -> HttpResponse:
     if request.method == 'POST':
         topic = 'Dropbox'
         check_send_webhook_message(request, user_profile, topic,
                                    "File has been updated on Dropbox!")
         return json_success()
     else:
-        return HttpResponse(request.GET['challenge'])
+        if challenge is None:
+            raise RequestVariableMissingError("challenge")
+        return HttpResponse(challenge, content_type="text/plain; charset=UTF-8")
