c573392721159cac842c7d68d2dda519f946a772
Mateusz Mandera <mateusz.mandera@zulip.com>
2020-05-20 18:42:35 +0200

    sharding: Replace ugly execs in puppet.
    We don't need these ugly gymnastics, as we already have access
    to the zulip_scripts_path variable in puppet exactly for this purpose.

--- puppet/zulip/manifests/sharding.pp
+++ puppet/zulip/manifests/sharding.pp
@@ -3,21 +3,10 @@ class zulip::sharding {
   include zulip::common
   include zulip::nginx
 
-  # These two execs below are rather ugly logic to have in puppet,
-  # but the advantage is that if the format of the config file generated
-  # by the sharding script changes, they will automatically get re-generated
-  # by puppet apply in the process of upgrading zulip, without requiring
-  # the administrator to think about this step.
+  $sharding_script = "${::zulip_scripts_path}/lib/sharding.py"
   exec { 'sharding_script':
     subscribe => File['/etc/zulip/zulip.conf'],
-    command   => '/home/zulip/deployments/current/scripts/lib/sharding.py',
-    onlyif    => 'test -f /home/zulip/deployments/current/scripts/lib/sharding.py\
-  -a ! -f /home/zulip/deployments/next/scripts/lib/sharding.py',
-  }
-  exec { 'sharding_script_next':
-    subscribe => File['/etc/zulip/zulip.conf'],
-    command   => '/home/zulip/deployments/next/scripts/lib/sharding.py',
-    onlyif    => 'test -f /home/zulip/deployments/next/scripts/lib/sharding.py',
+    command   => "bash -c '${sharding_script}'"
   }
   # The file entries below serve only to initialize the sharding config files
   # with the correct default content for the "only one shard" setup. For this
