69f630e46a6d7712ef652738e64cbcc0933af42f
Tim Abbott <tabbott@zulip.com>
2020-05-20 17:57:22 -0700

    portico: Fix a few issues with /for/research.

--- zerver/tests/test_docs.py
+++ zerver/tests/test_docs.py
@@ -134,6 +134,7 @@ class DocPageTest(ZulipTestCase):
         self._test('/hello/', 'Chat for distributed teams', landing_missing_strings=["Login"])
         self._test('/why-zulip/', 'Why Zulip?')
         self._test('/for/open-source/', 'for open source projects')
+        self._test('/for/research/', 'for researchers')
         self._test('/for/companies/', 'in a company')
         self._test('/for/working-groups-and-communities/', 'standards bodies')
         self._test('/for/mystery-hunt/', 'four SIPB alums')
