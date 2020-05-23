d10d8ca0d956c5fd3287c8772a64af1da2c49e2e
Aman Agrawal <amanagr@zulip.com>
2020-05-23 12:34:51 +0530

    recent_topics: Toggle topic display according to filters.

--- frontend_tests/node_tests/recent_topics.js
+++ frontend_tests/node_tests/recent_topics.js
@@ -1,6 +1,8 @@
 zrequire('message_util');
 
-set_global('$', global.make_zjquery());
+set_global('$', global.make_zjquery({
+    silent: true,
+}));
 set_global('hashchange', {
     exit_overlay: () => {},
 });
@@ -25,7 +27,13 @@ set_global('timerender', {
 });
 set_global('unread', {
     unread_topic_counter: {
-        get: () => { return 1; },
+        get: (stream_id, topic) => {
+            if (stream_id === 1 && topic === "topic-1") {
+                // Only stream1, topic-1 is read.
+                return 0;
+            }
+            return 1;
+        },
     },
 });
 set_global('hash_util', {
@@ -47,8 +55,8 @@ const stream1 = 1;
 const stream2 = 2;
 const stream3 = 3;
 
-// Topics in the stream
-const topic1 = "topic-1";  // No Other sender
+// Topics in the stream, all unread except topic1 & stream1.
+const topic1 = "topic-1";  // No Other sender & read.
 const topic2 = "topic-2";  // Other sender
 const topic3 = "topic-3";  // User not present
 const topic4 = "topic-4";  // User not present
@@ -268,7 +276,7 @@ run_test("test_recent_topics_launch", () => {
                 stream_id: 1,
                 stream: 'stream1',
                 topic: 'topic-1',
-                unread_count: 1,
+                unread_count: 0,
                 last_msg_time: 'Just now',
                 stream_url: 'https://www.example.com',
                 topic_url: 'https://www.example.com',
@@ -293,6 +301,274 @@ run_test("test_recent_topics_launch", () => {
 
     // incorrect topic_key
     assert.equal(rt.inplace_rerender('stream_unknown:topic_unknown'), false);
+});
+
+run_test('test_filter_all', () => {
+    // Just tests inplace rerender of a message
+    // in All topics filter.
+    let expected = {
+        count_senders: 0,
+        hidden: false,
+        last_msg_time: 'Just now',
+        senders: [1, 2],
+        stream: 'stream1',
+        stream_id: 1,
+        stream_url: 'https://www.example.com',
+        topic: 'topic-1',
+        topic_url: 'https://www.example.com',
+        unread_count: 0,
+    };
+
+    global.stub_templates(function (template_name, data) {
+        assert.equal(template_name, 'recent_topic_row');
+        assert.deepEqual(data, expected);
+        return '<recent_topics row stub>';
+    });
+
+    // topic is not muted
+    const rt = zrequire('recent_topics');
+    rt.process_messages([messages[0]]);
+
+    expected = {
+        stream_id: 1,
+        stream: 'stream1',
+        topic: 'topic-7',
+        unread_count: 1,
+        last_msg_time: 'Just now',
+        stream_url: 'https://www.example.com',
+        topic_url: 'https://www.example.com',
+        hidden: true,
+        senders: [1, 2],
+        count_senders: 0,
+    };
+
+    // topic is muted (=== hidden)
+    rt.process_messages([messages[9]]);
+});
+
+run_test('test_filter_unread', () => {
+    // Tests rerender of all topics when filter changes to "unread".
+    const expected =   {
+        recent_topics: [
+            {
+                count_senders: 0,
+                hidden: true,
+                last_msg_time: 'Just now',
+                senders: [1, 2],
+                stream: 'stream1',
+                stream_id: 1,
+                stream_url: 'https://www.example.com',
+                topic: 'topic-7',
+                topic_url: 'https://www.example.com',
+                unread_count: 1,
+            },
+            {
+                count_senders: 0,
+                hidden: false,
+                last_msg_time: 'Just now',
+                senders: [1, 2],
+                stream: 'stream1',
+                stream_id: 1,
+                stream_url: 'https://www.example.com',
+                topic: 'topic-6',
+                topic_url: 'https://www.example.com',
+                unread_count: 1,
+            },
+            {
+                count_senders: 0,
+                hidden: false,
+                last_msg_time: 'Just now',
+                senders: [1, 2],
+                stream: 'stream1',
+                stream_id: 1,
+                stream_url: 'https://www.example.com',
+                topic: 'topic-5',
+                topic_url: 'https://www.example.com',
+                unread_count: 1,
+            },
+            {
+                count_senders: 0,
+                hidden: false,
+                last_msg_time: 'Just now',
+                senders: [1, 2],
+                stream: 'stream1',
+                stream_id: 1,
+                stream_url: 'https://www.example.com',
+                topic: 'topic-4',
+                topic_url: 'https://www.example.com',
+                unread_count: 1,
+            },
+            {
+                count_senders: 0,
+                hidden: false,
+                last_msg_time: 'Just now',
+                senders: [1, 2],
+                stream: 'stream1',
+                stream_id: 1,
+                stream_url: 'https://www.example.com',
+                topic: 'topic-3',
+                topic_url: 'https://www.example.com',
+                unread_count: 1,
+            },
+            {
+                count_senders: 0,
+                hidden: false,
+                last_msg_time: 'Just now',
+                senders: [1, 2],
+                stream: 'stream1',
+                stream_id: 1,
+                stream_url: 'https://www.example.com',
+                topic: 'topic-2',
+                topic_url: 'https://www.example.com',
+                unread_count: 1,
+            },
+            {
+                count_senders: 0,
+                hidden: true,  // This is the only thing we are testing here
+                last_msg_time: 'Just now',
+                senders: [1, 2],
+                stream: 'stream1',
+                stream_id: 1,
+                stream_url: 'https://www.example.com',
+                topic: 'topic-1',
+                topic_url: 'https://www.example.com',
+                unread_count: 0,
+            },
+        ],
+    };
+
+    const rt = zrequire('recent_topics');
+
+    global.stub_templates(function () {
+        return '<recent_topics table stub>';
+    });
+    rt.process_messages(messages);
+
+    $('#recent_topics_filter_buttons').removeClass('btn-recent-selected');
+    global.stub_templates(function (template_name, data) {
+        assert.equal(template_name, 'recent_topics_table');
+        assert.deepEqual(data, expected);
+        return '<recent_topics table stub>';
+    });
+    rt.set_filter('unread');
+
+    // Unselect "unread" filter by clicking twice.
+    expected.recent_topics[6].hidden = false;
+    rt.set_filter('unread');
+
+    // Now clicking "all" filter should have no change to expected data.
+    rt.set_filter('all');
+});
+
+run_test('test_filter_participated', () => {
+    // Tests rerender of all topics when filter changes to "unread".
+    const expected =   {
+        recent_topics: [
+            {
+                count_senders: 0,
+                hidden: true,
+                last_msg_time: 'Just now',
+                senders: [1, 2],
+                stream: 'stream1',
+                stream_id: 1,
+                stream_url: 'https://www.example.com',
+                topic: 'topic-7',
+                topic_url: 'https://www.example.com',
+                unread_count: 1,
+            },
+            {
+                count_senders: 0,
+                hidden: false,
+                last_msg_time: 'Just now',
+                senders: [1, 2],
+                stream: 'stream1',
+                stream_id: 1,
+                stream_url: 'https://www.example.com',
+                topic: 'topic-6',
+                topic_url: 'https://www.example.com',
+                unread_count: 1,
+            },
+            {
+                count_senders: 0,
+                hidden: false,
+                last_msg_time: 'Just now',
+                senders: [1, 2],
+                stream: 'stream1',
+                stream_id: 1,
+                stream_url: 'https://www.example.com',
+                topic: 'topic-5',
+                topic_url: 'https://www.example.com',
+                unread_count: 1,
+            },
+            {
+                count_senders: 0,
+                hidden: true,
+                last_msg_time: 'Just now',
+                senders: [1, 2],
+                stream: 'stream1',
+                stream_id: 1,
+                stream_url: 'https://www.example.com',
+                topic: 'topic-4',
+                topic_url: 'https://www.example.com',
+                unread_count: 1,
+            },
+            {
+                count_senders: 0,
+                hidden: true,
+                last_msg_time: 'Just now',
+                senders: [1, 2],
+                stream: 'stream1',
+                stream_id: 1,
+                stream_url: 'https://www.example.com',
+                topic: 'topic-3',
+                topic_url: 'https://www.example.com',
+                unread_count: 1,
+            },
+            {
+                count_senders: 0,
+                hidden: false,
+                last_msg_time: 'Just now',
+                senders: [1, 2],
+                stream: 'stream1',
+                stream_id: 1,
+                stream_url: 'https://www.example.com',
+                topic: 'topic-2',
+                topic_url: 'https://www.example.com',
+                unread_count: 1,
+            },
+            {
+                count_senders: 0,
+                hidden: false,
+                last_msg_time: 'Just now',
+                senders: [1, 2],
+                stream: 'stream1',
+                stream_id: 1,
+                stream_url: 'https://www.example.com',
+                topic: 'topic-1',
+                topic_url: 'https://www.example.com',
+                unread_count: 0,
+            },
+        ],
+    };
+
+    const rt = zrequire('recent_topics');
+
+    global.stub_templates(function () {
+        return '<recent_topics table stub>';
+    });
+    rt.process_messages(messages);
+
+    $('#recent_topics_filter_buttons').removeClass('btn-recent-selected');
+    global.stub_templates(function (template_name, data) {
+        assert.equal(template_name, 'recent_topics_table');
+        assert.deepEqual(data, expected);
+        return '<recent_topics table stub>';
+    });
+    rt.set_filter('participated');
+
+    expected.recent_topics[3].hidden = false;
+    expected.recent_topics[4].hidden = false;
+    rt.set_filter('all');
 });
 
 // template rendering is tested in test_recent_topics_launch.
