89a48bcc1f895a883ba98c843732bbe00f1230bc
Priyank Patel <priyankp390@gmail.com>
2020-06-02 18:14:57 +0000

    js: Convert util.js to a es6 module.
    This was done automatically using a recast/jscodeshift script, followed
    by a tools/lint --only eslint,tsc --fix to convert let to const and to
    remove unnesscary semi-colons where needed.
    
    No updates to test is required because we don't mock any of util's
    internals in tests. And this module already purged from window which
    made this transition easy.

--- static/js/util.js
+++ static/js/util.js
@@ -1,7 +1,7 @@
 // From MDN: https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Math/random
-exports.random_int = function random_int(min, max) {
+export function random_int(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+}
 
 // Like C++'s std::lower_bound.  Returns the first index at which
 // `value` could be inserted without changing the ordering.  Assumes
@@ -14,7 +14,7 @@ exports.random_int = function random_int(min, max) {
 //
 // Usage: lower_bound(array, value, [less])
 //        lower_bound(array, first, last, value, [less])
-exports.lower_bound = function (array, arg1, arg2, arg3, arg4) {
+export function lower_bound(array, arg1, arg2, arg3, arg4) {
     let first;
     let last;
     let value;
@@ -62,28 +62,28 @@ exports.lower_bound = function (array, arg1, arg2, arg3, arg4) {
         }
     }
     return first;
-};
+}
 
 function lower_same(a, b) {
     return a.toLowerCase() === b.toLowerCase();
 }
 
-exports.same_stream_and_topic = function util_same_stream_and_topic(a, b) {
+export function same_stream_and_topic(a, b) {
     // Streams and topics are case-insensitive.
     return a.stream_id === b.stream_id &&
         lower_same(a.topic, b.topic);
-};
+}
 
-exports.is_pm_recipient = function (user_id, message) {
+export function is_pm_recipient(user_id, message) {
     const recipients = message.to_user_ids.split(',');
     return recipients.includes(user_id.toString());
-};
+}
 
-exports.extract_pm_recipients = function (recipients) {
+export function extract_pm_recipients(recipients) {
     return recipients.split(/\s*[,;]\s*/).filter((recipient) => recipient.trim() !== "");
-};
+}
 
-exports.same_recipient = function util_same_recipient(a, b) {
+export function same_recipient(a, b) {
     if (a === undefined || b === undefined) {
         return false;
     }
@@ -98,19 +98,19 @@ exports.same_recipient = function util_same_recipient(a, b) {
         }
         return a.to_user_ids === b.to_user_ids;
     case 'stream':
-        return exports.same_stream_and_topic(a, b);
+        return same_stream_and_topic(a, b);
     }
 
     // should never get here
     return false;
-};
+}
 
-exports.same_sender = function util_same_sender(a, b) {
+export function same_sender(a, b) {
     return a !== undefined && b !== undefined &&
             a.sender_email.toLowerCase() === b.sender_email.toLowerCase();
-};
+}
 
-exports.normalize_recipients = function (recipients) {
+export function normalize_recipients(recipients) {
     // Converts a string listing emails of message recipients
     // into a canonical formatting: emails sorted ASCIIbetically
     // with exactly one comma and no spaces between each.
@@ -119,13 +119,13 @@ exports.normalize_recipients = function (recipients) {
     recipients = recipients.filter((s) => s.length > 0);
     recipients.sort();
     return recipients.join(',');
-};
+}
 
 // Avoid URI decode errors by removing characters from the end
 // one by one until the decode succeeds.  This makes sense if
 // we are decoding input that the user is in the middle of
 // typing.
-exports.robust_uri_decode = function (str) {
+export function robust_uri_decode(str) {
     let end = str.length;
     while (end > 0) {
         try {
@@ -138,13 +138,13 @@ exports.robust_uri_decode = function (str) {
         }
     }
     return '';
-};
+}
 
 // If we can, use a locale-aware sorter.  However, if the browser
 // doesn't support the ECMAScript Internationalization API
 // Specification, do a dumb string comparison because
 // String.localeCompare is really slow.
-exports.make_strcmp = function () {
+export function make_strcmp() {
     try {
         const collator = new Intl.Collator();
         return collator.compare;
@@ -155,16 +155,17 @@ exports.make_strcmp = function () {
     return function util_strcmp(a, b) {
         return a < b ? -1 : a > b ? 1 : 0;
     };
-};
-exports.strcmp = exports.make_strcmp();
+}
 
-exports.escape_regexp = function (string) {
+export const strcmp = make_strcmp();
+
+export function escape_regexp(string) {
     // code from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
     // Modified to escape the ^ to appease jslint. :/
     return string.replace(/([.*+?\^=!:${}()|\[\]\/\\])/g, "\\$1");
-};
+}
 
-exports.array_compare = function util_array_compare(a, b) {
+export function array_compare(a, b) {
     if (a.length !== b.length) {
         return false;
     }
@@ -175,7 +176,7 @@ exports.array_compare = function util_array_compare(a, b) {
         }
     }
     return true;
-};
+}
 
 /* Represents a value that is expensive to compute and should be
  * computed on demand and then cached.  The value can be forcefully
@@ -209,9 +210,9 @@ exports.find_wildcard_mentions = function (message_content) {
         return null;
     }
     return mention[3];
-};
+}
 
-exports.move_array_elements_to_front = function util_move_array_elements_to_front(array, selected) {
+export function move_array_elements_to_front(array, selected) {
     const selected_hash = new Set(selected);
     const selected_elements = [];
     const unselected_elements = [];
@@ -219,19 +220,19 @@ exports.move_array_elements_to_front = function util_move_array_elements_to_fron
         (selected_hash.has(element) ? selected_elements : unselected_elements).push(element);
     }
     return [...selected_elements, ...unselected_elements];
-};
+}
 
 // check by the userAgent string if a user's client is likely mobile.
-exports.is_mobile = function () {
+export function is_mobile() {
     const regex = "Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini";
     return new RegExp(regex, "i").test(window.navigator.userAgent);
-};
+}
 
 function to_int(s) {
     return parseInt(s, 10);
 }
 
-exports.sorted_ids = function (ids) {
+export function sorted_ids(ids) {
     // This mapping makes sure we are using ints, and
     // it also makes sure we don't mutate the list.
     let id_list = ids.map(to_int);
@@ -239,30 +240,30 @@ exports.sorted_ids = function (ids) {
     id_list = _.uniq(id_list, true);
 
     return id_list;
-};
+}
 
-exports.set_match_data = function (target, source) {
+export function set_match_data(target, source) {
     target.match_subject = source.match_subject;
     target.match_content = source.match_content;
-};
+}
 
-exports.get_match_topic = function (obj) {
+export function get_match_topic(obj) {
     return obj.match_subject;
-};
+}
 
-exports.get_draft_topic = function (obj) {
+export function get_draft_topic(obj) {
     // We will need to support subject for old drafts.
     return obj.topic || obj.subject || '';
-};
+}
 
-exports.get_reload_topic = function (obj) {
+export function get_reload_topic(obj) {
     // When we first upgrade to releases that have
     // topic=foo in the code, the user's reload URL
     // may still have subject=foo from the prior version.
     return obj.topic || obj.subject || '';
-};
+}
 
-exports.get_edit_event_topic = function (obj) {
+export function get_edit_event_topic(obj) {
     if (obj.topic === undefined) {
         return obj.subject;
     }
@@ -270,27 +271,27 @@ exports.get_edit_event_topic = function (obj) {
     // This code won't be reachable till we fix the
     // server, but we use it now in tests.
     return obj.topic;
-};
+}
 
-exports.get_edit_event_orig_topic = function (obj) {
+export function get_edit_event_orig_topic(obj) {
     return obj.orig_subject;
-};
+}
 
-exports.get_edit_event_prev_topic = function (obj) {
+export function get_edit_event_prev_topic(obj) {
     return obj.prev_subject;
-};
+}
 
-exports.is_topic_synonym = function (operator) {
+export function is_topic_synonym(operator) {
     return operator === 'subject';
-};
+}
 
-exports.convert_message_topic = function (message) {
+export function convert_message_topic(message) {
     if (message.topic === undefined) {
         message.topic = message.subject;
     }
-};
+}
 
-exports.clean_user_content_links = function (html) {
+export function clean_user_content_links(html) {
     const content = new DOMParser().parseFromString(html, "text/html").body;
     for (const elt of content.getElementsByTagName("a")) {
         // Ensure that all external links have target="_blank"
@@ -341,4 +342,4 @@ exports.clean_user_content_links = function (html) {
         elt.setAttribute("title", ["", legacy_title].includes(elt.title) ? title : `${title}\n${elt.title}`);
     }
     return content.innerHTML;
-};
+}
