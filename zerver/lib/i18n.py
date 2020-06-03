10805abd6e1da87861f48e8ade6e4974fb653d46
Anders Kaseorg <anders@zulip.com>
2020-06-02 21:02:53 -0700

    python: Fix errors from pycodestyle 2.6.0.
    zerver/lib/i18n.py:34:28: E741 ambiguous variable name 'l'
    zerver/lib/webhooks/common.py:103:34: E225 missing whitespace around operator
    zerver/tests/test_queue_worker.py:563:9: E306 expected 1 blank line before a nested definition, found 0
    
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- zerver/lib/i18n.py
+++ zerver/lib/i18n.py
@@ -31,9 +31,9 @@ def get_language_list() -> List[Dict[str, Any]]:
         return languages['name_map']
 
 def get_language_list_for_templates(default_language: str) -> List[Dict[str, Dict[str, str]]]:
-    language_list = [l for l in get_language_list()
-                     if 'percent_translated' not in l or
-                        l['percent_translated'] >= 5.]
+    language_list = [lang for lang in get_language_list()
+                     if 'percent_translated' not in lang or
+                        lang['percent_translated'] >= 5.]
 
     formatted_list = []
     lang_len = len(language_list)
