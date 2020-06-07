b0a0ae215f45e09acb80e749ccbc652356aeeb40
Sharif Naas <sharifnaas@sharifnaas.com>
2020-06-06 19:50:31 -0700

    js: Extract message_edit_history.js.

--- static/js/bundles/app.js
+++ static/js/bundles/app.js
@@ -98,6 +98,7 @@ import "../stream_create.js";
 import "../stream_edit.js";
 import "../subs.js";
 import "../message_edit.js";
+import "../message_edit_history.js";
 import "../condense.js";
 import "../resize.js";
 import "../list_render.js";
--- static/js/global.d.ts
+++ static/js/global.d.ts
@@ -68,6 +68,7 @@ declare let local_message: any;
 declare let localstorage: any;
 declare let markdown: any;
 declare let message_edit: any;
+declare let message_edit_history: any;
 declare let message_events: any;
 declare let message_fetch: any;
 declare let message_flags: any;
