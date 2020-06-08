4b19aca135f77b449909cb82ff9fc0ec4bb51a02
jagansivam28 <jagansivam71@gmail.com>
2020-06-09 04:47:56 +0530

    settings UI : Change user avatar image implementation.
    Change user avatar image implementation to match
    realm icon image implementation since similar implementation
    between similar widgets may help in future deduplication.

--- static/js/user_events.js
+++ static/js/user_events.js
@@ -71,7 +71,7 @@ exports.update_person = function update(person) {
             page_params.avatar_source = person.avatar_source;
             page_params.avatar_url = url;
             page_params.avatar_url_medium = person.avatar_url_medium;
-            $("#user-avatar-block").attr("style", "background-image:url(" + person.avatar_url_medium + ")");
+            $("#user-avatar-block").attr("src", person.avatar_url_medium);
         }
 
         message_live_update.update_avatar(person_obj.user_id, person.avatar_url);
