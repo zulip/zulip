711f2a6b512cae0bda330cc54a3b712f54502b4a
Alex Vandiver <alexmv@zulip.com>
2020-06-11 17:26:48 -0700

    puppet: Remove unnecessary nagios authentication setup.
    Since the nagios authentication is stored _in the database_, it is
    unnecessary to run if the database is simply a replica of the
    production database.  The only case in which this statement would have
    an effect is if the postgres node contains a _different_ (or empty)
    database, which `setup_disks` now effectively prevents.
    
    Remove the unnecessary step.

--- puppet/zulip_ops/manifests/postgres_appdb.pp
+++ puppet/zulip_ops/manifests/postgres_appdb.pp
@@ -10,14 +10,4 @@ class zulip_ops::postgres_appdb {
     mode    => '0640',
     source  => 'puppet:///modules/zulip_ops/postgresql/pg_hba.conf',
   }
-
-  file { "/usr/share/postgresql/${zulip::base::postgres_version}/zulip_nagios_setup.sql":
-    ensure  => file,
-    require => Package["postgresql-${zulip::base::postgres_version}"],
-    owner   => 'postgres',
-    group   => 'postgres',
-    mode    => '0640',
-    source  => 'puppet:///modules/zulip_ops/postgresql/zulip_nagios_setup.sql',
-  }
-
 }
