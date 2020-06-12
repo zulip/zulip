4284631e95d6f1fcc1f3e8edf741f1e9803d16da
Steve Howell <showell@zulip.com>
2020-06-12 16:41:23 +0000

    dead code: Remove integration_bot_widget code.
    This was work in progress from summer 2017
    that never got completed.
    
    See 534aa0b105529a71a2a8061e2fabf0647402ccbc

--- static/js/global.d.ts
+++ static/js/global.d.ts
@@ -56,7 +56,6 @@ declare let hotspots: any;
 declare let i18n: any;
 declare let info_overlay: any;
 declare let input_pill: any;
-declare let integration_bot_widget: any;
 declare let invite: any;
 declare let keydown_util: any;
 declare let lightbox: any;
