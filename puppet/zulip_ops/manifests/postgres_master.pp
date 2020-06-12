0774f54c1bac62b801df0fc3da17c78db209529c
Alex Vandiver <alexmv@zulip.com>
2020-06-11 17:26:32 -0700

    puppet: Move to `setup_disks` to postgres_common.
    The tooling should now be run no matter if the node is a primary or
    replica.

--- puppet/zulip_ops/manifests/postgres_master.pp
+++ puppet/zulip_ops/manifests/postgres_master.pp
@@ -2,31 +2,12 @@ class zulip_ops::postgres_master {
   include zulip_ops::base
   include zulip_ops::postgres_appdb
 
-  $master_packages = [# Packages needed for disk + RAID configuration
-                      'xfsprogs',
-                      ]
-  package { $master_packages: ensure => 'installed' }
-
   file { '/etc/sysctl.d/40-postgresql.conf':
     ensure => file,
     owner  => 'root',
     group  => 'root',
     mode   => '0644',
     source => 'puppet:///modules/zulip_ops/postgresql/40-postgresql.conf.master',
-  }
-
-  file { '/root/setup_disks.sh':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0744',
-    source => 'puppet:///modules/zulip_ops/postgresql/setup_disks.sh',
-  }
-
-  exec { 'setup_disks':
-    command => '/root/setup_disks.sh',
-    require => Package["postgresql-${zulip::base::postgres_version}", 'xfsprogs'],
-    unless  => 'test $(readlink /var/lib/postgresql) = "/srv/postgresql/" -a -d /srv/postgresql',
   }
 
   # This one will probably fail most of the time
