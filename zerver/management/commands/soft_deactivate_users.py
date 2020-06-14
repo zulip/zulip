f21ae922f3f613ca057906f8e97ec78a22369843
Anders Kaseorg <anders@zulip.com>
2020-06-13 19:27:02 -0700

    logging: Pass more format arguments to logging.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- zerver/management/commands/soft_deactivate_users.py
+++ zerver/management/commands/soft_deactivate_users.py
@@ -73,7 +73,7 @@ class Command(ZulipBaseCommand):
 
             users_to_activate = get_users_from_emails(user_emails, filter_kwargs)
             users_activated = do_soft_activate_users(users_to_activate)
-            logger.info('Soft Reactivated %d user(s)' % (len(users_activated),))
+            logger.info('Soft Reactivated %d user(s)', len(users_activated))
 
         elif deactivate:
             if user_emails:
@@ -83,7 +83,7 @@ class Command(ZulipBaseCommand):
             else:
                 users_deactivated = do_auto_soft_deactivate_users(int(options['inactive_for']),
                                                                   realm)
-            logger.info('Soft Deactivated %d user(s)' % (len(users_deactivated),))
+            logger.info('Soft Deactivated %d user(s)', len(users_deactivated))
 
         else:
             self.print_help("./manage.py", "soft_deactivate_users")
