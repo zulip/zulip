07626a8c277785d4e8b5fb24cee1de70b0a18953
Clara Dantas <clara.moraesd@gmail.com>
2020-06-19 19:28:11 -0300

    test_openapi: Remove /realm from pending_endpoints.

--- zerver/tests/test_openapi.py
+++ zerver/tests/test_openapi.py
@@ -292,7 +292,6 @@ class OpenAPIArgumentsTest(ZulipTestCase):
         '/zcommand',
 
         #### These "organization settings" endpoint have modest value to document:
-        '/realm',
         '/realm/domains',
         '/realm/domains/{domain}',
         '/bots',
