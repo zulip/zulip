596bfb47c49d8fd83822e2cece4fbd6678ab29e8
Anders Kaseorg <anders@zulip.com>
2020-06-20 20:15:08 -0700

    papertrail: Replace custom validator with check_dict.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- zerver/webhooks/papertrail/view.py
+++ zerver/webhooks/papertrail/view.py
@@ -1,11 +1,11 @@
-from typing import Any, Dict, Optional
+from typing import Any, Dict
 
 from django.http import HttpRequest, HttpResponse
-from django.utils.translation import ugettext as _
 
 from zerver.decorator import api_key_only_webhook_view
 from zerver.lib.request import REQ, has_request_variables
 from zerver.lib.response import json_success
+from zerver.lib.validator import check_dict, check_list, check_string
 from zerver.lib.webhooks.common import check_send_webhook_message
 from zerver.models import UserProfile
 
@@ -17,17 +17,19 @@ SEARCH_TEMPLATE = """
 ```
 """.strip()
 
-def ensure_keys(name: str, data: Any) -> Optional[str]:
-    if 'events' in data and 'saved_search' in data:
-        saved_search = data['saved_search']
-        if 'name' in saved_search and 'html_search_url' in saved_search:
-            return None
-    return _("Missing expected keys")
-
 @api_key_only_webhook_view('Papertrail')
 @has_request_variables
-def api_papertrail_webhook(request: HttpRequest, user_profile: UserProfile,
-                           payload: Dict[str, Any]=REQ(validator=ensure_keys)) -> HttpResponse:
+def api_papertrail_webhook(
+    request: HttpRequest,
+    user_profile: UserProfile,
+    payload: Dict[str, Any] = REQ(validator=check_dict([
+        ("events", check_list(check_dict([]))),
+        ("saved_search", check_dict([
+            ("name", check_string),
+            ("html_search_url", check_string),
+        ])),
+    ])),
+) -> HttpResponse:
 
     matches = MATCHES_TEMPLATE.format(
         name=payload["saved_search"]["name"],
