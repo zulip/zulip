d7745674dca97c674758d64559751636cf43c3de
Anders Kaseorg <anders@zulip.com>
2020-07-04 18:00:50 -0700

    zthumbor: Fix strict_optional errors.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- zthumbor/loaders/helpers.py
+++ zthumbor/loaders/helpers.py
@@ -44,4 +44,5 @@ def separate_url_and_source_type(url):
     # type: (Text) -> Tuple[Text, Text]
     THUMBNAIL_URL_PATT = re.compile('^(?P<actual_url>.+)/source_type/(?P<source_type>.+)')
     matches = THUMBNAIL_URL_PATT.match(url)
+    assert matches is not None
     return (matches.group('source_type'), matches.group('actual_url'))
