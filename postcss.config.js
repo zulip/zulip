08b3ce0983112274f3a7232f2b599d5bb2d9343d
Rohitt Vashishtha <aero31aero@gmail.com>
2020-07-05 22:24:20 +0000

    webpack: Append prefers-color-scheme block to CSS files programatically.
    We do this by adding a new plugin 'postcss-raw-append' which appends the
    given CSS block at the end of the file being processed. We need to add
    this plugin before postcss-extend-rule and after postcss-wrap.

--- postcss.config.js
+++ postcss.config.js
@@ -9,19 +9,40 @@ const maybe_handle_flatpickr = (file, opts) => {
     return opts;
 };
 
+const maybe_append_prefered_color_scheme = (file, opts) => {
+    // If a file contains night-mode specific CSS, add it here:
+    const night_mode_files = [
+        'night_mode.scss',
+    ];
+
+    const data = `
+@media (prefers-color-scheme: dark) {
+    body.color-scheme-automatic {
+        @extend body.night-mode;
+    }
+}`;
+    const flatpickr = file.basename === 'dark.css' && file.dirname.includes('flatpickr/dist/themes');
+    if (night_mode_files.includes(file.basename) || flatpickr) {
+        opts.plugins['postcss-raw-append'] = { data };
+    }
+    return opts;
+};
+
 module.exports = ({ file }) => {
     let opts = {
         parser: file.extname === ".scss" ? "postcss-scss" : false,
         plugins: {
             // Warning: despite appearances, order is significant
+            "postcss-wrap": false, // Maybe add the plugin at this position
+            "postcss-raw-append": false,
             "postcss-nested": {},
             "postcss-extend-rule": {},
             "postcss-simple-vars": {},
             "postcss-calc": {},
-            "postcss-wrap": false, // Maybe add the plugin at this position
             autoprefixer: {},
         },
     };
     opts = maybe_handle_flatpickr(file, opts);
+    opts = maybe_append_prefered_color_scheme(file, opts);
     return opts;
 };
