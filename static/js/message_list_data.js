e168b1ed02a1c85e230802078c6d1d32c91f8336
Ryan Rehman <ryanrehman99@gmail.com>
2020-07-05 18:08:40 +0530

    message list: Change locally echoed message ids synchronously.
    There is a bug and race issue that occurs when a message is selected
    while we are in the process of reifying a locally echoed message,
    raising the "Selected message id not in MessageList" error.
    The code flow to get the exception is as follows:
    
    * A user sends a message to the current narrow we are in.
    * Before the new message event is received, we sent a message to
      the same message list which renders it with a locally echoed id.
    * One of the ways of getting the exception is to already have the
      locally sent message selected, before receiving an acknowledgment
      from the server.
    * Thus the Message List Data's `selected_id` now points to the new
      message id. The exception is raised on entering the `was_selected`
      if block inside `message_list_view` which tries to re-select the
      message.
    
    Updating the `_rerender_message` code for this special case won't fix
    the entire bug because, as mentioned above there are other ways of
    getting the exception:
    
    Ideally, after all our synchronous work (`echo.process_from_server`)
    has completed we would expect the re-order and re-render work of the
    `change_message_id` would occur first, due to the timer of the
    setTimeout being set to 0.
    
    However as evident from the race condition existing, this isn't always
    the case. `change_message_id` function is responsible for 3 things:
    updation, re-ordering and re-rendering.
    The first one which is responsible for updating the message list's
    local cache, occurs synchronously while for the latter two, they both
    occur asynchronously.
    
    Before the setTimeout which is responsible for the latter two actions,
    is encountered the user might select the message by clicking or more
    commonly by scrolling, which causes this message selection event to be
    ahead of the setTimeout in the callback queue.
    During this time frame, our race condition takes place.
    
    And even though the message id is updated it's Message List is not
    in the correct sort order, which leads to `closest_id` !== `id` in
    `MessageList_select_id` being true and raising the exception.
    
    Now, we only asynchronously call the re_render function, to guarantee
    the data is always correct and UI updates should be done at the end.
    
    Fixes #15346.

--- static/js/message_list_data.js
+++ static/js/message_list_data.js
@@ -521,9 +521,7 @@ MessageListData.prototype = {
             this._selected_id = new_id;
         }
 
-        setTimeout(() => {
-            this.reorder_messages(new_id, opts);
-        }, 0);
+        this.reorder_messages(new_id, opts);
     },
 
     reorder_messages: function (new_id, opts) {
@@ -553,7 +551,7 @@ MessageListData.prototype = {
                 self._all_items.sort(message_sort_func);
             }
 
-            opts.re_render();
+            setTimeout(opts.re_render, 0);
         }
     },
 
