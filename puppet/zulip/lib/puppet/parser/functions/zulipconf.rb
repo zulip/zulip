17002f2a0ed64dd5ab9581d7004f06b7755cdbd5
Alex Vandiver <alexmv@zulip.com>
2020-07-06 23:15:15 +0000

    puppet: Allow passing an alternate config path to zulip-puppet-apply.
    When temporary configuration changes are desired, this lets one set up
    an alternate `zulip.conf` to apply while leaving the true one in place.

--- puppet/zulip/lib/puppet/parser/functions/zulipconf.rb
+++ puppet/zulip/lib/puppet/parser/functions/zulipconf.rb
@@ -2,7 +2,8 @@ module Puppet::Parser::Functions
   newfunction(:zulipconf, :type => :rvalue) do |args|
     default = args.pop
     joined = args.join(" ")
-    output = `/usr/bin/crudini --get /etc/zulip/zulip.conf #{joined} 2>&1`; result=$?.success?
+    zulip_conf_path = lookupvar('zulip_conf_path')
+    output = `/usr/bin/crudini --get #{zulip_conf_path} #{joined} 2>&1`; result=$?.success?
     if result
       output.strip()
     else
