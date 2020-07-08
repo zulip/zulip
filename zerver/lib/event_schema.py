b116f1e9118938c63c7372f314976ba668951714
Steve Howell <showell@zulip.com>
2020-07-08 11:35:37 +0000

    event_schema: Extract check_stream_update.

--- zerver/lib/event_schema.py
+++ zerver/lib/event_schema.py
@@ -18,7 +18,7 @@ from zerver.lib.validator import (
     check_union,
     equals,
 )
-from zerver.models import Realm
+from zerver.models import Realm, Stream
 
 # These fields are used for "stream" events, and are included in the
 # larger "subscription" events that also contain personal settings.
@@ -73,6 +73,16 @@ check_value = check_union(
     ]
 )
 
+check_optional_value = check_union(
+    [
+        # force vertical formatting
+        check_bool,
+        check_int,
+        check_string,
+        equals(None),
+    ]
+)
+
 
 """
 realm/update events are flexible for values;
@@ -123,3 +133,56 @@ check_stream_create = check_events_dict(
         ("streams", check_list(check_dict_only(basic_stream_fields))),
     ]
 )
+
+_check_stream_update = check_events_dict(
+    required_keys=[
+        ("type", equals("stream")),
+        ("op", equals("update")),
+        ("property", check_string),
+        ("value", check_optional_value),
+        ("name", check_string),
+        ("stream_id", check_int),
+    ],
+    optional_keys=[
+        ("rendered_description", check_string),
+        ("history_public_to_subscribers", check_bool),
+    ],
+)
+
+
+def check_stream_update(var_name: str, event: Dict[str, Any],) -> None:
+    _check_stream_update(var_name, event)
+    prop = event["property"]
+    value = event["value"]
+
+    extra_keys = set(event.keys()) - {
+        "id",
+        "type",
+        "op",
+        "property",
+        "value",
+        "name",
+        "stream_id",
+    }
+
+    if prop == "description":
+        assert extra_keys == {"rendered_description"}
+        assert isinstance(value, str)
+    elif prop == "email_address":
+        assert extra_keys == set()
+        assert isinstance(value, str)
+    elif prop == "invite_only":
+        assert extra_keys == {"history_public_to_subscribers"}
+        assert isinstance(value, bool)
+    elif prop == "message_retention_days":
+        assert extra_keys == set()
+        if value is not None:
+            assert isinstance(value, int)
+    elif prop == "name":
+        assert extra_keys == set()
+        assert isinstance(value, str)
+    elif prop == "stream_post_policy":
+        assert extra_keys == set()
+        assert value in Stream.STREAM_POST_POLICY_TYPES
+    else:
+        raise AssertionError(f"Unknown property: {prop}")
