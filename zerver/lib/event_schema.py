728ef90807e67a9ba7d236008b59548f4023dbac
Steve Howell <showell@zulip.com>
2020-07-08 00:14:11 +0000

    event_schema: Extract check_realm_update.

--- zerver/lib/event_schema.py
+++ zerver/lib/event_schema.py
@@ -5,9 +5,18 @@ It will contain schemas (aka validators) for Zulip events.
 
 Right now it's only intended to be used by test code.
 """
-from typing import Dict, Sequence, Tuple
+from typing import Any, Dict, Sequence, Tuple
 
-from zerver.lib.validator import Validator, check_dict_only, check_int
+from zerver.lib.validator import (
+    Validator,
+    check_bool,
+    check_dict_only,
+    check_int,
+    check_string,
+    check_union,
+    equals,
+)
+from zerver.models import Realm
 
 
 def check_events_dict(
@@ -35,3 +44,55 @@ def check_events_dict(
         required_keys=list(required_keys) + [("id", check_int)],
         optional_keys=optional_keys,
     )
+
+
+check_value = check_union(
+    [
+        # force vertical formatting
+        check_bool,
+        check_int,
+        check_string,
+    ]
+)
+
+
+"""
+realm/update events are flexible for values;
+we will use a more strict checker to check
+types in a context-specific manner
+"""
+_check_realm_update = check_events_dict(
+    required_keys=[
+        ("type", equals("realm")),
+        ("op", equals("update")),
+        ("property", check_string),
+        ("value", check_value),
+    ]
+)
+
+
+def check_realm_update(var_name: str, event: Dict[str, Any],) -> None:
+    """
+    Realm updates have these two fields:
+
+        property
+        value
+
+    We check not only the basic schema, but also that
+    the value people actually matches the type from
+    Realm.property_types that we have configured
+    for the property.
+    """
+    _check_realm_update(var_name, event)
+    prop = event["property"]
+    value = event["value"]
+
+    property_type = Realm.property_types[prop]
+    if property_type in (bool, int, str):
+        assert isinstance(value, property_type)
+    elif property_type == (int, type(None)):
+        assert isinstance(value, int)
+    elif property_type == (str, type(None)):
+        assert isinstance(value, str)
+    else:
+        raise AssertionError(f"Unexpected property type {property_type}")
