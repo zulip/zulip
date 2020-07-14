67773bc8344fd2f4681564449e2fd5b3d656d169
Anders Kaseorg <anders@zulip.com>
2020-07-14 15:33:36 -0700

    support: Move eslint-disable comment to Prettier-compatible position.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- static/js/analytics/support.js
+++ static/js/analytics/support.js
@@ -3,7 +3,8 @@ $(() => {
         e.preventDefault();
         const string_id = $(this).data("string-id");
         const message = 'Do you really want to scrub the realm "' + string_id + '"? This action is irreversible.';
-        if (confirm(message)) { // eslint-disable-line no-alert
+        // eslint-disable-next-line no-alert
+        if (confirm(message)) {
             this.form.submit();
         }
     });
