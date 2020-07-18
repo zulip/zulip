4bf09719b3b222dc97f1ceadbb9032fa56104896
Anders Kaseorg <anders@zulip.com>
2020-07-17 18:14:20 -0700

    dependencies: Upgrade JavaScript dependencies.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- tools/webpack-helpers.ts
+++ tools/webpack-helpers.ts
@@ -25,32 +25,19 @@ interface ExportLoaderOptions {
     path: string;
     name?: string | string[];
 }
-function getExposeLoaders(optionsArr: ExportLoaderOptions[]): RuleSetRule[] {
-    const exposeLoaders = [];
-    for (const loaderEntry of optionsArr) {
-        const path = loaderEntry.path;
-        let name = "";
-        const useArr = [cacheLoader];
-        // If no name is provided, infer it
-        if (!loaderEntry.name) {
-            name = basename(path, ".js");
-            useArr.push({loader: "expose-loader", options: name});
-        } else {
-            // If name is an array
-            if (Array.isArray(loaderEntry.name)) {
-                for (const exposeName of loaderEntry.name) {
-                    useArr.push({loader: "expose-loader", options: exposeName});
-                }
-                // If name is a string
-            } else {
-                useArr.push({loader: "expose-loader", options: loaderEntry.name});
-            }
-        }
-        exposeLoaders.push({
-            test: require.resolve(path),
-            use: useArr,
-        });
-    }
-    return exposeLoaders;
+
+export function getExposeLoaders(optionsArr: ExportLoaderOptions[]): RuleSetRule[] {
+    return optionsArr.map(({path, name}) => ({
+        test: require.resolve(path),
+        use: [
+            cacheLoader,
+            {
+                loader: "expose-loader",
+                options: {
+                    // If no name is provided, infer it
+                    exposes: name ?? basename(path, ".js"),
+                },
+            },
+        ],
+    }));
 }
-export {getExposeLoaders};
