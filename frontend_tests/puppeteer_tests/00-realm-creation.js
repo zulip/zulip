ee140bc1b70c3fe044fba1188201e43aba6f6993
Priyansh Garg <priyanshgarg30@gmail.com>
2020-07-22 12:19:51 +0530

    registration: Improve registration form.
    Improved markup of help-text.
    Showing Email as plain-text instead of disabled input.
    Changed page heading to 'Create your organization' in realm creation form
    and 'Create your account' in normal signup form.
    Grouped org settings and user settings with fieldsets.
    Reduced space between Password field and Password strength bar.
    
    Also, updated the corresponding test cases.
    
    Partially Fixes: #15750.

--- frontend_tests/puppeteer_tests/00-realm-creation.js
+++ frontend_tests/puppeteer_tests/00-realm-creation.js
@@ -42,7 +42,7 @@ async function realm_creation_tests(page) {
     // checking the text in <p> tag under pitch class is as expected.
     await page.waitForSelector(".pitch");
     const text_in_pitch = await page.evaluate(() => document.querySelector(".pitch p").innerText);
-    assert(text_in_pitch === "We just need you to do one last thing.");
+    assert(text_in_pitch === "You’re almost there! We just need you to do one last thing.");
 
     // fill the form.
     const params = {
