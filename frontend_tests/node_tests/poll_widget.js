0ef6d7dd706365a1dee65b3a99551fa05a2bf28f
Anders Kaseorg <anders@zulip.com>
2020-07-22 17:04:27 -0700

    poll_widget: Convert poll_data_holder to an ES6 class PollData.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- frontend_tests/node_tests/poll_widget.js
+++ frontend_tests/node_tests/poll_widget.js
@@ -7,14 +7,14 @@ set_global("people", {});
 const return_false = () => false;
 const return_true = () => true;
 
-run_test("poll_data_holder my question", () => {
+run_test("PollData my question", () => {
     const is_my_poll = true;
     const question = "Favorite color?";
 
     const sender_id = 99;
     people.my_current_user_id = () => sender_id;
 
-    const data_holder = poll_widget.poll_data_holder(is_my_poll, question, []);
+    const data_holder = new poll_widget.PollData(is_my_poll, question, []);
 
     let data = data_holder.get_widget_data();
 
