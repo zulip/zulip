4f195de78df8b690c13fc3f85fd6568a17c85da7
Steve Howell <showell@zulip.com>
2020-07-23 22:02:31 +0000

    node tests: Split out settings_emoji.
    This keeps the emoji tests free of jQuery
    concerns.

--- frontend_tests/node_tests/emoji.js
+++ frontend_tests/node_tests/emoji.js
@@ -1,32 +1,9 @@
-set_global("$", global.make_zjquery());
 set_global("page_params", {
     emojiset: "google",
     realm_emoji: {},
 });
-set_global("upload_widget", {});
 
 zrequire("emoji");
-zrequire("settings_emoji");
-
-run_test("build_emoji_upload_widget", () => {
-    let build_widget_stub = false;
-    upload_widget.build_widget = function (
-        get_file_input,
-        file_name_field,
-        input_error,
-        clear_button,
-        upload_button,
-    ) {
-        assert.deepEqual(get_file_input(), $("#emoji_file_input"));
-        assert.deepEqual(file_name_field, $("#emoji-file-name"));
-        assert.deepEqual(input_error, $("#emoji_file_input_error"));
-        assert.deepEqual(clear_button, $("#emoji_image_clear_button"));
-        assert.deepEqual(upload_button, $("#emoji_upload_button"));
-        build_widget_stub = true;
-    };
-    settings_emoji.build_emoji_upload_widget();
-    assert(build_widget_stub);
-});
 
 run_test("get_canonical_name", () => {
     emoji.active_realm_emojis = new Map(
