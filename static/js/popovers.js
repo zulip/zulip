1720c5a244db584518dcf8c8837a30acbccb3129
ritik <ritikcn05@gmail.com>
2020-07-24 22:50:44 +0530

    user_status

--- static/js/popovers.js
+++ static/js/popovers.js
@@ -849,6 +849,20 @@ exports.register_click_handlers = function () {
         e.stopPropagation();
         e.preventDefault();
     });
+    
+    $('body').on('click', '.status', (e) => {
+        exports.hide_all();
+        console.log(user_status);
+        if (user_status.server_set_away())
+        {
+            user_status.server_set_away();
+           }
+        else  {
+            user_status.server_revoke_away();
+        }
+        e.stopPropagation();
+        e.preventDefault();
+        });
 
     $("body").on("click", ".set_away_status", (e) => {
         exports.hide_all();
