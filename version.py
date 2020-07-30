928b8ad0315b3b8fe8f6083afdb282bffb21ddf0
Tim Abbott <tabbott@zulip.com>
2020-07-30 15:52:52 -0700

    version: Update version after 3.1 release.

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 import os
 
-ZULIP_VERSION = "3.1"
+ZULIP_VERSION = "3.1+git"
 # Add information on number of commits and commit hash to version, if available
 zulip_git_version_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'zulip-git-version')
 if os.path.exists(zulip_git_version_file):
