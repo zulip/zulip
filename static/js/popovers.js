47d94e62d29210d5e7f5cb689a0b3969bb1ae69a
ritik <ritikcn05@gmail.com>
2020-07-30 17:07:28 +0530

    dummy1

--- static/js/popovers.js
+++ static/js/popovers.js
@@ -850,17 +850,6 @@ exports.register_click_handlers = function () {
         e.preventDefault();
     });
 
-    $("body").on("click", ".status", (e) => {
-        exports.hide_all();
-        if (user_status.server_set_away()) {
-            user_status.server_set_away();
-        } else {
-            user_status.server_revoke_away();
-        }
-        e.stopPropagation();
-        e.preventDefault();
-    });
-
     $("body").on("click", ".set_away_status", (e) => {
         exports.hide_all();
         user_status.server_set_away();
