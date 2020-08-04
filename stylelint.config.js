7c7f030d54a0c6e266965f9a6da60ffcc0a66e33
Anders Kaseorg <anders@zulip.com>
2020-08-04 14:50:46 -0700

    styles: Enable most stylelint-config-recommended rules.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,8 +1,19 @@
 "use strict";
 
 module.exports = {
-    extends: ["stylelint-config-prettier"],
+    extends: ["stylelint-config-recommended", "stylelint-config-prettier"],
     rules: {
+        // Add some exceptions for recommended rules
+        "at-rule-no-unknown": [true, {ignoreAtRules: ["extend"]}],
+        "property-no-unknown": [true, {ignoreProperties: [/^mso-/, "user-drag"]}],
+
+        // Disable recommended rules we don't comply with yet
+        "declaration-block-no-duplicate-properties": null,
+        "declaration-block-no-shorthand-property-overrides": null,
+        "font-family-no-missing-generic-family-keyword": null,
+        "no-descending-specificity": null,
+        "no-duplicate-selectors": null,
+
         // Stylistic rules for CSS.
         "function-whitespace-after": "always",
 
