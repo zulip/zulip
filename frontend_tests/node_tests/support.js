ac91a4594fb937b742f31152b57037247f07952e
Vishnu KS <yo@vishnuks.com>
2020-08-13 22:32:01 +0530

    support: Confirm the string_id before scrubbing the realm.
    Adding an extra confirmation in case the string_id entered
    in search was wrong in first place.

--- frontend_tests/node_tests/support.js
+++ frontend_tests/node_tests/support.js
@@ -18,6 +18,37 @@ set_global("$", global.make_zjquery());
 run_test("scrub_realm", () => {
     jquery_init();
     const click_handler = $("body").get_on_handler("click", ".scrub-realm-button");
+
+    const fake_this = $.create("fake-.scrub-realm-button");
+    fake_this.data = (name) => {
+        assert.equal(name, "string-id");
+        return "zulip";
+    };
+
+    let submit_form_called = false;
+    fake_this.form = {
+        submit: () => {
+            submit_form_called = true;
+        },
+    };
+    const event = {
+        preventDefault: () => {},
+    };
+
+    window.prompt = () => "zulip";
+    click_handler.call(fake_this, event);
+    assert(submit_form_called);
+
+    submit_form_called = false;
+    window.prompt = () => "invalid-string-id";
+    let alert_called = false;
+    window.alert = () => {
+        alert_called = true;
+    };
+    click_handler.call(fake_this, event);
+    assert(!submit_form_called);
+    assert(alert_called);
+
     assert.equal(typeof click_handler, "function");
 
     assert.equal(document.querySelectorAll(".scrub-realm-button").length, 1);
