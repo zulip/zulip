8adf5304007b8e78ca421ca7654b6a7a62aa6001
Alex Vandiver <alexmv@zulip.com>
2020-09-08 16:35:13 -0700

    puppet: Generate sharding in puppet, then refresh-sharding-and-restart.
    This supports running puppet to pick up new sharding changes, which
    will warn of the need to finalize them via
    `refresh-sharding-and-restart`, or simply running that directly.

--- puppet/zulip/manifests/tornado_sharding.pp
+++ puppet/zulip/manifests/tornado_sharding.pp
@@ -7,13 +7,17 @@ class zulip::tornado_sharding {
   # with the correct default content for the "only one shard" setup. For this
   # reason they use "replace => false", because the files are managed by
   # the sharding script afterwards and puppet shouldn't overwrite them.
+
+  # The sha256 of the empty hash, used to fingerprint the configs as
+  # having been generated with a null sharding configuration.
+  $empty_sha256 = sha256('')
   file { '/etc/zulip/nginx_sharding.conf':
     ensure  => file,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
     notify  => Service['nginx'],
-    content => "set \$tornado_server http://tornado;\n",
+    content => "# Configuration hash: ${empty_sha256}\nset \$tornado_server http://tornado;\n",
     replace => false,
   }
   file { '/etc/zulip/sharding.json':
@@ -24,6 +28,14 @@ class zulip::tornado_sharding {
     mode    => '0644',
     content => "{}\n",
     replace => false,
+  }
+
+  exec { 'write_updated_sharding':
+    command   => "${::zulip_scripts_path}/lib/sharding.py",
+    unless    => "${::zulip_scripts_path}/lib/sharding.py --verify",
+    require   => [File['/etc/zulip/nginx_sharding.conf']],
+    logoutput => true,
+    loglevel  => 'warning',
   }
 
   # The number of Tornado processes to run on the server; this
