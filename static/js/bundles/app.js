07254cfa15b3013c6e3f54112d304220eeea95a1
Aman Agrawal <amanagr@zulip.com>
2020-09-11 13:43:01 +0530

    landing-page: Redesign landing page and other pages in same theme.

--- static/js/bundles/app.js
+++ static/js/bundles/app.js
@@ -1,4 +1,21 @@
-import "./common";
+import "core-js/features/promise";
+import "core-js/features/symbol";
+import "../../../tools/debug-require";
+import "jquery";
+import "../page_params";
+import "../csrf";
+import "../blueslip";
+import "../../third/bootstrap/css/bootstrap.css";
+import "../common";
+import "../../third/bootstrap/css/bootstrap-btn.css";
+import "../../third/bootstrap/js/bootstrap.js";
+import "simplebar/dist/simplebar.css";
+import "font-awesome/css/font-awesome.css";
+import "../../assets/icons/zulip-icons.font";
+import "source-sans-pro/source-sans-pro.css";
+import "../../styles/pygments.css";
+import "@uppy/core/dist/style.css";
+import "@uppy/progress-bar/dist/style.css";
 
 // Import Third party libraries
 import "../../third/bootstrap-notify/js/bootstrap-notify";
