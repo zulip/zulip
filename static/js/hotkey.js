328549f009a9d9ce667b8833fad37b24b051f944
sahil839 <sahilbatra839@gmail.com>
2020-09-22 22:49:40 +0530

    popovers: Enable keyboard support for user info popovers.
    This commit enables keyboard support for user info popovers for
    navigating through popover options using up/down keys.
    
    We add get_user_info_popover_items function, whose implementation
    is different from other similar functions. Instead of using
    popover_data.$tip we directly use $("div.user-info-popover")
    because when we open the popover of bot owner from the bot
    popover, the element which opens the popover is removed from
    DOM and popover_data is undefined.

--- static/js/hotkey.js
+++ static/js/hotkey.js
@@ -306,6 +306,11 @@ exports.process_enter_key = function (e) {
         return true;
     }
 
+    if (popovers.user_info_popped()) {
+        popovers.user_info_popover_handle_keyboard("enter");
+        return true;
+    }
+
     if (stream_popover.stream_popped()) {
         stream_popover.stream_sidebar_menu_handle_keyboard("enter");
         return true;
@@ -524,7 +529,7 @@ exports.process_hotkey = function (e, hotkey) {
         return false;
     }
 
-    if (overlays.settings_open()) {
+    if (overlays.settings_open() && !popovers.user_info_popped()) {
         return false;
     }
 
@@ -576,6 +581,11 @@ exports.process_hotkey = function (e, hotkey) {
 
         if (popovers.message_info_popped()) {
             popovers.user_info_popover_for_message_handle_keyboard(event_name);
+            return true;
+        }
+
+        if (popovers.user_info_popped()) {
+            popovers.user_info_popover_handle_keyboard(event_name);
             return true;
         }
 
