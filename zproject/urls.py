b34fc26f80c51b63de3392b9428d148ba14ec3ec
Steve Howell <showell@zulip.com>
2020-10-01 21:13:56 +0000

    wip: Try out oauth2_provider.
    Right now we manually install django-oauth-toolkit.
    
    This includes:
    
        - help Django find the relevant templates
          for the application view
    
        - add in relevant views from the toolkit
    
        - validate access tokens for Zulip views
    
    TODO:
    
        We should lock down the views and only allow admin
        users to create applications, probably.
    
        And we should automatically install the toolkit.
    
        We should refine read/write scopes.  (For now we
        assume users just authorize with write scope.)
    
        We can clean up the template-related code (to
        de-duplicate the logic we use for the two-factor
        templates).
    
        And still lots else to do, like writing tests, etc.

--- zproject/urls.py
+++ zproject/urls.py
@@ -890,6 +890,27 @@ urls += [
     path('api/<slug:article>', api_documentation_view),
 ]
 
+# Experimental oauth provider support.
+import oauth2_provider.views as oauth2_views
+
+oauth2_endpoint_views = [
+    # OAuth2 Application Management endpoints
+	path('applications/', oauth2_views.ApplicationList.as_view(), name="list"),
+	path('applications/register/', oauth2_views.ApplicationRegistration.as_view(), name="register"),
+	path('applications/<pk>/', oauth2_views.ApplicationDetail.as_view(), name="detail"),
+	path('applications/<pk>/delete/', oauth2_views.ApplicationDelete.as_view(), name="delete"),
+	path('applications/<pk>/update/', oauth2_views.ApplicationUpdate.as_view(), name="update"),
+
+	# tokens
+    path('authorize/', oauth2_views.AuthorizationView.as_view(), name="authorize"),
+    path('token/', oauth2_views.TokenView.as_view(), name="token"),
+    path('revoke-token/', oauth2_views.RevokeTokenView.as_view(), name="revoke-token"),
+]
+
+urls += [
+    path('o/', include((oauth2_endpoint_views, 'oauth2_provider'),)),
+]
+
 # Two Factor urls
 if settings.TWO_FACTOR_AUTHENTICATION_ENABLED:
     urls += [path('', include(tf_urls)),
