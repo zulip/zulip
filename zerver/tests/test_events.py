c7ee8802ea7ce43715aa18a6c1b3fde300e3788c
sahil839 <sahilbatra839@gmail.com>
2020-10-02 16:37:40 +0530

    tests: Add tests for all the values of wildcard_mention_policy.

--- zerver/tests/test_events.py
+++ zerver/tests/test_events.py
@@ -1730,7 +1730,7 @@ class RealmPropertyActionTest(BaseAction):
             invite_to_stream_policy=[3, 2, 1],
             private_message_policy=[2, 1],
             user_group_edit_policy=[1, 2],
-            wildcard_mention_policy=[3, 2, 1],
+            wildcard_mention_policy=[6, 5, 4, 3, 2, 1],
             email_address_visibility=[Realm.EMAIL_ADDRESS_VISIBILITY_ADMINS],
             bot_creation_policy=[Realm.BOT_CREATION_EVERYONE],
             video_chat_provider=[
