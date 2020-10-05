045cdb4ed045861f302471efa56b1525289f2d8b
Aman Agrawal <amanagr@zulip.com>
2020-10-05 19:39:25 +0530

    login_to_access: Modal which blocks access for spectator.
    We will use this modal for any narrow / hash or other UI element that
    requires an actual account to use, to provide something reasonable to
    occur when a user clicks on those things.

---
+++ static/js/login_to_access.js
@@ -0,0 +1,31 @@
+// Module for displaying the modal asking spectators to login when
+// attempting to do things that are not possible as a specatator (like
+// add an emoji reaction, star a message, etc.).  While in many cases,
+// we will prefer to hide menu options that don't make sense for
+// spectators, this modal is useful for everything that doesn't make
+// sense to remove from a design perspective.
+
+import $ from "jquery";
+
+import render_login_to_access_modal from "../templates/login_to_access.hbs";
+
+import {page_params} from "./page_params";
+
+export function show() {
+    // Hide all overlays, popover and go back to the previous hash if the
+    // hash has changed.
+    let login_link;
+    if (page_params.development_environment) {
+        login_link = "/devlogin";
+    } else {
+        login_link = "/login";
+    }
+
+    $("#login-to-access-modal-holder").html(
+        render_login_to_access_modal({
+            signup_link: "/register",
+            login_link,
+        }),
+    );
+    $("#login_to_access_modal").modal("show");
+}
