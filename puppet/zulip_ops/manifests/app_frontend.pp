eb462775bc6029f983c1bf7f463e0432c0cc243c
Alex Vandiver <alexmv@zulip.com>
2020-10-12 17:21:30 -0700

    puppet: Move frontend monitoring into its own file.
    This allows it to be pulled in for deploys like czo, which don't use
    the full `zulip_ops::app_frontend`, but we wish to monitor.

--- puppet/zulip_ops/manifests/app_frontend.pp
+++ puppet/zulip_ops/manifests/app_frontend.pp
@@ -4,6 +4,7 @@ class zulip_ops::app_frontend {
   include zulip::rabbit
   include zulip::postfix_localmail
   include zulip::static_asset_compiler
+  include zulip::app_frontend_monitoring
   $app_packages = [# Needed for the ssh tunnel to the redis server
     'autossh',
   ]
@@ -44,26 +45,5 @@ class zulip_ops::app_frontend {
     group  => 'root',
     mode   => '0644',
     source => 'puppet:///modules/zulip_ops/cron.d/fetch-contributor-data',
-  }
-
-  # Enable some munin plugins
-  $munin_plugins = [
-    'rabbitmq_connections',
-    'rabbitmq_consumers',
-    'rabbitmq_messages',
-    'rabbitmq_messages_unacknowledged',
-    'rabbitmq_messages_uncommitted',
-    'rabbitmq_queue_memory',
-    'zulip_send_receive_timing',
-  ]
-  zulip_ops::munin_plugin { $munin_plugins: }
-
-  file { '/etc/cron.d/rabbitmq-monitoring':
-    ensure  => file,
-    require => Package[rabbitmq-server],
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    source  => 'puppet:///modules/zulip/cron.d/rabbitmq-monitoring',
   }
 }
