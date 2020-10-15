9a17d70cd7d7d302c989ae0ba15912cbadce671e
Anders Kaseorg <anders@zulip.com>
2020-10-14 19:55:57 -0700

    shfmt: Reformat shell scripts with shfmt.
    https://github.com/mvdan/sh
    
    Signed-off-by: Anders Kaseorg <anders@zulip.com>
    (cherry picked from commit dfaea9df65dfd8e382502128d390e305464770f6)

--- scripts/setup/install-wal-g.sh
+++ scripts/setup/install-wal-g.sh
@@ -8,11 +8,11 @@ HASH="$2"
 
 cd /tmp
 wget -qO "wal-g-$VERSION.tar.gz" \
-     "https://github.com/wal-g/wal-g/releases/download/v$VERSION/wal-g.linux-amd64.tar.gz"
+    "https://github.com/wal-g/wal-g/releases/download/v$VERSION/wal-g.linux-amd64.tar.gz"
 
 # Check not against the arbitrary provided sha256 on Github, but
 # against the (same) sha256 that we hardcode as "known good".
-echo "$HASH  wal-g-$VERSION.tar.gz" > "wal-g-$VERSION.tar.gz.sha256"
+echo "$HASH  wal-g-$VERSION.tar.gz" >"wal-g-$VERSION.tar.gz.sha256"
 sha256sum -c "wal-g-$VERSION.tar.gz.sha256"
 
 tar xzf "wal-g-$VERSION.tar.gz"
