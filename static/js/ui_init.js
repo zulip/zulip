edac9fb099413a197d7ad07ec4afd8e5ad89c38b
Harsh Sharma <sharmaharsh@me.com>
2020-10-19 22:53:03 +0530

    right_sidebar: Add JS tooltip to keyboard icon to prevent flickering
    fixed capitalization according to the guidelines

--- static/js/ui_init.js
+++ static/js/ui_init.js
@@ -275,7 +275,11 @@ exports.initialize_kitchen_sink_stuff = function () {
 
     // We disable animations here because they can cause the tooltip
     // to change shape while fading away in weird way.
-    $("#keyboard-icon").tooltip({animation: false});
+    $("#keyboard-icon").tooltip({
+        title: i18n.t("Keyboard shortcuts __hotkey__", {hotkey: "(?)"}),
+        animation: false,
+        placement: "left",
+    });
 
     $("body").on("mouseover", ".message_edit_content", function () {
         $(this).closest(".message_row").find(".copy_message").show();
