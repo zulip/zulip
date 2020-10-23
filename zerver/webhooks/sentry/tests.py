85c8739a9ef22590765af034b70a8cf939f21289
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-10-23 11:50:24 +0530

    webhooks/sentry: Support integration configured as webhook.
    Sentry allows adding simple webhooks without going through the process
    of creating an Internal Integration in Sentry's Integration
    Platform[1] (which our docs recommend).
    
    The payload from sent from such a (simple) webhook integration is
    slightly different from the payload sent by an Internal Integration
    webhook. This commit tries to wrangle this payload into a form that is
    usable by our webhook handler to send a notification message.
    
    [1]: https://sentry.io/integration-platform/

--- zerver/webhooks/sentry/tests.py
+++ zerver/webhooks/sentry/tests.py
@@ -82,6 +82,29 @@ Traceback:
 ```"""
         self.check_webhook("event_for_exception_python", expected_topic, expected_message)
 
+    def test_webhook_event_for_exception_python(self) -> None:
+        expected_topic = "ValueError: new sentry error."
+        expected_message = """
+**New exception:** [ValueError: new sentry error.](https://sentry.io/organizations/bar-foundation/issues/1972208801/event/c916dccfd58e41dcabaebef0091f0736/)
+```quote
+**level:** error
+**timestamp:** 2020-10-21 23:25:11
+**filename:** trigger-exception.py
+```
+
+Traceback:
+```python3
+
+
+     if __name__ == "__main__":
+         sentry_sdk.init(dsn=DSN_SECRET)
+         try:
+--->         raise ValueError("new sentry error.")
+         except Exception as e:
+             sentry_sdk.capture_exception(e)
+```"""
+        self.check_webhook("webhook_event_for_exception_python", expected_topic, expected_message)
+
     def test_event_for_message_golang(self) -> None:
         expected_topic = "A test message event from golang."
         expected_message = """
