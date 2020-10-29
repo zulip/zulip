ea4febe7ee2a755ba92a873daa5c5ce45f627ac2
Abhijeet Prasad Bodas <abhijeetbodas2001@gmail.com>
2020-10-29 12:30:39 +0530

    logging: Add logging in deferred_work queue processor.
    Adds logging statements in deferred_work queue consume.

--- zerver/worker/queue_processors.py
+++ zerver/worker/queue_processors.py
@@ -768,6 +768,8 @@ class DeferredWorker(QueueProcessingWorker):
     MAX_CONSUME_SECONDS = None
 
     def consume(self, event: Dict[str, Any]) -> None:
+        logger.info("START deferred_work queue consume %s", event['type'])
+        start = time.time()
         if event['type'] == 'mark_stream_messages_as_read':
             user_profile = get_user_profile_by_id(event['user_profile_id'])
 
@@ -795,7 +797,6 @@ class DeferredWorker(QueueProcessingWorker):
                         event['user_profile_id'])
                 retry_event(self.queue_name, event, failure_processor)
         elif event['type'] == 'realm_export':
-            start = time.time()
             realm = Realm.objects.get(id=event['realm_id'])
             output_dir = tempfile.mkdtemp(prefix="zulip-export-")
             export_event = RealmAuditLog.objects.get(id=event['id'])
@@ -843,6 +844,9 @@ class DeferredWorker(QueueProcessingWorker):
                 "Completed data export for %s in %s",
                 user_profile.realm.string_id, time.time() - start,
             )
+
+        end = time.time()
+        logger.info("DONE deferred_work queue consume %s (%dms)", event['type'], (end-start)*1000)
 
 @assign_queue('test', is_test_queue=True)
 class TestWorker(QueueProcessingWorker):
