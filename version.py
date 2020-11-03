04fa084b516d07d7c45f66d6a391574592157acd
Aman Agrawal <amanagr@zulip.com>
2020-11-03 10:17:33 +0530

    dev_requirements: Upgrade zulint.
    This tells users how autofix errors for linters which support it.
    This is important since only way to fix prettier errors is
    running lint with `--fix` which now the linter will gladly print
    with the error.

--- version.py
+++ version.py
@@ -43,4 +43,4 @@ API_FEATURE_LEVEL = 35
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = '114.3'
+PROVISION_VERSION = '114.4'
