458927d242d38f35cccd6c75003fa34fe11df734
Mateusz Mandera <mateusz.mandera@zulip.com>
2020-12-13 17:14:42 +0100

    django3: Remove remaining postgresql_psycopg2 use.
    Removed in Django 3.0.

--- zerver/migrations/0261_pregistrationuser_clear_invited_as_admin.py
+++ zerver/migrations/0261_pregistrationuser_clear_invited_as_admin.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 from django.db import migrations
-from django.db.backends.postgresql_psycopg2.schema import DatabaseSchemaEditor
+from django.db.backends.postgresql.schema import DatabaseSchemaEditor
 from django.db.migrations.state import StateApps
 
 
