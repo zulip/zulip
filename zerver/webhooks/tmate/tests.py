6a777ae4d2a3202c32fc57bad9cce965a4ef8d13
Nicolas Pace <nicopace@gmail.com>
2021-01-21 00:27:01 -0300

    hook works.

--- zerver/webhooks/tmate/tests.py
+++ zerver/webhooks/tmate/tests.py
@@ -1,52 +1,16 @@
-from typing import Dict
-
 from zerver.lib.test_classes import WebhookTestCase
 
 
-class ZenDeskHookTests(WebhookTestCase):
-    STREAM_NAME = 'tmate'
-    URL_TEMPLATE = "/api/v1/external/tmate?stream={stream}"
+class TmateHookTests(WebhookTestCase):
+    STREAM_NAME = 'test'
+    URL_TEMPLATE = "/api/v1/external/tmate?&api_key={api_key}"
+    FIXTURE_DIR_NAME = 'tmate'
 
-    def get_payload(self, fixture_name: str) -> Dict[str, str]:
-        return {
-            'ticket_title': self.TICKET_TITLE,
-            'ticket_id': str(self.TICKET_ID),
-            'message': self.MESSAGE,
-            'stream': self.STREAM_NAME,
-        }
+    # Note: Include a test function per each distinct message condition your integration supports
+    def test_hello_message(self) -> None:
+        expected_topic = "Hello World";
+        expected_message = "Hello! I am happy to be here! :smile: \nThe Wikipedia featured article for today is **[Marilyn Monroe](https://en.wikipedia.org/wiki/Marilyn_Monroe)**";
 
-    def do_test(self, expected_topic: str, expected_message: str) -> None:
-        self.api_stream_message(
-            self.test_user,
-            "",
-            expected_topic,
-            expected_message,
-            content_type=None,
-        )
-
-    def test_short_topic(self) -> None:
-        self.TICKET_ID = 4
-        self.TICKET_TITLE = "Test ticket"
-        self.MESSAGE = "some message"
-        self.do_test(
-            expected_topic='#4: Test ticket',
-            expected_message="some message",
-        )
-
-    def test_long_subject(self) -> None:
-        self.TICKET_ID = 4
-        self.TICKET_TITLE = "Test ticket" + "!" * 80
-        self.MESSAGE = "some message"
-        self.do_test(
-            expected_topic="#4: Test ticket" + "!" * 42 + "...",
-            expected_message="some message",
-        )
-
-    def test_long_content(self) -> None:
-        self.TICKET_ID = 5
-        self.TICKET_TITLE = "Some ticket"
-        self.MESSAGE = "New comment:\n> It is better\n* here"
-        self.do_test(
-            expected_topic="#5: Some ticket",
-            expected_message="New comment:\n> It is better\n* here",
-        )
+        # use fixture named tmate_hello
+        self.check_webhook('session_open', expected_topic, expected_message,
+                           content_type="application/x-www-form-urlencoded")
