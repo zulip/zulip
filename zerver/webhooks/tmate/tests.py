07f04155d08989e856b6a59156830cef23138e97
Nicolas Pace <nicopace@gmail.com>
2021-01-23 15:06:44 -0300

    Added tests, implementation and logo.

--- zerver/webhooks/tmate/tests.py
+++ zerver/webhooks/tmate/tests.py
@@ -3,14 +3,38 @@ from zerver.lib.test_classes import WebhookTestCase
 
 class TmateHookTests(WebhookTestCase):
     STREAM_NAME = 'test'
-    URL_TEMPLATE = "/api/v1/external/tmate?&api_key={api_key}"
+    URL_TEMPLATE = "/api/v1/external/tmate?&api_key={api_key}&stream=test"
     FIXTURE_DIR_NAME = 'tmate'
 
-    # Note: Include a test function per each distinct message condition your integration supports
-    def test_hello_message(self) -> None:
-        expected_topic = "Hello World";
-        expected_message = "Hello! I am happy to be here! :smile: \nThe Wikipedia featured article for today is **[Marilyn Monroe](https://en.wikipedia.org/wiki/Marilyn_Monroe)**";
+    def test_session_register(self) -> None:
+        expected_topic = "Session a1ba02fc-5684-11eb-9f1d-0242ac150002";
+        expected_message = "There is a new session registered.\nConnect with: `ssh -p2222 kxEEYXAN8eQPpvWhnenxLXRmd@remotesupport.librerouter.org` or [this link](http://remotesupport.librerouter.org:4000/t/kxEEYXAN8eQPpvWhnenxLXRmd).";
 
         # use fixture named tmate_hello
-        self.check_webhook('session_open', expected_topic, expected_message,
+        self.check_webhook('session_register', expected_topic, expected_message,
+                           content_type="application/x-www-form-urlencoded")
+
+
+    def test_session_close(self) -> None:
+        expected_topic = "Session: a1ba02fc-5684-11eb-9f1d-0242ac150002";
+        expected_message = "Session closed.";
+
+        # use fixture named tmate_hello
+        self.check_webhook('session_close', expected_topic, expected_message,
+                           content_type="application/x-www-form-urlencoded")
+
+    def test_session_left(self) -> None:
+        expected_topic = "Session: a1ba02fc-5684-11eb-9f1d-0242ac150002";
+        expected_message = "Someone with id ae4b50c0-5684-11eb-9762-0242ac150002 left.";
+
+        # use fixture named tmate_hello
+        self.check_webhook('session_left', expected_topic, expected_message,
+                           content_type="application/x-www-form-urlencoded")
+
+    def test_session_join(self) -> None:
+        expected_topic = "Session: a1ba02fc-5684-11eb-9f1d-0242ac150002";
+        expected_message = "Someone from 181.44.61.146 id ae4b50c0-5684-11eb-9762-0242ac150002 joined through web.";
+
+        # use fixture named tmate_hello
+        self.check_webhook('session_join', expected_topic, expected_message,
                            content_type="application/x-www-form-urlencoded")
--- zerver/webhooks/tmate/view.py
+++ zerver/webhooks/tmate/view.py
@@ -7,11 +7,64 @@ from zerver.lib.response import json_success
 from zerver.lib.webhooks.common import check_send_webhook_message
 from zerver.models import UserProfile
 
-from pprint import pprint
+import json
 
-@webhook_view('Statuspage')
-def api_tmate_webhook(request: HttpRequest, user_profile: UserProfile) -> HttpResponse:
-    subject = "webhook test"
-    message = str(request)
+def api_tmate_webhook_register(request: HttpRequest, user_profile: UserProfile) -> HttpResponse:
+    json_body = json.loads(request.body)
+
+    subject_template = "Session {entity_id}"
+    subject = subject_template.format(**json_body)
+
+    message = 'There is a new session registered.\nConnect with: `%s` or [this link](%s).' % (json_body['params']['ssh_cmd_fmt'] % json_body['params']['stoken'], json_body['params']['web_url_fmt'] % json_body['params']['stoken'])
+
     check_send_webhook_message(request, user_profile, subject, message)
     return json_success()
+
+def api_tmate_webhook_join(request: HttpRequest, user_profile: UserProfile) -> HttpResponse:
+    json_body = json.loads(request.body)
+
+    subject_template = "Session: {entity_id}"
+    subject = subject_template.format(**json_body)
+
+    message = 'Someone from %s id %s joined through %s.' % (json_body['params']['ip_address'], json_body['params']['id'], json_body['params']['type'])
+
+    check_send_webhook_message(request, user_profile, subject, message)
+    return json_success()
+
+def api_tmate_webhook_left(request: HttpRequest, user_profile: UserProfile) -> HttpResponse:
+    json_body = json.loads(request.body)
+
+    subject_template = "Session: {entity_id}"
+    subject = subject_template.format(**json_body)
+
+    message = 'Someone with id %s left.' % json_body['params']['id']
+
+    check_send_webhook_message(request, user_profile, subject, message)
+    return json_success()
+
+def api_tmate_webhook_close(request: HttpRequest, user_profile: UserProfile) -> HttpResponse:
+    json_body = json.loads(request.body)
+
+    subject_template = "Session: {entity_id}"
+    subject = subject_template.format(**json_body)
+
+    message = 'Session closed.'
+
+    check_send_webhook_message(request, user_profile, subject, message)
+    return json_success()
+
+@webhook_view('Tmate')
+@has_request_variables
+def api_tmate_webhook(request: HttpRequest, user_profile: UserProfile) -> HttpResponse:
+    json_body = json.loads(request.body)
+
+    if json_body['type'] == "session_register":
+        return api_tmate_webhook_register(request, user_profile)
+    elif json_body['type'] == "session_join":
+        return api_tmate_webhook_join(request, user_profile)
+    elif json_body['type'] == "session_left":
+        return api_tmate_webhook_left(request, user_profile)
+    elif json_body['type'] == "session_close":
+        return api_tmate_webhook_close(request, user_profile)
+    else:
+        return
