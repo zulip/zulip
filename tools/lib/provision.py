7eeca9da4675384c3e2389a8c521a6c6a1e7e31e
Ganesh Pawar <pawarg256@gmail.com>
2021-02-02 17:06:59 +0530

    provision: Add provision support for Ubuntu 20.10(Groovy).
    PostgreSQL 13 is used when os_version is 20.10.

--- tools/lib/provision.py
+++ tools/lib/provision.py
@@ -96,6 +96,8 @@ elif vendor == "ubuntu" and os_version in ["19.04", "19.10"]:  # disco, eoan
     POSTGRESQL_VERSION = "11"
 elif vendor == "ubuntu" and os_version == "20.04":  # focal
     POSTGRESQL_VERSION = "12"
+elif vendor == "ubuntu" and os_version == "20.10":  # groovy
+    POSTGRESQL_VERSION = "13"
 elif vendor == "neon" and os_version == "20.04":  # KDE Neon
     POSTGRESQL_VERSION = "12"
 elif vendor == "fedora" and os_version == "29":
