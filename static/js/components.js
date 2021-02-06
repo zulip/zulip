zulip/zulip

commit f8fbae4d8ed16cceabc23806aa421e1481e14e8d

components: Generate properly escaped HTML.
Signed-off-by: Anders Kaseorg <anders@zulip.com>

static/js/components.js
@@ -23,15 +23,12 @@ exports.toggle = function (opts) {
         for (const [i, value] of opts.values.entries()) {
             // create a tab with a tab-id so they don't have to be referenced
             // by text value which can be inconsistent.
-            const tab = $(
-                "<div class='ind-tab' data-tab-key='" +
-                    value.key +
-                    "' data-tab-id='" +
-                    i +
-                    "' tabindex='0'>" +
-                    value.label +
-                    "</div>",
-            );
+            const tab = $("<div>", {
+                class: "ind-tab",
+                "data-tab-key": value.key,
+                "data-tab-id": i,
+                tabindex: 0,
+            }).text(value.label);
 
             // add proper classes for styling in CSS.
             if (i === 0) {

frontend_tests/node_tests/components.js
@@ -118,7 +118,7 @@ run_test("basics", () => {
         return self;
     })();
 
-    set_global("$", (sel) => {
+    set_global("$", (sel, attributes) => {
         if (sel.stub) {
             // The component often redundantly re-wraps objects.
             return sel;
@@ -129,12 +129,45 @@ run_test("basics", () => {
                 return switcher;
             case "<div class='tab-switcher stream_sorter_toggle'></div>":
                 return switcher;
-            case "<div class='ind-tab' data-tab-key='keyboard-shortcuts' data-tab-id='0' tabindex='0'>translated: Keyboard shortcuts</div>":
-                return make_tab(0);
-            case "<div class='ind-tab' data-tab-key='message-formatting' data-tab-id='1' tabindex='0'>translated: Message formatting</div>":
-                return make_tab(1);
-            case "<div class='ind-tab' data-tab-key='search-operators' data-tab-id='2' tabindex='0'>translated: Search operators</div>":
-                return make_tab(2);
+            case "<div>": {
+                const tab_id = attributes["data-tab-id"];
+                assert.deepEqual(
+                    attributes,
+                    [
+                        {
+                            class: "ind-tab",
+                            "data-tab-key": "keyboard-shortcuts",
+                            "data-tab-id": 0,
+                            tabindex: 0,
+                        },
+                        {
+                            class: "ind-tab",
+                            "data-tab-key": "message-formatting",
+                            "data-tab-id": 1,
+                            tabindex: 0,
+                        },
+                        {
+                            class: "ind-tab",
+                            "data-tab-key": "search-operators",
+                            "data-tab-id": 2,
+                            tabindex: 0,
+                        },
+                    ][tab_id],
+                );
+                return {
+                    text: (text) => {
+                        assert.equal(
+                            text,
+                            [
+                                "translated: Keyboard shortcuts",
+                                "translated: Message formatting",
+                                "translated: Search operators",
+                            ][tab_id],
+                        );
+                        return make_tab(tab_id);
+                    },
+                };
+            }
             default:
                 throw new Error("unknown selector: " + sel);
         }

commit 6bc83b38bf61915df6610d0a40360739bcb97e0c

components: Allow passing labels as HTML (but explicitly).
Fixes the sorting button labels in stream settings, which were
regressed by commit f8fbae4d8ed16cceabc23806aa421e1481e14e8d (because
the HTML was not marked as being HTML).

Signed-off-by: Anders Kaseorg <anders@zulip.com>

static/js/components.js
@@ -28,7 +28,15 @@ exports.toggle = function (opts) {
                 "data-tab-key": value.key,
                 "data-tab-id": i,
                 tabindex: 0,
-            }).text(value.label);
+            });
+
+            /* istanbul ignore if */
+            if (value.label_html !== undefined) {
+                const html = value.label_html;
+                tab.html(html);
+            } else {
+                tab.text(value.label);
+            }
 
             // add proper classes for styling in CSS.
             if (i === 0) {

static/js/subs.js
@@ -559,17 +559,19 @@ exports.setup_page = function (callback) {
         exports.sort_toggler = components.toggle({
             values: [
                 {
-                    label: `<i class="fa fa-sort-alpha-asc" title="${i18n.t("Sort by name")}"></i>`,
+                    label_html: `<i class="fa fa-sort-alpha-asc" title="${i18n.t(
+                        "Sort by name",
+                    )}"></i>`,
                     key: "by-stream-name",
                 },
                 {
-                    label: `<i class="fa fa-user-o" title="${i18n.t(
+                    label_html: `<i class="fa fa-user-o" title="${i18n.t(
                         "Sort by number of subscribers",
                     )}"></i>`,
                     key: "by-subscriber-count",
                 },
                 {
-                    label: `<i class="fa fa-bar-chart" title="${i18n.t(
+                    label_html: `<i class="fa fa-bar-chart" title="${i18n.t(
                         "Sort by estimated weekly traffic",
                     )}"></i>`,
                     key: "by-weekly-traffic",

frontend_tests/node_tests/components.js
@@ -51,6 +51,17 @@ run_test("basics", () => {
             return i;
         };
 
+        self.text = function (text) {
+            assert.equal(
+                text,
+                [
+                    "translated: Keyboard shortcuts",
+                    "translated: Message formatting",
+                    "translated: Search operators",
+                ][i],
+            );
+        };
+
         self.trigger = function (type) {
             if (type === "focus") {
                 focused_tab = i;
@@ -154,19 +165,7 @@ run_test("basics", () => {
                         },
                     ][tab_id],
                 );
-                return {
-                    text: (text) => {
-                        assert.equal(
-                            text,
-                            [
-                                "translated: Keyboard shortcuts",
-                                "translated: Message formatting",
-                                "translated: Search operators",
-                            ][tab_id],
-                        );
-                        return make_tab(tab_id);
-                    },
-                };
+                return make_tab(tab_id);
             }
             default:
                 throw new Error("unknown selector: " + sel);