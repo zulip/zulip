fa076d16b662674f056268909bdd61444e5ff917
Anders Kaseorg <anders@zulip.com>
2021-02-10 08:11:54 -0800

    js: Convert static/js/billing/billing.js to ES6 module.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- static/js/global.d.ts
+++ static/js/global.d.ts
@@ -13,7 +13,6 @@ declare let admin: any;
 declare let alert_words_ui: any;
 declare let attachments_ui: any;
 declare let avatar: any;
-declare let billing: any;
 declare let blueslip: any;
 declare let bot_data: any;
 declare let buddy_data: any;
