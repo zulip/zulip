97ff721074d1ebfd31878539d8503841b78d6eb4
Anders Kaseorg <anders@zulip.com>
2021-02-10 08:14:36 -0800

    js: Convert static/js/stats/stats.js to ES6 module.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- static/js/stats/stats.js
+++ static/js/stats/stats.js
@@ -1,6 +1,4 @@
-"use strict";
-
-const Plotly = require("plotly.js/lib/core");
+import Plotly from "plotly.js/lib/core";
 
 Plotly.register([require("plotly.js/lib/bar"), require("plotly.js/lib/pie")]);
 
