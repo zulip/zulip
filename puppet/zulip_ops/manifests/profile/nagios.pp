8f3283936047191b24bd280f8298ede915ffb4ec
Alex Vandiver <alexmv@zulip.com>
2021-02-19 14:49:25 -0800

    puppet: Remove puppet_ops configuration for separate loadbalancer host.

--- puppet/zulip_ops/manifests/profile/nagios.pp
+++ puppet/zulip_ops/manifests/profile/nagios.pp
@@ -27,7 +27,6 @@ class zulip_ops::profile::nagios {
   $hosts_postgresql_primary = split(zulipconf('nagios', 'hosts_postgresql_primary', undef), ',')
   $hosts_postgresql_replica = split(zulipconf('nagios', 'hosts_postgresql_replica', undef), ',')
   $hosts_redis = split(zulipconf('nagios', 'hosts_redis', undef), ',')
-  $hosts_loadbalancer = split(zulipconf('nagios', 'hosts_loadbalancer', undef), ',')
   $hosts_stats = split(zulipconf('nagios', 'hosts_stats', undef), ',')
   $hosts_fullstack = split(zulipconf('nagios', 'hosts_fullstack', undef), ',')
 
