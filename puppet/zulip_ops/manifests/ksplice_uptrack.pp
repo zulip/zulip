a759d26a327cd4337d68eaa1d45d6a69edc9161c
Alex Vandiver <alexmv@zulip.com>
2021-02-26 14:49:02 -0800

    puppet: Make ksplice config not world-readable, use 'adm' group.
    This matches the configuration that ksplice itself creates the file
    and directory with.

--- puppet/zulip_ops/manifests/ksplice_uptrack.pp
+++ puppet/zulip_ops/manifests/ksplice_uptrack.pp
@@ -2,15 +2,15 @@ class zulip_ops::ksplice_uptrack {
   file { '/etc/uptrack':
     ensure => 'directory',
     owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
+    group  => 'adm',
+    mode   => '0750',
   }
   $ksplice_access_key = zulipsecret('secrets','ksplice_access_key','')
   file { '/etc/uptrack/uptrack.conf':
     ensure  => file,
     owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
+    group   => 'adm',
+    mode    => '0640',
     content => template('zulip_ops/uptrack/uptrack.conf.erb'),
   }
   $setup_apt_repo_file = "${::zulip_scripts_path}/lib/setup-apt-repo-ksplice"
