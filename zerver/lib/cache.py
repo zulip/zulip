28eff4adfc555d45af8778c1a7791a497612c150
Mateusz Mandera <mateusz.mandera@zulip.com>
2021-03-04 14:36:31 +0100

    actions: Change caching of create_mirror_user_if_needed.
    Emails are not unique, so we can only sensibly cache using keys formed
    with both email and realm.
    
    This requires adding a new cache key function for caching by delivery
    email - user_profile_delivery_email_cache_key.

--- zerver/lib/cache.py
+++ zerver/lib/cache.py
@@ -502,6 +502,10 @@ def user_profile_cache_key(email: str, realm: "Realm") -> str:
     return user_profile_cache_key_id(email, realm.id)
 
 
+def user_profile_delivery_email_cache_key(delivery_email: str, realm: "Realm") -> str:
+    return f"user_profile_by_delivery_email:{make_safe_digest(delivery_email.strip())}:{realm.id}"
+
+
 def bot_profile_cache_key(email: str) -> str:
     return f"bot_profile:{make_safe_digest(email.strip())}"
 
@@ -586,6 +590,9 @@ def delete_user_profile_caches(user_profiles: Iterable["UserProfile"]) -> None:
         for api_key in get_all_api_keys(user_profile):
             keys.append(user_profile_by_api_key_cache_key(api_key))
         keys.append(user_profile_cache_key(user_profile.email, user_profile.realm))
+        keys.append(
+            user_profile_delivery_email_cache_key(user_profile.delivery_email, user_profile.realm)
+        )
         if user_profile.is_bot and is_cross_realm_bot_email(user_profile.email):
             # Handle clearing system bots from their special cache.
             keys.append(bot_profile_cache_key(user_profile.email))
