e19eb9ef59b0cf062306338e3e3bc0103a201df9
Cyril Pletinckx <cyril.pletinckx@student.uclouvain.be>
2021-03-04 15:58:20 +0100

    import/export: Fix deprecated authentication method for Slack.
    The query string parameter authentication method is now deprecated for
    newly created Slack applications since the 24th of February[1].  This
    causes Slack imports to fail, claiming that the token has none of the
    required scopes.
    
    Two methods can be used to solve this problem: either include the
    authentication token in the header of an HTTP GET request, or include
    it in the body of an HTTP POST request. The former is preferred, as
    the code was already written to use HTTP GET requests.
    
    Change the way the parameters are passed to the "requests.get" method
    calls, to pass the token via the `Authorization` header.
    
    [1] https://api.slack.com/changelog/2020-11-no-more-tokens-in-querystrings-for-newly-created-apps
    
    Fixes: #17408.

--- zerver/tests/test_slack_importer.py
+++ zerver/tests/test_slack_importer.py
@@ -58,13 +58,15 @@ class MockResponse:
         return self.json_data
 
 # This method will be used by the mock to replace requests.get
-def mocked_requests_get(*args: List[str], **kwargs: List[str]) -> MockResponse:
-    if args[0] == 'https://slack.com/api/users.list?token=xoxp-valid-token':
-        return MockResponse({"ok": True, "members": "user_data"}, 200)
-    elif args[0] == 'https://slack.com/api/users.list?token=xoxp-invalid-token':
-        return MockResponse({"ok": False, "error": "invalid_auth"}, 200)
-    else:
+def mocked_requests_get(*args: str, headers: Dict[str, str]) -> MockResponse:
+    if args != ("https://slack.com/api/users.list",):
         return MockResponse(None, 404)
+
+    if (headers.get("Authorization") != "Bearer xoxp-valid-token"):
+        return MockResponse({"ok": False, "error": "invalid_auth"}, 200)
+
+    return MockResponse({"ok": True, "members": "user_data"}, 200)
+
 
 class SlackImporter(ZulipTestCase):
     logger = logging.getLogger()
