a124a4dfcf607e1c20dae77c820c9905060b034f
Mateusz Mandera <mateusz.mandera@zulip.com>
2021-03-08 11:48:14 +0100

    bots: Pass realm to get_system_bot calls in actions.py.

--- zerver/tests/test_users.py
+++ zerver/tests/test_users.py
@@ -771,8 +771,8 @@ class QueryCountTest(ZulipTestCase):
                         acting_user=None,
                     )
 
-        self.assert_length(queries, 83)
-        self.assert_length(cache_tries, 26)
+        self.assert_length(queries, 84)
+        self.assert_length(cache_tries, 27)
 
         peer_add_events = [event for event in events if event["event"].get("op") == "peer_add"]
 
