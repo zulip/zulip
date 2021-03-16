cdb41ef078d22b18d8220eaedfa665c8afabbaa2
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-03-16 11:06:22 +0530

    default_streams_list: Add a clear search button in the input field.

--- static/js/settings_streams.js
+++ static/js/settings_streams.js
@@ -57,6 +57,11 @@ export function build_default_stream_table() {
     });
 
     loading.destroy_indicator($("#admin_page_default_streams_loading_indicator"));
+
+    $("#clear_default_streams_list_search_button").on("click", ()=>{
+        $("#default_streams_list_search").val("");
+        build_default_stream_table();
+    });
 }
 
 export function update_default_streams_table() {
