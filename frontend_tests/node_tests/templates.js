2e910842d8ae7e4037a535211a1454aacd68e44f
YashRE42 <33805964+YashRE42@users.noreply.github.com>
2021-03-21 12:29:44 +0000

    template.js: Add test for "or" helper.

--- frontend_tests/node_tests/templates.js
+++ frontend_tests/node_tests/templates.js
@@ -15,3 +15,12 @@ run_test("and", () => {
     const html = require("./templates/and.hbs")(args);
     assert.equal(html, "<p>empty and</p>\n<p>last and</p>\n\n");
 });
+
+run_test("or", () => {
+    const args = {
+        last: true,
+    };
+
+    const html = require("./templates/or.hbs")(args);
+    assert.equal(html, "\n<p>last or</p>\n<p>true or</p>\n");
+});
