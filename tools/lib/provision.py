zulip/zulip

commit 5c20ee998c2c79a08fba8e2edc827fb058ab6c8d

build-tsearch-extras: Add support for Debian/Ubuntu.

tools/lib/provision.py
@@ -14,7 +14,8 @@ ZULIP_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__f
 
 sys.path.append(ZULIP_PATH)
 from scripts.lib.zulip_tools import run, run_as_root, OKBLUE, ENDC, WARNING, \
-    get_dev_uuid_var_path, FAIL, parse_lsb_release, file_or_package_hash_updated
+    get_dev_uuid_var_path, FAIL, parse_lsb_release, file_or_package_hash_updated, \
+    overwrite_symlink
 from scripts.lib.setup_venv import (
     VENV_DEPENDENCIES, REDHAT_VENV_DEPENDENCIES,
     THUMBOR_VENV_DEPENDENCIES, YUM_THUMBOR_VENV_DEPENDENCIES,
@@ -337,6 +338,12 @@ def install_yum_deps(deps_to_install, retry=False):
     # Use vendored pg_hba.conf, which enables password authentication.
     run_as_root(["cp", "-a", "puppet/zulip/files/postgresql/centos_pg_hba.conf", pg_hba_conf])
     # Later steps will ensure postgres is started
+
+    # Link in tsearch data files
+    overwrite_symlink("/usr/share/myspell/en_US.dic", "/usr/pgsql-%s/share/tsearch_data/en_us.dict"
+                      % (POSTGRES_VERSION,))
+    overwrite_symlink("/usr/share/myspell/en_US.aff", "/usr/pgsql-%s/share/tsearch_data/en_us.affix"
+                      % (POSTGRES_VERSION,))
 
 def main(options):
     # type: (Any) -> int

scripts/lib/build-tsearch-extras
@@ -2,14 +2,13 @@
 set -x
 set -e
 
-# Has only been tested on Red Hat systems; gmake is wrong for others.
-
 cd "$(mktemp -d)"
+distro=$(lsb_release -is)
 
 git clone https://github.com/zulip/tsearch_extras
-(cd tsearch_extras && gmake && make install)
-
-PGVER=10
-# Link installed hunspell-en-US to tsearch data
-ln -sf /usr/share/myspell/en_US.dic "/usr/pgsql-$PGVER/share/tsearch_data/en_us.dict"
-ln -sf /usr/share/myspell/en_US.aff "/usr/pgsql-$PGVER/share/tsearch_data/en_us.affix"
+if [ "$distro" = "Ubuntu" ] || [ "$distro" == "Debian" ]; then
+    make="make"
+else
+    make="gmake"
+fi
+(cd tsearch_extras && "$make" && "$make" install)

commit c1628e7605c543bd564d36348b33e55c68ad2a88

provision: Upgrade support for Fedora to version 33.
Note that the `overwrite_symlink` changes fix a bug introduced in
5c20ee998c2c79a08fba8e2edc827fb058ab6c8d, that we need root
permissions to do those operations.

scripts/lib/setup-yum-repo
@@ -53,6 +53,7 @@ elif [ "$is_rhel" = true ]; then
     yum localinstall -y https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-latest-x86_64/pgdg-redhat10-10-2.noarch.rpm
     yum localinstall -y https://packages.groonga.org/centos/groonga-release-latest.noarch.rpm
 else
-    # TODO only fedora29 for now
-    dnf install -y "https://download.postgresql.org/pub/repos/yum/$PGVER/fedora/fedora-29-x86_64/pgdg-fedora$PGVER-$PGVER-4.noarch.rpm"
+    # TODO only fedora33 for now
+    PGVER=13
+    dnf install -y "https://download.postgresql.org/pub/repos/yum/reporpms/F-33-x86_64/pgdg-fedora-repo-latest.noarch.rpm"
 fi

tools/lib/provision.py
@@ -26,7 +26,6 @@ from scripts.lib.zulip_tools import (
     WARNING,
     get_dev_uuid_var_path,
     os_families,
-    overwrite_symlink,
     parse_os_release,
     run_as_root,
 )
@@ -108,8 +107,8 @@ elif vendor == "ubuntu" and os_version == "20.10":  # groovy
     POSTGRESQL_VERSION = "13"
 elif vendor == "neon" and os_version == "20.04":  # KDE Neon
     POSTGRESQL_VERSION = "12"
-elif vendor == "fedora" and os_version == "29":
-    POSTGRESQL_VERSION = "10"
+elif vendor == "fedora" and os_version == "33":
+    POSTGRESQL_VERSION = "13"
 elif vendor == "rhel" and os_version.startswith("7."):
     POSTGRESQL_VERSION = "10"
 elif vendor == "centos" and os_version == "7":
@@ -135,18 +134,7 @@ COMMON_DEPENDENCIES = [
     "unzip",  # Needed for Slack import
     "crudini",  # Used for shell tooling w/ zulip.conf
     # Puppeteer dependencies from here
-    "gconf-service",
-    "libgconf-2-4",
-    "libgtk-3-0",
-    "libatk-bridge2.0-0",
-    "libx11-xcb1",
-    "libxcb-dri3-0",
-    "libgbm1",
-    "libxss1",
-    "fonts-freefont-ttf",
-    "libappindicator1",
     "xdg-utils",
-    "xvfb",
     # Puppeteer dependencies end here.
 ]
 
@@ -156,6 +144,19 @@ UBUNTU_COMMON_APT_DEPENDENCIES = [
     "hunspell-en-us",
     "puppet-lint",
     "default-jre-headless",  # Required by vnu-jar
+    # Puppeteer dependencies from here
+    "fonts-freefont-ttf",
+    "gconf-service",
+    "libappindicator1",
+    "libatk-bridge2.0-0",
+    "libgbm1",
+    "libgconf-2-4",
+    "libgtk-3-0",
+    "libx11-xcb1",
+    "libxcb-dri3-0",
+    "libxss1",
+    "xvfb",
+    # Puppeteer dependencies end here.
     *THUMBOR_VENV_DEPENDENCIES,
 ]
 
@@ -165,6 +166,17 @@ COMMON_YUM_DEPENDENCIES = [
     "hunspell-en-US",
     "rubygem-puppet-lint",
     "nmap-ncat",
+    "ccache",  # Required to build pgroonga from source.
+    # Puppeteer dependencies from here
+    "at-spi2-atk",
+    "GConf2",
+    "gtk3",
+    "libX11-xcb",
+    "libxcb",
+    "libXScrnSaver",
+    "mesa-libgbm",
+    "xorg-x11-server-Xvfb",
+    # Puppeteer dependencies end here.
     *YUM_THUMBOR_VENV_DEPENDENCIES,
 ]
 
@@ -323,13 +335,21 @@ def install_yum_deps(deps_to_install: List[str]) -> None:
     # Later steps will ensure PostgreSQL is started
 
     # Link in tsearch data files
-    overwrite_symlink(
-        "/usr/share/myspell/en_US.dic",
-        f"/usr/pgsql-{POSTGRESQL_VERSION}/share/tsearch_data/en_us.dict",
+    run_as_root(
+        [
+            "ln",
+            "-nsf",
+            "/usr/share/myspell/en_US.dic",
+            f"/usr/pgsql-{POSTGRESQL_VERSION}/share/tsearch_data/en_us.dict",
+        ]
     )
-    overwrite_symlink(
-        "/usr/share/myspell/en_US.aff",
-        f"/usr/pgsql-{POSTGRESQL_VERSION}/share/tsearch_data/en_us.affix",
+    run_as_root(
+        [
+            "ln",
+            "-nsf",
+            "/usr/share/myspell/en_US.aff",
+            f"/usr/pgsql-{POSTGRESQL_VERSION}/share/tsearch_data/en_us.affix",
+        ]
     )
 
 