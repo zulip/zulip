a00f5dd90e78de5813b5d1721d6dce12c223ea50
PIG208 <359101898@qq.com>
2021-03-26 03:41:46 +0800

    static: Make alert-box available for portico pages.

--- static/js/bundles/portico.js
+++ static/js/bundles/portico.js
@@ -1,5 +1,6 @@
 import "./common";
 import "../i18n";
+import "../portico/alert_popup";
 import "../portico/header";
 import "../portico/google-analytics";
 import "../../styles/portico/portico_styles.css";
---
+++ static/js/portico/alert_popup.js
@@ -0,0 +1,7 @@
+import $ from "jquery";
+
+import * as alert_popup from "../alert_popup";
+
+$(() => {
+    alert_popup.initialize();
+});
