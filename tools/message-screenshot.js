f6998d6feedc52746398246ea8987aaf71b749eb
Riken Shah <rikenshah.02@gmail.com>
2021-04-01 18:07:19 +0530

    puppeteer_tests: Remove sequential numbers from test files.
    The only downside of this is that it makes it harder to control the
    order of these tests; which isn't that important.  And the structure
    of naming each with its test order fundamentally requires renaming
    files when adding/deleting tests, so if we want to control the default
    test order, we'd be better off doing that by just hardcoding a list in
    the test runner code.

--- tools/message-screenshot.js
+++ tools/message-screenshot.js
@@ -26,7 +26,7 @@ if (options.messageId === undefined) {
     process.exit(1);
 }
 
-// TODO: Refactor to share code with frontend_tests/puppeteer_tests/01-realm-creation.js
+// TODO: Refactor to share code with frontend_tests/puppeteer_tests/realm-creation.ts
 async function run() {
     const browser = await puppeteer.launch({
         args: [
