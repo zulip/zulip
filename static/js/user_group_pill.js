be7021268a5d3687d6cba33cfdf2378b35fa4d6f
m-e-l-u-h-a-n <purushottam.tiwari.cd.cse19@itbhu.ac.in>
2021-04-05 02:01:02 +0530

    setting(stream): Support user group pills when typeahead is unused.
    This extra commit adds support for creating user group pills
    in cases that do not use typeahead like, pasting the group
    name, copying it from the user group pill.
    
    This completes the remaining work required to support
    addition of all members of a user groups to stream.
    
    Fixes #15186.

--- static/js/user_group_pill.js
+++ static/js/user_group_pill.js
@@ -1,5 +1,34 @@
 import * as user_groups from "./user_groups";
 
+function display_pill(group) {
+    return group.name + ": " + group.members.size + " users";
+}
+
+export function create_item_from_group_name(group_name, current_items) {
+    group_name = group_name.trim();
+    const group = user_groups.get_user_group_from_name(group_name);
+    if (!group) {
+        return undefined;
+    }
+
+    const in_current_items = current_items.find((item) => item.id === group.id);
+    if (in_current_items !== undefined) {
+        return undefined;
+    }
+
+    const item = {
+        display_value: display_pill(group),
+        id: group.id,
+        group_name: group.name,
+    };
+
+    return item;
+}
+
+export function get_group_name_from_item(item) {
+    return item.group_name;
+}
+
 function get_user_ids_from_user_groups(items) {
     let user_ids = [];
     const group_ids = items.map((item) => item.id).filter(Boolean);
@@ -22,7 +51,7 @@ export function get_user_ids(pill_widget) {
 export function append_user_group(group, pill_widget) {
     if (group !== undefined && group !== null) {
         pill_widget.appendValidatedData({
-            display_value: group.name + ": " + group.members.size + " users",
+            display_value: display_pill(group),
             id: group.id,
         });
         pill_widget.clear_text();
