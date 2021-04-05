zulip/zulip

commit 1eafb1d8b3af0a2cee3c3d58647ca3829bc5419c

recent_topics: Move from overlay to a narrow-like view.
Recent Topics is no longer an overlay now, but note that it is
also not a typical messages narrow. It can reside between
an overlay and a Filter in the sense that it is dispalyed as
a typical Filter narrow but has properties of an Overlay.

Compose box is not visible in this view as it will be confusing
to many users and hence compose shortcuts have also been disabled.

Keyboard shortcuts that apply on messages have also been disabled.

The remaining shortcuts that apply to a narrow are still accessible
here.

static/js/hotkey.js
@@ -476,7 +476,11 @@ exports.process_hotkey = function (e, hotkey) {
         case "vim_right":
         case "tab":
         case "shift_tab":
-            if (overlays.recent_topics_open()) {
+            if (
+                window.location.hash === "#recent_topics" &&
+                !popovers.any_active() &&
+                !overlays.is_active()
+            ) {
                 return recent_topics.change_focused_element(e, event_name);
             }
     }
@@ -520,10 +524,6 @@ exports.process_hotkey = function (e, hotkey) {
         }
         if (event_name === "open_drafts" && overlays.drafts_open()) {
             overlays.close_overlay("drafts");
-            return true;
-        }
-        if (event_name === "open_recent_topics" && overlays.recent_topics_open()) {
-            overlays.close_overlay("recent_topics");
             return true;
         }
         return false;
@@ -700,12 +700,6 @@ exports.process_hotkey = function (e, hotkey) {
 
     // Shortcuts that don't require a message
     switch (event_name) {
-        case "compose": // 'c': compose
-            compose_actions.start("stream", {trigger: "compose_hotkey"});
-            return true;
-        case "compose_private_message":
-            compose_actions.start("private", {trigger: "compose_hotkey"});
-            return true;
         case "narrow_private":
             return do_narrow_action((target, opts) => {
                 narrow.by("is", "private", opts);
@@ -738,6 +732,26 @@ exports.process_hotkey = function (e, hotkey) {
         case "p_key":
             narrow.narrow_to_next_pm_string();
             return true;
+        case "open_recent_topics":
+            hashchange.go_to_location("#");
+            return true;
+    }
+
+    // We don't want hotkeys below this to work when recent topics is
+    // open. These involve compose box hotkeys and hotkeys that can only
+    // be done performed on a message.
+    if (recent_topics.is_visible()) {
+        return true;
+    }
+
+    // Compose box hotkeys
+    switch (event_name) {
+        case "compose": // 'c': compose
+            compose_actions.start("stream", {trigger: "compose_hotkey"});
+            return true;
+        case "compose_private_message":
+            compose_actions.start("private", {trigger: "compose_hotkey"});
+            return true;
         case "open_drafts":
             drafts.launch();
             return true;
@@ -754,9 +768,6 @@ exports.process_hotkey = function (e, hotkey) {
             return true;
         case "copy_with_c":
             copy_and_paste.copy_handler();
-            return true;
-        case "open_recent_topics":
-            hashchange.go_to_location("recent_topics");
             return true;
     }
 

frontend_tests/node_tests/hotkey.js
@@ -74,6 +74,9 @@ set_global("current_msg_list", {
         return 101;
     },
 });
+set_global("recent_topics", {
+    is_visible: () => false,
+});
 
 function return_true() {
     return true;
@@ -251,7 +254,6 @@ run_test("basic_chars", () => {
                     is_active,
                     settings_open,
                     info_overlay_open,
-                    recent_topics_open: return_false,
                 });
                 test_normal_typing();
             }
@@ -264,7 +266,6 @@ run_test("basic_chars", () => {
     overlays.streams_open = return_false;
     overlays.lightbox_open = return_false;
     overlays.drafts_open = return_false;
-    overlays.recent_topics = return_false;
 
     page_params.can_create_streams = true;
     overlays.streams_open = return_true;
@@ -298,15 +299,6 @@ run_test("basic_chars", () => {
     test_normal_typing();
     overlays.is_active = return_false;
     assert_mapping("d", drafts, "launch");
-
-    // Test opening and closing of recent topics
-    overlays.is_active = return_true;
-    overlays.recent_topics_open = return_true;
-    assert_mapping("t", overlays, "close_overlay");
-    overlays.recent_topics_open = return_false;
-    test_normal_typing();
-    overlays.is_active = return_false;
-    assert_mapping("t", hashchange, "go_to_location");
 
     // Next, test keys that only work on a selected message.
     const message_view_only_keys = "@+>RjJkKsSuvi:GM";

frontend_tests/node_tests/hashchange.js
@@ -18,10 +18,12 @@ const hash_util = zrequire("hash_util");
 const hashchange = zrequire("hashchange");
 const stream_data = zrequire("stream_data");
 zrequire("navigate");
+zrequire("recent_topics");
 
 set_global("search", {
     update_button_visibility: () => {},
 });
+set_global((recent_topics.is_visible = () => false));
 set_global("document", "document-stub");
 const history = set_global("history", {});
 

frontend_tests/node_tests/recent_topics.js
@@ -8,17 +8,26 @@ const {run_test} = require("../zjsunit/test");
 const $ = require("../zjsunit/zjquery");
 
 zrequire("message_util");
+zrequire("narrow_state");
 
 const noop = () => {};
-
-set_global("hashchange", {
-    exit_overlay: noop,
+set_global("top_left_corner", {
+    narrow_to_recent_topics: noop,
 });
-const overlays = set_global("overlays", {
-    open_overlay: (opts) => {
-        overlays.close_callback = opts.on_close;
-    },
-    recent_topics_open: () => true,
+set_global("stream_list", {
+    handle_narrow_deactivated: noop,
+});
+set_global("compose_actions", {
+    cancel: noop,
+});
+set_global("narrow", {
+    narrow_title: "",
+});
+set_global("notifications", {
+    redraw_title: noop,
+});
+set_global("message_view_header", {
+    render_title_area: noop,
 });
 
 const people = zrequire("people");
@@ -73,6 +82,9 @@ const ListWidget = set_global("ListWidget", {
     },
     hard_redraw: noop,
     render_item: (item) => ListWidget.modifier(item),
+});
+set_global("drafts", {
+    update_draft: noop,
 });
 
 // Custom Data
@@ -283,7 +295,7 @@ function verify_topic_data(all_topics, stream, topic, last_msg_id, participated)
     assert.equal(topic_data.participated, participated);
 }
 
-let rt = zrequire("recent_topics");
+let rt = reset_module("recent_topics");
 
 function stub_out_filter_buttons() {
     // TODO: We probably want more direct tests that make sure
@@ -299,7 +311,7 @@ function stub_out_filter_buttons() {
     }
 }
 
-run_test("test_recent_topics_launch", () => {
+run_test("test_recent_topics_show", () => {
     // Note: unread count and urls are fake,
     // since they are generated in external libraries
     // and are not to be tested here.
@@ -325,10 +337,7 @@ run_test("test_recent_topics_launch", () => {
     rt = reset_module("recent_topics");
     rt.process_messages(messages);
 
-    rt.launch();
-
-    // Test if search text is selected
-    overlays.close_callback();
+    rt.show();
 
     // incorrect topic_key
     assert.equal(rt.inplace_rerender("stream_unknown:topic_unknown"), false);
@@ -360,8 +369,8 @@ run_test("test_filter_all", () => {
     row_data = generate_topic_data([[1, "topic-1", 0, false, true]]);
     i = row_data.length;
     rt = reset_module("recent_topics");
-
     stub_out_filter_buttons();
+    rt.is_visible = () => true;
     rt.set_filter("all");
     rt.process_messages([messages[0]]);
 
@@ -401,6 +410,7 @@ run_test("test_filter_unread", () => {
     let i = 0;
 
     rt = reset_module("recent_topics");
+    rt.is_visible = () => true;
 
     stub_templates(() => "<recent_topics table stub>");
     stub_out_filter_buttons();
@@ -466,9 +476,11 @@ run_test("test_filter_participated", () => {
     let i = 0;
 
     rt = reset_module("recent_topics");
+    rt.is_visible = () => true;
     stub_templates(() => "<recent_topics table stub>");
     stub_out_filter_buttons();
     rt.process_messages(messages);
+
     assert.equal(rt.inplace_rerender("1:topic-4"), true);
 
     // Set muted filter
@@ -522,6 +534,7 @@ stub_templates(() => "<recent_topics table stub>");
 run_test("basic assertions", () => {
     rt = reset_module("recent_topics");
     stub_out_filter_buttons();
+    rt.is_visible = () => true;
     rt.set_filter("all");
     rt.process_messages(messages);
     let all_topics = rt.get();
@@ -601,6 +614,7 @@ run_test("basic assertions", () => {
 run_test("test_reify_local_echo_message", () => {
     rt = reset_module("recent_topics");
     stub_out_filter_buttons();
+    rt.is_visible = () => true;
     rt.set_filter("all");
     rt.process_messages(messages);
 
@@ -765,6 +779,7 @@ run_test("test_topic_edit", () => {
 
 run_test("test_search", () => {
     rt = reset_module("recent_topics");
+    rt.is_visible = () => true;
     assert.equal(rt.topic_in_search_results("t", "general", "Recent Topic"), true);
     assert.equal(rt.topic_in_search_results("T", "general", "Recent Topic"), true);
     assert.equal(rt.topic_in_search_results("to", "general", "Recent Topic"), true);

frontend_tests/node_tests/top_left_corner.js
@@ -82,7 +82,15 @@ run_test("narrowing", () => {
     assert(!$(".top_left_mentions").hasClass("active-filter"));
     assert(!$(".top_left_private_messages").hasClass("active-filter"));
     assert(!$(".top_left_starred_messages").hasClass("active-filter"));
+    assert(!$(".top_left_recent_topics").hasClass("active-filter"));
     assert(pm_closed);
+
+    top_left_corner.narrow_to_recent_topics();
+    assert(!$(".top_left_all_messages").hasClass("active-filter"));
+    assert(!$(".top_left_mentions").hasClass("active-filter"));
+    assert(!$(".top_left_private_messages").hasClass("active-filter"));
+    assert(!$(".top_left_starred_messages").hasClass("active-filter"));
+    assert($(".top_left_recent_topics").hasClass("active-filter"));
 });
 
 run_test("update_count_in_dom", () => {

frontend_tests/node_tests/tutorial.js
@@ -437,9 +437,9 @@ const message_events = zrequire("message_events");
 
 run_test("insert_message", (override) => {
     override(pm_list, "update_private_messages", noop);
-    override(overlays, "recent_topics_open", () => false);
 
     const helper = test_helper();
+    set_global((recent_topics.is_visible = () => false));
 
     const new_message = {
         sender_id: isaac.user_id,
@@ -635,7 +635,6 @@ run_test("unread_ops", (override) => {
 
     // Let the real code skip over details related to active overlays.
     override(overlays, "is_active", () => false);
-    override(overlays, "recent_topics_open", () => false);
 
     // First, test for a message list that cannot read messages.  Here
     // we use with_field to limit the scope of our stub function.

frontend_tests/node_tests/ui_init.js
@@ -111,6 +111,9 @@ zrequire("topic_list");
 zrequire("sent_messages");
 zrequire("top_left_corner");
 zrequire("starred_messages");
+zrequire("user_status");
+zrequire("user_status_ui");
+zrequire("recent_topics");
 
 const ui_init = rewiremock.proxy(() => zrequire("ui_init"), {
     "../../static/js/emojisets": {

static/js/hashchange.js
@@ -36,10 +36,19 @@ function set_hash(hash) {
     }
 }
 
+function maybe_hide_recent_topics() {
+    if (recent_topics.is_visible()) {
+        recent_topics.hide();
+        return true;
+    }
+    return false;
+}
+
 exports.changehash = function (newhash) {
     if (changing_hash) {
         return;
     }
+    maybe_hide_recent_topics();
     message_viewport.stop_auto_scrolling();
     set_hash(newhash);
 };
@@ -53,8 +62,9 @@ exports.save_narrow = function (operators) {
 };
 
 function activate_home_tab() {
+    const coming_from_recent_topics = maybe_hide_recent_topics();
     ui_util.change_tab_to("#message_feed_container");
-    narrow.deactivate();
+    narrow.deactivate(coming_from_recent_topics);
     floating_recipient_bar.update();
     search.update_button_visibility();
     // We need to maybe scroll to the selected message
@@ -76,7 +86,6 @@ function is_overlay_hash(hash) {
         "settings",
         "organization",
         "invite",
-        "recent_topics",
         "keyboard-shortcuts",
         "message-formatting",
         "search-operators",
@@ -96,6 +105,7 @@ function do_hashchange_normal(from_reload) {
     const hash = window.location.hash.split("/");
     switch (hash[0]) {
         case "#narrow": {
+            maybe_hide_recent_topics();
             ui_util.change_tab_to("#message_feed_container");
             const operators = hash_util.parse_narrow(hash);
             if (operators === undefined) {
@@ -125,6 +135,9 @@ function do_hashchange_normal(from_reload) {
         case "#":
             activate_home_tab();
             break;
+        case "#recent_topics":
+            recent_topics.show();
+            break;
         case "#keyboard-shortcuts":
         case "#message-formatting":
         case "#search-operators":
@@ -133,7 +146,6 @@ function do_hashchange_normal(from_reload) {
         case "#streams":
         case "#organization":
         case "#settings":
-        case "#recent_topics":
             blueslip.error("overlay logic skipped for: " + hash);
             break;
     }
@@ -222,10 +234,6 @@ function do_hashchange_overlay(old_hash) {
         return;
     }
 
-    if (base === "recent_topics") {
-        recent_topics.launch();
-        return;
-    }
     if (base === "keyboard-shortcuts") {
         info_overlay.show("keyboard-shortcuts");
         return;

commit 5823ab8bc2e50d864d43ed396b9ebb30b6e8cd1c

hotkey: Fix incorrect preventDefault for recent_topics hotkeys.
This logic correctly prevents the hotkeys implemented below it from
being active when "Recent topics" is open.  We expect to change some
parts of that soon (see #17685), but in any case, we should always
return false in the hotkey code when we don't process a key, so that
default browser behavior works.

This fixes browser shortcuts like Ctrl+C, Ctrl+V, etc. when recent
topics is loaded.

This bug was introduced in 1eafb1d8b3af0a2cee3c3d58647ca3829bc5419c.

Thanks to ganpa3 for noticing this bug.

static/js/hotkey.js
@@ -756,7 +756,7 @@ export function process_hotkey(e, hotkey) {
     // open. These involve compose box hotkeys and hotkeys that can only
     // be done performed on a message.
     if (recent_topics.is_visible()) {
-        return true;
+        return false;
     }
 
     // Compose box hotkeys