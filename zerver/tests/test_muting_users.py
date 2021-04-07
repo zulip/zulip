bb468fa56e015e357e20e3b9489a82dbda58f068
Abhijeet Prasad Bodas <abhijeetbodas2001@gmail.com>
2021-04-07 22:50:46 +0530

    refactor: Respect example user conventions in test_muting_users.py.
    This makes it consistent with the rest of the codebase.
    Hamlet and Cordelia are usually the main users which do
    things in automated tests.

--- zerver/tests/test_muting_users.py
+++ zerver/tests/test_muting_users.py
@@ -7,18 +7,19 @@ from zerver.lib.user_mutes import add_user_mute, get_user_mutes, user_is_muted
 
 
 class MutedUsersTests(ZulipTestCase):
+    # Hamlet does the muting/unmuting, and Cordelia gets muted/unmuted.
     def test_get_user_mutes(self) -> None:
-        othello = self.example_user("othello")
+        hamlet = self.example_user("hamlet")
         cordelia = self.example_user("cordelia")
 
-        muted_users = get_user_mutes(othello)
+        muted_users = get_user_mutes(hamlet)
         self.assertEqual(muted_users, [])
         mute_time = datetime(2021, 1, 1, tzinfo=timezone.utc)
 
         with mock.patch("zerver.lib.user_mutes.timezone_now", return_value=mute_time):
-            add_user_mute(user_profile=othello, muted_user=cordelia)
+            add_user_mute(user_profile=hamlet, muted_user=cordelia)
 
-        muted_users = get_user_mutes(othello)
+        muted_users = get_user_mutes(hamlet)
         self.assertEqual(len(muted_users), 1)
 
         self.assertDictEqual(
@@ -30,16 +31,16 @@ class MutedUsersTests(ZulipTestCase):
         )
 
     def test_add_muted_user_mute_self(self) -> None:
-        user = self.example_user("hamlet")
-        self.login_user(user)
+        hamlet = self.example_user("hamlet")
+        self.login_user(hamlet)
 
-        url = "/api/v1/users/me/muted_users/{}".format(user.id)
-        result = self.api_post(user, url)
+        url = "/api/v1/users/me/muted_users/{}".format(hamlet.id)
+        result = self.api_post(hamlet, url)
         self.assert_json_error(result, "Cannot mute self")
 
     def test_add_muted_user_mute_bot(self) -> None:
-        user = self.example_user("hamlet")
-        self.login_user(user)
+        hamlet = self.example_user("hamlet")
+        self.login_user(hamlet)
 
         bot_info = {
             "full_name": "The Bot of Hamlet",
@@ -51,75 +52,71 @@ class MutedUsersTests(ZulipTestCase):
         muted_id = result.json()["user_id"]
 
         url = "/api/v1/users/me/muted_users/{}".format(muted_id)
-        result = self.api_post(user, url)
+        result = self.api_post(hamlet, url)
         # Currently we do not allow muting bots. This is the error message
         # from `access_user_by_id`.
         self.assert_json_error(result, "No such user")
 
     def test_add_muted_user_mute_twice(self) -> None:
-        user = self.example_user("hamlet")
-        self.login_user(user)
-        muted_user = self.example_user("cordelia")
-        muted_id = muted_user.id
+        hamlet = self.example_user("hamlet")
+        self.login_user(hamlet)
+        cordelia = self.example_user("cordelia")
 
         add_user_mute(
-            user_profile=user,
-            muted_user=muted_user,
+            user_profile=hamlet,
+            muted_user=cordelia,
         )
 
-        url = "/api/v1/users/me/muted_users/{}".format(muted_id)
-        result = self.api_post(user, url)
+        url = "/api/v1/users/me/muted_users/{}".format(cordelia.id)
+        result = self.api_post(hamlet, url)
         self.assert_json_error(result, "User already muted")
 
     def test_add_muted_user_valid_data(self) -> None:
-        user = self.example_user("hamlet")
-        self.login_user(user)
-        muted_user = self.example_user("cordelia")
-        muted_id = muted_user.id
+        hamlet = self.example_user("hamlet")
+        self.login_user(hamlet)
+        cordelia = self.example_user("cordelia")
         mute_time = datetime(2021, 1, 1, tzinfo=timezone.utc)
 
         with mock.patch("zerver.views.muting.timezone_now", return_value=mute_time):
-            url = "/api/v1/users/me/muted_users/{}".format(muted_id)
-            result = self.api_post(user, url)
+            url = "/api/v1/users/me/muted_users/{}".format(cordelia.id)
+            result = self.api_post(hamlet, url)
             self.assert_json_success(result)
 
         self.assertIn(
             {
-                "id": muted_id,
+                "id": cordelia.id,
                 "timestamp": datetime_to_timestamp(mute_time),
             },
-            get_user_mutes(user),
+            get_user_mutes(hamlet),
         )
-        self.assertTrue(user_is_muted(user, muted_user))
+        self.assertTrue(user_is_muted(hamlet, cordelia))
 
     def test_remove_muted_user_unmute_before_muting(self) -> None:
-        user = self.example_user("hamlet")
-        self.login_user(user)
-        muted_user = self.example_user("cordelia")
-        muted_id = muted_user.id
+        hamlet = self.example_user("hamlet")
+        self.login_user(hamlet)
+        cordelia = self.example_user("cordelia")
 
-        url = "/api/v1/users/me/muted_users/{}".format(muted_id)
-        result = self.api_delete(user, url)
+        url = "/api/v1/users/me/muted_users/{}".format(cordelia.id)
+        result = self.api_delete(hamlet, url)
         self.assert_json_error(result, "User is not muted")
 
     def test_remove_muted_user_valid_data(self) -> None:
-        user = self.example_user("hamlet")
-        self.login_user(user)
-        muted_user = self.example_user("cordelia")
-        muted_id = muted_user.id
+        hamlet = self.example_user("hamlet")
+        self.login_user(hamlet)
+        cordelia = self.example_user("cordelia")
         mute_time = datetime(2021, 1, 1, tzinfo=timezone.utc)
 
-        add_user_mute(user_profile=user, muted_user=muted_user, date_muted=mute_time)
+        add_user_mute(user_profile=hamlet, muted_user=cordelia, date_muted=mute_time)
 
-        url = "/api/v1/users/me/muted_users/{}".format(muted_id)
-        result = self.api_delete(user, url)
+        url = "/api/v1/users/me/muted_users/{}".format(cordelia.id)
+        result = self.api_delete(hamlet, url)
 
         self.assert_json_success(result)
         self.assertNotIn(
             {
-                "id": muted_id,
+                "id": cordelia.id,
                 "timestamp": datetime_to_timestamp(mute_time),
             },
-            get_user_mutes(user),
+            get_user_mutes(hamlet),
         )
-        self.assertFalse(user_is_muted(user, muted_user))
+        self.assertFalse(user_is_muted(hamlet, cordelia))
