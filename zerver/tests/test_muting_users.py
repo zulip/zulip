e47dc6cf37e9508d5229fba29c8ef9735964888a
Abhijeet Prasad Bodas <abhijeetbodas2001@gmail.com>
2021-04-07 22:33:15 +0530

    refactor: Use variables for dates in test_muting_users.py.
    This cleans up some code added in 3bfcaa39689c9996cb7eb0ec65bd37f727074f6e.
    
    Also fixes some indentation to be more readable:
    - `mock.patch` is in a single line.
    - Dictionaries are one field per line.

--- zerver/tests/test_muting_users.py
+++ zerver/tests/test_muting_users.py
@@ -15,10 +15,7 @@ class MutedUsersTests(ZulipTestCase):
         self.assertEqual(muted_users, [])
         mute_time = datetime(2021, 1, 1, tzinfo=timezone.utc)
 
-        with mock.patch(
-            "zerver.lib.user_mutes.timezone_now",
-            return_value=mute_time,
-        ):
+        with mock.patch("zerver.lib.user_mutes.timezone_now", return_value=mute_time):
             add_user_mute(user_profile=othello, muted_user=cordelia)
 
         muted_users = get_user_mutes(othello)
@@ -79,17 +76,20 @@ class MutedUsersTests(ZulipTestCase):
         self.login_user(user)
         muted_user = self.example_user("cordelia")
         muted_id = muted_user.id
+        mute_time = datetime(2021, 1, 1, tzinfo=timezone.utc)
 
-        mock_date_muted = datetime(2021, 1, 1, tzinfo=timezone.utc).timestamp()
-        with mock.patch(
-            "zerver.views.muting.timezone_now",
-            return_value=datetime(2021, 1, 1, tzinfo=timezone.utc),
-        ):
+        with mock.patch("zerver.views.muting.timezone_now", return_value=mute_time):
             url = "/api/v1/users/me/muted_users/{}".format(muted_id)
             result = self.api_post(user, url)
             self.assert_json_success(result)
 
-        self.assertIn({"id": muted_id, "timestamp": mock_date_muted}, get_user_mutes(user))
+        self.assertIn(
+            {
+                "id": muted_id,
+                "timestamp": datetime_to_timestamp(mute_time),
+            },
+            get_user_mutes(user),
+        )
         self.assertTrue(user_is_muted(user, muted_user))
 
     def test_remove_muted_user_unmute_before_muting(self) -> None:
@@ -107,17 +107,19 @@ class MutedUsersTests(ZulipTestCase):
         self.login_user(user)
         muted_user = self.example_user("cordelia")
         muted_id = muted_user.id
+        mute_time = datetime(2021, 1, 1, tzinfo=timezone.utc)
 
-        add_user_mute(
-            user_profile=user,
-            muted_user=muted_user,
-            date_muted=datetime(2021, 1, 1, tzinfo=timezone.utc),
-        )
+        add_user_mute(user_profile=user, muted_user=muted_user, date_muted=mute_time)
 
         url = "/api/v1/users/me/muted_users/{}".format(muted_id)
         result = self.api_delete(user, url)
 
-        mock_date_muted = datetime(2021, 1, 1, tzinfo=timezone.utc).timestamp()
         self.assert_json_success(result)
-        self.assertNotIn({"id": muted_id, "timestamp": mock_date_muted}, get_user_mutes(user))
+        self.assertNotIn(
+            {
+                "id": muted_id,
+                "timestamp": datetime_to_timestamp(mute_time),
+            },
+            get_user_mutes(user),
+        )
         self.assertFalse(user_is_muted(user, muted_user))
