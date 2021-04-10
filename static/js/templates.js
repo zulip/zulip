0892e98012f2ffd58367c7cb1ae4678e084a3d6d
Anders Kaseorg <anders@zulip.com>
2021-04-09 20:53:13 -0700

    i18n: Convert Handlebars messages to FormatJS.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- static/js/templates.js
+++ static/js/templates.js
@@ -1,6 +1,6 @@
 import Handlebars from "handlebars/runtime";
 
-import {i18n} from "./i18n";
+import {default_html_elements, intl} from "./i18n";
 import * as util from "./util";
 
 // Below, we register Zulip-specific extensions to the handlebars API.
@@ -50,29 +50,16 @@ Handlebars.registerHelper({
     },
 });
 
-// Note that this i18n caching strategy does not allow us to support
-// live-updating the UI language without reloading the Zulip browser
-// window.  That constraint would be very hard to change in any case,
-// though, because of how Zulip renders some strings using the backend
-// Jinja2 templating engine, so we don't consider this important.
-const t_cache = new Map();
-
-Handlebars.registerHelper("t", (i18n_key) => {
+Handlebars.registerHelper("t", (message) => {
     // Marks a string for translation.
     // Example usage:
     //     {{t "some English text"}}
 
-    const cache_result = t_cache.get(i18n_key);
-    if (cache_result !== undefined) {
-        return cache_result;
-    }
-    const result = i18n.t(i18n_key);
-    const safe_result = new Handlebars.SafeString(result);
-    t_cache.set(i18n_key, safe_result);
-    return safe_result;
+    const descriptor = {id: message, defaultMessage: message};
+    return intl.formatMessage(descriptor);
 });
 
-Handlebars.registerHelper("tr", (context, options) => {
+Handlebars.registerHelper("tr", function (context, options) {
     // Marks a block for translation.
     // Example usage 1:
     //     {{#tr context}}
@@ -81,22 +68,36 @@ Handlebars.registerHelper("tr", (context, options) => {
     //
     // Example usage 2:
     //     {{#tr context}}
-    //         <p>This __variable__ will get value from context</p>
+    //         <p>This {variable} will get value from context</p>
     //     {{/tr}}
     //
     // Notes:
     //     1. `context` is very important. It can be `this` or an
     //        object or key of the current context.
-    //     2. Use `__` instead of `{{` and `}}` to declare expressions
-    const result = i18n.t(
-        options
-            .fn(context)
-            .trim()
-            .split("\n")
-            .map((s) => s.trim())
-            .join(" "),
-        context,
-    );
+    //     2. Use `{` and `}` instead of `{{` and `}}` to declare
+    //        expressions.
+    const message = options
+        .fn(context)
+        .trim()
+        .split("\n")
+        .map((s) => s.trim())
+        .join(" ");
+    const descriptor = {id: message, defaultMessage: message};
+    const result = intl.formatMessage(descriptor, {
+        ...default_html_elements,
+        ...Object.fromEntries(
+            Object.entries(options.fn.partials ?? {}).map(([name, value]) => [
+                name,
+                (s) => value(this, {data: {"partial-block": () => s.join("")}}),
+            ]),
+        ),
+        ...Object.fromEntries(
+            Object.entries(context ?? {}).map(([key, value]) => [
+                key,
+                Handlebars.Utils.escapeExpression(value),
+            ]),
+        ),
+    });
     return new Handlebars.SafeString(result);
 });
 
