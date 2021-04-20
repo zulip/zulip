b3aae80bcbe6a6e62d1eeb8c981400de225cbac9
Anders Kaseorg <anders@zulip.com>
2021-04-20 16:37:51 -0700

    templates: Use HTML5 doctype for emails.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -45,4 +45,4 @@ API_FEATURE_LEVEL = 57
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = "141.0"
+PROVISION_VERSION = "141.1"
