zulip/zulip

commit 6f7b973d3b30d96d350971c0f99390ee99c31742

settings: Fix error handler for  Default Streams.
Updates the error handler code to be consistent with how errors
are displayed everywhere else.

static/js/settings_streams.js
@@ -107,17 +107,19 @@ function make_stream_default(stream_name) {
     var data = {
         stream_name: stream_name,
     };
+    var default_stream_status = $("#admin-default-stream-status");
+    default_stream_status.hide();
 
     channel.post({
         url: '/json/default_streams',
         data: data,
         error: function (xhr) {
             if (xhr.status.toString().charAt(0) === "4") {
-                $(".active_stream_row button").closest("td").html(
-                    $("<p>").addClass("text-error").text(JSON.parse(xhr.responseText).msg));
+                ui_report.error(i18n.t("Failed!"), xhr, default_stream_status);
             } else {
-                $(".active_stream_row button").text(i18n.t("Failed!"));
+                ui_report.error(i18n.t("Failed!"), default_stream_status);
             }
+            default_stream_status.show();
         },
     });
 }

static/templates/settings/default-streams-list-admin.handlebars
@@ -13,6 +13,7 @@
             <div class="control-group" id="default_stream_inputs">
                 <label for="default_stream_name" class="control-label">{{t "Stream name" }}</label>
                 <input class="create_default_stream" type="text" placeholder="{{t "Stream name" }}" name="stream_name" autocomplete="off"></input>
+                <div class="alert" id="admin-default-stream-status"></div>
             </div>
             <div class="control-group">
                 <div class="controls">

commit a6f4230895c3c141886b954206f2950356ea02dc

settings: Remove broken default streams error handler.
For unknown reasons, 6f7b973d3b30d96d350971c0f99390ee99c31742
introduced an invalid semi-duplicate call to ui_report.error that
passed the wrong number of parameters.

We should just call `ui_report.error`, which already had logic to
handle the distinction between 40x and 50x errors.

Fixes an issue reported in #18273.

static/js/settings_streams.js
@@ -80,11 +80,7 @@ function make_stream_default(stream_id) {
         url: "/json/default_streams",
         data,
         error(xhr) {
-            if (xhr.status.toString().charAt(0) === "4") {
-                ui_report.error($t_html({defaultMessage: "Failed"}), xhr, default_stream_status);
-            } else {
-                ui_report.error($t_html({defaultMessage: "Failed"}), default_stream_status);
-            }
+            ui_report.error($t_html({defaultMessage: "Failed"}), xhr, default_stream_status);
             default_stream_status.show();
         },
     });