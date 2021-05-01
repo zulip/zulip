1d9fb4f9881c687ea4007ea2e44fd16807360e50
Mateusz Mandera <mateusz.mandera@zulip.com>
2021-05-01 15:34:59 +0200

    django: Upgrade Zulip to Django 3.2 LTS.
    This is a straightforward upgrade in terms of changes needed.
    
    Necessary changes were:
    - Set `DEFAULT_AUTO_FIELD`
      https://docs.djangoproject.com/en/3.2/releases/3.2/#customizing-type-of-auto-created-primary-keys
    
    - `The default_app_config application configuration variable is deprecated, due
      to the now automatic AppConfig discovery.`
      https://docs.djangoproject.com/en/3.2/releases/3.2/#automatic-appconfig-discovery
    
    To handle this one, we can remove default_app_config from
    zerver/__init__.py because it satisfies what release notes describe in
    https://docs.djangoproject.com/en/3.2/releases/3.2/#automatic-appconfig-discovery:
    "Most pluggable applications define an AppConfig subclass in an apps.py
    submodule. Many define a default_app_config variable pointing to this
    class in their __init__.py.  When the apps.py submodule exists and
    defines a single AppConfig subclass, Django now uses that configuration
    automatically, so you can remove default_app_config."
    
    An important note is that rebuild-test-database needs to be run after
    this upgrade in dev environment - if tests are run with test db that was
    built on the previous version, they will fail due to a mysterious bug
    (?), where changing attributes of a user and .save()ing after logging in
    in the test via self.login_user, causes getting logged out - the next
    requests via self.client_get etc. are unauthed for some reason,
    unless self.login_user is called again. This behavior is no longer
    exhibited upon rebuilding the test db - and I can't reproduce it in
    production or dev db. So this can likely be reasonably dismissed as some
    quirk of the test client system that won't be relevant in the future and
    doesn't impact production.

--- version.py
+++ version.py
@@ -45,4 +45,4 @@ API_FEATURE_LEVEL = 61
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = "141.3"
+PROVISION_VERSION = "142.0"
