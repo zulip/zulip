95ccb8ff85db0a2fd18982d6ee9cfdfc2063bc35
Abhijeet Prasad Bodas <abhijeetbodas2001@gmail.com>
2021-05-01 19:19:22 +0530

    zcommand: Add backend code for digress slash command.
    This commit adds backend code for the digress
    slash command, which will send two tombstone
    messages to old and new threads when used, so
    as to suggest moving the current conversation
    to another topic.
    
    Before sending the messages, we check that-
    * The old and new (stream+topic) combination is not
      the same.
    * The user can themselves access as well as send
      messages to the involved streams.
    * The old topic isn't empty for the user.

--- zerver/lib/zcommand.py
+++ zerver/lib/zcommand.py
@@ -2,8 +2,15 @@ from typing import Any, Dict, Optional
 
 from django.utils.translation import gettext as _
 
-from zerver.lib.actions import do_set_user_display_setting
+from zerver.lib.actions import do_set_user_display_setting, send_message_moved_breadcrumbs
 from zerver.lib.exceptions import JsonableError
+from zerver.lib.message import truncate_topic
+from zerver.lib.streams import (
+    access_stream_by_name,
+    access_stream_for_send_message,
+    subscribed_to_stream,
+)
+from zerver.lib.topic import messages_for_topic, user_message_exists_for_topic
 from zerver.models import UserProfile
 
 
@@ -75,4 +82,69 @@ def process_zcommands(
                 setting_value=False,
             )
         )
+    elif command == "digress":
+        data_keys = [
+            "old_stream",
+            "old_topic",
+            "new_stream",
+            "new_topic",
+        ]
+        if not data or not all(key in data for key in data_keys):
+            raise JsonableError(_("Invalid data."))
+
+        if data["old_stream"] == data["new_stream"] and data["old_topic"] == data["new_topic"]:
+            raise JsonableError(_("Cannot digress to the same topic."))
+
+        (old_stream, ignored_old_stream_sub) = access_stream_by_name(
+            user_profile, data["old_stream"]
+        )
+
+        (new_stream, ignored_new_stream_sub) = access_stream_by_name(
+            user_profile, data["new_stream"]
+        )
+
+        # Because /digress is just a shortcut for manually sending messages,
+        # we make sure that the user has permission to send messages to these
+        # streams themselves.
+        access_stream_for_send_message(user_profile, old_stream, None)
+        access_stream_for_send_message(user_profile, new_stream, None)
+
+        # Allow digressing from a topic only if it isn't empty for the user.
+        error_message = _("Old topic #**{}>{}** does not exist.").format(
+            data["old_stream"], data["old_topic"]
+        )
+
+        if old_stream.is_history_realm_public() or (
+            subscribed_to_stream(user_profile, old_stream.id)
+            and old_stream.is_history_public_to_subscribers()
+        ):
+            # Stream history is visible to the user. Allow digress if the topic
+            # isn't empty (that is, at least one Message exists).
+            messages = messages_for_topic(old_stream.recipient.id, data["old_topic"])
+            if len(messages) < 1:
+                raise JsonableError(error_message)
+
+        if (not old_stream.is_public()) and (not old_stream.is_history_public_to_subscribers()):
+            # Stream history isn't visible. Allow digress if there is at least
+            # one UserMessage.
+            if not user_message_exists_for_topic(
+                user_profile=user_profile,
+                recipient_id=old_stream.recipient_id,
+                topic_name=data["old_topic"],
+            ):
+                raise JsonableError(error_message)
+
+        data["new_topic"] = truncate_topic(data["new_topic"])
+        old_thread_notification_string = _("{user} digressed to the new topic: {new_location}")
+        new_thread_notification_string = _("{user} digressed this from old topic: {old_location}")
+        send_message_moved_breadcrumbs(
+            user_profile,
+            old_stream,
+            data["old_topic"],
+            old_thread_notification_string,
+            new_stream,
+            data["new_topic"],
+            new_thread_notification_string,
+        )
+        return {}
     raise JsonableError(_("No such command: {}").format(command))
--- zerver/tests/test_zcommand.py
+++ zerver/tests/test_zcommand.py
@@ -1,3 +1,8 @@
+from typing import Any, Dict
+
+import orjson
+
+from zerver.lib.actions import do_change_stream_invite_only
 from zerver.lib.test_classes import ZulipTestCase
 from zerver.models import UserProfile
 
@@ -80,3 +85,245 @@ class ZcommandTest(ZulipTestCase):
         result = self.client_post("/json/zcommand", payload)
         self.assert_json_success(result)
         self.assert_in_response("You are still in fixed width mode", result)
+
+    def get_digress_payload(self, data: Dict[str, str]) -> Dict[str, Any]:
+        return dict(
+            command="/digress",
+            command_data=orjson.dumps(data).decode(),
+        )
+
+    def test_digress_incomplete_data(self) -> None:
+        self.login("hamlet")
+        payload = self.get_digress_payload(dict(old_stream="general"))
+        result = self.client_post("/json/zcommand", payload)
+        self.assert_json_error(result, "Invalid data.")
+
+    def test_digress_same_thread(self) -> None:
+        self.login("hamlet")
+        payload = self.get_digress_payload(
+            dict(
+                old_stream="general",
+                old_topic="old topic name",
+                new_stream="general",
+                new_topic="old topic name",
+            )
+        )
+        result = self.client_post("/json/zcommand", payload)
+        self.assert_json_error(result, "Cannot digress to the same topic.")
+
+    def test_digress_from_public_stream(self) -> None:
+        self.login("hamlet")
+        hamlet = self.example_user("hamlet")
+        self.make_stream("general")
+        self.subscribe(hamlet, "general")
+        self.make_stream("test")
+        self.subscribe(hamlet, "test")
+        payload = self.get_digress_payload(
+            dict(
+                old_stream="general",
+                old_topic="old topic name",
+                new_stream="test",
+                new_topic="new topic name",
+            )
+        )
+
+        # Empty topic.
+        result = self.client_post("/json/zcommand", payload)
+        self.assert_json_error(result, "Old topic #**general>old topic name** does not exist.")
+
+        self.client_post(
+            "/json/messages",
+            {
+                "type": "stream",
+                "to": "general",
+                "client": "test suite",
+                "content": "Test message",
+                "topic": "old topic name",
+            },
+        )
+
+        # Happy path.
+        result = self.client_post("/json/zcommand", payload)
+        self.assert_json_success(result)
+        message = self.get_last_message()
+        self.assertEqual(message.topic_name(), "old topic name")
+        self.assertEqual(
+            message.content,
+            f"@_**King Hamlet|{hamlet.id}** digressed to the new topic: #**test>new topic name**",
+        )
+
+        message = self.get_second_to_last_message()
+        self.assertEqual(message.topic_name(), "new topic name")
+        self.assertEqual(
+            message.content,
+            f"@_**King Hamlet|{hamlet.id}** digressed this from old topic: #**general>old topic name**",
+        )
+
+    def test_digress_from_private_stream_with_shared_history(self) -> None:
+        # Data setup.
+        self.login("hamlet")
+        desdemona = self.example_user("desdemona")
+        hamlet = self.example_user("hamlet")
+        general = self.make_stream("general")
+        self.subscribe(desdemona, "general")
+        self.make_stream("test")
+        self.subscribe(hamlet, "test")
+        payload = self.get_digress_payload(
+            dict(
+                old_stream="general",
+                old_topic="old topic name",
+                new_stream="test",
+                new_topic="new topic name",
+            )
+        )
+        do_change_stream_invite_only(general, invite_only=True, history_public_to_subscribers=True)
+
+        # Inaccessible stream.
+        result = self.client_post("/json/zcommand", payload)
+        self.assert_json_error(result, "Invalid stream name 'general'")
+
+        # Accessible stream, empty topic.
+        self.subscribe(hamlet, "general")
+        result = self.client_post("/json/zcommand", payload)
+        self.assert_json_error(result, "Old topic #**general>old topic name** does not exist.")
+
+        # Have Desmodena send a message to #general while Hamlet was not subscribed,
+        # to make sure Hamlet does not have a UserMessage object.
+        # Hamlet should still be able to digress, because history is visible.
+        self.unsubscribe(hamlet, "general")
+        self.login("desdemona")
+        self.client_post(
+            "/json/messages",
+            {
+                "type": "stream",
+                "to": "general",
+                "client": "test suite",
+                "content": "Test message",
+                "topic": "old topic name",
+            },
+        )
+        self.login("hamlet")
+        self.subscribe(hamlet, "general")
+        result = self.client_post("/json/zcommand", payload)
+        self.assert_json_success(result)
+        message = self.get_last_message()
+        self.assertEqual(message.topic_name(), "old topic name")
+        self.assertEqual(
+            message.content,
+            f"@_**King Hamlet|{hamlet.id}** digressed to the new topic: #**test>new topic name**",
+        )
+
+        message = self.get_second_to_last_message()
+        self.assertEqual(message.topic_name(), "new topic name")
+        self.assertEqual(
+            message.content,
+            f"@_**King Hamlet|{hamlet.id}** digressed this from old topic: #**general>old topic name**",
+        )
+
+    def test_digress_from_private_stream_with_protected_history(self) -> None:
+        # Data setup.
+        self.login("hamlet")
+        desdemona = self.example_user("desdemona")
+        hamlet = self.example_user("hamlet")
+        general = self.make_stream("general")
+        self.subscribe(desdemona, "general")
+        self.make_stream("test")
+        self.subscribe(hamlet, "test")
+        payload = self.get_digress_payload(
+            dict(
+                old_stream="general",
+                old_topic="old topic name",
+                new_stream="test",
+                new_topic="new topic name",
+            )
+        )
+        do_change_stream_invite_only(general, invite_only=True, history_public_to_subscribers=False)
+
+        # Inaccessible stream.
+        result = self.client_post("/json/zcommand", payload)
+        self.assert_json_error(result, "Invalid stream name 'general'")
+
+        # Accessible stream, empty topic.
+        self.subscribe(hamlet, "general")
+        result = self.client_post("/json/zcommand", payload)
+        self.assert_json_error(result, "Old topic #**general>old topic name** does not exist.")
+
+        # Have Desmodena send a message to #general while Hamlet was not subscribed,
+        # to make sure Hamlet does not have a UserMessage object.
+        # Hamlet should not be able to digress, because the old topic is technically
+        # empty (for Hamlet).
+        self.unsubscribe(hamlet, "general")
+        self.login("desdemona")
+        self.client_post(
+            "/json/messages",
+            {
+                "type": "stream",
+                "to": "general",
+                "client": "test suite",
+                "content": "Test message",
+                "topic": "old topic name",
+            },
+        )
+        self.login("hamlet")
+        self.subscribe(hamlet, "general")
+        result = self.client_post("/json/zcommand", payload)
+        self.assert_json_error(result, "Old topic #**general>old topic name** does not exist.")
+
+        # Message sent while Hamlet is subscribed.
+        self.client_post(
+            "/json/messages",
+            {
+                "type": "stream",
+                "to": "general",
+                "client": "test suite",
+                "content": "Test message",
+                "topic": "old topic name",
+            },
+        )
+        # Happy path.
+        result = self.client_post("/json/zcommand", payload)
+        self.assert_json_success(result)
+        message = self.get_last_message()
+        self.assertEqual(message.topic_name(), "old topic name")
+        self.assertEqual(
+            message.content,
+            f"@_**King Hamlet|{hamlet.id}** digressed to the new topic: #**test>new topic name**",
+        )
+
+        message = self.get_second_to_last_message()
+        self.assertEqual(message.topic_name(), "new topic name")
+        self.assertEqual(
+            message.content,
+            f"@_**King Hamlet|{hamlet.id}** digressed this from old topic: #**general>old topic name**",
+        )
+
+    def test_digress_new_topic_cleaned(self) -> None:
+        self.login("hamlet")
+        hamlet = self.example_user("hamlet")
+        self.make_stream("general")
+        self.make_stream("test")
+        self.subscribe(hamlet, "test")
+        self.client_post(
+            "/json/messages",
+            {
+                "type": "stream",
+                "to": "general",
+                "client": "test suite",
+                "content": "Test message",
+                "topic": "old topic name",
+            },
+        )
+        payload = self.get_digress_payload(
+            dict(
+                old_stream="general",
+                old_topic="old topic name",
+                new_stream="test",
+                new_topic=" This topic is really really really really really really really long.",
+            )
+        )
+        result = self.client_post("/json/zcommand", payload)
+        self.assert_json_success(result)
+        message = self.get_second_to_last_message()
+        self.assertEqual(
+            message.topic_name(), "This topic is really really really really really really ..."
+        )
