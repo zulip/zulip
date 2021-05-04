d6cf1ef27b17fc76b82e68a9587198106ee33218
Gaurav Pandey <gauravguitarrocks@gmail.com>
2021-05-04 13:24:27 +0530

    api: Fix encoding of strings in hotspot endpoint.
    * Remove unnecessary json_validator for string parameter.
    * Update frontend to pass right parameters.
    
    Fixes part of #18035.

--- zerver/views/hotspots.py
+++ zerver/views/hotspots.py
@@ -6,14 +6,13 @@ from zerver.lib.actions import do_mark_hotspot_as_read
 from zerver.lib.hotspots import ALL_HOTSPOTS
 from zerver.lib.request import REQ, has_request_variables
 from zerver.lib.response import json_error, json_success
-from zerver.lib.validator import check_string
 from zerver.models import UserProfile
 
 
 @human_users_only
 @has_request_variables
 def mark_hotspot_as_read(
-    request: HttpRequest, user: UserProfile, hotspot: str = REQ(json_validator=check_string)
+    request: HttpRequest, user: UserProfile, hotspot: str = REQ()
 ) -> HttpResponse:
     if hotspot not in ALL_HOTSPOTS:
         return json_error(_("Unknown hotspot: {}").format(hotspot))
