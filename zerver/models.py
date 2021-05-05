564a3fd2d5340e4e8915a1657fa17c34e1c23938
Siddharth Asthana <siddharthasthana31@gmail.com>
2021-05-05 12:44:38 +0530

    migration: Rename edit_history_json to edit_history in AbstractMessage.
    This migration is the final step in migrating edit_history in
    AbstractMessage from TextField to JSONField. The previous three
    migrations have already backfilled the data and removed the edit_history
    field. This migration renames edit_history_json to edit_history.

--- zerver/models.py
+++ zerver/models.py
@@ -2130,7 +2130,7 @@ class AbstractMessage(models.Model):
 
     # A JSON-encoded list of objects describing any past edits to this
     # message, oldest first.
-    edit_history_json: List[Dict[str, Any]] = models.JSONField(default=list)
+    edit_history: List[Dict[str, Any]] = models.JSONField(default=list)
 
     has_attachment: bool = models.BooleanField(default=False, db_index=True)
     has_image: bool = models.BooleanField(default=False, db_index=True)
