6339e7fd47433723466d22d21cf0af3db8dcb52e
Alex Vandiver <alexmv@zulip.com>
2021-05-06 16:57:30 -0700

    outgoing_http: Put the X-Smokescreen-Role in the proxy headers.

--- zerver/lib/outgoing_http.py
+++ zerver/lib/outgoing_http.py
@@ -5,17 +5,19 @@ from urllib3 import HTTPResponse
 
 
 class OutgoingSession(requests.Session):
-    def __init__(self, timeout: int, headers: Optional[Dict[str, str]] = None) -> None:
+    def __init__(self, role: str, timeout: int) -> None:
         super().__init__()
-        outgoing_adapter = OutgoingHTTPAdapter(timeout=timeout)
+        outgoing_adapter = OutgoingHTTPAdapter(role=role, timeout=timeout)
         self.mount("http://", outgoing_adapter)
         self.mount("https://", outgoing_adapter)
 
 
 class OutgoingHTTPAdapter(requests.adapters.HTTPAdapter):
+    role: str
     timeout: int
 
-    def __init__(self, timeout: int, *args: Any, **kwargs: Any) -> None:
+    def __init__(self, role: str, timeout: int, *args: Any, **kwargs: Any) -> None:
+        self.role = role
         self.timeout = timeout
         super().__init__(*args, *kwargs)
 
@@ -23,3 +25,6 @@ class OutgoingHTTPAdapter(requests.adapters.HTTPAdapter):
         if kwargs.get("timeout") is None:
             kwargs["timeout"] = self.timeout
         return super().send(*args, **kwargs)
+
+    def proxy_headers(self, proxy: str) -> Dict[str, str]:
+        return {"X-Smokescreen-Role": self.role}
--- zerver/tests/test_outgoing_http.py
+++ zerver/tests/test_outgoing_http.py
@@ -1,10 +1,34 @@
+import os
 from typing import Any
+from unittest import mock
 
 import requests
 import responses
 
 from zerver.lib.outgoing_http import OutgoingSession
 from zerver.lib.test_classes import ZulipTestCase
+
+
+class RequestMockWithProxySupport(responses.RequestsMock):
+    def _on_request(
+        self,
+        adapter: requests.adapters.HTTPAdapter,
+        request: requests.PreparedRequest,
+        **kwargs: Any,
+    ) -> requests.Response:
+        if "proxies" in kwargs and request.url:
+            proxy_uri = requests.utils.select_proxy(request.url, kwargs["proxies"])
+            if proxy_uri is not None:
+                request = requests.Request(
+                    method="GET",
+                    url="{}/".format(proxy_uri),
+                    headers=adapter.proxy_headers(proxy_uri),
+                ).prepare()
+        return super()._on_request(  # type: ignore[misc]  # This is an undocumented internal API
+            adapter,
+            request,
+            **kwargs,
+        )
 
 
 class RequestMockWithTimeoutAsHeader(responses.RequestsMock):
@@ -24,15 +48,24 @@ class RequestMockWithTimeoutAsHeader(responses.RequestsMock):
 
 
 class TestOutgoingHttp(ZulipTestCase):
+    @mock.patch.dict(os.environ, {"http_proxy": "http://localhost:4242"})
+    def test_proxy_headers(self) -> None:
+        with RequestMockWithProxySupport() as mock_requests:
+            mock_requests.add(responses.GET, "http://localhost:4242/")
+            OutgoingSession(role="testing", timeout=1).get("http://example.com/")
+            self.assertEqual(len(mock_requests.calls), 1)
+            headers = mock_requests.calls[0].request.headers
+            self.assertEqual(headers["X-Smokescreen-Role"], "testing")
+
     def test_timeouts(self) -> None:
         with RequestMockWithTimeoutAsHeader() as mock_requests:
             mock_requests.add(responses.GET, "http://example.com/")
-            OutgoingSession(timeout=17).get("http://example.com/")
+            OutgoingSession(role="testing", timeout=17).get("http://example.com/")
             self.assertEqual(len(mock_requests.calls), 1)
             self.assertEqual(mock_requests.calls[0].request.headers["X-Timeout"], 17)
 
         with RequestMockWithTimeoutAsHeader() as mock_requests:
             mock_requests.add(responses.GET, "http://example.com/")
-            OutgoingSession(timeout=17).get("http://example.com/", timeout=42)
+            OutgoingSession(role="testing", timeout=17).get("http://example.com/", timeout=42)
             self.assertEqual(len(mock_requests.calls), 1)
             self.assertEqual(mock_requests.calls[0].request.headers["X-Timeout"], 42)
