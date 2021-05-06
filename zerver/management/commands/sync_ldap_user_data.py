1b67011b5fb63d379fde6df58ffe15d32446f18f
Tim Abbott <tabbott@zulip.com>
2021-05-06 09:44:47 -0700

    ldap: Fix incorrect check for deactivating all owners.
    We apparently failed to update this when we did the migration from the
    administrator role to the owner role as the top role in Zulip.

--- zerver/management/commands/sync_ldap_user_data.py
+++ zerver/management/commands/sync_ldap_user_data.py
@@ -39,8 +39,8 @@ def sync_ldap_user_data(user_profiles: List[UserProfile], deactivation_protectio
                 raise Exception(error_msg)
             for string_id in realms:
                 if not UserProfile.objects.filter(is_bot=False, is_active=True, realm__string_id=string_id,
-                                                  role__gte=UserProfile.ROLE_REALM_ADMINISTRATOR).exists():
-                    error_msg = ("Ldap sync would have deactivated all administrators of realm %s. " +
+                                                  role__gte=UserProfile.ROLE_REALM_OWNER).exists():
+                    error_msg = ("Ldap sync would have deactivated all owners of realm %s. " +
                                  "This is most likely due " +
                                  "to a misconfiguration of ldap settings. Rolling back...\n" +
                                  "Use the --force option if the mass deactivation is intended.")
