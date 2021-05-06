9f83d0ed86d660b156a2609e13fc88bcdd1001fa
Alex Vandiver <alexmv@zulip.com>
2021-05-06 16:57:30 -0700

    outgoing_http: Put the X-Smokescreen-Role in the proxy headers.

--- zerver/lib/outgoing_webhook.py
+++ zerver/lib/outgoing_webhook.py
@@ -34,11 +34,11 @@ class OutgoingWebhookServiceInterface(metaclass=abc.ABCMeta):
         self.user_profile: UserProfile = user_profile
         self.service_name: str = service_name
         self.session: requests.Session = OutgoingSession(
+            role="webhook",
             timeout=10,
         )
         self.session.headers.update(
             {
-                "X-Smokescreen-Role": "webhook",
                 "User-Agent": "ZulipOutgoingWebhook/" + ZULIP_VERSION,
             }
         )
