1e2fb0d4a7dca3b80a43e55f7d3d7388198f245a
Siddharth Asthana <siddharthasthana31@gmail.com>
2021-05-07 20:12:28 +0530

    migration: Add extra_data_json field in RealmAuditLog model.
    Note that we use the DjangoJSONEncoder so that we have builtin support
    for parsing Decimal and datetime.
    
    During this intermediate state, the migration that creates
    extra_data_json field has been run. We prepare for running the backfilling
    migration that populates extra_data_json from extra_data.
    
    This change implements double-write, which is important to keep the
    state of extra data consistent. For most extra_data usage, this is
    handled by the overriden `save` method on `AbstractRealmAuditLog`, where
    we either generates extra_data_json using orjson.loads or
    ast.literal_eval.
    
    While backfilling ensures that old realm audit log entries have
    extra_data_json populated, double-write ensures that any new entries
    generated will also have extra_data_json set. So that we can then safely
    rename extra_data_json to extra_data while ensuring the non-nullable
    invariant.
    
    For completeness, we additionally set RealmAuditLog.NEW_VALUE for
    the USER_FULL_NAME_CHANGED event. This cannot be handled with the
    overridden `save`.
    
    This addresses: https://github.com/zulip/zulip/pull/23116#discussion_r1040277795
    
    Note that extra_data_json at this point is not used yet. So the test
    cases do not need to switch to testing extra_data_json. This is later
    done after we rename extra_data_json to extra_data.
    
    Double-write for the remote server audit logs is special, because we only
    get the dumped bytes from an external source. Luckily, none of the
    payload carries extra_data that is not generated using orjson.dumps for
    audit logs of event types in SYNC_BILLING_EVENTS. This can be verified
    by looking at:
    
    `git grep -A 6 -E "event_type=.*(USER_CREATED|USER_ACTIVATED|USER_DEACTIVATED|USER_REACTIVATED|USER_ROLE_CHANGED|REALM_DEACTIVATED|REALM_REACTIVATED)"`
    
    Therefore, we just need to populate extra_data_json doing an
    orjson.loads call after a None-check.
    
    Co-authored-by: Zixuan James Li <p359101898@gmail.com>

--- zerver/models.py
+++ zerver/models.py
@@ -1,3 +1,4 @@
+import ast
 import datetime
 import secrets
 import time
@@ -10,6 +11,7 @@ from typing import (
     Any,
     Callable,
     Dict,
+    Iterable,
     List,
     Optional,
     Pattern,
@@ -39,6 +41,7 @@ from django.contrib.contenttypes.fields import GenericRelation
 from django.contrib.postgres.indexes import GinIndex
 from django.contrib.postgres.search import SearchVectorField
 from django.core.exceptions import ValidationError
+from django.core.serializers.json import DjangoJSONEncoder
 from django.core.validators import MinLengthValidator, RegexValidator, URLValidator, validate_email
 from django.db import models, transaction
 from django.db.backends.base.base import BaseDatabaseWrapper
@@ -4450,6 +4453,7 @@ class AbstractRealmAuditLog(models.Model):
     ROLE_COUNT_BOTS = "12"
 
     extra_data = models.TextField(null=True)
+    extra_data_json = models.JSONField(default=dict, encoder=DjangoJSONEncoder)
 
     # Event types
     USER_CREATED = 101
@@ -4553,6 +4557,27 @@ class AbstractRealmAuditLog(models.Model):
 
     class Meta:
         abstract = True
+
+    def save(
+        self,
+        force_insert: bool = False,
+        force_update: bool = False,
+        using: Optional[str] = None,
+        update_fields: Optional[Iterable[str]] = None,
+    ) -> None:
+        # Unless extra_data is set and extra_data_json is unset (defaults to {})
+        # we do not attempt to auto convert extra_data to extra_data_json
+        if self.extra_data is not None and not self.extra_data_json:
+            try:
+                if self.extra_data.startswith("{'"):
+                    self.extra_data_json = ast.literal_eval(self.extra_data)
+                else:
+                    self.extra_data_json = orjson.loads(self.extra_data)
+            except Exception: # nocoverage # This is not intended to happen at all for correctly written code
+                raise Exception(
+                    "extra_data_json must be explicitly set if extra_data is not str()'d from a dict or JSON-encoded."
+                )
+        super().save(force_insert, force_update, using, update_fields)
 
 
 class RealmAuditLog(AbstractRealmAuditLog):
