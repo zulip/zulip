9ba48c4ed364d0d23c196cd87656143bfb983a99
Anders Kaseorg <anders@zulip.com>
2021-05-07 17:27:36 -0700

    requirements: Upgrade Python requirements.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- scripts/lib/setup_venv.py
+++ scripts/lib/setup_venv.py
@@ -84,7 +84,17 @@ def get_venv_dependencies(vendor: str, os_version: str) -> List[str]:
 def install_venv_deps(pip: str, requirements_file: str) -> None:
     pip_requirements = os.path.join(ZULIP_PATH, "requirements", "pip.txt")
     run([pip, "install", "--force-reinstall", "--require-hashes", "-r", pip_requirements])
-    run([pip, "install", "--no-deps", "--require-hashes", "-r", requirements_file])
+    run(
+        [
+            pip,
+            "install",
+            "--use-deprecated=legacy-resolver",  # https://github.com/pypa/pip/issues/5780
+            "--no-deps",
+            "--require-hashes",
+            "-r",
+            requirements_file,
+        ]
+    )
 
 
 def get_index_filename(venv_path: str) -> str:
