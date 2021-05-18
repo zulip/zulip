f4524467e06f828bd783a003bc1ecc0ec708394b
Priyank Patel <priyankp390@gmail.com>
2021-05-18 23:28:56 +0000

    ts: Convert reload_state to typescript.

--- static/js/reload_state.ts
+++ static/js/reload_state.ts
@@ -9,33 +9,33 @@
 
 let reload_in_progress = false;
 let reload_pending = false;
-export let csrf_failed_handler;
+export let csrf_failed_handler: (() => void) | undefined;
 
-export function clear_for_testing() {
+export function clear_for_testing(): void {
     reload_in_progress = false;
     reload_pending = false;
     csrf_failed_handler = undefined;
 }
 
-export function is_pending() {
+export function is_pending(): boolean {
     return reload_pending;
 }
 
-export function is_in_progress() {
+export function is_in_progress(): boolean {
     return reload_in_progress;
 }
 
-export function set_state_to_pending() {
+export function set_state_to_pending(): void {
     // Why do we never set this back to false?
     // Because the reload is gonna happen next. :)
     // I was briefly confused by this, hence the comment.
     reload_pending = true;
 }
 
-export function set_state_to_in_progress() {
+export function set_state_to_in_progress(): void {
     reload_in_progress = true;
 }
 
-export function set_csrf_failed_handler(handler) {
+export function set_csrf_failed_handler(handler: () => void): void {
     csrf_failed_handler = handler;
 }
