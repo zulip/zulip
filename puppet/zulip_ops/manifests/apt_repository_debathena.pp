f3eea72c2a959eb43cc1fe2c7ef8c1e091ac3cc7
Alex Vandiver <alexmv@zulip.com>
2021-05-21 16:23:43 -0700

    setup: Merge multiple setup-apt-repo scripts into one.
    This moves the `.asc` files into subdirectories, and writes out the
    according `.list` files into them.  It moves from templates to
    written-out `.list` files for clarity and ease of
    implementation (Debian and Ubuntu need different templates for
    `zulip`), and as a way of making explicit which releases are supported
    for each list.  For the special-case of the PGroonga signing key, we
    source an additional file within the directory.
    
    This simplifies the process for adding another class of `.list` file.

--- puppet/zulip_ops/manifests/apt_repository_debathena.pp
+++ puppet/zulip_ops/manifests/apt_repository_debathena.pp
@@ -1,6 +1,7 @@
 class zulip_ops::apt_repository_debathena {
-  $setup_file = "${::zulip_scripts_path}/lib/setup-apt-repo-debathena"
+  $setup_apt_repo_file = "${::zulip_scripts_path}/lib/setup-apt-repo"
   exec { 'setup_apt_repo_debathena':
-    command => "bash -c '${setup_file}'",
+    command => "${setup_apt_repo_file} --list zulip_debathena",
+    unless  => "${setup_apt_repo_file} --list zulip_debathena --verify",
   }
 }
