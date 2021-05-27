7e75336c994d78f72fb9cec95b76e213d35b7e02
sahil839 <sahilbatra839@gmail.com>
2021-05-27 18:24:42 +0530

    registration: Show spinner and disable button while processing.
    This commit disables the button and shows a loading spinner on
    the button when signup request is being processed to avoid race
    conditions caused by user clicking on the button multiple times.
    
    The fix is done observing that for the case when form is invalid
    the whole page is rerendered and thus we do not need to remove
    the spinner and enable the button again and for other errors
    we redirect to some other page.
    
    And for the validation taking place in client-side, the button
    is disabled and spinner is shown, only is form is valid, by
    using "$('#registration').valid()".

--- static/js/portico/signup.js
+++ static/js/portico/signup.js
@@ -103,6 +103,14 @@ $(() => {
         $("#timezone").val(new Intl.DateTimeFormat().resolvedOptions().timeZone);
     }
 
+    $("#registration").on("submit", () => {
+        if ($("#registration").valid()) {
+            $(".register-button .loader").css("display", "inline-block");
+            $(".register-button").prop("disabled", true);
+            $(".register-button span").hide();
+        }
+    });
+
     // Code in this block will be executed when the /accounts/send_confirm
     // endpoint is visited i.e. accounts_send_confirm.html is rendered.
     if ($("[data-page-id='accounts-send-confirm']").length > 0) {
