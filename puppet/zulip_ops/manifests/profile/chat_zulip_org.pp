3ebd627c509bda418be03e664c392aeebcafb87e
Alex Vandiver <alexmv@zulip.com>
2021-06-02 11:02:34 -0700

    puppet: Fix "import" -> "include" in chat_zulip_org.

--- puppet/zulip_ops/manifests/profile/chat_zulip_org.pp
+++ puppet/zulip_ops/manifests/profile/chat_zulip_org.pp
@@ -1,10 +1,10 @@
 class zulip_ops::profile::chat_zulip_org {
-  import zulip::profile::standalone
-  import zulip::profile::smokescreen
-  import zulip::postfix_localmail
-  import zulip::postgresql_backups
+  include zulip::profile::standalone
+  include zulip::profile::smokescreen
+  include zulip::postfix_localmail
+  include zulip::postgresql_backups
 
-  import zulip_ops::profile::base
+  include zulip_ops::profile::base
   zulip_ops::firewall_allow { 'http': }
   zulip_ops::firewall_allow { 'https': }
   zulip_ops::firewall_allow { 'smtp': }
