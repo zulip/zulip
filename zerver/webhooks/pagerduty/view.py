422d3e0f0678c2fb808a56c4a2a0e92127c26bf1
PIG208 <359101898@qq.com>
2021-06-04 23:26:13 +0800

    integrations: Add V3 support for PagerDuty.

--- zerver/webhooks/pagerduty/view.py
+++ zerver/webhooks/pagerduty/view.py
@@ -1,5 +1,5 @@
 # Webhooks for external integrations.
-from typing import Any, Dict, Sequence
+from typing import Any, Dict
 
 from django.http import HttpRequest, HttpResponse
 
@@ -27,35 +27,45 @@ PAGER_DUTY_EVENT_NAMES_V2 = {
     "incident.assign": "assigned",
 }
 
+PAGER_DUTY_EVENT_NAMES_V3 = {
+    "incident.triggered": "triggered",
+    "incident.acknowledged": "acknowledged",
+    "incident.unacknowledged": "unacknowledged",
+    "incident.resolved": "resolved",
+    "incident.reassigned": "reassigned",
+}
+
 AGENT_TEMPLATE = "[{username}]({url})"
 
 INCIDENT_WITH_SERVICE_AND_ASSIGNEE = (
-    "Incident [{incident_num}]({incident_url}) {action} by [{service_name}]"
+    "Incident [{incident_num_title}]({incident_url}) {action} by [{service_name}]"
     "({service_url}) (assigned to {assignee_info}).\n\n{trigger_message}"
 )
 
 TRIGGER_MESSAGE = "``` quote\n{message}\n```"
 
+NUM_TITLE = "{incident_title} (#{incident_num})"
+
 INCIDENT_WITH_ASSIGNEE = """
-Incident [{incident_num}]({incident_url}) {action} by {assignee_info}.
+Incident [{incident_num_title}]({incident_url}) {action} by {assignee_info}.
 
 {trigger_message}
 """.strip()
 
 INCIDENT_ASSIGNED = """
-Incident [{incident_num}]({incident_url}) {action} to {assignee_info}.
+Incident [{incident_num_title}]({incident_url}) {action} to {assignee_info}.
 
 {trigger_message}
 """.strip()
 
 INCIDENT_RESOLVED_WITH_AGENT = """
-Incident [{incident_num}]({incident_url}) resolved by {agent_info}.
+Incident [{incident_num_title}]({incident_url}) resolved by {agent_info}.
 
 {trigger_message}
 """.strip()
 
 INCIDENT_RESOLVED = """
-Incident [{incident_num}]({incident_url}) resolved.
+Incident [{incident_num_title}]({incident_url}) resolved.
 
 {trigger_message}
 """.strip()
@@ -66,7 +76,7 @@ def build_pagerduty_formatdict(message: Dict[str, Any]) -> Dict[str, Any]:
     format_dict["action"] = PAGER_DUTY_EVENT_NAMES[message["type"]]
 
     format_dict["incident_id"] = message["data"]["incident"]["id"]
-    format_dict["incident_num"] = message["data"]["incident"]["incident_number"]
+    format_dict["incident_num_title"] = message["data"]["incident"]["incident_number"]
     format_dict["incident_url"] = message["data"]["incident"]["html_url"]
 
     format_dict["service_name"] = message["data"]["incident"]["service"]["name"]
@@ -108,7 +118,7 @@ def build_pagerduty_formatdict_v2(message: Dict[str, Any]) -> Dict[str, Any]:
     format_dict["action"] = PAGER_DUTY_EVENT_NAMES_V2[message["event"]]
 
     format_dict["incident_id"] = message["incident"]["id"]
-    format_dict["incident_num"] = message["incident"]["incident_number"]
+    format_dict["incident_num_title"] = message["incident"]["incident_number"]
     format_dict["incident_url"] = message["incident"]["html_url"]
 
     format_dict["service_name"] = message["incident"]["service"]["name"]
@@ -136,21 +146,65 @@ def build_pagerduty_formatdict_v2(message: Dict[str, Any]) -> Dict[str, Any]:
     return format_dict
 
 
+def build_pagerduty_formatdict_v3(event: Dict[str, Any]) -> Dict[str, Any]:
+    format_dict = {}
+    format_dict["action"] = PAGER_DUTY_EVENT_NAMES_V3[event["event_type"]]
+
+    format_dict["incident_id"] = event["data"]["id"]
+    format_dict["incident_url"] = event["data"]["html_url"]
+    format_dict["incident_num_title"] = NUM_TITLE.format(
+        incident_num=event["data"]["number"], incident_title=event["data"]["title"]
+    )
+
+    format_dict["service_name"] = event["data"]["service"]["summary"]
+    format_dict["service_url"] = event["data"]["service"]["html_url"]
+
+    assignees = event["data"]["assignees"]
+    if assignees:
+        assignee = assignees[0]
+        format_dict["assignee_info"] = AGENT_TEMPLATE.format(
+            username=assignee["summary"], url=assignee["html_url"]
+        )
+    else:
+        format_dict["assignee_info"] = "nobody"
+
+    agent = event.get("agent")
+    if agent is not None:
+        format_dict["agent_info"] = AGENT_TEMPLATE.format(
+            username=agent["summary"],
+            url=agent["html_url"],
+        )
+
+    # V3 doesn't have trigger_message
+    format_dict["trigger_message"] = ""
+
+    return format_dict
+
+
 def send_formated_pagerduty(
     request: HttpRequest, user_profile: UserProfile, message_type: str, format_dict: Dict[str, Any]
 ) -> None:
-    if message_type in ("incident.trigger", "incident.unacknowledge"):
+    if message_type in (
+        "incident.trigger",
+        "incident.triggered",
+        "incident.unacknowledge",
+        "incident.unacknowledged",
+    ):
         template = INCIDENT_WITH_SERVICE_AND_ASSIGNEE
-    elif message_type == "incident.resolve" and format_dict.get("agent_info") is not None:
+    elif (
+        message_type == "incident.resolve" or message_type == "incident.resolved"
+    ) and format_dict.get("agent_info") is not None:
         template = INCIDENT_RESOLVED_WITH_AGENT
-    elif message_type == "incident.resolve" and format_dict.get("agent_info") is None:
+    elif (
+        message_type == "incident.resolve" or message_type == "incident.resolved"
+    ) and format_dict.get("agent_info") is None:
         template = INCIDENT_RESOLVED
-    elif message_type == "incident.assign":
+    elif message_type == "incident.assign" or message_type == "incident.reassigned":
         template = INCIDENT_ASSIGNED
     else:
         template = INCIDENT_WITH_ASSIGNEE
 
-    subject = "Incident {incident_num}".format(**format_dict)
+    subject = "Incident {incident_num_title}".format(**format_dict)
     body = template.format(**format_dict)
     check_send_webhook_message(request, user_profile, subject, body)
 
@@ -160,34 +214,48 @@ def send_formated_pagerduty(
 def api_pagerduty_webhook(
     request: HttpRequest,
     user_profile: UserProfile,
-    payload: Dict[str, Sequence[Dict[str, Any]]] = REQ(argument_type="body"),
+    payload: Dict[str, Any] = REQ(argument_type="body"),
 ) -> HttpResponse:
-    for message in payload["messages"]:
-        message_type = message.get("type")
+    messages = payload.get("messages")
 
-        # If the message has no "type" key, then this payload came from a
-        # Pagerduty Webhook V2.
-        if message_type is None:
-            break
+    if messages is not None:
+        for message in messages:
+            message_type = message.get("type")
 
-        if message_type not in PAGER_DUTY_EVENT_NAMES:
-            raise UnsupportedWebhookEventType(message_type)
+            # If the message has no "type" key, then this payload came from a
+            # Pagerduty Webhook V2.
+            if message_type is None:
+                break
 
-        format_dict = build_pagerduty_formatdict(message)
-        send_formated_pagerduty(request, user_profile, message_type, format_dict)
+            if message_type not in PAGER_DUTY_EVENT_NAMES:
+                raise UnsupportedWebhookEventType(message_type)
 
-    for message in payload["messages"]:
-        event = message.get("event")
+            format_dict = build_pagerduty_formatdict(message)
+            send_formated_pagerduty(request, user_profile, message_type, format_dict)
 
-        # If the message has no "event" key, then this payload came from a
-        # Pagerduty Webhook V1.
-        if event is None:
-            break
+        for message in messages:
+            event = message.get("event")
 
-        if event not in PAGER_DUTY_EVENT_NAMES_V2:
-            raise UnsupportedWebhookEventType(event)
+            # If the message has no "event" key, then this payload came from a
+            # Pagerduty Webhook V1.
+            if event is None:
+                break
 
-        format_dict = build_pagerduty_formatdict_v2(message)
-        send_formated_pagerduty(request, user_profile, event, format_dict)
+            if event not in PAGER_DUTY_EVENT_NAMES_V2:
+                raise UnsupportedWebhookEventType(event)
+
+            format_dict = build_pagerduty_formatdict_v2(message)
+            send_formated_pagerduty(request, user_profile, event, format_dict)
+    else:
+        if "event" in payload:
+            # V3 has no "messages" field, and it has key "event" instead
+            event = payload["event"]
+            event_type = event.get("event_type")
+
+            if event_type not in PAGER_DUTY_EVENT_NAMES_V3:
+                raise UnsupportedWebhookEventType(event_type)
+
+            format_dict = build_pagerduty_formatdict_v3(event)
+            send_formated_pagerduty(request, user_profile, event_type, format_dict)
 
     return json_success()
