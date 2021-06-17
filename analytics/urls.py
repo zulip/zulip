59b5270850b2819096695903e5ae72ed2cc833ba
Tim Abbott <tabbott@zulip.com>
2021-06-17 15:12:02 -0700

    analytics: Rename analytics/views/legacy.py.

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include
 from django.urls import path
 
-from analytics.views.legacy import get_activity
+from analytics.views.installation_activity import get_activity
 from analytics.views.realm_activity import get_realm_activity
 from analytics.views.stats import (
     get_chart_data,
