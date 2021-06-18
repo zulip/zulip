zulip/zulip

commit 0a1eaf0384d24eb03f6acbc5c0cb617ee5df8243

i18n: Reorganize language_list logic.
This reorganization follows our modern coding conventions for avoid
leaving data in page_params that should be owned by a single module.

static/js/i18n.js
@@ -44,13 +44,24 @@ export function $t_html(descriptor, values) {
     });
 }
 
-// This formats language data for the language selection modal in a
-// 2-column format.
-export function get_language_list_columns(default_language) {
-    const language_list = [];
+let language_list;
 
-    // Only render languages with percentage translation >= 5%
-    for (const language of page_params.language_list) {
+export function get_language_name(language_code) {
+    const language_list_map = {};
+
+    // One-to-one mapping from code to name for all languages
+    for (const language of language_list) {
+        language_list_map[language.code] = language.name;
+    }
+    return language_list_map[language_code];
+}
+
+export function initialize(language_params) {
+    const language_list_raw = language_params.language_list;
+
+    // Limit offered languages to options with percentage translation >= 5%
+    language_list = [];
+    for (const language of language_list_raw) {
         if (language.percent_translated === undefined || language.percent_translated >= 5) {
             language_list.push({
                 code: language.code,
@@ -60,13 +71,16 @@ export function get_language_list_columns(default_language) {
             });
         }
     }
+}
 
+// This formats language data for the language selection modal in a
+// 2-column format.
+export function get_language_list_columns(default_language) {
     const formatted_list = [];
     const language_len = language_list.length;
     const firsts_end = Math.floor(language_len / 2) + (language_len % 2);
     const firsts = _.range(0, firsts_end);
     const seconds = _.range(firsts_end, language_len);
-
     const longest_zip = [];
 
     // Create a zip (itertool.zip_longest in python)

frontend_tests/node_tests/i18n.js
@@ -25,7 +25,7 @@ page_params.translation_data = {
 // `i18n.js` initializes FormatJS and is imported by
 // `templates.js`.
 unmock_module("../../static/js/i18n");
-const {$t, $t_html, get_language_list_columns} = zrequire("i18n");
+const {$t, $t_html, get_language_name, get_language_list_columns, initialize} = zrequire("i18n");
 
 run_test("$t", () => {
     // Normally the id would be provided by babel-plugin-formatjs, but
@@ -108,7 +108,7 @@ run_test("tr_tag", () => {
 });
 
 run_test("language_list", () => {
-    page_params.language_list = [
+    const language_list = [
         {
             code: "en",
             locale: "en",
@@ -127,6 +127,8 @@ run_test("language_list", () => {
             percent_translated: 32,
         },
     ];
+    initialize({language_list});
+    assert.equal(get_language_name("en"), "English");
 
     const successful_formatted_list = [
         {

static/js/settings_display.js
@@ -2,7 +2,7 @@ import $ from "jquery";
 
 import * as channel from "./channel";
 import * as emojisets from "./emojisets";
-import {$t_html} from "./i18n";
+import {$t_html, get_language_name} from "./i18n";
 import * as loading from "./loading";
 import * as overlays from "./overlays";
 import {page_params} from "./page_params";
@@ -13,16 +13,6 @@ import * as ui_report from "./ui_report";
 const meta = {
     loaded: false,
 };
-
-function get_default_language_name(language_code) {
-    const language_list_map = {};
-
-    // One-to-one mapping from code to name for all languages
-    for (const language of page_params.language_list) {
-        language_list_map[language.code] = language.name;
-    }
-    return language_list_map[language_code];
-}
 
 export let default_language_name;
 
@@ -223,6 +213,6 @@ export function update_page() {
 }
 
 export function initialize() {
-    const language_name = get_default_language_name(page_params.default_language);
+    const language_name = get_language_name(page_params.default_language);
     set_default_language_name(language_name);
 }

static/js/ui_init.js
@@ -27,6 +27,7 @@ import * as gear_menu from "./gear_menu";
 import * as giphy from "./giphy";
 import * as hashchange from "./hashchange";
 import * as hotspots from "./hotspots";
+import * as i18n from "./i18n";
 import * as invite from "./invite";
 import * as lightbox from "./lightbox";
 import * as linkifiers from "./linkifiers";
@@ -465,7 +466,9 @@ export function initialize_everything() {
     const user_groups_params = pop_fields("realm_user_groups");
 
     const user_status_params = pop_fields("user_status");
+    const i18n_params = pop_fields("language_list");
 
+    i18n.initialize(i18n_params);
     tippyjs.initialize();
     // We need to initialize compose early, because other modules'
     // initialization expects `#compose` to be already present in the

commit 33d2ef67c33259c5b6af0301d261bbc916104703

i18n: Fix organization setting for default language.
This fixes a puppeteer failure introduced in
0a1eaf0384d24eb03f6acbc5c0cb617ee5df8243.

static/js/admin.js
@@ -3,7 +3,7 @@ import $ from "jquery";
 import render_admin_tab from "../templates/settings/admin_tab.hbs";
 import render_settings_organization_settings_tip from "../templates/settings/organization_settings_tip.hbs";
 
-import {$t} from "./i18n";
+import {$t, language_list} from "./i18n";
 import * as overlays from "./overlays";
 import {page_params} from "./page_params";
 import * as settings from "./settings";
@@ -87,7 +87,7 @@ export function build_page() {
         ),
         realm_message_retention_days: page_params.realm_message_retention_days,
         realm_allow_edit_history: page_params.realm_allow_edit_history,
-        language_list: page_params.language_list,
+        language_list,
         realm_default_language: page_params.realm_default_language,
         realm_waiting_period_threshold: page_params.realm_waiting_period_threshold,
         realm_notifications_stream_id: page_params.realm_notifications_stream_id,

static/js/i18n.js
@@ -44,7 +44,7 @@ export function $t_html(descriptor, values) {
     });
 }
 
-let language_list;
+export let language_list;
 
 export function get_language_name(language_code) {
     const language_list_map = {};