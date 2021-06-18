57ea7a01f46eba5d0c9f0d40c3ff9144893a9450
Priyank Patel <priyankp390@gmail.com>
2021-06-18 16:03:46 +0000

    ts: Convert components module to TypeScript.

--- frontend_tests/node_tests/components.js
+++ frontend_tests/node_tests/components.js
@@ -7,6 +7,7 @@ const _ = require("lodash");
 const {$t} = require("../zjsunit/i18n");
 const {mock_jquery, zrequire} = require("../zjsunit/namespace");
 const {run_test} = require("../zjsunit/test");
+const blueslip = require("../zjsunit/zblueslip");
 
 let env;
 
@@ -281,4 +282,9 @@ run_test("basics", () => {
     assert.equal(env.focused_tab, 0);
     widget.goto("search-operators");
     assert.equal(env.focused_tab, 0);
+
+    blueslip.expect("warn", "Incorrect tab name given.", 3);
+    widget.disable_tab("incorrect-tab");
+    widget.enable_tab("incorrect-tab");
+    widget.goto("incorrect-tab");
 });
