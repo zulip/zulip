6036524651101bfe538ab7981e881bda2ab79cf1
Eeshan Garg <eeshan@zulip.com>
2021-06-23 18:30:05 -0230

    landing_page: Restructure top-level navigation.
    This commit restructures the top-level navigation on our landing
    page using dropdowns in a manner that allows us to advertise some
    important pages to our visitors:
    
    - Use cases for companies, open source projects, and communities.
    - Miscellaneous pages about the product are now accessible from the
      "Product" dropdown.
    - "Resources" are a few key resources our users may want to consult
      if they need help or support.

--- static/js/portico/header.js
+++ static/js/portico/header.js
@@ -10,12 +10,38 @@ $(() => {
         const $this = $(e.target);
 
         if (
-            $this.closest(".dropdown .dropdown-pill").length > 0 &&
-            !$(".dropdown").hasClass("show")
+            $this.closest(".dropdown .dropdown-label").length > 0 &&
+            !$this.closest("ul .dropdown").hasClass("show") &&
+            window.matchMedia("(max-width: 686px)").matches
         ) {
-            $(".dropdown").addClass("show");
+            $this.closest("ul .dropdown").addClass("show");
+        } else if (
+            $this.closest(".dropdown .dropdown-pill").length > 0 &&
+            !$this.closest("ul .dropdown").hasClass("show")
+        ) {
+            $this.closest("ul .dropdown").addClass("show");
         } else if (!$this.is(".dropdown ul") && $this.closest(".dropdown ul").length === 0) {
-            $(".dropdown").removeClass("show");
+            $this.closest("ul .dropdown").removeClass("show");
+        }
+    });
+
+    $(".nav-dropdown").on("mouseover", (e) => {
+        const $this = $(e.target);
+        if (
+            !$this.closest("ul .dropdown").hasClass("show") &&
+            window.matchMedia("(min-width: 687px)").matches
+        ) {
+            $this.closest("ul .dropdown").addClass("show");
+        }
+    });
+
+    $(".nav-dropdown").on("mouseout", (e) => {
+        const $this = $(e.target);
+        if (
+            $this.closest("ul .dropdown").hasClass("show") &&
+            window.matchMedia("(min-width: 687px)").matches
+        ) {
+            $this.closest("ul .dropdown").removeClass("show");
         }
     });
 });
