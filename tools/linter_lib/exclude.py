93f62b999ed8eb4abce8563dc58b83624dbb0ecd
Anders Kaseorg <anders@zulip.com>
2021-06-24 17:15:26 -0700

    nagios: Replace check_website_response with standard check_http plugin.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- tools/linter_lib/exclude.py
+++ tools/linter_lib/exclude.py
@@ -1,7 +1,6 @@
 # Exclude some directories and files from lint checking
 EXCLUDED_FILES = [
     # Third-party code that doesn't match our style
-    "puppet/zulip/files/nagios_plugins/zulip_nagios_server/check_website_response.sh",
     "static/third",
     # Transifex syncs translation.json files without trailing
     # newlines; there's nothing other than trailing newlines we'd be
