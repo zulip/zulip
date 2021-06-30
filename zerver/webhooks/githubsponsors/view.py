d18a28c23db211f85333dcb168e36d42b37814ce
Sejal Welankar <sejal.welankar@wsu.edu>
2021-06-30 11:02:41 -0700

    mend

--- zerver/webhooks/githubsponsors/view.py
+++ zerver/webhooks/githubsponsors/view.py
@@ -2,11 +2,30 @@ from typing import Any, Dict
 
 from django.http import HttpRequest, HttpResponse
 
-from zerver.decorator import webhook_view
+from zerver.decorator import log_exception_to_webhook_logger, webhook_view
 from zerver.lib.request import REQ, has_request_variables
 from zerver.lib.response import json_success
-from zerver.lib.webhooks.common import check_send_webhook_message
+from zerver.lib.webhooks.common import check_send_webhook_message, get_http_headers_from_filename
 from zerver.models import UserProfile
+
+fixture_to_headers = get_http_headers_from_filename("HTTP_X_GITHUB_EVENT")
+
+
+class Helper:
+    def __init__(
+        self,
+        payload: Dict[str, Any],
+        include_title: bool,
+    ) -> None:
+        self.payload = payload
+        self.include_title = include_title
+
+    def log_unsupported(self, event: str) -> None:
+        summary = f"The '{event}' event isn't currently supported by the GitHub webhook"
+        log_exception_to_webhook_logger(
+            summary=summary,
+            unsupported_event=True,
+        )
 
 
 @webhook_view("GithubSponsors")
