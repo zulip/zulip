17eed06eb15766e66d643e2ddf1bf5f4cfefcc96
Aman Agrawal <amanagr@zulip.com>
2021-07-06 15:40:03 +0530

    message_view_header: Don't include stream name is tooltip.
    This fixes the bug where stream name overflows out of the tooltip
    background.

--- static/js/message_view_header.js
+++ static/js/message_view_header.js
@@ -53,7 +53,7 @@ function make_message_view_header(filter) {
         message_view_header.formatted_sub_count = get_formatted_sub_count(sub_count);
         // the "title" is passed as a variable and doesn't get translated (nor should it)
         message_view_header.sub_count_tooltip_text = $t(
-            {defaultMessage: "{count} users are subscribed to #{title}"},
+            {defaultMessage: "This stream has {count} subscribers."},
             {count: message_view_header.sub_count, title: message_view_header.title},
         );
         message_view_header.stream_settings_link =
