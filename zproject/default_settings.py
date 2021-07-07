98194b9316fed7c10ffb497ab84191ab245bf885
Anders Kaseorg <anders@zulip.com>
2021-07-06 17:08:14 -0700

    blueslip: Remove SAVE_FRONTEND_STACKTRACES setting.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- zproject/default_settings.py
+++ zproject/default_settings.py
@@ -288,10 +288,6 @@ EMAIL_BACKEND: Optional[str] = None
 # Set in settings.py when email isn't configured.
 WARN_NO_EMAIL = False
 
-# Whether to keep extra frontend stack trace data.
-# TODO: Investigate whether this should be removed and set one way or other.
-SAVE_FRONTEND_STACKTRACES = False
-
 # If True, disable rate-limiting and other filters on sending error messages
 # to admins, and enable logging on the error-reporting itself.  Useful
 # mainly in development.
