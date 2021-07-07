06b186dc7e9f9ac9876ad35901e9610375f18dda
ericluoliu <erikiceliu@gmail.com>
2021-07-07 16:41:24 -0700

    git_shortlog: Add python script for combining shortlogs git_shortlog: Add python script for combining shortlogsChanges were made to allow checking of total contributors across multiple Zulip repositories. The change was made by adding a python script.

--- totalContributors.py
+++ totalContributors.py
@@ -1,6 +1,6 @@
-import subprocess
+import os
 import pathlib
-import os.path
+import subprocess
 import sys
 from datetime import datetime
 
@@ -15,9 +15,11 @@ def add_log(dict, input):
             dict[name] = value
 
 
-def retrieve_log(repo, version):
+def retrieve_log(repo, lower_version, upper_version):
     return subprocess.check_output(
-        ["git", "shortlog", "-s", version], universal_newlines=True, cwd=find_path(repo)
+        ["git", "shortlog", "-s", lower_version + ".." + upper_version],
+        universal_newlines=True,
+        cwd=find_path(repo),
     ).splitlines()
 
 
@@ -42,16 +44,21 @@ def find_path(repo):
 
 # extract git version and time
 if len(sys.argv) > 1:
-    previous_zulip_version = sys.argv[1]
+    lower_zulip_version = sys.argv[1].split("..")[0]
+    upper_zulip_version = sys.argv[1].split("..")[1]
 else:
-    previous_zulip_version = "4.3"
+    lower_zulip_version = "1.3.0"  # first Zulip version
+    upper_zulip_version = "4.3"  # latest Zulip version
 
-time = subprocess.check_output(
-    ["git", "log", "-1", "--format=%ai", previous_zulip_version], universal_newlines=True
+lower_time = subprocess.check_output(
+    ["git", "log", "-1", "--format=%ai", lower_zulip_version], universal_newlines=True
+).split()[0]
+upper_time = subprocess.check_output(
+    ["git", "log", "-1", "--format=%ai", upper_zulip_version], universal_newlines=True
 ).split()[0]
 
 out_dict = {}
-zulip = retrieve_log("zulip", previous_zulip_version)
+zulip = retrieve_log("zulip", lower_zulip_version, upper_zulip_version)
 add_log(out_dict, zulip)
 for repo_name in [
     "zulip-mobile",
@@ -60,11 +67,17 @@ for repo_name in [
     "python-zulip-api",
     "zulip-terminal",
 ]:
-    repo_version = find_version(time, repo_name)
-    repo_log = retrieve_log(repo_name, repo_version)
+    lower_repo_version = find_version(lower_time, repo_name)
+    upper_repo_version = find_version(upper_time, repo_name)
+    repo_log = retrieve_log(repo_name, lower_repo_version, upper_repo_version)
     add_log(out_dict, repo_log)
 
 for keys in out_dict:
     print(str(out_dict[keys]) + "\t" + keys)
 
-print("Previous Zulip version " + previous_zulip_version)
+print(
+    "Total contributions across all Zulip repos within Zulip versions "
+    + lower_zulip_version
+    + " and "
+    + upper_zulip_version
+)
