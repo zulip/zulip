9b2186e232ca0653b557563957d539f1b141c69c
Anders Kaseorg <anders@zulip.com>
2021-07-19 14:27:29 -0700

    Revert "Revert "request: Refactor to record rate limit data using ZulipRequestNotes.""
    This reverts commit 49eab4efef8d9e1561f926f1e4db47e66d5e6807.
    
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- zerver/middleware.py
+++ zerver/middleware.py
@@ -540,8 +540,9 @@ class RateLimitMiddleware(MiddlewareMixin):
             return response
 
         # Add X-RateLimit-*** headers
-        if hasattr(request, "_ratelimits_applied"):
-            self.set_response_headers(response, request._ratelimits_applied)
+        ratelimits_applied = get_request_notes(request).ratelimits_applied
+        if len(ratelimits_applied) > 0:
+            self.set_response_headers(response, ratelimits_applied)
 
         return response
 
