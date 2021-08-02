zulip/zulip

commit de04f0ad6770fade5350f0b1261233334d9e3f4d

notifications: Calculate PMs/mentions settings like other settings.
Previously, we checked for the `enable_offline_email_notifications` and
`enable_offline_push_notifications` settings (which determine whether the
user will receive notifications for PMs and mentions) just before sending
notifications. This has a few problem:

1. We do not have access to all the user settings in the notification
handlers (`handle_missedmessage_emails` and `handle_push_notifications`),
and therefore, we cannot correctly determine whether the notification should
be sent. Checks like the following which existed previously, will, for
example, incorrectly not send notifications even when stream email
notifications are enabled-
```
if not receives_offline_email_notifications(user_profile):
    return
```
With this commit, we simply do not enqueue notifications if the "offline"
settings are disabled, which fixes that bug.

Additionally, this also fixes a bug with the "online push notifications"
feature, which was, if someone were to:
* turn off notifications for PMs and mentions (`enable_offline_push_notifications`)
* turn on stream push notifications (`enable_stream_push_notifications`)
* turn on "online push" (`enable_online_push_notifications`)

then, they would still receive notifications for PMs when online.
This isn't how the "online push enabled" feature is supposed to work;
it should only act as a wrapper around the other notification settings.

The buggy code was this in `handle_push_notifications`:
```
if not (
    receives_offline_push_notifications(user_profile)
    or receives_online_push_notifications(user_profile)
):
    return

    // send notifications
```

This commit removes that code, and extends our `notification_data.py` logic
to cover this case, along with tests.

2. The name for these settings is slightly misleading. They essentially
talk about "what to send notifications for" (PMs and mentions), and not

zerver/lib/email_notifications.py
@@ -41,7 +41,6 @@ from zerver.models import (
     get_context_for_message,
     get_display_recipient,
     get_user_profile_by_id,
-    receives_offline_email_notifications,
 )
 
 
@@ -585,8 +584,6 @@ def handle_missedmessage_emails(
     }
 
     user_profile = get_user_profile_by_id(user_profile_id)
-    if not receives_offline_email_notifications(user_profile):
-        return
 
     # Note: This query structure automatically filters out any
     # messages that were permanently deleted, since those would now be

zerver/lib/actions.py
@@ -1456,6 +1456,8 @@ def render_incoming_message(
 class RecipientInfoResult(TypedDict):
     active_user_ids: Set[int]
     online_push_user_ids: Set[int]
+    pm_mention_email_disabled_user_ids: Set[int]
+    pm_mention_push_disabled_user_ids: Set[int]
     stream_email_user_ids: Set[int]
     stream_push_user_ids: Set[int]
     wildcard_mention_user_ids: Set[int]
@@ -1586,6 +1588,8 @@ def get_recipient_info(
         query = UserProfile.objects.filter(is_active=True).values(
             "id",
             "enable_online_push_notifications",
+            "enable_offline_email_notifications",
+            "enable_offline_push_notifications",
             "is_bot",
             "bot_type",
             "long_term_idle",
@@ -1628,6 +1632,16 @@ def get_recipient_info(
         lambda r: r["enable_online_push_notifications"],
     )
 
+    # We deal with only the users who have disabled this setting, since that
+    # will ususally be much smaller a set than those who have enabled it (which
+    # is the default)
+    pm_mention_email_disabled_user_ids = get_ids_for(
+        lambda r: not r["enable_offline_email_notifications"]
+    )
+    pm_mention_push_disabled_user_ids = get_ids_for(
+        lambda r: not r["enable_offline_push_notifications"]
+    )
+
     # Service bots don't get UserMessage rows.
     um_eligible_user_ids = get_ids_for(
         lambda r: not is_service_bot(r),
@@ -1656,6 +1670,8 @@ def get_recipient_info(
     info: RecipientInfoResult = dict(
         active_user_ids=active_user_ids,
         online_push_user_ids=online_push_user_ids,
+        pm_mention_email_disabled_user_ids=pm_mention_email_disabled_user_ids,
+        pm_mention_push_disabled_user_ids=pm_mention_push_disabled_user_ids,
         stream_push_user_ids=stream_push_user_ids,
         stream_email_user_ids=stream_email_user_ids,
         wildcard_mention_user_ids=wildcard_mention_user_ids,
@@ -1859,6 +1875,8 @@ def build_message_send_dict(
         rendering_result=rendering_result,
         active_user_ids=info["active_user_ids"],
         online_push_user_ids=info["online_push_user_ids"],
+        pm_mention_email_disabled_user_ids=info["pm_mention_email_disabled_user_ids"],
+        pm_mention_push_disabled_user_ids=info["pm_mention_push_disabled_user_ids"],
         stream_push_user_ids=info["stream_push_user_ids"],
         stream_email_user_ids=info["stream_email_user_ids"],
         muted_sender_user_ids=info["muted_sender_user_ids"],
@@ -2012,14 +2030,20 @@ def do_send_messages(
         sender = send_request.message.sender
         message_type = wide_message_dict["type"]
         active_users_data = [
-            UserMessageNotificationsData.from_user_id_sets(
-                user_id=user_id,
-                flags=user_flags.get(user_id, []),
-                online_push_user_ids=send_request.online_push_user_ids,
-                stream_push_user_ids=send_request.stream_push_user_ids,
-                stream_email_user_ids=send_request.stream_email_user_ids,
-                wildcard_mention_user_ids=send_request.wildcard_mention_user_ids,
-                muted_sender_user_ids=send_request.muted_sender_user_ids,
+            ActivePresenceIdleUserData(
+                alerted="has_alert_word" in user_flags.get(user_id, []),
+                notifications_data=UserMessageNotificationsData.from_user_id_sets(
+                    user_id=user_id,
+                    flags=user_flags.get(user_id, []),
+                    private_message=(message_type == "private"),
+                    online_push_user_ids=send_request.online_push_user_ids,
+                    pm_mention_push_disabled_user_ids=send_request.pm_mention_push_disabled_user_ids,
+                    pm_mention_email_disabled_user_ids=send_request.pm_mention_email_disabled_user_ids,
+                    stream_push_user_ids=send_request.stream_push_user_ids,
+                    stream_email_user_ids=send_request.stream_email_user_ids,
+                    wildcard_mention_user_ids=send_request.wildcard_mention_user_ids,
+                    muted_sender_user_ids=send_request.muted_sender_user_ids,
+                ),
             )
             for user_id in send_request.active_user_ids
         ]
@@ -2027,7 +2051,6 @@ def do_send_messages(
         presence_idle_user_ids = get_active_presence_idle_user_ids(
             realm=sender.realm,
             sender_id=sender.id,
-            message_type=message_type,
             active_users_data=active_users_data,
         )
 
@@ -2037,6 +2060,10 @@ def do_send_messages(
             message_dict=wide_message_dict,
             presence_idle_user_ids=presence_idle_user_ids,
             online_push_user_ids=list(send_request.online_push_user_ids),
+            pm_mention_push_disabled_user_ids=list(send_request.pm_mention_push_disabled_user_ids),
+            pm_mention_email_disabled_user_ids=list(
+                send_request.pm_mention_email_disabled_user_ids
+            ),
             stream_push_user_ids=list(send_request.stream_push_user_ids),
             stream_email_user_ids=list(send_request.stream_email_user_ids),
             wildcard_mention_user_ids=list(send_request.wildcard_mention_user_ids),
@@ -6057,6 +6084,10 @@ def do_update_message(
         )
 
         event["online_push_user_ids"] = list(info["online_push_user_ids"])
+        event["pm_mention_push_disabled_user_ids"] = list(info["pm_mention_push_disabled_user_ids"])
+        event["pm_mention_email_disabled_user_ids"] = list(
+            info["pm_mention_email_disabled_user_ids"]
+        )
         event["stream_push_user_ids"] = list(info["stream_push_user_ids"])
         event["stream_email_user_ids"] = list(info["stream_email_user_ids"])
         event["muted_sender_user_ids"] = list(info["muted_sender_user_ids"])
@@ -6673,11 +6704,15 @@ def gather_subscriptions(
     return (subscribed, unsubscribed)
 
 
+class ActivePresenceIdleUserData(TypedDict):
+    alerted: bool
+    notifications_data: UserMessageNotificationsData
+
+
 def get_active_presence_idle_user_ids(
     realm: Realm,
     sender_id: int,
-    message_type: str,
-    active_users_data: List[UserMessageNotificationsData],
+    active_users_data: List[ActivePresenceIdleUserData],
 ) -> List[int]:
     """
     Given a list of active_user_ids, we build up a subset
@@ -6691,17 +6726,16 @@ def get_active_presence_idle_user_ids(
     if realm.presence_disabled:
         return []
 
-    is_private_message = message_type == "private"
-
     user_ids = set()
     for user_data in active_users_data:
-        alerted = "has_alert_word" in user_data.flags
+        user_notifications_data: UserMessageNotificationsData = user_data["notifications_data"]
+        alerted = user_data["alerted"]
 
         # We only need to know the presence idle state for a user if this message would be notifiable
         # for them if they were indeed idle. Only including those users in the calculation below is a
         # very important optimization for open communities with many inactive users.
-        if user_data.is_notifiable(is_private_message, sender_id, idle=True) or alerted:
-            user_ids.add(user_data.user_id)
+        if user_notifications_data.is_notifiable(sender_id, idle=True) or alerted:
+            user_ids.add(user_notifications_data.user_id)
 
     return filter_presence_idle_user_ids(user_ids)
 

zerver/lib/message.py
@@ -111,6 +111,8 @@ class SendMessageRequest:
     mentioned_user_groups_map: Dict[int, int]
     active_user_ids: Set[int]
     online_push_user_ids: Set[int]
+    pm_mention_push_disabled_user_ids: Set[int]
+    pm_mention_email_disabled_user_ids: Set[int]
     stream_push_user_ids: Set[int]
     stream_email_user_ids: Set[int]
     muted_sender_user_ids: Set[int]

zerver/lib/notification_data.py
@@ -8,26 +8,34 @@ from zerver.models import NotificationTriggers
 @dataclass
 class UserMessageNotificationsData:
     user_id: int
-    flags: Collection[str]
-    mentioned: bool
     online_push_enabled: bool
+    pm_email_notify: bool
+    pm_push_notify: bool
+    mention_email_notify: bool
+    mention_push_notify: bool
     stream_push_notify: bool
     stream_email_notify: bool
     wildcard_mention_notify: bool
     sender_is_muted: bool
 
     def __post_init__(self) -> None:
-        if self.mentioned:
-            assert "mentioned" in self.flags
-        if self.wildcard_mention_notify:
-            assert "wildcard_mentioned" in self.flags
+        # Check that there's no dubious data.
+        if self.pm_email_notify or self.pm_push_notify:
+            assert not (self.stream_email_notify or self.stream_push_notify)
+
+        if self.stream_email_notify or self.stream_push_notify:
+            assert not (self.pm_email_notify or self.pm_push_notify)
 
     @classmethod
     def from_user_id_sets(
         cls,
+        *,
         user_id: int,
         flags: Collection[str],
+        private_message: bool,
         online_push_user_ids: Set[int],
+        pm_mention_push_disabled_user_ids: Set[int],
+        pm_mention_email_disabled_user_ids: Set[int],
         stream_push_user_ids: Set[int],
         stream_email_user_ids: Set[int],
         wildcard_mention_user_ids: Set[int],
@@ -36,10 +44,22 @@ class UserMessageNotificationsData:
         wildcard_mention_notify = (
             user_id in wildcard_mention_user_ids and "wildcard_mentioned" in flags
         )
+
+        pm_email_notify = user_id not in pm_mention_email_disabled_user_ids and private_message
+        mention_email_notify = (
+            user_id not in pm_mention_email_disabled_user_ids and "mentioned" in flags
+        )
+
+        pm_push_notify = user_id not in pm_mention_push_disabled_user_ids and private_message
+        mention_push_notify = (
+            user_id not in pm_mention_push_disabled_user_ids and "mentioned" in flags
+        )
         return cls(
             user_id=user_id,
-            flags=flags,
-            mentioned=("mentioned" in flags),
+            pm_email_notify=pm_email_notify,
+            mention_email_notify=mention_email_notify,
+            pm_push_notify=pm_push_notify,
+            mention_push_notify=mention_push_notify,
             online_push_enabled=(user_id in online_push_user_ids),
             stream_push_notify=(user_id in stream_push_user_ids),
             stream_email_notify=(user_id in stream_email_user_ids),
@@ -47,24 +67,18 @@ class UserMessageNotificationsData:
             sender_is_muted=(user_id in muted_sender_user_ids),
         )
 
-    # TODO: The following functions should also look at the `enable_offline_push_notifications` and
-    # `enable_offline_email_notifications` settings (for PMs and mentions), but currently they
-    # don't.
-
     # For these functions, acting_user_id is the user sent a message
     # (or edited a message) triggering the event for which we need to
     # determine notifiability.
-    def is_notifiable(self, private_message: bool, acting_user_id: int, idle: bool) -> bool:
-        return self.is_email_notifiable(
-            private_message, acting_user_id, idle
-        ) or self.is_push_notifiable(private_message, acting_user_id, idle)
+    def is_notifiable(self, acting_user_id: int, idle: bool) -> bool:
+        return self.is_email_notifiable(acting_user_id, idle) or self.is_push_notifiable(
+            acting_user_id, idle
+        )
 
-    def is_push_notifiable(self, private_message: bool, acting_user_id: int, idle: bool) -> bool:
-        return self.get_push_notification_trigger(private_message, acting_user_id, idle) is not None
+    def is_push_notifiable(self, acting_user_id: int, idle: bool) -> bool:
+        return self.get_push_notification_trigger(acting_user_id, idle) is not None
 
-    def get_push_notification_trigger(
-        self, private_message: bool, acting_user_id: int, idle: bool
-    ) -> Optional[str]:
+    def get_push_notification_trigger(self, acting_user_id: int, idle: bool) -> Optional[str]:
         if not idle and not self.online_push_enabled:
             return None
 
@@ -74,9 +88,12 @@ class UserMessageNotificationsData:
         if self.sender_is_muted:
             return None
 
-        if private_message:
+        # The order here is important. If, for example, both
+        # `mention_push_notify` and `stream_push_notify` are True, we
+        # want to classify it as a mention, since that's more salient.
+        if self.pm_push_notify:
             return NotificationTriggers.PRIVATE_MESSAGE
-        elif self.mentioned:
+        elif self.mention_push_notify:
             return NotificationTriggers.MENTION
         elif self.wildcard_mention_notify:
             return NotificationTriggers.WILDCARD_MENTION
@@ -85,14 +102,10 @@ class UserMessageNotificationsData:
         else:
             return None
 
-    def is_email_notifiable(self, private_message: bool, acting_user_id: int, idle: bool) -> bool:
-        return (
-            self.get_email_notification_trigger(private_message, acting_user_id, idle) is not None
-        )
+    def is_email_notifiable(self, acting_user_id: int, idle: bool) -> bool:
+        return self.get_email_notification_trigger(acting_user_id, idle) is not None
 
-    def get_email_notification_trigger(
-        self, private_message: bool, acting_user_id: int, idle: bool
-    ) -> Optional[str]:
+    def get_email_notification_trigger(self, acting_user_id: int, idle: bool) -> Optional[str]:
         if not idle:
             return None
 
@@ -102,9 +115,12 @@ class UserMessageNotificationsData:
         if self.sender_is_muted:
             return None
 
-        if private_message:
+        # The order here is important. If, for example, both
+        # `mention_email_notify` and `stream_email_notify` are True, we
+        # want to classify it as a mention, since that's more salient.
+        if self.pm_email_notify:
             return NotificationTriggers.PRIVATE_MESSAGE
-        elif self.mentioned:
+        elif self.mention_email_notify:
             return NotificationTriggers.MENTION
         elif self.wildcard_mention_notify:
             return NotificationTriggers.WILDCARD_MENTION

zerver/lib/push_notifications.py
@@ -35,8 +35,6 @@ from zerver.models import (
     UserProfile,
     get_display_recipient,
     get_user_profile_by_id,
-    receives_offline_push_notifications,
-    receives_online_push_notifications,
 )
 
 if TYPE_CHECKING:
@@ -903,11 +901,6 @@ def handle_push_notification(user_profile_id: int, missed_message: Dict[str, Any
     if not push_notifications_enabled():
         return
     user_profile = get_user_profile_by_id(user_profile_id)
-    if not (
-        receives_offline_push_notifications(user_profile)
-        or receives_online_push_notifications(user_profile)
-    ):
-        return
 
     try:
         (message, user_message) = access_message(user_profile, missed_message["message_id"])

zerver/lib/test_classes.py
@@ -1339,9 +1339,11 @@ Output:
     ) -> UserMessageNotificationsData:
         return UserMessageNotificationsData(
             user_id=user_id,
-            flags=kwargs.get("flags", []),
-            mentioned=kwargs.get("mentioned", False),
             online_push_enabled=kwargs.get("online_push_enabled", False),
+            pm_email_notify=kwargs.get("pm_email_notify", False),
+            pm_push_notify=kwargs.get("pm_push_notify", False),
+            mention_email_notify=kwargs.get("mention_email_notify", False),
+            mention_push_notify=kwargs.get("mention_push_notify", False),
             stream_email_notify=kwargs.get("stream_email_notify", False),
             stream_push_notify=kwargs.get("stream_push_notify", False),
             wildcard_mention_notify=kwargs.get("wildcard_mention_notify", False),
@@ -1363,7 +1365,6 @@ Output:
             user_notifications_data=user_notifications_data,
             message_id=message_id,
             acting_user_id=acting_user_id,
-            private_message=kwargs.get("private_message", False),
             mentioned_user_group_id=kwargs.get("mentioned_user_group_id", None),
             idle=kwargs.get("idle", True),
             already_notified=kwargs.get(

zerver/models.py
@@ -1854,18 +1854,6 @@ class UserGroupMembership(models.Model):
         unique_together = (("user_group", "user_profile"),)
 
 
-def receives_offline_push_notifications(user_profile: UserProfile) -> bool:
-    return user_profile.enable_offline_push_notifications and not user_profile.is_bot
-
-
-def receives_offline_email_notifications(user_profile: UserProfile) -> bool:
-    return user_profile.enable_offline_email_notifications and not user_profile.is_bot
-
-
-def receives_online_push_notifications(user_profile: UserProfile) -> bool:
-    return user_profile.enable_online_push_notifications and not user_profile.is_bot
-
-
 def remote_user_to_email(remote_user: str) -> str:
     if settings.SSO_APPEND_DOMAIN is not None:
         remote_user += "@" + settings.SSO_APPEND_DOMAIN

zerver/tests/test_email_notifications.py
@@ -23,14 +23,7 @@ from zerver.lib.email_notifications import (
 from zerver.lib.send_email import FromAddress, send_custom_email
 from zerver.lib.test_classes import ZulipTestCase
 from zerver.lib.user_groups import create_user_group
-from zerver.models import (
-    ScheduledEmail,
-    UserMessage,
-    UserProfile,
-    get_realm,
-    get_stream,
-    receives_offline_email_notifications,
-)
+from zerver.models import ScheduledEmail, UserMessage, UserProfile, get_realm, get_stream
 
 
 class TestCustomEmails(ZulipTestCase):
@@ -1361,37 +1354,3 @@ class TestMissedMessages(ZulipTestCase):
             + 'title="cloud with lightning and rain" style="height: 20px;">.</p>'
         )
         self.assertEqual(actual_output, expected_output)
-
-
-class TestReceivesNotificationsFunctions(ZulipTestCase):
-    def test_receivers_offline_notifications_when_user_is_a_bot(self) -> None:
-        hamlet = self.example_user("hamlet")
-        hamlet.is_bot = True
-
-        hamlet.enable_offline_email_notifications = True
-        self.assertFalse(receives_offline_email_notifications(hamlet))
-
-        hamlet.enable_offline_email_notifications = False
-        self.assertFalse(receives_offline_email_notifications(hamlet))
-
-        hamlet.enable_offline_email_notifications = True
-        self.assertFalse(receives_offline_email_notifications(hamlet))
-
-        hamlet.enable_offline_email_notifications = False
-        self.assertFalse(receives_offline_email_notifications(hamlet))
-
-    def test_receivers_offline_notifications_when_user_is_not_a_bot(self) -> None:
-        hamlet = self.example_user("hamlet")
-        hamlet.is_bot = False
-
-        hamlet.enable_offline_email_notifications = True
-        self.assertTrue(receives_offline_email_notifications(hamlet))
-
-        hamlet.enable_offline_email_notifications = False
-        self.assertFalse(receives_offline_email_notifications(hamlet))
-
-        hamlet.enable_offline_email_notifications = True
-        self.assertTrue(receives_offline_email_notifications(hamlet))
-
-        hamlet.enable_offline_email_notifications = False
-        self.assertFalse(receives_offline_email_notifications(hamlet))

commit 24b1439e9333b25207758178cfa575b96f4c6314

email_notifications: Avoid trying to email to bot users.
This fixes a regression in de04f0ad6770fade5350f0b1261233334d9e3f4d.

We'll do a proper test in a follow-up commit; this is a quick fix to
make sure master works.

The emails will bounce, but it'll create all sorts of infrastructure
headaches.

zerver/lib/email_notifications.py
@@ -584,6 +584,9 @@ def handle_missedmessage_emails(
     }
 
     user_profile = get_user_profile_by_id(user_profile_id)
+    if user_profile.is_bot:  # nocoverage # TODO -- needs a test.
+        # Never email bot users.
+        return
 
     # Note: This query structure automatically filters out any
     # messages that were permanently deleted, since those would now be