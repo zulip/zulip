ce3218b81a2bd637f4cf2a0dd350593a19c4e68c
Vishnu KS <yo@vishnuks.com>
2021-08-29 13:33:29 +0000

    billing: Migrate to Stripe hosted checkout page.

---
+++ corporate/lib/stripe_event_handler.py
@@ -0,0 +1,176 @@
+import logging
+from typing import Any, Callable, Union
+
+import stripe
+from django.conf import settings
+
+from corporate.lib.stripe import (
+    BillingError,
+    UpgradeWithExistingPlanError,
+    ensure_realm_does_not_have_active_plan,
+    process_initial_upgrade,
+    update_or_create_stripe_customer,
+)
+from corporate.models import Event, PaymentIntent, Session
+from zerver.models import get_user_by_delivery_email
+
+billing_logger = logging.getLogger("corporate.stripe")
+
+
+def error_handler(
+    func: Callable[[Any, Any], None],
+) -> Callable[[Union[stripe.checkout.Session, stripe.PaymentIntent], Event], None]:
+    def wrapper(
+        stripe_object: Union[stripe.checkout.Session, stripe.PaymentIntent], event: Event
+    ) -> None:
+        event.status = Event.EVENT_HANDLER_STARTED
+        event.save(update_fields=["status"])
+
+        try:
+            func(stripe_object, event.content_object)
+        except BillingError as e:
+            billing_logger.warning(
+                "BillingError in %s event handler: %s. stripe_object_id=%s, customer_id=%s metadata=%s",
+                event.type,
+                e.error_description,
+                stripe_object.id,
+                stripe_object.customer,
+                stripe_object.metadata,
+            )
+            event.status = Event.EVENT_HANDLER_FAILED
+            event.handler_error = {
+                "message": e.msg,
+                "description": e.error_description,
+            }
+            event.save(update_fields=["status", "handler_error"])
+        except Exception:
+            billing_logger.exception(
+                "Uncaught exception in %s event handler:",
+                event.type,
+                stack_info=True,
+            )
+            event.status = Event.EVENT_HANDLER_FAILED
+            event.handler_error = {
+                "description": f"uncaught exception in {event.type} event handler",
+                "message": BillingError.CONTACT_SUPPORT.format(email=settings.ZULIP_ADMINISTRATOR),
+            }
+            event.save(update_fields=["status", "handler_error"])
+        else:
+            event.status = Event.EVENT_HANDLER_SUCCEEDED
+            event.save()
+
+    return wrapper
+
+
+@error_handler
+def handle_checkout_session_completed_event(
+    stripe_session: stripe.checkout.Session, session: Session
+) -> None:
+    session.status = Session.COMPLETED
+    session.save()
+
+    stripe_setup_intent = stripe.SetupIntent.retrieve(stripe_session.setup_intent)
+    stripe_customer = stripe.Customer.retrieve(stripe_setup_intent.customer)
+    user = get_user_by_delivery_email(stripe_customer.email, session.customer.realm)
+    payment_method = stripe_setup_intent.payment_method
+
+    if session.type in [
+        Session.UPGRADE_FROM_BILLING_PAGE,
+        Session.RETRY_UPGRADE_WITH_ANOTHER_PAYMENT_METHOD,
+    ]:
+        ensure_realm_does_not_have_active_plan(user.realm)
+        update_or_create_stripe_customer(user, payment_method)
+        session.payment_intent.status = PaymentIntent.PROCESSING
+        session.payment_intent.last_payment_error = ()
+        session.payment_intent.save(update_fields=["status", "last_payment_error"])
+        try:
+            stripe.PaymentIntent.confirm(
+                session.payment_intent.stripe_payment_intent_id,
+                payment_method=payment_method,
+                off_session=True,
+            )
+        except stripe.error.CardError:
+            pass
+    elif session.type in [
+        Session.FREE_TRIAL_UPGRADE_FROM_BILLING_PAGE,
+        Session.FREE_TRIAL_UPGRADE_FROM_ONBOARDING_PAGE,
+    ]:
+        ensure_realm_does_not_have_active_plan(user.realm)
+        update_or_create_stripe_customer(user, payment_method)
+        process_initial_upgrade(
+            user,
+            int(stripe_setup_intent.metadata["licenses"]),
+            stripe_setup_intent.metadata["license_management"] == "automatic",
+            int(stripe_setup_intent.metadata["billing_schedule"]),
+            charge_automatically=True,
+            free_trial=True,
+        )
+    elif session.type in [Session.CARD_UPDATE_FROM_BILLING_PAGE]:
+        update_or_create_stripe_customer(user, payment_method)
+
+
+@error_handler
+def handle_payment_intent_succeeded_event(
+    stripe_payment_intent: stripe.PaymentIntent, payment_intent: PaymentIntent
+) -> None:
+    payment_intent.status = PaymentIntent.SUCCEEDED
+    payment_intent.save()
+    metadata = stripe_payment_intent.metadata
+    user = get_user_by_delivery_email(metadata["user_email"], payment_intent.customer.realm)
+
+    description = ""
+    for charge in stripe_payment_intent.charges:
+        description = f"Payment (Card ending in {charge.payment_method_details.card.last4})"
+        break
+
+    stripe.InvoiceItem.create(
+        amount=stripe_payment_intent.amount * -1,
+        currency="usd",
+        customer=stripe_payment_intent.customer,
+        description=description,
+        discountable=False,
+    )
+    try:
+        ensure_realm_does_not_have_active_plan(user.realm)
+    except UpgradeWithExistingPlanError as e:
+        stripe_invoice = stripe.Invoice.create(
+            auto_advance=True,
+            collection_method="charge_automatically",
+            customer=stripe_payment_intent.customer,
+            days_until_due=None,
+            statement_descriptor="Zulip Standard Credit",
+        )
+        stripe.Invoice.finalize_invoice(stripe_invoice)
+        raise e
+
+    process_initial_upgrade(
+        user,
+        int(metadata["licenses"]),
+        metadata["license_management"] == "automatic",
+        int(metadata["billing_schedule"]),
+        True,
+        False,
+    )
+
+
+@error_handler
+def handle_payment_intent_payment_failed_event(
+    stripe_payment_intent: stripe.PaymentIntent, payment_intent: Event
+) -> None:
+    payment_intent.status = PaymentIntent.get_status_integer_from_status_text(
+        stripe_payment_intent.status
+    )
+    billing_logger.info(
+        "Stripe payment intent failed: %s %s %s %s",
+        payment_intent.customer.realm.string_id,
+        stripe_payment_intent.last_payment_error.get("type"),
+        stripe_payment_intent.last_payment_error.get("code"),
+        stripe_payment_intent.last_payment_error.get("param"),
+    )
+    payment_intent.last_payment_error = {
+        "description": stripe_payment_intent.last_payment_error.get("type"),
+    }
+    payment_intent.last_payment_error["message"] = stripe_payment_intent.last_payment_error.get(
+        "message"
+    )
+    payment_intent.save(update_fields=["status", "last_payment_error"])
--- corporate/urls.py
+++ corporate/urls.py
@@ -4,9 +4,15 @@ from django.conf.urls import include
 from django.urls import path
 from django.views.generic import TemplateView
 
-from corporate.views.billing_page import billing_home, replace_payment_source, update_plan
+from corporate.views.billing_page import billing_home, update_plan
+from corporate.views.event_status import event_status, event_status_page
+from corporate.views.session import (
+    start_card_update_stripe_session,
+    start_retry_payment_intent_session,
+)
 from corporate.views.support import support_request
 from corporate.views.upgrade import initial_upgrade, sponsorship, upgrade
+from corporate.views.webhook import stripe_webhook
 from zerver.lib.rest import rest_path
 
 i18n_urlpatterns: Any = [
@@ -15,16 +21,23 @@ i18n_urlpatterns: Any = [
     path("zephyr-mirror/", TemplateView.as_view(template_name="corporate/zephyr-mirror.html")),
     path("jobs/", TemplateView.as_view(template_name="corporate/jobs.html")),
     # Billing
-    path("billing/", billing_home),
+    path("billing/", billing_home, name="billing_home"),
     path("upgrade/", initial_upgrade, name="initial_upgrade"),
     path("support/", support_request),
+    path("billing/event_status/", event_status_page, name="event_status_page"),
+    path("stripe/webhook/", stripe_webhook, name="stripe_webhook"),
 ]
 
 v1_api_and_json_patterns = [
     rest_path("billing/upgrade", POST=upgrade),
     rest_path("billing/sponsorship", POST=sponsorship),
     rest_path("billing/plan", PATCH=update_plan),
-    rest_path("billing/sources/change", POST=replace_payment_source),
+    rest_path("billing/session/start_card_update_session", POST=start_card_update_stripe_session),
+    rest_path(
+        "billing/session/start_retry_payment_intent_session",
+        POST=start_retry_payment_intent_session,
+    ),
+    rest_path("billing/event/status", GET=event_status),
 ]
 
 # Make a copy of i18n_urlpatterns so that they appear without prefix for English
