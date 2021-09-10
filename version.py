04075201a08182c8312cec5f57c7abefb0f8ad21
Anders Kaseorg <anders@zulip.com>
2021-09-10 12:28:29 -0700

    dependencies: Upgrade webpack-dev-server to 4.2.0.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -48,4 +48,4 @@ API_FEATURE_LEVEL = 97
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = "157.2"
+PROVISION_VERSION = "158.0"
