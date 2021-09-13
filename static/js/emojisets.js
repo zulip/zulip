f12306619185ecf06d0c7e160bacd34d036655b4
Anders Kaseorg <anders@zulip.com>
2021-09-13 15:58:21 -0700

    emoji: Remove octopus override.
    There’s no objective reason to override this emoji.  Organizations
    that really want to enforce a specific octopus can configure it as a
    custom emoji.
    
    (This has no visible effect for users of the Google classic emoji set
    that was the default until recently.)
    
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- static/js/emojisets.js
+++ static/js/emojisets.js
@@ -2,8 +2,6 @@ import google_blob_sheet from "emoji-datasource-google-blob/img/google/sheets-25
 import google_sheet from "emoji-datasource-google/img/google/sheets-256/64.png";
 import twitter_sheet from "emoji-datasource-twitter/img/twitter/sheets-256/64.png";
 
-import octopus_url from "../generated/emoji/images-google-64/1f419.png";
-
 import {user_settings} from "./user_settings";
 
 import google_blob_css from "!style-loader?injectType=lazyStyleTag!css-loader!../generated/emoji-styles/google-blob-sprite.css";
@@ -42,12 +40,4 @@ export async function select(name) {
 
 export function initialize() {
     select(user_settings.emojiset);
-
-    // Load the octopus image in the background, so that the browser
-    // will cache it for later use.  Note that we hardcode the octopus
-    // emoji to the old Google one because it's better.
-    //
-    // TODO: We should probably just make this work just like the Zulip emoji.
-    const octopus_image = new Image();
-    octopus_image.src = octopus_url;
 }
