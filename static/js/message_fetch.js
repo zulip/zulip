14f51014a46685c0e749a374fffcf77e480e3363
Aman Agrawal <amanagr@zulip.com>
2021-09-14 15:02:01 +0000

    message_fetch: For spectators, add web-public to narrow.
    Without this, server will return a 401 error.

--- static/js/message_fetch.js
+++ static/js/message_fetch.js
@@ -240,6 +240,18 @@ export function load_messages(opts) {
     data.client_gravatar = true;
     data = handle_operators_supporting_id_based_api(data);
 
+    if (page_params.is_spectator) {
+        const web_public_narrow = {negated: false, operator: "streams", operand: "web-public"};
+
+        if (!data.narrow) {
+            data.narrow = JSON.stringify([web_public_narrow]);
+        } else if (Filter.is_spectator_compatible(JSON.parse(data.narrow))) {
+            data.narrow = JSON.parse(data.narrow);
+            data.narrow.push(web_public_narrow);
+            data.narrow = JSON.stringify(data.narrow);
+        }
+    }
+
     channel.get({
         url: "/json/messages",
         data,
