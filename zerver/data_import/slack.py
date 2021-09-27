58b19761b89a3d9f432fcb696f6543558d4ca6ab
rht <rhtbot@protonmail.com>
2021-09-27 03:17:52 -0400

    slack import: Fix requests.get usage of get_slack_api_data.
    We also rewrite the tests using the `responses` module to avoid the
    problematic mocking that made this bug possible.
    
    Fixes #19833.

--- zerver/data_import/slack.py
+++ zerver/data_import/slack.py
@@ -1410,7 +1410,7 @@ def get_slack_api_data(slack_api_url: str, get_param: str, **kwargs: Any) -> Any
     if not kwargs.get("token"):
         raise AssertionError("Slack token missing in kwargs")
     token = kwargs.pop("token")
-    data = requests.get(slack_api_url, headers={"Authorization": f"Bearer {token}"}, **kwargs)
+    data = requests.get(slack_api_url, headers={"Authorization": f"Bearer {token}"}, params=kwargs)
 
     if data.status_code == requests.codes.ok:
         result = data.json()
