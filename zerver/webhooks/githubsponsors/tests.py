46eea60f88150a51f87a4ded774459a6d961cef0
iamu985 <frankbr985gomes@gmail.com>
2021-10-12 17:12:08 +0530

    Issue #18320 Github Sponsor integration, this notifies when a new sponsor has subscribed for a sponsorship, cancelled a subscription or changed their subscription tier

---
+++ zerver/webhooks/githubsponsors/tests.py
@@ -0,0 +1,62 @@
+from django.conf import settings
+
+from zerver.lib.test_classes import WebhookTestCase
+from zerver.models import get_realm, get_system_bot
+
+from .view import DatetimeParser
+
+
+class GithubSponsorHookTests(WebhookTestCase):
+    STREAM_NAME = "test"
+    URL_TEMPLATE = "/api/v1/external/githubsponsors?&api_key={api_key}&stream={stream}"
+    PM_URL_TEMPLATE = "/api/v1/external/githubsponsors?&api_key={api_key}"
+    WEBHOOK_DIR_NAME = "githubsponsors"
+
+    # NEW SPONSOR CREATED TEST
+    def test_github_sponsor_created_message(self) -> None:
+        expected_topic = "New Sponsorship Subscription"
+        expected_message = f':confetti: New Subscription for a Sponsorship! :confetti:\nmonalisa subscribed for a "$5 a month" Sponsorship on 20 December 2019, Friday'
+
+        # use fixture named github_sponsor_created
+        self.check_webhook(
+            "github_sponsor_created",
+            expected_topic,
+            expected_message,
+            content_type="application/json",
+        )
+
+    # SPONSOR TIER CHANGE TEST
+    def test_github_sponsor_pending_tier_change_message(self) -> None:
+        expected_topic = "Subscription Change"
+        expected_message = f':bullhorn: Subscription Tier Change :bullhorn:\nmonalisa changed their Sponsor subscription from "$10 a month" to "$5 a month" effective from 30 December 2019, Monday'
+
+        # using fixture named gihub_sponsor_downgrade
+        self.check_webhook(
+            "github_sponsor_downgrade",
+            expected_topic,
+            expected_message,
+            content_type="application/json",
+        )
+
+    def test_github_sponsor_cancelled_message(self) -> None:
+        expected_topic = "Cancelled Subscription"
+        expected_message = ":warning: Sponsorship Cancelled! :warning:\nSomebody cancelled their sponsorship subscription"
+
+        # using fixture github_sponsor_cancelled
+        self.check_webhook(
+            "github_sponsor_cancelled",
+            expected_topic,
+            expected_message,
+            content_type="application/json",
+        )
+
+    # NOT RELATED TO THE SPONSORSHIP WEBHOOK
+    # TESTING DATE PARSER HELPER CLASS IN THE VIEW
+    def test_github_sponsor_date_time_parser(self) -> None:
+        expected_date = "30 December 2019, Monday"
+        date_from_payload = "2019-12-30T00:00:00+00:00"
+
+        parser = DatetimeParser()
+        parsed_date = parser.parse(date_from_payload)
+
+        self.assertEqual(expected_date, parsed_date)
---
+++ zerver/webhooks/githubsponsors/view.py
@@ -0,0 +1,82 @@
+# datetime
+import datetime
+from typing import Any, Dict, Sequence, Union
+
+from django.http import HttpRequest, HttpResponse
+
+from zerver.decorator import webhook_view
+from zerver.lib.request import REQ, has_request_variables
+from zerver.lib.response import json_success
+from zerver.lib.webhooks.common import check_send_webhook_message
+from zerver.models import UserProfile
+
+
+class DatetimeParser:
+    def __init__(self):
+        self.date = None
+
+    def parse(self, date) -> str:
+        self.date = date
+        date_frm_payload = [int(i) for i in self.date.split("T")[0].split("-")]
+        date_string_object = datetime.datetime(
+            date_frm_payload[0], date_frm_payload[1], date_frm_payload[2]
+        )
+        formatted_date = date_string_object.strftime("%d %B %Y, %A")
+        return formatted_date
+
+
+@webhook_view("GitHubSponsors")
+@has_request_variables
+def api_githubsponsors_webhook(
+    request: HttpRequest,
+    user_profile: UserProfile,
+    payload: Dict[str, Any] = REQ(argument_type="body"),
+) -> HttpResponse:
+
+    # VARIABLES FROM THE PAYLOAD
+    parser = (
+        DatetimeParser()
+    )  # helper function to parse datetime object received from the payload to readable string format
+    ACTION = payload.get("action")
+    SENDER = payload["sender"]["login"]
+    if ACTION == "created":
+        TIER_NAME = payload["sponsorship"]["tier"]["name"]
+        DATE_SUBSCRIBED = parser.parse(payload["sponsorship"]["tier"]["created_at"])
+    if ACTION == "pending_tier_change":
+        DATE_CHANGED = parser.parse(payload["effective_date"])
+        TIER_FROM = payload["changes"]["tier"]["from"]["name"]
+        TIER_TO = payload["sponsorship"]["tier"]["name"]
+
+    # MESSAGE CONSTRUCTION
+    TOPICS = {
+        "created": "New Sponsorship Subscription",
+        "pending_tier_change": "Subscription Change",
+        "cancelled": "Cancelled Subscription",
+    }
+    ACTIONS = {
+        "created": ":confetti: New Subscription for a Sponsorship! :confetti:",
+        "pending_tier_change": ":bullhorn: Subscription Tier Change :bullhorn:",
+        "cancelled": ":warning: Sponsorship Cancelled! :warning:",
+    }
+
+    body = ACTIONS[ACTION]
+    if ACTION == "created":
+        body_template = '\n{sender} subscribed for a "{tier_name}" Sponsorship on {date_subscribed}'
+        body += body_template.format(
+            sender=SENDER, tier_name=TIER_NAME, date_subscribed=DATE_SUBSCRIBED
+        )
+    if ACTION == "pending_tier_change":
+        body_template = '\n{sender} changed their Sponsor subscription from "{tier_from}" to "{tier_to}" effective from {date_changed}'
+        body += body_template.format(
+            sender=SENDER, tier_from=TIER_FROM, tier_to=TIER_TO, date_changed=DATE_CHANGED
+        )
+    if ACTION == "cancelled":
+        body_template = "\nSomebody cancelled their sponsorship subscription"
+        body += body_template
+
+    topic = TOPICS[ACTION]
+
+    # SENDING THE MESSAGE
+    check_send_webhook_message(request, user_profile, topic, body)
+
+    return json_success()
