3ec41405bb2e3b3527ba33a2f9380b6a091bf33e
iamu985 <frankbr985gomes@gmail.com>
2021-10-14 10:25:04 +0000

    Removed Optional import from view.py file to fix import error from pylint

--- zerver/webhooks/githubsponsors/view.py
+++ zerver/webhooks/githubsponsors/view.py
@@ -1,6 +1,6 @@
 # datetime
 import datetime
-from typing import Any, Dict, Optional
+from typing import Any, Dict
 
 from django.http import HttpRequest, HttpResponse
 from typing_extensions import TypedDict
