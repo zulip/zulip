514801c509331ecb2679b5fa2cf51fde875358cb
Alex Vandiver <alexmv@zulip.com>
2021-11-17 09:40:24 -0800

    puppet: Split out golang toolchain into its own manifest.

--- puppet/zulip/manifests/profile/smokescreen.pp
+++ puppet/zulip/manifests/profile/smokescreen.pp
@@ -3,20 +3,7 @@
 class zulip::profile::smokescreen {
   include zulip::profile::base
   include zulip::supervisor
-
-  $golang_version = '1.16.4'
-  zulip::sha256_tarball_to { 'golang':
-    url     => "https://golang.org/dl/go${golang_version}.linux-amd64.tar.gz",
-    sha256  => '7154e88f5a8047aad4b80ebace58a059e36e7e2e4eb3b383127a28c711b4ff59',
-    install => {
-      'go/' => "/srv/golang-${golang_version}/",
-    },
-  }
-  file { '/srv/golang':
-    ensure  => 'link',
-    target  => "/srv/golang-${golang_version}/",
-    require => Zulip::Sha256_tarball_to['golang'],
-  }
+  include zulip::golang
 
   $version = 'bfca45c5e61f3587eaaf1dcc89a0c4116501cba3'
   zulip::sha256_tarball_to { 'smokescreen':
