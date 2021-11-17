21be36f15ff05faa288b5766540fe83cf53ae924
Alex Vandiver <alexmv@zulip.com>
2021-11-17 11:06:44 -0800

    puppet: Shorten golang version variable name.

--- puppet/zulip/manifests/golang.pp
+++ puppet/zulip/manifests/golang.pp
@@ -1,18 +1,18 @@
 # @summary go compiler and tools
 #
 class zulip::golang {
-  $golang_version = '1.17.3'
+  $version = '1.17.3'
   zulip::sha256_tarball_to { 'golang':
-    url     => "https://golang.org/dl/go${golang_version}.linux-amd64.tar.gz",
+    url     => "https://golang.org/dl/go${version}.linux-amd64.tar.gz",
     sha256  => '550f9845451c0c94be679faf116291e7807a8d78b43149f9506c1b15eb89008c',
     install => {
-      'go/' => "/srv/golang-${golang_version}/",
+      'go/' => "/srv/golang-${version}/",
     },
   }
 
   file { '/srv/golang':
     ensure  => 'link',
-    target  => "/srv/golang-${golang_version}/",
+    target  => "/srv/golang-${version}/",
     require => Zulip::Sha256_tarball_to['golang'],
   }
 }
