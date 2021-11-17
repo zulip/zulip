97c49f88895c2f0d11f289574b356d1537875be6
Alex Vandiver <alexmv@zulip.com>
2021-11-17 09:41:23 -0800

    puppet: Upgrade golang from 1.16.4 to 1.17.3.

--- puppet/zulip/manifests/golang.pp
+++ puppet/zulip/manifests/golang.pp
@@ -1,10 +1,10 @@
 # @summary go compiler and tools
 #
 class zulip::golang {
-  $golang_version = '1.16.4'
+  $golang_version = '1.17.3'
   zulip::sha256_tarball_to { 'golang':
     url     => "https://golang.org/dl/go${golang_version}.linux-amd64.tar.gz",
-    sha256  => '7154e88f5a8047aad4b80ebace58a059e36e7e2e4eb3b383127a28c711b4ff59',
+    sha256  => '550f9845451c0c94be679faf116291e7807a8d78b43149f9506c1b15eb89008c',
     install => {
       'go/' => "/srv/golang-${golang_version}/",
     },
