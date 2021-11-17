bee225782ad77564ec521fc4e5cae99333caffe7
Alex Vandiver <alexmv@zulip.com>
2021-11-17 09:55:25 -0800

    puppet: Switch smokescreen to using zulip::external_dep, so it tidies.
    (cherry picked from commit 3a7cef65825004daee9d57c52401e7cdeb5617ed)

--- puppet/zulip/manifests/profile/smokescreen.pp
+++ puppet/zulip/manifests/profile/smokescreen.pp
@@ -6,20 +6,22 @@ class zulip::profile::smokescreen {
   include zulip::golang
 
   $version = 'dc403015f563eadc556a61870c6ad327688abe88'
-  zulip::sha256_tarball_to { 'smokescreen':
-    url     => "https://github.com/stripe/smokescreen/archive/${version}.tar.gz",
-    sha256  => 'ad4b181d14adcd9425045152b903a343dbbcfcad3c1e7625d2c65d1d50e1959d',
-    install => {
-      "smokescreen-${version}/" => "/srv/zulip-smokescreen-src-${version}/",
-    },
+  $dir = "/srv/zulip-smokescreen-src-${version}/"
+
+  zulip::external_dep { 'smokescreen-src':
+    version        => $version,
+    url            => "https://github.com/stripe/smokescreen/archive/${version}.tar.gz",
+    sha256         => 'ad4b181d14adcd9425045152b903a343dbbcfcad3c1e7625d2c65d1d50e1959d',
+    tarball_prefix => "smokescreen-${version}/",
   }
+
   exec { 'compile smokescreen':
     command     => "${zulip::golang::bin} build -o /usr/local/bin/smokescreen-${version}",
-    cwd         => "/srv/zulip-smokescreen-src-${version}/",
+    cwd         => $dir,
     # GOCACHE is required; nothing is written to GOPATH, but it is required to be set
     environment => ['GOCACHE=/tmp/gocache', 'GOPATH=/root/go'],
     creates     => "/usr/local/bin/smokescreen-${version}",
-    require     => [File[$zulip::golang::bin], Zulip::Sha256_tarball_to['smokescreen']],
+    require     => [File[$zulip::golang::bin], File[$dir]],
   }
 
   file { '/usr/local/bin/smokescreen':
