93af6c7f0678bfbb80af4e2039ea2140b4db637d
Alex Vandiver <alexmv@zulip.com>
2021-11-17 11:21:39 -0800

    puppet: Factor out golang variables.

--- puppet/zulip/manifests/golang.pp
+++ puppet/zulip/manifests/golang.pp
@@ -2,17 +2,21 @@
 #
 class zulip::golang {
   $version = '1.17.3'
+
+  $dir = "/srv/golang-${version}/"
+  $bin = '/srv/golang/bin/go'
+
   zulip::sha256_tarball_to { 'golang':
     url     => "https://golang.org/dl/go${version}.linux-amd64.tar.gz",
     sha256  => '550f9845451c0c94be679faf116291e7807a8d78b43149f9506c1b15eb89008c',
     install => {
-      'go/' => "/srv/golang-${version}/",
+      'go/' => $dir,
     },
   }
 
   file { '/srv/golang':
     ensure  => 'link',
-    target  => "/srv/golang-${version}/",
+    target  => $dir,
     require => Zulip::Sha256_tarball_to['golang'],
   }
 }
