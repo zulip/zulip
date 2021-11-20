a7b2c7f7eae116d86904032faf0fa703d151c134
Aman Agrawal <amanagr@zulip.com>
2021-11-20 03:19:39 +0000

    recipient_row: Show globe icon for web public streams.

--- static/js/message_list_view.js
+++ static/js/message_list_view.js
@@ -148,6 +148,7 @@ function populate_group_from_message_container(group, message_container) {
         group.background_color = stream_data.get_color(message_container.msg.stream);
         group.color_class = color_class.get_css_class(group.background_color);
         group.invite_only = stream_data.get_invite_only(message_container.msg.stream);
+        group.is_web_public = stream_data.is_web_public(message_container.msg.stream_id);
         group.topic = message_container.msg.topic;
         group.match_topic = util.get_match_topic(message_container.msg);
         group.stream_url = message_container.stream_url;
