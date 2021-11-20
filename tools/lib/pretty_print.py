3cd667afe055ad0146e094a40a8ccc19f6f9c6fb
Steve Howell <showell@zulip.com>
2021-11-20 12:25:41 +0000

    linters: Rewrite check-templates.
    I rewrote most of tools/lib/pretty-printer.py, which
    was fairly easy due to being able to crib some
    important details from the previous implementation.
    
    The main motivation for the rewrite was that we weren't
    handling else/elif blocks correctly, and it was difficult
    to modify the previous code. The else/elif shortcomings
    were somewhat historical in nature--the original parser
    didn't recognize them (since they weren't in any Zulip
    templates at the time), and then the pretty printer was
    mostly able to hack around that due to the "nudge"
    strategy. Eventually the nudge strategy became too
    brittle.
    
    The "nudge" strategy was that we would mostly trust
    the existing templates, and we would just nudge over
    some lines in cases of obviously faulty indentation.
    
    Now we are bit more opinionated and rigorous, and
    we basically set the indentation explicitly for any
    line that is not in a code/script block. This leads
    to this diff touching several templates for mostly
    minor fix-ups.
    
    We aren't completely opinionated, as we respect the
    author's line wrapping decisions in many cases, and
    we also allow authors not to indent blocks within
    the template language's block constructs.

--- tools/lib/pretty_print.py
+++ tools/lib/pretty_print.py
@@ -1,223 +1,198 @@
 import subprocess
-from typing import Any, Dict, List
+from typing import List, Optional, Set
 
 from zulint.printer import ENDC, GREEN
 
-from .template_parser import is_django_block_tag, tokenize
+from .template_parser import Token, is_django_block_tag, tokenize
 
 
-def pretty_print_html(html: str, num_spaces: int = 4) -> str:
-    # We use 1-based indexing for both rows and columns.
+def requires_indent(line: str) -> bool:
+    line = line.lstrip()
+    return line.startswith("<")
+
+
+def open_token(token: Token) -> bool:
+    if token.kind in (
+        "handlebars_start",
+        "html_start",
+    ):
+        return True
+
+    if token.kind in (
+        "django_start",
+        "jinja2_whitespace_stripped_start",
+        "jinja2_whitespace_stripped_type2_start",
+    ):
+        return is_django_block_tag(token.tag)
+
+    return False
+
+
+def close_token(token: Token) -> bool:
+    return token.kind in (
+        "django_end",
+        "handlebars_end",
+        "html_end",
+        "jinja2_whitespace_stripped_end",
+    )
+
+
+def else_token(token: Token) -> bool:
+    return token.kind in (
+        "django_else",
+        "handlebars_else",
+    )
+
+
+def pop_unused_tokens(tokens: List[Token], row: int) -> bool:
+    while tokens and tokens[-1].line <= row:
+        token = tokens.pop()
+        if close_token(token):
+            return True
+    return False
+
+
+def indent_pref(row: int, tokens: List[Token], line: str) -> str:
+    opens = 0
+    closes = 0
+    is_else = False
+
+    while tokens and tokens[-1].line == row:
+        token = tokens.pop()
+        if open_token(token):
+            opens += 1
+        elif close_token(token):
+            closes += 1
+        elif else_token(token):
+            is_else = True
+
+    if is_else:
+        if opens and closes:
+            return "neutral"
+        return "else"
+
+    i = opens - closes
+    if i == 0:
+        return "neutral"
+    elif i == 1:
+        return "open"
+    elif i == -1:
+        return "close"
+    else:
+        print(i, opens, closes)
+        raise Exception(f"too many tokens on row {row}")
+
+
+def indent_level(s: str) -> int:
+    return len(s) - len(s.lstrip())
+
+
+def same_indent(s1: str, s2: str) -> bool:
+    return indent_level(s1) == indent_level(s2)
+
+
+def next_non_blank_line(lines: List[str], i: int) -> str:
+    next_line = ""
+    for j in range(i + 1, len(lines)):
+        next_line = lines[j]
+        if next_line.strip() != "":
+            break
+    return next_line
+
+
+def get_exempted_lines(tokens: List[Token]) -> Set[int]:
+    exempted = set()
+    for code_tag in ("code", "pre", "script"):
+        for token in tokens:
+            if token.kind == "html_start" and token.tag == code_tag:
+                start: Optional[int] = token.line
+
+            if token.kind == "html_end" and token.tag == code_tag:
+                # The pretty printer expects well-formed HTML, even
+                # if it's strangely formatted, so we expect start
+                # to be None.
+                assert start is not None
+
+                # We leave code blocks completely alone, including
+                # the start and end tags.
+                for i in range(start, token.line + 1):
+                    exempted.add(i)
+                    start = None
+    return exempted
+
+
+def pretty_print_html(html: str) -> str:
     tokens = tokenize(html)
+
+    exempted_lines = get_exempted_lines(tokens)
+
+    tokens.reverse()
     lines = html.split("\n")
 
-    # We will keep a stack of "start" tags so that we know
-    # when HTML ranges end.  Note that some start tags won't
-    # be blocks from an indentation standpoint.
-    stack: List[Dict[str, Any]] = []
-
-    # Seed our stack with a pseudo entry to make depth calculations
-    # easier.
-    info: Dict[str, Any] = dict(
-        block=False,
-        depth=-1,
-        line=-1,
-        token_kind="html_start",
-        tag="html",
-        extra_indent=0,
-        ignore_lines=[],
-    )
-    stack.append(info)
-
-    # Our main job is to figure out offsets that we use to nudge lines
-    # over by.
-    offsets: Dict[int, int] = {}
-
-    # Loop through our start/end tokens, and calculate offsets.  As
-    # we proceed, we will push/pop info dictionaries on/off a stack.
-    for token in tokens:
-
-        if (
-            token.kind
-            in (
-                "html_start",
-                "handlebars_start",
-                "handlebars_singleton",
-                "html_singleton",
-                "django_start",
-                "jinja2_whitespace_stripped_type2_start",
-                "jinja2_whitespace_stripped_start",
-            )
-            and stack[-1]["tag"] != "pre"
-        ):
-            # An HTML start tag should only cause a new indent if we
-            # are on a new line.
-            if token.tag not in ("extends", "include", "else", "elif") and (
-                is_django_block_tag(token.tag) or token.kind != "django_start"
-            ):
-                is_block = token.line > stack[-1]["line"]
-
-                if is_block:
-                    if (
-                        (
-                            token.kind == "handlebars_start"
-                            and stack[-1]["token_kind"] == "handlebars_start"
-                        )
-                        or (
-                            token.kind
-                            in {
-                                "django_start",
-                                "jinja2_whitespace_stripped_type2_start",
-                                "jinja2_whitespace_stripped_start",
-                            }
-                            and stack[-1]["token_kind"]
-                            in {
-                                "django_start",
-                                "jinja2_whitespace_stripped_type2_start",
-                                "jinja2_whitespace_stripped_start",
-                            }
-                        )
-                    ) and not stack[-1]["indenting"]:
-                        info = stack.pop()
-                        info["depth"] = info["depth"] + 1
-                        info["indenting"] = True
-                        info["adjust_offset_until"] = token.line
-                        stack.append(info)
-                    new_depth = stack[-1]["depth"] + 1
-                    extra_indent = stack[-1]["extra_indent"]
-                    line = lines[token.line - 1]
-                    adjustment = len(line) - len(line.lstrip()) + 1
-                    offset = (1 + extra_indent + new_depth * num_spaces) - adjustment
-                    info = dict(
-                        block=True,
-                        depth=new_depth,
-                        actual_depth=new_depth,
-                        line=token.line,
-                        tag=token.tag,
-                        token_kind=token.kind,
-                        line_span=token.line_span,
-                        offset=offset,
-                        extra_indent=token.col - adjustment + extra_indent,
-                        extra_indent_prev=extra_indent,
-                        adjustment=adjustment,
-                        indenting=True,
-                        adjust_offset_until=token.line,
-                        ignore_lines=[],
-                    )
-                    if token.kind in ("handlebars_start", "django_start"):
-                        info.update(dict(depth=new_depth - 1, indenting=False))
-                else:
-                    info = dict(
-                        block=False,
-                        depth=stack[-1]["depth"],
-                        actual_depth=stack[-1]["depth"],
-                        line=token.line,
-                        tag=token.tag,
-                        token_kind=token.kind,
-                        extra_indent=stack[-1]["extra_indent"],
-                        ignore_lines=[],
-                    )
-                stack.append(info)
-        elif (
-            token.kind
-            in (
-                "html_end",
-                "handlebars_end",
-                "html_singleton_end",
-                "django_end",
-                "handlebars_singleton_end",
-                "jinja2_whitespace_stripped_end",
-            )
-            and (stack[-1]["tag"] != "pre" or token.tag == "pre")
-        ):
-            info = stack.pop()
-            if info["block"]:
-                # We are at the end of an indentation block.  We
-                # assume the whole block was formatted ok before, just
-                # possibly at an indentation that we don't like, so we
-                # nudge over all lines in the block by the same offset.
-                start_line = info["line"]
-                end_line = token.line
-                if token.tag == "pre":
-                    offsets[start_line] = 0
-                    offsets[end_line] = 0
-                    stack[-1]["ignore_lines"].append(start_line)
-                    stack[-1]["ignore_lines"].append(end_line)
-                else:
-                    offsets[start_line] = info["offset"]
-                    line = lines[token.line - 1]
-                    adjustment = len(line) - len(line.lstrip()) + 1
-                    if adjustment == token.col and token.kind != "html_singleton_end":
-                        offsets[end_line] = (
-                            info["offset"]
-                            + info["adjustment"]
-                            - adjustment
-                            + info["extra_indent"]
-                            - info["extra_indent_prev"]
-                        )
-                    elif start_line + info["line_span"] - 1 == end_line and info["line_span"] > 1:
-                        offsets[end_line] = (
-                            1 + info["extra_indent"] + (info["depth"] + 1) * num_spaces
-                        ) - adjustment
-                        # We would like singleton tags and tags which spread over
-                        # multiple lines to have 2 space indentation.
-                        offsets[end_line] -= 2
-                    elif token.line != info["line"]:
-                        offsets[end_line] = info["offset"]
-                if token.tag != "pre" and token.tag != "script":
-                    for line_num in range(start_line + 1, end_line):
-                        # Be careful not to override offsets that happened
-                        # deeper in the HTML within our block.
-                        if line_num not in offsets:
-                            line = lines[line_num - 1]
-                            new_depth = info["depth"] + 1
-                            if (
-                                line.lstrip().startswith("{{else}}")
-                                or line.lstrip().startswith("{% else %}")
-                                or line.lstrip().startswith("{% elif")
-                            ):
-                                new_depth = info["actual_depth"]
-                            extra_indent = info["extra_indent"]
-                            adjustment = len(line) - len(line.lstrip()) + 1
-                            offset = (1 + extra_indent + new_depth * num_spaces) - adjustment
-                            if line_num <= start_line + info["line_span"] - 1:
-                                # We would like singleton tags and tags which spread over
-                                # multiple lines to have 2 space indentation.
-                                offset -= 2
-                            offsets[line_num] = offset
-                        elif (
-                            token.kind in ("handlebars_end", "django_end")
-                            and info["indenting"]
-                            and line_num < info["adjust_offset_until"]
-                            and line_num not in info["ignore_lines"]
-                        ):
-                            offsets[line_num] += num_spaces
-                elif token.tag != "pre":
-                    for line_num in range(start_line + 1, end_line):
-                        if line_num not in offsets:
-                            offsets[line_num] = info["offset"]
-                else:
-                    for line_num in range(start_line + 1, end_line):
-                        if line_num not in offsets:
-                            offsets[line_num] = 0
-                            stack[-1]["ignore_lines"].append(line_num)
-
-    # Now that we have all of our offsets calculated, we can just
-    # join all our lines together, fixing up offsets as needed.
+    open_offsets: List[str] = []
     formatted_lines = []
-    for i, line in enumerate(html.split("\n")):
-        row = i + 1
-        offset = offsets.get(row, 0)
-        pretty_line = line
+    next_offset: str = ""
+    tag_end_row: Optional[int] = None
+    tag_continuation_offset = ""
+
+    def line_offset(row: int, line: str, next_line: str) -> Optional[str]:
+        nonlocal next_offset
+        nonlocal tag_end_row
+        nonlocal tag_continuation_offset
+
+        if tag_end_row and row < tag_end_row:
+            was_closed = pop_unused_tokens(tokens, row)
+            if was_closed:
+                next_offset = open_offsets.pop()
+            return tag_continuation_offset
+
+        offset = next_offset
+        if tokens:
+            token = tokens[-1]
+            if token.line == row and token.line_span > 1:
+                if token.kind in ("django_comment", "handlebar_comment", "html_comment"):
+                    tag_continuation_offset = offset
+                else:
+                    tag_continuation_offset = offset + "  "
+                tag_end_row = row + token.line_span
+
+        pref = indent_pref(row, tokens, line)
+        if pref == "open":
+            if same_indent(line, next_line) and not requires_indent(line):
+                next_offset = offset
+            else:
+                next_offset = offset + " " * 4
+            open_offsets.append(offset)
+        elif pref == "else":
+            offset = open_offsets[-1]
+            if same_indent(line, next_line):
+                next_offset = offset
+            else:
+                next_offset = offset + " " * 4
+        elif pref == "close":
+            offset = open_offsets.pop()
+            next_offset = offset
+        return offset
+
+    def adjusted_line(row: int, line: str, next_line: str) -> str:
         if line.strip() == "":
-            pretty_line = ""
-        else:
-            if offset > 0:
-                pretty_line = (" " * offset) + pretty_line
-            elif offset < 0:
-                pretty_line = pretty_line[-1 * offset :]
-                assert line.strip() == pretty_line.strip()
-        formatted_lines.append(pretty_line)
+            return ""
+
+        offset = line_offset(row, line, next_line)
+
+        if row in exempted_lines:
+            return line.rstrip()
+
+        if offset is None:
+            return line.rstrip()
+
+        return offset + line.strip()
+
+    for i, line in enumerate(lines):
+        # We use 1-based indexing for both rows and columns.
+        next_line = next_non_blank_line(lines, i)
+        row = i + 1
+        formatted_lines.append(adjusted_line(row, line, next_line))
 
     return "\n".join(formatted_lines)
 
