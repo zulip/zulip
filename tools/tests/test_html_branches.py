42061359a9442e27df978ffe7e0d3719f49aac54
Steve Howell <showell@zulip.com>
2021-12-02 13:30:52 +0000

    dead code: Kill off most of html_branches.
    The code that was removed here was intended
    for tooling that either never got built or
    became obsolete.

--- tools/tests/test_html_branches.py
+++ tools/tests/test_html_branches.py
@@ -2,14 +2,7 @@ import os
 import unittest
 
 import tools.lib.template_parser
-from tools.lib.html_branches import (
-    Node,
-    build_id_dict,
-    get_tag_info,
-    html_branches,
-    html_tag_tree,
-    split_for_id_and_class,
-)
+from tools.lib.html_branches import build_id_dict, get_tag_info, split_for_id_and_class
 
 ZULIP_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 TEST_TEMPLATES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_template_data")
@@ -27,93 +20,6 @@ class TestHtmlBranches(unittest.TestCase):
         self.assertEqual(start_tag_info.text(), "p.test1.test2#test")
         self.assertEqual(end_tag_info.text(), "p")
         self.assertEqual(text.s, "foo")
-
-    def test_html_tag_tree(self) -> None:
-        html = """
-            <!-- test -->
-            <!DOCTYPE html>
-            <html>
-            <!-- test -->
-            <head>
-                <title>Test</title>
-                <meta charset="utf-8" />
-                <link rel="stylesheet" href="style.css" />
-            </head>
-            <body>
-                <p>Hello<br>world!</p>
-                <p>Goodbye<!-- test -->world!</p>
-            </body>
-            </html>
-            <!-- test -->
-        """
-
-        tree = html_tag_tree(html)
-
-        def serialize(node: Node) -> object:
-            return (
-                node.token and (node.token.kind, node.token.tag),
-                [serialize(child) for child in node.children],
-            )
-
-        expected = (
-            None,
-            [
-                (
-                    ("html_start", "html"),
-                    [
-                        (
-                            ("html_start", "head"),
-                            [
-                                (("html_start", "title"), []),
-                                (("html_singleton", "meta"), []),
-                                (("html_singleton", "link"), []),
-                            ],
-                        ),
-                        (
-                            ("html_start", "body"),
-                            [
-                                (
-                                    ("html_start", "p"),
-                                    [(("html_start", "br"), []), (("html_start", "p"), [])],
-                                )
-                            ],
-                        ),
-                    ],
-                )
-            ],
-        )
-        self.assertEqual(serialize(tree), expected)
-
-    def test_html_branches(self) -> None:
-        html = """
-            <!-- test -->
-            <!DOCTYPE html>
-            <html>
-            <!-- test -->
-            <head>
-                <title>Test</title>
-                <meta charset="utf-8" />
-                <link rel="stylesheet" href="style.css" />
-            </head>
-            <body>
-                <p>Hello<br>world!</p>
-                <p>Goodbye<!-- test -->world!</p>
-            </body>
-            </html>
-            <!-- test -->
-        """
-
-        branches = html_branches(html)
-        self.assertEqual(
-            [(branch.text(), branch.staircase_text()) for branch in branches],
-            [
-                ("html head title", "\n    html\n        head\n            title\n"),
-                ("html head meta", "\n    html\n        head\n            meta\n"),
-                ("html head link", "\n    html\n        head\n            link\n"),
-                ("html body p br", "\n    html\n        body\n            p\n                br\n"),
-                ("html body p p", "\n    html\n        body\n            p\n                p\n"),
-            ],
-        )
 
     def test_build_id_dict(self) -> None:
         templates = ["test_template1.html", "test_template2.html"]
