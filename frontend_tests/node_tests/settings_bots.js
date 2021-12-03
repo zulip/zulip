df910391147ef04d5607320ccdb2c7fa2b498d07
Swati Bhageria <swatib@umich.edu>
2021-12-02 22:03:26 -0500

    Add copy buttons to bot settings

--- frontend_tests/node_tests/settings_bots.js
+++ frontend_tests/node_tests/settings_bots.js
@@ -21,9 +21,7 @@ const bot_data_params = {
 
 const avatar = mock_esm("../../static/js/avatar");
 
-function ClipboardJS(sel) {
-    assert.equal(sel, "#copy_zuliprc");
-}
+function ClipboardJS() {}
 mock_cjs("clipboard", ClipboardJS);
 
 const bot_data = zrequire("bot_data");
