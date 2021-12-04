343cf41aa348d1977cc94f655ffafd0825808a38
Aman Agrawal <amanagr@zulip.com>
2021-12-04 05:00:37 +0000

    compose_ui: Use white spinner on both dark and light theme.
    Fixed alignment slightly.

--- static/js/compose_ui.js
+++ static/js/compose_ui.js
@@ -8,7 +8,6 @@ import * as loading from "./loading";
 import * as people from "./people";
 import * as popover_menus from "./popover_menus";
 import * as rtl from "./rtl";
-import * as settings_data from "./settings_data";
 import * as user_status from "./user_status";
 
 let full_size_status = false; // true or false
@@ -397,8 +396,8 @@ export function hide_compose_spinner() {
 }
 
 export function show_compose_spinner() {
-    const using_dark_theme = settings_data.using_dark_theme();
-    loading.show_button_spinner($("#compose-send-button .loader"), using_dark_theme);
+    // Always use white spinner.
+    loading.show_button_spinner($("#compose-send-button .loader"), true);
     $("#compose-send-button span").hide();
     $("#compose-send-button").addClass("disable-btn");
 }
