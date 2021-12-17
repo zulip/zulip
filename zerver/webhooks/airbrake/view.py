ac9a5ba894dc90c7ff9b851e69bd4a990a4a425b
Anders Kaseorg <anders@zulip.com>
2021-12-16 22:03:22 -0800

    airbrake: Strengthen types using WildValue.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- zerver/webhooks/airbrake/view.py
+++ zerver/webhooks/airbrake/view.py
@@ -1,11 +1,10 @@
 # Webhooks for external integrations.
-from typing import Any, Dict
-
 from django.http import HttpRequest, HttpResponse
 
 from zerver.decorator import webhook_view
 from zerver.lib.request import REQ, has_request_variables
 from zerver.lib.response import json_success
+from zerver.lib.validator import WildValue, check_string, to_wild_value
 from zerver.lib.webhooks.common import check_send_webhook_message
 from zerver.models import UserProfile
 
@@ -18,7 +17,7 @@ AIRBRAKE_MESSAGE_TEMPLATE = '[{error_class}]({error_url}): "{error_message}" occ
 def api_airbrake_webhook(
     request: HttpRequest,
     user_profile: UserProfile,
-    payload: Dict[str, Any] = REQ(argument_type="body"),
+    payload: WildValue = REQ(argument_type="body", converter=to_wild_value),
 ) -> HttpResponse:
     subject = get_subject(payload)
     body = get_body(payload)
@@ -26,14 +25,16 @@ def api_airbrake_webhook(
     return json_success(request)
 
 
-def get_subject(payload: Dict[str, Any]) -> str:
-    return AIRBRAKE_TOPIC_TEMPLATE.format(project_name=payload["error"]["project"]["name"])
+def get_subject(payload: WildValue) -> str:
+    return AIRBRAKE_TOPIC_TEMPLATE.format(
+        project_name=payload["error"]["project"]["name"].tame(check_string)
+    )
 
 
-def get_body(payload: Dict[str, Any]) -> str:
+def get_body(payload: WildValue) -> str:
     data = {
-        "error_url": payload["airbrake_error_url"],
-        "error_class": payload["error"]["error_class"],
-        "error_message": payload["error"]["error_message"],
+        "error_url": payload["airbrake_error_url"].tame(check_string),
+        "error_class": payload["error"]["error_class"].tame(check_string),
+        "error_message": payload["error"]["error_message"].tame(check_string),
     }
     return AIRBRAKE_MESSAGE_TEMPLATE.format(**data)
