b9d4811104e5ee2dd08e07cd95cbee4da07a1beb
moki1202 <shashanksingh819@gmail.com>
2021-12-21 18:38:52 +0530

    changes

--- zerver/views/realm_export.py
+++ zerver/views/realm_export.py
@@ -89,8 +89,8 @@ def delete_realm_export(request: HttpRequest, user: UserProfile, export_id: int)
         )
     except RealmAuditLog.DoesNotExist:
         raise JsonableError(_("Invalid data export ID"))
-    
-    export_data = orjson.decode(audit_log_entry.extra_data)
+
+    export_data = orjson.loads(audit_log_entry.extra_data)
     if audit_log_entry.extra_data is None:
         raise JsonableError(_("Export cannot be deleted until complete"))
     do_delete_realm_export(user, audit_log_entry)
