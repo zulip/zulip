696092f19a82909c4a02cf451d24c95a6e9981a1
Anders Kaseorg <anders@zulip.com>
2021-12-22 22:00:12 -0800

    test_timezone: Fix ambiguous_abbrevs type.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- zerver/tests/test_timezone.py
+++ zerver/tests/test_timezone.py
@@ -15,16 +15,16 @@ class TimeZoneTest(ZulipTestCase):
 
     def test_common_timezones(self) -> None:
         ambiguous_abbrevs = [
-            ("CDT", -18000),  # Central Daylight Time
-            ("CDT", -14400),  # Cuba Daylight Time
-            ("CST", -21600),  # Central Standard Time
-            ("CST", +28800),  # China Standard Time
-            ("CST", -18000),  # Cuba Standard Time
-            ("PST", -28800),  # Pacific Standard Time
-            ("PST", +28800),  # Philipine Standard Time
-            ("IST", +19800),  # India Standard Time
-            ("IST", +7200),  # Israel Standard Time
-            ("IST", +3600),  # Ireland Standard Time
+            ("CDT", -18000.0),  # Central Daylight Time
+            ("CDT", -14400.0),  # Cuba Daylight Time
+            ("CST", -21600.0),  # Central Standard Time
+            ("CST", +28800.0),  # China Standard Time
+            ("CST", -18000.0),  # Cuba Standard Time
+            ("PST", -28800.0),  # Pacific Standard Time
+            ("PST", +28800.0),  # Philipine Standard Time
+            ("IST", +19800.0),  # India Standard Time
+            ("IST", +7200.0),  # Israel Standard Time
+            ("IST", +3600.0),  # Ireland Standard Time
         ]
         missing = set(dict(reversed(ambiguous_abbrevs)).items()) - set(common_timezones.items())
         assert not missing, missing
