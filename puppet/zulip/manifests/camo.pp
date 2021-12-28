3c163a7d5e4a3f0382c71ef1a9b313f1fb4fa495
Alex Vandiver <alexmv@zulip.com>
2021-12-28 01:08:41 +0000

    puppet: Move slash out of $dir by convention.

--- puppet/zulip/manifests/camo.pp
+++ puppet/zulip/manifests/camo.pp
@@ -7,14 +7,14 @@ class zulip::camo (String $listen_address = '0.0.0.0') {
   }
 
   $version = '2.3.0'
-  $dir = "/srv/zulip-go-camo-${version}/"
-  $bin = "${dir}bin/go-camo"
+  $dir = "/srv/zulip-go-camo-${version}"
+  $bin = "${dir}/bin/go-camo"
 
   zulip::external_dep { 'go-camo':
     version        => $version,
     url            => "https://github.com/cactus/go-camo/releases/download/v${version}/go-camo-${version}.go1171.linux-${::architecture}.tar.gz",
     sha256         => '965506e6edb9d974c810519d71e847afb7ca69d1d01ae7d8be6d7a91de669c0c',
-    tarball_prefix => "go-camo-${version}/",
+    tarball_prefix => "go-camo-${version}",
     bin            => 'bin/go-camo',
   }
 
--- puppet/zulip_ops/manifests/prometheus/node.pp
+++ puppet/zulip_ops/manifests/prometheus/node.pp
@@ -5,14 +5,14 @@ class zulip_ops::prometheus::node {
   include zulip::supervisor
 
   $version = '1.1.2'
-  $dir = "/srv/zulip-node_exporter-${version}/"
-  $bin = "${dir}node_exporter"
+  $dir = "/srv/zulip-node_exporter-${version}"
+  $bin = "${dir}/node_exporter"
 
   zulip::external_dep { 'node_exporter':
     version        => $version,
     url            => "https://github.com/prometheus/node_exporter/releases/download/v${version}/node_exporter-${version}.linux-${::architecture}.tar.gz",
     sha256         => '8c1f6a317457a658e0ae68ad710f6b4098db2cad10204649b51e3c043aa3e70d',
-    tarball_prefix => "node_exporter-${version}.linux-${::architecture}/",
+    tarball_prefix => "node_exporter-${version}.linux-${::architecture}",
     bin            => 'node_exporter',
   }
 
