3fb1c37f665ad69ced5ceae13c93690cb0f73ffd
Sahil Batra <sahil@zulip.com>
2022-01-03 16:54:39 +0530

    actions: Use transaction.atomic for do_change_avatar_fields.

--- zerver/lib/actions.py
+++ zerver/lib/actions.py
@@ -4676,6 +4676,7 @@ def notify_avatar_url_change(user_profile: UserProfile) -> None:
     )
 
 
+@transaction.atomic(savepoint=False)
 def do_change_avatar_fields(
     user_profile: UserProfile,
     avatar_source: str,
