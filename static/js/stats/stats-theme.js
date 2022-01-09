21df449514d808b5c815ac1cdda0ae6c7e4bbe6a
priyansh <priyansh.gupta@students.iiit.ac.in>
2022-01-10 03:39:45 +0530

    changes made

--- static/js/stats/stats-theme.js
+++ static/js/stats/stats-theme.js
@@ -1,19 +1,18 @@
-//function to set theme for stats page
+//  function to set theme for stats page
 function setTheme(theme){
-    if(theme == "dark-theme"){
-        // document.body.removeClass("color-scheme-automatic").addClass("dark-theme");
+    'use strict';
+    if(theme === "dark-theme"){        
         document.querySelector('body').classList.remove('color-scheme-automatic');
         document.querySelector('body').classList.add('dark-theme');
 
     }
     else{
-        // document.body.removeClass("dark-theme").addClass("color-scheme-automatic");
         document.querySelector('body').classList.remove('dark-theme');
         document.querySelector('body').classList.add('color-scheme-automatic');
     }
 }
 
 
-//Reading value of theme
-let theme = localStorage.getItem('theme-value');
+//  Reading value of theme
+const theme = localStorage.getItem('theme-value');
 setTheme(theme);
