c9804a61531ff9d63eca8118742cc07dcec5328f
Lauryn Menard <lauryn.menard@gmail.com>
2022-01-13 17:52:46 +0100

    api_docs: Update regex for generating code example fixture.
    Updates regex in the openapi markdown extension to match api
    endpoint names that contain dashes, which is the case for
    `zulip-outgoing-webhook` and `rest-error-handling`.

--- zerver/openapi/markdown_extension.py
+++ zerver/openapi/markdown_extension.py
@@ -31,7 +31,7 @@ from zerver.openapi.openapi import (
     openapi_spec,
 )
 
-API_ENDPOINT_NAME = r"/[a-z_/-{}]+:[a-z]+"
+API_ENDPOINT_NAME = r"/[a-z_\-/-{}]+:[a-z]+"
 API_LANGUAGE = r"\w+"
 API_KEY_TYPE = r"fixture|example"
 MACRO_REGEXP = re.compile(
