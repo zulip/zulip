149ede04d448d0e4a14ebce2dab3ec9c4f7c503e
Anders Kaseorg <anders@zulip.com>
2022-01-21 21:50:24 -0800

    mypy: Add types-stripe.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -48,4 +48,4 @@ API_FEATURE_LEVEL = 113
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = "170.0"
+PROVISION_VERSION = "171.0"
