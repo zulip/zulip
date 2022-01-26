4b0322f70756841a2fe9a34865fc368a8f8e8004
Advait Jain <89680252+Zoronium@users.noreply.github.com>
2022-01-26 20:28:37 +0530

    Update stream_ui_updates.js

--- static/js/stream_ui_updates.js
+++ static/js/stream_ui_updates.js
@@ -76,7 +76,6 @@ export function update_toggler_for_sub_private_settings(sub){
     if (!hash_util.is_editing_stream(sub.stream_id)) {
         return;
     }
-    
     if (sub.invite_only && sub.history_public_to_subscribers){
         stream_edit.toggler.disable_tab("subscriber_settings");
     }
