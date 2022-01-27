b55081abe33859298939d26d64111049a2eb7d5d
priyansh <priyansh.gupta@students.iiit.ac.in>
2022-01-27 14:27:30 +0530

    Theme-setting: Created a variable for storing selected theme value in local storage

--- static/js/dark_theme.ts
+++ static/js/dark_theme.ts
@@ -1,13 +1,28 @@
 import $ from "jquery";
 
 export function enable(): void {
+    if (typeof Storage !== "undefined") {
+        localStorage.setItem("theme-value", "dark-theme");
+    } else {
+        // Sorry! No Web Storage support..
+    }
     $("body").removeClass("color-scheme-automatic").addClass("dark-theme");
 }
 
 export function disable(): void {
+    if (typeof Storage !== "undefined") {
+        localStorage.setItem("theme-value", "light-theme");
+    } else {
+        // Sorry! No Web Storage support..
+    }
     $("body").removeClass("color-scheme-automatic").removeClass("dark-theme");
 }
 
 export function default_preference_checker(): void {
+    if (typeof Storage !== "undefined") {
+        localStorage.setItem("theme-value", "light-theme");
+    } else {
+        // Sorry! No Web Storage support..
+    }
     $("body").removeClass("dark-theme").addClass("color-scheme-automatic");
 }
