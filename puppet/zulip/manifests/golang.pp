0ca1b59f74434e24cdb2e58db96123b0ab5e04ee
Alex Vandiver <alexmv@zulip.com>
2022-02-11 11:38:46 -0800

    puppet: Use goarch for golang.
    Fixes: #21051.

--- puppet/zulip/manifests/golang.pp
+++ puppet/zulip/manifests/golang.pp
@@ -2,13 +2,12 @@
 #
 class zulip::golang {
   $version = $zulip::common::versions['golang']['version']
-
   $dir = "/srv/zulip-golang-${version}"
   $bin = "${dir}/bin/go"
 
   zulip::external_dep { 'golang':
     version        => $version,
-    url            => "https://golang.org/dl/go${version}.linux-${::architecture}.tar.gz",
+    url            => "https://golang.org/dl/go${version}.linux-${zulip::common::goarch}.tar.gz",
     tarball_prefix => 'go',
   }
 }
