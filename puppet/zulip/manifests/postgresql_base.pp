bb4c0799ccf270fd5b30e62136564ce440f4b91c
Alex Vandiver <alexmv@zulip.com>
2022-02-15 10:38:20 -0800

    puppet: Switch to the canonical case for $::os['family'].
    The == operator in Puppet is case-insensitive for ASCII
    characters[1], which is potentially surprising.  Switch to the
    canonical case that `$::os['family']` returns.
    
    [1] https://puppet.com/docs/puppet/5.5/lang_expressions.html#string-encoding-and-comparisons

--- puppet/zulip/manifests/postgresql_base.pp
+++ puppet/zulip/manifests/postgresql_base.pp
@@ -4,7 +4,7 @@ class zulip::postgresql_base {
   include zulip::process_fts_updates
 
   case $::os['family'] {
-    'debian': {
+    'Debian': {
       $postgresql = "postgresql-${zulip::postgresql_common::version}"
       $postgresql_sharedir = "/usr/share/postgresql/${zulip::postgresql_common::version}"
       $postgresql_confdirs = [
@@ -20,7 +20,7 @@ class zulip::postgresql_base {
       $postgresql_dict_dict = '/var/cache/postgresql/dicts/en_us.dict'
       $postgresql_dict_affix = '/var/cache/postgresql/dicts/en_us.affix'
     }
-    'redhat': {
+    'RedHat': {
       $postgresql = "postgresql${zulip::postgresql_common::version}"
       $postgresql_sharedir = "/usr/pgsql-${zulip::postgresql_common::version}/share"
       $postgresql_confdirs = [
