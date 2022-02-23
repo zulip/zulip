b1b5ee67c73ab74deeba46bb236c61e7b8e65e2c
Anders Kaseorg <anders@zulip.com>
2022-02-22 22:45:57 -0800

    models: Manage index from migration 0083 with Django.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- zerver/models.py
+++ zerver/models.py
@@ -3131,6 +3131,12 @@ class UserMessage(AbstractUserMessage):
             models.Index(
                 "user_profile",
                 "message",
+                condition=Q(flags__andnz=AbstractUserMessage.flags.mentioned.mask),
+                name="zerver_usermessage_mentioned_message_id",
+            ),
+            models.Index(
+                "user_profile",
+                "message",
                 condition=Q(flags__andz=AbstractUserMessage.flags.read.mask),
                 name="zerver_usermessage_unread_message_id",
             ),
