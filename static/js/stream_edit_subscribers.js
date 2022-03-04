87aa318e3a52e87ca45dd6d1f4fa9d08e8b8ba28
Steve Howell <showell@zulip.com>
2022-03-04 10:49:47 +0000

    edit streams: Change headings and move filter box.
    The new layout is:
    
        Add Subscribers
    
            [input pill] [Add]
    
        Subscribers             [filter]
        <list of subscribers>

--- static/js/stream_edit_subscribers.js
+++ static/js/stream_edit_subscribers.js
@@ -265,7 +265,7 @@ function remove_subscriber({stream_id, target_user_id, list_entry}) {
 }
 
 export function update_subscribers_list(sub) {
-    // This is for the "Stream membership" section of the right panel.
+    // This is for the "Subscribers" tab of the right panel.
     // Render subscriptions only if stream settings is open
     if (!hash_util.is_editing_stream(sub.stream_id)) {
         blueslip.info("ignoring subscription for stream that is no longer being edited");
