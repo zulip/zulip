ff43449231703fbef35fd4abb05192bcef862196
Steve Howell <showell@zulip.com>
2022-03-18 15:18:44 +0000

    puppeteer tests: Split stream tests to two files.
    This is just moving code around.

--- frontend_tests/puppeteer_tests/stream_create.ts
+++ frontend_tests/puppeteer_tests/stream_create.ts
@@ -1,6 +1,6 @@
 import {strict as assert} from "assert";
 
-import type {ElementHandle, Page} from "puppeteer";
+import type {Page} from "puppeteer";
 
 import common from "../puppeteer_lib/common";
 
@@ -32,59 +32,6 @@ async function add_user_to_stream(page: Page, name: string): Promise<void> {
 async function stream_name_error(page: Page): Promise<string> {
     await page.waitForSelector("#stream_name_error", {visible: true});
     return await common.get_text_from_selector(page, "#stream_name_error");
-}
-
-async function open_streams_modal(page: Page): Promise<void> {
-    const all_streams_selector = "#add-stream-link";
-    await page.waitForSelector(all_streams_selector, {visible: true});
-    await page.click(all_streams_selector);
-
-    await page.waitForSelector("#subscription_overlay.new-style", {visible: true});
-    const url = await common.page_url_with_fragment(page);
-    assert.ok(url.includes("#streams/all"));
-}
-
-async function test_subscription_button(page: Page): Promise<void> {
-    const stream_selector = "[data-stream-name='Venice']";
-    const button_selector = `${stream_selector} .sub_unsub_button`;
-    const subscribed_selector = `${button_selector}.checked`;
-    const unsubscribed_selector = `${button_selector}:not(.checked)`;
-
-    async function subscribed(): Promise<ElementHandle | null> {
-        return await page.waitForSelector(subscribed_selector, {visible: true});
-    }
-
-    async function unsubscribed(): Promise<ElementHandle | null> {
-        return await page.waitForSelector(unsubscribed_selector, {visible: true});
-    }
-
-    // Make sure that Venice is even in our list of streams.
-    await page.waitForSelector(stream_selector, {visible: true});
-    await page.waitForSelector(button_selector, {visible: true});
-
-    // Note that we intentionally re-find the button after each click, since
-    // the live-update code may replace the whole row.
-    let button;
-
-    // We assume Venice is already subscribed, so the first line here
-    // should happen immediately.
-    button = await subscribed();
-
-    // Toggle subscriptions several times. This test code has been known
-    // to flake, so we add console statements. It appears that the console
-    // statements help prevent the flake, which is probably caused by some
-    // subtle race condition. We will hopefully diagnose the root cause of
-    // the flake, but I am confident that the test will mostly catch actual
-    // bugs in the real code, so as long as the console.info statements help
-    // here, we should just leave them in.
-    for (let i = 0; i < 10; i += 1) {
-        console.info(`\n unsubscribe/subscribe loop ${i}\n\n`);
-        await button!.click();
-        button = await unsubscribed();
-        await button!.click();
-        button = await subscribed();
-        console.info(`\n end loop ${i}\n\n`);
-    }
 }
 
 async function click_create_new_stream(page: Page): Promise<void> {
@@ -216,8 +163,7 @@ async function test_streams_search_feature(page: Page): Promise<void> {
 
 async function subscriptions_tests(page: Page): Promise<void> {
     await common.log_in(page);
-    await open_streams_modal(page);
-    await test_subscription_button(page);
+    await common.open_streams_modal(page);
     await test_stream_creation(page);
     await test_streams_search_feature(page);
 }
