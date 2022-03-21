90deaea7aeb81d74d43db148991fd7770003acf9
Anders Kaseorg <anders@zulip.com>
2022-03-21 15:17:21 -0700

    requirements: Upgrade asgiref.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>
    (cherry picked from commit 52b9c598757925c7b15a0f95889dbb98e71b7f45)

--- version.py
+++ version.py
@@ -48,4 +48,4 @@ API_FEATURE_LEVEL = 122
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = "185.0"
+PROVISION_VERSION = "185.1"
