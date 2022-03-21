1786fdc770c24665fb94289be476f2183d4a6bdb
Tim Abbott <tabbott@zulip.com>
2022-03-21 15:58:03 -0700

    recent topics: Revert time format changes for now.
    As detailed in this conversation:
    
    https://chat.zulip.org/#narrow/stream/137-feedback/topic/recent.20topics.20timestamps/near/1337670
    
    This time format change is not working out as an improvement for at
    least some users, myself included.
    
    I think we do want to use some of the refinements attempted here (and
    in particular, I'm keeping the new function with its nice test suite),
    but I think it's better to revert now and fix forward in a future
    release.
    
    See #19775 for added background.

--- static/js/recent_topics_ui.js
+++ static/js/recent_topics_ui.js
@@ -253,7 +253,7 @@ function format_topic(topic_data) {
     }
     const topic = last_msg.topic;
     const time = new Date(last_msg.timestamp * 1000);
-    const last_msg_time = timerender.format_time_modern(time);
+    const last_msg_time = timerender.last_seen_status_from_date(time);
     const full_datetime = timerender.get_full_datetime(time);
 
     // We hide the row according to filters or if it's muted.
