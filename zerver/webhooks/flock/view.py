a3a95998c71b068a223d99e6b14ee599f09e20f8
Christopher Chong <t.kleinchong@gmail.com>
2022-03-30 23:55:53 +0800

    flock: Strengthen types using WildValue.

--- zerver/webhooks/flock/view.py
+++ zerver/webhooks/flock/view.py
@@ -1,11 +1,10 @@
 # Webhooks for external integrations.
-from typing import Any, Dict
-
 from django.http import HttpRequest, HttpResponse
 
 from zerver.decorator import webhook_view
 from zerver.lib.request import REQ, has_request_variables
 from zerver.lib.response import json_success
+from zerver.lib.validator import WildValue, check_string, to_wild_value
 from zerver.lib.webhooks.common import check_send_webhook_message
 from zerver.models import UserProfile
 
@@ -15,13 +14,13 @@ from zerver.models import UserProfile
 def api_flock_webhook(
     request: HttpRequest,
     user_profile: UserProfile,
-    payload: Dict[str, Any] = REQ(argument_type="body"),
+    payload: WildValue = REQ(argument_type="body", converter=to_wild_value),
 ) -> HttpResponse:
-
-    if len(payload["text"]) != 0:
-        message_body = payload["text"]
+    text = payload["text"].tame(check_string)
+    if len(text) != 0:
+        message_body = text
     else:
-        message_body = payload["notification"]
+        message_body = payload["notification"].tame(check_string)
 
     topic = "Flock notifications"
     body = f"{message_body}"
