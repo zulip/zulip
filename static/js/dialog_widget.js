08d2fb8d6588387b6511af326cb55a984469d418
AnishaSingh0118 <yourclear>
2022-04-08 19:10:33 +0530

    Fix Disable Save changes Button when no changes have been made

--- static/js/dialog_widget.js
+++ static/js/dialog_widget.js
@@ -40,6 +40,14 @@ import * as overlays from "./overlays";
  *      7) If a caller needs to run code after the modal body is added
  *          to DOM, it can do so by passing a post_render hook.
  */
+ export function EnableDisable(txtPassportNumber) {
+                var btnSubmit = document.getElementById("btnSubmit");     
+                 if (txtPassportNumber.value.trim() != "") {
+                 btnSubmit.disabled = false;
+                 } else {
+                 btnSubmit.disabled = true;
+                 }
+            }
 
 export function hide_dialog_spinner() {
     $(".dialog_submit_button span").show();
