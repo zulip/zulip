129233853706c99228c3f68abcde135a69865f6a
Lauryn Menard <lauryn@zulip.com>
2022-04-12 17:41:07 +0200

    frontend: Add `org_type` to realm settings updates and events.
    Adds a drop-down menu for updating the organization type in the
    `organization_profile_admin` page. Implements front end for
    this setting to work / update like other organization profile,
    notification and permissions settings.
    
    One special note about this dropdown is that the listed options
    should change once an organization has successfully set a type
    other than 'unspecified' in the database. To accomplish this
    the initial settings overlay build checks the realm_org_type
    value in the page_params to select the correct options list,
    and when the dropdown value is reset, either for update events
    or for discarding changes, the page_params value is again used
    to check for whether the 'unspecified' value should be present
    as an option in the dropdown menu.
    
    Adds basic node test for the `server_events_dispatch`.
    Also adds a new help center documentation article for this
    organization setting that is linked to in the UI.
    
    Fixes #21692.

--- frontend_tests/node_tests/lib/events.js
+++ frontend_tests/node_tests/lib/events.js
@@ -342,6 +342,13 @@ exports.fixtures = {
         value: 42,
     },
 
+    realm__update__org_type: {
+        type: "realm",
+        op: "update",
+        property: "org_type",
+        value: 50,
+    },
+
     realm__update__signup_notifications_stream_id: {
         type: "realm",
         op: "update",
