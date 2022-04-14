c1ab2b0cbfb46274f842b60d7253ccfdf079c48c
Anders Kaseorg <anders@zulip.com>
2022-04-14 14:58:37 -0700

    actions: Move part into zerver.lib.test_classes.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>
    (cherry picked from commit 729019acdd28bddc255c7b14cccdc13955e71e97)

--- zerver/lib/actions.py
+++ zerver/lib/actions.py
@@ -1,12 +1,1 @@
-from typing import List
 
-from zerver.lib.topic import filter_by_topic_name_via_message
-from zerver.models import Message, Stream, UserMessage, UserProfile
-
-
-def get_topic_messages(user_profile: UserProfile, stream: Stream, topic_name: str) -> List[Message]:
-    query = UserMessage.objects.filter(
-        user_profile=user_profile,
-        message__recipient=stream.recipient,
-    ).order_by("id")
-    return [um.message for um in filter_by_topic_name_via_message(query, topic_name)]
