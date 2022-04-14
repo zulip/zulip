327ff9ea0f5e4712a34d767fee55a549cc1d3f39
Alex Vandiver <alexmv@zulip.com>
2022-04-14 12:52:41 -0700

    preview: Use a dataclass for the embed data.
    This is significantly cleaner than passing around `Dict[str, Any]` all
    of the time.

---
+++ zerver/lib/url_preview/types.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from typing_extensions import Literal
+
+
+@dataclass
+class UrlEmbedData:
+    type: Optional[str] = None
+    html: Optional[str] = None
+    title: Optional[str] = None
+    description: Optional[str] = None
+    image: Optional[str] = None
+
+    def merge(self, other: "UrlEmbedData") -> None:
+        if self.title is None and other.title is not None:
+            self.title = other.title
+        if self.description is None and other.description is not None:
+            self.description = other.description
+        if self.image is None and other.image is not None:
+            self.image = other.image
+
+
+@dataclass
+class UrlOEmbedData(UrlEmbedData):
+    type: Literal["photo", "video"]
+    html: Optional[str] = None
