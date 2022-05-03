7f6a77da31964f141a8c260aeabff62cb106affd
Alex Vandiver <alexmv@zulip.com>
2022-05-03 11:51:01 -0700

    puppet: Add a redis exporter.

--- puppet/zulip_ops/manifests/profile/chat_zulip_org.pp
+++ puppet/zulip_ops/manifests/profile/chat_zulip_org.pp
@@ -4,6 +4,7 @@ class zulip_ops::profile::chat_zulip_org {
 
   include zulip_ops::profile::base
   include zulip_ops::app_frontend_monitoring
+  include zulip_ops::prometheus::redis
   zulip_ops::firewall_allow { 'http': }
   zulip_ops::firewall_allow { 'https': }
   zulip_ops::firewall_allow { 'smtp': }
