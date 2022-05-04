e637ff626dbaaf39a39860b003e3d76cbb93c62d
Alex Vandiver <alexmv@zulip.com>
2022-05-04 00:27:08 +0000

    version: Fix latest major version and blog post link.

--- version.py
+++ version.py
@@ -13,9 +13,9 @@ if os.path.exists(zulip_git_version_file):
 ZULIP_VERSION = lines.pop(0).strip()
 ZULIP_MERGE_BASE = lines.pop(0).strip()
 
-LATEST_MAJOR_VERSION = "5.1"
+LATEST_MAJOR_VERSION = "5.0"
 LATEST_RELEASE_VERSION = "5.1"
-LATEST_RELEASE_ANNOUNCEMENT = "https://blog.zulip.com/2021/05/13/zulip-4-0-released/"
+LATEST_RELEASE_ANNOUNCEMENT = "https://blog.zulip.com/2022/03/29/zulip-5-0-released/"
 
 # Versions of the desktop app below DESKTOP_MINIMUM_VERSION will be
 # prevented from connecting to the Zulip server.  Versions above
