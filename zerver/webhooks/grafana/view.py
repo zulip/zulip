a1881dd545acb13cdd611175f6c926ac0da8a3ae
Alex Vandiver <alexmv@zulip.com>
2022-06-16 16:45:57 -0700

    grafana: Strengthen types using WildValue.

--- zerver/webhooks/grafana/view.py
+++ zerver/webhooks/grafana/view.py
@@ -5,6 +5,15 @@ from django.http import HttpRequest, HttpResponse
 from zerver.decorator import webhook_view
 from zerver.lib.request import REQ, has_request_variables
 from zerver.lib.response import json_success
+from zerver.lib.validator import (
+    check_float,
+    check_int,
+    check_string,
+    check_string_in,
+    check_union,
+    check_url,
+    to_wild_value,
+)
 from zerver.lib.webhooks.common import check_send_webhook_message
 from zerver.models import UserProfile
 
@@ -12,9 +21,7 @@ GRAFANA_TOPIC_TEMPLATE = "{alert_title}"
 
 GRAFANA_ALERT_STATUS_TEMPLATE = "{alert_icon} **{alert_state}**\n\n"
 
-GRAFANA_MESSAGE_TEMPLATE = (
-    "{alert_status}[{rule_name}]({rule_url})\n\n{alert_message}{eval_matches}"
-)
+GRAFANA_MESSAGE_TEMPLATE = "{alert_status}{rule}\n\n{alert_message}{eval_matches}"
 
 ALL_EVENT_TYPES = ["ok", "pending", "alerting", "paused"]
 
@@ -24,51 +31,62 @@ ALL_EVENT_TYPES = ["ok", "pending", "alerting", "paused"]
 def api_grafana_webhook(
     request: HttpRequest,
     user_profile: UserProfile,
-    payload: Dict[str, Any] = REQ(argument_type="body"),
+    payload: WildValue = REQ(argument_type="body", converter=to_wild_value),
 ) -> HttpResponse:
 
-    topic = GRAFANA_TOPIC_TEMPLATE.format(alert_title=payload["title"])
+    # Webhook content has no specification, but is output from
+    # https://github.com/grafana/grafana/blob/v8.5.x/pkg/services/alerting/notifiers/webhook.go
+
+    topic = GRAFANA_TOPIC_TEMPLATE.format(alert_title=payload["title"].tame(check_string))
 
     eval_matches_text = ""
-    eval_matches = payload.get("evalMatches")
-    if eval_matches is not None:
-        for match in eval_matches:
-            eval_matches_text += "**{}:** {}\n".format(match["metric"], match["value"])
+    if "evalMatches" in payload and payload["evalMatches"]:
+        for match in payload["evalMatches"]:
+            eval_matches_text += "**{}:** {}\n".format(
+                match["metric"].tame(check_string),
+                match["value"].tame(check_union([check_float, check_int])),
+            )
 
     message_text = ""
-    if payload.get("message") is not None:
-        message_text = payload["message"] + "\n\n"
+    if "message" in payload:
+        message_text = payload["message"].tame(check_string) + "\n\n"
 
-    if payload.get("state") is not None:
-        if payload.get("state") == "alerting":
-            alert_status = GRAFANA_ALERT_STATUS_TEMPLATE.format(
-                alert_icon=":alert:", alert_state=payload["state"].upper()
-            )
-        elif payload.get("state") == "ok":
-            alert_status = GRAFANA_ALERT_STATUS_TEMPLATE.format(
-                alert_icon=":squared_ok:", alert_state=payload["state"].upper()
-            )
-        else:
-            alert_status = GRAFANA_ALERT_STATUS_TEMPLATE.format(
-                alert_icon=":info:", alert_state=payload["state"].upper()
-            )
+    state = payload["state"].tame(
+        check_string_in(["no_data", "paused", "alerting", "ok", "pending", "unknown"])
+    )
+
+    if state == "alerting":
+        alert_status = GRAFANA_ALERT_STATUS_TEMPLATE.format(
+            alert_icon=":alert:", alert_state=state.upper()
+        )
+    elif state == "ok":
+        alert_status = GRAFANA_ALERT_STATUS_TEMPLATE.format(
+            alert_icon=":squared_ok:", alert_state=state.upper()
+        )
+    else:
+        alert_status = GRAFANA_ALERT_STATUS_TEMPLATE.format(
+            alert_icon=":info:", alert_state=state.upper()
+        )
+
+    rule = payload["ruleName"].tame(check_string)
+    if "ruleUrl" in payload:
+        rule = f"[{rule}]({payload['ruleUrl'].tame(check_url)})"
 
     body = GRAFANA_MESSAGE_TEMPLATE.format(
         alert_message=message_text,
         alert_status=alert_status,
-        rule_name=payload["ruleName"],
-        rule_url=payload["ruleUrl"],
+        rule=rule,
         eval_matches=eval_matches_text,
     )
 
-    if payload.get("imageUrl") is not None:
+    if "imageUrl" in payload is not None:
         body += "\n[Click to view visualization]({visualization})".format(
-            visualization=payload["imageUrl"]
+            visualization=payload["imageUrl"].tame(check_url)
         )
 
     body = body.strip()
 
     # send the message
-    check_send_webhook_message(request, user_profile, topic, body, payload.get("state"))
+    check_send_webhook_message(request, user_profile, topic, body, state)
 
     return json_success(request)
