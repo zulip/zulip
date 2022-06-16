bc25f016fca8e6a4da97dbd8834f05f6d7771f72
Alex Vandiver <alexmv@zulip.com>
2022-06-16 16:11:12 -0700

    slack_incoming: Support more syntax.
    This significantly extends the accepted support for Slack "blocks"[1], as
    well as their legacy "attachments"[2] syntax.
    
    Fixes: #21944.
    
    See also #22228, which this does not solve, but assuming we can decide
    which compromises to make, becomes a reasonably uncomplicated follow-up.
    
    [1]: https://api.slack.com/reference/block-kit/blocks
    [2]: https://api.slack.com/reference/messaging/attachments

--- zerver/webhooks/slack_incoming/view.py
+++ zerver/webhooks/slack_incoming/view.py
@@ -9,7 +9,16 @@ from zerver.decorator import webhook_view
 from zerver.lib.exceptions import JsonableError
 from zerver.lib.request import REQ, RequestVariableMissingError, has_request_variables
 from zerver.lib.response import json_success
-from zerver.lib.validator import WildValue, WildValueDict, check_string, check_url, to_wild_value
+from zerver.lib.types import Validator
+from zerver.lib.validator import (
+    WildValue,
+    check_dict,
+    check_int,
+    check_string,
+    check_string_in,
+    check_url,
+    to_wild_value,
+)
 from zerver.lib.webhooks.common import check_send_webhook_message
 from zerver.models import UserProfile
 
@@ -72,38 +81,131 @@ def api_slack_incoming_webhook(
 
 
 def render_block(block: WildValue) -> str:
-    block_type = block["type"].tame(check_string)
-    if block_type == "section":
-        body = ""
+    # https://api.slack.com/reference/block-kit/blocks
+    block_type = block["type"].tame(
+        check_string_in(["actions", "context", "divider", "header", "image", "input", "section"])
+    )
+    if block_type == "actions":
+        # Unhandled
+        return ""
+    elif block_type == "context" and block.get("elements"):
+        pieces = []
+        # Slack renders these pieces left-to-right, packed in as
+        # closely as possible.  We just render them above each other,
+        # for simplicity.
+        for element in block["elements"]:
+            element_type = element["type"].tame(check_string_in(["image", "plain_text", "mrkdwn"]))
+            if element_type == "image":
+                pieces.append(render_block_element(element))
+            else:
+                pieces.append(element.tame(check_text_block()))
+        return "\n\n".join(piece.strip() for piece in pieces if piece.strip() != "")
+    elif block_type == "divider":
+        return "----"
+    elif block_type == "header":
+        return "## " + block["text"].tame(check_text_block(plain_text_only=True))
+    elif block_type == "image":
+        image_url = block["image_url"].tame(check_url)
+        alt_text = block["alt_text"].tame(check_string)
+        if "title" in block:
+            alt_text = block["title"].tame(check_text_block(plain_text_only=True))
+        return f"[{alt_text}]({image_url})"
+    elif block_type == "input":
+        # Unhandled
+        pass
+    elif block_type == "section":
+        pieces = []
         if "text" in block:
-            text = block["text"]
-            while isinstance(text, WildValueDict):
-                text = text["text"]
-            body += "\n\n" + text.tame(check_string)
+            pieces.append(block["text"].tame(check_text_block()))
 
         if "accessory" in block:
-            accessory = block["accessory"]
-            accessory_type = accessory["type"].tame(check_string)
-            if accessory_type == "image":
-                # This should become ![text](url) once proper Markdown images are supported
-                alt_text = accessory["alt_text"].tame(check_string)
-                image_url = accessory["image_url"].tame(check_url)
-                body += f"\n[{alt_text}]({image_url})"
-        return body
+            pieces.append(render_block_element(block["accessory"]))
+
+        if "fields" in block:
+            # TODO -- these should be rendered in two columns,
+            # left-to-right.  We could render them sequentially,
+            # except some may be Title1 / Title2 / value1 / value2,
+            # which would be nonsensical when rendered sequentially.
+            pass
+
+        return "\n\n".join(piece.strip() for piece in pieces if piece.strip() != "")
 
     return ""
 
 
-def render_attachment(attachment: WildValue) -> str:
-    attachment_body = ""
-    if "title" in attachment and "title_link" in attachment:
-        title = attachment["title"].tame(check_string)
-        title_link = attachment["title_link"].tame(check_string)
-        attachment_body += f"[{title}]({title_link})\n"
-    if "text" in attachment:
-        attachment_body += attachment["text"].tame(check_string)
+def check_text_block(plain_text_only: bool = False) -> Validator[str]:
+    if plain_text_only:
+        type_validator = check_string_in(["plain_text"])
+    else:
+        type_validator = check_string
 
-    return attachment_body
+    def f(var_name: str, val: object) -> str:
+        block = check_dict(
+            [
+                ("type", type_validator),
+                ("text", check_string),
+            ],
+        )(var_name, val)
+
+        # We can't use `value_validator=check_string` above to let
+        # mypy know this is a str, because there's an optional boolean
+        # `emoji` key which can appear -- hence the assert.
+        text = block["text"]
+        assert isinstance(text, str)
+
+        # Ideally we would escape the content if it was plain text,
+        # but out flavor of Markdown doesn't support escapes. :(
+        return text
+
+    return f
+
+
+def render_block_element(element: WildValue) -> str:
+    # https://api.slack.com/reference/block-kit/block-elements
+    # Zulip doesn't support interactive elements, so we only render images here
+    element_type = element["type"].tame(check_string)
+    if element_type == "image":
+        image_url = element["image_url"].tame(check_url)
+        alt_text = element["alt_text"].tame(check_string)
+        return f"[{alt_text}]({image_url})"
+    else:
+        # Unsupported
+        return ""
+
+
+def render_attachment(attachment: WildValue) -> str:
+    # https://api.slack.com/reference/messaging/attachments
+    pieces = []
+    if "title" in attachment:
+        title = attachment["title"].tame(check_string)
+        if "title_link" in attachment:
+            title_link = attachment["title_link"].tame(check_url)
+            pieces.append(f"## [{title}]({title_link})")
+        else:
+            pieces.append(f"## {title}")
+    if "pretext" in attachment:
+        pieces.append(attachment["pretext"].tame(check_string))
+    if "text" in attachment:
+        pieces.append(attachment["text"].tame(check_string))
+    if "fields" in attachment:
+        fields = []
+        for field in attachment["fields"]:
+            title = field["title"].tame(check_string)
+            value = field["value"].tame(check_string)
+            fields.append(f"*{title}*: {value}")
+        pieces.append("\n".join(fields))
+    if "blocks" in attachment and attachment["blocks"]:
+        for block in attachment["blocks"]:
+            pieces.append(render_block(block))
+    if "image_url" in attachment:
+        pieces.append("[]({})".format(attachment["image_url"].tame(check_url)))
+    if "footer" in attachment:
+        pieces.append(attachment["footer"].tame(check_string))
+    if "ts" in attachment:
+        time = attachment["ts"].tame(check_int)
+        pieces.append(f"<time:{time}>")
+
+    return "\n\n".join(piece.strip() for piece in pieces if piece.strip() != "")
 
 
 def replace_links(text: str) -> str:
