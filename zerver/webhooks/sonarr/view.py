146dd68cf2ce1d1ec75d58f12b031018ac102b0d
Hari Prashant Bhimaraju <haripb01@gmail.com>
2022-06-23 19:51:12 +0530

    sonarr: Strengthen types using WildValue.

--- zerver/webhooks/sonarr/view.py
+++ zerver/webhooks/sonarr/view.py
@@ -1,11 +1,10 @@
-from typing import Any, Dict
-
 from django.http import HttpRequest, HttpResponse
 
 from zerver.decorator import webhook_view
 from zerver.lib.exceptions import UnsupportedWebhookEventType
 from zerver.lib.request import REQ, has_request_variables
 from zerver.lib.response import json_success
+from zerver.lib.validator import WildValue, check_bool, check_int, check_string, to_wild_value
 from zerver.lib.webhooks.common import check_send_webhook_message, get_setup_webhook_message
 from zerver.models import UserProfile
 
@@ -44,115 +43,128 @@ ALL_EVENT_TYPES = [
 def api_sonarr_webhook(
     request: HttpRequest,
     user_profile: UserProfile,
-    payload: Dict[str, Any] = REQ(argument_type="body"),
+    payload: WildValue = REQ(argument_type="body", converter=to_wild_value),
 ) -> HttpResponse:
     body = get_body_for_http_request(payload)
     subject = get_subject_for_http_request(payload)
 
-    check_send_webhook_message(request, user_profile, subject, body, payload["eventType"])
+    check_send_webhook_message(
+        request, user_profile, subject, body, payload["eventType"].tame(check_string)
+    )
     return json_success(request)
 
 
-def get_subject_for_http_request(payload: Dict[str, Any]) -> str:
-    if payload["eventType"] != "Test" and payload["eventType"] != "Health":
-        topic = SONARR_TOPIC_TEMPLATE.format(series_title=payload["series"]["title"])
-    elif payload["eventType"] == "Test":
+def get_subject_for_http_request(payload: WildValue) -> str:
+    event_type = payload["eventType"].tame(check_string)
+    if event_type != "Test" and event_type != "Health":
+        topic = SONARR_TOPIC_TEMPLATE.format(
+            series_title=payload["series"]["title"].tame(check_string)
+        )
+    elif event_type == "Test":
         topic = SONARR_TOPIC_TEMPLATE_TEST
-    elif payload["eventType"] == "Health":
-        topic = SONARR_TOPIC_TEMPLATE_HEALTH_CHECK.format(level=payload["level"])
+    elif event_type == "Health":
+        topic = SONARR_TOPIC_TEMPLATE_HEALTH_CHECK.format(level=payload["level"].tame(check_string))
 
     return topic
 
 
-def get_body_for_health_check_event(payload: Dict[str, Any]) -> str:
-    return SONARR_MESSAGE_TEMPLATE_HEALTH_CHECK.format(message=payload["message"])
+def get_body_for_health_check_event(payload: WildValue) -> str:
+    return SONARR_MESSAGE_TEMPLATE_HEALTH_CHECK.format(
+        message=payload["message"].tame(check_string)
+    )
 
 
-def get_body_for_episodes_renamed_event(payload: Dict[str, Any]) -> str:
-    return SONARR_MESSAGE_TEMPLATE_EPISODES_RENAMED.format(series_title=payload["series"]["title"])
+def get_body_for_episodes_renamed_event(payload: WildValue) -> str:
+    return SONARR_MESSAGE_TEMPLATE_EPISODES_RENAMED.format(
+        series_title=payload["series"]["title"].tame(check_string)
+    )
 
 
-def get_body_for_series_deleted_event(payload: Dict[str, Any]) -> str:
-    return SONARR_MESSAGE_TEMPLATE_SERIES_DELETED.format(series_title=payload["series"]["title"])
+def get_body_for_series_deleted_event(payload: WildValue) -> str:
+    return SONARR_MESSAGE_TEMPLATE_SERIES_DELETED.format(
+        series_title=payload["series"]["title"].tame(check_string)
+    )
 
 
-def get_body_for_episode_imported_upgrade_event(payload: Dict[str, Any]) -> str:
+def get_body_for_episode_imported_upgrade_event(payload: WildValue) -> str:
     data = {
-        "series_title": payload["series"]["title"],
-        "series_number": payload["episodes"][0]["seasonNumber"],
-        "episode_number": payload["episodes"][0]["episodeNumber"],
-        "episode_name": payload["episodes"][0]["title"],
-        "new_quality": payload["episodeFile"]["quality"],
-        "old_quality": payload["deletedFiles"][0]["quality"],
+        "series_title": payload["series"]["title"].tame(check_string),
+        "series_number": payload["episodes"][0]["seasonNumber"].tame(check_int),
+        "episode_number": payload["episodes"][0]["episodeNumber"].tame(check_int),
+        "episode_name": payload["episodes"][0]["title"].tame(check_string),
+        "new_quality": payload["episodeFile"]["quality"].tame(check_string),
+        "old_quality": payload["deletedFiles"][0]["quality"].tame(check_string),
     }
 
     return SONARR_MESSAGE_TEMPLATE_EPISODE_IMPORTED_UPGRADE.format(**data)
 
 
-def get_body_for_episode_imported_event(payload: Dict[str, Any]) -> str:
+def get_body_for_episode_imported_event(payload: WildValue) -> str:
     data = {
-        "series_title": payload["series"]["title"],
-        "series_number": payload["episodes"][0]["seasonNumber"],
-        "episode_number": payload["episodes"][0]["episodeNumber"],
-        "episode_name": payload["episodes"][0]["title"],
+        "series_title": payload["series"]["title"].tame(check_string),
+        "series_number": payload["episodes"][0]["seasonNumber"].tame(check_int),
+        "episode_number": payload["episodes"][0]["episodeNumber"].tame(check_int),
+        "episode_name": payload["episodes"][0]["title"].tame(check_string),
     }
 
     return SONARR_MESSAGE_TEMPLATE_EPISODE_IMPORTED.format(**data)
 
 
-def get_body_for_episode_grabbed_event(payload: Dict[str, Any]) -> str:
+def get_body_for_episode_grabbed_event(payload: WildValue) -> str:
     data = {
-        "series_title": payload["series"]["title"],
-        "series_number": payload["episodes"][0]["seasonNumber"],
-        "episode_number": payload["episodes"][0]["episodeNumber"],
-        "episode_name": payload["episodes"][0]["title"],
+        "series_title": payload["series"]["title"].tame(check_string),
+        "series_number": payload["episodes"][0]["seasonNumber"].tame(check_int),
+        "episode_number": payload["episodes"][0]["episodeNumber"].tame(check_int),
+        "episode_name": payload["episodes"][0]["title"].tame(check_string),
     }
 
     return SONARR_MESSAGE_TEMPLATE_EPISODE_GRABBED.format(**data)
 
 
-def get_body_for_episode_deleted_upgrade_event(payload: Dict[str, Any]) -> str:
+def get_body_for_episode_deleted_upgrade_event(payload: WildValue) -> str:
     data = {
-        "series_title": payload["series"]["title"],
-        "series_number": payload["episodes"][0]["seasonNumber"],
-        "episode_number": payload["episodes"][0]["episodeNumber"],
-        "episode_name": payload["episodes"][0]["title"],
+        "series_title": payload["series"]["title"].tame(check_string),
+        "series_number": payload["episodes"][0]["seasonNumber"].tame(check_int),
+        "episode_number": payload["episodes"][0]["episodeNumber"].tame(check_int),
+        "episode_name": payload["episodes"][0]["title"].tame(check_string),
     }
 
     return SONARR_MESSAGE_TEMPLATE_EPISODE_DELETED_UPGRADE.format(**data)
 
 
-def get_body_for_episode_deleted_event(payload: Dict[str, Any]) -> str:
+def get_body_for_episode_deleted_event(payload: WildValue) -> str:
     data = {
-        "series_title": payload["series"]["title"],
-        "series_number": payload["episodes"][0]["seasonNumber"],
-        "episode_number": payload["episodes"][0]["episodeNumber"],
-        "episode_name": payload["episodes"][0]["title"],
+        "series_title": payload["series"]["title"].tame(check_string),
+        "series_number": payload["episodes"][0]["seasonNumber"].tame(check_int),
+        "episode_number": payload["episodes"][0]["episodeNumber"].tame(check_int),
+        "episode_name": payload["episodes"][0]["title"].tame(check_string),
     }
 
     return SONARR_MESSAGE_TEMPLATE_EPISODE_DELETED.format(**data)
 
 
-def get_body_for_http_request(payload: Dict[str, Any]) -> str:
-    if payload["eventType"] == "Test":
+def get_body_for_http_request(payload: WildValue) -> str:
+    event_type = payload["eventType"].tame(check_string)
+
+    if event_type == "Test":
         return get_setup_webhook_message("Sonarr")
-    elif payload["eventType"] == "Health":
+    elif event_type == "Health":
         return get_body_for_health_check_event(payload)
-    elif payload["eventType"] == "Rename":
+    elif event_type == "Rename":
         return get_body_for_episodes_renamed_event(payload)
-    elif payload["eventType"] == "SeriesDelete":
+    elif event_type == "SeriesDelete":
         return get_body_for_series_deleted_event(payload)
-    elif payload["eventType"] == "Download" and "isUpgrade" in payload:
-        if payload["isUpgrade"]:
+    elif event_type == "Download" and "isUpgrade" in payload:
+        if payload["isUpgrade"].tame(check_bool):
             return get_body_for_episode_imported_upgrade_event(payload)
         else:
             return get_body_for_episode_imported_event(payload)
-    elif payload["eventType"] == "Grab":
+    elif event_type == "Grab":
         return get_body_for_episode_grabbed_event(payload)
-    elif payload["eventType"] == "EpisodeFileDelete" and "deleteReason" in payload:
-        if payload["deleteReason"] == "upgrade":
+    elif event_type == "EpisodeFileDelete" and "deleteReason" in payload:
+        if payload["deleteReason"].tame(check_string) == "upgrade":
             return get_body_for_episode_deleted_upgrade_event(payload)
         else:
             return get_body_for_episode_deleted_event(payload)
     else:
-        raise UnsupportedWebhookEventType(payload["eventType"])
+        raise UnsupportedWebhookEventType(event_type)
