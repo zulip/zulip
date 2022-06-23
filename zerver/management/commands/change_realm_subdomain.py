d70e0b4b17574453acb21137184923a8876bb8c4
Alex Vandiver <alexmv@zulip.com>
2022-06-23 14:57:39 -0700

    change_realm_subdomain: Support skipping the redirect.
    Sometimes (e.g. when moving an old realm out of the way of an import
    into that name) we do *not* wish to add a redirect realm.  Add a flag
    to support that.

--- zerver/management/commands/change_realm_subdomain.py
+++ zerver/management/commands/change_realm_subdomain.py
@@ -11,11 +11,21 @@ class Command(ZulipBaseCommand):
     def add_arguments(self, parser: ArgumentParser) -> None:
         self.add_realm_args(parser, required=True)
         parser.add_argument("new_subdomain", metavar="<new subdomain>", help="realm new subdomain")
+        parser.add_argument(
+            "--skip-redirect",
+            action="store_true",
+            help="Do not redirect the old name to the new one",
+        )
 
-    def handle(self, *args: Any, **options: str) -> None:
+    def handle(self, *args: Any, **options: Any) -> None:
         realm = self.get_realm(options)
         assert realm is not None  # Should be ensured by parser
 
         new_subdomain = options["new_subdomain"]
-        do_change_realm_subdomain(realm, new_subdomain, acting_user=None)
+        do_change_realm_subdomain(
+            realm,
+            new_subdomain,
+            acting_user=None,
+            add_deactivated_redirect=not options["skip_redirect"],
+        )
         print("Done!")
