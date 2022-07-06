2439914a506984acb36989e27517224ac11215fb
Anders Kaseorg <anders@zulip.com>
2022-07-06 16:12:49 -0700

    settings: Add two_factor.plugins.phonenumber to INSTALLED_APPS.
    I missed this in commit feff1d0411875c70e6c9ee5729af10cd7f7cbef2
    (#22383) for upgrading to django-two-factor-auth 1.14.0.
    
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- zproject/computed_settings.py
+++ zproject/computed_settings.py
@@ -218,6 +218,7 @@ INSTALLED_APPS = [
     "django_otp.plugins.otp_static",
     "django_otp.plugins.otp_totp",
     "two_factor",
+    "two_factor.plugins.phonenumber",
 ]
 if USING_PGROONGA:
     INSTALLED_APPS += ["pgroonga"]
