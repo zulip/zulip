bfd9fc86223c2446e8b38d2cdd5876caed50bfda
rodwan-bakkar <rodwan.bakkar@gmail.com>
2022-07-22 11:37:33 +0200

    integration: Fix integration with newrelic.
    Newrelic updated the payload that's sent via the webhook incoming call
    causing a bug in the newrelic webhook endpoint.
    This fixes the bug by updating the endpoint to respect the new format
    of the payload as well as the old format. This should be updated once
    the old format is EOLed.
    
    Fixes #22338.

--- zerver/webhooks/newrelic/view.py
+++ zerver/webhooks/newrelic/view.py
@@ -11,8 +11,21 @@ from zerver.lib.response import json_success
 from zerver.lib.webhooks.common import check_send_webhook_message, unix_milliseconds_to_timestamp
 from zerver.models import UserProfile
 
+# Newrelic planned to upgrade Alert Notification Channels to Workflows and Destinations
+# https://discuss.newrelic.com/t/plan-to-upgrade-alert-notification-channels-to-workflows-and-destinations/188205
+# This view will handle both old and new format but will keep it easy to delete the old code
+# once it is EOLed by the end of June, 2023
+
+# Once old is EOLed, delete the OPEN_TEMPLATE
 OPEN_TEMPLATE = """
 [Incident]({incident_url}) **opened** for condition: **{condition_name}** at <time:{iso_timestamp}>
+``` quote
+{details}
+```
+""".strip()
+
+ACTIVE_TEMPLATE = """
+[Incident]({incident_url}) **active** for condition: **{condition_name}** at <time:{iso_timestamp}>
 ``` quote
 {details}
 ```
@@ -24,7 +37,10 @@ DEFAULT_TEMPLATE = (
 
 TOPIC_TEMPLATE = """{policy_name} ({incident_id})""".strip()
 
-ALL_EVENT_TYPES = ["closed", "acknowledged", "open"]
+# Once old is EOLed, delete old and keep new
+OLD_EVENT_TYPES = ["closed", "acknowledged", "open"]
+NEW_EVENT_TYPES = ["created", "activated", "acknowledged", "closed"]
+ALL_EVENT_TYPES = list(set(OLD_EVENT_TYPES).union(set(NEW_EVENT_TYPES)))
 
 
 @webhook_view("NewRelic", all_event_types=ALL_EVENT_TYPES)
@@ -35,45 +51,102 @@ def api_newrelic_webhook(
     payload: Dict[str, Any] = REQ(argument_type="body"),
 ) -> HttpResponse:
 
-    info = {
-        "condition_name": payload.get("condition_name", "Unknown condition"),
-        "details": payload.get("details", "No details."),
-        "incident_url": payload.get("incident_url", "https://alerts.newrelic.com"),
-        "incident_acknowledge_url": payload.get(
-            "incident_acknowledge_url", "https://alerts.newrelic.com"
-        ),
-        "status": payload.get("current_state", "None"),
-        "iso_timestamp": "",
-        "owner": payload.get("owner", ""),
-    }
+    # Handle old format
+    # Once old is EOLed, delete if block and keep else block
+    if not payload.get("id"):
+        info = {
+            "condition_name": payload.get("condition_name", "Unknown condition"),
+            "details": payload.get("details", "No details."),
+            "incident_url": payload.get("incident_url", "https://alerts.newrelic.com"),
+            "incident_acknowledge_url": payload.get(
+                "incident_acknowledge_url", "https://alerts.newrelic.com"
+            ),
+            "status": payload.get("current_state", "None"),
+            "iso_timestamp": "",
+            "owner": payload.get("owner", ""),
+        }
 
-    unix_time = payload.get("timestamp", None)
-    if unix_time is None:
-        raise JsonableError(_("The newrelic webhook requires timestamp in milliseconds"))
+        unix_time = payload.get("timestamp", None)
+        if unix_time is None:
+            raise JsonableError(_("The newrelic webhook requires timestamp in milliseconds"))
 
-    info["iso_timestamp"] = unix_milliseconds_to_timestamp(unix_time, "newrelic")
+        info["iso_timestamp"] = unix_milliseconds_to_timestamp(unix_time, "newrelic")
 
-    # Add formatting to the owner field if owner is present
-    if info["owner"] != "":
-        info["owner"] = "by **{}** ".format(info["owner"])
+        # Add formatting to the owner field if owner is present
+        if info["owner"] != "":
+            info["owner"] = "by **{}** ".format(info["owner"])
 
-    # These are the three promised current_state values
-    if "open" in info["status"]:
-        content = OPEN_TEMPLATE.format(**info)
-    elif "acknowledged" in info["status"]:
-        content = DEFAULT_TEMPLATE.format(**info)
-    elif "closed" in info["status"]:
-        content = DEFAULT_TEMPLATE.format(**info)
+        # These are the three promised current_state values
+        if info["status"].lower() == "open":
+            content = OPEN_TEMPLATE.format(**info)
+        elif info["status"].lower() == "acknowledged":
+            content = DEFAULT_TEMPLATE.format(**info)
+        elif info["status"].lower() == "closed":
+            content = DEFAULT_TEMPLATE.format(**info)
+        else:
+            raise JsonableError(
+                _("The newrelic webhook requires current_state be in [open|acknowledged|closed]")
+            )
+
+        topic_info = {
+            "policy_name": payload.get("policy_name", "Unknown Policy"),
+            "incident_id": payload.get("incident_id", "Unknown ID"),
+        }
+        topic = TOPIC_TEMPLATE.format(**topic_info)
+
+        check_send_webhook_message(request, user_profile, topic, content, info["status"])
+        return json_success(request)
+
+    # Handle new format
     else:
-        raise JsonableError(
-            _("The newrelic webhook requires current_state be in [open|acknowledged|closed]")
-        )
+        info = {
+            "condition_name": payload.get("condition_name", "Unknown condition"),
+            "details": payload.get("details", "No details."),
+            "incident_url": payload.get("issueUrl", "https://alerts.newrelic.com"),
+            "incident_acknowledge_url": payload.get(
+                "incident_acknowledge_url", "https://alerts.newrelic.com"
+            ),
+            "status": payload.get("state", "None"),
+            "iso_timestamp": "",
+            "owner": payload.get("owner", ""),
+        }
 
-    topic_info = {
-        "policy_name": payload.get("policy_name", "Unknown Policy"),
-        "incident_id": payload.get("incident_id", "Unknown ID"),
-    }
-    topic = TOPIC_TEMPLATE.format(**topic_info)
+        unix_time = payload.get("createdAt", None)
+        if unix_time is None:
+            raise JsonableError(_("The newrelic webhook requires timestamp in milliseconds"))
 
-    check_send_webhook_message(request, user_profile, topic, content, info["status"])
-    return json_success(request)
+        info["iso_timestamp"] = unix_milliseconds_to_timestamp(unix_time, "newrelic")
+
+        # Add formatting to the owner field if owner is present
+        if info["owner"] != "":
+            info["owner"] = "by **{}** ".format(info["owner"])
+
+        # These are the three promised state values
+        if info["status"].lower() == "activated":
+            content = ACTIVE_TEMPLATE.format(**info)
+        elif info["status"].lower() == "acknowledged":
+            content = DEFAULT_TEMPLATE.format(**info)
+        elif info["status"].lower() == "closed":
+            content = DEFAULT_TEMPLATE.format(**info)
+        elif info["status"].lower() == "created":
+            content = DEFAULT_TEMPLATE.format(**info)
+        else:
+            raise JsonableError(
+                _(
+                    "The newrelic webhook requires state be in [created|activated|acknowledged|closed]"
+                )
+            )
+
+        policy_names_list = payload.get("alertPolicyNames", [])
+        if policy_names_list:
+            policy_names_str = ",".join(policy_names_list)
+        else:
+            policy_names_str = "Unknown Policy"
+        topic_info = {
+            "policy_name": policy_names_str,
+            "incident_id": payload.get("id", "Unknown ID"),
+        }
+        topic = TOPIC_TEMPLATE.format(**topic_info)
+
+        check_send_webhook_message(request, user_profile, topic, content, info["status"])
+        return json_success(request)
