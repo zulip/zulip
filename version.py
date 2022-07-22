aeea9e33665c5021e0d2c9f49ad028f5a68f1444
Matt Keller <matt@zulip.com>
2022-07-21 21:17:01 -0400

    version: Update version after 5.5 release.

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 import os
 
-ZULIP_VERSION = "5.5"
+ZULIP_VERSION = "5.5+git"
 
 # Add information on number of commits and commit hash to version, if available
 zulip_git_version_file = os.path.join(
