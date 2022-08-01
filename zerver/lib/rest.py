53cd73fb4384b858368eff422c2ad025d213e7d8
Zixuan James Li <p359101898@gmail.com>
2022-08-01 18:21:20 -0400

    rest: Implement get_target_view_function_or_response.
    As noted in the docstring, this is a temporary helper function that
    separates routing for paths that support multiple HTTP methods from
    `rest_dispatch` itself. We will need to replace this helper with
    class-based views in the future. The helper will also be handy to reduce
    duplication when splitting up `rest_dispatch` by authentication methods.
    
    Signed-off-by: Zixuan James Li <p359101898@gmail.com>

--- zerver/lib/rest.py
+++ zerver/lib/rest.py
@@ -41,6 +41,62 @@ def default_never_cache_responses(view_func: ViewFuncT) -> ViewFuncT:
     return cast(ViewFuncT, _wrapped_view_func)  # https://github.com/python/mypy/issues/1927
 
 
+def get_target_view_function_or_response(
+    request: HttpRequest, kwargs: Any
+) -> Union[Tuple[Callable[..., HttpResponse], Set[str]], HttpResponse]:
+    """
+    This pops handlers from kwargs and determine the target view function to be
+    used.
+    When the method is not supported, it returns a HttpResponse object, otherwise
+    a tuple of the handler and a set of view flags are returned.
+
+    TODO: This is a transition helper to remove handler delegation by HTTP method
+    from rest_dispatch. It decouples this routing functionality for the ease
+    of removal later. Eventually, we expect that paths with multiple supported
+    HTTP methods will be handled by class-based views provided by Django.
+    """
+    supported_methods: Dict[str, Any] = {}
+    request_notes = RequestNotes.get_notes(request)
+    if request_notes.saved_response is not None:
+        # For completing long-polled Tornado requests, we skip the
+        # view function logic and just return the response.
+        return request_notes.saved_response
+
+    # duplicate kwargs so we can mutate the original as we go
+    for arg in list(kwargs):
+        if arg in METHODS:
+            supported_methods[arg] = kwargs[arg]
+            del kwargs[arg]
+
+    if "GET" in supported_methods:
+        supported_methods.setdefault("HEAD", supported_methods["GET"])
+
+    target_function = None
+    view_flags: Set[str] = set()
+
+    if request.method == "OPTIONS":
+        response = HttpResponse(status=204)  # No content
+        response["Allow"] = ", ".join(sorted(supported_methods.keys()))
+        return response
+
+    # Override requested method if magic method=??? parameter exists
+    method_to_use = request.method
+    if request.POST and "method" in request.POST:
+        method_to_use = request.POST["method"]
+
+    if method_to_use in supported_methods:
+        entry = supported_methods[method_to_use]
+        if isinstance(entry, tuple):
+            target_function, view_flags = entry
+        else:
+            target_function = supported_methods[method_to_use]
+
+    if target_function is None:
+        return json_method_not_allowed(list(supported_methods.keys()))
+
+    return target_function, view_flags
+
+
 @default_never_cache_responses
 @csrf_exempt
 def rest_dispatch(request: HttpRequest, **kwargs: Any) -> HttpResponse:
@@ -66,106 +122,75 @@ def rest_dispatch(request: HttpRequest, **kwargs: Any) -> HttpResponse:
     Never make a urls.py pattern put user input into a variable called GET, POST,
     etc, as that is where we route HTTP verbs to target functions.
     """
-    supported_methods: Dict[str, Any] = {}
+    result = get_target_view_function_or_response(request, kwargs)
+    if isinstance(result, HttpResponse):
+        return result
+    else:
+        target_function, view_flags = result
     request_notes = RequestNotes.get_notes(request)
-    if request_notes.saved_response is not None:
-        # For completing long-polled Tornado requests, we skip the
-        # view function logic and just return the response.
-        return request_notes.saved_response
 
-    # duplicate kwargs so we can mutate the original as we go
-    for arg in list(kwargs):
-        if arg in METHODS:
-            supported_methods[arg] = kwargs[arg]
-            del kwargs[arg]
+    # Set request_notes.query for update_activity_user(), which is called
+    # by some of the later wrappers.
+    request_notes.query = target_function.__name__
 
-    if "GET" in supported_methods:
-        supported_methods.setdefault("HEAD", supported_methods["GET"])
+    # We want to support authentication by both cookies (web client)
+    # and API keys (API clients). In the former case, we want to
+    # do a check to ensure that CSRF etc is honored, but in the latter
+    # we can skip all of that.
+    #
+    # Security implications of this portion of the code are minimal,
+    # as we should worst-case fail closed if we miscategorize a request.
 
-    if request.method == "OPTIONS":
-        response = HttpResponse(status=204)  # No content
-        response["Allow"] = ", ".join(sorted(supported_methods.keys()))
-        return response
+    # for some special views (e.g. serving a file that has been
+    # uploaded), we support using the same URL for web and API clients.
+    if "override_api_url_scheme" in view_flags and "Authorization" in request.headers:
+        # This request uses standard API based authentication.
+        # For override_api_url_scheme views, we skip our normal
+        # rate limiting, because there are good reasons clients
+        # might need to (e.g.) request a large number of uploaded
+        # files or avatars in quick succession.
+        target_function = authenticated_rest_api_view(skip_rate_limiting=True)(target_function)
+    elif "override_api_url_scheme" in view_flags and request.GET.get("api_key") is not None:
+        # This request uses legacy API authentication.  We
+        # unfortunately need that in the React Native mobile apps,
+        # because there's no way to set the Authorization header in
+        # React Native.  See last block for rate limiting notes.
+        target_function = authenticated_uploads_api_view(skip_rate_limiting=True)(target_function)
+    # /json views (web client) validate with a session token (cookie)
+    elif not request.path.startswith("/api") and request.user.is_authenticated:
+        # Authenticated via sessions framework, only CSRF check needed
+        auth_kwargs = {}
+        if "override_api_url_scheme" in view_flags:
+            auth_kwargs["skip_rate_limiting"] = True
+        target_function = csrf_protect(authenticated_json_view(target_function, **auth_kwargs))
 
-    # Override requested method if magic method=??? parameter exists
-    method_to_use = request.method
-    if request.POST and "method" in request.POST:
-        method_to_use = request.POST["method"]
+    # most clients (mobile, bots, etc) use HTTP basic auth and REST calls, where instead of
+    # username:password, we use email:apiKey
+    elif "Authorization" in request.headers:
+        # Wrap function with decorator to authenticate the user before
+        # proceeding
+        target_function = authenticated_rest_api_view(
+            allow_webhook_access="allow_incoming_webhooks" in view_flags,
+        )(target_function)
+    elif (
+        request.path.startswith(("/json", "/avatar", "/user_uploads", "/thumbnail"))
+        and "allow_anonymous_user_web" in view_flags
+    ):
+        # For endpoints that support anonymous web access, we do that.
+        # TODO: Allow /api calls when this is stable enough.
+        target_function = csrf_protect(public_json_view(target_function))
+    else:
+        # Otherwise, throw an authentication error; our middleware
+        # will generate the appropriate HTTP response.
+        raise MissingAuthenticationError()
 
-    if method_to_use in supported_methods:
-        entry = supported_methods[method_to_use]
-        if isinstance(entry, tuple):
-            target_function, view_flags = entry
-        else:
-            target_function = supported_methods[method_to_use]
-            view_flags = set()
+    if request.method in ["DELETE", "PATCH", "PUT"]:
+        # process_as_post needs to be the outer decorator, because
+        # otherwise we might access and thus cache a value for
+        # request.POST.
+        target_function = process_as_post(target_function)
 
-        # Set request_notes.query for update_activity_user(), which is called
-        # by some of the later wrappers.
-        request_notes.query = target_function.__name__
-
-        # We want to support authentication by both cookies (web client)
-        # and API keys (API clients). In the former case, we want to
-        # do a check to ensure that CSRF etc is honored, but in the latter
-        # we can skip all of that.
-        #
-        # Security implications of this portion of the code are minimal,
-        # as we should worst-case fail closed if we miscategorize a request.
-
-        # for some special views (e.g. serving a file that has been
-        # uploaded), we support using the same URL for web and API clients.
-        if "override_api_url_scheme" in view_flags and "Authorization" in request.headers:
-            # This request uses standard API based authentication.
-            # For override_api_url_scheme views, we skip our normal
-            # rate limiting, because there are good reasons clients
-            # might need to (e.g.) request a large number of uploaded
-            # files or avatars in quick succession.
-            target_function = authenticated_rest_api_view(skip_rate_limiting=True)(target_function)
-        elif "override_api_url_scheme" in view_flags and request.GET.get("api_key") is not None:
-            # This request uses legacy API authentication.  We
-            # unfortunately need that in the React Native mobile apps,
-            # because there's no way to set the Authorization header in
-            # React Native.  See last block for rate limiting notes.
-            target_function = authenticated_uploads_api_view(skip_rate_limiting=True)(
-                target_function
-            )
-        # /json views (web client) validate with a session token (cookie)
-        elif not request.path.startswith("/api") and request.user.is_authenticated:
-            # Authenticated via sessions framework, only CSRF check needed
-            auth_kwargs = {}
-            if "override_api_url_scheme" in view_flags:
-                auth_kwargs["skip_rate_limiting"] = True
-            target_function = csrf_protect(authenticated_json_view(target_function, **auth_kwargs))
-
-        # most clients (mobile, bots, etc) use HTTP basic auth and REST calls, where instead of
-        # username:password, we use email:apiKey
-        elif "Authorization" in request.headers:
-            # Wrap function with decorator to authenticate the user before
-            # proceeding
-            target_function = authenticated_rest_api_view(
-                allow_webhook_access="allow_incoming_webhooks" in view_flags,
-            )(target_function)
-        elif (
-            request.path.startswith(("/json", "/avatar", "/user_uploads", "/thumbnail"))
-            and "allow_anonymous_user_web" in view_flags
-        ):
-            # For endpoints that support anonymous web access, we do that.
-            # TODO: Allow /api calls when this is stable enough.
-            target_function = csrf_protect(public_json_view(target_function))
-        else:
-            # Otherwise, throw an authentication error; our middleware
-            # will generate the appropriate HTTP response.
-            raise MissingAuthenticationError()
-
-        if request.method in ["DELETE", "PATCH", "PUT"]:
-            # process_as_post needs to be the outer decorator, because
-            # otherwise we might access and thus cache a value for
-            # request.POST.
-            target_function = process_as_post(target_function)
-
-        return target_function(request, **kwargs)
-
-    return json_method_not_allowed(list(supported_methods.keys()))
+    return target_function(request, **kwargs)
 
 
 def rest_path(
