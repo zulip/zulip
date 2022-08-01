a894dd9c7d585f37665a770e4f87f2f95831131b
Zixuan James Li <p359101898@gmail.com>
2022-08-01 11:16:53 -0400

    models: Duplicate __str__ in subclasses of AbstractUserMessage.
    The `message` attirbute doesn't get defined in the class where it is used.
    The cleanest solution will be using the `Self` type introduced in PEP
    673 and type it as something like `ForeignKey[Union[Self, Combinable],
    Self]`.
    
    Signed-off-by: Zixuan James Li <p359101898@gmail.com>

--- zerver/models.py
+++ zerver/models.py
@@ -3252,10 +3252,6 @@ class AbstractUserMessage(models.Model):
             mask <<= 1
         return flags
 
-    def __str__(self) -> str:
-        display_recipient = get_display_recipient(self.message.recipient)
-        return f"<{self.__class__.__name__}: {display_recipient} / {self.user_profile.email} ({self.flags_list()})>"
-
 
 class UserMessage(AbstractUserMessage):
     message = models.ForeignKey(Message, on_delete=CASCADE)
@@ -3309,6 +3305,10 @@ class UserMessage(AbstractUserMessage):
             ),
         ]
 
+    def __str__(self) -> str:
+        display_recipient = get_display_recipient(self.message.recipient)
+        return f"<{self.__class__.__name__}: {display_recipient} / {self.user_profile.email} ({self.flags_list()})>"
+
 
 def get_usermessage_by_message_id(
     user_profile: UserProfile, message_id: int
@@ -3328,6 +3328,10 @@ class ArchivedUserMessage(AbstractUserMessage):
     """
 
     message = models.ForeignKey(ArchivedMessage, on_delete=CASCADE)
+
+    def __str__(self) -> str:
+        display_recipient = get_display_recipient(self.message.recipient)
+        return f"<{self.__class__.__name__}: {display_recipient} / {self.user_profile.email} ({self.flags_list()})>"
 
 
 class AbstractAttachment(models.Model):
