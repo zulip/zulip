89948b274530182ee6d7b1d5fa438c693fb99e61
Zixuan James Li <p359101898@gmail.com>
2022-08-09 22:50:01 -0400

    migrations: Remove noop migration operations.
    Since validators do not affect the database, this migration is a noop.
    Removing the migration fails check-database-compatibility. We might
    eventually delete it when Django supports a cleaner method for deletion.
    
    TODO:
    Remove the type annotation when django-stubs is integrated.
    
    Signed-off-by: Zixuan James Li <p359101898@gmail.com>

--- zerver/migrations/0043_realm_filter_validators.py
+++ zerver/migrations/0043_realm_filter_validators.py
@@ -1,7 +1,6 @@
-import django.core.validators
-from django.db import migrations, models
+from typing import List
 
-from zerver.models import filter_format_validator, filter_pattern_validator
+from django.db import migrations
 
 
 class Migration(migrations.Migration):
@@ -10,17 +9,4 @@ class Migration(migrations.Migration):
         ("zerver", "0042_attachment_file_name_length"),
     ]
 
-    operations = [
-        migrations.AlterField(
-            model_name="realmfilter",
-            name="pattern",
-            field=models.TextField(validators=[filter_pattern_validator]),
-        ),
-        migrations.AlterField(
-            model_name="realmfilter",
-            name="url_format_string",
-            field=models.TextField(
-                validators=[django.core.validators.URLValidator, filter_format_validator]
-            ),
-        ),
-    ]
+    operations: List[migrations.operations.base.Operation] = []
