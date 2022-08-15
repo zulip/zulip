4c3c97617461d01f57a965bc6ea39f5d7fd9fa02
Zixuan James Li <p359101898@gmail.com>
2022-08-15 13:10:58 -0400

    models: Implicitly type model fields with django-stubs.
    Previously, we type the model fields with explicit type annotations
    manually with the approximate types. This was because the lack of types
    for Django.
    
    django-stubs provides more specific types for all these fields that
    incompatible with our previous approximate annotations. So now we can
    remove the inline type annotations and rely on the types defined in the
    stubs. This allows mypy to infer the types of the model fields for us.
    
    Signed-off-by: Zixuan James Li <p359101898@gmail.com>

--- confirmation/models.py
+++ confirmation/models.py
@@ -161,12 +161,12 @@ def confirmation_url(
 
 class Confirmation(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=CASCADE)
-    object_id: int = models.PositiveIntegerField(db_index=True)
+    object_id = models.PositiveIntegerField(db_index=True)
     content_object = GenericForeignKey("content_type", "object_id")
-    date_sent: datetime.datetime = models.DateTimeField(db_index=True)
-    confirmation_key: str = models.CharField(max_length=40, db_index=True)
-    expiry_date: Optional[datetime.datetime] = models.DateTimeField(db_index=True, null=True)
-    realm: Optional[Realm] = models.ForeignKey(Realm, null=True, on_delete=CASCADE)
+    date_sent = models.DateTimeField(db_index=True)
+    confirmation_key = models.CharField(max_length=40, db_index=True)
+    expiry_date = models.DateTimeField(db_index=True, null=True)
+    realm = models.ForeignKey(Realm, null=True, on_delete=CASCADE)
 
     # The following list is the set of valid types
     USER_REGISTRATION = 1
@@ -177,7 +177,7 @@ class Confirmation(models.Model):
     MULTIUSE_INVITE = 6
     REALM_CREATION = 7
     REALM_REACTIVATION = 8
-    type: int = models.PositiveSmallIntegerField()
+    type = models.PositiveSmallIntegerField()
 
     def __str__(self) -> str:
         return f"<Confirmation: {self.content_object}>"
@@ -264,7 +264,7 @@ class RealmCreationKey(models.Model):
 
     # True just if we should presume the email address the user enters
     # is theirs, and skip sending mail to it to confirm that.
-    presume_email_valid: bool = models.BooleanField(default=False)
+    presume_email_valid = models.BooleanField(default=False)
 
     class Invalid(Exception):
         pass
