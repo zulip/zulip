8da5368529b1ede62a2803983612d98bc739bd7b
Aman Agrawal <amanagr@zulip.com>
2022-08-16 11:22:16 +0000

    url: Move /self-hosting pages to the corporate folder.

--- corporate/urls.py
+++ corporate/urls.py
@@ -69,7 +69,7 @@ landing_page_urls = [
     path("team/", team_view),
     path("history/", landing_view, {"template_name": "corporate/history.html"}),
     path("why-zulip/", landing_view, {"template_name": "corporate/why-zulip.html"}),
-    path("self-hosting/", landing_view, {"template_name": "zerver/self-hosting.html"}),
+    path("self-hosting/", landing_view, {"template_name": "corporate/self-hosting.html"}),
     path("security/", landing_view, {"template_name": "zerver/security.html"}),
     # /for pages
     path("use-cases/", landing_view, {"template_name": "corporate/for/use-cases.html"}),
