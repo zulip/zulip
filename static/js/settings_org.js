zulip/zulip

commit 44266987ec1093fbaa0d1bea2e6ebb00b1e77a51

settings: Disable the save button if custom time limit input is invalid.
We disable the "Save changes" button if the custom time input for
edit and delete time limit is 0, -ve values or empty.

static/js/settings_org.js
@@ -633,6 +633,9 @@ export function change_save_button_state($element, state) {
     }
 
     if (state === "discarded") {
+        if ($saveBtn.prop("disabled")) {
+            $saveBtn.prop("disabled", false);
+        }
         show_hide_element($element, false, 0);
         return;
     }
@@ -1156,6 +1159,15 @@ export function build_page() {
             "id_realm_message_content_edit_limit_minutes",
             msg_edit_limit_dropdown_value === "custom_limit",
         );
+        if (msg_edit_limit_dropdown_value === "custom_limit") {
+            const val = get_property_value("realm_message_content_edit_limit_minutes");
+            if (val === null) {
+                $("#id_realm_message_content_edit_limit_minutes")
+                    .closest(".org-subsection-parent")
+                    .find(".subsection-changes-save button")
+                    .prop("disabled", true);
+            }
+        }
     });
 
     $("#id_realm_msg_delete_limit_setting").on("change", (e) => {
@@ -1164,6 +1176,16 @@ export function build_page() {
             "id_realm_message_content_delete_limit_minutes",
             msg_delete_limit_dropdown_value === "custom_limit",
         );
+
+        if (msg_delete_limit_dropdown_value === "custom_limit") {
+            const val = get_property_value("realm_message_content_delete_limit_minutes");
+            if (val === null) {
+                $("#id_realm_message_content_delete_limit_minutes")
+                    .closest(".org-subsection-parent")
+                    .find(".subsection-changes-save button")
+                    .prop("disabled", true);
+            }
+        }
     });
 
     $("#id_realm_message_retention_setting").on("change", (e) => {
@@ -1229,6 +1251,27 @@ export function build_page() {
         // no allowed domains otherwise it gets closed due to
         // the click event handler attached to `#settings_overlay_container`
         e.stopPropagation();
+    });
+
+    $("#org-msg-editing").on("input", ".admin-realm-time-limit-input", (e) => {
+        const edit_limit_value = Number.parseInt(
+            $("#id_realm_message_content_edit_limit_minutes").val(),
+            10,
+        );
+        const delete_limit_value = Number.parseInt(
+            $("#id_realm_message_content_delete_limit_minutes").val(),
+            10,
+        );
+
+        const disable_save_btn =
+            edit_limit_value <= 0 ||
+            delete_limit_value <= 0 ||
+            Number.isNaN(edit_limit_value) ||
+            Number.isNaN(delete_limit_value);
+        $(e.target)
+            .closest(".org-subsection-parent")
+            .find(".subsection-changes-save button")
+            .prop("disabled", disable_save_btn);
     });
 
     $("#show_realm_domains_modal").on("click", (e) => {

commit c3191ad9d5bae1e9f692e103b3a09e3d924af5fd

settings: Hide save-discard widget correctly when setting is not changed.
This commit fixes the bug where save-discard was not hidden when message
edit or delete setting is first changed from "Anytime" to any other value
and then again to "Any time". The save-discard widget should be hidden
since the setting is value was "Any time" already.

The bug was because check_property_changed returned true for
"realm_message_content_edit_limit_minutes" and
"realm_message_content_delete_limit_minutes" for above case as value of
setting in page_params for "Any time" case is "null" and
get_input_element_value returned "undefined" as the custom input box is
empty for "Any time" case.

This bug was introduced in #21837 where we changed the input box to be
empty for "Any time" case.

static/js/settings_org.js
@@ -794,6 +794,16 @@ function check_property_changed(elem, for_realm_default_settings) {
             changed_val = get_email_notification_batching_setting_element_value();
             break;
         }
+        case "realm_message_content_edit_limit_minutes":
+        case "realm_message_content_delete_limit_minutes": {
+            const input_val = Number.parseInt($(`#id_${CSS.escape(property_name)}`).val(), 10);
+            if (Number.isNaN(input_val)) {
+                changed_val = null;
+                break;
+            }
+            changed_val = input_val.toString();
+            break;
+        }
         case "realm_default_language":
             changed_val = $(
                 "#org-notifications .language_selection_widget .language_selection_button span",