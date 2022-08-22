zulip/zulip

commit 44266987ec1093fbaa0d1bea2e6ebb00b1e77a51

settings: Disable the save button if custom time limit input is invalid.
We disable the "Save changes" button if the custom time input for
edit and delete time limit is 0, -ve values or empty.

static/js/settings_org.js
@@ -633,6 +633,9 @@ export function change_save_button_state($element, state) {
     }
 
     if (state === "discarded") {
+        if ($saveBtn.prop("disabled")) {
+            $saveBtn.prop("disabled", false);
+        }
         show_hide_element($element, false, 0);
         return;
     }
@@ -1156,6 +1159,15 @@ export function build_page() {
             "id_realm_message_content_edit_limit_minutes",
             msg_edit_limit_dropdown_value === "custom_limit",
         );
+        if (msg_edit_limit_dropdown_value === "custom_limit") {
+            const val = get_property_value("realm_message_content_edit_limit_minutes");
+            if (val === null) {
+                $("#id_realm_message_content_edit_limit_minutes")
+                    .closest(".org-subsection-parent")
+                    .find(".subsection-changes-save button")
+                    .prop("disabled", true);
+            }
+        }
     });
 
     $("#id_realm_msg_delete_limit_setting").on("change", (e) => {
@@ -1164,6 +1176,16 @@ export function build_page() {
             "id_realm_message_content_delete_limit_minutes",
             msg_delete_limit_dropdown_value === "custom_limit",
         );
+
+        if (msg_delete_limit_dropdown_value === "custom_limit") {
+            const val = get_property_value("realm_message_content_delete_limit_minutes");
+            if (val === null) {
+                $("#id_realm_message_content_delete_limit_minutes")
+                    .closest(".org-subsection-parent")
+                    .find(".subsection-changes-save button")
+                    .prop("disabled", true);
+            }
+        }
     });
 
     $("#id_realm_message_retention_setting").on("change", (e) => {
@@ -1229,6 +1251,27 @@ export function build_page() {
         // no allowed domains otherwise it gets closed due to
         // the click event handler attached to `#settings_overlay_container`
         e.stopPropagation();
+    });
+
+    $("#org-msg-editing").on("input", ".admin-realm-time-limit-input", (e) => {
+        const edit_limit_value = Number.parseInt(
+            $("#id_realm_message_content_edit_limit_minutes").val(),
+            10,
+        );
+        const delete_limit_value = Number.parseInt(
+            $("#id_realm_message_content_delete_limit_minutes").val(),
+            10,
+        );
+
+        const disable_save_btn =
+            edit_limit_value <= 0 ||
+            delete_limit_value <= 0 ||
+            Number.isNaN(edit_limit_value) ||
+            Number.isNaN(delete_limit_value);
+        $(e.target)
+            .closest(".org-subsection-parent")
+            .find(".subsection-changes-save button")
+            .prop("disabled", disable_save_btn);
     });
 
     $("#show_realm_domains_modal").on("click", (e) => {

commit bcd7583a13390963c9c94a7ff1dcc62332f26be8

settings: Fix save button being disabled even for valid values.
On changing either one of message edit or delete limit setting
from "Any time" to "Custom", the "Save changes" button is not
enabled even after entering valid input.

We can see this bug if both the edit or delete limit setting is
set to "Anytime" initially and one of the setting is changed to
"Custom".

This is because the custom input for "Any time" case is empty even
though it is hidden and the check for disabling the button was
not checking whether the input is hidden or not.

This commit changes the code to consider the value in custom input
box only if the input is visible, i.e. the dropdown value is set to
"Custom".

This bug was introduced in #21837 where we added the functionality
to disable the save button.

static/js/settings_org.js
@@ -1258,11 +1258,19 @@ export function build_page() {
             10,
         );
 
+        const msg_edit_limit_dropdown_value = $("#id_realm_msg_edit_limit_setting").val();
+        const edit_limit_custom_value_invalid =
+            msg_edit_limit_dropdown_value === "custom_limit" &&
+            (edit_limit_value <= 0 || Number.isNaN(edit_limit_value));
+
+        const msg_delete_limit_dropdown_value = $("#id_realm_msg_delete_limit_setting").val();
+        const delete_limit_custom_value_invalid =
+            msg_delete_limit_dropdown_value === "custom_limit" &&
+            (delete_limit_value <= 0 || Number.isNaN(delete_limit_value));
+
         const disable_save_btn =
-            edit_limit_value <= 0 ||
-            delete_limit_value <= 0 ||
-            Number.isNaN(edit_limit_value) ||
-            Number.isNaN(delete_limit_value);
+            edit_limit_custom_value_invalid || delete_limit_custom_value_invalid;
+
         $(e.target)
             .closest(".org-subsection-parent")
             .find(".subsection-changes-save button")