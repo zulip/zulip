7666ff603d5dae0deb4847684c37d8e454563a9b
Anders Kaseorg <anders@zulip.com>
2022-08-31 15:46:08 -0700

    sharding: Configure Tornado sharding with nginx map.
    https://nginx.org/en/docs/http/ngx_http_map_module.html
    
    Since Puppet doesn’t manage the contents of nginx_sharding.conf after
    its initial creation, it needs to be renamed so we can give it
    different default contents.
    
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- puppet/zulip/manifests/tornado_sharding.pp
+++ puppet/zulip/manifests/tornado_sharding.pp
@@ -5,14 +5,21 @@ class zulip::tornado_sharding {
   # with the correct default content for the "only one shard" setup. For this
   # reason they use "replace => false", because the files are managed by
   # the sharding script afterwards and Puppet shouldn't overwrite them.
-  file { '/etc/zulip/nginx_sharding.conf':
+  file { '/etc/zulip/nginx_sharding_map.conf':
     ensure  => file,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
     notify  => Service['nginx'],
-    content => "set \$tornado_server http://tornado;\n",
+    content => @(EOT),
+      map "" $tornado_server {
+          default http://tornado;
+      }
+      | EOT
     replace => false,
+  }
+  file { '/etc/zulip/nginx_sharding.conf':
+    ensure => absent,
   }
   file { '/etc/zulip/sharding.json':
     ensure  => file,
@@ -29,7 +36,7 @@ class zulip::tornado_sharding {
   exec { 'stage_updated_sharding':
     command   => "${::zulip_scripts_path}/lib/sharding.py",
     onlyif    => "${::zulip_scripts_path}/lib/sharding.py --errors-ok",
-    require   => [File['/etc/zulip/nginx_sharding.conf'], File['/etc/zulip/sharding.json']],
+    require   => [File['/etc/zulip/nginx_sharding_map.conf'], File['/etc/zulip/sharding.json']],
     logoutput => true,
     loglevel  => warning,
   }
