5bdd419f50518733f456a2e2618e473de7371d4c
Aman Agrawal <amanagr@zulip.com>
2022-09-01 18:23:28 +0000

    recent_topics: Add button to filter streams.

--- static/js/tippyjs.js
+++ static/js/tippyjs.js
@@ -366,4 +366,9 @@ export function initialize() {
         // Avoid inheriting `position: relative` CSS on the stream sorter widget.
         appendTo: () => document.body,
     });
+
+    delegate("body", {
+        target: "#rt_select_stream_widget .fa-times",
+        content: $t({defaultMessage: "Clear stream filter"}),
+    });
 }
