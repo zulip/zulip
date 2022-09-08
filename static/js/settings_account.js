9290a2f27085ead6c3979666a8918d8ade3dc7ba
AtmegaBuzz <swapnilshinde9382@gmail.com>
2022-09-08 22:54:12 +0530

    close profile preview go back to settings

--- static/js/settings_account.js
+++ static/js/settings_account.js
@@ -719,8 +719,7 @@ export function set_up() {
     });
 
     $("#show_my_user_profile_modal").on("click", (e) => {
-        e.stopPropagation();
-        e.preventDefault();
+        overlays.close_overlay("settings");
 
         const user = people.get_by_user_id(people.my_current_user_id());
         user_profile.show_user_profile(user);
