aeb80fb1abbd90b339f3bed9a8627f06de0bb0db
Dima Surkin <dmitry.surkin@gmail.com>
2022-09-11 17:47:28 +0500

    pupeeteer: lint recent-topics

--- frontend_tests/puppeteer_tests/recent-topics.ts
+++ frontend_tests/puppeteer_tests/recent-topics.ts
@@ -15,11 +15,14 @@ async function mute_topic_test(page: Page): Promise<void> {
     await common.log_in(page);
     await page.click(".top_left_recent_topics");
     await page.waitForSelector('#recent_topics_table tr[id^="recent_topic"]', {visible: true});
-    const topics_quantitiy = await page.evaluate(() => $('#recent_topics_table tr[id^="recent_topic"]').length);
+    const topics_quantitiy = await page.evaluate(
+        () => $('#recent_topics_table tr[id^="recent_topic"]').length,
+    );
     const last_topic_id = await click_mute_and_return_last_topic_id(page);
 
     await page.waitForFunction(
-        (expected_length: number) => $('#recent_topics_table tr[id^="recent_topic"]').length === expected_length,
+        (expected_length: number) =>
+            $('#recent_topics_table tr[id^="recent_topic"]').length === expected_length,
         {},
         topics_quantitiy - 1,
     );
