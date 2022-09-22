83bd70956225495152646cef90a5f64dadbb7996
Anders Kaseorg <anders@zulip.com>
2022-09-22 12:36:55 -0700

    Revert "zulip-puppet-apply: Work around broken Puppet on Ubuntu 22.04."
    This reverts commit 25c87cc7da800f6d314f34708cf6cf4720fcdd68 (#21328).
    
    This upstream Ubuntu bug was fixed.
    
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- scripts/lib/ruby3hack.rb
+++
@@ -1,7 +0,0 @@
-# Work around https://bugs.launchpad.net/ubuntu/+source/puppet/+bug/1969939.
-
-require 'fileutils'
-
-def FileUtils.symlink(src, dest, options = {}, **kwargs)
-  FileUtils.ln_s(src, dest, **options, **kwargs)
-end
