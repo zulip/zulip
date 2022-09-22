5771e7fb0c7e93afe2c0e9ba648683f729aa6176
Anders Kaseorg <anders@zulip.com>
2022-09-22 13:09:34 -0700

    tornado: Support sharding by user ID.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- zerver/tornado/descriptors.py
+++ zerver/tornado/descriptors.py
@@ -1,5 +1,7 @@
 from typing import TYPE_CHECKING, Dict, Optional
 
+from django.conf import settings
+
 if TYPE_CHECKING:
     from zerver.tornado.event_queue import ClientDescriptor
 
@@ -16,3 +18,15 @@ def set_descriptor_by_handler_id(handler_id: int, client_descriptor: "ClientDesc
 
 def clear_descriptor_by_handler_id(handler_id: int) -> None:
     del descriptors_by_handler_id[handler_id]
+
+
+current_port: Optional[int] = None
+
+
+def is_current_port(port: int) -> Optional[int]:
+    return settings.TEST_SUITE or current_port == port
+
+
+def set_current_port(port: int) -> None:
+    global current_port
+    current_port = port
