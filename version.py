8b43a50a78f5a3e697723eb4f3ee4bd7da2876af
Zixuan James Li <p359101898@gmail.com>
2022-10-02 16:27:33 -0400

    requirements: Add uri-template.
    This is later used for supporting RFC 6570 URI Template in place of the
    URL format string that we were using for linkifiers.
    
    Signed-off-by: Zixuan James Li <p359101898@gmail.com>

--- version.py
+++ version.py
@@ -48,4 +48,4 @@ API_FEATURE_LEVEL = 175
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (233, 0)
+PROVISION_VERSION = (233, 1)
