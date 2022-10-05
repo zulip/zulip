fcfebe2b75baf906047d2b4165956236f0f53cca
Alya Abbott <alya@zulip.com>
2022-10-05 11:05:19 -0700

    portico: Update /history page.

--- zerver/tests/test_docs.py
+++ zerver/tests/test_docs.py
@@ -172,10 +172,9 @@ class DocPageTest(ZulipTestCase):
             self._test("/team/", "industry veterans")
             self._test("/apps/", "Apps for every platform.")
 
-        self._test("/history/", "Cambridge, Massachusetts")
+        self._test("/history/", "Zulip released as open source!")
         # Test the i18n version of one of these pages.
-        self._test("/en/history/", "Cambridge, Massachusetts")
-
+        self._test("/en/history/", "Zulip released as open source!")
         self._test("/hello/", "Chat for distributed teams", landing_missing_strings=["Log in"])
         self._test("/attribution/", "Website attributions")
         self._test("/communities/", "Open communities directory")
