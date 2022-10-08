cbfc273b3cde2f09d752b80ed5aed3b1cbdc0949
Hari Prashant Bhimaraju <haripb01@gmail.com>
2022-10-08 22:57:08 +0530

    ifttt: Strengthen types using WildValue.

--- zerver/webhooks/ifttt/view.py
+++ zerver/webhooks/ifttt/view.py
@@ -1,5 +1,3 @@
-from typing import Any, Dict
-
 from django.http import HttpRequest, HttpResponse
 from django.utils.translation import gettext as _
 
@@ -7,6 +5,7 @@ from zerver.decorator import webhook_view
 from zerver.lib.exceptions import JsonableError
 from zerver.lib.request import REQ, has_request_variables
 from zerver.lib.response import json_success
+from zerver.lib.validator import WildValue, check_none_or, check_string, to_wild_value
 from zerver.lib.webhooks.common import check_send_webhook_message
 from zerver.models import UserProfile
 
@@ -16,24 +15,18 @@ from zerver.models import UserProfile
 def api_iftt_app_webhook(
     request: HttpRequest,
     user_profile: UserProfile,
-    payload: Dict[str, Any] = REQ(argument_type="body"),
+    payload: WildValue = REQ(argument_type="body", converter=to_wild_value),
 ) -> HttpResponse:
-    topic = payload.get("topic")
-    content = payload.get("content")
+    topic = payload.get("topic").tame(check_none_or(check_string))
+    content = payload.get("content").tame(check_none_or(check_string))
 
     if topic is None:
-        topic = payload.get("subject")  # Backwards-compatibility
+        topic = payload.get("subject").tame(check_none_or(check_string))  # Backwards-compatibility
         if topic is None:
             raise JsonableError(_("Topic can't be empty"))
 
     if content is None:
         raise JsonableError(_("Content can't be empty"))
-
-    if not isinstance(topic, str):
-        raise JsonableError(_("Topic must be a string"))
-
-    if not isinstance(content, str):
-        raise JsonableError(_("Content must be a string"))
 
     check_send_webhook_message(request, user_profile, topic, content)
     return json_success(request)
