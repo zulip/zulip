e27eb0da97c4982eb413839e85840f16898ad995
Palash Baderia <palash.baderia@outlook.com>
2022-10-11 17:50:27 +0530

    user_info_popover: Add close button to close the popover.
    This commit adds a close button to all the user-info-popovers:
    * user-info-popover
    * message-info-popover
    * user_popover
    With this new change, now if we hover over the image, there is
    an overlay effect. because of which the image gets dim and the
    close button appears, on clicking the close button, the popover
    closes.

--- web/src/popovers.js
+++ web/src/popovers.js
@@ -807,6 +807,11 @@ export function register_click_handlers() {
         e.stopPropagation();
     });
 
+    $("body").on("click", ".exit_user_info_popover", (e) => {
+        hide_all_user_info_popovers();
+        e.stopPropagation();
+    });
+
     $("body").on("click", ".info_popover_actions .narrow_to_private_messages", (e) => {
         const user_id = elem_to_user_id($(e.target).parents("ul"));
         const email = people.get_by_user_id(user_id).email;
