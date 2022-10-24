95aa9a4f83e76b1242bfcb7e34aec8b955dc4a78
Lauryn Menard <lauryn@zulip.com>
2022-10-24 20:35:19 +0200

    compose-box: Remove reset max-height calculation for preview click.
    Removes call to reset_compose_message_max_height when clicking on
    the markdown preview button, which due to the `#compose` div element
    momentarily shrinking to be empty, caused the calculation of the
    max-height to grow larger on each click.
    
    Also refactors reset_compose_message_max_height to use the height
    from `getBoundingClientRect`, which defaults to zero when empty.
    
    And fixes a small discrepancy in how max-height is applied to
    a div element vs a textarea element, so that the visible height
    doesn't change between the preview and write modes in the compose
    box.
    
    Fixes #23277.

--- frontend_tests/node_tests/compose.js
+++ frontend_tests/node_tests/compose.js
@@ -758,11 +758,6 @@ test_ui("on_events", ({override, override_rewire}) => {
     })();
 
     (function test_markdown_preview_compose_clicked() {
-        let reset_compose_message_max_height_called = false;
-        override(resize, "reset_compose_message_max_height", () => {
-            reset_compose_message_max_height_called = true;
-        });
-
         // Tests setup
         function setup_visibilities() {
             $("#compose-textarea").show();
@@ -847,7 +842,6 @@ test_ui("on_events", ({override, override_rewire}) => {
 
         assert.equal($("#compose .preview_content").html(), "translated HTML: Nothing to preview");
         assert_visibilities();
-        assert.ok(reset_compose_message_max_height_called);
 
         let make_indicator_called = false;
         $("#compose-textarea").val("```foobarfoobar```");
