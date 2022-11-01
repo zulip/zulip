4c575ce377378b7bc20185e1b647e1d8dad571d3
Mateusz Mandera <mateusz.mandera@zulip.com>
2022-11-01 10:00:38 +0100

    delete_topic: Use the same timeout pattern as /mark_all_as_read.
    We don't want to use the nginx 502 timeout as an API pattern. In
    /mark_all_as_read we've already figured out how we want to handle this
    instead.

--- zerver/lib/test_helpers.py
+++ zerver/lib/test_helpers.py
@@ -717,3 +717,15 @@ def mock_queue_publish(
 
     with mock.patch(method_to_patch, side_effect=verify_serialize):
         yield inner
+
+
+@contextmanager
+def timeout_mock(mock_path: str) -> Iterator[None]:
+    # timeout() doesn't work in test environment with database operations
+    # and they don't get committed - so we need to replace it with a mock
+    # that just calls the function.
+    def mock_timeout(seconds: int, func: Callable[[], object]) -> object:
+        return func()
+
+    with mock.patch(f"{mock_path}.timeout", new=mock_timeout):
+        yield
