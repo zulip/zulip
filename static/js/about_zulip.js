f24ce315d68d5e7ec52bcfc6e9c375c658dbe48e
Javier González <jjgmancha@outlook.com>
2022-11-02 12:54:34 -0600

    popovers: Add a Copied! tooltip when copy button is clicked.
    `Version` and `Forked from` copy buttons now show a `Copied!` tooltip when clicked
    It implements a similar function used on `Saved as draft` notice
    
    Fixes: #23210

--- static/js/about_zulip.js
+++ static/js/about_zulip.js
@@ -1,9 +1,11 @@
 import ClipboardJS from "clipboard";
 import $ from "jquery";
+import tippy from "tippy.js";
 
 import render_about_zulip from "../templates/about_zulip.hbs";
 
 import * as browser_history from "./browser_history";
+import {$t} from "./i18n";
 import * as overlays from "./overlays";
 import {page_params} from "./page_params";
 
@@ -28,4 +30,25 @@ export function initialize() {
             page_params.zulip_merge_base !== page_params.zulip_version,
     });
     $(".app").append(rendered_about_zulip);
+
+    $("#about-zulip i.tippy-zulip-tooltip").on("click", (event) => {
+        show_tooltip($(event.target));
+    });
+}
+
+export function show_tooltip(element) {
+    // Display a tooltip to notify the user the version was copied.
+    const instance = tippy("#about-zulip i.tippy-zulip-tooltip", {
+        onUntrigger() {
+            remove_instance();
+        },
+    }).find((tippy) => tippy.id === element[0]._tippy.id);
+    instance.setContent($t({defaultMessage: "Copied!"}));
+    instance.show();
+    function remove_instance() {
+        if (!instance.state.isDestroyed) {
+            instance.destroy();
+        }
+    }
+    setTimeout(remove_instance, 3000);
 }
