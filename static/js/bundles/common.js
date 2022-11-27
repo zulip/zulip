27b5673f0c851a728e98e486c73bb9d9ec8a7d83
cledi01 <caleb.gorrila@gmail.com>
2022-11-26 21:04:39 -0500

    re-factoring progress bars from bootstrap

--- static/js/bundles/common.js
+++ static/js/bundles/common.js
@@ -13,5 +13,6 @@ import "source-sans/source-sans-3.css";
 import "source-code-pro/source-code-pro.css";
 import "../../styles/alerts.css";
 import "../../styles/pygments.css";
+import "../../styles/progress.css"
 import "@uppy/core/dist/style.css";
 import "@uppy/progress-bar/dist/style.css";
