b25e8d6465ff38bb14fc73f05079d52a5bf73f53
cledi01 <caleb.gorrila@gmail.com>
2022-11-26 22:08:01 -0500

    Rename css file to be more descriptive, and fix indentation size

--- static/js/bundles/common.js
+++ static/js/bundles/common.js
@@ -13,6 +13,6 @@ import "source-sans/source-sans-3.css";
 import "source-code-pro/source-code-pro.css";
 import "../../styles/alerts.css";
 import "../../styles/pygments.css";
-import "../../styles/progress.css"
+import "../../styles/progress_bar.css"
 import "@uppy/core/dist/style.css";
 import "@uppy/progress-bar/dist/style.css";
