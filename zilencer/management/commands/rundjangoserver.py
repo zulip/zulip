ba78bee8c473dd581006f8aa7f23582229e996b0
Anders Kaseorg <anders@zulip.com>
2022-12-25 15:35:56 -0800

    ruff: Fix DTZ005 `datetime.datetime.now()` without `tz` argument.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- zilencer/management/commands/rundjangoserver.py
+++ zilencer/management/commands/rundjangoserver.py
@@ -2,12 +2,13 @@ from datetime import datetime
 from functools import wraps
 from typing import Callable
 
+from dateutil.tz import tzlocal
 from django.core.management.commands.runserver import Command as DjangoCommand
 
 
 def output_styler(style_func: Callable[[str], str]) -> Callable[[str], str]:
     # Might fail to suppress the date line around midnight, but, whatever.
-    date_prefix = datetime.now().strftime("%B %d, %Y - ")
+    date_prefix = datetime.now(tzlocal()).strftime("%B %d, %Y - ")
 
     @wraps(style_func)
     def _wrapped_style_func(message: str) -> str:
