b2be1f007474ab5b9828fe65403c8e8468912e83
Sahil Batra <sahil@zulip.com>
2022-12-29 23:06:06 +0530

    stream_settings: Add live update code for can-remove-subscribers-group.
    This commit adds code to live update the UI element for
    can_remove_subscribers_group setting.

--- frontend_tests/node_tests/stream_events.js
+++ frontend_tests/node_tests/stream_events.js
@@ -243,6 +243,17 @@ test("update_property", ({override}) => {
         assert.equal(args.sub.stream_id, stream_id);
         assert.equal(args.val, 20);
     }
+
+    // Test stream can_remove_subscribers_group_id change event
+    {
+        const stub = make_stub();
+        override(stream_settings_ui, "update_can_remove_subscribers_group_id", stub.f);
+        stream_events.update_property(stream_id, "can_remove_subscribers_group_id", 3);
+        assert.equal(stub.num_calls, 1);
+        const args = stub.get_args("sub", "val");
+        assert.equal(args.sub.stream_id, stream_id);
+        assert.equal(args.val, 3);
+    }
 });
 
 test("marked_unsubscribed (code coverage)", () => {
