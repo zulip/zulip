5c14636021bba6766739462d81ca97aa4d4d392b
Sahil Batra <sahil@zulip.com>
2022-12-29 22:19:38 +0530

    stream_settings: Use can_remove_subscribers_group setting in webapp.
    This commit updates the frontend to show or hide the "Unsubscribe"
    button in subscribers list in stream settings as per the
    can_remove_subscribers_group setting for the stream.

--- static/js/stream_settings_data.js
+++ static/js/stream_settings_data.js
@@ -46,6 +46,7 @@ export function add_settings_fields(sub) {
     sub.can_change_stream_permissions = stream_data.can_change_permissions(sub);
     sub.can_access_subscribers = stream_data.can_view_subscribers(sub);
     sub.can_add_subscribers = stream_data.can_subscribe_others(sub);
+    sub.can_remove_subscribers = stream_data.can_unsubscribe_others(sub);
 
     sub.preview_url = hash_util.by_stream_url(sub.stream_id);
     sub.is_old_stream = sub.stream_weekly_traffic !== null;
