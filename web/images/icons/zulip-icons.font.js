2e36bddfd777e4669cefe548e812028f4c4d9d5b
Taiki-Choda <chotik013@gmail.com>
2023-01-22 01:06:29 +0900

    zulip_icons: Add icons for popover UI redesign.
    Icons will be used in redesigning the Profile and Settings popovers.

--- web/images/icons/zulip-icons.font.js
+++ web/images/icons/zulip-icons.font.js
@@ -6,6 +6,10 @@ module.exports = {
     files: [
         "./*.svg", // For web-only icons.
         "../../shared/icons/*.svg", // For icons to be shared with the mobile app.
+
+        "../../shared/icons/feather/*.svg",
+        "../../shared/icons/google/*.svg",
+        "../../shared/icons/fontawesome/*.svg",
     ],
     fontName: "zulip-icons",
     classPrefix: "zulip-icon-",
