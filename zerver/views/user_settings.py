c40b58e773df87e50861ae43f45c27b7ddf35ec7
Josh Klar <josh@zulip.com>
2023-02-02 15:09:30 -0800

    user_settings: Only rate limit PATCH /json/settings if appropriate.

--- zerver/views/user_settings.py
+++ zerver/views/user_settings.py
@@ -36,7 +36,7 @@ from zerver.lib.email_validation import (
 )
 from zerver.lib.exceptions import JsonableError, RateLimitedError, UserDeactivatedError
 from zerver.lib.i18n import get_available_language_codes
-from zerver.lib.rate_limiter import RateLimitedUser
+from zerver.lib.rate_limiter import RateLimitedUser, should_rate_limit
 from zerver.lib.request import REQ, has_request_variables
 from zerver.lib.response import json_success
 from zerver.lib.send_email import FromAddress, send_email
@@ -304,11 +304,12 @@ def json_change_settings(
             except ValidationError as e:
                 raise JsonableError(e.message)
 
-            ratelimited, time_until_free = RateLimitedUser(
-                user_profile, domain="email_change_by_user"
-            ).rate_limit()
-            if ratelimited:
-                raise RateLimitedError(time_until_free)
+            if should_rate_limit(request):
+                ratelimited, time_until_free = RateLimitedUser(
+                    user_profile, domain="email_change_by_user"
+                ).rate_limit()
+                if ratelimited:
+                    raise RateLimitedError(time_until_free)
 
             do_start_email_change_process(user_profile, new_email)
 
