2df2ef9f0f5389f5eb138ae04bc538201b90d308
Prakhar Pratyush <prakhar841301@gmail.com>
2023-02-03 17:27:43 +0530

    user_topics: Refactor 'do_mute_topic'.
    This commit is a step in the direction of having a common
    function to handle visibility_policy changes and event
    generation instead of separate functions for each
    visibility policy.
    
    In order to support different types of topic visibility policies,
    this renames 'do_topic_mute' to 'do_set_user_topic_visibility_policy'
    and refactors it to accept a parameter 'visibility_policy'.

--- zerver/actions/user_topics.py
+++ zerver/actions/user_topics.py
@@ -11,42 +11,6 @@ from zerver.models import Stream, UserProfile, UserTopic
 from zerver.tornado.django_api import send_event
 
 
-def do_mute_topic(
-    user_profile: UserProfile,
-    stream: Stream,
-    topic: str,
-    date_muted: Optional[datetime.datetime] = None,
-    ignore_duplicate: bool = False,
-) -> None:
-    if date_muted is None:
-        date_muted = timezone_now()
-    assert stream.recipient_id is not None
-    add_topic_mute(
-        user_profile,
-        stream.id,
-        stream.recipient_id,
-        topic,
-        date_muted,
-        ignore_duplicate=ignore_duplicate,
-    )
-
-    # This first muted_topics event is deprecated and will be removed
-    # once clients are migrated to handle the user_topic event type
-    # instead.
-    muted_topics_event = dict(type="muted_topics", muted_topics=get_topic_mutes(user_profile))
-    send_event(user_profile.realm, muted_topics_event, [user_profile.id])
-
-    user_topic_event: Dict[str, Any] = {
-        "type": "user_topic",
-        "stream_id": stream.id,
-        "topic_name": topic,
-        "last_updated": datetime_to_timestamp(date_muted),
-        "visibility_policy": UserTopic.MUTED,
-    }
-
-    send_event(user_profile.realm, user_topic_event, [user_profile.id])
-
-
 def do_unmute_topic(
     user_profile: UserProfile, stream: Stream, topic: str, *, skip_muted_topics_event: bool = False
 ) -> None:
@@ -70,6 +34,45 @@ def do_unmute_topic(
         "topic_name": topic,
         "last_updated": datetime_to_timestamp(date_unmuted),
         "visibility_policy": UserTopic.VISIBILITY_POLICY_INHERIT,
+    }
+
+    send_event(user_profile.realm, user_topic_event, [user_profile.id])
+
+
+def do_set_user_topic_visibility_policy(
+    user_profile: UserProfile,
+    stream: Stream,
+    topic: str,
+    *,
+    visibility_policy: int,
+    last_updated: Optional[datetime.datetime] = None,
+    ignore_duplicate: bool = False,
+) -> None:
+    if last_updated is None:
+        last_updated = timezone_now()
+
+    assert stream.recipient_id is not None
+    add_topic_mute(
+        user_profile,
+        stream.id,
+        stream.recipient_id,
+        topic,
+        last_updated,
+        ignore_duplicate=ignore_duplicate,
+    )
+
+    # This first muted_topics event is deprecated and will be removed
+    # once clients are migrated to handle the user_topic event type
+    # instead.
+    muted_topics_event = dict(type="muted_topics", muted_topics=get_topic_mutes(user_profile))
+    send_event(user_profile.realm, muted_topics_event, [user_profile.id])
+
+    user_topic_event: Dict[str, Any] = {
+        "type": "user_topic",
+        "stream_id": stream.id,
+        "topic_name": topic,
+        "last_updated": datetime_to_timestamp(last_updated),
+        "visibility_policy": visibility_policy,
     }
 
     send_event(user_profile.realm, user_topic_event, [user_profile.id])
