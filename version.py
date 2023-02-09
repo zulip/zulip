9efbfa22f29eddb98564914c5929d87f6fa63ef7
Anders Kaseorg <anders@zulip.com>
2023-02-08 16:11:31 -0800

    dependencies: Upgrade JavaScript dependencies.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -48,4 +48,4 @@ API_FEATURE_LEVEL = 162
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (220, 0)
+PROVISION_VERSION = (221, 0)
