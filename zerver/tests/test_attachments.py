bd80c048be1b0196b369a49ac9e1d629beef064f
Alex Vandiver <alexmv@zulip.com>
2023-02-28 02:49:04 +0000

    upload: Rename delete_message_image to use word "attachment".
    The table is named Attachment, and not all of them are images.

--- zerver/tests/test_attachments.py
+++ zerver/tests/test_attachments.py
@@ -29,13 +29,15 @@ class AttachmentsTests(ZulipTestCase):
     def test_remove_attachment_exception(self) -> None:
         user_profile = self.example_user("cordelia")
         self.login_user(user_profile)
-        with mock.patch("zerver.lib.attachments.delete_message_image", side_effect=Exception()):
+        with mock.patch(
+            "zerver.lib.attachments.delete_message_attachment", side_effect=Exception()
+        ):
             result = self.client_delete(f"/json/attachments/{self.attachment.id}")
         self.assert_json_error(
             result, "An error occurred while deleting the attachment. Please try again later."
         )
 
-    @mock.patch("zerver.lib.attachments.delete_message_image")
+    @mock.patch("zerver.lib.attachments.delete_message_attachment")
     def test_remove_attachment(self, ignored: Any) -> None:
         user_profile = self.example_user("cordelia")
         self.login_user(user_profile)
