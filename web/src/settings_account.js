25fb810d815c77bf736053207bd899fa9f5db424
Lauryn Menard <lauryn@zulip.com>
2023-03-21 16:31:43 +0100

    account-settings: Pass password page params to dialog widget.
    When the password change form was converted to a dialog widget
    modal, in commit f5fbf5f0e07f55, the page_params values for
    password_min_length and password_min_guesses that were referenced
    in the handlebars template `dialog_change_password.hbs` had no
    value to set, which meant the password quality bar was no longer
    checking the input value against those organization settings.
    
    Passes the page_params values for password_min_length and
    password_min_guesses to the html parameter in dialog_widet.launch
    for the password change modal so that those values are available
    and set when the template is rendered.

--- web/src/settings_account.js
+++ web/src/settings_account.js
@@ -588,7 +588,10 @@ export function set_up() {
 
         dialog_widget.launch({
             html_heading: $t_html({defaultMessage: "Change password"}),
-            html_body: render_dialog_change_password(),
+            html_body: render_dialog_change_password({
+                password_min_length: page_params.password_min_length,
+                password_min_guesses: page_params.password_min_guesses,
+            }),
             html_submit_button: $t_html({defaultMessage: "Change"}),
             loading_spinner: true,
             id: "change_password_modal",
