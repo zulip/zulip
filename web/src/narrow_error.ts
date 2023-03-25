a2af1439dbdce1c66fc0593e26d28a6cd9e9e054
xoldyckk <xoldyckk@gmail.com>
2023-03-26 00:19:44 +0530

    ts:migrate `web/src/narrow_error.js` to typescript
    `web/src/narrow_error.js`:-
    
    Changed file extension from `.js` to `.ts`.

--- web/src/narrow_error.ts
+++ web/src/narrow_error.ts
@@ -1,10 +1,20 @@
 import render_empty_feed_notice from "../templates/empty_feed_notice.hbs";
 
-export function narrow_error(narrow_banner_data) {
-    const title = narrow_banner_data.title;
+type NarrowBannerData = {
+    html?: string;
+    search_data?: {
+        query_words: string[];
+        has_stop_word: boolean;
+    };
+    title: string;
+};
+
+export function narrow_error(narrow_banner_data: NarrowBannerData): string {
     const html = narrow_banner_data.html;
     const search_data = narrow_banner_data.search_data;
+    const title = narrow_banner_data.title;
 
     const empty_feed_notice = render_empty_feed_notice({title, html, search_data});
+
     return empty_feed_notice;
 }
