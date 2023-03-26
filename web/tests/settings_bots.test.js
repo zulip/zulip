670e443e69c35bf44ae94169891293264352cd95
Prabhav Kumar Singh <singhprabhav09@gmail.com>
2023-03-26 14:35:07 +0530

    added function clipboard for copy_email and copy_api_key

--- web/tests/settings_bots.test.js
+++ web/tests/settings_bots.test.js
@@ -20,6 +20,14 @@ const bot_data_params = {
 };
 
 function ClipboardJS(sel) {
+    assert.equal(sel, "#copy_email");
+}
+
+function ClipboardJS(sel) {
+    assert.equal(sel, "#copy_api_key");
+}
+
+function ClipboardJS(sel) {
     assert.equal(sel, "#copy_zuliprc");
 }
 mock_cjs("clipboard", ClipboardJS);
