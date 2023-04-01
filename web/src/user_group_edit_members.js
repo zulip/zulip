1e681d6a6303c8ef97404d6c0eb5666bfd5f4cae
m-e-l-u-h-a-n <purushottam.tiwari.cd.cse19@itbhu.ac.in>
2023-04-02 02:56:04 +0530

    user groups: Fix how we decide if a group is being edited.
    We earlier used URL for checking if a group was being
    edited in the right pane of #groups overlay. This is
    hacky in cases where the URL might be changed by the
    user by toggling between all-groups and your-groups.
    
    As the information about the group being edited is
    reliably maintianed by user_group_settings_ui module
    we use that while checking if the group is being edited.

--- web/src/user_group_edit_members.js
+++ web/src/user_group_edit_members.js
@@ -8,7 +8,6 @@ import * as add_subscribers_pill from "./add_subscribers_pill";
 import * as blueslip from "./blueslip";
 import * as channel from "./channel";
 import * as confirm_dialog from "./confirm_dialog";
-import * as hash_util from "./hash_util";
 import {$t, $t_html} from "./i18n";
 import * as ListWidget from "./list_widget";
 import {page_params} from "./page_params";
@@ -37,7 +36,7 @@ function get_potential_members() {
 }
 
 export function update_member_list_widget(group_id, member_ids) {
-    if (!hash_util.is_editing_group(group_id)) {
+    if (!user_group_edit.is_editing_group(group_id)) {
         return;
     }
     const users = people.get_users_from_ids(member_ids);
