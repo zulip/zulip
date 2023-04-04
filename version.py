0d4dcc858351405dd286e9bb156fd409121b968e
Anders Kaseorg <anders@zulip.com>
2023-04-03 23:43:20 -0700

    install-transifex-cli: Upgrade Transifex CLI from 1.6.5 to 1.6.7.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -48,4 +48,4 @@ API_FEATURE_LEVEL = 170
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (229, 0)
+PROVISION_VERSION = (229, 1)
