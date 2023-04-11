09f4b9516987fbd1fedff126805235e9c4dd3cf0
Anders Kaseorg <anders@zulip.com>
2023-04-11 16:15:43 -0700

    dependencies: Downgrade autosize to 5.0.2 to fix scrollbar regression.
    https://chat.zulip.org/#narrow/stream/9-issues/topic/compose.20box.20scrollbars/near/1531138
    
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -48,4 +48,4 @@ API_FEATURE_LEVEL = 172
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (231, 0)
+PROVISION_VERSION = (232, 0)
