d0fc3f1c2e41a50b214669df64e434aa9e7031d0
Alex Vandiver <alexmv@zulip.com>
2023-04-12 02:09:54 +0000

    puppet: Add prod hooks to push zulip-cloud-current and notify CZO.

--- puppet/zulip_ops/manifests/prod_app_frontend_once.pp
+++ puppet/zulip_ops/manifests/prod_app_frontend_once.pp
@@ -1,5 +1,7 @@
 class zulip_ops::prod_app_frontend_once {
   include zulip::app_frontend_once
+  include zulip::hooks::push_git_ref
+  include zulip::hooks::zulip_notify
 
   file { '/etc/cron.d/update-first-visible-message-id':
     ensure => file,
