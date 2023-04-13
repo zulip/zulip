f4995127d342284d1d86f61d257c8aae85d2e90e
xoldyckk <xoldyckk@gmail.com>
2023-04-13 15:36:04 +0530

    ts: Migrate google_analytics.js to typescript.
    Added custom type declaration files for `ga-gtag` npm module. Added
    missing types in `page_params.ts`.

--- web/src/portico/google-analytics.ts
+++ web/src/portico/google-analytics.ts
@@ -2,11 +2,15 @@ import {gtag, install} from "ga-gtag";
 
 import {page_params} from "../page_params";
 
-export let config;
+type Info = {
+    page_path: string;
+};
+
+export let config: (info: Info) => void;
 
 if (page_params.google_analytics_id !== undefined) {
     install(page_params.google_analytics_id);
     config = (info) => gtag("config", page_params.google_analytics_id, info);
 } else {
-    config = () => {};
+    config = () => undefined;
 }
