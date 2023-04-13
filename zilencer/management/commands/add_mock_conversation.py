55978b20b0e22dab920787147d68c0ddbb233bbe
Alya Abbott <alya@zulip.com>
2023-04-13 14:38:21 -0700

    portico: Update script for conversation screenshot on /features.

--- zilencer/management/commands/add_mock_conversation.py
+++ zilencer/management/commands/add_mock_conversation.py
@@ -23,8 +23,7 @@ After running the script:
 From browser (ideally on high resolution screen):
 * Refresh to get the rendered tweet
 * Check that the whale emoji reaction comes before the thumbs_up emoji reaction
-* Remove the blue box (it's a box shadow on .selected_message .messagebox-content;
-  inspecting the selected element will find it fairly quickly)
+* Remove the blue box (find the CSS for .selected_message, and uncheck it)
 * Change the color of the stream to #a6c7e5
 * Shrink screen till the mypy link only just fits
 * Take screenshot that does not include the timestamps or bottom edge
@@ -40,23 +39,24 @@ From image editing program:
 
     def add_message_formatting_conversation(self) -> None:
         realm = get_realm("zulip")
-        stream = ensure_stream(realm, "zulip features", acting_user=None)
+        stream = ensure_stream(realm, "general", acting_user=None)
 
-        UserProfile.objects.filter(email__contains="stage").delete()
-        starr = do_create_user(
-            "1@stage.example.com", "password", realm, "Ada Starr", acting_user=None
+        UserProfile.objects.filter(delivery_email__contains="stage").delete()
+        dal = do_create_user("1@stage.example.com", "password", realm, "Dal Kim", acting_user=None)
+        self.set_avatar(dal, "static/images/characters/Dal-Kim.jpg")
+        bo = do_create_user("2@stage.example.com", "password", realm, "Bo Lin", acting_user=None)
+        self.set_avatar(bo, "static/images/characters/Bo-Lin.jpg")
+        maxy = do_create_user(
+            "3@stage.example.com", "password", realm, "Maxy Stert", acting_user=None
         )
-        self.set_avatar(starr, "static/images/characters/starr.png")
-        fisher = do_create_user(
-            "2@stage.example.com", "password", realm, "Bel Fisher", acting_user=None
-        )
-        self.set_avatar(fisher, "static/images/characters/fisher.png")
+        self.set_avatar(maxy, "static/images/characters/Maxy-Stert.jpg")
         twitter_bot = do_create_user(
-            "3@stage.example.com",
+            "4@stage.example.com",
             "password",
             realm,
             "Twitter Bot",
             bot_type=UserProfile.DEFAULT_BOT,
+            bot_owner=dal,
             acting_user=None,
         )
         self.set_avatar(twitter_bot, "static/images/features/twitter.png")
@@ -67,32 +67,32 @@ From image editing program:
 
         staged_messages: List[Dict[str, Any]] = [
             {
-                "sender": starr,
-                "content": "Hey @**Bel Fisher**, check out Zulip's Markdown formatting! "
+                "sender": dal,
+                "content": "Hey @**Bo Lin**, check out Zulip's Markdown formatting! "
                 "You can have:\n* bulleted lists\n  * with sub-bullets too\n"
                 "* **bold**, *italic*, and ~~strikethrough~~ text\n"
                 "* LaTeX for mathematical formulas, both inline -- $$O(n^2)$$ -- and displayed:\n"
                 "```math\n\\int_a^b f(t)\\, dt=F(b)-F(a)\n```",
             },
             {
-                "sender": fisher,
+                "sender": bo,
                 "content": "My favorite is the syntax highlighting for code blocks\n"
                 "```python\ndef fib(n: int) -> int:\n    # returns the n-th Fibonacci number\n"
                 "    return fib(n-1) + fib(n-2)\n```",
             },
             {
-                "sender": starr,
-                "content": "I think you forgot your base case there, Bel :laughing:\n"
+                "sender": maxy,
+                "content": "I think you forgot your base case there, Bo :laughing:\n"
                 "```quote\n```python\ndef fib(n: int) -> int:\n    # returns the n-th Fibonacci number\n"
                 "    return fib(n-1) + fib(n-2)\n```\n```",
             },
             {
-                "sender": fisher,
+                "sender": maxy,
                 "content": "I'm also a big fan of inline link, tweet, video, and image previews. "
                 "Check out this picture of Çet Whalin[](/static/images/features/whale.png)!",
             },
             {
-                "sender": starr,
+                "sender": dal,
                 "content": "I just set up a custom linkifier, "
                 "so `#1234` becomes [#1234](github.com/zulip/zulip/1234), "
                 "a link to the corresponding GitHub issue.",
@@ -102,7 +102,7 @@ From image editing program:
                 "content": "https://twitter.com/gvanrossum/status/786661035637772288",
             },
             {
-                "sender": fisher,
+                "sender": dal,
                 "content": "Oops, the Twitter bot I set up shouldn't be posting here. Let me go fix that.",
             },
         ]
@@ -111,7 +111,7 @@ From image editing program:
             internal_prep_stream_message(
                 message["sender"],
                 stream,
-                "message formatting",
+                "Zulip message formatting",
                 message["content"],
             )
             for message in staged_messages
@@ -123,7 +123,7 @@ From image editing program:
             id__in=message_ids, content__icontains="image previews"
         )
         (emoji_code, reaction_type) = emoji_name_to_emoji_code(realm, "whale")
-        do_add_reaction(starr, preview_message, "whale", emoji_code, reaction_type)
+        do_add_reaction(maxy, preview_message, "whale", emoji_code, reaction_type)
 
         twitter_message = Message.objects.get(id__in=message_ids, content__icontains="gvanrossum")
         # Setting up a twitter integration in dev is a decent amount of work. If you need
@@ -143,7 +143,7 @@ From image editing program:
         # Put a short pause between the whale reaction and this, so that the
         # thumbs_up shows up second
         (emoji_code, reaction_type) = emoji_name_to_emoji_code(realm, "thumbs_up")
-        do_add_reaction(starr, preview_message, "thumbs_up", emoji_code, reaction_type)
+        do_add_reaction(dal, preview_message, "thumbs_up", emoji_code, reaction_type)
 
     def handle(self, *args: Any, **options: str) -> None:
         self.add_message_formatting_conversation()
