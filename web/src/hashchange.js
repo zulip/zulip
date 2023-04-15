915b6cd9e75442c1dbf07e77956f172a543d299c
Lalit <lalitkumarsingh3716@gmail.com>
2023-04-15 22:22:44 +0530

    js: Break cyclic dependency between `admin.js` and `settings.js`.
    There was a direct dependency between `admin.js` and `settings.js` which
    was due to the fact that we needed to call `build_page` of both modules
    in each other's `launch` function.
    
    This is solved by lifting those `build_page` calls up from both modules to
    `hashchange.js` which is the module which calls the `launch` function
    of both modules.

--- web/src/hashchange.js
+++ web/src/hashchange.js
@@ -343,11 +343,15 @@ function do_hashchange_overlay(old_hash) {
     }
 
     if (base === "settings") {
+        settings.build_page();
+        admin.build_page();
         settings.launch(section);
         return;
     }
 
     if (base === "organization") {
+        settings.build_page();
+        admin.build_page();
         admin.launch(section);
         return;
     }
