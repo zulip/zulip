72d56d5d5987e7d10b203af4c1b765b621a1b674
Mateusz Mandera <mateusz.mandera@zulip.com>
2023-04-16 22:32:39 +0200

    auth: Remove Realm.AUTHENTICATION_FLAGS class attribute.
    With the removal of the authentication_methods bitfield this is now
    useless and just duplicates AUTH_BACKEND_NAME_MAP keys.

--- zproject/backends.py
+++ zproject/backends.py
@@ -137,7 +137,7 @@ def auth_enabled_helper(backends_to_check: List[str], realm: Optional[Realm]) ->
     if realm is not None:
         enabled_method_dict = realm.authentication_methods_dict()
     else:
-        enabled_method_dict = {method: True for method in Realm.AUTHENTICATION_FLAGS}
+        enabled_method_dict = {method: True for method in AUTH_BACKEND_NAME_MAP}
 
     pad_method_dict(enabled_method_dict)
     for supported_backend in supported_auth_backends():
