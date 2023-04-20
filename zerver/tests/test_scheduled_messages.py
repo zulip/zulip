d67c7ee48a8bd8301fd13bdb5a781d0c6bf81437
Aman Agrawal <amanagr@zulip.com>
2023-04-20 02:26:41 +0000

    urls: Add new endpoint to create scheduled messages.
    This will help us remove scheduled message and reminder logic
    from `/messages` code path.

--- zerver/tests/test_scheduled_messages.py
+++ zerver/tests/test_scheduled_messages.py
@@ -28,11 +28,10 @@ class ScheduledMessageTest(ZulipTestCase):
     def do_schedule_message(
         self,
         msg_type: str,
-        to: Union[str, int, List[str], List[int]],
+        to: Union[int, List[str], List[int]],
         msg: str,
-        defer_until: str = "",
+        deliver_at: str = "",
         tz_guess: str = "",
-        delivery_type: str = "send_later",
         scheduled_message_id: str = "",
     ) -> "TestHttpResponse":
         self.login("hamlet")
@@ -46,158 +45,131 @@ class ScheduledMessageTest(ZulipTestCase):
             "to": orjson.dumps(to).decode(),
             "content": msg,
             "topic": topic_name,
-            "delivery_type": delivery_type,
             "tz_guess": tz_guess,
+            "deliver_at": deliver_at,
         }
-        if defer_until:
-            payload["deliver_at"] = defer_until
 
         if scheduled_message_id:
             payload["scheduled_message_id"] = scheduled_message_id
 
-        # `Topic` cannot be empty according to OpenAPI specification.
-        intentionally_undocumented: bool = topic_name == ""
-        result = self.client_post(
-            "/json/messages", payload, intentionally_undocumented=intentionally_undocumented
-        )
+        result = self.client_post("/json/scheduled_messages", payload)
         return result
 
     def test_schedule_message(self) -> None:
         content = "Test message"
-        defer_until = timezone_now().replace(tzinfo=None) + datetime.timedelta(days=1)
-        defer_until_str = str(defer_until)
+        deliver_at = timezone_now().replace(tzinfo=None) + datetime.timedelta(days=1)
+        deliver_at_str = str(deliver_at)
+        verona_stream_id = self.get_stream_id("Verona")
 
         # Scheduling a message to a stream you are subscribed is successful.
-        result = self.do_schedule_message("stream", "Verona", content + " 1", defer_until_str)
+        result = self.do_schedule_message(
+            "stream", verona_stream_id, content + " 1", deliver_at_str
+        )
         message = self.last_scheduled_message()
         self.assert_json_success(result)
         self.assertEqual(message.content, "Test message 1")
         self.assertEqual(message.rendered_content, "<p>Test message 1</p>")
         self.assertEqual(message.topic_name(), "Test topic")
-        self.assertEqual(message.scheduled_timestamp, convert_to_UTC(defer_until))
-        self.assertEqual(message.delivery_type, ScheduledMessage.SEND_LATER)
-        # Scheduling a message for reminders.
-        result = self.do_schedule_message(
-            "stream", "Verona", content + " 2", defer_until_str, delivery_type="remind"
-        )
-        message = self.last_scheduled_message()
-        self.assert_json_success(result)
-        self.assertEqual(message.delivery_type, ScheduledMessage.REMIND)
+        self.assertEqual(message.scheduled_timestamp, convert_to_UTC(deliver_at))
 
         # Scheduling a private message is successful.
         othello = self.example_user("othello")
-        hamlet = self.example_user("hamlet")
         result = self.do_schedule_message(
-            "private", [othello.email], content + " 3", defer_until_str
+            "private", [othello.email], content + " 3", deliver_at_str
         )
         message = self.last_scheduled_message()
         self.assert_json_success(result)
         self.assertEqual(message.content, "Test message 3")
         self.assertEqual(message.rendered_content, "<p>Test message 3</p>")
-        self.assertEqual(message.scheduled_timestamp, convert_to_UTC(defer_until))
-        self.assertEqual(message.delivery_type, ScheduledMessage.SEND_LATER)
-
-        # Setting a reminder in PM's to other users causes a error.
-        result = self.do_schedule_message(
-            "private", [othello.email], content + " 4", defer_until_str, delivery_type="remind"
-        )
-        self.assert_json_error(result, "Reminders can only be set for streams.")
-
-        # Setting a reminder in PM's to ourself is successful.
-        # Required by reminders from message actions popover caret feature.
-        result = self.do_schedule_message(
-            "private", [hamlet.email], content + " 5", defer_until_str, delivery_type="remind"
-        )
-        message = self.last_scheduled_message()
-        self.assert_json_success(result)
-        self.assertEqual(message.content, "Test message 5")
-        self.assertEqual(message.delivery_type, ScheduledMessage.REMIND)
+        self.assertEqual(message.scheduled_timestamp, convert_to_UTC(deliver_at))
 
         # Scheduling a message while guessing time zone.
         tz_guess = "Asia/Kolkata"
         result = self.do_schedule_message(
-            "stream", "Verona", content + " 6", defer_until_str, tz_guess=tz_guess
+            "stream", verona_stream_id, content + " 6", deliver_at_str, tz_guess=tz_guess
         )
         message = self.last_scheduled_message()
         self.assert_json_success(result)
         self.assertEqual(message.content, "Test message 6")
         local_tz = zoneinfo.ZoneInfo(tz_guess)
-        utz_defer_until = defer_until.replace(tzinfo=local_tz)
-        self.assertEqual(message.scheduled_timestamp, convert_to_UTC(utz_defer_until))
-        self.assertEqual(message.delivery_type, ScheduledMessage.SEND_LATER)
+        utz_deliver_at = deliver_at.replace(tzinfo=local_tz)
+        self.assertEqual(message.scheduled_timestamp, convert_to_UTC(utz_deliver_at))
 
         # Test with users time zone setting as set to some time zone rather than
         # empty. This will help interpret timestamp in users local time zone.
         user = self.example_user("hamlet")
         user.timezone = "US/Pacific"
         user.save(update_fields=["timezone"])
-        result = self.do_schedule_message("stream", "Verona", content + " 7", defer_until_str)
+        result = self.do_schedule_message(
+            "stream", verona_stream_id, content + " 7", deliver_at_str
+        )
         message = self.last_scheduled_message()
         self.assert_json_success(result)
         self.assertEqual(message.content, "Test message 7")
         local_tz = zoneinfo.ZoneInfo(user.timezone)
-        utz_defer_until = defer_until.replace(tzinfo=local_tz)
-        self.assertEqual(message.scheduled_timestamp, convert_to_UTC(utz_defer_until))
-        self.assertEqual(message.delivery_type, ScheduledMessage.SEND_LATER)
+        utz_deliver_at = deliver_at.replace(tzinfo=local_tz)
+        self.assertEqual(message.scheduled_timestamp, convert_to_UTC(utz_deliver_at))
 
     def test_scheduling_in_past(self) -> None:
         # Scheduling a message in past should fail.
         content = "Test message"
-        defer_until = timezone_now()
-        defer_until_str = str(defer_until)
+        deliver_at = timezone_now()
+        deliver_at_str = str(deliver_at)
+        verona_stream_id = self.get_stream_id("Verona")
 
-        result = self.do_schedule_message("stream", "Verona", content + " 1", defer_until_str)
+        result = self.do_schedule_message(
+            "stream", verona_stream_id, content + " 1", deliver_at_str
+        )
         self.assert_json_error(result, "Time must be in the future.")
 
     def test_invalid_timestamp(self) -> None:
         # Scheduling a message from which timestamp couldn't be parsed
         # successfully should fail.
         content = "Test message"
-        defer_until = "Missed the timestamp"
+        deliver_at = "Missed the timestamp"
+        verona_stream_id = self.get_stream_id("Verona")
 
-        result = self.do_schedule_message("stream", "Verona", content + " 1", defer_until)
+        result = self.do_schedule_message("stream", verona_stream_id, content + " 1", deliver_at)
         self.assert_json_error(result, "Invalid time format")
 
-    def test_missing_deliver_at(self) -> None:
+    def test_invalid_deliver_at(self) -> None:
         content = "Test message"
+        verona_stream_id = self.get_stream_id("Verona")
 
-        result = self.do_schedule_message("stream", "Verona", content + " 1")
-        self.assert_json_error(
-            result, "Missing deliver_at in a request for delayed message delivery"
-        )
+        result = self.do_schedule_message("stream", verona_stream_id, content + " 1", "XYZ")
+        self.assert_json_error(result, "Invalid time format")
 
     def test_edit_schedule_message(self) -> None:
         content = "Original test message"
-        defer_until = timezone_now().replace(tzinfo=None) + datetime.timedelta(days=1)
-        defer_until_str = str(defer_until)
+        deliver_at = timezone_now().replace(tzinfo=None) + datetime.timedelta(days=1)
+        deliver_at_str = str(deliver_at)
+        verona_stream_id = self.get_stream_id("Verona")
 
         # Scheduling a message to a stream you are subscribed is successful.
-        result = self.do_schedule_message("stream", "Verona", content, defer_until_str)
+        result = self.do_schedule_message("stream", verona_stream_id, content, deliver_at_str)
         message = self.last_scheduled_message()
         self.assert_json_success(result)
         self.assertEqual(message.content, "Original test message")
         self.assertEqual(message.topic_name(), "Test topic")
-        self.assertEqual(message.scheduled_timestamp, convert_to_UTC(defer_until))
-        self.assertEqual(message.delivery_type, ScheduledMessage.SEND_LATER)
+        self.assertEqual(message.scheduled_timestamp, convert_to_UTC(deliver_at))
 
         # Edit content and time of scheduled message.
         edited_content = "Edited test message"
-        new_defer_until = defer_until + datetime.timedelta(days=3)
-        new_defer_until_str = str(new_defer_until)
+        new_deliver_at = deliver_at + datetime.timedelta(days=3)
+        new_deliver_at_str = str(new_deliver_at)
 
         result = self.do_schedule_message(
             "stream",
-            "Verona",
+            verona_stream_id,
             edited_content,
-            new_defer_until_str,
+            new_deliver_at_str,
             scheduled_message_id=str(message.id),
         )
         message = self.get_scheduled_message(str(message.id))
         self.assert_json_success(result)
         self.assertEqual(message.content, edited_content)
         self.assertEqual(message.topic_name(), "Test topic")
-        self.assertEqual(message.scheduled_timestamp, convert_to_UTC(new_defer_until))
-        self.assertEqual(message.delivery_type, ScheduledMessage.SEND_LATER)
+        self.assertEqual(message.scheduled_timestamp, convert_to_UTC(new_deliver_at))
 
     def test_fetch_scheduled_messages(self) -> None:
         self.login("hamlet")
@@ -206,10 +178,11 @@ class ScheduledMessageTest(ZulipTestCase):
         self.assert_json_success(result)
         self.assert_length(orjson.loads(result.content)["scheduled_messages"], 0)
 
+        verona_stream_id = self.get_stream_id("Verona")
         content = "Test message"
-        defer_until = timezone_now().replace(tzinfo=None) + datetime.timedelta(days=1)
-        defer_until_str = str(defer_until)
-        self.do_schedule_message("stream", "Verona", content, defer_until_str)
+        deliver_at = timezone_now().replace(tzinfo=None) + datetime.timedelta(days=1)
+        deliver_at_str = str(deliver_at)
+        self.do_schedule_message("stream", verona_stream_id, content, deliver_at_str)
 
         # Single scheduled message
         result = self.client_get("/json/scheduled_messages")
@@ -221,16 +194,16 @@ class ScheduledMessageTest(ZulipTestCase):
             scheduled_messages[0]["scheduled_message_id"], self.last_scheduled_message().id
         )
         self.assertEqual(scheduled_messages[0]["content"], content)
-        self.assertEqual(scheduled_messages[0]["to"], [self.get_stream_id("Verona")])
+        self.assertEqual(scheduled_messages[0]["to"], [verona_stream_id])
         self.assertEqual(scheduled_messages[0]["type"], "stream")
         self.assertEqual(scheduled_messages[0]["topic"], "Test topic")
         self.assertEqual(
-            scheduled_messages[0]["deliver_at"], int(convert_to_UTC(defer_until).timestamp() * 1000)
+            scheduled_messages[0]["deliver_at"], int(convert_to_UTC(deliver_at).timestamp() * 1000)
         )
 
         othello = self.example_user("othello")
         result = self.do_schedule_message(
-            "private", [othello.email], content + " 3", defer_until_str
+            "private", [othello.email], content + " 3", deliver_at_str
         )
 
         # Multiple scheduled messages
@@ -249,9 +222,11 @@ class ScheduledMessageTest(ZulipTestCase):
         self.login("hamlet")
 
         content = "Test message"
-        defer_until = timezone_now().replace(tzinfo=None) + datetime.timedelta(days=1)
-        defer_until_str = str(defer_until)
-        self.do_schedule_message("stream", "Verona", content, defer_until_str)
+        verona_stream_id = self.get_stream_id("Verona")
+        deliver_at = timezone_now().replace(tzinfo=None) + datetime.timedelta(days=1)
+        deliver_at_str = str(deliver_at)
+
+        self.do_schedule_message("stream", verona_stream_id, content, deliver_at_str)
         message = self.last_scheduled_message()
         self.logout()
 
--- zerver/views/scheduled_messages.py
+++ zerver/views/scheduled_messages.py
@@ -1,10 +1,29 @@
-from django.http import HttpRequest, HttpResponse
+import sys
+from typing import Optional, Sequence, Union
 
-from zerver.actions.scheduled_messages import delete_scheduled_message
-from zerver.lib.request import has_request_variables
+from dateutil.parser import parse as dateparser
+from django.http import HttpRequest, HttpResponse
+from django.utils.timezone import now as timezone_now
+from django.utils.translation import gettext as _
+
+from zerver.actions.message_send import extract_private_recipients
+from zerver.actions.scheduled_messages import (
+    check_schedule_message,
+    delete_scheduled_message,
+)
+from zerver.lib.exceptions import JsonableError
+from zerver.lib.request import REQ, RequestNotes, has_request_variables
 from zerver.lib.response import json_success
 from zerver.lib.scheduled_messages import get_all_scheduled_messages
+from zerver.lib.timestamp import convert_to_UTC
+from zerver.lib.topic import REQ_topic
+from zerver.lib.validator import check_int
 from zerver.models import UserProfile
+
+if sys.version_info < (3, 9):  # nocoverage
+    from backports import zoneinfo
+else:  # nocoverage
+    import zoneinfo
 
 
 @has_request_variables
@@ -20,3 +39,61 @@ def delete_scheduled_messages(
 ) -> HttpResponse:
     delete_scheduled_message(user_profile, scheduled_message_id)
     return json_success(request)
+
+
+@has_request_variables
+def scheduled_messages_backend(
+    request: HttpRequest,
+    user_profile: UserProfile,
+    recipient_type_name: str = REQ("type"),
+    req_to: str = REQ("to"),
+    topic_name: Optional[str] = REQ_topic(),
+    message_content: str = REQ("content"),
+    scheduled_message_id: Optional[int] = REQ(default=None, json_validator=check_int),
+    defer_until: str = REQ("deliver_at"),
+    tz_guess: Optional[str] = REQ("tz_guess"),
+) -> HttpResponse:
+    local_tz = "UTC"
+    if tz_guess:
+        local_tz = tz_guess
+    elif user_profile.timezone:
+        local_tz = user_profile.timezone
+
+    try:
+        deliver_at = dateparser(defer_until)
+    except ValueError:
+        raise JsonableError(_("Invalid time format"))
+
+    deliver_at_usertz = deliver_at
+    if deliver_at_usertz.tzinfo is None:
+        user_tz = zoneinfo.ZoneInfo(local_tz)
+        deliver_at_usertz = deliver_at.replace(tzinfo=user_tz)
+    deliver_at = convert_to_UTC(deliver_at_usertz)
+
+    if deliver_at <= timezone_now():
+        raise JsonableError(_("Time must be in the future."))
+
+    sender = user_profile
+    client = RequestNotes.get_notes(request).client
+    assert client is not None
+
+    if recipient_type_name == "stream":
+        # req_to is ID of the recipient stream.
+        message_to: Union[Sequence[str], Sequence[int]] = [int(req_to)]
+    else:
+        message_to = extract_private_recipients(req_to)
+
+    scheduled_message_id = check_schedule_message(
+        sender,
+        client,
+        recipient_type_name,
+        message_to,
+        topic_name,
+        message_content,
+        scheduled_message_id,
+        "send_later",
+        deliver_at,
+        realm=user_profile.realm,
+        forwarder_user_profile=user_profile,
+    )
+    return json_success(request, data={"scheduled_message_id": scheduled_message_id})
