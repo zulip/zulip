a4b055d87d9d1c2132edd90463089762108d983c
Tim Abbott <tabbott@zulip.com>
2023-05-01 17:24:11 -0700

    topic_list: Show muted unread counts in muted streams.
    When all the unread messages in a muted stream are in specifically
    muted topics, this ensures that the total unread count for the stream
    that the user sees before clicking "more topics" will match the total
    unreads number for the stream itself.
    
    This behavior is limited to muted streams, since in a normal / not
    muted stream, we don't display a "muted topics only" faded unread
    count by the stream's summary line to avoid distracting the user with
    it, we match that behavior for the "more topics" line.
    
    We also now display the `@` , again to ensure the stream's summary
    line never displays an `@` without some topic row having one.

--- web/tests/topic_list_data.test.js
+++ web/tests/topic_list_data.test.js
@@ -61,6 +61,7 @@ test("get_list_info w/real stream_topic_history", ({override}) => {
         items: [],
         more_topics_have_unread_mention_messages: false,
         more_topics_unreads: 0,
+        more_topics_unread_count_muted: false,
         num_possible_topics: 0,
     });
 
@@ -269,6 +270,7 @@ test("get_list_info unreads", ({override}) => {
     assert.equal(list_info.more_topics_unreads, 3);
     assert.equal(list_info.more_topics_have_unread_mention_messages, true);
     assert.equal(list_info.num_possible_topics, 16);
+    assert.equal(list_info.more_topics_unread_count_muted, false);
 
     assert.deepEqual(
         list_info.items.map((li) => li.topic_name),
@@ -285,6 +287,89 @@ test("get_list_info unreads", ({override}) => {
             "topic 10",
             "topic 11",
             "topic 12",
+        ],
+    );
+
+    // Now test with topics 4/8/9, all the ones with unreads, being muted.
+    override(user_topics, "is_topic_muted", (stream_id, topic_name) => {
+        assert.equal(stream_id, general.stream_id);
+        return ["topic 4", "topic 8", "topic 9"].includes(topic_name);
+    });
+    list_info = get_list_info();
+    assert.equal(list_info.items.length, 12);
+    assert.equal(list_info.more_topics_unreads, 3);
+    // Topic 14 now makes it above the "more topics" fold.
+    assert.equal(list_info.more_topics_have_unread_mention_messages, false);
+    assert.equal(list_info.num_possible_topics, 16);
+    assert.equal(list_info.more_topics_unread_count_muted, true);
+    assert.deepEqual(
+        list_info.items.map((li) => li.topic_name),
+        [
+            "topic 5",
+            "topic 0",
+            "topic 1",
+            "topic 2",
+            "topic 3",
+            "topic 6",
+            "topic 7",
+            "topic 10",
+            "topic 11",
+            "topic 12",
+            "topic 13",
+            "topic 14",
+        ],
+    );
+
+    add_unreads_with_mention("topic 8", 1);
+    list_info = get_list_info();
+    assert.equal(list_info.items.length, 12);
+    assert.equal(list_info.more_topics_unreads, 4);
+    // Topic 8's new mention gets counted here.
+    assert.equal(list_info.more_topics_have_unread_mention_messages, true);
+    assert.equal(list_info.num_possible_topics, 16);
+    assert.equal(list_info.more_topics_unread_count_muted, true);
+    assert.deepEqual(
+        list_info.items.map((li) => li.topic_name),
+        [
+            "topic 5",
+            "topic 0",
+            "topic 1",
+            "topic 2",
+            "topic 3",
+            "topic 6",
+            "topic 7",
+            "topic 10",
+            "topic 11",
+            "topic 12",
+            "topic 13",
+            "topic 14",
+        ],
+    );
+
+    // Adding an additional older unmuted topic with unreads should
+    // result in just the unmuted unreads being counted.
+    add_unreads("topic 15", 15);
+    list_info = get_list_info();
+    assert.equal(list_info.items.length, 12);
+    assert.equal(list_info.more_topics_unreads, 15);
+    assert.equal(list_info.more_topics_have_unread_mention_messages, true);
+    assert.equal(list_info.num_possible_topics, 16);
+    assert.equal(list_info.more_topics_unread_count_muted, false);
+    assert.deepEqual(
+        list_info.items.map((li) => li.topic_name),
+        [
+            "topic 5",
+            "topic 0",
+            "topic 1",
+            "topic 2",
+            "topic 3",
+            "topic 6",
+            "topic 7",
+            "topic 10",
+            "topic 11",
+            "topic 12",
+            "topic 13",
+            "topic 14",
         ],
     );
 });
