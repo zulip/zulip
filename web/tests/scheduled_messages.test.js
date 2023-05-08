b3db777d4a6bf8a01928c5737549934563559d22
Karl Stolley <karl@zulip.com>
2023-05-08 10:55:34 -0500

    scheduled_messages: Don't offer to reschedule in the past.
    This introduces a function that checks for both the existence and the
    expiration of the `selected_send_later_timestamp`.
    
    The logic it supports prevents users from scheduling a message to send
    in the past or less than five minutes into the future at the level of
    the UI (specifically the popover on the \vdots component of the Send
    button). That can happen if a user attempts to edit a previously
    scheduled message.
    
    Fixes #25439.

--- web/tests/scheduled_messages.test.js
+++ web/tests/scheduled_messages.test.js
@@ -7,51 +7,52 @@ const {run_test} = require("./lib/test");
 
 const scheduled_messages = zrequire("scheduled_messages");
 
+const per_day_stamps = {
+    "2023-04-30": {
+        today_nine_am: 1682845200000,
+        today_four_pm: 1682870400000,
+        tomorrow_nine_am: 1682931600000,
+        tomorrow_four_pm: 1682956800000,
+    },
+    "2023-05-01": {
+        today_nine_am: 1682931600000,
+        today_four_pm: 1682956800000,
+        tomorrow_nine_am: 1683018000000,
+        tomorrow_four_pm: 1683043200000,
+    },
+    "2023-05-02": {
+        today_nine_am: 1683018000000,
+        today_four_pm: 1683043200000,
+        tomorrow_nine_am: 1683104400000,
+        tomorrow_four_pm: 1683129600000,
+    },
+    "2023-05-03": {
+        today_nine_am: 1683104400000,
+        today_four_pm: 1683129600000,
+        tomorrow_nine_am: 1683190800000,
+        tomorrow_four_pm: 1683216000000,
+    },
+    "2023-05-04": {
+        today_nine_am: 1683190800000,
+        today_four_pm: 1683216000000,
+        tomorrow_nine_am: 1683277200000,
+        tomorrow_four_pm: 1683302400000,
+    },
+    "2023-05-05": {
+        today_nine_am: 1683277200000,
+        today_four_pm: 1683302400000,
+        tomorrow_nine_am: 1683363600000,
+        tomorrow_four_pm: 1683388800000,
+    },
+    "2023-05-06": {
+        today_nine_am: 1683363600000,
+        today_four_pm: 1683388800000,
+        tomorrow_nine_am: 1683450000000,
+        tomorrow_four_pm: 1683475200000,
+    },
+};
+
 function get_expected_send_opts(day, expecteds) {
-    const per_day_stamps = {
-        "2023-04-30": {
-            today_nine_am: 1682845200000,
-            today_four_pm: 1682870400000,
-            tomorrow_nine_am: 1682931600000,
-            tomorrow_four_pm: 1682956800000,
-        },
-        "2023-05-01": {
-            today_nine_am: 1682931600000,
-            today_four_pm: 1682956800000,
-            tomorrow_nine_am: 1683018000000,
-            tomorrow_four_pm: 1683043200000,
-        },
-        "2023-05-02": {
-            today_nine_am: 1683018000000,
-            today_four_pm: 1683043200000,
-            tomorrow_nine_am: 1683104400000,
-            tomorrow_four_pm: 1683129600000,
-        },
-        "2023-05-03": {
-            today_nine_am: 1683104400000,
-            today_four_pm: 1683129600000,
-            tomorrow_nine_am: 1683190800000,
-            tomorrow_four_pm: 1683216000000,
-        },
-        "2023-05-04": {
-            today_nine_am: 1683190800000,
-            today_four_pm: 1683216000000,
-            tomorrow_nine_am: 1683277200000,
-            tomorrow_four_pm: 1683302400000,
-        },
-        "2023-05-05": {
-            today_nine_am: 1683277200000,
-            today_four_pm: 1683302400000,
-            tomorrow_nine_am: 1683363600000,
-            tomorrow_four_pm: 1683388800000,
-        },
-        "2023-05-06": {
-            today_nine_am: 1683363600000,
-            today_four_pm: 1683388800000,
-            tomorrow_nine_am: 1683450000000,
-            tomorrow_four_pm: 1683475200000,
-        },
-    };
     const modal_opts = {
         send_later_tomorrow: {
             tomorrow_nine_am: {
@@ -117,7 +118,11 @@ run_test("scheduled_modal_opts", () => {
     // Extra options change based on the hour of day
     const options_by_hour = [
         {hour: "T06:00:00", extras: ["today_nine_am", "today_four_pm"]},
+        {hour: "T08:54:00", extras: ["today_nine_am", "today_four_pm"]},
+        {hour: "T08:57:00", extras: ["today_four_pm"]},
         {hour: "T11:00:00", extras: ["today_four_pm"]},
+        {hour: "T13:54:00", extras: ["today_four_pm"]},
+        {hour: "T13:57:00", extras: []},
         {hour: "T17:00:00", extras: []},
     ];
 
@@ -134,4 +139,23 @@ run_test("scheduled_modal_opts", () => {
             assert.deepEqual(modal_opts, expected_opts);
         }
     }
+});
+
+run_test("missing_or_expired_timestamps", () => {
+    let now_in_seconds = new Date("2023-05-03T08:54:00").getTime();
+    // The today at 9am option is not expired as of 8:54am (false)
+    assert.ok(
+        !scheduled_messages.is_send_later_timestamp_missing_or_expired(
+            per_day_stamps["2023-05-03"].today_nine_am / 1000,
+            now_in_seconds / 1000,
+        ),
+    );
+    // The today at 9am option is expired as of 8:57am (true)
+    now_in_seconds = new Date("2023-05-03T08:57:00").getTime();
+    assert.ok(
+        scheduled_messages.is_send_later_timestamp_missing_or_expired(
+            per_day_stamps["2023-05-03"].today_nine_am / 1000,
+            now_in_seconds / 1000,
+        ),
+    );
 });
