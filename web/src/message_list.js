zulip/zulip

commit 2084a91af9ca7c71b4dd020fb973292d92884324

message_list: Don't show empty narrow message unless certain.

web/src/message_list.js
@@ -441,7 +441,14 @@ export class MessageList {
         this.view.update_render_window(this.selected_idx(), false);
 
         if (this.narrowed) {
-            if (this.visibly_empty()) {
+            if (
+                this.visibly_empty() &&
+                this.data.has_found_oldest() &&
+                this.data.has_found_newest()
+            ) {
+                // Show the empty narrow message only if we're certain
+                // that the view doesn't have messages that we're
+                // waiting for the server to send us.
                 narrow_banner.show_empty_narrow_message();
             } else {
                 narrow_banner.hide_empty_narrow_message();

commit a97c4708e804db7a08a74c249a16c168a87dff23

message_list: Fix incorrect fetch_status check.
Fixes a typo introduced in 2084a91af9ca7c71b4dd020fb973292d92884324.

web/src/message_list.js
@@ -443,8 +443,8 @@ export class MessageList {
         if (this.narrowed) {
             if (
                 this.visibly_empty() &&
-                this.data.has_found_oldest() &&
-                this.data.has_found_newest()
+                this.data.fetch_status.has_found_oldest() &&
+                this.data.fetch_status.has_found_newest()
             ) {
                 // Show the empty narrow message only if we're certain
                 // that the view doesn't have messages that we're