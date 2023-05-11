520808f55ac57da817e691e58f9b99967e911ade
Anders Kaseorg <anders@zulip.com>
2023-05-10 17:34:34 -0700

    requirements: Upgrade Python requirements.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>
    (cherry picked from commit d0481be3e59c4cb1f1b6ef68f0f0df01414200cf)

--- zerver/middleware.py
+++ zerver/middleware.py
@@ -296,7 +296,7 @@ def write_log_line(
 
 
 class RequestContext(MiddlewareMixin):
-    def __call__(self, request: HttpRequest) -> HttpResponse:
+    def __call__(self, request: HttpRequest) -> HttpResponseBase:
         set_request(request)
         try:
             return self.get_response(request)
@@ -440,7 +440,7 @@ class LogRequests(MiddlewareMixin):
 
 
 class JsonErrorHandler(MiddlewareMixin):
-    def __init__(self, get_response: Callable[[HttpRequest], HttpResponse]) -> None:
+    def __init__(self, get_response: Callable[[HttpRequest], HttpResponseBase]) -> None:
         super().__init__(get_response)
         ignore_logger("zerver.middleware.json_error_handler")
 
@@ -566,7 +566,7 @@ class LocaleMiddleware(DjangoLocaleMiddleware):
 
 class RateLimitMiddleware(MiddlewareMixin):
     def set_response_headers(
-        self, response: HttpResponse, rate_limit_results: List[RateLimitResult]
+        self, response: HttpResponseBase, rate_limit_results: List[RateLimitResult]
     ) -> None:
         # The limit on the action that was requested is the minimum of the limits that get applied:
         limit = min(result.entity.max_api_calls() for result in rate_limit_results)
@@ -579,7 +579,9 @@ class RateLimitMiddleware(MiddlewareMixin):
         reset_time = time.time() + max(result.secs_to_freedom for result in rate_limit_results)
         response["X-RateLimit-Reset"] = str(int(reset_time))
 
-    def process_response(self, request: HttpRequest, response: HttpResponse) -> HttpResponse:
+    def process_response(
+        self, request: HttpRequest, response: HttpResponseBase
+    ) -> HttpResponseBase:
         if not settings.RATE_LIMITING:
             return response
 
@@ -592,7 +594,9 @@ class RateLimitMiddleware(MiddlewareMixin):
 
 
 class FlushDisplayRecipientCache(MiddlewareMixin):
-    def process_response(self, request: HttpRequest, response: HttpResponse) -> HttpResponse:
+    def process_response(
+        self, request: HttpRequest, response: HttpResponseBase
+    ) -> HttpResponseBase:
         # We flush the per-request caches after every request, so they
         # are not shared at all between requests.
         flush_per_request_caches()
