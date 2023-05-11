bfcaf067f1114d9a3d62c91a0bb0cd0911b8052f
Anders Kaseorg <anders@zulip.com>
2023-05-10 18:48:45 -0700

    Revert "db: Force use of TimeTrackingCursor to work around Django 4.2 bug."
    This reverts commit f1925487e89455edee58abbecc1688ca3b548864.
    
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- zproject/computed_settings.py
+++ zproject/computed_settings.py
@@ -7,7 +7,7 @@ from typing import Any, Dict, Final, List, Tuple, Union
 from urllib.parse import urljoin
 
 from scripts.lib.zulip_tools import get_tornado_ports
-from zerver.lib.db import TimeTrackingConnection
+from zerver.lib.db import TimeTrackingConnection, TimeTrackingCursor
 
 from .config import (
     DEPLOY_ROOT,
@@ -281,6 +281,7 @@ DATABASES: Dict[str, Dict[str, Any]] = {
         "CONN_MAX_AGE": 600,
         "OPTIONS": {
             "connection_factory": TimeTrackingConnection,
+            "cursor_factory": TimeTrackingCursor,
         },
     }
 }
