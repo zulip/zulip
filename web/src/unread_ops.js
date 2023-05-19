e81ae4127e92ac669f611f383ae88388609eb80c
Hardik Dharmani <Ddharmani99@gmail.com>
2023-05-19 23:18:11 +0530

    unread: Improve live update for direct mentions in muted topics.
    The "Mark as unread" event handler was not passing through the
    mentioned_me_directly value, which is now important to left sidebar
    rendering.
    
    See the extended comment for how this solution is incorrect/incomplete
    and has to fall back to guessing a potentially incorrect value in rare
    situations.
    
    Co-authored-by: Tim Abbott <tabbott@zulip.com>

--- web/src/unread_ops.js
+++ web/src/unread_ops.js
@@ -300,7 +300,7 @@ export function process_read_messages_event(message_ids) {
 }
 
 export function process_unread_messages_event({message_ids, message_details}) {
-    // This is the reverse of  process_unread_messages_event.
+    // This is the reverse of process_read_messages_event.
     message_ids = unread.get_read_message_ids(message_ids);
     if (message_ids.length === 0) {
         return;
@@ -312,12 +312,33 @@ export function process_unread_messages_event({message_ids, message_details}) {
 
     for (const message_id of message_ids) {
         const message = message_store.get(message_id);
+        const message_info = message_details[message_id];
+        let mentioned_me_directly;
 
         if (message) {
             message.unread = true;
+            mentioned_me_directly = message.mentioned_me_directly;
+        } else {
+            // BUG: If we don't have a copy of the message locally, we
+            // have no way to correctly compute whether the mentions
+            // are personal mentions or wildcard mentions, because
+            // message_info doesn't contain that information... so we
+            // guess that it's a personal mention.
+            //
+            // This is a correctness bug, but is likely very rare: We
+            // will have a copy of all unread messages locally once
+            // the app has finished the message_fetch backfill
+            // sequence (and also will certainly have this message if
+            // this is the client where the "Mark as unread" action
+            // was taken). Further, the distinction is only important
+            // for mentions in muted streams, where we count direct
+            // mentions as important enough to promote, and wildcard
+            // mentions as not.
+            //
+            // A possible fix would be to just fetch the fully message
+            // from the API here, but the right fix likely requires API changes.
+            mentioned_me_directly = message_info.mentioned;
         }
-
-        const message_info = message_details[message_id];
 
         let user_ids_string;
 
@@ -328,6 +349,7 @@ export function process_unread_messages_event({message_ids, message_details}) {
         unread.process_unread_message({
             id: message_id,
             mentioned: message_info.mentioned,
+            mentioned_me_directly,
             stream_id: message_info.stream_id,
             topic: message_info.topic,
             type: message_info.type,
