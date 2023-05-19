094c3da965e1fc730d9bf91e1d6fe5375f1ea08f
Satyam Bansal <sbansal1999@gmail.com>
2023-05-19 23:35:17 +0530

    stream_settings: Fix disabled subscribe button tooltip.
    Previously, hovering over the disabled subscribe button
    would not display any tooltip due to an undefined object
    being passed to the function that created the tooltip.

--- web/src/stream_ui_updates.js
+++ web/src/stream_ui_updates.js
@@ -44,9 +44,7 @@ export function initialize_disable_btn_hint_popover(
 }
 
 export function initialize_cant_subscribe_popover(sub) {
-    const $button_wrapper = stream_settings_containers
-        .get_edit_container(sub)
-        .find(".sub_unsub_button_wrapper");
+    const $button_wrapper = $(".settings .stream_settings_header .sub_unsub_button_wrapper");
     const $settings_button = stream_settings_ui.settings_button_for_sub(sub);
     initialize_disable_btn_hint_popover(
         $button_wrapper,
