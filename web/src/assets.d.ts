7346ce98f3c07f5a525496d82161440484a0927c
Lalit <lalitkumarsingh3716@gmail.com>
2023-05-21 12:45:52 +0530

    ts: Convert `emojisets` to TypeScript.
    Declared types for '*.png' modules and '!style-loader?*' modules in
    `assets.d.ts` and used them in `emojisets.ts`.

--- web/src/assets.d.ts
+++ web/src/assets.d.ts
@@ -7,3 +7,15 @@ declare module "*.ttf" {
     const url: string;
     export default url;
 }
+
+declare module "*.png" {
+    const url: string;
+    export default url;
+}
+
+// Declare the style loader for CSS files.  This is used in the
+// `import` statements in the `emojisets.ts` file.
+declare module "!style-loader?*" {
+    const css: {use: () => void; unuse: () => void};
+    export default css;
+}
