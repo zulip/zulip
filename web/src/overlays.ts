8654f839b5bdd19198b1ef0a6c50dc121b20ea10
Aman Agrawal <amanagr@zulip.com>
2023-05-27 22:06:43 +0530

    overlays: Stop document from occupying the extra scrollbar width.
    When overlay / modal is displayed, scrollbar is hidden due to
    the disabled scrolling on `html`. Reduce width of fixed elements
    that will be visible in background and `html` so that they don't
    occupy that extra space.
    
    Also, I was over-thinking how we can get the scrollbar width. The
    moment we allowed scrolling on `html`, it was easy to get the
    scrollbar width.

--- web/src/overlays.ts
+++ web/src/overlays.ts
@@ -50,11 +50,17 @@ function call_hooks(func_list: Hook[]): void {
 }
 
 export function disable_scrolling(): void {
-    $("html").css({"overflow-y": "hidden"});
+    // Why disable scrolling?
+    // Since fixed / absolute positined elements don't capture the scroll event unless
+    // they overflow their defined container. Since fixed / absolute elements are not treated
+    // as part of the document flow, we cannot capture `scroll` events on them and prevent propagation
+    // as event bubbling doesn't work naturally.
+    const scrollbar_width = window.innerWidth - document.documentElement.clientWidth;
+    $("html").css({"overflow-y": "hidden", "--disabled-scrollbar-width": `${scrollbar_width}px`});
 }
 
 function enable_scrolling(): void {
-    $("html").css({"overflow-y": "scroll"});
+    $("html").css({"overflow-y": "scroll", "--disabled-scrollbar-width": "0px"});
 }
 
 export function is_active(): boolean {
