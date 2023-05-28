ab8e2c278093367d3b5cd4c3529f88c4e4da5259
Aman Agrawal <amanagr@zulip.com>
2023-05-28 09:51:00 +0000

    scroll_bar: Use a CSS class to toggle fluid layout.

--- web/src/scroll_bar.ts
+++ web/src/scroll_bar.ts
@@ -4,9 +4,9 @@ import {user_settings} from "./user_settings";
 
 export function set_layout_width(): void {
     if (user_settings.fluid_layout_width) {
-        $(".header-main, .app .app-main, #compose-container").css("max-width", "inherit");
+        $("body").addClass("fluid_layout_width");
     } else {
-        $(".header-main, .app .app-main, #compose-container").css("max-width", "1400px");
+        $("body").removeClass("fluid_layout_width");
     }
 }
 
