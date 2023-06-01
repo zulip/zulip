5ae2e43da0f2efa00ec0afbb4f1fca4cb3b59278
Hardik Dharmani <Ddharmani99@gmail.com>
2023-06-01 23:57:00 +0530

    navbar: Add user avatar icon for personal_menu dropdown.
    This commit adds user avatar icon in navbar. This new avatar icon
    will be used as dropdown toggle button. Made `.column-right` a flex,
    every element in right-side navbar is in this div now rather than all
    elements positioned absolutely with hardcoded values like before.
    
    Fixes part of #22802

--- tools/linter_lib/custom_check.py
+++ tools/linter_lib/custom_check.py
@@ -679,6 +679,7 @@ html_rules: List["Rule"] = [
             # background image property is dynamically generated
             "web/templates/user_profile_modal.hbs",
             "web/templates/pm_list_item.hbs",
+            "web/templates/navbar.hbs",
             # Inline styling for an svg; could be moved to CSS files?
             "templates/zerver/landing_nav.html",
             "templates/zerver/billing_nav.html",
