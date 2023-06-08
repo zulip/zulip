1aeab407799901b7e1bcf24b12f51dbb9f511212
AcKindle3 <jychen630@outlook.com>
2023-06-08 18:48:46 -0400

    ts: Convert `stream_settings_containers.js` to TypeScript.
    Added assertion to enforce `sub` is not `undefined` and thus type
    safe for the rest.
    Added type annotation to function parameter and return value.

--- web/src/stream_settings_containers.js
+++
@@ -1,9 +0,0 @@
-import $ from "jquery";
-
-export function get_edit_container(sub) {
-    return $(
-        `#subscription_overlay .subscription_settings[data-stream-id='${CSS.escape(
-            sub.stream_id,
-        )}']`,
-    );
-}
