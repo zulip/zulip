d0d3edeb086e6d24dd077fa12946f8b1538aefca
Satyam Bansal <sbansal1999@gmail.com>
2023-06-10 17:09:20 +0530

    integrations: Add issue milestoned fixture to GitHub Integration.

--- zerver/webhooks/github/tests.py
+++ zerver/webhooks/github/tests.py
@@ -193,6 +193,19 @@ class GitHubWebhookTest(WebhookTestCase):
         expected_message = "[sbansal1999](https://github.com/sbansal1999) removed the bug label from [Issue #9](https://github.com/sbansal1999/testing-gh/issues/9)."
         self.check_webhook("issues__unlabeled", expected_topic, expected_message)
 
+    def test_issue_milestoned(self) -> None:
+        expected_topic = "testing-gh / issue #6 This is a sample issue to test GH I..."
+        expected_message = "sbansal1999 milestoned [issue #6](https://github.com/sbansal1999/testing-gh/issues/6):\n\n~~~ quote\nThis tests the functionality of the GH Integration. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id sagittis ex. In at malesuada velit. Nulla convallis sem ultrices sapien finibus, a auctor nibh egestas. Cras mattis tristique ex, vitae pretium ante tempor vel. Vestibulum lacus tellus, rutrum ut neque eu, ornare facilisis diam. Curabitur ultrices mauris dolor, at lacinia felis mollis in. Fusce tincidunt magna a erat mollis volutpat. Etiam fringilla, ipsum ac vehicula pellentesque, purus augue viverra nunc, vel lacinia felis lectus ac risus. Morbi magna elit, tempus quis euismod quis, auctor ac diam. Nam quis tristique ligula. Proin ut est accumsan, ornare risus vel, fringilla ipsum.\n~~~"
+
+        self.check_webhook("issues__milestoned", expected_topic, expected_message)
+
+    def test_issue_milestoned_with_custom_topic_in_url(self) -> None:
+        self.url = self.build_webhook_url(topic="notifications")
+        expected_topic = "notifications"
+        expected_message = "sbansal1999 milestoned [issue #6 This is a sample issue to test GH Integration Func](https://github.com/sbansal1999/testing-gh/issues/6):\n\n~~~ quote\nThis tests the functionality of the GH Integration. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id sagittis ex. In at malesuada velit. Nulla convallis sem ultrices sapien finibus, a auctor nibh egestas. Cras mattis tristique ex, vitae pretium ante tempor vel. Vestibulum lacus tellus, rutrum ut neque eu, ornare facilisis diam. Curabitur ultrices mauris dolor, at lacinia felis mollis in. Fusce tincidunt magna a erat mollis volutpat. Etiam fringilla, ipsum ac vehicula pellentesque, purus augue viverra nunc, vel lacinia felis lectus ac risus. Morbi magna elit, tempus quis euismod quis, auctor ac diam. Nam quis tristique ligula. Proin ut est accumsan, ornare risus vel, fringilla ipsum.\n~~~"
+
+        self.check_webhook("issues__milestoned", expected_topic, expected_message)
+
     def test_membership_msg(self) -> None:
         expected_message = (
             "baxterthehacker added [kdaigle](https://github.com/kdaigle) to the Contractors team."
