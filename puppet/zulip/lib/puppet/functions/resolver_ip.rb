92787869c741b58862e84180d565cace4f6b9488
Alex Vandiver <alexmv@zulip.com>
2023-06-14 12:18:58 -0400

    puppet: Support IPv6 nameservers.
    The syntax in `/etc/resolv.conf` does not include any brackets:
    ```
    nameserver 2001:db8::a3
    ```
    
    However, the format of the nginx `resolver` directive[^1] requires that
    IPv6 addresses be enclosed in brackets.
    
    Adjust the `resolver_ip` puppet function to surround any IPv6
    addresses extracted from `/etc/resolv.conf` with square brackets, and
    any addresses from `application_server.resolver` to gain brackets if
    necessary.
    
    Fixes: #26013.
    
    [^1]: http://nginx.org/en/docs/http/ngx_http_core_module.html#resolver

--- puppet/zulip/lib/puppet/functions/resolver_ip.rb
+++ puppet/zulip/lib/puppet/functions/resolver_ip.rb
@@ -6,6 +6,11 @@ Puppet::Functions.create_function(:resolver_ip) do
     if parsed[:nameserver].empty?
       raise 'No nameservers found in /etc/resolv.conf!  Configure one by setting application_server.nameserver in /etc/zulip/zulip.conf'
     end
-    parsed[:nameserver][0]
+    resolver = parsed[:nameserver][0]
+    if resolver.include?(':')
+      '[' + resolver + ']'
+    else
+      resolver
+    end
   end
 end
