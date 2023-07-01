zulip/zulip

commit b052c8980ecc7bdb2a58ed08c8971079c28177b8

settings: Add wildcard mention notifications for the followed topics.
This commit makes it possible for users to control the wildcard
mention notifications for messages sent to followed topics
via a global notification setting.

There is no support for configuring this setting
through the UI yet.

zerver/lib/email_notifications.py
@@ -453,11 +453,14 @@ def do_send_missedmessage_events_reply_in_zulip(
     )
 
     context.update(
-        mention="mentioned" in unique_triggers or "wildcard_mentioned" in unique_triggers,
+        mention="mentioned" in unique_triggers
+        or "wildcard_mentioned" in unique_triggers
+        or "followed_topic_wildcard_mentioned" in unique_triggers,
         personal_mentioned=personal_mentioned,
         wildcard_mentioned="wildcard_mentioned" in unique_triggers,
         stream_email_notify="stream_email_notify" in unique_triggers,
         followed_topic_email_notify="followed_topic_email_notify" in unique_triggers,
+        followed_topic_wildcard_mentioned="followed_topic_wildcard_mentioned" in unique_triggers,
         mention_count=triggers.count("mentioned") + triggers.count("wildcard_mentioned"),
         mentioned_user_group_name=mentioned_user_group_name,
     )

templates/zerver/emails/missed_message.html
@@ -31,6 +31,8 @@
     {% trans %}You are receiving this because @{{ mentioned_user_group_name }} was mentioned.{% endtrans %}<br />
     {% elif wildcard_mentioned %}
     {% trans %}You are receiving this because everyone was mentioned in #{{ stream_name }}.{% endtrans %}<br />
+    {% elif followed_topic_wildcard_mentioned %}
+    {% trans %}You are receiving this because you have wildcard mention notifications enabled for topics you follow.{% endtrans %}<br />
     {% elif stream_email_notify %}
     {% trans %}You are receiving this because you have email notifications enabled for #{{ stream_name }}.{% endtrans %}<br />
     {% elif followed_topic_email_notify %}

zerver/actions/message_edit.py
@@ -481,8 +481,12 @@ def do_update_message(
         event["all_bot_user_ids"] = list(info.all_bot_user_ids)
         if rendering_result.mentions_wildcard:
             event["wildcard_mention_user_ids"] = list(info.wildcard_mention_user_ids)
+            event["followed_topic_wildcard_mention_user_ids"] = list(
+                info.followed_topic_wildcard_mention_user_ids
+            )
         else:
             event["wildcard_mention_user_ids"] = []
+            event["followed_topic_wildcard_mention_user_ids"] = []
 
         do_update_mobile_push_notification(
             target_message,

zerver/actions/message_send.py
@@ -168,6 +168,7 @@ class RecipientInfoResult:
     wildcard_mention_user_ids: Set[int]
     followed_topic_email_user_ids: Set[int]
     followed_topic_push_user_ids: Set[int]
+    followed_topic_wildcard_mention_user_ids: Set[int]
     muted_sender_user_ids: Set[int]
     um_eligible_user_ids: Set[int]
     long_term_idle_user_ids: Set[int]
@@ -200,6 +201,7 @@ def get_recipient_info(
     wildcard_mention_user_ids: Set[int] = set()
     followed_topic_push_user_ids: Set[int] = set()
     followed_topic_email_user_ids: Set[int] = set()
+    followed_topic_wildcard_mention_user_ids: Set[int] = set()
     muted_sender_user_ids: Set[int] = get_muting_users(sender_id)
 
     if recipient.type == Recipient.PERSONAL:
@@ -234,6 +236,9 @@ def get_recipient_info(
                 followed_topic_push_notifications=F(
                     "user_profile__enable_followed_topic_push_notifications"
                 ),
+                followed_topic_wildcard_mentions_notify=F(
+                    "user_profile__enable_followed_topic_wildcard_mentions_notify"
+                ),
             )
             .values(
                 "user_profile_id",
@@ -242,6 +247,7 @@ def get_recipient_info(
                 "wildcard_mentions_notify",
                 "followed_topic_push_notifications",
                 "followed_topic_email_notifications",
+                "followed_topic_wildcard_mentions_notify",
                 "user_profile_email_notifications",
                 "user_profile_push_notifications",
                 "user_profile_wildcard_mentions_notify",
@@ -287,12 +293,14 @@ def get_recipient_info(
         followed_topic_push_user_ids = followed_topic_notification_recipients("push_notifications")
 
         if possible_wildcard_mention:
-            # We calculate `wildcard_mention_user_ids` only if there's a possible
-            # wildcard mention in the message. This is important so as to avoid
-            # unnecessarily sending huge user ID lists with thousands of elements
-            # to the event queue (which can happen because this setting is `True`
-            # by default for new users.)
+            # We calculate `wildcard_mention_user_ids` and `followed_topic_wildcard_mention_user_ids`
+            # only if there's a possible wildcard mention in the message. This is important so as
+            # to avoid unnecessarily sending huge user ID lists with thousands of elements to the
+            # event queue (which can happen because these settings are `True` by default for new users.)
             wildcard_mention_user_ids = notification_recipients("wildcard_mentions_notify")
+            followed_topic_wildcard_mention_user_ids = followed_topic_notification_recipients(
+                "wildcard_mentions_notify"
+            )
 
     elif recipient.type == Recipient.HUDDLE:
         message_to_user_ids = get_huddle_user_ids(recipient)
@@ -412,6 +420,7 @@ def get_recipient_info(
         wildcard_mention_user_ids=wildcard_mention_user_ids,
         followed_topic_push_user_ids=followed_topic_push_user_ids,
         followed_topic_email_user_ids=followed_topic_email_user_ids,
+        followed_topic_wildcard_mention_user_ids=followed_topic_wildcard_mention_user_ids,
         muted_sender_user_ids=muted_sender_user_ids,
         um_eligible_user_ids=um_eligible_user_ids,
         long_term_idle_user_ids=long_term_idle_user_ids,
@@ -566,8 +575,10 @@ def build_message_send_dict(
     # code block).
     if rendering_result.mentions_wildcard:
         wildcard_mention_user_ids = info.wildcard_mention_user_ids
+        followed_topic_wildcard_mention_user_ids = info.followed_topic_wildcard_mention_user_ids
     else:
         wildcard_mention_user_ids = set()
+        followed_topic_wildcard_mention_user_ids = set()
 
     """
     Once we have the actual list of mentioned ids from message
@@ -604,6 +615,7 @@ def build_message_send_dict(
         service_bot_tuples=info.service_bot_tuples,
         all_bot_user_ids=info.all_bot_user_ids,
         wildcard_mention_user_ids=wildcard_mention_user_ids,
+        followed_topic_wildcard_mention_user_ids=followed_topic_wildcard_mention_user_ids,
         links_for_embed=links_for_embed,
         widget_content=widget_content_dict,
         limit_unread_user_ids=limit_unread_user_ids,
@@ -902,6 +914,7 @@ def do_send_messages(
                 wildcard_mention_user_ids=send_request.wildcard_mention_user_ids,
                 followed_topic_push_user_ids=send_request.followed_topic_push_user_ids,
                 followed_topic_email_user_ids=send_request.followed_topic_email_user_ids,
+                followed_topic_wildcard_mention_user_ids=send_request.followed_topic_wildcard_mention_user_ids,
                 muted_sender_user_ids=send_request.muted_sender_user_ids,
                 all_bot_user_ids=send_request.all_bot_user_ids,
             )
@@ -929,6 +942,9 @@ def do_send_messages(
             wildcard_mention_user_ids=list(send_request.wildcard_mention_user_ids),
             followed_topic_push_user_ids=list(send_request.followed_topic_push_user_ids),
             followed_topic_email_user_ids=list(send_request.followed_topic_email_user_ids),
+            followed_topic_wildcard_mention_user_ids=list(
+                send_request.followed_topic_wildcard_mention_user_ids
+            ),
             muted_sender_user_ids=list(send_request.muted_sender_user_ids),
             all_bot_user_ids=list(send_request.all_bot_user_ids),
             disable_external_notifications=send_request.disable_external_notifications,

zerver/lib/message.py
@@ -167,6 +167,8 @@ class SendMessageRequest:
     service_bot_tuples: List[Tuple[int, int]]
     all_bot_user_ids: Set[int]
     wildcard_mention_user_ids: Set[int]
+    # IDs of users who have followed the topic the message is being sent to, and have the followed topic wildcard mentions notify setting ON.
+    followed_topic_wildcard_mention_user_ids: Set[int]
     links_for_embed: Set[str]
     widget_content: Optional[Dict[str, Any]]
     submessages: List[Dict[str, Any]] = field(default_factory=list)

zerver/lib/notification_data.py
@@ -21,6 +21,8 @@ class UserMessageNotificationsData:
     stream_email_notify: bool
     followed_topic_push_notify: bool
     followed_topic_email_notify: bool
+    followed_topic_wildcard_mention_push_notify: bool
+    followed_topic_wildcard_mention_email_notify: bool
     sender_is_muted: bool
     disable_external_notifications: bool
 
@@ -58,6 +60,7 @@ class UserMessageNotificationsData:
         wildcard_mention_user_ids: Set[int],
         followed_topic_push_user_ids: Set[int],
         followed_topic_email_user_ids: Set[int],
+        followed_topic_wildcard_mention_user_ids: Set[int],
         muted_sender_user_ids: Set[int],
         all_bot_user_ids: Set[int],
     ) -> "UserMessageNotificationsData":
@@ -76,6 +79,8 @@ class UserMessageNotificationsData:
                 stream_email_notify=False,
                 followed_topic_push_notify=False,
                 followed_topic_email_notify=False,
+                followed_topic_wildcard_mention_push_notify=False,
+                followed_topic_wildcard_mention_email_notify=False,
                 sender_is_muted=False,
                 disable_external_notifications=False,
             )
@@ -92,6 +97,11 @@ class UserMessageNotificationsData:
             and user_id not in pm_mention_email_disabled_user_ids
             and "wildcard_mentioned" in flags
         )
+        followed_topic_wildcard_mention_email_notify = (
+            user_id in followed_topic_wildcard_mention_user_ids
+            and user_id not in pm_mention_email_disabled_user_ids
+            and "wildcard_mentioned" in flags
+        )
 
         pm_push_notify = user_id not in pm_mention_push_disabled_user_ids and private_message
         mention_push_notify = (
@@ -99,6 +109,11 @@ class UserMessageNotificationsData:
         )
         wildcard_mention_push_notify = (
             user_id in wildcard_mention_user_ids
+            and user_id not in pm_mention_push_disabled_user_ids
+            and "wildcard_mentioned" in flags
+        )
+        followed_topic_wildcard_mention_push_notify = (
+            user_id in followed_topic_wildcard_mention_user_ids
             and user_id not in pm_mention_push_disabled_user_ids
             and "wildcard_mentioned" in flags
         )
@@ -115,6 +130,8 @@ class UserMessageNotificationsData:
             stream_email_notify=(user_id in stream_email_user_ids),
             followed_topic_push_notify=(user_id in followed_topic_push_user_ids),
             followed_topic_email_notify=(user_id in followed_topic_email_user_ids),
+            followed_topic_wildcard_mention_push_notify=followed_topic_wildcard_mention_push_notify,
+            followed_topic_wildcard_mention_email_notify=followed_topic_wildcard_mention_email_notify,
             sender_is_muted=(user_id in muted_sender_user_ids),
             disable_external_notifications=disable_external_notifications,
         )
@@ -160,6 +177,8 @@ class UserMessageNotificationsData:
             return NotificationTriggers.PRIVATE_MESSAGE
         elif self.mention_push_notify:
             return NotificationTriggers.MENTION
+        elif self.followed_topic_wildcard_mention_push_notify:
+            return NotificationTriggers.FOLLOWED_TOPIC_WILDCARD_MENTION
         elif self.wildcard_mention_push_notify:
             return NotificationTriggers.WILDCARD_MENTION
         elif self.followed_topic_push_notify:
@@ -186,6 +205,8 @@ class UserMessageNotificationsData:
             return NotificationTriggers.PRIVATE_MESSAGE
         elif self.mention_email_notify:
             return NotificationTriggers.MENTION
+        elif self.followed_topic_wildcard_mention_email_notify:
+            return NotificationTriggers.FOLLOWED_TOPIC_WILDCARD_MENTION
         elif self.wildcard_mention_email_notify:
             return NotificationTriggers.WILDCARD_MENTION
         elif self.followed_topic_email_notify:

zerver/lib/test_classes.py
@@ -1731,6 +1731,12 @@ Output:
             stream_push_notify=kwargs.get("stream_push_notify", False),
             followed_topic_email_notify=kwargs.get("followed_topic_email_notify", False),
             followed_topic_push_notify=kwargs.get("followed_topic_push_notify", False),
+            followed_topic_wildcard_mention_email_notify=kwargs.get(
+                "followed_topic_wildcard_mention_email_notify", False
+            ),
+            followed_topic_wildcard_mention_push_notify=kwargs.get(
+                "followed_topic_wildcard_mention_push_notify", False
+            ),
             sender_is_muted=kwargs.get("sender_is_muted", False),
             disable_external_notifications=kwargs.get("disable_external_notifications", False),
         )

zerver/migrations/0331_scheduledmessagenotificationemail.py
@@ -29,6 +29,10 @@ class Migration(migrations.Migration):
                             ("wildcard_mentioned", "Wildcard mention"),
                             ("stream_email_notify", "Stream notifications enabled"),
                             ("followed_topic_email_notify", "Followed topic notifications enabled"),
+                            (
+                                "followed_topic_wildcard_mentioned",
+                                "Followed topic wildcard mention",
+                            ),
                         ]
                     ),
                 ),

commit febc58de2c6d542745989155ea7254c452c5b44d

notifications: Fix the 'senders' list.
This commit fixes the incorrect calculation of the
'senders' list.

The effect of 'followed_topic_wildcard_mention'
wasn't considered earlier.

The bug was introduced in b052c8980ecc7bdb2a58ed08c8971079c28177b8.

zerver/lib/email_notifications.py
@@ -526,6 +526,7 @@ def do_send_missedmessage_events_reply_in_zulip(
                     for m in missed_messages
                     if m["trigger"] == NotificationTriggers.MENTION
                     or m["trigger"] == NotificationTriggers.WILDCARD_MENTION
+                    or m["trigger"] == NotificationTriggers.FOLLOWED_TOPIC_WILDCARD_MENTION
                 }
             )
         message = missed_messages[0]["message"]