ad2cfc4c3008f77b29a34ecabc24452259e43d3d
SrishtiHasijani31 <srishti.hasijani31@gmail.com>
2023-07-04 00:25:50 +0100

    test case updated

--- zerver/tests/test_send_translated_message.py
+++ zerver/tests/test_send_translated_message.py
@@ -1,24 +1,15 @@
 import unittest
-from django.contrib.auth.models import User
-from django.utils.translation import activate
 from zerver.lib.translate import translate_message
-import zerver.actions.message_send as self
-
-from zerver.models import UserProfile
 
 
 class MessageTranslationTestCase(unittest.TestCase):
-    def test_send_translated_message(self):
-        user = UserProfile.objects.create(
-            email='test@example.com',
-            user_preferred_language='es'
-        )
-
-        # Activate the user's preferred language
-        activate(user.user_preferred_language)
+    def test_send_message_translation(self) -> None:
+        user_profile = self.example_user('hamlet')
+        self.login_user(user_profile)
+        preferred_language = user_profile.user_preferred_language
 
         # Translate a message content using the user's preferred language
-        translated_content = translate_message('good morning', user.user_preferred_language)
+        translated_content = translate_message('good morning', preferred_language)
 
         # Assert that the message has been translated correctly
         self.assertEqual(translated_content, 'buenos días')
