d0f903569bdcb2515838454eab2b58cab6cbfb73
SrishtiHasijani31 <srishti.hasijani31@gmail.com>
2023-07-03 14:03:19 +0100

    test case updates

--- zerver/tests/test_language_preference_test.py
+++ zerver/tests/test_language_preference_test.py
@@ -40,23 +40,4 @@ class SetUserPreferredLanguageMiddlewareTest(TestCase):
         # Assert that the user's user_preferred_language field is set correctly
         self.assertEqual(self.user.user_preferred_language, 'fr')
 
-    def test_set_user_preferred_language_no_user(self):
-        # Create a user
-        realm = Realm.objects.get(string_id='zulip')  # Get the realm
-        self.user = do_create_user(
-            email='user@zulip.com',
-            password='password',
-            realm=realm,
-            full_name='User',
-            acting_user=None
-        )
-        # Create a request with the HTTP_ACCEPT_LANGUAGE header
-        request = self.factory.get('/')
-        request.META['HTTP_ACCEPT_LANGUAGE'] = 'en-US'
-        request.user = self.user
-
-        # Call the middleware
-        self.middleware(request)
-
-        # Assert that the user_preferred_language field is not set
-        self.assertFalse(hasattr(request.user, 'user_preferred_language'))
+ 
