19bf1ee42d0bf114da0b6a1af9fa56f3a1054f0a
SrishtiHasijani31 <srishti.hasijani31@gmail.com>
2023-07-03 13:07:35 +0100

    path updated

--- zerver/tests/test_language_preference_test.py
+++ zerver/tests/test_language_preference_test.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.test.client import RequestFactory
 
-from zulip.zerver.middleware import SetUserPreferredLanguageMiddleware
+from zerver.middleware import *
 
 
 class SetUserPreferredLanguageMiddlewareTestCase(TestCase):
