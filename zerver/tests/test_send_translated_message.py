ae243a4b6399865fcc086e8d7aee08e69a8cb58e
SrishtiHasijani31 <srishti.hasijani31@gmail.com>
2023-07-03 17:41:31 +0100

    test case updated

--- zerver/tests/test_send_translated_message.py
+++ zerver/tests/test_send_translated_message.py
@@ -1,45 +1,23 @@
 import unittest
-from unittest.mock import patch, MagicMock
-from django.contrib.auth.models import User
+from zerver.models import UserProfile
 from django.utils.translation import activate
 from zerver.lib.translate import translate_message
-from zerver.actions.create_user import do_create_user
-
-from zerver.models import Realm
 
 
-class MessageSendTestCase(unittest.TestCase):
+class MessageTranslationTestCase(unittest.TestCase):
     def test_send_message_translation(self):
-        # Create a sender with a preferred language
-        realm = Realm.objects.get(string_id='zulip')  # Get the realm
-        sender = do_create_user(
-            email='user@zulip.com',
-            password='password',
-            realm=realm,
-            full_name='User',
-            acting_user=None
+        # Create a user profile with a preferred language
+        user_profile = UserProfile.objects.create_user(
+            email='testuser@example.com',
+            password='testpassword',
+            user_preferred_language='fr'
         )
 
-        sender.user_preferred_language = 'fr'
-        sender.save()
+        # Translate the message content using the user's preferred language
+        translated_content = translate_message("good morning", user_profile.user_preferred_language)
 
-        # Create a mock object for SendMessageRequest
-        send_message_request = MagicMock()
-        send_message_request.sender = sender
-        send_message_request.message_content = 'good morning'
-
-        # Activate the sender's preferred language
-        activate(sender.user_preferred_language)
-
-        # Translate the message content using the sender's preferred language
-        translated_content = translate_message(send_message_request.message_content,
-                                               sender.user_preferred_language)
-
-        # Update the message content with the translated text
-        send_message_request.message_content = translated_content
-
-        # Verify that the translated message is included in the message payload
-        self.assertEqual(send_message_request.message_content, 'bonjour')
+        # Assert that the message content has been translated correctly
+        self.assertEqual(translated_content, 'bonjour')
 
 
 if __name__ == '__main__':
