b4e5f100f7bba7201d2e839d112cf32c67adef3d
Anders Kaseorg <anders@zulip.com>
2023-07-10 17:30:32 -0400

    sentry: Remove dependence on get_current_request.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- zproject/sentry.py
+++ zproject/sentry.py
@@ -24,7 +24,6 @@ def add_context(event: "Event", hint: "Hint") -> Optional["Event"]:
             return None
     from django.conf import settings
 
-    from zerver.lib.request import RequestNotes, get_current_request
     from zerver.models import get_user_profile_by_id
 
     with capture_internal_exceptions():
@@ -47,13 +46,6 @@ def add_context(event: "Event", hint: "Hint") -> Optional["Event"]:
         if "email" in user_info:
             del user_info["email"]
 
-        request = get_current_request()
-        if request:
-            request_notes = RequestNotes.get_notes(request)
-            if request_notes.client is not None:
-                event["tags"]["client"] = request_notes.client.name
-            if request_notes.realm is not None:
-                event["tags"].setdefault("realm", request_notes.realm.string_id)
     return event
 
 
