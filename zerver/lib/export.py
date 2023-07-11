8f1db3b5c5e1549992c64676d4e89c1916b8d472
Alex Vandiver <alexmv@zulip.com>
2023-07-11 20:34:13 +0000

    export: Skip crossrealm bots, if they are in the exported realm.
    This prevents them from being duplicated in the crossrealm users.
    
    (cherry picked from commit 54395612c7566e7bc4739b181241353a6b3c4a6d)

--- zerver/lib/export.py
+++ zerver/lib/export.py
@@ -992,7 +992,13 @@ def custom_fetch_user_profile(response: TableData, context: Context) -> None:
     realm = context["realm"]
     exportable_user_ids = context["exportable_user_ids"]
 
-    query = UserProfile.objects.filter(realm_id=realm.id)
+    query = UserProfile.objects.filter(realm_id=realm.id).exclude(
+        # These were, in some early versions of Zulip, inserted into
+        # the first realm that was created.  In those cases, rather
+        # than include them here, we will include them in the
+        # crossrealm user list, below.
+        email__in=settings.CROSS_REALM_BOT_EMAILS,
+    )
     exclude = EXCLUDED_USER_PROFILE_FIELDS
     rows = make_raw(list(query), exclude=exclude)
 
