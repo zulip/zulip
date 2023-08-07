237037270573bec0e1e4130efc4d6bbb429d78ea
Satyam Bansal <sbansal1999@gmail.com>
2023-08-07 15:55:33 +0530

    integrations: Extract integration event types returning function.

--- zerver/views/documentation.py
+++ zerver/views/documentation.py
@@ -21,6 +21,7 @@ from zerver.lib.integrations import (
     META_CATEGORY,
     HubotIntegration,
     WebhookIntegration,
+    get_all_event_types_for_integration,
 )
 from zerver.lib.request import REQ, RequestNotes, has_request_variables
 from zerver.lib.subdomains import get_subdomain
@@ -345,11 +346,9 @@ def integration_doc(request: HttpRequest, integration_name: str = REQ()) -> Http
     context["recommended_stream_name"] = integration.stream_name
     if isinstance(integration, WebhookIntegration):
         context["integration_url"] = integration.url[3:]
-        if (
-            hasattr(integration.function, "_all_event_types")
-            and integration.function._all_event_types is not None
-        ):
-            context["all_event_types"] = integration.function._all_event_types
+        all_event_types = get_all_event_types_for_integration(integration)
+        if all_event_types is not None:
+            context["all_event_types"] = all_event_types
     if isinstance(integration, HubotIntegration):
         context["hubot_docs_url"] = integration.hubot_docs_url
 
