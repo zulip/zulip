225be0bb97fa0ccd17e4f20908915e36bfc79ed3
Lauryn Menard <lauryn@zulip.com>
2023-08-09 12:04:46 +0200

    dropdown-list-widget: Remove dropdown-list-body class and reference.
    Removes the "dropdown-list-body" class and references as it is no
    longer used in the new DropdownWidget. The previous uses of the
    class were removed in commit 875d564f2d.

--- web/src/ui_init.js
+++ web/src/ui_init.js
@@ -302,11 +302,8 @@ export function initialize_kitchen_sink_stuff() {
 
     // Ignore wheel events in the compose area which weren't already handled above.
     $("#compose").on("wheel", (e) => {
-        // Except for the stream select dropdown and compose banners, which still needs scroll events.
-        if (
-            $(e.target).closest(".dropdown-list-body").length ||
-            $(e.target).closest("#compose_banners").length
-        ) {
+        // Except for the compose banners, which still need scroll events.
+        if ($(e.target).closest("#compose_banners").length) {
             return;
         }
         e.stopPropagation();
