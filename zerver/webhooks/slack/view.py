a8cee17584a78cde8bba3c85abea1066d8aa2bd5
Zixuan James Li <p359101898@gmail.com>
2023-08-12 03:35:14 -0400

    webhooks: Migrate webhooks with str parameters to use @typed_endpoint.
    These webhooks have some URL query params that do not need additional
    validation or parsing from JSON. So WebhookPaylaod is not applicable to
    these webhooks.

--- zerver/webhooks/slack/view.py
+++ zerver/webhooks/slack/view.py
@@ -5,8 +5,9 @@ from django.utils.translation import gettext as _
 from zerver.actions.message_send import check_send_stream_message
 from zerver.decorator import webhook_view
 from zerver.lib.exceptions import JsonableError
-from zerver.lib.request import REQ, RequestNotes, has_request_variables
+from zerver.lib.request import RequestNotes
 from zerver.lib.response import json_success
+from zerver.lib.typed_endpoint import typed_endpoint
 from zerver.models import UserProfile
 
 ZULIP_MESSAGE_TEMPLATE = "**{message_sender}**: {text}"
@@ -14,15 +15,16 @@ VALID_OPTIONS = {"SHOULD_NOT_BE_MAPPED": "0", "SHOULD_BE_MAPPED": "1"}
 
 
 @webhook_view("Slack", notify_bot_owner_on_invalid_json=False)
-@has_request_variables
+@typed_endpoint
 def api_slack_webhook(
     request: HttpRequest,
     user_profile: UserProfile,
-    user_name: str = REQ(),
-    text: str = REQ(),
-    channel_name: str = REQ(),
-    stream: str = REQ(default="slack"),
-    channels_map_to_topics: str = REQ(default="1"),
+    *,
+    user_name: str,
+    text: str,
+    channel_name: str,
+    stream: str = "slack",
+    channels_map_to_topics: str = "1",
 ) -> HttpResponse:
     if channels_map_to_topics not in list(VALID_OPTIONS.values()):
         raise JsonableError(_("Error: channels_map_to_topics parameter other than 0 or 1"))
