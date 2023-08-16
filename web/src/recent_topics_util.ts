290a9c6da3f93879e06a552b704ba5db28201ade
Junyao Chen <junyao.chen@nyu.edu>
2023-08-16 04:27:18 -0400

    ts: Migrate `recent_topics_util.js` to TypeScript.

---
+++ web/src/recent_topics_util.ts
@@ -0,0 +1,27 @@
+import type {DirectRecipient, StreamRecipient} from "./util";
+
+type Recipient = DirectRecipient | StreamRecipient;
+
+let is_rt_visible = false;
+
+export function set_visible(value: boolean): void {
+    is_rt_visible = value;
+}
+
+export function is_visible(): boolean {
+    return is_rt_visible;
+}
+
+export function get_topic_key(stream_id: number, topic: string): string {
+    return stream_id + ":" + topic.toLowerCase();
+}
+
+export function get_key_from_message(msg: Recipient): string | undefined {
+    if (msg.type === "private") {
+        // The to_user_ids field on a direct message object is a
+        // string containing the user IDs involved in the message in
+        // sorted order.
+        return msg.to_user_ids;
+    }
+    return get_topic_key(msg.stream_id, msg.topic);
+}
