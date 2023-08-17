14e9f653492bce0f408eca269d7d9670ac85fe5d
Karl Stolley <karl@zulip.com>
2023-08-17 11:12:56 -0500

    grid: Apply mobile-style grid when middle column is small.
    This introduces a new breakpoint. In the future, this kind of
    round-about sizing will be made unncessary by container queries,
    but while those ship in all modern browsers, their availability
    is quite recent:
    
    https://developer.mozilla.org/en-US/docs/Web/CSS/@container

--- web/src/css_variables.js
+++ web/src/css_variables.js
@@ -14,11 +14,15 @@ const ml = 425; // Mobile large
 const mm = 375; // Mobile medium
 const ms = 320; // Mobile small
 
+// Breakpoints for middle column
+const mc = 849; // Middle column as wide as it appears after the `sm` breakpoint
+
 module.exports = {
     media_breakpoints: {
         xs_min: xs + "px",
         sm_min: sm + "px",
         md_min: md + "px",
+        mc_min: mc + "px",
         lg_min: lg + "px",
         xl_min: xl + "px",
         ml_min: ml + "px",
@@ -30,6 +34,7 @@ module.exports = {
         xs,
         sm,
         md,
+        mc,
         lg,
         xl,
         ml,
