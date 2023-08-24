c306b2b077b3448f256fdfe304086a6da688ed46
Hemant Umre <hemantumre12@gmail.com>
2023-08-24 22:37:11 +0530

    list_widget: Add functionality to display empty list message.
    This commit introduces a function in list_widget.js that allows
    displaying a message when the list is empty using the `data-empty`
    dataset. The function checks the container type to determine the
    appropriate wrapper for the message and appends it to the container
    body.
    
    Fixes a part of #23072.

--- web/src/list_widget.ts
+++ web/src/list_widget.ts
@@ -1,6 +1,9 @@
 import $ from "jquery";
 import assert from "minimalistic-assert";
 
+import render_empty_list_widget_for_list from "../templates/empty_list_widget_for_list.hbs";
+import render_empty_list_widget_for_table from "../templates/empty_list_widget_for_table.hbs";
+
 import * as blueslip from "./blueslip";
 import * as scroll_util from "./scroll_util";
 
@@ -177,6 +180,44 @@ function is_scroll_position_for_render(scroll_container: HTMLElement): boolean {
     );
 }
 
+function get_column_count_for_table($table: JQuery): number {
+    let column_count = 0;
+    const $thead = $table.find("thead");
+    if ($thead.length) {
+        column_count = $thead.find("tr").children().length;
+    }
+    return column_count;
+}
+
+export function render_empty_list_message_if_needed($container: JQuery): void {
+    const empty_list_message = $container.data("empty");
+
+    if (!empty_list_message || $container.children().length) {
+        return;
+    }
+
+    let empty_list_widget;
+
+    if ($container.is("table, tbody")) {
+        let $table = $container;
+        if ($container.is("tbody")) {
+            $table = $container.closest("table");
+        }
+
+        const column_count = get_column_count_for_table($table);
+        empty_list_widget = render_empty_list_widget_for_table({
+            empty_list_message,
+            column_count,
+        });
+    } else {
+        empty_list_widget = render_empty_list_widget_for_list({
+            empty_list_message,
+        });
+    }
+
+    $container.append(empty_list_widget);
+}
+
 // @params
 // $container: jQuery object to append to.
 // list: The list of items to progressively append.
@@ -249,6 +290,7 @@ export function create<Key = unknown, Item = Key>(
 
             // Stop once the offset reaches the length of the original list.
             if (meta.offset >= meta.filtered_list.length) {
+                render_empty_list_message_if_needed($container);
                 return;
             }
 
