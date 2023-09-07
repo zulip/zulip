08229df6487f2dd58c2d606ac4d384f5041a92b1
Anders Kaseorg <anders@zulip.com>
2023-09-07 12:11:22 -0700

    models: Remove duplicate index definition for date_sent.
    Commit cf0eb46afcb22a236211ee2b3e3d23b8ef924eab added this to let
    Django understand the CREATE INDEX CONCURRENTLY statement that had
    been hidden in a RunSQL query in migration 0244.  However, migration
    0245 explained that same index to Django in a different way by setting
    db_index=True.  Move that to 0244 where the index is actually created,
    using SeparateDatabaseAndState.
    
    Also remove the part of the SQL in 0245 that was mirrored by dummy
    state_operations, and replace it with real operations.
    
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- zerver/migrations/0245_message_date_sent_finalize_part1.py
+++ zerver/migrations/0245_message_date_sent_finalize_part1.py
@@ -13,19 +13,16 @@ class Migration(migrations.Migration):
             """
             DROP TRIGGER zerver_message_date_sent_to_pub_date_trigger ON zerver_message;
             DROP FUNCTION zerver_message_date_sent_to_pub_date_trigger_function();
-
-            ALTER TABLE zerver_message ALTER COLUMN date_sent SET NOT NULL;
-            ALTER TABLE zerver_message ALTER COLUMN pub_date DROP NOT NULL;
-            """,
-            state_operations=[
-                # This just tells Django to, after running the above SQL, consider the AlterField below
-                # as done. The building of the index actually happened in the previous migration, not here,
-                # but nevertheless this seems like the correct place to put this fake AlterField.
-                migrations.AlterField(
-                    model_name="message",
-                    name="date_sent",
-                    field=models.DateTimeField(db_index=True, verbose_name="date sent"),
-                ),
-            ],
+            """
+        ),
+        migrations.AlterField(
+            model_name="message",
+            name="date_sent",
+            field=models.DateTimeField(db_index=True, verbose_name="date sent"),
+        ),
+        migrations.AlterField(
+            model_name="message",
+            name="pub_date",
+            field=models.DateTimeField(db_index=True, null=True, verbose_name="date published"),
         ),
     ]
