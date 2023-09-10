7deb5f7d1fc0ccbb99dd0016397c5987d802fa61
Lalit Kumar Singh <lalitkumarsingh3716@gmail.com>
2023-09-10 17:36:14 +0530

    user_topics: Pop `user_topics` from `page_params`.
    This avoids accessing the global page_params directly.

--- web/src/ui_init.js
+++ web/src/ui_init.js
@@ -608,6 +608,8 @@ export function initialize_everything() {
 
     const muted_users_params = pop_fields("muted_users");
 
+    const user_topics_params = pop_fields("user_topics");
+
     const user_status_params = pop_fields("user_status");
     const i18n_params = pop_fields("language_list");
     const user_settings_params = pop_fields("user_settings");
@@ -692,7 +694,7 @@ export function initialize_everything() {
     stream_data.initialize(stream_data_params);
     user_group_edit_members.initialize();
     pm_conversations.recent.initialize(pm_conversations_params);
-    user_topics.initialize();
+    user_topics.initialize(user_topics_params);
     muted_users.initialize(muted_users_params);
     stream_settings_ui.initialize();
     user_group_settings_ui.initialize();
