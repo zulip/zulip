0e5c6ec5b98c5c435e429ba3f0f5c5f300c572ff
Aman Agrawal <amanagr@zulip.com>
2023-09-20 05:35:46 +0000

    inbox: Fix inbox scrolled to top when returning from other narrows.
    This has to do with narrows and inbox view sharing the same
    scrolling container.

--- web/src/inbox_ui.js
+++ web/src/inbox_ui.js
@@ -462,7 +462,10 @@ export function complete_rerender() {
     );
     update_filters();
     show_empty_inbox_text(has_visible_unreads);
-
+    // If the focus is not on the inbox rows, the inbox view scrolls
+    // downwhen moving from other views to the inbox view. To avoid
+    // this, we scroll to top before restoring focus.
+    $("html").scrollTop(0);
     setTimeout(() => {
         // We don't want to focus on simplebar ever.
         $("#inbox-list .simplebar-content-wrapper").attr("tabindex", "-1");
