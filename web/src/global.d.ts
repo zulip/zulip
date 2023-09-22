a963335b913c67cbeba4c6a27e0921f153706b04
Anders Kaseorg <anders@zulip.com>
2023-09-22 14:25:08 -0700

    global: Specialize jQuery .val() for elements with known value types.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- web/src/global.d.ts
+++ web/src/global.d.ts
@@ -22,7 +22,21 @@ declare namespace JQueryValidation {
 }
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-interface JQuery {
+interface JQuery<TElement = HTMLElement> {
+    // Specialize .val() for elements with known value types.
+    // https://github.com/DefinitelyTyped/DefinitelyTyped/pull/66801
+    val():
+        | (TElement extends HTMLSelectElement & {type: "select-one"}
+              ? string
+              : TElement extends HTMLSelectElement & {type: "select-multiple"}
+              ? string[]
+              : TElement extends HTMLSelectElement
+              ? string | string[]
+              : TElement extends {value: string | number}
+              ? TElement["value"]
+              : string | number | string[])
+        | undefined;
+
     expectOne(): this;
     get_offset_to_window(): DOMRect;
     tab(action?: string): this; // From web/third/bootstrap
