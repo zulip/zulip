1128752bd0b4f946c0a098171bc4c61ff4dea763
Anders Kaseorg <anders@zulip.com>
2023-09-22 00:11:34 -0700

    compose_fade_users: Convert module to TypeScript.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- web/src/compose_fade_users.ts
+++ web/src/compose_fade_users.ts
@@ -1,7 +1,13 @@
 import * as compose_fade_helper from "./compose_fade_helper";
 import * as people from "./people";
 
-function update_user_row_when_fading($li, conf) {
+export type UserFadeConfig = {
+    get_user_id($li: JQuery): number;
+    fade($li: JQuery): void;
+    unfade($li: JQuery): void;
+};
+
+function update_user_row_when_fading($li: JQuery, conf: UserFadeConfig): void {
     const user_id = conf.get_user_id($li);
     const would_receive = compose_fade_helper.would_receive_message(user_id);
 
@@ -12,19 +18,19 @@ function update_user_row_when_fading($li, conf) {
     }
 }
 
-export function fade_users(items, conf) {
+export function fade_users(items: JQuery[], conf: UserFadeConfig): void {
     for (const $li of items) {
         update_user_row_when_fading($li, conf);
     }
 }
 
-export function display_users_normally(items, conf) {
+export function display_users_normally(items: JQuery[], conf: UserFadeConfig): void {
     for (const $li of items) {
         conf.unfade($li);
     }
 }
 
-export function update_user_info(items, conf) {
+export function update_user_info(items: JQuery[], conf: UserFadeConfig): void {
     if (compose_fade_helper.want_normal_display()) {
         display_users_normally(items, conf);
     } else {
