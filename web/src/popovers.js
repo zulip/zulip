3ab1388243c4d869a64386ed0e4e0ee264a08b34
Aman Agrawal <amanagr@zulip.com>
2023-09-28 13:47:04 +0000

    popovers: Simplify any_active.
    Remove unused functions along with it.

--- web/src/popovers.js
+++ web/src/popovers.js
@@ -1,25 +1,14 @@
+import $ from "jquery";
 import * as tippy from "tippy.js";
 
-import * as emoji_picker from "./emoji_picker";
-import * as playground_links_popover from "./playground_links_popover";
-import * as popover_menus from "./popover_menus";
-import * as stream_popover from "./stream_popover";
-import * as user_card_popover from "./user_card_popover";
-import * as user_group_popover from "./user_group_popover";
-
 export function any_active() {
-    // True if any popover (that this module manages) is currently shown.
-    // Expanded sidebars on mobile view count as popovers as well.
-    return (
-        popover_menus.any_active() ||
-        stream_popover.is_open() ||
-        user_group_popover.is_open() ||
-        user_card_popover.user_sidebar.is_open() ||
-        user_card_popover.message_user_card.is_open() ||
-        user_card_popover.user_card.is_open() ||
-        emoji_picker.is_open() ||
-        playground_links_popover.is_open()
-    );
+    // Checks if there are any interactive tippy instances (= popovers) present.
+    const $tippy_instances = $("[data-tippy-root]");
+    // Tippy instances with `interactive: true` are popovers by definition.
+    const num_interactive_instances = $tippy_instances.filter(
+        (_i, elt) => elt._tippy?.props.interactive === true,
+    ).length;
+    return Boolean(num_interactive_instances);
 }
 
 export function hide_all() {
