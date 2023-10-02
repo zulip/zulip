f978b2ea7c5dffdc21e80db09a84fcc2bb29026c
Anders Kaseorg <anders@zulip.com>
2023-10-02 15:06:31 -0700

    copied_tooltip: Convert module to TypeScript.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- web/src/copied_tooltip.ts
+++ web/src/copied_tooltip.ts
@@ -2,7 +2,11 @@ import tippy from "tippy.js";
 
 import {$t} from "./i18n";
 
-export function show_copied_confirmation(copy_button, on_hide_callback, timeout_in_ms = 1000) {
+export function show_copied_confirmation(
+    copy_button: HTMLElement,
+    on_hide_callback: () => void,
+    timeout_in_ms = 1000,
+): void {
     // Display a tooltip to notify the user the message or code was copied.
     const instance = tippy(copy_button, {
         placement: "top",
@@ -18,7 +22,7 @@ export function show_copied_confirmation(copy_button, on_hide_callback, timeout_
     });
     instance.setContent($t({defaultMessage: "Copied!"}));
     instance.show();
-    function remove_instance() {
+    function remove_instance(): void {
         if (!instance.state.isDestroyed) {
             instance.destroy();
         }
