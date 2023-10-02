f7899eccc93ba60a5bbd9640078a661556853408
Anders Kaseorg <anders@zulip.com>
2023-10-02 15:04:44 -0700

    copied_tooltip: Remove incorrect $ prefix from non-jQuery variable.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- web/src/copied_tooltip.js
+++ web/src/copied_tooltip.js
@@ -2,9 +2,9 @@ import tippy from "tippy.js";
 
 import {$t} from "./i18n";
 
-export function show_copied_confirmation($copy_button, on_hide_callback, timeout_in_ms = 1000) {
+export function show_copied_confirmation(copy_button, on_hide_callback, timeout_in_ms = 1000) {
     // Display a tooltip to notify the user the message or code was copied.
-    const instance = tippy($copy_button, {
+    const instance = tippy(copy_button, {
         placement: "top",
         appendTo: () => document.body,
         onUntrigger() {
