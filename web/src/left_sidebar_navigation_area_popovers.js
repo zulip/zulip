26721767723fb7cc4bf0c42eb46ac251b72483f8
Aman Agrawal <amanagr@zulip.com>
2023-10-03 16:40:44 +0000

    left_sidebar: Move all messages popover to inbox.
    Also, renamed files and variables as needed.

--- web/src/left_sidebar_navigation_area_popovers.js
+++ web/src/left_sidebar_navigation_area_popovers.js
@@ -1,7 +1,7 @@
 import $ from "jquery";
 
-import render_all_messages_sidebar_actions from "../templates/all_messages_sidebar_actions.hbs";
 import render_drafts_sidebar_actions from "../templates/drafts_sidebar_action.hbs";
+import render_left_sidebar_inbox_popover from "../templates/left_sidebar_inbox_popover.hbs";
 import render_starred_messages_sidebar_actions from "../templates/starred_messages_sidebar_actions.hbs";
 
 import * as channel from "./channel";
@@ -81,12 +81,12 @@ export function initialize() {
         },
     });
 
-    // All messages popover
-    popover_menus.register_popover_menu(".all-messages-sidebar-menu-icon", {
+    // Inbox popover
+    popover_menus.register_popover_menu(".inbox-sidebar-menu-icon", {
         ...popover_menus.left_sidebar_tippy_options,
         onMount(instance) {
             const $popper = $(instance.popper);
-            popover_menus.popover_instances.all_messages = instance;
+            popover_menus.popover_instances.left_sidebar_inbox_popover = instance;
 
             $popper.one("click", "#mark_all_messages_as_read", () => {
                 unread_ops.confirm_mark_all_as_read();
@@ -95,11 +95,11 @@ export function initialize() {
         },
         onShow(instance) {
             popovers.hide_all();
-            instance.setContent(parse_html(render_all_messages_sidebar_actions()));
+            instance.setContent(parse_html(render_left_sidebar_inbox_popover()));
         },
         onHidden(instance) {
             instance.destroy();
-            popover_menus.popover_instances.all_messages = undefined;
+            popover_menus.popover_instances.left_sidebar_inbox_popover = undefined;
         },
     });
 }
