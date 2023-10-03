5efbece30aafd85970f2c6afa335285b915c2951
Anders Kaseorg <anders@zulip.com>
2023-10-03 01:38:49 -0700

    stream_color: Extract update_stream_color to new module.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- web/src/stream_color.js
+++ web/src/stream_color.js
@@ -4,11 +4,6 @@ import mixPlugin from "colord/plugins/mix";
 import $ from "jquery";
 
 import {$t} from "./i18n";
-import * as inbox_util from "./inbox_util";
-import * as message_lists from "./message_lists";
-import * as message_view_header from "./message_view_header";
-import * as overlays from "./overlays";
-import * as row from "./rows";
 import * as settings_data from "./settings_data";
 import * as stream_data from "./stream_data";
 import * as stream_settings_ui from "./stream_settings_ui";
@@ -52,47 +47,6 @@ export function get_recipient_bar_color(color) {
         .toHex();
 }
 
-function update_table_message_recipient_stream_color(table, stream_name, recipient_bar_color) {
-    const $stream_labels = table.find(".stream_label");
-    for (const label of $stream_labels) {
-        const $label = $(label);
-        if ($label.text().trim() === stream_name) {
-            $label
-                .closest(".message_header_stream .message-header-contents")
-                .css({background: recipient_bar_color});
-        }
-    }
-}
-
-function update_stream_privacy_color(id, color) {
-    $(`.stream-privacy-original-color-${CSS.escape(id)}`).css("color", color);
-    color = get_stream_privacy_icon_color(color);
-    // `modified-color` is only used in recipient bars.
-    $(`.stream-privacy-modified-color-${CSS.escape(id)}`).css("color", color);
-}
-
-function update_message_recipient_color(stream_name, color) {
-    const recipient_color = get_recipient_bar_color(color);
-    for (const msg_list of message_lists.all_rendered_message_lists()) {
-        const $table = row.get_table(msg_list.table_name);
-        update_table_message_recipient_stream_color($table, stream_name, recipient_color);
-    }
-
-    // Update color for drafts if open.
-    if (overlays.drafts_open()) {
-        update_table_message_recipient_stream_color(
-            $(".drafts-container"),
-            stream_name,
-            recipient_color,
-        );
-    }
-
-    if (inbox_util.is_visible()) {
-        const stream_id = stream_data.get_stream_id(stream_name);
-        $(`#inbox-stream-header-${stream_id}`).css("background", recipient_color);
-    }
-}
-
 const stream_color_palette = [
     ["a47462", "c2726a", "e4523d", "e7664d", "ee7e4a", "f4ae55"],
     ["76ce90", "53a063", "94c849", "bfd56f", "fae589", "f5ce6e"],
@@ -113,34 +67,6 @@ export function set_colorpicker_color(colorpicker, color) {
         color,
         container: "#subscription_overlay .subscription_settings.show",
     });
-}
-
-export function update_stream_color(sub, color) {
-    sub.color = color;
-    const stream_id = sub.stream_id;
-    // The swatch in the subscription row header.
-    $(`.stream-row[data-stream-id='${CSS.escape(stream_id)}'] .icon`).css(
-        "background-color",
-        color,
-    );
-    // The swatch in the color picker.
-    set_colorpicker_color(
-        $(
-            `#subscription_overlay .subscription_settings[data-stream-id='${CSS.escape(
-                stream_id,
-            )}'] .colorpicker`,
-        ),
-        color,
-    );
-    $(
-        `#subscription_overlay .subscription_settings[data-stream-id='${CSS.escape(
-            stream_id,
-        )}'] .large-icon`,
-    ).css("color", color);
-
-    update_message_recipient_color(sub.name, color);
-    update_stream_privacy_color(stream_id, color);
-    message_view_header.colorize_message_view_header();
 }
 
 export const sidebar_popover_colorpicker_options_full = {
