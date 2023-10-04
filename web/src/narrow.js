3d804ef1860dda2b6b40d63a0f94b0b02a1ae530
Tim Abbott <tabbott@zulip.com>
2023-10-03 18:05:27 -0700

    pm_list: Rename handle_narrow_deactivated.
    It did the same thing as handle_narrow_activated when passed the
    home_msg_list filter.
    
    We still leave a renamed version for use by the inbox/recent views.

--- web/src/narrow.js
+++ web/src/narrow.js
@@ -559,9 +559,6 @@ export function activate(raw_operators, opts) {
 
         compose_actions.on_narrow(opts);
 
-        const current_filter = narrow_state.filter();
-
-        pm_list.handle_narrow_activated(current_filter);
         unread_ui.update_unread_banner();
 
         // It is important to call this after other important updates
@@ -1021,6 +1018,7 @@ function handle_post_view_change(msg_list) {
     update_narrow_title(filter);
     left_sidebar_navigation_area.handle_narrow_activated(filter);
     stream_list.handle_narrow_activated(filter);
+    pm_list.handle_narrow_activated(filter);
 }
 
 function handle_post_narrow_deactivate_processes(msg_list) {
@@ -1028,7 +1026,6 @@ function handle_post_narrow_deactivate_processes(msg_list) {
 
     compose_fade.update_message_list();
 
-    pm_list.handle_narrow_deactivated();
     message_edit.handle_narrow_deactivated();
     widgetize.set_widgets_for_list();
     message_feed_top_notices.update_top_of_narrow_notices(msg_list);
