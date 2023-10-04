ecba66e613897b1df76a5658792ee1d0b3534ec5
Aman Agrawal <amanagr@zulip.com>
2023-10-04 06:20:38 +0000

    compose_popovers: Rename to remove `_content`.

--- web/src/compose_popovers.js
+++ web/src/compose_popovers.js
@@ -3,7 +3,7 @@ import {delegate} from "tippy.js";
 
 import render_compose_control_buttons_popover from "../templates/compose_control_buttons_popover.hbs";
 import render_compose_select_enter_behaviour_popover from "../templates/compose_select_enter_behaviour_popover.hbs";
-import render_mobile_message_buttons_popover_content from "../templates/mobile_message_buttons_popover_content.hbs";
+import render_mobile_message_buttons_popover from "../templates/mobile_message_buttons_popover.hbs";
 
 import * as channel from "./channel";
 import * as common from "./common";
@@ -34,7 +34,7 @@ export function initialize() {
             popover_menus.on_show_prep(instance);
             instance.setContent(
                 parse_html(
-                    render_mobile_message_buttons_popover_content({
+                    render_mobile_message_buttons_popover({
                         is_in_private_narrow: narrow_state.narrowed_to_pms(),
                     }),
                 ),
