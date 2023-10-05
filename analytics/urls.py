40e6455579344c6dd6e6f545fc0f78927eba1347
Lauryn Menard <lauryn@MacBook-Pro.local>
2023-10-05 19:51:58 +0200

    analytics: Move integrations by client to separate activity page.
    Moves the tab in the main installation activity page to be a
    separate page: "/activity/integrations".

--- analytics/urls.py
+++ analytics/urls.py
@@ -4,7 +4,10 @@ from django.conf.urls import include
 from django.urls import path
 from django.urls.resolvers import URLPattern, URLResolver
 
-from analytics.views.installation_activity import get_installation_activity
+from analytics.views.installation_activity import (
+    get_installation_activity,
+    get_integrations_activity,
+)
 from analytics.views.realm_activity import get_realm_activity
 from analytics.views.remote_activity import get_remote_server_activity
 from analytics.views.stats import (
@@ -27,6 +30,7 @@ i18n_urlpatterns: List[Union[URLPattern, URLResolver]] = [
     # Server admin (user_profile.is_staff) visible stats pages
     path("activity", get_installation_activity),
     path("activity/remote", get_remote_server_activity),
+    path("activity/integrations", get_integrations_activity),
     path("activity/support", support, name="support"),
     path("activity/remote/support", remote_servers_support, name="remote_servers_support"),
     path("realm_activity/<realm_str>/", get_realm_activity),
