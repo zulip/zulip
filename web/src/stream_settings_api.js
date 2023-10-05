ba8e76116c6bdb096efd54741cb18694bc81db88
Anders Kaseorg <anders@zulip.com>
2023-10-04 23:17:51 -0700

    settings_ui: Fix type and $ prefix of status_element argument.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- web/src/stream_settings_api.js
+++ web/src/stream_settings_api.js
@@ -1,10 +1,10 @@
 import * as channel from "./channel";
 import * as settings_ui from "./settings_ui";
 
-export function bulk_set_stream_property(sub_data, status_element) {
+export function bulk_set_stream_property(sub_data, $status_element) {
     const url = "/json/users/me/subscriptions/properties";
     const data = {subscription_data: JSON.stringify(sub_data)};
-    if (!status_element) {
+    if (!$status_element) {
         return channel.post({
             url,
             data,
@@ -12,11 +12,11 @@ export function bulk_set_stream_property(sub_data, status_element) {
         });
     }
 
-    settings_ui.do_settings_change(channel.post, url, data, status_element);
+    settings_ui.do_settings_change(channel.post, url, data, $status_element);
     return undefined;
 }
 
-export function set_stream_property(sub, property, value, status_element) {
+export function set_stream_property(sub, property, value, $status_element) {
     const sub_data = {stream_id: sub.stream_id, property, value};
-    bulk_set_stream_property([sub_data], status_element);
+    bulk_set_stream_property([sub_data], $status_element);
 }
