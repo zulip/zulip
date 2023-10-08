7330fb3b64c163a4cd5a63656b830517d9628248
Aman Agrawal <amanagr@zulip.com>
2023-10-08 09:52:25 +0000

    inbox: Add topic visibility indicator.

--- web/src/inbox_ui.js
+++ web/src/inbox_ui.js
@@ -16,6 +16,7 @@ import * as left_sidebar_navigation_area from "./left_sidebar_navigation_area";
 import {localstorage} from "./localstorage";
 import * as message_store from "./message_store";
 import * as overlays from "./overlays";
+import {page_params} from "./page_params";
 import * as people from "./people";
 import * as popovers from "./popovers";
 import * as sidebar_ui from "./sidebar_ui";
@@ -38,7 +39,8 @@ const COLUMNS = {
     COLLAPSE_BUTTON: 0,
     RECIPIENT: 1,
     UNREAD_COUNT: 2,
-    ACTION_MENU: 3,
+    TOPIC_VISIBILITY: 3,
+    ACTION_MENU: 4,
 };
 let col_focus = COLUMNS.COLLAPSE_BUTTON;
 let row_focus = 0;
@@ -310,6 +312,15 @@ function format_topic(stream_id, topic, topic_unread_count, latest_msg_id) {
         is_collapsed: collapsed_containers.has(STREAM_HEADER_PREFIX + stream_id),
         mention_in_unread: unread.topic_has_any_unread_mentions(stream_id, topic),
         latest_msg_id,
+        // The following two fields (development & all_visibility_policies)
+        // are not specific to this context, but this is the easiest way
+        // we've figured out for passing the data to the template rendering.
+        development: page_params.development_environment,
+        all_visibility_policies: user_topics.all_visibility_policies,
+        visibility_policy: user_topics.get_topic_visibility_policy(stream_id, topic),
+        topic_muted: Boolean(user_topics.is_topic_muted(stream_id, topic)),
+        topic_unmuted: Boolean(user_topics.is_topic_unmuted(stream_id, topic)),
+        stream_muted: sub_store.get(stream_id).is_muted,
     };
 
     return context;
@@ -680,7 +691,9 @@ function set_list_focus(input_key) {
     }
 
     const $row_to_focus = $($all_rows.get(row_focus));
-    // This includes a fake collapse button for `inbox-row`.
+    // This includes a fake collapse button for `inbox-row` and a fake topic visibility
+    // button for `inbox-header`. The fake buttons help simplify code here and
+    // `$($cols_to_focus[col_focus]).trigger("focus");` at the end of this function.
     const $cols_to_focus = [$row_to_focus, ...$row_to_focus.find("[tabindex=0]")];
     const total_cols = $cols_to_focus.length;
     current_focus_id = $row_to_focus.attr("id");
@@ -717,6 +730,14 @@ function set_list_focus(input_key) {
             // For `inbox-row`, we focus entier row for COLUMNS.RECIPIENT.
             $row_to_focus.trigger("focus");
             return;
+        }
+    } else if (is_header_row && col_focus === COLUMNS.TOPIC_VISIBILITY) {
+        // `inbox-header` doesn't have a topic visibility indicator, so focus on
+        // button around it instead.
+        if (LEFT_NAVIGATION_KEYS.includes(input_key)) {
+            col_focus = COLUMNS.UNREAD_COUNT;
+        } else {
+            col_focus = COLUMNS.ACTION_MENU;
         }
     }
 
@@ -948,10 +969,19 @@ function get_focus_class_for_header() {
 
 function get_focus_class_for_row() {
     let focus_class = ".inbox-left-part";
-    if (col_focus === COLUMNS.UNREAD_COUNT) {
-        focus_class = ".unread_count";
-    } else if (col_focus === COLUMNS.ACTION_MENU) {
-        focus_class = ".inbox-topic-menu";
+    switch (col_focus) {
+        case COLUMNS.UNREAD_COUNT: {
+            focus_class = ".unread_count";
+            break;
+        }
+        case COLUMNS.ACTION_MENU: {
+            focus_class = ".inbox-topic-menu";
+            break;
+        }
+        case COLUMNS.TOPIC_VISIBILITY: {
+            focus_class = ".change_visibility_policy";
+            break;
+        }
     }
     return focus_class;
 }
