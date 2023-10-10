b9db04b0269af2d22b6864a2e53c77e7b882fec0
Anders Kaseorg <anders@zulip.com>
2023-10-09 18:44:51 -0700

    stream_edit: Extract toggler to new stream_edit_toggler module.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

---
+++ web/src/stream_edit_toggler.js
@@ -0,0 +1,23 @@
+import $ from "jquery";
+
+import * as components from "./components";
+import {$t} from "./i18n";
+
+export let toggler;
+export let select_tab = "personal_settings";
+
+export function setup_toggler() {
+    toggler = components.toggle({
+        child_wants_focus: true,
+        values: [
+            {label: $t({defaultMessage: "General"}), key: "general_settings"},
+            {label: $t({defaultMessage: "Personal"}), key: "personal_settings"},
+            {label: $t({defaultMessage: "Subscribers"}), key: "subscriber_settings"},
+        ],
+        callback(_name, key) {
+            $(".stream_section").hide();
+            $(`.${CSS.escape(key)}`).show();
+            select_tab = key;
+        },
+    });
+}
