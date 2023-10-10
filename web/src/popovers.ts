2163cb77701e0d2c49e59ef714178710e303bbc2
Anders Kaseorg <anders@zulip.com>
2023-10-10 16:14:01 -0700

    popovers: Convert module to TypeScript.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- web/src/popovers.ts
+++ web/src/popovers.ts
@@ -1,9 +1,9 @@
 import $ from "jquery";
 import * as tippy from "tippy.js";
 
-export function any_active() {
+export function any_active(): boolean {
     // Checks if there are any interactive tippy instances (= popovers) present.
-    const $tippy_instances = $("[data-tippy-root]");
+    const $tippy_instances = $<tippy.PopperElement>("div[data-tippy-root]");
     // Tippy instances with `interactive: true` are popovers by definition.
     const num_interactive_instances = $tippy_instances.filter(
         (_i, elt) => elt._tippy?.props.interactive === true,
@@ -11,7 +11,7 @@ export function any_active() {
     return Boolean(num_interactive_instances);
 }
 
-export function hide_all() {
+export function hide_all(): void {
     // Hides all tippy instances (tooltips and popovers).
     tippy.hideAll();
 }
