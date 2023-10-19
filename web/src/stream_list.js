26f7f35656e8cec8601839defd2aed814a05c3cd
roanster007 <rohan.gudimetla07@gmail.com>
2023-10-19 21:34:08 +0530

    toggle-stream-subtopics: Expand or collapse stream subtopics on clicking the respective stream topics.

--- web/src/stream_list.js
+++ web/src/stream_list.js
@@ -788,13 +788,21 @@ export function set_event_handlers({on_stream_click}) {
         if (e.metaKey || e.ctrlKey) {
             return;
         }
-        const stream_id = stream_id_for_elt($(e.target).parents("li"));
-        on_stream_click(stream_id, "sidebar");
 
-        clear_and_hide_search();
+        if ($("ul#stream_filters li").hasClass("active-filter stream-expanded")) {
+            $("ul#stream_filters li").removeClass("active-filter stream-expanded");
+            $("ul.topic-list").hide();
+        } else {
+            $("ul#stream_filters li").addClass("active-filter stream-expanded");
+            $("ul.topic-list").show();
+            const stream_id = stream_id_for_elt($(e.target).parents("li"));
+            on_stream_click(stream_id, "sidebar");
 
-        e.preventDefault();
-        e.stopPropagation();
+            clear_and_hide_search();
+
+            e.preventDefault();
+            e.stopPropagation();
+        }
     });
 
     $("#clear_search_stream_button").on("click", clear_search);
