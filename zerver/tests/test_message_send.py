b0ef76bf276fecbf3dff605fdd85610af6b7075e
Prakhar Pratyush <prakhar@zulip.com>
2023-10-19 21:41:53 +0530

    topic_mentions: Set 'topic_wildcard_mentioned' flag on @topic mention.
    Earlier, the 'wildcard_mentioned' flag was set for both the
    stream and topic wildcard mentions.
    
    Now, the 'topic_wildcard_mentioned' flag is set for topic
    wildcard mentions, and the 'wildcard_mentioned' flag is set for
    stream wildcard mentions.
    
    We will rename the 'wildcard_mentioned' flag to
    'stream_wildcard_mentioned' in a later commit.

--- zerver/tests/test_message_send.py
+++ zerver/tests/test_message_send.py
@@ -1911,8 +1911,8 @@ class StreamMessagesTest(ZulipTestCase):
         self.send_and_verify_stream_wildcard_mention_message("iago", test_fails=True)
         self.send_and_verify_stream_wildcard_mention_message("iago", sub_count=10)
 
-    def test_wildcard_mentioned_flag_topic_wildcard_mention(self) -> None:
-        # For topic wildcard mentions, the 'wildcard_mentioned' flag should be
+    def test_topic_wildcard_mentioned_flag(self) -> None:
+        # For topic wildcard mentions, the 'topic_wildcard_mentioned' flag should be
         # set for all the user messages for topic participants, irrespective of
         # their notifications settings.
         cordelia = self.example_user("cordelia")
@@ -1934,7 +1934,7 @@ class StreamMessagesTest(ZulipTestCase):
         self.assertTrue(
             UserMessage.objects.get(
                 user_profile=cordelia, message=message
-            ).flags.wildcard_mentioned.is_set
+            ).flags.topic_wildcard_mentioned.is_set
         )
 
         self.send_stream_message(hamlet, "Denmark", content="test", topic_name="topic-2")
@@ -1944,7 +1944,7 @@ class StreamMessagesTest(ZulipTestCase):
         self.assertTrue(
             UserMessage.objects.get(
                 user_profile=hamlet, message=message
-            ).flags.wildcard_mentioned.is_set
+            ).flags.topic_wildcard_mentioned.is_set
         )
 
         do_change_user_setting(iago, "wildcard_mentions_notify", True, acting_user=None)
@@ -1953,7 +1953,7 @@ class StreamMessagesTest(ZulipTestCase):
         self.assertFalse(
             UserMessage.objects.get(
                 user_profile=iago, message=message
-            ).flags.wildcard_mentioned.is_set
+            ).flags.topic_wildcard_mentioned.is_set
         )
 
     def test_invalid_wildcard_mention_policy(self) -> None:
