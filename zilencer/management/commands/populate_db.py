zulip/zulip

commit 58568a60d651ee2909b181366bfce5edc43b33f0

settings: Add automatically follow and unmute topics policy settings.
This commit adds two user settings, named
* `automatically_follow_topics_policy`
* `automatically_unmute_topics_in_muted_streams_policy`

The settings control the user's preference on which topics they
will automatically 'follow' or 'unmute in muted streams'.

The policies offer four options:
1. Topics I participate in
2. Topics I send a message to
3. Topics I start
4. Never (default)

There is no support for configuring the settings through the UI yet.

zilencer/management/commands/populate_db.py
@@ -30,6 +30,7 @@ from zerver.actions.realm_linkifiers import do_add_linkifier
 from zerver.actions.scheduled_messages import check_schedule_message
 from zerver.actions.streams import bulk_add_subscriptions
 from zerver.actions.user_groups import create_user_group_in_database
+from zerver.actions.user_settings import do_change_user_setting
 from zerver.actions.users import do_change_user_role
 from zerver.lib.bulk_create import bulk_create_streams
 from zerver.lib.generate_test_data import create_test_data, generate_topics
@@ -824,6 +825,29 @@ class Command(BaseCommand):
         user_profiles: List[UserProfile] = list(
             UserProfile.objects.filter(is_bot=False, realm=zulip_realm)
         )
+
+        # As we plan to change the default values for 'automatically_follow_topics_policy' and
+        # 'automatically_unmute_topics_in_muted_streams_policy' in the future, it will lead to
+        # skewing a lot of our tests, which now need to take into account extra events and database queries.
+        #
+        # We explicitly set the values for both settings to 'AUTOMATICALLY_CHANGE_VISIBILITY_POLICY_NEVER'
+        # to make the tests independent of the default values.
+        #
+        # We have separate tests to verify events generated, database query counts,
+        # and other important details related to the above-mentioned settings.
+        for user in user_profiles:
+            do_change_user_setting(
+                user,
+                "automatically_follow_topics_policy",
+                UserProfile.AUTOMATICALLY_CHANGE_VISIBILITY_POLICY_NEVER,
+                acting_user=None,
+            )
+            do_change_user_setting(
+                user,
+                "automatically_unmute_topics_in_muted_streams_policy",
+                UserProfile.AUTOMATICALLY_CHANGE_VISIBILITY_POLICY_NEVER,
+                acting_user=None,
+            )
 
         # Create a test realm emoji.
         IMAGE_FILE_PATH = static_path("images/test-images/checkbox.png")

tools/linter_lib/custom_check.py
@@ -230,6 +230,9 @@ python_rules = RuleList(
             "good_lines": ["topic_name"],
             "bad_lines": ['subject="foo"', " MAX_SUBJECT_LEN"],
             "exclude": FILES_WITH_LEGACY_SUBJECT,
+            "exclude_line": {
+                ("zerver/lib/message.py", "message__subject__iexact=message.topic_name(),"),
+            },
             "include_only": {
                 "zerver/data_import/",
                 "zerver/lib/",

version.py
@@ -33,7 +33,7 @@ DESKTOP_WARNING_VERSION = "5.9.3"
 # Changes should be accompanied by documentation explaining what the
 # new level means in api_docs/changelog.md, as well as "**Changes**"
 # entries in the endpoint's documentation in `zulip.yaml`.
-API_FEATURE_LEVEL = 213
+API_FEATURE_LEVEL = 214
 
 # Bump the minor PROVISION_VERSION to indicate that folks should provision
 # only when going from an old version of the code to a newer version. Bump

zerver/actions/message_send.py
@@ -30,6 +30,7 @@ from django.utils.translation import override as override_language
 from django_stubs_ext import ValuesQuerySet
 
 from zerver.actions.uploads import do_claim_attachments
+from zerver.actions.user_topics import do_set_user_topic_visibility_policy
 from zerver.lib.addressee import Addressee
 from zerver.lib.alert_words import get_alert_word_automaton
 from zerver.lib.cache import cache_with_key, user_profile_delivery_email_cache_key
@@ -50,7 +51,9 @@ from zerver.lib.message import (
     check_user_group_mention_allowed,
     normalize_body,
     render_markdown,
+    set_visibility_policy_possible,
     truncate_topic,
+    visibility_policy_for_send_message,
     wildcard_mention_allowed,
 )
 from zerver.lib.muted_users import get_muting_users
@@ -180,6 +183,7 @@ class RecipientInfoResult:
     service_bot_tuples: List[Tuple[int, int]]
     all_bot_user_ids: Set[int]
     topic_participant_user_ids: Set[int]
+    sender_muted_stream: Optional[bool]
 
 
 class ActiveUserDict(TypedDict):
@@ -212,6 +216,7 @@ def get_recipient_info(
     stream_wildcard_mention_in_followed_topic_user_ids: Set[int] = set()
     muted_sender_user_ids: Set[int] = get_muting_users(sender_id)
     topic_participant_user_ids: Set[int] = set()
+    sender_muted_stream: Optional[bool] = None
 
     if recipient.type == Recipient.PERSONAL:
         # The sender and recipient may be the same id, so
@@ -275,7 +280,14 @@ def get_recipient_info(
             .order_by("user_profile_id")
         )
 
-        message_to_user_ids = [row["user_profile_id"] for row in subscription_rows]
+        message_to_user_ids = list()
+        for row in subscription_rows:
+            message_to_user_ids.append(row["user_profile_id"])
+            # We store the 'sender_muted_stream' information here to avoid db query at
+            # a later stage when we perform automatically unmute topic in muted stream operation.
+            if row["user_profile_id"] == sender_id:
+                sender_muted_stream = row["is_muted"]
+
         user_id_to_visibility_policy = stream_topic.user_id_to_visibility_policy_dict()
 
         def notification_recipients(setting: str) -> Set[int]:
@@ -466,6 +478,7 @@ def get_recipient_info(
         service_bot_tuples=service_bot_tuples,
         all_bot_user_ids=all_bot_user_ids,
         topic_participant_user_ids=topic_participant_user_ids,
+        sender_muted_stream=sender_muted_stream,
     )
 
 
@@ -642,6 +655,7 @@ def build_message_send_dict(
 
     message_send_dict = SendMessageRequest(
         stream=stream,
+        sender_muted_stream=info.sender_muted_stream,
         local_id=local_id,
         sender_queue_id=sender_queue_id,
         realm=realm,
@@ -896,6 +910,8 @@ def do_send_messages(
 
     # This next loop is responsible for notifying other parts of the
     # Zulip system about the messages we just committed to the database:
+    # * Sender automatically follows or unmutes the topic depending on 'automatically_follow_topics_policy'
+    #   and 'automatically_unmute_topics_in_muted_streams_policy' user settings.
     # * Notifying clients via send_event
     # * Triggering outgoing webhooks via the service event queue.
     # * Updating the `first_message_id` field for streams without any message history.
@@ -910,6 +926,39 @@ def do_send_messages(
             # assert needed because stubs for django are missing
             assert send_request.stream is not None
             realm_id = send_request.stream.realm_id
+            sender = send_request.message.sender
+
+            # Determine and set the visibility_policy depending on 'automatically_follow_topics_policy'
+            # and 'automatically_unmute_topics_in_muted_streams_policy'.
+            if set_visibility_policy_possible(sender, send_request.message) and not (
+                sender.automatically_follow_topics_policy
+                == sender.automatically_unmute_topics_in_muted_streams_policy
+                == UserProfile.AUTOMATICALLY_CHANGE_VISIBILITY_POLICY_NEVER
+            ):
+                try:
+                    user_topic = UserTopic.objects.get(
+                        user_profile=sender,
+                        stream_id=send_request.stream.id,
+                        topic_name__iexact=send_request.message.topic_name(),
+                    )
+                    visibility_policy = user_topic.visibility_policy
+                except UserTopic.DoesNotExist:
+                    visibility_policy = UserTopic.VisibilityPolicy.INHERIT
+
+                new_visibility_policy = visibility_policy_for_send_message(
+                    sender,
+                    send_request.message,
+                    send_request.stream,
+                    send_request.sender_muted_stream,
+                    visibility_policy,
+                )
+                if new_visibility_policy:
+                    do_set_user_topic_visibility_policy(
+                        user_profile=sender,
+                        stream=send_request.stream,
+                        topic=send_request.message.topic_name(),
+                        visibility_policy=new_visibility_policy,
+                    )
 
         # Deliver events to the real-time push system, as well as
         # enqueuing any additional processing triggered by the message.

zerver/actions/reactions.py
@@ -1,10 +1,18 @@
 from typing import Any, Dict, Optional
 
 from zerver.actions.create_user import create_historical_user_messages
+from zerver.actions.user_topics import do_set_user_topic_visibility_policy
 from zerver.lib.emoji import check_emoji_request, get_emoji_data
 from zerver.lib.exceptions import ReactionExistsError
-from zerver.lib.message import access_message, update_to_dict_cache
+from zerver.lib.message import (
+    access_message,
+    set_visibility_policy_possible,
+    should_change_visibility_policy,
+    update_to_dict_cache,
+    visibility_policy_for_participation,
+)
 from zerver.lib.stream_subscription import subscriber_ids_with_stream_history_access
+from zerver.lib.streams import access_stream_by_id
 from zerver.models import Message, Reaction, Recipient, Stream, UserMessage, UserProfile
 from zerver.tornado.django_api import send_event_on_commit
 
@@ -81,6 +89,32 @@ def do_add_reaction(
     )
 
     reaction.save()
+
+    # Determine and set the visibility_policy depending on 'automatically_follow_topics_policy'
+    # and 'automatically_unmute_topics_in_muted_streams_policy'.
+    if set_visibility_policy_possible(
+        user_profile, message
+    ) and UserProfile.AUTOMATICALLY_CHANGE_VISIBILITY_POLICY_ON_PARTICIPATION in [
+        user_profile.automatically_follow_topics_policy,
+        user_profile.automatically_unmute_topics_in_muted_streams_policy,
+    ]:
+        stream_id = message.recipient.type_id
+        (stream, sub) = access_stream_by_id(user_profile, stream_id)
+        assert stream is not None
+        if sub:
+            new_visibility_policy = visibility_policy_for_participation(user_profile, sub.is_muted)
+            if new_visibility_policy and should_change_visibility_policy(
+                new_visibility_policy,
+                user_profile,
+                stream_id,
+                topic_name=message.topic_name(),
+            ):
+                do_set_user_topic_visibility_policy(
+                    user_profile=user_profile,
+                    stream=stream,
+                    topic=message.topic_name(),
+                    visibility_policy=new_visibility_policy,
+                )
 
     notify_reaction_update(user_profile, message, reaction, "add")
 

zerver/actions/submessage.py
@@ -1,7 +1,14 @@
 from django.utils.translation import gettext as _
 
+from zerver.actions.user_topics import do_set_user_topic_visibility_policy
 from zerver.lib.exceptions import JsonableError
-from zerver.models import Realm, SubMessage, UserMessage
+from zerver.lib.message import (
+    set_visibility_policy_possible,
+    should_change_visibility_policy,
+    visibility_policy_for_participation,
+)
+from zerver.lib.streams import access_stream_by_id
+from zerver.models import Realm, SubMessage, UserMessage, UserProfile
 from zerver.tornado.django_api import send_event_on_commit
 
 
@@ -47,6 +54,33 @@ def do_add_submessage(
         content=content,
     )
     submessage.save()
+
+    # Determine and set the visibility_policy depending on 'automatically_follow_topics_policy'
+    # and 'automatically_unmute_topics_policy'.
+    sender = submessage.sender
+    if set_visibility_policy_possible(
+        sender, submessage.message
+    ) and UserProfile.AUTOMATICALLY_CHANGE_VISIBILITY_POLICY_ON_PARTICIPATION in [
+        sender.automatically_follow_topics_policy,
+        sender.automatically_unmute_topics_in_muted_streams_policy,
+    ]:
+        stream_id = submessage.message.recipient.type_id
+        (stream, sub) = access_stream_by_id(sender, stream_id)
+        assert stream is not None
+        if sub:
+            new_visibility_policy = visibility_policy_for_participation(sender, sub.is_muted)
+            if new_visibility_policy and should_change_visibility_policy(
+                new_visibility_policy,
+                sender,
+                stream_id,
+                topic_name=submessage.message.topic_name(),
+            ):
+                do_set_user_topic_visibility_policy(
+                    user_profile=sender,
+                    stream=stream,
+                    topic=submessage.message.topic_name(),
+                    visibility_policy=new_visibility_policy,
+                )
 
     event = dict(
         type="submessage",

zerver/lib/message.py
@@ -20,7 +20,7 @@ import ahocorasick
 import orjson
 from django.conf import settings
 from django.db import connection
-from django.db.models import Max, Sum
+from django.db.models import Max, QuerySet, Sum
 from django.utils.timezone import now as timezone_now
 from django.utils.translation import gettext as _
 from django_stubs_ext import ValuesQuerySet
@@ -47,9 +47,15 @@ from zerver.lib.stream_subscription import (
     get_subscribed_stream_recipient_ids_for_user,
     num_subscribers_for_stream_id,
 )
-from zerver.lib.streams import get_web_public_streams_queryset
+from zerver.lib.streams import can_access_stream_history, get_web_public_streams_queryset
 from zerver.lib.timestamp import datetime_to_timestamp
-from zerver.lib.topic import DB_TOPIC_NAME, MESSAGE__TOPIC, TOPIC_LINKS, TOPIC_NAME
+from zerver.lib.topic import (
+    DB_TOPIC_NAME,
+    MESSAGE__TOPIC,
+    TOPIC_LINKS,
+    TOPIC_NAME,
+    messages_for_topic,
+)
 from zerver.lib.types import DisplayRecipientT, EditHistoryEvent, UserDisplayRecipient
 from zerver.lib.url_preview.types import UrlEmbedData
 from zerver.lib.user_groups import is_user_in_group
@@ -147,6 +153,7 @@ class SendMessageRequest:
     message: Message
     rendering_result: MessageRenderingResult
     stream: Optional[Stream]
+    sender_muted_stream: Optional[bool]
     local_id: Optional[str]
     sender_queue_id: Optional[str]
     realm: Realm
@@ -1713,3 +1720,180 @@ def update_to_dict_cache(
 
     cache_set_many(items_for_remote_cache)
     return message_ids
+
+
+def visibility_policy_for_participation(
+    sender: UserProfile,
+    is_stream_muted: Optional[bool],
+) -> Optional[int]:
+    """
+    This function determines the visibility policy to set when a user
+    participates in a topic, depending on the 'automatically_follow_topics_policy'
+    and 'automatically_unmute_topics_in_muted_streams_policy' settings.
+    """
+    if (
+        sender.automatically_follow_topics_policy
+        == UserProfile.AUTOMATICALLY_CHANGE_VISIBILITY_POLICY_ON_PARTICIPATION
+    ):
+        return UserTopic.VisibilityPolicy.FOLLOWED
+
+    if (
+        is_stream_muted
+        and sender.automatically_unmute_topics_in_muted_streams_policy
+        == UserProfile.AUTOMATICALLY_CHANGE_VISIBILITY_POLICY_ON_PARTICIPATION
+    ):
+        return UserTopic.VisibilityPolicy.UNMUTED
+
+    return None
+
+
+def visibility_policy_for_send(
+    sender: UserProfile,
+    is_stream_muted: Optional[bool],
+) -> Optional[int]:
+    if (
+        sender.automatically_follow_topics_policy
+        == UserProfile.AUTOMATICALLY_CHANGE_VISIBILITY_POLICY_ON_SEND
+    ):
+        return UserTopic.VisibilityPolicy.FOLLOWED
+
+    if (
+        is_stream_muted
+        and sender.automatically_unmute_topics_in_muted_streams_policy
+        == UserProfile.AUTOMATICALLY_CHANGE_VISIBILITY_POLICY_ON_SEND
+    ):
+        return UserTopic.VisibilityPolicy.UNMUTED
+
+    return None
+
+
+def visibility_policy_for_send_message(
+    sender: UserProfile,
+    message: Message,
+    stream: Stream,
+    is_stream_muted: Optional[bool],
+    current_visibility_policy: int,
+) -> Optional[int]:
+    """
+    This function determines the visibility policy to set when a message
+    is sent to a topic, depending on the 'automatically_follow_topics_policy'
+    and 'automatically_unmute_topics_in_muted_streams_policy' settings.
+
+    It returns None when the policies can't make it more visible than the
+    current visibility policy.
+    """
+    # We prioritize 'FOLLOW' over 'UNMUTE' in muted streams.
+    # We need to carefully handle the following two cases:
+    #
+    # 1. When an action qualifies for multiple values. Example:
+    #    - starting a topic is INITIATION, PARTICIPATION as well as SEND
+    #    - sending a non-first message is PARTICIPATION as well as SEND
+    # action | 'automatically_follow_topics_policy' | 'automatically_unmute_topics_in_muted_streams_policy' | visibility_policy
+    #  start |    ON_PARTICIPATION / ON_SEND        |                   ON_INITIATION                       |     FOLLOWED
+    #  send  |    ON_SEND / ON_PARTICIPATION        |              ON_PARTICIPATION / ON_SEND               |     FOLLOWED
+    #
+    # 2. When both the policies have the same values.
+    # action | 'automatically_follow_topics_policy' | 'automatically_unmute_topics_in_muted_streams_policy' | visibility_policy
+    #  start |         ON_INITIATION                |                   ON_INITIATION                       |     FOLLOWED
+    #  partc |       ON_PARTICIPATION               |                 ON_PARTICIPATION                      |     FOLLOWED
+    #  send  |           ON_SEND                    |                     ON_SEND                           |     FOLLOWED
+    visibility_policy = None
+
+    if current_visibility_policy == UserTopic.VisibilityPolicy.FOLLOWED:
+        return visibility_policy
+
+    visibility_policy_participation = visibility_policy_for_participation(sender, is_stream_muted)
+    visibility_policy_send = visibility_policy_for_send(sender, is_stream_muted)
+
+    if UserTopic.VisibilityPolicy.FOLLOWED in (
+        visibility_policy_participation,
+        visibility_policy_send,
+    ):
+        return UserTopic.VisibilityPolicy.FOLLOWED
+
+    if UserTopic.VisibilityPolicy.UNMUTED in (
+        visibility_policy_participation,
+        visibility_policy_send,
+    ):
+        visibility_policy = UserTopic.VisibilityPolicy.UNMUTED
+
+    # If a topic has a visibility policy set, it can't be the case
+    # of initiation. We return early, thus saving a DB query.
+    if current_visibility_policy != UserTopic.VisibilityPolicy.INHERIT:
+        if visibility_policy and current_visibility_policy == visibility_policy:
+            return None
+        return visibility_policy
+
+    # Now we need to check if the user initiated the topic.
+    old_accessible_messages_in_topic: Union[QuerySet[Message], QuerySet[UserMessage]]
+    if can_access_stream_history(sender, stream):
+        old_accessible_messages_in_topic = messages_for_topic(
+            realm_id=sender.realm_id,
+            stream_recipient_id=message.recipient_id,
+            topic_name=message.topic_name(),
+        ).exclude(id=message.id)
+    else:
+        # We use the user's own message access to avoid leaking information in
+        # private streams with protected history.
+        old_accessible_messages_in_topic = UserMessage.objects.filter(
+            user_profile=sender,
+            message__recipient_id=message.recipient_id,
+            message__subject__iexact=message.topic_name(),
+        ).exclude(message_id=message.id)
+
+    if (
+        sender.automatically_follow_topics_policy
+        == UserProfile.AUTOMATICALLY_CHANGE_VISIBILITY_POLICY_ON_INITIATION
+        and not old_accessible_messages_in_topic.exists()
+    ):
+        return UserTopic.VisibilityPolicy.FOLLOWED
+
+    if (
+        is_stream_muted
+        and sender.automatically_unmute_topics_in_muted_streams_policy
+        == UserProfile.AUTOMATICALLY_CHANGE_VISIBILITY_POLICY_ON_INITIATION
+        and not old_accessible_messages_in_topic.exists()
+    ):
+        visibility_policy = UserTopic.VisibilityPolicy.UNMUTED
+
+    return visibility_policy
+
+
+def should_change_visibility_policy(
+    new_visibility_policy: int,
+    sender: UserProfile,
+    stream_id: int,
+    topic_name: str,
+) -> bool:
+    try:
+        user_topic = UserTopic.objects.get(
+            user_profile=sender, stream_id=stream_id, topic_name__iexact=topic_name
+        )
+    except UserTopic.DoesNotExist:
+        return True
+    current_visibility_policy = user_topic.visibility_policy
+
+    if new_visibility_policy == current_visibility_policy:
+        return False
+
+    # The intent of these "automatically follow or unmute" policies is that they
+    # can only increase the user's visibility policy for the topic. If a topic is
+    # already FOLLOWED, we don't change the state to UNMUTED due to these policies.
+    if current_visibility_policy == UserTopic.VisibilityPolicy.FOLLOWED:
+        return False
+
+    return True
+
+
+def set_visibility_policy_possible(user_profile: UserProfile, message: Message) -> bool:
+    """If the user can set a visibility policy."""
+    if not message.is_stream_message():
+        return False
+
+    if user_profile.is_bot:
+        return False
+
+    if user_profile.realm != message.get_realm():
+        return False
+
+    return True

zerver/migrations/0476_realmuserdefault_automatically_follow_topics_policy_and_more.py
@@ -1,0 +1,32 @@
+# Generated by Django 4.2.5 on 2023-09-19 10:52
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("zerver", "0475_realm_jitsi_server_url"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="realmuserdefault",
+            name="automatically_follow_topics_policy",
+            field=models.PositiveSmallIntegerField(default=3),
+        ),
+        migrations.AddField(
+            model_name="realmuserdefault",
+            name="automatically_unmute_topics_in_muted_streams_policy",
+            field=models.PositiveSmallIntegerField(default=3),
+        ),
+        migrations.AddField(
+            model_name="userprofile",
+            name="automatically_follow_topics_policy",
+            field=models.PositiveSmallIntegerField(default=3),
+        ),
+        migrations.AddField(
+            model_name="userprofile",
+            name="automatically_unmute_topics_in_muted_streams_policy",
+            field=models.PositiveSmallIntegerField(default=3),
+        ),
+    ]

commit 56488d55018935c97cb5828fbc0770fe277b387a

populate_db: Set user_topic policies to NEVER only for test-database.
Earlier, the 'automatically_follow_topics_policy' and
'automatically_unmute_topics_in_muted_streams_policy' were set
to 'NEVER' for both the test and dev databases.

This commit fixes the incorrect behavior.

Now, we set it to 'NEVER' only for the test-database.

We need the actual default value in our dev database.
We explicitly set it to NEVER for test-database as it skews other
tests by generating extra events and db queries. We have separate
tests with different values to test the intended behavior related
to these settings.

The bug was introduced in 58568a6.

zilencer/management/commands/populate_db.py
@@ -829,28 +829,29 @@ class Command(BaseCommand):
             UserProfile.objects.filter(is_bot=False, realm=zulip_realm)
         )
 
-        # As we plan to change the default values for 'automatically_follow_topics_policy' and
-        # 'automatically_unmute_topics_in_muted_streams_policy' in the future, it will lead to
-        # skewing a lot of our tests, which now need to take into account extra events and database queries.
-        #
-        # We explicitly set the values for both settings to 'AUTOMATICALLY_CHANGE_VISIBILITY_POLICY_NEVER'
-        # to make the tests independent of the default values.
-        #
-        # We have separate tests to verify events generated, database query counts,
-        # and other important details related to the above-mentioned settings.
-        for user in user_profiles:
-            do_change_user_setting(
-                user,
-                "automatically_follow_topics_policy",
-                UserProfile.AUTOMATICALLY_CHANGE_VISIBILITY_POLICY_NEVER,
-                acting_user=None,
-            )
-            do_change_user_setting(
-                user,
-                "automatically_unmute_topics_in_muted_streams_policy",
-                UserProfile.AUTOMATICALLY_CHANGE_VISIBILITY_POLICY_NEVER,
-                acting_user=None,
-            )
+        if options["test_suite"]:
+            # As we plan to change the default values for 'automatically_follow_topics_policy' and
+            # 'automatically_unmute_topics_in_muted_streams_policy' in the future, it will lead to
+            # skewing a lot of our tests, which now need to take into account extra events and database queries.
+            #
+            # We explicitly set the values for both settings to 'AUTOMATICALLY_CHANGE_VISIBILITY_POLICY_NEVER'
+            # to make the tests independent of the default values.
+            #
+            # We have separate tests to verify events generated, database query counts,
+            # and other important details related to the above-mentioned settings.
+            for user in user_profiles:
+                do_change_user_setting(
+                    user,
+                    "automatically_follow_topics_policy",
+                    UserProfile.AUTOMATICALLY_CHANGE_VISIBILITY_POLICY_NEVER,
+                    acting_user=None,
+                )
+                do_change_user_setting(
+                    user,
+                    "automatically_unmute_topics_in_muted_streams_policy",
+                    UserProfile.AUTOMATICALLY_CHANGE_VISIBILITY_POLICY_NEVER,
+                    acting_user=None,
+                )
 
         # Create a test realm emoji.
         IMAGE_FILE_PATH = static_path("images/test-images/checkbox.png")