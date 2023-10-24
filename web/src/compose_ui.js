160f80eff2c1867195a61b4c4bdbb8120fc10c4e
rahul <rahulrajesh2101@gmail.com>
2023-10-25 01:27:45 +0530

    compose_ui: use Intl.ListFormat rather than .join(", ").
    fixes zulip#26936.

--- web/src/compose_ui.js
+++ web/src/compose_ui.js
@@ -8,6 +8,7 @@ import {insert, replace, set, wrapSelection} from "text-field-edit";
 import * as bulleted_numbered_list_util from "./bulleted_numbered_list_util";
 import * as common from "./common";
 import {$t} from "./i18n";
+import {page_params} from "./page_params";
 import * as loading from "./loading";
 import * as people from "./people";
 import * as popover_menus from "./popover_menus";
@@ -249,7 +250,7 @@ export function compute_placeholder_text(opts) {
     // For direct messages
     if (opts.private_message_recipient) {
         const recipient_list = opts.private_message_recipient.split(",");
-        const recipient_names = recipient_list
+        const recipient_names = new Intl.ListFormat(page_params.request_language).format(recipient_list
             .map((recipient) => {
                 const user = people.get_by_email(recipient);
                 if (people.should_add_guest_user_indicator(user.user_id)) {
@@ -257,7 +258,7 @@ export function compute_placeholder_text(opts) {
                 }
                 return user.full_name;
             })
-            .join(", ");
+        )
 
         if (recipient_list.length === 1) {
             // If it's a single user, display status text if available
