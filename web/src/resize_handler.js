bee8ce4e56218bad5e93b0819c21da55901dc412
Aman Agrawal <amanagr@zulip.com>
2023-10-24 04:28:01 +0000

    popover_menus: Don't hide on scroll / resize.
    Tippyjs is equipped to handle reference element moving from
    its initial position so that the popover moves / changes
    along with the reference / window size.

--- web/src/resize_handler.js
+++ web/src/resize_handler.js
@@ -4,8 +4,6 @@ import * as compose_ui from "./compose_ui";
 import * as condense from "./condense";
 import * as message_lists from "./message_lists";
 import * as message_viewport from "./message_viewport";
-import * as popover_menus from "./popover_menus";
-import * as popovers from "./popovers";
 import * as resize from "./resize";
 import * as sidebar_ui from "./sidebar_ui";
 import * as util from "./util";
@@ -15,13 +13,9 @@ export let _old_width = $(window).width();
 export function handler() {
     const new_width = $(window).width();
 
-    // On mobile web, we want to avoid hiding a popover here on height change,
-    // especially if this resize was triggered by a virtual keyboard
-    // popping up when the user opened that very popover.
     const mobile = util.is_mobile();
     if (!mobile || new_width !== _old_width) {
         sidebar_ui.hide_all();
-        popovers.hide_all();
     }
 
     if (new_width !== _old_width) {
@@ -38,10 +32,6 @@ export function handler() {
     // but before we've loaded in the messages; in that case, don't
     // try to scroll to one.
     if (message_lists.current.selected_id() !== -1) {
-        if (mobile) {
-            popover_menus.set_suppress_scroll_hide();
-        }
-
         message_viewport.scroll_to_selected();
     }
 }
