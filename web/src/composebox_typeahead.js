79ebcc88abfc54a8c6b7a73a43288e924126e36f
DhruvGupta3377 <dhruvgupta3377@gmail.com>
2023-10-26 22:54:01 +0530

    added new typeaheads

--- web/src/composebox_typeahead.js
+++ web/src/composebox_typeahead.js
@@ -442,18 +442,18 @@ export const dev_only_slash_commands = [
 
 export const slash_commands = [
     {
-        text: $t({defaultMessage: "/me is excited (Display action text)"}),
+        text: $t({defaultMessage: "/me (Action Message)"}),
         name: "me",
         aliases: "",
     },
     {
-        text: $t({defaultMessage: "/poll Where should we go to lunch today? (Create a poll)"}),
+        text: $t({defaultMessage: "/poll (Create a poll)"}),
         name: "poll",
         aliases: "",
         placeholder: $t({defaultMessage: "Question"}),
     },
     {
-        text: $t({defaultMessage: "/todo (Create a todo list)"}),
+        text: $t({defaultMessage: "/todo (Create a collaborative to-do list)"}),
         name: "todo",
         aliases: "",
     },
