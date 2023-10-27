efd9036875cb6325399e2e9be9d3e5868fe4b4f9
David Rosa <rosatamsen@gmail.com>
2023-10-27 16:20:44 -0700

    help: Document option to paste URL to create a named link.
    - Adds a "Via paste" tab to document the new functionality.
    
    Fixes #26892.

--- zerver/lib/markdown/tabbed_sections.py
+++ zerver/lib/markdown/tabbed_sections.py
@@ -98,6 +98,7 @@ TAB_SECTION_LABELS = {
     "user": "User",
     "bot": "Bot",
     "on-sign-up": "On sign-up",
+    "via-paste": "Via paste",
     "via-markdown": "Via Markdown",
     "via-compose-box-buttons": "Via compose box buttons",
     "stream-compose": "Compose to a stream",
