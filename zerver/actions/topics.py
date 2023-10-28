0a6fc70c3985272e4ebf6d0f6d77d2772c74d3bd
Ammar Mirza <ammar.rashad1@gmail.com>
2023-10-28 18:42:57 +0500

    fix lint errors

--- zerver/actions/topics.py
+++ zerver/actions/topics.py
@@ -1,16 +1,9 @@
-from zerver.lib.streams import (
-    can_access_stream_user_ids,
-)
-from zerver.models import (
-    Topic,
-    UserProfile,
-    get_stream_by_id_in_realm,
-)
+from zerver.lib.streams import can_access_stream_user_ids
+from zerver.models import Topic, UserProfile, get_stream_by_id_in_realm
 from zerver.tornado.django_api import send_event
 
 
 def notify_topic_locked_status(topic: Topic, user_profile: UserProfile) -> None:
-
     stream = get_stream_by_id_in_realm(topic.stream.id, user_profile.realm)
 
     event = dict(
--- zerver/views/topics.py
+++ zerver/views/topics.py
@@ -1,7 +1,7 @@
 from django.http import HttpRequest, HttpResponse
+
 from zerver.actions.topics import notify_topic_locked_status
 from zerver.decorator import require_realm_moderator
-
 from zerver.lib.request import REQ, has_request_variables
 from zerver.lib.response import json_success
 from zerver.lib.topic import update_topic
@@ -21,13 +21,10 @@ def toggle_topic_locked_status(
         realm_id=user_profile.realm_id,
         stream_id=stream_id,
         topic_name=topic_name,
-        toggle_locked=True
+        toggle_locked=True,
     )
 
     if updated_topic is not None:
-        notify_topic_locked_status(
-            topic=updated_topic,
-            user_profile=user_profile
-        )
+        notify_topic_locked_status(topic=updated_topic, user_profile=user_profile)
 
     return json_success(request)
