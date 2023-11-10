92a4d93f511e6b2c74c4afca65797c8e07f065ff
Aman Agrawal <amanagr@zulip.com>
2023-11-10 12:53:08 +0000

    upgrade: Remove invoice payment instructions.

--- web/src/billing/upgrade.ts
+++ web/src/billing/upgrade.ts
@@ -5,7 +5,6 @@ import type {Prices} from "./helpers";
 import {page_params} from "./page_params";
 
 export const initialize = (): void => {
-    helpers.set_tab("upgrade");
     $("#add-card-button").on("click", (e) => {
         const license_management = $<HTMLInputElement>(
             "input[type=radio][name=license_management]:checked",
@@ -20,17 +19,6 @@ export const initialize = (): void => {
             window.location.replace(response_data.stripe_session_url);
         });
     });
-
-    $("#invoice-button").on("click", (e) => {
-        if (!helpers.is_valid_input($("#invoiced_licenses"))) {
-            return;
-        }
-        e.preventDefault();
-        helpers.create_ajax_request("/json/billing/upgrade", "invoice", [], "POST", () =>
-            window.location.replace("/billing/"),
-        );
-    });
-
     const prices: Prices = {
         annual: page_params.annual_price * (1 - page_params.percent_off / 100),
         monthly: page_params.monthly_price * (1 - page_params.percent_off / 100),
@@ -45,15 +33,6 @@ export const initialize = (): void => {
     );
     $("#autopay_monthly_price").text(
         `Pay monthly ($${helpers.format_money(prices.monthly)}/user/month)`,
-    );
-    $("#invoice_annual_price").text(helpers.format_money(prices.annual));
-    $("#invoice_annual_price_per_month").text(helpers.format_money(prices.annual / 12));
-
-    helpers.update_charged_amount(
-        prices,
-        helpers.schedule_schema.parse(
-            $<HTMLInputElement>("input[type=radio][name=schedule]:checked").val(),
-        ),
     );
 };
 
