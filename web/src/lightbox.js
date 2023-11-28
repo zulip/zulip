dc21c8c6bef02191ccc4f44b4b25ea51cb50b8dc
roanster007 <rohan.gudimetla07@gmail.com>
2023-11-28 18:31:02 +0530

    lightbox_view: Fix media title update on change in title.
    The metadata of the media is stored in Map for the session
    with the preview_src as keys, and hence, when title is
    changed the preview_src being same, the title change is
    not reflected inside lightbox view.
    
    This is fixed by modifing the asset_map (cache map) keys
    from preview_src to an array of preview_src, and the media
    title and using get_asset_map_key_for_media() to get
    the keys.
    
    Fixes #21311

--- web/src/lightbox.js
+++ web/src/lightbox.js
@@ -314,6 +314,10 @@ function display_video(payload) {
     $(".media-actions .open").attr("href", payload.url);
 }
 
+function get_asset_map_key_for_media($preview_src, $media) {
+    return [$preview_src, $media.parent().attr("aria-label") || $media.parent().attr("href")];
+}
+
 export function build_open_media_function(on_close) {
     if (on_close === undefined) {
         on_close = function () {
@@ -327,7 +331,7 @@ export function build_open_media_function(on_close) {
         // if the asset_map already contains the metadata required to display the
         // asset, just recall that metadata.
         let $preview_src = $media.attr("src");
-        let payload = asset_map.get($preview_src);
+        let payload = asset_map.get(get_asset_map_key_for_media($preview_src, $media));
         if (payload === undefined) {
             if ($preview_src.endsWith("&size=full")) {
                 // while fetching an image for canvas, `src` attribute supplies
@@ -341,7 +345,7 @@ export function build_open_media_function(on_close) {
                 // `data-thumbnail-src` attribute that we add to the
                 // canvas elements.
                 $preview_src = $preview_src.slice(0, -"full".length) + "thumbnail";
-                payload = asset_map.get($preview_src);
+                payload = asset_map.get(get_asset_map_key_for_media($preview_src, $media));
             }
             if (payload === undefined) {
                 payload = parse_media_data($media);
@@ -428,9 +432,9 @@ export function parse_media_data(media) {
     const $media = $(media);
     const preview_src = $media.attr("src");
 
-    if (asset_map.has(preview_src)) {
+    if (asset_map.has(get_asset_map_key_for_media(preview_src, $media))) {
         // check if media's data is already present in asset_map.
-        return asset_map.get(preview_src);
+        return asset_map.get(get_asset_map_key_for_media(preview_src, $media));
     }
 
     // if wrapped in the .youtube-video class, it will be length = 1, and therefore
@@ -504,7 +508,7 @@ export function parse_media_data(media) {
         url: util.is_valid_url(url) ? url : "",
     };
 
-    asset_map.set(preview_src, payload);
+    asset_map.set(get_asset_map_key_for_media(preview_src, $media), payload);
     return payload;
 }
 
