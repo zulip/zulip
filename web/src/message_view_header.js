408a273ba09746111bcc33616a6e6297d28c2ecd
Sayam Samal <samal.sayam@gmail.com>
2023-11-30 05:52:26 +0530

    message_view_header: Rename variables and function to specify context.
    In this commit, we rename the variables `message_view_header` and
    `message_view_header_data` with `context`. We also rename function
    `make_message_view_header` with `get_message_view_header_context`.
    
    This new naming convention provides better context about the use
    cases of the variables and functions.

--- web/src/message_view_header.js
+++ web/src/message_view_header.js
@@ -11,8 +11,8 @@ import * as recent_view_util from "./recent_view_util";
 import * as rendered_markdown from "./rendered_markdown";
 import * as search from "./search";
 
-function make_message_view_header(filter) {
-    const message_view_header = {};
+function get_message_view_header_context(filter) {
+    const context = {};
     if (recent_view_util.is_visible()) {
         return {
             title: $t({defaultMessage: "Recent conversations"}),
@@ -31,30 +31,30 @@ function make_message_view_header(filter) {
             zulip_icon: "all-messages",
         };
     }
-    message_view_header.title = filter.get_title();
-    message_view_header.is_spectator = page_params.is_spectator;
-    filter.add_icon_data(message_view_header);
+    context.title = filter.get_title();
+    context.is_spectator = page_params.is_spectator;
+    filter.add_icon_data(context);
     if (filter.has_operator("stream") && !filter._sub) {
-        message_view_header.sub_count = "0";
-        message_view_header.formatted_sub_count = "0";
-        message_view_header.rendered_narrow_description = $t({
+        context.sub_count = "0";
+        context.formatted_sub_count = "0";
+        context.rendered_narrow_description = $t({
             defaultMessage: "This stream does not exist or is private.",
         });
-        return message_view_header;
+        return context;
     }
     if (filter._sub) {
         // We can now be certain that the narrow
         // involves a stream which exists and
         // the current user can access.
         const current_stream = filter._sub;
-        message_view_header.rendered_narrow_description = current_stream.rendered_description;
+        context.rendered_narrow_description = current_stream.rendered_description;
         const sub_count = peer_data.get_subscriber_count(current_stream.stream_id);
-        message_view_header.sub_count = sub_count;
-        message_view_header.stream = current_stream;
-        message_view_header.stream_settings_link =
+        context.sub_count = sub_count;
+        context.stream = current_stream;
+        context.stream_settings_link =
             "#streams/" + current_stream.stream_id + "/" + current_stream.name;
     }
-    return message_view_header;
+    return context;
 }
 
 export function colorize_message_view_header() {
@@ -66,12 +66,12 @@ export function colorize_message_view_header() {
     $("#message_view_header a.stream i").css("color", filter._sub.color);
 }
 
-function append_and_display_title_area(message_view_header_data) {
+function append_and_display_title_area(context) {
     const $message_view_header_elem = $("#message_view_header");
     $message_view_header_elem.empty();
-    const rendered = render_message_view_header(message_view_header_data);
+    const rendered = render_message_view_header(context);
     $message_view_header_elem.append(rendered);
-    if (message_view_header_data.stream_settings_link) {
+    if (context.stream_settings_link) {
         colorize_message_view_header();
     }
     $message_view_header_elem.removeClass("notdisplayed");
@@ -89,8 +89,8 @@ function build_message_view_header(filter) {
         search.open_search_bar_and_close_narrow_description();
         $("#search_query").val(narrow_state.search_string());
     } else {
-        const message_view_header_data = make_message_view_header(filter);
-        append_and_display_title_area(message_view_header_data);
+        const context = get_message_view_header_context(filter);
+        append_and_display_title_area(context);
         search.close_search_bar_and_open_narrow_description();
     }
 }
