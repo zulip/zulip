f291eaa0a87af1b939eb0c819ed46f84c564bb07
Aman Agrawal <amanagr@zulip.com>
2023-12-01 05:44:59 +0000

    upgrade: Make card add / update work for remote servers.

--- corporate/views/event_status.py
+++ corporate/views/event_status.py
@@ -6,9 +6,15 @@ from django.shortcuts import render
 
 from corporate.lib.decorator import (
     authenticated_remote_realm_management_endpoint,
+    authenticated_remote_server_management_endpoint,
     self_hosting_management_endpoint,
 )
-from corporate.lib.stripe import EventStatusRequest, RealmBillingSession, RemoteRealmBillingSession
+from corporate.lib.stripe import (
+    EventStatusRequest,
+    RealmBillingSession,
+    RemoteRealmBillingSession,
+    RemoteServerBillingSession,
+)
 from zerver.decorator import require_organization_member, zulip_login_required
 from zerver.lib.response import json_success
 from zerver.lib.typed_endpoint import typed_endpoint
@@ -39,6 +45,22 @@ def event_status(
 def remote_realm_event_status(
     request: HttpRequest,
     billing_session: RemoteRealmBillingSession,
+    *,
+    stripe_session_id: Optional[str] = None,
+    stripe_payment_intent_id: Optional[str] = None,
+) -> HttpResponse:  # nocoverage
+    event_status_request = EventStatusRequest(
+        stripe_session_id=stripe_session_id, stripe_payment_intent_id=stripe_payment_intent_id
+    )
+    data = billing_session.get_event_status(event_status_request)
+    return json_success(request, data)
+
+
+@authenticated_remote_server_management_endpoint
+@typed_endpoint
+def remote_server_event_status(
+    request: HttpRequest,
+    billing_session: RemoteServerBillingSession,
     *,
     stripe_session_id: Optional[str] = None,
     stripe_payment_intent_id: Optional[str] = None,
