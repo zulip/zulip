bdea2b87b9191846657c6f8495279e072ac12b3f
Mateusz Mandera <mateusz.mandera@zulip.com>
2023-12-04 19:42:14 +0100

    populate_db: Create RemoteZulipServer with proper details from settings.
    This creates a valid registration, for two reasons:
    1. Avoid the need to run "manage.py register_server" in dev env to
       register, when wanting to to test stuff with
       `PUSH_NOTIFICATION_BOUNCER_URL = "http://localhost:9991"`.
    2. Avoid breaking RemoteRealm syncing, due to duplicate registrations
       (first set of registrations that gets set up with the dummy
       RemoteZulipServer in populate_db, and the second that gets set up via
       the regular syncing mechanism with the new RemoteZulipServer created
       during register_server).

--- zerver/lib/test_classes.py
+++ zerver/lib/test_classes.py
@@ -2385,11 +2385,12 @@ def get_topic_messages(user_profile: UserProfile, stream: Stream, topic_name: st
 class BouncerTestCase(ZulipTestCase):
     @override
     def setUp(self) -> None:
-        # Set a deterministic uuid for convenience.
+        # Set a deterministic uuid and a nice hostname for convenience.
         self.server_uuid = "6cde5f7a-1f7e-4978-9716-49f69ebfc9fe"
         self.server = RemoteZulipServer.objects.all().latest("id")
 
         self.server.uuid = self.server_uuid
+        self.server.hostname = "demo.example.com"
         self.server.save()
 
         super().setUp()
