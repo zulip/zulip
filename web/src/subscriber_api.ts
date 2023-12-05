02cbf92b368759e4b93ab88dacdecf51be5f98d6
Varun Singh <varunsingh9114@gmail.com>
2023-12-05 18:39:32 +0530

    subscriber_api.js:migrated to typescript

--- web/src/subscriber_api.ts
+++ web/src/subscriber_api.ts
@@ -1,6 +1,6 @@
 import * as channel from "./channel";
 import * as people from "./people";
-
+import type { StreamSubscription } from "./sub_store";
 /*
     This module simply encapsulates our legacy API for subscribing
     or unsubscribing users from streams. Callers don't need to
@@ -8,12 +8,17 @@ import * as people from "./people";
     nor how to JSON.stringify things, nor the URL scheme.
 */
 
-export function add_user_ids_to_stream(user_ids, sub, success, failure) {
+//declaring type for the function parameters
+type ids=number[];
+type succ_type=()=>void;
+type fail_type=(xhr:JQuery.jqXHR<unknown>)=>void;
+
+export function add_user_ids_to_stream(user_ids:ids, sub:StreamSubscription, success:succ_type, failure:fail_type):JQuery.jqXHR<unknown> | undefined {
     // TODO: use stream_id when backend supports it
-    const stream_name = sub.name;
+    const stream_name:string = sub.name;
     if (user_ids.length === 1 && people.is_my_user_id(Number(user_ids[0]))) {
         // Self subscribe
-        const color = sub.color;
+        const color:string = sub.color;
         return channel.post({
             url: "/json/users/me/subscriptions",
             data: {subscriptions: JSON.stringify([{name: stream_name, color}])},
@@ -32,7 +37,7 @@ export function add_user_ids_to_stream(user_ids, sub, success, failure) {
     });
 }
 
-export function remove_user_id_from_stream(user_id, sub, success, failure) {
+export function remove_user_id_from_stream(user_id:ids, sub:StreamSubscription, success:succ_type, failure:fail_type):JQuery.jqXHR<unknown> | undefined {
     // TODO: use stream_id when backend supports it
     const stream_name = sub.name;
     return channel.del({
