a780dda2ecdc9aef2aac455c44f92e33adbf49bb
Varun Singh <varunsingh9114@gmail.com>
2023-12-07 17:00:36 +0530

    ts:subscriber.ts migrated to typescipt

--- web/src/subscriber_api.ts
+++ web/src/subscriber_api.ts
@@ -1,6 +1,6 @@
 import * as channel from "./channel";
 import * as people from "./people";
-import type { StreamSubscription } from "./sub_store";
+import type {StreamSubscription} from "./sub_store";
 /*
     This module simply encapsulates our legacy API for subscribing
     or unsubscribing users from streams. Callers don't need to
@@ -8,17 +8,17 @@ import type { StreamSubscription } from "./sub_store";
     nor how to JSON.stringify things, nor the URL scheme.
 */
 
-//declaring type for the function parameters
-type ids=number[];
-type succ_type=()=>void;
-type fail_type=(xhr:JQuery.jqXHR<unknown>)=>void;
-
-export function add_user_ids_to_stream(user_ids:ids, sub:StreamSubscription, success:succ_type, failure:fail_type):JQuery.jqXHR<unknown> | undefined {
+export function add_user_ids_to_stream(
+    user_ids: number[],
+    sub: StreamSubscription,
+    success: () => void,
+    failure: (xhr: JQuery.jqXHR<unknown>) => void,
+): JQuery.jqXHR<unknown> | undefined {
     // TODO: use stream_id when backend supports it
-    const stream_name:string = sub.name;
+    const stream_name: string = sub.name;
     if (user_ids.length === 1 && people.is_my_user_id(Number(user_ids[0]))) {
         // Self subscribe
-        const color:string = sub.color;
+        const color = sub.color;
         return channel.post({
             url: "/json/users/me/subscriptions",
             data: {subscriptions: JSON.stringify([{name: stream_name, color}])},
@@ -37,7 +37,12 @@ export function add_user_ids_to_stream(user_ids:ids, sub:StreamSubscription, suc
     });
 }
 
-export function remove_user_id_from_stream(user_id:ids, sub:StreamSubscription, success:succ_type, failure:fail_type):JQuery.jqXHR<unknown> | undefined {
+export function remove_user_id_from_stream(
+    user_id: number[],
+    sub: StreamSubscription,
+    success: () => void,
+    failure: (xhr: JQuery.jqXHR<unknown>) => void,
+): JQuery.jqXHR<unknown> | undefined {
     // TODO: use stream_id when backend supports it
     const stream_name = sub.name;
     return channel.del({
