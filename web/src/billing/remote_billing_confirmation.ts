86d3693680d7f01626162d77f0e43f4473074450
Mateusz Mandera <mateusz.mandera@zulip.com>
2023-12-10 17:57:27 +0100

    remote_billing: Fix up templates and HTML names in them.
    These pages are generally used by both remoterealm and legacy server
    flows, so should have general names.

--- web/src/billing/remote_billing_confirmation.ts
+++ web/src/billing/remote_billing_confirmation.ts
@@ -1,23 +1,25 @@
 import $ from "jquery";
 
 export function initialize(): void {
-    $("#remote-realm-confirm-login-form").find(".loader").hide();
+    $("#remote-billing-confirm-login-form").find(".loader").hide();
 
-    $("#remote-realm-confirm-login-form").validate({
+    $("#remote-billing-confirm-login-form").validate({
         errorClass: "text-error",
         wrapper: "div",
         submitHandler(form) {
-            $("#remote-realm-confirm-login-form").find(".loader").show();
-            $("#remote-realm-confirm-login-button .remote-realm-confirm-login-button-text").hide();
+            $("#remote-billing-confirm-login-form").find(".loader").show();
+            $(
+                "#remote-billing-confirm-login-button .remote-billing-confirm-login-button-text",
+            ).hide();
 
             form.submit();
         },
         invalidHandler() {
-            $("#remote-realm-confirm-login-form .alert.alert-error").remove();
+            $("#remote-billing-confirm-login-form .alert.alert-error").remove();
         },
         showErrors(error_map) {
             if (error_map.id_terms) {
-                $("#remote-realm-confirm-login-form .alert.alert-error").remove();
+                $("#remote-billing-confirm-login-form .alert.alert-error").remove();
             }
             this.defaultShowErrors!();
         },
