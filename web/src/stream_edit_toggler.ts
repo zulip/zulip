f3fbe3b308599861dd9e3ea75d7b1b79231e15fc
roanster007 <rohan.gudimetla07@gmail.com>
2023-12-15 01:42:36 +0530

    stream_settings: Fix going back in stream settings modal.
    Previously, when we try to go back in the stream settings modal,
    we just cycled between different tabs of the top right corner.
    
    This is because, we were trying to set the browser history hash
    two times, once through subscription_stream_hash, and second
    when we setup stream settings, which invokes the setup_toggler,
    which sets the hash again in its callback. This causes the error.
    
    This is fixed by removing the portion which sets the browser
    history hash once more in the setup_toggler callback of
    stream_edit.

--- web/src/stream_edit_toggler.ts
+++ web/src/stream_edit_toggler.ts
@@ -4,11 +4,10 @@ import * as browser_history from "./browser_history";
 import * as components from "./components";
 import * as hash_util from "./hash_util";
 import {$t} from "./i18n";
-import * as sub_store from "./sub_store";
 import type {StreamSubscription} from "./sub_store";
 
 export let toggler: components.Toggle;
-export let select_tab = "personal";
+export const select_tab = "personal";
 
 export function setup_subscriptions_stream_hash(
     sub: StreamSubscription,
@@ -29,13 +28,6 @@ export function setup_toggler(): void {
         callback(_name, key) {
             $(".stream_section").hide();
             $(`[data-stream-section="${CSS.escape(key)}"]`).show();
-            select_tab = key;
-            const $stream_header = $("#streams_overlay_container .stream_settings_header");
-            const stream_id = Number.parseInt($stream_header.attr("data-stream-id") ?? "", 10);
-            const sub = sub_store.get(stream_id);
-            if (sub) {
-                setup_subscriptions_stream_hash(sub, select_tab);
-            }
         },
     });
 }
