25592eba2214c2cebba1f2c3732b4d4be6f493d1
Anders Kaseorg <anders@zulip.com>
2023-12-15 12:04:40 -0800

    models: Extract zerver.models.alert_words.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- zerver/models/__init__.py
+++ zerver/models/__init__.py
@@ -2,17 +2,11 @@ from typing import List, Tuple, TypeVar, Union
 
 from django.db import models
 from django.db.backends.base.base import BaseDatabaseWrapper
-from django.db.models import CASCADE
-from django.db.models.signals import post_delete, post_save
 from django.db.models.sql.compiler import SQLCompiler
 from django_stubs_ext import ValuesQuerySet
 from typing_extensions import override
 
-from zerver.lib.cache import (
-    cache_delete,
-    realm_alert_words_automaton_cache_key,
-    realm_alert_words_cache_key,
-)
+from zerver.models.alert_words import AlertWord as AlertWord
 from zerver.models.bots import BotConfigData as BotConfigData
 from zerver.models.bots import BotStorageData as BotStorageData
 from zerver.models.bots import Service as Service
@@ -131,31 +125,3 @@ def query_for_ids(
         params=(tuple(user_ids),),
     )
     return query
-
-
-class AlertWord(models.Model):
-    # Realm isn't necessary, but it's a nice denormalization.  Users
-    # never move to another realm, so it's static, and having Realm
-    # here optimizes the main query on this table, which is fetching
-    # all the alert words in a realm.
-    realm = models.ForeignKey(Realm, db_index=True, on_delete=CASCADE)
-    user_profile = models.ForeignKey(UserProfile, on_delete=CASCADE)
-    # Case-insensitive name for the alert word.
-    word = models.TextField()
-
-    class Meta:
-        unique_together = ("user_profile", "word")
-
-
-def flush_realm_alert_words(realm_id: int) -> None:
-    cache_delete(realm_alert_words_cache_key(realm_id))
-    cache_delete(realm_alert_words_automaton_cache_key(realm_id))
-
-
-def flush_alert_word(*, instance: AlertWord, **kwargs: object) -> None:
-    realm_id = instance.realm_id
-    flush_realm_alert_words(realm_id)
-
-
-post_save.connect(flush_alert_word, sender=AlertWord)
-post_delete.connect(flush_alert_word, sender=AlertWord)
