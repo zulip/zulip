a8172591cab42d9dee6585d7425d327f06780094
Lauryn Menard <lauryn@zulip.com>
2023-12-15 17:16:39 +0100

    support: Show current discount for current plan.
    This should match any discount information shown for the remote
    realm or server.

--- analytics/views/support.py
+++ analytics/views/support.py
@@ -56,6 +56,7 @@ if settings.BILLING_ENABLED:
         RemoteServerBillingSession,
         SupportType,
         SupportViewRequest,
+        format_discount_percentage,
     )
     from corporate.lib.support import (
         PlanData,
@@ -364,6 +365,7 @@ def support(
     context["get_realm_admin_emails_as_string"] = get_realm_admin_emails_as_string
     context["get_discount"] = get_customer_discount_for_support_view
     context["get_org_type_display_name"] = get_org_type_display_name
+    context["format_discount"] = format_discount_percentage
     context["realm_icon_url"] = realm_icon_url
     context["Confirmation"] = Confirmation
     context["sorted_realm_types"] = sorted(
@@ -513,6 +515,7 @@ def remote_servers_support(
     context["remote_realms_support_data"] = realm_support_data
     context["get_plan_type_name"] = get_plan_type_string
     context["get_org_type_display_name"] = get_org_type_display_name
+    context["format_discount"] = format_discount_percentage
     context["SPONSORED_PLAN_TYPE"] = RemoteZulipServer.PLAN_TYPE_COMMUNITY
 
     return render(
