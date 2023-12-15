7ada0f672e99a907db857e0e0b22c1739f012cdf
Anders Kaseorg <anders@zulip.com>
2023-12-14 17:43:18 -0800

    models: Extract zerver.models.realm_emoji.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- zerver/lib/events.py
+++ zerver/lib/events.py
@@ -80,12 +80,12 @@ from zerver.models import (
     UserStatus,
     UserTopic,
     custom_profile_fields_for_realm,
-    get_all_custom_emoji_for_realm,
     get_default_stream_groups,
     get_realm_playgrounds,
     linkifiers_for_realm,
 )
 from zerver.models.constants import MAX_TOPIC_NAME_LENGTH
+from zerver.models.realm_emoji import get_all_custom_emoji_for_realm
 from zerver.models.realms import get_realm_domains
 from zerver.tornado.django_api import get_user_events, request_event_queue
 from zproject.backends import email_auth_enabled, password_auth_enabled
