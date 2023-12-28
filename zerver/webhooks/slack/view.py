36c6ce057eedec0a24c7d79b99dd1e1a7220d35e
AlexiusTatius <mehtabsinghcse@gmail.com>
2023-12-28 11:43:05 +0530

    webhooks: Migrate slack integration to use check_send_webhook_message.
    -This commit migrates api_slack_webhook to use check_send_webhook_message
     it resolves the bug faced in slack webhook, where we earlier couldn't send
     notification to specific topic by adding &topic=to_URL.
    -Earlier the message was always sent to "channel:<slack channel name>".
    -Now, if the topic specified in the URL, then the message will sent
     to that topic
    -If the topic is not specified, then the message will be sent to
     "channel:<slack channel name>
    -Included additional parameter "user_specified_topic" in
     api_slack_webhook to handle the user defined topic.
    -This commit also handles the variable of channels_map_to_topics.
    
    Fixes: #27601

--- zerver/webhooks/slack/view.py
+++ zerver/webhooks/slack/view.py
@@ -2,12 +2,12 @@ from django.http import HttpRequest
 from django.http.response import HttpResponse
 from django.utils.translation import gettext as _
 
-from zerver.actions.message_send import check_send_stream_message
 from zerver.decorator import webhook_view
 from zerver.lib.exceptions import JsonableError
 from zerver.lib.request import RequestNotes
 from zerver.lib.response import json_success
 from zerver.lib.typed_endpoint import typed_endpoint
+from zerver.lib.webhooks.common import OptionalUserSpecifiedTopicStr, check_send_webhook_message
 from zerver.models import UserProfile
 
 ZULIP_MESSAGE_TEMPLATE = "**{message_sender}**: {text}"
@@ -25,12 +25,16 @@ def api_slack_webhook(
     channel_name: str,
     stream: str = "slack",
     channels_map_to_topics: str = "1",
+    user_specified_topic: OptionalUserSpecifiedTopicStr = None,
 ) -> HttpResponse:
     if channels_map_to_topics not in VALID_OPTIONS.values():
         raise JsonableError(_("Error: channels_map_to_topics parameter other than 0 or 1"))
 
     if channels_map_to_topics == VALID_OPTIONS["SHOULD_BE_MAPPED"]:
-        topic = f"channel: {channel_name}"
+        if user_specified_topic is not None:
+            topic = user_specified_topic
+        else:
+            topic = f"channel: {channel_name}"
     else:
         stream = channel_name
         topic = _("Message from Slack")
@@ -38,5 +42,12 @@ def api_slack_webhook(
     content = ZULIP_MESSAGE_TEMPLATE.format(message_sender=user_name, text=text)
     client = RequestNotes.get_notes(request).client
     assert client is not None
-    check_send_stream_message(user_profile, client, stream, topic, content)
+    check_send_webhook_message(
+        request,
+        user_profile,
+        topic,
+        content,
+        stream=stream,
+        user_specified_topic=user_specified_topic,
+    )
     return json_success(request)
