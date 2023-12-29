c6d461a93ff31701b17d3ff93df200f7f2bd246f
Anders Kaseorg <anders@zulip.com>
2023-12-28 17:23:51 -0800

    page_params: Move page_load_time to zulip_test.
    This isn’t sent by the server; it’s a client-side global variable.
    
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- web/src/zulip_test.js
+++ web/src/zulip_test.js
@@ -11,4 +11,5 @@ export {last_visible as last_visible_row, id as row_id} from "./rows";
 export {cancel as cancel_compose} from "./compose_actions";
 export {page_params, page_params_parse_time} from "./page_params";
 export {initiate as initiate_reload} from "./reload";
+export {page_load_time} from "./setup";
 export {add_user_id_to_new_stream} from "./stream_create_subscribers";
