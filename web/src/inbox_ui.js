c7fc82727f6c91626daec456b1146cd64d9d9f30
CIC4DA <dhruv.jain9100@gmail.com>
2024-01-08 15:11:19 +0530

    inbox_ui: Use Intl.ListFormat rather than .join(", ").

--- web/src/inbox_ui.js
+++ web/src/inbox_ui.js
@@ -152,8 +152,7 @@ function format_dm(user_ids_string, unread_count, latest_msg_id) {
                 status_emoji_info: user_status.get_status_emoji(recipient_id),
             }),
         )
-        .sort()
-        .join(", ");
+        .sort();
 
     let user_circle_class;
     let is_bot = false;
@@ -165,7 +164,7 @@ function format_dm(user_ids_string, unread_count, latest_msg_id) {
     const context = {
         conversation_key: user_ids_string,
         is_direct: true,
-        rendered_dm_with,
+        rendered_dm_with: util.format_array_as_list(rendered_dm_with, "long", "conjunction"),
         is_group: recipient_ids.length > 1,
         user_circle_class,
         is_bot,
