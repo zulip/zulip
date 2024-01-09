d01a81188f8a647c1b47f85aa9747152d16677bb
Alex Vandiver <alexmv@zulip.com>
2024-01-09 14:20:56 -0500

    puppet: Adjust uptrack permissions and ownership to match package's.
    This reverts a759d26a327cd4337d68eaa1d45d6a69edc9161c; apparently the
    package has switched back.

--- puppet/zulip_ops/manifests/ksplice_uptrack.pp
+++ puppet/zulip_ops/manifests/ksplice_uptrack.pp
@@ -4,14 +4,14 @@ class zulip_ops::ksplice_uptrack {
     file { '/etc/uptrack':
       ensure => directory,
       owner  => 'root',
-      group  => 'adm',
-      mode   => '0750',
+      group  => 'root',
+      mode   => '0755',
     }
     file { '/etc/uptrack/uptrack.conf':
       ensure  => file,
       owner   => 'root',
-      group   => 'adm',
-      mode    => '0640',
+      group   => 'root',
+      mode    => '0644',
       content => template('zulip_ops/uptrack/uptrack.conf.erb'),
     }
     $setup_apt_repo_file = "${::zulip_scripts_path}/lib/setup-apt-repo"
