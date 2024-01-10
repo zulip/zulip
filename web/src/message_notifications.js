7a320f9127ee977f63051a8fea8482f9d4a6194c
tubamouqeem <tubamouqeem@gmail.com>
2024-01-10 06:21:12 +0500

    notifications: Fix truncation of word boundaries.
    This truncation logic produces [...] for long words and needs to be fixed.
    
    Fixes #25260.

--- web/src/message_notifications.js
+++ web/src/message_notifications.js
@@ -43,16 +43,12 @@ function get_notification_content(message) {
         content = "New direct message from " + message.sender_full_name;
     }
 
-    if (content.length > 150) {
-        let i;
-        // Truncate content at a word boundary
-        for (i = 150; i > 0; i -= 1) {
-            if (content[i] === " ") {
-                break;
-            }
-        }
-        content = content.slice(0, i);
-        content += " [...]";
+    const characterLimit = 150;
+    const ellipsis = "…";
+
+    if (content.length > characterLimit) {
+        // Truncate content not at a word boundary
+        content = content.slice(0, characterLimit - 3) + ellipsis;
     }
 
     return content;
