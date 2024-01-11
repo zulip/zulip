4b14f0efadce4e2a7556547631e45e19315e14fa
evykassirer <evy@zulip.com>
2024-01-10 23:18:20 -0800

    stream_topic_history_util: Convert module to typescript.

--- web/src/stream_topic_history_util.ts
+++ web/src/stream_topic_history_util.ts
@@ -1,7 +1,18 @@
+import {z} from "zod";
+
 import * as channel from "./channel";
 import * as stream_topic_history from "./stream_topic_history";
 
-export function get_server_history(stream_id, on_success) {
+const stream_topic_history_response_schema = z.object({
+    topics: z.array(
+        z.object({
+            name: z.string(),
+            max_id: z.number(),
+        }),
+    ),
+});
+
+export function get_server_history(stream_id: number, on_success: () => void): void {
     if (stream_topic_history.has_history_for(stream_id)) {
         on_success();
         return;
@@ -13,11 +24,12 @@ export function get_server_history(stream_id, on_success) {
     stream_topic_history.add_request_pending_for(stream_id);
     const url = "/json/users/me/" + stream_id + "/topics";
 
-    channel.get({
+    void channel.get({
         url,
         data: {},
         success(data) {
-            const server_history = data.topics;
+            const clean_data = stream_topic_history_response_schema.parse(data);
+            const server_history = clean_data.topics;
             stream_topic_history.add_history(stream_id, server_history);
             stream_topic_history.remove_request_pending_for(stream_id);
             on_success();
