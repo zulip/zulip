ff814c9ce6bb7dea61873a876d8c9bd79530b44e
Aman Agrawal <amanagr@zulip.com>
2024-01-13 07:31:23 +0000

    pm_list: Remove not needed narrow_state mock.

--- web/tests/pm_list.test.js
+++ web/tests/pm_list.test.js
@@ -6,15 +6,10 @@ const {zrequire} = require("./lib/namespace");
 const {run_test} = require("./lib/test");
 const $ = require("./lib/zjquery");
 
-const narrow_state = zrequire("narrow_state");
 const pm_list = zrequire("pm_list");
 
 run_test("update_dom_with_unread_counts", () => {
     let counts;
-
-    // simulate an active narrow
-    narrow_state.set_current_filter("stub");
-    assert.equal(narrow_state.active(), true);
 
     const $total_count = $.create("total-count-stub");
     const $private_li = $(
