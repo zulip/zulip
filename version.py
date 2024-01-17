fe600fbd5bbbcf7181c865c3377a69e9ca4ac255
nimishmedatwal <medatwalnimish@gmail.com>
2024-01-17 19:00:24 +0530

    upgrade: Upgrade version of @giphy/js-fetch-api.

--- version.py
+++ version.py
@@ -48,4 +48,4 @@ API_FEATURE_LEVEL = 242
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (264, 0)
+PROVISION_VERSION = (265, 0)
