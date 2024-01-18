e78ca6bddd2b8dfc5747a92fd0b537846f783dc3
sanchi-t <ssharmas10662@gmail.com>
2024-01-18 21:08:48 +0530

    subscription: Include archived channels in streams list.
    `is_archived` field is added to the stream and types.
    
    Include a new `archived_channeels` client capability, to allow clients
    to access data on archived channels, without breaking
    backwards-compatibility for existing clients that don't know how to
    handle these.
    
    Also, included `exclude_archived` parameter to `/get-streams`,
    which defaults to `true` as basic clients may not be interested
    in archived streams.

--- zerver/lib/home.py
+++ zerver/lib/home.py
@@ -156,6 +156,7 @@ def build_page_params_for_home_page_load(
         linkifier_url_template=True,
         user_list_incomplete=True,
         include_deactivated_groups=True,
+        archived_channels=False,
     )
 
     if user_profile is not None:
