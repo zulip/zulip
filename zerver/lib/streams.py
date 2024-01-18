46916a315796ebf8f192c0250199f74210565a88
sanchi-t <ssharmas10662@gmail.com>
2024-01-18 21:08:48 +0530

    subscription: Include archived streams in unsubscribed streams list.
    `is_archived` field is added to the stream and types.
    
    Include a new `archived_streams` client capability, to allow clients
    to access data on archived streams, without breaking
    backwards-compatibility for existing clients that don't know how to
    handle these.
    
    Also, included `exclude__archived` parameter to `/get-streams`,
    which defaults to `true` as basic clients may not be interested
    in archived streams.

--- zerver/lib/streams.py
+++ zerver/lib/streams.py
@@ -867,6 +867,7 @@ def stream_to_dict(stream: Stream, recent_traffic: dict[int, int] | None = None)
         stream_weekly_traffic = None
 
     return APIStreamDict(
+        is_archived=stream.deactivated,
         can_remove_subscribers_group=stream.can_remove_subscribers_group_id,
         creator_id=stream.creator_id,
         date_created=datetime_to_timestamp(stream.date_created),
@@ -897,6 +898,7 @@ def get_streams_for_user(
     include_public: bool = True,
     include_web_public: bool = False,
     include_subscribed: bool = True,
+    exclude_archived: bool = True,
     include_all_active: bool = False,
     include_owner_subscribed: bool = False,
 ) -> list[Stream]:
@@ -905,8 +907,11 @@ def get_streams_for_user(
 
     include_public = include_public and user_profile.can_access_public_streams()
 
-    # Start out with all active streams in the realm.
-    query = Stream.objects.filter(realm=user_profile.realm, deactivated=False)
+    # Start out with all streams in the realm.
+    query = Stream.objects.filter(realm=user_profile.realm)
+
+    if exclude_archived:
+        query = query.filter(deactivated=False)
 
     if include_all_active:
         streams = query.only(*Stream.API_FIELDS)
@@ -960,6 +965,7 @@ def do_get_streams(
     include_public: bool = True,
     include_web_public: bool = False,
     include_subscribed: bool = True,
+    exclude_archived: bool = True,
     include_all_active: bool = False,
     include_default: bool = False,
     include_owner_subscribed: bool = False,
@@ -971,6 +977,7 @@ def do_get_streams(
         include_public,
         include_web_public,
         include_subscribed,
+        exclude_archived,
         include_all_active,
         include_owner_subscribed,
     )
