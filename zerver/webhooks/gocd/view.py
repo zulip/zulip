ad13d4c30be912026998be0f7917ce5253000fb0
swayam0322 <swayamgupta33@gmail.com>
2024-01-30 06:41:11 +0530

    integrations: replace old gocd integration with generic webhook plugin.
    Earlier the GoCD plugin implementation had a faulty implementation
    by using the <trackingtool> in the GoCD configuration
    
    Now the Generic Webhook Plugin by Sentry is being used for GoCD
    integration replacing the old implementation with newly added docs
    and test.

--- zerver/webhooks/gocd/view.py
+++ zerver/webhooks/gocd/view.py
@@ -1,6 +1,4 @@
 # Webhooks for external integrations.
-import json
-import os
 
 from django.http import HttpRequest, HttpResponse
 
@@ -9,13 +7,26 @@ from zerver.lib.response import json_success
 from zerver.lib.typed_endpoint import JsonBodyPayload, typed_endpoint
 from zerver.lib.validator import WildValue, check_string
 from zerver.lib.webhooks.common import check_send_webhook_message
+from zerver.lib.webhooks.git import get_short_sha
 from zerver.models import UserProfile
 
-MESSAGE_TEMPLATE = """\
-Author: {}
-Build status: {} {}
-Details: [build log]({})
-Comment: {}"""
+COMMIT_INFO_TEMPLATE = (
+    """Triggered on [`{commit_details}`]({commit_link}) on branch `{branch_name}`."""
+)
+TOPIC_TEMPLATE = "{pipeline} / {stage}"
+
+SCHEDULED_BODY_TEMPLATE = """
+**Pipeline** {pipeline}/{stage}: {status}.
+- **Commit**: {commit_details}
+- **Started**: {start_time}
+"""
+
+COMPLETED_BODY_TEMPLATE = """
+**Build** {pipeline}/{stage}: {status} {emoji}.
+- **Commit**: {commit_details}
+- **Started**: {start_time}
+- **Finished**: {end_time}
+"""
 
 
 @webhook_view("Gocd")
@@ -26,31 +37,60 @@ def api_gocd_webhook(
     *,
     payload: JsonBodyPayload[WildValue],
 ) -> HttpResponse:
-    modifications = payload["build_cause"]["material_revisions"][0]["modifications"][0]
-    result = payload["stages"][0]["result"].tame(check_string)
-    material = payload["build_cause"]["material_revisions"][0]["material"]
-
-    if result == "Passed":
-        emoji = ":thumbs_up:"
-    elif result == "Failed":
-        emoji = ":thumbs_down:"
-
-    build_details_file = os.path.join(os.path.dirname(__file__), "fixtures/build_details.json")
-
-    with open(build_details_file) as f:
-        contents = json.load(f)
-        build_link = contents["build_details"]["_links"]["pipeline"]["href"]
-
-    body = MESSAGE_TEMPLATE.format(
-        modifications["user_name"].tame(check_string),
-        result,
-        emoji,
-        build_link,
-        modifications["comment"].tame(check_string),
-    )
-    branch = material["description"].tame(check_string).split(",")
-    topic_name = branch[0].split(" ")[1]
-
-    check_send_webhook_message(request, user_profile, topic_name, body)
-
+    type = payload["type"].tame(check_string)
+    if type == "stage":
+        body = get_body(payload)
+        topic_name = get_topic(payload)
+        check_send_webhook_message(request, user_profile, topic_name, body)
     return json_success(request)
+
+
+def get_topic(payload: WildValue) -> str:
+    return TOPIC_TEMPLATE.format(
+        pipeline=payload["data"]["pipeline"]["name"].tame(check_string),
+        stage=payload["data"]["pipeline"]["stage"]["name"].tame(check_string),
+    )
+
+
+def get_commit_details(payload: WildValue) -> str:
+    material = payload["data"]["pipeline"]["build-cause"][0]["material"]
+    url = material["git-configuration"]["url"].tame(check_string)
+    revision = payload["data"]["pipeline"]["build-cause"][0]["modifications"][0]["revision"].tame(
+        check_string
+    )
+    commit_sha = get_short_sha(revision)
+    url = f"{url}/commit/{commit_sha}"
+    branch = material["git-configuration"]["branch"].tame(check_string)
+    return COMMIT_INFO_TEMPLATE.format(
+        commit_details=commit_sha,
+        commit_link=url,
+        branch_name=branch,
+    )
+
+
+def get_body(payload: WildValue) -> str:
+    state = payload["data"]["pipeline"]["stage"].get("state").tame(check_string)
+    if state == "Building":
+        commit = get_commit_details(payload)
+        return SCHEDULED_BODY_TEMPLATE.format(
+            pipeline=payload["data"]["pipeline"]["name"].tame(check_string),
+            stage=payload["data"]["pipeline"]["stage"]["name"].tame(check_string),
+            status=state,
+            commit_details=commit,
+            start_time=payload["data"]["pipeline"]["stage"]["create-time"].tame(check_string),
+        )
+    else:
+        commit = get_commit_details(payload)
+        result = payload["data"]["pipeline"]["stage"].get("result").tame(check_string)
+        emoji = ":thumbs_up:" if result == "Passed" else ":thumbs_down:"
+        return COMPLETED_BODY_TEMPLATE.format(
+            pipeline=payload["data"]["pipeline"].get("name").tame(check_string),
+            stage=payload["data"]["pipeline"]["stage"].get("name").tame(check_string),
+            status=result,
+            emoji=emoji,
+            commit_details=commit,
+            start_time=payload["data"]["pipeline"]["stage"].get("create-time").tame(check_string),
+            end_time=payload["data"]["pipeline"]["stage"]
+            .get("last-transition-time")
+            .tame(check_string),
+        )
