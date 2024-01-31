8a206126500ea894f2cfd9edc9f96d72ecdce4af
Alex Vandiver <alexmv@zulip.com>
2024-01-31 12:07:08 -0500

    puppet: Use a dedicated user for redis tunneling.

--- puppet/zulip_ops/manifests/profile/redis.pp
+++ puppet/zulip_ops/manifests/profile/redis.pp
@@ -10,4 +10,17 @@ class zulip_ops::profile::redis inherits zulip_ops::profile::base {
     group   => 'nagios',
     content => "${zulip::profile::redis::redis_password}\n",
   }
+
+  user { 'redistunnel':
+    ensure     => present,
+    uid        => '1080',
+    gid        => '1080',
+    groups     => ['zulip'],
+    shell      => '/bin/true',
+    home       => '/home/redistunnel',
+    managehome => true,
+  }
+  zulip_ops::user_dotfiles { 'redistunnel':
+    authorized_keys => true,
+  }
 }
