4c98c8c3c6d248a8a38df6c783e6b7c5d6163991
swayam0322 <swayamgupta33@gmail.com>
2024-02-01 22:09:42 +0530

    find-account: Send email when no Zulip accounts found.
    Previously, email addresses that weren't connected to a Zulip account
    were ignored but now they receive an email stating their email isn't
    connected to a Zulip account.
    
    Also, removes the "Thanks for using Zulip!" line at the end of the
    find accounts email that's sent when a Zulip account is found.
    Updates the i18n test that used this string with another in the
    German translation from this a successful account found email.
    
    Fixes part of #3128
    
    Co-authored-by: Lauryn Menard <lauryn@zulip.com>

--- zerver/tests/test_i18n.py
+++ zerver/tests/test_i18n.py
@@ -54,9 +54,7 @@ class EmailTranslationTestCase(ZulipTestCase):
             {"email": "new-email@zulip.com"},
             HTTP_ACCEPT_LANGUAGE="pt",
         )
-        check_translation(
-            "Danke, dass du", "post", "/accounts/find/", {"emails": hamlet.delivery_email}
-        )
+        check_translation("Danke für", "post", "/accounts/find/", {"emails": hamlet.delivery_email})
         check_translation(
             "Hallo",
             "post",
