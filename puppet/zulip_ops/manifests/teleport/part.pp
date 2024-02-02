dc58b8aceabb6fea7b0d4b5f8ddb7b8c941aacbf
Alex Vandiver <alexmv@zulip.com>
2024-02-02 00:57:28 -0500

    puppet: Call systemd reload after adding or changing a teleport service.

--- puppet/zulip_ops/manifests/teleport/part.pp
+++ puppet/zulip_ops/manifests/teleport/part.pp
@@ -2,6 +2,8 @@
 #
 define zulip_ops::teleport::part() {
   $part = $name
+
+  include zulip::systemd_daemon_reload
   file { "/etc/systemd/system/teleport_${part}.service":
     require => [
       Package[teleport],
@@ -10,12 +12,12 @@ define zulip_ops::teleport::part() {
     group   => 'root',
     mode    => '0644',
     content => template('zulip_ops/teleport.service.template.erb'),
-    notify  => Service["teleport_${part}"],
+    notify  => [Exec['reload systemd'], Service["teleport_${part}"]],
   }
 
   service {"teleport_${part}":
     ensure  => running,
     enable  => true,
-    require => [Service['supervisor'], Service['teleport']],
+    require => [Service['supervisor'], Service['teleport'], Exec['reload systemd']],
   }
 }
