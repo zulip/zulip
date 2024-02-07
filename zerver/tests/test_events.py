1d3813ec4f0d8df9f617ed9c0eeac19dfcc9e43c
Alex Vandiver <alexmv@zulip.com>
2024-02-07 21:03:15 +0000

    tornado: Track which queues were inherited from old Tornado instances.

--- zerver/tests/test_events.py
+++ zerver/tests/test_events.py
@@ -251,6 +251,7 @@ from zerver.tornado.event_queue import (
     allocate_client_descriptor,
     clear_client_event_queues_for_testing,
     create_heartbeat_event,
+    mark_clients_to_reload,
     send_web_reload_client_events,
 )
 from zerver.views.realm_playgrounds import access_playground_by_id
@@ -288,6 +289,7 @@ class BaseAction(ZulipTestCase):
         pronouns_field_type_supported: bool = True,
         linkifier_url_template: bool = True,
         user_list_incomplete: bool = False,
+        client_is_old: bool = False,
     ) -> List[Dict[str, Any]]:
         """
         Make sure we have a clean slate of client descriptors for these tests.
@@ -335,6 +337,9 @@ class BaseAction(ZulipTestCase):
             linkifier_url_template=linkifier_url_template,
             user_list_incomplete=user_list_incomplete,
         )
+
+        if client_is_old:
+            mark_clients_to_reload([client.event_queue.id])
 
         # We want even those `send_event` calls which have been hooked to
         # `transaction.on_commit` to execute in tests.
@@ -3457,8 +3462,14 @@ class NormalActionsTest(BaseAction):
         check_has_zoom_token("events[0]", events[0], value=False)
 
     def test_web_reload_client_event(self) -> None:
+        self.verify_action(
+            lambda: send_web_reload_client_events(),
+            client_is_old=False,
+            num_events=0,
+            state_change_expected=False,
+        )
         with self.assertRaises(WebReloadClientError):
-            self.verify_action(lambda: send_web_reload_client_events())
+            self.verify_action(lambda: send_web_reload_client_events(), client_is_old=True)
 
     def test_display_setting_event_not_sent(self) -> None:
         events = self.verify_action(
