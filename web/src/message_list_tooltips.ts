a3e03b135c8abe6d34cbcf88c8f08a7ea070d7a1
sujal <sujalshah28092004@gmail.com>
2024-02-29 15:53:43 +0530

    message_feed: Modify tooltips to clarify draft saving.
    Update the tooltips for the "Cancel" and "Spinner".  Provide clear
    information about the draft saving behavior when a message fails to
    send successfully.
    
    Fixes part of #29100.

--- web/src/message_list_tooltips.ts
+++ web/src/message_list_tooltips.ts
@@ -8,7 +8,6 @@ import render_message_edit_notice_tooltip from "../templates/message_edit_notice
 import render_message_inline_image_tooltip from "../templates/message_inline_image_tooltip.hbs";
 import render_narrow_tooltip from "../templates/narrow_tooltip.hbs";
 
-import {$t} from "./i18n";
 import * as message_lists from "./message_lists";
 import * as popover_menus from "./popover_menus";
 import * as reactions from "./reactions";
@@ -221,12 +220,6 @@ export function initialize(): void {
 
     message_list_tooltip(".slow-send-spinner", {
         onShow(instance) {
-            instance.setContent(
-                $t({
-                    defaultMessage:
-                        "Your message is taking longer than expected to be sent. Sending…",
-                }),
-            );
             const $elem = $(instance.reference);
 
             // We need to check for removal of local class from message_row since
