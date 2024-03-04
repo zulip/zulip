1715739345085b3b328caa9d7db850ccea878434
Kirsty21 <kirstywilliams71421@gmail.com>
2024-03-04 12:19:15 -0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   .vagrant-invalid/bundler/global.sol
    	new file:   .vagrant-invalid/machines/default/hyperv/vagrant_cwd
    	new file:   .vagrant-invalid/rgloader/loader.rb
    	modified:   help/include/go-to-recent-conversations.md
    	modified:   web/images/icons/zulip-icons.font.js
    	new file:   web/shared/icons/history.svg
    	modified:   web/templates/left_sidebar.hbs

--- web/images/icons/zulip-icons.font.js
+++ web/images/icons/zulip-icons.font.js
@@ -5,7 +5,7 @@
 module.exports = {
     files: [
         "./*.svg", // For web-only icons.
-        "../../shared/icons/*.svg", // For icons to be shared with the mobile app.
+        "../../shared/icons/*.svg", // For icons to be shared with the mobile app. 
     ],
     fontName: "zulip-icons",
     classPrefix: "zulip-icon-",
