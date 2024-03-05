1cba83f9c531da03c93e6ff8ba08771179535501
Alex Vandiver <alexmv@zulip.com>
2024-03-05 16:23:28 +0000

    settings: Use bool-ish get_config on postgresql.missing_dictionaries.

--- zproject/computed_settings.py
+++ zproject/computed_settings.py
@@ -311,7 +311,7 @@ elif (
         PASSWORD=get_secret("postgres_password"),
         HOST="localhost",
     )
-POSTGRESQL_MISSING_DICTIONARIES = bool(get_config("postgresql", "missing_dictionaries", None))
+POSTGRESQL_MISSING_DICTIONARIES = get_config("postgresql", "missing_dictionaries", False)
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
