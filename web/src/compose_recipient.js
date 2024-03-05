zulip/zulip

commit 5c993f0479937e365a8fed161b14fdf2aba783f2

direct_message: Allow DMs to bots and self regardless of policy.
Previously, in DM disabled org messaging to bot was not working when
starting new conversation and adding bot as recipient because of not
updating on recipient change. And secondly, self messaging was not
allowed.
This commit ensures that the DM to bot and self are allowed irrespective
of dm restrictions.

tests: Verify DMs adhere to DM restriction policy.

Fixes #28412

Signed-off-by: sayyedarib <sayyedaribhussain4321@gmail.com>

web/src/compose_recipient.js
@@ -95,6 +95,7 @@ function update_fade() {
 export function update_on_recipient_change() {
     update_fade();
     update_narrow_to_recipient_visibility();
+    check_posting_policy_for_compose_box();
 }
 
 export function get_posting_policy_error_message() {
@@ -212,7 +213,6 @@ export function on_compose_select_recipient_update() {
         stream_bar.decorate(stream_id, $stream_header_colorblock);
     }
 
-    check_posting_policy_for_compose_box();
     update_on_recipient_change();
 }
 

web/src/compose_validate.js
@@ -679,12 +679,9 @@ function validate_private_message() {
     const user_ids = compose_pm_pill.get_user_ids();
     const $banner_container = $("#compose_banners");
 
-    if (
-        realm.realm_private_message_policy ===
-            settings_config.private_message_policy_values.disabled.code &&
-        (user_ids.length !== 1 || !people.get_by_user_id(user_ids[0]).is_bot)
-    ) {
-        // Unless we're composing to a bot
+    const user_ids_string = user_ids.join(",");
+
+    if (!people.user_can_direct_message(user_ids_string)) {
         compose_banner.show_error_message(
             $t({defaultMessage: "Direct messages are disabled in this organization."}),
             compose_banner.CLASSNAMES.private_messages_disabled,

web/src/people.ts
@@ -805,9 +805,12 @@ export function user_can_direct_message(recipient_ids_string: string): boolean {
     // message to the target user (or group of users) represented by a
     // user ids string.
 
-    // Regardless of policy, we allow sending direct messages to bots.
+    // Regardless of policy, we allow sending direct messages to bots and to self.
     const recipient_ids = user_ids_string_to_ids_array(recipient_ids_string);
-    if (recipient_ids.length === 1 && is_valid_bot_user(recipient_ids[0])) {
+    if (
+        recipient_ids.length === 1 &&
+        (is_valid_bot_user(recipient_ids[0]) || is_my_user_id(recipient_ids[0]))
+    ) {
         return true;
     }
 

zerver/actions/message_send.py
@@ -1530,8 +1530,10 @@ def check_private_message_policy(
     realm: Realm, sender: UserProfile, user_profiles: Sequence[UserProfile]
 ) -> None:
     if realm.private_message_policy == Realm.PRIVATE_MESSAGE_POLICY_DISABLED:
-        if sender.is_bot or (len(user_profiles) == 1 and user_profiles[0].is_bot):
-            # We allow direct messages only between users and bots,
+        if sender.is_bot or (
+            len(user_profiles) == 1 and (user_profiles[0].is_bot or user_profiles[0] == sender)
+        ):
+            # We allow direct messages only between users and bots or to oneself,
             # to avoid breaking the tutorial as well as automated
             # notifications from system bots to users.
             return

zerver/tests/test_message_send.py
@@ -2613,6 +2613,73 @@ class InternalPrepTest(ZulipTestCase):
         # wasn't automatically created.
         Stream.objects.get(name=stream_name, realm_id=realm.id)
 
+    def test_direct_message_to_self_and_bot_in_dm_disabled_org(self) -> None:
+        """
+        Test that a user can send a direct message to themselves and to a bot in a DM disabled organization
+        """
+        sender = self.example_user("hamlet")
+        sender.realm.private_message_policy = Realm.PRIVATE_MESSAGE_POLICY_DISABLED
+        sender.realm.save()
+
+        #  Create a non-bot user
+        recipient_user = self.example_user("othello")
+        recipient_user.realm = sender.realm
+
+        # Create a new bot user
+        bot = do_create_user(
+            email="test-bot@zulip.com",
+            password="",
+            realm=sender.realm,
+            full_name="Test Bot",
+            bot_type=UserProfile.DEFAULT_BOT,
+            bot_owner=sender,
+            acting_user=None,
+        )
+
+        # Test sending a message to self
+        result = self.api_post(
+            sender,
+            "/api/v1/messages",
+            {
+                "type": "private",
+                "to": orjson.dumps([sender.id]).decode(),
+                "content": "Test message to self",
+            },
+        )
+        self.assert_json_success(result)
+
+        msg = self.get_last_message()
+        expected = "Test message to self"
+        self.assertEqual(msg.content, expected)
+
+        # Test sending a message to non-bot user
+        result = self.api_post(
+            sender,
+            "/api/v1/messages",
+            {
+                "type": "private",
+                "to": orjson.dumps([recipient_user.id]).decode(),
+                "content": "Test message",
+            },
+        )
+        self.assert_json_error(result, "Direct messages are disabled in this organization.")
+
+        # Test sending a message to the bot
+        result = self.api_post(
+            sender,
+            "/api/v1/messages",
+            {
+                "type": "private",
+                "to": orjson.dumps([bot.id]).decode(),
+                "content": "Test message to bot",
+            },
+        )
+        self.assert_json_success(result)
+
+        msg = self.get_last_message()
+        expected = "Test message to bot"
+        self.assertEqual(msg.content, expected)
+
 
 class TestCrossRealmPMs(ZulipTestCase):
     def make_realm(self, domain: str) -> Realm:

commit 44f313633fa8626d5ce5257467729fcf3153fb99

compose: Fix topics required banner closing on enter to send.
We were updating the compose banners on every `keyup` event on the
topic input. Since, `keyup` also gets triggered for the modifier and
non-printing keys such as "Enter", this lead to banner for topic
required being closed via the `check_posting_policy_for_compose_box`
when pressing "Enter" to send a message with no topic.

This bug was probably introduced in 5c993f0, which moved additional
logic into `update_on_recipient_change`.

To solve this issue, we use the `input` event instead of the `keyup`
event to update the compose banners only when the value inside the
input element changes.

This change also prevents the the compose banner from being closed
when we only press modifier keys - such as Shift.

web/src/compose_recipient.js
@@ -324,10 +324,10 @@ export function initialize() {
         on_hidden_callback,
     }).setup();
 
-    // `keyup` isn't relevant for streams since it registers as a change only
+    // `input` isn't relevant for streams since it registers as a change only
     // when an item in the dropdown is selected.
     $("#stream_message_recipient_topic,#private_message_recipient").on(
-        "keyup",
+        "input",
         update_on_recipient_change,
     );
     // changes for the stream dropdown are handled in on_compose_select_recipient_update