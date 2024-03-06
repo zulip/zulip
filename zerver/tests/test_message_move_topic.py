c71b7afe9f92d47c06acab20e90aff00a8415fd8
Alex Vandiver <alexmv@zulip.com>
2024-03-06 21:38:29 +0000

    message_edit: Handle user, not subscription, collections.
    Nothing about the subscription is relevant -- we instead use
    collections of User objects for readability.

--- zerver/tests/test_message_move_topic.py
+++ zerver/tests/test_message_move_topic.py
@@ -366,7 +366,7 @@ class MessageMoveTopicTest(ZulipTestCase):
         set_topic_visibility_policy(desdemona, muted_topics, UserTopic.VisibilityPolicy.MUTED)
         set_topic_visibility_policy(cordelia, muted_topics, UserTopic.VisibilityPolicy.MUTED)
 
-        with self.assert_database_query_count(35):
+        with self.assert_database_query_count(36):
             check_update_message(
                 user_profile=desdemona,
                 message_id=message_id,
