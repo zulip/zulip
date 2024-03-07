2d3d21c04e82e90a0ac29770a5e654621455ff10
sanchi-t <ssharmas10662@gmail.com>
2024-03-07 16:16:02 +0530

    user_profile: Display `Saved successfully` feedback.
    Visually confirms successful profile changes with a clear
    `Saved successfully` message.
    
    Fixes part of #26692.
    
    Co-authored-by: Kunal Sharma <v.shm.kunal@gmail.com>.
    Co-authored-by: Angelica <angelica.ferlin@gmail.com>.

--- web/src/user_profile.js
+++ web/src/user_profile.js
@@ -587,6 +587,11 @@ export function show_edit_bot_info_modal(user_id, $container) {
                 hide_button_spinner($submit_btn);
                 $submit_btn.prop("disabled", true);
                 original_values = get_current_values($("#bot-edit-form"));
+                ui_report.success(
+                    $t_html({defaultMessage: "Saved successfully!"}),
+                    $("#user-profile-modal .save-success"),
+                    1200,
+                );
                 $cancel_btn.prop("disabled", false);
             },
             error(xhr) {
@@ -859,6 +864,11 @@ export function show_edit_user_info_modal(user_id, $container) {
                 hide_button_spinner($submit_btn);
                 $submit_btn.prop("disabled", true);
                 original_values = get_current_values($("#edit-user-form"));
+                ui_report.success(
+                    $t_html({defaultMessage: "Saved successfully!"}),
+                    $("#user-profile-modal .save-success"),
+                    1200,
+                );
                 $cancel_btn.prop("disabled", false);
             },
             error(xhr) {
