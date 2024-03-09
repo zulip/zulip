293224424df47dc6e05b34a599223bde0e614061
Kenneth Rodrigues <kenneth012004@outlook.com>
2024-03-09 10:58:05 +0530

    message_reactions: Clean message reactions during initial processing.
    Clean reactions and set message_reactions in process_new_message.
    Earlier, message_reactions was being set
    after the predicate for the narrow was built,
    as a result functions called while building the predicate
    could not access it.

--- web/tests/message_events.test.js
+++ web/tests/message_events.test.js
@@ -124,6 +124,7 @@ run_test("update_messages", () => {
         {
             alerted: false,
             collapsed: false,
+            clean_reactions: new Map(),
             content: "<b>new content</b>",
             display_recipient: denmark.name,
             historical: false,
@@ -134,8 +135,8 @@ run_test("update_messages", () => {
             stream_wildcard_mentioned: false,
             topic_wildcard_mentioned: false,
             mentioned_me_directly: false,
+            message_reactions: [],
             raw_content: "**new content**",
-            reactions: [],
             reply_to: alice.email,
             sender_email: alice.email,
             sender_full_name: alice.full_name,
