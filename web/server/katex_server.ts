9a27ebb9b2bd0e672e40af3603d94f9bcada0ebb
Alex Vandiver <alexmv@zulip.com>
2024-03-13 16:29:10 +0000

    katex_server: Add Prometheus metrics.

--- web/server/katex_server.ts
+++ web/server/katex_server.ts
@@ -3,6 +3,7 @@ import crypto from "node:crypto";
 import bodyParser from "@koa/bodyparser";
 import katex from "katex";
 import Koa from "koa";
+import Prometheus from "prom-client";
 
 const host = "localhost";
 const port = Number(process.argv[2] ?? "9700");
@@ -29,6 +30,43 @@ const compare_secret = (given_secret: string): boolean => {
 
 const app = new Koa();
 app.use(bodyParser());
+
+Prometheus.collectDefaultMetrics();
+const httpRequestDurationSeconds = new Prometheus.Histogram({
+    name: "katex_http_request_duration_seconds",
+    help: "Duration of HTTP requests in seconds",
+    labelNames: ["method", "path", "status"] as const,
+    buckets: [
+        0.00001, 0.00002, 0.00005, 0.0001, 0.0002, 0.0005, 0.001, 0.002, 0.005, 0.01, 0.02, 0.05,
+        0.1,
+    ],
+});
+
+const httpRequestSizeBytes = new Prometheus.Histogram({
+    name: "katex_request_size_bytes",
+    help: "Size of successful KaTeX input in bytes",
+    labelNames: ["display_mode"] as const,
+    buckets: [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000],
+});
+
+const httpResponseSizeBytes = new Prometheus.Histogram({
+    name: "katex_response_size_bytes",
+    help: "Size of successful KaTeX output in bytes",
+    labelNames: ["display_mode"] as const,
+    buckets: [100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000, 500000],
+});
+
+app.use(async (ctx, next) => {
+    if (ctx.request.method === "GET" && ctx.request.path === "/metrics") {
+        ctx.body = await Prometheus.register.metrics();
+        return;
+    }
+    const endTimer = httpRequestDurationSeconds.startTimer();
+    await next();
+    const {method, path} = ctx.request;
+    const {status} = ctx.response;
+    httpRequestDurationSeconds.labels({method, path, status: String(status)}).observe(endTimer());
+});
 
 app.use((ctx, _next) => {
     if (ctx.request.method !== "POST" || ctx.request.path !== "/") {
@@ -59,8 +97,12 @@ app.use((ctx, _next) => {
         return;
     }
 
+    httpRequestSizeBytes.labels(String(is_display)).observe(Buffer.byteLength(content, "utf8"));
     try {
         ctx.body = katex.renderToString(content, {displayMode: is_display});
+        httpResponseSizeBytes
+            .labels(String(is_display))
+            .observe(Buffer.byteLength(ctx.body, "utf8"));
     } catch (error) {
         if (error instanceof katex.ParseError) {
             ctx.status = 400;
