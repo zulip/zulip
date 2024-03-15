cf73bf338dff283f854138c597f06659609d8b51
Anders Kaseorg <anders@zulip.com>
2024-03-15 16:24:43 -0700

    requirements: Fix Python 3.12 issue in pip._vendor.pkg_resources.
    https://github.com/pypa/setuptools/commit/76ef30913dce00de4bca773f2dda9dd25ff66ad8
    
    Signed-off-by: Anders Kaseorg <anders@zulip.com>
    (cherry picked from commit 0d30ae8f445a4e4f00d94f19d46d4e2a39f151a7)

--- version.py
+++ version.py
@@ -48,4 +48,4 @@ API_FEATURE_LEVEL = 237
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (263, 1)  # last bumped 2024-05-07 for Python requirements upgrade
+PROVISION_VERSION = (263, 2)  # last bumped 2024-03-15 to patch pip
