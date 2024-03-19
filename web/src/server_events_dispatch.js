d009a743f3bd4b4acdf5aa6350440d6016e7f1a9
Akarsh Jain <akarsh.jain.790@gmail.com>
2024-03-20 00:19:39 +0530

    notifications: Implement functionality for reaction notifications.
    This commit implements the functionality for reaction notifications
    based on the user settings  enable_dm_reaction_notifications and
    streams_reaction_notification.

--- web/src/server_events_dispatch.js
+++ web/src/server_events_dispatch.js
@@ -37,6 +37,7 @@ import * as overlays from "./overlays";
 import * as peer_data from "./peer_data";
 import * as people from "./people";
 import * as pm_list from "./pm_list";
+import {received_reaction} from "./reaction_notifications";
 import * as reactions from "./reactions";
 import * as realm_icon from "./realm_icon";
 import * as realm_logo from "./realm_logo";
@@ -189,6 +190,7 @@ export function dispatch_normal_event(event) {
             switch (event.op) {
                 case "add":
                     reactions.add_reaction(event);
+                    received_reaction(event);
                     break;
                 case "remove":
                     reactions.remove_reaction(event);
