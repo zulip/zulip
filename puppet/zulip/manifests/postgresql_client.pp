96dfec1f1d9443d308343c437479d1be3aae9a7e
Alex Vandiver <alexmv@zulip.com>
2024-03-20 16:22:11 -0400

    puppet: Install the same version of postgres-client as the server.
    We require a `pg_dump` whose version matches the version of the server
    we are configured against (see 3a8b4b020509).  Installing the latest
    `postgresql-client` does not guarantee that we have such a binary
    present.
    
    (cherry picked from commit 23504308fb4c799fcb76ad4a46d478f16c738d1c)

--- puppet/zulip/manifests/postgresql_client.pp
+++ puppet/zulip/manifests/postgresql_client.pp
@@ -1,8 +1,6 @@
 class zulip::postgresql_client {
-  # This may get us a more recent client than the database server is
-  # configured to be, ($zulip::postgresql_common::version), but
-  # they're compatible.
-  package { 'postgresql-client':
+  include zulip::postgresql_common
+  package { "postgresql-client-${zulip::postgresql_common::version}":
     ensure => installed,
   }
 }
