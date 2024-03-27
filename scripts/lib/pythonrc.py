13d11d8591f30dc7fb8e12ce09fbabb2853a8840
Tim Abbott <tabbott@zulip.com>
2024-03-27 11:52:55 -0700

    pythonrc: Add various frequently handy imports.

--- scripts/lib/pythonrc.py
+++ scripts/lib/pythonrc.py
@@ -1,8 +1,14 @@
 try:
-    from django.conf import settings  # noqa: F401
+    from django.conf import settings
 
     from analytics.models import *  # noqa: F403
     from zerver.models import *  # noqa: F403
+    from zerver.models.realms import *  # noqa: F403
+    from zerver.models.streams import *  # noqa: F403
+    from zerver.models.users import *  # noqa: F403
+
+    if settings.CORPORATE_ENABLED:
+        from corporate.lib.stripe import *  # noqa: F403
 except Exception:
     import traceback
 
