4d82226d08174400b65450ed3c074af1b19ff335
Prathamesh314 <kurvep18@gmail.com>
2024-03-31 15:09:44 +0530

    uploads: Fix spaces to dash bug on file upload

--- zerver/lib/url_preview/preview.py
+++ zerver/lib/url_preview/preview.py
@@ -21,7 +21,8 @@ link_regex = re.compile(
     r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|"  # domain...
     r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"  # ...or ip
     r"(?::\d+)?"  # optional port
-    r"(?:/[^\s]*)?$",  # allows spaces
+    # r"(?:/?|[/?]\S+)$",  #original
+    r"(?:/[^\s]*)?$", # This is will help in preview of links with spaces
     re.IGNORECASE,
 )
 
