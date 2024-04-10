5924a441342808e5fc46cf72512d0db9143d3b24
Anders Kaseorg <anders@zulip.com>
2024-04-10 14:56:20 -0700

    requirements: Upgrade Python requirements.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -48,4 +48,4 @@ API_FEATURE_LEVEL = 247
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (267, 1)
+PROVISION_VERSION = (267, 2)  # last bumped 2024-04-10 for Python requirements upgrade
