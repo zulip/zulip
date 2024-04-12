c3f08c4300038330558d17aac38d9edb6f4a7c79
Lauryn Menard <lauryn@zulip.com>
2024-04-12 13:45:46 +0200

    filter: Use "channels" operator for the Filter class.
    Update the Filter class to use "channels" as the canoncial operator
    for public stream searches and web-public message fetch narrows,
    but keep using "streams" for user-facing text and URLs.
    
    When searching, "channels" does not create any suggestions until
    a colon is added and then it shows suggestions with "streams". And
    when a search string with channel as an operator is entered, then
    it is replaced by "streams" as well.
    
    Part of stream to channel rename project.

--- web/src/hash_util.ts
+++ web/src/hash_util.ts
@@ -94,7 +94,7 @@ export function search_terms_to_hash(terms?: NarrowTerm[]): string {
 
         for (const term of terms) {
             // Support legacy tuples.
-            const operator = util.canonicalize_stream_synonym(term.operator);
+            const operator = util.canonicalize_stream_synonyms(term.operator);
             const operand = term.operand;
 
             const sign = term.negated ? "-" : "";
