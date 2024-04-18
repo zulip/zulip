72cfa4573cdf79a91b9891cb5ed6605fad05e0ae
Lauryn Menard <lauryn@zulip.com>
2024-04-18 21:33:03 +0200

    web-compose-typeahead: Update wildcard string for stream rename channel.
    Updates "Notify stream" string in the stream wildcard typeahead to
    be "Notify channel" instead.
    
    Part of stream to channel rename project.

--- web/tests/composebox_typeahead.test.js
+++ web/tests/composebox_typeahead.test.js
@@ -86,10 +86,10 @@ run_test("verify wildcard mentions typeahead for stream message", () => {
     assert.equal(mention_topic.email, "topic");
     assert.equal(mention_topic.full_name, "topic");
 
-    assert.equal(mention_all.special_item_text, "all (translated: Notify stream)");
-    assert.equal(mention_everyone.special_item_text, "everyone (translated: Notify stream)");
-    assert.equal(mention_stream.special_item_text, "stream (translated: Notify stream)");
-    assert.equal(mention_channel.special_item_text, "channel (translated: Notify stream)");
+    assert.equal(mention_all.special_item_text, "all (translated: Notify channel)");
+    assert.equal(mention_everyone.special_item_text, "everyone (translated: Notify channel)");
+    assert.equal(mention_stream.special_item_text, "stream (translated: Notify channel)");
+    assert.equal(mention_channel.special_item_text, "channel (translated: Notify channel)");
     assert.equal(mention_topic.special_item_text, "topic (translated: Notify topic)");
 
     compose_validate.stream_wildcard_mention_allowed = () => false;
