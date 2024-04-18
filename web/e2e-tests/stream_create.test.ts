36dc90d2eb9238aee6bdc7c57d46a464b61f54e6
Lauryn Menard <lauryn@zulip.com>
2024-04-18 16:26:11 +0200

    web-stream-create: Update frontend errors for stream rename channel.
    Updates translated error and warning strings when creating a new
    stream in the web app to use channel instead of stream.
    
    Part of stream to channel rename project.

--- web/e2e-tests/stream_create.test.ts
+++ web/e2e-tests/stream_create.test.ts
@@ -115,13 +115,13 @@ async function test_streams_with_empty_names_cannot_be_created(page: Page): Prom
     await page.waitForSelector("form#stream_creation_form", {visible: true});
     await common.fill_form(page, "form#stream_creation_form", {stream_name: "  "});
     await page.click("form#stream_creation_form button.finalize_create_stream");
-    assert.strictEqual(await stream_name_error(page), "Choose a name for the new stream.");
+    assert.strictEqual(await stream_name_error(page), "Choose a name for the new channel.");
 }
 
 async function test_streams_with_duplicate_names_cannot_be_created(page: Page): Promise<void> {
     await common.fill_form(page, "form#stream_creation_form", {stream_name: "Puppeteer"});
     await page.click("form#stream_creation_form button.finalize_create_stream");
-    assert.strictEqual(await stream_name_error(page), "A stream with this name already exists.");
+    assert.strictEqual(await stream_name_error(page), "A channel with this name already exists.");
 
     const cancel_button_selector = "form#stream_creation_form button.button.white";
     await page.click(cancel_button_selector);
