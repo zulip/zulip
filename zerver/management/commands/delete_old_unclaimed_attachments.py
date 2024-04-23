202551e824fcf7aa70feb3e2af6ac88bc0c1bcb5
Alex Vandiver <alexmv@zulip.com>
2024-04-23 19:30:37 +0000

    management: Add lockfiles to all _once management jobs.
    These would be bad if more than once ran at once.

--- zerver/management/commands/delete_old_unclaimed_attachments.py
+++ zerver/management/commands/delete_old_unclaimed_attachments.py
@@ -8,6 +8,7 @@ from typing_extensions import override
 
 from zerver.actions.uploads import do_delete_old_unclaimed_attachments
 from zerver.lib.attachments import get_old_unclaimed_attachments
+from zerver.lib.management import abort_unless_locked
 from zerver.lib.upload import all_message_attachments, delete_message_attachments
 from zerver.models import ArchivedAttachment, Attachment
 
@@ -44,6 +45,7 @@ class Command(BaseCommand):
         )
 
     @override
+    @abort_unless_locked
     def handle(self, *args: Any, **options: Any) -> None:
         delta_weeks = options["delta_weeks"]
         print(f"Deleting unclaimed attached files older than {delta_weeks} weeks")
--- zilencer/management/commands/invoice_plans.py
+++ zilencer/management/commands/invoice_plans.py
@@ -3,7 +3,7 @@ from typing import Any
 from django.conf import settings
 from typing_extensions import override
 
-from zerver.lib.management import ZulipBaseCommand
+from zerver.lib.management import ZulipBaseCommand, abort_unless_locked
 
 if settings.BILLING_ENABLED:
     from corporate.lib.stripe import invoice_plans_as_needed
@@ -13,6 +13,7 @@ class Command(ZulipBaseCommand):
     help = """Generates invoices for customers if needed."""
 
     @override
+    @abort_unless_locked
     def handle(self, *args: Any, **options: Any) -> None:
         if settings.BILLING_ENABLED:
             # Uncomment to test with a specific date.
