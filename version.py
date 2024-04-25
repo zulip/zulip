57867e4cb4c1f0d1e84f9c3e84ce3578ff89a7eb
Anders Kaseorg <anders@zulip.com>
2024-04-25 15:57:46 -0700

    install-transifex-cli: Upgrade Transifex CLI from 1.6.10 to 1.6.11.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -48,4 +48,4 @@ API_FEATURE_LEVEL = 255
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (269, 1)  # last bumped 2024-04-25 for Node.js upgrade
+PROVISION_VERSION = (269, 2)  # last bumped 2024-04-25 for Transifex CLI upgrade
