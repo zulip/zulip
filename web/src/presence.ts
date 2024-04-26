96fda8cedb6363ff65a312396594edc43623c617
Mateusz Mandera <mateusz.mandera@zulip.com>
2024-04-26 02:33:14 +0200

    presence: Fix typo in a comment.

--- web/src/presence.ts
+++ web/src/presence.ts
@@ -182,7 +182,7 @@ export function set_info(
         const user_id = Number.parseInt(user_id_str, 10);
 
         // Note: In contrast with all other state updates received
-        // receive from the server, presence data is updated via a
+        // from the server, presence data is updated via a
         // polling process rather than the events system
         // (server_events_dispatch.js).
         //
