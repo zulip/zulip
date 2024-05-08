68fd70f9527e475b7c2608ea03eea7ffdd6fb7ab
Alex Vandiver <alexmv@zulip.com>
2024-05-08 17:31:29 +0000

    check_send_receive_time: Print no output on success.

--- puppet/kandra/manifests/prod_app_frontend_once.pp
+++ puppet/kandra/manifests/prod_app_frontend_once.pp
@@ -21,7 +21,7 @@ class kandra::prod_app_frontend_once {
   zulip::cron { 'check_send_receive_time':
     hour      => '*',
     minute    => '*',
-    command   => '/usr/lib/nagios/plugins/zulip_app_frontend/check_send_receive_time --site=https://$(/home/zulip/deployments/current/scripts/get-django-setting NAGIOS_BOT_HOST) >/dev/null',
+    command   => '/usr/lib/nagios/plugins/zulip_app_frontend/check_send_receive_time --site=https://$(/home/zulip/deployments/current/scripts/get-django-setting NAGIOS_BOT_HOST)',
     use_proxy => false,
   }
   zulip::cron { 'check_user_zephyr_mirror_liveness':
