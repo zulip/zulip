634237f5734aed188642ba6896726be4ae167eff
Vector73 <v.shm.kunal@gmail.com>
2024-05-09 16:58:39 +0530

    emails: Replace occurrences of uri with url in email templates.
    This commit replaces occurrences of realm_uri with realm_url in email templates
    and other related backend files.
    
    Co-authored-by: Junyao Chen <junyao.chen@socitydao.org>

--- version.py
+++ version.py
@@ -48,4 +48,4 @@ API_FEATURE_LEVEL = 262
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (270, 3)  # last bumped 2024-05-15 for patching @types/plotly.js
+PROVISION_VERSION = (271, 0)  # last bumped 2024-05-24 for translation string changes
