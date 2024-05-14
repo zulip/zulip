cbb8e105a5b2a9aa80b74403bbad65c56d28994c
sanchi-t <ssharmas10662@gmail.com>
2024-05-15 01:28:48 +0530

    stream_create: Enable the creation of support streams.

--- web/src/stream_create.js
+++ web/src/stream_create.js
@@ -268,6 +268,13 @@ function create_stream() {
     );
     data.can_remove_subscribers_group = can_remove_subscribers_group_id;
 
+    const stream_topic_access_group_id = Number.parseInt(
+        stream_settings_components.new_stream_stream_topic_access_group_widget.value(),
+        10,
+    );
+
+    data.stream_topic_access_group = stream_topic_access_group_id;
+
     loading.make_indicator($("#stream_creating_indicator"), {
         text: $t({defaultMessage: "Creating channel..."}),
     });
@@ -460,4 +467,5 @@ export function set_up_handlers() {
     });
 
     stream_settings_components.new_stream_can_remove_subscribers_group_widget.setup();
+    stream_settings_components.new_stream_stream_topic_access_group_widget.setup();
 }
