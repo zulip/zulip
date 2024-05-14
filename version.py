6914d3151d24603d43295ce5eea8f6a4d2c37903
Anders Kaseorg <anders@zulip.com>
2024-05-14 11:32:47 -0700

    dependencies: Add @types/is-url to devDependencies.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -48,4 +48,4 @@ API_FEATURE_LEVEL = 260
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (270, 0)  # last bumped 2024-04-30 for Python requirements upgrade
+PROVISION_VERSION = (270, 1)  # last bumped 2024-05-14 for adding @types/is-url
