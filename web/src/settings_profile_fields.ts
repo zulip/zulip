622e026ac4ce00346b5f7bab022b56509037b5ba
tnmkr <tnmdotkr@gmail.com>
2024-05-28 18:17:43 +0530

    custom_profile_fields: Add "Alphabetize choices" button.
    This commit adds a "Alphabetize choices" button for SELECT also known as
    "List of options" type custom profile fields. This button is enabled
    only when the given options are not in an alphabetized order. The
    options themselves can get out of order from either user text input, or
    from list modifications like addition or drag.
    
    We follow case-insensitive alphabetization such that "vim" comes before
    "VS Code". This is similar to how we alphabetize elsewhere like stream
    lists.
    
    Fixes: #28607.

--- web/src/settings_profile_fields.ts
+++ web/src/settings_profile_fields.ts
@@ -22,6 +22,7 @@ import type {CustomProfileField} from "./state_data";
 import {current_user, realm} from "./state_data";
 import type {HTMLSelectOneElement, UserExternalAccountData} from "./types";
 import * as ui_report from "./ui_report";
+import * as util from "./util";
 
 type FieldChoice = {
     value: string;
@@ -398,6 +399,36 @@ function disable_submit_btn_if_no_property_changed(
     );
 }
 
+function alphabetize_profile_field_choices($sortable_element: JQuery): void {
+    assert($sortable_element[0] !== undefined);
+    const sortable_instance = SortableJS.get($sortable_element[0]);
+    assert(sortable_instance !== undefined);
+
+    const choices_array: [string, string][] = [];
+    const empty_choices_array: [string, string][] = [];
+
+    const choices_id_array = sortable_instance.toArray();
+    for (const choice_id of choices_id_array) {
+        const choice_value = $(sortable_instance.el)
+            .find<HTMLInputElement>(`div[data-value="${choice_id}"] input`)
+            .val()!;
+
+        // Remove empty choices from the array that we will sort. After sorting, we append these
+        // to the sorted array.;
+        if (choice_value.length === 0) {
+            empty_choices_array.push(["", choice_id]);
+            continue;
+        }
+
+        choices_array.push([choice_value, choice_id]);
+    }
+
+    choices_array.sort((a, b) => util.strcmp(a[0], b[0]));
+    choices_array.push(...empty_choices_array);
+
+    sortable_instance.sort(choices_array.map((v) => v[1]));
+}
+
 function set_up_select_field_edit_form(
     $profile_field_form: JQuery,
     field: CustomProfileField,
@@ -429,6 +460,7 @@ function set_up_select_field_edit_form(
         },
         filter: "input",
         preventOnFilter: false,
+        dataIdAttr: "data-value",
         onSort() {
             disable_submit_btn_if_no_property_changed($profile_field_form, field);
         },
@@ -510,6 +542,15 @@ function open_edit_form_modal(this: HTMLElement): void {
             "button.delete-choice",
             function (this: HTMLElement) {
                 delete_choice_row_for_edit(this, $profile_field_form, field);
+            },
+        );
+        $profile_field_form.on(
+            "click",
+            ".profile-field-choices-wrapper > button.alphabetize-choices-button",
+            function (this: HTMLElement) {
+                assert(this instanceof HTMLButtonElement);
+                alphabetize_profile_field_choices($edit_profile_field_choices_container);
+                disable_submit_btn_if_no_property_changed($profile_field_form, field);
             },
         );
 
@@ -734,6 +775,7 @@ function set_up_select_field(): void {
             },
             filter: "input",
             preventOnFilter: false,
+            dataIdAttr: "data-value",
         });
     }
 
@@ -760,6 +802,14 @@ function set_up_select_field(): void {
     $profile_field_choices.on("click", "button.delete-choice", function (this: HTMLElement) {
         delete_choice_row(this);
     });
+    $("#profile_field_choices_row").on(
+        "click",
+        "button.alphabetize-choices-button",
+        function (this: HTMLElement) {
+            assert(this instanceof HTMLButtonElement);
+            alphabetize_profile_field_choices($profile_field_choices);
+        },
+    );
 }
 
 function set_up_external_account_field(): void {
