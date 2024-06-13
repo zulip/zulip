fd50f12548c9e9991327d2fa12b8f33491d1d6d1
Alex Vandiver <alexmv@zulip.com>
2024-06-13 03:34:16 +0000

    requirements: Remove now-unused cairosvg.

--- version.py
+++ version.py
@@ -48,4 +48,4 @@ API_FEATURE_LEVEL = 266
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (281, 2)  # bumped 2024-06-18 for adding libvips
+PROVISION_VERSION = (282, 0)  # bumped 2024-06-18 for removing cairosvg
