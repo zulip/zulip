a4e545fbbe12fc7fa2a137e1e9c3185a1486ac5b
Prakhar Pratyush <prakhar841301@gmail.com>
2024-06-18 17:04:32 +0530

    docs: Add subsystem doc for 'Onboarding Steps'.
    Fixes part of #30043.

--- zerver/lib/onboarding_steps.py
+++ zerver/lib/onboarding_steps.py
@@ -1,3 +1,5 @@
+# See https://zulip.readthedocs.io/en/latest/subsystems/onboarding-steps.html
+# for documentation on this subsystem.
 from dataclasses import dataclass
 from typing import Any, Dict, List
 
