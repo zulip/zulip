39d4ddaa96339b15550b6c21aa61663a3a07041c
Anders Kaseorg <anders@zulip.com>
2024-06-21 13:42:22 -0700

    stream_edit_subscribers: Rename misleading variable.
    .safeParse does not return a “schema”.
    
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- web/src/stream_edit_subscribers.ts
+++ web/src/stream_edit_subscribers.ts
@@ -246,7 +246,7 @@ function subscribe_new_users({pill_user_ids}: {pill_user_ids: number[]}): void {
     function invite_failure(xhr: JQuery.jqXHR): void {
         let message = "Failed to subscribe user!";
 
-        const failure_response_schema = z
+        const parsed = z
             .object({
                 result: z.literal("error"),
                 msg: z.string(),
@@ -254,8 +254,8 @@ function subscribe_new_users({pill_user_ids}: {pill_user_ids: number[]}): void {
             })
             .safeParse(xhr.responseJSON);
 
-        if (failure_response_schema.success) {
-            message = failure_response_schema.data.msg;
+        if (parsed.success) {
+            message = parsed.data.msg;
         }
         show_stream_subscription_request_result({
             message,
