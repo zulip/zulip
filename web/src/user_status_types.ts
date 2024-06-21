68f158dfd43503aaf366e4af94abd067d86724ac
Anders Kaseorg <anders@zulip.com>
2024-06-21 12:13:00 -0700

    user_status: Validate parameters with Zod.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

---
+++ web/src/user_status_types.ts
@@ -0,0 +1,18 @@
+import {z} from "zod";
+
+export const user_status_schema = z.intersection(
+    z.object({
+        status_text: z.string().optional(),
+        away: z.boolean().optional(),
+    }),
+    z.union([
+        z.object({
+            emoji_name: z.string(),
+            emoji_code: z.string(),
+            reaction_type: z.enum(["zulip_extra_emoji", "realm_emoji", "unicode_emoji"]),
+        }),
+        z.object({
+            emoji_name: z.undefined(),
+        }),
+    ]),
+);
