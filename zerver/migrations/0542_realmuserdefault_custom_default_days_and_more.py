4ae4677fe4101dde25ebf427742bc95c9e4bc20b
Rishi Krishna <rishi1.krishna1@gmail.com>
2024-06-27 17:56:39 -0500

    migrations: Updated migration for feature.

--- zerver/migrations/0542_realmuserdefault_custom_default_days_and_more.py
+++ zerver/migrations/0542_realmuserdefault_custom_default_days_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("zerver", "0536_add_message_type"),
+        ("zerver", "0541_alter_realmauditlog_options"),
     ]
 
     operations = [
