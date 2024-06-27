95f5d298de0e9160d88b8f93e0f46bb241a237ba
Anders Kaseorg <anders@zulip.com>
2024-06-26 20:17:29 -0700

    install-node: Upgrade Node.js from 20.13.1 to 20.15.0.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -48,4 +48,4 @@ API_FEATURE_LEVEL = 266
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (284, 0)  # bumped 2024-06-26 for upgrading JavaScript dependencies
+PROVISION_VERSION = (284, 1)  # bumped 2024-06-26 for upgrading Node.js
