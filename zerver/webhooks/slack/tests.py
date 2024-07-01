7ff5040b11b3e6eaa77535cd1654270af4a3576d
PieterCK <pieterceka123@gmail.com>
2024-07-01 10:22:21 +0700

    slack-integration: Block requests from Slack retries.
    A Slack fail condition occurs when we don't respond with HTTP 200 within
    3 seconds after Slack calls our endpoint. If this happens, Slack will
    retry sending the same payload. This is often triggered because we need
    to perform callbacks when converting messages. To avoid sending the same
    message multiple times, we block subsequent retry calls from Slack.
    
    This commit returns early HTTP 200 response as soon as we get any retry
    calls from Slack.
    
    Part of #30465.
    
    (cherry picked from commit 42a22e6aaa00dd4fb702c5531b9d3a48a5326ddc)

--- zerver/webhooks/slack/tests.py
+++ zerver/webhooks/slack/tests.py
@@ -299,6 +299,18 @@ class SlackWebhookTests(WebhookTestCase):
             content_type="application/json",
         )
 
+    def test_block_slack_retries(self) -> None:
+        payload = self.get_body("message_with_normal_text")
+        with patch("zerver.webhooks.slack.view.check_send_webhook_message") as m:
+            result = self.client_post(
+                self.url,
+                payload,
+                headers={"X-Slack-Retry-Num": 1},
+                content_type="application/json",
+            )
+        self.assertFalse(m.called)
+        self.assert_json_success(result)
+
 
 class SlackLegacyWebhookTests(WebhookTestCase):
     CHANNEL_NAME = "slack"
