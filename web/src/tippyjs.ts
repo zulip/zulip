fa3956f7ffb3367ed4fe1c6e2fa81cc36ca98b84
Aditya <aditya.chaudhary1558@gmail.com>
2024-07-02 22:36:33 +0545

    tooltip: Add tippy tooltip for for not subscribed.
    This commit adds a tippy tooltip for the not subscribed icon in the typeahead warning for user.
    
    Fixes #30668

--- web/src/tippyjs.ts
+++ web/src/tippyjs.ts
@@ -135,6 +135,19 @@ export function initialize(): void {
         },
     });
 
+    tippy.delegate("body", {
+        target: ".fa-exclamation-triangle",
+        placement: "right",
+        appendTo: () => document.body,
+        onShow(instance) {
+            instance.setContent("You are not currently subscribed to this channel.");
+        },
+        onHidden(instance) {
+            instance.destroy();
+        },
+        maxWidth: 320,
+    });
+
     // Variant of .tippy-left-sidebar-tooltip configuration. Since
     // this element doesn't have an always visible label, and
     // thus hovering it is a way to find out what it does, give
