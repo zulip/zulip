37917bbae199dc9117458a79756a02f142e7fb79
Shubham Padia <shubham@zulip.com>
2024-07-09 11:53:59 +0000

    user_group_popover: Add support for displaying subgroup members.
    We list all the members of a group including recursive subgroup members
    in the user group popover.
    
    We also add another change in this commit to show `Everyone` if the
    group name is `role:everyone` by using
    `user_groups.get_display_group_name`.

--- web/src/user_group_popover.ts
+++ web/src/user_group_popover.ts
@@ -93,9 +93,11 @@ export function toggle_user_group_info_popover(
                 }
                 user_group_popover_instance = instance;
                 const args = {
-                    group_name: group.name,
+                    group_name: user_groups.get_display_group_name(group),
                     group_description: group.description,
-                    members: sort_group_members(fetch_group_members([...group.members])),
+                    members: sort_group_members(
+                        fetch_group_members([...user_groups.get_recursive_group_members(group)]),
+                    ),
                     group_edit_url: hash_util.group_edit_url(group, "general"),
                     is_guest: current_user.is_guest,
                 };
