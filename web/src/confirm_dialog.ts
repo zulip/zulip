ef51066c5ab0e53673f0f0966796bba904b09345
Kenneth Rodrigues <kenneth012004@outlook.com>
2024-07-11 02:00:47 +0530

    confirm_dialog: Prevent multiple dialogs from opening.
    Add a check to see if an existing modal is open before opening
    a new modal. This prevents multiple dialogs from opening.
    Thus when the user presses the enter key multiple times,
    only one dialog will open when `focus_submit_on_open` is set to false.

--- web/src/confirm_dialog.ts
+++ web/src/confirm_dialog.ts
@@ -1,12 +1,17 @@
+import $ from "jquery";
+
 import * as dialog_widget from "./dialog_widget";
 import type {DialogWidgetConfig} from "./dialog_widget";
 import {$t_html} from "./i18n";
 
 export function launch(conf: DialogWidgetConfig): string {
-    return dialog_widget.launch({
-        close_on_submit: true,
-        focus_submit_on_open: true,
-        html_submit_button: $t_html({defaultMessage: "Confirm"}),
-        ...conf,
-    });
+    if ($(".modal__container[role='dialog']").length === 0) {
+        return dialog_widget.launch({
+            close_on_submit: true,
+            focus_submit_on_open: true,
+            html_submit_button: $t_html({defaultMessage: "Confirm"}),
+            ...conf,
+        });
+    }
+    return "Dialog already open";
 }
