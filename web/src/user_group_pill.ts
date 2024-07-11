054f08c6dfef6a4a4ea94d31719e8c5fb9efd2ae
Shubham Padia <shubham@zulip.com>
2024-07-11 08:45:15 +0000

    user_group_pill: Exclude inactive members from pill.
    While using the pill in `stream_create`, it was noticed that deactivated
    users were also part of the user_ids returned by the user_group_pill,
    which we do not want to do.
    This pill is used in two areas: compose box and stream create/edit.
    This commit will only affect stream create/edit since compose box was
    just using the typeahead data from user_group_pill and nothing else.
    Stream and user pill handle inactive users already, so no change is
    needed there.

--- web/src/user_group_pill.ts
+++ web/src/user_group_pill.ts
@@ -1,5 +1,6 @@
 import {$t_html} from "./i18n";
 import type {InputPillContainer, InputPillItem} from "./input_pill";
+import * as people from "./people";
 import type {CombinedPillContainer, CombinedPillItem} from "./typeahead_helper";
 import type {UserGroup} from "./user_groups";
 import * as user_groups from "./user_groups";
@@ -18,10 +19,10 @@ export type UserGroupPillData = UserGroup & {
 };
 
 function display_pill(group: UserGroup): string {
-    const group_members = user_groups.get_recursive_group_members(group);
+    const group_members = get_group_members(group);
     return $t_html(
         {defaultMessage: "{group_name}: {group_size, plural, one {# user} other {# users}}"},
-        {group_name: user_groups.get_display_group_name(group), group_size: group_members.size},
+        {group_name: user_groups.get_display_group_name(group), group_size: group_members.length},
     );
 }
 
@@ -56,7 +57,7 @@ export function get_user_ids(pill_widget: UserGroupPillWidget | CombinedPillCont
     for (const user_group_item of pill_widget.items()) {
         if (user_group_item.type === "user_group") {
             const user_group = user_groups.get_user_group_from_id(user_group_item.group_id);
-            const group_members = user_groups.get_recursive_group_members(user_group);
+            const group_members = get_group_members(user_group);
             user_ids.push(...group_members);
         }
     }
@@ -65,6 +66,11 @@ export function get_user_ids(pill_widget: UserGroupPillWidget | CombinedPillCont
     user_ids.sort((a, b) => a - b);
 
     return user_ids;
+}
+
+function get_group_members(user_group: UserGroup): number[] {
+    const user_ids = [...user_groups.get_recursive_group_members(user_group)];
+    return user_ids.filter((user_id) => people.is_person_active(user_id));
 }
 
 export function append_user_group(group: UserGroup, pill_widget: CombinedPillContainer): void {
