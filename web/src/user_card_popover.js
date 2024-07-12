zulip/zulip

commit 5142f1279a45d00320904b369b092f0c9c116b90

popovers: Refactor show popovers as overlay on mobile widths.
This also fixes user_card_popover not being displayed as overlay
on mobile widths.

web/src/user_card_popover.js
@@ -311,7 +311,6 @@ function show_user_card_popover(
     private_msg_class,
     template_class,
     popover_placement,
-    show_as_overlay,
     on_mount,
 ) {
     const args = get_user_card_popover_data(
@@ -377,7 +376,9 @@ function show_user_card_popover(
                 }
             },
         },
-        {show_as_overlay},
+        {
+            show_as_overlay_on_mobile: true,
+        },
     );
 }
 

web/src/emoji_picker.js
@@ -9,7 +9,6 @@ import render_emoji_showcase from "../templates/popovers/emoji/emoji_showcase.hb
 
 import * as blueslip from "./blueslip";
 import * as compose_ui from "./compose_ui";
-import {media_breakpoints_num} from "./css_variables";
 import * as emoji from "./emoji";
 import * as keydown_util from "./keydown_util";
 import * as message_lists from "./message_lists";
@@ -686,21 +685,15 @@ export function toggle_emoji_popover(target, id, additional_popover_options) {
         current_message_id = id;
     }
 
-    let show_as_overlay = false;
-
-    // If the window is mobile-sized, we will render the
-    // emoji popover centered on the screen with the overlay.
-    if (window.innerWidth <= media_breakpoints_num.md) {
-        show_as_overlay = true;
-    }
-
     popover_menus.toggle_popover_menu(
         target,
         {
             ...get_default_emoji_popover_options(),
             ...additional_popover_options,
         },
-        {show_as_overlay},
+        {
+            show_as_overlay_on_mobile: true,
+        },
     );
 }
 function register_click_handlers() {

web/src/giphy.js
@@ -5,7 +5,6 @@ import render_giphy_picker from "../templates/giphy_picker.hbs";
 
 import * as blueslip from "./blueslip";
 import * as compose_ui from "./compose_ui";
-import {media_breakpoints_num} from "./css_variables";
 import {page_params} from "./page_params";
 import * as popover_menus from "./popover_menus";
 import * as rows from "./rows";
@@ -174,14 +173,6 @@ export function hide_giphy_popover() {
 }
 
 function toggle_giphy_popover(target) {
-    let show_as_overlay = false;
-
-    // If the window is mobile-sized, we will render the
-    // giphy popover centered on the screen with the overlay.
-    if (window.innerWidth <= media_breakpoints_num.md) {
-        show_as_overlay = true;
-    }
-
     popover_menus.toggle_popover_menu(
         target,
         {
@@ -236,7 +227,9 @@ function toggle_giphy_popover(target) {
                 hide_giphy_popover();
             },
         },
-        {show_as_overlay},
+        {
+            show_as_overlay_on_mobile: true,
+        },
     );
 }
 

web/src/popover_menus.js
@@ -6,6 +6,7 @@ import $ from "jquery";
 import tippy from "tippy.js";
 
 import * as blueslip from "./blueslip";
+import {media_breakpoints_num} from "./css_variables";
 import * as overlays from "./overlays";
 import * as popovers from "./popovers";
 
@@ -211,7 +212,9 @@ export function toggle_popover_menu(target, popover_props, options) {
     const instance = target._tippy;
     let mobile_popover_props = {};
 
-    if (options?.show_as_overlay) {
+    // If the window is mobile-sized, we will render the
+    // popover centered on the screen as an overlay.
+    if (options?.show_as_overlay_on_mobile && window.innerWidth <= media_breakpoints_num.md) {
         mobile_popover_props = {
             ...get_props_for_popover_centering(popover_props),
         };

web/src/user_group_popover.js
@@ -4,7 +4,6 @@ import render_user_group_info_popover from "../templates/popovers/user_group_inf
 
 import * as blueslip from "./blueslip";
 import * as buddy_data from "./buddy_data";
-import {media_breakpoints_num} from "./css_variables";
 import * as message_lists from "./message_lists";
 import * as people from "./people";
 import * as popover_menus from "./popover_menus";
@@ -57,14 +56,6 @@ export function toggle_user_group_info_popover(element, message_id) {
     const user_group_id = Number.parseInt($elt.attr("data-user-group-id"), 10);
     const group = user_groups.get_user_group_from_id(user_group_id);
 
-    let show_as_overlay = false;
-
-    // If the window is mobile-sized, we will render the
-    // user group popover centered on the screen with the overlay.
-    if (window.innerWidth <= media_breakpoints_num.md) {
-        show_as_overlay = true;
-    }
-
     popover_menus.toggle_popover_menu(
         element,
         {
@@ -98,7 +89,9 @@ export function toggle_user_group_info_popover(element, message_id) {
                 hide();
             },
         },
-        {show_as_overlay},
+        {
+            show_as_overlay_on_mobile: true,
+        },
     );
 }
 

commit 106e68f65af5a97e9f094e9d17478ca36475caea

user_card_popover: Fix on_mount hooks not being called.
The extra undefined parameter appears to be a bug introduced in
5142f1279a45d00320904b369b092f0c9c116b90; when the show_as_overlay
parmater was dropped, the other callers to show_user_card_popover did
not have their extra argument removed.

This had the effect of not setting initial keyboard focus when opening
the popover.

web/src/user_card_popover.js
@@ -409,7 +409,6 @@ function toggle_user_card_popover_for_message(element, user, message, on_mount) 
             "respond_personal_button",
             "message_user_card",
             "right",
-            undefined,
             on_mount,
         );
     }
@@ -491,7 +490,6 @@ function toggle_sidebar_user_card_popover($target) {
         "compose_private_message",
         "user_sidebar",
         "left",
-        undefined,
         (instance) => {
             /* See comment in get_props_for_popover_centering for explanation of this. */
             $(instance.popper).find(".tippy-box").addClass("show-when-reference-hidden");