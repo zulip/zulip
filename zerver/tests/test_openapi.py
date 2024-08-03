b0bd13207d2e28c8cc28f52b1a5564d2643c8a4b
Vector73 <v.shm.kunal@gmail.com>
2024-08-03 12:30:52 +0530

    api_docs: Document "/export/realm" endpoints.

--- zerver/tests/test_openapi.py
+++ zerver/tests/test_openapi.py
@@ -215,8 +215,6 @@ class OpenAPIArgumentsTest(ZulipTestCase):
         ## And this one isn't, and isn't really representable
         # "/user_uploads/{realm_id_str}/{filename}",
         #### These realm administration settings are valuable to document:
-        # List data exports for organization (GET) or request one (POST)
-        "/export/realm",
         # Delete a data export.
         "/export/realm/{export_id}",
         # Manage default streams and default stream groups
