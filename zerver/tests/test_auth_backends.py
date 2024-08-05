zulip/zulip

commit da9e4e6e54ddbd06e2c16db17eb7cb0678b5a786

backends: Implementation of restricting certain backends by plan.
Only affects zulipchat, by being based on the BILLING_ENABLED setting.

The restricted backends in this commit are
- AzureAD - restricted to Standard plan
- SAML - restricted to Plus plan, although it was already practically
  restricted due to requiring server-side configuration to be done by us

This restriction is placed upon **enabling** a backend - so
organizations that already have a backend enabled, will continue to be
able to use it. This allows us to make exceptions and enable a backend
for an org manually via the shell, and to grandfather organizations into
keeping the backend they have been relying on.

zerver/views/realm.py
@@ -21,6 +21,7 @@ from zerver.actions.realm_settings import (
     do_set_realm_zulip_update_announcements_stream,
     parse_and_set_setting_value_if_required,
     validate_authentication_methods_dict_from_api,
+    validate_plan_for_authentication_methods,
 )
 from zerver.decorator import require_realm_admin, require_realm_owner
 from zerver.forms import check_subdomain_available as check_subdomain
@@ -205,6 +206,7 @@ def update_realm(
         validate_authentication_methods_dict_from_api(realm, authentication_methods)
         if True not in authentication_methods.values():
             raise JsonableError(_("At least one authentication method must be enabled."))
+        validate_plan_for_authentication_methods(realm, authentication_methods)
 
     if video_chat_provider is not None and video_chat_provider not in {
         p["id"] for p in Realm.VIDEO_CHAT_PROVIDERS.values()

zerver/tests/test_auth_backends.py
@@ -7200,6 +7200,48 @@ class TestAdminSetBackends(ZulipTestCase):
                 "Invalid authentication method: NoSuchBackend. Valid methods are: ['Dev', 'Email']",
             )
 
+        with self.settings(
+            AUTHENTICATION_BACKENDS=(
+                "zproject.backends.EmailAuthBackend",
+                "zproject.backends.DevAuthBackend",
+                "zproject.backends.AzureADAuthBackend",
+            ),
+        ):
+            realm = get_realm("zulip")
+            self.assertEqual(
+                realm.authentication_methods_dict(), {"Dev": True, "Email": True, "AzureAD": True}
+            )
+
+            # AzureAD is not available without a Standard plan, but we start off with it enabled.
+            # Disabling the backend should work.
+            result = self.client_patch(
+                "/json/realm",
+                {
+                    "authentication_methods": orjson.dumps(
+                        # Github is not a supported authentication backend right now.
+                        {"Email": True, "Dev": True, "AzureAD": False}
+                    ).decode()
+                },
+            )
+            self.assert_json_success(result)
+            self.assertEqual(
+                realm.authentication_methods_dict(), {"Dev": True, "Email": True, "AzureAD": False}
+            )
+
+            # However, due to the lack of the necessary plan, enabling will fail.
+            result = self.client_patch(
+                "/json/realm",
+                {
+                    "authentication_methods": orjson.dumps(
+                        # Github is not a supported authentication backend right now.
+                        {"Email": True, "Dev": True, "AzureAD": True}
+                    ).decode()
+                },
+            )
+            self.assert_json_error(
+                result, "Authentication method AzureAD is not available on your current plan."
+            )
+
 
 class EmailValidatorTestCase(ZulipTestCase):
     def test_valid_email(self) -> None:

web/src/server_events_dispatch.js
@@ -290,7 +290,20 @@ export function dispatch_normal_event(event) {
                     switch (event.property) {
                         case "default":
                             for (const [key, value] of Object.entries(event.data)) {
-                                realm["realm_" + key] = value;
+                                if (key === "authentication_methods") {
+                                    for (const [auth_method, enabled] of Object.entries(
+                                        event.data.authentication_methods,
+                                    )) {
+                                        realm.realm_authentication_methods[auth_method].enabled =
+                                            enabled;
+                                    }
+                                    settings_org.populate_auth_methods(
+                                        event.data.authentication_methods,
+                                    );
+                                } else {
+                                    realm["realm_" + key] = value;
+                                }
+
                                 if (Object.hasOwn(realm_settings, key)) {
                                     settings_org.sync_realm_settings(key);
                                 }
@@ -304,11 +317,6 @@ export function dispatch_normal_event(event) {
                                 if (key === "edit_topic_policy") {
                                     message_live_update.rerender_messages_view();
                                 }
-                            }
-                            if (event.data.authentication_methods !== undefined) {
-                                settings_org.populate_auth_methods(
-                                    event.data.authentication_methods,
-                                );
                             }
                             break;
                         case "icon":

web/src/settings_components.js
@@ -88,7 +88,22 @@ export function get_property_value(property_name, for_realm_default_settings, su
         return "no_restriction";
     }
 
+    if (property_name === "realm_authentication_methods") {
+        return realm_authentication_methods_to_boolean_dict();
+    }
+
     return realm[property_name];
+}
+
+export function realm_authentication_methods_to_boolean_dict() {
+    const auth_method_to_bool = {};
+    for (const [auth_method_name, auth_method_info] of Object.entries(
+        realm.realm_authentication_methods,
+    )) {
+        auth_method_to_bool[auth_method_name] = auth_method_info.enabled;
+    }
+
+    return auth_method_to_bool;
 }
 
 export function extract_property_name($elem, for_realm_default_settings) {

web/src/settings_org.js
@@ -388,18 +388,29 @@ function can_configure_auth_methods() {
     return false;
 }
 
-export function populate_auth_methods(auth_methods) {
+export function populate_auth_methods(auth_method_to_bool_map) {
     if (!meta.loaded) {
         return;
     }
     const $auth_methods_list = $("#id_realm_authentication_methods").expectOne();
-    auth_methods = settings_components.sort_object_by_key(auth_methods);
+    auth_method_to_bool_map = settings_components.sort_object_by_key(auth_method_to_bool_map);
     let rendered_auth_method_rows = "";
-    for (const [auth_method, value] of Object.entries(auth_methods)) {
-        rendered_auth_method_rows += render_settings_admin_auth_methods_list({
+    for (const [auth_method, value] of Object.entries(auth_method_to_bool_map)) {
+        // Certain authentication methods are not available to be enabled without
+        // purchasing a plan, so we need to disable them in this UI.
+        // The restriction only applies to **enabling** the auth method, so this
+        // logic is dependent on the current value.
+        // The reason for that is that if for any reason, the auth method is already
+        // enabled (for example, because it was manually enabled for the organization
+        // by request, as an exception) - the organization should be able to disable it
+        // if they don't want it anymore.
+        const cant_be_enabled =
+            !realm.realm_authentication_methods[auth_method].available && !value;
+
+        const render_args = {
             method: auth_method,
             enabled: value,
-            disable_configure_auth_method: !can_configure_auth_methods(),
+            disable_configure_auth_method: !can_configure_auth_methods() || cant_be_enabled,
             // The negated character class regexp serves as an allowlist - the replace() will
             // remove *all* symbols *but* digits (\d) and lowecase letters (a-z),
             // so that we can make assumptions on this string elsewhere in the code.
@@ -407,7 +418,14 @@ export function populate_auth_methods(auth_methods) {
             // 1) It contains at least one allowed symbol
             // 2) No two auth method names are identical after this allowlist filtering
             prefix: "id_authmethod" + auth_method.toLowerCase().replaceAll(/[^\da-z]/g, "") + "_",
-        });
+        };
+
+        if (cant_be_enabled) {
+            render_args.unavailable_reason =
+                realm.realm_authentication_methods[auth_method].unavailable_reason;
+        }
+
+        rendered_auth_method_rows += render_settings_admin_auth_methods_list(render_args);
     }
     $auth_methods_list.html(rendered_auth_method_rows);
 }
@@ -967,7 +985,8 @@ export function build_page() {
     populate_realm_domains_label(realm.realm_domains);
 
     // Populate authentication methods table
-    populate_auth_methods(realm.realm_authentication_methods);
+
+    populate_auth_methods(settings_components.realm_authentication_methods_to_boolean_dict());
 
     for (const property_name of simple_dropdown_properties) {
         settings_components.set_property_dropdown_value(property_name);

web/templates/settings/admin_auth_methods_list.hbs
@@ -4,5 +4,6 @@
       prefix=prefix
       is_checked=enabled
       label=method
-      is_disabled=disable_configure_auth_method}}
+      is_disabled=disable_configure_auth_method
+      tooltip_text=unavailable_reason}}
 </div>

web/tests/dispatch.test.js
@@ -564,13 +564,16 @@ run_test("realm settings", ({override}) => {
     realm.realm_allow_message_editing = false;
     realm.realm_message_content_edit_limit_seconds = 0;
     realm.realm_edit_topic_policy = 3;
+    realm.realm_authentication_methods = {Google: {enabled: false, available: true}};
     override(settings_org, "populate_auth_methods", noop);
     dispatch(event);
     assert_same(realm.realm_create_multiuse_invite_group, 3);
     assert_same(realm.realm_allow_message_editing, true);
     assert_same(realm.realm_message_content_edit_limit_seconds, 5);
     assert_same(realm.realm_edit_topic_policy, 4);
-    assert_same(realm.realm_authentication_methods, {Google: true});
+    assert_same(realm.realm_authentication_methods, {
+        Google: {enabled: true, available: true},
+    });
 
     event = event_fixtures.realm__update_dict__icon;
     override(realm_icon, "rerender", noop);

zerver/actions/create_realm.py
@@ -34,7 +34,7 @@ from zerver.models import (
 )
 from zerver.models.realms import get_org_type_display_name, get_realm
 from zerver.models.users import get_system_bot
-from zproject.backends import all_implemented_backend_names
+from zproject.backends import all_default_backend_names
 
 if settings.CORPORATE_ENABLED:
     from corporate.lib.support import get_realm_support_url
@@ -261,11 +261,10 @@ def do_create_realm(
         create_system_user_groups_for_realm(realm)
         set_default_for_realm_permission_group_settings(realm)
 
-        # We create realms with all authentications methods enabled by default.
         RealmAuthenticationMethod.objects.bulk_create(
             [
                 RealmAuthenticationMethod(name=backend_name, realm=realm)
-                for backend_name in all_implemented_backend_names()
+                for backend_name in all_default_backend_names()
             ]
         )
 

commit 6ec81ed1bf478a2dfea0961519aecdaa19f4a5c2

auth: Fix re-enabling of SAML/AzureAD in organization settings UI.
This bug was introduced in da9e4e6e54ddbd06e2c16db17eb7cb0678b5a786.
validate validate_plan_for_authentication_methods is already called
inside validate_authentication_methods_dict_from_api, conditionally on
settings.BILLING_ENABLED. This additional, redundant call runs
regardless of BILLING_ENABLED, and thus prevents a self-hosted server
from enabling certain backends in the organization settings UI.

The impact of this is limited - in order to encounter this bug, a
self-hosted server would have to first disable the backend in the UI, as
self-hosted realms are created with all backend flags enabled. A backend
doesn't show up in the org settings UI until it is first enabled in
AUTHENTICATION_BACKENDS in settings.py - that's why this is a rare
state. A sequence of steps like this has to be followed to reproduce:
1. Add the backend to AUTHENTICATION_BACKENDS in settings.py.
2. Disable the backend in the org settings UI.
3. Now try to re-enable it, which fails due to the bug.

zerver/views/realm.py
@@ -25,7 +25,6 @@ from zerver.actions.realm_settings import (
     do_set_realm_zulip_update_announcements_stream,
     parse_and_set_setting_value_if_required,
     validate_authentication_methods_dict_from_api,
-    validate_plan_for_authentication_methods,
 )
 from zerver.decorator import require_realm_admin, require_realm_owner
 from zerver.forms import check_subdomain_available as check_subdomain
@@ -199,7 +198,6 @@ def update_realm(
         validate_authentication_methods_dict_from_api(realm, authentication_methods)
         if True not in authentication_methods.values():
             raise JsonableError(_("At least one authentication method must be enabled."))
-        validate_plan_for_authentication_methods(realm, authentication_methods)
 
     if video_chat_provider is not None and video_chat_provider not in {
         p["id"] for p in Realm.VIDEO_CHAT_PROVIDERS.values()

zerver/tests/test_auth_backends.py
@@ -7368,6 +7368,21 @@ class TestAdminSetBackends(ZulipTestCase):
                 result, "Authentication method AzureAD is not available on your current plan."
             )
 
+            # With BILLING_ENABLED=False, no such restrictions apply.
+            with self.settings(BILLING_ENABLED=False):
+                result = self.client_patch(
+                    "/json/realm",
+                    {
+                        "authentication_methods": orjson.dumps(
+                            {"Email": True, "Dev": True, "AzureAD": True}
+                        ).decode()
+                    },
+                )
+            self.assert_json_success(result)
+            self.assertEqual(
+                realm.authentication_methods_dict(), {"Dev": True, "Email": True, "AzureAD": True}
+            )
+
 
 class EmailValidatorTestCase(ZulipTestCase):
     def test_valid_email(self) -> None: