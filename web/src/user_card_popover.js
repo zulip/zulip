42dce988fa2dfb88565f556a562aa67fa5174192
opmkumar <sachinaniket2004@gmail.com>
2024-08-09 20:46:57 +0530

    popovers: Add close button to user cards.
    Fixes #25275.

--- web/src/user_card_popover.js
+++ web/src/user_card_popover.js
@@ -589,6 +589,11 @@ function register_click_handlers() {
         e.preventDefault();
     });
 
+    $("body").on("click", ".exit_user_card_popover", (e) => {
+        hide_all();
+        e.stopPropagation();
+    });
+
     $("body").on("click", ".user-card-popover-actions .narrow_to_messages_sent", (e) => {
         const user_id = elem_to_user_id($(e.target).parents("ul"));
         const email = people.get_by_user_id(user_id).email;
