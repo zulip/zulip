2ef905ef5aea223bfcbec509741bca8d966443e0
Tim Abbott <tabbott@zulip.com>
2024-08-13 12:43:03 -0700

    migrations: Tweak realm foreign key dependency.
    As best I can tell, this dependency is important only to ensure that
    we have a Realm table to link to.

--- confirmation/migrations/0005_confirmation_realm.py
+++ confirmation/migrations/0005_confirmation_realm.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("zerver", "0124_stream_enable_notifications"),
+        ("zerver", "0001_initial"),
         ("confirmation", "0004_remove_confirmationmanager"),
     ]
 
