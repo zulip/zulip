5133f34a05c54b4e99e58af852faaa519c974405
joseph <josephho678@gmail.com>
2024-08-21 19:55:13 +0000

    attachments: Reuse base attachments schemas.
    This is a prepatory commit to reuse the base attachments schemas in
    other schemas that are expecting attachments as a field.

---
+++ web/src/attachments.ts
@@ -0,0 +1,22 @@
+import {z} from "zod";
+
+const attachments_schema = z.array(
+    z.object({
+        id: z.number(),
+        name: z.string(),
+        path_id: z.string(),
+        size: z.number(),
+        create_time: z.number(),
+        messages: z.array(
+            z.object({
+                id: z.number(),
+                date_sent: z.number(),
+            }),
+        ),
+    }),
+);
+
+export const attachment_api_response_schema = z.object({
+    attachments: attachments_schema,
+    upload_space_used: z.number(),
+});
