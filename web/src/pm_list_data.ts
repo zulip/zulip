7e6a61a7c9beb89189223603748eec20d0cdaead
Pratik Chanda <pratikchanda2000@gmail.com>
2024-08-25 04:28:40 +0530

    left_sidebar: Show unread @-mention indicators for DMs in left sidebar.
    Earlier, in left sidebar, only streams had unread @-mention indicators
    in individual streams and topics. DMs lacked unread @-mention
    indicators.
    
    This commit introduces unread @-mention indicators for DM rows in left
    sidebar using `unread_mention_dms` which stores `user_ids_string` mapped
    to `message_id`s having mention.
    
    Fixes part of zulip#28849.

--- web/src/pm_list_data.ts
+++ web/src/pm_list_data.ts
@@ -46,6 +46,7 @@ type DisplayObject = {
     user_circle_class: string | undefined;
     is_group: boolean;
     is_bot: boolean;
+    has_unread_mention: boolean;
 };
 
 export function get_conversations(search_string = ""): DisplayObject[] {
@@ -81,6 +82,8 @@ export function get_conversations(search_string = ""): DisplayObject[] {
         const recipients_string = people.get_recipients(user_ids_string);
 
         const num_unread = unread.num_unread_for_user_ids_string(user_ids_string);
+        const has_unread_mention =
+            unread.num_unread_mentions_for_user_ids_strings(user_ids_string) > 0;
         const is_group = user_ids_string.includes(",");
         const is_active = user_ids_string === active_user_ids_string;
 
@@ -112,6 +115,7 @@ export function get_conversations(search_string = ""): DisplayObject[] {
             user_circle_class,
             is_group,
             is_bot,
+            has_unread_mention,
         };
         display_objects.push(display_object);
     }
