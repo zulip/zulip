b43b036b5c43502d39d48a2c76d125fb063e5514
Anders Kaseorg <anders@zulip.com>
2024-08-26 13:47:55 -0700

    install-shfmt: Upgrade shfmt from 3.8.0 to 3.9.0.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -50,4 +50,4 @@ API_FEATURE_LEVEL = 283  # Last bumped for can_manage_group
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (292, 1)  # bumped 2024-08-25 to upgrade Node.js
+PROVISION_VERSION = (292, 2)  # bumped 2024-08-25 to upgrade shfmt
