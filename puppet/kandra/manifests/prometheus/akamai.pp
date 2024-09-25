60759ab5fb25a58c25c38db84aee06fd675f4b47
Alex Vandiver <alexmv@zulip.com>
2024-09-25 13:08:25 -0400

    kandra: Use generic "vector" process, not dedicated "akamai" process.
    This makes the Vector configuration extensible, to allow it to be used
    not just for ingesting Akamai logs.

--- puppet/kandra/manifests/prometheus/akamai.pp
+++ puppet/kandra/manifests/prometheus/akamai.pp
@@ -5,33 +5,19 @@ class kandra::prometheus::akamai {
   include kandra::vector
   include zulip::supervisor
 
-  $bin = $kandra::vector::bin
-  $conf = '/etc/vector.toml'
   $pipelines = {
     'static' => zulipsecret('secrets', 'akamai_static_sqs_url', ''),
     'realm'  => zulipsecret('secrets', 'akamai_realm_sqs_url', ''),
   }
 
-  file { $conf:
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('kandra/vector.toml.template.erb'),
+  concat::fragment { 'vector_akamai':
+    target  => $kandra::vector::conf,
+    order   => '50',
+    content => template('kandra/vector_akamai.toml.template.erb'),
   }
-  file { "${zulip::common::supervisor_conf_dir}/prometheus_akamai_exporter.conf":
-    ensure  => file,
-    require => [
-      User[zulip],
-      Package[supervisor],
-      File['/etc/vector.toml'],
-      File[$bin],
-    ],
-    before  => Exec['Cleanup vector'],
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('kandra/supervisor/conf.d/prometheus_akamai_exporter.conf.template.erb'),
-    notify  => Service[supervisor],
+  concat::fragment { 'vector_akamai_export':
+    target  => $kandra::vector::conf,
+    content => ',"akamai_logs2metrics*"',
+    order   => '90',
   }
 }
