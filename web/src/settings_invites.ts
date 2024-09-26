30c2a80ba852df8962b6e2817cdf5698ce5f8f1c
sujal shah <sujalshah28092004@gmail.com>
2024-09-27 03:02:37 +0530

    invites: Add the ability to edit multiuse invites on frontend.
    This commit adds frontend changes required for admins to edit
    multiuse invites with respect to the role and associated streams.

--- web/src/settings_invites.ts
+++ web/src/settings_invites.ts
@@ -3,18 +3,24 @@ import {z} from "zod";
 
 import render_settings_resend_invite_modal from "../templates/confirm_dialog/confirm_resend_invite.hbs";
 import render_settings_revoke_invite_modal from "../templates/confirm_dialog/confirm_revoke_invite.hbs";
+import render_edit_invite_user_modal from "../templates/edit_invite_user_modal.hbs";
 import render_admin_invites_list from "../templates/settings/admin_invites_list.hbs";
 
 import * as blueslip from "./blueslip";
 import * as channel from "./channel";
 import * as confirm_dialog from "./confirm_dialog";
+import * as dialog_widget from "./dialog_widget";
 import {$t, $t_html} from "./i18n";
+import * as invite from "./invite";
+import * as invite_stream_picker_pill from "./invite_stream_picker_pill";
 import * as ListWidget from "./list_widget";
 import * as loading from "./loading";
 import * as people from "./people";
 import * as settings_config from "./settings_config";
 import * as settings_data from "./settings_data";
 import {current_user, realm} from "./state_data";
+import * as stream_data from "./stream_data";
+import * as stream_pill from "./stream_pill";
 import * as timerender from "./timerender";
 import * as ui_report from "./ui_report";
 import * as util from "./util";
@@ -38,6 +44,7 @@ export const invite_schema = z.intersection(
         z.object({
             is_multiuse: z.literal(true),
             link_url: z.string(),
+            stream_ids: z.array(z.number()),
         }),
     ]),
 );
@@ -52,12 +59,22 @@ type Invite = z.output<typeof invite_schema> & {
     notify_referrer_on_join?: boolean;
 };
 
-const meta = {
-    loaded: false,
+type Meta = {
+    loaded: boolean;
+    invites: Invite[];
+    invite_id?: number;
 };
+
+const meta: Meta = {
+    loaded: false,
+    invites: [],
+};
+
+let stream_pill_widget: stream_pill.StreamPillWidget;
 
 export function reset(): void {
     meta.loaded = false;
+    meta.invites = [];
 }
 
 function failed_listing_invites(xhr: JQuery.jqXHR): void {
@@ -92,6 +109,7 @@ function populate_invites(invites_data: {invites: Invite[]}): void {
 
     add_invited_as_text(invites_data.invites);
 
+    meta.invites = invites_data.invites;
     const $invites_table = $("#admin_invites_table").expectOne();
     ListWidget.create($invites_table, invites_data.invites, {
         name: "admin_invites_list",
@@ -109,6 +127,9 @@ function populate_invites(invites_data: {invites: Invite[]}): void {
             item.img_src = people.small_avatar_url_for_person(
                 people.get_by_user_id(item.invited_by_user_id),
             );
+            if (!settings_data.user_can_create_multiuse_invite()) {
+                item.disable_buttons = true;
+            }
             return render_admin_invites_list({invite: item});
         },
         filter: {
@@ -211,6 +232,55 @@ function do_resend_invite({$row, invite_id}: {$row: JQuery; invite_id: string}):
         },
     });
 }
+type GetInvitationData = {
+    csrfmiddlewaretoken: string | undefined;
+    invite_as: number;
+    stream_ids: string;
+};
+
+function get_invitation_data(): GetInvitationData {
+    const invite_as = Number.parseInt(String($("#invite_as").val()), 10);
+
+    let stream_ids: number[] = [];
+    let include_realm_default_subscriptions = false;
+    if (
+        $("#invite_select_default_streams").prop("checked") ||
+        !settings_data.user_can_subscribe_other_users()
+    ) {
+        include_realm_default_subscriptions = true;
+    } else {
+        stream_ids = stream_pill.get_stream_ids(stream_pill_widget);
+    }
+
+    const data = {
+        csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').attr("value"),
+        invite_as,
+        stream_ids: JSON.stringify(stream_ids),
+        include_realm_default_subscriptions: JSON.stringify(include_realm_default_subscriptions),
+    };
+    return data;
+}
+
+function do_edit_invite(): void {
+    const $invite_status = $("#dialog_error");
+    const data = get_invitation_data();
+
+    void channel.patch({
+        url: "/json/invites/multiuse/" + meta.invite_id,
+        data,
+        success() {
+            dialog_widget.close();
+        },
+        error() {
+            ui_report.message("", $invite_status, "alert-warning");
+        },
+        complete() {
+            $("#edit-invite-form .dialog_submit_button").text($t({defaultMessage: "Save changes"}));
+            $("#edit-invite-form .dialog_submit_button").prop("disabled", false);
+            $("#edit-invite-form .dialog_cancel_button").prop("disabled", false);
+        },
+    });
+}
 
 export function set_up(initialize_event_handlers = true): void {
     meta.loaded = true;
@@ -290,6 +360,94 @@ export function on_load_success(
         });
 
         $(".dialog_submit_button").attr("data-invite-id", invite_id);
+    });
+
+    $(".admin_invites_table").on("click", ".edit-invite", function (this: HTMLElement, e) {
+        // This click event must not get propagated to parent container otherwise the modal
+        // will not show up because of a call to `close_active` in `settings.js`.
+        e.preventDefault();
+        e.stopPropagation();
+
+        const invite_id = $(this).attr("data-invite-id")!;
+
+        $(".dialog_submit_button").attr("data-invite-id", invite_id);
+        meta.invite_id = Number.parseInt(invite_id, 10);
+        const initial_invite: Invite | undefined = meta.invites.find(
+            (invite) => invite.id === meta.invite_id && invite.is_multiuse,
+        );
+        const html_body = render_edit_invite_user_modal({
+            invite_as_options: settings_config.user_role_values,
+            is_admin: current_user.is_admin,
+            is_owner: current_user.is_owner,
+            show_select_default_streams_option: stream_data.get_default_stream_ids().length !== 0,
+            can_subscribe_other_users: settings_data.user_can_subscribe_other_users(),
+        });
+
+        function invite_user_modal_post_render(): void {
+            $("#edit-invite-form .dialog_submit_button").prop("disabled", true);
+            $("#edit-invite-form .dialog_submit_button").attr(
+                "data-loading-text",
+                $t({defaultMessage: "Updating link…"}),
+            );
+            if (initial_invite?.is_multiuse) {
+                const $stream_pill_container = $("#invite_streams_container .pill-container");
+                if (initial_invite.include_realm_default_subscriptions) {
+                    $("#invite_select_default_streams").prop("checked", true);
+                    $(".add_streams_container").hide();
+                } else {
+                    $("#invite_select_default_streams").prop("checked", false);
+                    $(".add_streams_container").show();
+                }
+                stream_pill_widget = invite_stream_picker_pill.create(
+                    $stream_pill_container,
+                    initial_invite.stream_ids,
+                );
+                $("#invite_as").val(initial_invite.invited_as);
+            }
+
+            function state_unchanged(): boolean {
+                if (!initial_invite || !initial_invite.is_multiuse) {
+                    return true;
+                }
+                const initial_streams = [...initial_invite.stream_ids].sort();
+                let selected_streams: number[] = [];
+                if (!$("#invite_select_default_streams").prop("checked")) {
+                    selected_streams = stream_pill.get_stream_ids(stream_pill_widget);
+                    selected_streams = selected_streams.sort();
+                }
+                return (
+                    selected_streams.length === initial_streams.length &&
+                    selected_streams.every((val, index) => val === initial_streams[index]) &&
+                    Number.parseInt(String($("#invite_as").val()), 10) ===
+                        initial_invite.invited_as &&
+                    $("#invite_select_default_streams").prop("checked") ===
+                        initial_invite.include_realm_default_subscriptions
+                );
+            }
+            $("#edit-invite-form").on("change", "#invite_as", () => {
+                $("#edit-invite-form .dialog_submit_button").prop("disabled", state_unchanged());
+            });
+
+            $("#edit-invite-form").on("change", "div.input", () => {
+                $("#edit-invite-form .dialog_submit_button").prop("disabled", state_unchanged());
+            });
+
+            $("#invite_select_default_streams").on("change", () => {
+                invite.set_streams_to_join_list_visibility();
+                $("#edit-invite-form .dialog_submit_button").prop("disabled", state_unchanged());
+            });
+        }
+
+        dialog_widget.launch({
+            html_heading: $t_html({defaultMessage: "Edit invite"}),
+            html_body,
+            html_submit_button: $t_html({defaultMessage: "Save changes"}),
+            id: "edit-invite-form",
+            loading_spinner: true,
+            on_click: do_edit_invite,
+            post_render: invite_user_modal_post_render,
+            help_link: "/help/invite-new-users#edit-a-reusable-invitation-link",
+        });
     });
 }
 
