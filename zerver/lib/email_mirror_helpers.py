3b96a7254d5cdae20c3cbf0394b3d58a40d64c0f
Azariush Hussain <azariushhussain@gmail.com>
2024-10-01 17:22:44 +0530

    error_mirror_helper: Update helper to add channel. key when no slug_name

--- zerver/lib/email_mirror_helpers.py
+++ zerver/lib/email_mirror_helpers.py
@@ -72,7 +72,7 @@ def encode_email_address_helper(name: str, email_token: str, show_sender: bool =
     if encoded_name:
         encoded_token = f"{encoded_name}.{email_token}"
     else:
-        encoded_token = email_token
+        encoded_token = f"channel.{email_token}"
 
     if show_sender:
         encoded_token += ".show-sender"
