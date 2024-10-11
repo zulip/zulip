89d0ad1d60d053bb19645f05ab0cbe2776a0047d
Sahil Batra <sahil@zulip.com>
2024-10-11 08:53:32 +0530

    user_group_edit: Add support to update subgroups of existing groups.

--- web/src/user_group_components.ts
+++ web/src/user_group_components.ts
@@ -1,7 +1,10 @@
 import $ from "jquery";
 
 import {$t_html} from "./i18n";
+import * as people from "./people";
+import type {User} from "./people";
 import type {UserGroup} from "./user_groups";
+import * as user_sort from "./user_sort";
 
 export let active_group_id: number | undefined;
 
@@ -59,4 +62,63 @@ export function update_footer_buttons(container_name: string): void {
         $("#groups_overlay #user_group_go_to_members").show();
         $("#groups_overlay #user_group_go_to_configure_settings").hide();
     }
+}
+
+export function sort_group_member_email(a: User | UserGroup, b: User | UserGroup): number {
+    if ("user_id" in a && "user_id" in b) {
+        return user_sort.sort_email(a, b);
+    }
+
+    if ("user_id" in a) {
+        return -1;
+    }
+
+    if ("user_id" in b) {
+        return 1;
+    }
+
+    return user_sort.compare_a_b(a.name.toLowerCase(), b.name.toLowerCase());
+}
+
+export function sort_group_member_name(a: User | UserGroup, b: User | UserGroup): number {
+    let a_name;
+    if ("user_id" in a) {
+        a_name = a.full_name;
+    } else {
+        a_name = a.name;
+    }
+
+    let b_name;
+    if ("user_id" in b) {
+        b_name = b.full_name;
+    } else {
+        b_name = b.name;
+    }
+
+    return user_sort.compare_a_b(a_name.toLowerCase(), b_name.toLowerCase());
+}
+
+export function build_group_member_matcher(query: string): (member: User | UserGroup) => boolean {
+    query = query.trim();
+
+    const termlets = query.toLowerCase().split(/\s+/);
+    const termlet_matchers = termlets.map((termlet) => people.build_termlet_matcher(termlet));
+
+    return function (member: User | UserGroup): boolean {
+        if ("user_id" in member) {
+            const email = member.email.toLowerCase();
+
+            if (email.startsWith(query)) {
+                return true;
+            }
+
+            return termlet_matchers.every((matcher) => matcher(member));
+        }
+
+        const group_name = member.name;
+        if (group_name.startsWith(query)) {
+            return true;
+        }
+        return false;
+    };
 }
