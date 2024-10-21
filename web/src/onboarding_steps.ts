69f4ef75c8314fa753d45903ece131dc008414ce
Prakhar Pratyush <prakhar@zulip.com>
2024-10-21 16:30:06 +0530

    onboarding_steps: Add 'onboarding_video' for new users.
    This commit adds a one-time modal to display onboarding video
    to new users.
    
    Includes an `ONBOARDING_VIDEO_URL` server-setting to specify
    the video's URL. When set to None, the modal is not displayed.
    
    Fixes #29304.

--- web/src/onboarding_steps.ts
+++ web/src/onboarding_steps.ts
@@ -1,7 +1,12 @@
+import $ from "jquery";
 import type {z} from "zod";
+
+import render_onboarding_video_modal from "../templates/onboarding_video_modal.hbs";
 
 import * as blueslip from "./blueslip.ts";
 import * as channel from "./channel.ts";
+import * as dialog_widget from "./dialog_widget.ts";
+import {$t_html} from "./i18n.ts";
 import * as people from "./people.ts";
 import type {NarrowTerm, StateData, onboarding_step_schema} from "./state_data.ts";
 
@@ -57,10 +62,37 @@ function narrow_to_dm_with_welcome_bot_new_user(
     }
 }
 
+function show_onboarding_video(): void {
+    if (ONE_TIME_NOTICES_TO_DISPLAY.has("onboarding_video")) {
+        const html_body = render_onboarding_video_modal({
+            video_src: "../../static/videos/onboarding-video.mp4",
+        });
+        dialog_widget.launch({
+            html_heading: $t_html({defaultMessage: "Welcome to Zulip!"}),
+            html_body,
+            on_click() {
+                post_onboarding_step_as_read("onboarding_video");
+            },
+            html_submit_button: $t_html({defaultMessage: "Skip video — I'm familiar with Zulip"}),
+            html_exit_button: $t_html({defaultMessage: "Watch later"}),
+            close_on_submit: true,
+            id: "onboarding-video-modal",
+            close_on_overlay_click: false,
+            post_render() {
+                $("#onboarding-video-wrapper video").on("play", () => {
+                    // Remove the custom play button overlaying the video.
+                    $("#onboarding-video-wrapper").addClass("hide-play-button");
+                });
+            },
+        });
+    }
+}
+
 export function initialize(
     params: StateData["onboarding_steps"],
     show_message_view: (raw_terms: NarrowTerm[], opts: {trigger: string}) => void,
 ): void {
     update_onboarding_steps_to_display(params.onboarding_steps);
     narrow_to_dm_with_welcome_bot_new_user(params.onboarding_steps, show_message_view);
+    show_onboarding_video();
 }
