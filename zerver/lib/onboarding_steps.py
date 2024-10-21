7504b34a0069dfc5fc014e4eb44fbc8e85f2138b
Prakhar Pratyush <prakhar@zulip.com>
2024-10-21 16:30:06 +0530

    onboarding_steps: Add 'onboarding_video' for new users.
    This commit adds a one-time modal to display onboarding video
    to new users.
    
    Fixes #29304.

--- zerver/lib/onboarding_steps.py
+++ zerver/lib/onboarding_steps.py
@@ -55,6 +55,9 @@ ONE_TIME_NOTICES: list[OneTimeNotice] = [
     OneTimeNotice(
         name="intro_resolve_topic",
     ),
+    OneTimeNotice(
+        name="onboarding_video",
+    ),
 ]
 
 ONE_TIME_ACTIONS = [OneTimeAction(name="narrow_to_dm_with_welcome_bot_new_user")]
