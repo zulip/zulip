36fa504699438e4bd34bddf7c29d828abc1a5702
Kislay Udbhav Verma <kislayuv27@gmail.com>
2024-10-25 00:24:05 +0530

    copy_and_paste: Pretty format /near links on paste.
    This extends the work done in #29302 where we pretty-formatted
    stream-topic urls.
    
    Now we suffix the stream-topic syntax text with `@💬` to indicate that
    it links to a message in that topic.
    
    Fixes #31920

--- web/tests/copy_and_paste.test.js
+++ web/tests/copy_and_paste.test.js
@@ -33,17 +33,20 @@ run_test("try_stream_topic_syntax_text", () => {
         ],
 
         ["http://different.origin.com/#narrow/channel/4-Rome/topic/old.20FAILED.20EXPORT"],
-
+        [
+            "http://zulip.zulipdev.com/#narrow/channel/4-Rome/topic/old.20FAILED.20EXPORT/near/100",
+            "[#Rome>old FAILED EXPORT@💬](#narrow/channel/4-Rome/topic/old.20FAILED.20EXPORT/near/100)",
+        ],
         // malformed urls
         ["http://zulip.zulipdev.com/narrow/channel/4-Rome/topic/old.20FAILED.20EXPORT"],
         ["http://zulip.zulipdev.com/#not_narrow/channel/4-Rome/topic/old.20FAILED.20EXPORT"],
         ["http://zulip.zulipdev.com/#narrow/not_stream/4-Rome/topic/old.20FAILED.20EXPORT"],
         ["http://zulip.zulipdev.com/#narrow/channel/4-Rome/not_topic/old.20FAILED.20EXPORT"],
-        ["http://zulip.zulipdev.com/#narrow/channel/4-Rome/topic/old.20FAILED.20EXPORT/near/100"],
         ["http://zulip.zulipdev.com/#narrow/channel/4-Rome/", "#**Rome**"],
         ["http://zulip.zulipdev.com/#narrow/channel/4-Rome/topic"],
         ["http://zulip.zulipdev.com/#narrow/topic/cheese"],
         ["http://zulip.zulipdev.com/#narrow/topic/pizza/stream/Rome"],
+        ["http://zulip.zulipdev.com/#narrow/channel/4-Rome/topic/old.20FAILED.20EXPORT/near/"],
 
         // When a url containing characters which are known to produce broken
         // #**stream>topic** urls is pasted, a normal markdown link syntax is produced.
