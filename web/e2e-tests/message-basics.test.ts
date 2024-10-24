d8f293fc3c5458b90459145e91317aa134a181ba
evykassirer <evy@zulip.com>
2024-10-24 12:13:44 -0700

    buddy_list: Don't go to DM narrow on avatar click.
    This commit also moves styles from user-presence-link
    onto selectable_sidebar_block, since the link is no
    longer wrapping the whole block contents.
    
    The `data-name` was also moved to the top level `<li>`
    because it's used in pupeteer tests.

--- web/e2e-tests/message-basics.test.ts
+++ web/e2e-tests/message-basics.test.ts
@@ -404,21 +404,21 @@ async function test_stream_search_filters_stream_list(page: Page): Promise<void>
 async function test_users_search(page: Page): Promise<void> {
     console.log("Search users using right sidebar");
     async function assert_in_list(page: Page, name: string): Promise<void> {
-        await page.waitForSelector(`#buddy-list-other-users li [data-name="${CSS.escape(name)}"]`, {
+        await page.waitForSelector(`#buddy-list-other-users li[data-name="${CSS.escape(name)}"]`, {
             visible: true,
         });
     }
 
     async function assert_selected(page: Page, name: string): Promise<void> {
         await page.waitForSelector(
-            `#buddy-list-other-users li.highlighted_user [data-name="${CSS.escape(name)}"]`,
+            `#buddy-list-other-users li.highlighted_user[data-name="${CSS.escape(name)}"]`,
             {visible: true},
         );
     }
 
     async function assert_not_selected(page: Page, name: string): Promise<void> {
         await page.waitForSelector(
-            `#buddy-list-other-users li.highlighted_user [data-name="${CSS.escape(name)}"]`,
+            `#buddy-list-other-users li.highlighted_user[data-name="${CSS.escape(name)}"]`,
             {hidden: true},
         );
     }
@@ -452,7 +452,7 @@ async function test_users_search(page: Page): Promise<void> {
     await arrow(page, "Down");
 
     // Now Iago must be highlighted
-    await page.waitForSelector('#buddy-list-other-users li.highlighted_user [data-name="Iago"]', {
+    await page.waitForSelector('#buddy-list-other-users li.highlighted_user[data-name="Iago"]', {
         visible: true,
     });
     await assert_not_selected(page, "King Hamlet");
