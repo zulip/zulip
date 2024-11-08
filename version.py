c960149778fd2b77ce49d255605cecbdb3ca3c1e
Anders Kaseorg <anders@zulip.com>
2024-11-08 14:23:37 -0800

    svgicons2svgfont: Patch to fix Node 22 punycode DeprecationWarning.
        (node:13734) [DEP0040] DeprecationWarning: The `punycode` module is deprecated. Please use a userland alternative instead.
        (Use `node --trace-deprecation ...` to show where the warning was created)
    
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -49,4 +49,4 @@ API_FEATURE_LEVEL = 319  # Last bumped for message-link class
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (299, 0)  # bumped 2024-11-08 to remove babel-plugin-rewire-ts
+PROVISION_VERSION = (299, 1)  # bumped 2024-11-08 to patch svgicons2svgfont
