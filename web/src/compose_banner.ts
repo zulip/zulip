2b315147bb9f2c205defec9ec2c74926fb09d9fa
Maneesh Shukla <shuklamaneesh24@gmail.com>
2024-11-11 23:36:47 +0530

    compose_banner: Return boolean value from append function.
    To prepare for changes that introduce the possibility of
    an append failing.
    
    Fixes: #25575.
    
    Co-authored-by: joshyhz <joshyap.dev@gmail.com>

--- web/src/compose_banner.ts
+++ web/src/compose_banner.ts
@@ -71,11 +71,13 @@ export function get_compose_banner_container($textarea: JQuery): JQuery {
 // This function provides a convenient way to add new elements
 // to a banner container. The function accepts a container element
 // as a parameter, to which a banner should be appended.
+// Returns a boolean value indicating whether the append had succeeded.
 export function append_compose_banner_to_banner_list(
     $banner: JQuery,
     $list_container: JQuery,
-): void {
+): boolean {
     scroll_util.get_content_element($list_container).append($banner);
+    return true;
 }
 
 export function update_or_append_banner(
