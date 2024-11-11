3cf7af7db9c2aaa0fbdcef35fa72de71be6a6cf5
Shubham Padia <shubham@zulip.com>
2024-11-11 09:44:13 +0000

    settings: Realms with limited plans cannot change can_create_groups.
    We do not allow realms on a limited plan to create groups. This commit
    adds a banner to upgrade to the can_create_groups org setting and
    disables editing it on the backend.
    This commit also adds a new method called
    `disable_group_permission_setting` to easily disable similar settings.

--- zerver/views/realm.py
+++ zerver/views/realm.py
@@ -218,6 +218,9 @@ def update_realm(
             message_retention_days_raw, Realm.MESSAGE_RETENTION_SPECIAL_VALUES_MAP
         )
 
+    if can_create_groups is not None:
+        realm.ensure_not_on_limited_plan()
+
     if (
         invite_to_realm_policy is not None
         or invite_required is not None
