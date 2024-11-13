06a9600aa7ef8f3ee3678b042c783479e2672692
Anders Kaseorg <anders@zulip.com>
2024-11-13 14:43:10 -0800

    bundles: Remove unnecessary bootstrap_typeahead import.
    Commit db24df488c0ed81c38ee1d914ae12ac223857c78 (#29286) made this a
    normal function rather than a jQuery plugin, so it doesn’t need to be
    imported for side effects.
    
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- web/src/bundles/app.ts
+++ web/src/bundles/app.ts
@@ -1,7 +1,6 @@
 import "./common.ts";
 
 // Import third party jQuery plugins
-import "../bootstrap_typeahead.ts";
 import "jquery-caret-plugin/dist/jquery.caret";
 import "../../third/jquery-idle/jquery.idle.js";
 import "spectrum-colorpicker";
