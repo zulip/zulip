ed72d1a080f54f3225fe487245e546bdbb8b4dcc
Tim Abbott <tabbott@zulip.com>
2024-11-22 17:16:35 -0800

    version: Update version after 9.3 release.

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 import os
 
-ZULIP_VERSION = "9.3"
+ZULIP_VERSION = "9.3+git"
 
 # Add information on number of commits and commit hash to version, if available
 zulip_git_version_file = os.path.join(
