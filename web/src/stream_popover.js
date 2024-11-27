6083cf69c8ea5c1d9e416c21d00701ef9de34e88
Anders Kaseorg <anders@zulip.com>
2024-11-27 14:04:11 -0800

    stream_popover: Fix get_options_for_dropdown_widget filtering.
    get_options_for_dropdown_widget returns an object with a stream:
    StreamSubscription key, not the stream itself.
    
    Introduced by commit fcac41313870e494dcd2a59df9242698855581ca
    (#26067).
    
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- web/src/stream_popover.js
+++ web/src/stream_popover.js
@@ -594,7 +594,7 @@ export async function build_move_topic_to_stream_popover(
 
         stream_widget_value = current_stream_id;
         const streams_list_options = () =>
-            stream_data.get_options_for_dropdown_widget().filter((stream) => {
+            stream_data.get_options_for_dropdown_widget().filter(({stream}) => {
                 if (stream.stream_id === current_stream_id) {
                     return true;
                 }
