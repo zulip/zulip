28286fa77d25d4befdc8b09da1090bc8a32a0cdf
opmkumar <sachinaniket2004@gmail.com>
2024-12-04 23:37:18 +0530

    realm: Add option to schedule data deletion while deactivating.
    Introduce a feature to schedule realm data deletion time during realm
    deactivation. This includes a server-level setting to configure the
    minimum and maximum allowed deletion days.
    
    Co-authored-by: Ujjawal Modi <umodi2003@gmail.com>
    
    Fixes #24677.

--- zerver/models/realms.py
+++ zerver/models/realms.py
@@ -184,6 +184,7 @@ class Realm(models.Model):  # type: ignore[django-manager-missing] # django-stub
     push_notifications_enabled_end_timestamp = models.DateTimeField(default=None, null=True)
 
     date_created = models.DateTimeField(default=timezone_now)
+    scheduled_deletion_date = models.DateTimeField(default=None, db_index=True, null=True)
     demo_organization_scheduled_deletion_date = models.DateTimeField(default=None, null=True)
     deactivated = models.BooleanField(default=False)
 
