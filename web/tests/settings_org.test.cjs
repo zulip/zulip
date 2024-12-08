c837d6535b58fb068b8b29757a34646ac3a3a212
Saubhagya Patel <patelsaubhagya0144@gmail.com>
2024-12-08 16:22:14 +0530

    settings: Use text cursor for "Name" when name changes are disabled.
    This commit includes the following changes:
     - Use a regular text cursor in place of a pointer cursor for the
       "Name" field label when the name changes are disabled.
     - Introduce a class named `cursor-text` that sets the property
       `cursor: text;`.
     - Implement tests to verify the above changes made.

--- web/tests/settings_org.test.cjs
+++ web/tests/settings_org.test.cjs
@@ -784,24 +784,28 @@ test("misc", ({override}) => {
     settings_account.update_name_change_display();
     assert.ok(!$("#full_name").prop("disabled"));
     assert.ok(!$("#full_name_input_container").hasClass("disabled_setting_tooltip"));
+    assert.ok(!$("label[for='full_name']").hasClass("cursor-text"));
 
     override(realm, "realm_name_changes_disabled", true);
     override(realm, "server_name_changes_disabled", false);
     settings_account.update_name_change_display();
     assert.ok($("#full_name").prop("disabled"));
     assert.ok($("#full_name_input_container").hasClass("disabled_setting_tooltip"));
+    assert.ok($("label[for='full_name']").hasClass("cursor-text"));
 
     override(realm, "realm_name_changes_disabled", true);
     override(realm, "server_name_changes_disabled", true);
     settings_account.update_name_change_display();
     assert.ok($("#full_name").prop("disabled"));
     assert.ok($("#full_name_input_container").hasClass("disabled_setting_tooltip"));
+    assert.ok($("label[for='full_name']").hasClass("cursor-text"));
 
     override(realm, "realm_name_changes_disabled", false);
     override(realm, "server_name_changes_disabled", true);
     settings_account.update_name_change_display();
     assert.ok($("#full_name").prop("disabled"));
     assert.ok($("#full_name_input_container").hasClass("disabled_setting_tooltip"));
+    assert.ok($("label[for='full_name']").hasClass("cursor-text"));
 
     override(realm, "realm_email_changes_disabled", false);
     settings_account.update_email_change_display();
@@ -833,6 +837,7 @@ test("misc", ({override}) => {
     settings_account.update_name_change_display();
     assert.ok(!$("#full_name").prop("disabled"));
     assert.ok(!$("#full_name_input_container").hasClass("disabled_setting_tooltip"));
+    assert.ok(!$("label[for='full_name']").hasClass("cursor-text"));
 
     settings_account.update_email_change_display();
     assert.ok(!$("#change_email_button").prop("disabled"));
