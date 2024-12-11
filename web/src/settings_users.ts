22f2e43d248aa09c6c870b7d9859b3b73fe77820
opmkumar <sachinaniket2004@gmail.com>
2024-12-12 01:38:43 +0530

    search: Add "x" button to the filter of Bots table.

--- web/src/settings_users.ts
+++ web/src/settings_users.ts
@@ -492,6 +492,17 @@ function active_create_table(active_users: number[]): void {
     $("#admin_users_table").show();
 }
 
+export function handle_clear_button_for_bots(): void {
+    $("#admin-bot-list").on("click", ".clear_filter", (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+
+        const $filter = $("#admin-bot-list").find(".search_container .search");
+        $filter.val("");
+        bot_list_widget.clear_text_filter();
+    });
+}
+
 function deactivated_create_table(deactivated_users: number[]): void {
     const $deactivated_users_table = $("#admin_deactivated_users_table");
     deactivated_section.list_widget = ListWidget.create(
@@ -741,6 +752,7 @@ function bots_handle_events(): void {
     handle_bot_deactivation($tbody);
     handle_reactivation($tbody);
     handle_edit_form($tbody);
+    handle_clear_button_for_bots();
 }
 
 export function set_up_humans(): void {
