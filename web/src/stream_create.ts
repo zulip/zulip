9d5a79be0c924983df1e97809b99ed7448ed2116
Maneesh Shukla <shuklamaneesh24@gmail.com>
2024-12-15 22:48:03 +0530

    channel_settings: Add error handling for stream creation.
    Fixes: #32138.

--- web/src/stream_create.ts
+++ web/src/stream_create.ts
@@ -423,21 +423,22 @@ function create_stream(): void {
             // The rest of the work is done via the subscribe event we will get
         },
         error(xhr): void {
-            const error_message = z.object({msg: z.string().optional()}).parse(xhr.responseJSON);
-            if (error_message?.msg?.includes("access")) {
-                // If we can't access the stream, we can safely
-                // assume it's a duplicate stream that we are not invited to.
-                //
-                // BUG: This check should be using error codes, not
-                // parsing the error string, so it works correctly
-                // with i18n.  And likely we should be reporting the
-                // error text directly rather than turning it into
-                // "Error creating channel"?
+            const error_code = z.object({code: z.string().optional()}).parse(xhr.responseJSON);
+            if (error_code?.code?.match("CANNOT_ACCESS_ARCHIVED_STREAM")) {
                 stream_name_error.report_already_exists();
                 stream_name_error.select();
                 const message = $t_html({
                     defaultMessage:
-                        "Error creating channel: A channel with this name already exists.",
+                        "Error creating channel: An archived channel with this name already exists.",
+                });
+
+                ui_report.error(message, undefined, $(".stream_create_info"));
+            } else if (error_code?.code?.match("CANNOT_ACCESS_PRIVATE_STREAM")) {
+                stream_name_error.report_already_exists();
+                stream_name_error.select();
+                const message = $t_html({
+                    defaultMessage:
+                        "Error creating channel: A private channel with this name already exists.",
                 });
 
                 ui_report.error(message, undefined, $(".stream_create_info"));
