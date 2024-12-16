89a4663c0c04050ca79985b5405c031f43b1f8f7
Maneesh Shukla <shuklamaneesh24@gmail.com>
2024-12-17 00:34:57 +0530

    deactivated_user: Show deactivated status for deactivated users.
    Fixes: #26833.

--- web/src/pm_list_data.ts
+++ web/src/pm_list_data.ts
@@ -47,6 +47,7 @@ type DisplayObject = {
     is_group: boolean;
     is_bot: boolean;
     has_unread_mention: boolean;
+    is_deactivated: boolean;
 };
 
 export function get_conversations(search_string = ""): DisplayObject[] {
@@ -86,6 +87,9 @@ export function get_conversations(search_string = ""): DisplayObject[] {
             unread.num_unread_mentions_for_user_ids_strings(user_ids_string) > 0;
         const is_group = user_ids_string.includes(",");
         const is_active = user_ids_string === active_user_ids_string;
+        const is_deactivated = !people.is_active_user_for_popover(
+            Number.parseInt(user_ids_string, 10) || 0,
+        );
 
         let user_circle_class;
         let status_emoji_info;
@@ -116,6 +120,7 @@ export function get_conversations(search_string = ""): DisplayObject[] {
             is_group,
             is_bot,
             has_unread_mention,
+            is_deactivated,
         };
         display_objects.push(display_object);
     }
