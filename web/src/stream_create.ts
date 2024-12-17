95f564ca0bfad407ef945234ca4bf02ba18de45e
Shubham Padia <shubham@zulip.com>
2024-12-17 05:41:21 +0000

    settings: Rephrase clicking enter to pressing enter.

--- web/src/stream_create.ts
+++ web/src/stream_create.ts
@@ -215,7 +215,7 @@ $("body").on("click", ".settings-sticky-footer #stream_creation_go_to_subscriber
             is_any_stream_group_widget_pending = true;
             // We are not appending any value here, but instead this is
             // a proxy to invoke the error state for a group widget
-            // that would usually get triggered on clicking enter.
+            // that would usually get triggered on pressing enter.
             widget.appendValue(widget.getCurrentText()!);
             break;
         }
