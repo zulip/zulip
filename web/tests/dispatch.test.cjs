4c48f6616fe81591e70974bbcd9391a6ac354e5c
whilstsomebody <vishwakarmarambhawan572@gmail.com>
2024-12-22 18:36:10 +0530

    compose_reply: Toggle compose reply button in interleaved views.
    This commit implements functionality to disable the compose reply
    button for DMs when the user is not permitted to send a DM.
    Previously, this behavior was limited to the DM view only. With
    this commit, it is extended to all views, including the DM feed
    view, combined feed view, mentions view, reactions view, starred
    messages view, and search view.
    
    Fixes: #32597.

--- web/tests/dispatch.test.cjs
+++ web/tests/dispatch.test.cjs
@@ -122,6 +122,7 @@ message_lists.current = {
         get_messages_sent_by_user: () => [],
         filter: new Filter([]),
     },
+    selected_message() {},
 };
 const cached_message_list = {
     get_row: noop,
