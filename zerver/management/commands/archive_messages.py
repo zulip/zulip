3b3ab05f670e0f2f45237b6cb5b6a587fa5e1295
opmkumar <sachinaniket2004@gmail.com>
2025-01-01 15:04:48 +0530

    cron: Add cron job logic to delete fully deactivated realms.
    Fixes #32763.

--- zerver/management/commands/archive_messages.py
+++ zerver/management/commands/archive_messages.py
@@ -2,6 +2,7 @@ from typing import Any
 
 from typing_extensions import override
 
+from zerver.actions.realm_settings import clean_deactivated_realm_data
 from zerver.lib.management import ZulipBaseCommand, abort_unless_locked
 from zerver.lib.retention import archive_messages, clean_archived_data
 
@@ -12,3 +13,4 @@ class Command(ZulipBaseCommand):
     def handle(self, *args: Any, **options: str) -> None:
         clean_archived_data()
         archive_messages()
+        clean_deactivated_realm_data()
