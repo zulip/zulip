d3b5aa1436fc2bbee53ce6f38232f8ff53522b91
Steve Howell <showell30@yahoo.com>
2025-01-01 17:08:17 +0000

    event checkers: Replace data_types.py with pydantic.
    This commit completely eliminates the home-grown
    data_types.py system for checking types.
    
    Now, in our unit tests and schema checkers, we use
    pydantic types to check the shape of the event
    objects that are either in our JSON fixtures or
    that get captured by test_events.py.
    
    The code in event_types.py was completely
    code-generated from the legacy version of
    event_schema.py, so there shouldn't be any
    manual errors in how I ported the types over.
    (The code generation was non-trivial, though,
    as I had to account for changes from
    snake_case to CamelCase and deal with some
    other anomalies.)

--- tools/linter_lib/custom_check.py
+++ tools/linter_lib/custom_check.py
@@ -12,6 +12,8 @@ FILES_WITH_LEGACY_SUBJECT = {
     # This basically requires a big DB migration:
     "zerver/lib/topic.py",
     "zerver/lib/topic_sqlalchemy.py",
+    # This is tied to legacy events.
+    "zerver/lib/event_types.py",
     # This is for backward compatibility.
     "zerver/tests/test_legacy_subject.py",
     # Other migration-related changes require extreme care.
