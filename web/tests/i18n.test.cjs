ebe1f69c8078fff56cdc1ee6a81600dfa008ec0a
Steve Howell <showell30@yahoo.com>
2025-01-07 01:43:51 +0000

    node test runner: Prevent Handlebars quirks.
    We re-require templates.ts before testing each
    new test module during the node tests.
    
    This makes it so that if we make mocks in two
    different tests, where such mocks influence
    how the Handlebars helpers work, then both
    tests will have registered their own copies
    of the Handlebars helpers.

--- web/tests/i18n.test.cjs
+++ web/tests/i18n.test.cjs
@@ -20,10 +20,6 @@ page_params.translation_data = {
         "<p>Le canal <b>{name}</b> n'existe pas.</p><p>Gérez vos abonnements <z-link>sur votre page canaux</z-link>.</p>",
 };
 
-// Re-register Zulip extensions so extensions registered previously with
-// mocked i18n.ts do not interfere with following tests.
-require("../src/templates.ts");
-
 // All of our other tests stub out i18n activity;
 // here we do a quick sanity check on the engine itself.
 // `i18n.ts` initializes FormatJS and is imported by
