zulip/zulip

commit efb2a5a38ddd4808571173ec783af94a981348d5

util: Defining common formatter function

web/src/util.ts
@@ -4,6 +4,7 @@ import * as blueslip from "./blueslip";
 import {$t} from "./i18n";
 import type {MatchedMessage, Message, RawMessage} from "./message_store";
 import type {UpdateMessageEvent} from "./types";
+import {user_settings} from "./user_settings";
 
 // From MDN: https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Math/random
 export function random_int(min: number, max: number): number {
@@ -488,4 +489,22 @@ export function is_valid_url(url: string, require_absolute = false): boolean {
         return false;
     }
     return true;
+}
+
+// Formats an array of strings as a Internationalized list using the specified language.
+export function format_array_as_list(
+    array: string[],
+    style: Intl.ListFormatStyle,
+    type: Intl.ListFormatType,
+): string {
+    // If Intl.ListFormat is not supported
+    if (Intl.ListFormat === undefined) {
+        return array.join(", ");
+    }
+
+    // Use Intl.ListFormat to format the array as a Internationalized list.
+    const list_formatter = new Intl.ListFormat(user_settings.default_language, {style, type});
+
+    // Return the formatted string.
+    return list_formatter.format(array);
 }

web/tests/util.test.js
@@ -338,3 +338,16 @@ run_test("is_valid_url", () => {
     assert.equal(util.is_valid_url("http://google.com/something?q=query#hash", true), true);
     assert.equal(util.is_valid_url("/abc/", true), false);
 });
+
+run_test("format_array_as_list", () => {
+    const array = ["apple", "banana", "orange"];
+    // when Intl exist
+    assert.equal(
+        util.format_array_as_list(array, "long", "conjunction"),
+        "apple, banana, and orange",
+    );
+
+    // when Intl.ListFormat does not exist
+    global.Intl.ListFormat = undefined;
+    assert.equal(util.format_array_as_list(array, "long", "conjunction"), "apple, banana, orange");
+});

commit f2c516664e069bb9b314da8f8dbd16ba535cdb3a

util node tests: Prevent Intl.ListFormat leaks.
These never happen in practice since util is near the
end of the alphabet, but if you had run the tests in
reverse order, you would have seen this leak.

This leak was introduced in the efb2a5a38dd commit.

web/tests/util.test.cjs
@@ -5,7 +5,7 @@ const assert = require("node:assert/strict");
 const _ = require("lodash");
 const MockDate = require("mockdate");
 
-const {set_global, zrequire} = require("./lib/namespace.cjs");
+const {set_global, with_overrides, zrequire} = require("./lib/namespace.cjs");
 const {run_test} = require("./lib/test.cjs");
 
 const blueslip = zrequire("blueslip");
@@ -369,12 +369,17 @@ run_test("format_array_as_list", () => {
     );
 
     // when Intl.ListFormat does not exist
-    global.Intl.ListFormat = undefined;
-    assert.equal(util.format_array_as_list(array, "long", "conjunction"), "apple, banana, orange");
-    assert.equal(
-        util.format_array_as_list_with_highlighted_elements(array, "long", "conjunction"),
-        "<b>apple</b>, <b>banana</b>, <b>orange</b>",
-    );
+    with_overrides(({override}) => {
+        override(global.Intl, "ListFormat", undefined);
+        assert.equal(
+            util.format_array_as_list(array, "long", "conjunction"),
+            "apple, banana, orange",
+        );
+        assert.equal(
+            util.format_array_as_list_with_highlighted_elements(array, "long", "conjunction"),
+            "<b>apple</b>, <b>banana</b>, <b>orange</b>",
+        );
+    });
 });
 
 run_test("get_remaining_time", () => {