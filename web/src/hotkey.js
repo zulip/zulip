965849f19d2d99f035c7b21179febeeadd3a6fe1
Harsh Bansal <harshbansal8705@gmail.com>
2025-01-10 17:05:57 +0530

    compose (misc): Fix send message on enter in topic input.
    This commit prevents sending the message when pressing
    enter while on focus at topic input box.
    
    The variable target_sel has a value of '#stream_messagee_recipient_topic'
    instead of 'input#stream_message_reciepient_topic', which is one of the
    reason of this bug.
    
    Fixes: #32596
    Co-Authored-By: Aman Agrawal <amanagr@zulip.com>

--- web/src/hotkey.js
+++ web/src/hotkey.js
@@ -499,6 +499,11 @@ export function process_enter_key(e) {
             return true;
         }
 
+        // Don't send the message if topic box is focused.
+        if (compose.is_topic_input_focused()) {
+            return true;
+        }
+
         return false;
     }
 
