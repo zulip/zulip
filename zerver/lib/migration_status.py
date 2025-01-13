6329cb1a39a142e00314c92d13918d178b9b16bd
PieterCK <pieterceka123@gmail.com>
2025-01-13 16:28:15 +0700

    migration_status: Move `get_migration_status` to a new file.
    This moves `get_migration_status` to its own file in
    zerver/lib/migration_status.py. This is a prep work to refactor the
    check migration function of import/export later on.
    
    Some of the imports are moved into `get_migration_status` because we're
    planning to share this file with `check-database-compatibility` which is
    also called when one does `production-upgrade`, so we'd want to avoid
    doing file-wide import on certain types of modules because it will fail
    under that scenario.
    
    In `test_fixtures.py`, `get_migration_status` is imported within
    `Database.what_to_do_with_migrations` so that it is called after
    `cov.start()` in `test-backend`. This is to avoid wierd interaction with
    coverage, see more details in #33063.
    
    Fixes #33063.

---
+++ zerver/lib/migration_status.py
@@ -0,0 +1,41 @@
+import os
+import re
+from importlib import import_module
+from io import StringIO
+from typing import Any
+
+
+def get_migration_status(**options: Any) -> str:
+    from django.apps import apps
+    from django.core.management import call_command
+    from django.db import DEFAULT_DB_ALIAS, connections
+    from django.utils.module_loading import module_has_submodule
+
+    verbosity = options.get("verbosity", 1)
+
+    for app_config in apps.get_app_configs():
+        if module_has_submodule(app_config.module, "management"):
+            import_module(".management", app_config.name)
+
+    app_label = options["app_label"] if options.get("app_label") else None
+    db = options.get("database", DEFAULT_DB_ALIAS)
+    out = StringIO()
+    command_args = ["--list"]
+    if app_label:
+        command_args.append(app_label)
+
+    call_command(
+        "showmigrations",
+        *command_args,
+        database=db,
+        no_color=options.get("no_color", False),
+        settings=options.get("settings", os.environ["DJANGO_SETTINGS_MODULE"]),
+        stdout=out,
+        skip_checks=options.get("skip_checks", True),
+        traceback=options.get("traceback", True),
+        verbosity=verbosity,
+    )
+    connections.close_all()
+    out.seek(0)
+    output = out.read()
+    return re.sub(r"\x1b\[(1|0)m", "", output)
