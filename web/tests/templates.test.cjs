bcd8dfe49fb3474503a6d47cd14586c8a73b688e
Harsh <116981900+reharsh@users.noreply.github.com>
2025-01-14 05:33:15 +0530

    tooltips: Replace URL title tags with Tippy tooltips.
    This commit:
    - Replaces title tags with Tippy tooltips for named URLs with no delays,
    allowing users to see the actual URL immediately, ensuring protection against
    fraudulent URLs.
    - Show Tippy tooltips in canonical form for plain URLs, internal Zulip links like uploads, said links in quotes with LONG HOVER DELAY.
    - for internal known links (channels / topics / messages), we don't show tooltips.
    
    fixes: #32973

--- web/tests/templates.test.cjs
+++ web/tests/templates.test.cjs
@@ -34,7 +34,7 @@ run_test("or", () => {
 run_test("rendered_markdown", () => {
     const html = require("./templates/rendered_markdown.hbs")();
     const expected_html =
-        '<a href="http://example.com" target="_blank" rel="noopener noreferrer" title="http://example.com/">good</a>\n';
+        '<a href="http://example.com" target="_blank" rel="noopener noreferrer" data-message-link-type="external_inline_url">good</a>\n';
     assert.equal(html, expected_html);
 });
 
