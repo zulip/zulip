14b7bde9545a9497038a785248075e16804ede90
Sahil Batra <sahil@zulip.com>
2025-01-15 16:39:34 +0530

    settings: Extract type for all realm level group settings.
    Zod schema and type for realm level group settings is moved
    to group_permission_settings.ts as we would define types for
    stream and group settings there and the change would also help
    in avoiding import cycles in further commits.

--- web/src/group_permission_settings.ts
+++ web/src/group_permission_settings.ts
@@ -33,3 +33,23 @@ export function get_group_permission_settings(): GroupSettingName[] {
         .array(group_setting_name_schema)
         .parse(Object.keys(realm.server_supported_permission_settings.group));
 }
+
+export const realm_group_setting_name_schema = z.enum([
+    "can_access_all_users_group",
+    "can_add_custom_emoji_group",
+    "can_add_subscribers_group",
+    "can_create_groups",
+    "can_create_public_channel_group",
+    "can_create_private_channel_group",
+    "can_create_web_public_channel_group",
+    "can_delete_any_message_group",
+    "can_delete_own_message_group",
+    "can_invite_users_group",
+    "can_manage_all_groups",
+    "can_move_messages_between_channels_group",
+    "can_move_messages_between_topics_group",
+    "create_multiuse_invite_group",
+    "direct_message_initiator_group",
+    "direct_message_permission_group",
+]);
+export type RealmGroupSettingName = z.infer<typeof realm_group_setting_name_schema>;
