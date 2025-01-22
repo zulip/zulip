3e97396dde76711ede740647aaafe032d5fa21dc
Prakhar Pratyush <prakhar@zulip.com>
2025-01-22 16:51:17 +0530

    get_stream_topics: Add support for empty topic name.
    This commit is a part of the work to support empty
    string as a topic name.
    
    Previously, empty string was not a valid topic name.
    
    Adds `allow_empty_topic_name` boolean parameter to
    `GET /users/me/{stream_id}/topics` endpoint to decide
    whether the topic names in the fetched `topics` array
    can be empty strings.
    
    If False, the topic names in the fetched response will
    have the value of `realm_empty_topic_display_name` field
    in `POST /register` response replacing "".
    
    Fixes part of #23291.

--- web/src/stream_topic_history_util.ts
+++ web/src/stream_topic_history_util.ts
@@ -27,7 +27,7 @@ export function get_server_history(stream_id: number, on_success: () => void): v
 
     void channel.get({
         url,
-        data: {},
+        data: {allow_empty_topic_name: true},
         success(raw_data) {
             const data = stream_topic_history_response_schema.parse(raw_data);
             const server_history = data.topics;
