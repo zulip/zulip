e0e0bfc7bc9d2b799e593fb6dfcf3d54b0eb3721
Lauryn Menard <lauryn@zulip.com>
2025-01-29 21:00:36 +0100

    util: Extract helper function for backoff of retries on error response.
    Extracts message_fetch and get_events algorithms for calculating the
    exponential backoff into a shared helper function.
    
    These match the algorithm that were designed for the Python API, except
    that we use a ratio of 2 rather than sqrt(2) in the message_fetch code
    path.

--- web/src/util.ts
+++ web/src/util.ts
@@ -1,5 +1,6 @@
 import Handlebars from "handlebars/runtime.js";
 import _ from "lodash";
+import {z} from "zod";
 
 import * as blueslip from "./blueslip.ts";
 import {$t} from "./i18n.ts";
@@ -544,4 +545,40 @@ export function get_final_topic_display_name(topic_name: string): string {
         return realm.realm_empty_topic_display_name;
     }
     return topic_name;
+}
+
+export function get_retry_backoff_seconds(
+    xhr: JQuery.jqXHR<unknown>,
+    attempts: number,
+    tighter_backoff = false,
+): number {
+    // We need to respect the server's rate-limiting headers, but beyond
+    // that, we also want to avoid contributing to a thundering herd if
+    // the server is giving us 500/502 responses.
+    //
+    // We do the maximum of the retry-after header and an exponential
+    // backoff.
+    let backoff_scale: number;
+    if (tighter_backoff) {
+        // Starts at 1-2s and ends at 16-32s after enough failures.
+        backoff_scale = Math.min(2 ** attempts, 32);
+    } else {
+        // Starts at 1-2s and ends at 45-90s after enough failures.
+        backoff_scale = Math.min(2 ** ((attempts + 1) / 2), 90);
+    }
+    // Add a bit jitter to backoff scale.
+    const backoff_delay_secs = ((1 + Math.random()) / 2) * backoff_scale;
+    let rate_limit_delay_secs = 0;
+    const rate_limited_error_schema = z.object({
+        "retry-after": z.number(),
+        code: z.literal("RATE_LIMIT_HIT"),
+    });
+    const parsed = rate_limited_error_schema.safeParse(xhr.responseJSON);
+    if (xhr.status === 429 && parsed?.success && parsed?.data) {
+        // Add a bit of jitter to the required delay suggested by the
+        // server, because we may be racing with other copies of the web
+        // app.
+        rate_limit_delay_secs = parsed.data["retry-after"] + Math.random() * 0.5;
+    }
+    return Math.max(backoff_delay_secs, rate_limit_delay_secs);
 }
