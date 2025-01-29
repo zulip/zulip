078ce8938076d8639bf0095003e595deb5a06aa4
Lauryn Menard <lauryn@zulip.com>
2025-01-29 21:00:36 +0100

    util: Extract helper function for backoff of retries on error response.
    Extracts message_fetch and get_events algorithms for calculating the
    exponential backoff into a shared helper function.
    
    These match the algorithm that were designed for the Python API, except
    that we use a ratio of 2 rather than sqrt(2) in the message_fetch code
    path.

--- web/tests/util.test.cjs
+++ web/tests/util.test.cjs
@@ -510,3 +510,46 @@ run_test("get_final_topic_display_name", ({override}) => {
     override(realm, "realm_empty_topic_display_name", "random topic name");
     assert.deepEqual(util.get_final_topic_display_name(""), "random topic name");
 });
+
+run_test("get_retry_backoff_seconds", () => {
+    const xhr_500_error = {
+        status: 500,
+    };
+
+    // Shorter backoff scale
+    // First retry should be between 1-2 seconds.
+    let backoff = util.get_retry_backoff_seconds(xhr_500_error, 1, true);
+    assert.ok(backoff >= 1);
+    assert.ok(backoff < 3);
+    // 100th retry should be between 16-32 seconds.
+    backoff = util.get_retry_backoff_seconds(xhr_500_error, 100, true);
+    assert.ok(backoff >= 16);
+    assert.ok(backoff <= 32);
+
+    // Longer backoff scale
+    // First retry should be between 1-2 seconds.
+    backoff = util.get_retry_backoff_seconds(xhr_500_error, 1);
+    assert.ok(backoff >= 1);
+    assert.ok(backoff <= 3);
+    // 100th retry should be between 45-90 seconds.
+    backoff = util.get_retry_backoff_seconds(xhr_500_error, 100);
+    assert.ok(backoff >= 45);
+    assert.ok(backoff <= 90);
+
+    const xhr_rate_limit_error = {
+        status: 429,
+        responseJSON: {
+            code: "RATE_LIMIT_HIT",
+            msg: "API usage exceeded rate limit",
+            result: "error",
+            "retry-after": 28.706807374954224,
+        },
+    };
+    // First retry should be greater than the retry-after value.
+    backoff = util.get_retry_backoff_seconds(xhr_rate_limit_error, 1);
+    assert.ok(backoff >= 28.706807374954224);
+    // 100th retry should be between 45-90 seconds.
+    backoff = util.get_retry_backoff_seconds(xhr_rate_limit_error, 100);
+    assert.ok(backoff >= 45);
+    assert.ok(backoff <= 90);
+});
