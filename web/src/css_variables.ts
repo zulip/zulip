dc326dd5ed2b0740c7e24c4ad2a25b2bceac2df8
Karl Stolley <karl@zulip.com>
2025-01-29 16:03:49 -0500

    css_variables: Remove unused breakpoints.

--- web/src/css_variables.ts
+++ web/src/css_variables.ts
@@ -13,14 +13,6 @@ const ms = 320; // Mobile small
 // Breakpoints for middle column
 const mc = 849; // Middle column as wide as it appears after the `sm` breakpoint
 
-// Breakpoints for showing and hiding compose buttons which do not always fit in
-// a single row below the compose box
-const cb1 = 1314;
-const cb2 = 1072;
-const cb3 = 860;
-const cb4 = 750;
-const cb5 = 504;
-
 export const media_breakpoints = {
     xs_min: xs + "px",
     sm_min: sm + "px",
@@ -31,11 +23,6 @@ export const media_breakpoints = {
     ml_min: ml + "px",
     mm_min: mm + "px",
     ms_min: ms + "px",
-    cb1_min: cb1 + "px",
-    cb2_min: cb2 + "px",
-    cb3_min: cb3 + "px",
-    cb4_min: cb4 + "px",
-    cb5_min: cb5 + "px",
     short_navbar_cutoff_height: "600px",
     // Used for main settings overlay and stream/subscription settings overlay
     // measured as the width of the overlay itself, not the width of the full
