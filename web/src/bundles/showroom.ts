ead2f2d2540e62c5d8bc6faa5185ab571f373e70
Sayam Samal <sayam@zulip.com>
2025-01-30 14:11:46 +0530

    showroom: Fix misordering of bootstrap.portico.css.
    This commit uses the same fix as the commit 6776e80, to include
    `bootstrap.portico.css` and prevent webpack from misordering it from the
    rest of the portico CSS chunk.

--- web/src/bundles/showroom.ts
+++ web/src/bundles/showroom.ts
@@ -1,9 +1,7 @@
-import "./common.ts";
-import "../portico/header.ts";
+import "./portico.ts";
 import "../portico/showroom.ts";
 
 // Import styles in the required order
-import "../../styles/portico/portico_styles.css";
 import "../../styles/portico/showroom.css";
 import "../../styles/app_variables.css";
 import "../../styles/buttons.css";
