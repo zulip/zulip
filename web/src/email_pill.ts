0fd526a7c6748e4f31d585ca8fdf1f6d749426c1
Maneesh Shukla <shuklamaneesh24@gmail.com>
2025-02-01 16:23:05 +0530

    email_pill: Allow comma into the name part of the email.

--- web/src/email_pill.ts
+++ web/src/email_pill.ts
@@ -1,4 +1,5 @@
-import {parseOneAddress} from "email-addresses";
+import {parseAddressList, parseOneAddress} from "email-addresses";
+import $ from "jquery";
 
 import type {InputPillConfig, InputPillContainer} from "./input_pill.ts";
 import * as input_pill from "./input_pill.ts";
@@ -43,10 +44,21 @@ export function get_current_email(
     pill_container: input_pill.InputPillContainer<EmailPill>,
 ): string | null {
     const current_text = pill_container.getCurrentText();
-    if (current_text !== null && parseOneAddress(current_text)) {
+    if (current_text !== null && parseAddressList(current_text)) {
         return current_text;
     }
     return null;
+}
+
+export function split_text_to_form_email_pills(raw_emails: string): string[] {
+    const parsed_emails = parseAddressList(raw_emails);
+    if (!parsed_emails) {
+        return [];
+    }
+
+    return parsed_emails
+        .filter((email) => email.type === "mailbox")
+        .map((email) => (email.name ? `"${email.name}" <${email.address}>` : email.address));
 }
 
 export function create_pills(
@@ -59,6 +71,34 @@ export function create_pills(
         create_item_from_text: create_item_from_email,
         get_text_from_item: get_email_from_item,
         get_display_value_from_item: get_email_from_item,
+        split_text_on_comma: false,
+        convert_to_pill_on_enter: false,
+    });
+
+    // We don't automatically create pills on paste. When the user
+    // presses enter, we validate the input then.
+    pill_container.createPillonPaste(() => false);
+
+    // We are keeping a separate "Enter" key handler because, for email pills,
+    // we want to include a comma between the display name.
+    $pill_container.on("keydown", ".input", function (e) {
+        if (e.key === "Enter") {
+            e.preventDefault();
+
+            const current_text = pill_container.getCurrentText();
+            if (current_text && current_text.trim() !== "") {
+                const email_addresses = split_text_to_form_email_pills(current_text.trim());
+
+                if (email_addresses?.length > 0) {
+                    for (const email of email_addresses) {
+                        pill_container.appendValue(email);
+                    }
+                    pill_container.clear_text();
+                } else {
+                    $(this).addClass("shake");
+                }
+            }
+        }
     });
     return pill_container;
 }
