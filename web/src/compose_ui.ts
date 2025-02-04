30b1ae7111f5b9915087cbdf7300387bb8238175
Karl Stolley <karl@zulip.com>
2025-02-04 14:07:13 -0600

    compose: Correctly focus when replying to general chat.

--- web/src/compose_ui.ts
+++ web/src/compose_ui.ts
@@ -202,13 +202,18 @@ export function maybe_show_scrolling_formatting_buttons(container_selector: stri
 function get_focus_area(opts: ComposeTriggeredOptions): string {
     // Set focus to "Topic" when narrowed to a stream+topic
     // and "Start new conversation" button clicked.
-    if (opts.message_type === "stream" && opts.stream_id && !opts.topic) {
+    if (
+        opts.message_type === "stream" &&
+        opts.stream_id &&
+        !opts.topic &&
+        realm.realm_mandatory_topics
+    ) {
         return "input#stream_message_recipient_topic";
     } else if (
         (opts.message_type === "stream" && opts.stream_id !== undefined) ||
         (opts.message_type === "private" && opts.private_message_recipient)
     ) {
-        if (opts.trigger === "clear topic button") {
+        if (opts.trigger === "clear topic button" || opts.trigger === "compose_hotkey") {
             return "input#stream_message_recipient_topic";
         }
         return "textarea#compose-textarea";
