e5ffad68b8ad5fd656049bd212d0a4fab5c681f5
PieterCK <pieterceka123@gmail.com>
2025-02-10 15:18:32 +0700

    state_data: Abstract `all_event_types` data.
    This creates a schema variable for the `all_event_types` object. This is
    a prep commit to refactor the integration_url_modal to better handle
    integrations state data.

--- web/src/state_data.ts
+++ web/src/state_data.ts
@@ -255,6 +255,8 @@ export const realm_linkifier_schema = z.object({
     id: z.number(),
 });
 
+export const integration_all_event_types_shcmea = z.nullable(z.array(z.string()));
+
 // Sync this with zerver.lib.webhooks.interfaced_setting.SettingContext
 export const interfaced_settings_context_schema = z.object({
     parameter_name: z.string(),
@@ -370,7 +372,7 @@ export const realm_schema = z.object({
         z.object({
             display_name: z.string(),
             name: z.string(),
-            all_event_types: z.nullable(z.array(z.string())),
+            all_event_types: integration_all_event_types_shcmea,
             config_options: integration_config_options_schema,
             interfaced_settings: integrations_interfaced_settings_schema,
         }),
