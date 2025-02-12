896f51285b79cc149e76fd3ab1c02785d621d062
sanchi-t <ssharmas10662@gmail.com>
2025-02-12 21:15:42 +0530

    stream_privacy: Add archive icon for archived channels.
    Archived channels are now displayed with an archived
    icon.

--- web/src/filter.ts
+++ web/src/filter.ts
@@ -1310,6 +1310,10 @@ export class Filter {
                     icon = "question-circle-o";
                     break;
                 }
+                if (sub.is_archived) {
+                    zulip_icon = "archive";
+                    break;
+                }
                 if (sub.invite_only) {
                     zulip_icon = "lock";
                     break;
