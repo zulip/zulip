0387ecc064a6a94d784f973088f836313268d5cf
apoorvapendse <apoorvavpendse@gmail.com>
2025-02-12 18:18:55 +0530

    user_groups: Disable actions for deactivated groups.
    Joining/leaving the group as well as
    adding or removing others is disabled.

--- web/src/settings_data.ts
+++ web/src/settings_data.ts
@@ -176,7 +176,10 @@ export function can_manage_user_group(group_id: number): boolean {
 
 export function can_add_members_to_user_group(group_id: number): boolean {
     const group = user_groups.get_user_group_from_id(group_id);
-
+    // We cannot add members if the group is deactivated.
+    if (group.deactivated) {
+        return false;
+    }
     if (
         user_has_permission_for_group_setting(
             group.can_add_members_group,
@@ -192,7 +195,10 @@ export function can_add_members_to_user_group(group_id: number): boolean {
 
 export function can_remove_members_from_user_group(group_id: number): boolean {
     const group = user_groups.get_user_group_from_id(group_id);
-
+    // We cannot remove members if the group is deactivated.
+    if (group.deactivated) {
+        return false;
+    }
     if (
         user_has_permission_for_group_setting(
             group.can_remove_members_group,
@@ -208,7 +214,10 @@ export function can_remove_members_from_user_group(group_id: number): boolean {
 
 export function can_join_user_group(group_id: number): boolean {
     const group = user_groups.get_user_group_from_id(group_id);
-
+    // One cannot join a deactivated group.
+    if (group.deactivated) {
+        return false;
+    }
     if (user_has_permission_for_group_setting(group.can_join_group, "can_join_group", "group")) {
         return true;
     }
@@ -217,8 +226,11 @@ export function can_join_user_group(group_id: number): boolean {
 }
 
 export function can_leave_user_group(group_id: number): boolean {
+    // One cannot leave a deactivated group.
     const group = user_groups.get_user_group_from_id(group_id);
-
+    if (group.deactivated) {
+        return false;
+    }
     if (user_has_permission_for_group_setting(group.can_leave_group, "can_leave_group", "group")) {
         return true;
     }
