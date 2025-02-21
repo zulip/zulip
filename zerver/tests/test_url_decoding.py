83abddb50b80e40b5870320703994ac3bb5a090b
PieterCK <pieterceka123@gmail.com>
2025-02-21 13:31:23 +0700

    url_decoding: Add the base `Filter` class.
    This is the Python copy of a class of the same name in frontend's
    `filter.Filter` class. Although it roughly follows the same set
    validations, it probably won't have the same set of methods with the
    frontend's `Filter`. It does not validate the referenced objects(users
    and channels), and validates terms differently--e.g, converts operand to
    prefered types when possible--, adjusting for its use in the backend.
    
    The server version will primarily be used to fix narrow links in
    exported messages during import.

--- zerver/tests/test_url_decoding.py
+++ zerver/tests/test_url_decoding.py
@@ -1,8 +1,15 @@
-import orjson
+from typing import Any, TypeAlias
 
+import orjson
+from typing_extensions import override
+
+from zerver.lib.narrow import BadNarrowOperatorError
 from zerver.lib.narrow_helpers import NarrowTerm
 from zerver.lib.test_classes import ZulipTestCase
-from zerver.lib.url_decoding import is_same_server_message_link, parse_narrow_url
+from zerver.lib.url_decoding import Filter, is_same_server_message_link, parse_narrow_url
+from zerver.models.realms import get_realm
+
+NarrowTermFixtureT: TypeAlias = dict[str, dict[str, list[dict[str, Any]]]]
 
 
 class URLDecodeTest(ZulipTestCase):
@@ -31,3 +38,27 @@ class NarrowURLDecodeTest(ZulipTestCase):
                         NarrowTerm(**term) for term in expected_output
                     ]
                     self.assertListEqual(parsed_terms, expected_narrow_terms)
+
+
+class NarrowTermFilterTest(ZulipTestCase):
+    @override
+    def setUp(self) -> None:
+        self.realm = get_realm("zulip")
+        return super().setUp()
+
+    def test_initialize_narrow_terms(self) -> None:
+        tests: NarrowTermFixtureT = orjson.loads(self.fixture_data("narrow_term_fixture.json"))
+        for test_name, test_case in tests.items():
+            narrow_terms_input = [NarrowTerm(**term) for term in test_case["valid_terms"]]
+            filter_instance = Filter(narrow_terms_input, self.realm)
+            actual_output = filter_instance.terms()
+
+            with self.subTest(name=test_name):
+                expected_output: list[NarrowTerm] = [
+                    NarrowTerm(**term) for term in test_case["expected_output"]
+                ]
+                self.assertListEqual(actual_output, expected_output)
+
+                for invalid_term in test_case["invalid_terms"]:
+                    with self.assertRaises(BadNarrowOperatorError):
+                        Filter([NarrowTerm(**invalid_term)], self.realm)
