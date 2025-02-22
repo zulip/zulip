5582fbae0279061101084753164d5775153796c2
Anders Kaseorg <anders@zulip.com>
2025-02-21 16:42:17 -0800

    requirements: Patch invalid escape sequences in talon.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -49,4 +49,4 @@ API_FEATURE_LEVEL = 354  # Last bumped for accounting groups for has_message_acc
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (316, 0)  # bumped 2025-02-21 to upgrade tusd
+PROVISION_VERSION = (316, 1)  # bumped 2025-02-21 to patch talon
