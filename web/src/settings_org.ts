82df6c85de2158d561aeaf7e7b2b63d7c06be3c9
Maneesh Shukla <shuklamaneesh24@gmail.com>
2025-03-04 23:13:26 +0530

    organization_settings: Restyle remaining sea-green button to action-button-quiet-brand.
    Fixes part of #33130.

--- web/src/settings_org.ts
+++ web/src/settings_org.ts
@@ -1522,5 +1522,9 @@ export function build_page(): void {
         realm_logo.build_realm_logo_widget(upload_realm_logo_or_icon, true);
     }
 
+    $("#id_org_profile_preview").on("click", () => {
+        window.open("/login/?preview=true", "_blank", "noopener,noreferrer");
+    });
+
     $("#organization-profile .deactivate_realm_button").on("click", deactivate_organization);
 }
