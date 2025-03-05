ff1299e75f027c72e076d6dd1a2d2e130055ef01
Aman Agrawal <amanagr@zulip.com>
2025-03-05 06:01:59 +0000

    filter: Use airplane icon for DMs in navbar.

--- web/src/filter.ts
+++ web/src/filter.ts
@@ -1337,7 +1337,7 @@ export class Filter {
                 break;
             }
             case "is-dm":
-                zulip_icon = "user";
+                zulip_icon = "send-dm";
                 break;
             case "is-starred":
                 zulip_icon = "star";
@@ -1346,7 +1346,7 @@ export class Filter {
                 zulip_icon = "at-sign";
                 break;
             case "dm":
-                zulip_icon = "user";
+                zulip_icon = "send-dm";
                 break;
             case "is-resolved":
                 icon = "check";
