037609c451c80e551d6be368d5ea0b872bffd7a8
Maneesh Shukla <shuklamaneesh24@gmail.com>
2025-03-06 19:56:39 +0530

    modals: Remove `data-dismiss` usage and update cancel actions.
    Replaced `data-dismiss="modal"` attributes with class/ID-based event
    handling to align with Micromodal usage. Also removed an unused CSS
    rule which was defined.

--- web/src/user_group_edit.ts
+++ web/src/user_group_edit.ts
@@ -1871,15 +1871,19 @@ export function initialize(): void {
         open_create_user_group();
     });
 
-    $("#groups_overlay_container").on("click", "#user_group_creation_form [data-dismiss]", (e) => {
-        e.preventDefault();
-        // we want to make sure that the click is not just a simulated
-        // click; this fixes an issue where hitting "Enter" would
-        // trigger this code path due to bootstrap magic.
-        if (e.clientY !== 0) {
-            open_right_panel_empty();
-        }
-    });
+    $("#groups_overlay_container").on(
+        "click",
+        "#user_group_creation_form .create_user_group_cancel",
+        (e) => {
+            e.preventDefault();
+            // we want to make sure that the click is not just a simulated
+            // click; this fixes an issue where hitting "Enter" would
+            // trigger this code path due to bootstrap magic.
+            if (e.clientY !== 0) {
+                open_right_panel_empty();
+            }
+        },
+    );
 
     $("#groups_overlay_container").on("click", ".group-row", show_right_section);
 
