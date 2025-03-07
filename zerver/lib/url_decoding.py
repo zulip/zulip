d775fb34b2e637bc414caf1223a5bf5879b1fb8a
PieterCK <pieterceka123@gmail.com>
2025-03-07 14:57:19 +0700

    url_decoding: Refactor a `is_same_server_narrow_link`.

--- zerver/lib/url_decoding.py
+++ zerver/lib/url_decoding.py
@@ -19,27 +19,33 @@ from zerver.models.realms import Realm
 from zerver.models.streams import Stream
 
 
-def is_same_server_message_link(url: str) -> bool:
+def is_same_server_narrow_link(url: str) -> bool:
     split_result = urlsplit(url)
     hostname = split_result.hostname
     fragment = split_result.fragment
 
     if hostname not in {None, settings.EXTERNAL_HOST_WITHOUT_PORT}:
         return False
+    fragment_parts = fragment.split("/")
+    category = fragment_parts[0]
 
+    return category == "narrow"
+
+
+def is_same_server_message_link(url: str) -> bool:
+    if not is_same_server_narrow_link(url):
+        return False
     # A message link always has category `narrow`, section `channel`
     # or `dm`, and ends with `/near/<message_id>`, where <message_id>
     # is a sequence of digits. The URL fragment of a message link has
     # at least 5 parts. e.g. '#narrow/dm/9,15-dm/near/43'
-    fragment_parts = fragment.split("/")
+    fragment_parts = urlsplit(url).fragment.split("/")
     if len(fragment_parts) < 5:
         return False
 
-    category = fragment_parts[0]
     section = fragment_parts[1]
     ends_with_near_message_id = fragment_parts[-2] == "near" and fragment_parts[-1].isdigit()
-
-    return category == "narrow" and section in {"channel", "dm"} and ends_with_near_message_id
+    return section in {"channel", "dm"} and ends_with_near_message_id
 
 
 CHANNEL_SYNONYMS = {"stream": "channel", "streams": "channels"}
