9d0c065ab50326f657a45ef37f381d26d616b0b1
Alex Vandiver <alexmv@zulip.com>
2025-03-13 14:54:01 +0000

    install-node: Upgrade Node.js from 20.15.1 to 22.14.0.
    This is the equivalent of 3af4900891a9, as a better fix than
    7c2212caa38a for the provisioning errors caused by key rotation in
    https://github.com/nodejs/corepack/issues/612

--- version.py
+++ version.py
@@ -49,4 +49,4 @@ API_FEATURE_LEVEL = 278  # Last bumped for backporting original-dimensions on sp
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (295, 0)  # bumped 2024-12-20 to upgrade Python requirements
+PROVISION_VERSION = (295, 1)  # bumped 2025-03-13 to upgrade Node.js
