2363d948bfc7ba243f9d5eb86d9ef833ac71cecc
Tim Abbott <tabbott@zulip.com>
2025-03-14 10:08:27 -0700

    user_events: Rename person_obj to user for clarity.

--- web/src/user_events.ts
+++ web/src/user_events.ts
@@ -58,9 +58,9 @@ export const user_update_schema = z.object({user_id: z.number()}).and(
 type UserUpdate = z.output<typeof user_update_schema>;
 
 export const update_person = function update(person: UserUpdate): void {
-    const person_obj = people.maybe_get_user_by_id(person.user_id);
+    const user = people.maybe_get_user_by_id(person.user_id);
 
-    if (!person_obj) {
+    if (!user) {
         blueslip.error("Got update_person event for unexpected user", {user_id: person.user_id});
         return;
     }
@@ -80,8 +80,8 @@ export const update_person = function update(person: UserUpdate): void {
 
     if ("delivery_email" in person) {
         const delivery_email = person.delivery_email;
-        person_obj.delivery_email = delivery_email;
-        user_profile.update_profile_modal_ui(person_obj, person);
+        user.delivery_email = delivery_email;
+        user_profile.update_profile_modal_ui(user, person);
         if (people.is_my_user_id(person.user_id)) {
             assert(delivery_email !== null);
             settings_account.update_email(delivery_email);
@@ -91,13 +91,13 @@ export const update_person = function update(person: UserUpdate): void {
     }
 
     if ("full_name" in person) {
-        people.set_full_name(person_obj, person.full_name);
+        people.set_full_name(user, person.full_name);
 
         settings_users.update_user_data(person.user_id, person);
         activity_ui.redraw();
         message_live_update.update_user_full_name(person.user_id, person.full_name);
         pm_list.update_private_messages();
-        user_profile.update_profile_modal_ui(person_obj, person);
+        user_profile.update_profile_modal_ui(user, person);
         if (people.is_my_user_id(person.user_id)) {
             current_user.full_name = person.full_name;
             settings_account.update_full_name(person.full_name);
@@ -105,24 +105,24 @@ export const update_person = function update(person: UserUpdate): void {
     }
 
     if ("role" in person) {
-        person_obj.role = person.role;
-        person_obj.is_owner = person.role === settings_config.user_role_values.owner.code;
-        person_obj.is_admin =
-            person.role === settings_config.user_role_values.admin.code || person_obj.is_owner;
-        person_obj.is_guest = person.role === settings_config.user_role_values.guest.code;
-        person_obj.is_moderator = person.role === settings_config.user_role_values.moderator.code;
+        user.role = person.role;
+        user.is_owner = person.role === settings_config.user_role_values.owner.code;
+        user.is_admin =
+            person.role === settings_config.user_role_values.admin.code || user.is_owner;
+        user.is_guest = person.role === settings_config.user_role_values.guest.code;
+        user.is_moderator = person.role === settings_config.user_role_values.moderator.code;
         settings_users.update_user_data(person.user_id, person);
-        user_profile.update_profile_modal_ui(person_obj, person);
+        user_profile.update_profile_modal_ui(user, person);
 
-        if (people.is_my_user_id(person.user_id) && current_user.is_owner !== person_obj.is_owner) {
-            current_user.is_owner = person_obj.is_owner;
+        if (people.is_my_user_id(person.user_id) && current_user.is_owner !== user.is_owner) {
+            current_user.is_owner = user.is_owner;
             settings_org.maybe_disable_widgets();
             settings_org.enable_or_disable_group_permission_settings();
             settings.update_lock_icon_in_sidebar();
         }
 
-        if (people.is_my_user_id(person.user_id) && current_user.is_admin !== person_obj.is_admin) {
-            current_user.is_admin = person_obj.is_admin;
+        if (people.is_my_user_id(person.user_id) && current_user.is_admin !== user.is_admin) {
+            current_user.is_admin = user.is_admin;
             settings_linkifiers.maybe_disable_widgets();
             settings_org.maybe_disable_widgets();
             settings_org.enable_or_disable_group_permission_settings();
@@ -135,16 +135,16 @@ export const update_person = function update(person: UserUpdate): void {
 
         if (
             people.is_my_user_id(person.user_id) &&
-            current_user.is_moderator !== person_obj.is_moderator
+            current_user.is_moderator !== user.is_moderator
         ) {
-            current_user.is_moderator = person_obj.is_moderator;
+            current_user.is_moderator = user.is_moderator;
         }
     }
 
     if ("avatar_url" in person) {
         const url = person.avatar_url;
-        person_obj.avatar_url = url;
-        person_obj.avatar_version = person.avatar_version;
+        user.avatar_url = url;
+        user.avatar_version = person.avatar_version;
 
         if (people.is_my_user_id(person.user_id)) {
             current_user.avatar_source = person.avatar_source;
@@ -154,13 +154,13 @@ export const update_person = function update(person: UserUpdate): void {
             $("#personal-menu .header-button-avatar").attr("src", `${person.avatar_url_medium}`);
         }
 
-        message_live_update.update_avatar(person_obj.user_id, person.avatar_url);
-        user_profile.update_profile_modal_ui(person_obj, person);
+        message_live_update.update_avatar(user.user_id, person.avatar_url);
+        user_profile.update_profile_modal_ui(user, person);
     }
 
     if ("custom_profile_field" in person) {
         people.set_custom_profile_field_data(person.user_id, person.custom_profile_field);
-        user_profile.update_user_custom_profile_fields(person_obj);
+        user_profile.update_user_custom_profile_fields(user);
         if (person.user_id === people.my_current_user_id()) {
             navbar_alerts.maybe_toggle_empty_required_profile_fields_banner();
 
@@ -191,22 +191,22 @@ export const update_person = function update(person: UserUpdate): void {
     }
 
     if ("timezone" in person) {
-        person_obj.timezone = person.timezone;
+        user.timezone = person.timezone;
     }
 
     if ("bot_owner_id" in person) {
-        assert(person_obj.is_bot);
-        person_obj.bot_owner_id = person.bot_owner_id;
-        user_profile.update_profile_modal_ui(person_obj, person);
+        assert(user.is_bot);
+        user.bot_owner_id = person.bot_owner_id;
+        user_profile.update_profile_modal_ui(user, person);
     }
 
     if ("is_active" in person) {
-        const is_bot_user = person_obj.is_bot;
+        const is_bot_user = user.is_bot;
         if (person.is_active) {
-            people.add_active_user(person_obj);
+            people.add_active_user(user);
             settings_users.update_view_on_reactivate(person.user_id, is_bot_user);
         } else {
-            people.deactivate(person_obj);
+            people.deactivate(user);
             stream_events.remove_deactivated_user_from_all_streams(person.user_id);
             user_group_edit.remove_deactivated_user_from_all_groups(person.user_id);
             settings_users.update_view_on_deactivate(person.user_id, is_bot_user);
