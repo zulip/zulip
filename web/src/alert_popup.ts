0e758b3115cc7ba01b5589916b07f6622f803cb9
Karl Stolley <karl@zulip.com>
2025-03-14 13:45:02 -0400

    alerts: Improve styles, behavior between app and portico.

--- web/src/alert_popup.ts
+++ web/src/alert_popup.ts
@@ -1,8 +1,8 @@
 import $ from "jquery";
 
 // this will hide the alerts that you click "x" on.
-$("body").on("click", ".alert-box > div .exit", function () {
-    const $alert = $(this).closest(".alert-box > div");
+$("body").on("click", ".alert-box .exit", function () {
+    const $alert = $(this).parent("div");
     $alert.addClass("fade-out");
     setTimeout(() => {
         $alert.removeClass("fade-out show");
