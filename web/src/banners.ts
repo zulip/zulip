7a96cec774f348296e9beb8e60913f4d2cd7b922
Tim Abbott <tabbott@zulip.com>
2025-03-13 17:37:24 -0700

    banners: Don't trigger spurious resize events.
    Resizes are very disruptive, and are not required for most
    banners. The one exception is the top-of-window banners, which ideally
    would also not require a resize, but do impact the geometry of space
    available to other components.

--- web/src/banners.ts
+++ web/src/banners.ts
@@ -28,17 +28,23 @@ export type AlertBanner = Banner & {
 export function open(banner: Banner | AlertBanner, $banner_container: JQuery): void {
     const banner_html = render_banner(banner);
     $banner_container.html(banner_html);
+    // TODO: Only do a resize for navbar_alerts banners, and try to
+    // avoid doing that at all. Resizes have major side effects.
     $(window).trigger("resize");
 }
 
 export function append(banner: Banner | AlertBanner, $banner_container: JQuery): void {
     const $banner_html = render_banner(banner);
     $banner_container.append($banner_html);
+    // TODO: Only do a resize for navbar_alerts banners, and try to
+    // avoid doing that at all. Resizes have major side effects.
     $(window).trigger("resize");
 }
 
 export function close($banner: JQuery): void {
     $banner.remove();
+    // TODO: Only do a resize for navbar_alerts banners, and try to
+    // avoid doing that at all. Resizes have major side effects.
     $(window).trigger("resize");
 }
 
@@ -46,7 +52,8 @@ export function fade_out_popup_banner($banner: JQuery): void {
     $banner.addClass("fade-out");
     // The delay is the same as the animation duration for fade-out.
     setTimeout(() => {
-        close($banner);
+        // We don't use close, because it triggers a resize, which is disruptive.
+        $banner.remove();
     }, 300);
 }
 
