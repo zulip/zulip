zulip/zulip

commit 96eb49d947d02d9a7f55f6a198c2dc4ee8635272

hotkey: Fix `=` to toggle reaction results in error.
Fixes #33574

This was due to the method used to get the emoji name was incorrect.

web/src/hotkey.js
@@ -1229,7 +1229,8 @@ export function process_hotkey(e, hotkey) {
                 return true;
             }
 
-            reactions.toggle_emoji_reaction(msg, first_reaction.emoji_name);
+            const canonical_name = emoji.get_emoji_name(first_reaction.emoji_code);
+            reactions.toggle_emoji_reaction(msg, canonical_name);
             return true;
         }
         case "toggle_topic_visibility_policy":

commit b26f54623c9e9f5aa7eb8331d3776a65be996e6c

hotkey: Fix upvote first emoji shortcut for custom emoji.
Fixes a bug introduced in #96eb49d, which broke the hotkey
for custom emoji.

web/src/hotkey.js
@@ -1229,7 +1229,9 @@ export function process_hotkey(e, hotkey) {
             }
 
             const canonical_name = emoji.get_emoji_name(first_reaction.emoji_code);
-            reactions.toggle_emoji_reaction(msg, canonical_name);
+            // `canonical_name` will be `undefined` for custom emoji, so we can default
+            // to the `emoji_name`.
+            reactions.toggle_emoji_reaction(msg, canonical_name ?? first_reaction.emoji_name);
             return true;
         }
         case "toggle_topic_visibility_policy":