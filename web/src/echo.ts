zulip/zulip

commit 238c35f2af9f7d3ef63609f55cee5f4d2d78ffd0

message_events_util: Remove unnecessary callback.
All callbacks want to add messages, so we just directly do that.

web/src/echo.ts
@@ -558,7 +558,6 @@ export function process_from_server(messages: ServerMessage[]): ServerMessage[] 
                 message_events_util.maybe_add_narrowed_messages(
                     msgs_to_rerender_or_add_to_narrow,
                     msg_list,
-                    message_util.add_new_messages,
                 );
             } else {
                 // In theory, we could just rerender messages where there were

web/src/message_events.ts
@@ -25,7 +25,6 @@ import * as message_notifications from "./message_notifications.ts";
 import * as message_parser from "./message_parser.ts";
 import * as message_store from "./message_store.ts";
 import {type Message, type RawMessage, raw_message_schema} from "./message_store.ts";
-import * as message_util from "./message_util.ts";
 import * as message_view from "./message_view.ts";
 import * as narrow_state from "./narrow_state.ts";
 import * as pm_list from "./pm_list.ts";
@@ -313,11 +312,7 @@ export function insert_new_messages(
                 continue;
             }
 
-            message_events_util.maybe_add_narrowed_messages(
-                messages,
-                list,
-                message_util.add_new_messages,
-            );
+            message_events_util.maybe_add_narrowed_messages(messages, list);
             continue;
         }
 
@@ -739,11 +734,7 @@ export function update_messages(events: UpdateMessageEvent[]): void {
                     // simply updated.
                     list.remove_and_rerender(event_msg_ids);
                     // For filters that cannot be processed locally, ask server.
-                    message_events_util.maybe_add_narrowed_messages(
-                        event_messages,
-                        list,
-                        message_util.add_messages,
-                    );
+                    message_events_util.maybe_add_narrowed_messages(event_messages, list);
                 }
             }
         }

web/src/message_events_util.ts
@@ -3,7 +3,7 @@ import {z} from "zod";
 import * as blueslip from "./blueslip.ts";
 import * as channel from "./channel.ts";
 import * as compose_notifications from "./compose_notifications.ts";
-import type {MessageList, RenderInfo} from "./message_list.ts";
+import type {MessageList} from "./message_list.ts";
 import * as message_lists from "./message_lists.ts";
 import * as message_store from "./message_store.ts";
 import type {Message} from "./message_store.ts";
@@ -24,7 +24,6 @@ const msg_match_narrow_api_response_schema = z.object({
 export function maybe_add_narrowed_messages(
     messages: Message[],
     msg_list: MessageList,
-    callback: (messages: Message[], msg_list: MessageList) => RenderInfo | undefined,
     attempt = 1,
 ): void {
     const ids: number[] = [];
@@ -83,7 +82,7 @@ export function maybe_add_narrowed_messages(
             // Remove the elsewhere_messages from the message list since
             // they don't match the filter as per data from server.
             msg_list.remove_and_rerender(elsewhere_messages.map((msg) => msg.id));
-            callback(new_messages, msg_list);
+            msg_list.add_messages(new_messages);
             unread_ops.process_visible();
             compose_notifications.notify_messages_outside_current_search(elsewhere_messages);
         },
@@ -112,7 +111,7 @@ export function maybe_add_narrowed_messages(
                 if (msg_list === message_lists.current) {
                     // Don't actually try again if we un-narrowed
                     // while waiting
-                    maybe_add_narrowed_messages(messages, msg_list, callback, attempt + 1);
+                    maybe_add_narrowed_messages(messages, msg_list, attempt + 1);
                 }
             }, delay);
         },

web/src/message_util.ts
@@ -1,7 +1,6 @@
 import assert from "minimalistic-assert";
 
 import {all_messages_data} from "./all_messages_data.ts";
-import type {MessageList, RenderInfo} from "./message_list.ts";
 import * as message_lists from "./message_lists.ts";
 import * as message_store from "./message_store.ts";
 import type {Message} from "./message_store.ts";
@@ -25,34 +24,6 @@ export function do_unread_count_updates(messages: Message[], expect_no_new_unrea
     }
 }
 
-export function add_messages(
-    messages: Message[],
-    msg_list: MessageList,
-    append_to_view_opts?: {messages_are_new: boolean},
-): RenderInfo | undefined {
-    if (!messages) {
-        return undefined;
-    }
-
-    const render_info = msg_list.add_messages(messages, append_to_view_opts);
-
-    return render_info;
-}
-
-export function add_new_messages(
-    messages: Message[],
-    msg_list: MessageList,
-): RenderInfo | undefined {
-    if (!msg_list.data.fetch_status.has_found_newest()) {
-        // We don't render newly received messages for the message list,
-        // if we haven't found the latest messages to be displayed in the
-        // narrow. Otherwise the new message would be rendered just after
-        // the previously fetched messages when that's inaccurate.
-        msg_list.data.fetch_status.update_expected_max_message_id(messages);
-        return undefined;
-    }
-    return add_messages(messages, msg_list, {messages_are_new: true});
-}
 export function get_count_of_messages_in_topic_sent_after_current_message(
     stream_id: number,
     topic: string,

commit cef8738a8e6483b19d7112d75483731c3c5ca8e4

message_list: Fix list not scrolling when new message is added.
Auto scrolling to view the new message is not working.
This bug was introduced in 238c35f2af9f7d3ef63609f55cee5f4d2d78ffd0.
Fixed by passing `messages_are_new` in the relevant functions.

web/src/echo.ts
@@ -579,11 +579,15 @@ export function process_from_server(messages: ServerMessage[]): ServerMessage[] 
 
     if (msgs_to_rerender_or_add_to_narrow.length > 0) {
         for (const msg_list of message_lists.all_rendered_message_lists()) {
+            // Since we already have this message locally echoed, it is ok to
+            // not scroll when this message is added to the view as user has
+            // already seen this message. Hence, we are not passing the
+            // `message_are_new` boolean as `true` here.
             if (!msg_list.data.filter.can_apply_locally()) {
                 // If this message list is a search filter that we
                 // cannot apply locally, we will not have locally
                 // echoed echoed the message at all originally, and
-                // must the server now whether to add it to the view.
+                // must request the server now whether to add it to the view.
                 message_events_util.maybe_add_narrowed_messages(
                     msgs_to_rerender_or_add_to_narrow,
                     msg_list,

web/src/message_events.ts
@@ -317,12 +317,13 @@ export function insert_new_messages(
                 continue;
             }
 
-            message_events_util.maybe_add_narrowed_messages(messages, list);
+            const messages_are_new = true;
+            message_events_util.maybe_add_narrowed_messages(messages, list, messages_are_new);
             continue;
         }
 
         // Update the message list's rendering for the newly arrived messages.
-        const render_info = list.add_messages(messages);
+        const render_info = list.add_messages(messages, {messages_are_new: true});
 
         // The render_info.need_user_to_scroll calculation, which
         // looks at message feed scroll positions to see whether the

web/src/message_events_util.ts
@@ -24,6 +24,7 @@ const msg_match_narrow_api_response_schema = z.object({
 export function maybe_add_narrowed_messages(
     messages: Message[],
     msg_list: MessageList,
+    messages_are_new = false,
     attempt = 1,
 ): void {
     const ids: number[] = [];
@@ -82,7 +83,7 @@ export function maybe_add_narrowed_messages(
             // Remove the elsewhere_messages from the message list since
             // they don't match the filter as per data from server.
             msg_list.remove_and_rerender(elsewhere_messages.map((msg) => msg.id));
-            msg_list.add_messages(new_messages);
+            msg_list.add_messages(new_messages, {messages_are_new});
             unread_ops.process_visible();
             compose_notifications.notify_messages_outside_current_search(elsewhere_messages);
         },
@@ -111,7 +112,7 @@ export function maybe_add_narrowed_messages(
                 if (msg_list === message_lists.current) {
                     // Don't actually try again if we un-narrowed
                     // while waiting
-                    maybe_add_narrowed_messages(messages, msg_list, attempt + 1);
+                    maybe_add_narrowed_messages(messages, msg_list, messages_are_new, attempt + 1);
                 }
             }, delay);
         },