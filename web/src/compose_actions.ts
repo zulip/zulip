zulip/zulip

commit b547dcd6431142a742f8812b649b9965bdff5e48

compose_banner: Flash only once for exceeded length.
https://chat.zulip.org/#narrow/channel/101-design/topic/invalid.20message.20border.20flashing/near/2094736

web/src/compose_validate.ts
@@ -890,7 +890,8 @@ export function validate_message_length($container: JQuery, trigger_flash = true
     if (message_too_long_for_compose) {
         if (trigger_flash) {
             $textarea.addClass("flash");
-            setTimeout(() => $textarea.removeClass("flash"), 1500);
+            // This must be synchronized with the `flash` CSS.
+            setTimeout(() => $textarea.removeClass("flash"), 500);
         }
         return false;
     }

web/styles/compose.css
@@ -358,7 +358,8 @@
 .edit-content-container:has(
         .message_edit_content.textarea-approaching-limit.flash
     ) {
-    animation: message-limit-flash 0.5s ease-in-out 3;
+    /* This should align with the timing in compose_validate.validate_message_length. */
+    animation: message-limit-flash 0.5s ease-in-out 1;
 }
 
 #message-content-container .composebox-buttons {

commit 9310fd5a991366e4048f345eb8915184fd55b12a

compose_validate: Remove on hover message validation.
This gets rid of the previous approach of
validating a compose message on hovering
the compose send button, which caused
numerous bugs.

We now validate the compose message to enable
or disable the send button based on the flags
and populate the send button tooltip content
for 6 main events:

1. After a file has been uploaded.

This is done because the send button
status is manipulated during file uploads.
So we would want to re-evaluate all the
flags that determine whether to disable the
send button and the appropriate tooltip.

This fixes https://chat.zulip.org/#narrow/channel/9-issues/topic/.F0.9F.8E.AFZulip.20cloud.20send.20button.20disabled.20after.20uploading.20image/with/2123793.

2. On updating recipient.

This is done to run the checks again
when we add/remove any recipient.
The tooltip we are concerned with is
the "Please specify a valid recipient",
which is generated for no recipients.
This also helps to determine tooltip to
show in realms with mandatory topics, in
case the user changed from some topic to an
empty recipient box.

3. On switching message type.

This is a crucial event where
we should re-evaluate the validity.
One example I noticed is having an
empty recipient input for stream
messages for a realm which doesn't
require a topic name and then
switching to Direct Message type.
This must disable the button
and populate the appropriate tooltip
from the previously enabled state.

4. Every keystroke in the compose box textarea.

This is done to deal with the class of
issues where the user might be hovering
over the compose send button while typing
text, as well as generally toggling the
send button's disabled state.
We should disable the button if the textarea
becomes empty or exceeds the acceptable realm
message length, which is why we run the
validation checks in `check_overflow_text`.
See https://chat.zulip.org/#narrow/channel/9-issues/topic/.F0.9F.8E.AF.F0.9F.9B.A0.EF.B8.8F.20incorrect.20hover.20behavior.20on.20.22send.22.20button/near/2123670.

5. On initializing the reply box.
I introduced this because of some
inconsistencies of tooltip content
on login where the composebox
is open by default.

6. When you clear the topic using
the `X` button.

This tries to fix the regressions caused
by #32362 and the lack of clarity regarding

web/src/compose_actions.ts
@@ -460,6 +460,7 @@ export let cancel = (): void => {
         }
         return;
     }
+    compose_validate.check_compose_content_validity_and_adjust_send_button_tooltip();
     hide_box();
     clear_box();
     compose_banner.clear_message_sent_banners();

web/src/compose_recipient.ts
@@ -119,6 +119,7 @@ export function update_on_recipient_change(): void {
     compose_validate.warn_if_guest_in_dm_recipient();
     drafts.update_compose_draft_count();
     check_posting_policy_for_compose_box();
+    compose_validate.check_compose_content_validity_and_adjust_send_button_tooltip();
 }
 
 export function get_posting_policy_error_message(): string {
@@ -179,6 +180,7 @@ function switch_message_type(message_type: MessageType): void {
     update_compose_for_message_type(opts);
     update_compose_area_placeholder_text();
     compose_ui.set_focus(opts);
+    compose_validate.check_compose_content_validity_and_adjust_send_button_tooltip();
 }
 
 function update_recipient_label(stream_id?: number): void {

web/src/compose_reply.ts
@@ -11,6 +11,7 @@ import * as compose_paste from "./compose_paste.ts";
 import * as compose_recipient from "./compose_recipient.ts";
 import * as compose_state from "./compose_state.ts";
 import * as compose_ui from "./compose_ui.ts";
+import * as compose_validate from "./compose_validate.ts";
 import * as copy_messages from "./copy_messages.ts";
 import * as hash_util from "./hash_util.ts";
 import {$t} from "./i18n.ts";
@@ -432,4 +433,5 @@ export function initialize(): void {
     $("body").on("click", ".compose_reply_button", () => {
         respond_to_message({trigger: "reply button"});
     });
+    compose_validate.check_compose_content_validity_and_adjust_send_button_tooltip();
 }

web/src/compose_setup.js
@@ -71,14 +71,6 @@ export function initialize() {
         compose_ui.handle_keyup(event, $("textarea#compose-textarea").expectOne());
     });
 
-    $("#compose-send-button").on("mouseenter", () => {
-        compose_validate.validate(false, false);
-        compose_validate.update_send_button_status();
-    });
-    $("#compose-send-button").on("mouseleave", () => {
-        $("#compose-send-button").removeClass("disabled-message-send-controls");
-    });
-
     $("textarea#compose-textarea").on("input propertychange", () => {
         if ($("#compose").hasClass("preview_mode")) {
             compose.render_preview_area();
@@ -606,6 +598,7 @@ export function initialize() {
         const $input = $("input#stream_message_recipient_topic");
         $input.val("");
         $input.trigger("focus");
+        compose_validate.check_compose_content_validity_and_adjust_send_button_tooltip();
     });
 
     $("input#stream_message_recipient_topic").on("focus", () => {

web/src/compose_tooltips.ts
@@ -228,6 +228,9 @@ export function initialize(): void {
             }
             return undefined;
         },
+        onHidden(instance) {
+            instance.destroy();
+        },
     });
 
     tippy.delegate("body", {

web/src/compose_validate.ts
@@ -1,5 +1,6 @@
 import $ from "jquery";
 import _ from "lodash";
+import type * as tippy from "tippy.js";
 
 import * as resolved_topic from "../shared/src/resolved_topic.ts";
 import render_compose_banner from "../templates/compose_banner/compose_banner.hbs";
@@ -29,9 +30,11 @@ import * as stream_data from "./stream_data.ts";
 import * as sub_store from "./sub_store.ts";
 import type {StreamSubscription} from "./sub_store.ts";
 import type {UserOrMention} from "./typeahead_helper.ts";
+import {parse_html} from "./ui_util.ts";
 import {toggle_user_group_info_popover} from "./user_group_popover.ts";
 import * as user_groups from "./user_groups.ts";
 import type {UserGroup} from "./user_groups.ts";
+import {user_settings} from "./user_settings.ts";
 import * as util from "./util.ts";
 
 let user_acknowledged_stream_wildcard = false;
@@ -67,6 +70,10 @@ export let wildcard_mention_threshold = 15;
 
 export function set_upload_in_progress(status: boolean): void {
     upload_in_progress = status;
+    if (!upload_in_progress) {
+        check_compose_content_validity_and_adjust_send_button_tooltip();
+        return;
+    }
     update_send_button_status();
 }
 
@@ -928,10 +935,47 @@ export function check_overflow_text($container: JQuery): number {
             set_message_too_long_for_compose(false);
         }
     }
-
+    // update the flags on every keystroke and disable the
+    // send button based on the state of the validity flags.
+    check_compose_content_validity_and_adjust_send_button_tooltip();
     return text.length;
 }
 
+export let check_compose_content_validity_and_adjust_send_button_tooltip = function (): void {
+    // We call this in a setTimeout to defer execution
+    // because of the faulty behavior on page reload,
+    // where the flags are set incorrectly.
+    // A reproducer for this is having a recipient in the
+    // pill-container and reloading the page.
+    // The tooltip content asks you to enter a
+    // valid recipient, which is faulty.
+    setTimeout(() => {
+        const is_valid = validate(false, false);
+        update_send_button_status();
+
+        const $send_button = $("#compose-send-button");
+        const element: tippy.ReferenceElement = util.the($send_button);
+        const instance = element._tippy;
+        if (!instance) {
+            return;
+        }
+        if (!is_valid) {
+            instance.setContent(get_disabled_send_tooltip());
+        } else {
+            if (user_settings.enter_sends) {
+                instance.setContent(parse_html($("#send-enter-tooltip-template").html()));
+            } else {
+                instance.setContent(parse_html($("#send-ctrl-enter-tooltip-template").html()));
+            }
+        }
+    }, 0);
+};
+
+export function rewire_check_compose_content_validity_and_adjust_send_button_tooltip(
+    value: typeof check_compose_content_validity_and_adjust_send_button_tooltip,
+): void {
+    check_compose_content_validity_and_adjust_send_button_tooltip = value;
+}
 export function validate_message_length($container: JQuery, trigger_flash = true): boolean {
     const $textarea = $container.find<HTMLTextAreaElement>(".message-textarea");
     // Match the behavior of compose_state.message_content of trimming trailing whitespace

web/tests/compose.test.cjs
@@ -12,6 +12,7 @@ const $ = require("./lib/zjquery.cjs");
 const {page_params} = require("./lib/zpage_params.cjs");
 
 const user_groups = zrequire("user_groups");
+const compose_validate = zrequire("compose_validate");
 
 set_global("document", {
     querySelector() {},
@@ -337,7 +338,11 @@ test_ui("send_message", ({override, override_rewire, mock_template}) => {
             success(payload);
             stub_state.send_msg_called += 1;
         });
-
+        override_rewire(
+            compose_validate,
+            "check_compose_content_validity_and_adjust_send_button_tooltip",
+            noop,
+        );
         override_rewire(echo, "reify_message_id", (local_id, message_id) => {
             assert.equal(typeof local_id, "string");
             assert.equal(typeof message_id, "number");
@@ -637,6 +642,12 @@ test_ui("update_fade", ({override, override_rewire}) => {
     override_rewire(compose_recipient, "update_narrow_to_recipient_visibility", () => {
         update_narrow_to_recipient_visibility_called = true;
     });
+    override_rewire(
+        compose_validate,
+        "check_compose_content_validity_and_adjust_send_button_tooltip",
+        noop,
+    );
+
     override_rewire(drafts, "update_compose_draft_count", noop);
     override(compose_pm_pill, "get_user_ids", () => []);
 

web/tests/compose_actions.test.cjs
@@ -9,6 +9,7 @@ const blueslip = require("./lib/zblueslip.cjs");
 const $ = require("./lib/zjquery.cjs");
 
 const user_groups = zrequire("user_groups");
+const compose_validate = zrequire("compose_validate");
 
 const nobody = {
     name: "role:nobody",
@@ -162,6 +163,12 @@ test("start", ({override, override_rewire, mock_template}) => {
 
     override_rewire(compose_recipient, "on_compose_select_recipient_update", noop);
     override_rewire(compose_recipient, "check_posting_policy_for_compose_box", noop);
+    override_rewire(
+        compose_validate,
+        "check_compose_content_validity_and_adjust_send_button_tooltip",
+        noop,
+    );
+
     override_rewire(stream_data, "can_post_messages_in_stream", () => true);
     mock_template("inline_decorated_stream_name.hbs", false, noop);
 
@@ -311,6 +318,12 @@ test("respond_to_message", ({override, override_rewire, mock_template}) => {
 
     override_rewire(compose_recipient, "on_compose_select_recipient_update", noop);
     override_rewire(compose_recipient, "check_posting_policy_for_compose_box", noop);
+    override_rewire(
+        compose_validate,
+        "check_compose_content_validity_and_adjust_send_button_tooltip",
+        noop,
+    );
+
     override_private_message_recipient({override});
     mock_template("inline_decorated_stream_name.hbs", false, noop);
 
@@ -495,6 +508,11 @@ test("quote_message", ({disallow, override, override_rewire}) => {
             assert.equal(topic, "");
         }
     });
+    override_rewire(
+        compose_validate,
+        "check_compose_content_validity_and_adjust_send_button_tooltip",
+        noop,
+    );
 
     $("textarea#compose-textarea").caret = noop;
     $("textarea#compose-textarea").attr("id", "compose-textarea");

web/tests/compose_validate.test.cjs
@@ -531,11 +531,15 @@ test_ui("test_stream_posting_permission", ({mock_template, override}) => {
     assert.ok(!banner_rendered);
 });
 
-test_ui("test_check_overflow_text", ({override}) => {
+test_ui("test_check_overflow_text", ({override, override_rewire}) => {
     const fake_compose_box = new FakeComposeBox();
 
     override(realm, "max_message_length", 10000);
-
+    override_rewire(
+        compose_validate,
+        "check_compose_content_validity_and_adjust_send_button_tooltip",
+        noop,
+    );
     // RED
     {
         fake_compose_box.set_textarea_val("a".repeat(10005));

web/tests/upload.test.cjs
@@ -28,6 +28,9 @@ mock_esm("@uppy/tus", {default: class Tus {}});
 const compose_actions = mock_esm("../src/compose_actions");
 const compose_reply = mock_esm("../src/compose_reply");
 const compose_state = mock_esm("../src/compose_state");
+const compose_validate = mock_esm("../src/compose_validate", {
+    set_upload_in_progress: noop,
+});
 const rows = mock_esm("../src/rows");
 
 const compose_ui = zrequire("compose_ui");
@@ -248,9 +251,9 @@ test("upload_files", async ({mock_template, override, override_rewire}) => {
         banner_shown = true;
         return "<banner-stub>";
     });
-    override(compose_state, "get_message_type", () => "stream");
+    override(compose_validate, "set_upload_in_progress", () => {});
     await upload.upload_files(uppy, config, files);
-    assert.ok($("#compose-send-button").hasClass("disabled-message-send-controls"));
+    assert.ok(!$("#compose-send-button").hasClass("disabled-message-send-controls"));
     assert.ok(banner_shown);
     assert.ok(compose_ui_insert_syntax_and_focus_called);
     assert.ok(compose_ui_autosize_textarea_called);
@@ -456,7 +459,7 @@ test("copy_paste", ({override, override_rewire}) => {
     assert.equal(upload_files_called, false);
 });
 
-test("uppy_events", ({override, override_rewire, mock_template}) => {
+test("uppy_events", ({override_rewire, mock_template}) => {
     $("#compose_banners .upload_banner .moving_bar").css = noop;
     $("#compose_banners .upload_banner").length = 0;
     override_rewire(compose_ui, "smart_insert_inline", noop);
@@ -518,7 +521,6 @@ test("uppy_events", ({override, override_rewire, mock_template}) => {
     override_rewire(compose_ui, "autosize_textarea", () => {
         compose_ui_autosize_textarea_called = true;
     });
-    override(compose_state, "get_message_type", () => "stream");
     on_upload_success_callback(file, response);
 
     assert.ok(compose_ui_replace_syntax_called);