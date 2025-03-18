a3c1b6a3262799912d36ce239e128a8d68a027df
Prakhar Pratyush <prakhar@zulip.com>
2025-03-18 15:37:46 +0530

    version: Bump API_FEATURE_LEVEL.
    This change was missed in 5f3896710f10d8600ece389e37b12e51b5bcde71.

--- version.py
+++ version.py
@@ -34,7 +34,7 @@ DESKTOP_WARNING_VERSION = "5.9.3"
 # new level means in api_docs/changelog.md, as well as "**Changes**"
 # entries in the endpoint's documentation in `zulip.yaml`.
 
-API_FEATURE_LEVEL = 368
+API_FEATURE_LEVEL = 369  # Last bumped for adding `navigation_tour_video_url`.
 
 # Bump the minor PROVISION_VERSION to indicate that folks should provision
 # only when going from an old version of the code to a newer version. Bump
