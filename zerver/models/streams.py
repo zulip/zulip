b9077e8c3f4076fc7bd589af4b1d419dfbd90170
Aditya Kumar Kasaudhan <74228301+Aditya8840@users.noreply.github.com>
2025-03-21 04:41:02 +0530

    streams: Add new can_unsubscribe_group permission setting.
    Fixes: #33413.

--- zerver/models/streams.py
+++ zerver/models/streams.py
@@ -141,6 +141,9 @@ class Stream(models.Model):
         UserGroup, on_delete=models.RESTRICT, related_name="+"
     )
     can_subscribe_group = models.ForeignKey(UserGroup, on_delete=models.RESTRICT, related_name="+")
+    can_unsubscribe_group = models.ForeignKey(
+        UserGroup, on_delete=models.RESTRICT, related_name="+"
+    )
 
     # The very first message ID in the stream.  Used to help clients
     # determine whether they might need to display "show all topics" for a
@@ -187,6 +190,13 @@ class Stream(models.Model):
             allow_nobody_group=True,
             allow_everyone_group=False,
             default_group_name=SystemGroups.NOBODY,
+        ),
+        "can_unsubscribe_group": GroupPermissionSetting(
+            require_system_group=False,
+            allow_internet_group=False,
+            allow_nobody_group=True,
+            allow_everyone_group=True,
+            default_group_name=SystemGroups.EVERYONE,
         ),
     }
 
@@ -250,6 +260,7 @@ class Stream(models.Model):
         "can_send_message_group_id",
         "can_remove_subscribers_group_id",
         "can_subscribe_group_id",
+        "can_unsubscribe_group_id",
         "is_recently_active",
     ]
 
