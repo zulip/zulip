d71423d5388f99f40be1db46878b123aec324b42
Alex Vandiver <alexmv@zulip.com>
2025-03-20 15:45:42 +0000

    message: Move join to recipients outside of LIMIT, via CTE.
    (cherry picked from commit 70ed1ee46c2b70cfae06286989d5250927e1f7f4)

--- zerver/lib/message.py
+++ zerver/lib/message.py
@@ -6,9 +6,10 @@ from typing import Any, TypedDict
 
 from django.conf import settings
 from django.db import connection
-from django.db.models import Exists, Max, OuterRef, QuerySet, Sum
+from django.db.models import Exists, F, Max, OuterRef, QuerySet, Sum
 from django.utils.timezone import now as timezone_now
 from django.utils.translation import gettext as _
+from django_cte import With
 from psycopg2.sql import SQL
 
 from analytics.lib.counts import COUNT_STATS
@@ -845,14 +846,17 @@ def get_raw_unread_data(
         .exclude(
             message__recipient_id__in=excluded_recipient_ids,
         )
+        .annotate(
+            recipient_id=F("message__recipient_id"),
+            sender_id=F("message__sender_id"),
+            topic=F(MESSAGE__TOPIC),
+        )
         .values(
             "message_id",
-            "message__sender_id",
-            MESSAGE__TOPIC,
-            "message__recipient_id",
-            "message__recipient__type",
-            "message__recipient__type_id",
+            "sender_id",
+            "topic",
             "flags",
+            "recipient_id",
         )
         .order_by("-message_id")
     )
@@ -867,8 +871,34 @@ def get_raw_unread_data(
             where=[UserMessage.where_unread()],
         )
 
-    # Limit unread messages for performance reasons.
-    user_msgs = list(user_msgs[:MAX_UNREAD_MESSAGES])
+    # Limit unread messages for performance reasons.  We do this
+    # inside a CTE, such that the join to Recipients, below, can't be
+    # implied to remove rows, and thus allows a Nested Loop join,
+    # potentially memoized to reduce the number of Recipient lookups.
+    cte = With(user_msgs[:MAX_UNREAD_MESSAGES])
+
+    user_msgs = (
+        cte.join(Recipient, id=cte.col.recipient_id)
+        .with_cte(cte)
+        .annotate(
+            message_id=cte.col.message_id,
+            sender_id=cte.col.sender_id,
+            recipient_id=cte.col.recipient_id,
+            topic=cte.col.topic,
+            flags=cte.col.flags,
+            recipient__type=F("type"),
+            recipient__type_id=F("type_id"),
+        )
+        .values(
+            "message_id",
+            "sender_id",
+            "topic",
+            "flags",
+            "recipient_id",
+            "recipient__type",
+            "recipient__type_id",
+        )
+    )
 
     rows = list(reversed(user_msgs))
     return extract_unread_data_from_um_rows(rows, user_profile)
@@ -938,13 +968,13 @@ def extract_unread_data_from_um_rows(
     for row in rows:
         total_unreads += 1
         message_id = row["message_id"]
-        msg_type = row["message__recipient__type"]
-        recipient_id = row["message__recipient_id"]
-        sender_id = row["message__sender_id"]
+        msg_type = row["recipient__type"]
+        recipient_id = row["recipient_id"]
+        sender_id = row["sender_id"]
 
         if msg_type == Recipient.STREAM:
-            stream_id = row["message__recipient__type_id"]
-            topic_name = row[MESSAGE__TOPIC]
+            stream_id = row["recipient__type_id"]
+            topic_name = row["topic"]
             stream_dict[message_id] = dict(
                 stream_id=stream_id,
                 topic=topic_name,
@@ -954,7 +984,7 @@ def extract_unread_data_from_um_rows(
 
         elif msg_type == Recipient.PERSONAL:
             if sender_id == user_profile.id:
-                other_user_id = row["message__recipient__type_id"]
+                other_user_id = row["recipient__type_id"]
             else:
                 other_user_id = sender_id
 
@@ -980,8 +1010,8 @@ def extract_unread_data_from_um_rows(
             mentions.add(message_id)
         if is_stream_wildcard_mentioned or is_topic_wildcard_mentioned:
             if msg_type == Recipient.STREAM:
-                stream_id = row["message__recipient__type_id"]
-                topic_name = row[MESSAGE__TOPIC]
+                stream_id = row["recipient__type_id"]
+                topic_name = row["topic"]
                 if not is_row_muted(stream_id, recipient_id, topic_name):
                     mentions.add(message_id)
             else:  # nocoverage # TODO: Test wildcard mentions in direct messages.
