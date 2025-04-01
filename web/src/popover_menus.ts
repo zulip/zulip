426b97a2e5f3072c2d9a2526dc046506951e3889
Kislay Verma <kislayuv27@gmail.com>
2025-04-01 13:48:50 +0530

    popover_menus: Fix bug in user card shortcut.
    When hovering over the user avatar, the tooltip
    "View user card" appears. If this tooltip appears
    over the currently selected message, then pressing
    `u` once doesn't show the user card popover, rather
    it only hides the tooltip. It is on the second press of `u`
    that the user card appears.
    
    This commit fixes that.
    
    We also add some comments to help clarify this code path.

--- web/src/popover_menus.ts
+++ web/src/popover_menus.ts
@@ -418,6 +418,12 @@ export function toggle_popover_menu(
 ): tippy.Instance {
     const instance = target._tippy;
     if (instance) {
+        // Ideally, we'd check that the _tippy object is a
+        // popover. For elements that host both a Tippy tooltip and a
+        // popover, this can incorrectly return early after hiding the
+        // Tippy tooltip.
+        //
+        // If we fix this, we can remove a few popovers.hide_all calls.
         hide_current_popover_if_visible(instance);
         return instance;
     }
