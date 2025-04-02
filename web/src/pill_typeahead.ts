d11537a8cd6a179e52090dfaf4902bad60df26fe
Shubham Padia <shubham@zulip.com>
2025-04-02 13:27:11 +0000

    streams: Change sorting order for add subscribers.
    See
    https://chat.zulip.org/#narrow/channel/101-design/topic/New.20channel.2Fgroup.20UX.20changes/with/2142992
    for more details on the order.
    We decided to have a common function called `sort_setting_options` which
    accepts a compare function for `group` and `stream` settings each.

--- web/src/pill_typeahead.ts
+++ web/src/pill_typeahead.ts
@@ -264,6 +264,7 @@ export function set_up_combined(
         user_group_source?: () => UserGroup[];
         exclude_bots?: boolean;
         update_func?: () => void;
+        is_stream_subscriber_input?: boolean;
     },
 ): void {
     if (!opts.user && !opts.user_group && !opts.stream) {
@@ -382,6 +383,13 @@ export function set_up_combined(
                 }
             }
 
+            if (opts.is_stream_subscriber_input) {
+                return typeahead_helper.sort_stream_setting_options({
+                    users,
+                    query,
+                    groups,
+                });
+            }
             return typeahead_helper.sort_recipients({
                 users,
                 query,
