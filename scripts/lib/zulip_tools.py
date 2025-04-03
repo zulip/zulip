4b11b7903de72f1f88af4ec714b1b83ea7384e23
Alex Vandiver <alexmv@zulip.com>
2025-04-03 14:31:49 +0000

    upgrade: Remove tsearch_extras cleanup code.
    This code was originally added in 382261dc729a in Zulip Server 3.0;
    since we can only directly upgrade from 5.0 or later, this code is
    guaranteed to have run already. Remove it.

--- scripts/lib/zulip_tools.py
+++ scripts/lib/zulip_tools.py
@@ -626,15 +626,6 @@ def get_deploy_options(config_file: configparser.RawConfigParser) -> list[str]:
     return shlex.split(get_config(config_file, "deployment", "deploy_options", ""))
 
 
-def run_psql_as_postgres(
-    config_file: configparser.RawConfigParser,
-    sql_query: str,
-) -> None:
-    dbname = get_config(config_file, "postgresql", "database_name", "zulip")
-    subcmd = shlex.join(["psql", "-v", "ON_ERROR_STOP=1", "-d", dbname, "-c", sql_query])
-    subprocess.check_call(["su", "postgres", "-c", subcmd])
-
-
 def get_tornado_ports(config_file: configparser.RawConfigParser) -> list[int]:
     ports = []
     if config_file.has_section("tornado_sharding"):
