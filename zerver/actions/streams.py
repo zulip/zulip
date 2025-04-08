7970e1fa451354c3ab9959b9ce3ef40d4f9cb49c
Prakhar Pratyush <prakhar@zulip.com>
2025-04-08 16:05:56 +0530

    streams: Fix error while sending notice in deactivated stream.
    Sending messages to a deactivated stream is not allowed with
    the exception of notices sent in "channel events" topic.
    
    Earlier, notice sent to a deactivated stream when it is
    deactivated was working correctly but it was resulting in
    an error in the following cases:
    * Renaming stream
    * Changing stream description
    * Changing message retention period
    * Changing posting permission
    * Changing access permission
    
    This commit makes sure to send notice successfully in those cases.

--- zerver/actions/streams.py
+++ zerver/actions/streams.py
@@ -1237,7 +1237,11 @@ def send_change_stream_permission_notification(
             new_policy=new_policy_name,
         )
         internal_send_stream_message(
-            sender, stream, str(Realm.STREAM_EVENTS_NOTIFICATION_TOPIC_NAME), notification_string
+            sender,
+            stream,
+            str(Realm.STREAM_EVENTS_NOTIFICATION_TOPIC_NAME),
+            notification_string,
+            archived_channel_notice=stream.deactivated,
         )
 
 
@@ -1467,7 +1471,11 @@ def send_stream_posting_permission_update_notification(
             new_setting_description=new_setting_description,
         )
         internal_send_stream_message(
-            sender, stream, str(Realm.STREAM_EVENTS_NOTIFICATION_TOPIC_NAME), notification_string
+            sender,
+            stream,
+            str(Realm.STREAM_EVENTS_NOTIFICATION_TOPIC_NAME),
+            notification_string,
+            archived_channel_notice=stream.deactivated,
         )
 
 
@@ -1526,6 +1534,7 @@ def do_rename_stream(stream: Stream, new_name: str, user_profile: UserProfile) -
                 old_channel_name=f"**{old_name}**",
                 new_channel_name=f"**{new_name}**",
             ),
+            archived_channel_notice=stream.deactivated,
         )
 
 
@@ -1554,7 +1563,11 @@ def send_change_stream_description_notification(
         )
 
         internal_send_stream_message(
-            sender, stream, str(Realm.STREAM_EVENTS_NOTIFICATION_TOPIC_NAME), notification_string
+            sender,
+            stream,
+            str(Realm.STREAM_EVENTS_NOTIFICATION_TOPIC_NAME),
+            notification_string,
+            archived_channel_notice=stream.deactivated,
         )
 
 
@@ -1644,7 +1657,11 @@ def send_change_stream_message_retention_days_notification(
             summary_line=summary_line,
         )
         internal_send_stream_message(
-            sender, stream, str(Realm.STREAM_EVENTS_NOTIFICATION_TOPIC_NAME), notification_string
+            sender,
+            stream,
+            str(Realm.STREAM_EVENTS_NOTIFICATION_TOPIC_NAME),
+            notification_string,
+            archived_channel_notice=stream.deactivated,
         )
 
 
