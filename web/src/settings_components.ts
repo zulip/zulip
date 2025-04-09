zulip/zulip

commit f496bd6350f74717906cf66fa65f3b2c872dd24b

settings: Remove unused "saved" state in save discard widget.

web/src/settings_components.ts
@@ -574,10 +574,6 @@ export function change_save_button_state($element: JQuery, state: string): void 
 
             $element.find(".discard-button").show();
             break;
-        case "saved":
-            data_status = "";
-            is_show = false;
-            break;
         case "saving":
             // We don't change the button text on the saving
             // state to avoid changing the button size while

web/tests/settings_org.test.cjs
@@ -189,12 +189,6 @@ function test_change_save_button_state() {
         assert.equal(props.hidden, true);
     }
     {
-        settings_components.change_save_button_state($save_button_controls, "saved");
-        assert.equal($save_button_text.text(), "translated: Save changes");
-        assert.equal(props.hidden, true);
-        assert.equal($save_button.attr("data-status"), "");
-    }
-    {
         settings_components.change_save_button_state($save_button_controls, "saving");
         assert.equal($save_button.attr("data-status"), "saving");
         assert.equal($discard_button.visible(), false);

commit 6a607e5b73b9ac99a2510c8c3ea7bd96aab00542

settings: Prevent save discard widget override.
This commit prevents the save button in the save discard widget from
showing the "Saved" label when the user has made some other changes
in the settings while the saving process was in action — which resulted
in the "Save changes" label in the save button, and thus shouldn't be
replaced with "Saved".

This commit also fixes the failing puppeteer tests in
`web/e2e-tests/admin.test.ts` which was introduced in #34081.

web/src/settings_components.ts
@@ -560,6 +560,13 @@ export function change_save_button_state($element: JQuery, state: string): void 
         return;
     }
 
+    if (state === "succeeded" && $save_button.attr("data-status") === "unsaved") {
+        // We don't show the "saved" state if the save button is in the "unsaved"
+        // state, as that would indicate that user has made some other changes
+        // during the saving process.
+        return;
+    }
+
     if (state !== "saving") {
         buttons.hide_button_loading_indicator($save_button);
     }