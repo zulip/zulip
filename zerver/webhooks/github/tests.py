3d6fa208cfd5cfda295006b74dcc97ae50e092e2
lsanleo <leonardo.santolin@itsdigitalacademy.com>
2025-04-10 11:43:49 +0200

    fix tests.py

--- zerver/webhooks/github/tests.py
+++ zerver/webhooks/github/tests.py
@@ -739,30 +739,3 @@ A temporary team so that I can get some webhook fixtures!
             TOPIC_SPONSORS,
             expected_message,
         )
-
-    def test_pull_request_approved(self) -> None:
-        expected_message = "**username** approved [PR #123](https://github.com/org/repo/pull/123)"
-        expected_topic = "repo / PR #123 Test PR"
-        self.check_webhook("pull_request__approved", expected_topic, expected_message)
-
-    def test_pull_request_converted_to_draft(self) -> None:
-        expected_message = (
-            "**username** converted [PR #123](https://github.com/org/repo/pull/123) to draft"
-        )
-        expected_topic = "repo / PR #123 Test PR"
-        self.check_webhook("pull_request__converted_to_draft", expected_topic, expected_message)
-
-    def test_pull_request_labeled(self) -> None:
-        expected_message = "[username](https://github.com/username) added the `bug` label to [PR #123](https://github.com/org/repo/pull/123)"
-        expected_topic = "repo / PR #123 Test PR"
-        self.check_webhook("pull_request__labeled", expected_topic, expected_message)
-
-    def test_pull_request_review_request_removed(self) -> None:
-        expected_message = "**username** removed [reviewer](https://github.com/reviewer) as a reviewer from [PR #123](https://github.com/org/repo/pull/123)"
-        expected_topic = "repo / PR #123 Test PR"
-        self.check_webhook("pull_request__review_request_removed", expected_topic, expected_message)
-
-    def test_pull_request_milestoned(self) -> None:
-        expected_message = "[username](https://github.com/username) added milestone [v1.0](https://github.com/org/repo/milestone/1) to [PR #123](https://github.com/org/repo/pull/123)"
-        expected_topic = "repo / PR #123 Test PR"
-        self.check_webhook("pull_request__milestoned", expected_topic, expected_message)
