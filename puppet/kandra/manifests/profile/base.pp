6e1760b09c3f99b68c70aced22b8adf7a7f3d961
Alex Vandiver <alexmv@zulip.com>
2025-04-11 17:52:03 +0000

    kandra: Add a couple useful command-line tools.

--- puppet/kandra/manifests/profile/base.pp
+++ puppet/kandra/manifests/profile/base.pp
@@ -32,8 +32,14 @@ class kandra::profile::base {
     'bind9-host',
     'git',
     'nagios-plugins-contrib',
+    'ripgrep',
+    'bat',
   ]
   zulip::safepackage { $org_base_packages: ensure => installed }
+  file { '/usr/local/bin/bat':
+    ensure => link,
+    target => '/usr/bin/batcat',
+  }
 
   # Uninstall the AWS kernel, but only after we install the usual one
   package { ['linux-image-aws', 'linux-headers-aws', 'linux-aws-*', 'linux-image-*-aws', 'linux-modules-*-aws']:
