ed01b212b41661dda9047eb33d2e4ddc712cd201
Sayam Samal <sayam@zulip.com>
2025-04-30 15:13:29 +0530

    blueslip: Move blueslip related code to its own files.

--- web/src/alert_popup.ts
+++ web/src/alert_popup.ts
@@ -8,15 +8,3 @@ $("body").on("click", ".alert-box .exit", function () {
         $alert.removeClass("fade-out show");
     }, 300);
 });
-
-$(".blueslip-error-container").on("click", ".stackframe", function () {
-    $(this).siblings(".code-context").toggle("fast");
-});
-
-$(".blueslip-error-container").on("click", ".exit", function () {
-    const $stacktrace = $(this).closest(".stacktrace");
-    $stacktrace.addClass("fade-out");
-    setTimeout(() => {
-        $stacktrace.removeClass("fade-out show");
-    }, 300);
-});
