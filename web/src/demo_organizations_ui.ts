33eb4a2039d9196f2dc37e67f75c311eec710048
Lauryn Menard <lauryn@zulip.com>
2025-04-30 17:12:21 +0200

    demo-orgs: Use banners for org settings demo organization warning.
    Migrate the demo organization warning that's shown on all the tabs
    for organization settings to use the shared banner code.
    
    Updates links in the current warning to be buttons in the banner,
    and matches the navbar alert banner for demo organizations.
    
    Fixes #34447.

--- web/src/demo_organizations_ui.ts
+++ web/src/demo_organizations_ui.ts
@@ -3,12 +3,13 @@ import assert from "minimalistic-assert";
 import {z} from "zod";
 
 import render_convert_demo_organization_form from "../templates/settings/convert_demo_organization_form.hbs";
-import render_demo_organization_warning from "../templates/settings/demo_organization_warning.hbs";
+import render_demo_organization_warning_container from "../templates/settings/demo_organization_warning.hbs";
 
+import * as banners from "./banners.ts";
+import type {ActionButton} from "./buttons.ts";
 import * as channel from "./channel.ts";
 import * as dialog_widget from "./dialog_widget.ts";
 import {$t} from "./i18n.ts";
-import * as keydown_util from "./keydown_util.ts";
 import * as settings_config from "./settings_config.ts";
 import * as settings_data from "./settings_data.ts";
 import * as settings_org from "./settings_org.ts";
@@ -26,13 +27,46 @@ export function get_demo_organization_deadline_days_remaining(): number {
 }
 
 export function insert_demo_organization_warning(): void {
-    const days_remaining = get_demo_organization_deadline_days_remaining();
-    const rendered_demo_organization_warning = render_demo_organization_warning({
-        is_demo_organization: realm.demo_organization_scheduled_deletion_date,
-        is_owner: current_user.is_owner,
-        days_remaining,
+    const demo_organization_warning_container = render_demo_organization_warning_container({
+        is_demo_organization: true,
     });
-    $(".organization-box").find(".settings-section").prepend($(rendered_demo_organization_warning));
+    $(".organization-box")
+        .find(".settings-section")
+        .prepend($(demo_organization_warning_container));
+    const days_remaining = get_demo_organization_deadline_days_remaining();
+    let buttons: ActionButton[] = [
+        {
+            attention: "borderless",
+            label: $t({defaultMessage: "Learn more"}),
+            custom_classes: "demo-organizations-help",
+        },
+    ];
+    if (current_user.is_owner) {
+        buttons = [
+            ...buttons,
+            {
+                attention: "quiet",
+                label: $t({defaultMessage: "Convert"}),
+                custom_classes: "convert-demo-organization",
+            },
+        ];
+    }
+    const demo_organization_warning_banner: banners.Banner = {
+        intent: days_remaining <= 7 ? "danger" : "info",
+        label: $t(
+            {
+                defaultMessage:
+                    "This demo organization will be automatically deleted in {days_remaining} days, unless it's converted into a permanent organization.",
+            },
+            {
+                days_remaining,
+            },
+        ),
+        buttons,
+        close_button: false,
+        custom_classes: "organization-settings-banner",
+    };
+    banners.append(demo_organization_warning_banner, $(".demo-organization-warning"));
 }
 
 export function do_convert_demo_organization(): void {
@@ -108,21 +142,13 @@ export function do_convert_demo_organization(): void {
 }
 
 export function handle_demo_organization_conversion(): void {
-    $(".convert-demo-organization-button").on("click", (e) => {
+    $(".demo-organization-warning").on("click", ".convert-demo-organization", (e) => {
         e.stopPropagation();
         e.preventDefault();
         do_convert_demo_organization();
     });
 
-    // Treat Enter with convert demo organization link as a click.
-    $(".demo-organization-warning").on(
-        "keyup",
-        ".convert-demo-organization-button[role=button]",
-        function (e) {
-            e.stopPropagation();
-            if (keydown_util.is_enter_event(e)) {
-                $(this).trigger("click");
-            }
-        },
-    );
+    $(".demo-organization-warning").on("click", ".demo-organizations-help", () => {
+        window.open("https://zulip.com/help/demo-organizations", "_blank", "noopener,noreferrer");
+    });
 }
