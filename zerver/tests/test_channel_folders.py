e93667cc061ad0550b10860138472725bef17157
Sahil Batra <sahil@zulip.com>
2025-05-07 12:14:13 +0530

    channel_folders: Add API endpoint to get all channel folders.
    Fixes part of #31972.

--- zerver/tests/test_channel_folders.py
+++ zerver/tests/test_channel_folders.py
@@ -1,5 +1,8 @@
+from typing import Any
+
 import orjson
 
+from zerver.actions.channel_folders import check_add_channel_folder
 from zerver.lib.test_classes import ZulipTestCase
 from zerver.models import ChannelFolder
 from zerver.models.realms import get_realm
@@ -70,3 +73,94 @@ class ChannelFolderCreationTest(ZulipTestCase):
         params = {"name": invalid_name, "description": "Channels for frontend discussions"}
         result = self.client_post("/json/channel_folders/create", params)
         self.assert_json_error(result, "Invalid character in channel folder name, at position 4.")
+
+
+class GetChannelFoldersTest(ZulipTestCase):
+    def test_get_channel_folders(self) -> None:
+        iago = self.example_user("iago")
+        desdemona = self.example_user("desdemona")
+        frontend_folder = check_add_channel_folder(
+            "Frontend", "Channels for frontend discussions", acting_user=iago
+        )
+        backend_folder = check_add_channel_folder(
+            "Backend", "Channels for **backend** discussions", acting_user=iago
+        )
+        marketing_folder = check_add_channel_folder(
+            "Marketing", "Channels for marketing discussions", acting_user=desdemona
+        )
+
+        lear_user = self.lear_user("cordelia")
+        check_add_channel_folder("Devops", "Channels for devops discussions", acting_user=lear_user)
+
+        def check_channel_folders_in_zulip_realm(
+            channel_folders: list[dict[str, Any]], marketing_folder_included: bool = True
+        ) -> None:
+            if marketing_folder_included:
+                self.assert_length(channel_folders, 3)
+            else:
+                self.assert_length(channel_folders, 2)
+
+            self.assertEqual(channel_folders[0]["id"], frontend_folder.id)
+            self.assertEqual(channel_folders[0]["name"], "Frontend")
+            self.assertEqual(channel_folders[0]["description"], "Channels for frontend discussions")
+            self.assertEqual(
+                channel_folders[0]["rendered_description"],
+                "<p>Channels for frontend discussions</p>",
+            )
+            self.assertEqual(channel_folders[0]["is_archived"], frontend_folder.is_archived)
+            self.assertEqual(channel_folders[0]["creator_id"], iago.id)
+
+            self.assertEqual(channel_folders[1]["id"], backend_folder.id)
+            self.assertEqual(channel_folders[1]["name"], "Backend")
+            self.assertEqual(
+                channel_folders[1]["description"], "Channels for **backend** discussions"
+            )
+            self.assertEqual(
+                channel_folders[1]["rendered_description"],
+                "<p>Channels for <strong>backend</strong> discussions</p>",
+            )
+            self.assertEqual(channel_folders[1]["is_archived"], backend_folder.is_archived)
+            self.assertEqual(channel_folders[1]["creator_id"], iago.id)
+
+            if marketing_folder_included:
+                self.assertEqual(channel_folders[2]["id"], marketing_folder.id)
+                self.assertEqual(channel_folders[2]["name"], "Marketing")
+                self.assertEqual(
+                    channel_folders[2]["description"], "Channels for marketing discussions"
+                )
+                self.assertEqual(
+                    channel_folders[2]["rendered_description"],
+                    "<p>Channels for marketing discussions</p>",
+                )
+                self.assertEqual(channel_folders[2]["is_archived"], marketing_folder.is_archived)
+                self.assertEqual(channel_folders[2]["creator_id"], desdemona.id)
+
+        self.login("iago")
+        result = self.client_get("/json/channel_folders")
+        channel_folders_data = orjson.loads(result.content)["channel_folders"]
+        check_channel_folders_in_zulip_realm(channel_folders_data)
+
+        # Check member user can also see all channel folders.
+        self.login("hamlet")
+        result = self.client_get("/json/channel_folders")
+        channel_folders_data = orjson.loads(result.content)["channel_folders"]
+        check_channel_folders_in_zulip_realm(channel_folders_data)
+
+        # Check guest can also see all channel folders.
+        self.login("polonius")
+        result = self.client_get("/json/channel_folders")
+        channel_folders_data = orjson.loads(result.content)["channel_folders"]
+        check_channel_folders_in_zulip_realm(channel_folders_data)
+
+        marketing_folder.is_archived = True
+        marketing_folder.save()
+
+        result = self.client_get("/json/channel_folders")
+        channel_folders_data = orjson.loads(result.content)["channel_folders"]
+        check_channel_folders_in_zulip_realm(channel_folders_data, False)
+
+        result = self.client_get(
+            "/json/channel_folders", {"include_archived": orjson.dumps(True).decode()}
+        )
+        channel_folders_data = orjson.loads(result.content)["channel_folders"]
+        check_channel_folders_in_zulip_realm(channel_folders_data)
--- zproject/urls.py
+++ zproject/urls.py
@@ -45,7 +45,7 @@ from zerver.views.auth import (
     start_social_login,
     start_social_signup,
 )
-from zerver.views.channel_folders import create_channel_folder
+from zerver.views.channel_folders import create_channel_folder, get_channel_folders
 from zerver.views.compatibility import check_global_compatibility
 from zerver.views.custom_profile_fields import (
     create_realm_custom_profile_field,
@@ -532,6 +532,7 @@ v1_api_and_json_patterns = [
         DELETE=remove_subscriptions_backend,
     ),
     rest_path("channel_folders/create", POST=create_channel_folder),
+    rest_path("channel_folders", GET=get_channel_folders),
     # topic-muting -> zerver.views.user_topics
     # (deprecated and will be removed once clients are migrated to use '/user_topics')
     rest_path("users/me/subscriptions/muted_topics", PATCH=update_muted_topic),
