ce8cd88d8ad79d6c01b86aceae4803670ea2b9ba
Vector73 <v.shm.kunal@gmail.com>
2025-05-10 11:34:52 +0000

    stream_settings: Add new `topics_policy` setting.
    Added `topics_policy` channel setting to configure sending
    messages in the empty topic.
    
    Fixes #33549.

--- zerver/lib/event_types.py
+++ zerver/lib/event_types.py
@@ -781,6 +781,7 @@ class BasicStreamFields(BaseModel):
     stream_id: int
     stream_post_policy: int
     stream_weekly_traffic: int | None
+    topics_policy: str
 
 
 class EventStreamCreate(BaseEvent):
@@ -852,6 +853,7 @@ class SingleSubscription(BaseModel):
     pin_to_top: bool
     push_notifications: bool | None
     subscribers: list[int]
+    topics_policy: str
     wildcard_mentions_notify: bool | None
 
 
