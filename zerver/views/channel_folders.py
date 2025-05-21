ffc0cfae3396955214c57056cf53988b3a07eba3
Sahil Batra <sahil@zulip.com>
2025-05-21 15:09:51 +0530

    folders: Don't allow archiving a folder if it contains channels.

--- zerver/views/channel_folders.py
+++ zerver/views/channel_folders.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
 from django.http import HttpRequest, HttpResponse
+from django.utils.translation import gettext as _
 from pydantic import Json, StringConstraints
 
 from zerver.actions.channel_folders import (
@@ -16,9 +17,11 @@ from zerver.lib.channel_folders import (
     get_channel_folder_by_id,
     get_channel_folders_in_realm,
 )
+from zerver.lib.exceptions import JsonableError
 from zerver.lib.response import json_success
 from zerver.lib.typed_endpoint import PathOnly, typed_endpoint
 from zerver.models.channel_folders import ChannelFolder
+from zerver.models.streams import Stream
 from zerver.models.users import UserProfile
 
 
@@ -71,6 +74,11 @@ def update_channel_folder(
 
     if is_archived is not None and channel_folder.is_archived != is_archived:
         if is_archived:
+            if Stream.objects.filter(folder=channel_folder).exists():
+                raise JsonableError(
+                    _("You need to remove all the channels from this folder to archive it.")
+                )
+
             do_archive_channel_folder(channel_folder, acting_user=user_profile)
         else:
             do_unarchive_channel_folder(channel_folder, acting_user=user_profile)
