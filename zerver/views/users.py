f533d64b6a76641e0ae8826d22ae828de08f17bf
PieterCK <pieterceka123@gmail.com>
2025-05-26 11:22:25 +0700

    actions: Refactor how `patch_bot_backend` update bot service.
    This updates `do_update_outgoing_webhook_service` to use
    `BotServicesOutgoing` for the service dict in the event because it's the
    schema `bot_data.update` expects.

--- zerver/views/users.py
+++ zerver/views/users.py
@@ -497,8 +497,6 @@ def patch_bot_backend(
         )
 
     if service_payload_url is not None:
-        check_valid_interface_type(service_interface)
-        assert service_interface is not None
         do_update_outgoing_webhook_service(bot, service_interface, service_payload_url)
 
     if config_data is not None:
