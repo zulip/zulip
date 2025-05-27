2f0acc2972b0120ce47c0cacd7881869d9aa333c
Shubham Padia <shubham@zulip.com>
2025-05-27 06:58:10 +0000

    icons: Let the system using the icons specify the fill-rule color.
    The fill-rule color is ignored when we generate webfonts in our main
    app. See
    https://zulip.readthedocs.io/en/latest/subsystems/icons.html#cleaning-up-the-svg-code
    for more details.

--- tools/linter_lib/custom_check.py
+++ tools/linter_lib/custom_check.py
@@ -975,6 +975,11 @@ svg_rules = RuleList(
             "description": "System icons ignore fill values, so do not include the fill property.",
             "include_only": {"web/shared/icons/", "web/images/icons/"},
         },
+        {
+            "pattern": r"fill-rule=(['\"])(.*?)\1",
+            "description": "System icons ignore fill-rule values, so do not include the fill-rule property.",
+            "include_only": {"web/shared/icons/", "web/images/icons/"},
+        },
     ],
 )
 
