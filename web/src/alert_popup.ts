f333f84c8a4ca5e5ae2189d464e88fb29956c307
Aman Agrawal <amanagr@zulip.com>
2025-05-27 11:22:18 +0530

    alert_popup: Fix stack trace not removed on clicking `x`.

--- web/src/alert_popup.ts
+++ web/src/alert_popup.ts
@@ -2,10 +2,10 @@ import $ from "jquery";
 
 // this will hide the alerts that you click "x" on.
 $("body").on("click", ".alert-box .exit", function () {
-    const $alert = $(this).parent("div");
-    $alert.addClass("fade-out");
+    const $stack_trace = $(this).closest(".stacktrace");
+    $stack_trace.addClass("fade-out");
     setTimeout(() => {
-        $alert.removeClass("fade-out show");
+        $stack_trace.removeClass("fade-out show");
     }, 300);
 });
 
