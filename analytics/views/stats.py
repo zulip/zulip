032f2ade740b666dbc8e4b112910146fdd90baa6
Lauryn Menard <lauryn@zulip.com>
2025-05-29 17:10:16 +0200

    analytics: Update labels for mobile apps for Flutter launch.
    Updates the labels in the"Messages set by client" chart so that
    the Flutter app is no longer labeled as "beta".
    
    The React Native app is now labeled as "old". And the older
    Android and iOS apps are now labeled as "ancient".

--- analytics/views/stats.py
+++ analytics/views/stats.py
@@ -599,13 +599,13 @@ def client_label_map(name: str) -> str:
     if name == "ZulipTerminal":
         return "Terminal app"
     if name == "ZulipAndroid":
-        return "Old Android app"
+        return "Ancient Android app"
     if name == "ZulipiOS":
-        return "Old iOS app"
+        return "Ancient iOS app"
     if name == "ZulipMobile":
-        return "Mobile app (React Native)"
+        return "Old mobile app (React Native)"
     if name in ["ZulipFlutter", "ZulipMobile/flutter"]:
-        return "Mobile app beta (Flutter)"
+        return "Mobile app (Flutter)"
     if name in ["ZulipPython", "API: Python"]:
         return "Python API"
     if name.startswith("Zulip") and name.endswith("Webhook"):
