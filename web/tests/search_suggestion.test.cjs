85dba4dceda03913f060843f13f440a2ca1bcb81
apoorvapendse <apoorvavpendse@gmail.com>
2025-05-30 17:19:58 +0530

    search_suggestion: Search topics from all subscribed channels.
    We search from all the subscribed channels if
    there is no channel operator.
    
    While listing the suggestions, priority is given to
    topics that are present in the currently
    narrowed channel.
    
    Fixes: #34557.
    
    Co-authored-by: Aman Vishwakarma <vishwakarmarambhawan572@gmail.com>
    Signed-off-by: apoorvapendse <apoorvavpendse@gmail.com>

--- web/tests/search_suggestion.test.cjs
+++ web/tests/search_suggestion.test.cjs
@@ -740,6 +740,51 @@ test("topic_suggestions", ({override, mock_template}) => {
         "topic:REXX",
     ];
     assert.deepEqual(suggestions.strings, expected);
+
+    suggestions = get_suggestions("topic:");
+    expected = ["topic:", "topic:ignore", `topic:team`, `topic:test`];
+    assert.deepEqual(suggestions.strings, expected);
+    override(narrow_state, "stream_id", () => "");
+
+    for (const topic_name of ["a", "b", "c", "trap", "talks", "tower"]) {
+        stream_topic_history.add_message({
+            stream_id: devel_id,
+            topic_name,
+        });
+    }
+
+    stream_data.subscribe_myself(stream_data.get_sub("devel"));
+    stream_data.subscribe_myself(stream_data.get_sub("office"));
+    suggestions = get_suggestions("topic:");
+    expected = [
+        "topic:",
+        "topic:REXX",
+        "topic:a",
+        "topic:b",
+        "topic:c",
+        "topic:ignore",
+        "topic:talks",
+        "topic:team",
+        "topic:test",
+        "topic:tower",
+        "topic:trap",
+    ];
+    assert.deepEqual(suggestions.strings, expected);
+
+    suggestions = get_suggestions("topic:t");
+    expected = ["topic:t", "topic:talks", "topic:team", "topic:test", "topic:tower", "topic:trap"];
+    assert.deepEqual(suggestions.strings, expected);
+
+    // Prioritize topics from currently narrowed channel
+    override(narrow_state, "stream_id", () => devel_id);
+    suggestions = get_suggestions("topic:t");
+    expected = ["topic:t", "topic:talks", "topic:tower", "topic:trap", "topic:team", "topic:test"];
+    assert.deepEqual(suggestions.strings, expected);
+
+    override(narrow_state, "stream_id", () => office_id);
+    suggestions = get_suggestions("topic:t");
+    expected = ["topic:t", "topic:team", "topic:test", "topic:talks", "topic:tower", "topic:trap"];
+    assert.deepEqual(suggestions.strings, expected);
 });
 
 test("topic_suggestions (limits)", () => {
