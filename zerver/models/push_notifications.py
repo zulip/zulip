4472047599b6b8831e6684937b5d935d7e98b07b
Prakhar Pratyush <prakhar@zulip.com>
2025-06-02 15:42:53 +0530

    zerver: Add `PushDevice` model.
    This commit adds a `PushDevice` model where each row
    corresponds to an account on an install of the app
    registered or registration-in-progress to bouncer to
    receive mobile push notifications.
    
    The status field indicates whether the registration
    was successful or pending.

--- zerver/models/push_notifications.py
+++ zerver/models/push_notifications.py
@@ -40,3 +40,46 @@ class PushDeviceToken(AbstractPushDeviceToken):
 
     class Meta:
         unique_together = ("user", "kind", "token")
+
+
+class PushDevice(models.Model):
+    """Each row corresponds to an account on an install of the app
+    registered or registration-in-progress to bouncer to receive
+    mobile push notifications.
+    """
+
+    # The logged-in user for an account within an install of the app.
+    user = models.ForeignKey(UserProfile, on_delete=CASCADE)
+
+    class TokenKind(models.IntegerChoices):
+        APNS = 1, "APNs"
+        FCM = 2, "FCM"
+
+    token_kind = models.PositiveSmallIntegerField(choices=TokenKind.choices)
+
+    # ID to identify an account within an install of the app.
+    push_account_id = models.BigIntegerField()
+
+    # Key to use to encrypt notifications.
+    push_public_key = models.TextField()
+
+    # ID to identify the corresponding record on RemotePushDevice.
+    # Set to NULL while registration to bouncer is in progress.
+    bouncer_device_id = models.BigIntegerField(null=True)
+
+    # Key used to encrypt 'encrypted_push_registration'.
+    # Set to NULL once registration to bouncer succeeds.
+    bouncer_public_key = models.TextField(null=True)
+
+    # Registration data encrypted by mobile client for bouncer.
+    # Set to NULL once registration to bouncer succeeds.
+    encrypted_push_registration = models.TextField(null=True)
+
+    class Meta:
+        indexes = [
+            # TODO: Add comment while adding query in future commit.
+            models.Index(
+                fields=["user", "push_account_id"],
+                name="zerver_push_device_user_push_account_id_idx",
+            )
+        ]
