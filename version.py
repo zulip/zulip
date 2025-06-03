1e4921bb8cc0a4d78614e65b897c6539f98a34cd
Anders Kaseorg <anders@zulip.com>
2025-06-03 15:31:09 -0700

    install-node: Upgrade Node.js from 22.15.0 to 22.16.0.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -49,4 +49,4 @@ API_FEATURE_LEVEL = 390
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (326, 8)  # bumped 2025-05-27 to add @uppy/utils
+PROVISION_VERSION = (326, 9)  # bumped 2025-06-03 to upgrade Node.js
