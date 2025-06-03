45845b4fe451914b6125946e4d7ed8e30824c2f7
Maneesh Shukla <shuklamaneesh24@gmail.com>
2025-06-03 18:37:38 +0530

    tooltips: Group tooltips for a smooth transition.
    This commit groups tooltips for a smooth transition when hovering
    over compose actions using Singleton.
    
    Fixes: #24825.

--- web/tests/compose_actions.test.cjs
+++ web/tests/compose_actions.test.cjs
@@ -36,6 +36,7 @@ set_global("requestAnimationFrame", (func) => func());
 const autosize = noop;
 autosize.update = noop;
 mock_esm("autosize", {default: autosize});
+mock_esm("../src/compose_tooltips", {initialize_compose_tooltips: noop});
 
 const channel = mock_esm("../src/channel");
 const compose_fade = mock_esm("../src/compose_fade", {
