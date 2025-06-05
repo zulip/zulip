f981ac63d06d043080416029974e1f24ce64b783
Anders Kaseorg <anders@zulip.com>
2025-06-05 15:26:48 -0700

    mypy: Add types-requests-oauthlib, types-uwsgi.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -49,4 +49,4 @@ API_FEATURE_LEVEL = 390
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (327, 1)  # bumped 2025-06-04 to upgrade uv
+PROVISION_VERSION = (328, 0)  # bumped 2025-06-05 to add types-requests-oauthlib, types-uwsgi
