825bf2cffa923b7970047214158ae84524136474
Maneesh Shukla <shuklamaneesh24@gmail.com>
2025-06-09 16:33:01 +0530

    settings: Allow downloading bot config from admin bot panel.
    This commit adds code to allow downloading bot config for
    all outgoing webhooks bot owned by the user from "Your bots"
    tabs in the "Bots" settings panel.
    
    Fixes part of #31156.
    
    Co-authored-by: Sujal Shah <sujalshah28092004@gmail.com>

--- web/src/server_events_dispatch.js
+++ web/src/server_events_dispatch.js
@@ -497,18 +497,24 @@ export function dispatch_normal_event(event) {
                     settings_bots.render_bots();
                     if (event.bot.owner_id === current_user.user_id) {
                         settings_users.redraw_your_bots_list();
+                        settings_users.toggle_bot_config_download_container();
                     }
                     break;
                 case "delete":
                     bot_data.del(event.bot.user_id);
                     settings_bots.render_bots();
                     settings_users.redraw_your_bots_list();
+                    settings_users.toggle_bot_config_download_container();
                     break;
                 case "update":
                     bot_data.update(event.bot.user_id, event.bot);
                     settings_bots.render_bots();
                     if ("owner_id" in event.bot) {
                         settings_users.redraw_your_bots_list();
+                        settings_users.toggle_bot_config_download_container();
+                    }
+                    if ("is_active" in event.bot) {
+                        settings_users.toggle_bot_config_download_container();
                     }
                     break;
                 default:
