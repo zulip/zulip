24e8ba97cb9f462ecabb800eae379bbcde9a306e
Anders Kaseorg <anders@zulip.com>
2025-06-10 10:47:24 -0700

    requirements: Upgrade Django from 5.1.10 to 5.2.3.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -49,4 +49,4 @@ API_FEATURE_LEVEL = 391
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (331, 0)  # bumped 2025-06-10 to upgrade django-cte
+PROVISION_VERSION = (332, 0)  # bumped 2025-06-10 to upgrade Django
