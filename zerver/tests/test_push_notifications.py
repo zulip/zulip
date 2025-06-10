0374bb5da77d1cd82ca13ff391c2805498a1a30c
Prakhar Pratyush <prakhar@zulip.com>
2025-06-10 22:52:32 +0530

    zilencer: Add enpoint to register push device.
    This commit adds a zilencer endpoint to let self-hosted
    servers register accounts (on various push devices) to
    whom mobile push notifications will be sent.
    
    POST "/api/v1/remotes/push/e2ee/register"
    Payload: realm_uuid, push_account_id, encrypted_push_registration,
    bouncer_public_key
    
    The post request needs to be authenticated with the server’s API key.
    
    Note: For Zulip Cloud, a background fact about the push bouncer is
    that it runs on the same server and database as the main application;
    it’s not a separate service.
    So, as an optimization, we plan to directly call the
    `do_register_remote_push_device` function and skip the HTTP request.

--- zerver/tests/test_push_notifications.py
+++ zerver/tests/test_push_notifications.py
@@ -18,6 +18,8 @@ from django.utils.crypto import get_random_string
 from django.utils.timezone import now
 from dns.resolver import NoAnswer as DNSNoAnswer
 from firebase_admin import exceptions as firebase_exceptions
+from nacl.encoding import Base64Encoder
+from nacl.public import PublicKey, SealedBox
 from requests.exceptions import ConnectionError
 from typing_extensions import override
 
@@ -87,6 +89,7 @@ from zilencer.views import DevicesToCleanUpDict
 
 if settings.ZILENCER_ENABLED:
     from zilencer.models import (
+        RemotePushDevice,
         RemotePushDeviceToken,
         RemoteRealm,
         RemoteRealmAuditLog,
@@ -1340,6 +1343,151 @@ class PushBouncerNotificationTest(BouncerTestCase):
 
         remote_realm.refresh_from_db()
         self.assertEqual(remote_realm.last_request_datetime, time_sent)
+
+
+class RegisterPushDeviceToBouncer(BouncerTestCase):
+    DEFAULT_SUBDOMAIN = ""
+
+    def get_register_push_device_payload(
+        self,
+        token: str = "apple-tokenaz",
+        token_kind: int = RemotePushDevice.TokenKind.APNS,
+        ios_app_id: str | None = "example.app",
+        timestamp: int | None = None,
+    ) -> dict[str, str | int]:
+        hamlet = self.example_user("hamlet")
+        remote_realm = RemoteRealm.objects.get(uuid=hamlet.realm.uuid)
+
+        if timestamp is None:
+            timestamp = datetime_to_timestamp(now())
+
+        push_registration = {
+            "token": token,
+            "token_kind": token_kind,
+            "ios_app_id": ios_app_id,
+            "timestamp": timestamp,
+        }
+
+        assert settings.PUSH_REGISTRATION_ENCRYPTION_KEYS
+        public_key_str: str = next(iter(settings.PUSH_REGISTRATION_ENCRYPTION_KEYS.keys()))
+        public_key = PublicKey(public_key_str.encode("utf-8"), Base64Encoder)
+        sealed_box = SealedBox(public_key)
+        encrypted_push_registration_bytes = sealed_box.encrypt(
+            orjson.dumps(push_registration), Base64Encoder
+        )
+        encrypted_push_registration = encrypted_push_registration_bytes.decode("utf-8")
+
+        payload: dict[str, str | int] = {
+            "user_uuid": str(hamlet.uuid),
+            "realm_uuid": str(remote_realm.uuid),
+            "push_account_id": 2408,
+            "encrypted_push_registration": encrypted_push_registration,
+            "bouncer_public_key": public_key_str,
+        }
+        return payload
+
+    def test_register_push_device_success(self) -> None:
+        remote_push_devices_count = RemotePushDevice.objects.count()
+        self.assertEqual(remote_push_devices_count, 0)
+
+        payload = self.get_register_push_device_payload()
+
+        result = self.uuid_post(
+            self.server_uuid,
+            "/api/v1/remotes/push/e2ee/register",
+            payload,
+        )
+        response_dict = self.assert_json_success(result)
+        device_id = response_dict["device_id"]
+
+        remote_push_devices = RemotePushDevice.objects.all()
+        self.assert_length(remote_push_devices, 1)
+        self.assertEqual(device_id, remote_push_devices[0].device_id)
+
+        # Idempotent
+        result = self.uuid_post(
+            self.server_uuid,
+            "/api/v1/remotes/push/e2ee/register",
+            payload,
+        )
+        response_dict = self.assert_json_success(result)
+        self.assertEqual(response_dict["device_id"], device_id)
+        remote_push_devices_count = RemotePushDevice.objects.count()
+        self.assertEqual(remote_push_devices_count, 1)
+
+    def test_register_push_device_error(self) -> None:
+        payload = self.get_register_push_device_payload()
+
+        invalid_realm_uuid_payload = {**payload, "realm_uuid": str(uuid.uuid4())}
+        with self.assertLogs("zilencer.views", level="INFO"):
+            result = self.uuid_post(
+                self.server_uuid,
+                "/api/v1/remotes/push/e2ee/register",
+                invalid_realm_uuid_payload,
+            )
+        self.assert_json_error(result, "Organization not registered", status_code=403)
+
+        invalid_bouncer_public_key_payload = {**payload, "bouncer_public_key": "invalid public key"}
+        result = self.uuid_post(
+            self.server_uuid,
+            "/api/v1/remotes/push/e2ee/register",
+            invalid_bouncer_public_key_payload,
+        )
+        self.assert_json_error(result, "Invalid bouncer_public_key")
+
+        liveness_timedout_payload = self.get_register_push_device_payload(
+            timestamp=datetime_to_timestamp(now() - timedelta(days=2))
+        )
+        result = self.uuid_post(
+            self.server_uuid,
+            "/api/v1/remotes/push/e2ee/register",
+            liveness_timedout_payload,
+        )
+        self.assert_json_error(result, "Push registration liveness timedout")
+
+        # Test the various cases resulting in InvalidEncryptedPushRegistrationError
+        payload = self.get_register_push_device_payload()
+        payload["encrypted_push_registration"] = "random-string-no-encryption"
+        result = self.uuid_post(
+            self.server_uuid,
+            "/api/v1/remotes/push/e2ee/register",
+            payload,
+        )
+        self.assert_json_error(result, "Invalid encrypted_push_registration")
+
+        invalid_ios_app_id_format_payload = self.get_register_push_device_payload(
+            ios_app_id="* -- +"
+        )
+        result = self.uuid_post(
+            self.server_uuid,
+            "/api/v1/remotes/push/e2ee/register",
+            invalid_ios_app_id_format_payload,
+        )
+        self.assert_json_error(result, "Invalid encrypted_push_registration")
+
+        invalid_token_kind_payload = self.get_register_push_device_payload(token_kind=8)
+        result = self.uuid_post(
+            self.server_uuid,
+            "/api/v1/remotes/push/e2ee/register",
+            invalid_token_kind_payload,
+        )
+        self.assert_json_error(result, "Invalid encrypted_push_registration")
+
+        missing_ios_app_id_payload = self.get_register_push_device_payload(ios_app_id=None)
+        result = self.uuid_post(
+            self.server_uuid,
+            "/api/v1/remotes/push/e2ee/register",
+            missing_ios_app_id_payload,
+        )
+        self.assert_json_error(result, "Invalid encrypted_push_registration")
+
+        invalid_token_payload = self.get_register_push_device_payload(token="")
+        result = self.uuid_post(
+            self.server_uuid,
+            "/api/v1/remotes/push/e2ee/register",
+            invalid_token_payload,
+        )
+        self.assert_json_error(result, "Invalid encrypted_push_registration")
 
 
 class TestAPNs(PushNotificationTestCase):
