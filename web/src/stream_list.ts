81d333562bdda4263174b937467ba2cb73fd6600
Evy Kassirer <evy@zulip.com>
2025-06-20 19:40:37 -0700

    stream_list: Don't toggle channel search on header click.
    Soon we'll make multiple channel sections and have them toggle
    open or closed when clicking the header.

--- web/src/stream_list.ts
+++ web/src/stream_list.ts
@@ -975,16 +975,6 @@ export function set_event_handlers({
 
     $("#clear_search_stream_button").on("click", clear_search);
 
-    $("#streams_header")
-        .expectOne()
-        .on("click", (e) => {
-            e.preventDefault();
-            if (e.target.id === "streams_inline_icon") {
-                return;
-            }
-            toggle_filter_displayed(e);
-        });
-
     function toggle_pm_header_icon(): void {
         if (pm_list.is_private_messages_collapsed()) {
             return;
