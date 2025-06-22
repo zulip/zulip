a6b5ce77dfdf1bf8c285dd1219b4d754529299fa
Pratik Chanda <pratikchanda2000@gmail.com>
2025-06-22 15:04:40 +0530

    user_settings: Add reaction notification fields to User model.
    This commit introduces reaction notification field namely
    `enable_reaction_desktop_notifications` &
    `enable_reaction_audible_notifications` for users to set their
    reaction notification preferences.
    
    Fixes part of zulip#27327.

--- zerver/views/realm.py
+++ zerver/views/realm.py
@@ -642,6 +642,8 @@ def update_realm_user_settings_defaults(
     enable_followed_topic_email_notifications: Json[bool] | None = None,
     enable_followed_topic_push_notifications: Json[bool] | None = None,
     enable_followed_topic_wildcard_mentions_notify: Json[bool] | None = None,
+    enable_reaction_desktop_notifications: Json[bool] | None = None,
+    enable_reaction_audible_notifications: Json[bool] | None = None,
     enable_offline_email_notifications: Json[bool] | None = None,
     enable_offline_push_notifications: Json[bool] | None = None,
     enable_online_push_notifications: Json[bool] | None = None,
