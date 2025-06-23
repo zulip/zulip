8400c2466548794ccb1aafd0cd2ee3da182045fa
Sahil Batra <sahil@zulip.com>
2025-06-23 20:07:11 +0530

    groups: Make two fields of GroupPermissionSetting optional.
    This commit makes require_system_group and allow_internet_group
    fields of GroupPermissionSetting optional with default value of
    False. "allow_internet_group" is False for all of the settings
    and "require_system_group" is True for only two settings, so it
    is better to not having to repeat them for every setting.

--- zerver/lib/types.py
+++ zerver/lib/types.py
@@ -343,11 +343,11 @@ class RealmPlaygroundDict(TypedDict):
 
 @dataclass
 class GroupPermissionSetting:
-    require_system_group: bool
-    allow_internet_group: bool
     allow_nobody_group: bool
     allow_everyone_group: bool
     default_group_name: str
+    require_system_group: bool = False
+    allow_internet_group: bool = False
     default_for_system_groups: str | None = None
     allowed_system_groups: list[str] = field(default_factory=list)
 
