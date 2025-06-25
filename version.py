8cd0fc7eb0d3a9f2a7111af926d6745d3816435f
Anders Kaseorg <anders@zulip.com>
2025-06-25 16:19:56 -0700

    install-uv: Upgrade uv from 0.7.11 to 0.7.15.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -49,4 +49,4 @@ API_FEATURE_LEVEL = 396
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (333, 0)  # bumped 2025-06-24 to upgrade Python requirements
+PROVISION_VERSION = (333, 1)  # bumped 2025-06-25 to upgrade uv
