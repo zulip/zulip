ff913c5ce4083a9392ca98d619d61fd895edd8f9
PieterCK <pieterceka123@gmail.com>
2025-06-26 20:09:42 +0700

    url_encoding: Refactor a `encode_user_ids`.
    This refactors out a function to encode the user ids into recipient slug
    when constructing narrow links.

--- zerver/tests/test_url_encoding.py
+++ zerver/tests/test_url_encoding.py
@@ -1,5 +1,5 @@
 from zerver.lib.test_classes import ZulipTestCase
-from zerver.lib.url_encoding import encode_stream
+from zerver.lib.url_encoding import encode_stream, encode_user_ids
 
 
 class URLEncodeTest(ZulipTestCase):
@@ -9,3 +9,13 @@ class URLEncodeTest(ZulipTestCase):
         self.assertEqual(encode_stream(123, "General"), "123-General")
         self.assertEqual(encode_stream(7, "random_stream"), "7-random_stream")
         self.assertEqual(encode_stream(9, "Verona", with_operator=True), "channel/9-Verona")
+
+    def test_encode_user_ids(self) -> None:
+        self.assertEqual(encode_user_ids([1, 2, 3]), "1,2,3-group")
+        self.assertEqual(encode_user_ids([3, 1, 2]), "1,2,3-group")
+        self.assertEqual(encode_user_ids([1, 2, 3], prefix="pm"), "1,2,3-pm")
+        self.assertEqual(encode_user_ids([1, 2, 3], with_operator=True), "dm/1,2,3-group")
+        self.assertEqual(encode_user_ids([1, 2, 3], with_operator=True, prefix="pm"), "dm/1,2,3-pm")
+        self.assertEqual(encode_user_ids([5]), "5-group")
+        with self.assertRaises(AssertionError):
+            self.assertEqual(encode_user_ids([]), "-group")
