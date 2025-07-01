f4bd731558e955bfb6613481695c3656168f445f
Alex Vandiver <alexmv@zulip.com>
2025-07-01 10:03:51 -0400

    docker: Start local mailserver on port 25.

--- puppet/zulip/manifests/profile/docker.pp
+++ puppet/zulip/manifests/profile/docker.pp
@@ -6,6 +6,7 @@ class zulip::profile::docker {
   include zulip::profile::base
   include zulip::profile::app_frontend
   include zulip::localhost_camo
+  include zulip::local_mailserver
   include zulip::supervisor
   include zulip::process_fts_updates
 
