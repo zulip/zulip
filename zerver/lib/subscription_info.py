59224c5fb7ae759432c16b89493872e2f84bf0e0
Saubhagya Patel <patelsaubhagya0144@gmail.com>
2025-07-05 00:32:48 +0530

    streams: Add backend implementation for `can_create_topic_group`.
    Fixes part of #19642.

--- zerver/lib/subscription_info.py
+++ zerver/lib/subscription_info.py
@@ -71,6 +71,9 @@ def get_web_public_subs(
         can_administer_channel_group = get_group_setting_value_for_register_api(
             stream.can_administer_channel_group_id, anonymous_group_membership
         )
+        can_create_topic_group = get_group_setting_value_for_register_api(
+            stream.can_create_topic_group_id, anonymous_group_membership
+        )
         can_move_messages_out_of_channel_group = get_group_setting_value_for_register_api(
             stream.can_move_messages_out_of_channel_group_id, anonymous_group_membership
         )
@@ -125,6 +128,7 @@ def get_web_public_subs(
             audible_notifications=audible_notifications,
             can_add_subscribers_group=can_add_subscribers_group,
             can_administer_channel_group=can_administer_channel_group,
+            can_create_topic_group=can_create_topic_group,
             can_move_messages_out_of_channel_group=can_move_messages_out_of_channel_group,
             can_move_messages_within_channel_group=can_move_messages_within_channel_group,
             can_send_message_group=can_send_message_group,
@@ -198,6 +202,9 @@ def build_stream_api_dict(
     can_administer_channel_group = get_group_setting_value_for_register_api(
         raw_stream_dict["can_administer_channel_group_id"], anonymous_group_membership
     )
+    can_create_topic_group = get_group_setting_value_for_register_api(
+        raw_stream_dict["can_create_topic_group_id"], anonymous_group_membership
+    )
     can_move_messages_out_of_channel_group = get_group_setting_value_for_register_api(
         raw_stream_dict["can_move_messages_out_of_channel_group_id"], anonymous_group_membership
     )
@@ -218,6 +225,7 @@ def build_stream_api_dict(
         is_archived=raw_stream_dict["deactivated"],
         can_add_subscribers_group=can_add_subscribers_group,
         can_administer_channel_group=can_administer_channel_group,
+        can_create_topic_group=can_create_topic_group,
         can_move_messages_out_of_channel_group=can_move_messages_out_of_channel_group,
         can_move_messages_within_channel_group=can_move_messages_within_channel_group,
         can_send_message_group=can_send_message_group,
@@ -253,6 +261,7 @@ def build_stream_dict_for_sub(
     is_archived = stream_dict["is_archived"]
     can_add_subscribers_group = stream_dict["can_add_subscribers_group"]
     can_administer_channel_group = stream_dict["can_administer_channel_group"]
+    can_create_topic_group = stream_dict["can_create_topic_group"]
     can_move_messages_out_of_channel_group = stream_dict["can_move_messages_out_of_channel_group"]
     can_move_messages_within_channel_group = stream_dict["can_move_messages_within_channel_group"]
     can_send_message_group = stream_dict["can_send_message_group"]
@@ -297,6 +306,7 @@ def build_stream_dict_for_sub(
         audible_notifications=audible_notifications,
         can_add_subscribers_group=can_add_subscribers_group,
         can_administer_channel_group=can_administer_channel_group,
+        can_create_topic_group=can_create_topic_group,
         can_move_messages_out_of_channel_group=can_move_messages_out_of_channel_group,
         can_move_messages_within_channel_group=can_move_messages_within_channel_group,
         can_send_message_group=can_send_message_group,
@@ -367,6 +377,9 @@ def build_stream_dict_for_never_sub(
     can_administer_channel_group_value = get_group_setting_value_for_register_api(
         raw_stream_dict["can_administer_channel_group_id"], anonymous_group_membership
     )
+    can_create_topic_group = get_group_setting_value_for_register_api(
+        raw_stream_dict["can_create_topic_group_id"], anonymous_group_membership
+    )
     can_move_messages_out_of_channel_group_value = get_group_setting_value_for_register_api(
         raw_stream_dict["can_move_messages_out_of_channel_group_id"], anonymous_group_membership
     )
@@ -391,6 +404,7 @@ def build_stream_dict_for_never_sub(
         is_archived=is_archived,
         can_add_subscribers_group=can_add_subscribers_group_value,
         can_administer_channel_group=can_administer_channel_group_value,
+        can_create_topic_group=can_create_topic_group,
         can_move_messages_out_of_channel_group=can_move_messages_out_of_channel_group_value,
         can_move_messages_within_channel_group=can_move_messages_within_channel_group_value,
         can_send_message_group=can_send_message_group_value,
