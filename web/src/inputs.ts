5898f706e2bcc2b4e519a75ec590c5579c927056
Sayam Samal <sayam@zulip.com>
2025-07-10 03:34:55 +0530

    left_sidebar: Standardize topic list filter input.
    This follow-up commit replaces the current left sidebar topic list
    filter input implementation with the redesigned input_wrapper
    component.
    
    This commit also serves as the base for supporting inputs using the
    search_pill_widget, and thus adjusts the previously defined logic at
    certain places to ensure that the input pills are handled and
    displayed accurately.
    
    Fixes part of #34476.

--- web/src/inputs.ts
+++ web/src/inputs.ts
@@ -1,12 +1,33 @@
 import $ from "jquery";
 
-$("body").on("input", ".input-element", function (this: HTMLInputElement, _e: JQuery.Event) {
+// We use the `input` tag in the selector to avoid conflicts with the pill containing
+// counterpart, which uses a `contenteditable` div instead of an input element.
+$("body").on("input", "input.input-element", function (this: HTMLInputElement, _e: JQuery.Event) {
     if (this.value.length === 0) {
         $(this).removeClass("input-element-nonempty");
     } else {
         $(this).addClass("input-element-nonempty");
     }
 });
+
+$("body").on(
+    "input change",
+    ".has-input-pills .pill-container",
+    function (this: HTMLInputElement, _e: JQuery.Event) {
+        // We define another event handler for inputs with pill, similar to the one above.
+        // However, due to the way inputs with pill use a contenteditable div instead of an
+        // input element, we need to check the textContent of the pill container instead of
+        // the value of an input element.
+        // Here we need to listen to the `change` event in conjunction with the `input` event
+        // to handle the addition or removal of input pills.
+        const value = this.textContent?.trim() ?? "";
+        if (value.length === 0) {
+            $(this).removeClass("input-element-nonempty");
+        } else {
+            $(this).addClass("input-element-nonempty");
+        }
+    },
+);
 
 $("body").on(
     "click",
