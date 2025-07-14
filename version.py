951b0a2ea21a9b63656fb8e3b4129f7a193732b5
Anders Kaseorg <anders@zulip.com>
2025-07-14 14:45:53 -0400

    install-uv: Upgrade uv from 0.7.15 to 0.7.21.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -49,4 +49,4 @@ API_FEATURE_LEVEL = 405
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (333, 7)  # bumped 2025-07-14 to add google-re2-stubs
+PROVISION_VERSION = (333, 8)  # bumped 2025-07-14 to upgrade uv
