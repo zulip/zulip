zulip/zulip

commit 0229f73fae455d48a6847e658f5aa3857eb43394

lightbox: Fix media title update on change in title.
Previously, when title of media element is updated
the change is not reflected in lightbox view.

This is because the title of the element is cached
from the last time when the media element was opened in
lightbox.

This is fixed by invalidating a message's asset_map cache
whenever the content of message is edited.

Fixes #21311

web/src/lightbox.ts
@@ -212,6 +212,10 @@ export function clear_for_testing(): void {
     asset_map.clear();
 }
 
+export function invalidate_asset_map_of_message(message_id: number): void {
+    asset_map.delete(message_id);
+}
+
 function set_selected_media_element($media: JQuery<HTMLMediaElement | HTMLImageElement>): void {
     // Clear out any previously selected element
     $(".media-to-select-in-lightbox-list").removeClass("media-to-select-in-lightbox-list");

web/src/message_events.ts
@@ -17,6 +17,7 @@ import * as direct_message_group_data from "./direct_message_group_data.ts";
 import * as drafts from "./drafts.ts";
 import * as echo from "./echo.ts";
 import type {Filter} from "./filter.ts";
+import * as lightbox from "./lightbox.ts";
 import * as message_edit from "./message_edit.ts";
 import * as message_edit_history from "./message_edit_history.ts";
 import * as message_events_util from "./message_events_util.ts";
@@ -458,6 +459,10 @@ export function update_messages(events: UpdateMessageEvent[]): void {
 
                 // Update raw_content, so that editing a few times in a row is fast.
                 anchor_message.raw_content = event.content;
+
+                // Editing a message may change the titles for linked
+                // media, so we must invalidate the asset map.
+                lightbox.invalidate_asset_map_of_message(event.message_id);
             }
 
             if (unread.update_message_for_mention(anchor_message, any_message_content_edited)) {

commit 608b9bc761ff16306449f55a2f2283aa79dc8e33

lightbox: Fix media asset preview in composebox.
0229f73fae455d48a6847e658f5aa3857eb43394 introduced a bug wherein
while caching the media assets keyed using the message id, previews of
media from composebox which also used same codepath attempted to find
the message id from the asset. This resulted in an assertion error.

This is fixed by making sure that compose previewable assets are
never cached nor attempted to fetch from cache since they don't
have a message id yet.

Co-authored-by: Tim Abbott <tabbott@zulip.com>

web/src/lightbox.ts
@@ -484,16 +484,39 @@ function supports_heic(): boolean {
 
 // retrieve the metadata from the DOM and store into the asset_map.
 export function parse_media_data(media: HTMLMediaElement | HTMLImageElement): Media {
-    const message_id = rows.get_message_id(media);
-    const canonical_url = canonical_url_of_media(media);
-    if (asset_map.has(message_id) && asset_map.get(message_id)?.has(canonical_url)) {
-        // Use the cached value
-        const payload = asset_map.get(message_id)!.get(canonical_url);
-        assert(payload !== undefined);
-        return payload;
-    }
-
     const $media = $(media);
+    const canonical_url = canonical_url_of_media(media);
+    let message_id;
+
+    // This includes the preview feature in the message-edit UI as well as compose.
+    const is_compose_preview_media = $media.closest(".preview_content").length > 0;
+    const $message_row = rows.get_closest_row($media);
+    let use_asset_map;
+    let sender_full_name;
+
+    if (is_compose_preview_media || rows.is_overlay_row($message_row)) {
+        // We don't use the asset map cache in compose/edit UIs or
+        // overlays, since the content is not stable.
+        sender_full_name = people.my_full_name();
+        use_asset_map = false;
+    } else if ($message_row.length > 0) {
+        use_asset_map = true;
+        message_id = rows.id($message_row);
+
+        if (asset_map.has(message_id) && asset_map.get(message_id)?.has(canonical_url)) {
+            // Use the cached value
+            const payload = asset_map.get(message_id)!.get(canonical_url);
+            assert(payload !== undefined);
+            return payload;
+        }
+
+        const message = message_store.get(message_id);
+        if (message === undefined) {
+            blueslip.error("Lightbox for unknown message", {message_id});
+        } else {
+            sender_full_name = message.sender_full_name;
+        }
+    }
 
     // if wrapped in the .youtube-video class, it will be length = 1, and therefore
     // cast to true.
@@ -501,9 +524,6 @@ export function parse_media_data(media: HTMLMediaElement | HTMLImageElement): Me
     const is_vimeo_video = $media.closest(".vimeo-video").length > 0;
     const is_embed_video = $media.closest(".embed-video").length > 0;
     const is_inline_video = $media.closest(".message_inline_video").length > 0;
-
-    // check if media is descendent of #compose .preview_content
-    const is_compose_preview_media = $media.closest("#compose .preview_content").length === 1;
 
     const $parent = $media.parent();
     let type: MediaType;
@@ -563,17 +583,6 @@ export function parse_media_data(media: HTMLMediaElement | HTMLImageElement): Me
             source = url;
         }
     }
-    let sender_full_name;
-    if (is_compose_preview_media) {
-        sender_full_name = people.my_full_name();
-    } else {
-        const message = message_store.get(message_id);
-        if (message === undefined) {
-            blueslip.error("Lightbox for unknown message", {message_id});
-        } else {
-            sender_full_name = message.sender_full_name;
-        }
-    }
 
     const payload = {
         user: sender_full_name,
@@ -586,7 +595,9 @@ export function parse_media_data(media: HTMLMediaElement | HTMLImageElement): Me
         url: util.is_valid_url(url) ? url : "",
     };
 
-    if (!is_loading_placeholder && canonical_url !== "") {
+    if (use_asset_map && !is_loading_placeholder && canonical_url !== "") {
+        // Update the asset_map, if we had a message ID involved.
+        assert(message_id !== undefined);
         if (!asset_map.has(message_id)) {
             asset_map.set(message_id, new Map<string, Media>());
         }