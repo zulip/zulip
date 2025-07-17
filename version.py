4dc802aa9a7d42045aa2baf84dbffcc5f59b281a
Shubham Padia <shubham@zulip.com>
2025-07-17 08:59:10 +0000

    help-beta: Add option to format mdx documents.
    Partially fixes #35129.
    
    Prettier could not be used because of prettier issue number 12209, not
    linked here to avoid spam backlinks on the original issue. Prettier does
    not have support for mdx v2 and v3.
    
    We are using remarkLintRulesLintRecommended and
    remarkPresentLintMarkdownStyleGuide as our starting set of rules.
    None of the rules were giving an error on the starting set, but some
    rules were giving lots of warnings on the generated mdx. They are
    set to false in this file, we can add them back later as and when
    required.
    
    We have not inserted this in the main lint tool, we should do that in
    the final cutover PR since we don't want the lint here to give any
    unexpected warnings when people are linting stuff unrelated to the mdx
    files.
    
    This commit has been tested out on the current state of help center to
    not produce any errors or warnings. The first run of format will produce
    tons of warnings which are the issues being auto-fixed by the linter.
    After that, the second run should produce zero errors.

--- version.py
+++ version.py
@@ -49,4 +49,4 @@ API_FEATURE_LEVEL = 408
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (334, 1)  # bumped 2025-07-16 to add some remark libraries
+PROVISION_VERSION = (334, 2)  # bumped 2025-07-17 to add mdx linting libraries
