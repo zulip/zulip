8d5abc34ac4029dec454fc170b42006718c3dc42
Tim Abbott <tabbott@zulip.com>
2025-07-18 16:49:11 -0700

    channel_folders: Fix stale code in a node test.

--- web/tests/channel_folders.test.cjs
+++ web/tests/channel_folders.test.cjs
@@ -60,8 +60,5 @@ run_test("basics", () => {
     assert.ok(channel_folders.is_valid_folder_id(frontend_folder.id));
     assert.ok(!channel_folders.is_valid_folder_id(999));
 
-    assert.equal(
-        channel_folders.get_channel_folder_name_from_id(frontend_folder.id),
-        frontend_folder.name,
-    );
+    assert.equal(channel_folders.get_channel_folder_by_id(frontend_folder.id), frontend_folder);
 });
