7e0c8b23d015c98b8d5af53b8bdd8bd8c85cc912
Niloth P <20315308+Niloth-p@users.noreply.github.com>
2025-07-20 16:17:11 +0530

    stream_list: Toggle topic list on channel buttons in `#topics` view.
    When the "Channel links in the left sidebar go to" setting is set to
    "List of topics", and the user is in a channel narrow without a topic,
    pressing the current channel button toggles the topic list to expand and
    collapse.
    
    Fixes #35197.

--- web/src/stream_list.ts
+++ web/src/stream_list.ts
@@ -1275,6 +1275,24 @@ export function on_sidebar_channel_click(
         user_settings.web_channel_default_view ===
         web_channel_default_view_values.list_of_topics.code
     ) {
+        if (current_narrow_stream_id === stream_id && current_topic === undefined) {
+            const $stream_li = get_stream_li(stream_id);
+            if (!$stream_li) {
+                // This is a sanity check.
+                blueslip.error("No stream_li for subscribed stream", {stream_id});
+            } else {
+                // Total topic-list states: 1.collapsed, 2.expanded 3.zoomed in
+                // Here, we toggle between the two states: expanded <-> collapsed
+                const is_topic_list_expanded = $stream_li.find(".topic-list").length > 0;
+                if (is_topic_list_expanded) {
+                    clear_topics();
+                } else {
+                    topic_list.rebuild_left_sidebar($stream_li, stream_id);
+                    topic_list.topic_state_typeahead?.lookup(true);
+                }
+            }
+        }
+
         browser_history.go_to_location(hash_util.by_channel_topic_list_url(stream_id));
         return;
     }
