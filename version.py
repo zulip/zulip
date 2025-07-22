d0f9fb73cc579f9c15a2c212d10ef8f2f06e8df2
Anders Kaseorg <anders@zulip.com>
2025-07-22 12:24:47 -0700

    dependencies: Upgrade JavaScript dependencies.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -49,4 +49,4 @@ API_FEATURE_LEVEL = 408
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (336, 1)  # bumped 2025-07-22 to upgrade Node.js
+PROVISION_VERSION = (337, 0)  # bumped 2025-07-22 to upgrade JavaScript dependencies
