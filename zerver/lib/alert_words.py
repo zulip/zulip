14cf9fc93b99142e3420a1be023f3dd6d1571dfa
Kate Liu <lhyhgshyd@163.com>
2025-07-30 14:45:02 -0700

    alert-words: Ensure soft-deleted words are excluded from matches.
    Co-authored-by: Trey Todd <trey.todd@gmail.com>
    Co-authored-by: Andy Espinoza <andyesp831@gmail.com>

--- zerver/lib/alert_words.py
+++ zerver/lib/alert_words.py
@@ -14,9 +14,9 @@ from zerver.models.alert_words import flush_realm_alert_words
 
 @cache_with_key(lambda realm: realm_alert_words_cache_key(realm.id), timeout=3600 * 24)
 def alert_words_in_realm(realm: Realm) -> dict[int, list[str]]:
-    user_ids_and_words = AlertWord.objects.filter(realm=realm, user_profile__is_active=True).values(
-        "user_profile_id", "word"
-    )
+    user_ids_and_words = AlertWord.objects.filter(
+        realm=realm, user_profile__is_active=True, deactivated=False
+    ).values("user_profile_id", "word")
     user_ids_with_words: dict[int, list[str]] = {}
     for id_and_word in user_ids_and_words:
         user_ids_with_words.setdefault(id_and_word["user_profile_id"], [])
@@ -47,20 +47,36 @@ def get_alert_word_automaton(realm: Realm) -> ahocorasick.Automaton:
 
 
 def user_alert_words(user_profile: UserProfile) -> list[str]:
-    return list(AlertWord.objects.filter(user_profile=user_profile).values_list("word", flat=True))
+    return list(AlertWord.objects.filter(user_profile=user_profile, deactivated=False).values_list("word", flat=True))
+
+def user_alert_words_with_deactivated(user_profile: UserProfile) -> list[AlertWord]:
+    # returns all alert words for a user, including deactivated ones
+    # this is used to check for duplicates when adding new alert words
+    return list(AlertWord.objects.filter(user_profile=user_profile))
 
 
 @transaction.atomic(savepoint=False)
 def add_user_alert_words(user_profile: UserProfile, new_words: Iterable[str]) -> list[str]:
-    existing_words_lower = {word.lower() for word in user_alert_words(user_profile)}
+    existing_alert_words = user_alert_words_with_deactivated(user_profile)
 
+    existing_words_map = {
+        alert_word.word.lower(): alert_word for alert_word in existing_alert_words
+    }
     # Keeping the case, use a dictionary to get the set of
     # case-insensitive distinct, new alert words
     word_dict: dict[str, str] = {}
     for word in new_words:
-        if word.lower() in existing_words_lower:
+        lower_word = word.lower()
+        # if the word already exists, we just reactivate it(soft delete)
+        # we activate it by setting deactivated to False instead of deleting it
+        # so that we can retain historical data for more accurate highlighting logic
+        if lower_word in existing_words_map:
+            alert_word = existing_words_map[lower_word]
+            if alert_word.deactivated:
+                alert_word.deactivated = False
+                alert_word.save()
             continue
-        word_dict[word.lower()] = word
+        word_dict[lower_word] = word
 
     AlertWord.objects.bulk_create(
         AlertWord(user_profile=user_profile, word=word, realm=user_profile.realm)
@@ -69,8 +85,11 @@ def add_user_alert_words(user_profile: UserProfile, new_words: Iterable[str]) ->
     # Django bulk_create operations don't flush caches, so we need to do this ourselves.
     flush_realm_alert_words(user_profile.realm_id)
 
-    return user_alert_words(user_profile)
-
+    return list(
+        AlertWord.objects.filter(user_profile=user_profile, deactivated=False).values_list(
+            "word", flat=True
+        )
+    )
 
 @transaction.atomic(savepoint=False)
 def remove_user_alert_words(user_profile: UserProfile, delete_words: Iterable[str]) -> list[str]:
@@ -78,5 +97,12 @@ def remove_user_alert_words(user_profile: UserProfile, delete_words: Iterable[st
     # We can clean this up if/when PostgreSQL has more native support for case-insensitive fields.
     # If we turn this into a bulk operation, we will need to call flush_realm_alert_words() here.
     for delete_word in delete_words:
-        AlertWord.objects.filter(user_profile=user_profile, word__iexact=delete_word).delete()
+        # Mark the alert word as deactivated instead of deleting it.
+        # This is to retain historical data for more accurate highlighting logic
+        AlertWord.objects.filter(user_profile=user_profile, word__iexact=delete_word).update(
+            deactivated=True
+        )
+
+    # Important: clear cache so that realm-level alert_words are updated
+    flush_realm_alert_words(user_profile.realm_id)
     return user_alert_words(user_profile)
