27ddc450cc7febd349b683f1b4cf12e574df78c8
Niloth P <20315308+Niloth-p@users.noreply.github.com>
2025-07-31 17:54:28 +0530

    integrations: Re-order part of the integrations registry alphabetically.
    Exceptions: This does not re-order the Bitbucket integrations or the
    Slack integrations in the alphabetical order, and instead leaves them
    reverse chronologically ordered to convey the relative importance of
    similar integrations.

--- zerver/lib/integrations.py
+++ zerver/lib/integrations.py
@@ -515,6 +515,9 @@ WEBHOOK_INTEGRATIONS: list[WebhookIntegration] = [
         url_options=[WebhookUrlOption.build_preset_config(PresetUrlOption.BRANCHES)],
     ),
     WebhookIntegration(
+        "basecamp", ["project-management"], [WebhookScreenshotConfig("doc_active.json")]
+    ),
+    WebhookIntegration(
         "beanstalk",
         ["version-control"],
         [
@@ -527,29 +530,26 @@ WEBHOOK_INTEGRATIONS: list[WebhookIntegration] = [
         ],
     ),
     WebhookIntegration(
-        "basecamp", ["project-management"], [WebhookScreenshotConfig("doc_active.json")]
-    ),
-    WebhookIntegration(
         "beeminder",
         ["misc"],
         # [WebhookScreenshotConfig("derail_worried.json")],
         display_name="Beeminder",
     ),
     WebhookIntegration(
-        "bitbucket3",
+        "bitbucket",
         ["version-control"],
         [
             WebhookScreenshotConfig(
-                "repo_push_update_single_branch.json",
-                "004.png",
-                "bitbucket",
-                bot_name="Bitbucket Server Bot",
-                channel="bitbucket",
+                "push.json",
+                "002.png",
+                channel="commits",
+                use_basic_auth=True,
+                payload_as_query_param=True,
             )
         ],
-        logo="images/integrations/logos/bitbucket.svg",
-        display_name="Bitbucket Server",
-        url_options=[WebhookUrlOption.build_preset_config(PresetUrlOption.BRANCHES)],
+        display_name="Bitbucket",
+        secondary_line_text="(Enterprise)",
+        legacy=True,
     ),
     WebhookIntegration(
         "bitbucket2",
@@ -568,20 +568,20 @@ WEBHOOK_INTEGRATIONS: list[WebhookIntegration] = [
         url_options=[WebhookUrlOption.build_preset_config(PresetUrlOption.BRANCHES)],
     ),
     WebhookIntegration(
-        "bitbucket",
+        "bitbucket3",
         ["version-control"],
         [
             WebhookScreenshotConfig(
-                "push.json",
-                "002.png",
-                channel="commits",
-                use_basic_auth=True,
-                payload_as_query_param=True,
+                "repo_push_update_single_branch.json",
+                "004.png",
+                "bitbucket",
+                bot_name="Bitbucket Server Bot",
+                channel="bitbucket",
             )
         ],
-        display_name="Bitbucket",
-        secondary_line_text="(Enterprise)",
-        legacy=True,
+        logo="images/integrations/logos/bitbucket.svg",
+        display_name="Bitbucket Server",
+        url_options=[WebhookUrlOption.build_preset_config(PresetUrlOption.BRANCHES)],
     ),
     WebhookIntegration(
         "buildbot", ["continuous-integration"], [WebhookScreenshotConfig("started.json")]
@@ -610,14 +610,14 @@ WEBHOOK_INTEGRATIONS: list[WebhookIntegration] = [
         "crashlytics", ["monitoring"], [WebhookScreenshotConfig("issue_message.json")]
     ),
     WebhookIntegration(
-        "dialogflow",
-        ["customer-support"],
-        [WebhookScreenshotConfig("weather_app.json", extra_params={"email": "iago@zulip.com"})],
-    ),
-    WebhookIntegration(
         "delighted",
         ["customer-support", "marketing"],
         [WebhookScreenshotConfig("survey_response_updated_promoter.json")],
+    ),
+    WebhookIntegration(
+        "dialogflow",
+        ["customer-support"],
+        [WebhookScreenshotConfig("weather_app.json", extra_params={"email": "iago@zulip.com"})],
     ),
     WebhookIntegration("dropbox", ["productivity"], [WebhookScreenshotConfig("file_updated.json")]),
     WebhookIntegration("errbit", ["monitoring"], [WebhookScreenshotConfig("error_message.json")]),
@@ -859,13 +859,13 @@ WEBHOOK_INTEGRATIONS: list[WebhookIntegration] = [
         "sentry", ["monitoring"], [WebhookScreenshotConfig("event_for_exception_python.json")]
     ),
     WebhookIntegration(
+        "slack", ["communication"], [WebhookScreenshotConfig("message_with_normal_text.json")]
+    ),
+    WebhookIntegration(
         "slack_incoming",
         ["communication", "meta-integration"],
         display_name="Slack-compatible webhook",
         logo="images/integrations/logos/slack.svg",
-    ),
-    WebhookIntegration(
-        "slack", ["communication"], [WebhookScreenshotConfig("message_with_normal_text.json")]
     ),
     WebhookIntegration(
         "sonarqube",
@@ -935,6 +935,7 @@ WEBHOOK_INTEGRATIONS: list[WebhookIntegration] = [
         [WebhookScreenshotConfig("publish_post.txt", "wordpress_post_created.png")],
         display_name="WordPress",
     ),
+    WebhookIntegration("zabbix", ["monitoring"], [WebhookScreenshotConfig("zabbix_alert.json")]),
     WebhookIntegration("zapier", ["meta-integration"]),
     WebhookIntegration(
         "zendesk",
@@ -951,7 +952,6 @@ WEBHOOK_INTEGRATIONS: list[WebhookIntegration] = [
             )
         ],
     ),
-    WebhookIntegration("zabbix", ["monitoring"], [WebhookScreenshotConfig("zabbix_alert.json")]),
 ]
 
 INTEGRATIONS: dict[str, Integration] = {
@@ -1049,6 +1049,7 @@ HUBOT_INTEGRATIONS: list[HubotIntegration] = [
     HubotIntegration("bonusly", ["hr"]),
     HubotIntegration("chartbeat", ["marketing"]),
     HubotIntegration("darksky", ["misc"], display_name="Dark Sky"),
+    HubotIntegration("google-translate", ["misc"], display_name="Google Translate"),
     HubotIntegration(
         "instagram",
         ["misc"],
@@ -1056,7 +1057,6 @@ HUBOT_INTEGRATIONS: list[HubotIntegration] = [
         logo="images/integrations/logos/instagra_m.svg",
     ),
     HubotIntegration("mailchimp", ["communication", "marketing"]),
-    HubotIntegration("google-translate", ["misc"], display_name="Google Translate"),
     HubotIntegration(
         "youtube",
         ["misc"],
