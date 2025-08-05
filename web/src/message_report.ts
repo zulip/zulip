b59992ee181ae9e5f2c19d541f539ecbf030329f
PieterCK <pieterceka123@gmail.com>
2025-08-05 17:52:58 +0700

    message_report_ui: Add message preview to report modal.
    This adds a preview of the message that the user wants to report to the
    "Report message" modal.
    
    The message preview uses a new modal called "message_preview.hbs". It is
    basically a single message in a message row but without message action
    buttons.
    
    Fixes #20047.

--- web/src/message_report.ts
+++ web/src/message_report.ts
@@ -2,6 +2,7 @@ import $ from "jquery";
 import assert from "minimalistic-assert";
 import type * as tippy from "tippy.js";
 
+import render_message_preview from "../templates/message_preview.hbs";
 import render_report_message_modal from "../templates/report_message_modal.hbs";
 
 import * as channel from "./channel.ts";
@@ -9,8 +10,15 @@ import * as dialog_widget from "./dialog_widget.ts";
 import type {Option} from "./dropdown_widget.ts";
 import * as dropdown_widget from "./dropdown_widget.ts";
 import {$t_html} from "./i18n.ts";
+import {get_timestr} from "./message_list_view.ts";
 import type {Message} from "./message_store.ts";
+import * as people from "./people.ts";
+import * as stream_color from "./stream_color.ts";
+import * as stream_data from "./stream_data.ts";
+import * as sub_store from "./sub_store.ts";
+import * as timerender from "./timerender.ts";
 import * as ui_report from "./ui_report.ts";
+import * as util from "./util.ts";
 
 export const message_report_type_options: Option[] = [
     {
@@ -31,11 +39,99 @@ export const message_report_type_options: Option[] = [
     },
 ];
 
+type MessagePreviewRenderContext = {
+    header_timestr: string;
+    include_sender: boolean;
+    msg: Message;
+    sender_is_bot: boolean;
+    should_add_guest_indicator_for_sender: boolean;
+    small_avatar_url: string;
+    timestr: string;
+} & (
+    | {
+          is_empty_string_topic: boolean;
+          is_stream: true;
+          recipient_bar_color: string;
+          stream_id: number;
+          stream_name: string;
+          stream_privacy_icon_color: string;
+          topic_display_name: string;
+      }
+    | {
+          is_stream: false;
+          is_dm_with_self: boolean;
+          recipients: string;
+      }
+);
+
+function render_report_message_preview(message: Message): void {
+    const $report_message_preview_container = $("#report-message-preview-container");
+    const time = new Date(message.timestamp * 1000);
+    const header_timestr = timerender.render_now(time).time_str;
+    const include_sender = true;
+    const sender_is_bot = people.sender_is_bot(message);
+    const should_add_guest_indicator_for_sender = people.should_add_guest_user_indicator(
+        message.sender_id,
+    );
+    const small_avatar_url = people.small_avatar_url(message);
+    const timestr = get_timestr(message);
+
+    let render_context: MessagePreviewRenderContext;
+    if (message.type === "stream") {
+        const color = stream_data.get_color(message.stream_id);
+        const recipient_bar_color = stream_color.get_recipient_bar_color(color);
+        const stream_id = message.stream_id;
+        const stream_name = sub_store.maybe_get_stream_name(stream_id);
+        const stream_privacy_icon_color = stream_color.get_stream_privacy_icon_color(color);
+        assert(stream_name !== undefined);
+
+        render_context = {
+            header_timestr,
+            include_sender,
+            is_empty_string_topic: message.topic === "",
+            is_stream: true as const,
+            msg: message,
+            recipient_bar_color,
+            sender_is_bot,
+            should_add_guest_indicator_for_sender,
+            small_avatar_url,
+            stream_id,
+            stream_name,
+            stream_privacy_icon_color,
+            timestr,
+            topic_display_name: util.get_final_topic_display_name(message.topic),
+        };
+    } else {
+        assert(message.type === "private");
+        const recipient_user_ids = people.pm_with_user_ids(message);
+        assert(recipient_user_ids !== undefined);
+        const is_dm_with_self = people.is_direct_message_conversation_with_self(recipient_user_ids);
+        const recipients = people.user_ids_to_full_names_string(recipient_user_ids);
+
+        render_context = {
+            header_timestr,
+            include_sender,
+            is_stream: false as const,
+            msg: message,
+            is_dm_with_self,
+            recipients,
+            sender_is_bot,
+            should_add_guest_indicator_for_sender,
+            small_avatar_url,
+            timestr,
+        };
+    }
+
+    const rendered_report_message_preview = render_message_preview(render_context);
+    $report_message_preview_container.append($(rendered_report_message_preview));
+}
+
 export function show_message_report_modal(message: Message): void {
     const html_body = render_report_message_modal({});
     let report_type_dropdown_widget: dropdown_widget.DropdownWidget;
 
     function message_report_post_render(): void {
+        render_report_message_preview(message);
         function get_message_report_types(): Option[] {
             // Keep this in sync with zerver/models/realms/Realm.REPORT_MESSAGE_REASONS
             return [
