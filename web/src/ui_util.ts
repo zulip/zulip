0d82991f2fa039800cc0b0b7e0d5fe1f30c45a67
Evy Kassirer <evy@zulip.com>
2025-08-12 17:54:48 -0700

    ui_util: Toggle classname for unread mentions instead of using hide/show.

--- web/src/ui_util.ts
+++ web/src/ui_util.ts
@@ -194,12 +194,12 @@ export function update_unread_mention_info_in_dom(
 ): void {
     const $unread_mention_info_span = $unread_mention_info_elem.find(".unread_mention_info");
     if (!stream_has_any_unread_mention_messages) {
-        $unread_mention_info_span.hide();
+        $unread_mention_info_span.toggleClass("no-display", true);
         $unread_mention_info_span.text("");
         return;
     }
 
-    $unread_mention_info_span.show();
+    $unread_mention_info_span.toggleClass("no-display", false);
     $unread_mention_info_span.text("@");
 }
 
