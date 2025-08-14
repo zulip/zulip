47c832a6a4d48705376da98c87290d9282bc3d38
Pratik Chanda <pratikchanda2000@gmail.com>
2025-08-14 18:31:48 +0530

    reload: Remove event listeners to handle reload logic for compose.
    Earlier, we would trigger events when compose opened or closed to
    reset reload timeout.
    
    This commit removes this in favor of normal function calls to
    `compose_started_handler` and `compose_done_handler` to do this.

--- web/tests/reload.test.cjs
+++ web/tests/reload.test.cjs
@@ -2,12 +2,16 @@
 
 const assert = require("node:assert/strict");
 
-const {zrequire} = require("./lib/namespace.cjs");
+const {mock_esm, set_global, zrequire} = require("./lib/namespace.cjs");
 const {run_test, noop} = require("./lib/test.cjs");
+
+const channel = mock_esm("../src/channel");
 
 // override file-level function call in reload.ts
 window.addEventListener = noop;
 const reload = zrequire("reload");
+
+set_global("document", {});
 
 run_test("old_metadata_string_is_stale", () => {
     assert.ok(reload.is_stale_refresh_token("#reload:234234235234", Date.now()), true);
@@ -35,4 +39,33 @@ run_test("old_token_is_stale ", () => {
             Date.parse("23 Jan 2022 00:00:00 GMT"),
         ),
     );
+});
+
+run_test("reload", ({override}) => {
+    let idle_timeout_created = false;
+    let idle_timeout_canceled = false;
+    override(document, "to_$", () => ({
+        idle() {
+            idle_timeout_created = true;
+            return {
+                cancel() {
+                    idle_timeout_canceled = true;
+                },
+            };
+        },
+    }));
+    channel.get = (opts) => {
+        assert.equal(opts.url, "/compatibility");
+        opts.success();
+    };
+
+    reload.initiate({});
+    assert.equal(idle_timeout_created, true);
+    assert.equal(idle_timeout_canceled, false);
+
+    idle_timeout_created = false;
+
+    reload.maybe_reload_after_compose_end();
+    assert.equal(idle_timeout_canceled, true);
+    assert.equal(idle_timeout_created, true);
 });
