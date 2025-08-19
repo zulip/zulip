315f97d3fd9b3e82e8a30f24c1e6760df3329ffe
Sahil Batra <sahil@zulip.com>
2025-08-19 08:59:06 +0530

    stream-settings: Allow filtering streams by folder.
    This commit adds code to allow filtering streams by folder
    in stream settings UI.
    
    Fixes part of #35779.
    
    Co-authored-by: Karl Stolley <karl@zulip.com>

--- web/src/stream_ui_updates.ts
+++ web/src/stream_ui_updates.ts
@@ -493,7 +493,10 @@ export function update_stream_row_in_settings_tab(sub: StreamSubscription): void
             (is_subscribed_stream_tab_active() && sub.subscribed) ||
             (is_not_subscribed_stream_tab_active() && !sub.subscribed)
         ) {
-            if (stream_settings_components.archived_status_filter_includes_channel(sub)) {
+            if (
+                stream_settings_components.archived_status_filter_includes_channel(sub) &&
+                stream_settings_components.folder_filter_includes_channel(sub)
+            ) {
                 $row.removeClass("notdisplayed");
             }
         } else if (sub.invite_only || current_user.is_guest) {
