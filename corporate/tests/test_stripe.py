bb7dcb6f1db3bb97f7e8e4b0f4bbee1821c67e4c
Lauryn Menard <lauryn@zulip.com>
2025-08-21 17:36:26 +0200

    corporate: Send fixed-price stale audit log internal billing email.
    Still send an internal billing notice when a fixed-price plan is
    invoiced while the remote server has stale audit log data. This
    does not trigger updating the stale_audit_log_data_email_sent
    field on the CustomerPlan as the plan will be invoiced in this
    case.

--- corporate/tests/test_stripe.py
+++ corporate/tests/test_stripe.py
@@ -10029,6 +10029,31 @@ class TestRemoteServerBillingFlow(StripeTestCase, RemoteServerTestCase):
         self.assertEqual(current_plan.next_invoice_date, updated_invoice_date)
         self.assertFalse(current_plan.stale_audit_log_data_email_sent)
 
+        from django.core.mail import outbox
+
+        message = outbox[-1]
+        self.assert_length(message.to, 1)
+        self.assertEqual(message.to[0], "sales@zulip.com")
+        self.assertEqual(
+            message.subject,
+            f"Stale audit log data for {self.billing_session.billing_entity_display_name}'s plan",
+        )
+        self.assertIn(
+            f"Support URL: {self.billing_session.support_url()}",
+            message.body,
+        )
+        self.assertIn(
+            f"Internal billing notice for {self.billing_session.billing_entity_display_name}.",
+            message.body,
+        )
+        self.assertIn(
+            "Unable to verify current licenses in use, but invoicing not delayed because customer has a fixed-price plan.",
+            message.body,
+        )
+        self.assertIn(
+            f"Last data upload: {last_audit_log_update.strftime('%Y-%m-%d')}", message.body
+        )
+
         # Audit log data is up-to-date when the plan is next invoiced.
         with time_machine.travel(updated_invoice_date, tick=False):
             send_server_data_to_push_bouncer(consider_usage_statistics=False)
