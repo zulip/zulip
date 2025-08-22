92deb6efc70209801f4356491babf88c776d735f
PieterCK <pieterceka123@gmail.com>
2025-08-22 14:07:35 +0700

    integrations: Rename url_option `MAPPING` to `CHANNEL_MAPPING`.
    This renames `PresetUrlOption.MAPPING` to `CHANNEL_MAPPING` to avoid
    confusion with an option that maps Slack channels to Zulip topics.

--- zerver/lib/webhooks/common.py
+++ zerver/lib/webhooks/common.py
@@ -57,7 +57,7 @@ OptionalUserSpecifiedTopicStr: TypeAlias = Annotated[str | None, ApiParamConfig(
 class PresetUrlOption(str, Enum):
     BRANCHES = "branches"
     IGNORE_PRIVATE_REPOSITORIES = "ignore_private_repositories"
-    MAPPING = "mapping"
+    CHANNEL_MAPPING = "mapping"
 
 
 @dataclass
@@ -95,7 +95,7 @@ class WebhookUrlOption:
                     label="Exclude notifications from private repositories",
                     validator=check_bool,
                 )
-            case PresetUrlOption.MAPPING:  # nocoverage # Not used yet
+            case PresetUrlOption.CHANNEL_MAPPING:  # nocoverage # Not used yet
                 return cls(
                     name=config.value,
                     label="",
