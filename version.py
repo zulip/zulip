1e391e09082fd23e9117b27f27a06520dae595ab
Shubham Padia <shubham@zulip.com>
2025-08-28 06:04:11 +0000

    starlight_help: Move @types/mdast away from dev dependencies.
    @types/mdast was in dev dependencies while it shouldn't have been, we
    move that away from dev dependencies and bump the version. We need the
    package when running the astro build step.

--- version.py
+++ version.py
@@ -49,4 +49,4 @@ API_FEATURE_LEVEL = 425
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (342, 1)  # bumped 2025-08-19 to add zstd
+PROVISION_VERSION = (343, 0)  # bumped 2025-08-28 to move @types/mdast away from dev dependencies.
