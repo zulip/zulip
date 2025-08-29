d867f4a659d60ac4471e3463a77862f6d1fa965f
Kislay Verma <kislayuv27@gmail.com>
2025-08-29 16:38:13 +0530

    stream_settings: Use `default_code_block_language`.
    For syntax highlighting in code blocks, the
    `default_code_block_language` of the stream is used in stream
    messages. If this property is not set, we fall back to the realm
    default code block language.
    For direct messages, only the realm default is used.
    
    - Added the field to the Stream type definition.
    - Modified the messages/render handler to accept a default
    code block language for rendering a message.
    
    Moving the message to a new channel doesn't change the
    code block language of the message, but further edits
    to the message content will use the new channel's language
    setting (or fall back to realm default).
    
    Fixes part of #17454.

--- zerver/actions/message_edit.py
+++ zerver/actions/message_edit.py
@@ -1721,6 +1721,14 @@ def check_update_message(
 
     if (
         isinstance(message_edit_request, StreamMessageEditRequest)
+        and message_edit_request.orig_stream.default_code_block_language
+    ):
+        default_code_block_language = message_edit_request.orig_stream.default_code_block_language
+    else:
+        default_code_block_language = message.realm.default_code_block_language
+
+    if (
+        isinstance(message_edit_request, StreamMessageEditRequest)
         and message_edit_request.is_topic_edited
     ):
         check_stream_topic_edit_permissions(
@@ -1752,6 +1760,7 @@ def check_update_message(
             message_edit_request.content,
             user_profile.realm,
             mention_data=mention_data,
+            default_code_block_language=default_code_block_language,
         )
         links_for_embed |= rendering_result.links_for_preview
 
@@ -1846,6 +1855,7 @@ def check_update_message(
             # `render_incoming_message` call earlier in this function.
             "message_realm_id": user_profile.realm_id,
             "urls": list(links_for_embed),
+            "default_code_block_language": default_code_block_language,
         }
         queue_event_on_commit("embed_links", event_data)
 
