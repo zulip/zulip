582bc7d303321c282e81491ae3327c04ea9d74f4
Anders Kaseorg <anders@zulip.com>
2025-09-04 15:42:24 -0700

    install-uv: Upgrade uv from 0.8.10 to 0.8.15.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>
    (cherry picked from commit 270c50651c0982fc127744ae24846a2b00bf658e)

--- version.py
+++ version.py
@@ -49,4 +49,4 @@ API_FEATURE_LEVEL = 421
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (342, 0)  # bumped 2025-10-01 to remove emoji-datasource-google-blob
+PROVISION_VERSION = (342, 1)  # bumped 2025-10-22 to upgrade uv
