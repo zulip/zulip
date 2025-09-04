6615756437b00098043230bf6d2b9b3f448888dc
Anders Kaseorg <anders@zulip.com>
2025-09-03 17:53:13 -0700

    lint: Don’t auto-fix with remark unless requested.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -49,4 +49,4 @@ API_FEATURE_LEVEL = 425
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (346, 0)  # bumped 2025-09-04 to remove remark-lint-list-item-indent
+PROVISION_VERSION = (346, 1)  # bumped 2025-09-04 to add remark-cli and friends
