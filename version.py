e5d6c9c82ad3b251d1a9a9aaec361c48b8134f65
Anders Kaseorg <anders@zulip.com>
2025-09-03 17:00:48 -0700

    eslint: Add eslint-plugin-mdx.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -49,4 +49,4 @@ API_FEATURE_LEVEL = 425
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (343, 0)  # bumped 2025-08-28 to move @types/mdast away from dev dependencies.
+PROVISION_VERSION = (343, 1)  # bumped 2025-09-03 to add eslint-plugin-mdx
