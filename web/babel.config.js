7157a424a838c2f4d73b51941296ad48b57b2487
Anders Kaseorg <anders@zulip.com>
2025-09-09 17:19:41 -0700

    babel: Type-check Babel configuration.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- web/babel.config.js
+++ web/babel.config.js
@@ -1,22 +1,20 @@
+// @ts-check
+
+/** @type {import("babel-plugin-formatjs/types").Options} */
+const formatJsOptions = {
+    additionalFunctionNames: ["$t", "$t_html"],
+    overrideIdFn: (_id, defaultMessage) => defaultMessage ?? "",
+};
+
+/** @type {import("@babel/preset-env").Options} */
+const presetEnvOptions = {
+    corejs: "3.45",
+    shippedProposals: true,
+    useBuiltIns: "usage",
+};
+
+/** @type {import("@babel/core").TransformOptions} */
 export default {
-    plugins: [
-        [
-            "formatjs",
-            {
-                additionalFunctionNames: ["$t", "$t_html"],
-                overrideIdFn: (_id, defaultMessage) => defaultMessage,
-            },
-        ],
-    ],
-    presets: [
-        [
-            "@babel/preset-env",
-            {
-                corejs: "3.45",
-                shippedProposals: true,
-                useBuiltIns: "usage",
-            },
-        ],
-        "@babel/typescript",
-    ],
+    plugins: [["formatjs", formatJsOptions]],
+    presets: [["@babel/preset-env", presetEnvOptions], "@babel/typescript"],
 };
