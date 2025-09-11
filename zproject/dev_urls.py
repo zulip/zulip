d1a31b96720e10b37569f73282751bd36faae71b
Shubham Padia <shubham@zulip.com>
2025-09-11 15:19:23 +0000

    help: Add default page to display on /help when help server is off.
    We've copied the button and error colors from portico_signin.css. We did
    not want the new HTML file to depend on portico_signin.css since they
    are unrelated. In addition, having those colors diverge over time might
    not be an issue.
    
    We make the raw mode work with /help and /help/ both.
    
    See https://chat.zulip.org/#narrow/channel/19-documentation/topic/edits.20not.20appearing.20with.20vagrant/near/2257442

--- zproject/dev_urls.py
+++ zproject/dev_urls.py
@@ -19,6 +19,7 @@ from zerver.views.development.dev_login import (
     dev_direct_login,
 )
 from zerver.views.development.email_log import clear_emails, email_page, generate_all_emails
+from zerver.views.development.help import help_dev_mode_view
 from zerver.views.development.integrations import (
     check_send_webhook_fixture_message,
     dev_panel,
@@ -106,6 +107,11 @@ urls = [
     path("devtools/buttons/", showroom_component_buttons),
     path("devtools/banners/", showroom_component_banners),
     path("devtools/inputs/", showroom_component_inputs),
+    # Development server for the help center in not run by default, we
+    # show this page with zulip.com and view source links instead.
+    path("help", help_dev_mode_view),
+    path("help/", help_dev_mode_view),
+    path("help/<path:subpath>", help_dev_mode_view),
 ]
 
 v1_api_mobile_patterns = [
