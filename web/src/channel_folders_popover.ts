1d3323571ea6464ddcf464d0e2700472b3dda8a7
Nirvedh Harpal <harpalnirvedh@gmail.com>
2025-09-11 18:47:14 +0000

    left-sidebar: Add expand/collapse all options to filter menu.
    This adds two new options to the left sidebar filter menu:
    
    - Expand all sections (zulip-icon-expand)
    - Collapse all sections (zulip-icon-collapse)
    
    These appear above the existing "Don't group channels by folder" entry,
    and expand/collapse all sidebar sections (Views, DMs, folders, etc.)
    at once.
    
    Fixes #35884.

--- web/src/channel_folders_popover.ts
+++ web/src/channel_folders_popover.ts
@@ -5,7 +5,10 @@ import type * as tippy from "tippy.js";
 import render_left_sidebar_channels_folder_setting_popover from "../templates/popovers/left_sidebar/left_sidebar_channels_folder_setting_popover.hbs";
 
 import * as channel from "./channel.ts";
+import * as left_sidebar_navigation_area from "./left_sidebar_navigation_area.ts";
+import * as pm_list from "./pm_list.ts";
 import * as popover_menus from "./popover_menus.ts";
+import * as stream_list from "./stream_list.ts";
 import {parse_html} from "./ui_util.ts";
 import {user_settings} from "./user_settings.ts";
 
@@ -21,6 +24,36 @@ function do_change_show_channel_folders(instance: tippy.Instance): void {
     popover_menus.hide_current_popover_if_visible(instance);
 }
 
+function expand_all_sections(instance: tippy.Instance): void {
+    // Expand Views section
+    left_sidebar_navigation_area.force_expand_views();
+
+    // Expand Direct Messages section
+    if (pm_list.is_private_messages_collapsed()) {
+        pm_list.expand();
+    }
+
+    // Expand all channel/stream sections
+    stream_list.expand_all_stream_sections();
+
+    popover_menus.hide_current_popover_if_visible(instance);
+}
+
+function collapse_all_sections(instance: tippy.Instance): void {
+    // Collapse Views section
+    left_sidebar_navigation_area.force_collapse_views();
+
+    // Collapse Direct Messages section
+    if (!pm_list.is_private_messages_collapsed()) {
+        pm_list.close();
+    }
+
+    // Collapse all channel/stream sections
+    stream_list.collapse_all_stream_sections();
+
+    popover_menus.hide_current_popover_if_visible(instance);
+}
+
 export function initialize(): void {
     popover_menus.register_popover_menu("#left-sidebar-search .channel-folders-sidebar-menu-icon", {
         ...popover_menus.left_sidebar_tippy_options,
@@ -30,6 +63,12 @@ export function initialize(): void {
             assert(instance.reference instanceof HTMLElement);
             $popper.one("click", "#left_sidebar_channel_folders", () => {
                 do_change_show_channel_folders(instance);
+            });
+            $popper.one("click", "#left_sidebar_expand_all", () => {
+                expand_all_sections(instance);
+            });
+            $popper.one("click", "#left_sidebar_collapse_all", () => {
+                collapse_all_sections(instance);
             });
         },
         onShow(instance) {
