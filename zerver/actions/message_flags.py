9ca73545af05dab2351d74c612e478a9c35667f4
Anders Kaseorg <anders@zulip.com>
2025-09-19 17:56:37 -0700

    message_flags: Fix type error.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- zerver/actions/message_flags.py
+++ zerver/actions/message_flags.py
@@ -38,7 +38,7 @@ def do_mark_all_as_read(user_profile: UserProfile, *, timeout: float | None = No
 
     # First, we clear mobile push notifications.  This is safer in the
     # event that the below logic times out and we're killed.
-    all_push_message_ids = (
+    all_push_message_ids = list(
         UserMessage.objects.filter(
             user_profile=user_profile,
         )
