665bce3c13c64deff85eb9cc3f159c34041ce796
Evy Kassirer <evy@zulip.com>
2025-09-27 14:30:56 -0700

    transmit: Pass params directly instead of using opts.

--- web/tests/transmit.test.cjs
+++ web/tests/transmit.test.cjs
@@ -158,10 +158,7 @@ run_test("reply_message_stream", ({override}) => {
     server_events_state.queue_id = 66;
     sent_messages.get_new_local_id = () => "99";
 
-    transmit.reply_message({
-        message: stream_message,
-        content,
-    });
+    transmit.reply_message(stream_message, content);
 
     assert.deepEqual(send_message_args, {
         sender_id: 44,
@@ -199,10 +196,7 @@ run_test("reply_message_private", ({override}) => {
     server_events_state.queue_id = 177;
     sent_messages.get_new_local_id = () => "199";
 
-    transmit.reply_message({
-        message: pm_message,
-        content,
-    });
+    transmit.reply_message(pm_message, content);
 
     assert.deepEqual(send_message_args, {
         sender_id: 155,
@@ -221,7 +215,5 @@ run_test("reply_message_errors", () => {
 
     blueslip.expect("error", "unknown message type");
 
-    transmit.reply_message({
-        message: bogus_message,
-    });
+    transmit.reply_message(bogus_message, "");
 });
