5763773e51fdfcfeb42d13a34e6e8d08408d93b2
PieterCK <pieterceka123@gmail.com>
2025-10-01 22:01:03 +0700

    ms_teams_importer: Convert team messages.
    This adds support for converting Microsoft Teams's team messages into
    Zulip's channel messages.
    
    This updates the microsoft teams export fixture file to also include a
    private channel with messages.

--- starlight_help/astro.config.mjs
+++ starlight_help/astro.config.mjs
@@ -515,6 +515,7 @@ export default defineConfig({
                     label: "Import an organization",
                     items: [
                         "import-from-mattermost",
+                        "import-from-microsoft-teams",
                         "import-from-slack",
                         "import-from-rocketchat",
                         "export-your-organization",
