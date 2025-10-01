9a11c1b64b43ea54c33fd3bbd11a99434e94a834
PieterCK <pieterceka123@gmail.com>
2025-10-01 22:09:48 +0700

    microsoft_teams_importer: Convert teams to channels.
    This adds support for converting Microsoft Team's teams into Zulip
    channels.

--- zerver/data_import/microsoft_teams.py
+++ zerver/data_import/microsoft_teams.py
@@ -13,6 +13,7 @@ from zerver.data_import.import_util import (
     ZerverFieldsT,
     build_realm,
     build_recipient,
+    build_stream,
     build_subscription,
     build_user_profile,
     build_zerver_realm,
@@ -25,8 +26,168 @@ from zerver.lib.export import do_common_export_processes
 from zerver.models.recipients import Recipient
 from zerver.models.users import UserProfile
 
+
+@dataclass
+class TeamMetadata:
+    """
+    Microsoft Teams' "teams" is equivalent to Zulip channels.
+    """
+
+    description: str
+    display_name: str
+    visibility: Literal["public", "private"]
+    is_archived: bool
+    zulip_channel_id: int
+    zulip_recipient_id: int
+
+
+AddedTeamsT: TypeAlias = dict[str, TeamMetadata]
+TeamIdToZulipRecipientIdT: TypeAlias = dict[str, int]
 MicrosoftTeamsUserIdToZulipUserIdT: TypeAlias = dict[str, int]
 MicrosoftTeamsFieldsT: TypeAlias = dict[str, Any]
+
+MICROSOFT_TEAMS_DEFAULT_ANNOUNCEMENTS_CHANNEL_NAME = "All company"
+
+
+def add_custom_announcements_channel(
+    realm: dict[str, Any],
+    realm_id: int,
+    default_announcement_channel_id: int | None,
+    microsoft_teams_user_id_to_zulip_user_id: MicrosoftTeamsUserIdToZulipUserIdT,
+) -> None:
+    channel_id = default_announcement_channel_id
+
+    # A team called "All Company" is always included in the export data
+    # regardless of whether the organization actually has or uses it.
+    # In the chance where it is used, we'll use that as the default
+    # announcement channels. If not, create an artificial one.
+    if default_announcement_channel_id is None:
+        recipient_id = NEXT_ID("recipient")
+        channel_id = NEXT_ID("channel")
+        channel = build_stream(
+            # Microsoft Teams export doesn't include teams creation date.
+            float(timezone_now().timestamp()),
+            realm_id,
+            MICROSOFT_TEAMS_DEFAULT_ANNOUNCEMENTS_CHANNEL_NAME,
+            "This is the default group for everyone in the network",
+            channel_id,
+            False,
+            False,
+        )
+        realm["zerver_stream"].append(channel)
+        channel_id = channel["id"]
+
+        recipient = build_recipient(channel_id, recipient_id, Recipient.STREAM)
+        realm["zerver_recipient"].append(recipient)
+
+        all_user_ids = list(microsoft_teams_user_id_to_zulip_user_id.values())
+        for user_id in all_user_ids:
+            sub = build_subscription(
+                recipient_id=recipient_id,
+                user_id=user_id,
+                subscription_id=NEXT_ID("subscription"),
+            )
+            realm["zerver_subscription"].append(sub)
+
+    realm["zerver_realm"][0]["new_stream_announcements_stream"] = channel_id
+    realm["zerver_realm"][0]["zulip_update_announcements_stream"] = channel_id
+    realm["zerver_realm"][0]["signup_announcements_stream"] = channel_id
+    logging.info("Using the channel 'All Company' as default announcements channel.")
+
+
+def convert_teams_to_channels(
+    microsoft_teams_user_id_to_zulip_user_id: MicrosoftTeamsUserIdToZulipUserIdT,
+    realm: dict[str, Any],
+    realm_id: int,
+    teams_data_dir: str,
+) -> AddedTeamsT:
+    team_data_folders = []
+    for f in os.listdir(teams_data_dir):
+        path = os.path.join(teams_data_dir, f)
+        if os.path.isdir(path):
+            team_data_folders.append(f)
+
+    logging.info("######### IMPORTING TEAMS STARTED #########\n")
+
+    # Build teams subscription map
+    raw_teams_subscription_data: dict[str, list[int]] = defaultdict(list)
+    for team_id in team_data_folders:
+        team_members_file_name = f"teamMembers_{team_id}.json"
+        team_members_file_path = os.path.join(teams_data_dir, team_id, team_members_file_name)
+        team_members: list[MicrosoftTeamsFieldsT] = get_data_file(team_members_file_path)
+
+        for member in team_members:
+            zulip_user_id = microsoft_teams_user_id_to_zulip_user_id[member["UserId"]]
+            raw_teams_subscription_data[team_id].append(zulip_user_id)
+
+    # Compile teamsSettings.json and teamsList.json and convert
+    # teams to Zulip channels.
+    teams_list = get_data_file(os.path.join(teams_data_dir, "teamsList.json"))
+    teams_settings = get_data_file(os.path.join(teams_data_dir, "teamsSettings.json"))
+    team_dict: dict[str, Any] = {team["GroupsId"]: team for team in teams_list}
+    default_announcement_channel_id: int | None = None
+    teams_metadata: AddedTeamsT = {}
+    for team_settings in teams_settings:
+        team_id = team_settings.get("Id")
+
+        if not team_id or team_id not in team_dict:  # nocoverage
+            # Normally, each team has corresponding data in both teamsList.json and
+            # teamsSettings.json, so this is likely a very rare edge case.
+            logging.info("A Team settings data is not in the list of exported teams: ")
+            logging.info(str(team_settings))
+            continue
+
+        compiled_team_data: MicrosoftTeamsFieldsT = {**team_dict[team_id], **team_settings}
+
+        channel_id = NEXT_ID("channel")
+        recipient_id = NEXT_ID("recipient")
+
+        channel = build_stream(
+            # Microsoft Teams export doesn't include teams creation date.
+            date_created=float(timezone_now().timestamp()),
+            realm_id=realm_id,
+            name=compiled_team_data["Name"],
+            description=compiled_team_data["Description"] or "",
+            stream_id=channel_id,
+            deactivated=compiled_team_data["IsArchived"],
+            invite_only=compiled_team_data["Visibility"] == "private",
+        )
+        realm["zerver_stream"].append(channel)
+
+        recipient = build_recipient(channel_id, recipient_id, Recipient.STREAM)
+        realm["zerver_recipient"].append(recipient)
+
+        for zulip_user_id in raw_teams_subscription_data[team_id]:
+            sub = build_subscription(
+                recipient_id=recipient_id,
+                user_id=zulip_user_id,
+                subscription_id=NEXT_ID("subscription"),
+            )
+            realm["zerver_subscription"].append(sub)
+
+        if compiled_team_data["Name"] == MICROSOFT_TEAMS_DEFAULT_ANNOUNCEMENTS_CHANNEL_NAME:
+            default_announcement_channel_id = channel_id  # nocoverage
+
+        teams_metadata[team_id] = TeamMetadata(
+            description=compiled_team_data["Description"],
+            display_name=compiled_team_data["DisplayName"],
+            visibility=compiled_team_data["Visibility"],
+            is_archived=compiled_team_data["IsArchived"],
+            zulip_channel_id=channel_id,
+            zulip_recipient_id=recipient_id,
+        )
+
+    # Microsoft Teams doesn't have a default team(channel). The
+    # closest thing to that is "org-wide" teams, but it's practically
+    # indistinguishable from a normal public team once exported.
+    add_custom_announcements_channel(
+        realm=realm,
+        realm_id=realm_id,
+        default_announcement_channel_id=default_announcement_channel_id,
+        microsoft_teams_user_id_to_zulip_user_id=microsoft_teams_user_id_to_zulip_user_id,
+    )
+
+    return teams_metadata
 
 
 @dataclass
@@ -219,12 +380,21 @@ def do_convert_directory(
     realm["zerver_recipient"] = []
     realm["zerver_subscription"] = []
 
-    convert_users(
+    microsoft_teams_user_id_to_zulip_user_id = convert_users(
         microsoft_teams_user_role_data=get_user_roles(microsoft_graph_api_token),
         realm=realm,
         realm_id=realm_id,
         timestamp=int(NOW),
         users_list=get_data_file(os.path.join(users_data_dir, "usersList.json")),
+    )
+
+    teams_data_dir = os.path.join(microsoft_teams_dir, "teams")
+
+    convert_teams_to_channels(
+        microsoft_teams_user_id_to_zulip_user_id=microsoft_teams_user_id_to_zulip_user_id,
+        realm=realm,
+        realm_id=realm_id,
+        teams_data_dir=teams_data_dir,
     )
 
     create_converted_data_files(realm, output_dir, "/realm.json")
