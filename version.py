01d3fd87147bde294179195488835a19fa1a23e2
Anders Kaseorg <anders@zulip.com>
2025-10-01 12:22:36 -0700

    emoji: Remove deprecated Google blobs emoji set.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>
    (cherry picked from commit 85c94599c5c6fd86dae9ac7a4f83b54cd21e369c)

--- version.py
+++ version.py
@@ -49,4 +49,4 @@ API_FEATURE_LEVEL = 421
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (341, 0)  # bumped 2025-09-15 to downgrade sharp
+PROVISION_VERSION = (342, 0)  # bumped 2025-10-01 to remove emoji-datasource-google-blob
