456354575eaf7aef5920da68fe9f8b399a149533
Alex Vandiver <alexmv@zulip.com>
2025-10-02 20:04:55 +0000

    kandra: Add a mirror of #signups to CZO.

--- puppet/kandra/manifests/prod_app_frontend_once.pp
+++ puppet/kandra/manifests/prod_app_frontend_once.pp
@@ -3,6 +3,7 @@ class kandra::prod_app_frontend_once {
   include zulip::hooks::push_git_ref
   include zulip::hooks::zulip_notify
   include kandra::hooks::zulip_notify_schema_diff
+  include kandra::mirror_to_czo
 
   zulip::cron { 'update-first-visible-message-id':
     hour   => '7',
