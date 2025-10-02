b37d6c701286bbc7836288efeefdc79d14d06b3c
PieterCK <pieterceka123@gmail.com>
2025-10-02 17:27:15 +0700

    integrations: Remove Slack from `WEBHOOK_SCREENSHOT_CONFIG`.
    Slack integration requires Slack API key and will make API calls to
    Slack. It'll also trip at `validate_account_and_subdomain` when
    receiving Slack calls from a dev environemnt. So this commit removes it
    from the tool.

--- zerver/lib/integrations.py
+++ zerver/lib/integrations.py
@@ -700,6 +700,8 @@ NO_SCREENSHOT_WEBHOOKS = (
     {"beeminder"}
     # Meta integrations - Docs won't have a screenshot
     | {"ifttt", "slack_incoming", "zapier"}
+    # Integrations that calls external API endpoints.
+    | {"slack"}
 )
 
 hubot_integration_names = {integration.name for integration in HUBOT_INTEGRATIONS}
@@ -848,7 +850,6 @@ WEBHOOK_SCREENSHOT_CONFIG: dict[str, list[WebhookScreenshotConfig]] = {
     "rundeck": [WebhookScreenshotConfig("start.json")],
     "semaphore": [WebhookScreenshotConfig("pull_request.json")],
     "sentry": [WebhookScreenshotConfig("event_for_exception_python.json")],
-    "slack": [WebhookScreenshotConfig("message_with_normal_text.json")],
     "sonarqube": [WebhookScreenshotConfig("error.json")],
     "sonarr": [WebhookScreenshotConfig("sonarr_episode_grabbed.json")],
     "splunk": [WebhookScreenshotConfig("search_one_result.json")],
