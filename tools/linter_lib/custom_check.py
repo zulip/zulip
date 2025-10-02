cd645bca71683d22da8175393259ee758100db1a
Lauryn Menard <lauryn@zulip.com>
2025-10-02 17:58:43 +0200

    templates: Rename template with realm creation form fields.
    Prep for adding support for demo organization registration via
    forms and templates.

--- tools/linter_lib/custom_check.py
+++ tools/linter_lib/custom_check.py
@@ -577,7 +577,7 @@ html_rules: list["Rule"] = [
         "pattern": r'placeholder="[^{#](?:(?!\.com).)+$',
         "description": "`placeholder` value should be translatable.",
         "exclude_line": {
-            ("templates/zerver/realm_creation_form.html", 'placeholder="acme"'),
+            ("templates/zerver/realm_creation_base_form_fields.html", 'placeholder="acme"'),
             ("templates/zerver/slack_import.html", 'placeholder="xoxb-…"'),
         },
         "exclude": {
