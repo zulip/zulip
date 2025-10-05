be791d5c3f834bed1ebfd9dcf3bc8e87ae3ea416
bedo <bedokhaled66@gmail.com>
2025-10-05 17:45:26 +0300

    idempotency: Add backend idempotency logic.
    Fixes #31219
    
    Add backend changes for the new generic idempotency
    system which is used to ensure idempotency
    for non-idempotent operations (e.g. POST). currently,
    applied to sending messages.
    
    Changes:
    
    New IdempotentRequest model.
    
    "idempotent_endpoint" decorator, the 1st entrance to this system:
    1- it inserts a row with the client idempotency key,
    this initially represents an unattempted work.
    2- Catches and caches any non-transient json_error,
    and marks the work as failed.
    
    "idempotent_work_transaction" decorator:
    This what actually ensure idempotency for the actual work
    (e.g. do_send_messages) and to more finely control the
    start/end of a transaction. It also handles concurrency,
    by locking the row during the transaction.
    
    "delete_old_idempotency_rows" file, a django command
    that deletes old idempotency rows, it runs as a cron job.
    
    "test_idempotent_request" file, tests idempotency
    for successful and failed requests. It also tests
    idempotency logic during concurrent requests.
    
    Performance:
    
    successful request:
    -New: 3 extra db queries.
    -Duplicate: 2 extra db queries, but keep in mind that the all
    db queries associated with doing the actual work are already avoided.
    
    Failed request (non-5xx errors):
    New: 2 db queries.
    Duplicate: 2 db queries.

---
+++ zerver/management/commands/delete_old_idempotency_rows.py
@@ -0,0 +1,36 @@
+from argparse import ArgumentParser
+from datetime import timedelta
+from typing import Any
+
+from django.utils.timezone import now as timezone_now
+from typing_extensions import override
+
+from zerver.lib.management import ZulipBaseCommand
+from zerver.models import IdempotentRequest
+
+
+class Command(ZulipBaseCommand):
+    help = """Delete old rows from IdempotentRequest.
+
+    Run as a cron job that runs every day."""
+
+    @override
+    def add_arguments(self, parser: ArgumentParser) -> None:
+        parser.add_argument(
+            "--retention_duration",
+            type=int,
+            default=IdempotentRequest.RETENTION_DURATION_IN_HRS,
+            help="Retention duration in hours; delete rows that are older than this period.",
+        )
+
+    @override
+    def handle(self, *args: Any, **options: Any) -> None:
+        retention_duration = options["retention_duration"]
+
+        expired_rows = IdempotentRequest.objects.filter(
+            timestamp__lte=timezone_now() - timedelta(hours=retention_duration)
+        ).delete()
+
+        print(
+            f"{expired_rows[1].get('zerver.IdempotentRequest', 0)} expired idempotency keys are removed."
+        )
