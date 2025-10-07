800cfc44210ad7ce6e7faa614eceaa9d5a0ce3d6
Anders Kaseorg <anders@zulip.com>
2025-10-07 10:53:16 -0700

    emoji_names: Rebuild with emoji-datasource 16.0.0.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -49,4 +49,4 @@ API_FEATURE_LEVEL = 427
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (352, 0)  # bumped 2025-10-07 to upgrade JavaScript dependencies
+PROVISION_VERSION = (353, 0)  # bumped 2025-10-07 to rebuild emoji_names
