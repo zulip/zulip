10e1e612de3d31410668580d821b95b38b055ee8
ubaidrmn <rehmanubaid2003@gmail.com>
2025-10-14 22:37:50 +0500

    integrations: Add support for linking to job and job run in DBT Cloud.

--- zerver/webhooks/dbt/tests.py
+++ zerver/webhooks/dbt/tests.py
@@ -25,3 +25,14 @@ Job #123 was kicked off from the UI by bwilliams@example.com at <time:2025-10-05
         expected_message = """:cross_mark: Daily Job (dbt build) deployment failed in **Production**.\n
 Job #123 was kicked off from the UI by bwilliams@example.com at <time:2023-01-31T21:14:41Z>."""
         self.check_webhook("job_run_errored", "Example Project", expected_message)
+
+
+class DBTHookWithAccessUrlTests(WebhookTestCase):
+    CHANNEL_NAME = "DBT"
+    URL_TEMPLATE = "/api/v1/external/dbt?&api_key={api_key}&stream={stream}&access_url=https%3A%2F%2Fexample.us1.dbt.com"
+    WEBHOOK_DIR_NAME = "dbt"
+
+    def test_dbt_webhook_with_valid_access_url(self) -> None:
+        expected_message = """:yellow_circle: Daily Job (dbt build) [deployment](https://example.us1.dbt.com/deploy/1/projects/167194/runs/12345) started in **Production**.\n
+[Job #123](https://example.us1.dbt.com/deploy/1/projects/167194/jobs/123) was kicked off from the UI by bwilliams@example.com at <time:2023-01-31T19:28:07Z>."""
+        self.check_webhook("job_run_started", "Example Project", expected_message)
