fd7113c9db3b80538680159f6fa5c44b10667b7c
The Dance <thedance@amamu.io>
2025-10-20 12:13:27 +0100

    video_calls: Add Nextcloud Talk as a video call provider.
    This adds Nextcloud Talk as video call provider option. The implementation
    creates public Nextcloud Talk conversations (roomType=3) via a new
    /json/calls/nextcloud_talk/create endpoint that allow guest access without
    requiring Nextcloud accounts.
    
    It includes backend changes, frontend changes, testing and documentation.
    
    Fixes #24439.
    
    Co-authored-by: Niloth P <20315308+Niloth-p@users.noreply.github.com>

--- zerver/lib/integrations.py
+++ zerver/lib/integrations.py
@@ -995,6 +995,9 @@ VIDEO_CALL_INTEGRATIONS: list[Integration] = [
         "big-blue-button", ["video-calling", "communication"], display_name="BigBlueButton"
     ),
     Integration("jitsi", ["video-calling", "communication"], display_name="Jitsi Meet"),
+    Integration(
+        "nextcloud-talk", ["video-calling", "communication"], display_name="Nextcloud Talk"
+    ),
     Integration("zoom", ["video-calling", "communication"]),
 ]
 
@@ -1162,7 +1165,7 @@ INTEGRATIONS_WITHOUT_SCREENSHOTS = (
     # Outgoing integrations
     | {"email", "onyx"}
     # Video call integrations
-    | {"big-blue-button", "jitsi", "zoom"}
+    | {"big-blue-button", "jitsi", "nextcloud-talk", "zoom"}
     | {
         # these integrations do not send messages
         "giphy",
