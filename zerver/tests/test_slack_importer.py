0714d79c6a677e0f0876a95ef80e702c1343e322
PieterCK <pieterceka123@gmail.com>
2025-10-21 17:41:47 +0700

    slack_importer: Improve uploads processing logic.
    This uses `run_parallel_queue` to download and export uploads while
    processing messages.

--- zerver/tests/test_slack_importer.py
+++ zerver/tests/test_slack_importer.py
@@ -21,6 +21,7 @@ from zerver.actions.create_realm import do_create_realm, get_email_address_visib
 from zerver.actions.create_user import do_create_user
 from zerver.actions.data_import import import_slack_data
 from zerver.data_import.import_util import (
+    UploadFileRequest,
     UploadRecordData,
     ZerverFieldsT,
     build_defaultstream,
@@ -28,6 +29,7 @@ from zerver.data_import.import_util import (
     build_subscription,
     build_usermessages,
     build_zerver_realm,
+    download_and_export_upload_file,
 )
 from zerver.data_import.sequencer import NEXT_ID
 from zerver.data_import.slack import (
@@ -227,6 +229,7 @@ class SlackImporter(ZulipTestCase):
                 domain_name="domain",
                 long_term_idle=set(),
                 convert_slack_threads=convert_slack_threads,
+                do_download_and_export_upload_file=lambda request: None,
             )
 
     @responses.activate
@@ -1347,9 +1350,10 @@ class SlackImporter(ZulipTestCase):
         zerver_usermessage: list[dict[str, Any]] = []
         subscriber_map: dict[int, set[int]] = {}
         added_channels: dict[str, tuple[str, int]] = {"random": ("c5", 1), "general": ("c6", 2)}
+        realm_id = 1
 
         conversion_result = channel_message_to_zerver_message(
-            1,
+            realm_id,
             user_data,
             slack_user_id_to_zulip_user_id,
             slack_recipient_name_to_zulip_recipient_id,
@@ -1361,6 +1365,7 @@ class SlackImporter(ZulipTestCase):
             "domain",
             set(),
             convert_slack_threads=False,
+            do_download_and_export_upload_file=lambda request: None,
         )
         zerver_message = conversion_result.zerver_message
         zerver_usermessage = conversion_result.zerver_usermessage
@@ -1433,7 +1438,8 @@ class SlackImporter(ZulipTestCase):
 
         # Test uploads
         self.assert_length(uploads, 1)
-        self.assertEqual(uploads[0].path, "https://files.slack.com/apple.png")
+        expected_file_name = "apple.png"
+        self.assertRegex(uploads[0].path, rf"{realm_id}/.*/.*/{expected_file_name}")
         self.assert_length(attachment, 1)
         self.assertEqual(attachment[0]["file_name"], "apple.png")
         self.assertEqual(attachment[0]["is_realm_public"], True)
@@ -1729,6 +1735,7 @@ class SlackImporter(ZulipTestCase):
             "domain",
             set(),
             convert_slack_threads=True,
+            do_download_and_export_upload_file=lambda request: None,
         )
 
         zerver_message = conversion_result.zerver_message
@@ -1840,6 +1847,7 @@ by Pieter
                 "domain",
                 output_dir=output_dir,
                 convert_slack_threads=False,
+                do_download_and_export_upload_file=lambda request: None,
                 chunk_size=1,
             )
 
@@ -2025,6 +2033,7 @@ by Pieter
             slack_user_id_to_zulip_user_id=slack_user_id_to_zulip_user_id,
             zerver_attachment=zerver_attachment,
             uploads_list=uploads_list,
+            do_download_and_export_upload_file=lambda request: None,
         )
         self.assert_length(zerver_attachment, 1)
         self.assert_length(uploads_list, 1)
@@ -2704,3 +2713,27 @@ by Pieter
                     "cancel_import": "true",
                 },
             )
+
+    @responses.activate
+    def test_download_and_export_upload_file_bad_requests(self) -> None:
+        request_url = "https://files.slack.com/files-pri/ABC/DEF.png?token=xoxe-AAA-BBB-CCC-DDD"
+
+        error_message = "The requested file could not be found."
+        responses.add(responses.GET, request_url, status=404, json=error_message)
+
+        with self.assertLogs(level="INFO") as logs, self.assertRaises(Exception) as e:
+            download_and_export_upload_file(
+                "",
+                UploadFileRequest(
+                    output_file_path="text.txt",
+                    request_url=request_url,
+                    params=None,
+                    headers=None,
+                    kwargs={},
+                ),
+            )
+        self.assertIn(
+            'INFO:root:HTTP error: 404, Response: "The requested file could not be found."',
+            logs.output,
+        )
+        self.assertEqual("Failed downloading file.", str(e.exception))
