8f3f75c4a9405900eeddeb9738dbf8076c676cd9
PieterCK <pieterceka123@gmail.com>
2025-10-22 16:57:59 +0700

    slack_importer: Move `build_uploads` to import util.
    This is a prep commit to standardize the uploads processing logic across
    all import tools.

--- zerver/data_import/import_util.py
+++ zerver/data_import/import_util.py
@@ -866,3 +866,24 @@ def validate_user_emails_for_import(user_emails: list[str]) -> None:
             f"Invalid email format, please fix the following email(s) and try again: {details}"
         )
         raise ValidationError(error_log)
+
+
+def build_uploads(
+    user_id: int,
+    realm_id: int,
+    email: str,
+    fileinfo: ZerverFieldsT,
+    s3_path: str,
+    uploads_list: list[ZerverFieldsT],
+) -> None:
+    upload = dict(
+        path=fileinfo["url_private"],  # Save Slack's URL here, which is used later while processing
+        realm_id=realm_id,
+        content_type=None,
+        user_profile_id=user_id,
+        last_modified=fileinfo["timestamp"],
+        user_profile_email=email,
+        s3_path=s3_path,
+        size=fileinfo["size"],
+    )
+    uploads_list.append(upload)
