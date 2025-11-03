06a7dca45fa970e68a2905d76399008b9ee232a4
Aman Agrawal <amanagr@zulip.com>
2025-11-03 20:11:37 +0530

    stripe: Upgrade to latest version.

--- version.py
+++ version.py
@@ -50,4 +50,4 @@ API_FEATURE_LEVEL = 430
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (356, 1)  # bumped 2025-11-03 to add uppy/image-editor.
+PROVISION_VERSION = (357, 0)  # bumped 2025-11-04 to upgrade Stripe to latest version
