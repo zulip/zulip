2c543bd17058d521e221c535d785fa71ad84d1f9
apoorvapendse <apoorvavpendse@gmail.com>
2025-11-08 15:20:42 +0530

    zerver: Use `third_party_api_state` instead of `zoom_token`.
    This jsonb field will now contain all the video call provider
    tokens such as access tokens for Zoom along with other
    third party API state.
    
    Signed-off-by: apoorvapendse <apoorvavpendse@gmail.com>

--- zerver/actions/video_calls.py
+++ zerver/actions/video_calls.py
@@ -6,8 +6,8 @@ from zerver.tornado.django_api import send_event_on_commit
 
 @transaction.atomic(durable=True)
 def do_set_zoom_token(user: UserProfile, /, token: dict[str, object] | None) -> None:
-    user.zoom_token = token
-    user.save(update_fields=["zoom_token"])
+    user.third_party_api_state["zoom"] = token
+    user.save(update_fields=["third_party_api_state"])
     send_event_on_commit(
         user.realm,
         dict(type="has_zoom_token", value=token is not None),
