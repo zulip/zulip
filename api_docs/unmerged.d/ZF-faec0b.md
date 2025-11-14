67ffb61d285c2683f4fe019221d52053020f65cf
Vector73 <v.shm.kunal@gmail.com>
2025-11-14 16:28:46 +0000

    settings: Add realm setting to disable channel event messages.
    Adds a realm setting `send_channel_events_messages` to disable
    channel event messages.
    
    Fixes #27928.

---
+++ api_docs/unmerged.d/ZF-faec0b.md
@@ -0,0 +1,3 @@
+* [`POST /register`](/api/register-queue), [`POST /events`](/api/get-events),
+  `PATCH /realm`: Added a new `send_channel_events_messages` realm setting indicating
+  whether channel event messages are sent in the organization.
--- web/src/state_data.ts
+++ web/src/state_data.ts
@@ -481,6 +481,7 @@ export const realm_schema = z.object({
     realm_push_notifications_enabled_end_timestamp: z.nullable(z.number()),
     realm_require_e2ee_push_notifications: z.boolean(),
     realm_require_unique_names: z.boolean(),
+    realm_send_channel_events_messages: z.boolean(),
     realm_send_welcome_emails: z.boolean(),
     realm_signup_announcements_stream_id: z.number(),
     realm_topics_policy: z.enum(["allow_empty_topic", "disable_empty_topic"]),
