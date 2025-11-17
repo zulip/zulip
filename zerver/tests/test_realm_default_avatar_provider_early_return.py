50747a617927d9dd7b5db0eb847029ff6bf0d795
Ved Narvekar <175046711+vednarvekar001@users.noreply.github.com>
2025-11-18 02:13:15 +0530

    lint: Fix new lint errors and clean up formatting issues.

--- zerver/tests/test_realm_default_avatar_provider_early_return.py
+++ zerver/tests/test_realm_default_avatar_provider_early_return.py
@@ -1,3 +1,4 @@
+from zerver.actions.realm_settings import do_change_realm_default_avatar_provider
 from zerver.lib.test_classes import ZulipTestCase
 from zerver.models.realms import get_realm
 
@@ -8,10 +9,6 @@ class RealmDefaultAvatarProviderEarlyReturnTest(ZulipTestCase):
         realm = get_realm("zulip")
 
         current = realm.default_avatar_provider
-
-        from zerver.actions.realm_settings import (
-            do_change_realm_default_avatar_provider,
-        )
 
         do_change_realm_default_avatar_provider(
             realm, current, acting_user=self.example_user("iago")
