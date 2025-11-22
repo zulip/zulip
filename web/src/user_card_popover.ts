2abe96b74320376398ab221d10b27465e3259746
apoorvapendse <apoorvavpendse@gmail.com>
2025-11-22 09:30:31 +0530

    message_feed: Don't trigger popover when selecting a user mention.
    The reason why moving the `e.stopPropagation` call
    shouldn't cause problems is because this event is
    anyways propagated to
    `$("#main_div").on("click", ".messagebox", select_message_function);`
    which also does an early return after doing
    the `mouse_drag.is_drag` check.
    
    Signed-off-by: apoorvapendse <apoorvavpendse@gmail.com>

--- web/src/user_card_popover.ts
+++ web/src/user_card_popover.ts
@@ -694,6 +694,10 @@ function register_click_handlers(): void {
     );
 
     $("#main_div").on("click", ".user-mention", function (this: HTMLElement, e) {
+        e.stopPropagation();
+        if (mouse_drag.is_drag(e)) {
+            return;
+        }
         const id_string = $(this).attr("data-user-id");
         // We fallback to email to handle legacy Markdown that was rendered
         // before we cut over to using data-user-id
@@ -702,7 +706,6 @@ function register_click_handlers(): void {
             return;
         }
         const $row = $(this).closest(".message_row");
-        e.stopPropagation();
         assert(message_lists.current !== undefined);
         const message = message_lists.current.get(rows.id($row));
         assert(message !== undefined);
