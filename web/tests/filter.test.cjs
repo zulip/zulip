9b7f6b5cafd6bdcb77ce7b5ff844ea13bf2146a5
Aman Agrawal <amanagr@zulip.com>
2025-11-25 14:17:20 +0530

    filter: Canonicalize term before checking validity of it.
    This will help us reduce the type of `operands` that we need
    to check for validity and perform necessary operation on
    `operand` before performing validation.

--- web/tests/filter.test.cjs
+++ web/tests/filter.test.cjs
@@ -2092,7 +2092,6 @@ test("is_valid_search_term", () => {
         );
     }
 
-    blueslip.expect("error", "Unexpected search term operator: foo");
     assert.equal(
         Filter.is_valid_search_term({
             operator: "foo",
