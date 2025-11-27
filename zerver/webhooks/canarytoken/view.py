2ecf493bd02ef80a5dab96c506a1acee0be67a00
Niloth P <20315308+Niloth-p@users.noreply.github.com>
2025-11-27 15:32:18 +0530

    webhooks/canarytoken: Use datetime_to_global_time and ISO 8601 syntax.

--- zerver/webhooks/canarytoken/view.py
+++ zerver/webhooks/canarytoken/view.py
@@ -1,9 +1,12 @@
 # Webhooks for external integrations.
 
+from datetime import datetime
+
 from django.http import HttpRequest, HttpResponse
 
 from zerver.decorator import webhook_view
 from zerver.lib.response import json_success
+from zerver.lib.timestamp import datetime_to_global_time
 from zerver.lib.typed_endpoint import JsonBodyPayload, typed_endpoint
 from zerver.lib.validator import WildValue, check_string
 from zerver.lib.webhooks.common import OptionalUserSpecifiedTopicStr, check_send_webhook_message
@@ -29,9 +32,11 @@ def api_canarytoken_webhook(
     """
     topic_name = "canarytoken alert"
     time = message["time"].tame(check_string)
-    formatted_time = time.replace("(UTC)", "").strip() + "Z"
+    formatted_time = datetime_to_global_time(
+        datetime.fromisoformat(time.replace("(UTC)", "").strip() + "+00:00")
+    )
     body = (
-        f"**:alert: Canarytoken has been triggered on <time:{formatted_time}>!**\n\n"
+        f"**:alert: Canarytoken has been triggered on {formatted_time}!**\n\n"
         f"{message['memo'].tame(check_string)} \n\n"
         f"[Manage this canarytoken]({message['manage_url'].tame(check_string)})"
     )
