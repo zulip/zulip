e2f5d80a8c4ecd7958c033251b3e0f7b175186d8
Pratik Chanda <pratikchanda2000@gmail.com>
2025-12-04 18:31:10 +0530

    banners: Change how banners are closed after specified `remove_after`.
    We set an individual timer on $banner for closing it instead of
    earlier having setTimeout on banner container. This helps in resetting
    the banner removal when a new banner replaces the old banner. The
    new banner gets removed after `remove_after` rather than the remaining
    time from when old banner was added.

--- web/src/channel_folders_ui.ts
+++ web/src/channel_folders_ui.ts
@@ -233,7 +233,7 @@ function render_channel_list(streams: StreamSubscription[], folder_id: number): 
         );
 
         function on_success(): void {
-            banners.open(
+            banners.open_and_close(
                 {
                     intent: "success",
                     label: $t_html({
@@ -254,7 +254,7 @@ function render_channel_list(streams: StreamSubscription[], folder_id: number): 
                 }),
                 xhr,
             );
-            banners.open(
+            banners.open_and_close(
                 {
                     intent: "danger",
                     label: error_message,
@@ -355,7 +355,7 @@ function render_add_channel_folder_widget(): void {
         assert(folder_id !== undefined);
         function on_success(): void {
             reset_add_channel_widget();
-            banners.open(
+            banners.open_and_close(
                 {
                     intent: "success",
                     label: $t_html({
@@ -376,7 +376,7 @@ function render_add_channel_folder_widget(): void {
                 }),
                 xhr,
             );
-            banners.open(
+            banners.open_and_close(
                 {
                     intent: "danger",
                     label: error_message,
