8c3c0e2ba146038891a18551a09392f466f0b3b2
Sahil Batra <sahil@zulip.com>
2025-12-04 17:03:39 +0530

    bots: Fetch bot API key before opening zuliprc and API key modal.

--- web/src/user_profile.ts
+++ web/src/user_profile.ts
@@ -1127,7 +1127,9 @@ export function show_edit_bot_info_modal(user_id: number, $container: JQuery): v
             e.preventDefault();
             e.stopPropagation();
             assert(bot !== undefined);
-            bot_helper.show_api_key_modal(bot.user_id);
+            void (async () => {
+                await bot_helper.show_api_key_modal(bot.user_id);
+            })();
         });
     }
 }
