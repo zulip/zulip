df731e3de9263b5256b0bf995397d77cd19781d1
Ankit Raj <ankitisop12@gmail.com>
2025-12-06 11:00:09 +0000

    digest: Inline CSS for developer preview.
    Fixes #36800.

--- zerver/views/digest.py
+++ zerver/views/digest.py
@@ -1,21 +1,23 @@
-import time
-from datetime import timedelta
-
-from django.conf import settings
+import css_inline
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
-from django.utils.timezone import now as timezone_now
+from django.template.loader import render_to_string
 
 from zerver.decorator import zulip_login_required
 from zerver.lib.digest import DIGEST_CUTOFF, get_digest_context
+from zerver.models import UserProfile
 
 
 @zulip_login_required
 def digest_page(request: HttpRequest) -> HttpResponse:
-    user_profile = request.user
-    assert user_profile.is_authenticated
-    cutoff = time.mktime((timezone_now() - timedelta(days=DIGEST_CUTOFF)).timetuple())
+    user = request.user
+    assert isinstance(user, UserProfile)
 
-    context = get_digest_context(user_profile, cutoff)
-    context.update(physical_address=settings.PHYSICAL_ADDRESS)
-    return render(request, "zerver/digest_base.html", context=context)
+    cutoff = DIGEST_CUTOFF
+
+    context = get_digest_context(user, cutoff)
+
+    raw_html = render_to_string("zerver/emails/digest.html", context, request=request)
+
+    inlined_html = css_inline.inline(raw_html)
+
+    return HttpResponse(inlined_html)
