424f2a531df696bc6cde58a02c1c3d994b635c33
apoorvapendse <apoorvavpendse@gmail.com>
2025-12-06 09:33:32 +0530

    muted_users: Introduce a "Hide" button for revealed messages.
    We get rid of the message action popover menu
    item and instead switch to a "Hide" button
    to hide a revealed message sent by a muted user.
    
    Signed-off-by: apoorvapendse <apoorvavpendse@gmail.com>

--- web/src/click_handlers.ts
+++ web/src/click_handlers.ts
@@ -167,6 +167,11 @@ export function initialize(): void {
             return true;
         }
 
+        // Hide button for a revealed message sent by a muted user.
+        if ($target.closest(".rehide-muted-user-message").length > 0) {
+            return true;
+        }
+
         return false;
     }
 
@@ -263,6 +268,20 @@ export function initialize(): void {
         message_lists.current.view.reveal_hidden_message(message_id);
         e.stopPropagation();
         e.preventDefault();
+    });
+
+    $("#main_div").on("click", ".rehide-muted-user-message", (e) => {
+        const message_id = Number($(e.currentTarget).attr("data-message-id"));
+        assert(message_lists.current !== undefined);
+        const $row = message_lists.current.get_row(message_id);
+        const message = message_lists.current.get(rows.id($row));
+        assert(message !== undefined);
+        const message_container = message_lists.current.view.message_containers.get(message.id);
+        assert(message_container !== undefined);
+        assert(!message_container.is_hidden);
+        message_lists.current.view.hide_revealed_message(message_id);
+        e.preventDefault();
+        e.stopPropagation();
     });
 
     $("#main_div").on("click", "a.stream", function (this: HTMLAnchorElement, e) {
