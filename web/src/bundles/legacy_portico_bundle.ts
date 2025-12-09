9c7e15bd165e6160eb8d27cd8db64239b41d1a59
Karl Stolley <karl@zulip.com>
2025-12-09 15:17:46 -0600

    portico: Establish and reference legacy portico styles bundle.

--- web/src/bundles/legacy_portico_bundle.ts
+++ web/src/bundles/legacy_portico_bundle.ts
@@ -4,5 +4,5 @@ import "../portico/google-analytics.ts";
 import "../portico/portico_modals.ts";
 import "../portico/tippyjs.ts";
 import "../../third/bootstrap/css/bootstrap.portico.css";
-import "../../styles/portico/portico_styles.css";
+import "../../styles/portico/legacy_portico_styles_bundle.css";
 import "tippy.js/dist/tippy.css";
