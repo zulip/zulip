3fa813dfaca192c38c32c572f21f8561b3cd5de9
Niloth P <20315308+Niloth-p@users.noreply.github.com>
2025-12-11 06:15:36 +0530

    integrations: Rebrand Clubhouse webhook integration to Shortcut.
    Fixes #22731.

--- zerver/lib/integrations.py
+++ zerver/lib/integrations.py
@@ -572,9 +572,6 @@ INCOMING_WEBHOOK_INTEGRATIONS: list[IncomingWebhookIntegration] = [
         display_name="CircleCI",
     ),
     IncomingWebhookIntegration(
-        "clubhouse", ["project-management"], [WebhookScreenshotConfig("story_create.json")]
-    ),
-    IncomingWebhookIntegration(
         "codeship",
         ["continuous-integration", "deployment"],
         [WebhookScreenshotConfig("error_build.json")],
@@ -846,6 +843,12 @@ INCOMING_WEBHOOK_INTEGRATIONS: list[IncomingWebhookIntegration] = [
     ),
     IncomingWebhookIntegration(
         "sentry", ["monitoring"], [WebhookScreenshotConfig("event_for_exception_python.json")]
+    ),
+    IncomingWebhookIntegration(
+        "shortcut",
+        ["project-management"],
+        [WebhookScreenshotConfig("story_create.json")],
+        legacy_names=["clubhouse"],
     ),
     IncomingWebhookIntegration(
         "slack",
