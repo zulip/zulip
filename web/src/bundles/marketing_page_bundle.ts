da05f556b781ddff8a49f13c46ad5978c44d92ff
Karl Stolley <karl@zulip.com>
2025-12-15 15:17:27 -0600

    portico: Create marketing-page bundle.

---
+++ web/src/bundles/marketing_page_bundle.ts
@@ -0,0 +1,8 @@
+import "./common.ts";
+import "../portico/header.ts";
+import "../portico/google-analytics.ts";
+// These are select stylesheets loaded in legacy areas
+// by the legacy_portico_styles_bundle.css file
+import "../../styles/portico/markdown.css";
+import "../../styles/portico/navbar.css";
+import "../../styles/portico/footer.css";
