60878b84a6f6c43e761e27fdba857dcdef61b7a1
Lauryn Menard <lauryn@zulip.com>
2025-12-16 17:16:10 +0100

    demo-orgs: Do not send deactivated realm email if no owner email.
    If the demo organization owner deactivates the realm before they
    configure an email address, don't send the deactivated realm email
    when doing the deactivation.

--- zerver/tests/test_realm.py
+++ zerver/tests/test_realm.py
@@ -563,6 +563,34 @@ class RealmTest(ZulipTestCase):
         self.assertEqual(confirmation.content_object, obj)
         self.assertEqual(confirmation.realm, realm)
 
+    def test_realm_deactivation_demo_organization_owner_email_not_configured(self) -> None:
+        demo_name = "demo deactivate"
+        result = self.submit_demo_creation_form(demo_name)
+        realm = Realm.objects.filter(name=demo_name).latest("date_created")
+        self.assertEqual(result.status_code, 302)
+        self.assertTrue(
+            result["Location"].startswith(
+                f"http://{realm.string_id}.testserver/accounts/login/subdomain"
+            )
+        )
+        self.assertIsNotNone(realm.demo_organization_scheduled_deletion_date)
+
+        result = self.client_get(result["Location"], subdomain=realm.string_id)
+        self.assertEqual(result.status_code, 302)
+        self.assertEqual(result["Location"], f"http://{realm.string_id}.testserver")
+
+        demo_owner_account = realm.get_first_human_user()
+        assert demo_owner_account is not None
+        self.assert_logged_in_user_id(demo_owner_account.id)
+        self.assertEqual(demo_owner_account.delivery_email, "")
+
+        result = self.client_post(
+            "/json/realm/deactivate", {"deletion_delay_days": 17}, subdomain=realm.subdomain
+        )
+        self.assert_json_success(result)
+        self.assert_length(mail.outbox, 0)
+        self.assert_logged_in_user_id(None)
+
     def test_do_send_realm_deactivation_email_no_acting_user(self) -> None:
         realm = get_realm("zulip")
         do_deactivate_realm(
