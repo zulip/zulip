31df51a44f1aa834466cfcd25aad887444ae11e7
The Dance <thedance@amamu.io>
2025-12-17 14:29:05 +0000

    test: Add helper function set_full_name.
    Refactor tests to use a helper method for changing user name.
    Hardcodes acting_user=None, treating the change as done by
    a management command, not another user.

--- zerver/lib/test_classes.py
+++ zerver/lib/test_classes.py
@@ -50,7 +50,7 @@ from zerver.actions.custom_profile_fields import do_update_user_custom_profile_d
 from zerver.actions.message_send import check_send_message, check_send_stream_message
 from zerver.actions.realm_settings import do_change_realm_permission_group_setting
 from zerver.actions.streams import bulk_add_subscriptions, bulk_remove_subscriptions
-from zerver.actions.user_settings import do_change_user_setting
+from zerver.actions.user_settings import do_change_full_name, do_change_user_setting
 from zerver.actions.users import do_change_user_role
 from zerver.decorator import do_two_factor_login
 from zerver.lib.cache import bounce_key_prefix_for_testing
@@ -2392,6 +2392,9 @@ class ZulipTestCase(ZulipTestCaseMixin, TestCase):
         self, user_profile: UserProfile, data: list[ProfileDataElementUpdateDict]
     ) -> None:
         do_update_user_custom_profile_data_if_changed(user_profile, data)
+
+    def set_full_name(self, user_profile: UserProfile, full_name: str) -> None:
+        do_change_full_name(user_profile, full_name, acting_user=None)
 
 
 def get_row_pks_in_all_tables() -> Iterator[tuple[str, set[int]]]:
