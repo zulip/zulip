9d5f340c058eb507b1f00b1497ab04d5b15d4df4
Karl Stolley <karl@zulip.com>
2025-12-19 11:57:01 -0600

    marketing_page: Clean up legacy portico header cruft.

--- web/src/bundles/legacy_portico_bundle.ts
+++ web/src/bundles/legacy_portico_bundle.ts
@@ -1,5 +1,5 @@
 import "./common.ts";
-import "../portico/header.ts";
+import "../portico/legacy_header.ts";
 import "../portico/google-analytics.ts";
 import "../portico/portico_modals.ts";
 import "../portico/tippyjs.ts";
