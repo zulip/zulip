a9ad8bbfff3d79664b5d0c537f6759450f78f41f
Steve Howell <showell30@yahoo.com>
2025-12-27 21:42:17 -0500

    wip

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
-import * as h from "./html";
- 
+import * as pure_dom from "./pure_dom";
+
 const template = `
 <i class="buddy-list-section-toggle zulip-icon zulip-icon-heading-triangle-right {{#if is_collapsed}}rotate-icon-right{{else}}rotate-icon-down{{/if}}" aria-hidden="true"></i>
 <h5 id="{{id}}" class="buddy-list-heading no-style hidden-for-spectators">
@@ -19,53 +19,7 @@ function test(info: {
     header_text: string,
     is_collapsed: boolean,
 }): void {
-    const { is_collapsed } = info;
-
-    const rotation_class = new h.TrustedIfElseString(
-        new h.Bool("is_collapsed", is_collapsed),
-        new h.TrustedSimpleString("rotate-icon-right"),
-        new h.TrustedSimpleString("rotate-icon-left"),
-    )
-
-    const section_icon = h.i_tag({
-        class_first: true,
-        classes: [
-            new h.TrustedSimpleString("buddy-list-section-toggle"),
-            new h.TrustedSimpleString("zulip-icon"),
-            new h.TrustedSimpleString("zulip-icon-heading-triangle-right"),
-            rotation_class,
-        ],
-        attrs: [
-            new h.Attr("aria-hidden", new h.TrustedSimpleString("true")),
-        ]
-    });
-
-    const h5 = h.h5_tag({
-        class_first: false,
-        classes: [
-            new h.TrustedSimpleString("buddy-list-heading"),
-            new h.TrustedSimpleString("hidden-for-spectators"),
-        ],
-        attrs: [
-            new h.Attr("id", h.trusted_var("id")),
-        ],
-    });
-
-    const user_count_outer_span = h.span_tag({
-        class_first: true,
-        classes: [
-            new h.TrustedSimpleString("buddy-list-heading-user-count-with-parens"),
-            new h.TrustedSimpleString("hide"),
-        ],
-        attrs: [],
-    });
-
-    const result = new h.Block([
-        section_icon,
-        h5,
-        user_count_outer_span,
-    ])
-
+    const result = pure_dom.buddy_list_section_header(info)
     p(result.to_source());
 
     // other stuff
@@ -75,5 +29,5 @@ function test(info: {
     // return render_section_header(info);
 }
 
-test({id: "x", header_text: "y", is_collapsed: false});
+test({id: "some_id", header_text: "HEADER TEXT", is_collapsed: false});
 
