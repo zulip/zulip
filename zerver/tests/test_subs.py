5bbb39a06cbdc919fe678cc69461c5e5ee7e7899
Sahil Batra <sahil@zulip.com>
2025-12-30 11:47:38 +0530

    streams: Do not prefetch group settings using select_related.
    We pre-fetched can_send_message_group and can_create_topic_group
    settings using select_related in get_stream_by_id_for_sending_message
    and get_stream_by_name_for_sending_message for saving DB queries
    when checking permission to send and edit messages.
    
    This commit updates the permission checking code to work by getting
    the recursive group memberships of the user, and then just comparing
    those with the IDs on the Stream object. After this change we do
    not need to prefetch those settings and can make the code efficient
    by avoiding generating lot of SQL which took time to parse.
    
    This commit also removes get_stream_by_name_for_sending_message,
    get_stream_by_id_for_sending_message and
    access_stream_by_id_for_message because these functions were
    added as other functions did not prefetch the group settings.
    But we do not prefetch group settings anymore, so there is no
    need of these message-specific fucntions.
    
    Fixes #36875.

--- zerver/tests/test_subs.py
+++ zerver/tests/test_subs.py
@@ -4041,7 +4041,7 @@ class SubscriptionAPITest(ZulipTestCase):
         streams_to_sub = ["multi_user_stream"]
         with (
             self.capture_send_event_calls(expected_num_events=5) as events,
-            self.assert_database_query_count(44),
+            self.assert_database_query_count(45),
         ):
             self.subscribe_via_post(
                 self.test_user,
@@ -4950,7 +4950,7 @@ class SubscriptionAPITest(ZulipTestCase):
         ]
 
         # Test creating a public stream when realm does not have a notification stream.
-        with self.assert_database_query_count(44):
+        with self.assert_database_query_count(45):
             self.subscribe_via_post(
                 self.test_user,
                 [new_streams[0]],
@@ -4958,7 +4958,7 @@ class SubscriptionAPITest(ZulipTestCase):
             )
 
         # Test creating private stream.
-        with self.assert_database_query_count(52):
+        with self.assert_database_query_count(53):
             self.subscribe_via_post(
                 self.test_user,
                 [new_streams[1]],
@@ -4970,7 +4970,7 @@ class SubscriptionAPITest(ZulipTestCase):
         new_stream_announcements_stream = get_stream(self.streams[0], self.test_realm)
         self.test_realm.new_stream_announcements_stream_id = new_stream_announcements_stream.id
         self.test_realm.save()
-        with self.assert_database_query_count(56):
+        with self.assert_database_query_count(58):
             self.subscribe_via_post(
                 self.test_user,
                 [new_streams[2]],
