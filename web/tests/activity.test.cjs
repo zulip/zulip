9e147261513248b5f9cc04fb2b7f9c4dccfbbccb
Steve Howell <showell30@yahoo.com>
2026-01-01 05:11:14 -0500

    node tests: Move direct_message_group_data test.
    The TODO is TO-DONE.

--- web/tests/activity.test.cjs
+++ web/tests/activity.test.cjs
@@ -33,7 +33,6 @@ const scroll_util = mock_esm("../src/scroll_util");
 
 set_global("document", _document);
 
-const direct_message_group_data = zrequire("direct_message_group_data");
 const muted_users = zrequire("muted_users");
 const presence = zrequire("presence");
 const people = zrequire("people");
@@ -153,53 +152,6 @@ run_test("reload_defaults", () => {
     activity_ui.clear_for_testing();
     blueslip.expect("warn", "get_filter_text() is called before initialization");
     assert.equal(activity_ui.get_filter_text(), "");
-});
-
-test("direct_message_group_data.process_loaded_messages", () => {
-    // TODO: move this to a module for just testing `direct_message_group_data`
-
-    const direct_message_group1 = "jill@zulip.com,norbert@zulip.com";
-    const timestamp1 = 1382479029; // older
-
-    const direct_message_group2 = "alice@zulip.com,fred@zulip.com";
-    const timestamp2 = 1382479033; // newer
-
-    const old_timestamp = 1382479000;
-
-    const messages = [
-        {
-            type: "private",
-            display_recipient: [{id: jill.user_id}, {id: norbert.user_id}],
-            timestamp: timestamp1,
-        },
-        {
-            type: "stream",
-        },
-        // direct message to myself
-        {
-            type: "private",
-            display_recipient: [{id: me.user_id}],
-        },
-        {
-            type: "private",
-            display_recipient: [{id: alice.user_id}, {id: fred.user_id}],
-            timestamp: timestamp2,
-        },
-        {
-            type: "private",
-            display_recipient: [{id: fred.user_id}, {id: alice.user_id}],
-            timestamp: old_timestamp,
-        },
-    ];
-
-    direct_message_group_data.process_loaded_messages(messages);
-
-    const user_ids_string1 = people.emails_strings_to_user_ids_string(direct_message_group1);
-    const user_ids_string2 = people.emails_strings_to_user_ids_string(direct_message_group2);
-    assert.deepEqual(direct_message_group_data.get_direct_message_groups(), [
-        user_ids_string2,
-        user_ids_string1,
-    ]);
 });
 
 test("presence_list_full_update", ({override, mock_template}) => {
