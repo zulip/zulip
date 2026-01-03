1ea491359147d1ed7c48fd7914ccb68bab201664
Anders Kaseorg <anders@zulip.com>
2026-01-02 17:25:40 -0800

    install-uv: Upgrade uv from 0.9.12 to 0.9.21.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>
    (cherry picked from commit 60969072637724d83968e814d8c1ba53afad0ad4)

--- version.py
+++ version.py
@@ -50,4 +50,4 @@ API_FEATURE_LEVEL = 421
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (345, 0)  # bumped 2025-11-25 to upgrade Python requirements
+PROVISION_VERSION = (345, 1)  # bumped 2026-01-02 to upgrade uv
