1e52eb941c1d3d65e282acad11e83b12c6a5d328
Pratik Chanda <pratikchanda2000@gmail.com>
2026-01-03 22:00:06 +0530

    message_link: Fix message link not copying.
    In message action popover and hotkey `L`, message link copy is not
    invoking the clipboard function.
    In b75ac64debd09463962bf194470e8d0f34e694c5, it was rewritten to
    use async/await, but the usage in hotkey `L` and message action
    popover was never invoking the function and simply creating it.
    
    We change this to invoke immediately on creation.

--- web/src/hotkey.ts
+++ web/src/hotkey.ts
@@ -1503,7 +1503,7 @@ function process_hotkey(e: JQuery.KeyDownEvent, hotkey: Hotkey): boolean {
                     show_copied_confirmation(util.the($message_time), {
                         custom_content: $t({defaultMessage: "Message link copied!"}),
                     });
-                });
+                })();
                 return true;
             }
             return false;
