96d7c5a2e0256a12036c13604caa0c3eb7ce2012
apoorvapendse <apoorvavpendse@gmail.com>
2026-01-06 13:49:07 +0530

    gifs: Introduce fallback placement options for GIFs.
    When trying to open the GIF picker in the edit textarea,
    the `top` placement of the picker causes it to go out
    of the viewport which can cause glitchy bugs including
    automatically closing the picker. Introducing fallback
    options for placments helps us to deal with this case.
    
    Signed-off-by: apoorvapendse <apoorvavpendse@gmail.com>

--- web/src/gif_picker_ui.ts
+++ web/src/gif_picker_ui.ts
@@ -172,6 +172,20 @@ function toggle_picker_popover(target: HTMLElement): void {
         {
             theme: "popover-menu",
             placement: "top",
+            popperOptions: {
+                modifiers: [
+                    {
+                        // The placement is set to top by default, and we use
+                        // bottom and left configurations as fallback, which is
+                        // useful for scenarios when opening the picker while editing
+                        // messages near the top of the viewport.
+                        name: "flip",
+                        options: {
+                            fallbackPlacements: ["bottom", "left"],
+                        },
+                    },
+                ],
+            },
             onCreate(instance) {
                 const provider = network.get_provider();
                 instance.setContent(
