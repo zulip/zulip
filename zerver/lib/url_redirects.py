42e32956f3fa42c94bac9f1c027738737a8f75f5
Lauryn Menard <lauryn@zulip.com>
2026-01-09 18:09:13 +0100

    help: Make help center canonical location for bots/integrations.
    The integrations-overview page was duplicated between the help
    center and the API docs, so we remove the API doc file and redirect
    to the help center.
    
    As all the other integrations and interactive bots pages have been
    migrated from the API docs to either the help center or RTD, we
    remove those links from the API docs sidebar.

--- zerver/lib/url_redirects.py
+++ zerver/lib/url_redirects.py
@@ -20,6 +20,7 @@ REDIRECTED_TO_HELP_DOCUMENTATION: list[URLRedirect] = [
     URLRedirect("/api/interactive-bots-api", "/help/interactive-bots-api"),
     URLRedirect("/api/writing-bots", "/help/writing-bots"),
     URLRedirect("/api/non-webhook-integrations", "/help/non-webhook-integrations"),
+    URLRedirect("/api/integrations-overview", "/help/integrations-overview"),
 ]
 
 API_DOCUMENTATION_REDIRECTS: list[URLRedirect] = [
