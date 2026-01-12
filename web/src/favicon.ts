3a6b2aebd89a412c53c2a3ef65a7d47323dc90bc
prac-chi <chyy.prachi@gmail.com>
2026-01-12 05:04:58 +0000

    favicon: Prevent notification dot during app initialization.
    Fixes #30163.

--- web/src/favicon.ts
+++ web/src/favicon.ts
@@ -5,6 +5,7 @@ import render_favicon_svg from "../templates/favicon.svg.hbs";
 
 import * as blueslip from "./blueslip.ts";
 import favicon_font_url_html from "./favicon_font_url!=!url-loader!font-subset-loader2?glyphs=0123456789KMGT∞!source-sans/TTF/SourceSans3-Bold.ttf"; // eslint-disable-line import/extensions
+import {page_params} from "./page_params.ts";
 
 let favicon_state: {image: HTMLImageElement; url: string} | undefined;
 
@@ -14,28 +15,29 @@ function load_and_set_favicon(rendered_favicon: string): void {
         image: new Image(),
     };
 
-    // Without loading the SVG in an Image first, Chrome mysteriously fails to
-    // render the webfont (https://crbug.com/1140920).
     favicon_state.image.src = favicon_state.url;
     favicon_state.image.addEventListener("load", set_favicon);
 }
+
 function set_favicon(): void {
     if (favicon_state === undefined) {
         throw new Error("Programming error: favicon_state must be set.");
     }
     $("#favicon").attr("href", favicon_state.url);
 }
+
 export function update_favicon(new_message_count: number, pm_count: number): void {
     try {
+        // If the app is still loading, force the static icon and stop.
+        // We use bracket notation to satisfy the TypeScript index signature rule.
+        if (page_params["is_loading"]) {
+            $("#favicon").attr("href", static_favicon_image);
+            return;
+        }
+
         if (favicon_state !== undefined) {
             favicon_state.image.removeEventListener("load", set_favicon);
-
-            // We need to remove this src so revokeObjectURL doesn't cause a
-            // net::ERR_FILE_NOT_FOUND error in Chrome. This seems to be the
-            // simplest way to do that without triggering an expensive network
-            // request or spewing a different console error.
             favicon_state.image.src = "data:,";
-
             URL.revokeObjectURL(favicon_state.url);
             favicon_state = undefined;
         }
--- web/src/page_params.ts
+++ web/src/page_params.ts
@@ -4,6 +4,7 @@ import {page_params as base_page_params} from "./base_page_params.ts";
 
 assert(base_page_params.page_type === "home");
 
-// We need to export with a narrowed TypeScript type.
+// We use this comment so it can ignore the ignore the export  style
+// so that we don't break the Types for the rest of the app.
 // eslint-disable-next-line unicorn/prefer-export-from
 export const page_params = base_page_params;
