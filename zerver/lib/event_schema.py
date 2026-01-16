694827bf3851a9bb013c5df5912278eb23e54c93
apoorvapendse <apoorvavpendse@gmail.com>
2026-01-16 20:21:55 +0530

    integrations: Add backend for Webex video call integration.
    Signed-off-by: apoorvapendse <apoorvavpendse@gmail.com>
    Co-authored-by: Lauryn Menard <lauryn@zulip.com>

--- zerver/lib/event_schema.py
+++ zerver/lib/event_schema.py
@@ -37,6 +37,7 @@ from zerver.lib.event_types import (
     EventDraftsAdd,
     EventDraftsRemove,
     EventDraftsUpdate,
+    EventHasWebexToken,
     EventHasZoomToken,
     EventHeartbeat,
     EventInvitesChanged,
@@ -252,6 +253,7 @@ check_web_reload_client_event = make_checker(EventWebReloadClient)
 _check_channel_folder_update = make_checker(EventChannelFolderUpdate)
 _check_delete_message = make_checker(EventDeleteMessage)
 _check_has_zoom_token = make_checker(EventHasZoomToken)
+_check_has_webex_token = make_checker(EventHasWebexToken)
 _check_legacy_presence = make_checker(EventLegacyPresence)
 _check_modern_presence = make_checker(EventModernPresence)
 _check_muted_topics = make_checker(EventMutedTopics)
@@ -330,6 +332,15 @@ def check_has_zoom_token(
     value: bool,
 ) -> None:
     _check_has_zoom_token(var_name, event)
+    assert event["value"] == value
+
+
+def check_has_webex_token(
+    var_name: str,
+    event: dict[str, object],
+    value: bool,
+) -> None:
+    _check_has_webex_token(var_name, event)
     assert event["value"] == value
 
 
