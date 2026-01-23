17fe03cce47333c8b9ec3704427d9c50eaefe34b
Sahil Batra <sahil@zulip.com>
2026-01-23 13:06:17 +0530

    templates: Do not use arrays for truthiness check.
    After changes in ea3cbd5, arrays cannot be used directly
    for truthiness check in if block, we should always use
    array length.

--- web/src/message_list_view.ts
+++ web/src/message_list_view.ts
@@ -788,7 +788,7 @@ export class MessageListView {
         for (const message of messages) {
             const message_reactions = reactions.get_message_reactions(message);
             message.message_reactions = message_reactions;
-            message.reminders = message_reminder.get_reminders(message.id);
+            message.reminders = message_reminder.get_reminders(message.id) ?? [];
 
             // These will be used to build the message container
             let include_recipient = false;
@@ -1083,7 +1083,8 @@ export class MessageListView {
     _get_message_template(message_container: MessageContainer): string {
         const msg_reactions = reactions.get_message_reactions(message_container.msg);
         message_container.msg.message_reactions = msg_reactions;
-        message_container.msg.reminders = message_reminder.get_reminders(message_container.msg.id);
+        message_container.msg.reminders =
+            message_reminder.get_reminders(message_container.msg.id) ?? [];
         let invite_only;
         let is_web_public;
         let is_archived;
