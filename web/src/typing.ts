5fac7a4bf9ecb476889f9f81f21d744f4ae22da0
Aman Agrawal <amanagr@zulip.com>
2026-01-29 13:42:46 +0530

    typing: Prevent `input` event from being called many times during paste.
    Note this only provides minor performance improvement during a paste.

--- web/src/typing.ts
+++ web/src/typing.ts
@@ -1,4 +1,5 @@
 import $ from "jquery";
+import _ from "lodash";
 import assert from "minimalistic-assert";
 
 import * as blueslip from "./blueslip.ts";
@@ -234,17 +235,21 @@ export function initialize(): void {
         notify_server_editing_stop,
     };
 
-    $(document).on("input", "#compose-textarea", () => {
-        // If our previous state was no typing notification, send a
-        // start-typing notice immediately.
-        const new_recipient = is_valid_conversation() ? get_recipient() : null;
-        typing_status.update(
-            worker,
-            new_recipient,
-            realm.server_typing_started_wait_period_milliseconds,
-            realm.server_typing_stopped_wait_period_milliseconds,
-        );
-    });
+    $(document).on(
+        "input",
+        "#compose-textarea",
+        _.throttle(() => {
+            // If our previous state was no typing notification, send a
+            // start-typing notice immediately.
+            const new_recipient = is_valid_conversation() ? get_recipient() : null;
+            typing_status.update(
+                worker,
+                new_recipient,
+                realm.server_typing_started_wait_period_milliseconds,
+                realm.server_typing_stopped_wait_period_milliseconds,
+            );
+        }, 25),
+    );
 
     $("body").on("input", ".message_edit_content", function (this: HTMLElement) {
         const $message_row = $(this).closest(".message_row");
