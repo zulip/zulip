f112f733090c70aa46b3d34461b48763a96d2b4f
Pritesh Shetty <priteshshetty89@gmail.com>
2026-02-01 09:09:15 +0000

    webhooks/github: Add emojis for PR review states and close actions.
    Fixes part of #37250.
    
    Co-authored-by: hassan <ms09070@st.habib.edu.pk>

--- zerver/webhooks/github/view.py
+++ zerver/webhooks/github/view.py
@@ -70,6 +70,19 @@ CHECK_RUN_CONCLUSION_EMOJI = {
     "stale": ":sleeping:",
 }
 
+PR_REVIEW_STATE_EMOJI = {
+    "approved": ":thumbs_up:",
+    "changes_requested": ":repeat:",
+    "commented": ":memo:",
+}
+
+PR_REVIEW_COMMENT_EMOJI = ":speech_balloon:"
+
+PR_CLOSE_ACTION_EMOJI = {
+    "merged": ":check:",
+    "closed without merge": ":cross_mark:",
+}
+
 
 class Helper:
     def __init__(
@@ -153,6 +166,7 @@ def get_closed_pull_request_body(helper: Helper) -> str:
         url=pull_request["html_url"].tame(check_string),
         number=pull_request["number"].tame(check_int),
         title=pull_request["title"].tame(check_string) if include_title else None,
+        emoji=PR_CLOSE_ACTION_EMOJI.get(action) if helper.include_emoji_indicators else None,
     )
 
 
@@ -677,6 +691,9 @@ def get_pull_request_review_body(helper: Helper) -> str:
         type="PR review",
         title=title if include_title else None,
         message=payload["review"]["body"].tame(check_none_or(check_string)),
+        emoji=PR_REVIEW_STATE_EMOJI.get(payload["review"]["state"].tame(check_string))
+        if helper.include_emoji_indicators
+        else None,
     )
 
 
@@ -770,6 +787,7 @@ def get_pull_request_review_comment_body(helper: Helper) -> str:
         message=message,
         type="PR review comment",
         title=title if include_title else None,
+        emoji=PR_REVIEW_COMMENT_EMOJI if helper.include_emoji_indicators else None,
     )
 
 
