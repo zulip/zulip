51a1e14687ef4a7e1a4092d0abb5c249ee9f0734
Anders Kaseorg <anders@zulip.com>
2026-02-05 14:37:38 -0800

    python: Use uv-managed Python.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- puppet/zulip/lib/facter/zulip_version.rb
+++ puppet/zulip/lib/facter/zulip_version.rb
@@ -4,7 +4,7 @@ Facter.add(:zulip_version) do
   setcode do
     Dir.chdir(File.expand_path("../../../../", __dir__)) do
       output, stderr, status = Open3.capture3(
-        "python3", "-c", "import version; print(version.ZULIP_VERSION_WITHOUT_COMMIT)"
+        "uv", "run", "--frozen", "--only-group=install", "python", "-c", "import version; print(version.ZULIP_VERSION_WITHOUT_COMMIT)"
       )
       if not status.success?
         Facter.debug("zulip_version error: #{stderr}")
