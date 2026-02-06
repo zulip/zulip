ad33ba7ba562e18c03e65026340c7a07eae4a8fc
Anders Kaseorg <anders@zulip.com>
2026-02-06 11:32:29 -0800

    puppet: Use Open3 for safer command execution.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>
    (cherry picked from commit 5a8c5cb563cd99637a35b93eaa4e9064eb14ad50)

--- puppet/zulip/lib/facter/zulip_version.rb
+++ puppet/zulip/lib/facter/zulip_version.rb
@@ -1,9 +1,11 @@
+require "open3"
+
 Facter.add(:zulip_version) do
   setcode do
     Dir.chdir("/home/zulip/deployments/current") do
-      output = `python3 -c 'import version; print(version.ZULIP_VERSION_WITHOUT_COMMIT)' 2>&1`
-      if not $?.success?
-        Facter.debug("zulip_version error: #{output}")
+      output, stderr, status = Open3.capture3("python3", "-c", "import version; print(version.ZULIP_VERSION_WITHOUT_COMMIT)")
+      if not status.success?
+        Facter.debug("zulip_version error: #{stderr}")
         nil
       else
         output.strip
