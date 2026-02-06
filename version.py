2e81c76131766ee60fc69f6fffbd3f264092262f
Anders Kaseorg <anders@zulip.com>
2026-02-05 16:13:15 -0800

    install-uv: Upgrade uv from 0.9.21 to 0.10.0.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -50,4 +50,4 @@ API_FEATURE_LEVEL = 459
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (369, 1)  # bumped 2026-02-02 to add compressorjs
+PROVISION_VERSION = (369, 2)  # bumped 2026-02-05 to upgrade uv
