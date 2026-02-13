d8702f195c4599bff348b1bfb7cc0743f1506c76
Alex Vandiver <alexmv@zulip.com>
2026-02-13 10:31:43 -0500

    puppet: Provide zulip_version from the same tree as puppet is run.
    This fixes a bug where the version puppet provided was the _current_
    version, which meant that what it provided lagged by one deployment.
    It also did not function on first install, as
    `/home/zulip/deployments/current` does not exist yet on first puppet
    run.
    
    Examine the `version.py` of from the same tree that puppet is being
    run from, which addresses both of these issues.

--- puppet/zulip/lib/facter/zulip_version.rb
+++ puppet/zulip/lib/facter/zulip_version.rb
@@ -2,8 +2,10 @@ require "open3"
 
 Facter.add(:zulip_version) do
   setcode do
-    Dir.chdir("/home/zulip/deployments/current") do
-      output, stderr, status = Open3.capture3("python3", "-c", "import version; print(version.ZULIP_VERSION_WITHOUT_COMMIT)")
+    Dir.chdir(File.expand_path("../../../../", __dir__)) do
+      output, stderr, status = Open3.capture3(
+        "python3", "-c", "import version; print(version.ZULIP_VERSION_WITHOUT_COMMIT)"
+      )
       if not status.success?
         Facter.debug("zulip_version error: #{stderr}")
         nil
