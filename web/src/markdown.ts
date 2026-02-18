d92ce1dc4acfd761dec6d26c11166d7fe8ed9c7e
wakeupguruu <vyasguru19@gmail.com>
2026-02-18 10:37:44 +0000

    markdown: Extract common marked options.
    Extract the options passed to `marked` into a `common_marked_options`
    constant for reuse and improved readability. A follow-up commit will
    need the same options for a new function.

--- web/src/markdown.ts
+++ web/src/markdown.ts
@@ -33,6 +33,25 @@ const preview_regexes = [
     /\S*(?:twitter|youtube)\.com\/\S*/,
 ];
 
+const common_marked_options = {
+    // Enable GitHub Flavored Markdown.
+    gfm: true,
+    // Enable GFM tables.
+    tables: true,
+    // Enable GFM line breaks.
+    breaks: true,
+    // Conform to obscure parts of markdown.pl as much as possible.
+    pedantic: false,
+    // Sanitize the output. Ignore any HTML that has been input.
+    sanitize: true,
+    // Use smarter list behavior than the original markdown.pl.
+    smartLists: true,
+    // Use "smart" typograhic punctuation for things like quotes and dashes.
+    smartypants: false,
+    // Custom Zulip extensions.
+    zulip: true,
+};
+
 function contains_preview_link(content: string): boolean {
     return preview_regexes.some((re) => re.test(content));
 }
@@ -836,14 +855,7 @@ export function parse({
         streamTopicMessageHandler,
         texHandler: handleTex,
         timestampHandler: handleTimestamp,
-        gfm: true,
-        tables: true,
-        breaks: true,
-        pedantic: false,
-        sanitize: true,
-        smartLists: true,
-        smartypants: false,
-        zulip: true,
+        ...common_marked_options,
         renderer,
         preprocessors: [preprocess_code_blocks, preprocess_translate_emoticons],
     };
