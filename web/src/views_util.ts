zulip/zulip

commit 25fb8faf45474a18d4c502e3be709f06284917db

views_util: Fix keyboard navigation not working when compose is open.
Even if the view has focus, keyboard navigation doesn't work if the
compose box is open.

web/src/views_util.ts
@@ -154,6 +154,16 @@ export function hide(opts: {$view: JQuery; set_visible: (value: boolean) => void
 export function is_in_focus(): boolean {
     let can_current_view_steal_focus = true;
     const focused_element = document.activeElement;
+
+    // If current view has focus we don't need to check anything else.
+    if (
+        focused_element instanceof HTMLElement &&
+        (focused_element.closest("#recent_view") !== null ||
+            focused_element.closest("#inbox-view") !== null)
+    ) {
+        return true;
+    }
+
     if (
         focused_element instanceof HTMLElement &&
         // Pill input elements.

commit b08fd3e1374ca85faeb5ad04984d6a46d14f4acb

views_util: Consider open view popovers.
This tries to correct for a regression introduced in
25fb8faf45474a18d4c502e3be709f06284917db that caused
the state of an open Inbox or Recents popover to be
disregarded, which meant those popovers failed to
receive focus, with or without the compose box open.

web/src/views_util.ts
@@ -160,11 +160,17 @@ export function is_in_focus(): boolean {
     let can_current_view_steal_focus = true;
     const focused_element = document.activeElement;
 
-    // If current view has focus we don't need to check anything else.
+    // If current view has focus and there's not a visibility or
+    // vdots popover with focus, we don't need to check anything else.
     if (
         focused_element instanceof HTMLElement &&
         (focused_element.closest("#recent_view") !== null ||
-            focused_element.closest("#inbox-view") !== null)
+            focused_element.closest("#inbox-view") !== null) &&
+        !(
+            focused_element.classList.contains("visibility-policy-indicator") ||
+            focused_element.classList.contains("inbox-action-button") ||
+            focused_element.classList.contains("recent_topic_actions")
+        )
     ) {
         return true;
     }