d48435f67d98bcd444f972ec4295926850ed404a
Sahil Batra <sahil@zulip.com>
2026-02-27 18:19:01 +0530

    realm: Check plan when updating workplace_users_group.
    This commit adds code to not allow self-hosted orgs,
    orgs with 100% sponsorship, orgs with fixed price plan
    and orgs with discounted plans to enable two tier billing.

--- zerver/lib/workplace_users.py
+++ zerver/lib/workplace_users.py
@@ -48,3 +48,49 @@ def validate_workplace_users_group(
                 "'workplace_users_group' must be a group whose membership can only be managed by organization administrators."
             )
         )
+
+
+def realm_eligible_for_non_workplace_pricing(realm: Realm) -> bool:
+    if realm.plan_type == Realm.PLAN_TYPE_SELF_HOSTED:
+        # Non-workplace plan pricing is not yet implemented
+        # for self-hosted plans.
+        return False
+
+    if realm.plan_type == Realm.PLAN_TYPE_STANDARD_FREE:
+        # Fully sponsored plans are completely free, so it
+        # would be distracting to offer menu options for
+        # discounted pricing.
+        return False
+
+    if realm.plan_type == Realm.PLAN_TYPE_LIMITED:
+        # We want to allow organizations to enable discounted
+        # pricing for non workplace users before they upgrade.
+        return True
+
+    from corporate.models.plans import get_current_plan_by_realm
+
+    customer_plan = get_current_plan_by_realm(realm)
+    assert customer_plan is not None
+    if customer_plan.fixed_price is not None:
+        # Discounted pricing for non-workplace users is
+        # currently incompatible with a fixed-price plan.
+        return False
+
+    return True
+
+
+def realm_on_discounted_cloud_plan(realm: Realm) -> bool:
+    if realm.plan_type == Realm.PLAN_TYPE_SELF_HOSTED:
+        return False
+
+    if realm.plan_type in {Realm.PLAN_TYPE_LIMITED, Realm.PLAN_TYPE_STANDARD_FREE}:
+        # Realm is on free plan or is fully sponsored.
+        return False
+
+    from corporate.models.customers import get_customer_by_realm
+
+    # We can assume that an active plan will be present
+    # if realm is not on free or fully sponsored plan.
+    customer = get_customer_by_realm(realm)
+    assert customer is not None
+    return customer.monthly_discounted_price > 0 or customer.annual_discounted_price > 0
--- zerver/views/realm.py
+++ zerver/views/realm.py
@@ -57,7 +57,11 @@ from zerver.lib.user_groups import (
     validate_group_setting_value_change,
 )
 from zerver.lib.validator import check_capped_url, check_string
-from zerver.lib.workplace_users import validate_workplace_users_group
+from zerver.lib.workplace_users import (
+    realm_eligible_for_non_workplace_pricing,
+    realm_on_discounted_cloud_plan,
+    validate_workplace_users_group,
+)
 from zerver.models import Realm, RealmReactivationStatus, RealmUserDefault, UserProfile
 from zerver.models.groups import SystemGroups
 from zerver.models.realms import (
@@ -387,6 +391,19 @@ def update_realm(
             )
 
             data["jitsi_server_url"] = jitsi_server_url
+
+    if workplace_users_group is not None:
+        if not realm_eligible_for_non_workplace_pricing(realm):
+            raise JsonableError(
+                _("Organization is not eligible for discounted pricing for non workplace users.")
+            )
+
+        if realm_on_discounted_cloud_plan(realm):
+            raise JsonableError(
+                _(
+                    "Discounted pricing for workplace users cannot be enabled with current discounted plan."
+                )
+            )
 
     # The user of `locals()` here is a bit of a code smell, but it's
     # restricted to the elements present in realm.property_types.
