6c959b677b1b24ebc5262934dcf0a91e48358684
sathwikshetty33 <sathwikshetty2k5@gmail.com>
2026-03-01 13:54:24 +0000

    bots: Save incoming webhook bots integration.
    This makes it possible to associate an integration with an
    incoming webhook bot.
    
    This is prep work to have incoming webhook bots pre-populate the
    "Selected integration" field in the Generate integration URL modal.
    
    Fixes part of zulip#30139.
    
    Co-authored-by: Aditya Kasaudhan <akasaudhan02@gmail.com>
    Co-authored-by: Mukul Goyal <96649866+Mukul1235@users.noreply.github.com>

--- web/src/bot_type_values.ts
+++ web/src/bot_type_values.ts
@@ -4,5 +4,6 @@ export const INCOMING_WEBHOOK_BOT_TYPE_INT = 2;
 export const OUTGOING_WEBHOOK_BOT_TYPE_INT = 3;
 
 // String forms used as HTML form values.
+export const INCOMING_WEBHOOK_BOT_TYPE = "2";
 export const OUTGOING_WEBHOOK_BOT_TYPE = "3";
 export const EMBEDDED_BOT_TYPE = "4";
