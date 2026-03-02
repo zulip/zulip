75f7a04da1d5b129e42d189e0eeb8b06ef4e6017
sathwikshetty33 <sathwikshetty2k5@gmail.com>
2026-03-02 11:45:34 +0000

    bots: Prepopulate integration_name in URL modal.
    Fixes part of zulip#30139.
    
    Co-authored-by: Aditya Kumar Kasaudhan <74228301+Aditya8840@users.noreply.github.com>
    Co-authored-by: Mukul Goyal <96649866+Mukul1235@users.noreply.github.com>

--- web/src/user_profile.ts
+++ web/src/user_profile.ts
@@ -1114,7 +1114,7 @@ export function show_edit_bot_info_modal(user_id: number, $container: JQuery): v
                             .animate({scrollTop: 0}, "fast");
                         return;
                     }
-                    integration_url_modal.show_generate_integration_url_modal(api_key);
+                    integration_url_modal.show_generate_integration_url_modal(api_key, bot.user_id);
                 })();
             },
         );
