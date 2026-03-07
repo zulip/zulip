1ec27d74aa7288cc26a4451aeb09d274a7ea6dc2
Aadyot1 <aadyotnandan@gmail.com>
2026-03-08 00:36:59 +0530

    drafts: Add Outbox tab to drafts overlay.
    Messages that fail to send were previously converted into regular drafts,
    making it unclear that the user had already attempted to send them.
    
    Add an Outbox tab in the drafts overlay to show unsent messages and
    provide resend and cancel actions.
    
    Fixes #32999.

--- web/tests/drafts.test.cjs
+++ web/tests/drafts.test.cjs
@@ -12,7 +12,17 @@ const $ = require("./lib/zjquery.cjs");
 
 const user_pill = mock_esm("../src/user_pill", {get_user_ids: () => []});
 const settings_data = mock_esm("../src/settings_data");
-const messages_overlay_ui = mock_esm("../src/messages_overlay_ui");
+const messages_overlay_ui = mock_esm("../src/messages_overlay_ui", {
+    get_and_clear_pending_restore_element_id: () => undefined,
+    set_initial_element: noop,
+});
+mock_esm("../src/components", {toggle: () => ({get: () => ({})})});
+const echo = mock_esm("../src/echo", {
+    resend_message_by_draft_id: noop,
+    abort_message_by_draft_id: noop,
+    // Default false so is_sending_saving drafts appear as regular drafts.
+    has_local_echo_for_draft: () => false,
+});
 
 const people = zrequire("people");
 const compose_state = zrequire("compose_state");
@@ -512,6 +522,7 @@ test("format_drafts", ({override, mock_template}) => {
     const expected = [
         {
             draft_id: "id1",
+            is_sending_saving: false,
             is_stream: true,
             stream_name: stream_1.name,
             stream_id: 30,
@@ -526,6 +537,7 @@ test("format_drafts", ({override, mock_template}) => {
         },
         {
             draft_id: "id2",
+            is_sending_saving: false,
             is_dm_with_self: true,
             is_stream: false,
             has_recipient_data: true,
@@ -535,6 +547,7 @@ test("format_drafts", ({override, mock_template}) => {
         },
         {
             draft_id: "id5",
+            is_sending_saving: false,
             is_dm_with_self: false,
             is_stream: false,
             recipients: "Iago, Zoe",
@@ -544,6 +557,7 @@ test("format_drafts", ({override, mock_template}) => {
         },
         {
             draft_id: "id4",
+            is_sending_saving: false,
             is_dm_with_self: false,
             is_stream: false,
             recipients: "Iago",
@@ -553,6 +567,7 @@ test("format_drafts", ({override, mock_template}) => {
         },
         {
             draft_id: "id3",
+            is_sending_saving: false,
             is_stream: true,
             stream_name: stream_2.name,
             stream_id: 40,
@@ -567,6 +582,7 @@ test("format_drafts", ({override, mock_template}) => {
         },
         {
             draft_id: "id6",
+            is_sending_saving: false,
             is_stream: true,
             stream_name: stream_2.name,
             stream_id: 40,
@@ -581,6 +597,7 @@ test("format_drafts", ({override, mock_template}) => {
         },
         {
             draft_id: "id7",
+            is_sending_saving: false,
             is_stream: false,
             has_recipient_data: false,
             recipients: "",
@@ -617,6 +634,7 @@ test("format_drafts", ({override, mock_template}) => {
         // Tests formatting and time-sorting of drafts
         assert.deepEqual(data.context.narrow_drafts, []);
         assert.deepEqual(data.context.other_drafts, expected);
+        assert.deepEqual(data.context.outbox_drafts, []);
         assert.ok(data);
         return "<draft table stub>";
     });
@@ -624,8 +642,9 @@ test("format_drafts", ({override, mock_template}) => {
     override(messages_overlay_ui, "set_initial_element", noop);
 
     $.set_results(".drafts-list", []);
-    $.set_results("#drafts_table .overlay-message-row", []);
+    $.set_results(".drafts-tab-pane .overlay-message-row", []);
     $.set_results(".draft-selection-checkbox", []);
+    $.set_results(".outbox-selection-checkbox", []);
     drafts_overlay_ui.launch();
 });
 
@@ -688,6 +707,7 @@ test("filter_drafts", ({override, mock_template}) => {
     const expected_pm_drafts = [
         {
             draft_id: "id2",
+            is_sending_saving: false,
             is_dm_with_self: true,
             is_stream: false,
             has_recipient_data: true,
@@ -697,6 +717,7 @@ test("filter_drafts", ({override, mock_template}) => {
         },
         {
             draft_id: "id5",
+            is_sending_saving: false,
             is_dm_with_self: true,
             is_stream: false,
             has_recipient_data: true,
@@ -706,6 +727,7 @@ test("filter_drafts", ({override, mock_template}) => {
         },
         {
             draft_id: "id4",
+            is_sending_saving: false,
             is_dm_with_self: true,
             is_stream: false,
             has_recipient_data: true,
@@ -718,6 +740,7 @@ test("filter_drafts", ({override, mock_template}) => {
     const expected_other_drafts = [
         {
             draft_id: "id1",
+            is_sending_saving: false,
             is_stream: true,
             stream_name: stream_1.name,
             stream_id: 30,
@@ -732,6 +755,7 @@ test("filter_drafts", ({override, mock_template}) => {
         },
         {
             draft_id: "id3",
+            is_sending_saving: false,
             is_stream: true,
             stream_name: stream_2.name,
             stream_id: 40,
@@ -764,6 +788,7 @@ test("filter_drafts", ({override, mock_template}) => {
         // Tests splitting up drafts by current narrow.
         assert.deepEqual(data.context.narrow_drafts, expected_pm_drafts);
         assert.deepEqual(data.context.other_drafts, expected_other_drafts);
+        assert.deepEqual(data.context.outbox_drafts, []);
         return "<draft table stub>";
     });
 
@@ -773,7 +798,171 @@ test("filter_drafts", ({override, mock_template}) => {
     compose_state.set_message_type("private");
 
     $.set_results(".drafts-list", []);
-    $.set_results("#drafts_table .overlay-message-row", []);
+    $.set_results(".drafts-tab-pane .overlay-message-row", []);
     $.set_results(".draft-selection-checkbox", []);
+    $.set_results(".outbox-selection-checkbox", []);
+    drafts_overlay_ui.launch();
+});
+
+test("outbox tab", ({override, mock_template}) => {
+    const outbox_draft = {
+        stream_id,
+        topic: "topic",
+        type: "stream",
+        content: "Sending message",
+        updatedAt: mock_current_timestamp,
+        is_sending_saving: true,
+        drafts_version: 1,
+    };
+    const ls = localstorage();
+    ls.set("drafts", {outbox_id: outbox_draft});
+
+    clock.setSystemTime(mock_current_timestamp);
+
+    override(user_pill, "get_user_ids", () => []);
+    compose_state.set_message_type("private");
+    override(messages_overlay_ui, "set_initial_element", noop);
+    override(settings_data, "using_dark_theme", () => false);
+    override(echo, "has_local_echo_for_draft", (id) => id === "outbox_id");
+
+    mock_template("draft_table_body.hbs", false, (data) => {
+        assert.deepEqual(data.context.narrow_drafts, []);
+        assert.deepEqual(data.context.other_drafts, []);
+        assert.equal(data.context.outbox_drafts.length, 1);
+        assert.equal(data.context.outbox_drafts[0].draft_id, "outbox_id");
+        assert.equal(data.context.outbox_drafts[0].is_sending_saving, true);
+        return "<draft table stub>";
+    });
+
+    $.set_results(".drafts-list", []);
+    $.set_results(".drafts-tab-pane .overlay-message-row", []);
+    $.set_results(".draft-selection-checkbox", []);
+    $.set_results(".outbox-selection-checkbox", []);
+    drafts_overlay_ui.launch();
+});
+
+test("outbox resend uses echo pipeline", ({override, mock_template}) => {
+    const outbox_draft = {
+        stream_id,
+        topic: "topic",
+        type: "stream",
+        content: "Sending message",
+        updatedAt: mock_current_timestamp,
+        is_sending_saving: true,
+        drafts_version: 1,
+    };
+    const ls = localstorage();
+    ls.set("drafts", {outbox_id: outbox_draft});
+
+    clock.setSystemTime(mock_current_timestamp);
+
+    override(user_pill, "get_user_ids", () => []);
+    compose_state.set_message_type("private");
+    override(messages_overlay_ui, "set_initial_element", noop);
+    override(settings_data, "using_dark_theme", () => false);
+    override(echo, "has_local_echo_for_draft", (id) => id === "outbox_id");
+
+    mock_template("draft_table_body.hbs", false, () => "<draft table stub>");
+
+    $.set_results(".drafts-list", []);
+    $.set_results(".drafts-tab-pane .overlay-message-row", []);
+    $.set_results(".draft-selection-checkbox", []);
+    $.set_results(".outbox-selection-checkbox", []);
+    drafts_overlay_ui.launch();
+
+    const $row = $.create("outbox row");
+    $row.attr("data-draft-id", "outbox_id");
+
+    const $resend_btn = $("#drafts_table .outbox-resend-message");
+    $resend_btn.set_closest_results(".overlay-message-row", $row);
+
+    let resend_called_with;
+    override(echo, "resend_message_by_draft_id", (draft_id) => {
+        resend_called_with = draft_id;
+    });
+
+    $resend_btn.trigger("click");
+    assert.equal(resend_called_with, "outbox_id");
+});
+
+test("outbox cancel propagates to echo abort pipeline", ({override, mock_template}) => {
+    const outbox_draft = {
+        stream_id,
+        topic: "topic",
+        type: "stream",
+        content: "Sending message",
+        updatedAt: mock_current_timestamp,
+        is_sending_saving: true,
+        drafts_version: 1,
+    };
+    const ls = localstorage();
+    ls.set("drafts", {outbox_id: outbox_draft});
+
+    clock.setSystemTime(mock_current_timestamp);
+
+    override(user_pill, "get_user_ids", () => []);
+    compose_state.set_message_type("private");
+    override(messages_overlay_ui, "set_initial_element", noop);
+    override(settings_data, "using_dark_theme", () => false);
+    override(echo, "has_local_echo_for_draft", (id) => id === "outbox_id");
+
+    mock_template("draft_table_body.hbs", false, () => "<draft table stub>");
+
+    $.set_results(".drafts-list", []);
+    $.set_results(".drafts-tab-pane .overlay-message-row", []);
+    $.set_results(".draft-selection-checkbox", []);
+    $.set_results(".outbox-selection-checkbox", []);
+    $.set_results(".outbox-tab-pane .overlay-message-row", []);
+    drafts_overlay_ui.launch();
+
+    const $row = $.create("outbox full cancel row");
+    $row.attr("data-draft-id", "outbox_id");
+    $row[0].remove = () => {};
+
+    const $cancel_btn = $("#drafts_table .outbox-cancel-message");
+    $cancel_btn.set_closest_results(".overlay-message-row", $row);
+
+    let abort_called_with;
+    override(echo, "abort_message_by_draft_id", (draft_id) => {
+        abort_called_with = draft_id;
+    });
+
+    $cancel_btn.trigger("click");
+
+    assert.equal(abort_called_with, "outbox_id");
+});
+
+test("ghost outbox draft treated as regular draft", ({override, mock_template}) => {
+    const ghost_draft = {
+        stream_id,
+        topic: "topic",
+        type: "stream",
+        content: "Ghost message",
+        updatedAt: mock_current_timestamp,
+        is_sending_saving: true,
+        drafts_version: 1,
+    };
+    const ls = localstorage();
+    ls.set("drafts", {ghost_id: ghost_draft});
+
+    clock.setSystemTime(mock_current_timestamp);
+
+    override(user_pill, "get_user_ids", () => []);
+    compose_state.set_message_type("private");
+    override(messages_overlay_ui, "set_initial_element", noop);
+    override(settings_data, "using_dark_theme", () => false);
+
+    mock_template("draft_table_body.hbs", false, (data) => {
+        assert.deepEqual(data.context.outbox_drafts, []);
+        assert.equal(data.context.other_drafts.length, 1);
+        assert.equal(data.context.other_drafts[0].draft_id, "ghost_id");
+        assert.equal(data.context.other_drafts[0].is_sending_saving, false);
+        return "<draft table stub>";
+    });
+
+    $.set_results(".drafts-list", []);
+    $.set_results(".drafts-tab-pane .overlay-message-row", []);
+    $.set_results(".draft-selection-checkbox", []);
+    $.set_results(".outbox-selection-checkbox", []);
     drafts_overlay_ui.launch();
 });
