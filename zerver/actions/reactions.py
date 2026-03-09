af3a79028963f0f225d83c212ecb94c5231bdb91
Sanket Srivastava <sanket17052006@gmail.com>
2026-03-09 13:03:31 +0000

    reactions: Add ReactionEvent TypedDict.

--- zerver/actions/reactions.py
+++ zerver/actions/reactions.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Literal
 
 from typing_extensions import TypedDict
 
@@ -25,8 +25,22 @@ class ReactionEventUserDict(TypedDict):
     full_name: str
 
 
+class ReactionEvent(TypedDict):
+    type: Literal["reaction"]
+    op: Literal["add", "remove"]
+    user_id: int
+    user: ReactionEventUserDict
+    message_id: int
+    emoji_name: str
+    emoji_code: str
+    reaction_type: str
+
+
 def notify_reaction_update(
-    user_profile: UserProfile, message: Message, reaction: Reaction, op: str
+    user_profile: UserProfile,
+    message: Message,
+    reaction: Reaction,
+    op: Literal["add", "remove"],
 ) -> None:
     user_dict: ReactionEventUserDict = {
         "user_id": user_profile.id,
@@ -34,7 +48,7 @@ def notify_reaction_update(
         "full_name": user_profile.full_name,
     }
 
-    event: dict[str, Any] = {
+    event: ReactionEvent = {
         "type": "reaction",
         "op": op,
         "user_id": user_profile.id,
