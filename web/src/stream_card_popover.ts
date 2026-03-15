2ba660b813d92ab57f95668ea5b7b80d0854c671
Pratik Chanda <pratikchanda2000@gmail.com>
2026-03-16 00:07:37 +0530

    stream_card_popover: Remove redundant code to close active modal.
    `modals.close_active_if_any` here indicates that it is used to handle
    navigation to channel settings and close any leftover modal(mentioned
    to be invite user modal). In 58ab97a0b4fde053c4990a70c312a295cd484c69,
    `modals.close_active_if_any` was added to run for any hashchange. We
    remove this extra call.

--- web/src/stream_card_popover.ts
+++ web/src/stream_card_popover.ts
@@ -5,7 +5,6 @@ import render_stream_card_popover from "../templates/popovers/stream_card_popove
 
 import * as browser_history from "./browser_history.ts";
 import * as hash_util from "./hash_util.ts";
-import * as modals from "./modals.ts";
 import * as peer_data from "./peer_data.ts";
 import * as popover_menus from "./popover_menus.ts";
 import * as stream_data from "./stream_data.ts";
@@ -48,9 +47,6 @@ export function initialize(): void {
                 const sub = sub_store.get(stream_id);
                 assert(sub !== undefined);
                 popover_menus.hide_current_popover_if_visible(instance);
-                // modals.close_active_if_any() is mainly used to handle navigation to channel settings
-                // using the popover that is opened when clicking on channel pills in the invite user modal.
-                modals.close_active_if_any();
                 const can_change_stream_permissions =
                     stream_data.can_change_permissions_requiring_metadata_access(sub);
                 let stream_edit_hash = hash_util.channels_settings_edit_url(sub, "general");
