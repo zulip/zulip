a481806e821cabf53223360bb35f4c985684b494
Nithyaraj Mudhaliyar <nithyarajmudhaliyar2007gmail.com>
2026-03-17 09:19:23 -0700

    markdown: Auto-link obsidian:// URLs.
    This whitelists the obsidian protocol in the markdown link regex
    and html_safelisted_schemes so bare links render correctly without
    needing angle brackets.
    
    It also fixes a bug in test_markdown.py where the replaced
    helper improperly forced an http:// prefix onto custom schemas.
    
    Fixes #21859.

--- zerver/lib/markdown/__init__.py
+++ zerver/lib/markdown/__init__.py
@@ -84,6 +84,7 @@ html_safelisted_schemes = (
     "magnet",
     "mailto",
     "matrix",
+    "obsidian",
     "mms",
     "news",
     "nntp",
@@ -98,6 +99,7 @@ html_safelisted_schemes = (
     "wtai",
     "xmpp",
 )
+auto_linked_schemes = ["https?", "obsidian"]
 allowed_schemes = ("http", "https", "ftp", "file", "mid", *html_safelisted_schemes)
 
 
@@ -229,6 +231,7 @@ def get_web_link_regex() -> Pattern[str]:
     # caching the value is super important here.
 
     tlds = r"|".join(list_of_tlds())
+    schemes_regex = r"|".join(auto_linked_schemes)
 
     # A link starts at a word boundary, and ends at space, punctuation, or end-of-input.
     #
@@ -257,7 +260,7 @@ def get_web_link_regex() -> Pattern[str]:
                              # (Double-negative lookbehind to allow start-of-string)
         (?P<url>             # Main group
             (?:(?:           # Domain part
-                https?://[\w.:@-]+?   # If it has a protocol, anything goes.
+                (?:{schemes_regex})://[\w.:@-]+?   # If it has a protocol, anything goes.
                |(?:                   # Or, if not, be more strict to avoid false-positives
                     (?:[\w-]+\.)+     # One or more domain components, separated by dots
                     (?:{tlds})        # TLDs
