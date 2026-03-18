f2ef37770ecf9fe8d2e2a9fca41f1096ea9ba5fa
Sahil Batra <sahil@zulip.com>
2026-03-18 14:58:05 +0530

    stream-edit: Show loading spinner on subscription toggle button.
    This commit adds code to show loading spinner on the subscription
    toggle button in right panel subheader while request is in process.
    
    We also change open_edit_panel_for_row to not be called when
    subscribing using the toggle button in right panel subheader
    as we know that the stream settings is already opened in right
    panel. So, we call open_edit_panel_row only when subscribing to
    stream by clicking on plus icon from stream row in left panel.
    This was also necessary because otherwise opening the settings
    in right panel led to rerendering of the button and spinner was
    not visible.

--- web/src/stream_settings_components.ts
+++ web/src/stream_settings_components.ts
@@ -7,6 +7,7 @@ import render_new_channel_members_title from "../templates/stream_settings/new_c
 import render_selected_stream_title from "../templates/stream_settings/selected_stream_title.hbs";
 
 import * as blueslip from "./blueslip.ts";
+import * as buttons from "./buttons.ts";
 import * as channel from "./channel.ts";
 import * as channel_folders from "./channel_folders.ts";
 import * as channel_folders_ui from "./channel_folders_ui.ts";
@@ -195,12 +196,16 @@ export function ajaxSubscribe(
     stream: string,
     color: string,
     $stream_row: JQuery | undefined,
+    $button_elem: JQuery | undefined,
 ): void {
     // Subscribe yourself to a single stream.
     let true_stream_name;
 
     if ($stream_row !== undefined) {
         display_subscribe_toggle_spinner($stream_row);
+    }
+    if ($button_elem !== undefined) {
+        buttons.show_button_loading_indicator($button_elem);
     }
     void channel.post({
         url: "/json/users/me/subscriptions",
@@ -231,10 +236,16 @@ export function ajaxSubscribe(
             if ($stream_row !== undefined) {
                 hide_subscribe_toggle_spinner($stream_row);
             }
+            if ($button_elem !== undefined) {
+                buttons.hide_button_loading_indicator($button_elem);
+            }
         },
         error(xhr) {
             if ($stream_row !== undefined) {
                 hide_subscribe_toggle_spinner($stream_row);
+            }
+            if ($button_elem !== undefined) {
+                buttons.hide_button_loading_indicator($button_elem);
             }
             ui_report.error(
                 $t_html({defaultMessage: "Error adding subscription"}),
@@ -245,10 +256,17 @@ export function ajaxSubscribe(
     });
 }
 
-function ajaxUnsubscribe(sub: StreamSubscription, $stream_row: JQuery | undefined): void {
+function ajaxUnsubscribe(
+    sub: StreamSubscription,
+    $stream_row: JQuery | undefined,
+    $button_elem: JQuery | undefined,
+): void {
     // TODO: use stream_id when backend supports it
     if ($stream_row !== undefined) {
         display_subscribe_toggle_spinner($stream_row);
+    }
+    if ($button_elem !== undefined) {
+        buttons.show_button_loading_indicator($button_elem);
     }
     void channel.del({
         url: "/json/users/me/subscriptions",
@@ -260,10 +278,16 @@ function ajaxUnsubscribe(sub: StreamSubscription, $stream_row: JQuery | undefine
             if ($stream_row !== undefined) {
                 hide_subscribe_toggle_spinner($stream_row);
             }
+            if ($button_elem !== undefined) {
+                buttons.hide_button_loading_indicator($button_elem);
+            }
         },
         error(xhr) {
             if ($stream_row !== undefined) {
                 hide_subscribe_toggle_spinner($stream_row);
+            }
+            if ($button_elem !== undefined) {
+                buttons.hide_button_loading_indicator($button_elem);
             }
             ui_report.error(
                 $t_html({defaultMessage: "Error removing subscription"}),
@@ -277,6 +301,7 @@ function ajaxUnsubscribe(sub: StreamSubscription, $stream_row: JQuery | undefine
 export function unsubscribe_from_private_stream(
     sub: StreamSubscription,
     $stream_row: JQuery | undefined,
+    $button_elem: JQuery | undefined,
 ): void {
     const invite_only = sub.invite_only;
     const sub_count = peer_data.get_subscriber_count(sub.stream_id);
@@ -295,7 +320,7 @@ export function unsubscribe_from_private_stream(
     });
 
     function unsubscribe_from_stream(): void {
-        ajaxUnsubscribe(sub, $stream_row);
+        ajaxUnsubscribe(sub, $stream_row, $button_elem);
     }
 
     confirm_dialog.launch({
@@ -308,19 +333,23 @@ export function unsubscribe_from_private_stream(
     });
 }
 
-export function sub_or_unsub(sub: StreamSubscription, $stream_row?: JQuery): void {
+export function sub_or_unsub(
+    sub: StreamSubscription,
+    $stream_row?: JQuery,
+    $button_elem?: JQuery,
+): void {
     if (sub.subscribed) {
         // TODO: This next line should allow guests to access web-public streams.
         if (
             (sub.invite_only && !stream_data.has_content_access_via_group_permissions(sub)) ||
             current_user.is_guest
         ) {
-            unsubscribe_from_private_stream(sub, $stream_row);
+            unsubscribe_from_private_stream(sub, $stream_row, $button_elem);
             return;
         }
-        ajaxUnsubscribe(sub, $stream_row);
+        ajaxUnsubscribe(sub, $stream_row, $button_elem);
     } else {
-        ajaxSubscribe(sub.name, sub.color, $stream_row);
+        ajaxSubscribe(sub.name, sub.color, $stream_row, $button_elem);
     }
 }
 
