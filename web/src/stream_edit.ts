b503f1c104a28287d0b7774b361f7c95beeb5d95
PieterCK <pieterceka123@gmail.com>
2026-03-26 21:23:46 +0700

    stream_settings: Rearrange channel property change alert notification.
    This moves the alert notification to mainly appear in the channel title
    next to the "Unsubscribe"/"Subscribe" button. If the screen size is too
    small, it'll appear in the right pane body instead.

--- web/src/stream_edit.ts
+++ web/src/stream_edit.ts
@@ -30,6 +30,7 @@ import type {User} from "./people.ts";
 import * as people from "./people.ts";
 import * as popovers from "./popovers.ts";
 import {postprocess_content} from "./postprocess_content.ts";
+import {resize_stream_subscribers_list} from "./resize.ts";
 import * as scroll_util from "./scroll_util.ts";
 import * as settings_components from "./settings_components.ts";
 import * as settings_config from "./settings_config.ts";
@@ -547,6 +548,7 @@ export function archive_stream(stream_id: number, $alert_element: JQuery): void 
         error(xhr) {
             ui_report.error($t_html({defaultMessage: "Failed"}), xhr, $alert_element);
             dialog_widget.hide_dialog_spinner();
+            resize_stream_subscribers_list();
         },
     });
 }
@@ -938,6 +940,7 @@ export function initialize(): void {
                         $(".stream_change_property_info"),
                     );
                     dialog_widget.hide_dialog_spinner();
+                    resize_stream_subscribers_list();
                 },
             });
         }
