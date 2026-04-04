7ab0333e4b8aa19464e7766a1d9dcce5a28cd4dc
Dhruv Shetty <sdhruv23@iitk.ac.in>
2026-04-04 16:34:05 +0530

    dependencies: Remove html2text dependency.
    Zulip server no longer imports html2text. The package remains in
    the lockfile as a transitive dep of zulip-bots.

--- version.py
+++ version.py
@@ -48,4 +48,4 @@ API_FEATURE_LEVEL = 507
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (384, 1)  # bumped 2026-07-07 to add markdownify dependency
+PROVISION_VERSION = (385, 0)  # bumped 2026-07-07 to remove html2text dependency
