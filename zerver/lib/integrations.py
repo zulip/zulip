b9d38d8fd4b46b569a73a0a86dcd981d2f565543
Niloth P <20315308+Niloth-p@users.noreply.github.com>
2026-04-07 03:56:02 +0530

    integrations/nextcloud-talk: Remove duplicate logo.

--- zerver/lib/integrations.py
+++ zerver/lib/integrations.py
@@ -990,7 +990,10 @@ VIDEO_CALL_INTEGRATIONS: list[Integration] = [
     ),
     Integration("jitsi", ["video-calling", "communication"], display_name="Jitsi Meet"),
     Integration(
-        "nextcloud-talk", ["video-calling", "communication"], display_name="Nextcloud Talk"
+        "nextcloud-talk",
+        ["video-calling", "communication"],
+        display_name="Nextcloud Talk",
+        logo="images/integrations/logos/nextcloud.svg",
     ),
     Integration("zoom", ["video-calling", "communication"]),
 ]
