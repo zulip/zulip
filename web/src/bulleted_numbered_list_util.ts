dd35e65c7ea2ac97611faba4f921e0b45f170b5e
sonusonukgupta-gif <sonusonukgupta-gif@users.noreply.github.com>
2026-04-12 17:37:34 +0530

    Fix #37737: Support auto-inserting bullets for sub-lists on Return/Shift+Return

--- web/src/bulleted_numbered_list_util.ts
+++ web/src/bulleted_numbered_list_util.ts
@@ -1,11 +1,13 @@
 export const get_last_line = (text: string): string => text.slice(text.lastIndexOf("\n") + 1);
 
-export const is_bulleted = (line: string): boolean =>
-    line.startsWith("- ") || line.startsWith("* ") || line.startsWith("+ ");
+export const is_bulleted = (line: string): boolean => {
+    const trimmed_line = line.trimStart();
+    return trimmed_line.startsWith("- ") || trimmed_line.startsWith("* ") || trimmed_line.startsWith("+ ");
+};
 
 // testing against regex for string with numbered syntax, that is,
-// any string starting with digit/s followed by a period and space
-export const is_numbered = (line: string): boolean => /^\d+\. /.test(line);
+// any string starting with optional whitespace, then digit/s followed by a period and space
+export const is_numbered = (line: string): boolean => /^\s*\d+\. /.test(line);
 
 export const strip_bullet = (line: string): string => line.slice(2);
 
