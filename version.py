6bcf8bfe4b315d7f1aeb6d61e7e40f62a327a93f
Anders Kaseorg <anders@zulip.com>
2026-04-16 15:14:34 -0700

    install-uv: Upgrade uv from 0.10.9 to 0.11.7.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -48,4 +48,4 @@ API_FEATURE_LEVEL = 490
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (374, 2)  # bumped 2026-04-10 to add bsonstream
+PROVISION_VERSION = (374, 3)  # bumped 2026-04-16 to upgrade uv
