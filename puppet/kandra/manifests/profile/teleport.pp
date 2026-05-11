3419c2ea2dee964ef1deeab7ab996167fe99a203
Alex Vandiver <alexmv@zulip.com>
2026-05-11 16:40:11 -0400

    kandra: Switch Teleport to multiplexed port 443.

--- puppet/kandra/manifests/profile/teleport.pp
+++ puppet/kandra/manifests/profile/teleport.pp
@@ -10,14 +10,8 @@ class kandra::profile::teleport inherits kandra::profile::base {
   }
   kandra::teleport::part { 'server': }
 
-  # https://goteleport.com/docs/admin-guide/#ports
-  # Port 443 is outward-facing, for UI
+  # https://goteleport.com/docs/reference/deployment/networking/#ports-with-tls-routing
   kandra::firewall_allow { 'teleport_server_ui': port => 443 }
-  # Port 3023 is outward-facing, for teleport clients to connect to.
-  kandra::firewall_allow { 'teleport_server_proxy': port => 3023 }
-  # Port 3034 is outward-facing, for teleport servers outside the
-  # cluster to connect back to establish reverse proxies.
-  kandra::firewall_allow { 'teleport_server_reverse': port => 3024 }
   # Port 3025 is inward-facing, for other nodes to look up auth information
   kandra::firewall_allow { 'teleport_server_auth': port => 3025 }
 }
