1635fa5801817331e7026fd93d852384d493479c
Alex Vandiver <alexmv@zulip.com>
2026-05-12 14:29:40 -0400

    kandra: Use to_yaml when writing YAML data.

--- puppet/kandra/manifests/teleport/application.pp
+++ puppet/kandra/manifests/teleport/application.pp
@@ -7,9 +7,23 @@ define kandra::teleport::application (
   $order = '50',
 ) {
   include kandra::teleport::application_top
+  $app_data = [
+    {
+      name        => $name,
+      description => $description,
+      uri         => "http://127.0.0.1:${port}",
+      labels      => {
+        name => $name,
+      },
+    },
+  ]
+
+  # This is appended to puppet/kandra/files/teleport_node.yaml, so
+  # must be indented, and the leading document dashes stripped.
+  $app_yaml = to_yaml($app_data).regsubst(/\A---\s*\n/, '').regsubst(/^/, '    ', 'G')
   concat::fragment { "teleport_app_${name}":
     target  => '/etc/teleport_node.yaml',
     order   => $order,
-    content => template('kandra/teleport_app.yaml.template.erb'),
+    content => $app_yaml,
   }
 }
