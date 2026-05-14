11b2e6d785b65d0bfe063ca79076f655578fc9d0
Alex Vandiver <alexmv@zulip.com>
2026-05-14 03:38:23 +0000

    kandra: Remove explicit return_per_object_metrics setting in rabbitmq.
    We already accomplish this by using the dedicated metrics endpoint that
    returns that variant[^1].
    
    [^1]: https://www.rabbitmq.com/docs/prometheus#per-object-endpoint

--- puppet/kandra/manifests/prometheus/rabbitmq.pp
+++ puppet/kandra/manifests/prometheus/rabbitmq.pp
@@ -10,14 +10,5 @@ class kandra::prometheus::rabbitmq {
     unless  => 'grep -q rabbitmq_prometheus /etc/rabbitmq/enabled_plugins',
     require => Service['rabbitmq-server'],
   }
-  exec { 'enable rabbitmq-prometheus-per-metric':
-    command => "rabbitmqctl eval 'application:set_env(rabbitmq_prometheus, return_per_object_metrics, true).'",
-    unless  => @("EOT"),
-      [ -f /usr/sbin/rabbitmqctl ] &&
-      /usr/sbin/rabbitmqctl eval 'application:get_env(rabbitmq_prometheus, return_per_object_metrics).' \
-        | grep -q true
-      | EOT
-    require => Exec['enable rabbitmq-prometheus'],
-  }
   kandra::firewall_allow { 'rabbitmq': port => '15692' }
 }
