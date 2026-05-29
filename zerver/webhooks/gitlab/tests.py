a26b88da7d61b22c490cd4fad6222558c13ee7ef
sathwikshetty33 <sathwikshetty2k5@gmail.com>
2026-05-29 12:56:42 +0000

    webhooks/gitlab: Add support for task work item events.
    Fixes part of zulip#39170.

--- zerver/webhooks/gitlab/tests.py
+++ zerver/webhooks/gitlab/tests.py
@@ -10,6 +10,7 @@ from zerver.lib.webhooks.git import COMMITS_LIMIT
 from zerver.models.realms import get_realm
 
 INCIDENT_TOPIC_NAME = "Example Project / incident #1 Catalog images failing to load"
+TASK_TOPIC_NAME = "Example Project / task #1 Optimize image loading in catalog"
 
 
 class GitlabHookTests(WebhookTestCase):
@@ -294,6 +295,70 @@ class GitlabHookTests(WebhookTestCase):
         self.check_webhook(
             "confidential_issue_hook__incident_event",
             INCIDENT_TOPIC_NAME,
+            expected_message,
+            custom_payload=payload,
+        )
+
+    def test_task_events(self) -> None:
+        payload = orjson.loads(self.get_body("issue_hook__task_event"))
+        expected_message = "Bo Williams {verb} [task #1](https://gitlab.com/bo-williams/example-project/-/work_items/1)."
+        for action, verb in [("close", "closed"), ("reopen", "reopened"), ("update", "updated")]:
+            payload["object_attributes"]["action"] = action
+            with self.subTest(action=action):
+                self.check_webhook(
+                    "issue_hook__task_event",
+                    TASK_TOPIC_NAME,
+                    expected_message.format(verb=verb),
+                    custom_payload=payload,
+                )
+
+    def test_create_task_with_assignee_event_message(self) -> None:
+        expected_message = "Bo Williams created [task #1](https://gitlab.com/bo-williams/example-project/-/work_items/1) (assigned to Kevin Lin):\n\n``` quote\nImplement lazy loading for images in the catalog to improve load times.\n```"
+        self.check_webhook("issue_hook__task_event", TASK_TOPIC_NAME, expected_message)
+
+    def test_create_task_without_assignee_event_message(self) -> None:
+        payload = orjson.loads(self.get_body("issue_hook__task_event"))
+        payload["object_attributes"]["assignee_id"] = None
+        payload["object_attributes"]["assignee_ids"] = []
+        payload.pop("assignees")
+        expected_message = "Bo Williams created [task #1](https://gitlab.com/bo-williams/example-project/-/work_items/1):\n\n``` quote\nImplement lazy loading for images in the catalog to improve load times.\n```"
+        self.check_webhook(
+            "issue_hook__task_event",
+            TASK_TOPIC_NAME,
+            expected_message,
+            custom_payload=payload,
+        )
+
+    def test_confidential_task_events(self) -> None:
+        payload = orjson.loads(self.get_body("confidential_issue_hook__task_event"))
+        expected_message = "Bo Williams {verb} [task #1](https://gitlab.com/bo-williams/example-project/-/work_items/1)."
+        for action, verb in [("close", "closed"), ("reopen", "reopened"), ("update", "updated")]:
+            payload["object_attributes"]["action"] = action
+            with self.subTest(action=action):
+                self.check_webhook(
+                    "confidential_issue_hook__task_event",
+                    TASK_TOPIC_NAME,
+                    expected_message.format(verb=verb),
+                    custom_payload=payload,
+                )
+
+    def test_create_confidential_task_with_assignee_event_message(self) -> None:
+        expected_message = "Bo Williams created [task #1](https://gitlab.com/bo-williams/example-project/-/work_items/1) (assigned to Kevin Lin):\n\n``` quote\nImplement lazy loading for images in the catalog to improve load times.\n```"
+        self.check_webhook(
+            "confidential_issue_hook__task_event",
+            TASK_TOPIC_NAME,
+            expected_message,
+        )
+
+    def test_create_confidential_task_without_assignee_event_message(self) -> None:
+        payload = orjson.loads(self.get_body("confidential_issue_hook__task_event"))
+        payload["object_attributes"]["assignee_id"] = None
+        payload["object_attributes"]["assignee_ids"] = []
+        payload.pop("assignees")
+        expected_message = "Bo Williams created [task #1](https://gitlab.com/bo-williams/example-project/-/work_items/1):\n\n``` quote\nImplement lazy loading for images in the catalog to improve load times.\n```"
+        self.check_webhook(
+            "confidential_issue_hook__task_event",
+            TASK_TOPIC_NAME,
             expected_message,
             custom_payload=payload,
         )
