49c8d1ca37e7d1c32afc00ed4cead7281521d7d1
Pratik Chanda <pratikchanda2000@gmail.com>
2026-06-02 00:18:38 +0530

    narrow_banner: Tighten `NarrowBannerData` type.
    Change `NarrowBannerData` to include union of `title` and
    `title_html`, but not both.
    
    The empty-feed notice template renders only one of these fields, so
    making them mutually exclusive better reflects the expected data shape.
    
    We have a preference on showing `title_html` and as such we remove
    the `title` field for empty mention banner.

--- web/src/narrow_error.ts
+++ web/src/narrow_error.ts
@@ -11,23 +11,13 @@ export type SearchData = {
 };
 
 export type NarrowBannerData = {
-    title: string;
-    title_html?: string;
     html?: string;
     search_data?: SearchData;
-};
+} & ({title: string} | {title_html: string});
 
 export function narrow_error(narrow_banner_data: NarrowBannerData): string {
-    const title = narrow_banner_data.title;
-    const title_html = narrow_banner_data.title_html;
-    const notice_html = narrow_banner_data.html;
-    const search_data = narrow_banner_data.search_data;
-
-    const empty_feed_notice = render_empty_feed_notice({
-        title,
-        title_html,
-        notice_html,
-        search_data,
+    return render_empty_feed_notice({
+        ...narrow_banner_data,
+        notice_html: narrow_banner_data.html,
     });
-    return empty_feed_notice;
 }
