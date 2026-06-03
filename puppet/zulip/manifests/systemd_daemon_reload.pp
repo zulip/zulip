06d416d27b3ea9763ba6653d7434ebb52c406b08
Alex Vandiver <alexmv@zulip.com>
2026-06-03 03:46:09 +0000

    puppet: Skip systemd daemon-reload when systemd is not booted.
    The guard only checked that the systemctl binary existed, which is
    still true in containers with systemd installed but not running as PID
    1 (e.g. in Docker containers in CI); "systemctl daemon-reload" then
    fails, aborting the puppet run.
    
    Check for /run/systemd/system instead, which only exists when
    systemd is the running init system.
    
    (cherry picked from commit dbe4330fda51a0fd0c3d75728072440b127e46a9)

--- puppet/zulip/manifests/systemd_daemon_reload.pp
+++ puppet/zulip/manifests/systemd_daemon_reload.pp
@@ -1,6 +1,9 @@
 class zulip::systemd_daemon_reload {
   exec { 'reload systemd':
-    command     => 'sh -c "! command -v systemctl > /dev/null || systemctl daemon-reload"',
+    # /run/systemd/system only exists if systemd is the running init
+    # system (see sd_booted(3)), as opposed to merely installed, as may
+    # be the case in containers.
+    command     => 'sh -c "! test -d /run/systemd/system || systemctl daemon-reload"',
     refreshonly => true,
   }
 }
