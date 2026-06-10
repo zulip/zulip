7d28f79db910b0a337f76c781cabe76f51aab9ab
Agnish Bauri <sakshiseksy@gmail.com>
2026-06-10 18:44:48 +0530

    test_users: Correct stale comment in mirror-dummy reactivation test.
    The comment said "deactivate" but the test POSTs to /reactivate and
    asserts the activation error, so correct it to match.

--- zerver/tests/test_users.py
+++ zerver/tests/test_users.py
@@ -2216,7 +2216,7 @@ class ActivateTest(ZulipTestCase):
         desdemona.is_mirror_dummy = True
         desdemona.save(update_fields=["is_mirror_dummy"])
 
-        # Cannot deactivate a user which is marked as "mirror dummy" from importing
+        # Cannot reactivate a user which is marked as "mirror dummy" from importing
         result = self.client_post(f"/json/users/{desdemona.id}/reactivate")
         self.assert_json_error(
             result, "Cannot activate a placeholder account; ask the user to sign up, instead."
