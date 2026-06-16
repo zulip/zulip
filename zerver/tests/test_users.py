09d3b055a17c832ccc90154b9fd32e87db6e01a9
bedo <bedokhaled66@gmail.com>
2026-06-16 19:01:15 +0300

    tests: Add performance tests when limited guest sends DM.
    pre-commit.
    
    Add 2 new tests and update existing test to document the current
    query count when a guest with limited user access
    sends a personal/group DM.
    
    Add TODOs for the optimization implemented in next commits.

--- zerver/tests/test_users.py
+++ zerver/tests/test_users.py
@@ -3784,6 +3784,26 @@ class GetProfileTest(ZulipTestCase):
         # no personal messages history between desdemona and polonius
         self.assertFalse(check_can_access_user(desdemona, polonius))
 
+    def test_check_can_access_user_for_a_guest_with_themself(self) -> None:
+        """
+        Test that the behaviour is the same and record query count
+        for both cases, a normal guest and a guest with limited user access,
+        when calling check_can_access_user() for a user on themself.
+        """
+        # A guest is not limited by default.
+        polonius = self.example_user("polonius")
+        with self.assert_database_query_count(1):
+            self.assertTrue(check_can_access_user(polonius, polonius))
+
+        # Give guests limited user access.
+        self.set_up_db_for_testing_user_access()
+        polonius = self.example_user("polonius")
+
+        # TODO: When guest has limited user access,
+        # we execute an extra query; we should avoid this.
+        with self.assert_database_query_count(2):
+            self.assertTrue(check_can_access_user(polonius, polonius))
+
     def test_get_users_involved_in_dms_excludes_deactivated_users(self) -> None:
         hamlet = self.example_user("hamlet")
         othello = self.example_user("othello")
