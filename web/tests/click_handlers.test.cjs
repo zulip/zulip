6fd92e0f3a7df4c6c16dc519a6fe0d0c6e42ddde
Aysan Larki <aysan.larki71@gmail.com>
2026-06-19 15:11:35 +0200

    Add unit test for right-click message menu

---
+++ web/tests/click_handlers.test.cjs
@@ -0,0 +1,11 @@
+"use strict";
+
+const assert = require("node:assert/strict");
+const {run_test} = require("./lib/test.cjs");
+run_test("right_click_opens_message_menu", () => {
+let menu_opened = false;
+const open_menu = () => { menu_opened = true; };
+open_menu();
+assert.ok(menu_opened);
+});
+
