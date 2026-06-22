4b8e7735751082d9d262e4088ae7640840d025ad
sathwikshetty33 <sathwikshetty2k5@gmail.com>
2026-06-22 05:32:21 +0000

    webhooks/gong: Add include_public_comments URL option.
    Co-Authored-By: Niloth P <20315308+Niloth-p@users.noreply.github.com>

--- zerver/lib/integrations.py
+++ zerver/lib/integrations.py
@@ -738,6 +738,11 @@ INCOMING_WEBHOOK_INTEGRATIONS: list[IncomingWebhookIntegration] = [
                 label="Include participant contact information (requires participant names)",
                 input_type="checkbox_enabled",
             ),
+            WebhookUrlOption(
+                "include_public_comments",
+                label="Include public comments in the notifications",
+                input_type="checkbox",
+            ),
         ],
     ),
     IncomingWebhookIntegration(
