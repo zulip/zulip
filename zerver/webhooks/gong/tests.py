d0c040ac8a03d1a6cd76c0fe7d4c55a67132bd60
sathwikshetty33 <sathwikshetty2k5@gmail.com>
2026-06-22 03:48:47 +0000

    webhooks/gong: Add include_trackers URL option.
    Co-Authored-By: Niloth P <20315308+Niloth-p@users.noreply.github.com>

--- zerver/webhooks/gong/tests.py
+++ zerver/webhooks/gong/tests.py
@@ -129,6 +129,16 @@ class GongHookTests(WebhookTestCase):
             content=f"{intro}\n\n{self.PARTICIPANTS_SECTION}\n\n{self.TRACKERS_SECTION}",
         )
 
+    def test_call_with_top_trackers_disabled(self) -> None:
+        self.url = self.build_webhook_url(include_trackers="false")
+        intro = self.MESSAGE_INTRO.format(title=self.CALL_TITLE)
+        self.check_webhook(
+            "call_completed",
+            self.EXPECTED_TOPIC,
+            f"{intro}\n\n{self.PARTICIPANTS_SECTION}\n\n{self.TOPICS_SECTION}",
+            content_type="application/json",
+        )
+
     def test_get_participants(self) -> None:
         name = KEVIN_NAME
         title = "Customer Success Manager"
