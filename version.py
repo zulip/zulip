7d85fc15464479fbb9c0c9f953e9b3532899a60e
Mateusz Mandera <mateusz.mandera@zulip.com>
2026-06-26 22:13:53 +0800

    Release Zulip Server 12.1.

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 import os
 
-ZULIP_VERSION = "12.0+git"
+ZULIP_VERSION = "12.1"
 
 # Add information on number of commits and commit hash to version, if available
 ZULIP_VERSION_WITHOUT_COMMIT = ZULIP_VERSION
@@ -15,7 +15,7 @@ ZULIP_VERSION = lines.pop(0).strip()
 ZULIP_MERGE_BASE = lines.pop(0).strip()
 
 LATEST_MAJOR_VERSION = "12.0"
-LATEST_RELEASE_VERSION = "12.0"
+LATEST_RELEASE_VERSION = "12.1"
 LATEST_RELEASE_ANNOUNCEMENT = "https://blog.zulip.com/zulip-server-12-0"
 
 # Versions of the desktop app below DESKTOP_MINIMUM_VERSION will be
