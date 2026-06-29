c6ad5ffc3fdff784a9c185c528a4c30ab4ec7c4f
John Seekins <john@robot-house.us>
2026-06-28 21:25:42 -0600

    migrations: Fix silly typos
    Some small changes to address mistakes in handling `kwargs`
    
    Signed-off-by: John Seekins <john@robot-house.us>

--- zerver/migrations/__init__.py
+++ zerver/migrations/__init__.py
@@ -5,12 +5,12 @@ from django.conf import settings
 
 def add_index(**kwargs):
     if settings.MIGRATE_WITH_CONCURRENT_INDICES:
-        return AddIndexConcurrently(kwargs)
-    else
-        return migrations.Index(kwargs)
+        return AddIndexConcurrently(**kwargs)
+    else:
+        return migrations.AddIndex(**kwargs)
 
 def remove_index(**kwargs):
     if settings.MIGRATE_WITH_CONCURRENT_INDICES:
-        return RemoveIndexConcurrently(kwargs)
-    else
-        return migrations.RemoveIndex(kwargs)
+        return RemoveIndexConcurrently(**kwargs)
+    else:
+        return migrations.RemoveIndex(**kwargs)
