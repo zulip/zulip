0667dad012ccf582d2ca79235528e0f867c29fe0
isatyamks <isatyamks@gmail.com>
2026-07-01 05:16:31 +0000

    refactor: Rename split_text_on_comma to allow_comma_in_item_text.
    The configuration property `split_text_on_comma` was somewhat confusing,
    as it was set to `false` in `email_pill.ts` and `search_pill.ts`, which
    could imply that those pills don't split text on commas at all. However,
    emails can legitimately contain commas in their display names (e.g.
    "Smith, John <abc@example.com>").
    
    Renaming this property to `allow_comma_in_item_text` makes the intent
    clearer. If `allow_comma_in_item_text` is `false` (the default), a comma
    is unconditionally treated as a delimiter. If `true`, a comma is allowed
    as part of the item text (e.g. for parsing complex emails), and we only
    split on it if necessary.

--- web/src/email_pill.ts
+++ web/src/email_pill.ts
@@ -91,7 +91,7 @@ export function create_pills(
         create_item_from_text: create_item_from_email,
         get_text_from_item: get_email_from_item,
         get_display_value_from_item: get_email_from_item,
-        split_text_on_comma: false,
+        allow_comma_in_item_text: true,
         split_text_to_form_pills: split_text_to_form_email_pills,
         generate_pill_html,
     });
