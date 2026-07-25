9e5e7d875eae4cbf81d6e3aa0a2564094ed2a458
Anders Kaseorg <anders@zulip.com>
2026-07-25 14:44:42 -0700

    requirements: Upgrade Python requirements.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>
    (cherry picked from commit 9bc7bac821b333e5b2d9f7f077c59792b1188b68)

--- version.py
+++ version.py
@@ -48,4 +48,4 @@ API_FEATURE_LEVEL = 500
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (380, 1)  # bumped 2026-07-25 to upgrade uv
+PROVISION_VERSION = (381, 0)  # bumped 2026-07-25 to upgrade Python requirements
