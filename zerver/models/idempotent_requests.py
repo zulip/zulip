d4247c6026c622a80a02f5d6376902dc7814fe7c
Bedo Khaled <bedokhaled66@gmail.com>
2026-08-07 03:04:54 +0300

    idempotency: Add idempotency class model and migration.
    Idempotency backend logic is implemented in next commit.

---
+++ zerver/models/idempotent_requests.py
@@ -0,0 +1,39 @@
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db import models
+from django.db.models.functions import Now
+from django.utils.timezone import now as timezone_now
+
+from zerver.models.realms import Realm
+from zerver.models.users import UserProfile
+
+
+class IdempotentRequest(models.Model):
+    """Ensure idempotency for non-idempotent HTTP requests (e.g., POST).
+
+    Currently, only used for message sending, but in future it should be applied to
+    any non-idempotent request having the possibility of being wrongly replayed.
+    """
+
+    realm = models.ForeignKey(Realm, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    # Only client-provided.
+    idempotency_key = models.UUIDField()
+
+    # We use both Now() and timezone_now to ensure timestamp is auto created
+    # when inserting via raw SQL and Django, respectively.
+    timestamp = models.DateTimeField(db_default=Now(), default=timezone_now, db_index=True)
+
+    # The cache of successful response or the json error.
+    cached_result = models.JSONField(default=None, null=True, encoder=DjangoJSONEncoder)
+
+    # None if the work has not been attempted.
+    succeeded = models.BooleanField(default=None, null=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["realm", "user", "idempotency_key"],
+                name="unique_realm_user_idempotency_key",
+            ),
+        ]
