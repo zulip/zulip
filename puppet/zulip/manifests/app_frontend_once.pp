76389908fed62f76ca49dc8c4cebd73db310d4c6
Bedo Khaled <bedokhaled66@gmail.com>
2026-08-07 03:22:25 +0300

    idempotency: Add cron job to delete expired rows.
    New delete_old_idempotency_rows management command that runs
    daily to clear out expired rows from IdempotentRequest table.

--- puppet/zulip/manifests/app_frontend_once.pp
+++ puppet/zulip/manifests/app_frontend_once.pp
@@ -53,6 +53,11 @@ class zulip::app_frontend_once {
     hour   => '6',
     minute => '0',
   }
+  zulip::cron { 'delete-old-idempotency-rows':
+    hour   => '7',
+    minute => '0',
+    manage => 'delete_old_idempotency_rows',
+  }
   zulip::cron { 'send-digest-emails':
     hour   => '18',
     minute => '0',
