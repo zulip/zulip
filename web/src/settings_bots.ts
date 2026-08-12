005adc8a0e8405b56e95dd6bc8031dcd1ce5f71d
PieterCK <kwok.pieter@zulip.com>
2026-08-12 21:17:01 +0700

    bots: Make bot `Service` triggers configurable.
    This adds a new field to the Service table called
    "triggers", which specifes a list of events that will trigger a bot's
    specific function.
    
    This also splits out existing triggers (direct mention and direct
    message) into their own trigger types.
    
    All existing message-triggered bots (outgoing webhook bot and embedded
    bot) should have ["all_direct_mentions", "dms_received"] as their
    `Service.triggers` field. This will be fixed by a custom migration
    backfill in the following commit.

--- web/src/settings_bots.ts
+++ web/src/settings_bots.ts
@@ -39,6 +39,7 @@ import type {HTMLSelectOneElement} from "./types.ts";
 import * as ui_report from "./ui_report.ts";
 import type {UploadWidget} from "./upload_widget.ts";
 import * as user_deactivation_ui from "./user_deactivation_ui.ts";
+import {show_bot_trigger_options} from "./user_profile.ts";
 import * as user_sort from "./user_sort.ts";
 import * as util from "./util.ts";
 
@@ -285,6 +286,12 @@ export function add_a_new_bot(): void {
         const service_name = $<HTMLSelectOneElement>(
             "select:not([multiple])#select_service_name",
         ).val()!;
+        const selected_bot_triggers = $("#bot_triggers_checkbox_container .setting-widget:checked")
+            .map(function () {
+                return this.id;
+            })
+            .get();
+
         const formData = new FormData();
         assert(csrf_token !== undefined);
         formData.append("csrfmiddlewaretoken", csrf_token);
@@ -296,6 +303,7 @@ export function add_a_new_bot(): void {
             case OUTGOING_WEBHOOK_BOT_TYPE: {
                 formData.append("payload_url", JSON.stringify(payload_url));
                 formData.append("interface_type", interface_type);
+                formData.append("service_triggers", JSON.stringify(selected_bot_triggers));
                 break;
             }
             case EMBEDDED_BOT_TYPE: {
@@ -309,6 +317,7 @@ export function add_a_new_bot(): void {
                     config_data[key] = value;
                 });
                 formData.append("config_data", JSON.stringify(config_data));
+                formData.append("service_triggers", JSON.stringify(selected_bot_triggers));
                 break;
             }
         }
@@ -336,6 +345,9 @@ export function add_a_new_bot(): void {
     }
 
     function set_up_form_fields(): void {
+        const $bot_triggers_container = $("#bot_triggers_checkbox_container");
+        const $bot_triggers_field = $("#bot_triggers_field");
+        $bot_triggers_field.hide();
         $("#create_bot_type").val(INCOMING_WEBHOOK_BOT_TYPE_INT);
         $("#payload_url_inputbox").hide();
         $("#create_payload_url").val("");
@@ -361,11 +373,14 @@ export function add_a_new_bot(): void {
             $("#config_inputbox").hide();
 
             $("#payload_url_inputbox").hide();
+            $bot_triggers_field.hide();
             $("#create_payload_url").removeClass("required");
             switch (bot_type) {
                 case OUTGOING_WEBHOOK_BOT_TYPE: {
                     $("#payload_url_inputbox").show();
                     $("#create_payload_url").addClass("required");
+                    $bot_triggers_field.show();
+                    show_bot_trigger_options($bot_triggers_container);
                     break;
                 }
                 case EMBEDDED_BOT_TYPE: {
@@ -373,6 +388,8 @@ export function add_a_new_bot(): void {
                     $("#select_service_name").addClass("required");
                     $("#select_service_name").trigger("change");
                     $("#config_inputbox").show();
+                    $bot_triggers_field.show();
+                    show_bot_trigger_options($bot_triggers_container);
                     break;
                 }
             }
