77cb0de6fc687b45cf8fdef30e44e6e7ac1f1c7b
Anders Kaseorg <anders@zulip.com>
2026-08-19 15:05:35 -0700

    install-uv: Upgrade uv from 0.11.32 to 0.12.5.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- version.py
+++ version.py
@@ -48,4 +48,4 @@ API_FEATURE_LEVEL = 509
 #   historical commits sharing the same major version, in which case a
 #   minor version bump suffices.
 
-PROVISION_VERSION = (388, 0)  # bumped 2026-08-19 to upgrade JavaScript dependencies
+PROVISION_VERSION = (388, 1)  # bumped 2026-08-19 to upgrade uv
