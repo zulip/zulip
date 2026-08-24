f1f6a01cc62cd223d33675a56478ff5c4abf0544
Harshul Adwani <harshuladwani@gmail.com>
2026-08-24 11:09:20 +0000

    markdown: Extract LI_RE from MarkdownListPreprocessor.
    This is a prep commit to add a new preprocessor that can use the
    same regex pattern.

--- zerver/lib/markdown/__init__.py
+++ zerver/lib/markdown/__init__.py
@@ -1533,6 +1533,17 @@ class Fence:
     is_code: bool
 
 
+LIST_ITEM_REGEX = re.compile(
+    r"""
+    ^[ ]*        # Start with zero or more spaces
+    ([*+-]|\d\.) # A bullet marker (*, +, or -) or a digit followed by "."
+    [ ]+         # Followed by one or more spaces
+    (.*)         # The list content, which could be empty
+    """,
+    re.VERBOSE,
+)
+
+
 class MarkdownListPreprocessor(markdown.preprocessors.Preprocessor):
     """Allows list blocks that come directly after another block
     to be rendered as a list.
@@ -1540,8 +1551,6 @@ class MarkdownListPreprocessor(markdown.preprocessors.Preprocessor):
     Detects paragraphs that have a matching list item that comes
     directly after a line of text, and inserts a newline between
     to satisfy Markdown"""
-
-    LI_RE = re.compile(r"^[ ]*([*+-]|\d\.)[ ]+(.*)")
 
     @override
     def run(self, lines: list[str]) -> list[str]:
@@ -1573,8 +1582,8 @@ class MarkdownListPreprocessor(markdown.preprocessors.Preprocessor):
             # If we're not in a fenced block and we detect an upcoming list
             # hanging off any block (including a list of another type), add
             # a newline.
-            li1 = self.LI_RE.match(lines[i])
-            li2 = self.LI_RE.match(lines[i + 1])
+            li1 = LIST_ITEM_REGEX.match(lines[i])
+            li2 = LIST_ITEM_REGEX.match(lines[i + 1])
             if (
                 not in_code_fence
                 and lines[i]
