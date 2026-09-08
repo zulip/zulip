9629decdd57d0170b06cb7f6c1555fc5104502b5
Niloth P <20315308+Niloth-p@users.noreply.github.com>
2026-09-08 05:31:44 +0000

    users: Split bot config validation by bot type.
    Co-authored-by: Sathwik Shetty <sathwikshetty2k5@gmail.com>

--- zerver/views/users.py
+++ zerver/views/users.py
@@ -87,8 +87,9 @@ from zerver.lib.users import (
     check_can_access_user,
     check_can_create_bot,
     check_full_name,
-    check_valid_bot_config,
     check_valid_bot_type,
+    check_valid_embedded_bot_config,
+    check_valid_incoming_webhook_bot_config,
     check_valid_interface_type,
     get_users_for_api,
     max_message_id_for_user,
@@ -716,8 +717,10 @@ def add_bot_backend(
             user_profile, default_events_register_stream_name
         )
 
-    if bot_type in (UserProfile.INCOMING_WEBHOOK_BOT, UserProfile.EMBEDDED_BOT) and service_name:
-        check_valid_bot_config(bot_type, service_name, config_data)
+    if bot_type == UserProfile.INCOMING_WEBHOOK_BOT and service_name:
+        check_valid_incoming_webhook_bot_config(service_name, config_data)
+    elif bot_type == UserProfile.EMBEDDED_BOT and service_name:
+        check_valid_embedded_bot_config(service_name, config_data)
 
     bot_profile = do_create_user(
         email=email,
