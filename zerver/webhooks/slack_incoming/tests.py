9c2986025402eba573c46b7b34b37866672f9206
piyushrajpandit <piyush06raj04@gmail.com>
2026-09-23 19:42:46 +0530

    slack_incoming: Add support for message_changed events.

--- zerver/webhooks/slack_incoming/tests.py
+++ zerver/webhooks/slack_incoming/tests.py
@@ -385,3 +385,12 @@ Sample footer.
 
     def test_broken_image(self) -> None:
         self.check_webhook("broken_image", "", "Some image here")
+
+    def test_message_changed(self) -> None:
+        expected_topic_name = "general"
+        expected_message = "Hello world from edited Slack message!"
+        self.check_webhook(
+            "message_changed",
+            expected_topic_name,
+            expected_message,
+        )
