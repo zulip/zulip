e9ffac376e68c54850138b7aff4a3f73a79fa7fe
Anders Kaseorg <anders@zulip.com>
2026-09-25 14:34:04 -0700

    tests: Fix i18n wrapper.
    Signed-off-by: Anders Kaseorg <anders@zulip.com>

--- web/tests/lib/i18n.cjs
+++ web/tests/lib/i18n.cjs
@@ -7,8 +7,8 @@ const i18n = zrequire("i18n");
 Object.assign(exports, i18n);
 
 exports.$t = (descriptor, values, opts) =>
-    i18n.$t({id: descriptor.defaultMessage, descriptor}, values, opts);
+    i18n.$t({id: descriptor.defaultMessage, ...descriptor}, values, opts);
 
 /* istanbul ignore next */
 exports.$t_html = (descriptor, values, opts) =>
-    i18n.$t_html({id: descriptor.defaultMessage, descriptor}, values, opts);
+    i18n.$t_html({id: descriptor.defaultMessage, ...descriptor}, values, opts);
